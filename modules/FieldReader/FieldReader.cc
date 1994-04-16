51bb76f04fffc52f0f731e442741667a15f7105d
Steven G. Parker <sparker@sci.utah.edu>
1994-04-16 22:43:19 +0000

    Initial module implementation

---
+++ modules/FieldReader/FieldReader.cc
@@ -0,0 +1,62 @@
+
+/*
+ *  FieldReader.cc:  The first module!
+ *
+ *  Written by:
+ *   Steven G. Parker
+ *   Department of Computer Science
+ *   University of Utah
+ *   March 1994
+ *
+ *  Copyright (C) 1994 SCI Group
+ */
+
+#include <FieldReader/FieldReader.h>
+#include <Field3D.h>
+#include <Field3DPort.h>
+#include <MUI.h>
+#include <NotFinished.h>
+#include <iostream.h>
+#include <fstream.h>
+
+FieldReader::FieldReader()
+: UserModule("FieldReader", Source)
+{
+    // Create the output data handle and port
+    outfield=new Field3DOPort(this, "Field", Field3DIPort::Atomic);
+    add_oport(outfield);
+
+    add_ui(new MUI_file_selection("IsoContour value", &filename));
+}
+
+FieldReader::FieldReader(const FieldReader& copy, int deep)
+: UserModule(copy, deep)
+{
+    NOT_FINISHED("FieldReader::FieldReader");
+}
+
+FieldReader::~FieldReader()
+{
+}
+
+Module* make_FieldReader()
+{
+    return new FieldReader;
+}
+
+Module* FieldReader::clone(int deep)
+{
+    return new FieldReader(*this, deep);
+}
+
+void FieldReader::execute()
+{
+    Piostream* stream=auto_istream(filename);
+    if(!stream)
+	return; // Can't open file...
+    Field3DHandle field(new Field3D);
+    // Read the file...
+    field->io(*stream);
+    delete stream;
+    outfield->send_field(field);
+}
---
+++ modules/FieldReader/FieldReader.h
@@ -0,0 +1,32 @@
+
+/*
+ *  FieldReader.h: Read a field from a file...
+ *
+ *  Written by:
+ *   Steven G. Parker
+ *   Department of Computer Science
+ *   University of Utah
+ *   March 1994
+ *
+ *  Copyright (C) 1994 SCI Group
+ */
+
+#ifndef SCI_project_module_FieldReader_h
+#define SCI_project_module_FieldReader_h
+
+#include <UserModule.h>
+class Field3DHandle;
+class Field3DOPort;
+
+class FieldReader : public UserModule {
+    Field3DOPort* outfield;
+    clString filename;
+public:
+    FieldReader();
+    FieldReader(const FieldReader&, int deep);
+    virtual ~FieldReader();
+    virtual Module* clone(int deep);
+    virtual void execute();
+};
+
+#endif
