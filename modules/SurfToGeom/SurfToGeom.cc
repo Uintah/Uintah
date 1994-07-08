7b7a25a450ba6eca306a49a61d4fe4b16004bec3
David Weinstein <dweinste@sci.utah.edu>
1994-07-08 06:17:03 +0000

    Module to convert a surface to geometry

---
+++ modules/SurfToGeom/SurfToGeom.cc
@@ -0,0 +1,66 @@
+
+/*
+ *  SurfToGeom.cc:  Convert a surface into geoemtry
+ *
+ *  Written by:
+ *   David Weinstein
+ *   Department of Computer Science
+ *   University of Utah
+ *   July 1994
+ *
+ *  Copyright (C) 1994 SCI Group
+ */
+
+#include <SurfToGeom/SurfToGeom.h>
+#include <Surface.h>
+#include <SurfacePort.h>
+#include <Geom.h>
+#include <GeometryPort.h>
+#include <ModuleList.h>
+#include <MUI.h>
+#include <NotFinished.h>
+#include <iostream.h>
+#include <fstream.h>
+
+static Module* make_SurfToGeom()
+{
+    return new SurfToGeom;
+}
+
+static RegisterModule db1("Surface", "Surface to Geometry", make_SurfToGeom);
+static RegisterModule db2("Visualization", "Surface to Geometry", 
+			  make_SurfToGeom);
+
+SurfToGeom::SurfToGeom()
+: UserModule("SurfToGeom", Filter)
+{
+    // Create the input port
+    isurface=new SurfaceIPort(this, "Surface", SurfaceIPort::Atomic);
+    add_iport(isurface);
+    ogeom=new GeometryOPort(this, "Geometry", GeometryOPort::Atomic);
+    add_oport(ogeom);
+}
+
+SurfToGeom::SurfToGeom(const SurfToGeom&copy, int deep)
+: UserModule(copy, deep)
+{
+    NOT_FINISHED("SurfToGeom::SurfToGeom");
+}
+
+SurfToGeom::~SurfToGeom()
+{
+}
+
+Module* SurfToGeom::clone(int deep)
+{
+    return new SurfToGeom(*this, deep);
+}
+
+void SurfToGeom::execute()
+{
+    SurfaceHandle surf;
+    if (!isurface->get(surf))
+	return;
+    ObjGroup *group=surf->getGeomFromSurface();
+    ogeom->send(group);
+}
