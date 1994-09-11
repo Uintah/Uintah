c6ccd46cd5758e22fcae76ad8cb09b8d7acfc064
Steven G. Parker <sparker@sci.utah.edu>
1994-09-11 20:53:44 +0000

    Added TODO file to keep track of needed bug fixes tmpl_hack helps C++ with template instantations - bug workarounds

--- tmpl_hack.cc
+++ tmpl_hack.cc
@@ -1,4 +1,33 @@
 
 #include <Classlib/Queue.h>
 
-Queue<int> hack1;
+typedef Queue<int> hack1;
+
+#include <Classlib/Array3.h>
+#include <Geometry/Vector.h>
+typedef Array3<Vector> hack2;
+
+#include <Datatypes/Mesh.h>
+typedef MeshHandle hack3;
+
+#include <Datatypes/TriSurface.h>
+#include <Classlib/Array1.h>
+typedef Array1<TSElement*> hack4;
+
+#include <Datatypes/ContourSet.h>
+typedef ContourSetHandle hack5;
+
+#include <Datatypes/ScalarField.h>
+typedef ScalarFieldHandle hack6;
+
+#include <Datatypes/VectorField.h>
+typedef VectorFieldHandle hack7;
+
+#include <Datatypes/GeometryPort.h>
+typedef Mailbox<GeomReply> hack8;
+
+#include <Datatypes/MeshPort.h>
+typedef MeshIPort hack9;
+
+#include <Datatypes/Matrix.h>
+typedef MatrixHandle hack10;
