acde56c8cf941523cea87964fc6c57b983362923
Steven G. Parker <sparker@sci.utah.edu>
1994-09-18 07:19:45 +0000

    Removed default values from PointVector Use TaskManager::exit_all instead of exit()

--- TCL/PointVector.tcl
+++ TCL/PointVector.tcl
@@ -6,19 +6,19 @@ proc makePoint {w title name command} {
     fscale $w.x -orient horizontal -variable x,$name \
 	    -from -10 -to 10 -label "X:" \
 	    -showvalue true -tickinterval 5 \
-	    -activeforeground SteelBlue2 -resolution .1 -digits 3 \
+	    -activeforeground SteelBlue2 -resolution .0001 -digits 3 \
 	    -command $command
     pack $w.x -side top -expand yes -fill x
     fscale $w.y -orient horizontal -variable y,$name \
 	    -from -10 -to 10 -label "Y:" \
 	    -showvalue true -tickinterval 5 \
-	    -activeforeground SteelBlue2 -resolution .1 -digits 3 \
+	    -activeforeground SteelBlue2 -resolution .0001 -digits 3 \
 	    -command $command
     pack $w.y -side top -expand yes -fill x
     fscale $w.z -orient horizontal -variable z,$name \
 	    -from -10 -to 10 -label "Z:" \
 	    -showvalue true -tickinterval 5 \
-	    -activeforeground SteelBlue2 -resolution .1 -digits 3 \
+	    -activeforeground SteelBlue2 -resolution .0001 -digits 3 \
 	    -command $command
     pack $w.z -side top -expand yes -fill x
 }
@@ -52,27 +52,24 @@ proc makeNormalVector {w title name command} {
     label $w.label -text $title
     pack $w.label -side top
     global x,$name y,$name z,$name
-    set x,$name 0
-    set y,$name 0
-    set z,$name 1
     global unv_update
     set unv_update 0
     fscale $w.x -orient horizontal -variable x,$name \
 	    -from -1 -to 1 -label "X:" \
 	    -showvalue true -tickinterval 1 \
-	    -activeforeground SteelBlue2 -resolution .01 -digits 3 \
+	    -activeforeground SteelBlue2 -resolution .00001 -digits 3 \
 	    -command "updateNormalVector x,$name y,$name z,$name \"$command\" "
     pack $w.x -side top -expand yes -fill x
     fscale $w.y -orient horizontal -variable y,$name \
 	    -from -1 -to 1 -label "Y:" \
 	    -showvalue true -tickinterval 1 \
-	    -activeforeground SteelBlue2 -resolution .01 -digits 3 \
+	    -activeforeground SteelBlue2 -resolution .00001 -digits 3 \
 	    -command "updateNormalVector y,$name z,$name x,$name \"$command\" "
     pack $w.y -side top -expand yes -fill x
     fscale $w.z -orient horizontal -variable z,$name \
 	    -from -1 -to 1 -label "Z:" \
 	    -showvalue true -tickinterval 1 \
-	    -activeforeground SteelBlue2 -resolution .01 -digits 3 \
+	    -activeforeground SteelBlue2 -resolution .00001 -digits 3 \
 	    -command "updateNormalVector z,$name x,$name y,$name \"$command\" "
     pack $w.z -side top -expand yes -fill x
 }
