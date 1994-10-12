7f90c108faf5029385fc86f63291839506ef94ce
Steven G. Parker <sparker@sci.utah.edu>
1994-10-12 21:20:28 +0000

    Moved Debug.h to Classlib

--- Constraints/BaseConstraint.cc
+++ Constraints/BaseConstraint.cc
@@ -15,7 +15,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <Constraints/BaseConstraint.h>
-#include <TCL/Debug.h>
+#include <Classlib/Debug.h>
 
 //static DebugSwitch bc_debug("BaseConstraint", "print");
 
