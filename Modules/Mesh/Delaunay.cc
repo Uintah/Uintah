3a9265105e913fe285ff738f5d12ca47ffc20427
Steven G. Parker <sparker@sci.utah.edu>
1994-10-14 22:17:38 +0000

    Implemented half of triangulation algorithm

--- Modules/Mesh/Delaunay.cc
+++ Modules/Mesh/Delaunay.cc
@@ -62,12 +62,13 @@ Module* Delaunay::clone(int deep)
 
 void Delaunay::execute()
 {
-    MeshHandle mesh;
-    if(!iport->get(mesh))
+    MeshHandle mesh_handle;
+    if(!iport->get(mesh_handle))
 	return;
 
     // Get our own copy of the mesh...
-    mesh.detach();
+    mesh_handle.detach();
+    Mesh* mesh=mesh_handle.get_rep();
     mesh->elems.remove_all();
 
     int nnodes=mesh->nodes.size();
@@ -82,15 +83,53 @@ void Delaunay::execute()
     bbox.extend(bbox.max()-Vector(epsilon, epsilon, epsilon));
     bbox.extend(bbox.min()+Vector(epsilon, epsilon, epsilon));
 
-    // Make the initial mesh with 
-    int bbox_node_start=nnodes;
-    Point bmin(bbox.min());
-    Point bmax(bbox.max());
-    for(i=0;i<2;i++)
-	for(int j=0;j<2;j++)
-	    for(int k=0;k<2;k++)
-		mesh->nodes.add(new Node(Point(i?bmax.x():bmin.x(),
-					       j?bmax.y():bmin.y(),
-					       k?bmax.z():bmin.z())));
-    NOT_FINISHED("Delaunay::execute");
+    // Make the bbox square...
+    Point center(bbox.center());
+    double le=bbox.longest_edge();
+    Vector diag(le, le, le);
+    Point bmin(center-diag/2.);
+    Point bmax(center+diag/2.);
+
+    // Make the initial mesh with a tetra which encloses the bounding
+    // box.  The first point is at the minimum point.  The other 3
+    // have one of the coordinates at bmin+diagonal*3.
+    mesh->nodes.add(new Node(bmin));
+    mesh->nodes.add(new Node(bmin+Vector(le*3, 0, 0)));
+    mesh->nodes.add(new Node(bmin+Vector(0, le*3, 0)));
+    mesh->nodes.add(new Node(bmin+Vector(0, 0, le*3)));
+
+    Element* el=new Element(mesh, 0, 1, 2, 3);
+    el->orient();
+    mesh->elems.add(el);
+
+    for(i=0;i<nnodes;i++){
+	// Add this node...
+	Point p(mesh->nodes[i]->p);
+
+	// Find which element this node is in
+	int in_element;
+	if(!mesh->locate(p, in_element)){
+	    error("Mesher upset - point outside of domain...");
+	    return;
+	}
+
+	Array1<int> to_remove;
+	to_remove.add(in_element);
+
+	// Find it's neighbors...
+	// We might be able to fix this loop to make it
+	// O(N) instead of O(n^2) - use a Queue
+	int i=0;
+	while(i<to_remove.size()){
+	    // See if the neighbor should also be removed...
+#if 0
+	    Element* e=mesh->elems[to_remove[i]];
+	    for(int j=0;j<4;j++){
+		int skip=0;
+		int neighbor=e->faces[j];
+		for(int ii=0;ii<to_remove.size();i++){
+		    if(neighbor==to_remove[ii])
+#endif
+	}
+    }
 }
