d1ed1de3b7748acb7e208ca1da22f77699250ede
Steven G. Parker <sparker@sci.utah.edu>
1994-10-28 00:24:43 +0000

    New types...

--- gnupt/HashTable_all.cc
+++ gnupt/HashTable_all.cc
@@ -6,11 +6,12 @@
 #include <Classlib/HashTable.cc>
 class SceneItem;
 class Persistent;
+class PortInfo;
 
 typedef HashTable<int, int> _dummy2_;
 typedef HashTable<int, SceneItem*> _dummy3_;
 typedef HashTableIter<int, SceneItem*> _dummy4_;
-typedef HashTable<int, _dummy3_*> _dummy5_;
-typedef HashTableIter<int, _dummy3_*> _dummy6_;
+typedef HashTable<int, PortInfo*> _dummy5_;
+typedef HashTableIter<int, PortInfo*> _dummy6_;
 typedef HashTable<Persistent*, int> _dummy7_;
 typedef HashTable<int, Persistent*> _dummy8_;
