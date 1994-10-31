d1f613e910107d6f7af301fee102599ae6d9b9ba
James Purciful <purciful@sci.utah.edu>
1994-10-31 04:49:51 +0000

    Adding multiple widgets; minor changes.

--- Modules/Visualization/FrameTest.tcl
+++ Modules/Visualization/FrameTest.tcl
@@ -12,8 +12,16 @@ proc uiFrameTest {modid} {
 
     button $w.f.button -text GO! -command $n
     pack $w.f.button -in $w.f -side left -padx 2 -pady 2 -anchor w
-    fscale $w.f.slide -label Scale -from 0.0 -to 1000.0 -length 6c -showvalue true \
-	    -orient horizontal -activeforeground SteelBlue2 -resolution .01 \
-	    -digits 3 -variable widget_scale,$w -command $n
+    global widget_scale,$modid
+    set widget_scale,$modid .01
+    fscale $w.f.slide -label Scale -from 0.0 -to 10.0 -length 6c -showvalue true \
+	    -orient horizontal -activeforeground SteelBlue2 -resolution .001 \
+	    -digits 8 -variable widget_scale,$modid -command $n
     pack $w.f.slide -in $w.f -side top -padx 2 -pady 2 -anchor w
+
+    frame $w.f.wids
+    radiobutton $w.f.wids.arrow -text ArrowWidget -variable widget_type,$modid -value 0
+    radiobutton $w.f.wids.frame -text FrameWidget -variable widget_type,$modid -value 1
+    pack $w.f.wids.arrow $w.f.wids.frame -in $w.f.wids -side left -padx 2 -pady 2 -anchor w
+    pack $w.f.wids -in $w.f
 }
