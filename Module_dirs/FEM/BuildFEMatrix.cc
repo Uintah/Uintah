8693bbc4cc74ae2d4b7fd72e79d4a1342a0881cc
Ruth Klepfer <rklepfer@sci.utah.edu>
1994-11-07 22:47:03 +0000

    adding the buildfematrix module

--- Module_dirs/FEM/BuildFEMatrix.cc
+++ Module_dirs/FEM/BuildFEMatrix.cc
@@ -1,11 +1,11 @@
 /*
- *  BuildFEMatrix.cc:  Unfinished modules
+ *  BuildFEMatrix.cc:  Builds the global finite element matrix
  *
  *  Written by:
- *   Steven G. Parker
- *   Department of Computer Science
+ *   Ruth Nicholson Klepfer
+ *   Department of Bioengineering
  *   University of Utah
- *   March 1994
+ *   October 1994
  *
  *  Copyright (C) 1994 SCI Group
  */
@@ -14,11 +14,18 @@
 #include <Dataflow/Module.h>
 #include <Dataflow/ModuleList.h>
 #include <Datatypes/MatrixPort.h>
+#include <Datatypes/Matrix.h>
+#include <Datatypes/DenseMatrix.h>
 #include <Datatypes/MeshPort.h>
+#include <Datatypes/Mesh.h>
 #include <Datatypes/SurfacePort.h>
 #include <Geometry/Point.h>
 
 class BuildFEMatrix : public Module {
+     MeshIPort* inmesh;
+     MatrixOPort * outmatrix;
+     void build_local_matrix(Element*,Matrix&,MeshHandle);
+     void add_lcl_gbl(Matrix&, Matrix&, int, MeshHandle);
 public:
     BuildFEMatrix(const clString& id);
     BuildFEMatrix(const BuildFEMatrix&, int deep);
@@ -32,14 +39,21 @@ static Module* make_BuildFEMatrix(const clString& id)
     return new BuildFEMatrix(id);
 }
 
-static RegisterModule db1("Unfinished", "BuildFEMatrix", make_BuildFEMatrix);
+static RegisterModule db1("Matrix", "BuildFEMatrix", make_BuildFEMatrix);
 
 BuildFEMatrix::BuildFEMatrix(const clString& id)
 : Module("BuildFEMatrix", id, Filter)
 {
-    add_iport(new MeshIPort(this, "Geometry", MeshIPort::Atomic));
+    // Create the input port
+    inmesh = new MeshIPort(this, "Geometry", MeshIPort::Atomic);
+    add_iport(inmesh);
+
     // Create the output port
-    add_oport(new MatrixOPort(this, "Geometry", MatrixIPort::Atomic));
+    outmatrix=new MatrixOPort(this, "Geometry", MatrixIPort::Atomic);
+    add_oport(outmatrix);
+
+    // Ask Dave about why this was different originally
+    // i.e. it was add_iport(new MeshIPort(this,"Geometry",...));
 }
 
 BuildFEMatrix::BuildFEMatrix(const BuildFEMatrix& copy, int deep)
@@ -59,5 +73,131 @@ Module* BuildFEMatrix::clone(int deep)
 
 void BuildFEMatrix::execute()
 {
-    NOT_FINISHED("BuildFEMatrix::execute");
+
+     MeshHandle mesh;
+     if(!inmesh->get(mesh))
+	  return;
+     Matrix* gbl_matrix=new DenseMatrix(mesh->nodes.size(),mesh->nodes.size());
+     DenseMatrix lcl_matrix(3,3);
+
+     for (int i=1; i<=mesh->elems.size(); i++){
+
+	  build_local_matrix(mesh->elems[i],lcl_matrix,mesh);
+	  add_lcl_gbl(*gbl_matrix,lcl_matrix,i,mesh);
+
+     }
+     outmatrix->send(gbl_matrix);
+
 }
+
+void BuildFEMatrix::build_local_matrix(Element *elem, 
+				       Matrix& lcl_a,
+				       MeshHandle mesh)
+{
+
+     Point pt;
+     Vector grad1,grad2,grad3,grad4;
+     double vol = mesh->get_grad(elem,pt,grad1,grad2,grad3,grad4);
+
+     DenseMatrix el_coefs(4,3);
+     // this 4x3 array holds the 3 gradients to be used 
+     // as coefficients for each of the four nodes of the 
+     // element
+
+     el_coefs[0][0]=grad1.x();
+     el_coefs[0][1]=grad1.y();
+     el_coefs[0][2]=grad1.z();
+
+     el_coefs[1][0]=grad2.x();
+     el_coefs[1][1]=grad2.y();
+     el_coefs[1][2]=grad2.z();
+
+     el_coefs[2][0]=grad3.x();
+     el_coefs[2][1]=grad3.y();
+     el_coefs[2][2]=grad3.z();
+
+     el_coefs[3][0]=grad4.x();
+     el_coefs[3][1]=grad4.y();
+     el_coefs[3][2]=grad4.z();
+
+     // cond_tensors are the sigma values for this element.
+     // where:
+     //  [0] => sigma xx
+     //  [1] => sigma xy and sigma yx
+     //  [2] => sigma xz and sigma zx
+     //  [3] => sigma yy
+     //  [4] => sigma yz and sigma zy
+     //  [5] => sigma zz
+
+     DenseMatrix el_cond(3,3);
+     // in el_cond, the indices tell you the directions
+     // the value is refering to. i.e. 0=x, 1=y, and 2=z
+     // so el_cond[1][2] is the same as sigma yz
+     el_cond[0][0] = mesh->cond_tensors[elem->cond][0];
+     el_cond[0][1] = mesh->cond_tensors[elem->cond][1];
+     el_cond[1][0] = mesh->cond_tensors[elem->cond][1];
+     el_cond[0][2] = mesh->cond_tensors[elem->cond][2];
+     el_cond[2][0] = mesh->cond_tensors[elem->cond][2];
+     el_cond[1][1] = mesh->cond_tensors[elem->cond][3];
+     el_cond[1][2] = mesh->cond_tensors[elem->cond][4];
+     el_cond[2][1] = mesh->cond_tensors[elem->cond][4];
+     el_cond[2][2] = mesh->cond_tensors[elem->cond][5];
+
+
+     // build the local matrix
+
+     for(int i=0; i< 4; i++) {
+	  for(int j=0; j< 4; j++) {
+	       lcl_a[i][j] = 0.0;
+	       for (int k=0; k< 3; k++){
+		    for (int l=0; l<3; l++){
+			 lcl_a[i][j] += 
+			      el_cond[k][l]*el_coefs[i][k]*el_coefs[j][l];
+		    }
+	       }
+	       lcl_a[i][j] *= vol;
+	  }
+     }
+	  
+}
+
+
+void BuildFEMatrix::add_lcl_gbl(Matrix& gbl_a, Matrix& lcl_a,
+				int el, MeshHandle mesh)
+{
+
+     for (int i=0; i<4; i++) // this four should eventually be a
+	  // variable ascociated with each element that indicates 
+	  // how many nodes are on that element. it will change with 
+	  // higher order elements
+     {	  
+	  int ii = mesh->elems[el]->n[i];
+	  for (int j=0; j<4; j++)
+	  {
+	       int jj = mesh->elems[el]->n[j];
+	       gbl_a[ii][jj] += lcl_a[i][j];
+	  }
+     }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
