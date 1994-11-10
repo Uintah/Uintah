af535469ca18c1bef33e9e34d154001c0782f1dd
Steven G. Parker <sparker@sci.utah.edu>
1994-11-10 03:39:43 +0000

    Halfway done...

--- Module_dirs/Surface/GenSurface.cc
+++ Module_dirs/Surface/GenSurface.cc
@@ -13,10 +13,21 @@
 #include <Classlib/NotFinished.h>
 #include <Dataflow/Module.h>
 #include <Dataflow/ModuleList.h>
+#include <Datatypes/BasicSurfaces.h>
 #include <Datatypes/SurfacePort.h>
 #include <Geometry/Point.h>
+#include <TCL/TCLvar.h>
 
 class GenSurface : public Module {
+    TCLstring surfacetype;
+    TCLPoint cyl_p1;
+    TCLPoint cyl_p2;
+    TCLdouble cyl_rad;
+    TCLint cyl_nu;
+    TCLint cyl_nv;
+    TCLint cyl_ndiscu;
+
+    SurfaceOPort* outport;
 public:
     GenSurface(const clString& id);
     GenSurface(const GenSurface&, int deep);
@@ -33,14 +44,22 @@ static Module* make_GenSurface(const clString& id)
 static RegisterModule db1("Unfinished", "GenSurface", make_GenSurface);
 
 GenSurface::GenSurface(const clString& id)
-: Module("GenSurface", id, Source)
+: Module("GenSurface", id, Source), surfacetype("surfacetype", id, this),
+  cyl_p1("cyl_p1", id, this), cyl_p2("cyl_p2", id, this),
+  cyl_rad("cyl_rad", id, this), cyl_nu("cyl_nu", id, this),
+  cyl_nv("cyl_nv", id, this), cyl_ndiscu("cyl_ndiscu", id, this)
 {
     // Create the output port
-    add_oport(new SurfaceOPort(this, "Geometry", SurfaceIPort::Atomic));
+    outport=new SurfaceOPort(this, "Geometry", SurfaceIPort::Atomic);
+    add_oport(outport);
+    surfacetype.set("cylinder");
 }
 
 GenSurface::GenSurface(const GenSurface& copy, int deep)
-: Module(copy, deep)
+: Module(copy, deep), surfacetype("surfacetype", id, this),
+  cyl_p1("cyl_p1", id, this), cyl_p2("cyl_p2", id, this),
+  cyl_rad("cyl_rad", id, this), cyl_nu("cyl_nu", id, this),
+  cyl_nv("cyl_nv", id, this), cyl_ndiscu("cyl_ndiscu", id, this)
 {
     NOT_FINISHED("GenSurface::GenSurface");
 }
@@ -56,5 +75,14 @@ Module* GenSurface::clone(int deep)
 
 void GenSurface::execute()
 {
-    NOT_FINISHED("GenSurface::execute");
+    Surface* surf=0;
+    clString st(surfacetype.get());
+    if(st=="cylinder"){
+	surf=new CylinderSurface(cyl_p1.get(), cyl_p2.get(), cyl_rad.get(),
+				 cyl_nu.get(), cyl_nv.get(), cyl_ndiscu.get());
+    } else {
+	error("Unknown surfacetype: "+st);
+    }
+    if(surf)
+        outport->send(SurfaceHandle(surf));
 }
