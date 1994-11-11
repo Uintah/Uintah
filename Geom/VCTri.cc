19919dfdd7c0bd7726818bd5392fe0f8822e91c1
David Weinstein <dweinste@sci.utah.edu>
1994-11-11 05:01:13 +0000

    Vertices have MaterialHandles

---
+++ Geom/VCTri.cc
@@ -0,0 +1,56 @@
+
+/*
+ *  VCTri.cc: Vertex Colored Triangles...
+ *
+ *  Written by:
+ *   David Weinstein
+ *   Department of Computer Science
+ *   University of Utah
+ *   November 1994
+ *
+ *  Copyright (C) 1994 SCI Group
+ */
+
+#include <Geom/VCTri.h>
+#include <Geometry/BBox.h>
+#include <Math/MinMax.h>
+
+GeomVCTri::GeomVCTri(const Point& p1, const Point& p2, const Point& p3,
+		     const MaterialHandle& m1, const MaterialHandle &m2,
+		     const MaterialHandle& m3)
+: GeomObj(1), p1(p1), p2(p2), p3(p3), m1(m1), m2(m2), m3(m3), 
+  n(Cross(p3-p1, p2-p1))
+{
+    if(n.length2() > 0)
+	n.normalize();
+}
+
+GeomVCTri::GeomVCTri(const GeomVCTri &copy)
+: GeomObj(1), p1(copy.p1), p2(copy.p2), p3(copy.p3), m1(copy.m1), m2(copy.m2),
+  m3(copy.m3), n(copy.n)
+{
+}
+
+GeomVCTri::~GeomVCTri()
+{
+}
+
+GeomObj* GeomVCTri::clone()
+{
+    return new GeomVCTri(*this);
+}
+
+void GeomVCTri::get_bounds(BBox& bb)
+{
+    bb.extend(p1);
+    bb.extend(p2);
+    bb.extend(p3);
+}
+
+void GeomVCTri::make_prims(Array1<GeomObj*>&,
+			  Array1<GeomObj*>& dontfree)
+{
+    GeomVCTri* VCTri=this;
+    dontfree.add(VCTri);
+}
+
---
+++ Geom/VCTri.h
@@ -0,0 +1,48 @@
+
+/*
+ *  VCTri.h: Triangles...
+ *
+ *  Written by:
+ *   David Weinstein
+ *   Department of Computer Science
+ *   University of Utah
+ *   Novemeber 1994
+ *
+ *  Copyright (C) 1994 SCI Group
+ */
+
+#ifndef SCI_Geom_VCTri_h
+#define SCI_Geom_VCTri_h 1
+
+#include <Geom/Geom.h>
+#include <Geometry/Point.h>
+
+class GeomVCTri : public GeomObj {
+public:
+    Point p1;
+    Point p2;
+    Point p3;
+    MaterialHandle m1;
+    MaterialHandle m2;
+    MaterialHandle m3;
+    Vector n;
+
+    GeomVCTri(const Point&, const Point&, const Point&, const MaterialHandle&,
+	      const MaterialHandle&, const MaterialHandle&);
+    GeomVCTri(const GeomVCTri&);
+    virtual ~GeomVCTri();
+
+    virtual GeomObj* clone();
+    virtual void get_bounds(BBox&);
+
+#ifdef SCI_OPENGL
+    virtual void objdraw(DrawInfoOpenGL*);
+#endif
+    virtual void objdraw(DrawInfoX11*);
+    virtual double depth(DrawInfoX11*);
+    virtual void get_hit(Vector&, Point&);
+    virtual void make_prims(Array1<GeomObj*>& free,
+			    Array1<GeomObj*>& dontfree);
+};
+
+#endif /* SCI_Geom_VCTri_h */
