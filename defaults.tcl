3f32ef1a7ba5978f3763d2ad694b2c7abd4ffd25
Steven G. Parker <sparker@sci.utah.edu>
1994-11-26 05:13:13 +0000

    Defaults for slider/scrollbar colors Change prompt More TODO

--- defaults.tcl
+++ defaults.tcl
@@ -5,6 +5,11 @@ option add *background pink2
 option add *sliderForeground pink2
 option add *activeBackground pink2
 option add *activeForeground mediumblue
+option add *Scrollbar*activeForeground SteelBlue2
+option add *Scrollbar*foreground plum2
+option add *Fscale*activeForeground SteelBlue2
 option add *selector red
-#option add *font screen14
 option add *font "-Adobe-Helvetica-bold-R-Normal--*-120-75-*"
+
+set tcl_prompt1 "puts -nonewline \"sr> \""
+set tcl_prompt2 "puts -nonewline \"sr>> \""
