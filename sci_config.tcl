547798d2e3982c5700c4808c1f4205ffdade6f59
Steven G. Parker <sparker@sci.utah.edu>
1995-01-10 20:58:27 +0000

    Use script to run sci_config

--- sci_config.tcl
+++ sci_config.tcl
@@ -1,322 +1,51 @@
+#!./itcl_wish -f
 
-proc read_config {} {
-    set f [open config.values r]
-    set result ""
-    while {[gets $f line] >= 0} {
-	set x [split $line]
-	if {[llength $x] != 2} {
-	    puts stderr "Error in config.values, line:"
-	    puts stderr $line
-	    exit
-	}
-	lappend result [lindex $x 0]
-	lappend result [lindex $x 1]
-    }
-    close $f
-    return $result
+source defaults.tcl
+source sci_conflib.tcl
+
+ConfigChoice .machine -choices {Linux SGI} -text "Machine: " \
+	-name MACHINE
+ConfigChoice .variant -choices {Debug Optimized} -text "Variant: " \
+	-name VARIANT
+ConfigChoice .compiler -choices {CFront Delta GNU} -text "Compiler: " \
+	-name COMPILER -file "cm"
+ConfigChoice .assertions -choices {1 2 3 4} -text "Assertion level: " \
+	-name "ASSERTIONLEVEL" -file ch
+ConfigBool .opengl -text "OpenGL? " -name OPENGL
+
+frame .bottom -borderwidth 10
+pack .bottom -side bottom
+
+button .bottom.apply -text "Apply" -command "ConfigBase :: apply"
+button .bottom.reset -text "Reset" -command "ConfigBase :: reset"
+button .bottom.cancel -text "Cancel" -command exit
+pack .bottom.apply .bottom.reset .bottom.cancel -side left -padx 10 -pady 4
+
+label .l -text "Directories: " -foreground red -relief groove -anchor nw
+pack .l -side top -anchor nw -fill x
+
+frame .left -relief groove -borderwidth 2
+pack .left -side left -anchor nw -fill y -padx 2 -pady 2
+set i 0
+foreach t {Classlib Comm Constraints Dataflow Datatypes Devices Geom \
+	   Geometry Malloc Math Multitask TCL Widgets config convert \
+	   doc etc gnupt scirun scripts} {
+	ConfigDir .left.$i -text "$t: " -name DIR_$t -dir $t -modname $t
+	incr i
 }
 
-set rebuild_all_symlinks 0
-
-itcl_class ConfigBase {
-    constructor {config} {
-	lappend instances $this
-
-	set value [ConfigBase :: lookup $name]
-	global $this-value
-	set $this-value $value
-	set orig_value $value
-    }
-    common instances
-    proc reset {} {
-	foreach t $instances {
-	    $t do_reset
-	}
-    }
-
-    common all_values [read_config]
-    proc lookup {id} {
-	set idx [lsearch $all_values $id]
-	if {$idx == -1} {
-	    return "<none>";
-	}
-	incr idx
-	return [lindex $all_values $idx]
-    }
-
-    protected orig_value
-    method do_reset {} {
-	global $this-value
-	set $this-value $orig_value
-    }
-
-    proc apply {} {
-	if {[.machine changed] || [.variant changed] || [.compiler changed]} {
-	    set rebuild_all_symlinks 1
-	} else {
-	    set rebuild_all_symlinks 0
-	}
-	set cv [open config.values w]
-	set ch [open config.h.tmp w]
-	set cm [open config_imake.h.tmp w]
-	foreach t $instances {
-	    $t do_apply $cv $ch $cm
-	}
-	close $cv
-	close $ch
-	close $cm
-
-	set failed [catch "exec diff config.h.tmp config.h > /dev/null"]
-	set need_make 0
-	if {$failed} {
-	    exec mv config.h.tmp config.h
-	    set need_make 1
-	} else {
-	    exec rm config.h.tmp
-	}
-	set failed [catch "exec diff config_imake.h.tmp config_imake.h > /dev/null"]
-	if {$failed} {
-	    exec mv config_imake.h.tmp config_imake.h
-	    if {![file exists Makefile]} {
-		exec xmkmf >@ stdout 2>@ stderr
-	    }
-	    exec make World &
-	} else {
-	    exec rm config_imake.h.tmp
-	    if {$need_make} {
-		exec make &
-	    }
-	}
-    }
-    method do_apply {cv ch cm} {
-	global $this-value
-	puts $cv "$name [set $this-value]"
-	$this do_apply_spec $ch $cm
-    }
-
-    public name ""
-    public file "both" {
-	if {$file != "both" && $file != "ch" && $file != "cm"} {
-	    puts "bad value for -file: $file"
-	    exit
-        }
-    }
-
-    proc vname {} {
-	set mach [.machine get]
-	set var [.variant get]
-	set compiler [.compiler get]
-	return "[string tolower $mach]-[string tolower $var]-[string tolower $compiler]"
-    }
-    method changed {} {
-	global $this-value
-	if {[set $this-value] != $orig_value} {
-	    return 1
-	} else {
-	    return 0
-	}
-    }
-    method get {} {
-	global $this-value
-	return [set $this-value]
-    }
+frame .right -relief groove -borderwidth 2
+pack .right -side right -anchor nw -fill y -padx 2 -pady 2
+set i 0
+foreach t {Contours FEM Fields Matrix Mesh Salmon Sound Surface \
+	   Visualization Writers} {
+	ConfigDir .right.$i -text "Modules/$t: " -name DIR_Modules_$t \
+		-dir Modules/$t -modname Module_$t
+	incr i
 }
 
-itcl_class ConfigChoice {
-    inherit ConfigBase
-    constructor {config} {
-	ConfigBase::constructor
-	set class [$this info class]
-	::rename $this $this-tmp-
-	::frame $this -class $class
-	::rename $this $this-win-
-	::rename $this-tmp- $this
-
-	label $this.label -text $text
-	pack $this.label -side left
-
-	set initted 1
-	$this repack
-
-	pack $this -side top -fill x
-	global $this-value
-	if {[set $this-value] == "<none>"} {
-	    set $this-value [lindex $choices 0]
-	}
-    }
-    destructor {
-    }
-
-    method repack {} {
-	if {!$initted} {
-	    return;
-	}
-	if {[winfo exists $this.l]} {
-	    pack forget $this.l
-	}
-	frame $this.l
-	pack $this.l -side left
-	set i 0
-	foreach n $choices {
-	    radiobutton $this.l.$i -value "$n" -variable $this-value \
-		    -text "$n"
-	    pack $this.l.$i -side left
-	    incr i
-	}
-    }
-
-    protected initted 0
-    public choices "" { $this repack }
-    public text ""
-
-    method do_apply_spec {ch cm} {
-	global $this-value
-	if {$file == "both" || $file == "ch"} {
-	    puts $ch "\#define SCI_[set name]_[set $this-value]"
-	}
-	if {$file == "both" || $file == "cm"} {
-	    puts $cm "\#define SCI_[set name]_[set $this-value]"
-	}
-    }
-
-}
-
-itcl_class ConfigBool {
-    inherit ConfigBase
-    constructor {config} {
-	ConfigBase::constructor
-	set class [$this info class]
-	::rename $this $this-tmp-
-	::frame $this -class $class
-	::rename $this $this-win-
-	::rename $this-tmp- $this
-
-	label $this.label -text $text
-	pack $this.label -side left
-	frame $this.l
-	pack $this.l -side left
-	radiobutton $this.l.true -value true -variable $this-value -text True
-	radiobutton $this.l.false -value false -variable $this-value -text False
-	pack $this.l.true $this.l.false -side left
-
-	pack $this -side top -fill x
-	global $this-value
-	if {[set $this-value] == "<none>"} {
-	    set $this-value false
-	}
-    }
-    destructor {
-    }
-
-    method do_apply_spec {ch cm} {
-	global $this-value
-	if {[set $this-value]} {
-	    if {$file == "both" || $file == "ch"} {
-		puts $ch "\#define SCI_$name"
-	    }
-	    if {$file == "both" || $file == "cm"} {
-		puts $cm "\#define SCI_$name"
-	    }
-	}
-    }
-    public text
-}
-
-itcl_class ConfigDir {
-    inherit ConfigBase
-    constructor {config} {
-	ConfigBase::constructor
-	set class [$this info class]
-	::rename $this $this-tmp-
-	::frame $this -class $class
-	::rename $this $this-win-
-	::rename $this-tmp- $this
-
-
-	label $this.label -text $text
-	pack $this.label -side left
-	frame $this.l
-	pack $this.l -side right
-	radiobutton $this.l.lib -value lib -variable $this-value \
-		-text "Library copy"
-	radiobutton $this.l.co -value co -variable $this-value \
-		-text "Checked out"
-	pack $this.l.lib $this.l.co -side left
-
-	pack $this -side top -fill x
-
-	global $this-value
-	if {[set $this-value] == "<none>"} {
-	    set $this-value lib
-	}
-    }
-
-    method do_apply_spec {ch cm} {
-	global $this-value
-	set value [set $this-value]
-	if {$value == "co"} {
-	    puts $cm "\#define SCI_$name"
-	}
-	set rebuild_this_symlinks 0
-	if {$value != $orig_value} {
-	    if {$value == "co"} {
-		# checkout the library
-		exec rm -r $dir
-		set p [pwd]
-		cd [file dirname $dir]
-		exec cvs checkout $modname >@ stdout 2>@ stderr
-		cd $p
-	    } else {
-		# check the library back in
-		exec cvs update $dir >& cvs_output
-		set f [open cvs_output]
-		set l ""
-		while {[gets $f line] >= 0} {
-		    set c [string index $line 0]
-		    if {$c == "?" || $c == "M" || $c == "R" || $c == "A"} {
-			lappend l $line
-		    }
-		}
-		close $f
-		exec rm cvs_output
-		if {$l != ""} {
-		    set s "There was an error checking in module: $dir"
-		    foreach t $l {
-			append s "\n" $t
-		    }
-		    append s "\n\nFix this error and press Apply again"
-		    tk_dialog .error "Error on checkin" $s \
-			    "" 0 Ok
-		    return
-		}
-		exec rm -r $dir
-		set rebuild_this_symlinks 1
-	    }
-	}
-
-	global rebuild_all_symlinks
-	if {$rebuild_this_symlinks || ($value == "lib" && $rebuild_all_symlinks)} {
-	    # Rebuild symbolic links
-	    catch "exec rm -rf $dir"
-	    exec mkdir $dir
-
-	    # Get vname
-	    set vname [ConfigBase :: vname]
-	    
-	    puts "vname is $vname"
-
-	    global env
-	    set root $env(SCI_DEVELOP)/work_lib/$vname/$dir
-	    if {[catch {set files [glob $root/*.tcl $root/lib*.o $root/lib*.a]}]} {
-		set files ""
-	    }
-	    foreach t $files {
-		exec ln -s $t $dir/[file tail $t]
-	    }
-	}
-	set orig_value $value
-    }
-    public text
-    public dir
-    public modname
+foreach t {sci tcl7.3 tk3.6 tkFScale} {
+	ConfigDir .right.$i -text "tcl/$t: " -name DIR_tcl_$t \
+		-dir tcl/$t -modname tcl_$t
+	incr i
 }
