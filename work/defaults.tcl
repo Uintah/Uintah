bb6d7593c084a5ff9faffd03bbbb6d730a39bc97
Steven G. Parker <sparker@sci.utah.edu>
1995-01-11 23:54:53 +0000

    Update configuration for missing dirs

--- work/defaults.tcl
+++ work/defaults.tcl
@@ -4,12 +4,17 @@
 option add *background pink2
 option add *sliderForeground pink2
 option add *activeBackground pink2
+option add *troughColor pink3
 option add *activeForeground mediumblue
-option add *Scrollbar*activeForeground SteelBlue2
+option add *Scrollbar*activeBackground SteelBlue2
 option add *Scrollbar*foreground plum2
-option add *Fscale*activeForeground SteelBlue2
+option add *Scrollbar*width .35c
+option add *Scale*width .35c
+option add *selectBackground "light blue"
 option add *selector red
 option add *font "-Adobe-Helvetica-bold-R-Normal--*-120-75-*"
+option add *Button*padX 1
+option add *Button*padY 1
 
 set tcl_prompt1 "puts -nonewline \"scirun> \""
 set tcl_prompt2 "puts -nonewline \"scirun>> \""
--- work/sci_config.tcl
+++ work/sci_config.tcl
@@ -19,7 +19,8 @@ pack .bottom -side bottom
 button .bottom.apply -text "Apply" -command "ConfigBase :: apply"
 button .bottom.reset -text "Reset" -command "ConfigBase :: reset"
 button .bottom.cancel -text "Cancel" -command exit
-pack .bottom.apply .bottom.reset .bottom.cancel -side left -padx 10 -pady 4
+pack .bottom.apply .bottom.reset .bottom.cancel -side left -padx 10 -pady 4 \
+	-ipadx 5 -ipady 5
 
 label .l -text "Directories: " -foreground red -relief groove -anchor nw
 pack .l -side top -anchor nw -fill x
@@ -37,14 +38,14 @@ foreach t {Classlib Comm Constraints Dataflow Datatypes Devices Geom \
 frame .right -relief groove -borderwidth 2
 pack .right -side right -anchor nw -fill y -padx 2 -pady 2
 set i 0
-foreach t {Contours FEM Fields Matrix Mesh Salmon Sound Surface \
-	   Visualization Writers} {
+foreach t {Contours FEM Fields Gradient Matrix Mesh Readers \
+	   Salmon Sound Surface Visualization Writers} {
 	ConfigDir .right.$i -text "Modules/$t: " -name DIR_Modules_$t \
 		-dir Modules/$t -modname Module_$t
 	incr i
 }
 
-foreach t {sci tcl7.3 tk3.6 tkFScale} {
+foreach t {sci tcl tk itcl} {
 	ConfigDir .right.$i -text "tcl/$t: " -name DIR_tcl_$t \
 		-dir tcl/$t -modname tcl_$t
 	incr i
