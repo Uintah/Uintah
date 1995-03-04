8fe14ad1d3275b3987447ecd7a29a8df68ee4e15
Steven G. Parker <sparker@sci.utah.edu>
1995-03-04 17:59:59 +0000

    Bug fixes for Delaunay

--- Module_dirs/Mesh/Delaunay.cc
+++ Module_dirs/Mesh/Delaunay.cc
@@ -21,6 +21,8 @@
 class Delaunay : public Module {
     MeshIPort* iport;
     MeshOPort* oport;
+    TCLint nnodes;
+    TCLint cleanup;
 public:
     Delaunay(const clString& id);
     Delaunay(const Delaunay&, int deep);
@@ -37,7 +39,8 @@ static Module* make_Delaunay(const clString& id)
 static RegisterModule db1("Mesh", "Delaunay", make_Delaunay);
 
 Delaunay::Delaunay(const clString& id)
-: Module("Delaunay", id, Filter)
+: Module("Delaunay", id, Filter), nnodes("nnodes", id, this),
+  cleanup("cleanup", id, this)
 {
     iport=new MeshIPort(this, "Input Mesh", MeshIPort::Atomic);
     add_iport(iport);
@@ -47,7 +50,8 @@ Delaunay::Delaunay(const clString& id)
 }
 
 Delaunay::Delaunay(const Delaunay& copy, int deep)
-: Module(copy, deep)
+: Module(copy, deep), nnodes("nnodes", id, this),
+  cleanup("cleanup", id, this)
 {
     NOT_FINISHED("Delaunay::Delaunay");
 }
@@ -71,10 +75,11 @@ void Delaunay::execute()
     mesh_handle.detach();
     Mesh* mesh=mesh_handle.get_rep();
     mesh->elems.remove_all();
+    mesh->compute_neighbors();
 
-    int nnodes=mesh->nodes.size();
+    int nn=mesh->nodes.size();
     BBox bbox;
-    for(int i=0;i<nnodes;i++)
+    for(int i=0;i<nn;i++)
 	bbox.extend(mesh->nodes[i]->p);
 
     double epsilon=1.e-4;
@@ -93,26 +98,45 @@ void Delaunay::execute()
     // Make the initial mesh with a tetra which encloses the bounding
     // box.  The first point is at the minimum point.  The other 3
     // have one of the coordinates at bmin+diagonal*3.
+#if 0
     mesh->nodes.add(new Node(bmin));
     mesh->nodes.add(new Node(bmin+Vector(le*3, 0, 0)));
     mesh->nodes.add(new Node(bmin+Vector(0, le*3, 0)));
     mesh->nodes.add(new Node(bmin+Vector(0, 0, le*3)));
+#endif
+    mesh->nodes.add(new Node(bmin-Vector(le, le, le)));
+    mesh->nodes.add(new Node(bmin+Vector(le*5, 0, 0)));
+    mesh->nodes.add(new Node(bmin+Vector(0, le*5, 0)));
+    mesh->nodes.add(new Node(bmin+Vector(0, 0, le*5)));
 
-    Element* el=new Element(mesh, nnodes+0, nnodes+1, nnodes+2, nnodes+3);
+    Element* el=new Element(mesh, nn+0, nn+1, nn+2, nn+3);
+    int onn=nn;
     el->orient();
     el->faces[0]=el->faces[1]=el->faces[2]=el->faces[3]=-1;
     mesh->elems.add(el);
 
-    for(int node=0;node<nnodes;node++){
+    nn=nnodes.get();
+    for(int node=0;node<nn;node++){
 	// Add this node...
-	update_progress(node, nnodes);
+	update_progress(node, nn);
 
 	if(!mesh->insert_delaunay(node)){
 	    error("Mesher upset - point outside of domain...");
 	    return;
 	}
-//	if(!mesh->elems[0])
-//	    mesh->pack_elems();
+	cerr << "This pack and compute is temporary...\n";
+	mesh->pack_elems();
+	mesh->compute_neighbors();
+	mesh->compute_face_neighbors();
+    }
+    mesh->pack_elems();
+    mesh->compute_neighbors();
+
+    if(cleanup.get()){
+	mesh->remove_delaunay(onn, 0);
+	mesh->remove_delaunay(onn+1, 0);
+	mesh->remove_delaunay(onn+2, 0);
+	mesh->remove_delaunay(onn+3, 0);
     }
     mesh->pack_elems();
     mesh->compute_neighbors();
