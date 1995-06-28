a8e9db4c6593f80865cbd76b82a03aff7df0a1e7
David Weinstein <dweinste@sci.utah.edu>
1995-06-28 20:24:42 +0000

    Added new Triangles GeomObj -- faster way to render and store lots of triangles

---
+++ Geom/Triangles.cc
@@ -0,0 +1,129 @@
+
+/*
+ *  Triangles.cc: Triangle Strip object
+ *
+ *  Written by:
+ *   Steven G. Parker & David Weinstein
+ *   Department of Computer Science
+ *   University of Utah
+ *   June 1995
+ *
+ *  Copyright (C) 1995 SCI Group
+ */
+
+#include <Geom/Triangles.h>
+#include <Classlib/NotFinished.h>
+#include <Geom/Tri.h>
+#include <Geometry/BBox.h>
+#include <Malloc/Allocator.h>
+
+GeomTriangles::GeomTriangles()
+{
+}
+
+GeomTriangles::GeomTriangles(const GeomTriangles& copy)
+: GeomVertexPrim(copy)
+{
+}
+
+GeomTriangles::~GeomTriangles() {
+}
+
+void GeomTriangles::add(const Point& p1, const Point& p2, const Point& p3) {
+    Vector n(Cross(p2-p1, p3-p1));
+    if(n.length2() > 0){
+	n.normalize();
+    } else {
+	cerr << "Degenerate triangle!!!\n" << endl;
+	return;
+    }
+    normals.add(n);
+    GeomVertexPrim::add(p1);
+    GeomVertexPrim::add(p2);
+    GeomVertexPrim::add(p3);
+}
+
+void GeomTriangles::add(const Point& p1, const Vector& v1,
+			const Point& p2, const Vector& v2,
+			const Point& p3, const Vector& v3) {
+    Vector n(Cross(p2-p1, p3-p1));
+    if(n.length2() > 0){
+	n.normalize();
+    } else {
+	cerr << "Degenerate triangle!!!\n" << endl;
+	return;
+    }
+    normals.add(n);
+    GeomVertexPrim::add(p1, v1);
+    GeomVertexPrim::add(p2, v2);
+    GeomVertexPrim::add(p3, v3);
+}
+
+void GeomTriangles::add(const Point& p1, const MaterialHandle& m1,
+			const Point& p2, const MaterialHandle& m2,
+			const Point& p3, const MaterialHandle& m3) {
+    Vector n(Cross(p2-p1, p3-p1));
+    if(n.length2() > 0){
+	n.normalize();
+    } else {
+	cerr << "Degenerate triangle!!!\n" << endl;
+	return;
+    }
+    normals.add(n);
+    GeomVertexPrim::add(p1, m1);
+    GeomVertexPrim::add(p2, m2);
+    GeomVertexPrim::add(p3, m3);
+}
+
+void GeomTriangles::add(const Point& p1, const Vector& v1, 
+			const MaterialHandle& m1, const Point& p2, 
+			const Vector& v2, const MaterialHandle& m2,
+			const Point& p3, const Vector& v3, 
+			const MaterialHandle& m3) {
+    Vector n(Cross(p2-p1, p3-p1));
+    if(n.length2() > 0){
+	n.normalize();
+    } else {
+	cerr << "Degenerate triangle!!!\n" << endl;
+	return;
+    }
+    normals.add(n);
+    GeomVertexPrim::add(p1, v1, m1);
+    GeomVertexPrim::add(p2, v2, m2);
+    GeomVertexPrim::add(p3, v3, m3);
+}
+
+void GeomTriangles::add(GeomVertex* v1, GeomVertex* v2, GeomVertex* v3) {
+    Vector n(Cross(v3->p - v1->p, v2->p - v1->p));
+    if(n.length2() > 0){
+	n.normalize();
+    } else {
+	cerr << "Degenerate triangle!!!\n" << endl;
+	return;
+    }
+    normals.add(n);
+    GeomVertexPrim::add(v1);
+    GeomVertexPrim::add(v2);
+    GeomVertexPrim::add(v3);
+}
+
+void GeomTriangles::make_prims(Array1<GeomObj*>&,
+			      Array1<GeomObj*>&)
+{
+    NOT_FINISHED("GeomTriangles::make_prims");
+}
+
+GeomObj* GeomTriangles::clone()
+{
+    return scinew GeomTriangles(*this);
+}
+
+void GeomTriangles::preprocess()
+{
+    NOT_FINISHED("GeomTriangles::preprocess");
+}
+
+void GeomTriangles::intersect(const Ray&, Material*, Hit&)
+{
+    NOT_FINISHED("GeomTriangles::intersect");
+}
---
+++ Geom/Triangles.h
@@ -0,0 +1,54 @@
+
+/*
+ *  Triangles.h: Triangle Strip object
+ *
+ *  Written by:
+ *   Steven G. Parker & David Weinstein
+ *   Department of Computer Science
+ *   University of Utah
+ *   June 1994
+ *
+ *  Copyright (C) 1995 SCI Group
+ */
+
+#ifndef SCI_Geom_Triangles_h
+#define SCI_Geom_Triangles_h 1
+
+#include <Geom/VertexPrim.h>
+
+class GeomTriangles : public GeomVertexPrim {
+private:
+    void add(const Point&);
+    void add(const Point&, const Vector&);
+    void add(const Point&, const MaterialHandle&);
+    void add(const Point&, const Vector&, const MaterialHandle&);
+    void add(GeomVertex*);
+public:
+    Array1<Vector> normals;
+    GeomTriangles();
+    GeomTriangles(const GeomTriangles&);
+    virtual ~GeomTriangles();
+    void add(const Point&, const Point&, const Point&);
+    void add(const Point&, const Vector&,
+	     const Point&, const Vector&,
+	     const Point&, const Vector&);
+    void add(const Point&, const MaterialHandle&,
+	     const Point&, const MaterialHandle&,
+	     const Point&, const MaterialHandle&);
+    void add(const Point&, const Vector&, const MaterialHandle&,
+	     const Point&, const Vector&, const MaterialHandle&,
+	     const Point&, const Vector&, const MaterialHandle&);
+    void add(GeomVertex*, GeomVertex*, GeomVertex*);
+    virtual GeomObj* clone();
+
+#ifdef SCI_OPENGL
+    virtual void draw(DrawInfoOpenGL*, Material*, double time);
+#endif
+    virtual void make_prims(Array1<GeomObj*>& free,
+			    Array1<GeomObj*>& dontfree);
+    virtual void preprocess();
+    virtual void intersect(const Ray& ray, Material*,
+			   Hit& hit);
+};
+
+#endif /* SCI_Geom_Triangles_h */
