c65791f7b5c18a3f03a96663279f4f857124c6af
Steven G. Parker <sparker@sci.utah.edu>
1995-09-08 18:04:20 +0000

    More stuff todo Added irix 6 config option

--- sci_config.tcl
+++ sci_config.tcl
@@ -13,6 +13,7 @@ ConfigChoice .assertions -choices {0 1 2 3 4} -text "Assertion level: " \
 	-name "ASSERTION_LEVEL" -file ch
 ConfigBool .opengl -text "OpenGL? " -name OPENGL
 ConfigBool .quarks -text "Quarks? " -name QUARKS
+ConfigBool .irix6 -text "Irix 6? " -name IRIX6
 
 frame .bottom -borderwidth 10
 pack .bottom -side bottom
@@ -23,30 +24,3 @@ button .bottom.cancel -text "Exit" -command exit
 pack .bottom.apply .bottom.reset .bottom.cancel -side left -padx 10 -pady 4 \
 	-ipadx 5 -ipady 5
 
-label .l -text "Directories: " -foreground red -relief groove -anchor nw
-pack .l -side top -anchor nw -fill x
-
-frame .left -relief groove -borderwidth 2
-pack .left -side left -anchor nw -fill y -padx 2 -pady 2
-set i 0
-foreach t {Classlib Comm Constraints Dataflow Datatypes Devices Geom \
-	   Geometry Malloc Math Multitask TCL Widgets} {
-	ConfigDir .left.$i -text "$t: " -name DIR_$t -dir $t -modname $t
-	incr i
-}
-
-frame .right -relief groove -borderwidth 2
-pack .right -side right -anchor nw -fill y -padx 2 -pady 2
-set i 0
-foreach t {Contours FEM Fields Matrix Mesh Readers \
-	   Salmon Sound Surface Visualization Writers} {
-	ConfigDir .right.$i -text "Modules/$t: " -name DIR_Modules_$t \
-		-dir Modules/$t -modname Module_$t
-	incr i
-}
-
-foreach t {sci tcl tk itcl} {
-	ConfigDir .right.$i -text "tcl/$t: " -name DIR_tcl_$t \
-		-dir tcl/$t -modname tcl_$t
-	incr i
-}
