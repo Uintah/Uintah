6cba3c57f683e59fc397851d433d0972bc5c1d08
Peter Pike Sloan <ppsloan@sci.utah.edu>
1995-11-05 11:37:35 +0000

    ArcBall code for ArcBall Rotation

---
+++ Modules/Salmon/Ball.h
@@ -0,0 +1,48 @@
+/***** Ball.h *****/
+#ifndef _H_Ball
+#define _H_Ball
+
+/* smoosh ken shoemakes stuff into c++, PPS */
+
+#include "BallAux.h"
+
+typedef enum AxisSet{NoAxes, CameraAxes, BodyAxes, OtherAxes, NSets} AxisSet;
+typedef double *ConstraintSet;
+class BallData {
+public:
+    BallData() { Init(); }
+    void Init(void);
+    void Place(HVect cntr, double rad) {
+	center = cntr;
+	radius = rad;
+    }
+    void Mouse(HVect vnow) { vNow = vnow; }
+    void UseSet(AxisSet);
+    void ShowResult(void);
+    void HideResult(void);
+    void Update(void);
+    void Value(HMatrix&);
+    void BeginDrag(void);
+    void EndDrag(void);
+    void Draw(void);
+    void DValue(HMatrix&); // value at qDown
+
+    HVect center;
+    double radius;
+    Quat qNow, qDown, qDrag;
+    HVect vNow, vDown, vFrom, vTo, vrFrom, vrTo;
+    HMatrix mNow, mDown;
+    Bool showResult, dragging;
+    ConstraintSet sets[NSets];
+    int setSizes[NSets];
+    AxisSet axisSet;
+    int axisIndex;
+};
+
+/* Private routines */
+void DrawAnyArc(HVect vFrom, HVect vTo);
+void DrawHalfArc(HVect n);
+void Ball_DrawConstraints(BallData *ball);
+void Ball_DrawDragArc(BallData *ball);
+void Ball_DrawResultArc(BallData *ball);
+#endif
