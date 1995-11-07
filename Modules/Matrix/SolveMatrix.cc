0a65182cb05430dbc48a22f4c3c2429cef7a5568
Steven G. Parker <sparker@sci.utah.edu>
1995-11-07 05:53:49 +0000

    New user interface Optimizations CG code moved here

--- Modules/Matrix/SolveMatrix.cc
+++ Modules/Matrix/SolveMatrix.cc
@@ -17,6 +17,7 @@
 #include <Datatypes/SurfacePort.h>
 #include <Geometry/Point.h>
 #include <Malloc/Allocator.h>
+#include <TCL/TCLvar.h>
 #include <strstream.h>
 
 class SolveMatrix : public Module {
@@ -24,21 +25,24 @@ class SolveMatrix : public Module {
     ColumnMatrixIPort* rhsport;
     ColumnMatrixOPort* solport;
     ColumnMatrixHandle solution;
+    void conjugate_gradient(Matrix*, ColumnMatrix&, ColumnMatrix&);
+
+    void append_values(int niter, const Array1<double>& errlist,
+		       int& last_update);
 public:
     SolveMatrix(const clString& id);
     SolveMatrix(const SolveMatrix&, int deep);
     virtual ~SolveMatrix();
     virtual Module* clone(int deep);
     virtual void execute();
-};
 
-class SolveMatrixUpdater : public MatrixUpdater {
-public:
-    clString solverid;
-    ColumnMatrixOPort* solport;
-    Module* module;
-    virtual void update(int, double, double, double,
-			const ColumnMatrix&);
+    TCLdouble target_error;
+    TCLint flops;
+    TCLdouble floprate;
+    TCLdouble orig_error;
+    TCLdouble current_error;
+    TCLstring method;
+    TCLint iteration;
 };
 
 extern "C" {
@@ -49,7 +53,10 @@ Module* make_SolveMatrix(const clString& id)
 };
 
 SolveMatrix::SolveMatrix(const clString& id)
-: Module("SolveMatrix", id, Filter)
+: Module("SolveMatrix", id, Filter), target_error("target_error", id, this),
+  flops("flops", id, this), floprate("floprate", id, this),
+  orig_error("orig_error", id, this), current_error("current_error", id, this),
+  method("method", id, this), iteration("iteration", id, this)
 {
     matrixport=scinew MatrixIPort(this, "Matrix", MatrixIPort::Atomic);
     add_iport(matrixport);
@@ -61,7 +68,10 @@ SolveMatrix::SolveMatrix(const clString& id)
 }
 
 SolveMatrix::SolveMatrix(const SolveMatrix& copy, int deep)
-: Module(copy, deep)
+: Module(copy, deep), target_error("target_error", id, this),
+  flops("flops", id, this), floprate("floprate", id, this),
+  orig_error("orig_error", id, this), current_error("current_error", id, this),
+  method("method", id, this), iteration("iteration", id, this)
 {
     NOT_FINISHED("SolveMatrix::SolveMatrix");
 }
@@ -83,31 +93,170 @@ void SolveMatrix::execute()
     ColumnMatrixHandle rhs;
     if(!rhsport->get(rhs))
 	return;
-    if(!solution.get_rep()){
+    if(!solution.get_rep() || solution->nrows() != rhs->nrows()){
 	solution=scinew ColumnMatrix(rhs->nrows());
 	solution->zero();
     } else {
 	solution.detach();
     }
-    SolveMatrixUpdater updater;
-    updater.solverid=id;
-    updater.solport=solport;
-    updater.module=this;
-    matrix->isolve(*solution.get_rep(), *rhs.get_rep(), 1.e-4,
-		   &updater);
-    solport->send(solution);
+
+    int size=matrix->nrows();
+    if(matrix->ncols() != size){
+	error(clString("Matrix should be square, but is ")
+	      +to_string(size)+" x "+to_string(matrix->ncols()));
+	return;
+    }
+    if(rhs->nrows() != size){
+	error(clString("Matrix size mismatch"));
+	return;
+    }
+
+    clString meth=method.get();
+    if(meth == "conjugate_gradient"){
+	conjugate_gradient(matrix.get_rep(),
+			   *solution.get_rep(), *rhs.get_rep());
+	solport->send(solution);
+    } else {
+	error(clString("Unknown solution method: ")+meth);
+    }
 }
 
-void SolveMatrixUpdater::update(int iter, double first_error,
-				double current_error, double final_error,
-				const ColumnMatrix& solution)
+void SolveMatrix::conjugate_gradient(Matrix* matrix,
+				    ColumnMatrix& lhs, ColumnMatrix& rhs)
 {
-    char buf[1000];
+    int size=matrix->nrows();
+
+    int flop=0;
+    flops.set(flop);
+    floprate.set(0);
+
+    iteration.set(0);
+    
+    ColumnMatrix diag(size);
+    // We should try to do a better job at preconditioning...
+    int i;
+    for(i=0;i<size;i++){
+	diag[i]=1./matrix->get(i,i);
+    }
+    flop+=size;
+
+    ColumnMatrix R(size);
+    flop+=matrix->mult(lhs, R);
+
+    for(i=0;i<size;i++){
+	R[i]=rhs[i]-R[i];
+    }
+    flop+=size;
+
+    double bnorm=rhs.vector_norm();
+    flop+=2*size;
+
+    ColumnMatrix Z(size);
+    flop+=matrix->mult(R, Z);
+
+    ColumnMatrix P(size);
+    double bkden=0;
+    double err=R.vector_norm();
+    flop+=2*size;
+
+    orig_error.set(err);
+    current_error.set(err);
+
+    int niter=0;
+    int toomany=2*size;
+    double max_error=target_error.get();
+
+    flops.set(flop);
+    double time=timer.time();
+    floprate.set((double)flop*1.e-6/time);
+    
+    TCL::execute(id+" reset_graph");
+    Array1<double> errlist;
+    errlist.add(err);
+    int last_update=0;
+    append_values(1, errlist, last_update);
+
+    double log_orig=log(err);
+    double log_targ=log(max_error);
+    while(niter < toomany && err > max_error){
+	niter++;
+
+	// Simple Preconditioning...
+	for(int idd=0;idd<size;idd++){
+	    Z[idd]=R[idd]*diag[idd];
+	}
+	flop+=size;
+	
+	// Calculate coefficient bk and direction vectors p and pp
+	double bknum=0;
+	int i;
+	for(i=0;i<size;i++)
+	    bknum+=Z[i]*R[i];
+	flop+=size;
+
+	if(niter==1){
+	    P=Z;
+	} else {
+	    double bk=bknum/bkden;
+	    for(int i=0;i<size;i++){
+		P[i]=bk*P[i]+Z[i];
+	    }
+	    flop+=2*size+1;
+	}
+	bkden=bknum;
+
+	// Calculate coefficient ak, new iterate x and new residuals r and rr
+	flop+=matrix->mult(P, Z);
+
+	double akden=0.0;
+	for(i=0;i<size;i++)
+	    akden+=Z[i]*P[i];
+	flop+=2*size;
+
+	double ak=bknum/akden;
+	for(i=0;i<size;i++){
+	    lhs[i]+=ak*P[i];
+	    R[i]-=ak*Z[i];
+	}
+	flop+=4*size;
+
+	err=R.vector_norm()/bnorm;
+	flop+=2*size;
+
+	errlist.add(err);
+
+	if(niter%10 == 0){
+	    iteration.set(niter);
+	    current_error.set(err);
+	    flops.set(flop);
+	    double time=timer.time();
+	    floprate.set((double)flop*1.e-6/time);
+
+	    append_values(niter, errlist, last_update);
+
+	    double progress=(log_orig-log(err))/(log_orig-log_targ);
+	    update_progress(progress);
+	}
+    }
+    iteration.set(niter);
+    current_error.set(err);
+    flops.set(flop);
+    time=timer.time();
+    floprate.set((double)flop*1.e-6/time);
+    TCL::execute(id+" finish_graph");
+    append_values(niter, errlist, last_update);
+}
+
+void SolveMatrix::append_values(int niter, const Array1<double>& errlist,
+				int& last_update)
+{
+    char buf[10000];
     ostrstream str(buf, 1000);
-    str << solverid << " update_iter " << iter << " " << first_error << " " << current_error << " " << final_error << '\0';
+    str << id << " append_graph " << niter << " \"";
+    for(int i=last_update;i<errlist.size();i++){
+	str << i << " " << errlist[i] << " ";
+    }
+    str << "\" ; update idletasks" << '\0';
     TCL::execute(str.str());
-#if 0
-    solport->send(ColumnMatrixHandle(solution.clone()));
-    module->multisend(solport);
-#endif
+    last_update=errlist.size();
 }
--- Modules/Matrix/SolveMatrix.tcl
+++ Modules/Matrix/SolveMatrix.tcl
@@ -6,6 +6,15 @@ itcl_class SolveMatrix {
 	set_defaults
     }
     method set_defaults {} {
+	global $this-target_error $this-method $this-orig_error
+	global $this-current_error $this-flops $this-floprate $this-iteration
+	set $this-target_error 1.e-4
+	set $this-method conjugate_gradient
+	set $this-orig_error 9.99999e99
+	set $this-current_error 9.99999e99
+	set $this-flops 0
+	set $this-floprate 0
+	set $this-iteration 0
     }
     method ui {} {
 	set w .ui$this
@@ -15,49 +24,105 @@ itcl_class SolveMatrix {
 	}
 
 	toplevel $w
-	wm minsize $w 200 20
+	wm minsize $w 300 20
 	set n "$this-c needexecute "
 
-	frame $w.iter
-	pack $w.iter -side top -fill x
-	label $w.iter.lab -text "Iteration: "
-	pack $w.iter.lab -side left
-	label $w.iter.val -text "0"
-	pack $w.iter.val -side right
+	make_labeled_radio $w.method "Solution Method" $n \
+		top $this-method \
+		{{"Jacobi" jacobi} \
+		{"Conjugate Gradient" conjugate_gradient}}
+
+	pack $w.method -side top -fill x -pady 2
+
+	expscale $w.target_error -orient horizontal -label "Target error:" \
+		-variable $this-target_error -command $n
+	pack $w.target_error -side top -fill x -pady 2
 
 
-	frame $w.first
-	pack $w.first -side top -fill x
-	label $w.first.lab -text "Original Error: "
-	pack $w.first.lab -side left
-	label $w.first.val -text "9.9999e99"
-	pack $w.first.val -side right
+	frame $w.converg -borderwidth 2 -relief ridge
+	pack $w.converg -side top -padx 2 -pady 2 -fill x
 
-	frame $w.current
-	pack $w.current -side top -fill x
-	label $w.current.lab -text "Current Error: "
-	pack $w.current.lab -side left
-	label $w.current.val -text "9.9999e99"
-	pack $w.current.val -side right
+	frame $w.converg.iter
+	pack $w.converg.iter -side top -fill x
+	label $w.converg.iter.lab -text "Iteration: "
+	pack $w.converg.iter.lab -side left
+	label $w.converg.iter.val -textvariable $this-iteration
+	pack $w.converg.iter.val -side right
 
-	frame $w.final
-	pack $w.final -side top -fill x
-	label $w.final.lab -text "Target Error: "
-	pack $w.final.lab -side left
-	label $w.final.val -text "9.9999e99"
-	pack $w.final.val -side right
+	frame $w.converg.first
+	pack $w.converg.first -side top -fill x
+	label $w.converg.first.lab -text "Original Error: "
+	pack $w.converg.first.lab -side left
+	label $w.converg.first.val -textvariable $this-orig_error
+	pack $w.converg.first.val -side right
 
+	frame $w.converg.current
+	pack $w.converg.current -side top -fill x
+	label $w.converg.current.lab -text "Current Error: "
+	pack $w.converg.current.lab -side left
+	label $w.converg.current.val -textvariable $this-current_error
+	pack $w.converg.current.val -side right
+
+	frame $w.converg.flopcount
+	pack $w.converg.flopcount -side top -fill x
+	label $w.converg.flopcount.lab -text "Flop Count: "
+	pack $w.converg.flopcount.lab -side left
+	label $w.converg.flopcount.val -textvariable $this-flops
+	pack $w.converg.flopcount.val -side right
+
+	frame $w.converg.floprate
+	pack $w.converg.floprate -side top -fill x
+	label $w.converg.floprate.lab -text "MFlops: "
+	pack $w.converg.floprate.lab -side left
+	label $w.converg.floprate.val -textvariable $this-floprate
+	pack $w.converg.floprate.val -side right
+
+	global $this-target_error
+	set err [set $this-target_error]
+
+	blt_graph $w.graph -title "Convergence" -height 250 \
+		-plotbackground gray50
+	$w.graph yaxis configure -logscale true -title "error (RMS)"
+	$w.graph xaxis configure -title "Iteration" \
+		-loose true
+
+	set iter 1
+	$w.graph element create "Target Error" -linewidth 1
+	$w.graph element configure "Target Error" -data "0 $err $iter $err"
+
+	pack $w.graph -fill x
     }
-    method update_iter {iter firsterr currenterr finalerr} {
+    method reset_graph {} {
 	set w .ui$this
-	if {[winfo exists $w]} {
-	    $w.iter.val config -text $iter
-	    $w.first.val config -text $firsterr
-	    $w.current.val config -text $currenterr
-	    $w.final.val config -text $finalerr
-	    update idletasks
+	if {![winfo exists $w]} {
+	    return
 	}
+	catch "$w.graph element delete {Target Error}"
+	catch "$w.graph element delete {Current Error}"
+	$w.graph element create "Target Error" -linewidth 0 -foreground blue
+	$w.graph element create "Current Error" -linewidth 0 -foreground red
+	global $this-target_error
+	set err [set $this-target_error]
+	set iter 1
+	$w.graph element configure "Target Error" -data "0 $err $iter $err"
+    }
+
+    method append_graph {iter values} {
+	set w .ui$this
+	if {![winfo exists $w]} {
+	    return
+	}
+	if {$values != ""} {
+	    $w.graph element append "Current Error" "$values"
+	}
+	global $this-target_error
+	set err [set $this-target_error]
+	$w.graph yaxis configure -min [expr $err/10]
+	$w.graph element configure "Target Error" -data "0 $err $iter $err"
+    }
+
+    method finish_graph {} {
+	set w .ui$this
+	$w.graph element configure "Current Error" -foreground green
     }
 }
-
-
