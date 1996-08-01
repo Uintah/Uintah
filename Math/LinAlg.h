5f97dce9a9e2a0e9e991c7d8b4978ca85e3b5927
Steven G. Parker <sparker@sci.utah.edu>
1996-08-01 04:38:26 +0000

    Change return types and add tridiagonal solver

--- Math/LinAlg.h
+++ Math/LinAlg.h
@@ -13,13 +13,19 @@
 #ifndef Math_LinAlg_h
 #define Math_LinAlg_h 1
 
+#ifdef __cplusplus
 extern "C" {
+#endif
     double linalg_norm2(int n, double* data);
-    double linalg_mult(int n, double* result, double* a, double* b);
-    double linalg_sub(int n, double* result, double* a, double* b);
+    void linalg_mult(int n, double* result, double* a, double* b);
+    void linalg_sub(int n, double* result, double* a, double* b);
     double linalg_dot(int n, double* a, double* b);
-    double linalg_smadd(int n, double* result, double s, double* a, double* b);
+    void linalg_smadd(int n, double* result, double s, double* a, double* b);
+    typedef double LinAlg_TriDiagRow[3];
+    void linalg_tridiag(int n, LinAlg_TriDiagRow* data, double* c);
+#ifdef __cplusplus
 };
+#endif
 
 #endif
 
