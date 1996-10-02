d9fbcb56a26916f11cf21673cdd3b11923b334df
No Author <null@sci.utah.edu>
1996-10-02 22:27:24 +0000

    This commit was manufactured by cvs2svn to create tag 'before_prune'.

--- gnupt/Queue_all.cc
+++
@@ -1,7 +0,0 @@
-
-#pragma implementation "Queue.h"
-
-#include <Classlib/Queue.cc>
-
-typedef Queue<int> _dummy1_;
-
--- gnupt/Stack_all.cc
+++
@@ -1,8 +0,0 @@
-
-#pragma implementation "Stack.h"
-
-#include <Classlib/Stack.cc>
-
-#include <Constraints/ConstraintSolver.h>
-
-typedef Stack<StackItem> _dummy0_;
