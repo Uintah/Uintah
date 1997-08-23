de66b7c8905ff37aedf4eb35f8c105fcd3aa332a
David Weinstein <dweinste@sci.utah.edu>
1997-08-23 04:02:27 +0000

    Works with updated Surface type

--- Modules/Surface/SurfToGeom.cc
+++ Modules/Surface/SurfToGeom.cc
@@ -18,6 +18,7 @@
 #include <Datatypes/ColorMapPort.h>
 #include <Datatypes/GeometryPort.h>
 #include <Datatypes/ScalarFieldPort.h>
+#include <Datatypes/SurfTree.h>
 #include <Datatypes/Surface.h>
 #include <Datatypes/SurfacePort.h>
 #include <Datatypes/ScalarField.h>
@@ -27,6 +28,7 @@
 #include <Geom/Geom.h>
 #include <Geom/Material.h>
 #include <Geom/Group.h>
+#include <Geom/Pt.h>
 #include <Geom/Sphere.h>
 #include <Geom/Tri.h>
 #include <Geom/Triangles.h>
@@ -39,10 +41,17 @@ class SurfToGeom : public Module {
     ColorMapIPort* icmap;
     GeometryOPort* ogeom;
 
+    Array1<MaterialHandle> c;
+
     TCLdouble range_min;
     TCLdouble range_max;
+    TCLdouble rad;
     TCLint best;
+    TCLint named;
     TCLint invert;
+    TCLint nodes;
+    TCLint resol;
+    TCLstring ntype;
     int have_sf, have_cm;
 
     void surf_to_geom(const SurfaceHandle&, GeomGroup*);
@@ -64,7 +73,9 @@ Module* make_SurfToGeom(const clString& id)
 SurfToGeom::SurfToGeom(const clString& id)
 : Module("SurfToGeom", id, Filter), range_min("range_min", id, this),
   range_max("range_max", id, this), best("best", id, this),
-  invert("invert", id, this)
+  invert("invert", id, this), nodes("nodes", id, this),
+  ntype("ntype", id, this), rad("rad", id, this), resol("resol", id, this),
+  named("named", id, this)
 {
     // Create the input port
     isurface=scinew SurfaceIPort(this, "Surface", SurfaceIPort::Atomic);
@@ -75,18 +86,29 @@ SurfToGeom::SurfToGeom(const clString& id)
     add_iport(icmap);
     ogeom=scinew GeometryOPort(this, "Geometry", GeometryIPort::Atomic);
     add_oport(ogeom);
+    c.resize(7);
+    c[0]=scinew Material(Color(.2,.2,.2),Color(.7,.1,.1),Color(.5,.5,.5),20);
+    c[1]=scinew Material(Color(.2,.2,.2),Color(.1,.7,.1),Color(.5,.5,.5),20);
+    c[2]=scinew Material(Color(.2,.2,.2),Color(.1,.1,.7),Color(.5,.5,.5),20);
+    c[3]=scinew Material(Color(.2,.2,.2),Color(.7,.7,.1),Color(.5,.5,.5),20);
+    c[4]=scinew Material(Color(.2,.2,.2),Color(.7,.1,.7),Color(.5,.5,.5),20);
+    c[5]=scinew Material(Color(.2,.2,.2),Color(.1,.7,.7),Color(.5,.5,.5),20);
+    c[6]=scinew Material(Color(.2,.2,.2),Color(.6,.6,.6),Color(.5,.5,.5),20);
 }
 
 SurfToGeom::SurfToGeom(const SurfToGeom&copy, int deep)
 : Module(copy, deep), range_min("range_min", id, this),
   range_max("range_max", id, this), best("best", id, this),
-  invert("invert", id, this)
+  invert("invert", id, this), nodes("nodes", id, this),
+  ntype("ntype", id, this), rad("rad", id, this), resol("resol", id, this),
+  named("named", id, this)
 {
     NOT_FINISHED("SurfToGeom::SurfToGeom");
 }
 
 SurfToGeom::~SurfToGeom()
 {
+	ogeom->delAll();
 }
 
 Module* SurfToGeom::clone(int deep)
@@ -100,11 +122,14 @@ MaterialHandle outmatl(new Material(Color(0,0,0), Color(0,0,0),
 void SurfToGeom::execute()
 {
     SurfaceHandle surf;
+    update_state(NeedData);
+    reset_vars();
     if (!isurface->get(surf)){
 	ogeom->delAll();
 	return;
     }
-
+    update_state(JustStarted);
+    reset_vars();
     ColorMapHandle cmap;
     int have_cm=icmap->get(cmap);
     ScalarFieldHandle sfield;
@@ -114,15 +139,25 @@ void SurfToGeom::execute()
     GeomGroup* spheres = scinew GeomGroup;
     GeomTrianglesPC* PCgroup = scinew GeomTrianglesPC;
     GeomTrianglesP* Pgroup = scinew GeomTrianglesP;
+    Array1<GeomTrianglesP* > PMgroup;
+    GeomPts* ptsGroup=scinew GeomPts(0);
 
     ScalarTriSurface* ss=surf->getScalarTriSurface();
     TriSurface* ts=surf->getTriSurface();
     PointsSurface* ps=surf->getPointsSurface();
+    SurfTree* st=surf->getSurfTree();
+
+    reset_vars();
+    int sph;
+    if (ntype.get() == "spheres") sph=1; else sph=0;
+    double radius=rad.get();
+    int res=resol.get();
+
+//    cerr << "sph="<<sph<<"   radius="<<radius<<"   resol="<<res<<" \n";
+
     if (ss) {
-	cerr << "Got it!\n";
 //	if (have_cm && cmap->non_diffuse_constant) {
 	if (have_cm) {
-	    cerr << "hiya\n";
 	    MaterialHandle mat1,mat2,mat3;
 	    double min, max;
 	    if (best.get()) {
@@ -146,110 +181,209 @@ void SurfToGeom::execute()
 	    }
 	    cerr << "min="<<min<<"  max="<<max<<"\n";
 	    double v1, v2, v3;
-	    for (int i=0; i< ss->elements.size(); i++) {
-		v1=ss->data[ss->elements[i]->i1];
-		v2=ss->data[ss->elements[i]->i2];
-		v3=ss->data[ss->elements[i]->i3];
-		mat1=cmap->lookup(v1);
-		mat2=cmap->lookup(v2);
-		mat3=cmap->lookup(v3);
-		PCgroup->add(ss->points[ss->elements[i]->i1],mat1->diffuse,
-			     ss->points[ss->elements[i]->i2],mat2->diffuse,
-			     ss->points[ss->elements[i]->i3],mat3->diffuse);
-		
+	    if (nodes.get()) {
+		if (!sph) { 
+		    ptsGroup=new GeomPts(3*ss->points.size());
+		    ptsGroup->pts.resize(3*ss->points.size());
+		}		
+		for (int i=0; i<ss->points.size(); i++) {
+		    if (sph) {
+			spheres->add(scinew GeomMaterial(scinew GeomSphere(ss->points[i], radius, res, res), cmap->lookup(ss->data[i])));
+		    } else {
+			Point newP=ss->points[i];
+			ptsGroup->pts[i*3]=newP.x();
+			ptsGroup->pts[i*3+1]=newP.y();
+			ptsGroup->pts[i*3+2]=newP.z();
+		    }
+		}
+	    } else {
+		for (int i=0; i< ss->elements.size(); i++) {
+		    v1=ss->data[ss->elements[i]->i1];
+		    v2=ss->data[ss->elements[i]->i2];
+		    v3=ss->data[ss->elements[i]->i3];
+		    mat1=cmap->lookup(v1);
+		    mat2=cmap->lookup(v2);
+		    mat3=cmap->lookup(v3);
+		    PCgroup->add(ss->points[ss->elements[i]->i1],mat1->diffuse,
+				 ss->points[ss->elements[i]->i2],mat2->diffuse,
+				 ss->points[ss->elements[i]->i3],mat3->diffuse);
+		    
+		}
 	    }
 	} else {
-	    cerr << "uhoh\n";
-	    for (int i=0; i< ss->elements.size(); i++) {
-		Pgroup->add(ss->points[ss->elements[i]->i1], 
-			    ss->points[ss->elements[i]->i2],
-			    ss->points[ss->elements[i]->i3]);
-		if (PCgroup) {
-		    delete PCgroup;
-		    PCgroup = 0;
+	    cerr << "no colormap\n";
+	    if (nodes.get()) {
+		if (!sph) { 
+		    ptsGroup=new GeomPts(3*ss->points.size());
+		    ptsGroup->pts.resize(3*ss->points.size());
+		}		
+		for (int i=0; i<ss->points.size(); i++) {
+		    if (sph) {
+			spheres->add(scinew GeomSphere(ss->points[i], radius, res, res));
+		    } else {
+			Point newP=ss->points[i];
+			ptsGroup->pts[i*3]=newP.x();
+			ptsGroup->pts[i*3+1]=newP.y();
+			ptsGroup->pts[i*3+2]=newP.z();
+		    }
+		}
+	    } else {
+		for (int i=0; i< ss->elements.size(); i++) {
+		    Pgroup->add(ss->points[ss->elements[i]->i1], 
+				ss->points[ss->elements[i]->i2],
+				ss->points[ss->elements[i]->i3]);
+		    if (PCgroup) {
+			delete PCgroup;
+			PCgroup = 0;
+		    }
 		}
 	    }
 	}
     } else if (ts) {
-	int ix=0;;
-	for (int i=0; i< ts->elements.size(); i++) {
-	    if (have_cm && have_sf) {
-		double interp;
-		MaterialHandle mat1,mat2,mat3;
-		int ok=1;
-		if (sfield->interpolate(ts->points[ts->elements[i]->i1], 
-					interp, ix, 1.e-4, 1.e-4)){
-		    mat1=cmap->lookup(interp);
-		} else {
-		    ix=0;
+	if (nodes.get()) {
+	    surf->monitor.read_lock();
+	    if (!sph) {
+		ptsGroup=new GeomPts(3*ts->points.size());
+		ptsGroup->pts.resize(3*ts->points.size());
+	    }		
+	    for (int i=0; i<ts->points.size(); i++) {
+		if (sph) {
+		    spheres->add(scinew GeomSphere(ts->points[i], radius, res, res));
+		} else {		      
+		    Point newP=ts->points[i];
+		    ptsGroup->pts[i*3]=newP.x();
+		    ptsGroup->pts[i*3+1]=newP.y();
+		    ptsGroup->pts[i*3+2]=newP.z();
+		}
+	    }
+	    surf->monitor.read_unlock();
+	} else {
+	    int ix=0;
+//	    cerr << "ts->elements.size()="<<ts->elements.size();
+//	    cerr << "   ts->points.size()="<<ts->points.size();
+	    for (int i=0; i< ts->elements.size(); i++) {
+		if (have_cm && have_sf) {
+		    double interp;
+		    MaterialHandle mat1,mat2,mat3;
+		    int ok=1;
 		    if (sfield->interpolate(ts->points[ts->elements[i]->i1], 
-					    interp, ix, 1.e-4, 30.)) {
+					    interp, ix, 1.e-4, 1.e-4)){
 			mat1=cmap->lookup(interp);
 		    } else {
-			mat1=outmatl; //ok=0;
+			ix=0;
+			if (sfield->interpolate(ts->points[ts->elements[i]->i1], 
+						interp, ix, 1.e-4, 30.)) {
+			    mat1=cmap->lookup(interp);
+			} else {
+			    mat1=outmatl; //ok=0;
+			}
 		    }
-		}
-		if (sfield->interpolate(ts->points[ts->elements[i]->i2], 
-				       interp, ix, 1.e-4, 1.e-4)){
-		    mat2=cmap->lookup(interp);
-		} else {
-		    ix=0;
 		    if (sfield->interpolate(ts->points[ts->elements[i]->i2], 
-				       interp, ix, 1.e-4, 30.)) {
+					    interp, ix, 1.e-4, 1.e-4)){
 			mat2=cmap->lookup(interp);
 		    } else {
-			mat2=outmatl; //ok=0;
+			ix=0;
+			if (sfield->interpolate(ts->points[ts->elements[i]->i2], 
+						interp, ix, 1.e-4, 30.)) {
+			    mat2=cmap->lookup(interp);
+			} else {
+			    mat2=outmatl; //ok=0;
+			}
 		    }
-		}
-		if (sfield->interpolate(ts->points[ts->elements[i]->i3], 
-				       interp, ix, 1.e-4, 1.e-4)){
-		    mat3=cmap->lookup(interp);
-		} else {
-		    ix=0;
-	  	    if (sfield->interpolate(ts->points[ts->elements[i]->i3], 
-				       interp, ix, 1.e-4, 30.)) {
+		    if (sfield->interpolate(ts->points[ts->elements[i]->i3], 
+					    interp, ix, 1.e-4, 1.e-4)){
 			mat3=cmap->lookup(interp);
 		    } else {
-			mat3=outmatl; //ok=0;
+			ix=0;
+			if (sfield->interpolate(ts->points[ts->elements[i]->i3], 
+						interp, ix, 1.e-4, 30.)) {
+			    mat3=cmap->lookup(interp);
+			} else {
+			    mat3=outmatl; //ok=0;
+			}
 		    }
-		}
-		if (ok) {
-		  if (cmap->non_diffuse_constant) {
-		    PCgroup->add(ts->points[ts->elements[i]->i1], mat1->diffuse,
-			       ts->points[ts->elements[i]->i2],mat2->diffuse,
-			       ts->points[ts->elements[i]->i3],mat3->diffuse);
-		    
-		  }
-		  else	
-		Pgroup->add(ts->points[ts->elements[i]->i1], 
-			    ts->points[ts->elements[i]->i2],
-			    ts->points[ts->elements[i]->i3]);
-//		    Pgroup->add(ts->points[ts->elements[i]->i1], mat1,
-//			       ts->points[ts->elements[i]->i2],mat2,
-//			       ts->points[ts->elements[i]->i3],mat3);
+		    if (ok) {
+			if (cmap->non_diffuse_constant) {
+			    PCgroup->add(ts->points[ts->elements[i]->i1], mat1->diffuse,
+					 ts->points[ts->elements[i]->i2],mat2->diffuse,
+					 ts->points[ts->elements[i]->i3],mat3->diffuse);
+			    
+			}
+			else	
+			    Pgroup->add(ts->points[ts->elements[i]->i1], 
+					ts->points[ts->elements[i]->i2],
+					ts->points[ts->elements[i]->i3]);
+			//		    Pgroup->add(ts->points[ts->elements[i]->i1], mat1,
+			//			       ts->points[ts->elements[i]->i2],mat2,
+			//			       ts->points[ts->elements[i]->i3],mat3);
+		    } else {
+			cerr << "One of the points was out of the field.\n";
+		    }
 		} else {
-		    cerr << "One of the points was out of the field.\n";
-		}
-	    } else {
-		Pgroup->add(ts->points[ts->elements[i]->i1], 
-			    ts->points[ts->elements[i]->i2],
-			    ts->points[ts->elements[i]->i3]);
-		if (PCgroup) {
-		    delete PCgroup;
-		    PCgroup = 0;
+		    Pgroup->add(ts->points[ts->elements[i]->i1], 
+				ts->points[ts->elements[i]->i2],
+				ts->points[ts->elements[i]->i3]);
+		    if (PCgroup) {
+			delete PCgroup;
+			PCgroup = 0;
+		    }
 		}
 	    }
 	}
     } else if(ps) {
-	Array1<NodeHandle> nodes;
-	ps->get_surfnodes(nodes);
-	for (int ii=0; ii<nodes.size(); ii++) {
-	    spheres->add(scinew GeomSphere(nodes[ii]->p, 5));
+	Array1<NodeHandle> nh;
+	ps->get_surfnodes(nh);
+
+//	cerr << "got "<<nh.size() <<" surfnodes.\n";
+	if (!sph) { 
+	    ptsGroup=new GeomPts(3*nh.size());
+	    ptsGroup->pts.resize(3*nh.size());
+	}		
+	for (int i=0; i<nh.size(); i++) {
+	    if (sph) {
+		spheres->add(scinew GeomSphere(nh[i]->p, radius, res, res));
+	    } else {	     
+		Point newP=nh[i]->p;
+		ptsGroup->pts[i*3]=newP.x();
+		ptsGroup->pts[i*3+1]=newP.y();
+		ptsGroup->pts[i*3+2]=newP.z();
+	    }
+	}
+    } else if (st) {
+	if (st->surfNames.size() != st->surfEls.size()) 
+	    st->surfNames.resize(st->surfEls.size());
+	if (nodes.get()) {
+	    if (!sph) { 
+		ptsGroup=new GeomPts(3*st->points.size());
+		ptsGroup->pts.resize(3*st->points.size());
+	    }		
+	    for (int i=0; i<st->points.size(); i++) {
+		if (sph) {
+		    spheres->add(scinew GeomSphere(st->points[i], radius, res, res));
+		} else {	     
+		    Point newP=st->points[i];
+		    ptsGroup->pts[i*3]=newP.x();
+		    ptsGroup->pts[i*3+1]=newP.y();
+		    ptsGroup->pts[i*3+2]=newP.z();
+		}
+	    }
+	} else {
+	    PMgroup.resize(st->surfEls.size());
+	    for (int i=0; i<PMgroup.size(); i++)
+		PMgroup[i] = scinew GeomTrianglesP;
+	    for (i=0; i< st->surfEls.size(); i++) {
+		if (!st->surfEls[i].size()) continue;
+		for (int j=0; j < st->surfEls[i].size(); j++) {
+		    int elIdx=st->surfEls[i][j];
+		    PMgroup[i]->add(st->points[st->elements[elIdx]->i1],
+				    st->points[st->elements[elIdx]->i2],
+				    st->points[st->elements[elIdx]->i3]);
+		}	
+	    }	
 	}
     } else {
 	error("Unknown representation for Surface in SurfToGeom");
     }
-//    GeomObj* topobj=group;
     GeomGroup* ngroup = scinew GeomGroup;
 
     if (PCgroup && PCgroup->size())
@@ -257,16 +391,21 @@ void SurfToGeom::execute()
     if (Pgroup && Pgroup->size())
 	ngroup->add(Pgroup);
     if (spheres && spheres->size())
-	ngroup->add(spheres);
-#if 0
-    // what is this for????
-    if (surf->name == "sagital.scalp") {
-	topobj=scinew GeomMaterial(group, scinew Material(Color(0,0,0),
-						    Color(0,.6,0), 
-						    Color(.5,.5,.5), 20));
-    }
-#endif
+	ngroup->add(scinew GeomMaterial(spheres, c[2]));
+    if (ptsGroup && ptsGroup->pts.size())
+	ngroup->add(ptsGroup);
     ogeom->delAll();
-//    ogeom->addObj(topobj, surf->name);
-    ogeom->addObj(ngroup,surf->name);
+    if (ngroup->size()) 
+	ogeom->addObj(scinew GeomMaterial(ngroup, c[0]), surf->name);
+
+    int nmd = named.get();
+    for (int i=0; i<PMgroup.size(); i++)
+	if (PMgroup[i]->size()) {
+	    if (st && st->surfNames[i] != clString("")) {
+		ogeom->addObj(scinew GeomMaterial(PMgroup[i], c[(st->matl[i])%7]), st->surfNames[i]);
+	    } else {
+		if (st && !nmd)
+		    ogeom->addObj(scinew GeomMaterial(PMgroup[i], c[(st->matl[i])%7]), clString("Surface ")+to_string(i));
+	    }
+	}
 }
--- Modules/Surface/SurfToGeom.tcl
+++ Modules/Surface/SurfToGeom.tcl
@@ -1,6 +1,3 @@
-
-catch {rename SurfToGeom ""}
-
 itcl_class SurfToGeom {
     inherit Module
     constructor {config} {
@@ -8,5 +5,65 @@ itcl_class SurfToGeom {
 	set_defaults
     }
     method set_defaults {} {
+	global $this-range_min
+	global $this-range_max
+	global $this-best
+	global $this-invert
+	global $this-ntype
+	global $this-rad
+	set $this-range_min -1
+	set $this-range_max 1
+	set $this-best 1
+	set $this-invert 0
+	set $this-nodes 0
+	set $this-named 0
+	set $this-ntype points
+	set $this-rad 1.0
+    }
+    method ui {} {
+	set w .ui$this
+	if {[winfo exists $w]} {
+	    raise $w
+	    return;
+	}
+	set n "$this-c needexecute"
+	global $this-range_min
+	global $this-range_max
+	global $this-nodes
+	global $this-ntype
+	global $this-rad
+	global $this-named
+	toplevel $w
+	frame $w.d
+	checkbutton $w.d.n -text "Render Nodes Only" -variable $this-nodes
+	make_labeled_radio $w.d.nodes "Render Nodes As: " "" \
+		left $this-ntype \
+		{{"Spheres" spheres} \
+		{"Points" points}}
+	checkbutton $w.d.named -text "Only Render Named (SurfTree)" \
+		-variable $this-named
+	
+	frame $w.d.sph -relief groove -borderwidth 2
+	expscale $w.d.sph.radius -orient horizontal -variable $this-rad \
+		-label "Radius:" \
+		-command "$this-c needexecute"
+	scale $w.d.sph.res -orient horizontal -variable $this-resol \
+		-label "Resolution:" -from 4 -to 20 \
+		-command "$this-c needexecute"
+	pack $w.d.sph.radius $w.d.sph.res -side left -fill x -expand 1
+	pack $w.d.n $w.d.nodes $w.d.named $w.d.sph -side top -fill x -expand 1
+	frame $w.f
+	expscale $w.f.min -orient horizontal -variable $this-range_min \
+		-label "range min:"
+	expscale $w.f.max -orient horizontal -variable $this-range_max \
+		-label "range max:"
+	pack $w.f.min $w.f.max -side left -expand 1
+	frame $w.b
+	global $this-best
+	global $this-invert
+	checkbutton $w.b.b -text Best -variable $this-best
+	checkbutton $w.b.i -text Invert -variable $this-invert
+	pack $w.b.b $w.b.i -side left -expand 1 -fill x
+	pack $w.d $w.f $w.b -side top -fill x
     }
 }
