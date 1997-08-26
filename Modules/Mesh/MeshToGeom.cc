124eac6e7f2bdfe4026adf3c25e6bc97234f5dbe
David Weinstein <dweinste@sci.utah.edu>
1997-08-26 23:33:41 +0000

    Pete added some stuff to this for me. -Dave

--- Modules/Mesh/MeshToGeom.cc
+++ Modules/Mesh/MeshToGeom.cc
@@ -1,6 +1,6 @@
 
 /*
- *  MeshToGeom.cc:  Convert a Meshace into geoemtry
+ *  MeshToGeom.cc:  Convert a Mesh into geoemtry
  *
  *  Written by:
  *   David Weinstein
@@ -19,6 +19,8 @@
 #include <Geom/Group.h>
 #include <Geom/Material.h>
 #include <Geom/Tri.h>
+#include <Geom/Triangles.h>
+#include <Geom/Pt.h>
 #include <Datatypes/MeshPort.h>
 #include <Datatypes/Mesh.h>
 #include <Malloc/Allocator.h>
@@ -73,26 +75,106 @@ void MeshToGeom::execute()
     if (!imesh->get(mesh))
 	return;
 
-    GeomGroup* group = scinew GeomGroup;
-    
-    for (int i=0; i<mesh->elems.size(); i++) {
-	group->add(scinew GeomTri(mesh->nodes[mesh->elems[i]->n[0]]->p,
-			       mesh->nodes[mesh->elems[i]->n[1]]->p,
-			       mesh->nodes[mesh->elems[i]->n[2]]->p));
-	group->add(scinew GeomTri(mesh->nodes[mesh->elems[i]->n[1]]->p,
-			       mesh->nodes[mesh->elems[i]->n[2]]->p,
-			       mesh->nodes[mesh->elems[i]->n[3]]->p));
-	group->add(scinew GeomTri(mesh->nodes[mesh->elems[i]->n[0]]->p,
-			       mesh->nodes[mesh->elems[i]->n[1]]->p,
-			       mesh->nodes[mesh->elems[i]->n[3]]->p));
-	group->add(scinew GeomTri(mesh->nodes[mesh->elems[i]->n[0]]->p,
-			       mesh->nodes[mesh->elems[i]->n[2]]->p,
-			       mesh->nodes[mesh->elems[i]->n[3]]->p));
+#if 0
+    GeomGroup* groups[7];
+    for(int i=0;i<7;i++) groups[i] = scinew GeomGroup;
+#else
+    GeomTrianglesP* groups[7];
+    for(int i=0;i<7;i++) groups[i] = scinew GeomTrianglesP;
+#endif
+    for (i=0; i<mesh->elems.size(); i++) {
+	if (mesh->elems[i]) {
+	    if ((mesh->nodes[mesh->elems[i]->n[0]].get_rep() == 0) ||
+		(mesh->nodes[mesh->elems[i]->n[1]].get_rep() == 0) ||
+		(mesh->nodes[mesh->elems[i]->n[2]].get_rep() == 0) ||
+		(mesh->nodes[mesh->elems[i]->n[3]].get_rep() == 0)) {
+		cerr << "Element shouldn't refer to empty node!\n";
+	    } else {
+		int cond = mesh->elems[i]->cond;
+#if 0
+	    groups[cond]->add(scinew GeomTri(mesh->nodes[mesh->elems[i]->n[0]]->p,
+				       mesh->nodes[mesh->elems[i]->n[1]]->p,
+				       mesh->nodes[mesh->elems[i]->n[2]]->p));
+	    groups[cond]->add(scinew GeomTri(mesh->nodes[mesh->elems[i]->n[1]]->p,
+				   mesh->nodes[mesh->elems[i]->n[2]]->p,
+				   mesh->nodes[mesh->elems[i]->n[3]]->p));
+	    groups[cond]->add(scinew GeomTri(mesh->nodes[mesh->elems[i]->n[0]]->p,
+				   mesh->nodes[mesh->elems[i]->n[1]]->p,
+				   mesh->nodes[mesh->elems[i]->n[3]]->p));
+	    groups[cond]->add(scinew GeomTri(mesh->nodes[mesh->elems[i]->n[0]]->p,
+				   mesh->nodes[mesh->elems[i]->n[2]]->p,
+				   mesh->nodes[mesh->elems[i]->n[3]]->p));
+
+#else
+	    groups[cond]->add(mesh->nodes[mesh->elems[i]->n[0]]->p,
+			      mesh->nodes[mesh->elems[i]->n[1]]->p,
+			      mesh->nodes[mesh->elems[i]->n[2]]->p);
+	    groups[cond]->add(mesh->nodes[mesh->elems[i]->n[1]]->p,
+			      mesh->nodes[mesh->elems[i]->n[2]]->p,
+			      mesh->nodes[mesh->elems[i]->n[3]]->p);
+	    groups[cond]->add(mesh->nodes[mesh->elems[i]->n[0]]->p,
+			      mesh->nodes[mesh->elems[i]->n[1]]->p,
+			      mesh->nodes[mesh->elems[i]->n[3]]->p);
+	    groups[cond]->add(mesh->nodes[mesh->elems[i]->n[0]]->p,
+			      mesh->nodes[mesh->elems[i]->n[2]]->p,
+			      mesh->nodes[mesh->elems[i]->n[3]]->p);
+#endif
+	}
+	} else {
+	    cerr << "Elements should have been packed!\n";
+	}
     }
-    GeomMaterial* matl=scinew GeomMaterial(group,
-					scinew Material(Color(0,0,0),
-						     Color(0,.6,0), 
-						     Color(.5,.5,.5), 20));
+    
+    GeomPts *pts[7];
+
+    for(i=0;i<7;i++)
+	pts[i] = scinew GeomPts(1);
+
+    for (i=0; i<mesh->elems.size(); i++) {
+	if (mesh->elems[i]) {
+	    pts[mesh->elems[i]->cond]->add(mesh->elems[i]->centroid());
+	}
+    }
+
+    GeomMaterial* matls[7];
+    GeomMaterial* matlsb[7];
+
+
     ogeom->delAll();
-    ogeom->addObj(matl, "Mesh1");
+	
+    Material *c[7];
+    c[0]=scinew Material(Color(.2,.2,.2),Color(.7,.1,.1),Color(.5,.5,.5),20);
+    c[1]=scinew Material(Color(.2,.2,.2),Color(.1,.7,.1),Color(.5,.5,.5),20);
+    c[2]=scinew Material(Color(.2,.2,.2),Color(.1,.1,.7),Color(.5,.5,.5),20);
+    c[3]=scinew Material(Color(.2,.2,.2),Color(.7,.7,.1),Color(.5,.5,.5),20);
+    c[4]=scinew Material(Color(.2,.2,.2),Color(.7,.1,.7),Color(.5,.5,.5),20);
+    c[5]=scinew Material(Color(.2,.2,.2),Color(.1,.7,.7),Color(.5,.5,.5),20);
+    c[6]=scinew Material(Color(.2,.2,.2),Color(.6,.6,.6),Color(.5,.5,.5),20);
+
+    for(i=0;i<7;i++) {
+	matls[i] = scinew GeomMaterial(pts[i],
+				       c[i]);
+
+	matlsb[i] = scinew GeomMaterial(groups[i],
+					c[i]);
+
+	clString tmps("Data ");
+	tmps += (char) ('0' + i);
+
+	clString tmpb("Tris ");
+	tmpb += (char) ('0' + i);
+
+	ogeom->addObj(matls[i],tmps());
+	ogeom->addObj(matlsb[i],tmpb());
+	
+    }	
+
+#if 0
+    GeomMaterial* matl=scinew GeomMaterial(group,
+					   scinew Material(Color(0,0,0),
+							   Color(0,.6,0), 
+							   Color(.5,.5,.5), 
+#endif							   20));
+//    ogeom->addObj(matl, "Mesh1");
+	
 }
