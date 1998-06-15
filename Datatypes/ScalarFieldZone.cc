1620b15617a0344fc721178b2b274d273043ae96
Steven G. Parker <sparker@sci.utah.edu>
1998-06-15 17:23:02 +0000

    new code

--- Datatypes/ScalarFieldZone.cc
+++ Datatypes/ScalarFieldZone.cc
@@ -14,6 +14,14 @@
 #include <Datatypes/ScalarFieldZone.h>
 #include <Classlib/NotFinished.h>
 #include <Classlib/String.h>
+#include <Malloc/Allocator.h>
+
+static Persistent* maker()
+{
+    return scinew ScalarFieldZone(0);
+}
+
+PersistentTypeID ScalarFieldZone::type_id("ScalarFieldZone", "ScalarField", maker);
 
 ScalarFieldZone::ScalarFieldZone(int nzones)
 : ScalarField(Zones), zones(nzones)
@@ -32,12 +40,28 @@ ScalarField* ScalarFieldZone::clone()
 
 void ScalarFieldZone::compute_bounds()
 {
-    NOT_FINISHED("ScalarFieldZone::compute_bounds");
+    if(zones.size()==0)
+	return;
+    zones[0]->get_bounds(bmin, bmax);
+    for(int i=1;i<zones.size();i++){
+	Point min, max;
+	zones[i]->get_bounds(min, max);
+	bmin=Min(min, bmin);
+	bmax=Max(max, bmax);
+    }
 }
 
 void ScalarFieldZone::compute_minmax()
 {
-    NOT_FINISHED("ScalarFieldZone::compute_minmax");
+    if(zones.size()==0)
+	return;
+    zones[0]->get_minmax(data_min, data_max);
+    for(int i=1;i<zones.size();i++){
+	double  zmin, zmax;
+	zones[i]->get_minmax(zmin, zmax);
+	data_min=Min(data_min, zmin);
+	data_max=Max(data_max, zmax);
+    }
 }
 
 Vector ScalarFieldZone::gradient(const Point&)
@@ -46,17 +70,23 @@ Vector ScalarFieldZone::gradient(const Point&)
     return Vector(0,0,0);
 }
 
-int ScalarFieldZone::interpolate(const Point&, double&, double epsilon1,
+int ScalarFieldZone::interpolate(const Point& p, double& v, double epsilon1,
 				 double epsilon2)
 {
-    NOT_FINISHED("ScalarFieldZone::interpolate");
+    for(int i=0;i<zones.size();i++){
+	if(zones[i]->interpolate(p, v, epsilon1, epsilon2))
+	    return 1;
+    }
     return 0;
 }
 
-int ScalarFieldZone::interpolate(const Point&, double&, int& ix,
+int ScalarFieldZone::interpolate(const Point& p, double& v, int& cache,
 				 double epsilon1, double epsilon2)
 {
-    NOT_FINISHED("ScalarFieldZone::interpolate");
+    for(int i=0;i<zones.size();i++){
+	if(zones[i]->interpolate(p, v, cache, epsilon1, epsilon2))
+	    return 1;
+    }
     return 0;
 }
 
