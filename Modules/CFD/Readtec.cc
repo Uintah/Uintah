621eb766e148593d1799228b19992f606ca95821
Phil Sutton <psutton@sci.utah.edu>
1998-10-13 21:41:41 +0000

    fixed parser - it now empties varnames and zonelist before a read, so things don't pile up and hog memory.

--- Modules/CFD/Readtec.cc
+++ Modules/CFD/Readtec.cc
@@ -37,7 +37,7 @@
 // A Zone contains an array with all the variable values, index by
 // variable number
 struct Zone {
-  int i, j;
+  int i, j, k;
   Array2<double> varvals;
 };
 
@@ -90,7 +90,7 @@ private:
   int readfile( char *filename, int *index );
   void CreateSFgrid( Zone *zone );
   void CreateVFgrid( Zone *zone );
-  void CreatePS( Zone *zone, int I, int J );
+  void CreatePS( Zone *zone, int I, int J, int K );
   void initvars();
   void checkfluidnum( char *name);
 public:
@@ -203,19 +203,25 @@ int Readtec::readfile( char *filename, int *index ) {
 	  getnext = 0;
 	}
 
-	varnames.add(name);
-	i++;
-	ASSERT( i < MAXVARS );
+	// only add variable name if it's not empty
+	// because sometimes we get variable lists that look like:
+	//   ...MO2Y,SN2X,SN2Y,,T3,E3,RHO3,...
+	//                    ^^
+	if( name[0] != '\0' ) {
+	  varnames.add(name);
+	  i++;
+	  ASSERT( i < MAXVARS );
+	}
       }
       numvars = i;
-//    for displaying variable list
-//      int j;
-//      for(j = 0; j < numvars; j++ ) 
-//	cout << varnames[j] << endl;
+      // for displaying variable list
+      //      int j;
+      //      for(j = 0; j < numvars; j++ ) 
+      //	cout << varnames[j] << endl;
 
     } else if( !strcmp( tok, "ZONE" ) ) {
       char *p = buf;
-      int I = 0, J = 1, k;
+      int I = 0, J = 1, K = 1, k;
       int block;
       char num[LINEMAX];
       char title[LINEMAX];
@@ -254,6 +260,19 @@ int Readtec::readfile( char *filename, int *index ) {
 	  num[k] = *p;
 	ASSERT(k<LINEMAX);
 	J = atoi(num);
+
+	// now find K, if it's there
+	while( ( isspace( *p ) || *p == ',' ) && *p != 0 ) p++; 
+	if( *p == 'K' ) {
+	  // find K
+	  while( *p != 'K' && *p != 0 ) p++;
+	  p++;
+	  while( (isspace( *p ) || *p == '=' ) && *p != 0 ) p++;
+	  for( k = 0; *p != ',' && *p != 0; p++, k++ )
+	    num[k] = *p;
+	  ASSERT(k<LINEMAX);
+	  K = atoi(num);
+	}
 	block = 1;
       } else if( *p == 'F' ) {
 	block = 0;
@@ -268,23 +287,39 @@ int Readtec::readfile( char *filename, int *index ) {
       int zindex;
       if( block ) {
 
-	zone1.varvals.newsize(MAXVARS,I*J);
+	zone1.varvals.newsize(MAXVARS,I*J*K);
 	// in a block, all values for a variable are listed together
 	for( zindex = 0; zindex < numvars; zindex++ ) {
-	  int i, j;
+	  int i, j, k;
 	  
-	  // read in all the values
-	  for( j = 0; j < J; j++ ) {
-	    for ( i = 0; i < I; i++ ) {
-	      char ch;
-	      while( isspace( in.peek() ) ) in.get(ch);
-	      gettoken( in, buf, LINEMAX );
-	      zone1.varvals(zindex,j*I+i) = atof(buf);
+	  if ( K == 1 ){
+	    // read in all the 2D values 
+	    for( j = 0; j < J; j++ ) {
+	      for ( i = 0; i < I; i++ ) {
+		char ch;
+		while( isspace( in.peek() ) ) in.get(ch);
+		gettoken( in, buf, LINEMAX );
+		zone1.varvals(zindex,j*I+i) = atof(buf);
+	      }
 	    }
-	  }
-	  zone1.i = I;
-	  zone1.j = J;
-	  
+	    zone1.i = I;
+	    zone1.j = J;
+	  } else {
+	    // read in all the 3D values 
+	    for( k = 0; k < K; k++ ) {
+	      for( j = 0; j < J; j++ ) {
+		for ( i = 0; i < I; i++ ) {
+		  char ch;
+		  while( isspace( in.peek() ) ) in.get(ch);
+		  gettoken( in, buf, LINEMAX );
+		  zone1.varvals(zindex,k*(I*J)+j*I+i) = atof(buf);
+		}
+	      }
+	    }
+	    zone1.i = I;
+	    zone1.j = J;
+	    zone1.k = K;
+	  } 
 	} // end for zindex
 	*index = zonelist.size();
       } else {
@@ -304,6 +339,7 @@ int Readtec::readfile( char *filename, int *index ) {
 	}
 	zone1.i = I;
 	zone1.j = 1;
+	zone1.k = 1;
 	
       } // end if block
 
@@ -313,6 +349,7 @@ int Readtec::readfile( char *filename, int *index ) {
     } // end if !strcmp( tok, "ZONE" )
 
   } // end while in.getline
+
   return 0;
 }  
 
@@ -328,32 +365,46 @@ void Readtec::execute()
   
   if( strcmp( filename, lastfileread ) ) {
     // new file - open and read.  Return if hit an error.
+    // first clear all variables
+    varnames.remove_all();
+    zonelist.remove_all();
     if( readfile( filename, &zoneindex ) )
       return;
     strcpy( lastfileread, filename );
   }    
 
   // set up the grids
-  Zone z = zonelist[zoneindex];
+  Zone *z = &zonelist[zoneindex];
   sfgrid = new ScalarFieldRG();
-  sfgrid->resize(z.i,z.j,2);
-  CreateSFgrid( &z );
+  if( z->k > 0 )
+    sfgrid->resize(z->i,z->j,z->k);
+  else
+    sfgrid->resize(z->i,z->j,2);
+  CreateSFgrid( z );
 
-  sfgrid->set_bounds(Point(0,0,0), Point(z.i, z.j, z.i) );
+  if( z->k > 0 )
+    sfgrid->set_bounds(Point(0,0,0), Point(z->i, z->j, z->k) );
+  else
+    sfgrid->set_bounds(Point(0,0,0), Point(z->i, z->j, z->i) );    
   sfgrid->compute_minmax();
 
   vfgrid = new VectorFieldRG();
-  vfgrid->resize(z.i,z.j,2);
-  CreateVFgrid( &z );
-  vfgrid->set_bounds(Point(0,0,0), Point(z.i,z.j,z.i) );
+  if( z->k > 0 )
+    vfgrid->resize(z->i,z->j,z->k);
+  else
+    vfgrid->resize(z->i,z->j,2);
+  CreateVFgrid( z );
+  if( z->k > 0 )
+    vfgrid->set_bounds(Point(0,0,0), Point(z->i,z->j,z->k) );
+  else
+    vfgrid->set_bounds(Point(0,0,0), Point(z->i,z->j,z->i) );
 
   pset = new cfdlibParticleSet();
   if( pfluid.get() - 1 >= zonelist.size() ) {
     cerr << "Error - particle set/fluid " << pfluid.get()
 	 << " does not exist" << endl;
   } else {
-    Zone z1 = zonelist[ pfluid.get() - 1 ];
-    CreatePS( &z1, z.i, z.j );
+    CreatePS( &zonelist[ pfluid.get() - 1 ], z->i, z->j, z->k );
   }
 
   sfout->send( ScalarFieldHandle(sfgrid) );
@@ -417,68 +468,102 @@ void Readtec::CreateSFgrid( Zone *zone ) {
     fprintf( stderr, "Error - variable %s not found\n", vname );
     return;
   }
-  int x,y;
-  for( y = 0; y < zone->j; y++ ) {
-    for( x = 0; x < zone->i; x++ ) {
-      sfgrid->grid(x,y,0) = zone->varvals(i, y*zone->i + x);
-      sfgrid->grid(x,y,1) = zone->varvals(i, y*zone->i + x);
+
+  int x,y,z;
+  if( zone->k > 0 ) {
+    for( z = 0; z < zone->k; z++ ) {
+      for( y = 0; y < zone->j; y++ ) {
+	for( x = 0; x < zone->i; x++ ) {
+	  sfgrid->grid(x,y,z) = 
+	    zone->varvals(i,z*(zone->i*zone->j) + y*zone->i + x);
+	}
+      }
+    }
+  } else {
+    for( y = 0; y < zone->j; y++ ) {
+      for( x = 0; x < zone->i; x++ ) {
+	sfgrid->grid(x,y,0) = zone->varvals(i, y*zone->i + x);
+	sfgrid->grid(x,y,1) = zone->varvals(i, y*zone->i + x);
+      }
     }
   }
-
+  
 }
 
 void Readtec::CreateVFgrid( Zone *zone ) {
   char vname1[VARNAMELEN];
   char vname2[VARNAMELEN];
+  char vname3[VARNAMELEN];
   int vnum = vvar.get();
   
   switch( vnum ) {
   case 0: sprintf( vname1, "U%d", vfluid.get() );
-    sprintf( vname2, "V%d", vfluid.get() ); break;
+    sprintf( vname2, "V%d", vfluid.get() ); 
+    sprintf( vname3, "W%d", vfluid.get() ); 
+    break;
   case 1: sprintf( vname1, "MO%dX", vfluid.get() );
-    sprintf( vname2, "MO%dY", vfluid.get() ); break;
+    sprintf( vname2, "MO%dY", vfluid.get() ); 
+    sprintf( vname3, "MO%dZ", vfluid.get() ); 
+    break;
   default:
     fprintf( stderr, "Error - vector variable #%d for fluid %d not found",
 	     vnum, vfluid.get() );
     return;
   }
 
-  int i, j;
+  int i, j, k;
   for( i = 0; i < numvars && strcmp(varnames[i],vname1); i++ );
   for( j = 0; j < numvars && strcmp(varnames[j],vname2); j++ );
+  for( k = 0; k < numvars && strcmp(varnames[k],vname3); k++ );
   if( i == 0 || j == 0 ) {
     fprintf( stderr, "Error - one or both of %s, %s not found\n", vname1,
 	     vname2 );
     return;
   }
 
-  int x, y;
-  for( y = 0; y < zone->j; y++ ) {
-    for( x = 0; x < zone->i; x++ ) {
-      vfgrid->grid(x,y,0) = Vector( zone->varvals(i, y*zone->i + x),
-				    zone->varvals(j, y*zone->i + x),0 );
-      vfgrid->grid(x,y,1) = Vector( zone->varvals(i, y*zone->i + x),
-				    zone->varvals(j, y*zone->i + x),0 );
+  int x, y, z;
+  if( zone->k > 0 ) {
+    for( z = 0; z < zone->k; z++ ) {
+      for( y = 0; y < zone->j; y++ ) {
+	for( x = 0; x < zone->i; x++ ) {
+	  vfgrid->grid(x,y,z) = 
+	    Vector(zone->varvals(i, z*(zone->i * zone->j) + y*zone->i + x),
+		   zone->varvals(j, z*(zone->i * zone->j) + y*zone->i + x),
+		   zone->varvals(k, z*(zone->i * zone->j) + y*zone->i + x));
+	}
+      }
+    }
+  } else {
+    for( y = 0; y < zone->j; y++ ) {
+      for( x = 0; x < zone->i; x++ ) {
+	vfgrid->grid(x,y,0) = Vector( zone->varvals(i, y*zone->i + x),
+				      zone->varvals(j, y*zone->i + x),0 );
+	vfgrid->grid(x,y,1) = Vector( zone->varvals(i, y*zone->i + x),
+				      zone->varvals(j, y*zone->i + x),0 );
+      }
     }
   }
-      
+  
 }
 
-void Readtec::CreatePS( Zone *zone, int I, int J ) {
+void Readtec::CreatePS( Zone *zone, int I, int J, int K ) {
   cfdlibTimeStep *ts = new cfdlibTimeStep;
   ts->time = 0.0;
   int i;
-
+  
   // find indices of the X and Y variables
   int xindex;
-  for( xindex = 0; xindex < MAXVARS && strcmp( varnames[xindex], "X" );
+  for( xindex = 0; xindex < varnames.size() && strcmp( varnames[xindex], "X" );
        xindex++ );
-  ASSERT(xindex < MAXVARS);
+  ASSERT(xindex < varnames.size());
   int yindex;
-  for( yindex = 0; yindex < MAXVARS && strcmp( varnames[yindex], "Y" );
+  for( yindex = 0; yindex < varnames.size() && strcmp( varnames[yindex], "Y" );
        yindex++ );
-  ASSERT(yindex < MAXVARS);
-
+  ASSERT(yindex < varnames.size());
+  int zindex;
+  for( zindex = 0; zindex < varnames.size() && strcmp( varnames[zindex], "Z" );
+       zindex++ );
+  
   // find variable name
   char varname[VARNAMELEN];
   if( numfluids == 1 )
@@ -486,20 +571,30 @@ void Readtec::CreatePS( Zone *zone, int I, int J ) {
   else if( pvar.get() == 0 )
     sprintf( varname, "P" );
   else
-    sprintf( varname, "%s%d", svartable[ pvar.get() ], pfluid.get() );
+    //    sprintf( varname, "%s%d", svartable[ pvar.get() ], pfluid.get() );
+    // Each particle set thinks it's #1
+    sprintf( varname, "%s%d", svartable[ pvar.get() ], 1 );
 
   // find variable in zone
   int varindex;
-  for( varindex = 0; varindex < MAXVARS &&
+  for( varindex = 0; varindex < varnames.size() &&
 	 strcmp( varnames[varindex], varname ); varindex++ );
-  ASSERT( varindex < MAXVARS );
+  if( varindex == varnames.size() ) {
+    fprintf( stderr, "Error - variable %s not found\n", varname );
+    return;
+  }
 
   ts->positions.resize( zone->i );
   ts->scalars.resize( zone->i );
   for( i = 0; i < zone->i; i++ ) {
-    ts->positions[i] = Vector( (double)I*zone->varvals(xindex,i)/(double)I,
-			       (double)J*zone->varvals(yindex,i)/(double)J,
-    			       (double)I/2.0 );
+    if( zindex < varnames.size() )
+      ts->positions[i] = Vector( (double)I*zone->varvals(xindex,i)/(double)I,
+				 (double)J*zone->varvals(yindex,i)/(double)J,
+				 (double)K*zone->varvals(zindex,i)/(double)K );
+    else
+      ts->positions[i] = Vector( (double)I*zone->varvals(xindex,i)/(double)I,
+				 (double)J*zone->varvals(yindex,i)/(double)J,
+				 (double)I/2.0 );
     ts->scalars[i] = zone->varvals( varindex, i );
   }
   pset->add(ts);
