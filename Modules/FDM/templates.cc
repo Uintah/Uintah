23c434ad56acc6e695448aabaf083b27fdf79f05
Steven G. Parker <sparker@sci.utah.edu>
1999-02-19 00:02:14 +0000

    Linux port Broke Datatypes/Colormap interdependency Fixed tons of missing symbols, etc.

---
+++ Modules/FDM/templates.cc
@@ -0,0 +1,10 @@
+
+/*
+ * Manual template instantiations for g++
+ */
+
+#include <Classlib/Array1.cc>
+#include <Classlib/HashTable.cc>
+
+template class Array1<char>;
+template class HashTable<int, double>;
