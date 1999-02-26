7752c5e771dcbd2e1d215991b2edd177ed86a619
David Weinstein <dmw@sci.utah.edu>
1999-02-26 20:59:33 +0000

    more stuff needed for source localization

---
+++ Datatypes/KludgeMessagePort.cc
@@ -0,0 +1,7 @@
+#include <Datatypes/KludgeMessagePort.h>
+
+clString SimpleIPort<KludgeMessageHandle>::port_type("KludgedMessage");
+clString SimpleIPort<KludgeMessageHandle>::port_color("SteelBlue2");
+
+clString SimpleIPort<AmoebaMessageHandle>::port_type("AmoebaMessage");
+clString SimpleIPort<AmoebaMessageHandle>::port_color("SteelBlue4");
