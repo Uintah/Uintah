4c338d85874c74a17abcf74660ea7bcd35d53075
David Weinstein <dmw@sci.utah.edu>
1999-04-22 20:29:19 +0000

    Removed old files.  Updated two modules.

--- Modules/Fields/PermuteField.cc
+++ Modules/Fields/PermuteField.cc
@@ -190,16 +190,16 @@ void PermuteField::execute()
 		ofc->resize(nx,nz,ny);
 		ofb=ofc;
 	    }
-	    ofb->set_bounds(Point(min.x(), min.y(), min.z()), 
-			    Point(max.x(), max.y(), max.z()));
+	    ofb->set_bounds(Point(min.x(), min.z(), min.y()), 
+			    Point(max.x(), max.z(), max.y()));
 	    for (int i=0, ii=basex; i<nx; i++, ii+=incrx)
 		for (int j=0, jj=basey; j<ny; j++, jj+=incry)
 		    for (int k=0, kk=basez; k<nz; k++, kk+=incrz)
-			if (ofd) ofd->grid(i,j,k)=ifd->grid(ii,kk,jj);
-			else if (off) off->grid(i,j,k)=iff->grid(ii,kk,jj);
-			else if (ofi) ofi->grid(i,j,k)=ifi->grid(ii,kk,jj);
-			else if (ofs) ofs->grid(i,j,k)=ifs->grid(ii,kk,jj);
-			else if (ofc) ofc->grid(i,j,k)=ifc->grid(ii,kk,jj);
+			if (ofd) ofd->grid(i,k,j)=ifd->grid(ii,jj,kk);
+			else if (off) off->grid(i,k,j)=iff->grid(ii,jj,kk);
+			else if (ofi) ofi->grid(i,k,j)=ifi->grid(ii,jj,kk);
+			else if (ofs) ofs->grid(i,k,j)=ifs->grid(ii,jj,kk);
+			else if (ofc) ofc->grid(i,k,j)=ifc->grid(ii,jj,kk);
 	} else if (map=="213") {
 	    if (ifd) {
 		ofd=scinew ScalarFieldRGdouble(); 
@@ -222,16 +222,16 @@ void PermuteField::execute()
 		ofc->resize(ny,nx,nz);
 		ofb=ofc;
 	    }
-	    ofb->set_bounds(Point(min.x(), min.y(), min.z()), 
-			    Point(max.x(), max.y(), max.z()));
+	    ofb->set_bounds(Point(min.y(), min.x(), min.z()), 
+			    Point(max.y(), max.x(), max.z()));
 	    for (int i=0, ii=basex; i<nx; i++, ii+=incrx)
 		for (int j=0, jj=basey; j<ny; j++, jj+=incry)
 		    for (int k=0, kk=basez; k<nz; k++, kk+=incrz)
 			if (ofd) ofd->grid(i,j,k)=ifd->grid(jj,ii,kk);
-			else if (off) off->grid(i,j,k)=iff->grid(jj,ii,kk);
-			else if (ofi) ofi->grid(i,j,k)=ifi->grid(jj,ii,kk);
-			else if (ofs) ofs->grid(i,j,k)=ifs->grid(jj,ii,kk);
-			else if (ofc) ofc->grid(i,j,k)=ifc->grid(jj,ii,kk);
+			else if (off) off->grid(j,i,k)=iff->grid(ii,jj,kk);
+			else if (ofi) ofi->grid(j,i,k)=ifi->grid(ii,jj,kk);
+			else if (ofs) ofs->grid(j,i,k)=ifs->grid(ii,jj,kk);
+			else if (ofc) ofc->grid(j,i,k)=ifc->grid(ii,jj,kk);
 	} else if (map=="231") {
 	    if (ifd) {
 		ofd=scinew ScalarFieldRGdouble(); 
@@ -254,19 +254,19 @@ void PermuteField::execute()
 		ofc->resize(ny,nz,nx);
 		ofb=ofc;
 	    }
-	    ofb->set_bounds(Point(min.x(), min.y(), min.z()), 
-			    Point(max.x(), max.y(), max.z()));
+	    ofb->set_bounds(Point(min.y(), min.z(), min.x()), 
+			    Point(max.y(), max.z(), max.x()));
 	    for (int i=0, ii=basex; i<nx; i++, ii+=incrx)
 		for (int j=0, jj=basey; j<ny; j++, jj+=incry)
 		    for (int k=0, kk=basez; k<nz; k++, kk+=incrz)
-			if (ofd) ofd->grid(i,j,k)=ifd->grid(ii,jj,kk);
-			else if (off) off->grid(i,j,k)=iff->grid(jj,kk,ii);
-			else if (ofi) ofi->grid(i,j,k)=ifi->grid(jj,kk,ii);
-			else if (ofs) ofs->grid(i,j,k)=ifs->grid(jj,kk,ii);
-			else if (ofc) ofc->grid(i,j,k)=ifc->grid(jj,kk,ii);
+			if (ofd) ofd->grid(j,k,i)=ifd->grid(ii,jj,kk);
+			else if (off) off->grid(j,k,i)=iff->grid(ii,jj,kk);
+			else if (ofi) ofi->grid(j,k,i)=ifi->grid(ii,jj,kk);
+			else if (ofs) ofs->grid(j,k,i)=ifs->grid(ii,jj,kk);
+			else if (ofc) ofc->grid(j,k,i)=ifc->grid(ii,jj,kk);
 	} else if (map=="312") {
 	    if (ifd) {
-		ofd=scinew ScalarFieldRGdouble(); 
+		ofd=scinew ScalarFieldRGdouble();
 		ofd->resize(nz,nx,ny);
 		ofb=ofd;
 	    } else if (iff) {
@@ -286,16 +286,16 @@ void PermuteField::execute()
 		ofc->resize(nz,nx,ny);
 		ofb=ofc;
 	    }
-	    ofb->set_bounds(Point(min.x(), min.y(), min.z()), 
-			    Point(max.x(), max.y(), max.z()));
+	    ofb->set_bounds(Point(min.z(), min.x(), min.y()), 
+			    Point(max.z(), max.x(), max.y()));
 	    for (int i=0, ii=basex; i<nx; i++, ii+=incrx)
 		for (int j=0, jj=basey; j<ny; j++, jj+=incry)
 		    for (int k=0, kk=basez; k<nz; k++, kk+=incrz)
-			if (ofd) ofd->grid(i,j,k)=ifd->grid(kk,ii,jj);
-			else if (off) off->grid(i,j,k)=iff->grid(kk,ii,jj);
-			else if (ofi) ofi->grid(i,j,k)=ifi->grid(kk,ii,jj);
-			else if (ofs) ofs->grid(i,j,k)=ifs->grid(kk,ii,jj);
-			else if (ofc) ofc->grid(i,j,k)=ifc->grid(kk,ii,jj);
+			if (ofd) ofd->grid(k,i,j)=ifd->grid(ii,jj,kk);
+			else if (off) off->grid(k,i,j)=iff->grid(ii,jj,kk);
+			else if (ofi) ofi->grid(k,i,j)=ifi->grid(ii,jj,kk);
+			else if (ofs) ofs->grid(k,i,j)=ifs->grid(ii,jj,kk);
+			else if (ofc) ofc->grid(k,i,j)=ifc->grid(ii,jj,kk);
 	} else if (map=="321") {
 	    if (ifd) {
 		ofd=scinew ScalarFieldRGdouble(); 
@@ -318,16 +318,16 @@ void PermuteField::execute()
 		ofc->resize(nz,ny,nx);
 		ofb=ofc;
 	    }
-	    ofb->set_bounds(Point(min.x(), min.y(), min.z()), 
-			    Point(max.x(), max.y(), max.z()));
+	    ofb->set_bounds(Point(min.z(), min.y(), min.x()), 
+			    Point(max.z(), max.y(), max.x()));
 	    for (int i=0, ii=basex; i<nx; i++, ii+=incrx)
 		for (int j=0, jj=basey; j<ny; j++, jj+=incry)
 		    for (int k=0, kk=basez; k<nz; k++, kk+=incrz)
-			if (ofd) ofd->grid(i,j,k)=ifd->grid(kk,jj,ii);
-			else if (off) off->grid(i,j,k)=iff->grid(kk,jj,ii);
-			else if (ofi) ofi->grid(i,j,k)=ifi->grid(kk,jj,ii);
-			else if (ofs) ofs->grid(i,j,k)=ifs->grid(kk,jj,ii);
-			else if (ofc) ofc->grid(i,j,k)=ifc->grid(kk,jj,ii);
+			if (ofd) ofd->grid(k,j,i)=ifd->grid(kk,jj,ii);
+			else if (off) off->grid(k,j,i)=iff->grid(kk,jj,ii);
+			else if (ofi) ofi->grid(k,j,i)=ifi->grid(kk,jj,ii);
+			else if (ofs) ofs->grid(k,j,i)=ifs->grid(kk,jj,ii);
+			else if (ofc) ofc->grid(k,j,i)=ifc->grid(kk,jj,ii);
 
 //	    outFld->resize(nz,ny,nx);
 //	    outFld->grid(i,j,k)=sfrg->grid(kk,jj,ii);
--- Modules/Fields/TransformField.cc
+++ Modules/Fields/TransformField.cc
@@ -18,71 +18,32 @@
 #include <Datatypes/ScalarFieldRG.h>
 #include <Datatypes/ScalarFieldPort.h>
 #include <Geometry/BBox.h>
+#include <Geometry/Transform.h>
 #include <TCL/TCLvar.h>
 #include <iostream.h>
 #include <stdio.h>
 #include <Malloc/Allocator.h>
 
-static Point transformPt(double m[4][4], const Point& p) {
-    double pIn[4];
-    double pOut[4];
-    pIn[0]=p.x(); pIn[1]=p.y(); pIn[2]=p.z(); pIn[3]=1;
-    pOut[0]=pOut[1]=pOut[2]=pOut[3]=0;
-
+static void MatToTransform(MatrixHandle mH, Transform& t) {
+    double a[16];
+    double *p=&(a[0]);
     for (int i=0; i<4; i++)
 	for (int j=0; j<4; j++)
-	    pOut[i] += m[i][j]*pIn[j];
-
-    return Point(pOut[0]/pOut[3], pOut[1]/pOut[3], pOut[2]/pOut[3]);
+	    *p++=(*mH.get_rep())[i][j];
+    t.set(a);
 }
 
-static void buildTransformMatrix(double m[4][4], MatrixHandle mH) {
+#if 0
+static void TransformToMat(Transform& t, MatrixHandle mH) {
+    double a[16];
+    t.get(a);
+    double *p=&(a[0]);
     for (int i=0; i<4; i++)
 	for (int j=0; j<4; j++)
-	    m[i][j]=(*mH.get_rep())[i][j];
+	    (*mH.get_rep())[i][j]=*p++;
 }
+#endif 
 
-static void buildInverseMatrix(double mA[4][4], double mI[4][4]) {
-    double a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p;
-    a=mA[0][0]; b=mA[0][1]; c=mA[0][2]; d=mA[0][3];
-    e=mA[1][0]; f=mA[1][1]; g=mA[1][2]; h=mA[1][3];
-    i=mA[2][0]; j=mA[2][1]; k=mA[2][2]; l=mA[2][3];
-    m=mA[3][0]; n=mA[3][1]; o=mA[3][2]; p=mA[3][3];
-
-    double q=a*f*k*p - a*f*l*o - a*j*g*p + a*j*h*o + a*n*g*l - a*n*h*k
-	- e*b*k*p + e*b*l*o + e*j*c*p - e*j*d*o - e*n*c*l + e*n*d*k
-	+ i*b*g*p - i*b*h*o - i*f*c*p + i*f*d*o + i*n*c*h - i*n*d*g
-	- m*b*g*l + m*b*h*k + m*f*c*l - m*f*d*k - m*j*c*h + m*j*d*g;
-
-    if (q<0.000000001) {
-	mI[0][0]=mI[1][1]=mI[2][2]=mI[3][3]=1;
-	mI[1][0]=mI[1][2]=mI[1][3]=0;
-	mI[2][0]=mI[2][1]=mI[2][3]=0;
-	mI[3][0]=mI[3][1]=mI[3][2]=0;
-	cerr << "ERROR - matrix is singular!!!\n";
-	return;
-    }
-    mI[0][0]=(f*k*p - f*l*o - j*g*p + j*h*o + n*g*l - n*h*k)/q;
-    mI[0][1]=-(b*k*p - b*l*o - j*c*p + j*d*o + n*c*l - n*d*k)/q;
-    mI[0][2]=(b*g*p - b*h*o - f*c*p + f*d*o + n*c*h - n*d*g)/q;
-    mI[0][3]=-(b*g*l - b*h*k - f*c*l + f*d*k + j*c*h - j*d*g)/q;
-
-    mI[1][0]=-(e*k*p - e*l*o - i*g*p + i*h*o + m*g*l - m*h*k)/q;
-    mI[1][1]=(a*k*p - a*l*o - i*c*p + i*d*o + m*c*l - m*d*k)/q;
-    mI[1][2]=-(a*g*p - a*h*o - e*c*p + e*d*o + m*c*h - m*d*g)/q;
-    mI[1][3]=(a*g*l - a*h*k - e*c*l + e*d*k + i*c*h - i*d*g)/q;
-
-    mI[2][0]=(e*j*p - e*l*n - i*f*p + i*h*n + m*f*l - m*h*j)/q;
-    mI[2][1]=-(a*j*p - a*l*n - i*b*p + i*d*n + m*b*l - m*d*j)/q;
-    mI[2][2]=(a*f*p - a*h*n - e*b*p + e*d*n + m*b*h - m*d*f)/q;
-    mI[2][3]=-(a*f*l - a*h*j - e*b*l + e*d*j + i*b*h - i*d*f)/q;
-
-    mI[3][0]=-(e*j*o - e*k*n - i*f*o + i*g*n + m*f*k - m*g*j)/q;
-    mI[3][1]=(a*j*o - a*k*n - i*b*o + i*c*n + m*b*k - m*c*j)/q;
-    mI[3][2]=-(a*f*o - a*g*n - e*b*o + e*c*n + m*b*g - m*c*f)/q;
-    mI[3][3]=(a*f*k - a*g*j - e*b*k + e*c*j + i*b*g - i*c*f)/q;
-}
-    
 class TransformField : public Module {
     ScalarFieldIPort *iport;
     MatrixIPort *imat;
@@ -145,32 +106,26 @@ void TransformField::execute()
     MatrixHandle mIH;
     imat->get(mIH);
     if (!mIH.get_rep()) return;
-
     if (matGen == mIH->generation && sfGen == sfIH->generation) return;
-
     if ((mIH->nrows() != 4) || (mIH->ncols() != 4)) return;
+    Transform t;
+    MatToTransform(mIH, t);
 
     ScalarFieldRG* sf=scinew ScalarFieldRG;
     osfh=sf;
-
     int nx=isfrg->nx;
     int ny=isfrg->ny;
     int nz=isfrg->nz;
-
     sf->resize(nx, ny, nz);
     Point min, max;
     isfrg->get_bounds(min, max);
     sf->set_bounds(min, max);
 
-    double m[4][4], mInv[4][4];
-    buildTransformMatrix(m, mIH);
-    buildInverseMatrix(m, mInv);
-
     for (int i=0; i<nx; i++)
 	for (int j=0; j<ny; j++)
 	    for (int k=0; k<nz; k++) {
 		Point oldp(isfrg->get_point(i,j,k));
-		Point newp(transformPt(mInv, oldp));
+		Point newp(t.unproject(oldp));
 		double val;
 		if (!isfrg->interpolate(newp, val)) val=0;
 		sf->grid(i,j,k)=val;
