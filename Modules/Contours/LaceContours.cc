085ec287617c268d42d9611e351d0738de76a514
David Weinstein <dmw@sci.utah.edu>
1999-05-14 22:06:23 +0000

    chaged ContourSet to support multiple structures

--- Modules/Contours/LaceContours.cc
+++ Modules/Contours/LaceContours.cc
@@ -19,7 +19,7 @@
 #include <Datatypes/ContourSetPort.h>
 #include <Datatypes/Surface.h>
 #include <Datatypes/SurfacePort.h>
-#include <Datatypes/TriSurface.h>
+#include <Datatypes/SurfTree.h>
 #include <Geometry/Grid.h>
 #include <Math/MiscMath.h>
 #include <Math/MinMax.h>
@@ -30,7 +30,8 @@
 class LaceContours : public Module {
     ContourSetIPort* incontour;
     SurfaceOPort* osurface;
-    void lace_contours(const ContourSetHandle& contour, TriSurface* surf);
+    void lace_contours(const ContourSetHandle& contour, int idx, 
+		       SurfTree* surf);
 public:
     LaceContours(const clString&);
     LaceContours(const LaceContours&, int deep);
@@ -77,48 +78,52 @@ void LaceContours::execute()
 {
     ContourSetHandle contours;
     if (!incontour->get(contours)) return;
-    TriSurface* surf=new TriSurface;
-    lace_contours(contours, surf);
+    SurfTree* surf=new SurfTree;
+    surf->surfI.resize(contours->contours.size());
+    for (int i=0; i<contours->contours.size(); i++) {
+	surf->surfI[i].matl=i;
+	lace_contours(contours, i, surf);
+    }
     osurface->send(SurfaceHandle(surf));
 }
 
-void LaceContours::lace_contours(const ContourSetHandle& contour, 
-				   TriSurface* surf) {
-    surf->name=contour->name;
+void LaceContours::lace_contours(const ContourSetHandle& contour, int idx,
+				   SurfTree* surf) {
+    surf->surfI[idx].name=contour->name[idx];
     Array1<int> row;	
     int i;
     int curr;
-    for (i=curr=0; i<contour->contours.size(); i++) {
-	row.add(curr);	
-	curr+=contour->contours[i].size();
-	for (int j=0; j<contour->contours[i].size(); j++) {
-	    Point p(contour->contours[i][j]-contour->origin);
+    for (i=curr=0; i<contour->contours[idx].size(); i++) {
+	row.add(curr);
+	curr+=contour->contours[idx][i].size();
+	for (int j=0; j<contour->contours[idx][i].size(); j++) {
+	    Point p(contour->contours[idx][i][j]-contour->origin);
 	    p=Point(0,0,0)+contour->basis[0]*p.x()+contour->basis[1]*p.y()+
 		contour->basis[2]*(p.z()*contour->space);
-	    surf->add_point(p);
+	    surf->nodes.add(p);
 	}
     }
    // i will be the index of the top contour being laced, i-1 being the other
-   for (i=1; i<contour->contours.size(); i++) {
+   for (i=1; i<contour->contours[idx].size(); i++) {
        int top=0;
        double dtemp;
-       int sz_top=contour->contours[i].size();
-       int sz_bot=contour->contours[i-1].size();
+       int sz_top=contour->contours[idx][i].size();
+       int sz_bot=contour->contours[idx][i-1].size();
        if ((sz_top < 2) && (sz_bot < 2)) {
 	   cerr << "Not enough points to lace!\n";
 	   return;
        }
        // 0 will be the index of our first bottom point, set top to be the 
        // index of the closest top point to it
-       double dist=Sqr(contour->contours[i][0].x()-
-		       contour->contours[i-1][0].x())+
-		   Sqr(contour->contours[i][0].y()-
-		       contour->contours[i-1][0].y());
+       double dist=Sqr(contour->contours[idx][i][0].x()-
+		       contour->contours[idx][i-1][0].x())+
+		   Sqr(contour->contours[idx][i][0].y()-
+		       contour->contours[idx][i-1][0].y());
        for (int start=1; start<sz_top; start++) {
-	   if ((dtemp=(Sqr(contour->contours[i][start].x()-
-			   contour->contours[i-1][0].x())+
-		       Sqr(contour->contours[i][start].y()-
-			   contour->contours[i-1][0].y())))<dist) {
+	   if ((dtemp=(Sqr(contour->contours[idx][i][start].x()-
+			   contour->contours[idx][i-1][0].x())+
+		       Sqr(contour->contours[idx][i][start].y()-
+			   contour->contours[idx][i-1][0].y())))<dist) {
 	       top=start;
 	       dist=dtemp;
 	   }
@@ -129,25 +134,26 @@ void LaceContours::lace_contours(const ContourSetHandle& contour,
        int jdone=(sz_top==1); // does this val have to change for us to
        int kdone=(sz_bot==1); // be done lacing
        for (int j=top,k=bot; !jdone || !kdone;) {
-	   double d1=Sqr(contour->contours[i][j].x()-
-			 contour->contours[i-1][(k+1)%sz_bot].x())+
-	       Sqr(contour->contours[i][j].y()-
-		   contour->contours[i-1][(k+1)%sz_bot].y());
-	   double d2=Sqr(contour->contours[i][(j+1)%sz_top].x()-
-			 contour->contours[i-1][k].x())+
-	             Sqr(contour->contours[i][(j+1)%sz_top].y()-
-			 contour->contours[i-1][k].y());
+	   double d1=Sqr(contour->contours[idx][i][j].x()-
+			 contour->contours[idx][i-1][(k+1)%sz_bot].x())+
+	       Sqr(contour->contours[idx][i][j].y()-
+		   contour->contours[idx][i-1][(k+1)%sz_bot].y());
+	   double d2=Sqr(contour->contours[idx][i][(j+1)%sz_top].x()-
+			 contour->contours[idx][i-1][k].x())+
+	             Sqr(contour->contours[idx][i][(j+1)%sz_top].y()-
+			 contour->contours[idx][i-1][k].y());
+	   int nf=surf->faces.size();
 	   if ((d1<d2 || jdone) && !kdone){ 	// bottom moves
-	       surf->add_triangle(row[i]+j, row[i-1]+k,
-				  row[i-1]+((k+1)%sz_bot));
+	       surf->faces.add(new TSElement(row[i]+j, row[i-1]+k, row[i-1]+((k+1)%sz_bot)));
 	       k=(k+1)%sz_bot;
 	       if (k==bot) kdone=1;
 	   } else {     			// top moves
-	       surf->add_triangle(row[i]+j,row[i-1]+k,
-				  row[i]+((j+1)%sz_top));
+	       surf->faces.add(new TSElement(row[i]+j,row[i-1]+k, row[i]+((j+1)%sz_top)));
 	       j=(j+1)%sz_top;
 	       if (j==top) jdone=1;
 	   }
+	   surf->surfI[idx].faces.add(nf);
+	   surf->surfI[idx].faceOrient.add(1);
        }
    }
 }
