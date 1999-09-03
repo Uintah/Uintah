7ade9f3328646b96abe8408668faeb06f411e2e5
David Weinstein <dmw@sci.utah.edu>
1999-09-03 06:06:27 +0000

    specialization methods

--- src/DaveW/ThirdParty/SparseLib/Leonid/TSVD.h
+++ src/DaveW/ThirdParty/SparseLib/Leonid/TSVD.h
@@ -52,9 +52,6 @@ public:
   
 };
 
-
-
-
 //TSVD<Data_Type> Class Implementation
 //---------------------------------------------------------------------------
 template<class Data_Type>
@@ -73,47 +70,6 @@ TSVD<Data_Type>::TSVD(MatrixDense<Data_Type> &AA,ZVector<Data_Type> &BB)
   
 }
 
-//----------------------------------------------------------------------------
-void TSVD<double>::set_sigma(double sigma_l){
-  
-  sigma = sigma_l;
-  
-}
-
-//----------------------------------------------------------------------------
-void TSVD<double>::factor(){
-  
-  SVD<double> svd(*A);
-  svd.solve();
-  
-  U = svd.get_U();
-  VT = svd.get_VT();
-  S = svd.get_S();
-  
-  
-  for(int i=0;i<nrows;i++){
-    for(int j=0;j<nrows;j++){
-      (*P)(i) = (*P)(i) + (*U)(j,i)*(*B)(j);
-    }}
-  
-}
-
-//----------------------------------------------------------------------------
-void TSVD<double>::solve(){
-  
-  (*X)=0.0;
-  
-  
-  for(int k=0;k<ncols;k++){
-    for(int i = 0;i<Min(nrows,ncols);i++){
-      if ((*S)(i) > sigma)
-	{
-	  (*X)(k) = (*X)(k) + (*P)(i)/(*S)(i)*(*VT)(i,k);
-	}
-    }}
-  
-  
-} 
 //---------------------------------------------------------------------------
 template <class Data_Type>
 void TSVD<Data_Type>:: info(){
@@ -122,6 +78,11 @@ void TSVD<Data_Type>:: info(){
   cout<<"TSVD:"<<endl;
   cout<<"Cut-off sigma = "<<sigma<<endl;
   cout<<"********************************************"<<endl;
+}
+
+template <class Data_Type>
+void TSVD<Data_Type>:: solve(){
+    cerr << "Error generic TSVD<Data_Type>:: solve() not implemented.\n";
 }
 
 //---------------------------------------------------------------------------  
