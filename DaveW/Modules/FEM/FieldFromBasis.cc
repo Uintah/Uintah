ed1f17add503fd31629dd8fa8c2b5427e76fed41
David Weinstein <dmw@sci.utah.edu>
1999-12-10 07:00:08 +0000

    build field from either reciprocity or RA-1 basis

--- DaveW/Modules/FEM/FieldFromBasis.cc
+++ DaveW/Modules/FEM/FieldFromBasis.cc
@@ -74,8 +74,8 @@ FieldFromBasis::FieldFromBasis(const clString& id)
 FieldFromBasis::~FieldFromBasis(){}
 
 void FieldFromBasis::execute() {
-    MeshHandle mesh_in;
-    if (!mesh_iport->get(mesh_in)) {
+    MeshHandle mesh;
+    if (!mesh_iport->get(mesh)) {
 	cerr << "FieldFromBasis -- couldn't get mesh.  Returning.\n";
 	return;
     }
@@ -85,46 +85,67 @@ void FieldFromBasis::execute() {
 	cerr << "FieldFromBasis -- couldn't get basis matrix.  Returning.\n";
 	return;
     }
-    int nelecs=basis->ncols()/3;
-    int nelems=mesh_in->elems.size();
-    if (nelems != basis->nrows()) {
-	cerr << "FieldFromBasis -- error, basis doesn't have the same number of elements \nas the mesh: basis->nrows()="<<basis->nrows()<<" mesh->elems.size()="<<nelems<<"\n";
-	return;
-    }
-
+    int nelems=mesh->elems.size();
+    int nnodes=mesh->nodes.size();
+    cerr << "basisH->nrows()="<<basisH->nrows()<<" cols="<<basisH->ncols()<<"\n";
+    cerr << "     mesh->elems.size()="<<nelems<<" mesh->nodes.size()="<<nnodes<<"\n";
     if (matrixGen == basisH->generation && sfh.get_rep()) {
 	field_oport->send(sfh);
 	return;
     }
     matrixGen=basisH->generation;
-
     int counter=0;
     Array1<double> errors(nelems);
     while (counter<nelems) {
-
 	if (counter && counter%100 == 0)
 	    cerr << "FieldFromBasis: "<<counter<<"/"<<nelems<<"\n";
 	// send element's basis matrix
-	DenseMatrix* bas=new DenseMatrix(nelecs,3);
-	int i;
-	for (i=0; i<nelecs; i++) 
-	    for (int j=0; j<3; j++)
-		(*bas)[i][j]=(*basis)[counter][i*3+j];
-	if (counter<(nelems-1)) elem_oport->send_intermediate(bas);
-	else elem_oport->send(bas);
-
-	// read error
-	ColumnMatrixHandle err_in;
-	if (!rms_iport->get(err_in) || !(err_in.get_rep()) || 
-	    (err_in->nrows() != 1)) {
-	    cerr <<"FieldFromBasis -- couldn't get error vector.\n";
+	if (nelems == basis->nrows()) {
+	    int nelecs=basis->ncols()/3;
+	    DenseMatrix* bas=new DenseMatrix(nelecs,3);
+	    int i;
+	    for (i=0; i<nelecs; i++) 
+		for (int j=0; j<3; j++)
+		    (*bas)[i][j]=(*basis)[counter][i*3+j];
+	    if (counter<(nelems-1)) elem_oport->send_intermediate(bas);
+	    else elem_oport->send(bas);
+	    
+	    // read error
+	    ColumnMatrixHandle err_in;
+	    if (!rms_iport->get(err_in) || !(err_in.get_rep()) || 
+		(err_in->nrows() != 1)) {
+		cerr <<"FieldFromBasis -- couldn't get error vector.\n";
+		return;
+	    }
+	    errors[counter]=(*err_in.get_rep())[0];
+	    counter++;
+	} else if (nnodes == basis->ncols()) {
+	    int nelecs=basis->nrows();
+	    DenseMatrix* bas=new DenseMatrix(nelecs,4);
+	    Element *e=mesh->elems[counter];
+	    int i;
+	    for (i=0; i<nelecs; i++) 
+		for (int j=0; j<4; j++)
+		    (*bas)[i][j]=(*basis)[i][e->n[j]];
+	    if (counter<(nelems-1)) elem_oport->send_intermediate(bas);
+	    else elem_oport->send(bas);
+	    
+	    // read error
+	    ColumnMatrixHandle err_in;
+	    if (!rms_iport->get(err_in) || !(err_in.get_rep()) || 
+		(err_in->nrows() != 1)) {
+		cerr <<"FieldFromBasis -- couldn't get error vector.\n";
+		return;
+	    }
+	    errors[counter]=(*err_in.get_rep())[0];
+	    counter++;
+	} else {
+	    cerr << "FieldFromBasis -- error, basis doesn't have the same number of rows \as mesh elements or mesh nodes: basis->nrows()="<<basis->nrows()<<" mesh->elems.size()="<<nelems<<" mesh->nodes.size()="<<nnodes<<"\n";
 	    return;
 	}
-	errors[counter]=(*err_in.get_rep())[0];
-	counter++;
     }
 
-    ScalarFieldUG *sfug=new ScalarFieldUG(mesh_in, 
+    ScalarFieldUG *sfug=new ScalarFieldUG(mesh, 
 					  ScalarFieldUG::ElementValues);
     sfug->data=errors;
     sfh=sfug;
@@ -136,6 +157,9 @@ void FieldFromBasis::execute() {
 
 //
 // $Log$
+// Revision 1.4  1999/12/10 07:00:08  dmw
+// build field from either reciprocity or RA-1 basis
+//
 // Revision 1.3  1999/10/07 02:06:35  sparker
 // use standard iostreams and complex type
 //
