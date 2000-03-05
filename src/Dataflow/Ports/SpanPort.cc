54225fb1fcf38aaa2a45bddbef3c84e7a5df7db4
Yarden Livnat <yarden@sci.utah.edu>
2000-03-05 14:59:08 +0000

    add definition of SpanTree and provide a port for it.

---
+++ src/Dataflow/Ports/SpanPort.cc
@@ -0,0 +1,28 @@
+
+/*
+ *  SpanPort.cc
+ *
+ *  Written by:
+ *   Yarden Livnat
+ *   Department of Computer Science
+ *   University of Utah
+ *   Nov. 1995
+ *
+ *  Copyright (C) 1995 SCI Group
+ */
+
+#include <PSECore/Datatypes/SpanPort.h>
+
+namespace PSECore {
+namespace Datatypes {
+
+using namespace SCICore::Datatypes;
+
+clString SimpleIPort<SpanForestHandle>::port_type("Span");
+clString SimpleIPort<SpanForestHandle>::port_color("SteelBlue4");
+
+} // End namespace Datatypes
+} // End namespace PSECore
+
+
+
