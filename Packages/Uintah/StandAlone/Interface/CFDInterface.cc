194a7b1835199d9d0166f1ec828e70bd5fed9e51
J. Davison de St. Germain <dav@sci.utah.edu>
2000-03-16 22:08:39 +0000

    Added the beginnings of cocoon docs.  Added namespaces.  Did a few other coding standards updates too

--- Packages/Uintah/StandAlone/Interface/CFDInterface.cc
+++ Packages/Uintah/StandAlone/Interface/CFDInterface.cc
@@ -1,5 +1,10 @@
+/* REFERENCED */
+static char *id="@(#) $Id$";
 
 #include "CFDInterface.h"
+
+namespace Uintah {
+namespace Interface {
 
 CFDInterface::CFDInterface()
 {
@@ -8,3 +13,13 @@ CFDInterface::CFDInterface()
 CFDInterface::~CFDInterface()
 {
 }
+
+} // end namespace Interface
+} // end namespace Uintah
+
+//
+// $Log$
+// Revision 1.2  2000/03/16 22:08:22  dav
+// Added the beginnings of cocoon docs.  Added namespaces.  Did a few other coding standards updates too
+//
+//
--- Packages/Uintah/StandAlone/Parallel/UintahParallelComponent.h
+++ Packages/Uintah/StandAlone/Parallel/UintahParallelComponent.h
@@ -1,16 +1,61 @@
-
 #ifndef Uintah_Parallel_UintahParallelComponent_h
 #define Uintah_Parallel_UintahParallelComponent_h
 
 #include <string>
+using std::string;
+
+namespace Uintah {
+namespace Parallel {
+
 class UintahParallelPort;
+
+/**************************************
+
+CLASS
+   UintahParallelComponent
+   
+   Short description...
+
+GENERAL INFORMATION
+
+   UintahParallelComponent.h
+
+   Steven G. Parker
+   Department of Computer Science
+   University of Utah
+
+   Center for the Simulation of Accidental Fires and Explosions (C-SAFE)
+  
+   Copyright (C) 2000 SCI Group
+
+KEYWORDS
+   Uintah_Parallel_Component, Component
+
+DESCRIPTION
+   Long description...
+  
+WARNING
+  
+****************************************/
 
 class UintahParallelComponent {
 public:
     UintahParallelComponent();
     virtual ~UintahParallelComponent();
 
-    void setPort(const std::string& name, UintahParallelPort* port);
+    //////////
+    // Insert Documentation Here:
+    void setPort(const string& name, UintahParallelPort* port);
 };
+
+} // end namespace Parallel
+} // end namespace Uintah
+
+//
+// $Log$
+// Revision 1.2  2000/03/16 22:08:39  dav
+// Added the beginnings of cocoon docs.  Added namespaces.  Did a few other coding standards updates too
+//
+//
 
 #endif
