4f184651622759956fc57d7de06604110c30bfac
J. Davison de St. Germain <dav@sci.utah.edu>
2000-03-16 22:26:14 +0000

    Added the beginnings of cocoon docs.  Added namespaces.  Did a few other coding standards updates too

--- src/Uintah/Components/Arches/Arches.cc
+++ src/Uintah/Components/Arches/Arches.cc
@@ -1,6 +1,11 @@
+/* REFERENCED */
+static char *id="@(#) $Id$";
 
 #include <Uintah/Components/Arches/Arches.h>
 #include <SCICore/Util/NotFinished.h>
+
+namespace Uintah {
+namespace Components {
 
 Arches::Arches()
 {
@@ -28,3 +33,13 @@ void Arches::timeStep(double t, double dt,
 {
     NOT_FINISHED("Arches::problemSetup");
 }
+
+} // end namespace Components
+} // end namespace Uintah
+
+//
+// $Log$
+// Revision 1.2  2000/03/16 22:26:13  dav
+// Added the beginnings of cocoon docs.  Added namespaces.  Did a few other coding standards updates too
+//
+//
--- src/Uintah/Components/Arches/Arches.h
+++ src/Uintah/Components/Arches/Arches.h
@@ -1,6 +1,5 @@
-
-#ifndef Uintah_Component_Arches_Arches_h
-#define Uintah_Component_Arches_Arches_h
+#ifndef UINTAH_COMPONENT_ARCHES_ARCHES_H
+#define UINTAH_COMPONENT_ARCHES_ARCHES_H
 
 /*
  * Placeholder - nothing here yet
@@ -9,22 +8,79 @@
 #include <Uintah/Parallel/UintahParallelComponent.h>
 #include <Uintah/Interface/CFDInterface.h>
 
+namespace Uintah {
+namespace Components {
+
+using Uintah::Parallel::UintahParallelComponent;
+using Uintah::Interface::CFDInterface;
+
+/**************************************
+
+CLASS
+   Arches
+   
+   Short description...
+
+GENERAL INFORMATION
+
+   Arches.h
+
+   Steven G. Parker
+   Department of Computer Science
+   University of Utah
+
+   Center for the Simulation of Accidental Fires and Explosions (C-SAFE)
+  
+   Copyright (C) 2000 SCI Group
+
+KEYWORDS
+   Arches
+
+DESCRIPTION
+   Long description...
+  
+WARNING
+  
+****************************************/
+
 class Arches : public UintahParallelComponent, public CFDInterface {
 public:
     Arches();
     virtual ~Arches();
 
-    virtual void problemSetup(const ProblemSpecP& params, GridP& grid,
+    //////////
+    // Insert Documentation Here:
+    virtual void problemSetup(const ProblemSpecP& params, 
+			      GridP& grid,
 			      DataWarehouseP&);
+    //////////
+    // Insert Documentation Here:
     virtual void computeStableTimestep(const LevelP& level,
-				       SchedulerP&, DataWarehouseP&);
-    virtual void timeStep(double t, double dt,
-			  const LevelP& level, SchedulerP&,
-			  const DataWarehouseP&, DataWarehouseP&);
+				       SchedulerP&,
+				       DataWarehouseP&);
+    //////////
+    // Insert Documentation Here:
+    virtual void timeStep(double t, 
+			  double dt,
+			  const LevelP& level,
+			  SchedulerP&,
+			  const DataWarehouseP&,
+			  DataWarehouseP&);
+
 private:
     Arches(const Arches&);
     Arches& operator=(const Arches&);
 };
+
+} // end namespace Components
+} // end namespace Uintah
+
+//
+// $Log$
+// Revision 1.2  2000/03/16 22:26:14  dav
+// Added the beginnings of cocoon docs.  Added namespaces.  Did a few other coding standards updates too
+//
+//
 
 #endif
 
