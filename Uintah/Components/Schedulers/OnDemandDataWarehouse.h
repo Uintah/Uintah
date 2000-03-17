da980d03138f3565083d2eb6cf63cc7f3a69b8b4
J. Davison de St. Germain <dav@sci.utah.edu>
2000-03-17 01:03:17 +0000

    Added some cocoon stuff, fixed some namespace stuff, etc

--- Uintah/Components/Schedulers/OnDemandDataWarehouse.h
+++ Uintah/Components/Schedulers/OnDemandDataWarehouse.h
@@ -1,28 +1,87 @@
-
-#ifndef UINTAH_HOMEBREW_ONDEMANDDATAWAREHOUSE_H
-#define UINTAH_HOMEBREW_ONDEMANDDATAWAREHOUSE_H
+#ifndef UINTAH_COMPONENTS_SCHEDULERS_ONDEMANDDATAWAREHOUSE_H
+#define UINTAH_COMPONENTS_SCHEDULERS_ONDEMANDDATAWAREHOUSE_H
 
 #include <Uintah/Interface/DataWarehouse.h>
 #include <SCICore/Thread/CrowdMonitor.h>
 #include <map>
 
+namespace Uintah {
+
+namespace Grid {
+    class DataItem;
+    class TypeDescription;
+    class Region;
+}
+
+namespace Components {
+
+using Uintah::Interface::DataWarehouse;
+using Uintah::Grid::DataItem;
+using Uintah::Grid::TypeDescription;
+using Uintah::Grid::Region;
+
+/**************************************
+
+CLASS
+   OnDemandDataWarehouse
+   
+   Short description...
+
+GENERAL INFORMATION
+
+   OnDemandDataWarehouse.h
+
+   Steven G. Parker
+   Department of Computer Science
+   University of Utah
+
+   Center for the Simulation of Accidental Fires and Explosions (C-SAFE)
+  
+   Copyright (C) 2000 SCI Group
+
+KEYWORDS
+   On_Demand_Data_Warehouse
+
+DESCRIPTION
+   Long description...
+  
+WARNING
+  
+****************************************/
+
 class OnDemandDataWarehouse : public DataWarehouse {
 public:
     OnDemandDataWarehouse();
     virtual ~OnDemandDataWarehouse();
+
+    //////////
+    // Insert Documentation Here:
     virtual void getBroadcastData(DataItem& di, const std::string& name,
 				  const TypeDescription*) const;
+
+    //////////
+    // Insert Documentation Here:
     virtual void getRegionData(DataItem& di, const std::string& name,
 			       const TypeDescription*,
 			       const Region*, int numGhostCells) const;
+
+    //////////
+    // Insert Documentation Here:
     virtual void putRegionData(const DataItem& di, const std::string& name,
 			       const TypeDescription*,
 			       const Region*, int numGhostCells);
+
+    //////////
+    // Insert Documentation Here:
     virtual void putBroadcastData(const DataItem& di, const std::string& name,
 				  const TypeDescription*);
+
+    //////////
+    // Insert Documentation Here:
     virtual void allocateRegionData(DataItem& di, const std::string& name,
 				    const TypeDescription*,
 				    const Region*, int numGhostCells);
+
 private:
     struct DataRecord {
 	DataItem* di;
@@ -31,10 +90,24 @@ private:
 	DataRecord(DataItem* di, const TypeDescription* td,
 		   const Region* region);
     };
-    mutable SCICore::Thread::CrowdMonitor lock;
     typedef std::map<std::string, DataRecord*> dbType;
-    dbType data;
-    bool d_allowCreation;
+
+
+    //////////
+    // Insert Documentation Here:
+    mutable SCICore::Thread::CrowdMonitor  d_lock;
+    dbType                                 d_data;
+    bool                                   d_allowCreation;
 };
+
+} // end namespace Components
+} // end namespace Uintah
+
+//
+// $Log$
+// Revision 1.3  2000/03/17 01:03:17  dav
+// Added some cocoon stuff, fixed some namespace stuff, etc
+//
+//
 
 #endif
