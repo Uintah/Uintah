05d7d2fc9429b529768d9fb9498faae5bfc01fd8
Chris Moulding <moulding@sci.utah.edu>
2000-03-18 00:19:21 +0000

    Experimental automatic package maker added

--- src/PSECore/Dataflow/sub.mk
+++ src/PSECore/Dataflow/sub.mk
@@ -9,7 +9,8 @@ SRCDIR   := PSECore/Dataflow
 
 SRCS     += $(SRCDIR)/Connection.cc $(SRCDIR)/ModuleHelper.cc \
 	$(SRCDIR)/Network.cc $(SRCDIR)/Port.cc $(SRCDIR)/Module.cc \
-	$(SRCDIR)/NetworkEditor.cc $(SRCDIR)/PackageDB.cc
+	$(SRCDIR)/NetworkEditor.cc $(SRCDIR)/PackageDB.cc \
+        $(SRCDIR)/CreateModulePackage.cc
 
 PSELIBS := PSECore/Comm SCICore/Exceptions SCICore/Thread \
 	SCICore/Containers SCICore/TclInterface SCICore/Util \
@@ -20,6 +21,9 @@ include $(OBJTOP_ABS)/scripts/smallso_epilogue.mk
 
 #
 # $Log$
+# Revision 1.2  2000/03/18 00:17:38  moulding
+# Experimental automatic package maker added
+#
 # Revision 1.1  2000/03/17 09:27:55  sparker
 # New makefile scheme: sub.mk instead of Makefile.in
 # Use XML-based files for module repository
