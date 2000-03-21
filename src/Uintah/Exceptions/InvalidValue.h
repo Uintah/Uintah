e6d904528ff71c5504070f10ea011d1900f5907f
Steven G. Parker <sparker@sci.utah.edu>
2000-03-21 18:53:11 +0000

    New Exception class

---
+++ src/Uintah/Exceptions/InvalidValue.h
@@ -0,0 +1,16 @@
+
+#ifndef UINTAH_HOMEBREW_InvalidValue_H
+#define UINTAH_HOMEBREW_InvalidValue_H
+
+#include <SCICore/Exceptions/Exception.h>
+#include <Uintah/Grid/ProblemSpecP.h>
+#include <string>
+
+class InvalidValue {
+    std::string msg;
+public:
+    InvalidValue(const std::string&, const Uintah::Grid::ProblemSpecP& context);
+    virtual std::string message() const;
+};
+
+#endif
