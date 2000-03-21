d4b6c29a38929b6b0964d774c53afafc184be2b3
J. Davison de St. Germain <dav@sci.utah.edu>
2000-03-21 02:14:48 +0000

    updated SingleVel to SingleVelContact

--- Uintah/Components/MPM/Contact/sub.mk
+++ Uintah/Components/MPM/Contact/sub.mk
@@ -5,10 +5,13 @@
 
 SRCDIR   := Uintah/Components/MPM/Contact
 
-SRCS     += $(SRCDIR)/NullContact.cc $(SRCDIR)/SingleVel.cc
+SRCS     += $(SRCDIR)/NullContact.cc $(SRCDIR)/SingleVelContact.cc
 
 #
 # $Log$
+# Revision 1.2  2000/03/21 02:14:48  dav
+# updated SingleVel to SingleVelContact
+#
 # Revision 1.1  2000/03/17 09:29:36  sparker
 # New makefile scheme: sub.mk instead of Makefile.in
 # Use XML-based files for module repository
