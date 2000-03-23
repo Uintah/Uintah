c553ddc44fe4f504857a00379c01c1f3042c427d
Steven G. Parker <sparker@sci.utah.edu>
2000-03-23 11:05:31 +0000

    Added *_sidl.h files to GENHDRS so that they will get built in time

--- testprograms/Component/argtest/sub.mk
+++ testprograms/Component/argtest/sub.mk
@@ -15,11 +15,15 @@ LIBS := $(GLOBUS_LIBS) -lglobus_nexus -lglobus_dc -lglobus_common -lglobus_io
 
 PROGRAM := $(SRCDIR)/argtest
 SRCS := $(SRCDIR)/argtest.cc $(SRCDIR)/argtest_sidl.cc
+GENHDRS := $(SRCDIR)/argtest_sidl.h
 
 include $(SRCTOP)/scripts/program.mk
 
 #
 # $Log$
+# Revision 1.4  2000/03/23 11:05:19  sparker
+# Added *_sidl.h files to GENHDRS so that they will get built in time
+#
 # Revision 1.3  2000/03/21 06:13:34  sparker
 # Added pattern rule for .sidl files
 # Compile component testprograms
--- testprograms/Component/objects/sub.mk
+++ testprograms/Component/objects/sub.mk
@@ -15,11 +15,15 @@ LIBS := $(GLOBUS_LIBS) -lglobus_nexus -lglobus_dc -lglobus_common -lglobus_io
 
 PROGRAM := $(SRCDIR)/objects
 SRCS := $(SRCDIR)/objects.cc $(SRCDIR)/objects_sidl.cc
+GENHDRS := $(SRCDIR)/objects_sidl.h
 
 include $(SRCTOP)/scripts/program.mk
 
 #
 # $Log$
+# Revision 1.4  2000/03/23 11:05:30  sparker
+# Added *_sidl.h files to GENHDRS so that they will get built in time
+#
 # Revision 1.3  2000/03/21 06:13:39  sparker
 # Added pattern rule for .sidl files
 # Compile component testprograms
