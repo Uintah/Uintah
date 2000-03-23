7404a69a7ddfff39274e3a211d6a52ec956b58fd
John Schmidt <jas@sci.utah.edu>
2000-03-23 19:47:55 +0000

    Moved the ProblemSpec stuff from Grid/ to Interface.

---
+++ Uintah/Interface/ProblemSpec.cc
@@ -0,0 +1,41 @@
+/* REFERENCED */
+static char *id="@(#) $Id$";
+
+#include "ProblemSpec.h"
+
+#include <iostream>
+
+using std::cerr;
+
+namespace Uintah {
+namespace Grid {
+
+ProblemSpec::ProblemSpec()
+{
+}
+
+ProblemSpec::~ProblemSpec()
+{
+}
+
+const TypeDescription* ProblemSpec::getTypeDescription()
+{
+    //cerr << "ProblemSpec::getTypeDescription() not done\n";
+    return 0;
+}
+
+} // end namespace Grid
+} // end namespace Uintah
+
+//
+// $Log$
+// Revision 1.1  2000/03/23 19:47:55  jas
+// Moved the ProblemSpec stuff from Grid/ to Interface.
+//
+// Revision 1.3  2000/03/21 18:52:11  sparker
+// Prototyped header file for new problem spec functionality
+//
+// Revision 1.2  2000/03/16 22:08:00  dav
+// Added the beginnings of cocoon docs.  Added namespaces.  Did a few other coding standards updates too
+//
+//
---
+++ Uintah/Interface/ProblemSpecP.h
@@ -0,0 +1,13 @@
+
+#ifndef UINTAH_HOMEBREW_ProblemSpecP_H
+#define UINTAH_HOMEBREW_ProblemSpecP_H
+
+namespace Uintah {
+    namespace Grid {
+	template<class T> class Handle;
+	class ProblemSpec;
+	typedef Handle<ProblemSpec> ProblemSpecP;
+    }
+}
+
+#endif
