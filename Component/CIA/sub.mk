19191d036bb6f8307b7f61ed9f8e414583e0e8cb
Steven G. Parker <sparker@sci.utah.edu>
2000-03-23 10:55:02 +0000

    Put back rule for creating CIA_sidl.h, since it requires the -cia flag   for the sidl compiler

--- Component/CIA/sub.mk
+++ Component/CIA/sub.mk
@@ -15,6 +15,17 @@ SRCS     += $(SRCDIR)/CIA_sidl.cc $(SRCDIR)/Class.cc \
 	$(SRCDIR)/NoSuchMethodException.cc $(SRCDIR)/Object.cc \
 	$(SRCDIR)/Throwable.cc 
 
+#
+# We cannot use the implicit rule for CIA, since it needs that
+# special -cia flag
+$(SRCDIR)/CIA_sidl.o: $(SRCDIR)/CIA_sidl.cc $(SRCDIR)/CIA_sidl.h
+
+$(SRCDIR)/CIA_sidl.cc: $(SRCDIR)/CIA.sidl $(SIDL_EXE)
+	$(SIDL_EXE) -cia -o $@ $<
+
+$(SRCDIR)/CIA_sidl.h: $(SRCDIR)/CIA.sidl $(SIDL_EXE)
+	$(SIDL_EXE) -cia -h -o $@ $<
+
 GENHDRS := $(SRCDIR)/CIA_sidl.h
 
 PSELIBS := Component/PIDL
@@ -24,6 +35,10 @@ include $(SRCTOP)/scripts/smallso_epilogue.mk
 
 #
 # $Log$
+# Revision 1.5  2000/03/23 10:55:02  sparker
+# Put back rule for creating CIA_sidl.h, since it requires the -cia flag
+#   for the sidl compiler
+#
 # Revision 1.4  2000/03/21 06:13:31  sparker
 # Added pattern rule for .sidl files
 # Compile component testprograms
