0ea4ca8b5e3a68f502181c1978111e1f58e81552
Steven G. Parker <sparker@sci.utah.edu>
2000-03-23 10:25:41 +0000

    New exception facility - retired old "Exception.h" classes

--- src/Core/Exceptions/Exception.h
+++ src/Core/Exceptions/Exception.h
@@ -16,15 +16,29 @@
 #ifndef SCICore_Exceptions_Exception_h
 #define SCICore_Exceptions_Exception_h
 
-#include <string>
+#include <sci_config.h>
+
+#if USE_SCI_THROW
+#define SCI_THROW(exc) do {SCICore::Exceptions::Exception::sci_throw(exc);throw exc;} while(SCICore::Exceptions::Exception::alwaysFalse())
+#else
+#define SCI_THROW(exc) throw exc
+#endif
 
 namespace SCICore {
     namespace Exceptions {
 	class Exception {
 	public:
-	    virtual std::string message() const=0;
+	    Exception();
+	    virtual ~Exception();
+	    virtual const char* message() const=0;
+	    virtual const char* type() const=0;
+
+	    static void sci_throw(const Exception& exc);
+	    static bool alwaysFalse();
 	protected:
 	private:
+	    Exception(const Exception&);
+	    Exception& operator=(const Exception&);
 	};
     }
 }
