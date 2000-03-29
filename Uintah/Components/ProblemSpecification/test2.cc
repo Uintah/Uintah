3cef29c0f4e269adc3aea2bee852706d3007e480
John Schmidt <jas@sci.utah.edu>
2000-03-29 23:42:44 +0000

    Testing of the require and get features for extracting out values from the input file.

--- Uintah/Components/ProblemSpecification/test2.cc
+++ Uintah/Components/ProblemSpecification/test2.cc
@@ -18,10 +18,32 @@ int main()
   prob_spec = reader.readInputFile("test.ups");
 
   cout << "works after reading file . . ." << endl;
+  
+  ProblemSpecP meta_prob_spec = prob_spec->findBlock("Meta");
 
-  ProblemSpecP testps = prob_spec->findBlock("Time");
+  ProblemSpecP time_prob_spec = prob_spec->findBlock("Time");
 
   cout << "Works after finding Time block" << endl;
+
+  string title;
+  meta_prob_spec->require("title",title);
+  cout << "title is " << title << endl;
+
+  double maxTime;
+  time_prob_spec->require("maxTime",maxTime);
+  cout << "maxTime is " << maxTime << endl;
+  
+  double initTime;
+  time_prob_spec->require("initTime",initTime);
+  cout << "initTime is " << initTime << endl;
+
+  double delt_min;
+  time_prob_spec->require("delt_min",delt_min);
+  cout << "delt_min is " << delt_min << endl;
+
+  double delt_max;
+  time_prob_spec->require("delt_max",delt_max);
+  cout << "delt_max is " << delt_max << endl;
   exit(1);
 }
 
