4bd6ae5857b000a302b935fb3b4c912c90babfdf
John Schmidt <jas@sci.utah.edu>
2000-04-06 02:27:47 +0000

    Added some tests for findNextBlock.

--- src/Uintah/Components/ProblemSpecification/test2.cc
+++ src/Uintah/Components/ProblemSpecification/test2.cc
@@ -15,10 +15,12 @@ int main()
   ProblemSpecReader reader;
   
   ProblemSpecP prob_spec;
-  prob_spec = reader.readInputFile("input1.ups");
+  prob_spec = reader.readInputFile("input.ups");
 
   cout << "works after reading file . . ." << endl;
-  
+
+  ProblemSpecP uintah_prob_spec = prob_spec->findBlock("Uintah_specification");
+    
   ProblemSpecP meta_prob_spec = prob_spec->findBlock("Meta");
 
   ProblemSpecP time_prob_spec = prob_spec->findBlock("Time");
@@ -26,6 +28,10 @@ int main()
   ProblemSpecP cfd_prob_spec = prob_spec->findBlock("CFD");
 
   ProblemSpecP ice_prob_spec = cfd_prob_spec->findBlock("ICE");
+
+  ProblemSpecP mat_prob_spec = prob_spec->findBlock("MaterialProperties");
+  
+  ProblemSpecP mpm_mat_ps = mat_prob_spec->findBlock("ARCHES");
 
    cout << "Works after finding ICE block" << endl;
 
@@ -49,16 +55,20 @@ int main()
   time_prob_spec->require("delt_max",delt_max);
   cout << "delt_max is " << delt_max << endl;
 
-  ProblemSpecP mat_prob_spec = ice_prob_spec->findBlock("material_properties");
+  ProblemSpecP ice_mat_prob_spec = 
+    ice_prob_spec->findBlock("material_properties");
 
   double viscosity;
-  mat_prob_spec->require("viscosity",viscosity);
+  ice_mat_prob_spec->require("viscosity",viscosity);
   cout << "viscosity is " << viscosity << endl;
 
-  
- 
-  
 
+  for (ProblemSpecP mat_ps = mpm_mat_ps->findBlock("stream"); mat_ps != 0;
+       mat_ps = mat_ps->findNextBlock("stream") ) {
+    double density;
+    mat_ps->require("density",density);
+    cout << "density is " << density << endl;
+  }
   exit(1);
 }
 
