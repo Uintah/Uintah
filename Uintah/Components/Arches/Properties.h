edfe6e5e56ca18a221b3c976053d42423dbfc25a
Rajesh Rawat <rawat@sci.utah.edu>
2000-04-09 00:53:51 +0000

    added PhysicalConstants and Properties classes

---
+++ Uintah/Components/Arches/Properties.h
@@ -0,0 +1,64 @@
+
+#ifndef Uintah_Component_Arches_Properties_h
+#define Uintah_Component_Arches_Properties_h
+
+/*
+ * Placeholder - nothing here yet
+ */
+
+#include <Uintah/Parallel/UintahParallelComponent.h>
+#include <Uintah/Interface/CFDInterface.h>
+#include <Uintah/Grid/Region.h>
+#include <Uintah/Parallel/ProcessorContext.h>
+namespace Uintah {
+namespace Components {
+  using Uintah::Interface::CFDInterface;
+  using Uintah::Parallel::UintahParallelComponent;
+  using Uintah::Interface::ProblemSpecP;
+  using Uintah::Grid::GridP;
+  using Uintah::Grid::LevelP;
+  using Uintah::Grid::Region;
+  using Uintah::Interface::DataWarehouseP;
+  using Uintah::Interface::SchedulerP;
+  using Uintah::Parallel::ProcessorContext;
+
+
+class Properties {
+public:
+    Properties();
+    ~Properties();
+
+    void problemSetup(const ProblemSpecP& params,
+			      DataWarehouseP&);
+    void sched_computeProps(const LevelP& level,
+			    SchedulerP&, DataWarehouseP& old,
+			    DataWarehouseP& new);
+    const int getNumMixVars() const{
+      return d_numMixingVars;
+    }
+private:
+    static const int MAX_MIXSTREAMS = 10;
+    void computeProps(const ProcessorContext*,
+		      const Region* region,
+		      const DataWarehouseP& old_dw,
+		      DataWarehouseP& new_dw);
+    Properties(const Properties&);
+    Properties& operator=(const Properties&);
+    int d_numMixingVars;
+    double d_denUnderrelax;
+//temp soln...limits num of mixing streams
+    Stream d_streams[MAX_MIXSTREAMS]; 
+    struct Stream {
+      double d_density;
+      double d_temperature;
+      Stream();
+      problemSetup(ProblemSpecP&);
+    };
+};
+
+} // end namespace Components
+} // end namespace Uintah
+
+
+#endif
+
