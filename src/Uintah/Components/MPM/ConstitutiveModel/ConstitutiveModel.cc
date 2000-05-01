107327f4b3c93baa20dc7d4deb858e40b3c259d6
John Schmidt <jas@sci.utah.edu>
2000-05-01 17:25:00 +0000

    Changed the var labels to be consistent with SerialMPM.

--- src/Uintah/Components/MPM/ConstitutiveModel/ConstitutiveModel.cc
+++ src/Uintah/Components/MPM/ConstitutiveModel/ConstitutiveModel.cc
@@ -13,17 +13,22 @@ using namespace Uintah::MPM;
 ConstitutiveModel::ConstitutiveModel()
 {
    // Constructor
-   px_label = new VarLabel("p.x", ParticleVariable<Point>::getTypeDescription(),
-	VarLabel::PositionVariable);
-   p_deformationMeasure_label = new VarLabel("p.deformationMeasure",
-                                             ParticleVariable<Matrix3>::getTypeDescription());
-   p_stress_label = new VarLabel("p.stress",
+   pXLabel = new VarLabel("p.x", ParticleVariable<Point>::getTypeDescription(),
+				VarLabel::PositionVariable);
+
+   pDeformationMeasureLabel = new VarLabel("p.deformationMeasure",
+                               ParticleVariable<Matrix3>::getTypeDescription());
+
+   pStressLabel = new VarLabel("p.stress",
                                  ParticleVariable<Matrix3>::getTypeDescription());
-   p_mass_label = new VarLabel("p.mass",
+
+   pMassLabel = new VarLabel("p.mass",
                                ParticleVariable<double>::getTypeDescription());
-   p_volume_label = new VarLabel("p.volume",
+
+   pVolumeLabel = new VarLabel("p.volume",
                                  ParticleVariable<double>::getTypeDescription());
-   g_velocity_label = new VarLabel("g.velocity",
+
+   gVelocityLabel = new VarLabel("g.velocity",
                                    NCVariable<Vector>::getTypeDescription());
 }
 
