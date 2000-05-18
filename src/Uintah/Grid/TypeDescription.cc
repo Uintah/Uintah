308d355c90355008df1cb19183f18e559d84a86f
Kurt Zimmerman <kuzimmer@sci.utah.edu>
2000-05-18 18:41:14 +0000

    Added Particle to Basis enum, created Type enum with Scalar,Point,Vector,Tensor,& Other

--- src/Uintah/Grid/TypeDescription.cc
+++ src/Uintah/Grid/TypeDescription.cc
@@ -3,13 +3,17 @@
 
 using namespace Uintah;
 
-TypeDescription::TypeDescription(bool reductionvar, Basis basis)
-   : d_reductionvar(reductionvar), d_basis(basis)
+TypeDescription::TypeDescription(bool reductionvar, Basis basis, Type type)
+   : d_reductionvar(reductionvar), d_basis(basis), d_type(type)
 {
 }
 
+
 //
 // $Log$
+// Revision 1.2  2000/05/18 18:41:14  kuzimmer
+// Added Particle to Basis enum, created Type enum with Scalar,Point,Vector,Tensor,& Other
+//
 // Revision 1.1  2000/05/07 06:02:13  sparker
 // Added beginnings of multiple patch support and real dependencies
 //  for the scheduler
