7942d70265ab55cf7c4cd1b1b88cb692f73db46f
Kurt Zimmerman <kuzimmer@sci.utah.edu>
2000-05-24 00:26:37 +0000

    Jim wants these updates so he can run Kurts stuff out of Steves 64 bit PSE. WARNING, I (dav) do not know quite what I do.

--- Kurt/Modules/Vis/sub.mk
+++ Kurt/Modules/Vis/sub.mk
@@ -12,6 +12,7 @@ SRCS     += $(SRCDIR)/GLTextureBuilder.cc  $(SRCDIR)/PadField.cc \
 		$(SRCDIR)/TextureVolVis.cc \
 		$(SRCDIR)/VolVis.cc \
 		$(SRCDIR)/ArchiveReader.cc \
+		$(SRCDIR)/KurtScalarFieldReader.cc \
 		$(SRCDIR)/VisControl.cc \
 		$(SRCDIR)/RescaleColorMapForParticles.cc \
 		$(SRCDIR)/ParticleVis.cc
@@ -30,6 +31,9 @@ include $(SRCTOP)/scripts/smallso_epilogue.mk
 
 #
 # $Log$
+# Revision 1.3  2000/05/24 00:25:26  kuzimmer
+# Jim wants these updates so he can run Kurts stuff out of Steves 64 bit PSE. WARNING, I (dav) do not know quite what I do.
+#
 # Revision 1.2  2000/05/21 01:19:20  kuzimmer
 # Added Archive Reader
 #
