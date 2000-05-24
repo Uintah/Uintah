074ec90e624ba4b7869eeb59eb5503647df1cc1a
Kurt Zimmerman <kuzimmer@sci.utah.edu>
2000-05-24 00:26:37 +0000

    Jim wants these updates so he can run Kurts stuff out of Steves 64 bit PSE. WARNING, I (dav) do not know quite what I do.

--- src/Packages/Kurt/Dataflow/GUI/sub.mk
+++ src/Packages/Kurt/Dataflow/GUI/sub.mk
@@ -13,6 +13,7 @@ $(SRCDIR)/tclIndex: $(SRCDIR)/VolVis.tcl $(SRCDIR)/PadField.tcl \
 	$(SRCDIR)/VisControl.tcl \
 	$(SRCDIR)/RescaleColorMapForParticles.tcl \
 	$(SRCDIR)/ParticleVis.tcl \
+	$(SRCDIR)/KurtScalarFieldReader.tcl \
 	$(SRCDIR)/ArchiveReader.tcl
 	$(SRCTOP)/scripts/createTclIndex $(SRCTOP)/Kurt/GUI
 
@@ -20,6 +21,9 @@ CLEANPROGS := $(CLEANPROGS) $(SRCDIR)/tclIndex
 
 #
 # $Log$
+# Revision 1.7  2000/05/24 00:26:37  kuzimmer
+# Jim wants these updates so he can run Kurts stuff out of Steves 64 bit PSE. WARNING, I (dav) do not know quite what I do.
+#
 # Revision 1.6  2000/05/21 01:19:18  kuzimmer
 # Added Archive Reader
 #
