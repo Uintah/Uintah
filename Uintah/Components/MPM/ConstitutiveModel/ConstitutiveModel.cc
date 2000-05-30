64a13eaa980a2ae110902f0aaf4c0ec112908871
J. Davison de St. Germain <dav@sci.utah.edu>
2000-05-30 17:50:07 +0000

    removed labels from constructor

--- Uintah/Components/MPM/ConstitutiveModel/ConstitutiveModel.cc
+++ Uintah/Components/MPM/ConstitutiveModel/ConstitutiveModel.cc
@@ -13,24 +13,6 @@ using namespace Uintah::MPM;
 
 ConstitutiveModel::ConstitutiveModel()
 {
-   // Constructor
-   pXLabel = new VarLabel("p.x", ParticleVariable<Point>::getTypeDescription(),
-				VarLabel::PositionVariable);
-
-   pDeformationMeasureLabel = new VarLabel("p.deformationMeasure",
-                               ParticleVariable<Matrix3>::getTypeDescription());
-
-   pStressLabel = new VarLabel("p.stress",
-                                 ParticleVariable<Matrix3>::getTypeDescription());
-
-   pMassLabel = new VarLabel("p.mass",
-                               ParticleVariable<double>::getTypeDescription());
-
-   pVolumeLabel = new VarLabel("p.volume",
-                                 ParticleVariable<double>::getTypeDescription());
-   gMomExedVelocityLabel = new VarLabel("g.momexedvelocity",
-				       NCVariable<Vector>::getTypeDescription());
-   deltLabel = new VarLabel("delt", delt_vartype::getTypeDescription());
 }
 
 ConstitutiveModel::~ConstitutiveModel()
