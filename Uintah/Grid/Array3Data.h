ec2cb5c90adbe27753ff504592fa9c6aca2b18d2
Jim Guilkey <guilkey@sci.utah.edu>
2000-05-31 17:21:54 +0000

    Changed a few scinews to news to fix some wacky error.

--- Uintah/Grid/Array3Data.h
+++ Uintah/Grid/Array3Data.h
@@ -89,11 +89,11 @@ namespace Uintah {
       : d_size(size)
       {
 	 if(d_size.x() && d_size.y() && d_size.z())
-	    d_data=scinew T[d_size.x()*d_size.y()*d_size.z()];
+	    d_data=new T[d_size.x()*d_size.y()*d_size.z()];
 	 else
 	    d_data=0;
-	 d_data3=scinew T**[d_size.x()];
-	 d_data3[0]=scinew T*[d_size.x()*d_size.y()];
+	 d_data3=new T**[d_size.x()];
+	 d_data3[0]=new T*[d_size.x()*d_size.y()];
 	 d_data3[0][0]=d_data;
 	 for(int i=1;i<d_size.x();i++){
 	    d_data3[i]=d_data3[i-1]+d_size.y();
@@ -117,6 +117,9 @@ namespace Uintah {
 
 //
 // $Log$
+// Revision 1.10  2000/05/31 17:21:54  guilkey
+// Changed a few scinews to news to fix some wacky error.
+//
 // Revision 1.9  2000/05/30 20:19:27  sparker
 // Changed new to scinew to help track down memory leaks
 // Changed region to patch
