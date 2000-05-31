8472bc32d5b61bedded28008698f232936ac04d4
Jim Guilkey <guilkey@sci.utah.edu>
2000-05-31 16:30:57 +0000

    Tidied the file up, added cvs logging.

--- src/Uintah/Components/MPM/MPMLabel.cc
+++ src/Uintah/Components/MPM/MPMLabel.cc
@@ -12,92 +12,90 @@ using namespace Uintah::MPM;
 MPMLabel::MPMLabel()
 {
   pDeformationMeasureLabel = new VarLabel("p.deformationMeasure",
-			    ParticleVariable<Matrix3>::getTypeDescription());
+			ParticleVariable<Matrix3>::getTypeDescription());
   
   pStressLabel = new VarLabel( "p.stress",
-			       ParticleVariable<Matrix3>::getTypeDescription() );
+			ParticleVariable<Matrix3>::getTypeDescription() );
   
   pVolumeLabel = new VarLabel( "p.volume",
-			       ParticleVariable<double>::getTypeDescription());
+			ParticleVariable<double>::getTypeDescription());
   
   pMassLabel = new VarLabel( "p.mass",
-			     ParticleVariable<double>::getTypeDescription() );
+			ParticleVariable<double>::getTypeDescription() );
   
   pVelocityLabel = new VarLabel( "p.velocity", 
-				 ParticleVariable<Vector>::getTypeDescription() );
+			ParticleVariable<Vector>::getTypeDescription() );
   
   pExternalForceLabel = new VarLabel( "p.externalforce",
-				      ParticleVariable<Vector>::getTypeDescription() );
+			ParticleVariable<Vector>::getTypeDescription() );
   
   pXLabel = new VarLabel( "p.x", ParticleVariable<Point>::getTypeDescription(),
-			  VarLabel::PositionVariable);
+			VarLabel::PositionVariable);
   
   pTemperatureLabel = new VarLabel( "p.temperature",
-				    ParticleVariable<double>::getTypeDescription() );
-
+			ParticleVariable<double>::getTypeDescription() );
   
   pTemperatureGradientLabel = new VarLabel( "p.temperatureGradient",
-					    ParticleVariable<Vector>::getTypeDescription() );
+			ParticleVariable<Vector>::getTypeDescription() );
 
   pTemperatureRateLabel  = new VarLabel( "p.temperatureRate",
-				    ParticleVariable<double>::getTypeDescription() );
+			ParticleVariable<double>::getTypeDescription() );
   
   //tan:
-  //  pSurfaceNormalLabel is used to define the surface normal of a boundary particle.
+  //  pSurfaceNormalLabel is used to define the surface normal of a
+  //  boundary particle.
   //  For the interior particle, the p.surfaceNormal vector is set to (0,0,0)
   //  in this way we can distinguish boundary particles to interior particles
   //
   pSurfaceNormalLabel = new VarLabel( "p.surfaceNormal",
-				      ParticleVariable<Vector>::getTypeDescription() );
+			ParticleVariable<Vector>::getTypeDescription() );
   
   gAccelerationLabel = new VarLabel( "g.acceleration",
-				     NCVariable<Vector>::getTypeDescription() );
+			NCVariable<Vector>::getTypeDescription() );
   
   gMomExedAccelerationLabel = new VarLabel( "g.momexedacceleration",
-					    NCVariable<Vector>::getTypeDescription() );
+			NCVariable<Vector>::getTypeDescription() );
   
   gMassLabel = new VarLabel( "g.mass",
-			     NCVariable<double>::getTypeDescription() );
+			NCVariable<double>::getTypeDescription() );
   
   gVelocityLabel = new VarLabel( "g.velocity",
-				 NCVariable<Vector>::getTypeDescription() );
+			NCVariable<Vector>::getTypeDescription() );
   
   gMomExedVelocityLabel = new VarLabel( "g.momexedvelocity",
-					NCVariable<Vector>::getTypeDescription() );
+			NCVariable<Vector>::getTypeDescription() );
   
   gExternalForceLabel = new VarLabel( "g.externalforce",
-				      NCVariable<Vector>::getTypeDescription() );
+			NCVariable<Vector>::getTypeDescription() );
   
   gInternalForceLabel = new VarLabel( "g.internalforce",
-				      NCVariable<Vector>::getTypeDescription() );
+			NCVariable<Vector>::getTypeDescription() );
   
   gVelocityStarLabel = new VarLabel( "g.velocity_star",
-				     NCVariable<Vector>::getTypeDescription() );
+			NCVariable<Vector>::getTypeDescription() );
   
   gMomExedVelocityStarLabel = new VarLabel( "g.momexedvelocity_star",
-					    NCVariable<Vector>::getTypeDescription() );
+			NCVariable<Vector>::getTypeDescription() );
   
   gSelfContactLabel = new VarLabel( "g.selfContact",
-				    NCVariable<bool>::getTypeDescription() );
+			NCVariable<bool>::getTypeDescription() );
   
   cSelfContactLabel = new VarLabel( "c.selfContact",
-				    CCVariable<bool>::getTypeDescription() );
+			CCVariable<bool>::getTypeDescription() );
   
   cSurfaceNormalLabel = new VarLabel( "c.surfaceNormalLabel",
-				      CCVariable<Vector>::getTypeDescription() );
+			CCVariable<Vector>::getTypeDescription() );
   
   gTemperatureRateLabel = new VarLabel("g.temperatureRate",
-			       NCVariable<double>::getTypeDescription());
+			NCVariable<double>::getTypeDescription());
 
   gInternalHeatRateLabel = new VarLabel("g.internalTemperatureRate",
-				NCVariable<double>::getTypeDescription());
+			NCVariable<double>::getTypeDescription());
 
   gExternalHeatRateLabel = new VarLabel("g.externalTemperatureRate",
-				NCVariable<double>::getTypeDescription());
+			NCVariable<double>::getTypeDescription());
   
-  // I'm not sure about this one:
-  delTLabel = 
-    new VarLabel( "delT", delt_vartype::getTypeDescription() );
+  delTLabel = new VarLabel( "delT", delt_vartype::getTypeDescription() );
 } 
 
 const MPMLabel* MPMLabel::getLabels()
@@ -108,4 +106,7 @@ const MPMLabel* MPMLabel::getLabels()
   return instance;
 }
 
-
+// $Log$
+// Revision 1.4  2000/05/31 16:30:57  guilkey
+// Tidied the file up, added cvs logging.
+//
