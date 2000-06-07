aff4d50bea0d8cab87f1390139fef13638c264ff
Chris Moulding <moulding@sci.utah.edu>
2000-06-07 00:11:42 +0000

    made some modifications that will allow the module make to edit and add to this file

--- Dataflow/Modules/Example/sub.mk
+++ Dataflow/Modules/Example/sub.mk
@@ -7,7 +7,9 @@ include $(SRCTOP)/scripts/smallso_prologue.mk
 
 SRCDIR   := PSECommon/Modules/Example
 
-SRCS     += $(SRCDIR)/GeomPortTest.cc
+SRCS     += \
+	$(SRCDIR)/GeomPortTest.cc\
+#[INSERT NEW MODULE HERE]
 
 PSELIBS := PSECore/Dataflow PSECore/Datatypes SCICore/Containers \
 	SCICore/Thread SCICore/Geom SCICore/TclInterface
@@ -17,6 +19,10 @@ include $(SRCTOP)/scripts/smallso_epilogue.mk
 
 #
 # $Log$
+# Revision 1.3  2000/06/07 00:11:34  moulding
+# made some modifications that will allow the module make to edit and add
+# to this file
+#
 # Revision 1.2  2000/03/20 19:36:54  sparker
 # Added VPATH support
 #
