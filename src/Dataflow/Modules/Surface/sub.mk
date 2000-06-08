2f2fd906ed03f73cbb1ede831031237784337e3a
Chris Moulding <moulding@sci.utah.edu>
2000-06-08 22:46:36 +0000

    Added a comment note about not messing with the module maker comment lines, and how to edit this file by hand.

--- src/Dataflow/Modules/Surface/sub.mk
+++ src/Dataflow/Modules/Surface/sub.mk
@@ -3,6 +3,16 @@
 # $Id$
 #
 
+# *** NOTE ***
+# 
+# Do not remove or modify the comment line:
+#
+# #[INSERT NEW ?????? HERE]
+#
+# It is required by the module maker to properly edit this file.
+# if you want to edit this file by hand, see the "Create A New Module"
+# documentation on how to do it correctly.
+
 include $(SRCTOP)/scripts/smallso_prologue.mk
 
 SRCDIR   := PSECommon/Modules/Surface
@@ -27,6 +37,10 @@ include $(SRCTOP)/scripts/smallso_epilogue.mk
 
 #
 # $Log$
+# Revision 1.4  2000/06/08 22:46:30  moulding
+# Added a comment note about not messing with the module maker comment lines,
+# and how to edit this file by hand.
+#
 # Revision 1.3  2000/06/07 00:11:40  moulding
 # made some modifications that will allow the module make to edit and add
 # to this file
