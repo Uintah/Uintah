d179269e6fa96f0896e638ba8066f7b64564ca73
Kurt Zimmerman <kuzimmer@sci.utah.edu>
2000-06-20 20:13:38 +0000

    updated so that Archiver reader will compile in Uintah

--- Packages/Uintah/Core/Datatypes/sub.mk
+++ Packages/Uintah/Core/Datatypes/sub.mk
@@ -1,7 +1,8 @@
 #
 # Makefile fragment for this subdirectory
 # $Id$
-#
+
+include $(SRCTOP)/scripts/smallso_prologue.mk
 
 SRCDIR := Uintah/Datatypes
 
@@ -9,8 +10,28 @@ SUBDIRS := $(SRCDIR)/Particles
 
 include $(SRCTOP)/scripts/recurse.mk
 
+SRCS     += $(SRCDIR)/Archive.cc  $(SRCDIR)/ArchivePort.cc 
+
+PSELIBS := SCICore/Exceptions SCICore/Geometry \
+	SCICore/Persistent SCICore/Datatypes \
+	SCICore/Containers SCICore/Thread Uintah/Grid Uintah/Interface \
+        Uintah/Exceptions PSECore/XMLUtil 
+
+LIBS := $(XML_LIBRARY)
+
+ifeq ($(BUILD_PARALLEL),yes)
+PSELIBS := $(PSELIBS) Component/CIA Component/PIDL
+LIBS := $(LIBS) $(GLOBUS_LIBS) -lglobus_nexus
+endif
+
+include $(SRCTOP)/scripts/smallso_epilogue.mk
+
+
 #
 # $Log$
+# Revision 1.3  2000/06/20 20:13:27  kuzimmer
+# updated so that Archiver reader will compile in Uintah
+#
 # Revision 1.2  2000/03/20 19:38:29  sparker
 # Added VPATH support
 #
@@ -19,4 +40,8 @@ include $(SRCTOP)/scripts/recurse.mk
 # Use XML-based files for module repository
 # Plus many other changes to make these two things work
 #
+#
+#
+# Makefile fragment for this subdirectory
+# $Id$
 #
