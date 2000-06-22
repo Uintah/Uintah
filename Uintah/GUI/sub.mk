e704e47ce43b9edbc2cef7139f9351ad0d8bed7f
Kurt Zimmerman <kuzimmer@sci.utah.edu>
2000-06-22 17:44:07 +0000

    removed commented out code and modified the sub.mk so that the GUI comes up.

--- Uintah/GUI/sub.mk
+++ Uintah/GUI/sub.mk
@@ -12,13 +12,18 @@ $(SRCDIR)/tclIndex: $(SRCDIR)/ChemVis.tcl $(SRCDIR)/PartToGeom.tcl \
 	$(SRCDIR)/RescaleParticleColorMap.tcl \
 	$(SRCDIR)/TecplotFileSelector.tcl $(SRCDIR)/TriangleReader.tcl \
 	$(SRCDIR)/application.tcl $(SRCDIR)/cfdGridLines.tcl \
-	$(SRCDIR)/test.tcl $(SRCDIR)/GridLines.tcl
+	$(SRCDIR)/test.tcl $(SRCDIR)/GridLines.tcl \
+	$(SRCDIR)/ArchiveReader.tcl
 	$(SRCTOP)/scripts/createTclIndex $(SRCTOP)/Uintah/GUI
 
 CLEANPROGS := $(CLEANPROGS) $(SRCDIR)/tclIndex
 
 #
 # $Log$
+# Revision 1.3  2000/06/22 17:44:07  kuzimmer
+# removed commented out code and modified the sub.mk so that the GUI
+# comes up.
+#
 # Revision 1.2  2000/03/20 19:38:34  sparker
 # Added VPATH support
 #
