705ab8c136803fd0f8c5b5aa5fc95239ba623fff
James Bigler <bigler@sci.utah.edu>
2000-06-26 21:28:10 +0000

    Added += opporator.

--- Uintah/Grid/NodeIterator.h
+++ Uintah/Grid/NodeIterator.h
@@ -36,70 +36,92 @@ WARNING
   
 ****************************************/
 
-   class NodeIterator {
-   public:
-      inline ~NodeIterator() {}
+  class NodeIterator {
+  public:
+    inline ~NodeIterator() {}
+    
+    //////////
+    // Insert Documentation Here:
+    inline NodeIterator operator++(int) {
+      NodeIterator old(*this);
       
-      //////////
-      // Insert Documentation Here:
-      inline NodeIterator operator++(int) {
-	 NodeIterator old(*this);
-	 
-	 if(++d_ix >= d_e.x()){
-	    d_ix = d_s.x();
-	    if(++d_iy >= d_e.y()){
-	       d_iy = d_s.y();
-	       ++d_iz;
-	    }
-	 }
-	 return old;
+      if(++d_ix >= d_e.x()){
+	d_ix = d_s.x();
+	if(++d_iy >= d_e.y()){
+	  d_iy = d_s.y();
+	  ++d_iz;
+	}
       }
-      
-      //////////
-      // Insert Documentation Here:
-      inline bool done() const {
-	 return d_ix >= d_e.x() || d_iy >= d_e.y() || d_iz >= d_e.z();
-      }
+      return old;
+    }
+    
+    //////////
+    // Insert Documentation Here:
+    inline NodeIterator operator+=(int step) {
+      NodeIterator old(*this);
 
-      IntVector operator*() {
-	 return IntVector(d_ix, d_iy, d_iz);
+      for (int i = 0; i < step; i++) {
+	if(++d_ix >= d_e.x()){
+	  d_ix = d_s.x();
+	  if(++d_iy >= d_e.y()){
+	    d_iy = d_s.y();
+	    ++d_iz;
+	  }
+	}
+	if (done())
+	  break;
       }
-      IntVector index() const {
-	 return IntVector(d_ix, d_iy, d_iz);
-      }
-      inline NodeIterator(const IntVector& s, const IntVector& e)
-	 : d_s(s), d_e(e) {
-	    d_ix = s.x();
-	    d_iy = s.y();
-	    d_iz = s.z();
-      }
-      inline IntVector current() const {
-	 return IntVector(d_ix, d_iy, d_iz);
-      }
-      inline IntVector begin() const {
-	 return d_s;
-      }
-      inline IntVector end() const {
-	 return d_e;
-      }
-      inline NodeIterator(const NodeIterator& copy)
-	 : d_ix(copy.d_ix), d_iy(copy.d_iy), d_iz(copy.d_iz),
-	   d_s(copy.d_s), d_e(copy.d_e) {
-      }
-
-   private:
-      NodeIterator();
-      NodeIterator& operator=(const NodeIterator& copy);
-      
-      //////////
-      // Insert Documentation Here:
-      IntVector d_s,d_e;
-      int d_ix, d_iy, d_iz;
-   };
+      return old;
+    }
+    
+    //////////
+    // Insert Documentation Here:
+    inline bool done() const {
+      return d_ix >= d_e.x() || d_iy >= d_e.y() || d_iz >= d_e.z();
+    }
+    
+    IntVector operator*() {
+      return IntVector(d_ix, d_iy, d_iz);
+    }
+    IntVector index() const {
+      return IntVector(d_ix, d_iy, d_iz);
+    }
+    inline NodeIterator(const IntVector& s, const IntVector& e)
+      : d_s(s), d_e(e) {
+	d_ix = s.x();
+	d_iy = s.y();
+	d_iz = s.z();
+    }
+    inline IntVector current() const {
+      return IntVector(d_ix, d_iy, d_iz);
+    }
+    inline IntVector begin() const {
+      return d_s;
+    }
+    inline IntVector end() const {
+      return d_e;
+    }
+    inline NodeIterator(const NodeIterator& copy)
+      : d_ix(copy.d_ix), d_iy(copy.d_iy), d_iz(copy.d_iz),
+	d_s(copy.d_s), d_e(copy.d_e) {
+    }
+    
+  private:
+    NodeIterator();
+    NodeIterator& operator=(const NodeIterator& copy);
+    
+    //////////
+    // Insert Documentation Here:
+    IntVector d_s,d_e;
+    int d_ix, d_iy, d_iz;
+  };
 } // end namespace Uintah
 
 //
 // $Log$
+// Revision 1.13  2000/06/26 21:28:10  bigler
+// Added += opporator.
+//
 // Revision 1.12  2000/06/16 05:19:21  sparker
 // Changed arrays to fortran order
 //
