2e740d21909be84037e8b99bdeaaae5dc4547150
Kurt Zimmerman <kuzimmer@sci.utah.edu>
2000-07-11 19:58:47 +0000

    modified so that CCVariables and Matrix3 subtype can be recognized in archive reader

--- src/Uintah/Grid/TypeUtils.h
+++ src/Uintah/Grid/TypeUtils.h
@@ -1,6 +1,8 @@
 #ifndef UINTAH_HOMEBREW_TypeUtils_H
 #define UINTAH_HOMEBREW_TypeUtils_H
 
+class Matrix3;
+
 namespace SCICore {
    namespace Geometry {
       class Point;
@@ -16,11 +18,15 @@ namespace Uintah {
    const TypeDescription* fun_getTypeDescription(long*);
    const TypeDescription* fun_getTypeDescription(SCICore::Geometry::Point*);
    const TypeDescription* fun_getTypeDescription(SCICore::Geometry::Vector*);
+   const TypeDescription* fun_getTypeDescription(Matrix3*);
    
 } // end namespace Uintah
 
 //
 // $Log$
+// Revision 1.3  2000/07/11 19:58:47  kuzimmer
+// modified so that CCVariables and Matrix3 subtype can be recognized in archive reader
+//
 // Revision 1.2  2000/06/02 17:22:15  guilkey
 // Added long_type to the the TypeDescription and TypeUtils.
 //
