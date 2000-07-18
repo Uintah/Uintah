599701e0d7689fd89d0ceca640d3aa3072994b38
Alexei Samsonov <samsonov@sci.utah.edu>
2000-07-18 23:06:55 +0000

    Transfering EditPath, PathReader and PathWriter modules to PSECommon package

---
+++ src/Dataflow/GUI/PathReader.tcl
@@ -0,0 +1,20 @@
+catch {rename DaveW_Readers_PathReader ""}
+
+itcl_class PSECommon_Readers_PathReader {
+    inherit Module
+    constructor {config} {
+	set name PathReader
+	set_defaults
+    }
+    method set_defaults {} {
+    }
+    method ui {} {
+	set w .ui[modname]
+	if {[winfo exists $w]} {
+	    raise $w
+	    return;
+	}
+	toplevel $w
+	makeFilebox $w $this-filename "$this-c needexecute" "destroy $w"
+    }
+}
