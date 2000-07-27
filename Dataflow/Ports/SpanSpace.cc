97bdbd06367036639c797bbc82ee40f731ac3593
Yarden Livnat <yarden@sci.utah.edu>
2000-07-27 16:26:46 +0000

    use SpanSpace instead of the old SpanTree

--- Dataflow/Ports/SpanSpace.cc
+++ Dataflow/Ports/SpanSpace.cc
@@ -10,15 +10,42 @@
  *  Copyright (C) 1995 SCI Group
  */
 
+#include <SCICore/Datatypes/Datatype.h>
+#include <SCICore/Containers/Array1.h>
+#include <SCICore/Datatypes/ScalarField.h>
+#include <SCICore/Containers/LockingHandle.h>
+#include <SCICore/Geometry/BBox.h>
+#include <PSECore/Datatypes/SpanSpace.h>
+
+
 namespace PSECore {
   namespace Datatypes {
     
-#ifdef UNIVERSE
-    
+    using namespace SCICore::Datatypes;
+
+    SpanSpaceBuildUG::SpanSpaceBuildUG ( ScalarFieldUG *field)
+      {
+	Array1<Element*> &elems = field->mesh->elems;
+	Array1<double> &data = field->data;
+	
+	for (int i=0; i<field->mesh->elems.size(); i++ ) {
+	  int *n = elems[i]->n;
+	  
+	  double min, max;
+	  min = max = data[n[0]];
+	  for ( int j=1; j<4; j++) {
+	    double v = data[n[j]];
+	    if ( v < min ) min = v;
+	    else if ( v > max ) max = v;
+	  }
+	  
+	  span.add( SpanPoint<double>( min, max, i ) );
+	}
+      }
+
     PersistentTypeID SpanUniverse::type_id("SpanUniverse", "Datatype", 0);
     
 #define SpanUniverse_VERSION 0
-#endif
     
 /*
 void SpanForest::io(Piostream& stream) {
