6439d83f9feab1e2bb34558aa26c3b189663b390
Alexei Samsonov <samsonov@sci.utah.edu>
2000-08-09 07:15:55 +0000

    final version and Cocoon comments

--- Core/Datatypes/Path.h
+++ Core/Datatypes/Path.h
@@ -1,32 +1,60 @@
-/*
- *  Path.h: Camera path
- *
- *  Written by:
- *   David Weinstein & Alexei Samsonov
- *   Department of Computer Science
- *   University of Utah
- *   December 1999, July 2000
- *
- *  Copyright (C) 1999 SCI Group
- */
+/*----------------------------------------------------------------------
+CLASS
+    Path
+
+    Datatype for storing and manipulating camera path. Used in EditPath module
+
+GENERAL INFORMATION
+
+    Created by:
+    Alexei Samsonov
+    Department of Computer Science
+    University of Utah
+    July 2000
+    
+    Copyright (C) 2000 SCI Group
+
+KEYWORDS
+    Quaternion
+
+DESCRIPTION
+    Provides much of functionality of EditPath module. 
+
+PATTERNS
+   
+WARNING    
+    
+
+POSSIBLE REVISIONS
+    Adding additional interpolation modes (subject to experiment)
+----------------------------------------------------------------------*/
 
 #ifndef SCI_SCICore_Datatypes_Path_h
 #define SCI_SCICore_Datatypes_Path_h 1
 
+#include <SCICore/share/share.h>
+#include <SCICore/Datatypes/Datatype.h>
 #include <SCICore/Containers/Array1.h>
-#include <SCICore/Containers/Array2.h>
 #include <SCICore/Containers/LockingHandle.h>
+#include <SCICore/Containers/Handle.h>
+
 #include <SCICore/Containers/String.h>
 #include <SCICore/Math/LinearPWI.h>
-#include <SCICore/Datatypes/Datatype.h>
-#include <SCICore/Geom/View.h>
+#include <SCICore/Math/CubicPWI.h>
 
+#include <SCICore/Geom/View.h>
+#include <SCICore/Geometry/Quaternion.h>
+#include <SCICore/Geometry/Vector.h>
+#include <SCICore/Geometry/Point.h>
 
 namespace SCICore {
 namespace Datatypes {
 
 using namespace SCICore::Math;
+using namespace SCICore::Geometry;
+
 using SCICore::Containers::LockingHandle;
+using SCICore::Containers::Handle;
 using SCICore::Containers::Array1;
 using SCICore::GeomSpace::View;
 using SCICore::Containers::clString;
@@ -36,83 +64,153 @@ using SCICore::PersistentSpace::PersistentTypeID;
 class Path;
 typedef LockingHandle<Path> PathHandle;
 
-enum PathType { linear, other };
+//typedef Handle<PiecewiseInterp<Point> > PointPWI;
+//typedef Handle<PiecewiseInterp<Vector> > VectorPWI;
+//typedef Handle<PiecewiseInterp<double> > SimplePWI;
 
-class Path : public Datatype {
-  enum { Default, Linear, Other };
-  PathType  path_t;
-  int       keyF;
-  double    pathP;
-  bool      is_loop;
-  bool      is_built;
-  bool      is_back;
+
+enum {KEYFRAMED=0, LINEAR, CUBIC};
+enum {NO_ACC=0, SMOOTH, USERMODE};
+
+class SCICORESHARE Path : public Datatype {
+ 
+  double    pathP;                 // current value of distance from the start point
+  int       keyF;                  // keyframe nearest to the current path point
+  double    path_prm;              // current value of corresponding parameter
+                                   // (changes from 0 to 1)
+
+  int       is_loop, is_built, is_sampled, is_back, is_acc, is_run;
   
-  double    stepSize;
-
-  View      default_view;
-
-  Array1<float> param;
-
-  LinearPWI eyeP_x, eyeP_y, eyeP_z, lookatP_x, lookatP_y, lookatP_z, upV_x, upV_y, upV_z, fov;
-  bool      calc_view(double, View&);
+  int       path_t;
+  int       acc_t;
   
+  int       ns;                    // discretization of path
+  double    step_size;              // average distance between neighboor path points              
+  
+  // input data for path calculation
+
+  Array1<View>   keyViews;         // keyframes
+  Array1<double> param;            // and corresponding parameter value(based on linear distance)
+  double         path_dist;        // total distance of the current path
+
+  Array1<Quaternion> rotFrames;    // quaternion representation of camera frame orientation
+				   // (direction of view and up vector)
+  Array1<double> q_param;          // quaternion parametrization (by angle)
+  				   // every interval of param linearly maps to q_param
+  
+
+  Array1<double> speedVal;         // speed values at each key frame
+  Array1<int>    accPatt;
+  
+  Array1<double> dist_prm;         // arc-length parametrization
+
+  PiecewiseInterp<Point>* eyeP;
+  PiecewiseInterp<Point>* lookatP;
+  PiecewiseInterp<Vector>* upV;
+  PiecewiseInterp<double>* fov;
+
+  // smooth start/end mode support
+  Array1<double> sint;
+  int count;
+  double sin_series;
+  
+  int calc_view(double, View&);    // return number of current interval and hence number of current view
+  bool resample_path();
+  double get_delta();
+
+  // syncronization between arc-length parameter and initial parametrization
+  inline void set_param();
+  inline void set_arc_param();
+
 public:
-    // input data for path calculation
-    Array1<View>   keyViews;
-    Array1<double> speedVal;
-    Array1<int>    accPatt;
    
-    // resulting path points 
-    Array1<double> pathPoints;
-    
-    void reset();
-
-    // base view points manipulation
-    void set_default_view(const View&);
-    void del_keyF(int);
-    void ins_keyF(int, const View&, double speed=Default, int acc_patt=Default);
-    void add_keyF(const View&, double speed=1, int acc_patt=Default);
-    int  get_acc_patt(int);
-    double get_speed_val(int);
+    void   reset();
+    void   del_keyF(int);
+    bool   ins_keyF(int, const View&, double speed=1, int acc_patt=NO_ACC);
+    bool   add_keyF(const View&, double speed=1, int acc_patt=NO_ACC);
+    int    get_acc_t (int n=0) const;
+    bool   set_acc_t(int);
+    double get_speed_val(int) const;
 
     bool get_keyF(int, View&);
     int  get_num_views() const;
 
     // path points manipulation
-    void set_step(double);
+    bool   set_step(double);
     double get_step() const;
        
-    void set_path_type(PathType t);
-    PathType get_path_type() const;
+    bool     set_path_t(int t);
+    int      get_path_t() const;
+  
+    bool build_path();
     
-    bool is_looped() const;
-    bool is_backed() const;
-    void set_loop(bool);
-    void set_back(bool);
+    inline bool is_looped() const;
+    inline bool is_backed() const;
+    inline bool is_started() const;
     
-    // return next path view, the nearest view behind, current speed and acceleration
-    bool get_nextPP(View&, int&, double&, double&);
-    bool get_nextKF(View&, int&, double&, int&); 
+    bool set_loop(bool);
+    bool set_back(bool);
     void seek_start();
     void seek_end();
+    void stop();
 
+    // return next path view, the nearest view behind, current speed and acceleration
+    bool get_nextPP(View&, int&, double&, double&);
+    bool get_nextKF(View&, int&, double&, double&); 
+  
     Path();
-//    Path(const Path&);
     virtual Path* clone();
     virtual ~Path();
-
-    bool      built_path();
 
     // Persistent representation...
     virtual void io(Piostream&);
     static PersistentTypeID type_id;
 };
 
+inline bool Path::is_looped() const{
+  return is_loop;
+}
+
+inline bool Path::is_backed() const{
+  return is_back;
+}
+
+inline bool Path::is_started() const{
+  return is_run;
+}
+
+inline void Path::set_param(){
+  int ind=(int)((pathP/path_dist)*(ns-1));
+      
+  if (ind == ns-1)
+    path_prm=dist_prm[ind];
+  else {
+    double delta=path_dist/(ns-1);
+    double w=(pathP-ind*delta)/delta;
+    path_prm=dist_prm[ind+1]*w+(1-w)*dist_prm[ind];
+  }
+}
+
+inline void Path::set_arc_param(){
+  int sz=dist_prm.size();
+  ASSERT(sz>=ns && ns > 0 && path_prm<=dist_prm[sz-1]);
+  int j=1;
+  while (dist_prm[j]<path_prm) j++;
+  
+  double  w=(path_prm-dist_prm[j-1])/(dist_prm[j]-dist_prm[j-1]);
+  double delta=path_dist/(ns-1);
+  pathP=(j*w+(1-w)*(j-1))*delta;    
+}
+
+
 } // End namespace Datatypes
 } // End namespace SCICore
 
 //
 // $Log$
+// Revision 1.2  2000/08/09 07:15:55  samsonov
+// final version and Cocoon comments
+//
 // Revision 1.1  2000/07/19 06:40:05  samsonov
 // Path datatype moved form DaveW
 //
@@ -125,3 +223,6 @@ public:
 //
 
 #endif
+
+
+
