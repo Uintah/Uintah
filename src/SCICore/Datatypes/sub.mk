d6bb241e3f19239d6f17dce0325e9801867fd9f2
Marty Cole <mcole@sci.utah.edu>
2000-08-31 23:09:08 +0000

    fix build, bad comment

--- src/SCICore/Datatypes/sub.mk
+++ src/SCICore/Datatypes/sub.mk
@@ -33,7 +33,7 @@ SRCS += $(GENSRCS) $(SRCDIR)/TriSurface.cc $(SRCDIR)/BasicSurfaces.cc \
 	$(SRCDIR)/VectorFieldRGCC.cc $(SRCDIR)/templates.cc \
 	$(SRCDIR)/Geom.cc $(SRCDIR)/Attrib.cc \
 	$(SRCDIR)/Field.cc $(SRCDIR)/SField.cc \
-	$(SRCDIR)/GenSField.cc \ #$(SRCDIR)/GenFunction.cc\
+	$(SRCDIR)/GenSField.cc \
 	$(SRCDIR)/FieldWrapper.cc $(SRCDIR)/Domain.cc \
 	$(SRCDIR)/SField.cc $(SRCDIR)/VField.cc \
 	$(SRCDIR)/TField.cc $(SRCDIR)/LatticeGeom.cc \
@@ -98,6 +98,9 @@ clean::
 
 #
 # $Log$
+# Revision 1.3.2.6  2000/08/31 23:09:08  mcole
+# fix build, bad comment
+#
 # Revision 1.3.2.5  2000/08/24 16:36:57  mcole
 # remove GenFunction from build until it compiles
 #
