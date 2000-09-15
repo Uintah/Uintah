7be539df9489a74e254eecdbb4d09ea554d6aa2c
Kurt Zimmerman <kuzimmer@sci.utah.edu>
2000-09-15 20:39:07 +0000

    needed to update these Datatypes

--- Core/Datatypes/TensorParticles.cc
+++ Core/Datatypes/TensorParticles.cc
@@ -31,8 +31,8 @@ TensorParticles::TensorParticles()
 }
 
 TensorParticles::TensorParticles(const ParticleVariable<Point>& positions,
-			       const ParticleVariable<Matrix3>& tensors,
-			       void* callbackClass) :
+				 const ParticleVariable<Matrix3>& tensors,
+				 void* callbackClass) :
   positions(positions), tensors(tensors), cbClass(callbackClass)
 {
 }
@@ -43,5 +43,68 @@ TensorParticles::~TensorParticles()
 }
 
 
+void TensorParticles::compute_bounds()
+{
+  if( have_bounds )
+    return;
+
+  Point min(1e30,1e30,1e30), max(-1e30,-1e30,-1e30);
+
+  ParticleSubset *ps = positions.getParticleSubset();
+  for(ParticleSubset::iterator iter = ps->begin();
+      iter != ps->end(); iter++){
+    max = SCICore::Geometry::Max(positions[ *iter ], max);
+    min = SCICore::Geometry::Min(positions[ *iter ], min);
+  }
+
+  if (min == max) {
+    min = Point(0,0,0);
+    max = Point(1,1,1);
+  }
+  have_bounds = true;
+  bmin = min;
+  bmax = max;
+}
+
+void TensorParticles::get_bounds(Point& p0, Point& p1)
+{
+  if( !have_bounds)
+    compute_bounds();
+
+  p0 = bmin;
+  p1 = bmax;
+}
+
+void TensorParticles::compute_minmax()
+{
+  if( have_minmax )
+    return;
+
+  double min = 1e30, max = -1e30;
+  ParticleSubset *ps = positions.getParticleSubset();
+  for(ParticleSubset::iterator iter = ps->begin();
+      iter != ps->end(); iter++){
+    max = ( tensors[ *iter ].Norm() > max ) ?
+      tensors[ *iter ].Norm() : max;
+    min = ( tensors[ *iter ].Norm() < min ) ?
+      tensors[ *iter ].Norm() : min;
+  }
+  if (min == max) {
+    min -= 0.001;
+    max += 0.001;
+  }
+  have_minmax = true;
+  data_min = min;
+  data_max = max;
+}
+
+void TensorParticles::get_minmax(double& v0, double& v1)
+{
+  if(!have_minmax)
+    compute_minmax();
+
+  v0 = data_min;
+  v1 = data_max; 
+}
 } // end namespace Datatypes
 } // end namespace Kurt
