a9b50d354eebb5ba8f392eace534a2413d9a605c
Steven G. Parker <sparker@sci.utah.edu>
2000-09-20 15:50:30 +0000

    Added problemSetup interface to scheduler Added ability to get/release the loadBalancer from the scheduler   (used for getting processor assignments to create per-processor    tasks in arches) Added getPatchwiseProcessorAssignment to LoadBalancer interface

--- Uintah/Interface/LoadBalancer.h
+++ Uintah/Interface/LoadBalancer.h
@@ -5,6 +5,7 @@
 #include <Uintah/Parallel/UintahParallelPort.h>
 
 namespace Uintah {
+   class Patch;
    class ProcessorGroup;
    class TaskGraph;
 /**************************************
@@ -42,6 +43,8 @@ WARNING
        virtual ~LoadBalancer();
 
        virtual void assignResources(TaskGraph& tg, const ProcessorGroup* resources) = 0;
+       virtual int getPatchwiseProcessorAssignment(const Patch* patch,
+						   const ProcessorGroup* resources) = 0;
 
     private:
        LoadBalancer(const LoadBalancer&);
@@ -52,6 +55,13 @@ WARNING
 
 //
 // $Log$
+// Revision 1.2  2000/09/20 15:50:30  sparker
+// Added problemSetup interface to scheduler
+// Added ability to get/release the loadBalancer from the scheduler
+//   (used for getting processor assignments to create per-processor
+//    tasks in arches)
+// Added getPatchwiseProcessorAssignment to LoadBalancer interface
+//
 // Revision 1.1  2000/06/17 07:06:46  sparker
 // Changed ProcessorContext to ProcessorGroup
 //
