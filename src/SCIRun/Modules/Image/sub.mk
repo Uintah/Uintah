fd5d31a500a25066fd7b53872ad075463125d45c
Marty Cole <mcole@sci.utah.edu>
2000-09-28 02:55:06 +0000

    merge trunk into FIELD_REDESIGN branch

--- src/SCIRun/Modules/Image/sub.mk
+++ src/SCIRun/Modules/Image/sub.mk
@@ -3,21 +3,50 @@
 # $Id$
 #
 
+# *** NOTE ***
+# 
+# Do not remove or modify the comment line:
+#
+# #[INSERT NEW ?????? HERE]
+#
+# It is required by the module maker to properly edit this file.
+# if you want to edit this file by hand, see the "Create A New Module"
+# documentation on how to do it correctly.
+
 include $(SRCTOP)/scripts/smallso_prologue.mk
 
 SRCDIR   := SCIRun/Modules/Image
 
-SRCS     += $(SRCDIR)/Binop.cc $(SRCDIR)/Edge.cc $(SRCDIR)/FFT.cc \
-	$(SRCDIR)/FFTImage.cc $(SRCDIR)/FilterImage.cc \
-	$(SRCDIR)/Gauss.cc $(SRCDIR)/Hist.cc $(SRCDIR)/HistEq.cc \
-	$(SRCDIR)/IFFT.cc $(SRCDIR)/IFFTImage.cc \
-	$(SRCDIR)/ImageConvolve.cc $(SRCDIR)/ImageGen.cc \
-	$(SRCDIR)/ImageSel.cc $(SRCDIR)/ImageToGeom.cc \
-	$(SRCDIR)/Noise.cc $(SRCDIR)/PMFilterImage.cc \
-	$(SRCDIR)/Radon.cc $(SRCDIR)/Segment.cc $(SRCDIR)/Sharpen.cc \
-	$(SRCDIR)/Snakes.cc $(SRCDIR)/Subsample.cc $(SRCDIR)/Ted.cc \
-	$(SRCDIR)/Threshold.cc $(SRCDIR)/Transforms.cc $(SRCDIR)/Turk.cc \
-	$(SRCDIR)/Unop.cc $(SRCDIR)/ViewHist.cc $(SRCDIR)/WhiteNoiseImage.cc
+SRCS     += \
+	$(SRCDIR)/Binop.cc\
+	$(SRCDIR)/Edge.cc\
+	$(SRCDIR)/FFT.cc\
+	$(SRCDIR)/FFTImage.cc\
+	$(SRCDIR)/FilterImage.cc\
+	$(SRCDIR)/Gauss.cc\
+	$(SRCDIR)/Hist.cc\
+	$(SRCDIR)/HistEq.cc\
+	$(SRCDIR)/IFFT.cc\
+	$(SRCDIR)/IFFTImage.cc\
+	$(SRCDIR)/ImageConvolve.cc\
+	$(SRCDIR)/ImageGen.cc\
+	$(SRCDIR)/ImageSel.cc\
+	$(SRCDIR)/ImageToGeom.cc\
+	$(SRCDIR)/Noise.cc\
+	$(SRCDIR)/PMFilterImage.cc\
+	$(SRCDIR)/Radon.cc\
+	$(SRCDIR)/Segment.cc\
+	$(SRCDIR)/Sharpen.cc\
+	$(SRCDIR)/Snakes.cc\
+	$(SRCDIR)/Subsample.cc\
+	$(SRCDIR)/Ted.cc\
+	$(SRCDIR)/Threshold.cc\
+	$(SRCDIR)/Transforms.cc\
+	$(SRCDIR)/Turk.cc\
+	$(SRCDIR)/Unop.cc\
+	$(SRCDIR)/ViewHist.cc\
+	$(SRCDIR)/WhiteNoiseImage.cc\
+#[INSERT NEW MODULE HERE]
 
 PSELIBS := SCIRun/Datatypes/Image PSECore/Dataflow PSECore/Datatypes \
 	SCICore/Datatypes SCICore/Persistent SCICore/Exceptions \
@@ -29,6 +58,16 @@ include $(SRCTOP)/scripts/smallso_epilogue.mk
 
 #
 # $Log$
+# Revision 1.2.2.1  2000/09/28 02:55:00  mcole
+# merge trunk into FIELD_REDESIGN branch
+#
+# Revision 1.4  2000/06/08 22:46:35  moulding
+# Added a comment note about not messing with the module maker comment lines,
+# and how to edit this file by hand.
+#
+# Revision 1.3  2000/06/07 17:32:58  moulding
+# made changes to allow the module maker to add to and edit this file
+#
 # Revision 1.2  2000/03/20 19:38:12  sparker
 # Added VPATH support
 #
--- src/SCIRun/Modules/Mesh/sub.mk
+++ src/SCIRun/Modules/Mesh/sub.mk
@@ -3,15 +3,32 @@
 # $Id$
 #
 
+# *** NOTE ***
+# 
+# Do not remove or modify the comment line:
+#
+# #[INSERT NEW ?????? HERE]
+#
+# It is required by the module maker to properly edit this file.
+# if you want to edit this file by hand, see the "Create A New Module"
+# documentation on how to do it correctly.
+
 include $(SRCTOP)/scripts/smallso_prologue.mk
 
 SRCDIR   := SCIRun/Modules/Mesh
 
-SRCS     += $(SRCDIR)/Delaunay.cc $(SRCDIR)/ExtractMesh.cc \
-	$(SRCDIR)/HexMeshToGeom.cc $(SRCDIR)/InsertDelaunay.cc \
-	$(SRCDIR)/MakeScalarField.cc $(SRCDIR)/MeshBoundary.cc \
-	$(SRCDIR)/MeshInterpVals.cc $(SRCDIR)/MeshRender.cc \
-	$(SRCDIR)/MeshToGeom.cc $(SRCDIR)/MeshView.cc
+SRCS     += \
+	$(SRCDIR)/Delaunay.cc\
+	$(SRCDIR)/ExtractMesh.cc\
+	$(SRCDIR)/HexMeshToGeom.cc\
+	$(SRCDIR)/InsertDelaunay.cc\
+	$(SRCDIR)/MakeScalarField.cc\
+	$(SRCDIR)/MeshBoundary.cc\
+	$(SRCDIR)/MeshInterpVals.cc\
+	$(SRCDIR)/MeshRender.cc\
+	$(SRCDIR)/MeshToGeom.cc\
+	$(SRCDIR)/MeshView.cc
+#[INSERT NEW MODULE HERE]
 
 PSELIBS := PSECore/Dataflow PSECore/Datatypes PSECore/Widgets \
 	SCICore/Geom SCICore/Thread SCICore/Exceptions \
@@ -23,6 +40,32 @@ include $(SRCTOP)/scripts/smallso_epilogue.mk
 
 #
 # $Log$
+# Revision 1.2.2.1  2000/09/28 02:55:02  mcole
+# merge trunk into FIELD_REDESIGN branch
+#
+# Revision 1.8  2000/09/07 16:14:42  dmw
+# removed MeshNodeComponent from sub.mk -- it wasnt ready to be committed yet...
+#
+# Revision 1.7  2000/09/07 06:35:15  dmw
+# Removed SliceMeshToGeom.cc reference in sub.mk
+#
+# Revision 1.6  2000/09/07 00:16:16  zyp
+# Removing the SliceMeshToGeom.cc because I accidently added it in my
+# last commit.  Also in the last commit I didn't add the comments
+# properly, so I will mention them now.  I changed MakeScalarField to be
+# able to handle a ColumnMatrix of the size of the number of nodes in a
+# mesh or the number of elements and assign the values from the matrix accordingly.
+#
+# Revision 1.5  2000/09/07 00:12:19  zyp
+# MakeScalarField.cc
+#
+# Revision 1.4  2000/06/08 22:46:36  moulding
+# Added a comment note about not messing with the module maker comment lines,
+# and how to edit this file by hand.
+#
+# Revision 1.3  2000/06/07 17:32:59  moulding
+# made changes to allow the module maker to add to and edit this file
+#
 # Revision 1.2  2000/03/20 19:38:13  sparker
 # Added VPATH support
 #
