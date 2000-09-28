4766fd9834f2470cd4df56150e58bf4b70992bb5
Marty Cole <mcole@sci.utah.edu>
2000-09-28 03:20:20 +0000

    merge trunk into FIELD_REDESIGN branch

--- src/Packages/Kurt/Core/Datatypes/GLMIP.cc
+++ src/Packages/Kurt/Core/Datatypes/GLMIP.cc
@@ -5,7 +5,6 @@ namespace SCICore {
 namespace GeomSpace  {
 
 
-GLTexRenState* GLMIP::_instance = 0;
 
 GLMIP::GLMIP(const GLVolumeRenderer* glvr) :
   GLTexRenState( glvr )
@@ -24,16 +23,6 @@ void GLMIP::postDraw()
   glDisable(GL_BLEND);
 }
 
-GLTexRenState* GLMIP::Instance(const GLVolumeRenderer* glvr)
-{
-  // Not a true Singleton class, but this does make sure that 
-  // there is only one instance per volume renderer.
-  if( _instance == 0 ){
-    _instance = new GLMIP( glvr );
-  } 
-  
-  return _instance;
-}
 
 } // end namespace Datatypes
 } // end namespace Kurt
