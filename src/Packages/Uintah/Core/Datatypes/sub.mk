93805a5227b899fec7c7092a37b5412dcc7aae4d
No Author <null@sci.utah.edu>
2000-09-28 03:20:20 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.4.2'.

--- src/Packages/Uintah/Core/Datatypes/sub.mk
+++ src/Packages/Uintah/Core/Datatypes/sub.mk
@@ -1,7 +1,8 @@
 #
 # Makefile fragment for this subdirectory
 # $Id$
-#
+
+include $(SRCTOP)/scripts/smallso_prologue.mk
 
 SRCDIR := Uintah/Datatypes
 
@@ -9,8 +10,40 @@ SUBDIRS := $(SRCDIR)/Particles
 
 include $(SRCTOP)/scripts/recurse.mk
 
+SRCS     += $(SRCDIR)/Archive.cc  $(SRCDIR)/ArchivePort.cc \
+	$(SRCDIR)/NCScalarField.cc $(SRCDIR)/CCScalarField.cc \
+	$(SRCDIR)/NCVectorField.cc $(SRCDIR)/CCVectorField.cc \
+	$(SRCDIR)/NCTensorField.cc $(SRCDIR)/CCTensorField.cc \
+	$(SRCDIR)/TensorField.cc $(SRCDIR)/TensorFieldPort.cc \
+	$(SRCDIR)/ScalarParticles.cc $(SRCDIR)/ScalarParticlesPort.cc \
+	$(SRCDIR)/VectorParticles.cc $(SRCDIR)/VectorParticlesPort.cc \
+	$(SRCDIR)/TensorParticles.cc $(SRCDIR)/TensorParticlesPort.cc 
+
+
+
+PSELIBS := SCICore/Exceptions SCICore/Geometry \
+	SCICore/Persistent SCICore/Datatypes \
+	SCICore/Containers SCICore/Thread Uintah/Grid Uintah/Interface \
+        Uintah/Exceptions PSECore/XMLUtil Uintah/Components/MPM
+
+LIBS := $(XML_LIBRARY)
+
+ifeq ($(BUILD_PARALLEL),yes)
+PSELIBS := $(PSELIBS) Component/CIA Component/PIDL
+LIBS := $(LIBS) $(GLOBUS_LIBS) -lglobus_nexus
+endif
+
+include $(SRCTOP)/scripts/smallso_epilogue.mk
+
+
 #
 # $Log$
+# Revision 1.4  2000/07/31 17:45:42  kuzimmer
+# Added files and modules for Field Extraction from uda
+#
+# Revision 1.3  2000/06/20 20:13:27  kuzimmer
+# updated so that Archiver reader will compile in Uintah
+#
 # Revision 1.2  2000/03/20 19:38:29  sparker
 # Added VPATH support
 #
@@ -19,4 +52,8 @@ include $(SRCTOP)/scripts/recurse.mk
 # Use XML-based files for module repository
 # Plus many other changes to make these two things work
 #
+#
+#
+# Makefile fragment for this subdirectory
+# $Id$
 #
