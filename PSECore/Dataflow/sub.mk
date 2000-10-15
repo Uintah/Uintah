384dc8ba4b222fbb09b27b2547e462ed4fde1e61
Chris Moulding <moulding@sci.utah.edu>
2000-10-15 04:17:20 +0000

    Phase 1 for the new module maker: Prepare the repository for the new module maker.
    added some new files, modified makefiles to include the new files, and
    modified XMLUtil to include some more useful functions.
    
    These changes do not bring the new module maker online.

--- PSECore/Dataflow/sub.mk
+++ PSECore/Dataflow/sub.mk
@@ -10,17 +10,28 @@ SRCDIR   := PSECore/Dataflow
 SRCS     += $(SRCDIR)/Connection.cc $(SRCDIR)/ModuleHelper.cc \
 	$(SRCDIR)/Network.cc $(SRCDIR)/Port.cc $(SRCDIR)/Module.cc \
 	$(SRCDIR)/NetworkEditor.cc $(SRCDIR)/PackageDB.cc \
+	$(SRCDIR)/FileUtils.cc $(SRCDIR)/GenFiles.cc\
+	$(SRCDIR)/ComponentNode.cc $(SRCDIR)/SkeletonFiles.cc\
         $(SRCDIR)/CreatePacCatMod.cc
 
 PSELIBS := PSECore/Comm SCICore/Exceptions SCICore/Thread \
 	SCICore/Containers SCICore/TclInterface SCICore/Util \
-	SCICore/TkExtensions SCICore/Geom
+	SCICore/TkExtensions SCICore/Geom PSECore/XMLUtil
 LIBS := $(TCL_LIBRARY) $(XML_LIBRARY)
 
 include $(SRCTOP)/scripts/smallso_epilogue.mk
 
 #
 # $Log$
+# Revision 1.5  2000/10/15 04:17:20  moulding
+# Phase 1 for the new module maker: Prepare the repository for the new
+# module maker.
+#
+# added some new files, modified makefiles to include the new files, and
+# modified XMLUtil to include some more useful functions.
+#
+# These changes do not bring the new module maker online.
+#
 # Revision 1.4  2000/06/07 00:02:32  moulding
 # removed the package maker, ane added the module maker
 #
