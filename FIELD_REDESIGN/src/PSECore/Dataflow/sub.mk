c243d5371a7bd112d5706b4822fd11bea24232a5
Chris Moulding <moulding@sci.utah.edu>
2000-10-16 23:15:31 +0000

    more Phase 1 changes to FIELD_REDESIGN branch.

--- FIELD_REDESIGN/src/PSECore/Dataflow/sub.mk
+++ FIELD_REDESIGN/src/PSECore/Dataflow/sub.mk
@@ -10,19 +10,30 @@ SRCDIR   := PSECore/Dataflow
 SRCS     += $(SRCDIR)/Connection.cc $(SRCDIR)/ModuleHelper.cc \
 	$(SRCDIR)/Network.cc $(SRCDIR)/Port.cc $(SRCDIR)/Module.cc \
 	$(SRCDIR)/NetworkEditor.cc $(SRCDIR)/PackageDB.cc \
+	$(SRCDIR)/FileUtils.cc $(SRCDIR)/GenFiles.cc\
+	$(SRCDIR)/ComponentNode.cc $(SRCDIR)/SkeletonFiles.cc\
         $(SRCDIR)/CreatePacCatMod.cc
 
 PSELIBS := PSECore/Comm SCICore/Exceptions SCICore/Thread \
 	SCICore/Containers SCICore/TclInterface SCICore/Util \
-	SCICore/TkExtensions SCICore/Geom
+	SCICore/TkExtensions SCICore/Geom PSECore/XMLUtil
 LIBS := $(TCL_LIBRARY) $(XML_LIBRARY)
 
 include $(SRCTOP)/scripts/smallso_epilogue.mk
 
 #
 # $Log$
-# Revision 1.3.2.1  2000/09/28 03:14:57  mcole
-# merge trunk into FIELD_REDESIGN branch
+# Revision 1.3.2.2  2000/10/16 23:15:24  moulding
+# more Phase 1 changes to FIELD_REDESIGN branch.
+#
+# Revision 1.5  2000/10/15 04:17:20  moulding
+# Phase 1 for the new module maker: Prepare the repository for the new
+# module maker.
+#
+# added some new files, modified makefiles to include the new files, and
+# modified XMLUtil to include some more useful functions.
+#
+# These changes do not bring the new module maker online.
 #
 # Revision 1.4  2000/06/07 00:02:32  moulding
 # removed the package maker, ane added the module maker
