39d398a22ac57c6f361657b33b0bcedb4989625d
Chris Moulding <moulding@sci.utah.edu>
2000-10-23 17:55:59 +0000

    more changes to new module maker

--- Dataflow/Network/FileUtils.cc
+++ Dataflow/Network/FileUtils.cc
@@ -5,13 +5,80 @@
 #include <string.h>
 #include <dirent.h>
 #include <PSECore/Dataflow/FileUtils.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <errno.h>
 
 namespace PSECore {
 namespace Dataflow {
 
+/* Normally, I would just use sed via system() to edit a file,
+   but for some reason system() calls never work from SCIRun 
+   processes in linux.  Oh well, sed isn't natively available
+   under windows, so I'd have to do something like this anyhow
+   - Chris Moulding */ 
+
+void InsertStringInFile(char* filename, char* match, char* replacement)
+{
+  char* newfilename = new char[strlen(filename)+2];
+  char c;
+  sprintf(newfilename,"%s~",filename);
+  FILE* ifile;
+  FILE* ofile;
+
+  /* create a copy of the original file */
+  ifile = fopen(filename,"r");
+  ofile = fopen(newfilename,"w");
+
+  c = (char)fgetc(ifile);
+  while (c!=EOF) {
+    fprintf(ofile,"%c",c);
+    c = (char)fgetc(ifile);
+  }
+  fclose(ifile);
+  fclose(ofile);
+
+  /* search the copy for an instance of "match" */
+  int index1 = 0, index2 = 0;
+  int foundat = -1;
+  ifile = fopen(newfilename,"r");
+  c = (char)fgetc(ifile);
+  while (c!=EOF) {
+    if (c==match[index2]) {
+      foundat = index1;
+      while (index2<strlen(match) && c!=EOF && c==match[index2]) {
+	c = (char)fgetc(ifile);
+	index1++;
+	index2++;
+      }
+      if (foundat>=0 && index2!=strlen(match)) {
+	foundat = -1;
+	index2 = 0;
+      } else
+	break;
+    }
+    c = (char)fgetc(ifile);
+    index1++;
+  }
+  fclose(ifile);
+
+  /* if an instance of match was found, 
+     insert the indicated string */
+  if (foundat>=0) {
+    index1 = 0;
+    ifile = fopen(newfilename,"r");
+    ofile = fopen(filename,"w");
+    c = (char)fgetc(ifile);
+    while (c!=EOF) {
+      if (index1==foundat)
+        fprintf(ofile,"%s",replacement);
+      fprintf(ofile,"%c",c);
+      c = (char)fgetc(ifile);
+      index1++;
+    }
+    fclose(ifile);
+    fclose(ofile);
+  } 
+}
+
+#if 0
 void InsertStringInFile(char* filename, char* match, char* replacement)
 {
   char* string = 0;
@@ -31,6 +98,7 @@ void InsertStringInFile(char* filename, char* match, char* replacement)
   delete[] string;
   delete[] mod;
 }
+#endif
 
 std::map<int,char*>* GetFilenamesEndingWith(char* d, char* ext)
 {
