e86986ad9af7eab3fd99532d6944b0ee50397cad
No Author <null@sci.utah.edu>
2000-10-26 10:07:00 +0000

    This commit was manufactured by cvs2svn to create branch 'FIELD_REDESIGN'.

---
+++ src/Moulding/Datatypes/sub.mk
@@ -0,0 +1,22 @@
+# *** NOTE ***
+#
+# Do not remove or modify the comment line:
+#
+# #[INSERT NEW ?????? HERE]
+#
+# It is required by the Component Wizard to properly edit this file.
+# if you want to edit this file by hand, see the "Create A New Component"
+# documentation on how to do it correctly.
+
+include $(OBJTOP_ABS)/scripts/smallso_prologue.mk
+
+SRCDIR   := Moulding/Datatypes
+
+SRCS     += \
+#[INSERT NEW CODE FILE HERE]
+
+PSELIBS :=
+LIBS :=
+
+include $(OBJTOP_ABS)/scripts/smallso_epilogue.mk
+
---
+++ src/Remote/GUI/sub.mk
@@ -0,0 +1,42 @@
+#
+# Makefile fragment for this subdirectory
+# $Id$
+#
+
+SRCDIR := Remote/GUI
+
+ALLTARGETS := $(ALLTARGETS) $(SRCDIR)/tclIndex
+
+$(SRCDIR)/tclIndex: \
+	$(SRCDIR)/remoteSalmon.tcl\
+#[INSERT NEW TCL FILE HERE]
+	$(SRCTOP)/scripts/createTclIndex $(SRCTOP)/Remote/GUI
+
+CLEANPROGS := $(CLEANPROGS) $(SRCDIR)/tclIndex
+
+#
+# $Log$
+# Revision 1.3  2000/10/24 05:57:45  moulding
+# new module maker Phase 2: new module maker goes online
+#
+# These changes clean out the last remnants of the old module maker and
+# bring the new module maker online.
+#
+# Revision 1.2  2000/06/06 16:39:10  dahart
+# Fixed a bug
+#
+# Revision 1.1  2000/06/06 15:29:24  dahart
+# - Added a new package / directory tree for the remote visualization
+# framework.
+# - Added a new Salmon-derived module with a small server-daemon,
+# network support, and a couple of very basic remote vis. services.
+#
+# Revision 1.2  2000/03/20 19:36:50  sparker
+# Added VPATH support
+#
+# Revision 1.1  2000/03/17 09:26:45  sparker
+# New makefile scheme: sub.mk instead of Makefile.in
+# Use XML-based files for module repository
+# Plus many other changes to make these two things work
+#
+#
