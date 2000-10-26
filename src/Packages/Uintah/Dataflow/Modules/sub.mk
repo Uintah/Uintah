e660b64ae70c9d2f9609600f53f9a95701a85760
Chris Moulding <moulding@sci.utah.edu>
2000-10-26 10:07:00 +0000

    merge HEAD into FIELD_REDESIGN

--- src/Packages/Uintah/Dataflow/Modules/sub.mk
+++ src/Packages/Uintah/Dataflow/Modules/sub.mk
@@ -5,13 +5,26 @@
 
 SRCDIR := Uintah/Modules
 
-SUBDIRS := $(SRCDIR)/MPMViz $(SRCDIR)/Readers \
-	$(SRCDIR)/Writers $(SRCDIR)/Visualization
+SUBDIRS := \
+	$(SRCDIR)/MPMViz\
+	$(SRCDIR)/Readers\
+	$(SRCDIR)/Writers\
+	$(SRCDIR)/Visualization\
+#[INSERT NEW CATEGORY DIR HERE]
 
 include $(SRCTOP)/scripts/recurse.mk
 
 #
 # $Log$
+# Revision 1.3.2.1  2000/10/26 10:06:18  moulding
+# merge HEAD into FIELD_REDESIGN
+#
+# Revision 1.4  2000/10/24 05:57:55  moulding
+# new module maker Phase 2: new module maker goes online
+#
+# These changes clean out the last remnants of the old module maker and
+# bring the new module maker online.
+#
 # Revision 1.3  2000/06/20 17:57:16  kuzimmer
 # Moved GridVisualizer to Uintah
 #
