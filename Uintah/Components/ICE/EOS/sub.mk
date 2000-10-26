486d291162b22a2e77e91237b52ba1be6d70f5e3
John Schmidt <jas@sci.utah.edu>
2000-10-26 23:37:05 +0000

    Added Harlow EOS.

--- Uintah/Components/ICE/EOS/sub.mk
+++ Uintah/Components/ICE/EOS/sub.mk
@@ -7,7 +7,7 @@ SRCDIR   := Uintah/Components/ICE/EOS
 
 SRCS     += $(SRCDIR)/EquationOfState.cc \
 	$(SRCDIR)/EquationOfStateFactory.cc \
-	$(SRCDIR)/IdealGas.cc 
+	$(SRCDIR)/IdealGas.cc $(SRCDIR)/Harlow.cc
 
 PSELIBS := Uintah/Interface Uintah/Grid Uintah/Parallel \
 	Uintah/Exceptions SCICore/Exceptions SCICore/Thread \
@@ -16,6 +16,9 @@ LIBS	:= $(XML_LIBRARY)
 
 #
 # $Log$
+# Revision 1.2  2000/10/26 23:37:05  jas
+# Added Harlow EOS.
+#
 # Revision 1.1  2000/10/06 04:02:16  jas
 # Move into a separate EOS directory.
 #
