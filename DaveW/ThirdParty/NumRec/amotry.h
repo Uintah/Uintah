073e84971db97f5c55b128570b873f1f0b55a461
David Weinstein <dmw@sci.utah.edu>
2000-10-29 04:05:05 +0000

    updates to make downhill simplex more interactive

--- DaveW/ThirdParty/NumRec/amotry.h
+++ DaveW/ThirdParty/NumRec/amotry.h
@@ -2,6 +2,6 @@
 #define AMOTRY_H
 
 double* amotry(double **p, double  y[], double  psum[], int ndim,
-	       double *(*funk)(double []), int ihi, double fac, int extra);
+	       double *(*funk)(int), int ihi, double fac, int extra);
 
 #endif
--- DaveW/ThirdParty/NumRec/protozoa.cc
+++ DaveW/ThirdParty/NumRec/protozoa.cc
@@ -268,7 +268,6 @@ void protozoa(double **p, double y[], int ndim, double ftol,
 	
 	    solveMatrix(&A,&x,squareTermSize,mpts);
 
-	    
    // so you have the matrix solution.  Now to find the minimum value for the quadratic equation you take the derivative of ax^2 + bx (for each dimension x,y,z) and set it to zero so x = -b/2a
    for (i=1;i<=ndim;i++){
        if (x[i+ndim] <=  0) {  // it is convex instead of concave
@@ -393,3 +392,105 @@ void protozoa(double **p, double y[], int ndim, double ftol,
 
 #undef SWAP
 #undef GET_PSUM
+
+
+#if 0
+void protozoa(double **p, double y[], int ndim, double ftol,
+	    double *(*funk)(int), int *nfunk, int extra, int *stop)
+{
+
+    int i,k,maxrow,ihi,ilo,inhi,j,mpts=2*ndim+1;
+    double rtol,sum,swap,ysave,*ytry,tmp;
+    Array1<double> x(mpts+1);
+    Array1<double> fac(mpts+1);
+    Array1<double> row(mpts+2);
+    Array2<double> A(mpts+2,mpts+1);
+    Array1<double> psum(mpts+1);
+    Array1<int> indx(mpts+1);
+    
+    int NMAX=*nfunk;
+    *nfunk=0;
+    cerr << "calculate sum\n";
+    //  GET_PSUM
+	for (;;) {
+	    //    cerr << "find hi\n";
+	    ilo=1;
+	    ihi = y[1]>y[2] ? (inhi=2,1) : (inhi=1,2);
+	    for (i=1;i<=mpts;i++) {
+		if (y[i] <= y[ilo]) ilo=i;
+		if (y[i] > y[ihi]) {
+		    inhi=ihi;
+		    ihi=i;
+		} else if (y[i] > y[inhi] && i != ihi) inhi=i;
+	    }
+	    rtol=2.0*fabs(y[ihi]-y[ilo])/(fabs(y[ihi])+fabs(y[ilo]));
+
+	    if (rtol < ftol || *stop) {
+		SWAP(y[1],y[ilo])
+		    for (i=1;i<=ndim+extra;i++) SWAP(p[1][i],p[ilo][i])
+						    break;
+	    }
+	    if (*nfunk >= NMAX) {
+		cerr << "NMAX exceeded";
+		break;
+	    }
+	    //*nfunk += 2;
+	    //    cerr << "assign the matrix\n";
+
+	    // This parts creates the matrix of of ax^2 + bx + cy^2 + dy +... + 1 = error
+
+	    for (i=1;i<=mpts;i++) {
+		A(mpts,i)=1;
+		A(mpts+1,i)= y[i]*y[i];
+		for (j=1;j<=ndim;j++) {
+		    A(j,i)= p[i][j];
+		    A(j+ndim,i) = p[i][j]*p[i][j];
+		}
+	    }
+	    //    cerr << "invert the matrix\n";
+	
+	    //do some gaussian elimination:
+   for (i=1;i<=mpts;i++) {
+
+      /* Find the row with the largest first value */
+      maxrow = i;
+      for (j=i+1;j<=mpts;j++) {
+         if (abs(A(i,j)) > abs(A(i,maxrow)))
+            maxrow = j;
+      }
+
+      /* Swap the maxrow and ith row */
+      for (k=i;k<=mpts+1;k++) {
+         tmp = A(k,i);
+         A(k,i) = A(k,maxrow);
+         A(k,maxrow) = tmp;
+      }
+
+      /* Singular matrix? */
+      if (abs(A(i,i)) < 0.000000001)
+	  cerr << "urghh... This matrix isn't looking very healthy" << endl;
+
+      /* Eliminate the ith element of the jth row */
+      for (j=i+1;j<=mpts;j++) {
+         for (k=mpts+1;k>=i;k--) {
+            A(k,j) -= A(k,i) * A(i,j) / A(i,i);
+         }
+      }
+   }
+
+   /* Do the back substitution */
+   for (j=mpts;j>=1;j--) {
+      tmp = 0;
+      for (k=j+1;k<=mpts;k++)
+         tmp += A(k,j) * x[k];
+      x[j] = (A(mpts+1,j) - tmp) / A(j,j);
+   }
+
+
+   // so you have the matrix solution.  Now to find the minimum value for the quadratic equation you take the derivative of ax^2 + bx (for each dimension x,y,z) and set it to zero so x = -b/2a
+   for (i=1;i<=ndim;i++){
+       if (x[i+ndim] <=  0) {  // it is convex instead of concave
+	   cerr << "convexity detected on axis " << i << endl;
+	   GET_PSUM
+	   p[ihi][i] = psum[i]/mpts;
+#endif
