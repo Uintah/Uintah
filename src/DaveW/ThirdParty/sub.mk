1625340c47f95fd7dfb298f749875597692340c1
David Weinstein <dmw@sci.utah.edu>
2000-10-29 04:05:05 +0000

    updates to make downhill simplex more interactive

--- src/DaveW/ThirdParty/sub.mk
+++ src/DaveW/ThirdParty/sub.mk
@@ -5,12 +5,15 @@
 
 SRCDIR := DaveW/ThirdParty
 
-SUBDIRS := $(SRCDIR)/NumRec $(SRCDIR)/Nrrd $(SRCDIR)/OldLinAlg
+SUBDIRS := $(SRCDIR)/Nrrd $(SRCDIR)/NumRec $(SRCDIR)/OldLinAlg
 
 include $(SRCTOP)/scripts/recurse.mk
 
 #
 # $Log$
+# Revision 1.3  2000/10/29 04:05:01  dmw
+# updates to make downhill simplex more interactive
+#
 # Revision 1.2  2000/03/20 19:36:27  sparker
 # Added VPATH support
 #
