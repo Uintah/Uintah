f16e1190339d0e18f2ffa835238a4d53e420a467
Kurt Zimmerman <kuzimmer@sci.utah.edu>
2000-11-02 19:23:51 +0000

    Changes for querying particles

--- Uintah/GUI/ParticleFieldExtractor.tcl
+++ Uintah/GUI/ParticleFieldExtractor.tcl
@@ -39,6 +39,15 @@ itcl_class Uintah_Visualization_ParticleFieldExtractor {
     
     protected pf ""
 
+################################################
+    protected var_list ""
+    protected mat_list ""
+    protected var_val_list {}
+    protected graph_data_names ""
+    protected time_list {}
+    protected num_materials 0
+    protected num_colors 240
+###############################################
 
     constructor {config} { 
         set name ParticleFieldExtractor 
@@ -193,6 +202,7 @@ itcl_class Uintah_Visualization_ParticleFieldExtractor {
 	    }
 	    #puts [$parent.m.p$i configure -variable]
 	}
+	set num_materials $ns
     }
 
     method isOn { bval } {
@@ -253,45 +263,6 @@ itcl_class Uintah_Visualization_ParticleFieldExtractor {
 	}
     }      
 	
-    method graph { id var args } {
-	global $this-pvVar
-	set w .graph[modname]$id
-        if {[winfo exists $w]} { 
-            destroy $w 
-	}
-	toplevel $w
-#	wm minsize $w 300 300
-
-	button $w.close -text "Close" -command "destroy $w"
-	pack $w.close -side bottom -anchor s -expand yes -fill x
-	
-	blt::graph $w.graph -title "Particle Value" -height 250 \
-		-plotbackground gray99
-
-	set max 1e-10
-	set min 1e+10
-
-	for { set i 1 } { $i < [llength $args] } { set i [expr $i + 2]} {
-	    set val [lindex $args $i]
-	    if { $max < $val } { set max $val }
-	    if { $min > $val } { set min $val }
-	}
-	if { ($max - $min) > 1000 || ($max - $min) < 1e-3 } {
-	    $w.graph yaxis configure -logscale true -title $var
-	} else {
-	    $w.graph yaxis configure -title $var
-	}
-
-	$w.graph xaxis configure -title "Timestep" \
-		-loose true
-	$w.graph element create "Particle Value" -linewidth 2 -color blue \
-	    -pixels 3
-
-	pack $w.graph
-	if { $args != "" } {
-	    $w.graph element configure "Particle Value" -data "$args"
-	}
-    }
     method infoFrame { id } {
 	set w .info[modname]$id
         if {[winfo exists $w]} { 
@@ -324,5 +295,237 @@ itcl_class Uintah_Visualization_ParticleFieldExtractor {
 	    pack $w.$args1.b -side left -expand yes -fill x
 	}
     }
+
+#################################################################3
+    method create_part_graph_window { part_id } {
+	# var_list,mat_list, and time_list should
+	# be initialized by this point
+	# part_id must not have any periods in it
+
+        set w .ui[modname]$part_id
+
+	#$this setVar_list "p.mass" "p.stress" "p.cheese"
+	#$this setMat_list 3 3 3
+	#$this setTime_list 0 1 2 3 4
+	
+        if {[winfo exists $w]} { 
+	    wm deiconify $w
+            raise $w 
+        } else { 
+	    # build the window
+	    toplevel $w
+	    $this buildVarFrame $w
+
+	    wm deiconify $w
+            raise $w 
+	}
+    }
+    method setVar_list { args } {
+	set var_list $args
+	puts "var_list is now $var_list"
+    }
+    method setMat_list { args } {
+	set mat_list $args
+	puts "mat_list is now $mat_list"
+    }
+    method setTime_list { args } {
+	set time_list $args
+	puts "time_list is now $time_list"
+    }
+    method reset_var_val {} {
+	set var_val_list {}
+    }
+    method set_var_val { args } {
+	set val_list $args
+	lappend var_val_list $val_list
+	puts "Args were $args"
+	puts "New var_val_list: $var_val_list"
+    }
+    method setGraph_names { args } {
+	set graph_data_names $args
+	puts "graph_data_names is now $graph_data_names"
+    }
+    method mat_sel { var num_mat val} {
+	for {set j 0} { $j < $num_mat} {incr j} {
+	    set nvar $var
+	    append nvar $j
+	    set $this-matvar$nvar $val
+	}
+    }
+    method make_mat_menu {w mat i} {
+	set fname "$w.mat$mat"
+	menubutton $fname -text "Material" \
+		-menu $fname.list -relief groove
+	pack $fname -side right -padx 2 -pady 2
+	
+	menu $fname.list
+	$fname.list add command -label "Sel All" \
+		-command "$this mat_sel $i $mat 1"
+	$fname.list add command -label "Sel None" \
+		-command "$this mat_sel $i $mat 0"
+	for {set j 0} { $j < $mat} {incr j} {
+	    set var $i
+	    append var $j
+	    set $this-matvar$var 0
+	    $fname.list add checkbutton \
+		    -variable $this-matvar$var \
+		    -label "Mat $j"
+	}
+    }
+    method graphbutton {name var_index num_mat} {
+#	$this-c graph $name [set $this-vmat$i] $i
+	set val_list {}
+	set num_vals 0
+	for {set j 0} { $j < $num_mat} {incr j} {
+	    set nvar $var_index
+	    append nvar $j
+	    if {[set $this-matvar$nvar] != 0} {
+		lappend val_list $j
+		incr num_vals
+	    }
+	}
+	puts "Calling $this-c graph"
+	puts "name      = $name"
+	puts "num_mat   = $num_mat"
+	puts "var_index = $var_index"
+	puts "num_vals  = $num_vals"
+	puts "val_list  = $val_list"
+	set call "$this-c graph $name $var_index $num_vals"
+	for {set i 0} { $i < [llength $val_list] } { incr i } {
+	    set insert [lindex $val_list $i]
+	    append call " $insert"
+	}
+	eval $call
+	puts "call = $call"
+    }
+    method addVar {w name mat i} {
+	set fname "$w.var$i"
+	frame $fname
+	pack $fname -side top -fill x -padx 2 -pady 2
+
+	label $fname.label -text "$name"
+	pack $fname.label -side left -padx 2 -pady 2
+
+	button $fname.button -text "Graph" -command "$this graphbutton $name $i $mat"
+	pack $fname.button -side right -padx 2 -pady 2
+
+	if {$mat > $num_materials} {
+	    set num_materials $mat
+	    puts "num_materials is now $num_materials"
+	}
+
+	make_mat_menu $fname $mat $i
+    }
+    method buildVarFrame {w } {
+	if {[llength $var_list] > 0} {
+	    frame $w.vars -borderwidth 3 -relief ridge
+	    pack $w.vars -side top -fill x -padx 2 -pady 2
+	    
+	    puts "var_list length [llength $var_list]"
+	    for {set i 0} { $i < [llength $var_list] } { incr i } {
+		set newvar [lindex $var_list $i]
+#		set newmat [lindex $mat_list $i]
+#		addVar $w.vars $newvar $newmat $i
+		addVar $w.vars $newvar $num_materials $i
+	    }
+	}
+    }
+
+    method graph_data { id var args } {
+	set w .graph[modname]$id
+        if {[winfo exists $w]} { 
+            destroy $w 
+	}
+	toplevel $w
+#	wm minsize $w 300 300
+
+	puts "id = $id"
+	puts "var = $var"
+	puts "args = $args"
+
+	button $w.close -text "Close" -command "destroy $w"
+	pack $w.close -side bottom -anchor s -expand yes -fill x
+	
+	blt::graph $w.graph -title "Plot of $var with materials $args" \
+		-height 250 -plotbackground gray99
+
+	set max 1e-10
+	set min 1e+10
+
+	puts "length of var_val_list = [llength $var_val_list]"
+	puts "length of args         = $args"
+	for { set i 0 } { $i < [llength $args] } {incr i} {
+	    set mat_vals [lindex $var_val_list [lindex $args $i]]
+	    for { set j 0 } { $j < [llength $mat_vals] } {incr j} {
+		set val [lindex $mat_vals $j]
+		if { $max < $val } { set max $val }
+		if { $min > $val } { set min $val }
+	    }
+	}
+	
+	if { ($max - $min) > 1000 || ($max - $min) < 1e-3 } {
+	    $w.graph yaxis configure -logscale true -title $var
+	} else {
+	    $w.graph yaxis configure -title $var
+	}
+	
+	$w.graph xaxis configure -title "Timestep" -loose true
+	
+	puts "length of var_val_list = [llength $var_val_list]"
+	for { set i 0 } { $i < [llength $args] } {incr i} {
+	    puts "adding line"
+	    set mat_index  [lindex $args $i]
+	    set mat_vals [lindex $var_val_list $mat_index]
+	    puts "\[llength mat_vals\] = [llength $mat_vals]"
+	    puts "mat_vals = $mat_vals"
+	    set color_ind [expr round(double($mat_index) / ($num_materials-1) * ($num_colors - 1))]
+	    $w.graph element create "Material $mat_index" -linewidth 2 \
+		-pixels 3 -color [$this get_color $color_ind] \
+		-xdata $time_list -ydata $mat_vals
+	}
+	
+	pack $w.graph
+    }
+
+    method get_color { index } {
+	set color_scheme {
+	    { 255 0 0}  { 255 102 0}
+	    { 255 204 0}  { 255 234 0}
+	    { 204 255 0}  { 102 255 0}
+	    { 0 255 0}    { 0 255 102}
+	    { 0 255 204}  { 0 204 255}
+	    { 0 102 255}  { 0 0 255}}
+	#set color_scheme { {255 0 0} {0 255 0} {0 0 255} }
+	set incr {}
+	set upper_bounds [expr [llength $color_scheme] -1]
+	for {set j 0} { $j < $upper_bounds} {incr j} {
+	    set c1 [lindex $color_scheme $j]
+	    set c2 [lindex $color_scheme [expr $j + 1]]
+	    set incr_a {}
+	    lappend incr_a [expr [lindex $c2 0] - [lindex $c1 0]]
+	    lappend incr_a [expr [lindex $c2 1] - [lindex $c1 1]]
+	    lappend incr_a [expr [lindex $c2 2] - [lindex $c1 2]]
+	    lappend incr $incr_a
+	}
+	lappend incr {0 0 0}
+	puts "incr = $incr"
+	set step [expr $num_colors / [llength $color_scheme]]
+	set ind [expr $index % $num_colors] 
+	set i [expr $ind / $step]
+	set im [expr double($ind % $step)/$step]
+	puts "i = $i  im = $im"
+	set curr_color [lindex $color_scheme $i]
+	set curr_incr [lindex $incr $i]
+	puts "curr_color = $curr_color, curr_incr = $curr_incr"
+	set r [expr [lindex $curr_color 0]+round([lindex $curr_incr 0] * $im)] 
+	set g [expr [lindex $curr_color 1]+round([lindex $curr_incr 1] * $im)] 
+	set b [expr [lindex $curr_color 2]+round([lindex $curr_incr 2] * $im)] 
+	set c [format "#%02x%02x%02x" $r $g $b]
+	puts "r=$r, g=$g, b=$b, c=$c"
+	return $c
+    }
+
 	    	    
 }
+
+
