694151fbe6a6cfd2b52929426b48971e1eb6f334
John Schmidt <jas@sci.utah.edu>
2000-11-14 04:05:52 +0000

    Now storing patches with two indices: extraCell and interior cell limits.

--- src/Uintah/Components/SimulationController/SimulationController.cc
+++ src/Uintah/Components/SimulationController/SimulationController.cc
@@ -324,51 +324,64 @@ void SimulationController::problemSetup(const ProblemSpecP& params,
       
       for(ProblemSpecP box_ps = level_ps->findBlock("Box");
 	  box_ps != 0; box_ps = box_ps->findNextBlock("Box")){
-	 Point lower;
-	 box_ps->require("lower", lower);
-	 Point upper;
-	 box_ps->require("upper", upper);
-
-	 IntVector lowCell = level->getCellIndex(lower);
-	 IntVector highCell = level->getCellIndex(upper);
-	 Point lower2 = level->getNodePosition(lowCell);
-	 Point upper2 = level->getNodePosition(highCell);
-	 double diff_lower = (lower2-lower).length();
-	 if(diff_lower > 1.e-6)
-	    throw ProblemSetupException("Box lower corner does not coincide with grid");
-	 double diff_upper = (upper2-upper).length();
-	 if(diff_upper > 1.e-6){
-	    cerr << "upper=" << upper << '\n';
-	    cerr << "lowCell =" << lowCell << '\n';
-	    cerr << "highCell =" << highCell << '\n';
-	    cerr << "upper2=" << upper2 << '\n';
-	    cerr << "diff=" << diff_upper << '\n';
-	    throw ProblemSetupException("Box upper corner does not coincide with grid");
-	 }
-	 IntVector extraCells;
-	 if(box_ps->get("extraCells", extraCells)){
-	    lowCell = lowCell-extraCells;
-	    highCell = highCell+extraCells;
-	 }
-
-	 IntVector resolution(highCell-lowCell);
-	 if(resolution.x() < 1 || resolution.y() < 1 || resolution.z() < 1)
-	    throw ProblemSetupException("Degeneration patch");
-
-	 IntVector patches;
-	 if(box_ps->get("patches", patches)){
-	    for(int i=0;i<patches.x();i++){
-	       for(int j=0;j<patches.y();j++){
-		  for(int k=0;k<patches.z();k++){
-		     IntVector startcell = resolution*IntVector(i,j,k)/patches;
-		     IntVector endcell = resolution*IntVector(i+1,j+1,k+1)/patches;
-		     level->addPatch(startcell+lowCell, endcell+lowCell);
-		  }
-	       }
+	Point lower;
+	box_ps->require("lower", lower);
+	Point upper;
+	box_ps->require("upper", upper);
+	
+	IntVector lowCell = level->getCellIndex(lower);
+	IntVector highCell = level->getCellIndex(upper);
+	Point lower2 = level->getNodePosition(lowCell);
+	Point upper2 = level->getNodePosition(highCell);
+	double diff_lower = (lower2-lower).length();
+	if(diff_lower > 1.e-6)
+	  throw ProblemSetupException("Box lower corner does not coincide with grid");
+	double diff_upper = (upper2-upper).length();
+	if(diff_upper > 1.e-6){
+	  cerr << "upper=" << upper << '\n';
+	  cerr << "lowCell =" << lowCell << '\n';
+	  cerr << "highCell =" << highCell << '\n';
+	  cerr << "upper2=" << upper2 << '\n';
+	  cerr << "diff=" << diff_upper << '\n';
+	  throw ProblemSetupException("Box upper corner does not coincide with grid");
+	}
+	// Determine the interior cell limits.  For no extraCells, the limits
+	// will be the same.  For extraCells, the interior cells will have
+	// different limits so that we can develop a CellIterator that will
+	// use only the interior cells instead of including the extraCell
+	// limits.
+	IntVector inLowCell = lowCell;
+	IntVector inHighCell = highCell;
+	
+	IntVector extraCells;
+	if(box_ps->get("extraCells", extraCells)){
+	  lowCell = lowCell-extraCells;
+	  highCell = highCell+extraCells;
+	}
+	
+	IntVector resolution(highCell-lowCell);
+	IntVector inResolution(inHighCell-inLowCell);
+	if(resolution.x() < 1 || resolution.y() < 1 || resolution.z() < 1)
+	  throw ProblemSetupException("Degeneration patch");
+	
+	IntVector patches;
+	IntVector inLowIndex,inHighIndex;
+	if(box_ps->get("patches", patches)){
+	  for(int i=0;i<patches.x();i++){
+	    for(int j=0;j<patches.y();j++){
+	      for(int k=0;k<patches.z();k++){
+		IntVector startcell = resolution*IntVector(i,j,k)/patches;
+		IntVector inStartCell=inResolution*IntVector(i,j,k)/patches;
+		IntVector endcell = resolution*IntVector(i+1,j+1,k+1)/patches;
+		IntVector inEndCell=inResolution*IntVector(i+1,j+1,k+1)/patches;
+		level->addPatch(startcell+lowCell, endcell+lowCell,
+				inStartCell+inLowCell,inEndCell+inLowCell);
+	      }
 	    }
-	 } else {
-	    level->addPatch(lowCell, highCell);
-	 }
+	  }
+	} else {
+	  level->addPatch(lowCell, highCell,inLowCell,inHighCell);
+	}
       }
       level->finalizeLevel();
       level->assignBCS(grid_ps);
@@ -527,6 +540,9 @@ void SimulationController::scheduleTimeAdvance(double t, double delt,
 
 //
 // $Log$
+// Revision 1.49  2000/11/14 04:05:52  jas
+// Now storing patches with two indices: extraCell and interior cell limits.
+//
 // Revision 1.48  2000/11/03 19:02:36  witzel
 // Added code to output average ellapsed wall clock times between
 // time steps to a file "avg_elapsed_walltime.txt" if the
