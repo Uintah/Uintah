58ea99499d25896c048352d6d437e79ab5b21644
John Schmidt <jas@sci.utah.edu>
2000-11-21 21:53:24 +0000

    Added methods for the different schedules that make up scheduleTimeAdvance.

--- src/Uintah/Components/ICE/ICE.h
+++ src/Uintah/Components/ICE/ICE.h
@@ -17,264 +17,288 @@
 using SCICore::Geometry::Vector;
 
 namespace Uintah {
-namespace ICESpace {
-
-class ICE : public UintahParallelComponent, public CFDInterface {
-public:
-   ICE(const ProcessorGroup* myworld);
-   virtual ~ICE();
-
-   struct fflux { double d_fflux[6]; };
-   struct eflux { double d_eflux[12]; };
-   
-   virtual void problemSetup(
-                        const ProblemSpecP& params, 
-                        GridP& grid,
-			SimulationStateP&);
-
-   virtual void scheduleInitialize(
-                        const LevelP& level,
-			   SchedulerP&,
-			   DataWarehouseP&);
-
-   
-   virtual void scheduleComputeStableTimestep(const LevelP&,
-			   SchedulerP&,
-			   DataWarehouseP&);
-
-   virtual void scheduleTimeAdvance(double t, 
-				    double dt, 
-				    const LevelP&, 
-				    SchedulerP&,
-				    DataWarehouseP&, 
-				    DataWarehouseP&);
-
-   void setICELabel(ICELabel* Ilb)
-   {
-        lb = Ilb;
-   };
-
- public:
-
-   void actuallyInitialize(const ProcessorGroup*,
-			   const Patch* patch,
-			   DataWarehouseP&  old_dw,
-			   DataWarehouseP& new_dw);
-
-   void actuallyComputeStableTimestep(const ProcessorGroup*,
-				      const Patch* patch,
-				      DataWarehouseP&,
+  namespace ICESpace {
+    
+    class ICE : public UintahParallelComponent, public CFDInterface {
+    public:
+      ICE(const ProcessorGroup* myworld);
+      virtual ~ICE();
+      
+      struct fflux { double d_fflux[6]; };
+      struct eflux { double d_eflux[12]; };
+      
+      virtual void problemSetup(const ProblemSpecP& params, 
+				GridP& grid,
+				SimulationStateP&);
+      
+      virtual void scheduleInitialize(const LevelP& level,
+				      SchedulerP&,
 				      DataWarehouseP&);
+      
+      
+      virtual void scheduleComputeStableTimestep(const LevelP&,
+						 SchedulerP&,
+						 DataWarehouseP&);
+      
+      virtual void scheduleTimeAdvance(double t, 
+				       double dt, 
+				       const LevelP&, 
+				       SchedulerP&,
+				       DataWarehouseP&, 
+				       DataWarehouseP&);
+      
+      void scheduleStep1a(const Patch* patch, 
+			  SchedulerP&,
+			  DataWarehouseP&, 
+			  DataWarehouseP&);
+      
+      void scheduleStep1b(const Patch* patch, 
+			  SchedulerP&,
+			  DataWarehouseP&, 
+			  DataWarehouseP&);
+      
+      void scheduleStep1c(const Patch* patch, 
+			  SchedulerP&,
+			  DataWarehouseP&, 
+			  DataWarehouseP&);
+      
+      void scheduleStep1d(const Patch* patch, 
+			  SchedulerP&,
+			  DataWarehouseP&, 
+			  DataWarehouseP&);
+      
+      void scheduleStep2(const Patch* patch, 
+			 SchedulerP&,
+			 DataWarehouseP&, 
+			 DataWarehouseP&);
+      
+      void scheduleStep3(const Patch* patch, 
+			 SchedulerP&,
+			 DataWarehouseP&, 
+			 DataWarehouseP&);
+      
+      void scheduleStep4a(const Patch* patch, 
+			  SchedulerP&,
+			  DataWarehouseP&, 
+			  DataWarehouseP&);
+      
+      void scheduleStep4b(const Patch* patch, 
+			  SchedulerP&,
+			  DataWarehouseP&, 
+			  DataWarehouseP&);
+      
+      void scheduleStep5a(const Patch* patch, 
+			  SchedulerP&,
+			  DataWarehouseP&, 
+			  DataWarehouseP&);
+      
+      void scheduleStep5b(const Patch* patch, 
+			  SchedulerP&,
+			  DataWarehouseP&, 
+			  DataWarehouseP&);
 
+      void scheduleStep6and7(const Patch* patch, 
+			     SchedulerP&,
+			     DataWarehouseP&, 
+			     DataWarehouseP&);
 
-
-   void actually_Bottom_of_main_loop(const ProcessorGroup*,
+      void setICELabel(ICELabel* Ilb)
+	{
+	  lb = Ilb;
+	};
+      
+    public:
+      
+      void actuallyInitialize(const ProcessorGroup*,
+			      const Patch* patch,
+			      DataWarehouseP&  old_dw,
+			      DataWarehouseP& new_dw);
+      
+      void actuallyComputeStableTimestep(const ProcessorGroup*,
+					 const Patch* patch,
+					 DataWarehouseP&,
+					 DataWarehouseP&);
+      
+      
+      
+      void actually_Bottom_of_main_loop(const ProcessorGroup*,
+					const Patch* patch,
+					DataWarehouseP&,
+					DataWarehouseP&);
+      
+      void actually_Top_of_main_loop(const ProcessorGroup*,
 				     const Patch* patch,
-			  	     DataWarehouseP&,
+				     DataWarehouseP&,
 				     DataWarehouseP&);
-
-   void actually_Top_of_main_loop(const ProcessorGroup*,
-			          const Patch* patch,
-			          DataWarehouseP&,
-			          DataWarehouseP&);
-
-   // compute speedSound
-   void actuallyStep1a(const ProcessorGroup*,
-		       const Patch* patch,
-		       DataWarehouseP&,
-		       DataWarehouseP&);
-
-   // calculateEquilibrationPressure
-   void actuallyStep1b(const ProcessorGroup*,
-		       const Patch* patch,
-		       DataWarehouseP&,
-		       DataWarehouseP&);
-
-   // computeFCVelocity
-   void actuallyStep1c(const ProcessorGroup*,
-		       const Patch* patch,
-		       DataWarehouseP&,
-		       DataWarehouseP&);
-
-   // momentumExchangeFCVelocity
-   void actuallyStep1d(const ProcessorGroup*,
-		       const Patch* patch,
-		       DataWarehouseP&,
-		       DataWarehouseP&);
-
-   // computeDivFCVelocity
-   // computeExplicitDelPress
-   void actuallyStep2(const ProcessorGroup*,
-		      const Patch* patch,
-		      DataWarehouseP&,
-		      DataWarehouseP&);
-
-   // computeFaceCenteredPressure
-   void actuallyStep3(const ProcessorGroup*,
-		      const Patch* patch,
-		      DataWarehouseP&,
-		      DataWarehouseP&);
-
-   void actuallyStep4a(const ProcessorGroup*,
-		      const Patch* patch,
-		      DataWarehouseP&,
-		      DataWarehouseP&);
-
-   void actuallyStep4b(const ProcessorGroup*,
-		      const Patch* patch,
-		      DataWarehouseP&,
-		      DataWarehouseP&);
-
-   void actuallyStep5a(const ProcessorGroup*,
-		      const Patch* patch,
-		      DataWarehouseP&,
-		      DataWarehouseP&);
-
-   void actuallyStep5b(const ProcessorGroup*,
-		      const Patch* patch,
-		      DataWarehouseP&,
-		      DataWarehouseP&);
-
-   void actuallyStep6and7(const ProcessorGroup*,
-		          const Patch* patch,
-		          DataWarehouseP&,
-		          DataWarehouseP&);
-
-                        
-void before_each_step_wrapper(
-                        const Patch* patch,
-                        int     m,
-                        CCVariable<double>& press_cc,
-                        double  ****press_CC,
-                        CCVariable<double>& rho_cc,
-                        double  ****rho_CC,
-                        CCVariable<double>& temp_cc,
-                        double  ****Temp_CC,
-                        CCVariable<Vector>& vel_cc,
-                        double  ****uvel_CC,
-                        double  ****vvel_CC,
-                        double  ****wvel_CC);
-                        
-void after_each_step_wrapper(
-                        const Patch* patch,
-                        int     m,
-                        CCVariable<double>& press_cc,
-                        double  ****press_CC,
-                        CCVariable<double>& rho_cc,
-                        double  ****rho_CC,
-                        CCVariable<double>& temp_cc,
-                        double  ****Temp_CC,
-                        CCVariable<Vector>& vel_cc,
-                        double  ****uvel_CC,
-                        double  ****vvel_CC,
-                        double  ****wvel_CC);
-			  
-   
- private:
-  friend const TypeDescription* fun_getTypeDescription(fflux*);
-  friend const TypeDescription* fun_getTypeDescription(eflux*);
-
-  void setBC(CCVariable<double>& variable,const std::string& type, 
-	     const Patch* p);
-  void setBC(CCVariable<double>& variable,const std::string& type,
-	     const std::string& comp, const Patch* p);
-
-  void setBC(FCVariable<double>& variable,const std::string& type, 
-	     const Patch* p);
-  void setBC(FCVariable<double>& variable,const std::string& type,
-	     const std::string& comp, const Patch* p);
-  
-  void influxOutfluxVolume(const FCVariable<double>& uvel_CC,
-			   const FCVariable<double>& vvel_CC,
-			   const FCVariable<double>& wvel_CC,
-			   const double& delT, const Patch* patch,
-			   CCVariable<fflux>& OFS, CCVariable<eflux>& OFE,
-			   CCVariable<fflux>& IFS, CCVariable<eflux>& IFE);
-
-  void outflowVolCentroid(const FCVariable<double>& uvel_CC,
-			  const FCVariable<double>& vvel_CC,
-			  const FCVariable<double>& wvel_CC,
-			  const double& delT, const Vector& dx,
+      
+      // compute speedSound
+      void actuallyStep1a(const ProcessorGroup*,
+			  const Patch* patch,
+			  DataWarehouseP&,
+			  DataWarehouseP&);
+      
+      // calculateEquilibrationPressure
+      void actuallyStep1b(const ProcessorGroup*,
+			  const Patch* patch,
+			  DataWarehouseP&,
+			  DataWarehouseP&);
+      
+      // computeFCVelocity
+      void actuallyStep1c(const ProcessorGroup*,
+			  const Patch* patch,
+			  DataWarehouseP&,
+			  DataWarehouseP&);
+      
+      // momentumExchangeFCVelocity
+      void actuallyStep1d(const ProcessorGroup*,
+			  const Patch* patch,
+			  DataWarehouseP&,
+			  DataWarehouseP&);
+      
+      // computeDivFCVelocity
+      // computeExplicitDelPress
+      void actuallyStep2(const ProcessorGroup*,
+			 const Patch* patch,
+			 DataWarehouseP&,
+			 DataWarehouseP&);
+      
+      // computeFaceCenteredPressure
+      void actuallyStep3(const ProcessorGroup*,
+			 const Patch* patch,
+			 DataWarehouseP&,
+			 DataWarehouseP&);
+      
+      void actuallyStep4a(const ProcessorGroup*,
+			  const Patch* patch,
+			  DataWarehouseP&,
+			  DataWarehouseP&);
+      
+      void actuallyStep4b(const ProcessorGroup*,
+			  const Patch* patch,
+			  DataWarehouseP&,
+			  DataWarehouseP&);
+      
+      void actuallyStep5a(const ProcessorGroup*,
+			  const Patch* patch,
+			  DataWarehouseP&,
+			  DataWarehouseP&);
+      
+      void actuallyStep5b(const ProcessorGroup*,
+			  const Patch* patch,
+			  DataWarehouseP&,
+			  DataWarehouseP&);
+      
+      void actuallyStep6and7(const ProcessorGroup*,
+			     const Patch* patch,
+			     DataWarehouseP&,
+			     DataWarehouseP&);
+      
+      
+    private:
+      friend const TypeDescription* fun_getTypeDescription(fflux*);
+      friend const TypeDescription* fun_getTypeDescription(eflux*);
+      
+      void setBC(CCVariable<double>& variable,const std::string& type, 
+		 const Patch* p);
+      void setBC(CCVariable<double>& variable,const std::string& type,
+		 const std::string& comp, const Patch* p);
+      
+      void setBC(FCVariable<double>& variable,const std::string& type, 
+		 const Patch* p);
+      void setBC(FCVariable<double>& variable,const std::string& type,
+		 const std::string& comp, const Patch* p);
+      
+      void influxOutfluxVolume(const FCVariable<double>& uvel_CC,
+			       const FCVariable<double>& vvel_CC,
+			       const FCVariable<double>& wvel_CC,
+			       const double& delT, const Patch* patch,
+			       CCVariable<fflux>& OFS, CCVariable<eflux>& OFE,
+			       CCVariable<fflux>& IFS, CCVariable<eflux>& IFE);
+      
+      void outflowVolCentroid(const FCVariable<double>& uvel_CC,
+			      const FCVariable<double>& vvel_CC,
+			      const FCVariable<double>& wvel_CC,
+			      const double& delT, const Vector& dx,
+			      CCVariable<fflux>& r_out_x,
+			      CCVariable<fflux>& r_out_y,
+			      CCVariable<fflux>& r_out_z,
+			      CCVariable<eflux>& r_out_x_CF,
+			      CCVariable<eflux>& r_out_y_CF,
+			      CCVariable<eflux>& r_out_z_CF);
+      
+      void advectQFirst(const CCVariable<double>& q_CC,
+			const Patch* patch,
+			const CCVariable<fflux>& OFS,
+			const CCVariable<eflux>& OFE,
+			const CCVariable<fflux>& IFS,
+			const CCVariable<eflux>& IFE,
+			CCVariable<fflux>& q_out,
+			CCVariable<eflux>& q_out_EF,
+			CCVariable<fflux>& q_in,
+			CCVariable<eflux>& q_in_EF,
+			CCVariable<double>& q_advected);
+      
+      void qOutfluxFirst(const CCVariable<double>& q_CC,
+			 const Patch* patch,
+			 CCVariable<fflux>& q_out,
+			 CCVariable<eflux>& q_out_EF);
+      
+      
+      void qInflux(const CCVariable<fflux>& q_out,
+		   const CCVariable<eflux>& q_out_EF,
+		   const Patch* patch,
+		   CCVariable<fflux>& q_in,
+		   CCVariable<eflux>& q_in_EF);
+      
+      void qOutfluxSecond(CCVariable<fflux>& OFS,
+			  CCVariable<fflux>& IFS,
 			  CCVariable<fflux>& r_out_x,
 			  CCVariable<fflux>& r_out_y,
 			  CCVariable<fflux>& r_out_z,
 			  CCVariable<eflux>& r_out_x_CF,
 			  CCVariable<eflux>& r_out_y_CF,
-			  CCVariable<eflux>& r_out_z_CF);
-
-  void advectQFirst(const CCVariable<double>& q_CC,
-		    const Patch* patch,
-		    const CCVariable<fflux>& OFS,
-		    const CCVariable<eflux>& OFE,
-		    const CCVariable<fflux>& IFS,
-		    const CCVariable<eflux>& IFE,
-		    CCVariable<fflux>& q_out,
-		    CCVariable<eflux>& q_out_EF,
-		    CCVariable<fflux>& q_in,
-		    CCVariable<eflux>& q_in_EF,
-		    CCVariable<double>& q_advected);
-
-  void qOutfluxFirst(const CCVariable<double>& q_CC,
-		     const Patch* patch,
-		     CCVariable<fflux>& q_out,
-		     CCVariable<eflux>& q_out_EF);
-
-
-  void qInflux(const CCVariable<fflux>& q_out,
-	       const CCVariable<eflux>& q_out_EF,
-	       const Patch* patch,
-	       CCVariable<fflux>& q_in,
-	       CCVariable<eflux>& q_in_EF);
-
-  void qOutfluxSecond(CCVariable<fflux>& OFS,
-		      CCVariable<fflux>& IFS,
-		      CCVariable<fflux>& r_out_x,
-		      CCVariable<fflux>& r_out_y,
-		      CCVariable<fflux>& r_out_z,
-		      CCVariable<eflux>& r_out_x_CF,
-		      CCVariable<eflux>& r_out_y_CF,
-		      CCVariable<eflux>& r_out_z_CF,
-		      const Vector& dx);
-
- ICELabel* lb; 
- SimulationStateP d_sharedState;
- double d_SMALL_NUM;
- double d_pressure;
- Vector d_K_mom, d_K_heat; // exchange coefficients -- off diagonal terms
- 
- struct ic {
-   Vector d_velocity;
-   double d_micro_density;
-   double d_temperature;
-   double d_volume_fraction;
- };
-
- vector<struct ic> d_ic;
-
- 
- ICE(const ICE&);
- ICE& operator=(const ICE&);
-
- const VarLabel* IFS_CCLabel;
- const VarLabel* OFS_CCLabel;
- const VarLabel* IFE_CCLabel;
- const VarLabel* OFE_CCLabel;
- const VarLabel* q_outLabel;
- const VarLabel* q_out_EFLabel;
- const VarLabel* q_inLabel;
- const VarLabel* q_in_EFLabel;
-
-};
-
+			  CCVariable<eflux>& r_out_z_CF,
+			  const Vector& dx);
+      
+      ICELabel* lb; 
+      SimulationStateP d_sharedState;
+      double d_SMALL_NUM;
+      double d_pressure;
+      Vector d_K_mom, d_K_heat; // exchange coefficients -- off diagonal terms
+      
+      struct ic {
+	Vector d_velocity;
+	double d_micro_density;
+	double d_temperature;
+	double d_volume_fraction;
+      };
+      
+      vector<struct ic> d_ic;
+      
+      
+      ICE(const ICE&);
+      ICE& operator=(const ICE&);
+      
+      const VarLabel* IFS_CCLabel;
+      const VarLabel* OFS_CCLabel;
+      const VarLabel* IFE_CCLabel;
+      const VarLabel* OFE_CCLabel;
+      const VarLabel* q_outLabel;
+      const VarLabel* q_out_EFLabel;
+      const VarLabel* q_inLabel;
+      const VarLabel* q_in_EFLabel;
+      
+    };
+    
 #define TOP        0          /* index used to designate the top cell face    */
 #define BOTTOM     1          /* index used to designate the bottom cell face */
 #define RIGHT      2          /* index used to designate the right cell face  */
 #define LEFT       3          /* index used to designate the left cell face   */
 #define FRONT      4          /* index used to designate the front cell face  */
 #define BACK       5          /* index used to designate the back cell face   */
-
-// Definitions for edge corners (just called corners in Todd's ICE)
-// T=TOP, B=BOTTOM, R=RIGHT, L=LEFT, F=FRONT, b=BACK
+    
+    // Definitions for edge corners (just called corners in Todd's ICE)
+    // T=TOP, B=BOTTOM, R=RIGHT, L=LEFT, F=FRONT, b=BACK
 #define TR   0 
 #define TL   1
 #define BL   2
@@ -287,12 +311,15 @@ void after_each_step_wrapper(
 #define FL   9
 #define bR   10
 #define bL   11
-}
+  }
 }
 
 #endif
 
 // $Log$
+// Revision 1.37  2000/11/21 21:53:24  jas
+// Added methods for the different schedules that make up scheduleTimeAdvance.
+//
 // Revision 1.36  2000/11/15 00:51:54  guilkey
 // Changed code to take advantage of the ICEMaterial stuff I committed
 // recently in preparation for coupling the two codes.
