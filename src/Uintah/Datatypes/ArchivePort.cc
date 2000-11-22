64f81a23c84e413a1c915bdb215d6dad056de833
Chris Moulding <moulding@sci.utah.edu>
2000-11-22 18:34:33 +0000

    added extern "C" make functions for input and output ports (to be used by the autoport facility).

--- src/Uintah/Datatypes/ArchivePort.cc
+++ src/Uintah/Datatypes/ArchivePort.cc
@@ -1,11 +1,21 @@
 
-#include "ArchivePort.h"
+#include <Uintah/Datatypes/ArchivePort.h>
+#include <Uintah/share/share.h>
 
 namespace PSECore {
 namespace Datatypes {
 
 using namespace Uintah::Datatypes;
 
+
+extern "C" {
+UINTAHSHARE IPort* make_ArchiveIPort(Module* module, const clString& name) {
+  return new SimpleIPort<ArchiveHandle>(module,name);
+}
+UINTAHSHARE OPort* make_ArchiveOPort(Module* module, const clString& name) {
+  return new SimpleOPort<ArchiveHandle>(module,name);
+}
+}
 
 template<> clString SimpleIPort<ArchiveHandle>::port_type("Archive");
 template<> clString SimpleIPort<ArchiveHandle>::port_color("lightsteelblue4");
--- src/Uintah/Datatypes/TensorFieldPort.cc
+++ src/Uintah/Datatypes/TensorFieldPort.cc
@@ -1,11 +1,22 @@
 
-#include "TensorFieldPort.h"
-
+#include <Uintah/Datatypes/TensorFieldPort.h>
+#include <Uintah/share/share.h>
 namespace PSECore {
 namespace Datatypes {
 
 
 using SCICore::Datatypes::TensorFieldHandle;
+
+extern "C" {
+UINTAHSHARE IPort* make_TensorFieldIPort(Module* module,
+					 const clString& name) {
+  return new SimpleIPort<TensorFieldHandle>(module,name);
+}
+UINTAHSHARE OPort* make_TensorFieldOPort(Module* module,
+					 const clString& name) {
+  return new SimpleOPort<TensorFieldHandle>(module,name);
+}
+}
 
 template<> clString SimpleIPort<TensorFieldHandle>::port_type("TensorField");
 template<> clString SimpleIPort<TensorFieldHandle>::port_color("yellow4");
