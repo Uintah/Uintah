b5b687e5c81d961bcec7509dce0343b224b2a363
Chris Moulding <moulding@sci.utah.edu>
2000-11-22 17:42:51 +0000

    added extern "C" make functions for input and output ports (to be used by the autoport facility).

--- src/Kurt/Datatypes/VisParticleSetPort.cc
+++ src/Kurt/Datatypes/VisParticleSetPort.cc
@@ -1,11 +1,23 @@
 
-#include "VisParticleSetPort.h"
+#include <Kurt/Datatypes/VisParticleSetPort.h>
+#include <Kurt/share/share.h>
 
 namespace PSECore {
 namespace Datatypes {
 
 using namespace Kurt::Datatypes;
 
+
+extern "C" {
+KurtSHARE IPort* make_VisParticleSetIPort(Module* module,
+					 const clString& name) {
+  return new SimpleIPort<VisParticleSetHandle>(module,name);
+}
+KurtSHARE OPort* make_VisParticleSetOPort(Module* module,
+					 const clString& name) {
+  return new SimpleOPort<VisParticleSetHandle>(module,name);
+}
+}
 
 template<> clString SimpleIPort<VisParticleSetHandle>::port_type("VisParticleSet");
 template<> clString SimpleIPort<VisParticleSetHandle>::port_color("chartreuse2");
