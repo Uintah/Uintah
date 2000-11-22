a8e0878e97fbec8682751df7d2847320980bbd3b
Wayne Witzel <witzel@sci.utah.edu>
2000-11-22 02:29:35 +0000

    List timesteps using DataArchive queryTimesteps instead of listing the directories, and changed Task to GV_Task and TaskGraph to GV_TaskGraph to avoid conflicts.

--- Uintah/tools/graphview/sub.mk
+++ Uintah/tools/graphview/sub.mk
@@ -7,16 +7,21 @@ SRCDIR := Uintah/tools/graphview
 
 PROGRAM := $(SRCDIR)/graphview
 
-SRCS := $(SRCDIR)/graphview.cc	$(SRCDIR)/TaskGraph.cc 	\
+SRCS := $(SRCDIR)/graphview.cc	$(SRCDIR)/GV_TaskGraph.cc 	\
 	$(SRCDIR)/DaVinci.cc
 
-PSELIBS := PSECore/XMLUtil SCICore/Exceptions SCICore/Thread
+PSELIBS := PSECore/XMLUtil SCICore/Exceptions SCICore/Thread Uintah/Interface
 LIBS := $(XML_LIBRARY)
 
 include $(SRCTOP)/scripts/program.mk
 
 #
 # $Log$
+# Revision 1.3  2000/11/22 02:29:35  witzel
+# List timesteps using DataArchive queryTimesteps instead of listing
+# the directories, and changed Task to GV_Task and TaskGraph to GV_TaskGraph
+# to avoid conflicts.
+#
 # Revision 1.2  2000/08/03 23:34:42  witzel
 # *** empty log message ***
 #
