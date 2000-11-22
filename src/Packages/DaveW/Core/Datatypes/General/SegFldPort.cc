af75b8e1f464d95277fd4782d01be5e8cd531007
Chris Moulding <moulding@sci.utah.edu>
2000-11-22 17:30:16 +0000

    added extern "C" make functions for input and output ports (to be used by the autoport facility).

--- src/Packages/DaveW/Core/Datatypes/General/SegFldPort.cc
+++ src/Packages/DaveW/Core/Datatypes/General/SegFldPort.cc
@@ -13,12 +13,24 @@
  */
 
 #include <DaveW/Datatypes/General/SegFldPort.h>
+#include <DaveW/share/share.h>
 
 //namespace DaveW {
 //namespace Datatypes {
 
 using namespace SCICore::Containers;
 using namespace DaveW::Datatypes;
+
+extern "C" {
+DaveWSHARE IPort* make_SegFldIPort(Module* module,
+					 const clString& name) {
+  return new SimpleIPort<SegFldHandle>(module,name);
+}
+DaveWSHARE OPort* make_SegFldOPort(Module* module,
+					 const clString& name) {
+  return new SimpleOPort<SegFldHandle>(module,name);
+}
+}
 
 template<> clString SimpleIPort<SegFldHandle>::port_type("SegFld");
 template<> clString SimpleIPort<SegFldHandle>::port_color("Green");
@@ -28,6 +40,10 @@ template<> clString SimpleIPort<SegFldHandle>::port_color("Green");
 
 //
 // $Log$
+// Revision 1.4  2000/11/22 17:30:15  moulding
+// added extern "C" make functions for input and output ports (to be used
+// by the autoport facility).
+//
 // Revision 1.3  1999/09/01 05:27:36  dmw
 // more DaveW datatypes...
 //
