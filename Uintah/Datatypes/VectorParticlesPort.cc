2bc21317a7f647747344a14a303eaef1a6f02bf9
Chris Moulding <moulding@sci.utah.edu>
2000-11-22 18:34:33 +0000

    added extern "C" make functions for input and output ports (to be used by the autoport facility).

--- Uintah/Datatypes/VectorParticlesPort.cc
+++ Uintah/Datatypes/VectorParticlesPort.cc
@@ -1,11 +1,23 @@
 
-#include "VectorParticlesPort.h"
+#include <Uintah/Datatypes/VectorParticlesPort.h>
+#include <Uintah/share/share.h>
 
 namespace PSECore {
 namespace Datatypes {
 
 using namespace Uintah::Datatypes;
 
+
+extern "C" {
+UINTAHSHARE IPort* make_VectorParticlesIPort(Module* module,
+					     const clString& name) {
+  return new SimpleIPort<VectorParticlesHandle>(module,name);
+}
+UINTAHSHARE OPort* make_VectorParticlesOPort(Module* module,
+					     const clString& name) {
+  return new SimpleOPort<VectorParticlesHandle>(module,name);
+}
+}
 
 template<> clString SimpleIPort<VectorParticlesHandle>::port_type("VectorParticles");
 template<> clString SimpleIPort<VectorParticlesHandle>::port_color("chartreuse3");
