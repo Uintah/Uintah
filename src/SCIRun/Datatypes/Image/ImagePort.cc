ddb856e039a5dd38bfab2f28841b468725c2d0e0
Chris Moulding <moulding@sci.utah.edu>
2000-11-22 18:19:56 +0000

    added extern "C" make functions for input and output ports (to be used by the autoport facility).

--- src/SCIRun/Datatypes/Image/ImagePort.cc
+++ src/SCIRun/Datatypes/Image/ImagePort.cc
@@ -13,9 +13,19 @@
  */
 
 #include <SCIRun/Datatypes/Image/ImagePort.h>
+#include <SCIRun/share/share.h>
 
 using namespace PSECore::Datatypes;
 using namespace SCICore::Datatypes;
+
+extern "C" {
+SCIRUNSHARE IPort* make_ImageIPort(Module* module, const clString& name) {
+  return new SimpleIPort<ImageHandle>(module,name);
+}
+SCIRUNSHARE OPort* make_ImageOPort(Module* module, const clString& name) {
+  return new SimpleOPort<ImageHandle>(module,name);
+}
+}
 
 template<> clString SimpleIPort<ImageHandle>::port_type("Image");
 template<> clString SimpleIPort<ImageHandle>::port_color("misty rose");
@@ -23,6 +33,10 @@ template<> clString SimpleIPort<ImageHandle>::port_color("misty rose");
 
 //
 // $Log$
+// Revision 1.3  2000/11/22 18:19:56  moulding
+// added extern "C" make functions for input and output ports (to be used
+// by the autoport facility).
+//
 // Revision 1.2  1999/08/31 08:55:29  sparker
 // Bring SCIRun modules up to speed
 //
