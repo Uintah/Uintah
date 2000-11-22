d684453f83d465a3d7e8bfccc4f019d5423908ac
Chris Moulding <moulding@sci.utah.edu>
2000-11-22 18:34:33 +0000

    added extern "C" make functions for input and output ports (to be used by the autoport facility).

--- Core/Datatypes/ScalarParticlesPort.cc
+++ Core/Datatypes/ScalarParticlesPort.cc
@@ -1,11 +1,22 @@
 
-#include "ScalarParticlesPort.h"
-
+#include <Uintah/Datatypes/ScalarParticlesPort.h>
+#include <Uintah/share/share.h>
 namespace PSECore {
 namespace Datatypes {
 
 using namespace Uintah::Datatypes;
 
+
+extern "C" {
+UINTAHSHARE IPort* make_ScalarParticlesIPort(Module* module,
+					     const clString& name) {
+  return new SimpleIPort<ScalarParticlesHandle>(module,name);
+}
+UINTAHSHARE OPort* make_ScalarParticlesOPort(Module* module,
+					     const clString& name) {
+  return new SimpleOPort<ScalarParticlesHandle>(module,name);
+}
+}
 
 template<> clString SimpleIPort<ScalarParticlesHandle>::port_type("ScalarParticles");
 template<> clString SimpleIPort<ScalarParticlesHandle>::port_color("chartreuse");
--- Core/Datatypes/TensorParticlesPort.cc
+++ Core/Datatypes/TensorParticlesPort.cc
@@ -1,11 +1,23 @@
 
-#include "TensorParticlesPort.h"
+#include <Uintah/Datatypes/TensorParticlesPort.h>
+#include <Uintah/share/share.h>
 
 namespace PSECore {
 namespace Datatypes {
 
 using namespace Uintah::Datatypes;
 
+
+extern "C" {
+UINTAHSHARE IPort* make_TensorParticlesIPort(Module* module,
+					     const clString& name) {
+  return new SimpleIPort<TensorParticlesHandle>(module,name);
+}
+UINTAHSHARE OPort* make_TensorParticlesOPort(Module* module,
+					     const clString& name) {
+  return new SimpleOPort<TensorParticlesHandle>(module,name);
+}
+}
 
 template<> clString SimpleIPort<TensorParticlesHandle>::port_type("TensorParticles");
 template<> clString SimpleIPort<TensorParticlesHandle>::port_color("chartreuse4");
