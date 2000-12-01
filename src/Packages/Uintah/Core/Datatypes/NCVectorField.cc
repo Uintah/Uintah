45c00d0e62c641332e26a2a5dc8049f303bbab89
Kurt Zimmerman <kuzimmer@sci.utah.edu>
2000-12-01 20:13:00 +0000

    Added hashing scheme for quicker lookups into fields

--- src/Packages/Uintah/Core/Datatypes/NCVectorField.cc
+++ src/Packages/Uintah/Core/Datatypes/NCVectorField.cc
@@ -120,26 +120,28 @@ int NCVectorField::interpolate(const Point& p, Vector& value)
 
     IntVector ni[8];
     double S[8];
-    (*r)->findCellAndWeights(p, ni, S);
-    value=Vector(0,0,0);
-    for(int k = 0; k < 8; k++){
-      if((*r)->containsNode(ni[k])){
-	value += _vars[i][ni[k]]*S[k];
-      } else {
-	Level::const_patchIterator r1;
-	int j;
-	for(j = 0, r1 = _level->patchesBegin();
-	    r1 != _level->patchesEnd(); r1++, j++)
-	  if( (*r1)->containsNode(ni[k])){
-	    value += _vars[j][ni[k]]*S[k];
-	    break;
-	  }
+    if( (*r)->findCell( p, *ni ) ) {
+      (*r)->findCellAndWeights(p, ni, S);
+      value=Vector(0,0,0);
+      for(int k = 0; k < 8; k++){
+	if((*r)->containsNode(ni[k])){
+	  value += _vars[i][ni[k]]*S[k];
+	} else {
+	  Level::const_patchIterator r1;
+	  int j;
+	  for(j = 0, r1 = _level->patchesBegin();
+	      r1 != _level->patchesEnd(); r1++, j++)
+	    if( (*r1)->containsNode(ni[k])){
+	      value += _vars[j][ni[k]]*S[k];
+	      break;
+	    }
+	}
       }
+      return 1;
     }
   }
-  return 1;
+  return 0;
 }
-
 
   
 } // Datatypes
