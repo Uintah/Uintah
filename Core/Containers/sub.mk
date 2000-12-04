75758f5dd0c9129c3877713b653c6c167395895f
Wayne Witzel <witzel@sci.utah.edu>
2000-12-04 22:50:24 +0000

    Added ConsecutiveRangeSet

--- Core/Containers/sub.mk
+++ Core/Containers/sub.mk
@@ -9,7 +9,7 @@ SRCDIR   := SCICore/Containers
 
 SRCS     += $(SRCDIR)/Sort.cc $(SRCDIR)/String.cc $(SRCDIR)/PQueue.cc \
 	$(SRCDIR)/TrivialAllocator.cc $(SRCDIR)/BitArray1.cc \
-	$(SRCDIR)/templates.cc
+	$(SRCDIR)/templates.cc $(SRCDIR)/ConsecutiveRangeSet.cc
 
 PSELIBS := SCICore/Exceptions SCICore/Tester SCICore/Thread
 
@@ -17,6 +17,9 @@ include $(SRCTOP)/scripts/smallso_epilogue.mk
 
 #
 # $Log$
+# Revision 1.4  2000/12/04 22:50:24  witzel
+# Added ConsecutiveRangeSet
+#
 # Revision 1.3  2000/03/21 03:01:26  sparker
 # Partially fixed special_get method in SimplePort
 # Pre-instantiated a few key template types, in an attempt to reduce
