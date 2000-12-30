e444e8034dcdee3ab11d0ddd250ea3990f2445cc
David Weinstein <dmw@sci.utah.edu>
2000-12-30 03:03:27 +0000

    minor bug fixes

--- src/Packages/Butson/Dataflow/GUI/sub.mk
+++ src/Packages/Butson/Dataflow/GUI/sub.mk
@@ -5,6 +5,5 @@ SRCDIR := Packages/Butson/Dataflow/GUI
 ALLTARGETS := $(ALLTARGETS) $(SRCDIR)/tclIndex
 
 $(SRCDIR)/tclIndex: \
-	$(SRCDIR)/DipoleInSphere.tcl
 
 CLEANPROGS := $(CLEANPROGS) $(SRCDIR)/tclIndex
