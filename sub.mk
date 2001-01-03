834e72fa5a4a05d03cc0a488ec47189fd7764168
David Weinstein <dmw@sci.utah.edu>
2001-01-03 03:53:27 +0000

    extended Yardens OBJTOP to SRCTOP fix to all of the Package sub.mk files

--- sub.mk
+++ sub.mk
@@ -1,15 +1,15 @@
 #Makefile fragment for the Packages/Uintah directory
 
-include $(OBJTOP_ABS)/scripts/largeso_prologue.mk
+include $(SRCTOP_ABS)/scripts/largeso_prologue.mk
 
 SRCDIR := Packages/Uintah
 SUBDIRS := \
 	$(SRCDIR)/Core \
 	$(SRCDIR)/Dataflow
 
-include $(OBJTOP_ABS)/scripts/recurse.mk
+include $(SRCTOP_ABS)/scripts/recurse.mk
 
 PSELIBS := 
 LIBS := $(TK_LIBRARY) $(GL_LIBS) -lm
 
-include $(OBJTOP_ABS)/scripts/largeso_epilogue.mk
+include $(SRCTOP_ABS)/scripts/largeso_epilogue.mk
