2bc70d012b5a8c40851620e6e7c7631142568685
Yarden Livnat <yarden@sci.utah.edu>
2001-01-04 05:24:31 +0000

    taken out of the Field class this class manages attributes and should be use as a base class for more sophisticated managers. the Field class now inherit from AttribManager rather than directly from Datatype. when a new AttribMangeri is created , Field should inherit from the new manager class.
    this change allows now other objected to have managed attributed
    associated with them.

---
+++ Core/Datatypes/AttribManager.h
@@ -0,0 +1,95 @@
+// AttribManager.h - Manage a collection of attributes
+//
+//  Written by:
+//   Yarden Livnat
+//   Department of Computer Science
+//   University of Utah
+//   Jan 2001
+//
+//  Copyright (C) 2001 SCI Institute
+
+#ifndef SCI_project_AttirbManager_h
+#define SCI_project_AttirbManager_h 1
+
+#include <Core/Datatypes/Datatype.h>
+#include <Core/Datatypes/Attrib.h>
+#include <Core/Containers/Array1.h>
+
+#include <functional>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+
+namespace SCIRun{
+
+using std::vector;
+using std::string;
+using std::map;
+
+typedef map<string, AttribHandle> AttribMap;
+typedef Array1<AttribHandle>      AttribVector;
+
+class SCICORESHARE AttribManager: public Datatype{
+
+public:
+
+  // GROUP: Constructors/Destructor
+  //////////
+  //
+  AttribManager();
+  AttribManager(const AttribManager&);
+  virtual ~AttribManager();
+
+  //////////
+  // Persistent representation...
+  virtual void io(Piostream&);
+  static PersistentTypeID type_id;
+
+  void io_map(Piostream&);
+
+  // GROUP: Member functions to manipulate attributes and geometries
+  //////////
+  // 
+
+  //////////
+  // Total number of registred attributes
+  inline int getNumAttribs() const{
+    return d_attribHandles.size();
+  }
+
+  //////////
+  // Returns handle to the current active attribute
+  virtual const AttribHandle getAttrib() const;
+  
+  //////////
+  // set attribute with the name supplied as active one
+  void setCurrAttrib(string aName);
+  
+  //////////
+  // Return attribute with the name supplied
+  const AttribHandle getAttrib(string aName) const;
+  
+  //////////
+  // Returns non-const handle to share the attribute for writing
+  virtual AttribHandle shareAttrib(string aName);
+
+  //////////
+  // Adds attribute to the field and sets current attribute name to its name
+  void addAttribute(const AttribHandle&);
+  
+  //////////
+  // Removes attribute with particular order number from the field
+  void removeAttribute(string);
+  
+
+protected:
+  AttribMap       d_attribHandles;
+  string          d_currAttrib;
+};
+
+} // end namespace SCIRun
+
+#endif
+
+
