d3c23b8da33ca7498bf5f129344c7180bce34aa9
Jim Guilkey <guilkey@sci.utah.edu>
2001-01-11 20:11:16 +0000

    Working on getting momentum exchange to work.  It doesnt' yet.

--- Uintah/Components/MPMICE/MPMICELabel.cc
+++ Uintah/Components/MPMICE/MPMICELabel.cc
@@ -14,7 +14,7 @@ using namespace Uintah::MPMICESpace;
 MPMICELabel::MPMICELabel()
 {
   // Cell centered variables
-  cVelocityLabel = scinew VarLabel( "c.velocity",
+  cMomentumLabel = scinew VarLabel( "c.momentum",
 			CCVariable<Vector>::getTypeDescription() );
   cMassLabel = scinew VarLabel( "c.mass",
 			CCVariable<double>::getTypeDescription() );
@@ -22,11 +22,14 @@ MPMICELabel::MPMICELabel()
 
 MPMICELabel::~MPMICELabel()
 {
-  delete cVelocityLabel;
+  delete cMomentumLabel;
   delete cMassLabel;
 }
 
 // $Log$
+// Revision 1.2  2001/01/11 20:11:16  guilkey
+// Working on getting momentum exchange to work.  It doesnt' yet.
+//
 // Revision 1.1  2000/12/28 20:26:36  guilkey
 // More work on coupling MPM and ICE
 //
