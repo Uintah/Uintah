4a71d9eb0332f900732d06e46d76a61de2d9ae8f
J. Davison de St. Germain <dav@sci.utah.edu>
2001-01-15 22:51:03 +0000

    Moved to new location in this tree

---
+++ StandAlone/tools/graphview/sub.mk
@@ -0,0 +1,14 @@
+# Makefile fragment for this subdirectory
+
+SRCDIR := Packages/Uintah/StandAlone/tools/graphview
+
+PROGRAM := $(SRCDIR)/graphview
+
+SRCS := $(SRCDIR)/graphview.cc	$(SRCDIR)/GV_TaskGraph.cc 	\
+	$(SRCDIR)/DaVinci.cc
+
+PSELIBS := Uintah/CCA/Ports Dataflow/XMLUtil Core/Exceptions Core/Thread 
+LIBS := $(XML_LIBRARY)
+
+include $(SRCTOP)/scripts/program.mk
+
