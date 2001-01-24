1d52503dc5ae95ff3cf9cdd1cf4cf61bb0c78deb
Jim Guilkey <guilkey@sci.utah.edu>
2001-01-24 02:04:40 +0000

    Added an EOS to give density as a function of pressure and vice versa. This is a stopgap solution.

--- CCA/Components/MPM/ConstitutiveModel/ConstitutiveModel.cc
+++ CCA/Components/MPM/ConstitutiveModel/ConstitutiveModel.cc
@@ -25,5 +25,39 @@ ConstitutiveModel::~ConstitutiveModel()
 }
 
 
+//for coupling to ICE
+double ConstitutiveModel::computeRhoMicro(double pressure)
+{
+
+  // This is really only valid now for CompNeoHook(Plas)
+  double rho_orig = 1000.0;
+  double rho_cur;
+  double p_ref=101325.0;
+  double p_gauge = pressure - p_ref;
+  double bulk = 2000.0;
+
+  rho_cur = rho_orig*(p_gauge/bulk + sqrt((p_gauge/bulk)*(p_gauge/bulk) +1));
+
+//  cout << rho_cur << endl;
+
+  return rho_cur;
+}
+
+void ConstitutiveModel::computePressEOS(const double  rho_cur, double& pressure,                                              double& dp_drho, double& ss_new)
+{
+
+  // This is really only valid now for CompNeoHook(Plas)
+  double rho_orig = 1000.0;
+  double p_ref=101325.0;
+  double bulk = 2000.0;
+
+  double p_g = .5*bulk*(rho_cur/rho_orig - rho_orig/rho_cur);
+  pressure = p_ref + p_g;
+  dp_drho  = .5*bulk*(rho_orig/(rho_cur*rho_cur) + 1./rho_orig);
+  ss_new   = sqrt(bulk/rho_cur);
+
+//  cout << "rho_cur = " << rho_cur << " press = " << pressure << " dp_drho = " << dp_drho << endl;
+
+}
 
 
