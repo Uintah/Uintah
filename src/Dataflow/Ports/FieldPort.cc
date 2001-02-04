41a10d6d8a6bef7f1af03844f38f8fb1383fb1ab
Chris Moulding <moulding@sci.utah.edu>
2001-02-04 01:59:23 +0000

    add port maker functions (for autoports)

--- src/Dataflow/Ports/FieldPort.cc
+++ src/Dataflow/Ports/FieldPort.cc
@@ -8,9 +8,18 @@
 
 
 #include <Dataflow/Ports/FieldPort.h>
+#include <Core/Malloc/Allocator.h>
 
 namespace SCIRun {
 
+extern "C" {
+PSECORESHARE IPort* make_FieldIPort(Module* module, const clString& name) {
+  return scinew SimpleIPort<FieldHandle>(module,name);
+}
+PSECORESHARE OPort* make_FieldOPort(Module* module, const clString& name) {
+  return scinew SimpleOPort<FieldHandle>(module,name);
+}
+}
 
 template<> clString SimpleIPort<FieldHandle>::port_type("Field");
 template<> clString SimpleIPort<FieldHandle>::port_color("yellow");
