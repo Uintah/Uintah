3c1c577e2f9b1828d54e4f399eadef880bb34dd1
Chris Moulding <moulding@sci.utah.edu>
2001-02-05 16:25:57 +0000

    fix compile bug.

--- src/Packages/Moulding/Dataflow/Modules/Visualization/GenField.cc
+++ src/Packages/Moulding/Dataflow/Modules/Visualization/GenField.cc
@@ -11,9 +11,7 @@
 #include <Core/Malloc/Allocator.h>
 #include <Core/Datatypes/Field.h>
 #include <Dataflow/Ports/FieldPort.h>
-#include <Core/Datatypes/VField.h>
 #include <Core/Datatypes/LatticeGeom.h>
-#include <Core/Datatypes/FlatAttrib.h>
 
 #include <Packages/Moulding/share/share.h>
 
