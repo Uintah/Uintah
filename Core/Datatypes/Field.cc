7ec5789cb45a17bbdc4f2996ec7482cd6aa3d2a5
Marty Cole <mcole@sci.utah.edu>
2001-02-20 22:04:45 +0000

    add some Persistent io, add data at, upgrade new mesh and fields

--- Core/Datatypes/Field.cc
+++ Core/Datatypes/Field.cc
@@ -3,9 +3,34 @@
 
 namespace SCIRun{
 
+// initialize the static member type_id
+PersistentTypeID Field::type_id(type_name(0), "FieldData", NULL);
+
+Field::Field() :
+  data_at_(NONE)
+{
+}
+
 Field::~Field()
 {
 }
 
+const double FIELD_VERSION = 1.0;
+
+void 
+Field::io(Piostream& stream){
+
+  stream.begin_class(Field::type_name(0).c_str(), FIELD_VERSION);
+  data_location &tmp = data_at_;
+  Pio(stream, (unsigned int&)tmp);
+  stream.end_class();
+}
+
+const string 
+Field::type_name(int)
+{
+  static const string name = "Field";
+  return name;
+}
 
 }
--- Core/Datatypes/MeshBase.cc
+++ Core/Datatypes/MeshBase.cc
@@ -3,8 +3,28 @@
 
 namespace SCIRun{
 
+// initialize the static member type_id
+PersistentTypeID MeshBase::type_id(type_name(0), "MeshBaseData", NULL);
+
+
 MeshBase::~MeshBase() 
 {
+}
+
+const double MESHBASE_VERSION = 1.0;
+
+void 
+MeshBase::io(Piostream& stream) {
+
+  stream.begin_class(MeshBase::type_name(0).c_str(), MESHBASE_VERSION);
+  stream.end_class();
+}
+
+const string 
+MeshBase::type_name(int)
+{
+  static const string name = "MeshBase";
+  return name;
 }
 
 }
