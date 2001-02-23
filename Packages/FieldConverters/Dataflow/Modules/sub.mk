9d6dabeaab8216d09d27f2b0427effb82f0bae17
Chris Moulding <moulding@sci.utah.edu>
2001-02-23 22:14:37 +0000

    new package for stand-alone executables used to convert to/from new fields

---
+++ Packages/FieldConverters/Dataflow/Modules/sub.mk
@@ -0,0 +1,18 @@
+# *** NOTE ***
+#
+# Do not remove or modify the comment line:
+#
+# #[INSERT NEW ?????? HERE]
+#
+# It is required by the Component Wizard to properly edit this file.
+# if you want to edit this file by hand, see the "Create A New Component"
+# documentation on how to do it correctly.
+
+SRCDIR := Packages/FieldConverters/Dataflow/Modules
+
+SUBDIRS := \
+#[INSERT NEW CATEGORY DIR HERE]
+
+include $(SRCTOP)/scripts/recurse.mk
+
+
