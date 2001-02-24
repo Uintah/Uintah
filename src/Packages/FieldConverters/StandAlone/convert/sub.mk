3845b9419ca47088fe0b24cc8f30c6e585429201
David Weinstein <dmw@sci.utah.edu>
2001-02-24 02:20:39 +0000

    added StandAlone converter programs

---
+++ src/Packages/FieldConverters/StandAlone/convert/sub.mk
@@ -0,0 +1,42 @@
+# Makefile fragment for this subdirectory
+
+SRCDIR := Packages/FieldConverters/StandAlone/convert
+
+ifeq ($(LARGESOS),yes)
+PSELIBS := Core Packages/FieldConverters/Core
+else
+PSELIBS := Core/Datatypes Core/Containers Core/Persistent Core/Exceptions Core/Thread Core/Geometry Packages/FieldConverters/Core/Datatypes
+endif
+LIBS := -lm
+
+PROGRAM := $(SRCDIR)/OldSFRGtoNewLatticeVol
+SRCS := $(SRCDIR)/OldSFRGtoNewLatticeVol.cc
+include $(SRCTOP)/scripts/program.mk
+
+PROGRAM := $(SRCDIR)/NewLatticeVolToOldSFRG
+SRCS := $(SRCDIR)/NewLatticeVolToOldSFRG.cc
+include $(SRCTOP)/scripts/program.mk
+
+PROGRAM := $(SRCDIR)/OldSFUGtoNewTetVol
+SRCS := $(SRCDIR)/OldSFUGtoNewTetVol.cc
+include $(SRCTOP)/scripts/program.mk
+
+PROGRAM := $(SRCDIR)/NewTetVolToOldSFUG
+SRCS := $(SRCDIR)/NewTetVolToOldSFUG.cc
+include $(SRCTOP)/scripts/program.mk
+
+PROGRAM := $(SRCDIR)/OldMeshToNewTetVol
+SRCS := $(SRCDIR)/OldMeshToNewTetVol.cc
+include $(SRCTOP)/scripts/program.mk
+
+PROGRAM := $(SRCDIR)/NewTetVolToOldMesh
+SRCS := $(SRCDIR)/NewTetVolToOldMesh.cc
+include $(SRCTOP)/scripts/program.mk
+
+PROGRAM := $(SRCDIR)/OldSurfaceToNewTriSurf
+SRCS := $(SRCDIR)/OldSurfaceToNewTriSurf.cc
+include $(SRCTOP)/scripts/program.mk
+
+PROGRAM := $(SRCDIR)/NewTriSurfToOldSurface
+SRCS := $(SRCDIR)/NewTriSurfToOldSurface.cc
+include $(SRCTOP)/scripts/program.mk
