caeb73faf2185914d8f8c712d1c2b501b334112a
Chris Moulding <moulding@sci.utah.edu>
2001-03-01 08:50:26 +0000

    remove ShowField and some converters from the build: they don't compile on linux

--- src/Packages/FieldConverters/StandAlone/convert/sub.mk
+++ src/Packages/FieldConverters/StandAlone/convert/sub.mk
@@ -17,21 +17,21 @@ PROGRAM := $(SRCDIR)/NewLatticeVolToOldSFRG
 SRCS := $(SRCDIR)/NewLatticeVolToOldSFRG.cc
 include $(SRCTOP)/scripts/program.mk
 
-PROGRAM := $(SRCDIR)/OldSFUGtoNewTetVol
-SRCS := $(SRCDIR)/OldSFUGtoNewTetVol.cc
-include $(SRCTOP)/scripts/program.mk
+#PROGRAM := $(SRCDIR)/OldSFUGtoNewTetVol
+#SRCS := $(SRCDIR)/OldSFUGtoNewTetVol.cc
+#include $(SRCTOP)/scripts/program.mk
 
-PROGRAM := $(SRCDIR)/NewTetVolToOldSFUG
-SRCS := $(SRCDIR)/NewTetVolToOldSFUG.cc
-include $(SRCTOP)/scripts/program.mk
+#PROGRAM := $(SRCDIR)/NewTetVolToOldSFUG
+#SRCS := $(SRCDIR)/NewTetVolToOldSFUG.cc
+#include $(SRCTOP)/scripts/program.mk
 
-PROGRAM := $(SRCDIR)/OldMeshToNewTetVol
-SRCS := $(SRCDIR)/OldMeshToNewTetVol.cc
-include $(SRCTOP)/scripts/program.mk
+#PROGRAM := $(SRCDIR)/OldMeshToNewTetVol
+#SRCS := $(SRCDIR)/OldMeshToNewTetVol.cc
+#include $(SRCTOP)/scripts/program.mk
 
-PROGRAM := $(SRCDIR)/NewTetVolToOldMesh
-SRCS := $(SRCDIR)/NewTetVolToOldMesh.cc
-include $(SRCTOP)/scripts/program.mk
+#PROGRAM := $(SRCDIR)/NewTetVolToOldMesh
+#SRCS := $(SRCDIR)/NewTetVolToOldMesh.cc
+#include $(SRCTOP)/scripts/program.mk
 
 PROGRAM := $(SRCDIR)/OldSurfaceToNewTriSurf
 SRCS := $(SRCDIR)/OldSurfaceToNewTriSurf.cc
