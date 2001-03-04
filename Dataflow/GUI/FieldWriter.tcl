ff934f98b34458cb8f05b55d392e3585e8c08117
David Weinstein <dmw@sci.utah.edu>
2001-03-04 03:30:43 +0000

    check for SCIRUN_DATA environment variable

--- Dataflow/GUI/FieldWriter.tcl
+++ Dataflow/GUI/FieldWriter.tcl
@@ -38,6 +38,8 @@ itcl_class SCIRun_DataIO_FieldWriter {
 	if { $initdir==""} {
 	    if {[info exists env(SCI_DATA)]} {
 		set initdir $env(SCI_DATA)
+	    } elseif {[info exists env(SCIRUN_DATA)]} {
+		set initdir $env(SCIRUN_DATA)
 	    }
 	}
 
