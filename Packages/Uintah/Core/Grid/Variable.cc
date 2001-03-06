4c1eef4f29b8d2a4f3b35dfceafaa35f913a6196
Wayne Witzel <witzel@sci.utah.edu>
2001-03-06 19:24:47 +0000

    Added support for "tryall" compression mode.

--- Packages/Uintah/Core/Grid/Variable.cc
+++ Packages/Uintah/Core/Grid/Variable.cc
@@ -1,6 +1,7 @@
 #include <Packages/Uintah/Core/Grid/Variable.h>
 #include <Core/Exceptions/ErrnoException.h>
 #include <Core/Malloc/Allocator.h>
+#include <Dataflow/XMLUtil/XMLUtil.h>
 #include <sstream>
 #include <zlib.h>
 #include <math.h>
@@ -23,22 +24,35 @@ void Variable::setForeign()
    d_foreign = true;
 }
 
-void Variable::emit(OutputContext& oc, string compressionMode)
+void Variable::emit(OutputContext& oc, const string& compressionModeHint)
 {
   bool use_rle = false;
   bool use_gzip = false;
+  bool try_all = false;
 
-  if ((compressionMode == "rle, gzip") || (compressionMode == "gzip, rle")) {
+  bool used_rle = false;
+  bool used_gzip = false;
+
+  if (compressionModeHint == "tryall") {
+    use_rle = false; // try without rle first
+    use_gzip = true;
+    try_all = true;
+  }
+  else if ((compressionModeHint == "rle, gzip") ||
+	   (compressionModeHint == "gzip, rle")) {
     use_rle = true;
     use_gzip = true;
   }
-  else if (compressionMode == "rle")
+  else if (compressionModeHint == "rle")
     use_rle = true;
-  else if (compressionMode == "gzip")
+  else if (compressionModeHint == "gzip")
     use_gzip = true;
-  else if (compressionMode != "") {
-    throw InvalidCompressionMode(compressionMode);
+  else if (compressionModeHint != "" && compressionModeHint != "none") {
+    throw InvalidCompressionMode(compressionModeHint);
   }
+
+  used_rle = use_rle;
+  used_gzip = use_gzip;
   
   ostringstream outstream;
 
@@ -46,39 +60,106 @@ void Variable::emit(OutputContext& oc, string compressionMode)
     emitRLE(outstream, oc.varnode);
   else
     emitNormal(outstream, oc.varnode);
-  
-  unsigned long outstreamsize = outstream.str().size();
+
+  string preGzip = outstream.str();
+  string preGzip2;
+  string buffer; // trying to avoid copying the strings back and forth
+  string buffer2;
+  string* writeoutString = &preGzip;
   if (use_gzip) {
-    // use gzip compression
+    writeoutString = gzipCompress(&preGzip, &buffer);
+    if (writeoutString != &buffer)
+      used_gzip = false; // gzip wasn't better, so it wasn't used
 
-    // follows compress guidelines: 1% more than source size + 12
-    unsigned long bufsize =
-      (unsigned long)(ceil((double)outstreamsize * 1.01) + 12); 
-
-    char* buffer = scinew char[bufsize];
-    if (compress((Bytef*)buffer, &bufsize,
-		 (const Bytef*)outstream.str().c_str(), outstreamsize) != Z_OK)
-      cerr << "compress failed in Uintah::Variable::emit\n";
+    if (try_all) {
+      ostringstream outstream2;
+      try {
+	emitRLE(outstream2, oc.varnode);
+	preGzip2 = outstream2.str();
+	string* writeoutString2 = gzipCompress(&preGzip2, &buffer2);
 	
-    // first write the uncompressed data size
-    ssize_t s = ::write(oc.fd, &outstreamsize, sizeof(unsigned long));
-    if(s != sizeof(unsigned long))
-      throw ErrnoException("Variable::emit (write call)", errno);
+	if ((*writeoutString2).size() < (*writeoutString).size()) {
+	  // rle was making it worse
+	  writeoutString->clear(); // erase the old one to save space
+	  writeoutString = writeoutString2; 
+	  used_rle = true;
+	  if (writeoutString2 == &buffer2)
+	    used_gzip = true;
+	  else
+	  used_gzip = false;
+	}
+	else
+	  writeoutString2->clear(); // doesn't get used, so erase it
+      }
+      catch (InvalidCompressionMode& ) {
+	// just trying, if it doesn't like it then forget it
+      }
+    }
+  }
 
-    oc.cur += sizeof(unsigned long);
+  const char* writebuffer = (*writeoutString).c_str();
+  unsigned long writebufferSize = (*writeoutString).size();
+  ssize_t s = ::write(oc.fd, writebuffer, writebufferSize);
 
-    // then write the compressed data
-    s = ::write(oc.fd, buffer, bufsize);
-    if(s != (long)bufsize)
-      throw ErrnoException("Variable::emit (write call)", errno);
+  if(s != (long)writebufferSize)
+    throw ErrnoException("Variable::emit (write call)", errno);
+  oc.cur += writebufferSize;
 
-    oc.cur += bufsize;
-    delete[] buffer;
+  string compressionMode = compressionModeHint;
+  if (try_all || (used_gzip != use_gzip) || (used_rle != use_rle)) {
+    // compression mode string changes
+    if (used_gzip) {
+      if (used_rle)
+	compressionMode = "rle, gzip";
+      else
+	compressionMode = "gzip";
+    }
+    else if (used_rle) {
+      compressionMode = "rle";
+    }
+    else
+      compressionMode = "";
+  }
+
+  if (compressionMode != "" && compressionMode != "none")
+    appendElement(oc.varnode, "compression", compressionMode);
+}
+
+string* Variable::gzipCompress(string* pUncompressed, string* pBuffer)
+{
+  unsigned long uncompressedSize = pUncompressed->size();
+
+  // follows compress guidelines: 1% more than source size + 12
+  unsigned long compressBufsize =
+    (unsigned long)(ceil((double)uncompressedSize * 1.01) + 12); 
+
+  pBuffer->resize(compressBufsize + sizeof(unsigned long));
+  char* buf = (char*)pBuffer->c_str(); // casting from const, careful
+  buf += sizeof(unsigned long); /* the first part will give the size of
+				   the uncompressed data */
+  
+  if (compress((Bytef*)buf, &compressBufsize,
+	       (const Bytef*)pUncompressed->c_str(), uncompressedSize) != Z_OK)
+    cerr << "compress failed in Uintah::Variable::gzipCompress\n";
+
+  pBuffer->resize(compressBufsize + sizeof(unsigned long));
+  if (pBuffer->size() > uncompressedSize) {
+    // gzip made it worse -- forget that
+    // (this should rarely, if ever, happen, but just in case)
+    pBuffer->clear(); /* the other buffer isn't needed, erase it to
+			 save space */
+    return pUncompressed;
   }
   else {
-    ::write(oc.fd, outstream.str().c_str(), outstreamsize);
-    oc.cur += outstreamsize;
-  }
+    // write out the uncompressed size to the first part of the buffer
+    char* pbyte = (char*)(&uncompressedSize);
+    for (int i = 0; i < sizeof(unsigned long); i++, pbyte++) {
+      (*pBuffer)[i] = *pbyte;
+    }
+    pUncompressed->clear(); /* the original buffer isn't needed, erase it to
+			       save space */
+    return pBuffer;
+  } 
 }
 
 
