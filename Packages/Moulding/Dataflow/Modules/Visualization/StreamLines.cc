acab682116b0ba0862b1925c5ee516b9b99da3e5
Chris Moulding <moulding@sci.utah.edu>
2001-03-07 07:16:22 +0000

    use the new query_interpolate() interface to do linear interpolation of vectors

--- Packages/Moulding/Dataflow/Modules/Visualization/StreamLines.cc
+++ Packages/Moulding/Dataflow/Modules/Visualization/StreamLines.cc
@@ -9,12 +9,14 @@
 
 #include <Dataflow/Network/Module.h>
 #include <Core/Malloc/Allocator.h>
-#include <Core/Datatypes/Field.h>
+#include <Core/Datatypes/GenericField.h>
 #include <Dataflow/Ports/FieldPort.h>
 #include <Core/Containers/Array1.h>
 #include <Core/Geometry/Point.h>
 #include <Core/Geometry/Vector.h>
 #include <Core/Datatypes/LatticeVol.h>
+#include <Core/Datatypes/LatVolMesh.h>
+#include <Dataflow/Network/NetworkEditor.h>
 
 #include <iostream>
 #include <vector>
@@ -25,6 +27,8 @@
 namespace Moulding {
 
 using namespace SCIRun;
+using std::cerr;
+using std::endl;
 
 // LUTs for the RK-fehlberg algorithm 
 double a[]   ={16.0/135, 0, 6656.0/12825, 28561.0/56430, -9.0/50, 2.0/55};
@@ -50,11 +54,17 @@ public:
 private:
   // data members
 
-  FieldIPort*       d_vfport;
-  FieldIPort*       d_sfport;
+  FieldIPort                 *vfport_;
+  FieldIPort                 *sfport_;
+  FieldOPort                 *oport_;
 
-  FieldHandle       d_vfhandle;
-  FieldHandle       d_sfhandle;
+  FieldHandle                vfhandle_;
+  FieldHandle                sfhandle_;
+  FieldHandle                ohandle_;
+
+  Field                      *vf_;
+
+  GenericInterpolate<Vector> *interp_;
 
   // member functions
 
@@ -74,11 +84,16 @@ StreamLines::StreamLines(const clString& id)
   : Module("StreamLines", id, Source)
   //, "Visualization", "Moulding")
 {
-  d_vfport = scinew FieldIPort(this, "Flow field", FieldIPort::Atomic);
-  add_iport(d_vfport);
+  vfport_ = scinew FieldIPort(this, "Flow field", FieldIPort::Atomic);
+  add_iport(vfport_);
 
-  d_sfport = scinew FieldIPort(this, "Seed field", FieldIPort::Atomic);
-  add_iport(d_sfport);
+  sfport_ = scinew FieldIPort(this, "Seed field", FieldIPort::Atomic);
+  add_iport(sfport_);
+
+  oport_ = scinew FieldOPort(this, "Stream lines", FieldIPort::Atomic);
+  add_oport(oport_);
+
+  vf_ = 0;
 }
 
 StreamLines::~StreamLines()
@@ -91,37 +106,22 @@ StreamLines::ComputeRKFTerms(Array1<Vector>& v /* storage for terms */,
 			     float s           /* current step size */)
 {
   int check = 0;
-#if 0
-  check |= vlinterpolate->vlinterpolate(p,
-					v[0]);
-  check |= vlinterpolate->vlinterpolate(p+
-					v[0]*d[1][0],
-					v[1]);
-  check |= vlinterpolate->vlinterpolate(p+
-					v[0]*d[2][0]+
-					v[1]*d[2][1],
-					v[2]);
-  check |= vlinterpolate->vlinterpolate(p+
-					v[0]*d[3][0]+
-					v[1]*d[3][1]+
-					v[2]*d[3][2],
-					v[3]);
-  check |= vlinterpolate->vlinterpolate(p+
-					v[0]*d[4][0]+
-					v[1]*d[4][1]+
-					v[2]*d[4][2]+
-					v[3]*d[4][3],
-					v[4]);
-  check |= vlinterpolate->vlinterpolate(p+
-					v[0]*d[5][0]+
-					v[1]*d[5][1]+
-					v[2]*d[5][2]+
-					v[3]*d[5][3]+
-					v[4]*d[5][4],
-					v[5]);
 
-  for (int loop=0;loop<5;loop++)
-    v[loop]*=s;
+#if 0
+  check |= interp_(*vf_,p,v[0]);
+  check |= interp_(*vf_,p+v[0]*d[1][0],v[1]);
+  check |= interp_(*vf_,p+v[0]*d[2][0]+v[1]*d[2][1],v[2]);
+  check |= interp_(*vf_,p+v[0]*d[3][0]+v[1]*d[3][1]+v[2]*d[3][2],v[3]);
+  check |= interp_(*vf_,p+v[0]*d[4][0]+v[1]*d[4][1]+v[2]*d[4][2]+
+		   v[3]*d[4][3],v[4]);
+  check |= interp_(*vf_,p+v[0]*d[5][0]+v[1]*d[5][1]+v[2]*d[5][2]+
+		   v[3]*d[5][3]+v[4]*d[5][4],v[5]);
+
+  v[0]*=s;
+  v[1]*=s;
+  v[2]*=s;
+  v[3]*=s;
+  v[4]*=s;
 #endif
   return check;
 }
@@ -145,7 +145,7 @@ StreamLines::FindStreamLineSupport(Array1<Point>& v /* storage for points */,
 
   // add the initial point to the list of points found.
   v.add(x);
-  std::cerr << "found point = " << x << endl;
+  cerr << "found point = " << x << endl;
 
   for (loop=0;loop<n;loop++) {
 
@@ -170,16 +170,27 @@ StreamLines::FindStreamLineSupport(Array1<Point>& v /* storage for points */,
     x = x + terms[0]*a[0]+terms[1]*a[1]+terms[2]*a[2]+
             terms[3]*a[3]+terms[4]*a[4]+terms[5]*a[5];
     v.add(x);
-    std::cerr << "found point = " << x << endl;
+    cerr << "found point = " << x << endl;
   }
 }
 
 void StreamLines::execute()
 {
-  d_vfport->get(d_vfhandle);
-  if(!d_vfhandle.get_rep())
+  if (!vfport_->get(vfhandle_))
     return;
-  
+
+  if(!(vf_ = vfhandle_.get_rep()))
+    return;
+
+  // we expect that the field is a vector field
+  if (vf_->get_type_name(1) != "Vector") {
+    postMessage("StreamLines: ERROR: The input field is not a vector field."
+		"  Exiting.");
+    return;
+  }
+
+  interp_ = (GenericInterpolate<Vector>*)vf_->query_interpolate();
+    
   Array1<Point> support;
   FindStreamLineSupport(support,Point(32,32,32),.01,.05,100);
 }
