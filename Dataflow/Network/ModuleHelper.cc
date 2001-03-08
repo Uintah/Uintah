c99a68d8c8bdb25a605ef87862a789508e24e3ab
David Weinstein <dmw@sci.utah.edu>
2001-03-08 03:34:30 +0000

    fixed so pid show up for Viewer

--- Dataflow/Network/ModuleHelper.cc
+++ Dataflow/Network/ModuleHelper.cc
@@ -40,44 +40,43 @@ ModuleHelper::~ModuleHelper()
 
 void ModuleHelper::run()
 {
-
-    if(module->have_own_dispatch){
+  module->pid_.set(getpid());
+  if(module->have_own_dispatch){
+    module->do_execute();
+  } else {
+    for(;;){
+      MessageBase* msg=module->mailbox.receive();
+      switch(msg->type){
+      case MessageTypes::ExecuteModule:
 	module->do_execute();
-    } else {
-        module->pid_.set(getpid());
-	for(;;){
-	    MessageBase* msg=module->mailbox.receive();
-	    switch(msg->type){
-	    case MessageTypes::ExecuteModule:
-		module->do_execute();
-		break;
-	    case MessageTypes::TriggerPort:
-		{
-		    Scheduler_Module_Message* smsg=(Scheduler_Module_Message*)msg;
-		    smsg->conn->oport->resend(smsg->conn);
-		}
-		break;
-	    case MessageTypes::Demand:
-  	        {
-#if 0
-		    Demand_Message* dmsg=(Demand_Message*)msg;
-		    if(dmsg->conn->oport->have_data()){
-		        dmsg->conn->oport->resend(dmsg->conn);
-		    } else {
-		        dmsg->conn->demand++;
-			while(dmsg->conn->demand)
-			    module->do_execute();
-		    }
-#endif
-		}
-		break;
-	    default:
-		cerr << "Illegal Message type: " << msg->type << endl;
-		break;
-	    }
-	    delete msg;
+	break;
+      case MessageTypes::TriggerPort:
+	{
+	  Scheduler_Module_Message* smsg=(Scheduler_Module_Message*)msg;
+	  smsg->conn->oport->resend(smsg->conn);
 	}
+	break;
+      case MessageTypes::Demand:
+	{
+#if 0
+	  Demand_Message* dmsg=(Demand_Message*)msg;
+	  if(dmsg->conn->oport->have_data()){
+	    dmsg->conn->oport->resend(dmsg->conn);
+	  } else {
+	    dmsg->conn->demand++;
+	    while(dmsg->conn->demand)
+	      module->do_execute();
+	  }
+#endif
+	}
+	break;
+      default:
+	cerr << "Illegal Message type: " << msg->type << endl;
+	break;
+      }
+      delete msg;
     }
+  }
 }
 
 } // End namespace SCIRun
