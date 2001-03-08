7b90b551f91e982b5feb7a4125d5b72703bd3d87
David Weinstein <dmw@sci.utah.edu>
2001-03-08 03:35:09 +0000

    removed minmax from Field

--- src/Dataflow/Modules/Visualization/sub.mk
+++ src/Dataflow/Modules/Visualization/sub.mk
@@ -15,12 +15,12 @@ SRCS     += \
 	$(SRCDIR)/GLTextureBuilder.cc\
 	$(SRCDIR)/GenStandardColorMaps.cc\
 	$(SRCDIR)/GenTransferFunc.cc\
-	$(SRCDIR)/Isosurface.cc\
 	$(SRCDIR)/RescaleColorMap.cc\
 	$(SRCDIR)/ShowField.cc\
 	$(SRCDIR)/Streamline.cc\
 	$(SRCDIR)/TexCuttingPlanes.cc\
 	$(SRCDIR)/TextureVolVis.cc\
+#	$(SRCDIR)/Isosurface.cc\
 [INSERT NEW CODE FILE HERE]
 
 
