4e01247b39a454d07777afa0073f25c3b40eb46a
Marty Cole <mcole@sci.utah.edu>
2001-03-12 21:25:43 +0000

    retire unused code

--- Core/ThirdParty/mpeg/sub.mk
+++
@@ -1,9 +0,0 @@
-# Makefile fragment for this subdirectory
-
-SRCDIR   := Dataflow/ThirdParty/mpeg
-
-SRCS     += $(SRCDIR)/mpeg.o $(SRCDIR)/codec.o $(SRCDIR)/huffman.o \
-	$(SRCDIR)/io.o $(SRCDIR)/chendct.o $(SRCDIR)/lexer.o \
-	$(SRCDIR)/marker.o $(SRCDIR)/me.o $(SRCDIR)/mem.o \
-	$(SRCDIR)/stat.o $(SRCDIR)/stream.o $(SRCDIR)/transform.o
-
