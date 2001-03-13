7739ca4f214e1a33769017aae00189ac96b5ca31
Chris Moulding <moulding@sci.utah.edu>
2001-03-13 09:39:46 +0000

    fix largesos build

--- Packages/Nrrd/sub.mk
+++ Packages/Nrrd/sub.mk
@@ -17,8 +17,6 @@
 
 #Makefile fragment for the Packages/Nrrd directory
 
-include $(SRCTOP_ABS)/scripts/largeso_prologue.mk
-
 SRCDIR := Packages/Nrrd
 SUBDIRS := \
 	$(SRCDIR)/Core \
@@ -26,7 +24,3 @@ SUBDIRS := \
 
 include $(SRCTOP_ABS)/scripts/recurse.mk
 
-PSELIBS := 
-LIBS := $(NRRD_LIBRARY) -lm
-
-include $(SRCTOP_ABS)/scripts/largeso_epilogue.mk
