3a7b8ee334a2569a2481066dd72fe6db24680f56
Steven G. Parker <sparker@sci.utah.edu>
2001-03-22 22:53:14 +0000

    Use tags: <constitutive_model type="comp_mooney_rivlin"> </comp_mooney_rivlin> instead of <comp_mooney_rivlin> </comp_mooney_rivlin>
    Throw exception if CM not found

--- CCA/Components/MPM/ConstitutiveModel/ConstitutiveModelFactory.cc
+++ CCA/Components/MPM/ConstitutiveModel/ConstitutiveModelFactory.cc
@@ -4,6 +4,7 @@
 #include "CompNeoHookPlas.h"
 #include "ViscoScram.h"
 #include "HypoElastic.h"
+#include <Packages/Uintah/Core/Exceptions/ProblemSetupException.h>
 #include <Core/Malloc/Allocator.h>
 #include <fstream>
 #include <iostream>
@@ -16,28 +17,28 @@ using namespace Uintah;
 
 ConstitutiveModel* ConstitutiveModelFactory::create(ProblemSpecP& ps)
 {
-   for (ProblemSpecP child = ps->findBlock(); child != 0;
-	child = child->findNextBlock()) {
-      std::string mat_type = child->getNodeName();
-      if (mat_type == "comp_mooney_rivlin")
-	 return(scinew CompMooneyRivlin(child));
+   ProblemSpecP child = ps->findBlock("constitutive_model");
+   if(!child)
+      throw ProblemSetupException("Cannot find constitutive_model tag");
+   string mat_type;
+   if(!child->getAttribute("type", mat_type))
+      throw ProblemSetupException("No type for constitutive_model");
+   
+   if (mat_type == "comp_mooney_rivlin")
+      return(scinew CompMooneyRivlin(child));
+   
+   else if (mat_type ==  "comp_neo_hook")
+      return(scinew CompNeoHook(child));
       
-      else if (mat_type ==  "comp_neo_hook")
-	 return(scinew CompNeoHook(child));
-      
-      else if (mat_type == "comp_neo_hook_plastic")
-	 return(scinew CompNeoHookPlas(child));
-
-      else if (mat_type ==  "visco_scram")
-	 return(scinew ViscoScram(child));
-      
-      else if (mat_type ==  "hypo_elastic")
-	 return(scinew HypoElastic(child));
-      
-      else {
-	 cerr << "Unknown Material Type R (" << mat_type << ")" << std::endl;;
-	 //      exit(1);
-      }
-   }
-   return 0;
+   else if (mat_type == "comp_neo_hook_plastic")
+      return(scinew CompNeoHookPlas(child));
+   
+   else if (mat_type ==  "visco_scram")
+      return(scinew ViscoScram(child));
+   
+   else if (mat_type ==  "hypo_elastic")
+      return(scinew HypoElastic(child));
+   
+   else 
+      throw ProblemSetupException("Unknown Material Type R ("+mat_type+")");
 }
