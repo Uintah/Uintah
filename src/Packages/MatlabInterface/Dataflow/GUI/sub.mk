daa15f66faa040d48bc6ce24113739beee32eb88
Chris Moulding <moulding@sci.utah.edu>
2001-03-22 18:55:45 +0000

    add new (empty) MatlabInterface package

---
+++ src/Packages/MatlabInterface/Dataflow/GUI/sub.mk
@@ -0,0 +1,22 @@
+# *** NOTE ***
+#
+# Do not remove or modify the comment line:
+#
+# #[INSERT NEW ?????? HERE]
+#
+# It is required by the Component Wizard to properly edit this file.
+# if you want to edit this file by hand, see the "Create A New Component"
+# documentation on how to do it correctly.
+
+SRCDIR := Packages/MatlabInterface/Dataflow/GUI
+
+ALLTARGETS := $(ALLTARGETS) $(SRCDIR)/tclIndex
+
+$(SRCDIR)/tclIndex: \
+	$(SRCDIR)/VolumeRender.tcl\
+#[INSERT NEW TCL FILE HERE]
+	$(SRCTOP)/scripts/createTclIndex $(SRCTOP)/Packages/MatlabInterface/Dataflow/GUI
+
+CLEANPROGS := $(CLEANPROGS) $(SRCDIR)/tclIndex
+
+
