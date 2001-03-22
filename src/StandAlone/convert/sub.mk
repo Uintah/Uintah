5c7dc35585f3aabc557e3eff35df2ce1b1bf5a12
David Weinstein <dmw@sci.utah.edu>
2001-03-22 05:20:47 +0000

    StandAlone/convert programs for converting to/from raw/field files (note: not all of them work yet)

---
+++ src/StandAlone/convert/sub.mk
@@ -0,0 +1,59 @@
+#
+#  The contents of this file are subject to the University of Utah Public
+#  License (the "License"); you may not use this file except in compliance
+#  with the License.
+#  
+#  Software distributed under the License is distributed on an "AS IS"
+#  basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+#  License for the specific language governing rights and limitations under
+#  the License.
+#  
+#  The Original Source Code is SCIRun, released March 12, 2001.
+#  
+#  The Original Source Code was developed by the University of Utah.
+#  Portions created by UNIVERSITY are Copyright (C) 2001, 1994 
+#  University of Utah. All Rights Reserved.
+#
+
+# Makefile fragment for this subdirectory
+
+SRCDIR := StandAlone/convert
+
+ifeq ($(LARGESOS),yes)
+PSELIBS := Core
+else
+PSELIBS := Core/Datatypes Core/Containers Core/Persistent Core/Exceptions Core/Thread Core/Geometry Core/Math
+endif
+LIBS := -lm
+
+PROGRAM := $(SRCDIR)/RawToContourField
+SRCS := $(SRCDIR)/RawToContourField.cc
+include $(SRCTOP)/scripts/program.mk
+
+PROGRAM := $(SRCDIR)/RawToLatticeVol
+SRCS := $(SRCDIR)/RawToLatticeVol.cc
+include $(SRCTOP)/scripts/program.mk
+
+PROGRAM := $(SRCDIR)/RawToTetVol
+SRCS := $(SRCDIR)/RawToTetVol.cc
+include $(SRCTOP)/scripts/program.mk
+
+PROGRAM := $(SRCDIR)/RawToTriSurf
+SRCS := $(SRCDIR)/RawToTriSurf.cc
+include $(SRCTOP)/scripts/program.mk
+
+PROGRAM := $(SRCDIR)/ContourFieldToRaw
+SRCS := $(SRCDIR)/ContourFieldToRaw.cc
+include $(SRCTOP)/scripts/program.mk
+
+PROGRAM := $(SRCDIR)/LatticeVolToRaw
+SRCS := $(SRCDIR)/LatticeVolToRaw.cc
+include $(SRCTOP)/scripts/program.mk
+
+PROGRAM := $(SRCDIR)/TetVolToRaw
+SRCS := $(SRCDIR)/TetVolToRaw.cc
+include $(SRCTOP)/scripts/program.mk
+
+PROGRAM := $(SRCDIR)/TriSurfToRaw
+SRCS := $(SRCDIR)/TriSurfToRaw.cc
+include $(SRCTOP)/scripts/program.mk
