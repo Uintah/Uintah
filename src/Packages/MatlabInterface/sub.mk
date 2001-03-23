8eda0a02c9bd74611cf9555b126b986bfdd7bab9
Marty Cole <mcole@sci.utah.edu>
2001-03-23 19:11:39 +0000

    add license to files

--- src/Packages/MatlabInterface/sub.mk
+++ src/Packages/MatlabInterface/sub.mk
@@ -1,4 +1,19 @@
-include $(SRCTOP)/scripts/largeso_prologue.mk
+#
+#  The contents of this file are subject to the University of Utah Public
+#  License (the "License"); you may not use this file except in compliance
+#  with the License.
+#  
+#  Software distributed under the License is distributed on an "AS IS"
+#  basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+#  License for the specific language governing rights and limitations under
+#  the License.
+#  
+#  The Original Source Code is SCIRun, released March 12, 2001.
+#  
+#  The Original Source Code was developed by the University of Utah.
+#  Portions created by UNIVERSITY are Copyright (C) 2001, 1994 
+#  University of Utah. All Rights Reserved.
+#
 
 SRCDIR := Packages/MatlabInterface
 
@@ -7,10 +22,3 @@ SUBDIRS := \
         $(SRCDIR)/Dataflow \
 
 include $(SRCTOP)/scripts/recurse.mk
-
-PSELIBS := 
-LIBS := $(TK_LIBRARY) $(GL_LIBS) -lm
-
-include $(SRCTOP)/scripts/largeso_epilogue.mk
-
-
