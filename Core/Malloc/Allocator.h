f21b15ea88bf2c44309fabfd4a38821276f23545
Steven G. Parker <sparker@sci.utah.edu>
2001-03-25 06:53:03 +0000

    Provided a mechanism to access the subset of stats that are fast   to compute

--- Core/Malloc/Allocator.h
+++ Core/Malloc/Allocator.h
@@ -64,6 +64,14 @@ void GetGlobalStats(Allocator*,
 		    size_t& bytes_fragmented,
 		    size_t& bytes_inuse,
 		    size_t& bytes_inhunks);
+void GetGlobalStats(Allocator*,
+		    size_t& nalloc, size_t& sizealloc,
+		    size_t& nfree, size_t& sizefree,
+		    size_t& nfillbin,
+		    size_t& nmmap, size_t& sizemmap,
+		    size_t& nmunmap, size_t& sizemunmap,
+		    size_t& highwater_alloc, size_t& highwater_mmap,
+		    size_t& nlonglocks, size_t& nnaps);
 int GetNbins(Allocator*);
 void GetBinStats(Allocator*, int binno, size_t& minsize, size_t& maxsize,
 		 size_t& nalloc, size_t& nfree, size_t& ninlist);
