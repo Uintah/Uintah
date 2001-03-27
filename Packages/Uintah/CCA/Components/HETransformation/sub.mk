d15653385a28a9f23a6b1e08adb412bada5ad0a7
John Schmidt <jas@sci.utah.edu>
2001-03-27 20:08:01 +0000

    Moved MPMICE/Combustion stuff to the HETransformation component.

---
+++ Packages/Uintah/CCA/Components/HETransformation/sub.mk
@@ -0,0 +1,14 @@
+# Makefile fragment for this subdirectory
+
+include $(SRCTOP)/scripts/smallso_prologue.mk
+
+SRCDIR   := Packages/Uintah/CCA/Components/HETransformation
+
+SRCS     += $(SRCDIR)/NullBurn.cc $(SRCDIR)/SimpleBurn.cc \
+	$(SRCDIR)/BurnFactory.cc  $(SRCDIR)/Burn.cc
+
+PSELIBS	:= Packages/Uintah/Core/ProblemSpec 
+
+LIBS := $(XML_LIBRARY) -lm
+
+include $(SRCTOP)/scripts/smallso_epilogue.mk
