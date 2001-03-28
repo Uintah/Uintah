332bab612a127084777d819c106f85579a3b5bd0
Steven G. Parker <sparker@sci.utah.edu>
2001-03-28 15:16:25 +0000

    press_matl needed to actually contain material 0

--- ICE.cc
+++ ICE.cc
@@ -290,7 +290,8 @@ void ICE::scheduleTimeAdvance(double t, double dt,const LevelP& level,
   const MaterialSet* ice_matls = d_sharedState->allICEMaterials();
   const MaterialSet* mpm_matls = d_sharedState->allMPMMaterials();
   const MaterialSet* all_matls = d_sharedState->allMaterials();  
-  const MaterialSubset* press_matl    = scinew MaterialSubset();
+  MaterialSubset* press_matl    = scinew MaterialSubset();
+  press_matl->add(0);
   press_matl ->addReference();
   const MaterialSubset* ice_matls_sub = ice_matls->getUnion();
   const MaterialSubset* mpm_matls_sub = mpm_matls->getUnion();
