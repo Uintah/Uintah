1df368b34d2b7d4097615e4cb1422748370783de
Marty Cole <mcole@sci.utah.edu>
2001-03-28 16:40:44 +0000

    add cylinder option for rendering edges

--- Dataflow/GUI/ShowField.tcl
+++ Dataflow/GUI/ShowField.tcl
@@ -31,9 +31,12 @@ itcl_class SCIRun_Visualization_ShowField {
 	global $this-def-color-r
 	global $this-def-color-g
 	global $this-def-color-b
-	global $this-scale
+	global $this-node_scale
+	global $this-edge_scale
 	set $this-node_display_type Spheres
-	set $this-scale 0.03
+	set $this-edge_display_type Lines
+	set $this-node_scale 0.03
+	set $this-edge_scale 0.03
 	set $this-def-color-r 0.5
 	set $this-def-color-g 0.5
 	set $this-def-color-b 0.5
@@ -82,7 +85,7 @@ itcl_class SCIRun_Visualization_ShowField {
 	 
 	 frame $frame.colorFrame
 	 frame $frame.colorFrame.col -relief ridge -borderwidth \
-		 4 -height 0.7c -width 0.7c \
+		 4 -height 0.8c -width 1.0c \
 		 -background [format #%04x%04x%04x $ir $ig $ib]
 	 
 	 set cmmd "$this raiseColor $frame.colorFrame.col $color $colMsg"
@@ -90,8 +93,8 @@ itcl_class SCIRun_Visualization_ShowField {
 		 -text "Default Color" -command $cmmd
 	 
 	 #pack the node color frame
-	 pack $frame.colorFrame.set_color $frame.colorFrame.col -side left 
-	 pack $frame.colorFrame -side bottom 
+	 pack $frame.colorFrame.set_color $frame.colorFrame.col -side left
+	 pack $frame.colorFrame -side left
 
     }
 
@@ -107,66 +110,105 @@ itcl_class SCIRun_Visualization_ShowField {
 	frame $window.options
  
 	# node frame holds ui related to vert display (left side)
-	frame $window.options.disp -relief groove -borderwidth 2
+	frame $window.options.disp -borderwidth 2
+	frame $window.options.disp.node -relief groove -borderwidth 2
+	frame $window.options.disp.edge -relief groove -borderwidth 2
+	frame $window.options.disp.face -relief groove -borderwidth 2
+
 	pack $window.options.disp -padx 2 -pady 2 -side left -fill y
+
 	set n "$this-c needexecute"	
 
 	label $window.options.disp.frame_title -text "Display Options"
 
-	checkbutton $window.options.disp.show_nodes \
-		-text "Display Nodes" \
+	checkbutton $window.options.disp.node.show_nodes \
+		-text "Show Nodes" \
 		-command "$this-c toggle_display_nodes" \
 		-variable $this-nodes-on
 	#$window.options.disp.show_nodes select
 
 	global $this-node_display_type
 	set b $this-node_display_type
-	make_labeled_radio $window.options.disp.radio \
+	make_labeled_radio $window.options.disp.node.radio \
 		"Node Display Type" "$this-c node_display_type" top \
 		$this-node_display_type {Spheres Axes Points}
 
 	if {$b == "Spheres"} {
-	    $window.options.disp.radio.0 select
+	    $window.options.disp.node.radio.0 select
 	}
 	if {$b == "Axes"} {
-	    $window.options.disp.radio.1 select
+	    $window.options.disp.node.radio.1 select
 	}
 	if {$b == "Points"} {
-	    $window.options.disp.radio.1 select
+	    $window.options.disp.node.radio.2 select
 	}
 
 
-	checkbutton $window.options.disp.show_edges \
+	checkbutton $window.options.disp.edge.show_edges \
 		-text "Show Edges" \
 		-command "$this-c toggle_display_edges" \
 		-variable $this-edges-on
+
+	global $this-edge_display_type
+	set e $this-edge_display_type
+	make_labeled_radio $window.options.disp.edge.radio \
+		"Edge Display Type" "$this-c edge_display_type" top \
+		$this-edge_display_type {Lines Cylinders}
+
+	if {$e == "Lines"} {
+	    $window.options.disp.node.radio.0 select
+	}
+	if {$e == "Cylinders"} {
+	    $window.options.disp.node.radio.1 select
+	}
+
 	#$window.options.disp.show_edges select
 
-	checkbutton $window.options.disp.show_faces \
+	checkbutton $window.options.disp.face.show_faces \
 		-text "Show Faces" \
 		-command "$this-c toggle_display_faces" \
 		-variable $this-faces-on
 	#$window.options.disp.show_faces select
 
 	#pack the node radio button
-	pack $window.options.disp.frame_title $window.options.disp.show_nodes \
-		$window.options.disp.radio \
-		$window.options.disp.show_edges \
-		$window.options.disp.show_faces \
-		-side top -fill x -padx 2 -pady 2 -anchor w
+	pack $window.options.disp.frame_title -side top
+	pack $window.options.disp.node $window.options.disp.edge \
+		$window.options.disp.face -side left -fill y \
+		-padx 3 -pady 2 -anchor w
 
+	pack $window.options.disp.node.show_nodes \
+		$window.options.disp.node.radio -fill y -anchor w
 	
-	expscale $window.options.disp.slide -label NodeScale \
+	expscale $window.options.disp.node.slide -label NodeScale \
 		-orient horizontal \
-		-variable $this-scale -command "$this-c scale"
+		-variable $this-node_scale -command "$this-c node_scale"
+	frame $window.options.disp.edge.space -height 0.68c 
+	pack $window.options.disp.edge.show_edges \
+		$window.options.disp.edge.radio \
+		$window.options.disp.edge.space \
+		-side top -fill y -anchor w
 
-	addColorSelection $window.options.disp $this-def-color \
-		"default_color_change"
+	expscale $window.options.disp.edge.slide -label CylinderScale \
+		-orient horizontal \
+		-variable $this-edge_scale -command "$this-c edge_scale"
 
-	bind $window.options.disp.slide.scale <ButtonRelease> "$this-c needexecute"
+	pack $window.options.disp.face.show_faces \
+		-side top -fill y -anchor w
+
+
+	bind $window.options.disp.node.slide.scale <ButtonRelease> \
+		"$this-c needexecute"
+	bind $window.options.disp.edge.slide.scale <ButtonRelease> \
+		"$this-c needexecute"
 	#add bottom frame for execute and dismiss buttons
 	frame $window.control -relief groove -borderwidth 2
-	pack $window.options $window.control -padx 2 -pady 2 -side top
+	frame $window.def_col -borderwidth 2
+
+	addColorSelection $window.def_col $this-def-color \
+		"default_color_change"
+
+	pack $window.options $window.def_col $window.control \
+		-padx 2 -pady 2 -side top
 
 	button $window.control.execute -text Execute -command $n
 	button $window.control.dismiss -text Dismiss -command "destroy $window"
