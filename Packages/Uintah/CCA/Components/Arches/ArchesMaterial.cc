87cedf4c8c9f8ef992514abcaded77759e343d01
Steven G. Parker <sparker@sci.utah.edu>
2001-04-03 06:13:41 +0000

    Added placebo archesmaterial until the correct one can get checked in. Added placebo petscsolver so we do not get unresolved symbols Fixed a warning in SmagorinskyModel

---
+++ Packages/Uintah/CCA/Components/Arches/ArchesMaterial.cc
@@ -0,0 +1,17 @@
+
+#include <Packages/Uintah/CCA/Components/Arches/ArchesMaterial.h>
+
+using namespace Uintah;
+
+ArchesMaterial::ArchesMaterial()
+{
+}
+
+ArchesMaterial::~ArchesMaterial()
+{
+}
+
+Burn* ArchesMaterial::getBurnModel()
+{
+  return 0;
+}
