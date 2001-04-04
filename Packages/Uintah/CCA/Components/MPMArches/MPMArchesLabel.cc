44241e45be6442840b66ea854148926445b183c6
Rajesh Rawat <rawat@sci.utah.edu>
2001-04-04 15:44:23 +0000

    modified MPMArches from the old tree written by Kumar to work in the new SCIRun added all the fortran subroutines required by MPMArches

--- Packages/Uintah/CCA/Components/MPMArches/MPMArchesLabel.cc
+++ Packages/Uintah/CCA/Components/MPMArches/MPMArchesLabel.cc
@@ -14,88 +14,374 @@ using namespace Uintah;
 
 MPMArchesLabel::MPMArchesLabel()
 {
-  // Cell centered variables
-  cMassLabel    = scinew VarLabel( "c.mass",
-			CCVariable<double>::getTypeDescription() );
-  cVolumeLabel  = scinew VarLabel( "c.volume",
-			CCVariable<double>::getTypeDescription() );
-  vel_CCLabel   = scinew VarLabel( "vel_CC",
-			CCVariable<Vector>::getTypeDescription() );
+  // Cell centered variables for solid materials
+
+  cMassLabel             = scinew VarLabel( "c.mass",
+					    CCVariable<double>::getTypeDescription() );
+  cVolumeLabel           = scinew VarLabel( "c.volume",
+					    CCVariable<double>::getTypeDescription() );
+  solid_fraction_CCLabel = scinew VarLabel( "solid_fraction",
+					    CCVariable<double>::getTypeDescription() );
+
+  // Cell-centered solid velocity labels
+
+  vel_CCLabel            = scinew VarLabel( "vel_CC",
+					    CCVariable<Vector>::getTypeDescription() );
+  xvel_CCLabel = scinew VarLabel("xvel_CC", 
+				 CCVariable<double>::getTypeDescription() );
+  yvel_CCLabel = scinew VarLabel("yvel_CC", 
+				 CCVariable<double>::getTypeDescription() );
+  zvel_CCLabel = scinew VarLabel("zvel_CC", 
+				 CCVariable<double>::getTypeDescription() );
+
+  // face-centered solid velocity labels
+
+  xvel_FCXLabel = scinew VarLabel("xvel_FCX",
+			     SFCXVariable<double>::getTypeDescription() );
+  xvel_FCYLabel = scinew VarLabel("xvel_FCY",
+			     SFCYVariable<double>::getTypeDescription() );
+  xvel_FCZLabel = scinew VarLabel("xvel_FCZ",
+			     SFCZVariable<double>::getTypeDescription() );
+
+  yvel_FCXLabel = scinew VarLabel("yvel_FCX",
+			     SFCXVariable<double>::getTypeDescription() );
+  yvel_FCYLabel = scinew VarLabel("yvel_FCY",
+			     SFCYVariable<double>::getTypeDescription() );
+  yvel_FCZLabel = scinew VarLabel("yvel_FCZ",
+			     SFCZVariable<double>::getTypeDescription() );
+
+  zvel_FCXLabel = scinew VarLabel("zvel_FCX",
+			     SFCXVariable<double>::getTypeDescription() );
+  zvel_FCYLabel = scinew VarLabel("zvel_FCY",
+			     SFCYVariable<double>::getTypeDescription() );
+  zvel_FCZLabel = scinew VarLabel("zvel_FCZ",
+			     SFCZVariable<double>::getTypeDescription() );
+
+  // labels for solid material calculations:
+
+  // used for temporary computation:
+
+  // CC
+
+  // x-drag momentum acting at cell center
+  DragForceX_CCLabel = scinew VarLabel("DragForceX_CC",
+				 CCVariable<double>::getTypeDescription() );
+  // y-drag momentum acting at cell center
+  DragForceY_CCLabel = scinew VarLabel("DragForceY_CC",
+				 CCVariable<double>::getTypeDescription() );
+  // z-drag momentum acting at cell center
+  DragForceZ_CCLabel = scinew VarLabel("DragForceZ_CC",
+				 CCVariable<double>::getTypeDescription() );
+
+  // FCX
+
+  // y-drag momentum acting on x-face (before redistribution)
+  DragForceY_FCX_BRLabel = scinew VarLabel("DragForceY_FCX_BR",
+				 SFCXVariable<double>::getTypeDescription() );
+  // z-drag momentum acting on x-face (before redistribution)
+  DragForceZ_FCX_BRLabel = scinew VarLabel("DragForceZ_FCX_BR",
+				 SFCXVariable<double>::getTypeDescription() );
+
+  // FCY
+
+  // x-drag momentum acting on y-face (before redistribution)
+  DragForceX_FCY_BRLabel = scinew VarLabel("DragForceX_FCY_BR",
+				 SFCYVariable<double>::getTypeDescription() );
+  // z-drag momentum acting on y-face (before redistribution)
+  DragForceZ_FCY_BRLabel = scinew VarLabel("DragForceZ_FCY_BR",
+				 SFCYVariable<double>::getTypeDescription() );
+
+  // FCZ
+
+  // x-drag momentum acting on z-face (before redistribution)
+  DragForceX_FCZ_BRLabel = scinew VarLabel("DragForceX_FCZ_BR",
+				 SFCZVariable<double>::getTypeDescription() );
+  // y-drag momentum acting on z-face (before redistribution)
+  DragForceY_FCZ_BRLabel = scinew VarLabel("DragForceY_FCZ_BR",
+				 SFCZVariable<double>::getTypeDescription() );
+
+  // Passed to MPM:
+
+  // Drag Forces:
+
+  // FCX
+
+  // x-drag momentum acting on x-face
+  DragForceX_FCXLabel = scinew VarLabel("DragForceX_FCX",
+				 SFCXVariable<double>::getTypeDescription() );
+  // y-drag momentum acting on x-face
+  DragForceY_FCXLabel = scinew VarLabel("DragForceY_FCX",
+				 SFCXVariable<double>::getTypeDescription() );
+  // z-drag momentum acting on x-face
+  DragForceZ_FCXLabel = scinew VarLabel("DragForceZ_FCX",
+				 SFCXVariable<double>::getTypeDescription() );
+
+  // FCY
+
+  // x-drag momentum acting on y-face
+  DragForceX_FCYLabel = scinew VarLabel("DragForceX_FCY",
+				 SFCYVariable<double>::getTypeDescription() );
+  // y-drag momentum acting on y-face
+  DragForceY_FCYLabel = scinew VarLabel("DragForceY_FCY",
+				 SFCYVariable<double>::getTypeDescription() );
+  // z-drag momentum acting on y-face
+  DragForceZ_FCYLabel = scinew VarLabel("DragForceZ_FCY",
+				 SFCYVariable<double>::getTypeDescription() );
+
+  // FCZ
+
+  // x-drag momentum acting on z-face
+  DragForceX_FCZLabel = scinew VarLabel("DragForceX_FCZ",
+				 SFCZVariable<double>::getTypeDescription() );
+  // y-drag momentum acting on z-face
+  DragForceY_FCZLabel = scinew VarLabel("DragForceY_FCZ",
+				 SFCZVariable<double>::getTypeDescription() );
+  // z-drag momentum acting on z-face
+  DragForceZ_FCZLabel = scinew VarLabel("DragForceZ_FCZ",
+				 SFCZVariable<double>::getTypeDescription() );
+
+  // Pressure Forces:
+
+  // pressure force acting on x-face
+  PressureForce_FCXLabel = scinew VarLabel("PressureForce_FCX",
+				 SFCXVariable<double>::getTypeDescription() );
+  // pressure force acting on y-face
+  PressureForce_FCYLabel = scinew VarLabel("PressureForce_FCY",
+				 SFCYVariable<double>::getTypeDescription() );
+  // pressure force acting on z-face
+  PressureForce_FCZLabel = scinew VarLabel("PressureForce_FCZ",
+				 SFCZVariable<double>::getTypeDescription() );
+
+  // labels for gas calculations
+
+  // Gas void fraction
   void_frac_CCLabel  = scinew VarLabel( "void_frac_CC",
 			CCVariable<double>::getTypeDescription() );
 
-  // drag momentum acting on x-face
-  momExDragForceFCXLabel = scinew VarLabel("momExDragForceFCX",
-				 SFCXVariable<Vector>::getTypeDescription() );
-  // pressure force acting on x-face
-  momExPressureForceFCXLabel = scinew VarLabel("momExPressureForceFCX",
-				 SFCXVariable<Vector>::getTypeDescription() );
-  // drag momentum acting on y-face
-  momExDragForceFCYLabel = scinew VarLabel("momExDragForceFCY",
-				 SFCYVariable<Vector>::getTypeDescription() );
-  // pressure force acting on y-face
-  momExPressureForceFCYLabel = scinew VarLabel("momExPressureForceFCY",
-				 SFCYVariable<Vector>::getTypeDescription() );
-  // drag momentum acting on z-face
-  momExDragForceFCZLabel = scinew VarLabel("momExDragForceFCZ",
-				 SFCZVariable<Vector>::getTypeDescription() );
-  // pressure force acting on z-face
-  momExPressureForceFCZLabel = scinew VarLabel("momExPressureForceFCZ",
-				 SFCZVariable<Vector>::getTypeDescription() );
+  // u-velocity source term labels
 
-  // U-Velocity Linear Src Labels from multi-material
+
+  // U-Velocity Linear Src Labels from multi-material (CC)
+  // used for temporary computation
+  d_uVel_mmLinSrc_CCLabel = scinew VarLabel("uVel_mmLinSrc_CC",
+				      CCVariable<double>::getTypeDescription() );
+  // U-Velocity Linear Src Labels from multi-material (CC) (Collected)
+  // used for temporary computation
+  d_uVel_mmLinSrc_CC_CollectLabel = scinew VarLabel("uVel_mmLinSrc_CC_Collect",
+				      CCVariable<double>::getTypeDescription() );
+  // U-Velocity Linear Src Labels from multi-material (FCX)
+  // used in actual summation of source
   d_uVel_mmLinSrcLabel = scinew VarLabel("uVel_mmLinSrc",
 				      SFCXVariable<double>::getTypeDescription() );
-  // U-Velocity Non Linear Src Labels from multi-material
+  // U-Velocity Linear Src Labels from multi-material (FCY)
+  // used for temporary computation
+  d_uVel_mmLinSrc_FCYLabel = scinew VarLabel("uVel_mmLinSrc_FCY",
+				      SFCYVariable<double>::getTypeDescription() );
+  // U-Velocity Linear Src Labels from multi-material (FCZ)
+  // used for temporary computation
+  d_uVel_mmLinSrc_FCZLabel = scinew VarLabel("uVel_mmLinSrc_FCZ",
+				      SFCZVariable<double>::getTypeDescription() );
+
+
+  // U-Velocity Non Linear Src Labels from multi-material (CC)
+  // used for temporary computation
+  d_uVel_mmNonlinSrc_CCLabel = scinew VarLabel("uVel_mmNonlinSrc_CC",
+				    CCVariable<double>::getTypeDescription() );
+  // U-Velocity Non Linear Src Labels from multi-material (CC) (Collected)
+  // used for temporary computation
+  d_uVel_mmNonlinSrc_CC_CollectLabel = scinew VarLabel("uVel_mmNonlinSrc_CC_Collect",
+				    CCVariable<double>::getTypeDescription() );
+  // U-Velocity Non Linear Src Labels from multi-material (FCX)
+  // used in actual summation of source
   d_uVel_mmNonlinSrcLabel = scinew VarLabel("uVel_mmNonlinSrc",
 				    SFCXVariable<double>::getTypeDescription() );
-  // V-Velocity Linear Src Labels from multi-material
-  d_vVel_mmLinSrcLabel = scinew VarLabel("vVel_mmLinSrc",
-				      SFCYVariable<double>::getTypeDescription() );
-  // V-Velocity Non Linear Src Labels from multi-material
-  d_vVel_mmNonlinSrcLabel = scinew VarLabel("vVel_mmNonlinSrc",
+  // U-Velocity Non Linear Src Labels from multi-material (FCY)
+  // used for temporary computation
+  d_uVel_mmNonlinSrc_FCYLabel = scinew VarLabel("uVel_mmNonlinSrc_FCY",
 				    SFCYVariable<double>::getTypeDescription() );
-  // W-Velocity Linear Src Labels from multi-material
-  d_wVel_mmLinSrcLabel = scinew VarLabel("wVel_mmLinSrc",
-				      SFCZVariable<double>::getTypeDescription() );
-  // W-Velocity Non Linear Src Labels from multi-material
-  d_wVel_mmNonlinSrcLabel = scinew VarLabel("wVel_mmNonlinSrc",
+  // U-Velocity Non Linear Src Labels from multi-material (FCZ)
+  // used for temporary computation
+  d_uVel_mmNonlinSrc_FCZLabel = scinew VarLabel("uVel_mmNonlinSrc_FCZ",
 				    SFCZVariable<double>::getTypeDescription() );
 
-   // produced by scheduleInterpolateCCToFC
-   // if these are duplicates of some of the above, feel free to
-   // straighten this out
-   d_xVelFCLabel = scinew VarLabel("xVelFC",
-				SFCXVariable<double>::getTypeDescription() );
+  // v-velocity source term labels
 
-   d_yVelFCLabel = scinew VarLabel("yVelFC",
-                                SFCYVariable<double>::getTypeDescription() );
 
-   d_zVelFCLabel = scinew VarLabel("zVelFC",
-                                SFCZVariable<double>::getTypeDescription() );
+  // V-Velocity Linear Src Labels from multi-material (CC)
+  // used for temporary computation
+  d_vVel_mmLinSrc_CCLabel = scinew VarLabel("vVel_mmLinSrc_CC",
+				      CCVariable<double>::getTypeDescription() );
+  // V-Velocity Linear Src Labels from multi-material (CC) (Collected)
+  // used for temporary computation
+  d_vVel_mmLinSrc_CC_CollectLabel = scinew VarLabel("vVel_mmLinSrc_CC_Collect",
+				      CCVariable<double>::getTypeDescription() );
+  // V-Velocity Linear Src Labels from multi-material (FCY)
+  // used in actual summation of source
+  d_vVel_mmLinSrcLabel = scinew VarLabel("vVel_mmLinSrc",
+				      SFCYVariable<double>::getTypeDescription() );
+  // V-Velocity Linear Src Labels from multi-material (FCZ)
+  // used for temporary computation
+  d_vVel_mmLinSrc_FCZLabel = scinew VarLabel("vVel_mmLinSrc_FCZ",
+				      SFCZVariable<double>::getTypeDescription() );
+  // V-Velocity Linear Src Labels from multi-material (FCX)
+  // used for temporary computation
+  d_vVel_mmLinSrc_FCXLabel = scinew VarLabel("vVel_mmLinSrc_FCX",
+				      SFCXVariable<double>::getTypeDescription() );
+
+
+  // V-Velocity Non Linear Src Labels from multi-material (CC)
+  // used for temporary computation
+  d_vVel_mmNonlinSrc_CCLabel = scinew VarLabel("vVel_mmNonlinSrc_CC",
+				    CCVariable<double>::getTypeDescription() );
+  // V-Velocity Non Linear Src Labels from multi-material (CC) (Collected)
+  // used for temporary computation
+  d_vVel_mmNonlinSrc_CC_CollectLabel = scinew VarLabel("vVel_mmNonlinSrc_CC_Collect",
+				    CCVariable<double>::getTypeDescription() );
+  // V-Velocity Non Linear Src Labels from multi-material (FCY)
+  // used in actual summation of source
+  d_vVel_mmNonlinSrcLabel = scinew VarLabel("vVel_mmNonlinSrc",
+				    SFCYVariable<double>::getTypeDescription() );
+  // V-Velocity Non Linear Src Labels from multi-material (FCZ)
+  // used for temporary computation
+  d_vVel_mmNonlinSrc_FCZLabel = scinew VarLabel("vVel_mmNonlinSrc_FCZ",
+				    SFCZVariable<double>::getTypeDescription() );
+  // V-Velocity Non Linear Src Labels from multi-material (FCX)
+  // used for temporary computation
+  d_vVel_mmNonlinSrc_FCXLabel = scinew VarLabel("vVel_mmNonlinSrc_FCX",
+				    SFCXVariable<double>::getTypeDescription() );
+
+  // w-velocity source term labels
+
+
+  // W-Velocity Linear Src Labels from multi-material (CC)
+  // used for temporary computation
+  d_wVel_mmLinSrc_CCLabel = scinew VarLabel("wVel_mmLinSrc_CC",
+				      CCVariable<double>::getTypeDescription() );
+  // W-Velocity Linear Src Labels from multi-material (CC) (Collected)
+  // used for temporary computation
+  d_wVel_mmLinSrc_CC_CollectLabel = scinew VarLabel("wVel_mmLinSrc_CC_Collect",
+				      CCVariable<double>::getTypeDescription() );
+  // W-Velocity Linear Src Labels from multi-material (FCZ)
+  // used in actual summation of source
+  d_wVel_mmLinSrcLabel = scinew VarLabel("wVel_mmLinSrc",
+				      SFCZVariable<double>::getTypeDescription() );
+  // W-Velocity Linear Src Labels from multi-material (FCX)
+  // used for temporary computation
+  d_wVel_mmLinSrc_FCXLabel = scinew VarLabel("wVel_mmLinSrc_FCX",
+				      SFCXVariable<double>::getTypeDescription() );
+  // W-Velocity Linear Src Labels from multi-material (FCY)
+  // used for temporary computation
+  d_wVel_mmLinSrc_FCYLabel = scinew VarLabel("wVel_mmLinSrc_FCY",
+				      SFCYVariable<double>::getTypeDescription() );
+
+
+  // W-Velocity Non Linear Src Labels from multi-material (CC)
+  // used for temporary computation
+  d_wVel_mmNonlinSrc_CCLabel = scinew VarLabel("wVel_mmNonlinSrc_CC",
+				    CCVariable<double>::getTypeDescription() );
+  // W-Velocity Non Linear Src Labels from multi-material (CC) (Collected)
+  // used for temporary computation
+  d_wVel_mmNonlinSrc_CC_CollectLabel = scinew VarLabel("wVel_mmNonlinSrc_CC_Collect",
+				    CCVariable<double>::getTypeDescription() );
+  // W-Velocity Non Linear Src Labels from multi-material (FCZ)
+  // used in actual summation of source
+  d_wVel_mmNonlinSrcLabel = scinew VarLabel("wVel_mmNonlinSrc",
+				    SFCZVariable<double>::getTypeDescription() );
+  // W-Velocity Non Linear Src Labels from multi-material (FCX)
+  // used for temporary computation
+  d_wVel_mmNonlinSrc_FCXLabel = scinew VarLabel("wVel_mmNonlinSrc_FCX",
+				    SFCXVariable<double>::getTypeDescription() );
+  // W-Velocity Non Linear Src Labels from multi-material (FCY)
+  // used for temporary computation
+  d_wVel_mmNonlinSrc_FCYLabel = scinew VarLabel("wVel_mmNonlinSrc_FCY",
+				    SFCYVariable<double>::getTypeDescription() );
 
 } 
 
 MPMArchesLabel::~MPMArchesLabel()
 {
-  delete void_frac_CCLabel;
   delete cMassLabel;
   delete cVolumeLabel;
-  delete vel_CCLabel;
-  delete momExDragForceFCXLabel;
-  delete momExPressureForceFCXLabel;
-  delete momExDragForceFCYLabel;
-  delete momExPressureForceFCYLabel;
-  delete momExDragForceFCZLabel;
-  delete momExPressureForceFCZLabel;
-  delete d_uVel_mmLinSrcLabel;
-  delete d_uVel_mmNonlinSrcLabel;
-  delete d_vVel_mmLinSrcLabel;
-  delete d_vVel_mmNonlinSrcLabel;
-  delete d_wVel_mmLinSrcLabel;
-  delete d_wVel_mmNonlinSrcLabel;
+  delete solid_fraction_CCLabel;
 
-  delete d_xVelFCLabel;
-  delete d_yVelFCLabel;
-  delete d_zVelFCLabel;
+  delete vel_CCLabel;
+  delete xvel_CCLabel;
+  delete yvel_CCLabel;
+  delete zvel_CCLabel;
+
+  delete xvel_FCXLabel;
+  delete xvel_FCYLabel;
+  delete xvel_FCZLabel;
+
+  delete yvel_FCXLabel;
+  delete yvel_FCYLabel;
+  delete yvel_FCZLabel;
+
+  delete zvel_FCXLabel;
+  delete zvel_FCYLabel;
+  delete zvel_FCZLabel;
+
+  delete DragForceX_CCLabel;
+  delete DragForceY_CCLabel;
+  delete DragForceZ_CCLabel;
+
+  delete DragForceY_FCX_BRLabel;
+  delete DragForceZ_FCX_BRLabel;
+
+  delete DragForceX_FCY_BRLabel;
+  delete DragForceZ_FCY_BRLabel;
+
+  delete DragForceX_FCZ_BRLabel;
+  delete DragForceY_FCZ_BRLabel;
+
+  delete DragForceX_FCXLabel;
+  delete DragForceY_FCXLabel;
+  delete DragForceZ_FCXLabel;
+
+  delete DragForceX_FCYLabel;
+  delete DragForceY_FCYLabel;
+  delete DragForceZ_FCYLabel;
+
+  delete DragForceX_FCZLabel;
+  delete DragForceY_FCZLabel;
+  delete DragForceZ_FCZLabel;
+
+  delete PressureForce_FCXLabel;
+  delete PressureForce_FCYLabel;
+  delete PressureForce_FCZLabel;
+
+  delete void_frac_CCLabel;
+
+  delete d_uVel_mmLinSrc_CCLabel;
+  delete d_uVel_mmLinSrc_CC_CollectLabel;
+  delete d_uVel_mmLinSrcLabel;
+  delete d_uVel_mmLinSrc_FCYLabel;
+  delete d_uVel_mmLinSrc_FCZLabel;
+  delete d_uVel_mmNonlinSrc_CCLabel;
+  delete d_uVel_mmNonlinSrc_CC_CollectLabel;
+  delete d_uVel_mmNonlinSrcLabel;
+  delete d_uVel_mmNonlinSrc_FCYLabel;
+  delete d_uVel_mmNonlinSrc_FCZLabel;
+
+  delete d_vVel_mmLinSrc_CCLabel;
+  delete d_vVel_mmLinSrc_CC_CollectLabel;
+  delete d_vVel_mmLinSrcLabel;
+  delete d_vVel_mmLinSrc_FCZLabel;
+  delete d_vVel_mmLinSrc_FCXLabel;
+  delete d_vVel_mmNonlinSrc_CCLabel;
+  delete d_vVel_mmNonlinSrc_CC_CollectLabel;
+  delete d_vVel_mmNonlinSrcLabel;
+  delete d_vVel_mmNonlinSrc_FCZLabel;
+  delete d_vVel_mmNonlinSrc_FCXLabel;
+
+  delete d_wVel_mmLinSrc_CCLabel;
+  delete d_wVel_mmLinSrc_CC_CollectLabel;
+  delete d_wVel_mmLinSrcLabel;
+  delete d_wVel_mmLinSrc_FCXLabel;
+  delete d_wVel_mmLinSrc_FCYLabel;
+  delete d_wVel_mmNonlinSrc_CCLabel;
+  delete d_wVel_mmNonlinSrc_CC_CollectLabel;
+  delete d_wVel_mmNonlinSrcLabel;
+  delete d_wVel_mmNonlinSrc_FCXLabel;
+  delete d_wVel_mmNonlinSrc_FCYLabel;
 }
--- Packages/Uintah/CCA/Components/MPMArches/MPMArchesLabel.h
+++ Packages/Uintah/CCA/Components/MPMArches/MPMArchesLabel.h
@@ -13,30 +13,106 @@ namespace Uintah {
       MPMArchesLabel();
       ~MPMArchesLabel();
 
-      const VarLabel* void_frac_CCLabel; // reqd by Arches
       const VarLabel* cMassLabel;
       const VarLabel* cVolumeLabel;
+      const VarLabel* solid_fraction_CCLabel;
+
+      // velocity labels for solid materials
+
       const VarLabel* vel_CCLabel;
+      const VarLabel* xvel_CCLabel;
+      const VarLabel* yvel_CCLabel;
+      const VarLabel* zvel_CCLabel;
+
+      const VarLabel* xvel_FCXLabel;
+      const VarLabel* xvel_FCYLabel;
+      const VarLabel* xvel_FCZLabel;
+
+      const VarLabel* yvel_FCXLabel;
+      const VarLabel* yvel_FCYLabel;
+      const VarLabel* yvel_FCZLabel;
+
+      const VarLabel* zvel_FCXLabel;
+      const VarLabel* zvel_FCYLabel;
+      const VarLabel* zvel_FCZLabel;
+
       // reqd by MPM
-      const VarLabel* momExDragForceFCXLabel; 
-      const VarLabel* momExDragForceFCYLabel; 
-      const VarLabel* momExDragForceFCZLabel; 
-      const VarLabel* momExPressureForceFCXLabel; 
-      const VarLabel* momExPressureForceFCYLabel; 
-      const VarLabel* momExPressureForceFCZLabel; 
-      // reqd by momentum eqns for Arches
+
+      const VarLabel* DragForceX_CCLabel;
+      const VarLabel* DragForceY_CCLabel;
+      const VarLabel* DragForceZ_CCLabel;
+
+      const VarLabel* DragForceY_FCX_BRLabel; 
+      const VarLabel* DragForceZ_FCX_BRLabel; 
+
+      const VarLabel* DragForceX_FCY_BRLabel; 
+      const VarLabel* DragForceZ_FCY_BRLabel; 
+
+      const VarLabel* DragForceX_FCZ_BRLabel; 
+      const VarLabel* DragForceY_FCZ_BRLabel; 
+
+      const VarLabel* DragForceX_FCXLabel; 
+      const VarLabel* DragForceY_FCXLabel; 
+      const VarLabel* DragForceZ_FCXLabel; 
+
+      const VarLabel* DragForceX_FCYLabel; 
+      const VarLabel* DragForceY_FCYLabel; 
+      const VarLabel* DragForceZ_FCYLabel; 
+
+      const VarLabel* DragForceX_FCZLabel; 
+      const VarLabel* DragForceY_FCZLabel; 
+      const VarLabel* DragForceZ_FCZLabel; 
+
+      const VarLabel* PressureForce_FCXLabel; 
+      const VarLabel* PressureForce_FCYLabel; 
+      const VarLabel* PressureForce_FCZLabel; 
+
+      // reqd by Arches
+
+      const VarLabel* void_frac_CCLabel;
+
+      // reqd by momentum eqns:
+      // u-momentum equation source labels
+
+      const VarLabel* d_uVel_mmLinSrc_CCLabel;
+      const VarLabel* d_uVel_mmLinSrc_CC_CollectLabel;
       const VarLabel* d_uVel_mmLinSrcLabel;
+      const VarLabel* d_uVel_mmLinSrc_FCYLabel;
+      const VarLabel* d_uVel_mmLinSrc_FCZLabel;
+
+      const VarLabel* d_uVel_mmNonlinSrc_CCLabel;
+      const VarLabel* d_uVel_mmNonlinSrc_CC_CollectLabel;
       const VarLabel* d_uVel_mmNonlinSrcLabel;
+      const VarLabel* d_uVel_mmNonlinSrc_FCYLabel;
+      const VarLabel* d_uVel_mmNonlinSrc_FCZLabel;
+
+      // v-momentum equation source labels
+
+      const VarLabel* d_vVel_mmLinSrc_CCLabel;
+      const VarLabel* d_vVel_mmLinSrc_CC_CollectLabel;
       const VarLabel* d_vVel_mmLinSrcLabel;
+      const VarLabel* d_vVel_mmLinSrc_FCZLabel;
+      const VarLabel* d_vVel_mmLinSrc_FCXLabel;
+
+      const VarLabel* d_vVel_mmNonlinSrc_CCLabel;
+      const VarLabel* d_vVel_mmNonlinSrc_CC_CollectLabel;
       const VarLabel* d_vVel_mmNonlinSrcLabel;
+      const VarLabel* d_vVel_mmNonlinSrc_FCZLabel;
+      const VarLabel* d_vVel_mmNonlinSrc_FCXLabel;
+
+      // w-momentum equation source labels
+
+      const VarLabel* d_wVel_mmLinSrc_CCLabel;
+      const VarLabel* d_wVel_mmLinSrc_CC_CollectLabel;
       const VarLabel* d_wVel_mmLinSrcLabel;
+      const VarLabel* d_wVel_mmLinSrc_FCXLabel;
+      const VarLabel* d_wVel_mmLinSrc_FCYLabel;
+
+      const VarLabel* d_wVel_mmNonlinSrc_CCLabel;
+      const VarLabel* d_wVel_mmNonlinSrc_CC_CollectLabel;
       const VarLabel* d_wVel_mmNonlinSrcLabel;
-      // produced by scheduleInterpolateCCToFC
-      // if these are duplicates of some of the above, feel free to
-      // straighten this out
-      const VarLabel* d_xVelFCLabel;
-      const VarLabel* d_yVelFCLabel;
-      const VarLabel* d_zVelFCLabel;
+      const VarLabel* d_wVel_mmNonlinSrc_FCXLabel;
+      const VarLabel* d_wVel_mmNonlinSrc_FCYLabel;
     };
 
 } // end namespace Uintah
