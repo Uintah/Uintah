2485670b21be29441dca5d126516b2935c981f29
Wayne Witzel <witzel@sci.utah.edu>
2001-04-06 00:09:18 +0000

    MaterialSet Handle

---
+++ src/Packages/Uintah/Core/Grid/MaterialSetP.h
@@ -0,0 +1,12 @@
+#ifndef UINTAH_HOMEBREW_MaterialSetP_H
+#define UINTAH_HOMEBREW_MaterialSetP_H
+
+#include <Packages/Uintah/Core/ProblemSpec/Handle.h>
+#include <Packages/Uintah/Core/Grid/ComputeSet.h>
+
+namespace Uintah {
+  typedef Handle<MaterialSet> MaterialSetP;
+}
+
+#endif
+
