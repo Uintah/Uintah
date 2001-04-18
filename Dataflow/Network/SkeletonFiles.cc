f9b1e82e64824a7cdafa67d5c7fec0d7b06a7df0
Chris Moulding <moulding@sci.utah.edu>
2001-04-18 17:39:45 +0000

    add / between $(OBJTOP) and createTclIndex

--- Dataflow/Network/SkeletonFiles.cc
+++ Dataflow/Network/SkeletonFiles.cc
@@ -295,7 +295,7 @@ char gui_submk_skeleton[] = \
 "\n"
 "$(SRCDIR)/tclIndex: \\\n"
 "#[INSERT NEW TCL FILE HERE]\n"
-"\t$(OBJTOP)createTclIndex "
+"\t$(OBJTOP)/createTclIndex "
 "$(SRCTOP)/Packages/%s/Dataflow/GUI\n" /* package name */
 "\n"
 "CLEANPROGS := $(CLEANPROGS) $(SRCDIR)/tclIndex\n"
