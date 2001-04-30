90bf8e90d8e0ef4003a3a4305c3c71cbc1269313
Wayne Witzel <witzel@sci.utah.edu>
2001-04-30 17:15:15 +0000

    Let numerical comparisons pass if they are within some absolute error or within some relative error.  It only used relative errors before.

--- Test/helpers/compare_dat_files.cc
+++ Test/helpers/compare_dat_files.cc
@@ -11,20 +11,20 @@ int main(int argc, char** argv)
   int i;
 
   if (argc < 5) {
-    cerr << "usage: " << argv[0] << " {rel error allowed} {uda directory 1} {uda directory 2} {dat file names}\n";
+    cerr << "usage: " << argv[0] << " {abs error allowed} {rel error allowed} {uda directory 1} {uda directory 2} {dat file names}\n";
     return -10;
   }
 
   bool failed = false;
   char datfilename[500];
 
-  for (int argv_index = 4; argv_index < argc; argv_index++) {
+  for (int argv_index = 5; argv_index < argc; argv_index++) {
     cout << "Comparing " << argv[argv_index] << "... ";
 
     ifstream datfile[2];
     for (i = 0; i < 2; i++) {
       *datfilename = '\0';
-      strcpy(datfilename, argv[i+2]);
+      strcpy(datfilename, argv[i+3]);
       strcat(datfilename, argv[argv_index]);
       datfile[i].open(datfilename);
       if (datfile[i].fail()) {
@@ -33,7 +33,8 @@ int main(int argc, char** argv)
       }
     }
     
-    double allowable_rel_error = atof(argv[1]);
+    double allowable_abs_error = atof(argv[1]);
+    double allowable_rel_error = atof(argv[2]);
     
     double d[2];
     double max_abs;
@@ -45,11 +46,14 @@ int main(int argc, char** argv)
       if (fabs(d[1]) > max_abs)
         d[1] = fabs(d[1]);
 
-      if (max_abs > 0) {
-        double rel_error = fabs(d[1] - d[0]) / max_abs;
-        if (rel_error > greatest_rel_error) {
-          greatest_rel_error = rel_error;
-        }
+      if (max_abs > 0) // otherwise, both are zero
+      {
+	if (fabs(d[1] - d[0]) > allowable_abs_error) {
+	  double rel_error = fabs(d[1] - d[0]) / max_abs;
+	  if (rel_error > greatest_rel_error) {
+	    greatest_rel_error = rel_error;
+	  }
+	}
       }
     }
     
