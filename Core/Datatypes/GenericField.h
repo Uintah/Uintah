18cae0ad69c23997f8b115afd11926da0d668696
Chris Moulding <moulding@sci.utah.edu>
2001-05-19 00:26:04 +0000

    fix SGI compile

--- Core/Datatypes/GenericField.h
+++ Core/Datatypes/GenericField.h
@@ -186,7 +186,7 @@ const string GenericField<Mesh, FData>::type_name(int n)
 template <class Mesh, class FData>
 void GenericField<Mesh, FData>::io(Piostream& stream)
 {
-  stream.begin_class(type_name(), GENERICFIELD_VERSION);
+  stream.begin_class(type_name(-1), GENERICFIELD_VERSION);
   Field::io(stream);
   mesh_->io(stream);
   Pio(stream, fdata_);
