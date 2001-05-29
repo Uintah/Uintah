cf79538f554d0d6b609246f0964a6895c722e2f6
Chris Moulding <moulding@sci.utah.edu>
2001-05-29 23:26:13 +0000

    more progress

--- Dataflow/Modules/Fields/EditField.cc
+++ Dataflow/Modules/Fields/EditField.cc
@@ -25,15 +25,15 @@ using std::pair;
 
 class PSECORESHARE EditField : public Module {
 public:
-  GuiString name_;
-  GuiString bboxmin_;
-  GuiString bboxmax_;
+  GuiString fldname_;
   GuiString typename_;
   GuiString datamin_;
   GuiString datamax_;
   GuiString numnodes_;
   GuiString numelems_;
   GuiString dataat_;
+  GuiDouble minx_,miny_,minz_;
+  GuiDouble maxx_,maxy_,maxz_;
 
   EditField(const string& id);
 
@@ -54,15 +54,19 @@ extern "C" PSECORESHARE Module* make_EditField(const string& id) {
 
 EditField::EditField(const string& id)
   : Module("EditField", id, Source, "Fields", "SCIRun"),
-    name_("name", id, this),
-    bboxmin_("bboxmin", id, this),
-    bboxmax_("bboxmax", id, this),
-    typename_("typename", id, this),
-    datamin_("datamin", id, this),
-    datamax_("datamax", id, this),
-    numnodes_("numnodes", id, this),
-    numelems_("numelems", id, this),
-    dataat_("dataat", id, this)
+    fldname_("fldname2", id, this),
+    typename_("typename2", id, this),
+    datamin_("datamin2", id, this),
+    datamax_("datamax2", id, this),
+    numnodes_("numnodes2", id, this),
+    numelems_("numelems2", id, this),
+    dataat_("dataat2", id, this),
+    minx_("minx2", id, this),
+    miny_("miny2", id, this),
+    minz_("minz2", id, this),
+    maxx_("maxx2", id, this),
+    maxy_("maxy2", id, this),
+    maxz_("maxz2", id, this)
 {
 }
 
@@ -87,7 +91,7 @@ EditField::set_nums(Field *f) {
     count++;++ni;
   }
 
-  numnodes_.set(to_string(count));
+  TCL::execute(string("set ")+id+"-numnodes "+to_string(count));
   count = 0;
 
   elem_iter ei = mesh->tbegin((elem_iter*)0);
@@ -95,14 +99,12 @@ EditField::set_nums(Field *f) {
     count++;++ei;
   }
 
-  numelems_.set(to_string(count));
+  TCL::execute(string("set ")+id+"-numelems "+to_string(count));
 }
 
 void EditField::clear_vals() 
 {
-  name_.set("---");
-  bboxmin_.set("---");
-  bboxmax_.set("---");
+  fldname_.set("---");
   typename_.set("---");
   datamin_.set("---");
   datamax_.set("---");
@@ -113,23 +115,31 @@ void EditField::clear_vals()
 }
 
 void EditField::execute(){
-  FieldIPort *iport = (FieldIPort*)get_iport(0);
+  FieldIPort *iport=0; 
   FieldHandle fh;
   Field *f=0;
-  if (!iport || !iport->get(fh) || !(f=fh.get_rep())) {
+  if (!(iport=(FieldIPort*)get_iport(0)) || 
+      !iport->get(fh) || 
+      !(f=fh.get_rep())) {
     clear_vals();
     return;
   }
 
   string tname(f->get_type_name(-1));
 
-  typename_.set(tname);
+  TCL::execute(string("set ")+id+"-typename "+tname);
+
   switch(f->data_at()) {
-  case Field::CELL: dataat_.set("Field::CELL"); break;
-  case Field::FACE: dataat_.set("Field::FACE"); break;
-  case Field::EDGE: dataat_.set("Field::EDGE"); break;
-  case Field::NODE: dataat_.set("Field::NODE"); break; 
-  case Field::NONE: dataat_.set("Field::NONE"); break;
+  case Field::CELL: 
+    TCL::execute(string("set ")+id+"-dataat Field::CELL"); break;
+  case Field::FACE: 
+    TCL::execute(string("set ")+id+"-dataat Field::FACE"); break;
+  case Field::EDGE: 
+    TCL::execute(string("set ")+id+"-dataat Field::EDGE"); break;
+  case Field::NODE: 
+    TCL::execute(string("set ")+id+"-dataat Field::NODE"); break;
+  case Field::NONE: 
+    TCL::execute(string("set ")+id+"-dataat Field::NONE"); break;
   }
 
   if (tname=="TetVol<double>") {
@@ -142,7 +152,7 @@ void EditField::execute(){
     set_nums((LatticeVol<Vector>*)f);
   } else if (tname=="TriSurf<double>") {
     set_nums((TriSurf<double>*)f);
-  } else if (tname=="TirSurf<Vector>") {
+  } else if (tname=="TriSurf<Vector>") {
     set_nums((TriSurf<Vector>*)f);
   }
 
@@ -150,37 +160,31 @@ void EditField::execute(){
   Point min = bbox.min();
   Point max = bbox.max();
 
-  string bbmin("( ");
-  string bbmax("( ");
-  bbmin += to_string(min.x())+", "+
-           to_string(min.y())+", "+
-           to_string(min.z())+" ) ";
-  bbmax += to_string(max.x())+", "+
-           to_string(max.y())+", "+
-           to_string(max.z())+" ) ";
-
-  bboxmin_.set(bbmin);
-  bboxmax_.set(bbmax);
+  TCL::execute(string("set ")+id+"-minx "+to_string(min.x()));
+  TCL::execute(string("set ")+id+"-miny "+to_string(min.y()));
+  TCL::execute(string("set ")+id+"-minz "+to_string(min.z()));
+  TCL::execute(string("set ")+id+"-maxx "+to_string(max.x()));
+  TCL::execute(string("set ")+id+"-maxy "+to_string(max.y()));
+  TCL::execute(string("set ")+id+"-maxz "+to_string(max.z()));
 
   pair<double,double> minmax(1,0);
   if (f->get("minmax",minmax)) {
-    datamin_.set(to_string(minmax.first));
-    datamax_.set(to_string(minmax.second));
+    TCL::execute(string("set ")+id+"-datamin "+to_string(minmax.first));
+    TCL::execute(string("set ")+id+"-datamax "+to_string(minmax.second));
   } else {
-    datamin_.set("<Not Applicable>");
-    datamax_.set("<Not Applicable>");
+    TCL::execute(string("set ")+id+"-datamin \"--- Not Applicable ---\"");
+    TCL::execute(string("set ")+id+"-datamax \"--- Not Applicable ---\"");
   }
-      
+  
   string fldname;
   if (f->get("name",fldname))
-    name_.set(fldname);
+    TCL::execute(string("set ")+id+"-fldname "+fldname);
   else
-    name_.set("<No Name Assigned>");
-
-
+    TCL::execute(string("set ")+id+"-fldname \"--- Name Not Assigned ---\"");
+  
   TCL::execute(id + " update_attributes");    
 }
-
+    
 void EditField::tcl_command(TCLArgs& args, void* userdata)
 {
   Module::tcl_command(args, userdata);
