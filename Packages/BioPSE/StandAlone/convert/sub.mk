e725f7bf213983eb7d3ac05f18cf307f7a452ff2
David Weinstein <dmw@sci.utah.edu>
2001-06-18 08:19:04 +0000

    added a converter for EGI

---
+++ Packages/BioPSE/StandAlone/convert/sub.mk
@@ -0,0 +1,14 @@
+# Makefile fragment for this subdirectory
+
+SRCDIR := Packages/BioPSE/StandAlone/convert
+
+ifeq ($(LARGESOS),yes)
+PSELIBS := Packages/BioPSE/StandAlone/convert
+else
+PSELIBS := Core/Datatypes Core/Containers Core/Persistent Core/Exceptions Core/Thread Core/Geometry
+endif
+LIBS := -lm
+
+PROGRAM := $(SRCDIR)/EGItoMat
+SRCS := $(SRCDIR)/EGItoMat.cc
+include $(SRCTOP)/scripts/program.mk
