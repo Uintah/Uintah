0c4b3d77cc0a955516325f3064abd86dc120c938
Todd Harman <harman@sci.utah.edu>
2001-07-02 23:14:53 +0000

    fillFace() and fillFaceFlux      -passing in patch      -Using different logic to get upper and lower patch indices.       We need all the faces inside the computational domain       for both single and multi-patch problems.  Previously, an offset was       passed in but that doesn't work for multipatch problems.

--- src/Packages/Uintah/Core/Grid/SFCZVariable.h
+++ src/Packages/Uintah/Core/Grid/SFCZVariable.h
@@ -104,18 +104,26 @@ WARNING
     }
 
     // Replace the values on the indicated face with value
-    void fillFace(Patch::FaceType face, const T& value,
-		  IntVector offset = IntVector(0,0,0))
+    void fillFace(const Patch* patch, Patch::FaceType face,
+                  const T& value, IntVector offset = IntVector(0,0,0))
     { 
-      IntVector low,hi;
-      low = getLowIndex() + offset;
-      hi = getHighIndex() - offset;
       //__________________________________
-      //  Note that getHighIndex returns 
-      //  IntVector (ncells+gc, ncells+gc, ncells+gc+1] 
-      //cout<< "fillFace: SCZVariable.h"<<endl;
-      //cout<< "low: "<<low<<endl;
-      //cout<< "hi: "<<hi<<endl;
+      // Add (0,0,1) to low index when no 
+      // neighbor patches are present
+      IntVector low,hi; 
+      int numGC = 0;
+      low = patch->getCellLowIndex();
+      low+=IntVector(patch->getBCType(Patch::xminus)==Patch::Neighbor?numGC:0,
+		       patch->getBCType(Patch::yminus)==Patch::Neighbor?numGC:0,
+		       patch->getBCType(Patch::zminus)==Patch::Neighbor?numGC:1);
+      hi  = patch->getCellHighIndex();
+      hi +=IntVector(patch->getBCType(Patch::xplus) ==Patch::Neighbor?numGC:0,
+		       patch->getBCType(Patch::yplus) ==Patch::Neighbor?numGC:0,
+		       patch->getBCType(Patch::zplus) ==Patch::Neighbor?numGC:0);
+      // cout<< "fillFace: SFCZVariable.h"<<endl;
+      // cout<< "low: "<<low<<" Low + offset: "<<low + offset<<endl;
+      // cout<< "hi:  "<<hi <<" hi  - offset: "<<hi -offset<<endl;
+
       switch (face) {
       case Patch::xplus:
 	for (int j = low.y(); j<hi.y(); j++) {
@@ -168,18 +176,27 @@ WARNING
     };
 
     // Set the Neumann BC condition using a 1st order approximation
-    void fillFaceFlux(Patch::FaceType face, const T& value, const Vector& dx,
-		      IntVector offset = IntVector(0,0,0))
+    void fillFaceFlux(const Patch* patch, Patch::FaceType face, 
+                      const T& value, const Vector& dx,
+		        IntVector offset = IntVector(0,0,0))
     { 
-      IntVector low,hi;
-      low = getLowIndex() + offset;
-      hi = getHighIndex() - offset;
       //__________________________________
-      //  Note that getHighIndex returns 
-      //  IntVector (ncells+gc, ncells+gc, ncells+gc+1] 
-      //cout<< "fillFaceFlux: SCZVariable.h"<<endl;
-      //cout<< "low: "<<low<<endl;
-      //cout<< "hi: "<<hi<<endl;
+      // Add (0,0,1) to low index when no 
+      // neighbor patches are present 
+      IntVector low,hi; 
+      int numGC = 0;
+      low = patch->getCellLowIndex();
+      low+=IntVector(patch->getBCType(Patch::xminus)==Patch::Neighbor?numGC:0,
+		       patch->getBCType(Patch::yminus)==Patch::Neighbor?numGC:0,
+		       patch->getBCType(Patch::zminus)==Patch::Neighbor?numGC:1);
+      hi  = patch->getCellHighIndex();
+      hi +=IntVector(patch->getBCType(Patch::xplus) ==Patch::Neighbor?numGC:0,
+		       patch->getBCType(Patch::yplus) ==Patch::Neighbor?numGC:0,
+		       patch->getBCType(Patch::zplus) ==Patch::Neighbor?numGC:0);
+      // cout<< "fillFaceflux: SFCZVariable.h"<<endl;
+      // cout<< "low: "<<low<<" Low + offset: "<<low + offset<<endl;
+      // cout<< "hi:  "<<hi <<" hi  - offset: "<<hi -offset<<endl;     
+
       switch (face) {
       case Patch::xplus:
 	for (int j = low.y(); j<hi.y(); j++) {
