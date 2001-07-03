403b0605e511c6602cc3d3b185d2c31577814182
J. Davison de St. Germain <dav@sci.utah.edu>
2001-07-03 21:32:36 +0000

    matrix3 move support

--- Packages/Uintah/testprograms/TestMatrix3/sub.mk
+++ Packages/Uintah/testprograms/TestMatrix3/sub.mk
@@ -7,16 +7,15 @@ SRCDIR := Packages/Uintah/testprograms/TestMatrix3
 SRCS := $(SRCDIR)/testmatrix3.cc
 
 PSELIBS := \
-	Packages/Uintah/CCA/Ports \
-	Packages/Uintah/Core/Grid \
-	Packages/Uintah/Core/Parallel \
-	Packages/Uintah/Core/Exceptions \
-	Packages/Uintah/Core/Math \
-	Packages/Uintah/CCA/Components/MPM \
+	Packages/Uintah/CCA/Ports              \
+	Packages/Uintah/Core/Grid              \
+	Packages/Uintah/Core/Parallel          \
+	Packages/Uintah/Core/Exceptions        \
+	Packages/Uintah/Core/Math              \
 	Packages/Uintah/testprograms/TestSuite \
 	Core/Exceptions \
-	Core/Thread \
-	Core/Geometry \
+	Core/Thread     \
+	Core/Geometry   \
 	Dataflow/XMLUtil
 
 LIBS := $(XML_LIBRARY) $(MPI_LIBRARY) -lm
