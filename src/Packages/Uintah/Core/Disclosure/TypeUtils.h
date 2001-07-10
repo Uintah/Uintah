3b38669e0e1dda5a7fbacec2721db9797832bbd4
Wayne Witzel <witzel@sci.utah.edu>
2001-07-10 17:44:20 +0000

    Added long64 type which is a "long long".

--- src/Packages/Uintah/Core/Disclosure/TypeUtils.h
+++ src/Packages/Uintah/Core/Disclosure/TypeUtils.h
@@ -13,10 +13,13 @@ class TypeDescription;
 
 using namespace SCIRun;
 
+typedef long long long64;
+
 const TypeDescription* fun_getTypeDescription(bool*);
 const TypeDescription* fun_getTypeDescription(int*);
 const TypeDescription* fun_getTypeDescription(short int*);
 const TypeDescription* fun_getTypeDescription(long*);
+const TypeDescription* fun_getTypeDescription(long64*);
 const TypeDescription* fun_getTypeDescription(double*);
 const TypeDescription* fun_getTypeDescription(Point*);
 const TypeDescription* fun_getTypeDescription(Vector*);
