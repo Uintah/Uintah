d9c3cb89473cee09fa400a841ceed70c584b6a41
Steven G. Parker <sparker@sci.utah.edu>
2001-07-27 20:46:58 +0000

    Removed extraneous include Added new exception PetscError

---
+++ Packages/Uintah/Core/Exceptions/PetscError.cc
@@ -0,0 +1,33 @@
+
+#include <Packages/Uintah/Core/Exceptions/PetscError.h>
+#include <sstream>
+
+using namespace Uintah;
+using namespace std;
+
+PetscError::PetscError(int petsc_code, const std::string& msg)
+    : petsc_code(petsc_code)
+{
+  ostringstream out;
+  out << "PETSc error: " << petsc_code << ", " << msg;
+  d_msg = out.str();
+}
+
+PetscError::PetscError(const PetscError& copy)
+    : petsc_code(copy.petsc_code), d_msg(copy.d_msg)
+{
+}
+
+PetscError::~PetscError()
+{
+}
+
+const char* PetscError::message() const
+{
+    return d_msg.c_str();
+}
+
+const char* PetscError::type() const
+{
+    return "Uintah::Exceptions::PetscError";
+}
--- Packages/Uintah/Core/Exceptions/sub.mk
+++ Packages/Uintah/Core/Exceptions/sub.mk
@@ -5,12 +5,13 @@ include $(SCIRUN_SCRIPTS)/smallso_prologue.mk
 SRCDIR   := Packages/Uintah/Core/Exceptions
 
 SRCS     += \
+	$(SRCDIR)/InvalidCompressionMode.cc \
 	$(SRCDIR)/InvalidGrid.cc            \
 	$(SRCDIR)/InvalidValue.cc           \
 	$(SRCDIR)/ParameterNotFound.cc      \
+	$(SRCDIR)/PetscError.cc             \
 	$(SRCDIR)/ProblemSetupException.cc  \
 	$(SRCDIR)/TypeMismatchException.cc  \
-	$(SRCDIR)/InvalidCompressionMode.cc \
 	$(SRCDIR)/VariableNotFoundInGrid.cc
 
 PSELIBS := \
