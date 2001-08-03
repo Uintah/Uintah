65b659f791baa33a0ae488d0d90ce05fd69facb6
Yarden Livnat <yarden@sci.utah.edu>
2001-08-03 16:17:24 +0000

    init MIT package

---
+++ Packages/MIT/Dataflow/Modules/DataIO/DistributionReader.cc
@@ -0,0 +1,36 @@
+/*
+ *  DistributionReader.cc:
+ *
+ *  Written by:
+ *   Yarden Livnat
+ *   July 2001
+ *
+ */
+
+#include <Packages/MIT/Dataflow/Ports/MetropolisPorts.h>
+#include <Dataflow/Modules/DataIO/GenericReader.h>
+
+namespace MIT {
+
+using namespace SCIRun;
+
+template class GenericReader<DistributionHandle>;
+
+class DistributionReader : public GenericReader<DistributionHandle> {
+public:
+  DistributionReader(const string& id);
+};
+
+extern "C" Module* make_DistributionReader(const string& id) {
+  return new DistributionReader(id);
+}
+
+DistributionReader::DistributionReader(const string& id)
+  : GenericReader<DistributionHandle>("DistributionReader", id, "DataIO", "MIT")
+{
+}
+
+
+} // End namespace MIT
+
+
