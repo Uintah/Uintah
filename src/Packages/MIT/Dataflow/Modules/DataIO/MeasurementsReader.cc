cacf80af9f926d8ede865babbc68a78ee09fdb1a
Yarden Livnat <yarden@sci.utah.edu>
2001-08-03 16:17:24 +0000

    init MIT package

---
+++ src/Packages/MIT/Dataflow/Modules/DataIO/MeasurementsReader.cc
@@ -0,0 +1,36 @@
+/*
+ *  MeasurementsReader.cc:
+ *
+ *  Written by:
+ *   Yarden Livnat
+ *   July 2001
+ *
+ */
+
+#include <Packages/MIT/Dataflow/Ports/MetropolisPorts.h>
+#include <Dataflow/Modules/DataIO/GenericReader.h>
+
+namespace MIT {
+
+using namespace SCIRun;
+
+template class GenericReader<MeasurementsHandle>;
+
+class MeasurementsReader : public GenericReader<MeasurementsHandle> {
+public:
+  MeasurementsReader(const string& id);
+};
+
+extern "C" Module* make_MeasurementsReader(const string& id) {
+  return new MeasurementsReader(id);
+}
+
+MeasurementsReader::MeasurementsReader(const string& id)
+  : GenericReader<MeasurementsHandle>("MeasurementsReader", id, "DataIO", "MIT")
+{
+}
+
+
+} // End namespace MIT
+
+
