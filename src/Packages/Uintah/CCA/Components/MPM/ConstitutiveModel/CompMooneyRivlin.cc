c6288540665bce01a121991beb88cf8de73cf44b
Jim Guilkey <guilkey@sci.utah.edu>
2001-08-06 21:32:35 +0000

    Added pStressLabel to the list of variables to be relocated at the end of each timestep.  This should allow the viz tools to work, as it will allow a version of the stress to be saved that is consistent with the other particle variables.

--- src/Packages/Uintah/CCA/Components/MPM/ConstitutiveModel/CompMooneyRivlin.cc
+++ src/Packages/Uintah/CCA/Components/MPM/ConstitutiveModel/CompMooneyRivlin.cc
@@ -292,7 +292,10 @@ void CompMooneyRivlin::addParticleState(std::vector<const VarLabel*>& from,
 					std::vector<const VarLabel*>& to)
 {
    from.push_back(lb->pDeformationMeasureLabel);
+   from.push_back(lb->pStressLabel);
+
    to.push_back(lb->pDeformationMeasureLabel_preReloc);
+   to.push_back(lb->pStressLabel_preReloc);
 }
 
 void CompMooneyRivlin::addComputesAndRequires(Task* task,
