a302c06358cc2bc81e38c2c1c880f0e81451c2b2
Yarden Livnat <yarden@sci.utah.edu>
2001-08-08 21:18:13 +0000

    redone

--- Core/GUI/Graph.tcl
+++ Core/GUI/Graph.tcl
@@ -14,45 +14,48 @@
 package require Iwidgets 3.1
 
 class Graph {
-    inherit OpenGLWindow
 
-    constructor {args} {
-	set name Graph
-	set_defaults
-    }
+    variable graph
+    variable parent-window
 
-    method set_defaults {} {
-	set $this-graph 0
-	set $this-parent-window 0
+    constructor {} {
+	global $this-gl-window 
     }
 
     method ui { w } {
-	global $this-ogl
+	global $this-gl-window
+
+	puts "Graph ui $w"
+	set parent-window $w
+
+	# menubar
+	iwidgets::menubar $w.menu 
+	puts "tcl graph menu at $w.menu"
 	
-	set $this-parent-window $w
+	# Options
+	frame $w.opt
 
-	# Info winfow
-	# iwidgets::Labeledframe $w.info -labelpos nw -labeltext "Info"
-
-	# Control
-	frame $w.ctrl
-#
-#       Graph
-#
+	#
+	# Graph
+	#
 	iwidgets::scrolledframe $w.graph -width 450 -height 350 \
 	    -hscrollmode dynamic -vscrollmode dynamic 
 
-#
-#
-#          OpenGL window of Graph
-#  
-	set graph [$w.graph childsite]
-	OpenGLWindow $this-ogl
-	$this-ogl ui $this $graph
+	#
+	#
+	# OpenGL window of Graph
+	#  
+	set $this-gl-window [$w.graph childsite].gl
+	#OpenGLWindow ogl
+	#ogl ui $graph.gl $this
 
 	
+	# Info winfow
+	#iwidgets::Labeledframe $w.info -labelpos nw -labeltext "Info"
+	
 	# pack $w.info 
-	pack $w.ctrl -anchor w 
+	pack $w.menu -fill x -expand yes
+	pack $w.opt -anchor w 
 	pack $w.graph -expand yes  -fill y
     }
 
--- Core/GUI/OpenGLWindow.tcl
+++ Core/GUI/OpenGLWindow.tcl
@@ -11,33 +11,30 @@
 #  Copyright (C) 2001 SCI Group
 #
 
-itcl_class OpenGLWindow {
-    constructor {config} {
-	set name OpenGLWindow
-	set_defaults
+class OpenGLWindow {
+
+    variable w
+
+    constructor {} {
     }
 
-    method set_defaults {} {
-	set $this-window 0
-    }
+    method ui { window parent } {
+	set w $window
 
-    method ui { parent w } {
-	global $this-opengl-win
-
-	set $this-opengl-win $w.gl
-
-	opengl $w.gl -geometry 500x300 -doublebuffer true \
+	opengl $w -geometry 500x300 -doublebuffer true \
 	    -direct true -rgba true 
-	
 
-	bind $w.gl <Map> "$parent-c map $w.gl"
-	bind $w.gl <Expose> "$parent-c redraw $w.gl"
-	pack $w.gl
+	bind $w <Map> "$this-c map $w"
+	bind $w <Expose> "$parent-c redraw $w"
+	pack $w
     }
 
     method change_params { n } {
     }
 	
+    method setobj { obj } {
+	bindtags $w [list [$obj getbinds] $w all]
+    }
 }
 	
 	
