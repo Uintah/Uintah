74ebf9d05663ec1d4f12a6997ade9531db6d6319
Wayne Witzel <witzel@sci.utah.edu>
2001-08-22 21:02:22 +0000

    Changed emitRLE to return a boolean instead of throwing an exception.

--- src/Packages/Uintah/Core/Grid/CCVariable.h
+++ src/Packages/Uintah/Core/Grid/CCVariable.h
@@ -395,7 +395,7 @@ WARNING
 	throw InternalError("Cannot yet write non-flat objects!\n");
     }
 
-    virtual void emitRLE(ostream& out, DOM_Element /*varnode*/)
+    virtual bool emitRLE(ostream& out, DOM_Element /*varnode*/)
     {
       const TypeDescription* td = fun_getTypeDescription((T*)0);
       if(td->isFlat()){
@@ -404,6 +404,7 @@ WARNING
       }
       else
 	throw InternalError("Cannot yet write non-flat objects!\n");
+      return true;
     }
 
     virtual void readNormal(istream& in)
--- src/Packages/Uintah/Core/Grid/SFCXVariable.h
+++ src/Packages/Uintah/Core/Grid/SFCXVariable.h
@@ -330,7 +330,7 @@ WARNING
 	throw InternalError("Cannot yet write non-flat objects!\n");
     }
   
-    virtual void emitRLE(ostream& out, DOM_Element /*varnode*/)
+    virtual bool emitRLE(ostream& out, DOM_Element /*varnode*/)
     {
       const TypeDescription* td = fun_getTypeDescription((T*)0);
       if(td->isFlat()){
@@ -339,6 +339,7 @@ WARNING
       }
       else
 	throw InternalError("Cannot yet write non-flat objects!\n");
+      return true;
     }
   
     virtual void readNormal(istream& in)
