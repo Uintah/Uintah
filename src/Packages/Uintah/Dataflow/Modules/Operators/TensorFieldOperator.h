94213c317c304168ba1a6c98b1a6e85c0ce53b49
Kurt Zimmerman <kuzimmer@sci.utah.edu>
2001-09-13 22:09:02 +0000

    unified code for unary field operations via UnaryFieldOperator  and added a ScalarField Operator

--- src/Packages/Uintah/Dataflow/Modules/Operators/TensorFieldOperator.h
+++ src/Packages/Uintah/Dataflow/Modules/Operators/TensorFieldOperator.h
@@ -2,6 +2,7 @@
 #define __DERIVE_TENSORFIELDOPERATOR_H__
 
 #include "TensorOperatorFunctors.h"
+#include "UnaryFieldOperator.h"
 #include "OperatorThread.h"
 #include <Core/GuiInterface/GuiVar.h>
 #include <Core/Geometry/IntVector.h>
@@ -19,7 +20,7 @@ using std::endl;
 
 namespace Uintah {
 using namespace SCIRun;
-class TensorFieldOperator: public Module {
+class TensorFieldOperator: public Module, public UnaryFieldOperator {
 public:
   TensorFieldOperator(const string& id);
   virtual ~TensorFieldOperator() {}
@@ -30,13 +31,6 @@ private:
   template<class TensorField, class ScalarField>
     void performOperation(TensorField* tensorField, ScalarField* scalarField);
 
-  template<class TensorField, class Field>
-    void initField(TensorField* tensorField, Field* field);
-
-  template<class TensorField, class ScalarField, class TensorOp >
-    void computeScalars(TensorField* tensorField, ScalarField* scalarField,
-			TensorOp op /* TensorOp should be a functor for
-				       converting tensors scalars */ );    
   //    TCLstring tcl_status;
   GuiInt guiOperation;
 
@@ -105,82 +99,6 @@ void TensorFieldOperator::performOperation(TensorField* tensorField,
   }
 }
 
-template<class TensorField, class ScalarField>
-void TensorFieldOperator::initField(TensorField* tensorField,
-				    ScalarField* field)
-{
-  ASSERT( tensorField->data_at() == Field::CELL ||
-	  tensorField->data_at() == Field::NODE );
-
-  typename TensorField::mesh_handle_type tmh = tensorField->get_typed_mesh();
-  typename ScalarField::mesh_handle_type fmh = field->get_typed_mesh();
-  BBox box;
-  box = tmh->get_bounding_box();
-  //resize the geometry
-  fmh->set_nx(tmh->get_nx());
-  fmh->set_ny(tmh->get_ny());
-  fmh->set_nz(tmh->get_nz());
-  fmh->set_transform(tmh->get_transform());
-  //resize the data storage
-  field->resize_fdata();
-
-}
-
-template<class TensorField, class ScalarField, class TensorOp >
-void TensorFieldOperator::computeScalars(TensorField* tensorField,
-					 ScalarField* scalarField,
-					 TensorOp op
-					 /* TensorOp should be a functor for
-					    converting tensors scalars */ )
-{
-  // so far only node and cell centered data
-  ASSERT( tensorField->data_at() == Field::CELL ||
-	  tensorField->data_at() == Field::NODE );
-
-  Matrix3 M;
-  typename TensorField::mesh_handle_type tmh = tensorField->get_typed_mesh();
-  typename ScalarField::mesh_handle_type smh = scalarField->get_typed_mesh();
-
-  if( tensorField->get_type_name(0) != "LevelField"){
-    if( tensorField->data_at() == Field::CELL){
-      typename TensorField::mesh_type::Cell::iterator t_it; tmh->begin(t_it);
-      typename ScalarField::mesh_type::Cell::iterator s_it; smh->begin(s_it);
-      typename TensorField::mesh_type::Cell::iterator t_end; tmh->end(t_end);
-      
-      for( ; t_it != t_end; ++t_it, ++s_it){
-	scalarField->fdata()[*s_it] = op(tensorField->fdata()[*t_it]);
-      }
-    } else {
-      typename TensorField::mesh_type::Node::iterator t_it; tmh->begin(t_it);
-      typename ScalarField::mesh_type::Node::iterator s_it; smh->begin(s_it);
-      typename TensorField::mesh_type::Node::iterator t_end; tmh->end(t_end);
-      
-      for( ; t_it != t_end; ++t_it, ++s_it){
-	scalarField->fdata()[*s_it] = op(tensorField->fdata()[*t_it]);
-      }
-    }  
-  } else {
-    int max_workers = Max(Thread::numProcessors()/3, 4);
-    Semaphore* thread_sema = scinew Semaphore( "tensor operator semaphore",
-					       max_workers); 
-    vector<ShareAssignArray3<Matrix3> > tdata = tensorField->fdata();
-    vector<ShareAssignArray3<Matrix3> >::iterator vit = tdata.begin();
-    vector<ShareAssignArray3<Matrix3> >::iterator vit_end = tdata.end();
-
-    IntVector offset( (*vit).getLowIndex() );
-    for(;vit != vit_end; ++vit) {
-      thread_sema->down();
-      Thread *thrd = 
-	scinew Thread(
-	  scinew OperatorThread< Matrix3, ScalarField, TensorOp >
-	  ( *vit, scalarField, offset, op, thread_sema ),
-	  "tensor operator worker");
-      thrd->detach();
-    }
-    thread_sema->down(max_workers);
-    if(thread_sema) delete thread_sema;
-  }
-}
 } // end namespace Uintah 
 
 #endif // __DERIVE_TENSORFIELDOPERATOR_H__
--- src/Packages/Uintah/Dataflow/Modules/Operators/VectorFieldOperator.h
+++ src/Packages/Uintah/Dataflow/Modules/Operators/VectorFieldOperator.h
@@ -2,6 +2,7 @@
 #define __OPERATORS_VECTORFIELDOPERATOR_H__
 
 #include "VectorOperatorFunctors.h"
+#include "UnaryFieldOperator.h"
 #include "OperatorThread.h"
 #include <Core/GuiInterface/GuiVar.h>
 #include <Dataflow/Network/Module.h>
@@ -22,7 +23,7 @@ using std::endl;
 namespace Uintah {
 using namespace SCIRun;
 
-  class VectorFieldOperator: public Module {
+  class VectorFieldOperator: public Module, public UnaryFieldOperator {
   public:
     VectorFieldOperator(const string& id);
     virtual ~VectorFieldOperator() {}
@@ -32,13 +33,6 @@ using namespace SCIRun;
   private:
     template<class VectorField, class ScalarField>
      void performOperation(VectorField* vectorField, ScalarField* scalarField);
-    template<class VectorField, class Field>
-     void initField(VectorField* vectorField, Field* field);
-    
-    template<class VectorField, class ScalarField, class VectorOp >
-     void computeScalars(VectorField* vectorField, ScalarField* scalarField,
-			 VectorOp op /* VectorOp should be a functor for
-					converting tensors scalars */ );
 
     //    TCLstring tcl_status;
     GuiInt guiOperation;
@@ -69,113 +63,6 @@ void VectorFieldOperator::performOperation(VectorField* vectorField,
     std::cerr << "VectorFieldOperator::performOperation: "
 	      << "Unexpected Operation Type #: " << guiOperation.get() << "\n";
   }
-}
-
-template<class VectorField, class ScalarField>
-void VectorFieldOperator::initField(VectorField* vectorField,
-				    ScalarField* field)
-{
-  ASSERT( vectorField->data_at() == Field::CELL ||
-	  vectorField->data_at() == Field::NODE );
-
-  typename VectorField::mesh_handle_type vmh = vectorField->get_typed_mesh();
-  typename ScalarField::mesh_handle_type fmh = field->get_typed_mesh();
-  BBox box;
-  box = vmh->get_bounding_box();
-  //resize the geometry
-  fmh->set_nx(vmh->get_nx());
-  fmh->set_ny(vmh->get_ny());
-  fmh->set_nz(vmh->get_nz());
-  fmh->set_transform(vmh->get_transform());
-  //resize the data storage
-  field->resize_fdata();
-
-}
-
-template<class VectorField, class ScalarField, class VectorOp >
-void VectorFieldOperator::computeScalars(VectorField* vectorField,
-					 ScalarField* scalarField,
-					 VectorOp op 
-					 /* VectorOp should be a functor for
-					    converting vectors scalars */ )
-{
-  // so far only node and cell centered data
-  ASSERT( vectorField->data_at() == Field::CELL ||
-	  vectorField->data_at() == Field::NODE );
-
-
-  typename VectorField::mesh_handle_type vmh = vectorField->get_typed_mesh();
-  typename ScalarField::mesh_handle_type smh = scalarField->get_typed_mesh();
-  if( vectorField->get_type_name(0) != "LevelField"){
-    if( vectorField->data_at() == Field::CELL){
-      typename VectorField::mesh_type::Cell::iterator v_it; vmh->begin(v_it);
-      typename VectorField::mesh_type::Cell::iterator v_end; vmh->end(v_end);
-      typename ScalarField::mesh_type::Cell::iterator s_it; smh->begin(s_it);
-    
-      /*     cerr<<"v_it = ("<<(*v_it).i_<<","<<(*v_it).j_<<","<<(*v_it).k_<< */
-      /*       "), v_end = ("<<(*v_end).i_<<","<<(*v_end).j_<<","<<(*v_end).k_<<")\n"; */
-      for( ; v_it != v_end; ++v_it, ++s_it){
-	scalarField->fdata()[*s_it] = op(vectorField->fdata()[*v_it]);
-      }
-    } else {
-      typename VectorField::mesh_type::Node::iterator v_it; vmh->begin(v_it);
-      typename VectorField::mesh_type::Node::iterator v_end; vmh->end(v_end);
-      typename ScalarField::mesh_type::Node::iterator s_it; smh->begin(s_it);
-  
-      for( ; v_it != v_end; ++v_it, ++s_it){
-	scalarField->fdata()[*s_it] = op(vectorField->fdata()[*v_it]);
-      }
-    }  
-  } else {
-    int max_workers = Max(Thread::numProcessors()/3, 4);
-    Semaphore* thread_sema = scinew Semaphore( "vector operator semaphore",
-					       max_workers); 
-    vector<ShareAssignArray3<Vector> > tdata = vectorField->fdata();
-    vector<ShareAssignArray3<Vector> >::iterator vit = tdata.begin();
-    vector<ShareAssignArray3<Vector> >::iterator vit_end = tdata.end();
-
-#if 0
-    float minx,maxx,miny,maxy,minz,maxz,minl,maxl;
-    Array3<Vector>::iterator it((*vit).begin());
-    Array3<Vector>::iterator it_end((*vit).end());
-    minx = maxx = (*it)[0];
-    miny = maxy = (*it)[1];
-    minz = maxz = (*it)[2];
-    minl = maxl = (*it).length();
-    for(;vit != vit_end; ++vit) {
-      it = (*vit).begin();
-      it_end = (*vit).end();
-      for(;it != it_end; ++it) {
-	minx = Min(minx,(*it)[0]);
-	maxx = Max(maxx,(*it)[0]);
-	miny = Min(miny,(*it)[1]);
-	maxy = Max(maxy,(*it)[1]);
-	minz = Min(minz,(*it)[2]);
-	maxz = Max(maxz,(*it)[2]);
-	minl = Min(minl,(*it).length());
-	maxl = Max(maxl,(*it).length());
-      }
-    }
-    cout << "minx = " << minx << ", maxx = " << maxx << endl;
-    cout << "miny = " << miny << ", maxy = " << maxy << endl;
-    cout << "minz = " << minz << ", maxz = " << maxz << endl;
-    cout << "minl = " << minl << ", maxl = " << maxl << endl;
-    vit = tdata.begin();
-#endif
-    IntVector offset( (*vit).getLowIndex() );
-    for(;vit != vit_end; ++vit) {
-      thread_sema->down();
-      Thread *thrd = 
-	scinew Thread(
-	  scinew OperatorThread< Vector, ScalarField, VectorOp >
-	  ( *vit, scalarField, offset, op, thread_sema ),
-	  "vector operator worker");
-      thrd->detach();
-    }
-    thread_sema->down(max_workers);
-    if(thread_sema) delete thread_sema;
-  }
-    
 }
 
 }
