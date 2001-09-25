e853fd614441a2c8bcea8aedc00393f99811c8da
Wayne Witzel <witzel@sci.utah.edu>
2001-09-25 21:59:52 +0000

    Changed the way in which you get data out of the DataWarehouse. The reason for this change is to prevent anyone from changing data in the DataWarehouse that they shouldn't be allowed to change while minimizing the need to copy data when it can be shared.  There are now four different ways (member functions of DataWarehouse) to get data out of the DataWarehouse: get, getModifiable, copyOut, and getCopy.

--- src/Packages/Uintah/CCA/Ports/DataWarehouse.cc
+++ src/Packages/Uintah/CCA/Ports/DataWarehouse.cc
@@ -1,5 +1,6 @@
 
 #include <Packages/Uintah/CCA/Ports/DataWarehouse.h>
+#include <Packages/Uintah/CCA/Ports/Scheduler.h>
 #include <Core/Geometry/Vector.h>
 
 #include <iostream>
@@ -9,9 +10,13 @@ using namespace SCIRun;
 
 using std::cerr;
 
-DataWarehouse::DataWarehouse(const ProcessorGroup* myworld, 
+bool DataWarehouse::show_warnings = true;
+
+
+DataWarehouse::DataWarehouse(const ProcessorGroup* myworld,
+			     const Scheduler* scheduler,
 			     int generation)
-  : d_myworld(myworld), d_generation( generation )
+  : d_myworld(myworld), d_scheduler(scheduler), d_generation(generation)
 {
 }
 
@@ -19,3 +24,21 @@ DataWarehouse::~DataWarehouse()
 {
 }
 
+void DataWarehouse::copyOut(ParticleVariableBase& var, const VarLabel* label,
+			    ParticleSubset* pset)
+{
+  constParticleVariableBase* constVar = var.cloneConstType();
+  this->get(*constVar, label, pset);
+  var.copyData(&constVar->getBaseRep());
+  delete constVar;
+}
+
+void DataWarehouse::getCopy(ParticleVariableBase& var, const VarLabel* label,
+			    ParticleSubset* pset)
+{
+  constParticleVariableBase* constVar = var.cloneConstType();
+  this->get(*constVar, label, pset);
+  var.allocate(pset);
+  var.copyData(&constVar->getBaseRep());
+  delete constVar;
+}
