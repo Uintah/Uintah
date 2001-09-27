ded36f94f99289dfc9e0a60d5e345cc1f1f88789
Kurt Zimmerman <kuzimmer@sci.utah.edu>
2001-09-27 20:02:26 +0000

    Updated the AverageThread portion of the file

--- Dataflow/Modules/Operators/OperatorThread.h
+++ Dataflow/Modules/Operators/OperatorThread.h
@@ -88,30 +88,33 @@ public:
   Array3<Data>::iterator it_end(data_.end());
   IntVector min(data_.getLowIndex() - offset_);
   IntVector size(data_.getHighIndex() - min - offset_);
-  typename ScalarField::mesh_type mesh(m, min.x(), min.y(), min.z(),
-				       size.x(), size.y(), size.z());
   double ave = 0;
   int counter = 0;
   if( sf_->data_at() == Field::CELL ) {
+    typename ScalarField::mesh_type mesh(m, min.x(), min.y(), min.z(),
+					 size.x()+1, size.y()+1, size.z()+1);
     typename ScalarField::mesh_type::Cell::iterator s_it; mesh.begin(s_it);
     typename ScalarField::mesh_type::Cell::iterator s_it_end; mesh.end(s_it_end);
     for(; s_it != s_it_end; ++it, ++s_it){
-      sf_->fdata()[*s_it] = (sf_->fdata()[*s_it]*(*it))/2.0 ;
+      sf_->fdata()[*s_it] = (sf_->fdata()[*s_it]+(*it))/2.0 ;
       ave += sf_->fdata()[*s_it];
       ++counter;
     }
   } else {
+    typename ScalarField::mesh_type mesh(m, min.x(), min.y(), min.z(),
+					 size.x(), size.y(), size.z());
     typename ScalarField::mesh_type::Node::iterator s_it; mesh.begin(s_it);
     typename ScalarField::mesh_type::Node::iterator s_it_end; mesh.end(s_it_end);
     for(; s_it != s_it_end; ++it, ++s_it){
-      sf_->fdata()[*s_it] = (sf_->fdata()[*s_it]*(*it))/2.0 ;
+      sf_->fdata()[*s_it] = (sf_->fdata()[*s_it]+(*it))/2.0 ;
       ave += sf_->fdata()[*s_it];
       ++counter;
     }
   }
-  ave /= counter;
+  cerr<<"sum  = "<<ave<<", counter = "<<counter<<", ave = "<<ave/counter<<endl;
+
   lock_->lock();
-  aveVal_ += ave;
+  aveVal_ += ( ave/counter );
   lock_->unlock();
   sema_->up();
 }
