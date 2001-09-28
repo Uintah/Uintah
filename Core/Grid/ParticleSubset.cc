30f39e8bb921e9bebd6fa459ded7fdcd69e6252f
Wayne Witzel <witzel@sci.utah.edu>
2001-09-28 17:01:52 +0000

    Include algo.h for sort function -- linux needs it

--- Core/Grid/ParticleSubset.cc
+++ Core/Grid/ParticleSubset.cc
@@ -4,6 +4,7 @@
 #include <Packages/Uintah/Core/Disclosure/TypeUtils.h>
 #include <Core/Exceptions/InternalError.h>
 #include <Core/Malloc/Allocator.h>
+#include <algo.h>
 
 using namespace Uintah;
 using namespace std;
