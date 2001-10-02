d592da7addacfce5d5a249e604a4ef710647cb11
J. Davison de St. Germain <dav@sci.utah.edu>
2001-10-02 19:29:09 +0000

    updates to work with new dir structure.

---
+++ testprograms/Component/framework/REI/sub.mk
@@ -0,0 +1,12 @@
+#
+# Makefile fragment for this subdirectory
+#
+
+SRCDIR := testprograms/Component/framework/REI
+
+PSELIBS +=
+
+SRCS    += \
+	$(SRCDIR)/scr.cc \
+	$(SRCDIR)/scrInterfaceImpl.cc
+
