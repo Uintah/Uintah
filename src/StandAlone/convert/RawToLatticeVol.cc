f883f9600feed36cad386eaf3dbad15657790721
David Weinstein <dmw@sci.utah.edu>
2001-10-19 17:02:32 +0000

    Added support for more datatypes

--- src/StandAlone/convert/RawToLatticeVol.cc
+++ src/StandAlone/convert/RawToLatticeVol.cc
@@ -54,9 +54,9 @@ main(int argc, char **argv) {
   int ni, nj, nk;
   double minx, miny, minz, maxx, maxy, maxz;
   cin >> ni >> nj >> nk;
-  cout << "ASCII or Binary (a/b)? ";
-  string ascii_or_binary;
-  cin >> ascii_or_binary;
+  cout << "ASCII or Binary input (a/b)? ";
+  string ascii_or_binary_input;
+  cin >> ascii_or_binary_input;
   cout << "Datatype (d/f/ui/i/us/s/uc/c)? ";
   string datatype;
   cin >> datatype;
@@ -64,6 +64,9 @@ main(int argc, char **argv) {
   cin >> minx >> miny >> minz;
   cout << "Max x y z? ";
   cin >> maxx >> maxy >> maxz;
+  cout << "ASCII or Binary output (a/b)? ";
+  string ascii_or_binary_output;
+  cin >> ascii_or_binary_output;
 
   Point min(minx, miny, minz), max(maxx, maxy, maxz);
   LatVolMeshHandle lvm = new LatVolMesh(ni, nj, nk, min, max);
@@ -76,7 +79,7 @@ main(int argc, char **argv) {
       new LatticeVol<double>(lvm, Field::NODE);
     fH=lv;
     double *data=&(lv->fdata()(0,0,0));
-    if (ascii_or_binary == "a") {
+    if (ascii_or_binary_input == "a") {
       double f;
       for (int i=0; i<ni*nj*nk; i++) {
 	fscanf(fin, "%lf", &f);
@@ -85,26 +88,12 @@ main(int argc, char **argv) {
     } else {
       fread(data, sizeof(double), ni*nj*nk, fin);
     }
-  } else if (datatype == "uc") {
-    LatticeVol<unsigned char> *lv = 
-      new LatticeVol<unsigned char>(lvm, Field::NODE);
-    fH=lv;
-    unsigned char *data=&(lv->fdata()(0,0,0));
-    if (ascii_or_binary == "a") {
-      unsigned char f;
-      for (int i=0; i<ni*nj*nk; i++) {
-	fscanf(fin, "%c", &f);
-	*data++ = f;
-      }
-    } else {
-      fread(data, sizeof(unsigned char), ni*nj*nk, fin);
-    }
   } else if (datatype == "f") {
     LatticeVol<float> *lv = 
       new LatticeVol<float>(lvm, Field::NODE);
     fH=lv;
     float *data=&(lv->fdata()(0,0,0));
-    if (ascii_or_binary == "a") {
+    if (ascii_or_binary_input == "a") {
       float f;
       for (int i=0; i<ni*nj*nk; i++) {
 	fscanf(fin, "%f", &f);
@@ -113,8 +102,101 @@ main(int argc, char **argv) {
     } else {
       fread(data, sizeof(float), ni*nj*nk, fin);
     }
+  } else if (datatype == "ui") {
+    LatticeVol<unsigned int> *lv = 
+      new LatticeVol<unsigned int>(lvm, Field::NODE);
+    fH=lv;
+    unsigned int *data=&(lv->fdata()(0,0,0));
+    if (ascii_or_binary_input == "a") {
+      unsigned int f;
+      for (int i=0; i<ni*nj*nk; i++) {
+	fscanf(fin, "%u", &f);
+	*data++ = f;
+      }
+    } else {
+      fread(data, sizeof(unsigned int), ni*nj*nk, fin);
+    }
+  } else if (datatype == "i") {
+    LatticeVol<int> *lv = 
+      new LatticeVol<int>(lvm, Field::NODE);
+    fH=lv;
+    int *data=&(lv->fdata()(0,0,0));
+    if (ascii_or_binary_input == "a") {
+      int f;
+      for (int i=0; i<ni*nj*nk; i++) {
+	fscanf(fin, "%d", &f);
+	*data++ = f;
+      }
+    } else {
+      fread(data, sizeof(int), ni*nj*nk, fin);
+    }
+  } else if (datatype == "us") {
+    LatticeVol<unsigned short> *lv = 
+      new LatticeVol<unsigned short>(lvm, Field::NODE);
+    fH=lv;
+    unsigned short *data=&(lv->fdata()(0,0,0));
+    if (ascii_or_binary_input == "a") {
+      unsigned short f;
+      for (int i=0; i<ni*nj*nk; i++) {
+	fscanf(fin, "%hu", &f);
+	*data++ = f;
+      }
+    } else {
+      fread(data, sizeof(unsigned short), ni*nj*nk, fin);
+    }
+  } else if (datatype == "s") {
+    LatticeVol<short> *lv = 
+      new LatticeVol<short>(lvm, Field::NODE);
+    fH=lv;
+    short *data=&(lv->fdata()(0,0,0));
+    if (ascii_or_binary_input == "a") {
+      short f;
+      for (int i=0; i<ni*nj*nk; i++) {
+	fscanf(fin, "%hd", &f);
+	*data++ = f;
+      }
+    } else {
+      fread(data, sizeof(short), ni*nj*nk, fin);
+    }
+  } else if (datatype == "uc") {
+    LatticeVol<unsigned char> *lv = 
+      new LatticeVol<unsigned char>(lvm, Field::NODE);
+    fH=lv;
+    unsigned char *data=&(lv->fdata()(0,0,0));
+    if (ascii_or_binary_input == "a") {
+      unsigned short f;
+      for (int i=0; i<ni*nj*nk; i++) {
+	fscanf(fin, "%hu", &f);
+	*data++ = (unsigned char)f;
+      }
+    } else {
+      fread(data, sizeof(unsigned char), ni*nj*nk, fin);
+    }
+  } else if (datatype == "c") {
+    LatticeVol<char> *lv = 
+      new LatticeVol<char>(lvm, Field::NODE);
+    fH=lv;
+    char *data=&(lv->fdata()(0,0,0));
+    if (ascii_or_binary_input == "a") {
+      short f;
+      for (int i=0; i<ni*nj*nk; i++) {
+	fscanf(fin, "%hd", &f);
+	*data++ = (char)f;
+      }
+    } else {
+      fread(data, sizeof(char), ni*nj*nk, fin);
+    }
+  } else {
+    cerr << "Error -- unknown datatype: "<<datatype<<"\n";
   }
-  BinaryPiostream out_stream(argv[2], Piostream::Write);
-  Pio(out_stream, fH);
+  if (ascii_or_binary_output == "a") {
+    TextPiostream out_stream(argv[2], Piostream::Write);
+    Pio(out_stream, fH);
+  } else if (ascii_or_binary_output == "b") {
+    BinaryPiostream out_stream(argv[2], Piostream::Write);
+    Pio(out_stream, fH);
+  } else {
+    cerr << "Error -- unknown ASCII/binary flag: "<<ascii_or_binary_output<<"\n";
+  }
   return 0;  
 }    
