6396ff33dafb39080c7bbb1d4ec09a7899fa8e7b
Kurt Zimmerman <kuzimmer@sci.utah.edu>
2001-10-23 21:20:31 +0000

    Removed unecessary template instantiations

--- src/Packages/Uintah/Core/Datatypes/cd_templates.cc
+++ src/Packages/Uintah/Core/Datatypes/cd_templates.cc
@@ -2,8 +2,10 @@
 #include "LevelField.h"
 #include <Core/Geometry/Vector.h>
 #include <Packages/Uintah/Core/Math/Matrix3.h>
-
+#include <Core/Util/NotFinished.h>
+#include <utility>
 using namespace Uintah;
+using std::pair;
 
 #ifdef __sgi
 #pragma set woff 1468
@@ -13,19 +15,6 @@ template class LevelField<SCIRun::Vector>;
 template class GenericField<LevelMesh, LevelData<SCIRun::Vector> >;
 template class LevelField<Matrix3>;
 template class GenericField<LevelMesh, LevelData<Matrix3> >;
-
-void Pio(Piostream& stream, LevelData<int>& data);
-void Pio(Piostream& stream, LevelData<long>& data);
-void Pio(Piostream& stream, LevelData<double>& data);
-void Pio(Piostream& stream, LevelData<Vector>& data);
-void Pio(Piostream& stream, LevelData<Matrix3>& data);
-
-// template class LevelFieldSFI<SCIRun::Vector>;
-// template class LevelFieldSFI<Matrix3>;
-// template class LevelFieldSFI<double>;
-// template class LevelFieldSFI<int>;
-// template class LevelFieldSFI<long>;
-
 
 #ifdef __sgi
 #pragma reset woff 1468
