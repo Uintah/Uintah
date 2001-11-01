347f08a4df2f4437561c1cf8f8aef7365f86e576
James Bigler <bigler@sci.utah.edu>
2001-11-01 21:26:04 +0000

    Changed Material pointers to Color pointers.  Added inv_diag to change some divides to multiplies.  Added class's own phong equation for performace tweaks.  Replaced x/y/z_weight_high with 1-weith_low.

--- src/Packages/rtrt/Core/VolumeVis.cc
+++ src/Packages/rtrt/Core/VolumeVis.cc
@@ -12,6 +12,9 @@
 #include <Packages/rtrt/Core/Worker.h>
 #include <Packages/rtrt/Core/ScalarTransform1D.h>
 #include <Packages/rtrt/Core/Array1.h>
+#include <Packages/rtrt/Core/Scene.h>
+#include <Packages/rtrt/Core/Point.h>
+#include <Packages/rtrt/Core/Vector.h>
 #include <float.h>
 #include <iostream>
 
@@ -20,19 +23,38 @@ using namespace rtrt;
 
 VolumeVis::VolumeVis(BrickArray3<float>& _data, float data_min, float data_max,
 		     int nx, int ny, int nz,
-		     Point min, Point max, const Array1<Material*> &matls,
-		     int nmatls, const Array1<float> &alphas, int nalphas):
+		     Point min, Point max, const Array1<Color*> &matls,
+		     int nmatls, const Array1<float> &alphas, int nalphas,
+		     double spec_coeff, double ambient, double diffuse,
+		     double specular):
   Object(this), diag(max - min),
   data_min(data_min), data_max(data_max),
   data_diff_inv(1/(data_max - data_min)),
   nx(nx), ny(ny), nz(nz),
   min(min), max(max), matls(matls), nmatls(nmatls),
-  alphas(alphas), nalphas(nalphas)
+  alphas(alphas), nalphas(nalphas), spec_coeff(spec_coeff),
+  ambient(ambient), diffuse(diffuse), specular(specular)
 {
   data.share(_data);
   delta_x2 = 2 * (max.x() - min.x())/nx;
   delta_y2 = 2 * (max.y() - min.y())/ny;
   delta_z2 = 2 * (max.z() - min.z())/nz;
+
+  if (diag.x() == 0)
+    inv_diag.x(0);
+  else
+    inv_diag.x(1.0/diag.x());
+
+  if (diag.y() == 0)
+    inv_diag.y(0);
+  else
+    inv_diag.y(1.0/diag.y());
+
+  if (diag.z() == 0)
+    inv_diag.z(0);
+  else
+    inv_diag.z(1.0/diag.z());
+  
 }
 
 VolumeVis::~VolumeVis() {
@@ -100,6 +122,68 @@ void VolumeVis::light_intersect(Light* , const Ray& lightray,
   intersect(lightray, hit, ds, ppc);
 }
 
+// All incoming vectors should be normalized
+
+// Parameters:
+//   N, objnorm - the normal of the surface
+//   V, viewnorm - direction from the point to the eye
+//   L, lightnorm - direction from the point to the light
+//   point - the location of intersection
+//   object_color - the unlit color of the surface
+/*
+  R = 2.0*(N.L)*N - L
+  
+  I found this vector also needs to be normalized. Then, we feed these
+  vectors into the illumination equation for the phong lighting model, which
+  is
+  
+  I = Ia*ka*Oda + fatt*Ip[kd*Od(N.L) + ks(R.V)^n]
+  
+  Here, the variables are:
+  
+  * Ia is the ambient intensity
+  * ka is the ambient co-efficient
+  * Oda is the colour for the ambient
+  * fatt is the atmospheric attenuation factor, ie depth shading
+  * Ip is the intensity of the point light source
+  * kd is the diffuse co-efficient
+  * Od is the objects colour
+  * ks is the specular co-efficient
+  * n is the objects shinyness
+  * N is the normal vector
+  * L is the lighting vector
+  * R is the reflection vector
+  * V is the viewing vector
+  */
+Color VolumeVis::color(const Vector &N, const Vector &V, const Vector &L, 
+		       const Color &object_color, const Color &light_color) {
+
+  Color result; // the resulting color
+
+  double L_N_dot = L.dot(N);
+
+  // the dot product is negative then the objects face points
+  // away from the light and should be shaded.
+  if (L_N_dot > 0) {
+    // do the ambient, diffuse, and specular calculations
+    Vector R;
+    double attinuation = 1;
+
+    //    R = vector_norm(vector_sub(vector_mult(N, 2* L_N_dot),L));
+    R = (2.0 * N.dot(L) * N - L).normal();
+    double spec = attinuation * specular * pow(Max(R.dot(V),0.0), spec_coeff);
+
+    result = light_color * (object_color *(ambient+attinuation*diffuse*L_N_dot)
+			    + Color(spec, spec, spec));
+  }
+  else {
+    // do only the ambient calculations
+    result = light_color * object_color * ambient;
+  }
+
+  return result;
+}
+	
 Vector VolumeVis::normal(const Point&, const HitInfo& hit) {
   // the normal should be placed in the scratchpad
   Vector* norm = (Vector*)hit.scratchpad;
@@ -135,7 +219,7 @@ void VolumeVis::shade(Color& result, const Ray& ray,
   Color total(0,0,0);
   //cout << "data.x = " << data.dim1() << ", data.y = " << data.dim2() << ", data.z = " << data.dim3() << endl;
   Vector p;
-  HitInfo new_hit = hit;
+  //  HitInfo new_hit = hit;
   for(float t = t_min; t < t_max; t += t_inc) {
     // opaque values are 0, so terminate the ray at alpha values close to zero
     if (alpha > 0.02) {
@@ -143,30 +227,27 @@ void VolumeVis::shade(Color& result, const Ray& ray,
       p = ray.origin() + ray.direction() * t - min;
       //cout << "p = " << p << ", diag = " << diag << endl;
       // interpolate the point
-      float norm = p.x() / diag.x();
+      float norm = p.x() * inv_diag.x();
       float step = norm * (nx - 1);
       int x_low = bound((int)step, 0, data.dim1()-1);
       //      float x_index_high = ceilf(step);
       int x_high = bound(x_low+1, 0, data.dim1()-1);
       float x_weight_low = x_high - step;
-      float x_weight_high = 1 - x_weight_low; // step - index_high
-      //cout << "norm = " << norm << ", step = " << step << ", x_low = " << x_low << ", x_high = " << x_high << ", x_weight_low = " << x_weight_low << ", x_weight_high = " << x_weight_high << endl;
-      norm = p.y() / diag.y();
+      //cout << "norm = " << norm << ", step = " << step << ", x_low = " << x_low << ", x_high = " << x_high << ", x_weight_low = " << x_weight_low << ", x_weight_high = " << (1 - x_weight_low) << endl;
+      norm = p.y() * inv_diag.y();
       step = norm * (ny - 1);
       int y_low = bound((int)step, 0, data.dim2()-1);
       //      float y_index_high = ceilf(step);
       int y_high = bound(y_low+1, 0, data.dim2()-1);
       float y_weight_low = y_high - step;
-      float y_weight_high = 1 - y_weight_low; // step - index_high
-      //cout << "norm = " << norm << ", step = " << step << ", y_low = " << y_low << ", y_high = " << y_high << ", y_weight_low = " << y_weight_low << ", y_weight_high = " << y_weight_high << endl;
-      norm = p.z() / diag.z();
+      //cout << "norm = " << norm << ", step = " << step << ", y_low = " << y_low << ", y_high = " << y_high << ", y_weight_low = " << y_weight_low << ", y_weight_high = " << (1 - y_weight_low) << endl;
+      norm = p.z() * inv_diag.z();
       step = norm * (nz - 1);
       int z_low = bound((int)step, 0, data.dim3()-1);
       //      float z_index_high = ceilf(step);
       int z_high = bound(z_low+1, 0, data.dim3()-1);
       float z_weight_low = z_high - step;
-      float z_weight_high = 1 - z_weight_low; // step - index_high
-      //cout << "norm = " << norm << ", step = " << step << ", z_low = " << z_low << ", z_high = " << z_high << ", z_weight_low = " << z_weight_low << ", z_weight_high = " << z_weight_high << endl;
+      //cout << "norm = " << norm << ", step = " << step << ", z_low = " << z_low << ", z_high = " << z_high << ", z_weight_low = " << z_weight_low << ", z_weight_high = " << (1 - z_weight_low) << endl;
       //cout << "data(0,0,0) = "; flush(cout); cout << data(0,0,0) << endl;
 
       float a,b,c,d,e,f,g,h;
@@ -182,15 +263,15 @@ void VolumeVis::shade(Color& result, const Ray& ray,
 
       float lz1, lz2, lz3, lz4, ly1, ly2, value;
 
-      lz1 = a * z_weight_low + b * z_weight_high;
-      lz2 = c * z_weight_low + d * z_weight_high;
-      lz3 = e * z_weight_low + f * z_weight_high;
-      lz4 = g * z_weight_low + h * z_weight_high;
+      lz1 = a * z_weight_low + b * (1 - z_weight_low);
+      lz2 = c * z_weight_low + d * (1 - z_weight_low);
+      lz3 = e * z_weight_low + f * (1 - z_weight_low);
+      lz4 = g * z_weight_low + h * (1 - z_weight_low);
 
-      ly1 = lz1 * y_weight_low + lz2 * y_weight_high;
-      ly2 = lz3 * y_weight_low + lz4 * y_weight_high;
+      ly1 = lz1 * y_weight_low + lz2 * (1 - y_weight_low);
+      ly2 = lz3 * y_weight_low + lz4 * (1 - y_weight_low);
 
-      value = ly1 * x_weight_low + ly2 * x_weight_high;
+      value = ly1 * x_weight_low + ly2 * (1 - x_weight_low);
       
       //cout << "value = " << value << endl;
       float normalized = (value - data_min) * data_diff_inv;
@@ -199,34 +280,38 @@ void VolumeVis::shade(Color& result, const Ray& ray,
 	// the point is contributing, so compute the color
 
 	// compute the gradient and tuck it away for the normal function to get
-	Vector* p_vector = (Vector*)new_hit.scratchpad;
+	//	Vector* p_vector = (Vector*)new_hit.scratchpad;
+	Vector gradient;
 	float dx = ly2 - ly1;
 	
 	float dy, dy1, dy2;
 	dy1 = lz2 - lz1;
 	dy2 = lz4 - lz3;
-	dy = dy1 * x_weight_low + dy2 * x_weight_high;
+	dy = dy1 * x_weight_low + dy2 * (1 - x_weight_low);
 	
 	float dz, dz1, dz2, dz3, dz4, dzly1, dzly2;
 	dz1 = b - a;
 	dz2 = d - c;
 	dz3 = f - e;
 	dz4 = h - g;
-	dzly1 = dz1 * y_weight_low + dz2 * y_weight_high;
-	dzly2 = dz3 * y_weight_low + dz4 * y_weight_high;
-	dz = dzly1 * x_weight_low + dzly2 * x_weight_high;
+	dzly1 = dz1 * y_weight_low + dz2 * (1 - y_weight_low);
+	dzly2 = dz3 * y_weight_low + dz4 * (1 - y_weight_low);
+	dz = dzly1 * x_weight_low + dzly2 * (1 - x_weight_low);
 	if (dx || dy || dz)
-	  *p_vector = (Vector(dx,dy,dz)).normal();
+	  gradient = (Vector(dx,dy,dz)).normal();
+	//	  *p_vector = (Vector(dx,dy,dz)).normal();
 	else
-	  *p_vector = Vector(0,0,0);
+	  //	  *p_vector = Vector(0,0,0);
+	  gradient = Vector(0,0,0);
 
 	int idx=bound((int)(normalized*(nmatls-1)), 0, nmatls - 1);
-	Color temp;
-	new_hit.min_t = t;
-	matls[idx]->shade(temp, ray, new_hit, depth, atten, accumcolor, cx);
-	if(temp.red()>1){
-	  //cerr << "+";
-	}
+	Light* light=cx->scene->light(0);
+	Vector light_dir;
+	light_dir = light->get_pos()-p;
+	Color temp = color(gradient, ray.direction(), light_dir.normal(), 
+			   *(matls[idx]), light->get_color());
+	//	new_hit.min_t = t;
+	//	matls[idx]->shade(temp, ray, new_hit, depth, atten, accumcolor, cx);
 	total += temp * (alphas[alpha_idx] * alpha);
 	alpha = alpha * (1 - alphas[alpha_idx]);
       }
