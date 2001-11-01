c19ab44869432397f8acfee19a2b7597f50dcc46
James Bigler <bigler@sci.utah.edu>
2001-11-01 21:26:24 +0000

    Changed Material pointers to Color pointers.  Added inv_diag to change some divides to multiplies.  Added class's own phong equation for performace tweaks.

--- Packages/rtrt/Core/VolumeVis.h
+++ Packages/rtrt/Core/VolumeVis.h
@@ -18,24 +18,28 @@ namespace rtrt {
 class VolumeVis : public Object, public Material {
 protected:
   Vector diag;
+  Vector inv_diag;
   BrickArray3<float> data;
   //ScalarTransform1D<float,Material*> *matl_transform;
   float data_min, data_max, data_diff_inv;
   int nx, ny, nz;
   Point min, max;
-  Array1<Material*> matls;
+  Array1<Color*> matls;
   int nmatls;
+  double spec_coeff, ambient, diffuse, specular;
   Array1<float> alphas;
   int nalphas;
   //  ScalarTransform1D<float,float> *alpha_transform;
   float delta_x2, delta_y2, delta_z2;
   int bound(const int val, const int min, const int max);
-  
+  Color color(const Vector &N, const Vector &V, const Vector &L, 
+	      const Color &object_color, const Color &light_color);
 public:
   VolumeVis(BrickArray3<float>& data, float data_min, float data_max,
 	    int nx, int ny, int nz,
-	    Point min, Point max, const Array1<Material*> &matls, int nmatls,
-	    const Array1<float> &alphas, int nalphas);
+	    Point min, Point max, const Array1<Color*> &matls, int nmatls,
+	    const Array1<float> &alphas, int nalphas, double spec_coeff,
+	    double ambient, double diffuse, double specular);
   virtual ~VolumeVis();
   virtual void intersect(const Ray& ray, HitInfo& hit, DepthStats* st,
 			 PerProcessorContext*);
