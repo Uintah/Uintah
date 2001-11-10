bf619fc52675738fae3bb50a3779a5448605cb29
Wayne Witzel <witzel@sci.utah.edu>
2001-11-10 17:43:22 +0000

    Needed to change format string when calling compare_sus_runs.

--- Packages/Uintah/Test/helpers/runSusTest.py
+++ Packages/Uintah/Test/helpers/runSusTest.py
@@ -55,7 +55,7 @@ def runSusTest(test, mode, susdir, algo, do_restart = "no"):
     errors_to = environ['ERRORS_TO']
     if environ['ERRORMAIL'] != "yes":
 	errors_to = ""
-    rc = system("compare_sus_runs %s %s/%s-%s %s/%s-%s %s '%s' > compare_sus_runs.log 2>&1" % (testname, test_root, compare_root, susdir, errors_to))
+    rc = system("compare_sus_runs %s %s %s %s '%s' > compare_sus_runs.log 2>&1" % (testname, test_root, compare_root, susdir, errors_to))
     if rc != 0:
 	if rc != 65280:
     	    print "\t*** Warning, test %s failed uda comparison with error code %s" % (testname, rc)
