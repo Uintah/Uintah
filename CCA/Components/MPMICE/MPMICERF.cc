06f604472df35db05848b434bfa99bcd6f94781a
Todd Harman <harman@sci.utah.edu>
2001-11-30 23:45:12 +0000

    R H O _ M I C R O   - >   S P _ V O L computeRateFormPressure()   - changed dwindex to indx   - carry forward sp_vol_CC   - calculation of rho_CC is now identical to what is done in     computeEquilibrationPressure()

--- CCA/Components/MPMICE/MPMICERF.cc
+++ CCA/Components/MPMICE/MPMICERF.cc
@@ -34,6 +34,7 @@ void MPMICE::computeRateFormPressure(const ProcessorGroup*,
     StaticArray<double> compressibility(numALLMatls);
     StaticArray<CCVariable<double> > vol_frac(numALLMatls);
     StaticArray<CCVariable<double> > rho_micro(numALLMatls);
+    StaticArray<CCVariable<double> > sp_vol_new(numALLMatls);
     StaticArray<CCVariable<double> > rho_CC(numALLMatls);
     StaticArray<CCVariable<double> > speedSound_new(numALLMatls);
     StaticArray<CCVariable<double> > f_theta(numALLMatls);
@@ -50,37 +51,40 @@ void MPMICE::computeRateFormPressure(const ProcessorGroup*,
     
     for (int m = 0; m < numALLMatls; m++) {
       Material* matl = d_sharedState->getMaterial( m );
-      int dwindex = matl->getDWIndex();
+      int indx = matl->getDWIndex();
       ICEMaterial* ice_matl = dynamic_cast<ICEMaterial*>(matl);
       MPMMaterial* mpm_matl = dynamic_cast<MPMMaterial*>(matl);
       if(ice_matl){                    // I C E
-        old_dw->get(Temp[m],   Ilb->temp_CCLabel, dwindex, patch,Ghost::None,0);
+        old_dw->get(Temp[m],   Ilb->temp_CCLabel,  indx,patch,Ghost::None,0);
         old_dw->get(rho_top[m],Ilb->rho_CC_top_cycleLabel,
-                                            dwindex, patch,Ghost::None,0);
+                                                   indx,patch,Ghost::None,0);
         old_dw->get(sp_vol_CC[m],
-                            Ilb->sp_vol_CCLabel,dwindex,patch,Ghost::None,0);
+                               Ilb->sp_vol_CCLabel,indx,patch,Ghost::None,0);
         cv[m]    = ice_matl->getSpecificHeat();
         gamma[m] = ice_matl->getGamma();
       }
       if(mpm_matl){                    // M P M    
-        new_dw->get(Temp[m],   MIlb->temp_CCLabel,dwindex, patch,Ghost::None,0);
-        new_dw->get(mat_vol[m],MIlb->cVolumeLabel,dwindex, patch,Ghost::None,0);
-        new_dw->get(mass_CC[m],MIlb->cMassLabel,  dwindex, patch,Ghost::None,0);
+        new_dw->get(Temp[m],   MIlb->temp_CCLabel,indx, patch,Ghost::None,0);
+        new_dw->get(mat_vol[m],MIlb->cVolumeLabel,indx, patch,Ghost::None,0);
+        new_dw->get(mass_CC[m],MIlb->cMassLabel,  indx, patch,Ghost::None,0);
         cv[m] = mpm_matl->getSpecificHeat();
       }
-      new_dw->allocate(rho_CC[m],    Ilb->rho_CCLabel,       dwindex, patch);
-      new_dw->allocate(vol_frac[m],  Ilb->vol_frac_CCLabel,  dwindex, patch);
-      new_dw->allocate(f_theta[m],   Ilb->f_theta_CCLabel,   dwindex, patch);
-      new_dw->allocate(matl_press[m],Ilb->matl_press_CCLabel,dwindex, patch);
-      new_dw->allocate(rho_micro[m], Ilb->rho_micro_CCLabel, dwindex, patch);
-      new_dw->allocate(speedSound_new[m],Ilb->speedSound_CCLabel,dwindex,patch);
+      new_dw->allocate(sp_vol_new[m],Ilb->sp_vol_CCLabel,    indx, patch);
+      new_dw->allocate(rho_CC[m],    Ilb->rho_CCLabel,       indx, patch);
+      new_dw->allocate(vol_frac[m],  Ilb->vol_frac_CCLabel,  indx, patch);
+      new_dw->allocate(f_theta[m],   Ilb->f_theta_CCLabel,   indx, patch);
+      new_dw->allocate(matl_press[m],Ilb->matl_press_CCLabel,indx, patch);
+      new_dw->allocate(rho_micro[m], Ilb->rho_micro_CCLabel, indx, patch);
+      new_dw->allocate(speedSound_new[m],Ilb->speedSound_CCLabel,indx,patch);
       speedSound_new[m].initialize(0.0);
     }
     
     press_new.initialize(0.0);
 
-    // Compute rho_micro, speedSound, volfrac, rho_CC
+    //__________________________________
+    // Compute rho_micro,matl_press, total_vol, speedSound
     for (CellIterator iter = patch->getExtraCellIterator();!iter.done();iter++){
+      IntVector c = *iter;
       double total_mat_vol = 0.0;
       for (int m = 0; m < numALLMatls; m++) {
         Material* matl = d_sharedState->getMaterial( m );
@@ -88,56 +92,78 @@ void MPMICE::computeRateFormPressure(const ProcessorGroup*,
         MPMMaterial* mpm_matl = dynamic_cast<MPMMaterial*>(matl);
 
         if(ice_matl){                // I C E
-         rho_micro[m][*iter] = 1.0/sp_vol_CC[m][*iter];
-         ice_matl->getEOS()->computePressEOS(rho_micro[m][*iter],gamma[m],
-                                         cv[m],Temp[m][*iter],
+         rho_micro[m][c] = 1.0/sp_vol_CC[m][c];
+         ice_matl->getEOS()->computePressEOS(rho_micro[m][c],gamma[m],
+                                         cv[m],Temp[m][c],
                                          press_eos[m],dp_drho[m],dp_de[m]);
          
          compressibility[m]=
                      ice_matl->getEOS()->getCompressibility(press_eos[m]);
 
-          mat_mass[m]   = rho_top[m][*iter] * cell_vol;
-         mat_volume[m] = mat_mass[m] * sp_vol_CC[m][*iter];
+         mat_mass[m]   = rho_top[m][c] * cell_vol;
+         mat_volume[m] = mat_mass[m] * sp_vol_CC[m][c];
 
          tmp = dp_drho[m] + dp_de[m] * 
-           (press_eos[m]/(rho_micro[m][*iter]*rho_micro[m][*iter]));
+           (press_eos[m]*(sp_vol_CC[m][c] * sp_vol_CC[m][c]));  
         } 
 
         if(mpm_matl){                //  M P M
-          rho_micro[m][*iter] = mass_CC[m][*iter]/mat_vol[m][*iter];
-           mat_mass[m]   = mass_CC[m][*iter];
+          rho_micro[m][c] = mass_CC[m][c]/mat_vol[m][c];
+          mat_mass[m]     = mass_CC[m][c];
 
           compressibility[m]=
               mpm_matl->getConstitutiveModel()->getCompressibility();
 
           mpm_matl->getConstitutiveModel()->
-            computePressEOSCM(rho_micro[m][*iter],press_eos[m], press_ref,
+            computePressEOSCM(rho_micro[m][c],press_eos[m], press_ref,
                               dp_drho[m], tmp,mpm_matl);
 
-          mat_volume[m] = mat_vol[m][*iter];
+          mat_volume[m] = mat_vol[m][c];
         }              
-
-       matl_press[m][*iter] = press_eos[m];
+        matl_press[m][c] = press_eos[m];
         total_mat_vol += mat_volume[m];
-        speedSound_new[m][*iter] = sqrt(tmp);
-
+        speedSound_new[m][c] = sqrt(tmp);
        }  // for ALLMatls...
-
+       
+       
+      //__________________________________
+      // Compute 1/f_theta
        double f_theta_denom = 0.0;
        for (int m = 0; m < numALLMatls; m++) {
-         vol_frac[m][*iter] = mat_volume[m]/total_mat_vol;
-         rho_CC[m][*iter] = vol_frac[m][*iter]*rho_micro[m][*iter];
-        f_theta_denom += vol_frac[m][*iter]*compressibility[m];
+         vol_frac[m][c] = mat_volume[m]/total_mat_vol;
+         f_theta_denom += vol_frac[m][c]*compressibility[m];
        }
+       //__________________________________
+       // Compute press_new
        for (int m = 0; m < numALLMatls; m++) {
-       f_theta[m][*iter] = vol_frac[m][*iter]*compressibility[m]/f_theta_denom;
-       press_new[*iter] += f_theta[m][*iter]*matl_press[m][*iter];
-       rho_CC[m][*iter]  = mat_mass[m]/cell_vol;
+         f_theta[m][c] = vol_frac[m][c]*compressibility[m]/f_theta_denom;
+         press_new[c] += f_theta[m][c]*matl_press[m][c];
        }
     } // for(CellIterator...)
-
-
-    //  Set BCs on density and pressure
+    
+    //__________________________________
+    // Now change how rho_CC is defined to 
+    // rho_CC = mass/cell_volume  NOT mass/mat_volume 
+    for (int m = 0; m < numALLMatls; m++) {
+      Material* matl = d_sharedState->getMaterial( m );
+      ICEMaterial* ice_matl = dynamic_cast<ICEMaterial*>(matl);
+      MPMMaterial* mpm_matl = dynamic_cast<MPMMaterial*>(matl);
+      if(ice_matl){
+        for (CellIterator iter = patch->getExtraCellIterator();!iter.done();iter++){
+          IntVector c = *iter;
+          rho_CC[m][c] = rho_top[m][c];
+        }
+      }
+      if(mpm_matl){
+        for (CellIterator iter = patch->getExtraCellIterator();!iter.done();iter++){
+          IntVector c = *iter;
+          rho_CC[m][c] = mass_CC[m][c]/cell_vol;
+        }
+      }
+    } 
+    
+    //__________________________________
+    //  Update boundary conditions
     for (int m = 0; m < numALLMatls; m++)   {
        Material* matl = d_sharedState->getMaterial( m );
        ICEMaterial* ice_matl = dynamic_cast<ICEMaterial*>(matl);
@@ -145,24 +171,32 @@ void MPMICE::computeRateFormPressure(const ProcessorGroup*,
        if(ice_matl){
          d_ice->setBC(rho_CC[m],   "Density" ,patch, dwi);
        }  
-       d_ice->setBC(matl_press[m],rho_micro[SURROUND_MAT],"Pressure",patch,dwi);
+       d_ice->setBC(matl_press[m],rho_micro[SURROUND_MAT],
+                    "rho_micro", "Pressure", patch, 0, new_dw);
     }  
+    d_ice->setBC(press_new, rho_micro[SURROUND_MAT], 
+                "rho_micro", "Pressure", patch, 0,  new_dw);
 
     //__________________________________
-    //  Update BC
-    d_ice->setBC(press_new, rho_micro[SURROUND_MAT], "Pressure",patch,0);
+    // compute sp_vol_CC
+    for (int m = 0; m < numALLMatls; m++)   {
+      for (CellIterator iter=patch->getExtraCellIterator();!iter.done();iter++) {
+        IntVector c = *iter;
+        sp_vol_new[m][c] = 1.0/rho_micro[m][c];
 
+      }
+    } 
     //__________________________________
-    //    Put all matls into new dw
+    //    carry sp_vol_CC forward for MPMICE:computeEquilibrationPressure
     for (int m = 0; m < numALLMatls; m++)   {
       Material* matl = d_sharedState->getMaterial( m );
-      int dwindex = matl->getDWIndex();
-      new_dw->put( vol_frac[m],      Ilb->vol_frac_CCLabel,   dwindex, patch);
-      new_dw->put( f_theta[m],       Ilb->f_theta_CCLabel,    dwindex, patch);
-      new_dw->put( matl_press[m],    Ilb->matl_press_CCLabel, dwindex, patch);
-      new_dw->put( speedSound_new[m],Ilb->speedSound_CCLabel, dwindex, patch);
-      new_dw->put( rho_micro[m],     Ilb->rho_micro_CCLabel,  dwindex, patch);
-      new_dw->put( rho_CC[m],        Ilb->rho_CCLabel,        dwindex, patch);
+      int indx = matl->getDWIndex();
+      new_dw->put( sp_vol_new[m], Ilb->sp_vol_CCLabel,     indx, patch); 
+      new_dw->put( vol_frac[m],      Ilb->vol_frac_CCLabel,   indx, patch);
+      new_dw->put( f_theta[m],       Ilb->f_theta_CCLabel,    indx, patch);
+      new_dw->put( matl_press[m],    Ilb->matl_press_CCLabel, indx, patch);
+      new_dw->put( speedSound_new[m],Ilb->speedSound_CCLabel, indx, patch);
+      new_dw->put( rho_CC[m],        Ilb->rho_CCLabel,        indx, patch);
     }
     new_dw->put(press_new,Ilb->press_equil_CCLabel,0,patch);
 
