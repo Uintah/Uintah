172a71787a4f6247bf875a260139a707ebabf198
James Bigler <bigler@sci.utah.edu>
2001-12-04 03:53:02 +0000

    Made functions inlined and made the index calculation external to the function so that it could be called outside.  Added interpolation function.

--- Packages/rtrt/Core/ScalarTransform1D.h
+++ Packages/rtrt/Core/ScalarTransform1D.h
@@ -38,17 +38,69 @@ public:
 
   // access members
 
+  ////////////////////////////////////////////////////////////
   // does no checking for whether val lies between min and max
-  RType lookup(const LType val) const;
+  inline int get_lookup_index(const LType val) const {
+    //return (int)((val - min) * linear_factor * (results->size()-1) + 0.5);
+    return (int)((val - min) * linear_factor * (results->size()-1));
+  }
+  inline RType lookup(const LType val) const {
+    return (*results)[get_lookup_index(val)];
+  }
+    
+  ////////////////////////////////////////////////////////////
   // same as lookup() though it clams val between min and max
-  RType lookup_bound(const LType val) const;
-  // looks up with the passed in min and max
-  RType lookup(const LType val, const LType _min, const LType _max);
-  // clamps index to fit in the range of good values
-  RType lookup_index(const int index) const;
-  // returns &results[i], no bounds checking
-  RType operator[](const int index) const;
+  inline int get_bound_lookup_index(const LType val) const {
+    //int index=(int)((val - min) * linear_factor * (results->size()-1) + 0.5);
+    int index = (int)((val - min) * linear_factor * (results->size()-1));
+    return bound(index, 0, results->size()-1);
+    
+  }
+  inline RType lookup_bound(const LType val) const {
+    return (*results)[get_bound_lookup_index(val)];
+  }
 
+
+  ////////////////////////////////////////////////////////////
+  // looks up with the passed in min and max
+  inline int get_lookup_index(const LType val, const LType _min,
+			      const LType _max) const {
+    //int index=(int)((val - _min) / (_max - _min) * (results->size()-1) + 0.5)
+    int index = (int)((val - _min) / (_max - _min) * (results->size()-1));
+    return bound(index, 0, results->size()-1);
+  }
+  inline RType lookup(const LType val, const LType _min,
+		      const LType _max) const {
+    return (*results)[get_lookup_index(val,_min,_max)];
+  }
+
+
+  ////////////////////////////////////////////////////////////
+  // clamps index to fit in the range of good values
+  inline RType lookup_with_index(const int index) const {
+    return (*results)[bound(index, 0, results->size()-1)];
+  }
+
+
+  ////////////////////////////////////////////////////////////
+  // returns &results[i], no bounds checking
+  inline RType& operator[](const int index) const {
+    return (*results)[index];
+  }
+
+  ////////////////////////////////////////////////////////////
+  // returns the interpolated value of
+  inline RType interpolate(const LType val) const {
+    double step = (val - min) * linear_factor * (results->size()-1);
+    int x_low = bound((int)step, 0, results->size()-2);
+    double x_weight_low = (x_low+1) - step;
+    return (*results)[x_low] * x_weight_low +
+      (*results)[x_low+1] * (1 - x_weight_low);
+  }
+  
+  Array1<RType> *get_results_ptr() {
+    return results;
+  }
   // min and max operators
   void get_min_max(LType &_min, LType &_max) const;
   void scale(LType _min, LType _max);
@@ -67,35 +119,6 @@ ScalarTransform1D<LType,RType>::ScalarTransform1D(const Array1<RType> &r):
   min(0), max(1), linear_factor(1), isscaled(false)
 {
   results = new Array1<RType>(r);
-}
-
-template <class LType, class RType>
-RType ScalarTransform1D<LType,RType>::lookup(const LType val) const {
-  int index = (int)((val - min) * linear_factor * (results->size()-1) + 0.5);
-  return (*results)[index];
-}
-
-template <class LType, class RType>
-RType ScalarTransform1D<LType,RType>::lookup_bound(const LType val) const {
-  int index = (int)((val - min) * linear_factor * (results->size()-1) + 0.5);
-  return (*results)[bound(index, 0, results->size()-1)];
-}
-
-template <class LType, class RType>
-RType ScalarTransform1D<LType,RType>::lookup(const LType val,const LType _min,
-					     const LType _max) {
-  int index = (int)((val - _min) / (_max - _min) * (results->size()-1) + 0.5);
-  return (*results)[bound(index, 0, results->size()-1)];
-}
-
-template <class LType, class RType>
-RType ScalarTransform1D<LType,RType>::lookup_index(const int index) const {
-  return (*results)[bound(index, 0, results->size()-1)];
-}
-
-template <class LType, class RType>
-RType ScalarTransform1D<LType,RType>::operator[](const int index) const {
-  return (*results)[index];
 }
 
 // min and max operators
