2ef7e8d6e2f8d7d5a23999eed6f9da8fc2334bee
James Bigler <bigler@sci.utah.edu>
2001-12-04 03:48:20 +0000

    Moved the color and alpha transfer funtions from VolumeVis to here.  Using ScalarTransform1D for alpha and color lookups.  Fixed histogram creation using BrickArray3D.  Added coloring of histogram based on the color transfer function.

--- src/Packages/rtrt/Core/VolumeVisDpy.h
+++ src/Packages/rtrt/Core/VolumeVisDpy.h
@@ -5,6 +5,8 @@
 #include <GL/glx.h>
 #include <GL/glu.h>
 #include <Packages/rtrt/Core/Array1.h>
+#include <Packages/rtrt/Core/Color.h>
+#include <Packages/rtrt/Core/ScalarTransform1D.h>
 #include <Core/Thread/Runnable.h>
 
 namespace rtrt {
@@ -23,7 +25,11 @@ class VolumeVisDpy : public Runnable {
   char* in_file;
   float data_min, data_max;
   float scale;
+  float original_t_inc, current_t_inc, t_inc;
   
+  ScalarTransform1D<float,Color*> color_transform;
+  ScalarTransform1D<float,float> alpha_transform;
+
   void compute_hist(GLuint fid);
   void draw_hist(GLuint fid, XFontStruct* font_struct);
   //  void move(float* range, int x, int y, int& redraw_range);
@@ -31,12 +37,14 @@ class VolumeVisDpy : public Runnable {
   void write_data_file(char *out_file);
   
 public:
-  VolumeVisDpy(char *in_file=0);
+  VolumeVisDpy(Array1<Color*> *matls, Array1<float> *alphas, float t_inc,
+	       char *in_file=0);
   virtual ~VolumeVisDpy();
   void attach(VolumeVis* volume);
   virtual void run();
   void setup_vars();
   void animate(bool& changed);
+  void rescale_alphas(float new_t_inc);
 };
 
 } // end namespace rtrt
