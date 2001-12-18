471089a33e5dcf397089c0d6cbcc3ca3dc5d1d7c
Todd Harman <harman@sci.utah.edu>
2001-12-18 13:11:32 +0000

    Somehow a older version of fillFaceNormal() was accidentally checked in,   this has been corrected.

--- Core/Grid/CCVariable_special.cc
+++ Core/Grid/CCVariable_special.cc
@@ -3,15 +3,16 @@
 
 namespace Uintah {
 
-// Use to apply symmetry boundary conditions.  On the
-// indicated face, replace the component of the vector
-// normal to the face with 0.0
+ // Use to apply symmetry boundary conditions. 
+ // Tangential components: zero gradient
+ // Normal components:   -(interior value)
+ // This only takes care of the symmetric faces
+ // Need to set the rest of the faces with fillFaceFlux
 template <>
 void
 CCVariable<Vector>::fillFaceNormal(Patch::FaceType face,
 				   IntVector offset)
 {
-#if 0
   IntVector low,hi;
   low = getLowIndex() + offset;
   hi = getHighIndex() - offset;
@@ -19,54 +20,67 @@ CCVariable<Vector>::fillFaceNormal(Patch::FaceType face,
   case Patch::xplus:
     for (int j = low.y(); j<hi.y(); j++) {
       for (int k = low.z(); k<hi.z(); k++) {
-	(*this)[IntVector(hi.x()-1,j,k)] =
-	  Vector(0.0,(*this)[IntVector(hi.x()-1,j,k)].y(),
-		 (*this)[IntVector(hi.x()-1,j,k)].z());
+        (*this)[IntVector(hi.x()-1,j,k)] =
+          Vector(-(*this)[IntVector(hi.x()-2,j,k)].x(),
+                 (*this)[IntVector(hi.x()-2,j,k)].y(),
+                   (*this)[IntVector(hi.x()-2,j,k)].z());
+         //cout<<"fillFaceNORMAL Xplus "<<IntVector(hi.x()-1,j,k)<<endl;
+
       }
     }
     break;
   case Patch::xminus:
     for (int j = low.y(); j<hi.y(); j++) {
       for (int k = low.z(); k<hi.z(); k++) {
-	(*this)[IntVector(low.x(),j,k)] = 
-	  Vector(0.0,(*this)[IntVector(low.x(),j,k)].y(),
-		 (*this)[IntVector(low.x(),j,k)].z());
+        (*this)[IntVector(low.x(),j,k)] = 
+          Vector(-(*this)[IntVector(low.x()+1,j,k)].x(),
+                 (*this)[IntVector(low.x()+1,j,k)].y(),
+                   (*this)[IntVector(low.x()+1,j,k)].z());
+         //cout<<"fillFaceNORMAL Xminus "<<IntVector(low.x(),j,k)<<endl;
       }
     }
     break;
   case Patch::yplus:
     for (int i = low.x(); i<hi.x(); i++) {
       for (int k = low.z(); k<hi.z(); k++) {
-	(*this)[IntVector(i,hi.y()-1,k)] =
-	  Vector((*this)[IntVector(i,hi.y()-1,k)].x(),0.0,
-		 (*this)[IntVector(i,hi.y()-1,k)].z());
+        (*this)[IntVector(i,hi.y()-1,k)] =
+          Vector( (*this)[IntVector(i,hi.y()-2,k)].x(),
+                -(*this)[IntVector(i,hi.y()-2,k)].y(),
+                   (*this)[IntVector(i,hi.y()-2,k)].z());
+         //cout<<"fillFaceNORMAL Yplus "<<IntVector(i,hi.y()-1,k)<<endl;
       }
     }
     break;
   case Patch::yminus:
     for (int i = low.x(); i<hi.x(); i++) {
       for (int k = low.z(); k<hi.z(); k++) {
-	(*this)[IntVector(i,low.y(),k)] =
-	  Vector((*this)[IntVector(i,low.y(),k)].x(),0.0,
-		 (*this)[IntVector(i,low.y(),k)].z());
+        (*this)[IntVector(i,low.y(),k)] =
+          Vector( (*this)[IntVector(i,low.y()+1,k)].x(),
+                -(*this)[IntVector(i,low.y()+1,k)].y(),
+                   (*this)[IntVector(i,low.y()+1,k)].z());
+         //cout<<"fillFaceNORMAL Yminus "<<IntVector(i,low.y(),k)<<endl;
       }
     }
     break;
   case Patch::zplus:
     for (int i = low.x(); i<hi.x(); i++) {
       for (int j = low.y(); j<hi.y(); j++) {
-	(*this)[IntVector(i,j,hi.z()-1)] =
-	  Vector((*this)[IntVector(i,j,hi.z()-1)].x(),
-		 (*this)[IntVector(i,j,hi.z()-1)].y(),0.0);
+        (*this)[IntVector(i,j,hi.z()-1)] =
+          Vector( (*this)[IntVector(i,j,hi.z()-2)].x(),
+                   (*this)[IntVector(i,j,hi.z()-2)].y(),
+                -(*this)[IntVector(i,j,hi.z()-2)].z());
+         //cout<<"fillFaceNORMAL Zplus "<<IntVector(i,j,hi.z()-1)<<endl;
       }
     }
     break;
   case Patch::zminus:
     for (int i = low.x(); i<hi.x(); i++) {
       for (int j = low.y(); j<hi.y(); j++) {
-	(*this)[IntVector(i,j,low.z())] =
-	  Vector((*this)[IntVector(i,j,low.z())].x(),
-		 (*this)[IntVector(i,j,low.z())].y(),0.0);
+        (*this)[IntVector(i,j,low.z())] =
+          Vector( (*this)[IntVector(i,j,low.z()+1)].x(),
+                   (*this)[IntVector(i,j,low.z()+1)].y(),
+                -(*this)[IntVector(i,j,low.z()+1)].z());
+         //cout<<"fillFaceNORMAL Zminus "<<IntVector(i,j,low.z())<<endl;
       }
     }
     break;
@@ -75,7 +89,6 @@ CCVariable<Vector>::fillFaceNormal(Patch::FaceType face,
   case Patch::invalidFace:
     break;
   }
-#endif
 }
 
 } // end namespace Uintah
