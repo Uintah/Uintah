2c5b2fa84ead52db926889800d2b71d5f445862c
Wayne Witzel <witzel@sci.utah.edu>
2002-01-10 17:46:28 +0000

    Fixed the way it gets the 'periodic' xml data

--- CCA/Ports/DataArchive.cc
+++ CCA/Ports/DataArchive.cc
@@ -215,8 +215,8 @@ DataArchive::queryGrid( double time )
 		  || r.getNodeName().equals("cellspacing")){
 	  // Nothing - handled above
 	} else if(r.getNodeName().equals("periodic")) {
-	  if(!get(r, "periodic", periodicBoundaries))
-	    throw InternalError("Error parsing peridoc");
+	  if(!get(n, "periodic", periodicBoundaries))
+	    throw InternalError("Error parsing periodoc");
 	} else if(r.getNodeType() != DOM_Node::TEXT_NODE){
 	  cerr << "WARNING: Unknown level data: " << ::toString(n.getNodeName()) << '\n';
 	}
