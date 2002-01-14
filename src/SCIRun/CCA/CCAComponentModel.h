52e51b9e0cc77dd7a743df762f9b0c94aa145dc9
Steven G. Parker <sparker@sci.utah.edu>
2002-01-14 04:52:58 +0000

    Rename Core/CCA/ccaspec to Core/CCA/spec Get rid of duplicate parameter name in header file

--- src/SCIRun/CCA/CCAComponentModel.h
+++ src/SCIRun/CCA/CCAComponentModel.h
@@ -31,7 +31,7 @@
 
 #include <SCIRun/ComponentModel.h>
 #include <SCIRun/ComponentInstance.h>
-#include <Core/CCA/ccaspec/cca_sidl.h>
+#include <Core/CCA/spec/cca_sidl.h>
 #include <string>
 #include <map>
 
