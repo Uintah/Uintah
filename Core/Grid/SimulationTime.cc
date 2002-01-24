16120e5415ac0984461df7eb70d14488a0c0982c
J. Davison de St. Germain <dav@sci.utah.edu>
2002-01-24 20:51:43 +0000

    added ability to specify exact number of time steps for a simulation run

--- Core/Grid/SimulationTime.cc
+++ Core/Grid/SimulationTime.cc
@@ -15,5 +15,21 @@ SimulationTime::SimulationTime(const ProblemSpecP& params)
   time_ps->require("delt_min", delt_min);
   time_ps->require("delt_max", delt_max);
   time_ps->require("timestep_multiplier", delt_factor);
+
+  string max_iters;
+  if( time_ps->getOptional( "max_iterations", max_iters ) )
+    {
+      num_time_steps = atoi( max_iters.c_str() );
+      if( num_time_steps < 1 )
+	{
+	  cerr << "Negative number of time steps is not allowed.\n";
+	  cerr << "reseting to MAXINT time steps\n";
+	  num_time_steps = MAXINT;
+	}
+    }
+  else
+    {
+      num_time_steps = MAXINT;
+    }
 }
 
