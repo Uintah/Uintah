310324cfab30f7ad5ab8de0ca5d750ed70f0609c
J. Davison de St. Germain <dav@sci.utah.edu>
2002-01-24 22:08:38 +0000

    fixed reading of max_iterations.  Thanks Todd.

--- src/Packages/Uintah/Core/Grid/SimulationTime.cc
+++ src/Packages/Uintah/Core/Grid/SimulationTime.cc
@@ -17,19 +17,13 @@ SimulationTime::SimulationTime(const ProblemSpecP& params)
   time_ps->require("delt_max", delt_max);
   time_ps->require("timestep_multiplier", delt_factor);
 
-  string max_iters;
-  if( time_ps->getOptional( "max_iterations", max_iters ) )
+  num_time_steps = MAXINT;
+  time_ps->get( "max_iterations", num_time_steps );
+
+  if( num_time_steps < 1 )
     {
-      num_time_steps = atoi( max_iters.c_str() );
-      if( num_time_steps < 1 )
-	{
-	  cerr << "Negative number of time steps is not allowed.\n";
-	  cerr << "reseting to MAXINT time steps\n";
-	  num_time_steps = MAXINT;
-	}
-    }
-  else
-    {
+      cerr << "Negative number of time steps is not allowed.\n";
+      cerr << "reseting to MAXINT time steps\n";
       num_time_steps = MAXINT;
     }
 }
