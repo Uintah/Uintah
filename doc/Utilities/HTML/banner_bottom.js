6be20f12ca142731a0a5c7026bab9e6d7657efa9
Ted Dustman <dustman@sci.utah.edu>
2002-04-16 22:14:33 +0000

    Code for generating SCIRun bottom banner

---
+++ doc/Utilities/HTML/banner_bottom.js
@@ -0,0 +1,17 @@
+// Generate bottom banner.
+document.write('<div class="bottom-banner-margins">')
+document.write('<table width="100%" height="32" cellpadding="0" cellspacing="0" border="0">')
+document.write('<tr>')
+document.write('<td width="%100" align="left" background="',treetop,'doc/Utilities/Figures/banner_bottom_fill.jpg">')
+document.write('<img border="0" height="32" width="444" src="',treetop,'doc/Utilities/Figures/banner_bottom.jpg">')
+document.write('</td>')
+document.write('</tr>')
+document.write('</table>')
+document.write('</div>')
+document.write('<center>')
+document.write('<font face="arial, helvetica, sans-serif" size="-2">Scientific\
+Computing and Imaging Institute &#149; 50 S. Central Campus Dr. Rm\
+3490 &#149; Salt Lake City, UT 84112<br>\
+\
+(801) 585-1867 &#149; fax: (801) 585-6513 &#149; <a href="http://www.utah.edu/disclaimer/disclaimer_home.html">Disclaimer</a></font>')
+document.write('</center>')
