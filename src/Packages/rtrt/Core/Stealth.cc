72644268ac26232df1dee4d60f46543720cf563b
J. Davison de St. Germain <dav@sci.utah.edu>
2002-05-03 05:51:48 +0000

    initial commit of movable eye position

---
+++ src/Packages/rtrt/Core/Stealth.cc
@@ -0,0 +1,161 @@
+
+#include <Packages/rtrt/Core/Stealth.h>
+
+#include <math.h>
+
+using namespace rtrt;
+using namespace SCIRun;
+using std::cerr;
+
+Stealth::Stealth( double scale /* = 100 */ ) :
+  speed_( 0 ), horizontal_speed_( 0 ), vertical_speed_( 0 ),
+  pitch_speed_( 0 ), rotate_speed_( 0 ),
+  accel_cnt_( 0 ), horizontal_accel_cnt_( 0 ),
+  vertical_accel_cnt_( 0 ), pitch_accel_cnt_( 0 ), rotate_accel_cnt_( 0 ),
+  scale_( scale )
+{
+}
+
+Stealth::~Stealth()
+{
+}
+
+void
+Stealth::print()
+{
+  cerr << "Stealth print not done yet!\n";
+}
+
+void
+Stealth::slideRight()
+{
+  increase_a_speed( horizontal_speed_, horizontal_accel_cnt_ );
+}
+
+void
+Stealth::slideLeft()
+{
+  decrease_a_speed( horizontal_speed_, horizontal_accel_cnt_ );
+}
+
+void
+Stealth::goUp()
+{
+  increase_a_speed( vertical_speed_, vertical_accel_cnt_ );
+}
+
+void
+Stealth::goDown()
+{
+  decrease_a_speed( vertical_speed_, vertical_accel_cnt_ );
+}
+
+void
+Stealth::accelerate()
+{
+  increase_a_speed( speed_, accel_cnt_ );
+}
+
+void
+Stealth::decelerate()
+{
+  decrease_a_speed( speed_, accel_cnt_ );
+}
+
+void
+Stealth::increase_a_speed( double & speed, int & accel_cnt )
+{
+  accel_cnt++;
+
+  // Amount to scale acceleration by.  Probably need to find out "size"
+  // (dimensions?) of data set to set this appropriately.
+  double scale = 100;
+  
+  // Amount of acceleration doubles for each consecutive request.
+  double amount;
+
+  if( accel_cnt == 0 )
+    amount = 1 / scale;
+  else if( accel_cnt > 0 )
+    amount = pow(2.0, (double)accel_cnt - 1.0) / scale;
+  else
+    amount = pow(2.0, (double)(-accel_cnt)) / scale;
+
+  printf("accelerating by %lf\n", amount);
+
+  speed += amount;
+}
+
+void
+Stealth::decrease_a_speed( double & speed, int & accel_cnt )
+{
+  accel_cnt--;
+
+  // Amount to scale acceleration by.  Probably need to find out "size"
+  // (dimensions?) of data set to set this appropriately.
+  double scale = 100;
+  
+  // Amount of acceleration doubles for each consecutive request.
+  double amount = pow(2.0, (double)abs(accel_cnt)) / scale;
+
+  if( accel_cnt >= 0 )
+    amount = pow(2.0, accel_cnt) / scale;
+  else
+    amount = pow(2.0, (double)(-accel_cnt) - 1.0) / scale;
+
+  printf("reversing by %lf\n", amount);
+
+  speed -= amount;
+}
+
+void
+Stealth::turnRight()
+{
+  increase_a_speed( rotate_speed_, rotate_accel_cnt_ );
+}
+
+void
+Stealth::turnLeft()
+{
+  decrease_a_speed( rotate_speed_, rotate_accel_cnt_ );
+}
+
+void
+Stealth::pitchUp()
+{
+  increase_a_speed( pitch_speed_, pitch_accel_cnt_ );
+}
+
+void
+Stealth::pitchDown()
+{
+  decrease_a_speed( pitch_speed_, pitch_accel_cnt_ );
+}
+
+void
+Stealth::stopAllMovement()
+{
+  speed_ = 0;
+  horizontal_speed_ = 0;
+  vertical_speed_ = 0;
+  pitch_speed_ = 0;
+  rotate_speed_ = 0;
+
+  accel_cnt_ = 0;
+  horizontal_accel_cnt_ = 0;
+  vertical_accel_cnt_ = 0;
+  pitch_accel_cnt_ = 0;
+  rotate_accel_cnt_ = 0;
+}
+
+void
+Stealth::slowDown()
+{
+  double slow_factor = .9;
+  speed_            /= slow_factor;
+  horizontal_speed_ /= slow_factor;
+  vertical_speed_   /= slow_factor;
+  pitch_speed_      /= slow_factor;
+  rotate_speed_     /= slow_factor;
+}
+
---
+++ src/Packages/rtrt/Core/Stealth.h
@@ -0,0 +1,125 @@
+
+
+#ifndef STEALTH_H
+#define STEALTH_H 1
+
+#include <iostream>
+
+#include <Core/Geometry/Point.h>
+#include <Core/Geometry/Vector.h>
+
+namespace rtrt {
+
+using namespace SCIRun;
+using std::cout;
+
+// "Stealth" comes from the DoD term for an invisible watcher on the
+// simulated battlefield.  The stealths are used in relation to
+// cameras.  They contain movement information, but the camera
+// contains direction information.
+  
+class Stealth {
+
+  ////////////////////////////////////////////////////////
+  // Stealth Movement
+  //
+  //   This camera does NOT move like an airplane.
+  // The driver (user) can specify movement in the following
+  // manner.
+  //
+  //     +  Accelerate Forward
+  //     -  Accelerate Backward
+  //     <- Begin turning left 
+  //     -> Begin turning right
+  //     ^  Begin pitching forward (look down)
+  //     v  Begin pitching backward (look up)
+  //     7 (keypad) Accelerate to the left
+  //     9 (keypad) Accelerate to the right
+  //     0 (keypad) Stop ALL movement
+  //     . (keypad) Slow down (in all movements (including turns))
+  //
+  //   Acceleration is in respect to the direction that the eye is
+  // looking.  
+
+public:
+
+  // Default scale of 50 is good if the eye is 5 units (or so) from
+  // the area of interest and the area of interest is ~20 units across.
+  Stealth( double scale = 50 );
+  ~Stealth();
+
+  // Tells the eye to update its position based on its current
+  // velocity vector.
+  void updatePosition();
+
+  inline double getSpeed( int direction ) const {
+    switch( direction ) {
+    case 0:
+      return speed_;
+    case 1:
+      return horizontal_speed_;
+    case 2:
+      return vertical_speed_;
+    case 3:
+      return pitch_speed_;
+    case 4:
+      return rotate_speed_;
+    default:
+      cout << "Error in Stealth::getSpeed, bad direction " << direction 
+	   << "\n";
+      exit( 1 );
+      return 0.0;
+    }
+  }
+
+  // Slows down in all dimensions (pitch, speed, turn, etc);
+  void slowDown();
+  void stopAllMovement();
+
+  void slideLeft();
+  void slideRight();
+
+  void goUp();
+  void goDown();
+
+  void accelerate();
+  void decelerate();
+  void turnRight();
+  void turnLeft();
+  void pitchUp();
+  void pitchDown();
+
+  // Display the Stealth's speeds, etc.
+  void print();
+
+private:
+
+  void increase_a_speed( double & speed, int & accel_cnt );
+  void decrease_a_speed( double & speed, int & accel_cnt );
+
+  // Scale is based on the size of the "universe".  It effects how fast
+  // the stealth will move.
+  double scale_;
+
+  // Speeds (in units per frame)
+  double speed_;            // + forward, - backward
+  double horizontal_speed_; // + right, - left
+  double vertical_speed_;   // + up, - down
+  double pitch_speed_;      // + down, - up
+  double rotate_speed_;     // + right, - left
+
+  // Acceleration counts represent the number of velocity change 
+  // requests that the user has made.  They are used to judge how
+  // much velocity to add to the velocity vector.  The more requests,
+  // the faster we "accelerate" for the next similar request.
+  int accel_cnt_;            // + forward, - backward. 
+  int horizontal_accel_cnt_; // + right, - left
+  int vertical_accel_cnt_;   // + up, - down
+  int pitch_accel_cnt_;      // + up, - down
+  int rotate_accel_cnt_;     // + right, - left
+
+};
+
+} // end namespace rtrt
+
+#endif
