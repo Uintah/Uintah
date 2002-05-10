78977fb3da6964ff8fee3010395298ef0f667d14
Wayne Witzel <witzel@sci.utah.edu>
2002-05-10 20:26:45 +0000

    I didn't mean to make that last commit.  Anyway, I added "using SCIRun::InternalError" (yes, within namespace Uintah { ... }).

--- src/Packages/Uintah/Core/Grid/ComputeSet.h
+++ src/Packages/Uintah/Core/Grid/ComputeSet.h
@@ -12,7 +12,7 @@
 #include <sstream>
 
 namespace Uintah {
-  using namespace SCIRun::InternalError;
+  using SCIRun::InternalError;
   using std::vector;
   using std::cerr;
   using std::ostringstream;
