36dc7240fe5e09a9db9bc9eee848a46da9d6cfb0
Simon PREMOZE <premoze@sci.utah.edu>
2002-05-12 18:47:29 +0000

    Hierarchical grid.

---
+++ Packages/rtrt/Core/HierarchicalGrid.h
@@ -0,0 +1,52 @@
+#ifndef HIERARCHICALGRID_H
+#define HIERARCHICALGRID_H 1
+
+#include <Packages/rtrt/Core/Object.h>
+#include <Packages/rtrt/Core/Grid.h>
+#include <Packages/rtrt/Core/BBox.h>
+
+namespace rtrt {
+
+struct GridTree;
+struct BoundedObject;
+
+class HierarchicalGrid : public Grid {
+
+public:
+
+    //
+    // Create a hierarchical grid of maximum 3 levels deep
+    //
+    // nside -- number of grid cells at level1
+    // nSideLevel2 -- number of grid cells at level2
+    // nSideLevel3 -- number of grid cells at level3
+    // minObjects1 -- number of objects per cell before they are embedded in 
+    //               another grid at level1
+    // minObjects2 -- number of objects per cell before they are embedded in 
+    //               another grid at level2
+    //
+    
+    HierarchicalGrid( Object* obj, 
+		      int nside,
+		      int nSideLevel2, int nSideLevel3,
+		      int minObjects1, int minObjects2,
+		      int level );
+
+    virtual ~HierarchicalGrid( void );
+
+    virtual void preprocess(double maxradius, int& pp_offset, int& scratchsize);
+    // All other methods are the same as in parent grid class
+
+private:
+
+    int _nSidesLevel2, _nSidesLevel3;
+    int _minObjects1, _minObjects2;
+    int _level;
+
+    static int L1counter, L2counter, L3counter;
+
+};
+
+} // end namespace rtrt
+
+#endif
