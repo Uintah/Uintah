8fedd61b104199143134bd6d713c86557854fec6
Wayne Witzel <witzel@sci.utah.edu>
2002-05-16 19:14:12 +0000

    Handle multiple malloc_stats files that will now be generated by multi processor runs.  Note:  the highwater will now be the sum of highwaters of all of the processes.

--- Packages/Uintah/Test/helpers/highwater_check.pl
+++ Packages/Uintah/Test/helpers/highwater_check.pl
@@ -3,21 +3,28 @@
 $testfilename = $ARGV[0];
 $comparefilename = $ARGV[1];
 
+$test_highwater = 0;
+$compare_highwater = 0;
+
 open(TESTFILE, $testfilename);
 open(COMPAREFILE, $comparefilename);
 
 @test_text = <TESTFILE>;
 $test_text = join("", @test_text);
-$test_text =~ /highwater alloc:\s*(\w+)\s/;
-$test_highwater = $1;
+while ($test_text =~ /highwater alloc:\s*(\w+)\s/) {
+    $test_highwater += $1;
+    $test_text = $';
+}
 
 @compare_text = <COMPAREFILE>;
 $compare_text = join("", @compare_text);
-$compare_text =~ /highwater alloc:\s*(\w+)\s/;
-$compare_highwater = $1;
+while ($compare_text =~ /highwater alloc:\s*(\w+)\s/) {
+    $compare_highwater += $1;
+    $compare_text = $';
+}
 
-print "New memory highwater " . $test_highwater . "\n";
-print "Old memory highwater " . $compare_highwater . "\n";
+print "New total memory highwater " . $test_highwater . "\n";
+print "Old total memory highwater " . $compare_highwater . "\n";
 if ($test_highwater > 1.5 * $compare_highwater) {
     print "New memory exceeds old memory in highwater usage by more than 50%" . "\n";
     exit 1;
