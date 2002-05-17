2f92126f01ba078bac44679673f418e9e961b379
David Weinstein <dmw@sci.utah.edu>
2002-05-17 03:00:16 +0000

    - Cache group bboxes

--- src/Packages/rtrt/Core/Group.cc
+++ src/Packages/rtrt/Core/Group.cc
@@ -1,6 +1,8 @@
 
 #include <Packages/rtrt/Core/Group.h>
+#include <Packages/rtrt/Core/HitInfo.h>
 #include <iostream>
+#include <values.h>
 
 using namespace rtrt;
 
@@ -17,17 +19,63 @@ void Group::light_intersect(Light* light, const Ray& ray, HitInfo& hit,
 			    double dist, Color& atten, DepthStats* st,
 			    PerProcessorContext* ppc)
 {
-    for(int i=0;i<objs.size();i++){
-	objs[i]->light_intersect(light, ray, hit, dist, atten, st, ppc);
-    }
+  double min_t = MAXDOUBLE;
+  if (!bbox.intersect(ray, min_t)) return;
+
+  int i;
+  for(i=0;i<objs.size();i++){
+    objs[i]->light_intersect(light, ray, hit, dist, atten, st, ppc);
+  }
 }
 
 void Group::intersect(const Ray& ray, HitInfo& hit, DepthStats* st,
 		      PerProcessorContext* ppc)
 {
-    for(int i=0;i<objs.size();i++){
-	objs[i]->intersect(ray, hit, st, ppc);
+#if 1
+  for(int i=0;i<objs.size();i++){
+    objs[i]->intersect(ray, hit, st, ppc);
+  }
+#else
+  double min_t = MAXDOUBLE;
+  if (!bbox.intersect(ray, min_t)) return;
+  
+  int i,j;
+  if (!all_children_are_groups) {
+    for(i=0;i<objs.size();i++){
+      objs[i]->intersect(ray, hit, st, ppc);
     }
+    return;
+  }
+
+  // sort kids' bboxes so we can test intersections against the closest ones 
+  //   first
+  Array1<std::pair<int, double> > bbox_dist(objs.size());
+  for (i=0; i<objs.size(); i++) {
+    Group *g = dynamic_cast<Group*>(objs[i]);
+    min_t = MAXDOUBLE;
+    g->bbox.intersect(ray, min_t);
+    bbox_dist[i].first = i;
+    bbox_dist[i].second = min_t;
+  }
+  int swapi;
+  double swapd;
+  for (i=0; i<objs.size()-1; i++) {
+    for (j=i+1; j<objs.size(); j++) {
+      if (bbox_dist[j].second < bbox_dist[i].second) {
+	swapi=bbox_dist[j].first;
+	swapd=bbox_dist[j].second;
+	bbox_dist[j].first=bbox_dist[i].first;
+	bbox_dist[j].second=bbox_dist[i].second;
+	bbox_dist[i].first=swapi;
+	bbox_dist[i].second=swapd;
+      }
+    }
+  }
+  for (i=0; i<objs.size(); i++) {
+    if (hit.min_t <= bbox_dist[i].second) return;
+    objs[bbox_dist[i].first]->intersect(ray, hit, st, ppc);
+  }
+#endif
 }
 
 void Group::multi_light_intersect(Light* light, const Point& orig,
@@ -70,16 +118,21 @@ void Group::collect_prims(Array1<Object*>& prims)
 
 void Group::preprocess(double maxradius, int& pp_offset, int& scratchsize)
 {
-    for(int i=0;i<objs.size();i++){
-	objs[i]->preprocess(maxradius, pp_offset, scratchsize);
-    }
+  all_children_are_groups=1;
+  for(int i=0;i<objs.size();i++) {
+    objs[i]->preprocess(maxradius, pp_offset, scratchsize);
+    objs[i]->compute_bounds(bbox, pp_offset);
+    if (dynamic_cast<Group *>(objs[i]) == 0) all_children_are_groups=0;
+  }
+  if (all_children_are_groups) {
+    cerr << "YES!  ALL CHILDREN ARE GROUPS!\n";
+  }
 }
 
-void Group::compute_bounds(BBox& bbox, double offset)
+
+void Group::compute_bounds(BBox& bb, double offset)
 {
-    for(int i=0;i<objs.size();i++){
-	objs[i]->compute_bounds(bbox, offset);
-    }
+  bb.extend(bbox);
 }
 
 void Group::prime(int n)
