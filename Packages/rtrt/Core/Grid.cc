58ff4d67385d0b0080f916f1d43bacba08b0b7b4
David Weinstein <dmw@sci.utah.edu>
2002-05-17 09:23:08 +0000

    - Consolidated HierarchicalGrid code

--- Packages/rtrt/Core/Grid.cc
+++ Packages/rtrt/Core/Grid.cc
@@ -1,4 +1,3 @@
-
 #include <Packages/rtrt/Core/Grid.h>
 #include <Packages/rtrt/Core/BBox.h>
 #include <Packages/rtrt/Core/Array1.h>
@@ -23,45 +22,6 @@ Grid::Grid(Object* obj, int nsides)
 
 Grid::~Grid()
 {
-}
-
-static inline void calc_se(const BBox& obj_bbox, const BBox& bbox,
-			   const Vector& diag,
-			   int nx, int ny, int nz,
-			   int& sx, int& sy, int& sz,
-			   int& ex, int& ey, int& ez)
-{
-    Vector s((obj_bbox.min()-bbox.min())/diag);
-    Vector e((obj_bbox.max()-bbox.min())/diag);
-    sx=(int)(s.x()*nx);
-    sy=(int)(s.y()*ny);
-    sz=(int)(s.z()*nz);
-    ex=(int)(e.x()*nx);
-    ey=(int)(e.y()*ny);
-    ez=(int)(e.z()*nz);
-    if(sx < 0 || ex >= nx){
-	cerr << "NX out of bounds!\n";
-	cerr << "sx=" << sx << ", ex=" << ex << '\n';
-	cerr << "e=" << e << '\n';
-	cerr << "obj_bbox=" << obj_bbox.min() << ", " << obj_bbox.max() << '\n';
-	cerr << "bbox=" << bbox.min() << ", " << bbox.max() << '\n';
-	cerr << "diag=" << diag << '\n';
-	exit(1);
-    }
-    if(sy < 0 || ey >= ny){
-	cerr << "NY out of bounds!\n";
-	cerr << "sy=" << sy << ", ey=" << ey << '\n';
-	exit(1);
-    }
-    if(sz < 0 || ez >= nz){
-	cerr << "NZ out of bounds!\n";
-	cerr << "sz=" << sz << ", ez=" << ez << '\n';
-	cerr << "e=" << e << '\n';
-	cerr << "obj_bbox=" << obj_bbox.min() << ", " << obj_bbox.max() << '\n';
-	cerr << "bbox=" << bbox.min() << ", " << bbox.max() << '\n';
-	cerr << "diag=" << diag << '\n';
-	exit(1);
-    }
 }
 
 void Grid::preprocess(double maxradius, int& pp_offset, int& scratchsize)
