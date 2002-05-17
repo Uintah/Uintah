5840ce9284dc0a650729c231299aa464430cf2de
David Weinstein <dmw@sci.utah.edu>
2002-05-17 20:15:28 +0000

    - New module to reclassify poorly shaped tets

--- src/Packages/CardioWave/Dataflow/GUI/sub.mk
+++ src/Packages/CardioWave/Dataflow/GUI/sub.mk
@@ -30,6 +30,7 @@ ALLTARGETS := $(ALLTARGETS) $(SRCDIR)/tclIndex
 
 $(SRCDIR)/tclIndex: \
 	$(SRCDIR)/CreateSimpleMesh.tcl \
+	$(SRCDIR)/ReclassifyInteriorTets.tcl \
 	$(SRCDIR)/RemoveInteriorTets.tcl \
 	$(SRCDIR)/SetupFVMatrix.tcl \
 	$(OBJTOP)/createTclIndex $(SRCTOP)/Packages/CardioWave/Dataflow/GUI
