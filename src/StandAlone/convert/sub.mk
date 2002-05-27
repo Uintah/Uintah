c69decef30cb5b890c76e1105979bbec84b96952
David Weinstein <dmw@sci.utah.edu>
2002-05-27 19:23:39 +0000

    - Reverting sub.mk

--- src/StandAlone/convert/sub.mk
+++ src/StandAlone/convert/sub.mk
@@ -27,6 +27,10 @@ PSELIBS := Core/Datatypes Core/Util Core/Containers Core/Persistent \
 endif
 LIBS := $(XML_LIBRARY) -lm
 
+PROGRAM := $(SRCDIR)/BugProgram
+SRCS := $(SRCDIR)/BugProgram.cc
+include $(SCIRUN_SCRIPTS)/program.mk
+
 PROGRAM := $(SRCDIR)/CVRTItoTetVolDirichlet
 SRCS := $(SRCDIR)/CVRTItoTetVolDirichlet.cc
 include $(SCIRUN_SCRIPTS)/program.mk
@@ -101,4 +105,8 @@ include $(SCIRUN_SCRIPTS)/program.mk
 
 PROGRAM := $(SRCDIR)/RawToTriSurf
 SRCS := $(SRCDIR)/RawToTriSurf.cc
+include $(SCIRUN_SCRIPTS)/program.mk
+
+PROGRAM := $(SRCDIR)/SingleTet
+SRCS := $(SRCDIR)/SingleTet.cc
 include $(SCIRUN_SCRIPTS)/program.mk
