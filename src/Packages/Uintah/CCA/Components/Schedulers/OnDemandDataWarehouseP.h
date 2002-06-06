b557efa44a4457ea89631a5b7c66765f1802f3f7
Wayne Witzel <witzel@sci.utah.edu>
2002-06-06 19:24:38 +0000

    Handle to an OnDemandDataWarehouse

---
+++ src/Packages/Uintah/CCA/Components/Schedulers/OnDemandDataWarehouseP.h
@@ -0,0 +1,11 @@
+
+#ifndef UINTAH_HOMEBREW_OnDemandDataWarehouseP_H
+#define UINTAH_HOMEBREW_OnDemandDataWarehouseP_H
+
+namespace Uintah {
+  template<class T> class Handle;
+  class OnDemandDataWarehouse;
+  typedef Handle<OnDemandDataWarehouse> OnDemandDataWarehouseP;
+}
+
+#endif
