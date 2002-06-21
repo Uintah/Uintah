86077711505ca15481c1374c072ba2d01ec9caf8
J. Davison de St. Germain <dav@sci.utah.edu>
2002-06-21 18:10:36 +0000

    made getMin/Max const

--- src/Core/Datatypes/ColorMap.h
+++ src/Core/Datatypes/ColorMap.h
@@ -107,8 +107,8 @@ public:
   virtual void io(Piostream&);
   static PersistentTypeID type_id;
 
-  double getMin() { return min_; }
-  double getMax() { return max_; }
+  double getMin() const { return min_; }
+  double getMax() const { return max_; }
 
   Color FindColor(double t);
   double FindAlpha(double t);
