f7f20377fb1b22a562d5aa4e2ad6cb829fa4fa9d
Chris Moulding <moulding@sci.utah.edu>
2002-06-24 01:26:12 +0000

    cylinders with an arc section cut out

---
+++ Packages/rtrt/Core/UVCylinderArc.h
@@ -0,0 +1,45 @@
+
+#ifndef UVCylinderArc_H
+#define UVCylinderArc_H 1
+
+#include <Core/Geometry/Point.h>
+#include <Core/Geometry/Vector.h>
+#include <Packages/rtrt/Core/Object.h>
+#include <Packages/rtrt/Core/UVMapping.h>
+#include <Packages/rtrt/Core/UV.h>
+#include <Core/Geometry/Transform.h>
+#include <stdlib.h>
+
+namespace rtrt {
+
+using SCIRun::Vector;
+using SCIRun::Point;
+using SCIRun::Transform;
+
+class UVCylinderArc : public Object, public UVMapping {
+protected:
+    Point top;
+    Point bottom;
+    double radius;
+    double theta0,theta1;
+    Vector tex_scale;
+    Transform xform;
+    Transform ixform;
+public:
+    UVCylinderArc(Material* matl, const Point& bottom, const Point& top, 
+               double radius);
+    virtual ~UVCylinderArc();
+    virtual void intersect(const Ray& ray, HitInfo& hit, DepthStats* st,
+			   PerProcessorContext*);
+    virtual void preprocess(double maxradius, int& pp_offset, int& scratchsize);
+    virtual Vector normal(const Point&, const HitInfo& hit);
+    virtual void compute_bounds(BBox&, double offset);
+    virtual void print(ostream& out);
+    virtual void uv(UV&, const Point&, const HitInfo&);
+    void set_tex_scale(const Vector &v) { tex_scale = v; }
+    void set_arc(double t0, double t1) { theta0 = t0; theta1 = t1; }
+};
+
+} // end namespace rtrt
+
+#endif
