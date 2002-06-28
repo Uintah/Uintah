1973b7e8f84b8773e6688b5bfb2b355a16d01edb
J. Davison de St. Germain <dav@sci.utah.edu>
2002-06-28 04:26:14 +0000

    added ambient recognition

--- Packages/rtrt/Core/Background.h
+++ Packages/rtrt/Core/Background.h
@@ -13,29 +13,37 @@ using SCIRun::Dot;
 
 class Background {
     Color avg;
+    Color origAvg_;
 public:
     Background(const Color& avg);
     virtual ~Background();
-
 
     // expects a unit vector
     virtual void color_in_direction( const Vector& v, Color& c) const = 0;
 
     // gives some approximate value
-  inline const Color& average( ) const {
-    return avg;
-  }
+    inline const Color& average( ) const {
+      return avg;
+    }
+
+    virtual void updateAmbient( double scale ) {
+      avg = origAvg_ * scale;
+    }
 
 };
 
 class ConstantBackground : public Background {
 protected:
     Color C;
+    Color origC_;
 public:
     ConstantBackground(const Color& C);
     virtual ~ConstantBackground();
 
     virtual void color_in_direction(const Vector& v, Color& c) const; 
+    virtual void updateAmbient( double scale ) {
+      C = origC_ * scale;
+    }
 };
 
 
@@ -43,6 +51,8 @@ class LinearBackground : public Background {
 protected:
     Color C1;
     Color C2;
+    Color origC1_;
+    Color origC2_;
     Vector direction_to_C1;
 public:
     LinearBackground( const Color& C1, const Color& C2,  const Vector& direction_to_C1);
@@ -51,6 +61,10 @@ public:
     
     virtual void color_in_direction(const Vector& v, Color& c) const ;
 
+    virtual void updateAmbient( double scale ) {
+      C1 = origC1_ * scale;
+      C2 = origC2_ * scale;
+    }
 };
 
   class EnvironmentMapBackground : public Background 
@@ -65,6 +79,8 @@ public:
       virtual void color_in_direction(const Vector& v, Color& c) const ;
       
   protected:
+
+      double ambientScale_;
       
       void read_image( char* filename );
       inline Vector ChangeFromBasis( const Vector& a ) const 
@@ -77,6 +93,9 @@ public:
 	  return Vector( Dot( a, _u), Dot( a, _v ), Dot( a, _up ) );
       }
       
+      virtual void updateAmbient( double scale ) {
+	ambientScale_ = scale;
+      }
       texture* _text;
       int _width, _height;
       Array2<Color> _image;
