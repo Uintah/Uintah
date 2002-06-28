a50995a17be068d0fcffcb114d3f08442be92fb4
J. Davison de St. Germain <dav@sci.utah.edu>
2002-06-28 04:06:03 +0000

    updated shadows to use enumerated type instead of string

--- Packages/rtrt/Core/Shadows/ShadowBase.cc
+++ Packages/rtrt/Core/Shadows/ShadowBase.cc
@@ -2,6 +2,12 @@
 #include <Packages/rtrt/Core/Shadows/ShadowBase.h>
 using namespace rtrt;
 
+char * ShadowBase::shadowTypeNames[] = { "No Shadows",
+					 "Single Soft Shadow",
+					 "Hard Shadows",
+					 "Glass Shadows",
+					 "Soft Shadows",
+					 "Uncached Shadows" };
 
 ShadowBase::ShadowBase()
   : name("unknown")
