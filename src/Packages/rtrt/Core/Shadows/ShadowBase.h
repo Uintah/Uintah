02eb3fb4c90ab5ba56c4cf1f18498aebc9600f69
J. Davison de St. Germain <dav@sci.utah.edu>
2002-06-28 04:06:03 +0000

    updated shadows to use enumerated type instead of string

--- src/Packages/rtrt/Core/Shadows/ShadowBase.h
+++ src/Packages/rtrt/Core/Shadows/ShadowBase.h
@@ -8,29 +8,40 @@ namespace SCIRun {
 }
 
 namespace rtrt {
-  using SCIRun::Point;
-  using SCIRun::Vector;
-  class Light;
-  class Color;
-  class Context;
-  class Scene;
-  class ShadowBase {
-    const char* name;
-  public:
-    ShadowBase();
-    virtual ~ShadowBase();
-    virtual void preprocess(Scene* scene, int& pp_offset, int& scratchsize);
-    virtual bool lit(const Point& hitpos, Light* light,
-		     const Vector& light_dir, double dist, Color& atten,
-		     int depth, Context* cx) = 0;
-    void setName(const char* name) {
-      this->name=name;
-    }
-    const char* getName() {
-      return name;
-    }
-  };
-}
+
+using SCIRun::Point;
+using SCIRun::Vector;
+
+class Light;
+class Color;
+class Context;
+class Scene;
+
+enum ShadowType { No_Shadows = 0, Single_Soft_Shadow, Hard_Shadows,
+		  Glass_Shadows, Soft_Shadows, Uncached_Shadows };
+
+class ShadowBase {
+  const char* name;
+public:
+  ShadowBase();
+  virtual ~ShadowBase();
+
+  virtual void preprocess(Scene* scene, int& pp_offset, int& scratchsize);
+  virtual bool lit(const Point& hitpos, Light* light,
+		   const Vector& light_dir, double dist, Color& atten,
+		   int depth, Context* cx) = 0;
+  void setName(const char* name) {
+    this->name=name;
+  }
+  const char* getName() {
+    return name;
+  }
+
+  static char * shadowTypeNames[];
+
+};
+
+} // end namespace rtrt
 
 #endif
 
