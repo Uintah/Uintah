9517418ac4166baf4fca9d28e21622003d023fa4
Chris Moulding <moulding@sci.utah.edu>
2002-07-06 23:15:16 +0000

    make UVSphere inherit from Object

--- src/Packages/rtrt/Core/UVSphere.cc
+++ src/Packages/rtrt/Core/UVSphere.cc
@@ -17,26 +17,74 @@ the change in mappying to a UVSphere map.
 using namespace rtrt;
 using namespace SCIRun;
 
-UVSphere::UVSphere(Point c, double r)
+UVSphere::UVSphere(Material *matl, Point c, double r) 
+  : Object(matl,this), radius(r), cen(c)
 {
-radius = r;
-cen = c;
 }
 
 UVSphere::~UVSphere()
 {
 }
 
+void UVSphere::intersect(const Ray& ray, HitInfo& hit, DepthStats* st,
+                         PerProcessorContext*)
+{
+  Vector OC=cen-ray.origin();
+  double tca=Dot(OC, ray.direction());
+  double l2oc=OC.length2();
+  double rad2=radius*radius;
+  st->sphere_isect++;
+  if(l2oc <= rad2){
+    // Inside the sphere
+    double t2hc=rad2-l2oc+tca*tca;
+    double thc=sqrt(t2hc);
+    double t=tca+thc;
+    hit.hit(this, t);
+    st->sphere_hit++;
+    return;
+  } else {
+    if(tca < 0.0){
+      // Behind ray, no intersections...
+      return;
+    } else {
+      double t2hc=rad2-l2oc+tca*tca;
+      if(t2hc <= 0.0){
+        // Ray misses, no intersections
+        return;
+      } else {
+        double thc=sqrt(t2hc);
+        hit.hit(this, tca-thc);
+        hit.hit(this, tca+thc);
+        st->sphere_hit++;
+        return;
+      }
+    }
+  }	
+}
+
+Vector UVSphere::normal(const Point& hitpos, const HitInfo&)
+{
+  Vector n=hitpos-cen;
+  n*=1./radius;
+  return n;
+}
+
+void UVSphere::compute_bounds(BBox& bbox, double offset)
+{
+  bbox.extend(cen, radius+offset);
+}
+
 //vv = acos(m.z()/radius) / M_PI;
 //uu = acos(m.x() / (radius * sin (M_PI*vv)))* over2pi;
 void UVSphere::uv(UV& uv, const Point& hitpos, const HitInfo&)  
 {
-Vector m(hitpos-cen);  
-double uu,vv,theta,phi;  
-theta = acos(m.z()/radius);
-phi = atan2(m.y(), m.x());
-if(phi < 0) phi += 6.28318530718;
- uu = phi * .159154943092; // 1_pi
- vv = (M_PI - theta) * .318309886184; // 1 / ( 2 * pi )
-uv.set( uu,vv);
+  Vector m(hitpos-cen);  
+  double uu,vv,theta,phi;  
+  theta = acos(m.z()/radius);
+  phi = atan2(m.y(), m.x());
+  if(phi < 0) phi += 6.28318530718;
+  uu = phi * .159154943092; // 1_pi
+  vv = (M_PI - theta) * .318309886184; // 1 / ( 2 * pi )
+  uv.set( uu,vv);
 }
+
--- src/Packages/rtrt/Core/UVSphere.h
+++ src/Packages/rtrt/Core/UVSphere.h
@@ -10,9 +10,13 @@ Email:		ramsey@cs.utah.edu
 #ifndef UVSPHERE_H
 #define UVSPHERE_H 1
 
+#include <Packages/rtrt/Core/Object.h>
 #include <Packages/rtrt/Core/UVMapping.h>
 #include <Packages/rtrt/Core/vec.h>
 #include <Packages/rtrt/Core/UV.h>
+#include <Packages/rtrt/Core/BBox.h>
+#include <Packages/rtrt/Core/HitInfo.h>
+#include <Packages/rtrt/Core/Stats.h>
 #include <Core/Geometry/Point.h>
 #include <Core/Geometry/Vector.h>
 
@@ -22,33 +26,38 @@ namespace rtrt {
 using namespace rtrt;
 using namespace SCIRun;
 
-class UVSphere : public UVMapping {
-   Point cen; 
-   double radius;
-public:
-    UVSphere(Point c, double r);
-    virtual ~UVSphere();
-    virtual void uv(UV& uv, const Point&, const HitInfo& hit);
-    virtual void get_frame(const Point &hitpos, const HitInfo&hit,const Vector &norm,  Vector &pu, Vector &pv)
+class UVSphere : public Object, UVMapping {
+  Point cen; 
+  double radius;
+ public:
+  UVSphere(Material *m, Point c, double r);
+  virtual ~UVSphere();
+  virtual void uv(UV& uv, const Point&, const HitInfo& hit);
+  virtual void intersect(const Ray& ray, HitInfo& hit, DepthStats* st,
+			 PerProcessorContext*);
+  virtual Vector normal(const Point&, const HitInfo& hit);
+  virtual void compute_bounds(BBox&, double offset);
+  virtual void get_frame(const Point &hitpos, const HitInfo&hit,
+                         const Vector &norm,  Vector &pu, Vector &pv)
   {
-     UV uv_m;
-     float u,v;
-     double phi,theta;
-     uv(uv_m,hitpos,hit);
-     u = uv_m.u();
-     v = uv_m.v();
-     phi = 6.28318530718 * u;
-     theta = -(M_PI*v) + M_PI;
-     pu = Vector(-6.28318530718* radius * sin(phi) * sin(theta),
-     	 6.28318530718 * radius * sin(phi) * cos(theta), 	0);
-     pv = Vector(M_PI * radius * cos(phi) * cos(theta),
-     	 M_PI * radius * cos(phi) * sin(theta),
-     	 -1 * M_PI * radius * sin(phi));
-     VXV3(pu,norm,pv);
-     VXV3(pv,norm,pu);
+    UV uv_m;
+    float u,v;
+    double phi,theta;
+    uv(uv_m,hitpos,hit);
+    u = uv_m.u();
+    v = uv_m.v();
+    phi = 6.28318530718 * u;
+    theta = -(M_PI*v) + M_PI;
+    pu = Vector(-6.28318530718* radius * sin(phi) * sin(theta),
+                6.28318530718 * radius * sin(phi) * cos(theta), 	0);
+    pv = Vector(M_PI * radius * cos(phi) * cos(theta),
+                M_PI * radius * cos(phi) * sin(theta),
+                -1 * M_PI * radius * sin(phi));
+    VXV3(pu,norm,pv);
+    VXV3(pv,norm,pu);
   }
 };
-
+ 
 
 } // end namespacertrt
 #endif
