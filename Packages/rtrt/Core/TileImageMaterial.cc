a826850354762e9d863960d2b6a9d2286c043719
Chris Moulding <moulding@sci.utah.edu>
2002-07-06 06:50:06 +0000

    new material TileImageMaterial (no test in shade function for u or v mode)

---
+++ Packages/rtrt/Core/TileImageMaterial.cc
@@ -0,0 +1,82 @@
+
+#include <Packages/rtrt/Core/TileImageMaterial.h>
+#include <Packages/rtrt/Core/HitInfo.h>
+#include <Packages/rtrt/Core/UVMapping.h>
+#include <Packages/rtrt/Core/UV.h>
+#include <Core/Geometry/Point.h>
+#include <Packages/rtrt/Core/Ray.h>
+#include <Packages/rtrt/Core/Object.h>
+#include <Packages/rtrt/Core/PPMImage.h>
+#include <iostream>
+#include <stdio.h>
+#include <fstream>
+#include <stdlib.h>
+
+using namespace rtrt;
+using namespace std;
+
+TileImageMaterial::TileImageMaterial(int, const string &texfile, 
+                                     double Kd,
+                                     const Color& specular, double specpow,
+                                     double refl, bool flipped=false)
+  : Kd(Kd), specular(specular),
+    specpow(specpow), refl(refl),  transp(0), valid_(false)
+{
+  read_hdr_image(texfile);
+  outcolor=Color(0,0,0);
+}
+
+TileImageMaterial::TileImageMaterial(const string &texfile, 
+                                     double Kd,
+                                     const Color& specular, double specpow,
+                                     double refl, bool flipped=false)
+  : Kd(Kd), specular(specular),
+    specpow(specpow), refl(refl),  transp(0), valid_(false)
+{
+  PPMImage ppm(texfile);
+  int nu, nv;
+  ppm.get_dimensions_and_data(image, nu, nv, flipped);
+  outcolor=Color(0,0,0);
+}
+
+TileImageMaterial::TileImageMaterial(const string &texfile, 
+                                     double Kd,
+                                     const Color& specular, double specpow,
+                                     double refl,  double transp, 
+                                     bool flipped=false)
+  : Kd(Kd), specular(specular),
+    specpow(specpow), refl(refl),  transp(transp), valid_(false)
+{
+  PPMImage ppm(texfile);
+  int nu, nv;
+  ppm.get_dimensions_and_data(image, nu, nv, flipped);
+  outcolor=Color(0,0,0);
+}
+
+void TileImageMaterial::shade(Color& result, const Ray& ray,
+                              const HitInfo& hit, int depth, 
+                              double atten, const Color& accumcolor,
+                              Context* cx)
+{
+  UVMapping* map=hit.hit_obj->get_uvmapping();
+  UV uv;
+  Point hitpos(ray.origin()+ray.direction()*hit.min_t);
+  map->uv(uv, hitpos, hit);
+  Color diffuse;
+  double u=uv.u();
+  double v=uv.v();
+  
+  int iu=(int)u;
+  u-=iu;
+  if (u < 0) u += 1;
+  int iv=(int)v;
+  v-=iv;
+  if (v < 0) v += 1;
+  
+  diffuse = interp_color(image,u,v);
+  
+  phongshade(result, diffuse, specular, specpow, refl,
+             ray, hit, depth,  atten,
+             accumcolor, cx);
+}
+
---
+++ Packages/rtrt/Core/TileImageMaterial.h
@@ -0,0 +1,31 @@
+
+#ifndef TILEIMAGEMATERIAL_H
+#define TILEIMAGEMATERIAL_H 1
+
+#include <Packages/rtrt/Core/ImageMaterial.h>
+
+namespace rtrt { 
+
+class TileImageMaterial : public ImageMaterial {
+
+public:
+    TileImageMaterial(int /* oldstyle */, const string &filename, 
+		  double Kd, const Color& specular,
+		  double specpow, double refl=0, bool flipped=0);
+    TileImageMaterial(const string &filename,
+		  double Kd, const Color& specular,
+		  double specpow, double refl, 
+		  double transp=0, bool flipped=false);
+    TileImageMaterial(const string &filename,
+		  double Kd, const Color& specular,
+		  double specpow, double refl=0, bool flipped=false);
+    virtual ~TileImageMaterial() {}
+    virtual void shade(Color& result, const Ray& ray,
+                       const HitInfo& hit, int depth, 
+                       double atten, const Color& accumcolor,
+                       Context* cx);
+};
+
+} // end namespace rtrt
+
+#endif
