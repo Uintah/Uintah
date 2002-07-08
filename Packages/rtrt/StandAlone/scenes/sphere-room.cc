59bbbdf2d93f5abfa1a417c16e11f30f21762c64
Chris Moulding <moulding@sci.utah.edu>
2002-07-08 01:47:09 +0000

    add planets models with tetures, and starfield environment map (the earth is shiny over the oceans but not the land!)

--- Packages/rtrt/StandAlone/scenes/sphere-room.cc
+++ Packages/rtrt/StandAlone/scenes/sphere-room.cc
@@ -6,6 +6,7 @@
 #include <Core/Geometry/Transform.h>
 #include <Packages/rtrt/Core/Group.h>
 #include <Packages/rtrt/Core/Sphere.h>
+#include <Packages/rtrt/Core/UVSphere.h>
 #include <Packages/rtrt/Core/Parallelogram.h>
 #include <Packages/rtrt/Core/Rect.h>
 #include <Packages/rtrt/Core/UVCylinder.h>
@@ -16,6 +17,7 @@
 #include <Packages/rtrt/Core/CoupledMaterial.h>
 #include <Packages/rtrt/Core/DielectricMaterial.h>
 #include <Packages/rtrt/Core/ImageMaterial.h>
+#include <Packages/rtrt/Core/TileImageMaterial.h>
 #include <Packages/rtrt/Core/MapBlendMaterial.h>
 #include <Packages/rtrt/Core/MultiMaterial.h>
 #include <Packages/rtrt/Core/PBNMaterial.h>
@@ -37,7 +39,7 @@ using namespace rtrt;
 #define ROOMRADIUS    4
 #define WALLTHICKNESS .1
 
-#define INSCILAB 1
+#define INSCILAB 0
 
 extern "C"
 Scene* make_scene(int /*argc*/, char* /*argv*/[], int /*nworkers*/)
@@ -46,47 +48,159 @@ Scene* make_scene(int /*argc*/, char* /*argv*/[], int /*nworkers*/)
 
   Material *white = new LambertianMaterial(Color(1,1,1));
 
-  ImageMaterial *stadium = 
-    new ImageMaterial(
+  TileImageMaterial *earthmap = 
+    new TileImageMaterial(
 #if INSCILAB
-                      "/home/sci/moulding/images/StadiumAtNoon.ppm",
+                          "/home/sci/moulding/images/earth.ppm",
 #else
-                      "/home/moulding/images/StadiumAtNoon.ppm",
+                          "/home/moulding/images/earth.ppm",
 #endif
-                      ImageMaterial::Tile,
-                      ImageMaterial::Tile, 1,
-                      Color(1,1,1), 4000);
-  
-  ImageMaterial *bookcoverimg = 
-    new ImageMaterial(1,
-#if INSCILAB
-                      "/usr/sci/data/Geometry/textures/i3d97.smaller.gamma",
-#else
-                      "/home/moulding/i3d97.smaller.gamma",
-#endif
-                      ImageMaterial::Tile,
-                      ImageMaterial::Tile, 1,
-                      Color(1,1,1), 4000,0, true);
+                          1,
+                          Color(1,1,1), 0,0, false);
 
-  ImageMaterial *matl0 = new ImageMaterial(
+  TileImageMaterial *earthmap_specular = 
+    new TileImageMaterial(
+#if INSCILAB
+                          "/home/sci/moulding/images/earth.ppm",
+#else
+                          "/home/moulding/images/earth.ppm",
+#endif
+                          1,
+                          Color(1,1,1), 100,0, false);
+
+  TileImageMaterial *earthcloudmap = 
+    new TileImageMaterial(
+#if INSCILAB
+                          "/home/sci/moulding/images/earth.ppm",
+#else
+                          "/home/moulding/images/earth.ppm",
+#endif
+                          1,
+                          Color(1,1,1), 0,0, false);
+
+  TileImageMaterial *marsmap = 
+    new TileImageMaterial(
+#if INSCILAB
+                          "/home/sci/moulding/images/mars.ppm",
+#else
+                          "/home/moulding/images/mars.ppm",
+#endif
+                          1,
+                          Color(1,1,1), 0,0, false);
+
+  TileImageMaterial *neptunemap = 
+    new TileImageMaterial(
+#if INSCILAB
+                          "/home/sci/moulding/images/neptune.ppm",
+#else
+                          "/home/moulding/images/neptune.ppm",
+#endif
+                          1,
+                          Color(1,1,1), 0,0, false);
+
+  TileImageMaterial *jupitermap = 
+    new TileImageMaterial(
+#if INSCILAB
+                          "/home/sci/moulding/images/jupiter.ppm",
+#else
+                          "/home/moulding/images/jupiter.ppm",
+#endif
+                          1,
+                          Color(1,1,1), 0,0, false);
+
+  TileImageMaterial *saturnmap = 
+    new TileImageMaterial(
+#if INSCILAB
+                          "/home/sci/moulding/images/saturn.ppm",
+#else
+                          "/home/moulding/images/saturn.ppm",
+#endif
+                          1,
+                          Color(1,1,1), 0,0, false);
+
+  TileImageMaterial *lunamap = 
+    new TileImageMaterial(
+#if INSCILAB
+                          "/home/sci/moulding/images/luna.ppm",
+#else
+                          "/home/moulding/images/luna.ppm",
+#endif
+                          1,
+                          Color(1,1,1), 0,0, false);
+
+  TileImageMaterial *mercurymap = 
+    new TileImageMaterial(
+#if INSCILAB
+                          "/home/sci/moulding/images/mercury.ppm",
+#else
+                          "/home/moulding/images/mercury.ppm",
+#endif
+                          1,
+                          Color(1,1,1), 0,0, false);
+
+  TileImageMaterial *venusmap = 
+    new TileImageMaterial(
+#if INSCILAB
+                          "/home/sci/moulding/images/venus.ppm",
+#else
+                          "/home/moulding/images/venus.ppm",
+#endif
+                          1,
+                          Color(1,1,1), 0,0, false);
+
+  TileImageMaterial *iomap = 
+    new TileImageMaterial(
+#if INSCILAB
+                          "/home/sci/moulding/images/io.ppm",
+#else
+                          "/home/moulding/images/io.ppm",
+#endif
+                          1,
+                          Color(1,1,1), 0,0, false);
+
+  TileImageMaterial *stadium = 
+    new TileImageMaterial(
+#if INSCILAB
+                          "/home/sci/moulding/images/StadiumAtNoon.ppm",
+#else
+                          "/home/moulding/images/StadiumAtNoon.ppm",
+#endif
+                          1,
+                          Color(1,1,1), 1000,0, false);
+  
+  TileImageMaterial *bookcoverimg = 
+    new TileImageMaterial(
+#if INSCILAB
+                      "/usr/sci/data/Geometry/textures/i3d97.ppm",
+#else
+                      "/home/moulding/i3d97.ppm",
+#endif
+                      1,
+                      Color(1,1,1), 4000, 0, true);
+
+  TileImageMaterial *matl0 = new TileImageMaterial(
 #if INSCILAB                                         
                                            "/home/sci/moulding/holo.ppm",
 #else
-                                           "/home/moulding/holo.ppm",
+                                           "/home/moulding/images/holowall.ppm",
 #endif
-                                           ImageMaterial::Tile,
-                                           ImageMaterial::Tile,
-                                           4,Color(.5,.5,.5),40,0,0, true);
+                                           4,Color(.5,.5,.5),40,0,0,true);
 
   CrowMarble *crow = new CrowMarble(10,Vector(0,1,0),Color(.1,.1,.8),
                                     Color(.9,.8,.9),Color(.6,.6,.8));
 
   //MultiMaterial *matl1 = new MultiMaterial();
-  MapBlendMaterial *matl1 = new MapBlendMaterial("/home/sci/moulding/images/scuff.ppm",matl0,white);
+#if INSCILAB
+  MapBlendMaterial *matl1 = new MapBlendMaterial("/home/sci/moulding/images/scuff.ppm",matl0,stadium);
+#else
+  MapBlendMaterial *matl1 = new MapBlendMaterial("/home/moulding/images/scuff.ppm",matl0,stadium);
+#endif
   //PBNMaterial *matl1 = new PBNMaterial("/home/sci/moulding/images/pbnmaterial.ppm");
 
   //matl1->insert(stadium,255);
   //matl1->insert(crow,0);
+  MapBlendMaterial *earthspec = new MapBlendMaterial("/home/moulding/images/earthspec4k.ppm",earthmap_specular,earthmap);
+  MapBlendMaterial *earthblend = new MapBlendMaterial("/home/moulding/images/earthclouds.ppm",white,earthspec);
 
   UVCylinderArc* uvcylarc0 = new UVCylinderArc(matl1,Point(ROOMCENTER,0),
                                                Point(ROOMCENTER,DOORHEIGHT),
@@ -108,34 +222,47 @@ Scene* make_scene(int /*argc*/, char* /*argv*/[], int /*nworkers*/)
 
   Rect* floor = new Rect(matl1,Point(ROOMCENTER,0),Vector(ROOMRADIUS,0,0),
                           Vector(0,ROOMRADIUS,0));
-  Object* obj1 = new Sphere(matl0,Point(5,5,0),1);
+  UVSphere *obj1 = new UVSphere(matl0,Point(5,5,0),1);
   Point p1(ROOMCENTER,.2);
   Parallelogram *bookcover = new Parallelogram(bookcoverimg,p1,
-                                               Vector(0,-1,0),
-                                               Vector(-.774,0,0));
+                                               Vector(.774,0,0),
+                                               Vector(0,1,0));
+
+  UVSphere *earth = new UVSphere(earthblend,Point(0,0,8),2);
+  UVSphere *mars  = new UVSphere(marsmap,Point(0,4,8),1);
+  UVSphere *jupiter = new UVSphere(jupitermap,Point(0,50,6),20);
+  UVSphere *neptune = new UVSphere(neptunemap,Point(20,4,8),5);
+  UVSphere *saturn = new  UVSphere(saturnmap,Point(20,15,6),6);
+  UVSphere *luna = new UVSphere(lunamap,Point(4,0,8),.66);
+  UVSphere *mercury = new UVSphere(mercurymap,Point(-4,0,7),.66);
+  UVSphere *venus = new UVSphere(venusmap,Point(-8,-4,6),2);
+  UVSphere *io = new UVSphere(iomap,Point(-10,-20,8),3);
+
   
   uvcylarc0->set_arc((DOORWIDTH)*M_PI,(.5-DOORWIDTH)*M_PI);
   uvcylarc1->set_arc((.5+DOORWIDTH)*M_PI,(2-DOORWIDTH)*M_PI);
   uvcylarc2->set_arc((DOORWIDTH)*M_PI,(.5-DOORWIDTH)*M_PI);
   uvcylarc3->set_arc((.5+DOORWIDTH)*M_PI,(2-DOORWIDTH)*M_PI);
 
-  //uvcylarc0->set_tex_scale(Vector(CYLTEXSCALEX,
-  //                              CYLTEXSCALEY/HEIGHTRATIO,0));
-  //uvcylarc1->set_tex_scale(Vector(CYLTEXSCALEX,
-  //                              CYLTEXSCALEY/HEIGHTRATIO,0));
-  //uvcyl0->set_tex_scale(Vector(CYLTEXSCALEX,CYLTEXSCALEY,0));
-  //floor->set_tex_scale(Vector(5,5,0));
-  
   Group * group = new Group();
-  group->add( uvcylarc0 );
-  group->add( uvcylarc1 );
-  group->add( uvcylarc2 );
-  group->add( uvcylarc3 );
-  group->add( uvcyl0 );
-  group->add( uvcyl1 );
-  group->add( floor );
-  group->add( obj1 );
-  group->add( bookcover );
+  //group->add( uvcylarc0 );
+  //group->add( uvcylarc1 );
+  //group->add( uvcylarc2 );
+  //group->add( uvcylarc3 );
+  //group->add( uvcyl0 );
+  //group->add( uvcyl1 );
+  //group->add( floor );
+  //group->add( obj1 );
+  //group->add( bookcover );
+  group->add( earth );
+  //group->add( mars );
+  //group->add( jupiter );
+  //group->add( neptune );
+  //group->add( saturn );
+  group->add( luna );
+  //group->add( venus );
+  //group->add( mercury );
+  //group->add( io );
 
   double ambient_scale=1.0;
   Color bgcolor(0.1, 0.2, 0.45);
@@ -148,9 +275,8 @@ Scene* make_scene(int /*argc*/, char* /*argv*/[], int /*nworkers*/)
                          ambient_scale, Arc_Ambient);
   scene->add_light( new Light(Point(ROOMCENTER,9.5), Color(1,1,1), 0.0) );
 
-  scene->set_background_ptr( new LinearBackground( Color(1.0, 1.0, 1.0),
-                                                   Color(0.0,0.0,0.0),
-                                                   Vector(0,0,1)) );
+  scene->set_background_ptr( new EnvironmentMapBackground("/home/moulding/images/tycho8.ppm"));
+
 /*
   scene->select_shadow_mode("hard");
 */
