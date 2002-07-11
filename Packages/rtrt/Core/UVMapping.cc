c906b89b221eb9e311b7eda1fd0c8bd7fdc9785e
David Weinstein <dmw@sci.utah.edu>
2002-07-11 00:50:06 +0000

    - Made get_frame a little smarter

--- Packages/rtrt/Core/UVMapping.cc
+++ Packages/rtrt/Core/UVMapping.cc
@@ -15,9 +15,14 @@ UVMapping::~UVMapping()
 }
 
 
-void UVMapping::get_frame(const Point &, const HitInfo&,const Vector &norm, Vector &v2, Vector &v3)
-    {
-      Vector v(1,0,0);
-      VXV3(v2,norm,v);
-      VXV3(v3,norm,v2);
-      }
+void UVMapping::get_frame(const Point &, const HitInfo&,const Vector &norm, Vector &v2, Vector &v3) {
+  Vector v(1,0,0);
+  v2=Cross(norm,v);
+  if (v2.length2()<1.e-8) {
+    v=Vector(0,1,0);
+    v2=Cross(norm,v);
+  }
+  v2.normalize();
+  v3=Cross(norm,v2);
+  v3.normalize();
+}
