7d35ba4b7ca109a71fe3548f5ab7da5698522158
J. Davison de St. Germain <dav@sci.utah.edu>
2002-07-12 23:34:54 +0000

    cleaned up includes. fixed namespace problems.  got pio to compile.  removed a few compiler warnings.

--- src/Packages/rtrt/Core/BumpObject.cc
+++ src/Packages/rtrt/Core/BumpObject.cc
@@ -5,12 +5,12 @@
 #include <Packages/rtrt/Core/Light.h>
 #include <Packages/rtrt/Core/BBox.h>
 #include <Packages/rtrt/Core/Stats.h>
+
 #include <iostream>
 
 using namespace rtrt;
+using namespace std;
 using namespace SCIRun;
-
-
 
 
 BumpObject::BumpObject(Vector &vector, UVMapping* uv)
@@ -23,8 +23,9 @@ BumpObject::~BumpObject()
 {
 }
 
-void BumpObject::intersect(const Ray& ray, HitInfo& hit, DepthStats* st,
-		       PerProcessorContext*) {
+void BumpObject::intersect(const Ray& /*ray*/, HitInfo& /*hit*/, 
+			   DepthStats* /*st*/,
+			   PerProcessorContext*) {
   cout << "If you're interesecting this object - stop it. This is the Bump Object and should only be used for its normal." << endl;
 }
 
@@ -40,10 +41,10 @@ void BumpObject::light_intersect(Light* light, const Ray& ray, HitInfo&,
 }
 */
 
-void BumpObject::multi_light_intersect(Light*, const Point& orig,
-				   const Array1<Vector>& dirs,
-				   const Array1<Color>& attens,
-				   double,
-				   DepthStats*, PerProcessorContext*) {
+void BumpObject::multi_light_intersect(Light*, const Point& /*orig*/,
+				       const Array1<Vector>& /*dirs*/,
+				       const Array1<Color>& /*attens*/,
+				       double,
+				       DepthStats*, PerProcessorContext*) {
   cout << "If you're multi light interesecting this object - stop it. This is the Bump Object and should only be used for its normal." << endl;
 }
