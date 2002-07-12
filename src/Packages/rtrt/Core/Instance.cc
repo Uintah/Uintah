8c3f808069cc60611bb47ab6e2fbe1438ac23964
Marty Cole <mcole@sci.utah.edu>
2002-07-12 15:17:55 +0000

    correct versions

--- src/Packages/rtrt/Core/Instance.cc
+++ src/Packages/rtrt/Core/Instance.cc
@@ -1,24 +1,34 @@
-
 #include <Packages/rtrt/Core/Instance.h>
-#include <values.h>
 
-using namespace rtrt;
-using namespace SCIRun;
+SCIRun::Persistent* obj_maker() {
+  return new Instance();
+}
 
-const int INSTANCE_VERSION = 1;
+// initialize the static member type_id
+SCIRun::PersistentTypeID Instance::type_id("Instance", "Object", obj_maker);
+
+const int UVSPHERE_VERSION = 1;
+
 void 
 Instance::io(SCIRun::Piostream &str)
 {
-  str.begin_class("Instance", INSTANCE_VERSION);
+  str.begin_class("Instance", UVSPHERE_VERSION);
   Object::io(str);
+  Material::io(str);
+  Pio(str, o);
+  Pio(str, currentTransform);
+  Pio(str, bbox);
   str.end_class();
 }
 
 namespace SCIRun {
-void 
-Pio(SCIRun::Piostream &str, rtrt::Instance *&i)
+void SCIRun::Pio(SCIRun::Piostream& stream, rtrt::Instance*& obj)
 {
-  str.begin_cheap_delim();
-  str.end_cheap_delim();
+  SCIRun::Persistent* pobj=obj;
+  stream.io(pobj, rtrt::Instance::type_id);
+  if(stream.reading()) {
+    obj=dynamic_cast<rtrt::Instance*>(pobj);
+    ASSERT(obj != 0)
+  }
 }
 } // end namespace SCIRun
