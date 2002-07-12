a10c769e5b9a67f73ebac1b00b34d0d2227aa989
Chris Moulding <moulding@sci.utah.edu>
2002-07-12 05:43:47 +0000

    cleanup

--- Packages/rtrt/Core/UVSphere.cc
+++ Packages/rtrt/Core/UVSphere.cc
@@ -36,21 +36,18 @@ void UVSphere::preprocess(double, int&, int&)
     xform.rotate(right, Vector(1,0,0));
     xform.rotate(up, Vector(0,0,1));
     xform.pre_scale(Vector(1./radius, 1./radius, 1./radius));
-    print(cerr);
-    xform.print();
     ixform.load_identity();
     ixform.pre_scale(Vector(radius, radius, radius));
     ixform.rotate(Vector(0,0,1), up);
     ixform.rotate(Vector(1,0,0), right);
     ixform.pre_translate(cen.asVector());
-    ixform.print();
 }
 
 void UVSphere::intersect(const Ray& ray, HitInfo& hit, DepthStats* st,
                          PerProcessorContext*)
 {
   Vector v=xform.project(ray.direction());
-  double dist_scale=v.normalize();
+  double dist_scale = v.normalize();
   Ray xray(xform.project(ray.origin()), v);
   Vector xOC=-xform.project(ray.origin()).asVector();
   double tca=Dot(xOC, xray.direction());
@@ -87,10 +84,9 @@ void UVSphere::intersect(const Ray& ray, HitInfo& hit, DepthStats* st,
 
 Vector UVSphere::normal(const Point& hitpos, const HitInfo&)
 {
-  Vector n=xform.project(hitpos).asVector();
-  Vector xn=ixform.project(n);
-  xn.normalize();
-  return xn;
+  Vector n(hitpos-cen);
+  n/=radius;
+  return n;
 }
 
 void UVSphere::compute_bounds(BBox& bbox, double offset)
