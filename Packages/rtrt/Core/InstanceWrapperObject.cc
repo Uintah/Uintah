f955567d2824d2db64cb3ab2bb5c8062c613ec9d
Marty Cole <mcole@sci.utah.edu>
2002-07-12 15:17:55 +0000

    correct versions

--- Packages/rtrt/Core/InstanceWrapperObject.cc
+++ Packages/rtrt/Core/InstanceWrapperObject.cc
@@ -1,23 +1,36 @@
-
 #include <Packages/rtrt/Core/InstanceWrapperObject.h>
-#include <values.h>
 
-using namespace rtrt;
-using namespace SCIRun;
+SCIRun::Persistent* instanceWrapperObject_maker() {
+  return new InstanceWrapperObject();
+}
+
+// initialize the static member type_id
+SCIRun::PersistentTypeID 
+InstanceWrapperObject::type_id("InstanceWrapperObject", 
+			       "Persistent", 
+			       instanceWrapperObject_maker);
 
 const int INSTANCEWRAPPEROBJECT_VERSION = 1;
+
 void 
 InstanceWrapperObject::io(SCIRun::Piostream &str)
 {
   str.begin_class("InstanceWrapperObject", INSTANCEWRAPPEROBJECT_VERSION);
+  Pio(str, obj);
+  Pio(str, bb);
+  Pio(str, was_processed);
+  Pio(str, computed_bbox);
   str.end_class();
 }
 
 namespace SCIRun {
-void 
-Pio(SCIRun::Piostream &str, rtrt::InstanceWrapperObject *&i)
+void SCIRun::Pio(SCIRun::Piostream& stream, rtrt::InstanceWrapperObject*& obj)
 {
-  str.begin_cheap_delim();
-  str.end_cheap_delim();
+  SCIRun::Persistent* pobj=obj;
+  stream.io(pobj, rtrt::InstanceWrapperObject::type_id);
+  if(stream.reading()) {
+    obj=dynamic_cast<rtrt::InstanceWrapperObject*>(pobj);
+    ASSERT(obj != 0)
+  }
 }
 } // end namespace SCIRun
