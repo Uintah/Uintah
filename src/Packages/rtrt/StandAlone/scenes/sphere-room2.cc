731fa9b4185979ef6647e89ff25c4418cfc6bb70
Chris Moulding <moulding@sci.utah.edu>
2002-07-12 22:41:42 +0000

    use Light2 instead of Light for sun

--- src/Packages/rtrt/StandAlone/scenes/sphere-room2.cc
+++ src/Packages/rtrt/StandAlone/scenes/sphere-room2.cc
@@ -12,6 +12,7 @@
 #include <Packages/rtrt/Core/Camera.h>
 #include <Packages/rtrt/Core/Scene.h>
 #include <Packages/rtrt/Core/Light.h>
+#include <Packages/rtrt/Core/Light2.h>
 
 #include <iostream>
 #include <math.h>
@@ -31,9 +32,10 @@ using namespace std;
 #define INSCILAB               0
 #define SYSTEM_SIZE_SCALE      1.438848E-4/*1.438848E-6*/
 #define SYSTEM_DISTANCE_SCALE  6.76E-8/*3.382080E-9*/
-#define SYSTEM_TIME_SCALE1     .5
-#define SYSTEM_TIME_SCALE2     .5
-#if 1
+#define SYSTEM_TIME_SCALE1     .4
+#define SYSTEM_TIME_SCALE2     .02
+#define FLIP_IMAGES            true
+#if 0
 #define IMAGEDIR      "/home/moulding/images/"
 #else
 #define IMAGEDIR      "/home/sci/moulding/images/"
@@ -65,7 +67,7 @@ satellite_data table[] = {
   { 6378, 1.496E8, .99727, 365.26, .0167, 23.45, 0,
     0, 0, 0, "earth.ppm" },
   
-  { 1737.4, 384400*100, 27.32166, 27.32166, .0549, 1.5424, 5.1454,
+  { 1737.4, 384400*50, 27.32166, 27.32166, .0549, 1.5424, 5.1454,
     1, 0, 0, "luna.ppm" },
   
   { 2439.7, 5.791E7, 58.65, 87.97, .2056, 0, 7.004, 
@@ -89,16 +91,14 @@ satellite_data table[] = {
   { 24746, 4.5043E9, 1.52, 60190, .0097, 28.31, 1.774,
     0, 8, 0, "neptune.ppm" },
   
-  { 1137, 5.91352E9, -6.3872, 90779, .2482, 122.52, 17.148,
+  { 1137*6, 5.91352E9, -6.3872, 90779, .2482, 122.52, 17.148,
     0, 1, 0, "pluto.ppm" },
 
-#if 0
-  { 1815, 421600, 1.769, 1.769, .004, 0, .04,
-    5, 0, 0, "io.ppm" },
+  { 1815, 421600*400, 1.769, 1.769, .004, 0, .04,
+    6, 0, 0, "io.ppm" },
 
-  { 1569, 670900, 3.55, 3.55, .009, 0, .47,
-    5, 0, 0, "europa.ppm" },
-#endif
+  { 1569, 670900*400, 3.55, 3.55, .009, 0, .47,
+    6, 0, 0, "europa.ppm" },
 
   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
 };
@@ -110,8 +110,8 @@ Scene *make_scene(int /*argc*/, char* /*argv*/[], int /*nworkers*/)
 
   Camera cam( Point(20,20,20), Point( 0,0,0 ), Vector(0,0,1), 45.0 );
 
-  // creat a scene
-  double ambient_scale=0.5;
+  // create a scene
+  double ambient_scale=0.1;
   Color bgcolor(0.1, 0.2, 0.45);
   Color cdown(0.82, 0.62, 0.62);
   Color cup(0.1, 0.3, 0.8);
@@ -119,35 +119,39 @@ Scene *make_scene(int /*argc*/, char* /*argv*/[], int /*nworkers*/)
   Scene* scene=new Scene(solar_system, cam,
                          bgcolor, cdown, cup, groundplane,
                          ambient_scale, Constant_Ambient);
-  scene->add_light( new Light(Point(0,0,0), Color(1,1,1), 0.0) );
-  scene->set_background_ptr( new EnvironmentMapBackground(IMAGEDIR"tycho8.ppm"));
   scene->select_shadow_mode(No_Shadows);
+  
+  EnvironmentMapBackground *starfield = 
+    new EnvironmentMapBackground(IMAGEDIR"tycho8.ppm");
+  scene->set_background_ptr( starfield );
 
+  // materials
   Material *white = new LambertianMaterial(Color(1,1,1));
 
   TileImageMaterial *sol_m = 
-    new TileImageMaterial(IMAGEDIR"sunmap.ppm", 1, Color(1,1,1), 0, 0, false);
-
-  HaloMaterial *corona_m = new HaloMaterial(new LightMaterial(Color(1,1,1)),4);
+    new TileImageMaterial(IMAGEDIR"sunmap.ppm", 1, Color(1,1,1), 0, 0, 
+                          FLIP_IMAGES);
 
   MapBlendMaterial *earth_spec = 
     new MapBlendMaterial(IMAGEDIR"earthspec4k.ppm", 
                          new TileImageMaterial(IMAGEDIR"earth.ppm", 1, 
-                                               Color(1,1,1), 100, 0, false),
+                                               Color(1,1,1), 100, 0, 
+                                               FLIP_IMAGES),
                          new TileImageMaterial(IMAGEDIR"earth.ppm", 1, 
-                                               Color(1,1,1), 0, 0, false));
+                                               Color(1,1,1), 0, 0, 
+                                               FLIP_IMAGES),
+                         true);
 
   MapBlendMaterial *earth_m = 
     new MapBlendMaterial(IMAGEDIR"earthclouds.ppm", white, earth_spec);
-                                            
-  double radius = 1;
-  double orb_radius = 0;
+              
 
-  // build sol and the earth first
-  cerr << "radius = " << radius << endl;
-  cerr << "orb radius = " << orb_radius << endl;  
-  Satellite *sol = new Satellite("Sol", sol_m, Point(0,0,0), radius*.7);
-  Sphere *corona = new Sphere(corona_m, Point(0,0,0), radius);
+  // objects
+  double radius;
+  double orb_radius;
+
+  // build the sun (represented later as a light in the scene)
+  Satellite *sol = new Satellite("Sol", sol_m, Point(0,0,0), .01);
 
   radius = table[1].radius_*SYSTEM_SIZE_SCALE;
   orb_radius = table[1].orb_radius_*SYSTEM_DISTANCE_SCALE;
@@ -163,24 +167,21 @@ Scene *make_scene(int /*argc*/, char* /*argv*/[], int /*nworkers*/)
   cerr << "sol = " << sol << endl;
   earth->set_orb_speed(1./table[1].orb_speed_*SYSTEM_TIME_SCALE2);
   earth->set_rev_speed(1./table[1].rot_speed_*SYSTEM_TIME_SCALE1);
-  earth->set_up(Vector(sin(table[1].tilt_),0,cos(table[1].tilt_)));
+  earth->set_up(Vector(cos(table[1].tilt_),0,sin(table[1].tilt_)));
   earth->set_parent(sol);
   table[1].self_ = earth;
   cerr << "earth = " << earth << endl;
 
-  solar_system->add( sol );
-  solar_system->add( corona );
   solar_system->add( earth );
 
-  scene->addObjectOfInterest(sol,true);
   scene->addObjectOfInterest(earth,true);
 
   // build the other satellites
-  for (unsigned loop=2; loop<6/*table[loop].radius_!=0*/; ++loop) {
+  for (unsigned loop=2; table[loop].radius_!=0; ++loop) {
 
     Material *newmat = 
         new TileImageMaterial(string(IMAGEDIR)+table[loop].tmap_filename_,
-                              1, Color(1,1,1), 0, 0, false);
+                              1, Color(1,1,1), 0, 0, FLIP_IMAGES);
 
     radius = table[loop].radius_*SYSTEM_SIZE_SCALE;
     orb_radius = table[loop].orb_radius_*SYSTEM_DISTANCE_SCALE;
@@ -188,7 +189,7 @@ Scene *make_scene(int /*argc*/, char* /*argv*/[], int /*nworkers*/)
     cerr << "radius = " << radius << endl;
     cerr << "orb radius = " << orb_radius << endl;
     
-    Satellite *newsat = new Satellite(string("Planet ")+(char)loop,
+    Satellite *newsat = new Satellite(string("Planet ")+(char)(loop+101),
                                       newmat, Point(orb_radius,0,0), 
                                       radius, 0);
     table[loop].self_ = newsat;
@@ -204,6 +205,11 @@ Scene *make_scene(int /*argc*/, char* /*argv*/[], int /*nworkers*/)
 
     scene->addObjectOfInterest( newsat, true );
   }
+
+  // add the light (the sun, as mentioned above)
+  Light2 *light = new Light2(sol_m, Color(1,1,1), Point(0,0,0), 1);
+  scene->add_light( light );
+  scene->addObjectOfInterest(light->getSphere(),true);
 
   return scene;
 }
