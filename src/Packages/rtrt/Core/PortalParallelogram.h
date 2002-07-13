ca7fdbf990beb2757bf233e2b019521011f07d8c
Chris Moulding <moulding@sci.utah.edu>
2002-07-13 18:31:44 +0000

    fixed portal transforms (hopefully for good)

--- src/Packages/rtrt/Core/PortalParallelogram.h
+++ src/Packages/rtrt/Core/PortalParallelogram.h
@@ -14,42 +14,21 @@ class PortalParallelogram : public Parallelogram
  protected:
 
   PortalMaterial      portal_m_;
-  PortalParallelogram *other_end_;
-
-  void set_opposite_end(PortalParallelogram *b) { other_end_ = b; set(); }
-  PortalParallelogram *get_opposite_end() { return other_end_; }
-  void set()
-  {
-    if (other_end_)
-      portal_m_.set(anchor, u, v,
-                    other_end_->get_anchor(), 
-                    other_end_->get_u(), other_end_->get_v());
-  }
 
  public:
 
-  PortalParallelogram(const Point &p, const Vector &u, const Vector &v,
-                      PortalParallelogram *other=0)
-    : Parallelogram(&portal_m_, p, u, v), other_end_(other) {}
+  PortalParallelogram(const Point &p, const Vector &u, const Vector &v)
+    : Parallelogram(&portal_m_, p, u, v) { portal_m_.set(p,u,v); }
   virtual ~PortalParallelogram() {}
 
-  virtual void preprocess(double /*maxradius*/, 
-                          int& /*pp_offset*/, 
-                          int& /*scratchsize*/)
-  {
-    set();
-  }
-  
-  void attach(PortalParallelogram *a)
-  {
-    set_opposite_end(a);
-    a->set_opposite_end(this);
-  }
+  PortalMaterial *get_portal() { return &portal_m_; }
 
   static void attach(PortalParallelogram *a, PortalParallelogram *b)
   {
-    a->set_opposite_end(b);
-    b->set_opposite_end(a);
+    PortalMaterial *a_ = a->get_portal();
+    PortalMaterial *b_ = b->get_portal();
+
+    a_->attach(b_); /* this call also does b_->attach(a_) */
   }
 };
 
