1f4da77331a1cde8eb1838857a2c1a0d6fd2d24b
Chris Moulding <moulding@sci.utah.edu>
2002-07-15 01:31:53 +0000

    add '2' functions to support Reinhard's stuff

--- Packages/rtrt/Core/Array1.h
+++ Packages/rtrt/Core/Array1.h
@@ -19,11 +19,7 @@
 #include <Core/Persistent/Persistent.h>
 
 namespace rtrt {
- template<class T> class Array1;
-}
-
-namespace rtrt {
-
+  
 using SCIRun::Piostream;
 
 #ifdef __GNUG__
@@ -32,58 +28,65 @@ using SCIRun::Piostream;
 
 template<class T>
 class Array1 {
-  T* objs;
-  int _size;
-  int nalloc;
-  int default_grow_size;
+    T* objs;
+    int _size;
+    int nalloc;
+    int default_grow_size;
 public:
-  Array1(const Array1&);
-  Array1(int size=0, int default_grow_size=10, int asize=-1);
-  Array1<T>& operator=(const Array1&);
-  ~Array1();
-  // Accesses the nth element of the array
-  inline T& operator[](int n) const {
-    ASSERTRANGE(n, 0, _size);
-    return objs[n];
-  }
-  // Returns the size of the array
-  inline int size() const{ return _size;}
+    Array1(const Array1&);
+    Array1(int size=0, int default_grow_size=10, int asize=-1);
+    Array1<T>& operator=(const Array1&);
+    ~Array1();
+    // Accesses the nth element of the array
+    inline T& operator[](int n) const {
+	ASSERTRANGE(n, 0, _size);
+	return objs[n];
+    }
+    // Returns the size of the array
+    inline int size() const{ return _size;}
 
-  // Make the array larger by count elements
-  void grow(int count, int grow_size=10);
+    // Make the array larger by count elements
+    void grow(int count, int grow_size=10);
 
-  // Add one element to the array.  equivalent to:
-  //  grow(1)
-  //  array[array.size()-1]=data;
-  void add(const T&);
+    // Add one element to the array.  equivalent to:
+    //  grow(1)
+    //  array[array.size()-1]=data;
+    void add(const T&);
+    int  add2(const T&);         // return index number
 
-  // Insert one element into the array.  This is very inefficient
-  // if you insert anything besides the (new) last element.
-  void insert(int, const T&);
+    // Insert one element into the array.  This is very inefficient
+    // if you insert anything besides the (new) last element.
+    void insert(int, const T&);
 
-  // Remove one element from the array.  This is very inefficient
-  // if you remove anything besides the last element.
-  void remove(int);
+    // Remove one element from the array.  This is very inefficient
+    // if you remove anything besides the last element.
+    void remove(int);
 
-  // Remove all elements in the array.  The array is not freed,
-  // and the number of allocated elements remains the same.
-  void remove_all();
+    // Remove all elements in the array.  The array is not freed,
+    // and the number of allocated elements remains the same.
+    void remove_all();
 
-  // Change the size of the array.
-  void resize(int newsize);
+    // Change the size of the array.
+    void resize(int newsize);
 
-  // Changes size, makes exact if currently smaller...
-  void setsize(int newsize);
+    // Changes size, makes exact if currently smaller...
+    void setsize(int newsize);
 
-  // Initialize all elements of the array
-  void initialize(const T& val);
+    // Initialize all elements of the array
+    void initialize(const T& val);
 
-  // Get the array information
-  T* get_objs();
+    // Get the array information
+    T* get_objs();
+
+    void Pio(Piostream&, Array1<T>&);
+    void Pio(Piostream&, Array1<T>*&);
 };
 
-} // end namespace rtrt
+template<class T> void Pio(Piostream&, Array1<T>&);
+template<class T> void Pio(Piostream&, Array1<T>*&);
 
 #include <Packages/rtrt/Core/Array1.cc>
+
+} // end namespace rtrt
 
 #endif // Packages_rtrt_Core_Array1_h
