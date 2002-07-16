66427524d3c2c2581469a801887ebcb2941c6e5a
David Weinstein <dmw@sci.utah.edu>
2002-07-16 20:33:17 +0000

    - Need to have Glpyh in the sub.mk so glyph_threshold gets resolved

--- src/Packages/rtrt/Core/sub.mk
+++ src/Packages/rtrt/Core/sub.mk
@@ -148,6 +148,7 @@ SRCS += $(SRCDIR)/Worker.cc \
 	$(SRCDIR)/InstanceWrapperObject.cc \
 	$(SRCDIR)/PPMImage.cc \
 	$(SRCDIR)/Satellite.cc \
+	$(SRCDIR)/Glyph.cc \
 	$(SRCDIR)/RingSatellite.cc \
 	$(SRCDIR)/Grid2.cc \
 
