c14cd3e84556f34362d6db9daff447c637d2f4f4
Wayne Witzel <witzel@sci.utah.edu>
2002-07-16 20:37:17 +0000

    Bryan forgot to make his fix to both relocateParticles functions

--- src/Packages/Uintah/CCA/Components/Schedulers/Relocate.cc
+++ src/Packages/Uintah/CCA/Components/Schedulers/Relocate.cc
@@ -618,7 +618,7 @@ MPIRelocate::relocateParticles(const ProcessorGroup* pg,
 
       ASSERT(is_sorted(pset->begin(), pset->end()));
       ASSERT(is_sorted(delset->begin(), delset->end()));
-      ASSERTEQ(*pset->begin(), 0);
+      ASSERT(pset->begin() == NULL || *pset->begin() == 0);
 
       for(ParticleSubset::iterator iter = pset->begin();
 	  iter != pset->end(); iter++){
