f73d1f494baf80dd87f95e824fec5682941d52fa
Chris Moulding <moulding@sci.utah.edu>
2002-07-20 06:00:02 +0000

    declare member functions as type void

--- SIGGRAPH02/src/Packages/rtrt/Core/TexturedTri2.h
+++ SIGGRAPH02/src/Packages/rtrt/Core/TexturedTri2.h
@@ -21,9 +21,10 @@ class TexturedTri2 : public TexturedTri
     : TexturedTri(m,p0,p1,p2) { tex_trans_.load_identity(); }
   virtual ~TexturedTri2() {}
 
-  const translate_tex(const Vector &v) { tex_trans_.pre_translate(v); }
+  void translate_tex(const Vector &v)
+    { tex_trans_.pre_translate(v); }
 
-  const scale_tex(double x, double y, double z) 
+  void scale_tex(double x, double y, double z)
     { tex_trans_.pre_scale( Vector(x,y,z) ); }
     
   void uv(UV& uv, const Point&, const HitInfo& hit)
