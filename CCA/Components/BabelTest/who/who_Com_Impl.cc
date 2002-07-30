3594a84c4124d9177d3dc26604852a2ddda14aea
Keming Zhang <kzhang@sci.utah.edu>
2002-07-30 20:20:23 +0000

    Added Files: babel component  	sub.mk who.hh who.sidl who_Com_Impl.cc who_Com_Impl.hh  	who_IDPort_Impl.cc who_IDPort_Impl.hh who_UIPort_Impl.cc 	who_UIPort_Impl.hh

---
+++ CCA/Components/BabelTest/who/who_Com_Impl.cc
@@ -0,0 +1,62 @@
+// 
+// File:          who_Com_Impl.cc
+// Symbol:        who.Com-v1.0
+// Symbol Type:   class
+// Babel Version: 0.7.0
+// SIDL Created:  20020730 13:51:05 MST
+// Generated:     20020730 13:51:13 MST
+// Description:   Server-side implementation for who.Com
+// 
+// WARNING: Automatically generated; only changes within splicers preserved
+// 
+// 
+#include "who_Com_Impl.hh"
+
+// DO-NOT-DELETE splicer.begin(who.Com._includes)
+#include "who.hh"
+#include "govcca.hh"
+// DO-NOT-DELETE splicer.end(who.Com._includes)
+
+// user defined constructor
+void who::Com_impl::_ctor() {
+  // DO-NOT-DELETE splicer.begin(who.Com._ctor)
+  // add construction details here
+  // DO-NOT-DELETE splicer.end(who.Com._ctor)
+}
+
+// user defined destructor
+void who::Com_impl::_dtor() {
+  // DO-NOT-DELETE splicer.begin(who.Com._dtor)
+  // add destruction details here
+  // DO-NOT-DELETE splicer.end(who.Com._dtor)
+}
+
+// user defined static methods: (none)
+
+// user defined non-static methods:
+/**
+ * Obtain Services handle, through which the component communicates with the
+ * framework. This is the one method that every CCA Component must implement. 
+ */
+void
+who::Com_impl::setServices (
+  /*in*/ govcca::Services svc ) 
+throw () 
+{
+  // DO-NOT-DELETE splicer.begin(who.Com.setServices)
+  who::IDPort idPort=who::IDPort::_create();
+  svc.addProvidesPort(idPort,"idport","gov.cca.IDPort",0);
+
+  who::UIPort uiPort=who::UIPort::_create();
+  svc.addProvidesPort(uiPort,"babel.ui","gov.cca.UIPort",0);
+  // DO-NOT-DELETE splicer.end(who.Com.setServices)
+}
+
+
+// DO-NOT-DELETE splicer.begin(who.Com._misc)
+extern "C" govcca::Component make_Babel_who()
+{
+  return who::Com::_create();
+}
+// DO-NOT-DELETE splicer.end(who.Com._misc)
+
