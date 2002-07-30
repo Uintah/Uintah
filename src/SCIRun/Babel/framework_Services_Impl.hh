b6936212c0742b101e8a8fa9d5a07320436feec3
Keming Zhang <kzhang@sci.utah.edu>
2002-07-30 20:26:16 +0000

    Added Files:  	BabelComponentDescription.cc BabelComponentDescription.h  	BabelComponentInstance.cc BabelComponentInstance.h  	BabelComponentModel.cc BabelComponentModel.h  	BabelPortInstance.cc BabelPortInstance.h framework.hh  	framework.sidl framework_Component.cc framework_Component.hh  	framework_ComponentID.cc framework_ComponentID.hh  	framework_ComponentID_Impl.cc framework_ComponentID_Impl.hh  	framework_Component_Impl.cc framework_Component_Impl.hh  	framework_Port.cc framework_Port.hh framework_Port_Impl.cc  	framework_Port_Impl.hh framework_Services.cc  	framework_Services.hh framework_Services_Impl.cc  	framework_Services_Impl.hh framework_TypeMap.cc 	framework_TypeMap.hh framework_TypeMap_Impl.cc 	framework_TypeMap_Impl.hh sub.mk

---
+++ src/SCIRun/Babel/framework_Services_Impl.hh
@@ -0,0 +1,218 @@
+// 
+// File:          framework_Services_Impl.hh
+// Symbol:        framework.Services-v1.0
+// Symbol Type:   class
+// Babel Version: 0.7.0
+// SIDL Created:  20020730 13:51:29 MST
+// Generated:     20020730 13:51:36 MST
+// Description:   Server-side implementation for framework.Services
+// 
+// WARNING: Automatically generated; only changes within splicers preserved
+// 
+// 
+
+#ifndef included_framework_Services_Impl_hh
+#define included_framework_Services_Impl_hh
+
+#ifndef included_SIDL_cxx_hh
+#include "SIDL_cxx.hh"
+#endif
+#ifndef included_framework_Services_IOR_h
+#include "framework_Services_IOR.h"
+#endif
+// 
+// Includes for all method dependencies.
+// 
+#ifndef included_SIDL_BaseInterface_hh
+#include "SIDL_BaseInterface.hh"
+#endif
+#ifndef included_framework_Services_hh
+#include "framework_Services.hh"
+#endif
+#ifndef included_govcca_ComponentID_hh
+#include "govcca_ComponentID.hh"
+#endif
+#ifndef included_govcca_Port_hh
+#include "govcca_Port.hh"
+#endif
+#ifndef included_govcca_TypeMap_hh
+#include "govcca_TypeMap.hh"
+#endif
+
+
+// DO-NOT-DELETE splicer.begin(framework.Services._includes)
+// Put additional includes or other arbitrary code here...
+#include <SCIRun/Babel/BabelPortInstance.h>
+using namespace SCIRun;
+// DO-NOT-DELETE splicer.end(framework.Services._includes)
+
+namespace framework { 
+
+  /**
+   * Symbol "framework.Services" (version 1.0)
+   */
+  class Services_impl
+  // DO-NOT-DELETE splicer.begin(framework.Services._inherits)
+  // Put additional inheritance here...
+  // DO-NOT-DELETE splicer.end(framework.Services._inherits)
+  {
+
+  private:
+    // Pointer back to IOR.
+    // Use this to dispatch back through IOR vtable.
+    Services self;
+
+    // DO-NOT-DELETE splicer.begin(framework.Services._implementation)
+    std::map<std::string, PortInstance*> ports;
+    govcca::Component component;
+    // DO-NOT-DELETE splicer.end(framework.Services._implementation)
+
+  private:
+    // private default constructor (required)
+    Services_impl() {} 
+
+  public:
+    // SIDL constructor (required)
+    // Note: alternate Skel constructor doesn't call addref()
+    // (fixes bug #275)
+    Services_impl( struct framework_Services__object * s ) : self(s,
+      true) { _ctor(); }
+
+    // user defined construction
+    void _ctor();
+
+    // virtual destructor (required)
+    virtual ~Services_impl() { _dtor(); }
+
+    // user defined destruction
+    void _dtor();
+
+  public:
+
+    /**
+     * user defined non-static method.
+     */
+    void*
+    getData() throw () 
+    ;
+
+    /**
+     * Ask for a previously registered Port; will return a Port or generate an error. 
+     */
+    govcca::Port
+    getPort (
+      /*in*/ std::string name
+    )
+    throw () 
+    ;
+
+
+    /**
+     * Ask for a previously registered Port and return that Port if it is
+     * available or return null otherwise. 
+     */
+    govcca::Port
+    getPortNonblocking (
+      /*in*/ std::string name
+    )
+    throw () 
+    ;
+
+
+    /**
+     * Modified according to Motion 31 
+     */
+    void
+    registerUsesPort (
+      /*in*/ std::string name,
+      /*in*/ std::string type,
+      /*in*/ govcca::TypeMap properties
+    )
+    throw () 
+    ;
+
+
+    /**
+     * Notify the framework that a Port, previously registered by this component,
+     * is no longer needed. 
+     */
+    void
+    unregisterUsesPort (
+      /*in*/ std::string name
+    )
+    throw () 
+    ;
+
+
+    /**
+     * Exports a Port implemented by this component to the framework.  
+     * This Port is now available for the framework to connect to other components. 
+     * Modified according to Motion 31 
+     */
+    void
+    addProvidesPort (
+      /*in*/ govcca::Port inPort,
+      /*in*/ std::string name,
+      /*in*/ std::string type,
+      /*in*/ govcca::TypeMap properties
+    )
+    throw () 
+    ;
+
+
+    /**
+     * Notifies the framework that a previously exported Port is no longer 
+     * available for use.
+     */
+    void
+    removeProvidesPort (
+      /*in*/ std::string name
+    )
+    throw () 
+    ;
+
+
+    /**
+     * Notifies the framework that this component is finished with this Port.   
+     * releasePort() method calls exactly match getPort() mehtod calls.  After 
+     * releasePort() is invoked all references to the released Port become invalid. 
+     */
+    void
+    releasePort (
+      /*in*/ std::string name
+    )
+    throw () 
+    ;
+
+    /**
+     * user defined non-static method.
+     */
+    govcca::TypeMap
+    createTypeMap() throw () 
+    ;
+    /**
+     * user defined non-static method.
+     */
+    govcca::TypeMap
+    getPortProperties (
+      /*in*/ std::string portName
+    )
+    throw () 
+    ;
+
+
+    /**
+     * Get a reference to the component to which this Services object belongs. 
+     */
+    govcca::ComponentID
+    getComponentID() throw () 
+    ;
+  };  // end class Services_impl
+
+} // end namespace framework
+
+// DO-NOT-DELETE splicer.begin(framework.Services._misc)
+// Put miscellaneous things here...
+// DO-NOT-DELETE splicer.end(framework.Services._misc)
+
+#endif
