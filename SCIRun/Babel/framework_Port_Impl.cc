18d16d5a1931d96d9f5c130d26ce6568f9d39cd7
Keming Zhang <kzhang@sci.utah.edu>
2002-07-30 20:26:16 +0000

    Added Files:  	BabelComponentDescription.cc BabelComponentDescription.h  	BabelComponentInstance.cc BabelComponentInstance.h  	BabelComponentModel.cc BabelComponentModel.h  	BabelPortInstance.cc BabelPortInstance.h framework.hh  	framework.sidl framework_Component.cc framework_Component.hh  	framework_ComponentID.cc framework_ComponentID.hh  	framework_ComponentID_Impl.cc framework_ComponentID_Impl.hh  	framework_Component_Impl.cc framework_Component_Impl.hh  	framework_Port.cc framework_Port.hh framework_Port_Impl.cc  	framework_Port_Impl.hh framework_Services.cc  	framework_Services.hh framework_Services_Impl.cc  	framework_Services_Impl.hh framework_TypeMap.cc 	framework_TypeMap.hh framework_TypeMap_Impl.cc 	framework_TypeMap_Impl.hh sub.mk

---
+++ SCIRun/Babel/framework_Port_Impl.cc
@@ -0,0 +1,40 @@
+// 
+// File:          framework_Port_Impl.cc
+// Symbol:        framework.Port-v1.0
+// Symbol Type:   class
+// Babel Version: 0.7.0
+// SIDL Created:  20020730 13:51:30 MST
+// Generated:     20020730 13:51:36 MST
+// Description:   Server-side implementation for framework.Port
+// 
+// WARNING: Automatically generated; only changes within splicers preserved
+// 
+// 
+#include "framework_Port_Impl.hh"
+
+// DO-NOT-DELETE splicer.begin(framework.Port._includes)
+// Put additional includes or other arbitrary code here...
+// DO-NOT-DELETE splicer.end(framework.Port._includes)
+
+// user defined constructor
+void framework::Port_impl::_ctor() {
+  // DO-NOT-DELETE splicer.begin(framework.Port._ctor)
+  // add construction details here
+  // DO-NOT-DELETE splicer.end(framework.Port._ctor)
+}
+
+// user defined destructor
+void framework::Port_impl::_dtor() {
+  // DO-NOT-DELETE splicer.begin(framework.Port._dtor)
+  // add destruction details here
+  // DO-NOT-DELETE splicer.end(framework.Port._dtor)
+}
+
+// user defined static methods: (none)
+
+// user defined non-static methods: (none)
+
+// DO-NOT-DELETE splicer.begin(framework.Port._misc)
+// Put miscellaneous code here
+// DO-NOT-DELETE splicer.end(framework.Port._misc)
+
