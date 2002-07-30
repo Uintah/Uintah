1f61a2f07931c41ab1bbac1be8baeda58f626cf9
Keming Zhang <kzhang@sci.utah.edu>
2002-07-30 20:10:15 +0000

    Add babel enviroment and babel components common submake files.  	babel_component.mk babel_defs.mk

---
+++ src/scripts/babel_component.mk
@@ -0,0 +1,37 @@
+include $(SCIRUN_SCRIPTS)/smallso_prologue.mk
+
+#define BABELDIR BABEL BABEL_REPOSITORY CCASIDL here
+include $(SCIRUN_SCRIPTS)/babel_defs.mk
+
+COMPONENT:=$(notdir $(SRCDIR))
+
+CCASIDL := ${SRCTOP}/Core/Babel/cca.sidl
+OUTDIR := ${SRCTOP_ABS}/${SRCDIR}
+BABEL_REPOSITORY := Core/Babel/repository
+
+${OUTDIR}/${COMPONENT}.make: ${OUTDIR}/${COMPONENT}.sidl Core/Babel/timestamp
+	$(BABEL) -sC++ -o$(dir $@) -R${BABEL_REPOSITORY} $<	
+	mv  $(dir $@)babel.make $@
+
+${OUTDIR}/cca.make: ${CCASIDL} 
+	$(BABEL) -cC++ -o/${dir $@} $<
+	mv $(dir $@)babel.make $@
+${OUTDIR}/srcfile.make: ${OUTDIR}/cca.make ${OUTDIR}/${COMPONENT}.make
+	cd $(dir $@) && $(MERGESRC) $(notdir $^)
+
+include ${OUTDIR}/srcfile.make
+
+INCLUDES+= -I${BABELDIR}/include 
+
+SRCS     += $(CSRC:%=$(SRCDIR)/%) $(CCSRC:%=$(SRCDIR)/%)
+
+PSELIBS := Core/CCA/Component/CIA Core/CCA/Component/PIDL Core/CCA/Component/Comm\
+	Core/CCA/spec Core/Thread Core/Containers Core/Exceptions
+
+#QT_LIBDIR := /home/sparker/SCIRun/SCIRun_Thirdparty_32_linux/lib
+#LIBS := $(QT_LIBS)
+
+LIBS := -L${BABELDIR}/lib -lsidl
+
+include $(SCIRUN_SCRIPTS)/smallso_epilogue.mk
+
