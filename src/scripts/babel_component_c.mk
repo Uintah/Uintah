eca56ee7b3ad2cfaf1b66004f33549b415fc02f8
Keming Zhang <kzhang@sci.utah.edu>
2002-08-01 22:57:30 +0000

    Modified Files:  	babel_component.mk so_epilogue.mk  Added Files:  	babel_component_c.mk babel_component_f77.mk

--- src/scripts/babel_component_c.mk
+++ src/scripts/babel_component_c.mk
@@ -8,20 +8,29 @@ COMPONENT:=$(notdir $(SRCDIR))
 OUTDIR := ${SRCTOP_ABS}/${SRCDIR}
 
 ${OUTDIR}/${COMPONENT}.make: ${OUTDIR}/${COMPONENT}.sidl Core/Babel/timestamp
-	$(BABEL) -sC++ -o$(dir $@) -R${BABEL_REPOSITORY} $<	
+	$(BABEL) -sC -o$(dir $@) -R${BABEL_REPOSITORY} $<	
+	mv  $(dir $@)babel.make $@
+
+${OUTDIR}/${COMPONENT}_stub.make: ${OUTDIR}/${COMPONENT}.sidl Core/Babel/timestamp
+	$(BABEL) -cC++ -o$(dir $@) -R${BABEL_REPOSITORY} $<	
 	mv  $(dir $@)babel.make $@
 
 ${OUTDIR}/cca.make: ${CCASIDL} 
+	$(BABEL) -cC -o/${dir $@} $<
+	mv $(dir $@)babel.make $@
+
+${OUTDIR}/cca_stub.make: ${CCASIDL} 
 	$(BABEL) -cC++ -o/${dir $@} $<
 	mv $(dir $@)babel.make $@
-${OUTDIR}/srcfile.make: ${OUTDIR}/cca.make ${OUTDIR}/${COMPONENT}.make
+
+${OUTDIR}/srcfile.make: ${OUTDIR}/cca.make ${OUTDIR}/${COMPONENT}.make ${OUTDIR}/cca_stub.make ${OUTDIR}/${COMPONENT}_stub.make
 	cd $(dir $@) && $(MERGESRC) $(notdir $^)
 
 include ${OUTDIR}/srcfile.make
 
 INCLUDES+= -I${BABELDIR}/include 
 
-SRCS     += $(XSRCS) $(CSRC:%=$(SRCDIR)/%) $(CCSRC:%=$(SRCDIR)/%)
+SRCS    += $(XSRCS) $(CSRC:%=$(SRCDIR)/%) $(CCSRC:%=$(SRCDIR)/%) 
 
 PSELIBS := Core/CCA/Component/CIA Core/CCA/Component/PIDL Core/CCA/Component/Comm\
 	Core/CCA/spec Core/Thread Core/Containers Core/Exceptions
