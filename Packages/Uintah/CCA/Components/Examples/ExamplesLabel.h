345526a9e0be439dd9c293b3bdf711ec08143ca5
Steven G. Parker <sparker@sci.utah.edu>
2002-09-25 02:57:50 +0000

    3D version parallel added buoyancy/gravity use solver component made it work

--- Packages/Uintah/CCA/Components/Examples/ExamplesLabel.h
+++ Packages/Uintah/CCA/Components/Examples/ExamplesLabel.h
@@ -16,6 +16,22 @@ namespace Uintah {
     const VarLabel* yvelocity;
     const VarLabel* zvelocity;
     const VarLabel* density;
+    const VarLabel* temperature;
+    const VarLabel* pressure;
+    const VarLabel* ccvelocity;
+
+    const VarLabel* xvelocity_matrix;
+    const VarLabel* xvelocity_rhs;
+    const VarLabel* yvelocity_matrix;
+    const VarLabel* yvelocity_rhs;
+    const VarLabel* zvelocity_matrix;
+    const VarLabel* zvelocity_rhs;
+    const VarLabel* density_matrix;
+    const VarLabel* density_rhs;
+    const VarLabel* pressure_matrix;
+    const VarLabel* pressure_rhs;
+    const VarLabel* temperature_matrix;
+    const VarLabel* temperature_rhs;
 
     // For Burger
     const VarLabel* u;
