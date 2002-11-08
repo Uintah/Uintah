7e5d003ac703d662d14c49623e7fb7d195d751aa
Bryan Jeffrey Worthen <worthen@sci.utah.edu>
2002-11-08 18:48:17 +0000

    reflect new location of DataArchive; remove XML libs from LIBS

--- src/Packages/Uintah/CCA/Components/PatchCombiner/sub.mk
+++ src/Packages/Uintah/CCA/Components/PatchCombiner/sub.mk
@@ -8,7 +8,7 @@ SRCS     += $(SRCDIR)/PatchCombiner.cc
 
 PSELIBS := \
 	Packages/Uintah/CCA/Components/Schedulers        \
-	Packages/Uintah/CCA/Ports        \
+	Packages/Uintah/Core/DataArchive \
 	Packages/Uintah/Core/Parallel    \
 	Packages/Uintah/Core/Grid        \
 	Packages/Uintah/Core/Disclosure  \
@@ -18,10 +18,9 @@ PSELIBS := \
 	Core/Exceptions  \
 	Core/Containers  \
 	Core/Thread      \
-	Core/Util        \
-	Dataflow/XMLUtil 
+	Core/Util
 
-LIBS := $(XML_LIBRARY) $(MPI_LIBRARY) $(M_LIBRARY)
+LIBS := $(MPI_LIBRARY) $(M_LIBRARY)
 
 include $(SCIRUN_SCRIPTS)/smallso_epilogue.mk
 
