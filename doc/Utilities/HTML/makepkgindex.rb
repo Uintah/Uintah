bde8d3925d3090a4902b0a35db02b4955f21baed
Ted Dustman <dustman@sci.utah.edu>
2002-11-08 00:23:56 +0000

    Mark latex based module descriptions to distinguish them from xml based module descriptions.

--- doc/Utilities/HTML/makepkgindex.rb
+++ doc/Utilities/HTML/makepkgindex.rb
@@ -23,7 +23,7 @@
 #
 
 class Entry
-  attr_reader :file, :base
+  attr_reader :base, :file
   def initialize(file)
     @file = file
     @base = File.basename(file)
@@ -31,17 +31,36 @@ class Entry
   def <=>(e)
     @base <=> e.base
   end
+  def get
+    "<td><a href='#{@file}'>#{File.basename(@file, '.html')}</a></td>"
+  end
 end
 
-packageName = ARGV[0]
-xmlDir = ARGV[1]
-texDir = ARGV[2]
+class LatexEntry < Entry
+  @@count = 0
+  def initialize(file)
+    super
+    @@count += 1
+  end
+  def get
+    "<td><a href='#{@file}'>#{File.basename(@file, '.html')}</a><sup>*</sup></td>"
+  end
+  def LatexEntry.count
+    @@count
+  end
+end
 
-begin
-  File.open("#{packageName}.html", "w") do |index|
+def main
 
-# Insert boilerplate at top of file
-  index.print <<EndOfString
+  packageName = ARGV[0]
+  xmlDir = ARGV[1]
+  texDir = ARGV[2]
+
+  begin
+    File.open("#{packageName}.html", "w") do |index|
+
+      # Insert boilerplate at top of file
+      index.print <<EndOfString
 <!doctype html public "-//w3c//dtd html 4.0 transitional//en">
 <!--
 The contents of this file are subject to the University of Utah Public
@@ -78,56 +97,58 @@ document.write('<script language="JavaScript" src="',treetop,'doc/Utilities/HTML
 </script>
 EndOfString
 
-  # Generate page heading.
-  index.print("<p class='title'> #{packageName} Module Descriptions</p>\n")
-  index.print("<hr size='1'/>")
+      # Generate page heading.
+      index.print("<p class='title'> #{packageName} Module Descriptions</p>\n")
+      index.print("<hr size='1'/>")
 
-  # Generate index of modules.  Alphabetical order in 3 columns.
-  entries = []
-  Dir["#{xmlDir}/*.html"].each do |f|
-    entries << Entry.new(f)
-  end
-  Dir["#{texDir}/*/*/*.html"].each do |f|
-    entries << Entry.new(f)
-  end
-  entries.sort!
-  numCols = 3
-  numRows = entries.size() / numCols
-  extras = entries.size() % numCols
-  numRowsArray = [ numRows, numRows, numRows ]
-  if extras > 0
-    numRowsArray[0] += 1
-  end
-  if extras > 1
-    numRowsArray[1] += 1
-  end
-  etable = []
-  etable[0] = entries[0, numRowsArray[0]]
-  etable[1] = entries[numRowsArray[0], numRowsArray[1]]
-  etable[2] = entries[numRowsArray[0] + numRowsArray[1], numRowsArray[2]]
-  index.print("<table align='center' cellspacing='5'>\n")
-  numRows.times do |i|
-    index.print("<tr>\n")
-    numCols.times do |j|
-      file = (etable[j][i]).file
-      index.print("<td><a href='#{file}'>#{File.basename(file, '.html')}</a></td>\n")
-    end
-    index.print("</tr>\n")
-  end
-  if extras > 0
-    index.print("<tr>\n")
-    file = (etable[0][numRows]).file
-    index.print("<td><a href='#{file}'>#{File.basename(file, '.html')}</a></td>\n")
-    if extras > 1
-      file = (etable[1][numRows]).file
-      index.print("<td><a href='#{file}'>#{File.basename(file, '.html')}</a></td>\n")
-    end        
-    index.print("</tr>\n")
-  end
-  index.print("</table>\n")
+      # Generate index of modules.  Alphabetical order in 3 columns.
+      entries = []
+      Dir["#{xmlDir}/*.html"].each do |f|
+          entries << Entry.new(f)
+      end
+		    
+      Dir["#{texDir}/*/*/*.html"].each do |f|
+        entries << LatexEntry.new(f)
+      end
+      entries.sort!
+      numCols = 3
+      numRows = entries.size() / numCols
+      extras = entries.size() % numCols
+      numRowsArray = [ numRows, numRows, numRows ]
+      if extras > 0
+        numRowsArray[0] += 1
+      end
+      if extras > 1
+        numRowsArray[1] += 1
+      end
+      etable = []
+      etable[0] = entries[0, numRowsArray[0]]
+      etable[1] = entries[numRowsArray[0], numRowsArray[1]]
+      etable[2] = entries[numRowsArray[0] + numRowsArray[1], numRowsArray[2]]
+      index.print("<table align='center' cellspacing='5'>\n")
+      numRows.times do |i|
+        index.print("<tr>\n")
+        numCols.times do |j|
+          index.print((etable[j][i]).get, "\n")
+        end
+        index.print("</tr>\n")
+      end
+      if extras > 0
+        index.print("<tr>\n")
+        index.print((etable[0][numRows]).get, "\n")
+        if extras > 1
+          index.print((etable[1][numRows]).get, "\n")
+        end        
+        index.print("</tr>\n")
+      end
+      index.print("</table>\n")
 
-  # Generate end of file boilerplate.
-  index.print <<EndOfString
+      if LatexEntry.count > 0
+        index.print("<p><sup>*</sup>Denotes additional documentation for a module.</p>\n")
+      end
+
+      # Generate end of file boilerplate.
+      index.print <<EndOfString
 <script language="JavaScript">
 document.write('<script language="JavaScript" src="',treetop,'doc/Utilities/HTML/banner_bottom.js"><\\/script>');
 </script>
@@ -135,9 +156,13 @@ document.write('<script language="JavaScript" src="',treetop,'doc/Utilities/HTML
 </html>
 EndOfString
 
+      end
+
+  rescue => oops
+    print("Unable to build #{packageName}.html for package\n")
+    print("Reason: ", oops.message, "\n")
   end
 
-rescue => oops
-  print("Unable to build #{packageName}.html for package\n")
-  print("Reason: ", oops.message, "\n")
 end
+
+main
