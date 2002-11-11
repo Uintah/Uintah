06b1ee5048efc614596cca48d2053f2853faf18d
Steven G. Parker <sparker@sci.utah.edu>
2002-11-11 16:12:31 +0000

    No longer necessary

--- src/scripts/babel_defs.mk
+++
@@ -1,6 +0,0 @@
-CCASIDL := Core/Babel/cca.sidl
-BABELDIR:= /usr/sci/projects/CCA/Thirdparty/1.0.0/Linux/babel-0.7.0
-BABEL:=$(BABELDIR)/bin/babel
-BABEL_REPOSITORY:= Core/Babel/repository
-#MERGESRC:=$(BABELDIR)/bin/mergesrc
-MERGESRC:=java -classpath $(BABELDIR)/bin/ mergesrc
