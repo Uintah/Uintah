024af971f3b32716b521940b9d7820b495a432b4
John Schmidt <jas@sci.utah.edu>
2002-11-11 22:12:46 +0000

    Added special case exception for FileGeometryPiece in both countParticles and createParticles.

--- Packages/Uintah/CCA/Components/MPM/ConstitutiveModel/ParticleCreator.cc
+++ Packages/Uintah/CCA/Components/MPM/ConstitutiveModel/ParticleCreator.cc
@@ -13,6 +13,7 @@
 #include <Packages/Uintah/CCA/Components/MPM/ConstitutiveModel/MPMMaterial.h>
 #include <Packages/Uintah/CCA/Components/MPM/ConstitutiveModel/ConstitutiveModel.h>
 #include <Packages/Uintah/Core/Grid/VarLabel.h>
+#include <Packages/Uintah/Core/Grid/FileGeometryPiece.h>
 
 using namespace Uintah;
 using std::vector;
@@ -54,59 +55,105 @@ ParticleCreator::createParticles(MPMMaterial* matl,
       count = 0;
       continue;
     }
-    
-    IntVector ppc = (*obj)->getNumParticlesPerCell();
-    Vector dxpp = patch->dCell()/(*obj)->getNumParticlesPerCell();
-    Vector dcorner = dxpp*0.5;
-    // Size as a fraction of the cell size
-    Vector size(1./((double) ppc.x()),
-		1./((double) ppc.y()),
-		1./((double) ppc.z()));
-    
-    for(CellIterator iter = patch->getCellIterator(); !iter.done(); iter++){
-      Point lower = patch->nodePosition(*iter) + dcorner;
-      for(int ix=0;ix < ppc.x(); ix++){
-	for(int iy=0;iy < ppc.y(); iy++){
-	  for(int iz=0;iz < ppc.z(); iz++){
-	    IntVector idx(ix, iy, iz);
-	    Point p = lower + dxpp*idx;
-	    IntVector cell_idx = iter.index();
-	    // If the assertion fails then we may just need to change
-	    // the format of particle ids such that the cell indices
-	    // have more bits.
-	    ASSERT(cell_idx.x() <= 0xffff && cell_idx.y() <= 0xffff
-		   && cell_idx.z() <= 0xffff);
+
+    // Special case exception for FileGeometryPieces
+    FileGeometryPiece* fgp = dynamic_cast<FileGeometryPiece*>(piece);
+    if (fgp) {
+      Vector dxpp = patch->dCell()/(*obj)->getNumParticlesPerCell();
+      IntVector ppc = (*obj)->getNumParticlesPerCell();
+      Vector size(1./((double) ppc.x()),
+		  1./((double) ppc.y()),
+		  1./((double) ppc.z()));
+      vector<Point>::const_iterator itr;
+      vector<Point>* points = fgp->getPoints();
+      vector<double>* volumes = fgp->getVolume();
+      int i = 0;
+      for (itr = points->begin(); itr != points->end(); ++itr) {
+	if (b2.contains(*itr)) {
+	  position[start+count] = (*itr);
+	  if (volumes->empty())
+	    pvolume[start+count]=dxpp.x()*dxpp.y()*dxpp.z();
+	  else
+	    pvolume[start+count] = (*volumes)[i];
+	  pvelocity[start+count]=(*obj)->getInitialVelocity();
+	  ptemperature[start+count]=(*obj)->getInitialTemperature();
+	  pmass[start+count]=matl->getInitialDensity()*pvolume[start+count];
+	  // Apply the force BC if applicable
+	  Vector pExtForce(0,0,0);
+	  applyForceBC(dxpp, *itr, pmass[start+count], pExtForce);
+	  pexternalforce[start+count] = pExtForce;
+	  
+	  // Determine if particle is on the surface
+	  IntVector cell_idx;
+	  if(patch->findCell(position[start+count],cell_idx)){
 	    long64 cellID = ((long64)cell_idx.x() << 16) |
 	      ((long64)cell_idx.y() << 32) |
 	      ((long64)cell_idx.z() << 48);
-	    if(piece->inside(p)){
-	      position[start+count]=p;
-	      pvolume[start+count]=dxpp.x()*dxpp.y()*dxpp.z();
-	      pvelocity[start+count]=(*obj)->getInitialVelocity();
-	      ptemperature[start+count]=(*obj)->getInitialTemperature();
+	    short int& myCellNAPID = cellNAPID[cell_idx];
+	    ASSERT(myCellNAPID < 0x7fff);
+	    myCellNAPID++;
+	    pparticleID[start+count] = cellID | (long64)myCellNAPID;
+	  }
+	  psize[start+count] = size;
+	  count++;
+	}
+	i++;
+      }
 
-              // Calculate particle mass
-              double partMass = matl->getInitialDensity()*pvolume[start+count];
-	      pmass[start+count] = partMass;
-
-              // Apply the force BC if applicable
-              Vector pExtForce(0,0,0);
-              applyForceBC(dxpp, p, partMass, pExtForce);
-	      pexternalforce[start+count] = pExtForce;
-
-	      // Determine if particle is on the surface
-	      short int& myCellNAPID = cellNAPID[cell_idx];
-	      pparticleID[start+count] = cellID | (long64)myCellNAPID;
-	      psize[start+count] = size;
-	      ASSERT(myCellNAPID < 0x7fff);
-	      myCellNAPID++;
-	      count++;
-
-	    }  // if inside
-	  }  // loop in z
-	}  // loop in y
-      }  // loop in x
-    } // for
+    } else {
+      IntVector ppc = (*obj)->getNumParticlesPerCell();
+      Vector dxpp = patch->dCell()/(*obj)->getNumParticlesPerCell();
+      Vector dcorner = dxpp*0.5;
+      // Size as a fraction of the cell size
+      Vector size(1./((double) ppc.x()),
+		  1./((double) ppc.y()),
+		  1./((double) ppc.z()));
+      
+      for(CellIterator iter = patch->getCellIterator(); !iter.done(); iter++){
+	Point lower = patch->nodePosition(*iter) + dcorner;
+	for(int ix=0;ix < ppc.x(); ix++){
+	  for(int iy=0;iy < ppc.y(); iy++){
+	    for(int iz=0;iz < ppc.z(); iz++){
+	      IntVector idx(ix, iy, iz);
+	      Point p = lower + dxpp*idx;
+	      IntVector cell_idx = iter.index();
+	      // If the assertion fails then we may just need to change
+	      // the format of particle ids such that the cell indices
+	      // have more bits.
+	      ASSERT(cell_idx.x() <= 0xffff && cell_idx.y() <= 0xffff
+		     && cell_idx.z() <= 0xffff);
+	      long64 cellID = ((long64)cell_idx.x() << 16) |
+		((long64)cell_idx.y() << 32) |
+		((long64)cell_idx.z() << 48);
+	      if(piece->inside(p)){
+		position[start+count]=p;
+		pvolume[start+count]=dxpp.x()*dxpp.y()*dxpp.z();
+		pvelocity[start+count]=(*obj)->getInitialVelocity();
+		ptemperature[start+count]=(*obj)->getInitialTemperature();
+		
+		// Calculate particle mass
+		double partMass = matl->getInitialDensity()*pvolume[start+count];
+		pmass[start+count] = partMass;
+		
+		// Apply the force BC if applicable
+		Vector pExtForce(0,0,0);
+		applyForceBC(dxpp, p, partMass, pExtForce);
+		pexternalforce[start+count] = pExtForce;
+		
+		// Determine if particle is on the surface
+		short int& myCellNAPID = cellNAPID[cell_idx];
+		pparticleID[start+count] = cellID | (long64)myCellNAPID;
+		psize[start+count] = size;
+		ASSERT(myCellNAPID < 0x7fff);
+		myCellNAPID++;
+		count++;
+		
+	      }  // if inside
+	    }  // loop in z
+	  }  // loop in y
+	}  // loop in x
+      } // for
+    } // end of else
     start += count;
   }
 
@@ -185,6 +232,20 @@ ParticleCreator::countParticles(GeometryObject* obj, const Patch* patch) const
    Box b = b1.intersect(b2);
    if(b.degenerate())
       return 0;
+
+   // Special case exception for FileGeometryPiece
+   FileGeometryPiece* fgp = dynamic_cast<FileGeometryPiece*>(piece);
+   if (fgp) {
+     particleIndex fgp_count = 0;
+     vector<Point>::const_iterator itr;
+     vector<Point>* points = fgp->getPoints();
+     for (itr = points->begin(); itr != points->end(); ++itr) {
+       if (b2.contains(*itr))
+	 fgp_count++;
+     }
+     return fgp_count;
+   }
+
 
    IntVector ppc = obj->getNumParticlesPerCell();
    Vector dxpp = patch->dCell()/obj->getNumParticlesPerCell();
