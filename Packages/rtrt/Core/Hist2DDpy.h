b05eed6ca8fe2a877bcae90adbc33bca499ac53a
James Bigler <bigler@sci.utah.edu>
2002-11-12 20:50:53 +0000

    Now inherits from DpyBase.  Added button* functions and removed move.  Added perp_line and new_perp_line which is the perpendicular line with respect to isoline at (px1,py1).  Added compute_perp function to compute the perpendicular line.  Added draw_isoline with makes the GL calls to draw an isoline given some boundaries.  Added some extra variables for keeping the center and intersection points around.

--- Packages/rtrt/Core/Hist2DDpy.h
+++ Packages/rtrt/Core/Hist2DDpy.h
@@ -2,8 +2,8 @@
 #ifndef RTRT_Hist2DDPY_H
 #define RTRT_Hist2DDPY_H 1
 
-#include <Core/Thread/Runnable.h>
 #include <X11/Xlib.h>
+#include <Packages/rtrt/Core/DpyBase.h>
 #include <Packages/rtrt/Core/Array1.h>
 
 namespace rtrt {
@@ -19,33 +19,54 @@ struct ImplicitLine {
   float operator()(float x, float y) const;
 };
 
-class Hist2DDpy : public Runnable {
+class Hist2DDpy : public DpyBase {
   Array1<VolumeVGBase*> vols;
   int** hist;
   int histmax;
-  int xres, yres;
   float vdatamin, vdatamax, gdatamin, gdatamax;
   void compute_hist(unsigned int fid);
   void draw_hist(unsigned int fid, XFontStruct* font_struct,
 		 bool redraw_hist);
-  enum BTN {
-    Press, Release, Motion
-  };
-  void move(int x, int y, BTN what);
+
+  // These handle mouse button events.  button indicates which button.
+  // x and y are the location measured from the upper left corner of the
+  // window.
+  virtual void button_pressed(MouseButton button, const int x, const int y);
+  virtual void button_released(MouseButton button, const int x, const int y);
+  virtual void button_motion(MouseButton button, const int x, const int y);
+  bool need_hist; // initialized to true
+  bool redraw_isoval; // initialized to false
+
   ImplicitLine new_isoline;
   ImplicitLine clipline;
   ImplicitLine new_clipline;
+  ImplicitLine perp_line, new_perp_line;
+
+  // Computes the line perpendicular to l1 at (x,y) and stores it in l2.
+  void compute_perp(ImplicitLine &l1, ImplicitLine &l2,
+		    const float x, const float y, const float ratio = 1);
+
+  // Using the bounds specified draws the line
+  void draw_isoline(ImplicitLine &line, float xmin, float ymin,
+		    float xmax, float ymax);
+  
   bool clip;
   bool new_clip;
+  bool use_perp, new_use_perp; // defaults to true
   
   bool have_line;
   float px0, py0, px1, py1;
+  float cx, cy; // the center of the screen
+  float ix, iy; // the intersection point along the diagonal
   void set_p();
   void set_line();
   int whichp;
   bool have_p;
   
   void set_clip();
+
+  // Called at the start of run.
+  virtual void init();
 public:
   ImplicitLine isoline;
   Hist2DDpy();
