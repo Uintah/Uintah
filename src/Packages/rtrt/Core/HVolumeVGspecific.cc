50fe8411d936534efd2ab2e0724eac7a255ba827
James Bigler <bigler@sci.utah.edu>
2002-11-17 04:20:52 +0000

    Added unsigned char.

--- src/Packages/rtrt/Core/HVolumeVGspecific.cc
+++ src/Packages/rtrt/Core/HVolumeVGspecific.cc
@@ -17,3 +17,17 @@ void VG<unsigned short, unsigned short>::setmax()
   v=USHRT_MAX;
   g=USHRT_MAX;
 }
+
+template<>
+void VG<unsigned char, unsigned char>::setmin()
+{
+  v=0;
+  g=0;
+}
+
+template<>
+void VG<unsigned char, unsigned char>::setmax()
+{
+  v=255;
+  g=255;
+}
