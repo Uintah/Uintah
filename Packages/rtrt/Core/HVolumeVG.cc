667419013fa6024545228b2bdccfaff2264d954b
James Bigler <bigler@sci.utah.edu>
2002-11-17 04:21:09 +0000

    Removed code that breaks rendering.

--- Packages/rtrt/Core/HVolumeVG.cc
+++ Packages/rtrt/Core/HVolumeVG.cc
@@ -701,7 +701,7 @@ void HVolumeVG<T,A,B>::intersect(Ray& ray, HitInfo& hit,
     y1=yinv_dir*(max.y()-orig.y());
     diy_dy=1;
     ddy=1;
-  } else if(dir.y() <-1.e-6){
+  } else {
     y0=yinv_dir*(max.y()-orig.y());
     y1=yinv_dir*(min.y()-orig.y());
     diy_dy=-1;
