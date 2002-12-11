9528b624a141f24aff9c17762c76a98a401650fb
John Schmidt <jas@sci.utah.edu>
2002-12-11 18:02:42 +0000

    Commented out the MPMDriver, MPMAlgorithm, Implicit and Explicit source files.

--- src/Packages/Uintah/CCA/Components/MPM/sub.mk
+++ src/Packages/Uintah/CCA/Components/MPM/sub.mk
@@ -9,11 +9,11 @@ SRCS     += $(SRCDIR)/SerialMPM.cc \
 	$(SRCDIR)/MPMLabel.cc \
 	$(SRCDIR)/Solver.cc 	\
 	$(SRCDIR)/PetscSolver.cc \
-	$(SRCDIR)/SimpleSolver.cc \
-	$(SRCDIR)/MPMAlgorithm.cc \
-	$(SRCDIR)/MPMDriver.cc \
-	$(SRCDIR)/Implicit.cc \
-	$(SRCDIR)/Explicit.cc 
+	$(SRCDIR)/SimpleSolver.cc 
+#	$(SRCDIR)/MPMAlgorithm.cc \
+#	$(SRCDIR)/MPMDriver.cc \
+#	$(SRCDIR)/Implicit.cc \
+#	$(SRCDIR)/Explicit.cc 
 
 SUBDIRS := $(SRCDIR)/ConstitutiveModel $(SRCDIR)/Contact \
 	$(SRCDIR)/ThermalContact \
