bdd958472a65b74793c5eb8e4d3b53dcb1efbbe4
David Weinstein <dmw@sci.utah.edu>
2002-12-20 23:45:21 +0000

    - Moved support for specifying a min/max data value from from the   ChangeFieldBounds module to the TransformScalarData module

--- src/Dataflow/GUI/TransformScalarData.tcl
+++ src/Dataflow/GUI/TransformScalarData.tcl
@@ -22,8 +22,18 @@ itcl_class SCIRun_Fields_TransformScalarData {
         set_defaults
     }
     method set_defaults {} {
+        global $this-method
         global $this-function
+	global $this-imin
+	global $this-imax
+	global $this-omin
+	global $this-omax
+	set $this-method "function"
 	set $this-function "x+10"
+	set $this-imin 0
+	set $this-imax 1
+	set $this-omin 0
+	set $this-omax 1
     }
     method make_entry {w text v c} {
         frame $w
@@ -45,9 +55,41 @@ itcl_class SCIRun_Fields_TransformScalarData {
         wm minsize $w 170 20
         frame $w.f
 
+	global $this-method
 	global $this-function
-	make_entry $w.f.f "Function:" $this-function "$this-c needexecute"	
-	pack $w.f.f -side top -expand 1 -fill x
+	global $this-imin
+	global $this-imax
+	global $this-omin
+	global $this-omax
+
+	set c "$this-c needexecute"
+
+	frame $w.f.i -relief groove -borderwidth 2
+	label $w.f.i.l1 -text "Original Min/Max         Min: "
+	label $w.f.i.l2 -textvariable $this-imin
+	label $w.f.i.l3 -text " Max: "
+	label $w.f.i.l4 -textvariable $this-imax
+	pack $w.f.i.l1 $w.f.i.l2 $w.f.i.l3 $w.f.i.l4 -side left -expand 1
+
+	frame $w.f.f
+	radiobutton $w.f.f.r -text "Function         " -anchor w -just left \
+	    -variable $this-method -value "function"
+	entry $w.f.f.e -textvariable $this-function
+	bind $w.f.f.e <Return> $c
+	pack $w.f.f.r $w.f.f.e -side left -fill x -expand 1
+
+	frame $w.f.m
+	radiobutton $w.f.m.r -text "New MinMax        " -anchor w -just left \
+	    -variable $this-method -value "minmax"	
+	label $w.f.m.l1 -text "Min: "
+	entry $w.f.m.e1 -textvariable $this-omin -width 6
+	bind $w.f.m.e1 <Return> $c
+	label $w.f.m.l2 -text "Max: "
+	entry $w.f.m.e2 -textvariable $this-omax -width 6
+	bind $w.f.m.e2 <Return> $c
+	pack $w.f.m.r $w.f.m.l1 $w.f.m.e1 $w.f.m.l2 $w.f.m.e2 -side left
+
+	pack $w.f.i $w.f.f $w.f.m -side top -expand 1 -fill x
 	pack $w.f -expand 1 -fill x
     }
 }
