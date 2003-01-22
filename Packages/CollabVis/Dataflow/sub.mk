3c63c2666567b9b583d84ec2e82946d24dad3c3b
Allen Sanderson <allen@sci.utah.edu>
2003-01-22 20:46:10 +0000

    added CollabVis Package

---
+++ Packages/CollabVis/Dataflow/sub.mk
@@ -0,0 +1,16 @@
+include $(SCIRUN_SCRIPTS)/largeso_prologue.mk
+
+SRCDIR := Packages/CollabVis/Dataflow
+
+SUBDIRS := \
+        $(SRCDIR)/GUI \
+        $(SRCDIR)/Modules \
+
+include $(SCIRUN_SCRIPTS)/recurse.mk
+
+PSELIBS := 
+LIBS := $(TK_LIBRARY) $(GL_LIBRARY) $(M_LIBRARY)
+
+include $(SCIRUN_SCRIPTS)/largeso_epilogue.mk
+
+
