23018740c4e75db20f0656a54d49924a2707c5ff
Darby Brown <darbyb@sci.utah.edu>
2003-02-12 17:17:40 +0000

    more progress

--- src/Packages/Insight/Dataflow/Modules/DataIO/ImageFileReader.cc
+++ src/Packages/Insight/Dataflow/Modules/DataIO/ImageFileReader.cc
@@ -3,60 +3,94 @@
  *
  *  Written by:
  *   darbyb
- *   TODAY'S DATE HERE
+ *   January 2003
  *
  */
 
-#include "ImageFileReader.h"
+#include <Dataflow/Network/Module.h>
+#include <Core/Malloc/Allocator.h>
+#include <Core/GuiInterface/GuiVar.h>
+#include <Packages/Insight/share/share.h>
 
-//itk
-#include "itkCastImageFilter.h"
+#include <Insight/Dataflow/Ports/ITKDatatypePort.h>
 #include "itkImageFileReader.h"
-#include "itkPNGImageIO.h"
+
 
 namespace Insight {
 
 using namespace SCIRun;
 
+class InsightSHARE ImageFileReader : public Module {
+public:
+
+  //! GUI variables
+  GuiString gui_FileName_;
+
+  itk::Object::Pointer reader_;
+
+  ITKDatatypeOPort* outport_;
+  ITKDatatypeHandle handle_;
+  
+  ImageFileReader(GuiContext*);
+
+  virtual ~ImageFileReader();
+
+  virtual void execute();
+
+  virtual void tcl_command(GuiArgs&, void*);
+};
+
 DECLARE_MAKER(ImageFileReader)
+
 ImageFileReader::ImageFileReader(GuiContext* ctx)
   : Module("ImageFileReader", ctx, Source, "DataIO", "Insight"),
-    gui_filename_(ctx->subVar("filename"))
+    gui_FileName_(ctx->subVar("FileName"))
 {
-  reader_ = ReaderType::New();
-  io_ = IOType::New();
 }
+
 
 ImageFileReader::~ImageFileReader(){
 }
 
-void
- ImageFileReader::execute(){
 
-  outport_ = (ITKImageOPort *)get_oport("Image");
+void ImageFileReader::execute(){
+  // check ports
+  outport_ = (ITKDatatypeOPort *)get_oport("Image");
   if(!outport_) {
     error("Unable to initialize oport 'Image'.");
     return;
   }
 
-  string fn = gui_filename_.get();
+  string fn = gui_FileName_.get();
 
-  reader_->SetFileName( fn.c_str() );
-  reader_->SetImageIO(io_);
 
-  if(!handle_.get_rep())
+  // Can't determine image type by casting??
+  if(1)
   {
-    ITKImage *image = scinew ITKImage;
-    image->to_float_->SetInput(reader_->GetOutput());
-    handle_ = image;
+    typedef itk::ImageFileReader<itk::Image<float, 2> > FileReaderType;
+    reader_ = FileReaderType::New();
+    dynamic_cast<FileReaderType*>(reader_.GetPointer())->SetFileName( fn.c_str() );
+    dynamic_cast<FileReaderType*>(reader_.GetPointer())->Update();  
+    
+    if(!handle_.get_rep())
+    {
+      ITKDatatype *im = scinew ITKDatatype;
+      im->data_ = dynamic_cast<FileReaderType*>(reader_.GetPointer())->GetOutput();  
+      handle_ = im; 
+    }
+    
+    // Send the data downstream
+    outport_->send(handle_);
   }
-
-  // Send the data downstream
-  outport_->send(handle_);
+  else {
+    // unknown type
+    error("Unknown type");
+    return;
+  }
 }
 
-void
- ImageFileReader::tcl_command(GuiArgs& args, void* userdata)
+
+void ImageFileReader::tcl_command(GuiArgs& args, void* userdata)
 {
   Module::tcl_command(args, userdata);
 }
