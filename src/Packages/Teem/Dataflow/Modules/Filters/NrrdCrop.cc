1ae1a1aa60bc03060f201e195a346b5456923abf
Marty Cole <mcole@sci.utah.edu>
2003-02-13 01:30:43 +0000

    update Crop to utilize the tuple axis.  add funtions for getting index offset information from the tuple axis label. Introduce a min_max_widget in tcl.

--- src/Packages/Teem/Dataflow/Modules/Filters/NrrdCrop.cc
+++ src/Packages/Teem/Dataflow/Modules/Filters/NrrdCrop.cc
@@ -32,6 +32,7 @@
 #include <Core/GuiInterface/GuiVar.h>
 #include <Teem/Dataflow/Ports/NrrdPort.h>
 
+#include <sstream>
 #include <iostream>
 using std::endl;
 #include <stdio.h>
@@ -41,18 +42,20 @@ namespace SCITeem {
 class NrrdCrop : public Module {
   NrrdIPort* inrrd_;
   NrrdOPort* onrrd_;
-  GuiString minAxis0_;
-  GuiString maxAxis0_;
-  GuiString minAxis1_;
-  GuiString maxAxis1_;
-  GuiString minAxis2_;
-  GuiString maxAxis2_;
-  string last_minAxis0_;
-  string last_maxAxis0_;
-  string last_minAxis1_;
-  string last_maxAxis1_;
-  string last_minAxis2_;
-  string last_maxAxis2_;
+  GuiInt minAxis0_;
+  GuiInt maxAxis0_;
+  GuiInt minAxis1_;
+  GuiInt maxAxis1_;
+  GuiInt minAxis2_;
+  GuiInt maxAxis2_;
+  GuiInt minAxis3_;
+  GuiInt maxAxis3_;
+  GuiInt absmaxAxis0_;
+  GuiInt absmaxAxis1_;
+  GuiInt absmaxAxis2_;
+  GuiInt absmaxAxis3_;
+  int lastmin_[4];
+  int lastmax_[4];
   int last_generation_;
   NrrdDataHandle last_nrrdH_;
 public:
@@ -67,72 +70,30 @@ public:
 using namespace SCITeem;
 DECLARE_MAKER(NrrdCrop)
 
-NrrdCrop::NrrdCrop(SCIRun::GuiContext *ctx)
-  : Module("NrrdCrop", ctx, Filter, "Filters", "Teem"), 
+NrrdCrop::NrrdCrop(SCIRun::GuiContext *ctx) : 
+  Module("NrrdCrop", ctx, Filter, "Filters", "Teem"), 
   minAxis0_(ctx->subVar("minAxis0")),
   maxAxis0_(ctx->subVar("maxAxis0")),
   minAxis1_(ctx->subVar("minAxis1")),
   maxAxis1_(ctx->subVar("maxAxis1")),
   minAxis2_(ctx->subVar("minAxis2")),
   maxAxis2_(ctx->subVar("maxAxis2")), 
-  last_minAxis0_(std::string("")),
-  last_maxAxis0_(std::string("")),
-  last_minAxis1_(std::string("")),
-  last_maxAxis1_(std::string("")),
-  last_minAxis2_(std::string("")), 
-  last_maxAxis2_(std::string("")),
-  last_generation_(-1), last_nrrdH_(0)
+  minAxis3_(ctx->subVar("minAxis3")),
+  maxAxis3_(ctx->subVar("maxAxis3")),
+  absmaxAxis0_(ctx->subVar("absmaxAxis0")),
+  absmaxAxis1_(ctx->subVar("absmaxAxis1")),
+  absmaxAxis2_(ctx->subVar("absmaxAxis2")),
+  absmaxAxis3_(ctx->subVar("absmaxAxis3")),
+  last_generation_(-1), 
+  last_nrrdH_(0)
 {
+  for (int i = 0; i < 4; i++) {
+    lastmin_[i] = -1;
+    lastmax_[i] = -1;
+  }
 }
 
 NrrdCrop::~NrrdCrop() {
-}
-
-// edited from the Teem package: src/unrrdu/crop.c
-// we initialize n with the axis size - 1
-int
-NrrdCrop::getint(const char *str, int *n) {
-  if (!strlen(str)) return 1;
-  if ('M' == str[0]) {
-    if (1 < strlen(str)) {
-      if (('+' == str[1] || '-' == str[1])) {
-	int offset;
-        if (1 != sscanf(str+1, "%d", &offset)) {
-          return 1;
-        }
-	if (str[1] == '+') *n += offset;
-	else *n -= offset;
-        /* else we succesfully parsed the offset */
-      }
-      else {
-        /* something other than '+' or '-' after 'M' */
-        return 1;
-      }
-    }
-  }
-  else {
-    if (1 != sscanf(str, "%d", n)) {
-      return 1;
-    }
-    /* else we successfully parsed n */
-  }
-  return 0;
-}
-
-// look for M[+/-a] strings, as well just numbers
-// set the actual indices in the min/max arrays
-int NrrdCrop::valid_data(string *minS, string *maxS, 
-			      int *min, int *max, Nrrd *nrrd) {
-  string errstr("Error in NrrdCrop - bad subvolume range.");
-  for (int a=0; a<3; a++) {
-    const char *mins = minS[a].c_str();
-    const char *maxs = maxS[a].c_str();
-    min[a]=max[a]=nrrd->axis[a].size-1;
-    if (getint(mins, &(min[a]))) { msgStream_ << errstr; return 0; }
-    if (getint(maxs, &(max[a]))) { msgStream_ << errstr; return 0; }
-    if (min[a] >= max[a]) { msgStream_ << errstr; return 0; }
-  }
-  return 1;
 }
 
 void 
@@ -157,50 +118,108 @@ NrrdCrop::execute()
     error("Empty input Nrrd.");
     return;
   }
+  absmaxAxis0_.reset();
+  absmaxAxis1_.reset();
+  absmaxAxis2_.reset();
+  absmaxAxis3_.reset();
+  minAxis0_.reset();
+  maxAxis0_.reset();
+  minAxis1_.reset();
+  maxAxis1_.reset();
+  minAxis2_.reset();
+  maxAxis2_.reset();
+  minAxis3_.reset();
+  maxAxis3_.reset();
   
-  string minS[3], maxS[3];
-  minS[0]=minAxis0_.get();
-  maxS[0]=maxAxis0_.get();
-  minS[1]=minAxis1_.get();
-  maxS[1]=maxAxis1_.get();
-  minS[2]=minAxis2_.get();
-  maxS[2]=maxAxis2_.get();
-  if (last_generation_ == nrrdH->generation &&
-      last_minAxis0_ == minS[0] &&
-      last_maxAxis0_ == maxS[0] &&
-      last_minAxis1_ == minS[1] &&
-      last_maxAxis1_ == maxS[1] &&
-      last_minAxis2_ == minS[2] &&
-      last_maxAxis2_ == maxS[2] &&
-      last_nrrdH_.get_rep()) {
-    onrrd_->send(last_nrrdH_);
-    return;
+  if (last_generation_ != nrrdH->generation) {
+    ostringstream str;
+    
+    absmaxAxis0_.set(nrrdH->get_tuple_axis_size() - 1);
+    maxAxis0_.set(nrrdH->get_tuple_axis_size() - 1);
+    absmaxAxis1_.set(nrrdH->nrrd->axis[1].size - 1);
+    maxAxis1_.set(nrrdH->nrrd->axis[1].size - 1);
+    if (nrrdH->nrrd->dim > 2) { 
+      absmaxAxis2_.set(nrrdH->nrrd->axis[2].size - 1); 
+      maxAxis2_.set(nrrdH->nrrd->axis[2].size - 1);
+    } else {
+      absmaxAxis2_.set(0);
+      maxAxis2_.set(0);
+    }
+    if (nrrdH->nrrd->dim > 3) { 
+      absmaxAxis3_.set(nrrdH->nrrd->axis[3].size - 1); 
+      maxAxis3_.set(nrrdH->nrrd->axis[3].size - 1);
+    } else {
+      absmaxAxis3_.set(0);
+      maxAxis3_.set(0);
+    }
+    
+    str << id.c_str() << " set_max_vals" << endl; 
+    gui->execute(str.str());
   }
 
-  int min[3], max[3];
-  if (!valid_data(minS, maxS, min, max, nrrdH->nrrd)) return;
 
-  last_minAxis0_ = minS[0];
-  last_maxAxis0_ = maxS[0];
-  last_minAxis1_ = minS[1];
-  last_maxAxis1_ = maxS[1];
-  last_minAxis2_ = minS[2];
-  last_maxAxis2_ = maxS[2];
+  int min[4], max[4];
+  min[0] = minAxis0_.get();
+  max[0] = maxAxis0_.get();
+  min[1] = minAxis1_.get();
+  max[1] = maxAxis1_.get();
+  min[2] = minAxis2_.get();
+  max[2] = maxAxis2_.get();
+  min[3] = minAxis3_.get();
+  max[3] = maxAxis3_.get();
+  
+  if (last_generation_ == nrrdH->generation && last_nrrdH_.get_rep()) {
+    bool same = true;
+    for (int i = 0; i < 4; i++) {
+      if (lastmin_[i] != min[i]) {
+	same = false;
+	lastmin_[i] = min[i];
+      }
+      if (lastmax_[i] != max[i]) {
+	same = false;
+	lastmax_[i] = max[i];
+      }
+    }
+    if (same) {
+      onrrd_->send(last_nrrdH_);
+      return;
+    }
+  }
   last_generation_ = nrrdH->generation;
 
   Nrrd *nin = nrrdH->nrrd;
   Nrrd *nout = nrrdNew();
-  msgStream_ << "Crop: ("<<min[0]<<","<<min[1]<<","<<min[2]<<") -> (";
-  msgStream_ << max[0]<<","<<max[1]<<","<<max[2]<<")"<<endl;
 
+  // translate the tuple index into the real offsets for a tuple axis.
+  int tmin, tmax;
+  if (! nrrdH->get_tuple_index_info(min[0], max[0], tmin, tmax)) {
+    error("Tuple index out of range");
+    return;
+  }
+  vector<string> elems;
+  nrrdH->get_tuple_indecies(elems);
+  string olabel;
+  for (int i = min[0]; i <= max[0]; i++) {
+    if (i == min[0]) { // first one;
+      olabel = elems[i];
+    } else {
+      olabel = olabel + "," + elems[i];
+    }
+  }
+
+  min[0] = tmin;
+  max[0] = tmax;
   if (nrrdCrop(nout, nin, min, max)) {
     char *err = biffGetDone(NRRD);
     error(string("Trouble resampling: ") + err);
     msgStream_ << "  input Nrrd: nin->dim="<<nin->dim<<"\n";
     free(err);
   }
+
   NrrdData *nrrd = scinew NrrdData;
   nrrd->nrrd = nout;
+  nout->axis[0].label = strdup(olabel.c_str());
+  nrrd->copy_sci_data(*nrrdH.get_rep());
   last_nrrdH_ = nrrd;
   onrrd_->send(last_nrrdH_);
 }
