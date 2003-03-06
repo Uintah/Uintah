e2f50c5f06cba09b638ea1cf722b2f8a4bc585fc
Keming Zhang <kzhang@sci.utah.edu>
2003-03-06 21:48:14 +0000

    update gov.cca to sci.cca in CCA components
    Modified Files:
     	sub.mk
    
    Committing in Components/BabelTest/hello
     	hello_Com_Impl.cc hello_Com_Impl.hh hello_GoPort_Impl.cc
     	hello_GoPort_Impl.hh
    
    Committing in Components/BabelTest/who
     	who_Com_Impl.cc who_Com_Impl.hh who_IDPort_Impl.cc
     	who_IDPort_Impl.hh who_UIPort_Impl.cc who_UIPort_Impl.hh
    
    Committing in Components/BabelTest/whoc
     	sub.mk whoc_Com_Impl.c whoc_Com_Impl.h whoc_IDPort_Impl.c
     	whoc_IDPort_Impl.h whoc_UIPort_Impl.c whoc_UIPort_Impl.h

--- src/CCA/Components/BabelTest/whoc/whoc_Com_Impl.c
+++ src/CCA/Components/BabelTest/whoc/whoc_Com_Impl.c
@@ -3,15 +3,15 @@
  * Symbol:        whoc.Com-v1.0
  * Symbol Type:   class
  * Babel Version: 0.7.4
- * SIDL Created:  20030227 09:45:28 MST
- * Generated:     20030227 09:45:31 MST
+ * SIDL Created:  20030306 10:46:01 MST
+ * Generated:     20030306 10:46:04 MST
  * Description:   Server-side implementation for whoc.Com
  * 
  * WARNING: Automatically generated; only changes within splicers preserved
  * 
  * babel-version = 0.7.4
  * source-line   = 13
- * source-url    = file:/home/kzhang/SCIRun/src/Babel/Components/whoc/whoc.sidl
+ * source-url    = file:/home/sci/kzhang/SCIRun/src/CCA/Components/BabelTest/whoc/whoc.sidl
  */
 
 /*
--- src/CCA/Components/BabelTest/whoc/whoc_IDPort_Impl.h
+++ src/CCA/Components/BabelTest/whoc/whoc_IDPort_Impl.h
@@ -3,15 +3,15 @@
  * Symbol:        whoc.IDPort-v1.0
  * Symbol Type:   class
  * Babel Version: 0.7.4
- * SIDL Created:  20030227 09:45:24 MST
- * Generated:     20030227 09:45:31 MST
+ * SIDL Created:  20030306 10:46:00 MST
+ * Generated:     20030306 10:46:04 MST
  * Description:   Server-side implementation for whoc.IDPort
  * 
  * WARNING: Automatically generated; only changes within splicers preserved
  * 
  * babel-version = 0.7.4
  * source-line   = 7
- * source-url    = file:/home/kzhang/SCIRun/src/Babel/Components/whoc/whoc.sidl
+ * source-url    = file:/home/sci/kzhang/SCIRun/src/CCA/Components/BabelTest/whoc/whoc.sidl
  */
 
 #ifndef included_whoc_IDPort_Impl_h
