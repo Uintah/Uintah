d2952c74dab0447f80bdf3cb73fb82075629f2e1
James Bigler <bigler@sci.utah.edu>
2003-03-13 01:41:28 +0000

    material now defaults to 0 instead of requiring it on the commandline.  The user must know what material they want to query, as this program doesn't attempt to determine acceptable materials.  The program will exit gracefully however.  Updated some of the commandline arguments.

--- StandAlone/timeextract.cc
+++ StandAlone/timeextract.cc
@@ -18,7 +18,6 @@
 #include <Packages/Uintah/Core/Grid/CellIterator.h>
 #include <Packages/Uintah/Core/Math/Matrix3.h>
 #include <Packages/Uintah/Core/Grid/ShareAssignParticleVariable.h>
-#include <Core/Thread/Mutex.h>
 #include <Core/Math/MinMax.h>
 #include <Core/Geometry/Point.h>
 #include <Packages/Uintah/Core/Grid/Box.h>
@@ -41,87 +40,26 @@ using namespace SCIRun;
 using namespace std;
 using namespace Uintah;
 
-Mutex cerrLock( "cerrLock" );
-
-typedef struct{
-  vector<ShareAssignParticleVariable<double> > pv_double_list;
-  vector<ShareAssignParticleVariable<Point> > pv_point_list;
-  vector<ShareAssignParticleVariable<Vector> > pv_vector_list;
-  vector<ShareAssignParticleVariable<Matrix3> > pv_matrix3_list;
-  ShareAssignParticleVariable<Point> p_x;
-} MaterialData;
-
-// takes a string and replaces all occurances of old with newch
-string replaceChar(string s, char old, char newch) {
-  string result;
-  for (int i = 0; i<(int)s.size(); i++)
-    if (s[i] == old)
-      result += newch;
-    else
-      result += s[i];
-  return result;
-}
-
-// use this function to open a pair of files for outputing
-// data to the reat-time raytracer.
-//
-// in: pointers to the pointer to the files data and header
-//     the file names
-// out: inialized files for writing
-//      boolean reporting the success of the file creation
-bool setupOutFiles(FILE** data, FILE** header, string name, string head) {
-  FILE* datafile;
-  FILE* headerfile;
-  string headername = name + string(".") + head;
-
-  datafile = fopen(name.c_str(),"w");
-  if (!datafile) {
-    cerr << "Can't open output file " << name << endl;
-    return false;
-  }
-  
-  headerfile = fopen(headername.c_str(),"w");
-  if (!headerfile) {
-    cerr << "Can't open output file " << headername << endl;
-    return false;
-  }
-  
-  *data = datafile;
-  *header = headerfile;
-  return true;
-}
-
-// given the various parts of the name we piece together the full name
-string makeFileName(string raydatadir, string variable_file, string time_file, 
-		    string patchID_file, string materialType_file) {
-
-  string raydatafile;
-  if (raydatadir != "")
-    raydatafile+= raydatadir + string("/");
-  raydatafile+= string("TS_") + time_file + string("/");
-  if (variable_file != "")
-    raydatafile+= string("VAR_") + variable_file + string(".");
-  if (materialType_file != "")
-    raydatafile+= string("MT_") + materialType_file + string(".");
-  raydatafile+= string("PI_") + patchID_file;
-  return raydatafile;
-}
+bool verbose = false;
+bool quite = false;
 
 void usage(const std::string& badarg, const std::string& progname)
 {
     if(badarg != "")
 	cerr << "Error parsing argument: " << badarg << endl;
-    cerr << "Usage: " << progname << " [options] -m <material number> "
+    cerr << "Usage: " << progname << " [options] "
 	 << "-uda <archive file>\n\n";
     cerr << "Valid options are:\n";
-    cerr << "  -h[elp]\n";
-    cerr << "  -v <variable name>\n";
-    cerr << "  -asci\n";
-    cerr << "  -verbose (prints status of output)\n";
-    cerr << "  -timesteplow [int] (only outputs timestep from int)\n";
-    cerr << "  -timestephigh [int] (only outputs timesteps upto int)\n";
-    cerr << "  -index <x> <y> <z> (cell coordinates)\n";
-    cerr << "  -o <outputfilename>\n";
+    cerr << "  -h,--help\n";
+    cerr << "  -v,--variable <variable name>\n";
+    cerr << "  -m,--material <material number> [defaults to 0]\n";
+    //    cerr << "  -binary (prints out the data in binary)\n";
+    cerr << "  -tlow,--timesteplow [int] (only outputs timestep from int) [defaults to 0]\n";
+    cerr << "  -thigh,--timestephigh [int] (only outputs timesteps upto int) [defaults to last timestep]\n";
+    cerr << "  -i,--index <x> <y> <z> (cell coordinates) [defaults to 0,0,0]\n";
+    cerr << "  -o,--out <outputfilename> [defaults to stdout]\n";
+    cerr << "  -vv,--verbose (prints status of output)\n";
+    cerr << "  -q,--quite (only print data values)\n";
     exit(1);
 }
 
@@ -141,7 +79,7 @@ void printData(DataArchive* archive, string& variable_name, int material, IntVec
   // query time info from dataarchive
   archive->queryTimesteps(index, times);
   ASSERTEQ(index.size(), times.size());
-  cout << "There are " << index.size() << " timesteps:\n";
+  if (!quite) cout << "There are " << index.size() << " timesteps:\n";
       
   //------------------------------
   // figure out the lower and upper bounds on the timesteps
@@ -151,7 +89,7 @@ void printData(DataArchive* archive, string& variable_name, int material, IntVec
   }
 
   // set default max time value
-  if (time_step_upper == -1)
+  if (time_step_upper == (unsigned int)-1)
     time_step_upper = times.size() - 1;
 
   if (time_step_upper >= times.size() || time_step_upper < time_step_lower) {
@@ -161,7 +99,7 @@ void printData(DataArchive* archive, string& variable_name, int material, IntVec
   }
   
   double time = times[time_step_lower];
-  cout << "time = " << time << endl;
+  if (!quite) cout << "time = " << time << endl;
   GridP grid = archive->queryGrid(time);
   
   // set defaults for output stream
@@ -188,11 +126,12 @@ int main(int argc, char** argv)
   /*
    * Default values
    */
-  bool do_asci=false;
-  bool do_verbose = false;
+  bool do_binary=false;
+
   unsigned long time_step_lower = 0;
-  unsigned long time_step_upper = -1;  // default to be last timestep, but can
-                                       // be set to 0
+  // default to be last timestep, but can be set to 0
+  unsigned long time_step_upper = (unsigned long)-1;
+
   string input_uda_name;
   string output_file_name("-");
   IntVector var_id(0,0,0);
@@ -200,38 +139,41 @@ int main(int argc, char** argv)
   // Now the material index is kind of a hard thing.  There is no way
   // to reliably determine a default material.  Materials are defined
   // on the patch for each varialbe, so this subset of materials could
-  // change over patches.  Because of this we will force the user to
-  // specify the material.
-  int material = -1;
+  // change over patches.  We can guess, that there will be a material
+  // 0.  This shouldn't cause the program to crash.  It will spit out
+  // an exception and exit gracefully.
+  int material = 0;
   
   /*
    * Parse arguments
    */
   for(int i=1;i<argc;i++){
     string s=argv[i];
-    if(s == "-asci"){
-      do_asci=true;
-    } else if(s == "-v" || s == "--variable") {
+    if(s == "-v" || s == "--variable") {
       variable_name = string(argv[++i]);
-    } else if (s == "-m" || s == "-material") {
+    } else if (s == "-m" || s == "--material") {
       material = atoi(argv[++i]);
-    } else if (s == "-verbose") {
-      do_verbose = true;
-    } else if (s == "-timesteplow") {
+    } else if (s == "-vv" || s == "--verbose") {
+      verbose = true;
+    } else if (s == "-q" || s == "--quite") {
+      quite = true;
+    } else if (s == "-tlow" || s == "--timesteplow") {
       time_step_lower = strtoul(argv[++i],(char**)NULL,10);
-    } else if (s == "-timestephigh") {
+    } else if (s == "-thigh" || s == "--timestephigh") {
       time_step_upper = strtoul(argv[++i],(char**)NULL,10);
-    } else if (s == "-index" || s == "-i") {
+    } else if (s == "-i" || s == "--index") {
       unsigned long x = strtoul(argv[++i],(char**)NULL,10);
       unsigned long y = strtoul(argv[++i],(char**)NULL,10);
       unsigned long z = strtoul(argv[++i],(char**)NULL,10);
       var_id = IntVector(x,y,z);
-    } else if( (s == "-help") || (s == "-h") ) {
+    } else if( (s == "-h") || (s == "--help") ) {
       usage( "", argv[0] );
     } else if (s == "-uda") {
       input_uda_name = string(argv[++i]);
     } else if (s == "-o" || s == "--out") {
       output_file_name = string(argv[++i]);
+    } else if(s == "-binary") {
+      do_binary=true;
     } else {
       usage(s, argv[0]);
     }
@@ -242,11 +184,6 @@ int main(int argc, char** argv)
     usage("", argv[0]);
   }
 
-  if (material == -1) {
-    cerr << "Must specify material number.\n";
-    usage("", argv[0]);
-  }
-  
   try {
     DataArchive* archive = scinew DataArchive(input_uda_name);
     
@@ -255,7 +192,7 @@ int main(int argc, char** argv)
 
     archive->queryVariables(vars, types);
     ASSERTEQ(vars.size(), types.size());
-    cout << "There are " << vars.size() << " variables:\n";
+    if (verbose) cout << "There are " << vars.size() << " variables:\n";
     bool var_found = false;
     unsigned int var_index = 0;
     for (;var_index < vars.size(); var_index++) {
@@ -278,7 +215,7 @@ int main(int argc, char** argv)
       //      var = vars[0];
     }
 
-    cout << vars[var_index] << ": " << types[var_index]->getName() << endl;
+    if (!quite) cout << vars[var_index] << ": " << types[var_index]->getName() << " being extracted for material "<<material<<" at index "<<var_id<<endl;
 
     // get type and subtype of data
     const Uintah::TypeDescription* td = types[var_index];
