58e3db6b498e263e044d020023a73235cc58bbc5
Marty Cole <mjc@sci.utah.edu>
2003-03-18 16:09:18 +0000

    adjust gui at runtime to support n dimensional nrrds.

--- src/Packages/Teem/Dataflow/GUI/NrrdCrop.tcl
+++ src/Packages/Teem/Dataflow/GUI/NrrdCrop.tcl
@@ -1,4 +1,3 @@
-#
 #  The contents of this file are subject to the University of Utah Public
 #  License (the "License"); you may not use this file except in compliance
 #  with the License.
@@ -11,19 +10,12 @@
 #  The Original Source Code is SCIRun, released March 12, 2001.
 #  
 #  The Original Source Code was developed by the University of Utah.
-#  Portions created by UNIVERSITY are Copyright (C) 2001, 1994 
+#  Portions created by UNIVERSITY are Copyright (C) 2001, 1994
 #  University of Utah. All Rights Reserved.
-#
-
-##
- #  NrrdCrop.tcl: The NrrdCrop UI
- #  Written by:
- #   David Weinstein
- #   Department of Computer Science
- #   University of Utah
- #   Jan 2000
- #  Copyright (C) 2000 SCI Group
- ##
+#  
+#    File   : NrrdCrop.tcl
+#    Author : Martin Cole
+#    Date   : Tue Mar 18 08:46:53 2003
 
 catch {rename Teem_Filters_NrrdCrop ""}
 
@@ -34,30 +26,11 @@ itcl_class Teem_Filters_NrrdCrop {
         set_defaults
     }
     method set_defaults {} {
-        global $this-minAxis0
-        global $this-maxAxis0
-        global $this-minAxis1
-        global $this-maxAxis1
-        global $this-minAxis2
-        global $this-maxAxis2
-	global $this-minAxis3
-        global $this-maxAxis3
-	global $this-absmaxAxis0
-	global $this-absmaxAxis1
-	global $this-absmaxAxis2
-	global $this-absmaxAxis3
-        set $this-minAxis0 0
-        set $this-maxAxis0 127
-	set $this-absmaxAxis0 127
-        set $this-minAxis1 0
-        set $this-maxAxis1 127
-	set $this-absmaxAxis1 127
-        set $this-minAxis2 0
-        set $this-maxAxis2 127
-	set $this-absmaxAxis2 127
-	set $this-minAxis3 0
-        set $this-maxAxis3 127
-	set $this-absmaxAxis3 127
+	global $this-num-axes
+	global $this-reset
+
+	set $this-num-axes 0
+	set $this-reset 0
     }
     method make_entry {w text v c} {
         frame $w
@@ -73,14 +46,72 @@ itcl_class Teem_Filters_NrrdCrop {
 	set w .ui[modname]
 
 	if {[winfo exists $w]} {
-	    set_scale_max_value $w.f.ta [set $this-absmaxAxis0]
-	    set $this-maxAxis0 [set $this-absmaxAxis0]
-	    set_scale_max_value $w.f.a1 [set $this-absmaxAxis1]
-	    set $this-maxAxis1 [set $this-absmaxAxis1]
-	    set_scale_max_value $w.f.a2 [set $this-absmaxAxis2]
-	    set $this-maxAxis2 [set $this-absmaxAxis2]
-	    set_scale_max_value $w.f.a3 [set $this-absmaxAxis3]
-	    set $this-maxAxis3 [set $this-absmaxAxis3]
+	    for {set i 0} {$i < [set $this-num-axes]} {incr i} {
+
+		set ax $this-absmaxAxis$i
+		set_scale_max_value $w.f.mmf.a$i [set $ax]
+		if {[set $this-maxAxis$i] == -1 || [set $this-reset]} {
+		    set $this-maxAxis$i [set $this-absmaxAxis$i]
+		}
+	    }
+	    set $this-reset 0
+	}
+    }
+
+    method init_axes {} {
+	for {set i 0} {$i < [set $this-num-axes]} {incr i} {
+	    #puts "init_axes----$i"
+
+	    if { [catch { set t [set $this-minAxis$i] } ] } {
+		set $this-minAxis$i 0
+		#puts "made minAxis$i"
+	    }
+	    if { [catch { set t [set $this-maxAxis$i]}] } {
+		set $this-maxAxis$i -1
+		#puts "made maxAxis$i   [set $this-maxAxis$i]"
+	    }
+	    if { [catch { set t [set $this-absmaxAxis$i]}] } {
+		set $this-absmaxAxis$i 0
+		#puts "made absmaxAxis$i"
+	    }
+	}
+	make_min_max
+    }
+
+    method make_min_max {} {
+	set w .ui[modname]
+        if {[winfo exists $w]} {
+  
+	    if {[winfo exists $w.f.mmf.t]} {
+		destroy $w.f.mmf.t
+	    }
+	    for {set i 0} {$i < [set $this-num-axes]} {incr i} {
+		#puts $i
+		if {! [winfo exists $w.f.mmf.a$i]} {
+		    min_max_widget $w.f.mmf.a$i "Axis $i" \
+			$this-minAxis$i $this-maxAxis$i $this-absmaxAxis$i 
+		}
+	    }
+	}
+    }
+
+   method clear_axes {} {
+	set w .ui[modname]
+        if {[winfo exists $w]} {
+  
+	    if {[winfo exists $w.f.mmf.t]} {
+		destroy $w.f.mmf.t
+	    }
+	    for {set i 0} {$i < [set $this-num-axes]} {incr i} {
+		#puts $i
+		if {[winfo exists $w.f.mmf.a$i]} {
+		    destroy $w.f.mmf.a$i
+		}
+		unset $this-minAxis$i
+		unset $this-maxAxis$i
+		unset $this-absmaxAxis$i
+	    }
+	    set $this-reset 1
 	}
     }
 
@@ -94,17 +125,17 @@ itcl_class Teem_Filters_NrrdCrop {
         toplevel $w
         wm minsize $w 150 80
         frame $w.f
+	pack $w.f -padx 2 -pady 2 -side top -expand yes
 	
-
-        pack $w.f -padx 2 -pady 2 -side top -expand yes
-	min_max_widget $w.f.ta "Tuple Axis" \
-	    $this-minAxis0 $this-maxAxis0 $this-absmaxAxis0
-	min_max_widget $w.f.a1 Axis1 \
-	    $this-minAxis1 $this-maxAxis1 $this-absmaxAxis1
-	min_max_widget $w.f.a2 Axis2 \
-	    $this-minAxis2 $this-maxAxis2 $this-absmaxAxis2
-	min_max_widget $w.f.a3 Axis3 \
-	    $this-minAxis3 $this-maxAxis3 $this-absmaxAxis3
+	frame $w.f.mmf
+	pack $w.f.mmf -padx 2 -pady 2 -side top -expand yes
+	
+	if {[set $this-num-axes] == 0} {
+	    label $w.f.mmf.t -text "Need Execute to know the number of Axes."
+	    pack $w.f.mmf.t
+	} else {
+	    init_axes 
+	}
 
 	button $w.f.b -text "Execute" -command "$this-c needexecute"
 	pack $w.f.b -side top -expand 1 -fill x
