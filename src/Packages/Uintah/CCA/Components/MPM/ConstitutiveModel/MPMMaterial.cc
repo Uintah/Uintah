358a6c66f0bdbc1d00be400c1f4f19cd2f1ae6c6
John Schmidt <jas@sci.utah.edu>
2003-03-19 18:43:49 +0000

    Changed an if to else if for the parsing of the fracture tag when creating particles.

--- src/Packages/Uintah/CCA/Components/MPM/ConstitutiveModel/MPMMaterial.cc
+++ src/Packages/Uintah/CCA/Components/MPM/ConstitutiveModel/MPMMaterial.cc
@@ -74,10 +74,12 @@ MPMMaterial::MPMMaterial(ProblemSpecP& ps, MPMLabel* lb, int n8or27,
      d_particle_creator = scinew ImplicitParticleCreator(this,lb,n8or27,
 		                                         haveLoadCurve,
 							 doErosion);
-   if (integrator == "fracture") 
+
+   else if (integrator == "fracture") 
      d_particle_creator = scinew FractureParticleCreator(this,lb,n8or27,
 		                                         haveLoadCurve,
 							 doErosion);
+
    else if (dynamic_cast<Membrane*>(d_cm) != 0)
      d_particle_creator = scinew MembraneParticleCreator(this,lb,n8or27,
 		                                         haveLoadCurve,
