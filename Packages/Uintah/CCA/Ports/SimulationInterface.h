6386bd974f1f7e6a279964f380fe743c1dd668f9
Jim Guilkey <guilkey@sci.utah.edu>
2003-03-31 22:51:23 +0000

    Comment out unused variables in needRecompile

--- Packages/Uintah/CCA/Ports/SimulationInterface.h
+++ Packages/Uintah/CCA/Ports/SimulationInterface.h
@@ -79,8 +79,8 @@ WARNING
 
      //////////
      // ask the component if it needs to be recompiled
-     virtual bool need_recompile(double time, double dt,
-				   const GridP& grid) {return false;}
+     virtual bool need_recompile(double /*time*/, double /*dt*/,
+				   const GridP& /*grid*/) {return false;}
    private:
      SimulationInterface(const SimulationInterface&);
      SimulationInterface& operator=(const SimulationInterface&);
