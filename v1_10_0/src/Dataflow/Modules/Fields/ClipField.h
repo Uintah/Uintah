d43098a3587c803a88b35869f336f14e23879fc5
Michael Callahan <callahan@sci.utah.edu>
2003-04-10 23:51:29 +0000

    Copy mesh properties

--- v1_10_0/src/Dataflow/Modules/Fields/ClipField.h
+++ v1_10_0/src/Dataflow/Modules/Fields/ClipField.h
@@ -67,6 +67,7 @@ ClipFieldAlgoT<FIELD>::execute_cell(ModuleReporter *mod,
   typename FIELD::mesh_type *mesh =
     dynamic_cast<typename FIELD::mesh_type *>(fieldh->mesh().get_rep());
   typename FIELD::mesh_type *clipped = scinew typename FIELD::mesh_type();
+  *(PropertyManager *)clipped = *(PropertyManager *)mesh;
 
 #ifdef HAVE_HASH_MAP
   typedef hash_map<unsigned int,
@@ -162,6 +163,7 @@ ClipFieldAlgoT<FIELD>::execute_node(ModuleReporter *mod,
   typename FIELD::mesh_type *mesh =
     dynamic_cast<typename FIELD::mesh_type *>(fieldh->mesh().get_rep());
   typename FIELD::mesh_type *clipped = scinew typename FIELD::mesh_type();
+  *(PropertyManager *)clipped = *(PropertyManager *)mesh;
 
 #ifdef HAVE_HASH_MAP
   typedef hash_map<unsigned int,
