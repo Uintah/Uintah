69bb43049e4bd7941afaf16892c957fea4d6d013
Darby Brown <darbyb@sci.utah.edu>
2003-05-19 17:08:50 +0000

    classes for generating code

---
+++ src/Packages/Insight/Core/CodeGenerator/Parser.h
@@ -0,0 +1,51 @@
+/**************************************
+ *
+ * Parser.h
+ *
+ * Written by:
+ *   Darby J Van Uitert
+ *   SCI Institute
+ *   January 2003
+ *************************************/
+
+#include <xercesc/util/PlatformUtils.hpp>
+#include <xercesc/parsers/XercesDOMParser.hpp>
+
+#include <xercesc/dom/DOMNode.hpp>
+#include <xercesc/dom/DOMText.hpp>
+#include <xercesc/dom/DOMNamedNodeMap.hpp>
+#include <xercesc/dom/DOMNodeList.hpp>
+#include <xercesc/dom/DOMElement.hpp>
+#include <xercesc/dom/DOMNodeList.hpp>
+#include <xercesc/dom/DOMException.hpp> 
+
+#include <iostream>
+#include <fstream.h>
+
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::string;
+using std::ostream;
+
+class Parser {
+
+public:
+  Parser();
+  ~Parser();
+
+  // parse the file for includes and imports
+  DOMNode* read_input_file(string filename);
+  
+  // resolve <include> and <import> tags
+  void resolve_includes(DOMNode* );
+  void resolve_imports(DOMNode* );
+
+  // output tree
+  void write_node(ostream&, DOMNode*);
+  void output_content(ostream&,  string chars );
+
+
+private:
+
+};
