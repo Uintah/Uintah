13df3adebfedfcc524598bd144424d23d7d8d0cd
James Bigler <bigler@sci.utah.edu>
2003-05-28 15:10:20 +0000

    The clipping planes should now be textured with the color map that comes in on the module.  Added the ability to toggle the active state and material use of the cutting planes.

--- src/Packages/rtrt/Dataflow/Modules/Scenes/GeoProbeScene.cc
+++ src/Packages/rtrt/Dataflow/Modules/Scenes/GeoProbeScene.cc
@@ -18,6 +18,8 @@
 #include <Packages/rtrt/Core/Scene.h>
 #include <Packages/rtrt/Core/Group.h>
 #include <Packages/rtrt/Core/Rect.h>
+#include <Packages/rtrt/Core/PhongColorMapMaterial.h>
+#include <Packages/rtrt/Core/ScalarTransform1D.h>
 #include <Packages/rtrt/Core/LambertianMaterial.h>
 #include <Packages/rtrt/Core/CutMaterial.h>
 #include <Packages/rtrt/Core/ColorMap.h>
@@ -35,6 +37,10 @@
 #include <Core/GuiInterface/GuiVar.h>
 #include <Packages/rtrt/Dataflow/Ports/ScenePort.h>
 #include <Dataflow/Ports/ColorMapPort.h>
+
+// General SCIRun stuff
+#include <Core/Geom/ColorMap.h>
+#include <Core/Datatypes/Color.h>
 
 // general libs
 #include <iostream>
@@ -75,6 +81,16 @@ private:
   // Some state variables
   GuiInt xa_act_;
   GuiInt xa_mat_;
+  GuiInt xb_act_;
+  GuiInt xb_mat_;
+  GuiInt ya_act_;
+  GuiInt ya_mat_;
+  GuiInt yb_act_;
+  GuiInt yb_mat_;
+  GuiInt za_act_;
+  GuiInt za_mat_;
+  GuiInt zb_act_;
+  GuiInt zb_mat_;
   
   // Colors for the isosurface
   GuiDouble gui_color_r_;
@@ -95,6 +111,8 @@ private:
   void update_cutting_plane(string which, float val);
   void update_active_cut(string which);
   void update_usemat_cut(string which);
+
+  Material *gen_color_map(ColorMapHandle, Object *obj, float min, float max);
 };
 
 DECLARE_MAKER(GeoProbeScene)
@@ -105,7 +123,7 @@ GeoProbeScene::GeoProbeScene(GuiContext* ctx)
     first_execute_(1),
     cmap_generation_(-1),
     execute_string_(""),
-    xacp(0), xbcp(0), yacp(0), ybcp(0), zacp(0), zbcp(0), 
+    xacp(0), xbcp(0), yacp(0), ybcp(0), zacp(0), zbcp(0),
     xa_(ctx->subVar("xa")),
     xb_(ctx->subVar("xb")),
     ya_(ctx->subVar("ya")),
@@ -117,6 +135,16 @@ GeoProbeScene::GeoProbeScene(GuiContext* ctx)
     gui_color_b_(ctx->subVar("color-b")),
     xa_act_(ctx->subVar("xa-active")),
     xa_mat_(ctx->subVar("xa-usemat")),
+    xb_act_(ctx->subVar("xb-active")),
+    xb_mat_(ctx->subVar("xb-usemat")),
+    ya_act_(ctx->subVar("ya-active")),
+    ya_mat_(ctx->subVar("ya-usemat")),
+    yb_act_(ctx->subVar("yb-active")),
+    yb_mat_(ctx->subVar("yb-usemat")),
+    za_act_(ctx->subVar("za-active")),
+    za_mat_(ctx->subVar("za-usemat")),
+    zb_act_(ctx->subVar("zb-active")),
+    zb_mat_(ctx->subVar("zb-usemat")),
     //    surfmat(0),
     hvol(0),
     gpfilename_(ctx->subVar("gpfilename")),
@@ -138,9 +166,9 @@ Scene* GeoProbeScene::make_scene(Object *obj)
 	     Vector(-0.55671, -0.0136153, 0.830595),
 	     47.239);
   double ambient_scale=1.0;
-  Color bgcolor(0.1, 0.2, 0.45);
-  Color cdown(0.82, 0.62, 0.62);
-  Color cup(0.1, 0.3, 0.8);
+  rtrt::Color bgcolor(0.1, 0.2, 0.45);
+  rtrt::Color cdown(0.82, 0.62, 0.62);
+  rtrt::Color cup(0.1, 0.3, 0.8);
   Plane groundplane ( Point(0, 0, 0), Vector(0, 0, 1) );
   Scene* scene=new Scene(obj, cam, bgcolor, cdown, cup, groundplane,
 			 ambient_scale, Arc_Ambient);
@@ -149,13 +177,13 @@ Scene* GeoProbeScene::make_scene(Object *obj)
   scene->addObjectOfInterest(obj, true);
   
   // add a named light
-  Light *l = new Light(Point(5,-3,3), Color(1,1,.8)*2, 0);
+  Light *l = new Light(Point(10,-3,3), rtrt::Color(1,1,.8)*1, 0);
   l->name_="Spot";
   scene->add_light(l);
 
   // set the background
-  scene->set_background_ptr( new LinearBackground(Color(0.2, 0.4, 0.9),
-						  Color(0.0,0.0,0.0),
+  scene->set_background_ptr( new LinearBackground(rtrt::Color(0.2, 0.4, 0.9),
+						  rtrt::Color(0.0,0.0,0.0),
 						  Vector(0,0,1)) );
   // set the shadow mode
   scene->select_shadow_mode( Hard_Shadows );
@@ -210,8 +238,8 @@ void GeoProbeScene::execute()
     yb = max.y()*yb+min.y()*(1-yb);
     za = max.z()*za+min.z()*(1-za);
     zb = max.z()*zb+min.z()*(1-zb);
-    Group *g = new Group;
-    Material *surfmat = new LambertianMaterial(Color(gui_color_r_.get(),
+    Group *all_cuts = new Group;
+    Material *surfmat = new LambertianMaterial(rtrt::Color(gui_color_r_.get(),
 						     gui_color_g_.get(),
 						     gui_color_b_.get()));
     //    ColorMap *cmap =new ColorMap("/opt/SCIRun/data/Geometry/volumes/vol_cmap");
@@ -227,21 +255,46 @@ void GeoProbeScene::execute()
        min, max, datamin, datamax, data);
     // Add the cutting planes
     Object *obj = hvol;
-    obj = xacp = new CutPlane(obj, Vector(1/(max.x()-min.x()),0,0), xa_.get());
-    xacp->set_matl(new LambertianMaterial(Color(0.1,0.1,0.9)));
-    //    obj = xbcp = new CutPlane(obj, Vector(1/(min.x()-max.x()),0,0), 1-xa_.get());
-    //    xbcp->set_matl(new LambertianMaterial(Color(0.9,0.9,0.1)));
-    g->add(obj);
+    // Here is the material
+    Material *cut_mat = gen_color_map(cmH, hvol, datamin, datamax);
+    // The X clipping planes
+    obj = xacp =new CutPlane(obj, Vector(1/(max.x()-min.x()),0,0), xa_.get());
+    //    xacp->set_matl(new LambertianMaterial(rtrt::Color(0.1,0.1,0.9)));
+    xacp->set_matl(cut_mat);
+    
+    obj = xbcp =new CutPlane(obj, Vector(1/(min.x()-max.x()),0,0), -xb_.get());
+    //xbcp->set_matl(new LambertianMaterial(rtrt::Color(0.9,0.9,0.1)));
+    xbcp->set_matl(cut_mat);
+
+    // The Y clipping planes
+    obj = yacp =new CutPlane(obj, Vector(0,1/(max.y()-min.y()),0), ya_.get());
+    //    yacp->set_matl(new LambertianMaterial(rtrt::Color(0.1,0.5,0.2)));
+    yacp->set_matl(cut_mat);
+    
+    obj = ybcp =new CutPlane(obj, Vector(0,1/(min.y()-max.y()),0), -yb_.get());
+    //ybcp->set_matl(new LambertianMaterial(rtrt::Color(0.9,0.2,0.5)));
+    ybcp->set_matl(cut_mat);
+
+    // The Z clipping planes
+    obj = zacp =new CutPlane(obj, Vector(0,0,1/(max.z()-min.z())), za_.get());
+    //    zacp->set_matl(new LambertianMaterial(rtrt::Color(0.9,0.1,0.9)));
+    zacp->set_matl(cut_mat);
+    
+    obj = zbcp =new CutPlane(obj, Vector(0,0,1/(min.z()-max.z())), -zb_.get());
+    //zbcp->set_matl(new LambertianMaterial(rtrt::Color(0.2,0.9,0.9)));
+    zbcp->set_matl(cut_mat);
+
+    all_cuts->add(obj);
 #if 0
     BBox temp;
     hvol->compute_bounds(temp, 0);
     cout <<"hvol.compte_bounds.min = "<<temp.min()<<", max = "<<temp.max()<<"\n";
     //    cout <<"hvol.min = "<<hvol->min<<", datadiag = "<<hvol->datadiag<<"\n";
     temp.reset();
-    g->compute_bounds(temp, 0);
+    all_cuts->compute_bounds(temp, 0);
     cout <<"group.min = "<<temp.min()<<", max = "<<temp.max()<<"\n";
 #endif
-    Scene *scene = make_scene(g);
+    Scene *scene = make_scene(all_cuts);
     SceneContainer *container = scinew SceneContainer();
     container->put_scene(scene);
     sceneHandle_ = container;
@@ -257,8 +310,23 @@ void GeoProbeScene::execute()
 
 
   if (cmH->generation != cmap_generation_) {
+#if 1
     // need to update the colormap
-    // ...
+    Material *cut_mat = gen_color_map(cmH, hvol,
+				      iso_min_.get(), iso_max_.get());
+    // We know that they all share the same material
+    Material *old_mat = xacp->get_matl();
+    // Now reset the colormap for all the objects
+    xacp->set_matl(cut_mat);
+    xbcp->set_matl(cut_mat);
+    yacp->set_matl(cut_mat);
+    ybcp->set_matl(cut_mat);
+    zacp->set_matl(cut_mat);
+    zbcp->set_matl(cut_mat);
+    // Now delete old_mat.  We should get a handle on all the inside
+    // stuff to be sure to delete that stuff too.
+    delete old_mat;
+#endif
     cmap_generation_ = cmH->generation;
     execute_string_ = "";
   }
@@ -310,7 +378,7 @@ void GeoProbeScene::update_isosurface_color() {
   // Get a pointer to the old material, so that we can delete it.
   Material *oldmat = hvol->get_matl();
   // Create the new material
-  Material *newmat = new LambertianMaterial(Color(gui_color_r_.get(),
+  Material *newmat = new LambertianMaterial(rtrt::Color(gui_color_r_.get(),
 						  gui_color_g_.get(),
 						  gui_color_b_.get()));
   // Now set the material for the object
@@ -341,23 +409,25 @@ void GeoProbeScene::update_cutting_plane(string which, float val) {
   } else if (which == "xb") {
     //    cout << "xb";
     plane = xbcp;
-    val = 1 - val;
+    val = -val;
   } else if (which == "ya") {
     //    cout << "ya";
     plane = yacp;
   } else if (which == "yb") {
     //    cout << "yb";
     plane = ybcp;
+    val = -val;
   } else if (which == "za") {
     //    cout << "za";
     plane = zacp;
   } else if (which == "zb") {
     //    cout << "zb";
     plane = zbcp;
+    val = -val;
   }
   //  cout <<" with value "<<val<<"\n";
   if (plane) {
-    if (val == 0)
+    if (val == 0 || val == -1)
       val += 1e-3;
     if (val == 1)
       val -= 1e-3;
@@ -369,18 +439,34 @@ void GeoProbeScene::update_active_cut(string which) {
   if (first_execute_)
     // haven't initialized anything yet
     return;
+
+  CutPlane *cp = 0;
+  bool state = true;
   
   reset_vars();
   if (which == "xa") {
-    //    cout << "xa";
-    xacp->update_active_state(xa_act_.get() == 1);
+    cp = xacp;
+    state = xa_act_.get() == 1;
   } else if (which == "xb") {
+    cp = xbcp;
+    state = xb_act_.get() == 1;
   } else if (which == "ya") {
+    cp = yacp;
+    state = ya_act_.get() == 1;
   } else if (which == "yb") {
+    cp = ybcp;
+    state = yb_act_.get() == 1;
   } else if (which == "za") {
+    cp = zacp;
+    state = za_act_.get() == 1;
   } else if (which == "zb") {
+    cp = zbcp;
+    state = zb_act_.get() == 1;
   }
- 
+
+  if (cp) {
+    cp->update_active_state(state);
+  }
 }
 
 void GeoProbeScene::update_usemat_cut(string which) {
@@ -388,16 +474,64 @@ void GeoProbeScene::update_usemat_cut(string which) {
     // haven't initialized anything yet
     return;
   
+  CutPlane *cp = 0;
+  bool state = true;
+  
   reset_vars();
   if (which == "xa") {
-    xacp->update_usemat_state(xa_mat_.get() == 1);
+    cp = xacp;
+    state = xa_mat_.get() == 1;
   } else if (which == "xb") {
+    cp = xbcp;
+    state = xb_mat_.get() == 1;
   } else if (which == "ya") {
+    cp = yacp;
+    state = ya_mat_.get() == 1;
   } else if (which == "yb") {
+    cp = ybcp;
+    state = yb_mat_.get() == 1;
   } else if (which == "za") {
+    cp = zacp;
+    state = za_mat_.get() == 1;
   } else if (which == "zb") {
+    cp = zbcp;
+    state = zb_mat_.get() == 1;
+  }
+
+  if (cp) {
+    cp->update_usemat_state(state);
   }
  
+}
+
+// 
+Material *GeoProbeScene::gen_color_map(ColorMapHandle cmap, Object *obj,
+				       float min, float max) {
+  // Need to generate the ScalarTransform1D for color and alpha
+  int size = 256;
+  Array1<rtrt::Color> *colors = new Array1<rtrt::Color>(256);
+  Array1<float> *alphas = new Array1<float>(256);
+  for (int i = 0; i < size; i++) {
+    double index = (double)i/(size-1);
+    SCIRun::Color sc = cmap->FindColor(index);
+    rtrt::Color rc(sc.r(), sc.g(), sc.b());
+    (*colors)[i] = rc;
+    (*alphas)[i] = cmap->FindAlpha(index);
+#if 1
+    cout << "colors["<<i<<"] = "<<(*colors)[i];
+    cout << ", alpha = "<<(*alphas)[i]<<endl;
+#endif
+  }
+  
+  ScalarTransform1D<float, rtrt::Color> *color_trans;
+  color_trans = new ScalarTransform1D<float, rtrt::Color>(colors);
+  color_trans->scale(min, max);
+  
+  ScalarTransform1D<float, float> *alpha_trans;
+  alpha_trans = new ScalarTransform1D<float, float>(alphas);
+  alpha_trans->scale(min, max);
+
+  return new PhongColorMapMaterial(obj, color_trans, alpha_trans);
 }
 
 } // End namespace rtrt
