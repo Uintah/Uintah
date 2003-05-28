38a592ebeb931686a557df3ed9e5752ac776bd33
Jim Guilkey <guilkey@sci.utah.edu>
2003-05-28 01:08:41 +0000

    Added a new CM to work with the implicit code.  This is a linear elastic model that should make debugging easier (now that most of the debugging is done).

---
+++ Packages/Uintah/CCA/Components/MPM/ConstitutiveModel/HypoElasticImplicit.h
@@ -0,0 +1,120 @@
+//  HypoElasticImplicit.h 
+//  class ConstitutiveModel ConstitutiveModel data type -- 3D - 
+//  holds ConstitutiveModel
+//  information for the FLIP technique:
+//    This is for Compressible NeoHookean materials
+//    Features:
+//      Usage:
+
+
+
+#ifndef __HYPOELASTIC_IMPLICIT_CONSTITUTIVE_MODEL_H__
+#define __HYPOELASTIC_IMPLICIT_CONSTITUTIVE_MODEL_H__
+
+
+#include <math.h>
+#include "ConstitutiveModel.h"	
+#include <Packages/Uintah/Core/Math/Matrix3.h>
+#include <vector>
+#include <Packages/Uintah/Core/Disclosure/TypeDescription.h>
+#include <Packages/Uintah/CCA/Components/MPM/Solver.h>
+
+namespace Uintah {
+      class HypoElasticImplicit : public ConstitutiveModel {
+      private:
+         // Create datatype for storing model parameters
+	  bool d_useModifiedEOS; 
+      public:
+         struct CMData {
+            double G;
+            double K;
+         };
+      private:
+         CMData d_initialData;
+
+         // Prevent copying of this class
+         // copy constructor
+         HypoElasticImplicit(const HypoElasticImplicit &cm);
+         HypoElasticImplicit& operator=(const HypoElasticImplicit &cm);
+         int d_8or27;
+
+      public:
+         // constructors
+         HypoElasticImplicit(ProblemSpecP& ps,  MPMLabel* lb, int n8or27);
+       
+         // destructor
+         virtual ~HypoElasticImplicit();
+         // compute stable timestep for this patch
+         virtual void computeStableTimestep(const Patch* patch,
+                                            const MPMMaterial* matl,
+                                            DataWarehouse* new_dw);
+
+         virtual void computeStressTensor(const PatchSubset* patches,
+					  const MPMMaterial* matl,
+					  DataWarehouse* old_dw,
+					  DataWarehouse* new_dw,
+					  Solver* solver,
+					  const bool recursion);
+
+         virtual void computeStressTensor(const PatchSubset* patches,
+					  const MPMMaterial* matl,
+					  DataWarehouse* old_dw,
+					  DataWarehouse* new_dw);
+
+         // initialize  each particle's constitutive model data
+         virtual void initializeCMData(const Patch* patch,
+                                       const MPMMaterial* matl,
+                                       DataWarehouse* new_dw);
+
+         virtual void addInitialComputesAndRequires(Task* task,
+                                             const MPMMaterial* matl,
+                                             const PatchSet* patches) const;
+
+         virtual void addComputesAndRequires(Task* task,
+                                             const MPMMaterial* matl,
+                                             const PatchSet* patches,
+					     const bool recursion) const;
+
+         virtual void addComputesAndRequires(Task* task,
+                                             const MPMMaterial* matl,
+					     const PatchSet* patches) const;
+
+
+         virtual double computeRhoMicroCM(double pressure,
+                                          const double p_ref,
+                                          const MPMMaterial* matl);
+
+         virtual void computePressEOSCM(double rho_m, double& press_eos,
+                                        double p_ref,
+                                        double& dp_drho, double& ss_new,
+                                        const MPMMaterial* matl);
+
+         virtual double getCompressibility();
+
+         // class function to read correct number of parameters
+         // from the input file
+         static void readParameters(ProblemSpecP ps, double *p_array);
+
+         // class function to write correct number of parameters
+         // from the input file, and create a new object
+         static ConstitutiveModel* readParametersAndCreate(ProblemSpecP ps);
+
+         // member function to read correct number of parameters
+         // from the input file, and any other particle information
+         // need to restart the model for this particle
+         // and create a new object
+         static ConstitutiveModel* readRestartParametersAndCreate(
+                                                        ProblemSpecP ps);
+
+	 virtual void addParticleState(std::vector<const VarLabel*>& from,
+				       std::vector<const VarLabel*>& to);
+         // class function to create a new object from parameters
+         static ConstitutiveModel* create(double *p_array);
+
+      };
+} // End namespace Uintah
+      
+
+
+#endif  // __HYPOELASTIC_IMPLICIT_CONSTITUTIVE_MODEL_H__ 
+
