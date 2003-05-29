7b5e9f92c37ae65406968fbe8f1fde0e020bd1d9
Keming Zhang <kzhang@sci.utah.edu>
2003-05-29 05:41:57 +0000

    add a thread class for handlers and socket accept() Added Files: 	SocketThread.cc SocketThread.h Removed Files: 	SocketThreads.h

---
+++ Core/CCA/Component/Comm/SocketThread.h
@@ -0,0 +1,58 @@
+/*
+  The contents of this file are subject to the University of Utah Public
+  License (the "License"); you may not use this file except in compliance
+  with the License.
+  
+  Software distributed under the License is distributed on an "AS IS"
+  basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+  License for the specific language governing rights and limitations under
+  the License.
+  
+  The Original Source Code is SCIRun, released March 12, 2001.
+  
+  The Original Source Code was developed by the University of Utah.
+  Portions created by UNIVERSITY are Copyright (C) 2001, 1994 
+  University of Utah. All Rights Reserved.
+*/
+
+/*
+ *  QtUtils.cc:
+ *
+ *  Written by:
+ *   Keming Zhang
+ *   Department of Computer Science
+ *   University of Utah
+ *   May 2003
+ *
+ */
+
+
+#ifndef SOCKET_THREAD_H
+#define SOCKET_THREAD_H 
+
+#include <Core/Thread/Runnable.h>
+
+namespace SCIRun{
+  class SocketEpChannel;
+  class SocketSpChannel;
+
+  class SocketThread : public Runnable {
+  public:
+    SocketThread(SocketEpChannel *ep, int id);
+    SocketThread(SocketSpChannel *sp, int id);
+    //id>=0, handlers
+    //id==-1, accept
+    //id==-2, ...
+    
+    ~SocketThread() {}
+    void run();
+  private:
+    SocketEpChannel *ep;
+    SocketSpChannel *sp;
+    bool isEp;
+    int id;
+  };
+} // namespace SCIRun
+  
+#endif  
+
