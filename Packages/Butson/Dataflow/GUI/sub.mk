1ffdf4e713182daf905149594c4cba257ac97ba5
Michael Callahan <callahan@sci.utah.edu>
2003-05-30 22:06:14 +0000

    Add an AppendSparse module

--- Packages/Butson/Dataflow/GUI/sub.mk
+++ Packages/Butson/Dataflow/GUI/sub.mk
@@ -5,5 +5,9 @@ SRCDIR := Packages/Butson/Dataflow/GUI
 ALLTARGETS := $(ALLTARGETS) $(SRCDIR)/tclIndex
 
 $(SRCDIR)/tclIndex: \
+	$(SRCDIR)/AppendSparse.tcl \
+#[INSERT NEW TCL FILE HERE]
+
+	$(OBJTOP)/createTclIndex $(SRCTOP)/Packages/Butson/Dataflow/GUI
 
 CLEANPROGS := $(CLEANPROGS) $(SRCDIR)/tclIndex
