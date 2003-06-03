9dc5703e1e593ab1bab0093304ffd127add60401
Biswajit Banerjee <banerjee@sci.utah.edu>
2003-06-03 20:17:00 +0000

    Reflect change in location of GeometryPiece from Grid to Grid/GeomPiece

--- Packages/Uintah/CCA/Components/Examples/RegionDB.cc
+++ Packages/Uintah/CCA/Components/Examples/RegionDB.cc
@@ -1,10 +1,10 @@
 
 #include <Packages/Uintah/CCA/Components/Examples/RegionDB.h>
 #include <Packages/Uintah/Core/Exceptions/ProblemSetupException.h>
-#include <Packages/Uintah/Core/Grid/GeometryPiece.h>
-#include <Packages/Uintah/Core/Grid/GeometryPieceFactory.h>
-#include <Packages/Uintah/Core/Grid/BoxGeometryPiece.h>
-#include <Packages/Uintah/Core/Grid/DifferenceGeometryPiece.h>
+#include <Packages/Uintah/Core/Grid/GeomPiece/GeometryPiece.h>
+#include <Packages/Uintah/Core/Grid/GeomPiece/GeometryPieceFactory.h>
+#include <Packages/Uintah/Core/Grid/GeomPiece/BoxGeometryPiece.h>
+#include <Packages/Uintah/Core/Grid/GeomPiece/DifferenceGeometryPiece.h>
 #include <Packages/Uintah/Core/Grid/Grid.h>
 #include <Packages/Uintah/Core/Grid/Level.h>
 #include <iostream>
