2dcb484f104d0db99ff0db89e4b226e93d407a8f
Biswajit Banerjee <banerjee@sci.utah.edu>
2003-06-03 20:10:23 +0000

    1) Changed the location from Grid to Grid/GeomPiece 2) Added :      ShellGeometryFactory.cc ShellGeometryFactory.h      ShellGeometryPiece.cc ShellGeometryPiece.h      PlaneShellPiece.cc PlaneShellPiece.h      SphereShellPiece.cc SphereShellPiece.h      CylinderShellPiece.cc CylinderShellPiece.h

---
+++ Packages/Uintah/Core/Grid/GeomPiece/ShellGeometryPiece.cc
@@ -0,0 +1,14 @@
+#include <Packages/Uintah/Core/Grid/GeomPiece/ShellGeometryPiece.h>
+#include <Packages/Uintah/Core/ProblemSpec/ProblemSpec.h>
+
+using namespace Uintah;
+using namespace SCIRun;
+
+
+ShellGeometryPiece::ShellGeometryPiece()
+{
+}
+
+ShellGeometryPiece::~ShellGeometryPiece()
+{
+}
