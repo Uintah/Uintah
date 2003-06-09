b2dab1c80a8c103223690223a73f95c0ad813524
Marty Cole <mjc@sci.utah.edu>
2003-06-09 20:23:44 +0000

    remove ReactionDiffusion from build

--- Packages/Fusion/Dataflow/GUI/sub.mk
+++ Packages/Fusion/Dataflow/GUI/sub.mk
@@ -22,7 +22,7 @@ $(SRCDIR)/tclIndex: \
 	$(SRCDIR)/MDSPlusFieldReader.tcl\
 	$(SRCDIR)/MDSPlusDataReader.tcl\
 	$(SRCDIR)/Plot2DViewer.tcl\
-	$(SRCDIR)/ReactionDiffusion.tcl\
+	#$(SRCDIR)/ReactionDiffusion.tcl\
 #[INSERT NEW TCL FILE HERE]
 	$(OBJTOP)/createTclIndex $(SRCTOP)/Packages/Fusion/Dataflow/GUI
 
