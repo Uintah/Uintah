f85446485ea1492f8f93bf994918f79757cf551a
J. Davison de St. Germain <dav@sci.utah.edu>
2003-06-18 20:38:48 +0000

    File does not use mpi so removed include of mpi.h

--- Core/CCA/Component/Comm/Intra/IntraCommMPI.h
+++ Core/CCA/Component/Comm/Intra/IntraCommMPI.h
@@ -20,8 +20,6 @@
 #define INTRACOMM_MPI_H 
 
 #include <Core/CCA/Component/Comm/Intra/IntraComm.h>
-#include <sci_config.h> // For MPIPP_H on SGI
-#include <mpi.h>
 
 namespace SCIRun {
   /**************************************
