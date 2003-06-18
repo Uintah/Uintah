954d2b4101fb9b4de226e5126ce8aaa3350ba5dc
Jenny Simpson <simpson@sci.utah.edu>
2003-06-18 19:58:01 +0000

    Adding CollabVis files/dirs

--- src/Packages/CollabVis/Core/sub.mk
+++ src/Packages/CollabVis/Core/sub.mk
@@ -5,6 +5,31 @@ SRCDIR := Packages/CollabVis/Core
 SUBDIRS := \
         $(SRCDIR)/Datatypes \
 
+# CollabVis code begin
+ifeq ($(HAVE_COLLAB_VIS),yyyyes)
+  # SRCS += $(SRCDIR)/ViewServer.cc
+
+  SUBDIRS += $(SRCDIR)/Client
+
+  INCLUDES += -I$(SRCTOP)/$(SRCDIR)/Client
+
+  REMOTELIBS := -L$(SRCTOP)/$(SRCDIR)/Client/lib -L$(SRCTOP)/$(SRCDIR)/Client/Network/RMF/rmf2.0/RAMP/usr/local/lib/ -L$(SRCTOP)/$(SRCDIR)/Client/Network/RMF/rmf2.0/RMF/usr/local/lib/\
+	-lXML \
+	-lCompression\
+	-lExceptions \
+	-lLogging \
+	-lMessage \
+	-lNetwork \
+	-lProperties \
+	-lRendering \
+	-lThread \
+        -lRAMP \
+        -lRMF
+
+  LIBS += $(XML_LIBRARY) $(REMOTELIBS) -lm
+endif
+# CollabVis code end
+
 include $(SCIRUN_SCRIPTS)/recurse.mk
 
 PSELIBS := 
