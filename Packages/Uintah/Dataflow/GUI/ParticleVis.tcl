9efb34a130bb9b87cb8999f94b6a89ea85e300c2
James Bigler <bigler@sci.utah.edu>
2003-06-18 19:51:55 +0000

    Moved the Particle and Vector display controls to separate tabs in order to make the GUI not so blumin' huge.

--- Packages/Uintah/Dataflow/GUI/ParticleVis.tcl
+++ Packages/Uintah/Dataflow/GUI/ParticleVis.tcl
@@ -64,6 +64,95 @@ itcl_class Uintah_Visualization_ParticleVis {
 
     }
 
+    method add_partgeom_tab { dof } {
+	puts "in add_partgeom_tab"
+	set n "$this-c needexecute"
+
+ 	set f1 [$dof.tabs add -label "Geometry"]
+
+ 	make_labeled_radio $f1.geom "Particle Display type" $n \
+ 	     top $this-drawspheres { {Points 0} {"Spheres (picking mode)" 1} }
+ 	pack $f1.geom -side top -fill x
+
+ 	frame $f1.rad -relief flat
+ 	pack $f1.rad -side top -fill x
+ 	checkbutton $f1.rad.auto_radius -text "auto radius" \
+ 	    -variable $this-auto_radius -command $n
+ 	set r [expscale $f1.rad.radius -label "Radius:" -orient horizontal \
+ 		    -variable $this-radius -command $n ]
+ 	pack $f1.rad.radius $f1.rad.auto_radius -side bottom -fill x
+
+ 	scale $f1.res -label "Polygons:" -orient horizontal \
+ 	    -variable $this-polygons -command $n \
+ 	    -from 8 -to 400 -tickinterval 392
+
+ 	pack $f1.res -side top -expand yes -fill x
+
+ 	set old_show_nth $this-show_nth
+ 	scale $f1.nth -label "Show Nth Particle:" -orient horizontal \
+ 	    -variable $this-show_nth \
+ 	    -command "$n; $this showNth"  -from 1 -to 100 \
+ 	    -tickinterval 99 -resolution 1
+	
+ 	pack $f1.nth -side top -expand yes -fill x
+    }
+
+    method add_vectcontrol_tab { dof } {
+	puts "in add_vectcontrol_tab"
+	set n "$this-c needexecute"
+
+	set f2 [$dof.tabs add -label "Vectors"]
+
+	frame $f2.f -relief flat
+	pack $f2.f -side top -expand yes -fill x
+	checkbutton $f2.f.chk -text "Show Vectors" -variable $this-drawVectors \
+	    -command $n -offvalue 0 -onvalue 1
+	pack $f2.f.chk -side left -anchor w -expand yes -fill x
+
+	make_labeled_radio $f2.f.shaft "Shaft style:" $n \
+		top $this-drawcylinders { {Lines 0} {Cylinders 1} }
+	pack $f2.f.shaft -side left -padx 5 -anchor w
+
+	frame $f2.ls -relief flat
+	pack $f2.ls -side top -fill x
+	checkbutton $f2.ls.auto_length_scale -text "auto length scale" \
+	    -variable $this-auto_length_scale -command $n
+	expscale $f2.ls.length_scale -label "Length scale:" \
+	    -orient horizontal  -variable $this-length_scale \
+	    -command $n 
+	pack $f2.ls.length_scale $f2.ls.auto_length_scale -side bottom -fill x
+	expscale $f2.min_length -orient horizontal \
+		-label "Minimum Length:" \
+		-variable $this-min_crop_length -command $n
+
+	pack $f2.min_length -side top -fill x
+
+	expscale $f2.max_length -orient horizontal \
+		-label "Maximum Length (not used if equal to 0):" \
+		-variable $this-max_crop_length -command $n
+
+	pack $f2.max_length -side top -fill x
+
+
+	scale $f2.head_length -orient horizontal -label "Head length:" \
+		-from 0 -to 1 -length 3c \
+                -showvalue true \
+                -resolution 0.001 \
+		-variable $this-head_length -command $n 
+	pack $f2.head_length -side left -fill x -pady 2
+	scale $f2.width_scale -orient horizontal -label "Base width:" \
+		-from 0 -to 1 -length 3c \
+                -showvalue true \
+                -resolution 0.001 \
+		-variable $this-width_scale -command $n 
+	pack $f2.width_scale -side right -fill x -pady 2
+	scale $f2.shaft_scale -orient horizontal -label "Shaft Radius" \
+		-from 0 -to 1 -length 3c \
+		-showvalue true -resolution 0.001 \
+		-variable $this-shaft_rad -command $n
+	pack $f2.shaft_scale -side left -fill x -pady 2
+    }
+
     method ui {} {
 	set w .ui[modname]
 
@@ -85,34 +174,32 @@ itcl_class Uintah_Visualization_ParticleVis {
 
 	toplevel $w
 	wm minsize $w 300 20
-	frame $w.f1 -relief groove -borderwidth 2
-	pack $w.f1 -side top -expand yes -fill both
 
-	make_labeled_radio $w.f1.geom "Particle Geometry" $n \
-	     top $this-drawspheres { {Points 0} {"Spheres (picking mode)" 1} }
-	pack $w.f1.geom -side top -fill x
+	frame $w.options
+	pack $w.options -side top -expand yes -fill x
 
-	frame $w.f1.rad -relief flat
-	pack $w.f1.rad -side top -fill x
-	checkbutton $w.f1.rad.auto_radius -text "auto radius" \
-	    -variable $this-auto_radius -command $n
-	set r [expscale $w.f1.rad.radius -label "Radius:" -orient horizontal \
-		    -variable $this-radius -command $n ]
-	pack $w.f1.rad.radius $w.f1.rad.auto_radius -side bottom -fill x
+ 	frame $w.options.disp -borderwidth 2
+ 	pack $w.options.disp -padx 2 -pady 2 -side left \
+ 		-fill both -expand 1
 
-	scale $w.f1.res -label "Polygons:" -orient horizontal \
-	    -variable $this-polygons -command $n \
-	    -from 8 -to 400 -tickinterval 392
+ 	iwidgets::labeledframe $w.options.disp.frame_title \
+ 		-labelpos nw -labeltext "Display Options"
+ 	set dof [$w.options.disp.frame_title childsite]
 
-	pack $w.f1.res -side top -expand yes -fill x
+ 	iwidgets::tabnotebook  $dof.tabs -height 350 -width 330 \
+ 	    -raiseselect true 
 
-	set old_show_nth $this-show_nth
-	scale $w.f1.nth -label "Show Nth Particle:" -orient horizontal \
-	    -variable $this-show_nth \
-	    -command "$n; $this showNth"  -from 1 -to 100 \
-	    -tickinterval 99 -resolution 1
+ 	pack $dof.tabs -side top -fill x -expand yes
+
+	add_partgeom_tab $dof
+# #	add_partscale_tab $dof
+	add_vectcontrol_tab $dof
+
+	$dof.tabs view 0	
+
+ 	#pack notebook frame
+ 	pack $w.options.disp.frame_title -side top -expand yes -fill x
 	
-	pack $w.f1.nth -side top -expand yes -fill x
 
 	frame $w.f3 -relief groove -borderwidth 2
 	pack $w.f3 -side top -expand yes -fill both
@@ -129,60 +216,6 @@ itcl_class Uintah_Visualization_ParticleVis {
 	entry $sf.e2 -textvariable $this-max_
 	pack $sf.cb $sf.l1 $sf.e1 $sf.l2 $sf.e2 -side left \
 	    -expand yes -fill x -padx 2 -pady 2
-
-	frame $w.f2 -relief groove -borderwidth 2
-	pack $w.f2 -side top -expand yes -fill both
-	label $w.f2.l -text "Vector Controls\n"
-	pack $w.f2.l -side top -expand yes -fill x
-
-	frame $w.f2.f -relief flat
-	pack $w.f2.f -side top -expand yes -fill x
-	checkbutton $w.f2.f.chk -text "Show Vectors" -variable $this-drawVectors \
-	    -command $n -offvalue 0 -onvalue 1
-	pack $w.f2.f.chk -side left -anchor w -expand yes -fill x
-
-	make_labeled_radio $w.f2.f.shaft "Shaft style:" $n \
-		top $this-drawcylinders { {Lines 0} {Cylinders 1} }
-	pack $w.f2.f.shaft -side left -padx 5 -anchor w
-
-	frame $w.f2.ls -relief flat
-	pack $w.f2.ls -side top -fill x
-	checkbutton $w.f2.ls.auto_length_scale -text "auto length scale" \
-	    -variable $this-auto_length_scale -command $n
-	expscale $w.f2.ls.length_scale -label "Length scale:" \
-	    -orient horizontal  -variable $this-length_scale \
-	    -command $n 
-	pack $w.f2.ls.length_scale $w.f2.ls.auto_length_scale -side bottom -fill x
-	expscale $w.f2.min_length -orient horizontal \
-		-label "Minimum Length:" \
-		-variable $this-min_crop_length -command $n
-
-	pack $w.f2.min_length -side top -fill x
-
-	expscale $w.f2.max_length -orient horizontal \
-		-label "Maximum Length (not used if equal to 0):" \
-		-variable $this-max_crop_length -command $n
-
-	pack $w.f2.max_length -side top -fill x
-
-
-	scale $w.f2.head_length -orient horizontal -label "Head length:" \
-		-from 0 -to 1 -length 3c \
-                -showvalue true \
-                -resolution 0.001 \
-		-variable $this-head_length -command $n 
-	pack $w.f2.head_length -side left -fill x -pady 2
-	scale $w.f2.width_scale -orient horizontal -label "Base width:" \
-		-from 0 -to 1 -length 3c \
-                -showvalue true \
-                -resolution 0.001 \
-		-variable $this-width_scale -command $n 
-	pack $w.f2.width_scale -side right -fill x -pady 2
-	scale $w.f2.shaft_scale -orient horizontal -label "Shaft Radius" \
-		-from 0 -to 1 -length 3c \
-		-showvalue true -resolution 0.001 \
-		-variable $this-shaft_rad -command $n
-	pack $w.f2.shaft_scale -side left -fill x -pady 2
 
 
 	button $w.close -text "Close" -command "wm withdraw $w"
