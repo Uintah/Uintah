9cdf9173d37b071e5f21271f724c10cf3baf0385
Randy N Jones <jones@sci.utah.edu>
2003-07-14 19:18:21 +0000

    Changed the order of one #include statement for the picky Frost compiler

--- src/Packages/Uintah/Core/Grid/SpecializedRunLengthEncoder.h
+++ src/Packages/Uintah/Core/Grid/SpecializedRunLengthEncoder.h
@@ -3,8 +3,8 @@
 
 // RunLengthEncoder with speciailizations for basic types, Vector's,
 // and Point's. 
-#include <Core/Geometry/SpecializedRunLengthEncoder.h>
 #include <Packages/Uintah/Core/Math/Matrix3.h>
+#include <Core/Geometry/SpecializedRunLengthEncoder.h>
 
 // Specializations of the DefaultRunLengthSequencer, to override the
 // EqualElementSequencer version with the EqualIntervalSequencer
