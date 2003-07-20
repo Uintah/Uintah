947676f5edd9d7fd6662c35a51020a127c0eb4d8
Keming Zhang <kzhang@sci.utah.edu>
2003-07-20 22:00:06 +0000

    remove duplicated PHello

--- CCA/Components/sub.mk
+++ CCA/Components/sub.mk
@@ -25,7 +25,7 @@ ifeq ($(HAVE_QT),yes)
 SUBDIRS := $(SRCDIR)/Builder $(SRCDIR)/TxtBuilder $(SRCDIR)/Hello  $(SRCDIR)/ListPlotter \
 	$(SRCDIR)/ZList $(SRCDIR)/Viewer $(SRCDIR)/LinSolver \
 	$(SRCDIR)/FileReader $(SRCDIR)/FEM $(SRCDIR)/Tri $(SRCDIR)/TableTennis \
-	$(SRCDIR)/TTClient $(SRCDIR)/World $(SRCDIR)/PHello 
+	$(SRCDIR)/TTClient $(SRCDIR)/World 
 
 else
 SUBDIRS :=$(SRCDIR)/TxtBuilder $(SRCDIR)/Hello
