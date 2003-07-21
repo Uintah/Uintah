45a5380949cd51121501101f00f6a5a85a084171
Keming Zhang <kzhang@sci.utah.edu>
2003-07-21 22:07:24 +0000

    change Core/CCA/Component/... Core/CCA/...

--- testprograms/Component/exceptiontest/pingthrow.cc
+++ testprograms/Component/exceptiontest/pingthrow.cc
@@ -29,9 +29,9 @@
  */
 
 #include <iostream>
-#include <Core/CCA/Component/PIDL/PIDL.h>
+#include <Core/CCA/PIDL/PIDL.h>
 
-#include <Core/CCA/Component/PIDL/MalformedURL.h>
+#include <Core/CCA/PIDL/MalformedURL.h>
 
 #include <testprograms/Component/exceptiontest/PingThrow_impl.h>
 #include <testprograms/Component/exceptiontest/PingThrow_sidl.h>
