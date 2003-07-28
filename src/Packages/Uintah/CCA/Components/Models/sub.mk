f17fa6ab62e3bb16b8b5e1afa9b45f83bff9d4b3
Steven G. Parker <sparker@sci.utah.edu>
2003-07-28 19:17:11 +0000

    Preliminary infrastructure for CFD models Initial test model

---
+++ src/Packages/Uintah/CCA/Components/Models/sub.mk
@@ -0,0 +1,15 @@
+# Makefile fragment for this subdirectory
+include $(SCIRUN_SCRIPTS)/smallso_prologue.mk
+
+SRCDIR   := Packages/Uintah/CCA/Components/Models
+
+SRCS	+= \
+       $(SRCDIR)/ModelFactory.cc
+
+SUBDIRS := $(SRCDIR)/test
+include $(SCIRUN_SCRIPTS)/recurse.mk
+
+PSELIBS := 
+LIBS	:= 
+
+include $(SCIRUN_SCRIPTS)/smallso_epilogue.mk
