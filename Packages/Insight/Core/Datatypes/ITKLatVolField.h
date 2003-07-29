db862439de3efece56a51f6ccf8a07ae45ae8141
Darby Brown <darbyb@sci.utah.edu>
2003-07-29 16:15:14 +0000

    Defined more functions and put ASSERTFAIL instead of ASSERTS.  Have functions also check if their image has been set.

--- Packages/Insight/Core/Datatypes/ITKLatVolField.h
+++ Packages/Insight/Core/Datatypes/ITKLatVolField.h
@@ -16,6 +16,11 @@
 */
 
 
+/*************************************************************
+  This field is similar to the SCIRun::LatVolField except for
+  it points to an itk::Image< Data, 3 >. 
+*************************************************************/
+
 #ifndef Datatypes_ITKLatVolField_h
 #define Datatypes_ITKLatVolField_h
 
@@ -84,11 +89,31 @@ public:
   const_iterator *const_begin_;
   const_iterator *const_end_;
 
-  const iterator &begin() { return *begin_; }
-  const iterator &end() { return *end_; }
+  const iterator &begin() { 
+    if(image_set_)
+      return *begin_; 
+    else
+      ASSERTFAIL("ITKFData3d image not set");
+  }
+  const iterator &end() { 
+    if(image_set_)
+      return *end_; 
+    else
+      ASSERTFAIL("ITKFData3d image not set");
+  }
 
-  const const_iterator &begin() const { return *const_begin_; }
-  const const_iterator &end() const { return *const_end_; }
+  const const_iterator &begin() const { 
+    if(image_set_)
+      return *const_begin_; 
+    else
+      ASSERTFAIL("ITKFData3d image not set");
+  }
+  const const_iterator &end() const { 
+    if(image_set_)
+      return *const_end_; 
+    else
+      ASSERTFAIL("ITKFData3d image not set");
+  }
 
 
   ITKFData3d();
@@ -98,108 +123,134 @@ public:
   
   const value_type &operator[](typename LatVolMesh::Cell::index_type idx) const
   { 
-    typename image_type::IndexType pixel;
-    pixel[0] = idx.k_;
-    pixel[1] = idx.j_;
-    pixel[2] = idx.i_;
-    return image_->GetPixel( pixel ); 
+    if(image_set_) {
+      typename image_type::IndexType pixel;
+      pixel[0] = idx.i_;
+      pixel[1] = idx.j_;
+      pixel[2] = idx.k_;
+      return image_->GetPixel( pixel ); 
+    }
+    else {
+      ASSERTFAIL("ITKFData3D image not set");
+    }
   } 
   const value_type &operator[](typename LatVolMesh::Face::index_type idx) const
   { 
-    ASSERT(0);
-    typename image_type::IndexType pixel;
-    pixel[0] = 0;
-    pixel[1] = 0;
-    pixel[2] = 0;
-    return image_->GetPixel( pixel );  
+    ASSERTFAIL("const operator[] not defined for ITKLatVolField for Faces");
+    if(image_set_) {
+      typename image_type::IndexType pixel;
+      pixel[0] = 0;
+      pixel[1] = 0;
+      pixel[2] = 0;
+      return image_->GetPixel( pixel );  
+    }
+    else {
+      ASSERTFAIL("ITKFData3d image not set");
+    }
   }
   const value_type &operator[](typename LatVolMesh::Edge::index_type idx) const
   { 
-    ASSERT(0);
-    typename image_type::IndexType pixel;
-    pixel[0] = 0;
-    pixel[1] = 0;
-    pixel[2] = 0;
-    return image_->GetPixel( pixel );  
+    ASSERTFAIL("const operator[] not defined for ITKLatVolField for Edges");
+    if(image_set_) {
+      typename image_type::IndexType pixel;
+      pixel[0] = 0;
+      pixel[1] = 0;
+      pixel[2] = 0;
+      return image_->GetPixel( pixel );  
+    }
+    else {
+      ASSERTFAIL("ITKFData3d image not set");
+    }
   }
   const value_type &operator[](typename LatVolMesh::Node::index_type idx) const
   { 
-    typename image_type::IndexType pixel;
-    pixel[0] = idx.i_;
-    pixel[1] = idx.j_;
-    pixel[2] = idx.k_;
-    return image_->GetPixel( pixel );  
+    if(image_set_) {
+      typename image_type::IndexType pixel;
+      pixel[0] = idx.i_;
+      pixel[1] = idx.j_;
+      pixel[2] = idx.k_;
+      return image_->GetPixel( pixel ); 
+    }
+    else {
+      ASSERTFAIL("ITKFData3D image not set");
+    } 
   }
 
   value_type &operator[](typename LatVolMesh::Cell::index_type idx)
   { 
-    typename image_type::IndexType pixel;
-    pixel[0] = idx.k_;
-    pixel[1] = idx.j_;
-    pixel[2] = idx.i_;
-    return image_->GetPixel( pixel ); 
+    if(image_set_) {
+      typename image_type::IndexType pixel;
+      pixel[0] = idx.i_;
+      pixel[1] = idx.j_;
+      pixel[2] = idx.k_;
+      return image_->GetPixel( pixel ); 
+    }
+    else {
+      ASSERTFAIL("ITKFData3d image not set");
+    }
   }
   value_type &operator[](typename LatVolMesh::Face::index_type idx)
   {
-    ASSERT(0);
-    typename image_type::IndexType pixel;
-    pixel[0] = 0;
-    pixel[1] = 0;
-    pixel[2] = 0;
-    return image_->GetPixel( pixel );  
+    ASSERTFAIL("operator[] not defined for ITKLatVolField for Faces");
+    if(image_set_) {
+      typename image_type::IndexType pixel;
+      pixel[0] = 0;
+      pixel[1] = 0;
+      pixel[2] = 0;
+      return image_->GetPixel( pixel );  
+    }
+    else {
+      ASSERTFAIL("ITKFData3d image not set");
+    }
   }
   value_type &operator[](typename LatVolMesh::Edge::index_type idx)
   {
-    ASSERT(0);
-    typename image_type::IndexType pixel;
-    pixel[0] = 0;
-    pixel[1] = 0;
-    pixel[2] = 0;
-    return image_->GetPixel( pixel );  
+    ASSERTFAIL("operator[] not defined for ITKLatVolField for Edges");
+    if(image_set_) {
+      typename image_type::IndexType pixel;
+      pixel[0] = 0;
+      pixel[1] = 0;
+      pixel[2] = 0;
+      return image_->GetPixel( pixel );  
+    }
+    else {
+      ASSERTFAIL("ITKFData3d image not set");
+    }
   }
   value_type &operator[](typename LatVolMesh::Node::index_type idx)
   {
-    typename image_type::IndexType pixel;
-    pixel[0] = idx.k_;
-    pixel[1] = idx.j_;
-    pixel[2] = idx.i_;
-    return image_->GetPixel( pixel ); 
-  }
-
-  void resize(const LatVolMesh::Node::size_type &size)
-  { 
-    if( (size.i_ == dim3()) &&
-	(size.j_ == dim2()) &&
-	(size.k_ == dim1())) {
-      return;
+    if(image_set_) {
+      typename image_type::IndexType pixel;
+      pixel[0] = idx.i_;
+      pixel[1] = idx.j_;
+      pixel[2] = idx.k_;
+      return image_->GetPixel( pixel ); 
     }
     else {
-      // ASSERT(0);
-      std::cerr << "RESIZE " << size.i_ << " " << size.j_ << " " << size.k_ << std::endl;
+      ASSERTFAIL("ITKFData3d image not set");
     }
   }
+
+  // These do not do anything because and itk::Image takes care of
+  // allocation.  This field merely points to an itk::Image.
+  void resize(const LatVolMesh::Node::size_type &size)
+  { 
+  }
   void resize(const LatVolMesh::Edge::size_type &size)
-    { //ASSERT(0); 
-    }
+  { 
+  }
   void resize(const LatVolMesh::Face::size_type &size)
-  { //ASSERT(0); 
+  {
   }
   void resize(const LatVolMesh::Cell::size_type &size)
   { 
-    if( (size.i_ == dim3()) &&
-	(size.j_ == dim2()) &&
-	(size.k_ == dim1())) {
-      return;
-    }
-    else {
-      //ASSERT(0); 
-      std::cerr << "RESIZE " << size.i_ << " " << size.j_ << " " << size.k_ << std::endl;
-    }
   }
   
   void set_image(itk::Image<Data, 3>* img) {
+    // set the image
     image_ = img;
 
+    // create iterators
     if ( begin_ ) delete begin_;
     begin_ = new iterator(image_, image_->GetRequestedRegion());
     begin_->GoToBegin();
@@ -213,6 +264,8 @@ public:
     if ( const_end_ ) delete const_end_;
     const_end_ = new const_iterator(image_, image_->GetRequestedRegion());
     const_end_->GoToEnd();
+    
+    image_set_ = true;
   }
   
   unsigned int size() { return (dim1() * dim2() * dim3()); }
@@ -223,7 +276,10 @@ public:
 
   static const string type_name(int n = -1);
 
+  image_type::Pointer get_image() { return image_; }
+
 private:
+  bool image_set_;
   typename image_type::Pointer image_;
 };
 
@@ -231,6 +287,7 @@ private:
 template <class Data>
 ITKFData3d<Data>::ITKFData3d()
 {
+  image_set_ = false;
   image_ = image_type::New(); 
   begin_ = 0;
   end_ = 0;
@@ -241,6 +298,7 @@ ITKFData3d<Data>::ITKFData3d()
 template <class Data>
 ITKFData3d<Data>::ITKFData3d(int a)
 {
+  image_set_ = false;
   image_ = image_type::New(); 
   begin_ = 0;
   end_ = 0;
@@ -250,13 +308,14 @@ ITKFData3d<Data>::ITKFData3d(int a)
 
 template <class Data>
 ITKFData3d<Data>::ITKFData3d(const ITKFData3d& data) {
+  image_set_ = false;
   image_ = image_type::New();
 
   if(dynamic_cast<itk::Image<Data, 3>* >(data.image_.GetPointer() )) {
     set_image(dynamic_cast<itk::Image<Data, 3>* >(data.image_.GetPointer() ));
   }
   else {
-    ASSERT(0);
+    ASSERTFAIL("Incorrect image type in ITKFData3d copy constructor");
   }
   
 }
@@ -294,20 +353,36 @@ ITKFData3d<Data>::type_name(int n)
 template <class Data>
 unsigned int ITKFData3d<Data>::dim1()
 {
-  return image_->GetLargestPossibleRegion().GetSize()[0];
+  if(image_set_)
+    return image_->GetLargestPossibleRegion().GetSize()[0];
+  else {
+    ASSERTFAIL("ITKFData3d Image not set");
+    return 0;
+  }
 }
 
 template <class Data>
 unsigned int ITKFData3d<Data>::dim2()
 {
-  return image_->GetLargestPossibleRegion().GetSize()[1];
+  if(image_set_)
+    return image_->GetLargestPossibleRegion().GetSize()[1];
+  else {
+    ASSERTFAIL("ITKFData3d Image not set");
+    return 0;
+  }
 }
 
 template <class Data>
 unsigned int ITKFData3d<Data>::dim3()
 {
-  return image_->GetLargestPossibleRegion().GetSize()[2];
+  if(image_set_)
+    return image_->GetLargestPossibleRegion().GetSize()[2];
+  else {
+    ASSERTFAIL("ITKFData3d Image not set");
+    return 0;
+  }
 }
+
 
 ///////////////////////////////////////////////////
 template <class Data>
@@ -341,7 +416,10 @@ public:
   // LatVolField Specific methods.
   bool get_gradient(SCIRun::Vector &, const Point &);
 
+  itk::Object* get_image() { return fdata().get_image(); }
+
 private:
+  bool image_set_;
   static Persistent* maker();
 };
 
@@ -351,6 +429,8 @@ template <class Data>
 ITKLatVolField<Data>::ITKLatVolField()
   : GenericField<LatVolMesh, ITKFData3d<Data> >()
 {
+  // need to set image
+  image_set_ = false;
 }
 
 
@@ -358,6 +438,8 @@ template <class Data>
 ITKLatVolField<Data>::ITKLatVolField(Field::data_location data_at)
   : GenericField<LatVolMesh, ITKFData3d<Data> >(data_at)
 {
+  // need to set image
+  image_set_ = false;
 }
 
 
@@ -366,7 +448,8 @@ ITKLatVolField<Data>::ITKLatVolField(LatVolMeshHandle mesh,
 			     Field::data_location data_at)
   : GenericField<LatVolMesh, ITKFData3d<Data> >(mesh, data_at)
 {
-
+  // need to set image
+  image_set_ = false;
 }
 
 template <class Data>
@@ -382,9 +465,10 @@ void ITKLatVolField<Data>::SetImage(itk::Object* img)
 {
   if(dynamic_cast<itk::Image<Data, 3>* >(img)) {
     fdata().set_image(dynamic_cast<itk::Image<Data, 3>* >(img));
+    image_set_ = true;
   }
   else {
-    ASSERT(0);
+    ASSERTFAIL("Image data is incorrect type.");
   }
 }
 
@@ -472,22 +556,27 @@ template <class Data>
 void
 ITKLatVolField<Data>::io(Piostream &stream)
 {
-  ASSERT(0);
-  /*  
-  int version = stream.begin_class(type_name(-1), ITK_LAT_VOL_FIELD_VERSION);
-  GenericField<LatVolMesh, ITKFData3d<Data> >::io(stream); 
-  stream.end_class();                                                         
-  if (version < 2) { 
-    ITKFData3d <Data> temp;
-    temp.copy(fdata()); 
-    resize_fdata(); 
-    int i, j, k; 
-    for (i=0; i<fdata().dim1(); i++) 
-      for (j=0; j<fdata().dim2(); j++) 
+  ASSERTFAIL("ITKLatVolField::io not implemented yet");
+  if(image_set_) {
+    /*  
+	int version = stream.begin_class(type_name(-1), ITK_LAT_VOL_FIELD_VERSION);
+	GenericField<LatVolMesh, ITKFData3d<Data> >::io(stream); 
+	stream.end_class();                                                         
+	if (version < 2) { 
+	ITKFData3d <Data> temp;
+	temp.copy(fdata()); 
+	resize_fdata(); 
+	int i, j, k; 
+	for (i=0; i<fdata().dim1(); i++) 
+	for (j=0; j<fdata().dim2(); j++) 
 	for (k=0; k<fdata().dim3(); k++) 
-	  fdata()(i,j,k)=temp(k,j,i); 
+	fdata()(i,j,k)=temp(k,j,i); 
+	}
+    */  
   }
-  */  
+  else {
+    ASSERTFAIL("ITKLatVolField Image not set");
+  }
 }
 
 
@@ -499,83 +588,87 @@ template <> bool ITKLatVolField<Vector>::get_gradient(Vector &, const Point &p);
 template <class Data>
 bool ITKLatVolField<Data>::get_gradient(Vector &g, const Point &p)
 {
-  // for now we only know how to do this for fields with scalars at the nodes
-  if (query_scalar_interface().get_rep())
-  {
-    if( data_at() == Field::NODE)
+  if( image_set_ ) {
+    // for now we only know how to do this for fields with scalars at the nodes
+    if (query_scalar_interface().get_rep())
     {
-      mesh_handle_type mesh = get_typed_mesh();
-      const Point r = mesh->get_transform().unproject(p);
-      double x = r.x();
-      double y = r.y();
-      double z = r.z();
-      
+      if( data_at() == Field::NODE)
+      {
+	mesh_handle_type mesh = get_typed_mesh();
+	const Point r = mesh->get_transform().unproject(p);
+	double x = r.x();
+	double y = r.y();
+	double z = r.z();
+	
 #if 0
-      Vector pn=p-mesh->get_min();
-      Vector diagonal = mesh->diagonal();
-      int ni=mesh->get_ni();
-      int nj=mesh->get_nj();
-      int nk=mesh->get_nk();
-      double diagx=diagonal.x();
-      double diagy=diagonal.y();
-      double diagz=diagonal.z();
-      double x=pn.x()*(ni-1)/diagx;
-      double y=pn.y()*(nj-1)/diagy;
-      double z=pn.z()*(nk-1)/diagz;
+	Vector pn=p-mesh->get_min();
+	Vector diagonal = mesh->diagonal();
+	int ni=mesh->get_ni();
+	int nj=mesh->get_nj();
+	int nk=mesh->get_nk();
+	double diagx=diagonal.x();
+	double diagy=diagonal.y();
+	double diagz=diagonal.z();
+	double x=pn.x()*(ni-1)/diagx;
+	double y=pn.y()*(nj-1)/diagy;
+	double z=pn.z()*(nk-1)/diagz;
 #endif
-
-      int ni=mesh->get_ni();
-      int nj=mesh->get_nj();
-      int nk=mesh->get_nk();
-      int ix0 = (int)x;
-      int iy0 = (int)y;
-      int iz0 = (int)z;
-      int ix1 = ix0+1;
-      int iy1 = iy0+1;
-      int iz1 = iz0+1;
-      if(ix0<0 || ix1>=ni)return false;
-      if(iy0<0 || iy1>=nj)return false;
-      if(iz0<0 || iz1>=nk)return false;
-      double fx = x-ix0;
-      double fy = y-iy0;
-      double fz = z-iz0;
-      LatVolMesh *mp = mesh.get_rep();
-      double d000 = (double)value(LatVolMesh::Node::index_type(mp,ix0,iy0,iz0));
-      double d100 = (double)value(LatVolMesh::Node::index_type(mp,ix1,iy0,iz0));
-      double d010 = (double)value(LatVolMesh::Node::index_type(mp,ix0,iy1,iz0));
-      double d110 = (double)value(LatVolMesh::Node::index_type(mp,ix1,iy1,iz0));
-      double d001 = (double)value(LatVolMesh::Node::index_type(mp,ix0,iy0,iz1));
-      double d101 = (double)value(LatVolMesh::Node::index_type(mp,ix1,iy0,iz1));
-      double d011 = (double)value(LatVolMesh::Node::index_type(mp,ix0,iy1,iz1));
-      double d111 = (double)value(LatVolMesh::Node::index_type(mp,ix1,iy1,iz1));
-      double z00 = Interpolate(d000, d001, fz);
-      double z01 = Interpolate(d010, d011, fz);
-      double z10 = Interpolate(d100, d101, fz);
-      double z11 = Interpolate(d110, d111, fz);
-      double yy0 = Interpolate(z00, z01, fy);
-      double yy1 = Interpolate(z10, z11, fy);
-      double dx = (yy1-yy0);
-      double x00 = Interpolate(d000, d100, fx);
-      double x01 = Interpolate(d001, d101, fx);
-      double x10 = Interpolate(d010, d110, fx);
-      double x11 = Interpolate(d011, d111, fx);
-      double y0 = Interpolate(x00, x10, fy);
-      double y1 = Interpolate(x01, x11, fy);
-      double dz = (y1-y0);
-      double z0 = Interpolate(x00, x01, fz);
-      double z1 = Interpolate(x10, x11, fz);
-      double dy = (z1-z0);
-      g = mesh->get_transform().unproject(Vector(dx, dy, dz));
-      return true;
+	
+	int ni=mesh->get_ni();
+	int nj=mesh->get_nj();
+	int nk=mesh->get_nk();
+	int ix0 = (int)x;
+	int iy0 = (int)y;
+	int iz0 = (int)z;
+	int ix1 = ix0+1;
+	int iy1 = iy0+1;
+	int iz1 = iz0+1;
+	if(ix0<0 || ix1>=ni)return false;
+	if(iy0<0 || iy1>=nj)return false;
+	if(iz0<0 || iz1>=nk)return false;
+	double fx = x-ix0;
+	double fy = y-iy0;
+	double fz = z-iz0;
+	LatVolMesh *mp = mesh.get_rep();
+	double d000 = (double)value(LatVolMesh::Node::index_type(mp,ix0,iy0,iz0));
+	double d100 = (double)value(LatVolMesh::Node::index_type(mp,ix1,iy0,iz0));
+	double d010 = (double)value(LatVolMesh::Node::index_type(mp,ix0,iy1,iz0));
+	double d110 = (double)value(LatVolMesh::Node::index_type(mp,ix1,iy1,iz0));
+	double d001 = (double)value(LatVolMesh::Node::index_type(mp,ix0,iy0,iz1));
+	double d101 = (double)value(LatVolMesh::Node::index_type(mp,ix1,iy0,iz1));
+	double d011 = (double)value(LatVolMesh::Node::index_type(mp,ix0,iy1,iz1));
+	double d111 = (double)value(LatVolMesh::Node::index_type(mp,ix1,iy1,iz1));
+	double z00 = Interpolate(d000, d001, fz);
+	double z01 = Interpolate(d010, d011, fz);
+	double z10 = Interpolate(d100, d101, fz);
+	double z11 = Interpolate(d110, d111, fz);
+	double yy0 = Interpolate(z00, z01, fy);
+	double yy1 = Interpolate(z10, z11, fy);
+	double dx = (yy1-yy0);
+	double x00 = Interpolate(d000, d100, fx);
+	double x01 = Interpolate(d001, d101, fx);
+	double x10 = Interpolate(d010, d110, fx);
+	double x11 = Interpolate(d011, d111, fx);
+	double y0 = Interpolate(x00, x10, fy);
+	double y1 = Interpolate(x01, x11, fy);
+	double dz = (y1-y0);
+	double z0 = Interpolate(x00, x01, fz);
+	double z1 = Interpolate(x10, x11, fz);
+	double dy = (z1-z0);
+	g = mesh->get_transform().unproject(Vector(dx, dy, dz));
+	return true;
+      }
     }
+  return false;
   }
+  ASSERTFAIL("ITKLatVolField Image not set");
   return false;
 }
 
 template<class T> 
 void Pio(Piostream& stream, ITKFData3d<T>& array)
 {
-  ASSERT(0); 
+  ASSERTFAIL("Pio not written for ITKLatVolField");
 }
 
 } // end namespace Insight
