f33806ebf07f1d5e058ba3ae533e58b19068c5c9
Darby Brown <darbyb@sci.utah.edu>
2003-07-29 15:55:23 +0000

    Added copyright

--- Packages/Insight/Dataflow/GUI/sub.mk
+++ Packages/Insight/Dataflow/GUI/sub.mk
@@ -1,3 +1,21 @@
+#
+#  The contents of this file are subject to the University of Utah Public
+#  License (the "License"); you may not use this file except in compliance
+#  with the License.
+#  
+#  Software distributed under the License is distributed on an "AS IS"
+#  basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+#  License for the specific language governing rights and limitations under
+#  the License.
+#  
+#  The Original Source Code is SCIRun, released March 12, 2001.
+#  
+#  The Original Source Code was developed by the University of Utah.
+#  Portions created by UNIVERSITY are Copyright (C) 2001, 1994 
+#  University of Utah. All Rights Reserved.
+#
+
+
 # *** NOTE ***
 #
 # Do not remove or modify the comment line:
@@ -32,7 +50,16 @@ ALLTARGETS := $(ALLTARGETS) $(SRCDIR)/tclIndex
 Packages/Insight/Dataflow/GUI/%.tcl:  Packages/Insight/Dataflow/Modules/Filters/XML/sci_%.xml
 	$(CODEGEN) $(PATH_TO_SCIRUN)/Packages/Insight $(CATEGORY) $* -gui 
 
-$(SRCDIR)/tclIndex: $(INSIGHT_TCL_GEN)
+$(SRCDIR)/tclIndex: $(INSIGHT_TCL_GEN) \
+	$(SRCDIR)/ImageFileWriter.tcl \
+	$(SRCDIR)/ImageReaderFloat2D.tcl \
+	$(SRCDIR)/ImageReaderFloat3D.tcl \
+	$(SRCDIR)/ImageReaderUChar2D.tcl \
+	$(SRCDIR)/ImageReaderUChar3D.tcl \
+	$(SRCDIR)/ImageReaderUShort2D.tcl \
+	$(SRCDIR)/ImageReaderUShort3D.tcl \
+	$(SRCDIR)/ImageToField.tcl \
+	$(SRCDIR)/Switch.tcl \
 #[INSERT NEW TCL FILE HERE]
 	$(OBJTOP)/createTclIndex $(SRCTOP)/Packages/Insight/Dataflow/GUI
 
