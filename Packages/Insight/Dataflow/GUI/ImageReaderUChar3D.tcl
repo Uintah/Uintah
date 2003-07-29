a5c997ce727a67e6b802151b826ef413f42474af
Darby Brown <darbyb@sci.utah.edu>
2003-07-29 16:03:32 +0000

    New Readers

---
+++ Packages/Insight/Dataflow/GUI/ImageReaderUChar3D.tcl
@@ -0,0 +1,60 @@
+#
+#  The contents of this file are subject to the University of Utah Public
+#  License (the "License"); you may not use this file except in compliance
+#  with the License.
+#  
+#  Software distributed under the License is distributed on an "AS IS"
+#  basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+#  License for the specific language governing rights and limitations under
+#  the License.
+#  
+#  The Original Source Code is SCIRun, released March 12, 2001.
+#  
+#  The Original Source Code was developed by the University of Utah.
+#  Portions created by UNIVERSITY are Copyright (C) 2001, 1994 
+#  University of Utah. All Rights Reserved.
+#
+
+itcl_class Insight_DataIO_ImageReaderUChar3D {
+    inherit Module
+    constructor {config} {
+        set name ImageReaderUChar3D
+        set_defaults
+    }
+
+    method set_defaults {} {
+    }
+
+    method ui {} {
+        set w .ui[modname]
+        if {[winfo exists $w]} {
+	    set child [lindex [winfo children $w] 0]
+	    
+	    # $w withdrawn by $child's procedures
+	    raise $child
+	    return;
+        }
+        toplevel $w
+
+	set defext ".mhd"
+	set title "Open image file"
+	
+	# file types to appers in filter box
+	set types {
+	    {{Meta Image}        {.mhd} }
+	    {{PNG Image}        {.png} }
+	    {{All Files}       {.*}    }
+	}
+
+	makeOpenFilebox \
+		-parent $w \
+		-filevar $this-FileName \
+		-command "$this-c needexecute; destroy $w" \
+		-cancel "destroy $w" \
+		-title "Open Image File" \
+                -filetypes $types \
+		-defaultextension $defext
+    }
+}
+
+
