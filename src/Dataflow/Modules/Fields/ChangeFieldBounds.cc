d9965a780b8072be6ba28a5377900fe3595e47e0
Michael Callahan <callahan@sci.utah.edu>
2003-07-31 03:05:27 +0000

    Save widget state to network.  Also add reset button as requested by Butson

--- src/Dataflow/Modules/Fields/ChangeFieldBounds.cc
+++ src/Dataflow/Modules/Fields/ChangeFieldBounds.cc
@@ -64,6 +64,14 @@ public:
   GuiString		inputsizey_;
   GuiString		inputsizez_;
 
+  GuiDouble             box_scale_;
+  GuiPoint              box_center_;
+  GuiPoint              box_right_;
+  GuiPoint              box_down_;
+  GuiPoint              box_in_;
+
+  GuiInt                resetting_;
+
   CrowdMonitor		widget_lock_;
   BoxWidget *		box_;
   Transform		box_initial_transform_;
@@ -74,7 +82,7 @@ public:
 
   void clear_vals();
   void update_input_attributes(FieldHandle);
-  void build_widget(FieldHandle);
+  void build_widget(FieldHandle, bool reset);
 
   virtual void execute();
   virtual void widget_moved(bool);
@@ -90,14 +98,20 @@ ChangeFieldBounds::ChangeFieldBounds(GuiContext* ctx)
     outputsizex_(ctx->subVar("outputsizex")),
     outputsizey_(ctx->subVar("outputsizey")),
     outputsizez_(ctx->subVar("outputsizez")),
+    useoutputcenter_(ctx->subVar("useoutputcenter")),
+    useoutputsize_(ctx->subVar("useoutputsize")),
     inputcenterx_(ctx->subVar("inputcenterx", false)),
     inputcentery_(ctx->subVar("inputcentery", false)),
     inputcenterz_(ctx->subVar("inputcenterz", false)),
     inputsizex_(ctx->subVar("inputsizex", false)),
     inputsizey_(ctx->subVar("inputsizey", false)),
     inputsizez_(ctx->subVar("inputsizez", false)),
-    useoutputcenter_(ctx->subVar("useoutputcenter")),
-    useoutputsize_(ctx->subVar("useoutputsize")),
+    box_scale_(ctx->subVar("box-scale")),
+    box_center_(ctx->subVar("box-center")),
+    box_right_(ctx->subVar("box-right")),
+    box_down_(ctx->subVar("box-down")),
+    box_in_(ctx->subVar("box-in")),
+    resetting_(ctx->subVar("resetting"), false),
     widget_lock_("ChangeFieldBounds widget lock"),
     generation_(-1),
     widgetid_(0)
@@ -158,63 +172,73 @@ ChangeFieldBounds::update_input_attributes(FieldHandle f)
 
 
 void
-ChangeFieldBounds::build_widget(FieldHandle f)
+ChangeFieldBounds::build_widget(FieldHandle f, bool reset)
 {
-  double l2norm;
-  Point center;
-  Vector size;
-  BBox bbox = f->mesh()->get_bounding_box();
-  if (!bbox.valid()) {
-    warning("Input field is empty -- using unit cube.");
-    bbox.extend(Point(0,0,0));
-    bbox.extend(Point(1,1,1));
+  if (reset || box_scale_.get() <= 0)
+  {
+    Point center;
+    Vector size;
+    BBox bbox = f->mesh()->get_bounding_box();
+    if (!bbox.valid()) {
+      warning("Input field is empty -- using unit cube.");
+      bbox.extend(Point(0,0,0));
+      bbox.extend(Point(1,1,1));
+    }
+    box_initial_bounds_ = bbox;
+
+    // build a widget identical to the BBox
+    size = Vector(bbox.max()-bbox.min());
+    if (fabs(size.x())<1.e-4) {
+      size.x(2.e-4); 
+      bbox.extend(bbox.min()-Vector(1.e-4,0,0));
+    }
+    if (fabs(size.y())<1.e-4) {
+      size.y(2.e-4); 
+      bbox.extend(bbox.min()-Vector(0,1.e-4,0));
+    }
+    if (fabs(size.z())<1.e-4) {
+      size.z(2.e-4); 
+      bbox.extend(bbox.min()-Vector(0,0,1.e-4));
+    }
+    center = Point(bbox.min() + size/2.);
+
+    Vector sizex(size.x(),0,0);
+    Vector sizey(0,size.y(),0);
+    Vector sizez(0,0,size.z());
+
+    Point right(center + sizex/2.);
+    Point down(center + sizey/2.);
+    Point in(center +sizez/2.);
+
+    const double l2norm = size.length();
+
+    // Translate * Rotate * Scale.
+    Transform r;
+    Point unused;
+    box_initial_transform_.load_identity();
+    box_initial_transform_.pre_scale(Vector((right-center).length(),
+					    (down-center).length(),
+					    (in-center).length()));
+    r.load_frame(unused, (right-center).normal(),
+		 (down-center).normal(),
+		 (in-center).normal());
+    box_initial_transform_.pre_trans(r);
+    box_initial_transform_.pre_translate(center.asVector());
+
+
+    box_->SetScale(l2norm * 0.015);
+    box_->SetPosition(center, right, down, in);
+    box_scale_.set(-1.0);
   }
-  box_initial_bounds_ = bbox;
-
-  // build a widget identical to the BBox
-  size = Vector(bbox.max()-bbox.min());
-  if (fabs(size.x())<1.e-4) {
-    size.x(2.e-4); 
-    bbox.extend(bbox.min()-Vector(1.e-4,0,0));
+  else
+  {
+    const double l2norm = (box_right_.get().vector() +
+			   box_down_.get().vector() +
+			   box_in_.get().vector()).length();
+    box_->SetScale(l2norm * 0.015);
+    box_->SetPosition(box_center_.get(), box_right_.get(),
+		      box_down_.get(), box_in_.get());
   }
-  if (fabs(size.y())<1.e-4) {
-    size.y(2.e-4); 
-    bbox.extend(bbox.min()-Vector(0,1.e-4,0));
-  }
-  if (fabs(size.z())<1.e-4) {
-    size.z(2.e-4); 
-    bbox.extend(bbox.min()-Vector(0,0,1.e-4));
-  }
-  center = Point(bbox.min() + size/2.);
-
-
-
-  Vector sizex(size.x(),0,0);
-  Vector sizey(0,size.y(),0);
-  Vector sizez(0,0,size.z());
-
-  Point right(center + sizex/2.);
-  Point down(center + sizey/2.);
-  Point in(center +sizez/2.);
-
-  l2norm = size.length();
-
-  // Translate * Rotate * Scale.
-  Transform r;
-  Point unused;
-  box_initial_transform_.load_identity();
-  box_initial_transform_.pre_scale(Vector((right-center).length(),
-			     (down-center).length(),
-			     (in-center).length()));
-  r.load_frame(unused, (right-center).normal(),
-	       (down-center).normal(),
-	       (in-center).normal());
-  box_initial_transform_.pre_trans(r);
-  box_initial_transform_.pre_translate(center.asVector());
-
-
-  box_->SetScale(l2norm * 0.015);
-  box_->SetPosition(center, right, down, in);
 
   GeomGroup *widget_group = scinew GeomGroup;
   widget_group->add(box_->GetWidget());
@@ -228,6 +252,7 @@ ChangeFieldBounds::build_widget(FieldHandle f)
 			     &widget_lock_);
   ogport->flushViews();
 }
+
 
 void
 ChangeFieldBounds::execute()
@@ -257,12 +282,12 @@ ChangeFieldBounds::execute()
 
   // build the transform widget and set the the initial
   // field transform.
-  if (generation_ != fh.get_rep()->generation) 
+  if (generation_ != fh.get_rep()->generation || resetting_.get()) 
   {
     generation_ = fh.get_rep()->generation;
     // get and display the attributes of the input field
     update_input_attributes(fh);
-    build_widget(fh);
+    build_widget(fh, resetting_.get());
     BBox bbox = fh->mesh()->get_bounding_box();
     if (!bbox.valid()) {
       warning("Input field is empty -- using unit cube.");
@@ -291,7 +316,6 @@ ChangeFieldBounds::execute()
     Point down(center + sizey/2.);
     Point in(center +sizez/2.);
 
-    //double l2norm(size.length());
     Transform r;
     Point unused;
     field_initial_transform_.load_identity();
@@ -303,6 +327,8 @@ ChangeFieldBounds::execute()
 		 (in-center).normal());
     field_initial_transform_.pre_trans(r);
     field_initial_transform_.pre_translate(center.asVector());
+
+    resetting_.set(0);
   }
 
   if (useoutputsize_.get() || useoutputcenter_.get()) {
@@ -391,6 +417,11 @@ void ChangeFieldBounds::widget_moved(bool last)
     outputsizex_.set((right.x()-center.x())*2.);
     outputsizey_.set((down.y()-center.y())*2.);
     outputsizez_.set((in.z()-center.z())*2.);
+    box_scale_.set(box_->GetScale());
+    box_center_.set(center);
+    box_right_.set(right);
+    box_down_.set(down);
+    box_in_.set(in);
     want_to_execute();
   }
 }
