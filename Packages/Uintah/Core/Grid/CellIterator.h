f2122b2ce530eef5e8e8d1e80ecae428d6fabfb1
Steven G. Parker <sparker@sci.utah.edu>
2003-08-06 03:00:38 +0000

    Rewrote celliterator to be more friendly to the IBM compiler  - store current as IntVector instead of three ints  - done flag is computed in ++ May help other architectures too

--- Packages/Uintah/Core/Grid/CellIterator.h
+++ Packages/Uintah/Core/Grid/CellIterator.h
@@ -42,25 +42,28 @@ WARNING
     
     //////////
     // Insert Documentation Here:
-    inline CellIterator operator++(int) {
-      CellIterator old(*this);
-      
-      if(++d_ix >= d_e.x()){
-	d_ix = d_s.x();
-	if(++d_iy >= d_e.y()){
-	  d_iy = d_s.y();
-	  ++d_iz;
+    inline void operator++(int) {
+      // This does not return the old iterator due to performance problems
+      // on some compilers...
+      if(++d_cur.modifiable_x() >= d_e.x()){
+	d_cur.modifiable_x() = d_s.x();
+	if(++d_cur.modifiable_y() >= d_e.y()){
+	  d_cur.modifiable_y() = d_s.y();
+	  ++d_cur.modifiable_z();
+	  if(d_cur.modifiable_z() >= d_e.z())
+	    d_done=true;
 	}
       }
-      return old;
     }
 
     inline CellIterator& operator++() {
-      if(++d_ix >= d_e.x()){
-	d_ix = d_s.x();
-	if(++d_iy >= d_e.y()){
-	  d_iy = d_s.y();
-	  ++d_iz;
+      if(++d_cur.modifiable_x() >= d_e.x()){
+	d_cur.modifiable_x() = d_s.x();
+	if(++d_cur.modifiable_y() >= d_e.y()){
+	  d_cur.modifiable_y() = d_s.y();
+	  ++d_cur.modifiable_z();
+	  if(d_cur.modifiable_z() >= d_e.z())
+	    d_done=true;
 	}
       }
       return *this;
@@ -72,11 +75,13 @@ WARNING
       CellIterator old(*this);
       
       for (int i = 0; i < step; i++) {
-	if(++d_ix >= d_e.x()){
-	  d_ix = d_s.x();
-	  if(++d_iy >= d_e.y()){
-	    d_iy = d_s.y();
-	    ++d_iz;
+	if(++d_cur.modifiable_x() >= d_e.x()){
+	  d_cur.modifiable_x() = d_s.x();
+	  if(++d_cur.modifiable_y() >= d_e.y()){
+	    d_cur.modifiable_y() = d_s.y();
+	    ++d_cur.modifiable_z();
+	    if(d_cur.modifiable_z() >= d_e.z())
+	      d_done=true;
 	  }
 	}
 	if (done())
@@ -88,23 +93,15 @@ WARNING
     //////////
     // Insert Documentation Here:
     inline bool done() const {
-      return d_ix >= d_e.x() || d_iy >= d_e.y() || d_iz >= d_e.z();
+      return d_done;
     }
-    
-    IntVector operator*() {
-      return IntVector(d_ix, d_iy, d_iz);
-    }
-    IntVector index() const {
-      return IntVector(d_ix, d_iy, d_iz);
+     
+    const IntVector& operator*() const {
+      ASSERT(!d_done);
+      return d_cur;
     }
     inline CellIterator(const IntVector& s, const IntVector& e)
-      : d_s(s), d_e(e) {
-      d_ix = s.x();
-      d_iy = s.y();
-      d_iz = s.z();
-    }
-    inline IntVector current() const {
-      return IntVector(d_ix, d_iy, d_iz);
+      : d_s(s), d_e(e), d_cur(s), d_done(false) {
     }
     inline IntVector begin() const {
       return d_s;
@@ -113,8 +110,7 @@ WARNING
       return d_e;
     }
     inline CellIterator(const CellIterator& copy)
-      : d_s(copy.d_s), d_e(copy.d_e),
-	d_ix(copy.d_ix), d_iy(copy.d_iy), d_iz(copy.d_iz) {
+      : d_s(copy.d_s), d_e(copy.d_e), d_cur(copy.d_cur), d_done(copy.d_done) {
     }
     
   private:
@@ -124,10 +120,11 @@ WARNING
     //////////
     // Insert Documentation Here:
     IntVector d_s,d_e;
-    int d_ix, d_iy, d_iz;
+    IntVector d_cur;
+    bool d_done;
   };
 } // End namespace Uintah
   
 std::ostream& operator<<(std::ostream& out, const Uintah::CellIterator& b);
-
+ 
 #endif
