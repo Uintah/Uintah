a56753e8462c35250a80c9b587146e0d36505f0b
Kurt Zimmerman <kuzimmer@sci.utah.edu>
2003-08-21 21:55:15 +0000

    Fix problem with periodic boundaries

--- src/Packages/Uintah/Dataflow/Modules/Selectors/FieldExtractor.cc
+++ src/Packages/Uintah/Dataflow/Modules/Selectors/FieldExtractor.cc
@@ -170,4 +170,35 @@ void FieldExtractor::update_GUI(const string& var,
   }
 }
 
+bool 
+FieldExtractor::is_periodic_bcs(IntVector cellir, IntVector ir)
+{
+  if( cellir.x() == ir.x() ||
+      cellir.y() == ir.y() ||
+      cellir.z() == ir.z() )
+    return true;
+  else
+    return false;
+}
+
+void 
+FieldExtractor::get_periodic_bcs_range(IntVector cellmax, IntVector datamax,
+				       IntVector range, IntVector& newrange)
+{
+  if( cellmax.x() == datamax.x())
+    newrange.x( range.x() + 1 );
+  else
+    newrange.x( range.x() );
+  if( cellmax.y() == datamax.y())
+    newrange.y( range.y() + 1 );
+  else
+    newrange.y( range.y() );
+  if( cellmax.z() == datamax.z())
+    newrange.z( range.z() + 1 );
+  else
+    newrange.z( range.z() );
+}
+
+
+
 } // End namespace Uintah
