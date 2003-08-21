83597a4472ceb2db1070fd96e422757fbc4c47d2
David Weinstein <dmw@sci.utah.edu>
2003-08-21 22:22:14 +0000

    - Made LeadField and MisfitField construction work for non-tet meshes

--- Packages/BioPSE/Dataflow/Modules/LeadField/BuildElemLeadField.cc
+++ Packages/BioPSE/Dataflow/Modules/LeadField/BuildElemLeadField.cc
@@ -26,13 +26,15 @@
  *  Copyright (C) 1999 SCI Group
  */
 
-#include <Dataflow/Network/Module.h>
-#include <Dataflow/Ports/MatrixPort.h>
-#include <Dataflow/Ports/FieldPort.h>
 #include <Core/Datatypes/DenseMatrix.h>
 #include <Core/Datatypes/ColumnMatrix.h>
 #include <Core/Datatypes/PointCloudField.h>
 #include <Core/Datatypes/TetVolField.h>
+#include <Dataflow/Network/Module.h>
+#include <Dataflow/Ports/MatrixPort.h>
+#include <Dataflow/Ports/FieldPort.h>
+
+#include <Dataflow/Modules/Fields/FieldInfo.h>
 
 #include <iostream>
 #include <stdio.h>
@@ -61,12 +63,6 @@ using namespace SCIRun;
 
 
 class BuildElemLeadField : public Module {    
-  FieldIPort* mesh_iport_;
-  FieldIPort* interp_iport_;
-  MatrixIPort* sol_iport_;
-  MatrixOPort* rhs_oport_;
-  MatrixOPort* leadfield_oport_;
-
   MatrixHandle leadfield_;
   int last_mesh_generation_;
   int last_interp_generation_;
@@ -92,65 +88,51 @@ BuildElemLeadField::BuildElemLeadField(GuiContext *context)
 BuildElemLeadField::~BuildElemLeadField(){}
 
 void BuildElemLeadField::execute() {
-  mesh_iport_ = (FieldIPort *)get_iport("Domain Mesh");
-  interp_iport_ = (FieldIPort *)get_iport("Electrode Interpolant");
-  sol_iport_ = (MatrixIPort *)get_iport("Solution Vectors");
-  rhs_oport_ = (MatrixOPort *)get_oport("RHS Vector");
-  leadfield_oport_ = (MatrixOPort *)get_oport("Leadfield (nelecs x nelemsx3)");
+  FieldIPort *mesh_iport = (FieldIPort *)get_iport("Domain Mesh");
+  MatrixIPort *interp_iport = 
+    (MatrixIPort *)get_iport("Electrode Interpolant");
+  MatrixIPort *sol_iport = (MatrixIPort *)get_iport("Solution Vectors");
+  MatrixOPort *rhs_oport = (MatrixOPort *)get_oport("RHS Vector");
+  MatrixOPort* leadfield_oport = 
+    (MatrixOPort *)get_oport("Leadfield (nelecs x nelemsx3)");
 
-  if (!mesh_iport_) {
+  if (!mesh_iport) {
     error("Unable to initialize iport 'Domain Mesh'.");
     return;
   }
-  if (!interp_iport_) {
+  if (!interp_iport) {
     error("Unable to initialize iport 'Electrode Interpolant'.");
     return;
   }
-  if (!sol_iport_) {
+  if (!sol_iport) {
     error("Unable to initialize iport 'Solution Vectors'.");
     return;
   }
-  if (!rhs_oport_) {
+  if (!rhs_oport) {
     error("Unable to initialize oport 'RHS Vector'.");
     return;
   }
-  if (!leadfield_oport_) {
+  if (!leadfield_oport) {
     error("Unable to initialize oport 'Leadfield (nelecs x nelemsx3)'.");
     return;
   }
   
-
+  int nnodes;
+  int nelems;
   FieldHandle mesh_in;
-  if (!mesh_iport_->get(mesh_in)) {
+  if (!mesh_iport->get(mesh_in) || !mesh_in.get_rep()) {
     error("Couldn't get input mesh.");
     return;
   }
-  if (!mesh_in.get_rep() || mesh_in->get_type_name(0)!="TetVolField") {
-    error("Expected a TetVolField for the mesh.");
-    return;
-  }
-  TetVolMesh* mesh = 
-    (TetVolMesh*)dynamic_cast<TetVolMesh*>(mesh_in->mesh().get_rep());
+  const TypeDescription *meshtd = mesh_in->mesh()->get_type_description();
+  CompileInfoHandle ci = FieldInfoAlgoCount::get_compile_info(meshtd);
+  Handle<FieldInfoAlgoCount> algo;
+  if (!module_dynamic_compile(ci, algo)) return;
+  algo->execute(mesh_in->mesh(), nnodes, nelems);
 
-  FieldHandle interp_in;
-  if (!interp_iport_->get(interp_in)) {
+  MatrixHandle interp_in;
+  if (!interp_iport->get(interp_in) || !interp_in.get_rep()) {
     error("Couldn't get electrode interpolant.");
-    return;
-  }
-  if (!interp_in.get_rep() || interp_in->get_type_name(0)!="PointCloudField") {
-    error("Didn't get a PointCloudField for interp.");
-    return;
-  }
-  PointCloudMesh* interp_mesh = 
-    (PointCloudMesh*)dynamic_cast<PointCloudMesh*>(interp_in->mesh().get_rep());
-  //  PointCloudField<vector<pair<TetVolMesh::Node::index_type, double> > >* interp = 
-  //    dynamic_cast<PointCloudField<vector<pair<TetVolMesh::Node::index_type, double> > > *>(interp_in.get_rep());
-
-  PointCloudField<vector<pair<TetVolMesh::Node::index_type, double> > >* interp = 
-    (PointCloudField<vector<pair<TetVolMesh::Node::index_type, double> > > *)(interp_in.get_rep());
-
-  if (!interp) {
-    error("Interp Field wasn't a PointCloudField<vector<pair<TetVolMesh::Node::index_type,double>>>. It's a '" + interp_in->get_type_description()->get_name() + "'.");
     return;
   }
 
@@ -159,45 +141,42 @@ void BuildElemLeadField::execute() {
   last_mesh_generation_ = mesh_in->generation;
   last_interp_generation_ = interp_in->generation;
 
-  PointCloudMesh::Node::size_type insize;  interp_mesh->size(insize);
-  TetVolMesh::Node::size_type nsize;  mesh->size(nsize);
-  TetVolMesh::Cell::size_type csize;  mesh->size(csize);
-  int nelecs=insize;
-  int nnodes=nsize;
-  int nelems=csize;
+  int nelecs=interp_in->nrows();
   int counter=0;
   DenseMatrix *leadfield_mat=new DenseMatrix(nelecs, nelems*3);
   leadfield_mat->zero();
-  
+
   while (counter<(nelecs-1)) {
+    update_progress(counter*1./(nelecs-1));
     ColumnMatrix* rhs=new ColumnMatrix(nnodes);
     int i;
     for (i=0; i<nnodes; i++) (*rhs)[i]=0;
-    
-    vector<pair<TetVolMesh::Node::index_type,double> >::iterator iter = interp->fdata()[0].begin();
-    while (iter != interp->fdata()[0].end()) {
-      int idx = (*iter).first;
-      double val = (*iter).second;
-      (*rhs)[idx]+=val;
-      ++iter;
+
+    Array1<int> idx;
+    Array1<double> val;
+
+    interp_in->getRowNonzeros(0, idx, val);
+    if (!idx.size()) ASSERTFAIL("No mesh node assigned to this element!");
+    for (i=0; i<idx.size(); i++) {
+      if (idx[i] >= nnodes) ASSERTFAIL("Mesh node out of range!");
+      (*rhs)[idx[i]]+=val[i];
     }
 
-    iter = interp->fdata()[counter+1].begin();
-    while (iter != interp->fdata()[counter+1].end()) {
-      int idx = (*iter).first;
-      double val = (*iter).second;
-      (*rhs)[idx]-=val;
-      ++iter;
+    interp_in->getRowNonzeros(counter+1, idx, val);
+    if (!idx.size()) ASSERTFAIL("No mesh node assigned to this element!");
+    for (i=0; i<idx.size(); i++) {
+      if (idx[i] >= nnodes) ASSERTFAIL("Mesh node out of range!");
+      (*rhs)[idx[i]]-=val[i];
     }
-    
+
     if (counter<(nelecs-2)) {
-      rhs_oport_->send_intermediate(rhs);
+      rhs_oport->send_intermediate(rhs);
     } else {
-      rhs_oport_->send(rhs);
+      rhs_oport->send(rhs);
     }
     // read sol'n
     MatrixHandle sol_in;
-    if (!sol_iport_->get(sol_in)) {
+    if (!sol_iport->get(sol_in)) {
       error("Couldn't get solution vector.");
       return;
     }
@@ -205,11 +184,9 @@ void BuildElemLeadField::execute() {
       for (int j=0; j<3; j++) {
 	(*leadfield_mat)[counter+1][i*3+j]=-(*sol_in.get_rep())[i][j];
       }
-    msgStream_ << "BuildElemLeadField: "<<counter+1<<"/"<<nelecs-1<<"\n";
     counter++;
-    
   }
   leadfield_=leadfield_mat;
-  leadfield_oport_->send(leadfield_);
+  leadfield_oport->send(leadfield_);
 } 
 } // End namespace BioPSE
--- Packages/BioPSE/Dataflow/Modules/LeadField/BuildMisfitField.cc
+++ Packages/BioPSE/Dataflow/Modules/LeadField/BuildMisfitField.cc
@@ -32,8 +32,6 @@
 #include <Core/Containers/StringUtil.h>
 #include <Core/Datatypes/DenseMatrix.h>
 #include <Core/Datatypes/ColumnMatrix.h>
-#include <Core/Datatypes/PointCloudField.h>
-#include <Core/Datatypes/TetVolField.h>
 #include <Core/GuiInterface/GuiVar.h>
 #include <Core/Math/Mat.h>
 
@@ -42,9 +40,9 @@
 #include <math.h>
 
 // Take in a ColumnMatrix, c,  and a LeadField, L
-//   Scan c through all of the elements in L
-//   For each element, find the misfit for the optimal moment weights
-//   Store the misfit value at the cells (for now, push out to nodes)
+//   Scan c through all of the elements (three column groupings) in L
+//   For each "element", find the misfit for the optimal moment weights
+//   Send out a ColumnMatrix of the misfits
 
 namespace BioPSE {
 
@@ -56,21 +54,16 @@ using namespace SCIRun;
 
 
 class BuildMisfitField : public Module {    
-  FieldIPort* mesh_iport_;
-  MatrixIPort* leadfield_iport_;
-  MatrixIPort* measurements_iport_;
-  FieldOPort* mesh_oport_;
-
-  int last_mesh_generation_;
   int last_leadfield_generation_;
   int last_measurements_generation_;
-  FieldHandle last_mesh_;
+  MatrixHandle last_misfit_;
   string last_metric_;
   double last_pvalue_;
   GuiString metric_;
   GuiString pvalue_;
 public:
   BuildMisfitField(GuiContext *context);
+  double compute_misfit(double *b, double *bprime, int nr);
   virtual ~BuildMisfitField();
   virtual void execute();
 };
@@ -82,52 +75,89 @@ DECLARE_MAKER(BuildMisfitField)
 //---------------------------------------------------------------
 BuildMisfitField::BuildMisfitField(GuiContext *context)
   : Module("BuildMisfitField", context, Filter, "LeadField", "BioPSE"), 
-  last_mesh_generation_(-1), last_leadfield_generation_(-1),
+  last_leadfield_generation_(-1),
   last_measurements_generation_(-1),
-  last_mesh_(0), last_metric_(""), last_pvalue_(1),
+  last_misfit_(0), last_metric_(""), last_pvalue_(1),
   metric_(context->subVar("metric")), pvalue_(context->subVar("pvalue"))
 {
 }
 
 BuildMisfitField::~BuildMisfitField(){}
 
-void BuildMisfitField::execute() {
-  mesh_iport_ = (FieldIPort *)get_iport("FEM Mesh");
-  leadfield_iport_ = (MatrixIPort *)get_iport("Leadfield (nelecs x nelemsx3)");
-  measurements_iport_ = (MatrixIPort *)get_iport("Measurement Vector");
-  mesh_oport_ = (FieldOPort *)get_oport("Misfit Field");
-
-  if (!mesh_iport_) {
-    error("Unable to initialize iport 'FEM Mesh'.");
-    return;
+double BuildMisfitField::compute_misfit(double *b, double *bprime, int nr) {
+  double misfit;
+  double avg1=0, avg2=0;
+  int r;
+  for (r=0; r<nr; r++) {
+    avg1+=b[r];
+    avg2+=bprime[r];
   }
-  if (!leadfield_iport_) {
+  avg1/=nr;
+  avg2/=nr;
+  
+  double ccNum=0;
+  double ccDenom1=0;
+  double ccDenom2=0;
+  double rms=0;
+  
+  for (r=0; r<nr; r++) {
+    double shift1=(b[r]-avg1);
+    double shift2=(bprime[r]-avg2);
+    ccNum+=shift1*shift2;
+    ccDenom1+=shift1*shift1;
+    ccDenom2+=shift2*shift2;
+    double tmp=fabs(shift1-shift2);
+    if (last_pvalue_==1) rms+=tmp;
+    else if (last_pvalue_==2) rms+=tmp*tmp; 
+    else rms+=pow(tmp,last_pvalue_);
+  }
+
+  rms = pow(rms/nr,1/last_pvalue_);
+  double ccDenom=Sqrt(ccDenom1*ccDenom2);
+  double cc=Min(ccNum/ccDenom, 1000000.);
+  double ccInv=Min(1.0-fabs(ccNum/ccDenom), 1000000.);
+  double rmsRel=Min(rms/ccDenom1, 1000000.);
+  if (last_metric_ == "rms") {
+    return rms;
+  } else if (last_metric_ == "rmsRel") {
+    return rmsRel;
+  } else if (last_metric_ == "invCC") {
+    return ccInv;
+  } else if (last_metric_ == "CC") {
+    return cc;
+  } else {
+    cerr << "BuildMisfitField: error - unknown metric "<<last_metric_<<endl;
+    return 0;
+  }
+}
+
+void BuildMisfitField::execute() {
+  MatrixIPort* leadfield_iport_;
+  MatrixIPort* measurements_iport_;
+  MatrixOPort* misfit_oport_;
+
+  MatrixIPort *leadfield_iport = 
+    (MatrixIPort *)get_iport("Leadfield (nelecs x nelemsx3)");
+  MatrixIPort *measurements_iport = 
+    (MatrixIPort *)get_iport("Measurement Vector");
+  MatrixOPort *misfit_oport = 
+    (MatrixOPort *)get_oport("Misfit Vector");
+
+  if (!leadfield_iport) {
     error("Unable to initialize iport 'Leadfield (nelecs x nelemsx3)'.");
     return;
   }
-  if (!measurements_iport_) {
+  if (!measurements_iport) {
     error("Unable to initialize iport 'Measurement Vector'.");
     return;
   }
-  if (!mesh_oport_) {
-    error("Unable to initialize oport 'Misfit Field'.");
+  if (!misfit_oport) {
+    error("Unable to initialize oport 'Misfit Vector'.");
     return;
   }
-
-  FieldHandle mesh_in;
-  if (!mesh_iport_->get(mesh_in)) {
-    cerr << "BuildMisfitField -- couldn't get mesh.  Returning.\n";
-    return;
-  }
-  if (!mesh_in.get_rep() || mesh_in->get_type_name(0)!="TetVolField") {
-    cerr << "Error - BuildMisfitField didn't get a TetVolField for the mesh" << "\n";
-    return;
-  }
-  TetVolMesh* mesh = 
-    (TetVolMesh*)dynamic_cast<TetVolMesh*>(mesh_in->mesh().get_rep());
 
   MatrixHandle leadfield_in;
-  if (!leadfield_iport_->get(leadfield_in) || !leadfield_in.get_rep()) {
+  if (!leadfield_iport->get(leadfield_in) || !leadfield_in.get_rep()) {
     cerr << "BuildMisfitField -- couldn't get leadfield.  Returning.\n";
     return;
   }
@@ -138,7 +168,7 @@ void BuildMisfitField::execute() {
   }
 
   MatrixHandle measurements_in;
-  if (!measurements_iport_->get(measurements_in) || !measurements_in.get_rep()) {
+  if (!measurements_iport->get(measurements_in) || !measurements_in.get_rep()) {
     cerr << "BuildMisfitField -- couldn't get measurement vector.  Returning.\n";
     return;
   }
@@ -152,28 +182,20 @@ void BuildMisfitField::execute() {
     return;
   }
   int nr = cm->nrows();
-  TetVolMesh::Cell::size_type csize;  mesh->size(csize);
-  int ncells = csize;
-
-  if (ncells * 3 != dm->ncols()) {
-    cerr << "BuildMisfitField -- error, leadfield has "<<dm->ncols()<<" columns, and the mesh has "<<ncells<<" cells.\n";
-    return;
-  }
+  int nelems = dm->ncols()/3;
 
   string metric=metric_.get();
   double pvalue;
   string_to_double(pvalue_.get(), pvalue);
  
-  if (last_mesh_generation_ == mesh->generation &&
-      last_leadfield_generation_ == dm->generation &&
+  if (last_leadfield_generation_ == dm->generation &&
       last_measurements_generation_ == cm->generation &&
       last_metric_ == metric && last_pvalue_ == pvalue) {
     cerr << "BuildMisfitField -- sending same data again.\n";
-    mesh_oport_->send(last_mesh_);
+    misfit_oport->send(last_misfit_);
     return;
   }
 
-  last_mesh_generation_ = mesh->generation;
   last_leadfield_generation_ = dm->generation;
   last_measurements_generation_ = cm->generation;
   last_metric_ = metric;
@@ -182,12 +204,8 @@ void BuildMisfitField::execute() {
   double best_val;
   int best_idx;
 
-//  TetVolField<double> *tvd = scinew TetVolField<double>(TetVolMeshHandle(mesh), 
-//					      Field::CELL);
-  Array1<int> node_refs(ncells);
-  Array1<double> node_sums(ncells);
-  node_refs.initialize(0);
-  node_sums.initialize(0);
+  ColumnMatrix *misfit=new ColumnMatrix(nelems);
+  last_misfit_ = misfit;
 
   double *b = &((*cm)[0]);
   double *bprime = new double[nr];
@@ -197,72 +215,19 @@ void BuildMisfitField::execute() {
   for (c=0; c<3; c++)
     A[c] = new double[nr];
 
-  // ok data is valid, iterate through all of the triple columns
-  for (int i=0; i<ncells; i++) {
-//    if (i>0 && (i%100 == 0))
-//      cerr << i << "/" << ncells << "\n";
+  for (int i=0; i<nelems; i++) {
+    if (i%100 == 0) update_progress(i*1./nelems);
+
     for (r=0; r<nr; r++)
       for (c=0; c<3; c++)
 	A[c][r]=(*dm)[r][i*3+c];
 	
-    min_norm_least_sq_3(A, b, x, bprime, nr, 1);
-    double misfit;
-    double avg1=0, avg2=0;
-    int iterate;
-    for (iterate=0; iterate<nr; iterate++) {
-      avg1+=b[iterate];
-      avg2+=bprime[iterate];
-    }
-    avg1/=nr;
-    avg2/=nr;
-    
-    double ccNum=0;
-    double ccDenom1=0;
-    double ccDenom2=0;
-    double rms=0;
-    
-    for (iterate=0; iterate<nr; iterate++) {
-      double shift1=(b[iterate]-avg1);
-      double shift2=(bprime[iterate]-avg2);
-      
-      ccNum+=shift1*shift2;
-      ccDenom1+=shift1*shift1;
-      ccDenom2+=shift2*shift2;
-      //         double tmp=fabs((*ivec1)[iterate]-(*ivec2)[iterate]);
-      double tmp=fabs(shift1-shift2);
-      if (pvalue==1) rms+=tmp;
-      else if (pvalue==2) rms+=tmp*tmp; 
-      else rms+=pow(tmp,pvalue);
-    }
-    rms = pow(rms/nr,1/pvalue);
-    double ccDenom=Sqrt(ccDenom1*ccDenom2);
-    double cc=Min(ccNum/ccDenom, 1000000.);
-    double ccInv=Min(1.0-fabs(ccNum/ccDenom), 1000000.);
-    double rmsRel=Min(rms/ccDenom1, 1000000.);
-    if (metric == "rms") {
-      misfit=rms;
-    } else if (metric == "rmsRel") {
-      misfit=rmsRel;
-    } else if (metric == "invCC") {
-      misfit=ccInv;
-    } else if (metric == "CC") {
-      misfit=cc;
-    } else {
-      cerr << "BuildMisfitField: error - unknown metric "<<metric<<endl;
-      return;
-    }
+    min_norm_least_sq_3(A, b, x, bprime, nr);
 
-//    tvd->fdata()[i] = misfit;
-    TetVolMesh::Node::array_type::iterator ni;
-    TetVolMesh::Node::array_type na(4);
-    mesh->get_nodes(na,TetVolMesh::Cell::index_type(i));
-    for (ni = na.begin(); ni != na.end(); ++ni) {
-      node_refs[*ni]++;
-      node_sums[*ni]+=misfit;
-    }
-    
-    if (i==0 || misfit<best_val) {
-      best_val = misfit;
+    (*misfit)[i]=compute_misfit(b, bprime, nr);
+
+    if (i==0 || (*misfit)[i]<best_val) {
+      best_val = (*misfit)[i];
       best_idx = i;
     }
   }
@@ -273,15 +238,7 @@ void BuildMisfitField::execute() {
     delete[] A[c];
   }
 
-  // we only know how to isosurface when the data is at the nodes
-  TetVolField<double> *tvd =
-    scinew TetVolField<double>(TetVolMeshHandle(mesh), Field::NODE);
-  TetVolMesh::Node::size_type nsize;  mesh->size(nsize);
-  for (int i=0; i<nsize; i++) 
-    tvd->fdata()[i]=node_sums[i]/node_refs[i];
-
-  last_mesh_ = tvd;
-  mesh_oport_->send(last_mesh_);
+  misfit_oport->send(last_misfit_);
   cerr << "Best misfit was "<<best_val<<", which was cell "<<best_idx<<"\n";
 }
 } // End namespace BioPSE
