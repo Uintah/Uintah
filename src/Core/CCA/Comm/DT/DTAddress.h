32280fe4d5a3ff2667da4dbaeed89b5a11f0afc8
Keming Zhang <kzhang@sci.utah.edu>
2003-08-29 23:42:43 +0000

    add a Round Robin message scheduler. The packet size is set 100 for test.

--- src/Core/CCA/Comm/DT/DTAddress.h
+++ src/Core/CCA/Comm/DT/DTAddress.h
@@ -27,22 +27,80 @@
  *  Copyright (C) 1999 SCI Group
  */
 
-
 #ifndef CORE_CCA_COMM_DT_DTADDRESS_H
 #define CORE_CCA_COMM_DT_DTADDRESS_H
-
+#include <iostream>
 namespace SCIRun {
+  class DTPoint;
+
   class DTAddress{
   public:
     int port;
     long ip;
     bool operator<(const DTAddress &p) const{
-      return (port<=p.port && ip<p.ip) || (port<p.port && ip<=p.ip); 
+      return (ip<p.ip) || (ip==p.ip && port<p.port);
     }
+
     bool operator==(const DTAddress &p) const{
-      return port==p.port && ip==p.ip;
+      return ip==p.ip && port==p.port;
     }
   };
+
+
+  class DTDestination{
+  public:
+    DTAddress to_addr;
+    DTPoint *recver;
+
+    DTDestination(){}
+
+    DTDestination(DTPoint *recver, const DTAddress &to_addr){
+      this->recver=recver;
+      this->to_addr=to_addr;
+    }
+
+    void unset(){
+      recver=NULL;
+    }
+
+    bool isSet(){
+      return recver!=NULL;
+    }
+
+    bool operator<(const DTDestination &dest) const{
+      return (to_addr<dest.to_addr) || (to_addr==dest.to_addr && recver<dest.recver); 
+    }
+
+    bool operator==(const DTDestination &dest) const{
+      return (to_addr==dest.to_addr && recver==dest.recver);
+    }
+
+    void display(){
+      std::cerr<<"ip/port/recver="<<to_addr.ip<<"/"<<to_addr.port<<"/"<<recver<<std::endl;
+    }
+  };
+
+  class DTPacketID{
+  public:
+    DTDestination dest;
+    DTAddress fr_addr;
+
+    DTPacketID(){};
+
+    DTPacketID(const DTDestination &dest, const DTAddress &fr_addr){
+      this->dest=dest;
+      this->fr_addr=fr_addr;
+    }
+
+    bool operator<(const DTPacketID &pid) const{
+      return (dest<pid.dest) || (dest==pid.dest && fr_addr<pid.fr_addr); 
+    }
+
+    bool operator==(const DTPacketID &pid) const{
+      return (dest==pid.dest && fr_addr==pid.fr_addr);
+    }
+  };
+
 
 }// namespace SCIRun
 
--- src/Core/CCA/Comm/DT/DataTransmitter.h
+++ src/Core/CCA/Comm/DT/DataTransmitter.h
@@ -33,7 +33,7 @@
 
 
 #include <iostream>
-#include <deque>
+#include <vector>
 #include <map>
 #include <string>
 #include <Core/CCA/Comm/DT/DTAddress.h>
@@ -78,34 +78,44 @@ namespace SCIRun {
   private:
     void sendall(int sockfd, void *buf, int len);
     int recvall(int sockfd, void *buf, int len);
-    
+    void sendPacket(DTMessage *msg, int packetLen);    
 
-    std::deque<DTMessage*> send_msgQ;
-    std::deque<DTMessage*> recv_msgQ;
+    std::vector<DTMessage*> send_msgQ;
+    std::vector<DTMessage*> recv_msgQ;
+
+    typedef std::map<DTDestination, DTMessage*> RRMap;
+    RRMap send_msgMap;
+
+    typedef std::map<DTPacketID, DTMessage*> RVMap;
+    RVMap recv_msgMap;
+
+
     typedef std::map<DTPoint *, Semaphore *> SemaphoreMap;
     SemaphoreMap semamap;
 
-    static const int SEGMENT_LEN=1024*32;
     typedef std::map<DTAddress, int> SocketMap;
+
     SocketMap send_sockmap;
     SocketMap recv_sockmap;
 
     int sockfd; //listening socket
 
     char *hostname;
+
+    //This Data Tansmitter's address
     DTAddress addr;
 
     Mutex *sendQ_mutex;
     Mutex *recvQ_mutex;
+
     Mutex *send_sockmap_mutex;
     Mutex *recv_sockmap_mutex;
 
-    ConditionVariable *sendQ_cond;
-    ConditionVariable *recvQ_cond;
+    int newMsgCnt;
 
-    //number of TD threads have quit
-    //0: all are running, 3: all quit
-    //int nquit; 
+    ConditionVariable *sendQ_cond;
+
+    const static int PACKET_SIZE=100; //1024*32;
 
     bool quit;
   };
