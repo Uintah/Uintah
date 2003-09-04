9b9be3f8877ef176b0337a1cb329ff5209cbbcfa
Bryan Jeffrey Worthen <worthen@sci.utah.edu>
2003-09-04 02:00:59 +0000

    Catch the exceptions we are throwing Add one more axis to the dimension list (to compensate for taking the factor to -1 instead of 0 Add -div and -sub arguments

--- StandAlone/slb.cc
+++ StandAlone/slb.cc
@@ -17,6 +17,7 @@
 #include <Core/Malloc/Allocator.h>
 #include <iostream>
 #include <fstream>
+#include <string.h>
 
 // TODO:
 //  DONE Parse geometry from UPS file and fill in weights array properly
@@ -47,7 +48,7 @@ void bisect(const string& div, int num, int factor, Uintah::Primes::FactorType f
   else if(div[num] == 'z')
     index=2;
   else
-    throw InternalError("bad bisection axis: "+div);
+    throw InternalError("bad bisection axis: "+div[num]);
 
   if(factor == -1){
     static int idx = 0;
@@ -111,9 +112,10 @@ void usage( char *prog_name );
 
 void
 parseArgs( int argc, char *argv[], 
-           int & nump, float & weight, string & infile, string & outfile )
+           int & nump, float & weight, string & infile, string & outfile,
+           string & divisions, int & submultiples)
 {
-  if( argc != 5 ) {
+  if( argc < 4 || argc > 9 ) {
     usage( argv[0] );
   }
 
@@ -130,117 +132,167 @@ parseArgs( int argc, char *argv[],
 
   infile = argv[3];
   outfile = argv[4];
+
+  // parse optional arguments
+  for (int i = 5; i < argc; i++) {
+    if (strcmp(argv[i], "-div") == 0) {
+      i++;
+      if (i >= argc) {
+        cerr << "-div option needs an argument (i.e., -div xyz)\n";
+        exit( 1 );
+      }
+      for (unsigned int j = 0; j < strlen(argv[i]); j++) {
+        if (argv[i][j] != 'x' && argv[i][j] != 'y' && argv[i][j] != 'z') {
+          cerr << "Divisions string must be a combination of x, y, or z\n";
+          exit( 1 );
+        }
+      }
+      divisions = argv[i];
+    }
+    else if (strcmp(argv[i], "-sub") == 0) {
+      i++;
+      if (i >= argc) {
+        cerr << "-sub option needs an argument (i.e., -sub 2)\n";
+        exit( 1 );
+      }
+      submultiples = atoi(argv[i]);
+      if (submultiples < 1) {
+        cerr << "Number of Submultiples must be greater than 0\n";
+        exit( 1 );
+      }
+    }
+    else {
+      cerr << "Unknown option " << argv[i] << '\n';
+      exit( 1 );
+    }
+  }
 }
 
 void
 usage( char *prog_name )
 {
-  cout << "Usage: " << prog_name << " weight num_patches infile outfile\n";
+  cout << "Usage: " << prog_name << " weight num_patches infile outfile "
+       << "[-div <pattern>] [-sub <submult>] \n";
   cout << "    weight: 0.0 - 1.0.  0.0 => all cells, 1.0 => all particles\n";
   cout << "    num_patches: number of patches to divvy up domain into.\n";
   cout << "    infile:      .ups file to read in.\n";
   cout << "    outfile:     modified .ups file.\n";
+  cout << "    -div <pattern>:     OPTIONAL.  pattern is some combination\n"
+       << "        of x,y, and z, where the default is xyz\n";
+  cout << "    -sub <submult>:     divide original resolution by submult"
+       << "        for quicker estimation\n";
   exit( 1 );
 }
 
 int
 main(int argc, char *argv[])
 {
-  string divisions = "xyz";
-  Uintah::Parallel::determineIfRunningUnderMPI( argc, argv );
-  Uintah::Parallel::initializeManager( argc, argv, "" );
+  try {
+    Uintah::Parallel::determineIfRunningUnderMPI( argc, argv );
+    Uintah::Parallel::initializeManager( argc, argv, "" );
 
-  int    nump;
-  float  weight;
-  string infile;
-  string outfile;
-
-  parseArgs( argc, argv, nump, weight, infile, outfile );
-
-  ProblemSpecInterface* reader = scinew ProblemSpecReader(infile);
-
-  // Get the problem specification
-  ProblemSpecP ups = reader->readInputFile();
-  ups->writeMessages(true);
-  if(!ups)
+    int    nump;
+    float  weight;
+    string infile;
+    string outfile;
+    int submultiples = 1;
+    string divisions = "xyz";
+    
+    parseArgs( argc, argv, nump, weight, infile, outfile,
+               divisions, submultiples );
+    
+    ProblemSpecInterface* reader = scinew ProblemSpecReader(infile);
+    
+    // Get the problem specification
+    ProblemSpecP ups = reader->readInputFile();
+    ups->writeMessages(true);
+    if(!ups)
       throw ProblemSetupException("Cannot read problem specification");
-  
-  if(ups->getNodeName() != "Uintah_specification")
-    throw ProblemSetupException("Input file is not a Uintah specification");
-  
-  const ProcessorGroup* world = Uintah::Parallel::getRootProcessorGroup();
-
-  // Setup the initial grid
-  GridP grid=scinew Grid();
-  grid->problemSetup(ups, world);  
-
-  for (int l = 0; l < grid->numLevels(); l++) {
-    const LevelP &level = grid->getLevel(l);
     
-    IntVector low, high;
-    level->findCellIndexRange(low, high);
-    IntVector diff = high-low;
-    long cells = diff.x()*diff.y()*diff.z();
-    if(cells != level->totalCells())
-      throw ProblemSetupException("Currently slb can only handle square grids");
-
-    Uintah::Primes::FactorType factors;
-    int n = Uintah::Primes::factorize(nump, factors);
-    cerr << nump << ": ";
-    for(int i=0;i<n;i++){
-      cerr << factors[i] << " ";
-    }
-    cerr << '\n';
-
-    string div = divisions;
-    while(static_cast<int>(div.length()) < n)
-      div += divisions;
-
-    Array3<float> weights(low, high);
-    weights.initialize(weight);
-
-    // Parse the geometry from the UPS
-    ProblemSpecP mp = ups->findBlock("MaterialProperties");
-    ProblemSpecP mpm = mp->findBlock("MPM");
-    for (ProblemSpecP child = mpm->findBlock("material"); child != 0;
-         child = child->findNextBlock("material")) {
-      for (ProblemSpecP geom_obj_ps = child->findBlock("geom_object");
-           geom_obj_ps != 0;
-           geom_obj_ps = geom_obj_ps->findNextBlock("geom_object") ) {
-        vector<GeometryPiece*> pieces;
-        GeometryPieceFactory::create(geom_obj_ps, pieces);
-        
-        GeometryPiece* mainpiece;
-        if(pieces.size() == 0){
-          throw ProblemSetupException("No piece specified in geom_object");
-        } else if(pieces.size() > 1){
-          mainpiece = scinew UnionGeometryPiece(pieces);
-        } else {
-          mainpiece = pieces[0];
-        }
-
-        for(CellIterator iter(low, high); !iter.done(); iter++){
-          Point p = level->getCellPosition(*iter);
-          if(mainpiece->inside(p))
-            weights[*iter] = 1;
-        }
-
-        delete mainpiece;
+    if(ups->getNodeName() != "Uintah_specification")
+      throw ProblemSetupException("Input file is not a Uintah specification");
+    
+    const ProcessorGroup* world = Uintah::Parallel::getRootProcessorGroup();
+    
+    // Setup the initial grid
+    GridP grid=scinew Grid();
+    grid->problemSetup(ups, world);  
+    
+    for (int l = 0; l < grid->numLevels(); l++) {
+      const LevelP &level = grid->getLevel(l);
+      
+      IntVector low, high;
+      level->findCellIndexRange(low, high);
+      IntVector diff = high-low;
+      long cells = diff.x()*diff.y()*diff.z();
+      if(cells != level->totalCells())
+        throw ProblemSetupException("Currently slb can only handle square grids");
+      
+      Uintah::Primes::FactorType factors;
+      int n = Uintah::Primes::factorize(nump, factors);
+      cerr << nump << ": ";
+      for(int i=0;i<n;i++){
+        cerr << factors[i] << " ";
       }
+      cerr << '\n';
+      
+      string div = divisions;
+      while(static_cast<int>(div.length()) <= n)
+        div += divisions;
+      
+      Array3<float> weights(low, high);
+      weights.initialize(weight);
+      
+      // Parse the geometry from the UPS
+      ProblemSpecP mp = ups->findBlock("MaterialProperties");
+      ProblemSpecP mpm = mp->findBlock("MPM");
+      for (ProblemSpecP child = mpm->findBlock("material"); child != 0;
+           child = child->findNextBlock("material")) {
+        for (ProblemSpecP geom_obj_ps = child->findBlock("geom_object");
+             geom_obj_ps != 0;
+             geom_obj_ps = geom_obj_ps->findNextBlock("geom_object") ) {
+          vector<GeometryPiece*> pieces;
+          GeometryPieceFactory::create(geom_obj_ps, pieces);
+          
+          GeometryPiece* mainpiece;
+          if(pieces.size() == 0){
+            throw ProblemSetupException("No piece specified in geom_object");
+          } else if(pieces.size() > 1){
+            mainpiece = scinew UnionGeometryPiece(pieces);
+          } else {
+            mainpiece = pieces[0];
+          }
+          
+          for(CellIterator iter(low, high); !iter.done(); iter++){
+            Point p = level->getCellPosition(*iter);
+            if(mainpiece->inside(p))
+              weights[*iter] = 1;
+          }
+          
+          delete mainpiece;
+        }
+      }
+      int factor = n-1;
+      
+      // remove the 'Box' entry from the ups
+      ProblemSpecP g = ups->findBlock("Grid");
+      ProblemSpecP l = g->findBlock("Level");
+      ProblemSpecP box = l->findBlock("Box");
+      
+      l->removeChild(box);
+      
+      bisect(div, 0, factor, factors, low, high, weights, level, l); 
     }
-    int factor = n-1;
     
-    // remove the 'Box' entry from the ups
-    ProblemSpecP g = ups->findBlock("Grid");
-    ProblemSpecP l = g->findBlock("Level");
-    ProblemSpecP box = l->findBlock("Box");
-
-    l->removeChild(box);
-
-    bisect(div, 0, factor, factors, low, high, weights, level, l); 
+    ofstream out(outfile.c_str());
+    out << ups;
+  } catch (Exception& e) {
+    cerr << "Caught exception: " << e.message() << '\n';
+    if(e.stackTrace())
+      cerr << "Stack trace: " << e.stackTrace() << '\n';
+  } catch (std::exception e){
+    cerr << "Caught std exception: " << e.what() << '\n';
+  } catch(...){
+    cerr << "Caught unknown exception\n";
   }
-
-  ofstream out(outfile.c_str());
-  out << ups;
-
 }
