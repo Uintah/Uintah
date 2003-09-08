366684510d4ec54513031c1dc41fa286c1d0f43e
Seshadri Kumar <skumar@sci.utah.edu>
2003-09-08 05:48:35 +0000

    1. Created d_stationarySolid option to make stationary object    code more efficient 2. More support for cut cells; can now read input cutcell geometry    for p-cells; d_useCutCell logic switch added (needs to be    finished); added more labels for cutcells 3. Rearranged a bunch of code 4. Changed solid_fraction definition; now based on fraction of    *solid* volume 5. Added read_complex_geometry to sub.mk (forgot to add the file    itself :-)

--- CCA/Components/MPMArches/fortran/sub.mk
+++ CCA/Components/MPMArches/fortran/sub.mk
@@ -10,6 +10,7 @@ SRCS += $(SRCDIR)/collect_drag_cc.F \
 	$(SRCDIR)/interp_centertoface.F \
 	$(SRCDIR)/momentum_exchange_term_continuous_cc.F \
 	$(SRCDIR)/pressure_force.F \
+	$(SRCDIR)/read_complex_geometry.F \
 	$(SRCDIR)/taucal_cc.F \
 	$(SRCDIR)/walmom_cc.F
 
@@ -21,5 +22,6 @@ $(SRCDIR)/energy_exchange_term.o: $(SRCDIR)/energy_exchange_term_fort.h
 $(SRCDIR)/interp_centertoface.o: $(SRCDIR)/interp_centertoface_fort.h
 $(SRCDIR)/momentum_exchange_term_continuous_cc.o: $(SRCDIR)/momentum_exchange_term_continuous_cc_fort.h
 $(SRCDIR)/pressure_force.o: $(SRCDIR)/pressure_force_fort.h
+$(SRCDIR)/read_complex_geometry.o: $(SRCDIR)/read_complex_geometry_fort.h
 $(SRCDIR)/taucal_cc.o: $(SRCDIR)/taucal_cc_fort.h
 $(SRCDIR)/walmom_cc.o: $(SRCDIR)/walmom_cc_fort.h
