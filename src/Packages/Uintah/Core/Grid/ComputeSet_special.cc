6a9dfa5dd79decedec8ee439922ad367eca1aa98
Bryan Jeffrey Worthen <worthen@sci.utah.edu>
2003-10-15 19:31:41 +0000

    Include the specialized template functions in the .h file if we use PGI compilers, as they use a dofferent template model

--- src/Packages/Uintah/Core/Grid/ComputeSet_special.cc
+++ src/Packages/Uintah/Core/Grid/ComputeSet_special.cc
@@ -1,3 +1,6 @@
+#ifndef Uintah_Core_Grid_ComputeSet_special_cc
+#define Uintah_Core_Grid_ComputeSet_special_cc
+
 #include <Packages/Uintah/Core/Grid/ComputeSet.h>
 #include <Packages/Uintah/Core/Grid/Patch.h>
 #include <algorithm>
@@ -15,3 +18,8 @@ bool ComputeSubset<const Patch*>::compareElems(const Patch* e1,
 { return Patch::Compare()(e1, e2); }
   
 }
+
+#endif // #ifdef Uintah_Core_Grid_ComputeSet_special_cc
+       // this file needs to be included to instantiate the templates
+       // for some compilers
+
