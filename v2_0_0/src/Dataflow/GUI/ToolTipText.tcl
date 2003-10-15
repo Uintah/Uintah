f4051f7ee471fb7512b1ae10ae9164efc4ec13e4
J. Davison de St. Germain <dav@sci.utah.edu>
2003-10-15 21:34:19 +0000

    Added some more info.  Renamed HelpText to ToolTipText.

--- v2_0_0/src/Dataflow/GUI/ToolTipText.tcl
+++ v2_0_0/src/Dataflow/GUI/ToolTipText.tcl
@@ -1,24 +1,35 @@
 
+# This file contains the loadToolTipText function which just fills the
+# ToolTipText array with tool tips for each of the main GUI components.
 
-proc setupHelpText {} {
+proc loadToolTipText {} {
 
-    global HelpText
-    set HelpText(Module) "L - Select\nR - Menu"
-    set HelpText(Connection) "L - Highlight\nCTRL-M - Delete\nR - Menu"
-    set HelpText(Notes) "L - Edit\nM - Hide"
-    
-    set HelpText(FileMenu) [subst {\
-       Save:               \n\
-       Save As...:         \n\
-       Load...             \n\
-       Clear:              Removes (destroys) all modules from the network editory canvas.\n\
-       Save Postscript...: \n\
-       Execute All:        \n\
-       New:                \n\
-       Add Info...:        \n\
-       Quit:               } ]
+    global ToolTipText
+
+    # Module Help texts
+    set ToolTipText(Module) "L - Select\nR - Menu"
+    set ToolTipText(ModuleUI) "L - Open (or raise) GUI"
+    set ToolTipText(ModuleMessageBtn) "L - Open (or raise) Module Log"
+    set ToolTipText(ModuleTime) "Displays the amount of CPU time spent on this module."
+    set ToolTipText(ModuleProgress) "Displays the progress of this module towards completion."
+    set ToolTipText(ModulePort) "L - Highlight\nM - Display port name"
+
+    set ToolTipText(Connection) "L - Highlight\nCTRL-M - Delete\nR - Menu"
+    set ToolTipText(Notes) "L - Edit\nM - Hide"
+
+    set ToolTipText(FileMenu) [subst {\
+       Save                Save the current dataflow network.\n\
+       Save As...          Save the current dataflow network under a new name.\n\
+       Load...             Loads a new network.  The current network will be lost!\n\
+       Insert...           Inserts a new network with your current network.\n\
+       Clear               Removes (destroys) all modules from the network editory canvas.\n\
+       Execute All         Tells the dataflow network to execute all modules.\n\
+       New                 Allows the user to create a new module.  (Mostly for code developers!)\n\
+       Add Info...         Allows the user to annotate the current dataflow network.\n\
+       Quit                Quits SCIRun.} ]
 }
 
   
+
 
 
