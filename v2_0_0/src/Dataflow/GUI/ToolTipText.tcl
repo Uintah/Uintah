06b2f61b4ff6e6a7f047a0b45bb1f12116f882ca
J. Davison de St. Germain <dav@sci.utah.edu>
2003-10-16 20:25:15 +0000

    More tool tip text

--- v2_0_0/src/Dataflow/GUI/ToolTipText.tcl
+++ v2_0_0/src/Dataflow/GUI/ToolTipText.tcl
@@ -9,24 +9,33 @@ proc loadToolTipText {} {
     # Module Help texts
     set ToolTipText(Module) "L - Select\nR - Menu"
     set ToolTipText(ModuleUI) "L - Open (or raise) GUI"
-    set ToolTipText(ModuleMessageBtn) "L - Open (or raise) Module Log"
+    set ToolTipText(ModuleMessageBtn) "L - Open (or raise) Module Log.\nColor code: Gray -> No Message. Blue -> Informational Message.  Yellow -> Warning Message.  Red -> Error Message."
     set ToolTipText(ModuleTime) "Displays the amount of CPU time spent on this module."
     set ToolTipText(ModuleProgress) "Displays the progress of this module towards completion."
-    set ToolTipText(ModulePort) "L - Highlight\nM - Display port name"
+    set ToolTipText(ModulePort) "L - Highlight pipe(s) from this port (if a pipe exists).\nM - Connect port to another module (if an open port exists.)  Also displays the port's name."
+    set ToolTipText(ModulePortlight) "This 'Portlight' tells the status of the port.  Black -> Off.  Red -> On.\nL - Highlight pipe(s) from this port (if a pipe exists).\nM - Connect port to another module (if an open port exists.)  Also displays the port's name."
+    set ToolTipText(ModuleSubnetBtn) "L - Bring up SubNet editor."
 
     set ToolTipText(Connection) "L - Highlight\nCTRL-M - Delete\nR - Menu"
     set ToolTipText(Notes) "L - Edit\nM - Hide"
 
     set ToolTipText(FileMenu) [subst {\
-       Save                Save the current dataflow network.\n\
-       Save As...          Save the current dataflow network under a new name.\n\
-       Load...             Loads a new network.  The current network will be lost!\n\
-       Insert...           Inserts a new network with your current network.\n\
-       Clear               Removes (destroys) all modules from the network editory canvas.\n\
-       Execute All         Tells the dataflow network to execute all modules.\n\
-       New                 Allows the user to create a new module.  (Mostly for code developers!)\n\
-       Add Info...         Allows the user to annotate the current dataflow network.\n\
-       Quit                Quits SCIRun.} ]
+       Save             \tSave the current dataflow network.\n\
+       Save As...       \tSave the current dataflow network under a new name.\n\
+       Load...          \tLoads a new network.  The current network will be lost!\n\
+       Insert...        \tInserts a new network with your current network.\n\
+       Clear            \tRemoves (destroys) all modules from the network editory canvas.\n\
+       Execute All      \tTells the dataflow network to execute all modules.\n\
+       New              \tAllows the user to create a new module.  (Mostly for code developers!)\n\
+       Add Info...      \tAllows the user to annotate the current dataflow network.\n\
+       Quit             \tQuits SCIRun.} ]
+
+    set ToolTipText(HelpMenu) [subst {\
+       Show Tooltips    \tToggle on/off tool tips.\n\
+       About...         \tSCIRun splash screen.\n\
+       License...       \tDisplays SCIRun's User License.} ]
+
+    set ToolTipText(PackageMenus) "This menu allows you to create modules from this Package.  The modules are grouped by category."
 }
 
   
