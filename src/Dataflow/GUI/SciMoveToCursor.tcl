ee0f3fe5a3d40d92264b4c1201dbb3b0fdc40c6d
J. Davison de St. Germain <dav@sci.utah.edu>
2003-10-26 19:56:34 +0000

    Attempt to keep windows on screen

--- src/Dataflow/GUI/SciMoveToCursor.tcl
+++ src/Dataflow/GUI/SciMoveToCursor.tcl
@@ -7,10 +7,39 @@
 #   Moves the given "window" to near the cursor location.
 #
 
+set screenWidth [winfo screenwidth .]
+set screenHeight [winfo screenheight .]
+
 proc moveToCursor { window } {
 
-  set mouseXLoc [expr [winfo pointerx .] - 70]
-  set mouseYLoc [expr [winfo pointery .] - 70]
+  global screenHeight screenWidth
 
-  wm geometry $window +$mouseXLoc+$mouseYLoc
+  set cursorXLoc [expr [winfo pointerx .]]
+  set cursorYLoc [expr [winfo pointery .]]
+
+  # Neither commands "winfo width $window", or "winfo reqwidth
+  # $window" return the windows (soon to be) width.  (Ie: the window
+  # has not yet been "realized", and has a width of '1'.  Therefore I
+  # am just goin to assume GUI window size of 300x300 for now.
+  # This works fairly well for all but really big windows.
+  set guiWidth 300
+  set guiHeight 300
+
+  if { $cursorXLoc < 100 } {
+      set windowXLoc [expr $cursorXLoc / 2]
+  } elseif { $cursorXLoc > ($screenWidth - $guiWidth) } {
+      set windowXLoc [expr $screenWidth - $guiWidth - 20]
+  } else {
+      set windowXLoc [expr $cursorXLoc - 80]
+  }
+
+  if { $cursorYLoc < 100 } {
+      set windowYLoc [expr $cursorYLoc / 2]
+  } elseif { $cursorYLoc > ($screenHeight - $guiHeight) } {
+      set windowYLoc [expr $screenHeight - $guiHeight - 20]
+  } else {
+      set windowYLoc [expr $cursorYLoc - 80]
+  }
+
+  wm geometry $window +$windowXLoc+$windowYLoc
 }
