25049db1ad508840924db612190f14723dc19b71
Darby Brown <darbyb@sci.utah.edu>
2003-11-15 01:13:23 +0000

    * Now saves out all filenames that have been   loaded.

--- v2_0_0/src/Packages/Teem/Dataflow/GUI/AnalyzeToNrrd.tcl
+++ v2_0_0/src/Packages/Teem/Dataflow/GUI/AnalyzeToNrrd.tcl
@@ -26,9 +26,15 @@ itcl_class Teem_DataIO_AnalyzeToNrrd {
 	global $this-file
         global $this-file-del
         global $this-messages
+	global $this-num-files
+	global $this-max-files
+	global $this-filenames
 	set $this-file ""
         set $this-file-del ""
         set $this-messages ""
+	set $this-num-files 0
+	set $this-max-files 0
+	set $this-filenames ""
     }
 
     method ui {} {
@@ -65,10 +71,13 @@ itcl_class Teem_DataIO_AnalyzeToNrrd {
 	# Commented this out because the ui
 	# would no longer popup in BioTensor
 	# moveToCursor $w
+
+	sync_filenames
     }
 
 
     method ChooseFile { } {
+	global env
         #set w .ui[modname]
 	set w [format "%s-fb" .ui[modname]]
 
@@ -85,6 +94,19 @@ itcl_class Teem_DataIO_AnalyzeToNrrd {
 	toplevel $w -class TkFDialog
 
 	set defext ".hdr"
+
+	set initdir ""
+	
+	# place to put preferred data directory
+	# it's used if $this-filename is empty
+	
+	if {[info exists env(SCIRUN_DATA)]} {
+	    set initdir $env(SCIRUN_DATA)
+	} elseif {[info exists env(SCI_DATA)]} {
+	    set initdir $env(SCI_DATA)
+	} elseif {[info exists env(PSE_DATA)]} {
+	    set initdir $env(PSE_DATA)
+	}
 	
 	# File types to appers in filter box
 	set types {
@@ -103,6 +125,7 @@ itcl_class Teem_DataIO_AnalyzeToNrrd {
 	    -cancel "wm withdraw $w" \
 	    -title "Open Analyze File" \
 	    -filetypes $types \
+	    -initialdir $initdir \
 	    -defaultextension $defext \
 	    -command "wm withdraw $w; $this AddData" \
 
@@ -135,9 +158,27 @@ itcl_class Teem_DataIO_AnalyzeToNrrd {
                     }  
                 }
 
+		# initialize a filename variable and set it to the
+		# current file
+		global $this-num-files
+		global $this-max-files
+		# Only make a new variable if the max number of files
+		# that have been created is == the num-files
+		if {[set $this-num-files] == [set $this-max-files]} {
+		    global $this-filenames[set $this-num-files]
+		    set $this-filenames[set $this-num-files] [set $this-file]
+
+		    set $this-max-files [expr [set $this-max-files] + 1]
+		}
+
+		# increment num-files
+		set $this-num-files [expr [set $this-num-files] + 1]
+		
+
                 # Call the c++ function that adds this data to its data 
                 # structure.
-                $this-c add_data
+		global $this-file
+                $this-c add_data [set $this-file]
            
                 # Now add entry to selected data
                 $selected.list insert end [set $this-file]
@@ -153,14 +194,27 @@ itcl_class Teem_DataIO_AnalyzeToNrrd {
 	    set sd [$w.sd childsite]
 	    set selected $sd.selected
 
+	    global $this-num-files
             # Get the current cursor selection
             foreach i [$selected.list curselection] {
                 set $this-file-del [$selected.list get $i] 
                 $selected.list delete $i $i
 
+		# re-order and remove selected file
+		for {set x $i} {$x < [expr [set $this-num-files]-1]} {incr x} {
+		    global $this-filenames$x
+		    set next [expr $x +1]
+		    global $this-filenames$next
+		    set $this-filenames$x [set $this-filenames$next]
+		}
+
+		# decrement num-files
+		set $this-num-files [expr [set $this-num-files] - 1]
+
                 # Call the c++ function that deletes this data from its data 
                 # structure.
                 $this-c delete_data
+
             }
 
 	}
@@ -200,5 +254,52 @@ itcl_class Teem_DataIO_AnalyzeToNrrd {
             $dst insert end [$src get $i]
         }
 
+    }
+
+    method sync_filenames {} {
+	set w .ui[modname]
+
+	global $this-num-files
+
+	if {![winfo exists $w.sd]} {
+	    $this ui
+	    wm withdraw $w
+	}
+
+	set sd [$w.sd childsite]
+	set selected $sd.selected
+	
+	# make sure num-files corresponds to
+	# the number of files in the selection box
+	if {[set $this-num-files] != [$selected.list size]} {
+	    global $this-file
+
+	    # Make sure all filenames are in the
+	    # selected listbox
+	    
+	    # delete all of them
+	    $selected.list delete 0 end
+	    $this-c clear_data
+	    
+	    set num [set $this-num-files]
+	    set $this-num-files 0
+	    
+	    # add back in
+	    for {set i 0} {$i < $num} {incr i} {
+# 		if {[info exists $this-filenames$i]} {
+# 		    # Creat them
+# 		    set temp [set $this-filenames$i]
+# 		    unset $this-filenames$i
+# 		    set $this-file $temp
+# 		    global $this-filenames$i
+# 		    set $this-filenames$i [set $this-file]
+# 		    $this AddData
+# 		}
+		global $this-filenames$i
+		set $this-file [set $this-filenames$i]
+		$this AddData
+		
+	    }
+	} 
     }
 }
