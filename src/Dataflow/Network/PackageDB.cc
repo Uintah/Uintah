45061d36c12893dbfa2825f9392881b9862794b0
J. Davison de St. Germain <dav@sci.utah.edu>
2003-12-18 21:13:41 +0000

    Branch commit: Apple hack not necessary any longer.

--- src/Dataflow/Network/PackageDB.cc
+++ src/Dataflow/Network/PackageDB.cc
@@ -221,11 +221,11 @@ void PackageDB::loadPackage(bool resolve)
   else
     cerr << "Loading packages...\n";
 
-#ifdef __APPLE__
-  // A hack around a gcc (apple) init bug
-  GetLibrarySymbolAddress( "lib/libCore_Datatypes.dylib",
-			   "__ZN6SCIRun10CurveFieldINS_6TensorEE2ioERNS_9PiostreamE");
-#endif
+  //#ifdef __APPLE__
+  //  // A hack around a gcc (apple) init bug
+  //  GetLibrarySymbolAddress( "lib/libCore_Datatypes.dylib",
+  //			   "__ZN6SCIRun10CurveFieldINS_6TensorEE2ioERNS_9PiostreamE");
+  //#endif
 
   // the format of PACKAGE_PATH is a colon seperated list of paths to the
   // root(s) of package source trees.
