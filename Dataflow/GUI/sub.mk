13187a55a4cf314dc1fbc2d9c24edebacb1ab097
J. Davison de St. Germain <dav@sci.utah.edu>
2003-12-18 23:17:10 +0000

    Added GuiPreferences.tcl

--- Dataflow/GUI/sub.mk
+++ Dataflow/GUI/sub.mk
@@ -69,6 +69,7 @@ SRCS := \
 	$(SRCDIR)/GenStandardColorMaps.tcl \
 	$(SRCDIR)/GenTransferFunc.tcl \
 	$(SRCDIR)/GLTextureBuilder.tcl \
+	$(SRCDIR)/GuiPreferences.tcl \
 	$(SRCDIR)/ToolTipText.tcl \
 	$(SRCDIR)/Isosurface.tcl \
 	$(SRCDIR)/IsoClip.tcl \
