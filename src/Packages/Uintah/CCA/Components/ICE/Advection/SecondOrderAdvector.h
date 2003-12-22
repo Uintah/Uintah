632cf9e6c3e7f8f04ddf4584a5f81f0c9fa52037
Todd Harman <harman@sci.utah.edu>
2003-12-22 03:50:55 +0000

    Performance related.   -changed gradientLimiter(), q_CCMaxMin(), q_vertexMaxMin() and gradQ() to inline    functions that perform pointwise operations.

--- src/Packages/Uintah/CCA/Components/ICE/Advection/SecondOrderAdvector.h
+++ src/Packages/Uintah/CCA/Components/ICE/Advection/SecondOrderAdvector.h
@@ -59,16 +59,7 @@ namespace Uintah {
     template<class T> 
       void qAverageFlux(const CCVariable<T>& q_CC,
 		          const Patch* patch,
-		          CCVariable<T>& grad_lim,
-		          const CCVariable<T>& q_grad_x,
-		          const CCVariable<T>& q_grad_y,
-		          const CCVariable<T>& q_grad_z,
-		          CCVariable<facedata<T> >& q_OAFS);
-    template <class T>
-      void  allocateAndCompute_Q_ave(const CCVariable<T>& q_CC,
-                                     const Patch* patch,
-                                     DataWarehouse* new_dw,
-                                     CCVariable<facedata<T> >& q_OAFS );
+                        CCVariable<facedata<T> >& q_OAFS);
                                          
     template<class T, typename F> 
       void advectSlabs(CCVariable<facedata<T> >& q_OAFS,
