f659671388d205ee0ca36964994b5d57f64e4b2d
James Bigler <bigler@sci.utah.edu>
2004-01-05 21:01:00 +0000

    Added the ability to do silhouette edges.

--- Packages/rtrt/StandAlone/scenes/sketch.cc
+++ Packages/rtrt/StandAlone/scenes/sketch.cc
@@ -36,12 +36,17 @@ using SCIRun::Thread;
 extern "C" 
 Scene* make_scene(int argc, char* argv[], int nworkers)
 {
+  char *me = argv[0];
+  char *errS;
     int depth=3;
     Array1<char*> files;
     bool cut=false;
 #ifdef GROUP_IN_TIMEOBJ
     double rate=3;
 #endif
+    Array1<char*> sil_filenames;
+    Array1<Material*> gui_materials;
+    
     for(int i=1;i<argc;i++){
 	if(strcmp(argv[i], "-depth")==0){
 	    i++;
@@ -52,6 +57,8 @@ Scene* make_scene(int argc, char* argv[], int nworkers)
 	} else if(strcmp(argv[i], "-rate")==0){
 	  rate = atof(argv[++i]);
 #endif
+	} else if(strcmp(argv[i], "-sil")==0){
+	  sil_filenames.add(argv[++i]);
 	} else if(argv[i][0] != '-'){
 	    files.add(argv[i]);
 	} else {
@@ -66,6 +73,10 @@ Scene* make_scene(int argc, char* argv[], int nworkers)
     if(files.size()==0){
 	cerr << "Must specify at least one file\n";
 	return 0;
+    }
+    if(files.size() != sil_filenames.size()) {
+      cerr << "Must have the same number of files as silhouette files\n";
+      return 0;
     }
 
     Camera cam(Point(1,3,1), Point(0.5,0.5,0.5), Vector(0,1,0), 40);
@@ -82,8 +93,48 @@ Scene* make_scene(int argc, char* argv[], int nworkers)
 	// Now to add a new material
 	BBox bbox;
 	hvol->compute_bounds(bbox, 0);
-	Material *sm = new SketchMaterial<BrickArray3<short>, short>(hvol->blockdata, bbox);
+	// Load in the silhouette transfer function for use in the texture.
+	Nrrd *nrrdsil = nrrdNew();
+	if (nrrdLoad(nrrdsil, sil_filenames[0],0)) {
+	  fprintf(stderr, "%s: problem with gagePerVolumeAttach:\n%s\n",
+		  me, errS = biffGetDone(GAGE));
+	  free(errS);
+	  return 0;
+	}
+	// Now copy the data over to an Array2
+	if (nrrdsil->dim == 3) {
+	  // Need to project the data to only two dimensions.  The
+	  // first one is color, so we can just take the average of
+	  // it.  We also need to make sure they are floats.
+	  Nrrd *project = nrrdNew();
+	  nrrdProject(project, nrrdsil, 0, nrrdMeasureMean, nrrdTypeFloat);
+	  nrrdNuke(nrrdsil);
+	  nrrdsil = project;
+	} else if (nrrdsil->dim != 2) {
+	  cerr << me << ":Don't know how to deal with a nrrd of dim "<<nrrdsil->dim << "\n";
+	  return 0;
+	}
+	if (nrrdsil->type != nrrdTypeFloat) {
+	  Nrrd *new_nrrd = nrrdNew();
+	  nrrdConvert(new_nrrd, nrrdsil, nrrdTypeFloat);
+	  // since the data was copied blow away the memory for the old nrrd
+	  nrrdNuke(nrrdsil);
+	  nrrdsil = new_nrrd;
+	}
+	int silx = nrrdsil->axis[0].size;
+	int sily = nrrdsil->axis[1].size;
+	Array2<float> sil_trans(silx,sily);
+	float *data = (float*)(nrrdsil->data);
+	for(int y = 0; y < sily; y++)
+	  for(int x = 0; x < silx; x++)
+	    {
+	      sil_trans(x,y) = *data;
+	      data++;
+	    }
+	
+	Material *sm = new SketchMaterial<BrickArray3<short>, short>(hvol->blockdata, bbox, sil_trans, nrrdsil->axis[0].max);
 	hvol->set_matl(sm);
+	gui_materials.add(sm);
 #endif
 	obj = hvol;
     } else {
@@ -122,7 +173,9 @@ Scene* make_scene(int argc, char* argv[], int nworkers)
     Scene* scene=new Scene(obj, cam,
 			   bgcolor, cdown, cup, groundplane,
 			   ambient_scale);
-    scene->addObjectOfInterest(obj, true);
+    scene->addAnimateObject(obj);
+    for(int i = 0; i < gui_materials.size(); i++)
+      scene->addGuiMaterial(gui_materials[i]);
     //scene->add_light(new Light(Point(50,-30,30), Color(1.0,0.8,0.2), 0));
     Light *light0 = new Light(Point(1100,-600,3000), Color(1.0,1.0,1.0), 0);
     light0->name_ = "light 0";
