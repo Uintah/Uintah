6a56e40b45f3c8a61f6a02f338d42d12d674a37f
J. Davison de St. Germain <dav@sci.utah.edu>
2004-01-08 19:58:00 +0000

    Use /bin/sh as the scripting language for this script.  Clean it up and print usage if miss used.

--- Packages/MatlabInterface/matlab/engine/mlabengine.bat
+++ Packages/MatlabInterface/matlab/engine/mlabengine.bat
@@ -1,14 +1,16 @@
+#! /bin/sh
 
-if [ $1 = "stop" ]
-then
- echo STOP the matlab engine
+if test "$1" = "stop"; then
+ echo 
+ echo "Stopping the matlab engine.  THIS MAY TAKE A FEW SECONDS"
  echo 'mlabengine(5,[],'\''stop'\'');' | matlab -nosplash &
-fi
-
-if [ $1 = "start" ]
-then
- echo START the matlab engine
+elif test "$1" = "start"; then
+ echo "Starting the matlab engine.  THIS WILL TAKE A FEW SECONDS"
+ echo "  You can continue to use this shell, but Matlab output will"
+ echo "  periodically appear in this window."
  echo 'mlabengine(5,'\''127.0.0.1:5517'\'');' | matlab -nosplash &
+else
+ echo "Usage: $0 <start | stop>"
 fi
 
 # to check the system from *.c file
