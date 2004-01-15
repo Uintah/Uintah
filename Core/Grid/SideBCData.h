f7a62d9133473490aac3bb8f366c3adb89a15277
John Schmidt <jas@sci.utah.edu>
2004-01-15 20:13:48 +0000

    Implement computation of iterator bounds for face centered variables. Moved cell and node centered boundary iterator computation to individual boundary condition geometry pieces.

--- Core/Grid/SideBCData.h
+++ Core/Grid/SideBCData.h
@@ -6,62 +6,72 @@
 #include <Core/Geometry/Vector.h>
 #include <Core/Geometry/IntVector.h>
 #include <Packages/Uintah/Core/ProblemSpec/ProblemSpecP.h>
+#include <Packages/Uintah/Core/Grid/BCDataArray.h>
 #include <sgi_stl_warnings_off.h>
 #include <vector>
 #include <sgi_stl_warnings_on.h>
 
 namespace Uintah {
-using namespace SCIRun;
- using std::vector;
 
-/**************************************
+  /*!
 
-CLASS
-   SideBCData
-   
-   
-GENERAL INFORMATION
-
-   SideBCData.h
-
-   John A. Schmidt
-   Department of Mechanical Engineering
-   University of Utah
-
-   Center for the Simulation of Accidental Fires and Explosions (C-SAFE)
+  \class SideBCData
   
-   Copyright (C) 2000 SCI Group
-
-KEYWORDS
-   SideBCData
-
-DESCRIPTION
-   Long description...
+  \ brief Defines a boundary condition geometry for the entire side of the
+  domain.
   
-WARNING
+  \author John A. Schmidt \n
+  Department of Mechanical Engineering \n
+  University of Utah \n
+  Center for the Simulation of Accidental Fires and Explosions (C-SAFE) \n\n
+
+  */
+
+  using namespace SCIRun;
+  using std::vector;
+ 
+  class SideBCData : public BCGeomBase {
+
+  public:
+    /// Constructor
+    SideBCData();
+
+    /// Constructor with boundary condition data.
+    SideBCData(BCData& d_bc);
+
+    /// Destructor
+    virtual ~SideBCData();
+
+    /// Clone the boundary condition geometry -- allocates memory.
+    SideBCData* clone();
+
+    /// Get the boundary condition data
+    void getBCData(BCData& bc) const;
+
+    /// Add the boundary condition data
+    void addBCData(BCData& bc);
+
+    /// Add the old boundary condition data -- no longer used.
+    void addBC(BoundCondBase* bc);
+
+    /// Determines if a point is inside -- always returns true.
+    bool inside(const Point& p) const;
+
+    /// Print out the boundary condition geometry type.
+    virtual void print();
+
+    /// Determine the cell and node centered iterators
+    virtual void determineIteratorLimits(Patch::FaceType face,
+					 const Patch* patch, 
+					 vector<Point>& test_pts);
+
+    /// Determine the face centered iterators
+    virtual void determineSFLimits(Patch::FaceType face, const Patch* patch);
+    
+  private:
+    BCData d_bc;
+  };
   
-****************************************/
-
-   class SideBCData : public BCGeomBase {
-   public:
-     SideBCData();
-     SideBCData(BCData& d_bc);
-#if 0
-     SideBCData(const SideBCData& bc);
-     SideBCData& operator=(const SideBCData& bc);
-#endif
-     virtual ~SideBCData();
-     SideBCData* clone();
-     void getBCData(BCData& bc) const;
-     void addBCData(BCData& bc);
-     void addBC(BoundCondBase* bc);
-     bool inside(const Point& p) const;
-     virtual void print();
-     
-   private:
-     BCData d_bc;
-   };
-
 } // End namespace Uintah
 
 #endif
