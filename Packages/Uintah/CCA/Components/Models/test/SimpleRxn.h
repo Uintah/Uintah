4e0482144f5a2553ad8c007b78160e1f8a041582
Todd Harman <harman@sci.utah.edu>
2004-01-16 00:17:09 +0000

    Single scalar field (f) reaction model, base commit, nothing is reacting yet....

--- Packages/Uintah/CCA/Components/Models/test/SimpleRxn.h
+++ Packages/Uintah/CCA/Components/Models/test/SimpleRxn.h
@@ -4,6 +4,13 @@
 
 #include <Packages/Uintah/CCA/Ports/ModelInterface.h>
 #include <Packages/Uintah/Core/Grid/ComputeSet.h>
+#include <Packages/Uintah/Core/Grid/CCVariable.h>
+#include <Packages/Uintah/Core/Grid/SFCXVariable.h>
+#include <Packages/Uintah/Core/Grid/SFCYVariable.h>
+#include <Packages/Uintah/Core/Grid/SFCZVariable.h>
+#include <Packages/Uintah/Core/Grid/VarTypes.h>
+#include <map>
+#include <vector>
 
 namespace Uintah {
 
@@ -18,7 +25,7 @@ GENERAL INFORMATION
 
    SimpleRxn.h
 
-   Steven G. Parker
+   Todd Harman
    Department of Computer Science
    University of Utah
 
@@ -36,57 +43,86 @@ WARNING
   
 ****************************************/
 
+  class GeometryPiece;
   class SimpleRxn : public ModelInterface {
   public:
     SimpleRxn(const ProcessorGroup* myworld, ProblemSpecP& params);
     virtual ~SimpleRxn();
-
-    //////////
-    // Insert Documentation Here:
+    
     virtual void problemSetup(GridP& grid, SimulationStateP& sharedState,
 			      ModelSetup* setup);
-      
-    //////////
-    // Insert Documentation Here:
+    
     virtual void scheduleInitialize(SchedulerP&,
 				    const LevelP& level,
 				    const ModelInfo*);
 
-    //////////
-    // Insert Documentation Here:
     virtual void restartInitialize() {}
       
-    //////////
-    // Insert Documentation Here:
     virtual void scheduleComputeStableTimestep(SchedulerP&,
 					       const LevelP& level,
 					       const ModelInfo*);
       
-    //////////
-    // Insert Documentation Here:
     virtual void scheduleMassExchange(SchedulerP&,
 				      const LevelP& level,
 				      const ModelInfo*);
+                                  
     virtual void scheduleMomentumAndEnergyExchange(SchedulerP&,
 						   const LevelP& level,
 						   const ModelInfo*);
 
   private:
-    void massExchange(const ProcessorGroup*, const PatchSubset* patches,
-		      const MaterialSubset* matls, DataWarehouse*, 
-		      DataWarehouse* new_dw, const ModelInfo*);
+    void initialize(const ProcessorGroup*, 
+                    const PatchSubset* patches,
+		      const MaterialSubset* matls, 
+                    DataWarehouse*, 
+		      DataWarehouse* new_dw);
+                     
+    void momentumAndEnergyExchange(const ProcessorGroup*, 
+                                   const PatchSubset* patches,
+	                            const MaterialSubset* matls, 
+                                   DataWarehouse*, 
+	                            DataWarehouse* new_dw, 
+                                   const ModelInfo*);
+                                   
+    void massExchange(const ProcessorGroup*, 
+                      const PatchSubset* patches,
+                      const MaterialSubset*,
+                      DataWarehouse* old_dw,
+                      DataWarehouse* new_dw,
+                      const ModelInfo* mi);
 
     SimpleRxn(const SimpleRxn&);
     SimpleRxn& operator=(const SimpleRxn&);
 
     ProblemSpecP params;
+
     const Material* matl;
+    MaterialSet* mymatl;
 
-    MaterialSet* mymatls;
-    double rate;
-    double d_cv_0;  //specific heat
+    class Region {
+    public:
+      Region(GeometryPiece* piece, ProblemSpecP&);
 
-    VarLabel* massFraction;
+      GeometryPiece* piece;
+      double initialScalar;
+    };
+
+    class Scalar {
+    public:
+      int index;
+      string name;
+      VarLabel* scalar_CCLabel;
+      VarLabel* scalar_source_CCLabel;
+      vector<Region*> regions;
+      double f_stoic;
+      
+      double diff_coeff;
+      int  initialize_diffusion_knob;
+    };
+    Scalar* d_scalar;
+    
+    double d_cp;
+    SimulationStateP sharedState;
   };
 }
 
