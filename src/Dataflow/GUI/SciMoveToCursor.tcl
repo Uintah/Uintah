1ced3753f7bc68a58779742e3f6e4c39a28b8052
McKay Davis <mdavis@sci.utah.edu>
2004-01-22 21:34:23 +0000

    Dont post error when GUI enviornment variable aren't defined

--- src/Dataflow/GUI/SciMoveToCursor.tcl
+++ src/Dataflow/GUI/SciMoveToCursor.tcl
@@ -23,7 +23,8 @@ set screenHeight [winfo screenheight .]
 
 proc moveToCursor { window { leave_up "no" } } {
   global env
-  if ![boolToInt $env(SCIRUN_GUI_MoveGuiToMouse)] return
+    if { [info exists env(SCI_GUI_MoveGuiToMouse)] &&
+	 ![boolToInt $env(SCIRUN_GUI_MoveGuiToMouse)] } return
 
   # If we are currently running a script... ie, we are loading the net
   # from a file, then do not move GUI to the mouse.
