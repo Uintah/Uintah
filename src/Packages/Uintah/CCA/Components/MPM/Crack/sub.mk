3e5453354e11437d5cf5d53f2c8e4d6fc227d956
Yajun Guo <yguo@sci.utah.edu>
2004-01-30 19:16:19 +0000

    Crack.cc has been decomposed into several files. Those files are included here to get compiled and linked.

--- src/Packages/Uintah/CCA/Components/MPM/Crack/sub.mk
+++ src/Packages/Uintah/CCA/Components/MPM/Crack/sub.mk
@@ -3,4 +3,11 @@
 SRCDIR   := Packages/Uintah/CCA/Components/MPM/Crack
 
 SRCS     += \
-	$(SRCDIR)/Crack.cc
+	$(SRCDIR)/ReadAndDiscretizeCracks.cc \
+	$(SRCDIR)/ParticleNodePairVelocityField.cc \
+	$(SRCDIR)/CrackSurfaceContact.cc \
+	$(SRCDIR)/FractureParametersCalculation.cc \
+	$(SRCDIR)/CrackPropagation.cc \
+	$(SRCDIR)/MoveCracks.cc \
+	$(SRCDIR)/PrivateMethods.cc 
+
