bf353e4b2f8165c085b7012c5ad9e49ee0639ccf
McKay Davis <mdavis@sci.utah.edu>
2004-02-03 17:20:59 +0000

    [Bug 1562] Annotation remains when module destroyed.   This is fixed now.

--- src/Dataflow/GUI/Module.tcl
+++ src/Dataflow/GUI/Module.tcl
@@ -1612,7 +1612,7 @@ proc moduleDestroy {modid} {
     }
 
     # Delete Icon from canvases
-    $Subnet(Subnet$Subnet($modid)_canvas) delete $modid $modid-notes
+    $Subnet(Subnet$Subnet($modid)_canvas) delete $modid $modid-notes $modid-notes-shadow
     destroy $Subnet(Subnet$Subnet($modid)_canvas).module$modid
     $Subnet(Subnet$Subnet($modid)_minicanvas) delete $modid
     
