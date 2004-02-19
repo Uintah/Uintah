8f94c823288341764a39656d0f2928a7547ddced
John Schmidt <jas@sci.utah.edu>
2004-02-19 23:36:09 +0000

    Added ability to specify a random variable to the Density boundary condition.

--- src/Packages/Uintah/Core/Grid/BoundCondFactory.cc
+++ src/Packages/Uintah/Core/Grid/BoundCondFactory.cc
@@ -77,6 +77,7 @@ void BoundCondFactory::create(ProblemSpecP& child,
   
   else if (bc_attr["label"] == "Density" &&
 	   (bc_attr["var"]   == "Neumann"  ||
+	    bc_attr["var"]   == "Dirichlet_perturbed"  ||
 	    bc_attr["var"]   == "LODI" ||
 	    bc_attr["var"]   == "Dirichlet") ) {
     bc = scinew DensityBoundCond(child,bc_attr["var"]);
