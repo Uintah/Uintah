7fa836276350370493ae01cd3f82b423da94dbd1
J. Davison de St. Germain <dav@sci.utah.edu>
2004-02-26 01:37:35 +0000

    Indented a line.  Moved moving of window to before it is deiconified to prevent flickering

--- src/Dataflow/GUI/SciMoveToCursor.tcl
+++ src/Dataflow/GUI/SciMoveToCursor.tcl
@@ -23,8 +23,9 @@ set screenHeight [winfo screenheight .]
 
 proc moveToCursor { window { leave_up "no" } } {
   global env
-    if { [info exists env(SCI_GUI_MoveGuiToMouse)] &&
-	 ![boolToInt $env(SCIRUN_GUI_MoveGuiToMouse)] } return
+
+  if { [info exists env(SCI_GUI_MoveGuiToMouse)] &&
+       ![boolToInt $env(SCIRUN_GUI_MoveGuiToMouse)] } return
 
   # If we are currently running a script... ie, we are loading the net
   # from a file, then do not move GUI to the mouse.
@@ -42,6 +43,7 @@ proc moveToCursor { window { leave_up "no" } } {
   # height of a widget.  You have to "withdraw" it first and call
   # "update idletasks" to make it figure out its geometry ... so now
   # this will work!
+
   if { [winfo ismapped $window] == 0 } {
       if { $leave_up != "leave_up" } {
 	  wm withdraw $window
@@ -68,9 +70,9 @@ proc moveToCursor { window { leave_up "no" } } {
       set windowYLoc [expr $cursorYLoc - 80]
   }
 
+  wm geometry $window +$windowXLoc+$windowYLoc
+
   if { $leave_up == "leave_up" } {
       wm deiconify $window
   }
-
-  wm geometry $window +$windowXLoc+$windowYLoc
 }
