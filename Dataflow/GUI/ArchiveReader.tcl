b1e80dc2ea42f60b648082950594f3b19cec51a1
Kurt Zimmerman <kuzimmer@sci.utah.edu>
2004-02-26 20:25:25 +0000

    Updated so that a window containing the uda directory will stay exposed

--- Dataflow/GUI/ArchiveReader.tcl
+++ Dataflow/GUI/ArchiveReader.tcl
@@ -15,9 +15,42 @@ itcl_class Uintah_DataIO_ArchiveReader {
 
     method ui {} { 
 
+	set w .ui[modname]
+	if {[winfo exists $w]} {
+	    return;
+	}
+
 	global $this-filebase
-	set $this-filebase [tk_chooseDirectory -parent . -mustexist 1 \
-				-initialdir [set $this-filebase] ]
+	toplevel $w
+
+	set len [string length [set $this-filebase]]
+	if { $len == 0 } { set len 40 }
+	frame $w.f 
+	pack $w.f 
+	label $w.f.l -text "Choose Archive"
+	entry $w.f.e -textvariable $this-filebase -width $len -state disabled
+	frame $w.f.b
+	pack $w.f.l $w.f.e $w.f.b -side top -pady 2 -expand yes -fill x
+
+	button $w.f.b.b1 -text "Select UDA" -command "$this get_uda"
+	button $w.f.b.b2 -text "Execute" \
+	    -command "$this-c needexecute"
+	pack $w.f.b.b1 $w.f.b.b2 -side left -padx 2
+
+    }
+    method get_uda {} {
+	set w .ui[modname]
+	global $this-filebase
+	
+	set filebase [tk_chooseDirectory -parent $w \
+			-mustexist 1 -initialdir [set $this-filebase] ]
+	if { $filebase != "" } {
+	    set len [string length $filebase]
+	    incr len
+	    $w.f.e configure -width $len
+	    set $this-filebase $filebase
+	}
 	$this-c needexecute
     }
+	
 }
