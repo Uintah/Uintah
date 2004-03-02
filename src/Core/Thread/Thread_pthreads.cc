9cb2ef2c8237d0d55684c365fd15d447169349b7
Michael Callahan <callahan@sci.utah.edu>
2004-03-02 18:59:03 +0000

    Fix thread lock problem for RHE and similar

--- src/Core/Thread/Thread_pthreads.cc
+++ src/Core/Thread/Thread_pthreads.cc
@@ -857,6 +857,11 @@ Mutex::Mutex(const char* name)
 
 Mutex::~Mutex()
 {
+ // RHE 3.0 pthread_mutex_destroy returns EBUSY if you unlock an already
+ // unlocked thread first.  So we force a lock/unlock before
+ // destroying.  Probably better to just remove the unlock altogether
+ // but this breaks the shutdown behavior.
+  pthread_mutex_trylock(&priv_->mutex);
   pthread_mutex_unlock(&priv_->mutex);
   if(pthread_mutex_destroy(&priv_->mutex) != 0) {
     fprintf(stderr, "pthread_mutex_destroy() failed!!\n");
@@ -941,6 +946,11 @@ RecursiveMutex::RecursiveMutex(const char* name)
 
 RecursiveMutex::~RecursiveMutex()
 {
+ // RHE 3.0 pthread_mutex_destroy returns EBUSY if you unlock an already
+ // unlocked thread first.  So we force a lock/unlock before
+ // destroying.  Probably better to just remove the unlock altogether
+ // but this breaks the shutdown behavior.
+  pthread_mutex_trylock(&priv_->mutex);
   pthread_mutex_unlock(&priv_->mutex);
   if(pthread_mutex_destroy(&priv_->mutex) != 0)
       throw ThreadError(std::string("pthread_mutex_destroy: ")
