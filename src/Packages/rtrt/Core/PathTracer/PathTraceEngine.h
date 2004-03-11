97cdbb260ebc92f82e3b7029efbebb1e89caa5aa
Christiaan Gribble <cgribble@sci.utah.edu>
2004-03-11 01:27:20 +0000

    Added PathTraceLight, fleshed out constructors, other goodness to make it functional.

--- src/Packages/rtrt/Core/PathTracer/PathTraceEngine.h
+++ src/Packages/rtrt/Core/PathTracer/PathTraceEngine.h
@@ -17,12 +17,24 @@ namespace rtrt {
   class PerProcessorContext;
   class DepthStats;
 
+  class PathTraceLight {
+  public:
+    Point center;
+    double radius;
+    double area;
+    Color color;
+
+    PathTraceLight(const Point &cen, double radius, const Color &c);
+    Point random_point(double r1, double r2, double r3);
+  };
+
   class PathTraceContext {
   public:
-    PathTraceContext(Color &color, Object* geometry, int num_samples);
+    PathTraceContext(const Color &color, const PathTraceLight &light,
+		     Object* geometry, int num_samples, int max_depth);
 
     // Light source
-    
+    PathTraceLight light;
     // Object color
     Color color;
     // Acceleration geometry
@@ -30,6 +42,8 @@ namespace rtrt {
     // Number of samples to perform for each texel
     int num_samples;
     int num_samples_root;
+    // Maximum ray depth
+    int max_depth;
 
     // Stuff for PerProcessorContext
     int pp_size;
@@ -43,6 +57,8 @@ namespace rtrt {
     PathTraceContext* ptc;
     // Textures
     Group* local_spheres;
+    // Base filename for textures
+    char *basename;
     // Random number generator
     MusilRNG rng;
     // Our group of random samples
@@ -52,7 +68,7 @@ namespace rtrt {
     PerProcessorContext* ppc;
     DepthStats* depth_stats;
   public:
-    PathTraceWorker();
+    PathTraceWorker(Group *group, PathTraceContext *ptc, char *texname);
     ~PathTraceWorker();
     // This does all the work
     void run();
@@ -64,9 +80,9 @@ namespace rtrt {
     Array2<Color> texture;
     friend class PathTraceWorker;
   public:
-    TextureSphere(Point cen, double radius, int tex_res);
+    TextureSphere(const Point &cen, double radius, int tex_res=16);
   };
-  
+
 } // end namespace rtrt
 
 #endif
