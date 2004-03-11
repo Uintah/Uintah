7cf4ef3747c9b8040bcd04d3bfd05003615e543c
Christiaan Gribble <cgribble@sci.utah.edu>
2004-03-11 16:31:11 +0000

    Added TextureSphere::writeTexture; fixed variable scoping issues; added some comments; fixed shadow ray issue

--- Packages/rtrt/Core/PathTracer/PathTraceEngine.cc
+++ Packages/rtrt/Core/PathTracer/PathTraceEngine.cc
@@ -95,58 +95,58 @@ void PathTraceWorker::run() {
       cerr << "Object in local list (index="<<sindex<<") is not a TextureSphere\n";
       continue;
     }
+    
     int width = sphere->texture.dim1();
     int height = sphere->texture.dim2();
     // Iterate over each texel
     for(int y = 0; y < height; y++)
-      for(int x = 0; x < width; x++)
-      {
+      for(int x = 0; x < width; x++) {
         int sgindex = (int)(rng()*(NUM_SAMPLE_GROUPS-1));
         int sgindex2 = (int)(rng()*(NUM_SAMPLE_GROUPS-1));
 
         for(int sample = 0; sample < ptc->num_samples; sample++) {
           Point2D sample_point = sample_points[sgindex][sample];
+
           // Project 2D point onto sphere
-          double phi=2.0*M_PI*sample_point.x();
-          double theta=M_PI*(1.0-sample_point.y());
-          double x=cos(phi)*sin(theta);
-          double y=sin(phi)*sin(theta);
-          double z=cos(theta);
+	  Point origin;
+	  Vector normal;
+	  {
+            double phi=2.0*M_PI*sample_point.x();
+	    double theta=M_PI*(1.0-sample_point.y());
+	    double x=cos(phi)*sin(theta);
+	    double y=sin(phi)*sin(theta);
+	    double z=cos(theta);
           
-          Point origin=sphere->cen + sphere->radius*Vector(x,y,z);
-          Vector normal(x, y, z);
+	    origin=sphere->cen + sphere->radius*Vector(x,y,z);
+	    normal=Vector(x,y,z);
+	  }
 
           Color result(0,0,0);
-          for (int depth=0; depth < ptc->max_depth; depth++)
-          {
-	    cerr << "depth = "<<depth<<"\n";
-            // Compute direct illumination
-            PathTraceLight *light=&(ptc->light);
+          PathTraceLight *light=&(ptc->light);
+          for (int depth=0; depth < ptc->max_depth; depth++) {
+            // Compute direct illumination (assumes one light source)
             Point random_point=light->random_point(rng(),rng(),rng());
             
             // Shadow ray
             Vector sr_dir=random_point-origin;
             double distance=sr_dir.normalize();
-            Ray sr_ray(origin, sr_dir);
+            double dotprod=SCIRun::Dot(normal, sr_dir);
+            if (dotprod>0.0)
+            {
+              Ray sr_ray(origin, sr_dir);
+              HitInfo shadow_hit;
+              shadow_hit.min_t = distance;
+              Color shadow_color(1,1,1);
 
-            HitInfo shadow_hit;
-            shadow_hit.min_t = distance;
-            Color shadow_color(1,1,1);
-            ptc->geometry->light_intersect(sr_ray, shadow_hit, shadow_color, depth_stats, ppc);
-
-	    Color direct;
-	    //            if (!shadow_hit.was_hit)
-              direct = ptc->color*light->color*light->area*SCIRun::Dot(normal,sr_dir)*(1./(distance*distance*M_PI));
-	      //	    else
-	      //	      direct = Color(0,0,0);
-
-	    result += direct;
-	    cerr << "direct color = "<<direct<<", result = "<<result<<"\n";
+              ptc->geometry->light_intersect(sr_ray, shadow_hit, shadow_color, depth_stats, ppc);
+	      if (!shadow_hit.was_hit)
+                result += ptc->color*light->color*light->area*dotprod*(1./(distance*distance*M_PI));
+            }
 
             if (depth==ptc->max_depth-1)
               break;
 
-            // Determine next sphere
+            // Spawn a secondary ray
             // Pick a random direction on the hemisphere
             Vector v0(Cross(normal, Vector(1,0,0)));
             if(v0.length2() == 0)
@@ -165,52 +165,51 @@ void PathTraceWorker::run() {
               double z=sqrt(1.0 - x*x - y*y);
               v_out=Vector(x, y, z);
             }
-            Vector ray_dir=v_out.x()*v1+v_out.y()*v2+v_out.z()*normal;
+            Vector ray_dir=v1*v_out.x()+v2*v_out.y()+normal*v_out.z();
 
             Ray ray(origin, ray_dir);
 #if 0          
-            // Trace ray into our group
+            // Trace ray into our local spheres
             HitInfo local_hit;
             local_spheres->intersect(ray, local_hit, depth_stats, ppc);
-#endif
 
-            // Trace ray into our geometry
+	    // Trace ray into remaining geometry
             HitInfo global_hit;
             ptc->geometry->intersect(ray, global_hit, depth_stats, ppc);
 
-            // Next sphere's origin/normal
+	    // Resolve next sphere
+	    //  if local, set flag to indicate that we can accumulate
+	    //    the next result in it's texture (?)
+#else
+            // Trace ray into geometry
+            HitInfo global_hit;
+            ptc->geometry->intersect(ray, global_hit, depth_stats, ppc);
+	    
             if (global_hit.was_hit)
             {
+              // Set next sphere's origin/normal
               origin=origin+global_hit.min_t*ray_dir;
               normal=global_hit.hit_obj->normal(origin, global_hit);
             }
             else
             {
-              // accumulate bg color?
+              // Accumulate bg color?
               break;
             }
+#endif
           } // end depth
 
           // Store result
           sphere->texture(x,y)+=result;
         } // end sample
 
-        // Normalize
-        sphere->texture(x,y)=inv_num_samples*sphere->texture(x,y);
+        // Normalize result
+	sphere->texture(x,y)=inv_num_samples*sphere->texture(x,y);
       } // end texel
   } // end sphere
 
   // Write out textures
   for(int sindex = 0; sindex < local_spheres->objs.size(); sindex++) {
-    // Create the filename
-    char *buf = new char[strlen(basename) + 20];
-    sprintf(buf, "%s%05d.nrrd", basename, sindex);
-    FILE *out = fopen(buf, "wb");
-    if (!out) {
-      cerr << "Cannot open "<<buf<<" for writing\n";
-      continue;
-    }
-    
     TextureSphere *sphere  =
       dynamic_cast<TextureSphere*>(local_spheres->objs[sindex]);
     if (!sphere) {
@@ -218,28 +217,7 @@ void PathTraceWorker::run() {
       continue;
     }
 
-    int width = sphere->texture.dim1();
-    int height = sphere->texture.dim2();
-    fprintf(out, "NRRD0001\n");
-    fprintf(out, "type: float\n");
-    fprintf(out, "dimension: 3\n");
-    fprintf(out, "sizes: 3 %d %d\n", width, height);
-    fprintf(out, "spacings: NaN 1 1\n");
-    fprintf(out, "endian: big\n");
-    fprintf(out, "encoding: raw\n\n");
-    // Iterate over each texel
-    for(int y = 0; y < height; y++)
-      for(int x = 0; x < width; x++) {
-	float data[3];
-	data[0] = sphere->texture(x,y).red();
-	data[1] = sphere->texture(x,y).green();
-	data[2] = sphere->texture(x,y).blue();
-	if (fwrite (data, sizeof(float), 3, out) != 3) {
-	  cerr << "Trouble writing texel for sphere "<<sindex<<" at ["<<x<<", "<<y<<"]\n";
-	  return;
-	  }
-      }
-    fclose(out);
+    sphere->writeTexture(basename,sindex);
   }
 }
 
@@ -247,6 +225,42 @@ TextureSphere::TextureSphere(const Point &cen, double radius, int tex_res):
   Sphere(0, cen, radius), texture(tex_res, tex_res)
 {
   texture.initialize(0);
+}
+
+void TextureSphere::writeTexture(char* basename, int index)
+{
+  // Create the filename
+  char *buf = new char[strlen(basename) + 20];
+  sprintf(buf, "%s%05d.nrrd", basename, index);
+  FILE *out = fopen(buf, "wb");
+  if (!out) {
+    cerr << "Cannot open "<<buf<<" for writing\n";
+    return;
+  }
+  
+  int width = texture.dim1();
+  int height = texture.dim2();
+  fprintf(out, "NRRD0001\n");
+  fprintf(out, "type: float\n");
+  fprintf(out, "dimension: 3\n");
+  fprintf(out, "sizes: 3 %d %d\n", width, height);
+  fprintf(out, "spacings: NaN 1 1\n");
+  fprintf(out, "endian: big\n");
+  fprintf(out, "encoding: raw\n");
+  fprintf(out, "\n");
+  // Iterate over each texel
+  for(int y = 0; y < height; y++)
+    for(int x = 0; x < width; x++) {
+      float data[3];
+      data[0] = texture(x,y).red();
+      data[1] = texture(x,y).green();
+      data[2] = texture(x,y).blue();
+      if (fwrite(data, sizeof(float), 3, out) != 3) {
+	cerr << "Trouble writing texel for sphere "<<index<<" at ["<<x<<", "<<y<<"]\n";
+	return;
+      }
+    }
+  fclose(out);
 }
 
 PathTraceLight::PathTraceLight(const Point &cen, double radius,
