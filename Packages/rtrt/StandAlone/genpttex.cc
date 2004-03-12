4c8e0526e6af8edf15cd2e57a0ec2a03705a8ed7
Christiaan Gribble <cgribble@sci.utah.edu>
2004-03-12 16:00:18 +0000

    Now uses a single sphere at origin (until textures are debugged)

--- Packages/rtrt/StandAlone/genpttex.cc
+++ Packages/rtrt/StandAlone/genpttex.cc
@@ -18,6 +18,10 @@ Group *make_geometry( )
 {
   Group* group=new Group();
 
+  Object* obj0=new TextureSphere(Point(0,0,0), 1 );
+  group->add( obj0 );
+
+  /*
   Object* obj0=new TextureSphere(Point(-1,-1,0), 1 );
   group->add( obj0 );
 
@@ -29,17 +33,38 @@ Group *make_geometry( )
 
   Object* obj3=new TextureSphere(Point(1,-1,0), 1 );
   group->add( obj3 );
-
+  */
+  
   return group;
 }
 
 int main(int argc, char** argv)
 {
-  PathTraceLight ptlight(Point(1,1,-10), 0.8, 10000.0*Color(1,1,1));
+  double intensity=1000.0;
+  int num_samples=10000;
+  int depth=3;
+  for(int i=1;i<argc;i++) {
+    if(strcmp(argv[i],"-intensity")==0) {
+      intensity=atof(argv[++i]);
+    }
+    else if(strcmp(argv[i],"-num_samples")==0) {
+      num_samples=atoi(argv[++i]);
+    }
+    else if(strcmp(argv[i],"-depth")==0) {
+      depth=atoi(argv[++i]);
+    }
+    else {
+      cerr<<"unrecognized option \""<<argv[i]<<"\""<<endl;
+      exit(1);
+    }
+  }
+  
+  PathTraceLight ptlight(Point(10,10,10), 1.0, intensity*Color(1,1,1));
   Group *group = make_geometry();
-  PathTraceContext ptcontext(Color(0.1,0.7,0.2), ptlight, group, 100, 3);  
-
+  PathTraceContext ptcontext(Color(0.1,0.7,0.2), ptlight, group,
+			     num_samples, depth);
   PathTraceWorker ptworker(group, &ptcontext, "sphere");
+
   ptworker.run();
       
   return 0;
