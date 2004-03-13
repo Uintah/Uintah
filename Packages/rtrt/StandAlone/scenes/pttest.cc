05d8177f88e009b07b1ce32e7dc1acf5b1cc0c8b
James Bigler <bigler@sci.utah.edu>
2004-03-13 01:37:20 +0000

    Better file name generation.  Removed redundant code.  Render 8 spheres instead of 1.  Moved light source.

--- Packages/rtrt/StandAlone/scenes/pttest.cc
+++ Packages/rtrt/StandAlone/scenes/pttest.cc
@@ -7,7 +7,7 @@
 #include <Core/Geometry/Vector.h>
 #include <Core/Geometry/Transform.h>
 #include <Packages/rtrt/Core/Group.h>
-#include <Packages/rtrt/Core/UVSphere.h>
+#include <Packages/rtrt/Core/UVSphere2.h>
 #include <Packages/rtrt/Core/SharedTexture.h>
 
 #include <sgi_stl_warnings_off.h>
@@ -18,57 +18,36 @@
 
 using namespace rtrt;
 
-Group *make_geometry(char* tex0, char* tex1,
-		     char* tex2, char* tex3)
+#define NUM_TEXTURES 8
+
+float radius = 1;
+
+// Returns whether the object was added or not.  This is based on if
+// the texture was found.
+bool add_sphere(char *tex_name, const Point& center, Group *group) {
+  SharedTexture* matl = new SharedTexture(tex_name);
+  if (!matl->valid())
+  {
+    cerr << "AddSphere::texture is bad :" << tex_name << endl;
+    return false;
+  }
+  group->add( new UVSphere2(matl, center, radius) );
+  return true;
+}
+
+Group *make_geometry(char* tex_names[NUM_TEXTURES])
 {
   Group* group=new Group();
-  
-  SharedTexture* matl0 = new SharedTexture(tex0);
-  if (!matl0->valid())
-  {
-    cerr << "matl0 is invalid" << endl;
-    return 0;
-  }
-  Object* obj0=new UVSphere(matl0, Point(0,0,0), 1, Vector(0,1,0));
-  group->add( obj0 );
-  
-  /*
-  SharedTexture* matl0 = new SharedTexture(tex0);
-  if (!matl0->valid())
-  {
-    cerr << "matl0 is invalid" << endl;
-    return 0;
-  }
-  Object* obj0=new UVSphere(matl0, Point(-1,-1,0), 1, Vector(0,1,0));
-  group->add( obj0 );
 
-  SharedTexture* matl1 = new SharedTexture(tex1);
-  if (!matl1->valid())
-  {
-    cerr << "matl1 is invalid" << endl;
-    return 0;
-  }
-  Object* obj1=new UVSphere(matl1, Point(-1,1,0), 1, Vector(0,1,0));
-  group->add( obj1 );
+  add_sphere(tex_names[0], Point(-1,-1,-1), group);
+  add_sphere(tex_names[1], Point(-1, 1,-1), group);
+  add_sphere(tex_names[2], Point( 1, 1,-1), group);
+  add_sphere(tex_names[3], Point( 1,-1,-1), group);
 
-  SharedTexture* matl2 = new SharedTexture(tex2);
-  if (!matl2->valid())
-  {
-    cerr << "matl2 is invalid" << endl;
-    return 0;
-  }
-  Object* obj2=new UVSphere(matl2, Point(1,1,0), 1, Vector(0,1,0));
-  group->add( obj2 );
-
-  SharedTexture* matl3 = new SharedTexture(tex3);
-  if (!matl3->valid())
-  {
-    cerr << "matl3 is invalid" << endl;
-    return 0;
-  }
-  Object* obj3=new UVSphere(matl3, Point(1,-1,0), 1, Vector(0,1,0));
-  group->add( obj3 );
-  */
+  add_sphere(tex_names[4], Point(-1,-1, 1), group);
+  add_sphere(tex_names[5], Point(-1, 1, 1), group);
+  add_sphere(tex_names[6], Point( 1, 1, 1), group);
+  add_sphere(tex_names[7], Point( 1,-1, 1), group);
   
   return group;
 }
@@ -77,34 +56,34 @@ extern "C"
 Scene* make_scene(int argc, char** argv, int /*nworkers*/)
 {
   char *bg="/home/sci/cgribble/research/datasets/mpm/misc/envmap.ppm";
-  char *tex0="/home/sci/cgribble/SCIRun/irix.64/Packages/rtrt/StandAlone/sphere0.ppm";
-  char *tex1="/home/sci/cgribble/SCIRun/irix.64/Packages/rtrt/StandAlone/sphere1.ppm";
-  char *tex2="/home/sci/cgribble/SCIRun/irix.64/Packages/rtrt/StandAlone/sphere2.ppm";
-  char *tex3="/home/sci/cgribble/SCIRun/irix.64/Packages/rtrt/StandAlone/sphere3.ppm";
+  char *tex_basename="./sphere";
+
   for (int i=1;i<argc;i++)
   {
     if (strcmp(argv[i],"-bg")==0)
-      bg=argv[++i];
-    else if (strcmp(argv[i],"-tex0")==0)
-      tex0=argv[++i];
-    else if (strcmp(argv[i],"-tex1")==0)
-      tex1=argv[++i];
-    else if (strcmp(argv[i],"-tex2")==0)
-      tex2=argv[++i];
-    else if (strcmp(argv[i],"-tex3")==0)
-      tex3=argv[++i];
+      bg = argv[++i];
+    else if (strcmp(argv[i],"-tex")==0)
+      tex_basename = argv[++i];
     else
     {
       cerr << "unrecognized option \"" << argv[i] << "\"" << endl;
       exit(1);
     }
   }
- 
-  Group *group=make_geometry(tex0, tex1, tex2, tex3);
+
+  char *tex_names[NUM_TEXTURES];
+  // Make the tex_names
+  size_t name_length = strlen(tex_basename) + 15;
+  for(int i = 0; i < NUM_TEXTURES; i++) {
+    tex_names[i] = new char[name_length];
+    sprintf(tex_names[i], "%s%d.ppm", tex_basename, i); 
+  }
+  
+  Group *group=make_geometry(tex_names);
 
   Camera cam(Point(0,0,10), Point(0,0,0), Vector(0,1,0), 45.0);
 
-  double ambient_scale=1.0;
+  double ambient_scale=1;
   Color bgcolor(0,0,0);
   Color cdown(1,1,1);
   Color cup(1,1,1);
@@ -116,9 +95,11 @@ Scene* make_scene(int argc, char** argv, int /*nworkers*/)
   EnvironmentMapBackground *emap=new EnvironmentMapBackground(bg, Vector(0,1,0));
   scene->set_background_ptr(emap);
     
-  Light* mainLight = new Light(Point(10,10,10), Color(1,1,1), 1.0);
+  Light* mainLight = new Light(Point(-5,10,7.5), Color(1,1,1), 1.0);
   mainLight->name_ = "main light";
   scene->add_light( mainLight );
+
+  scene->select_shadow_mode(No_Shadows);
   
   return scene;
 }
