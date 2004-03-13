f9590fc1795cc65465412635ee3cdbeb5d80e893
James Bigler <bigler@sci.utah.edu>
2004-03-13 01:36:23 +0000

    Added more spheres.  Moved light source.  Added useful commands in comments.

--- Packages/rtrt/StandAlone/genpttex.cc
+++ Packages/rtrt/StandAlone/genpttex.cc
@@ -1,4 +1,17 @@
 
+/*
+
+ Some useful unu commands
+
+ # Use these to quantize the textures and copy them to ppms
+ 
+ unu join -a 3 -i old/sphere0000*.nrrd | unu quantize -b 8 | unu dice -a 3 -o sphere
+ echo 'for T in sphere*.png; do unu save -f pnm -i $T -o `basename $T .png`.ppm; done' | bash
+
+ # This coppies the first column to the end to help with texture blending.
+ 
+ echo 'for T in sphere?.ppm; do unu slice -a 1 -p M -i $T | unu reshape -s 3 1 64 | unu join -a 1 -i - $T -o $T;done' | bash
+*/
 
 #include <Packages/rtrt/Core/PathTracer/PathTraceEngine.h>
 #include <Packages/rtrt/Core/Group.h>
@@ -14,26 +27,27 @@ using namespace rtrt;
 using namespace SCIRun;
 using namespace std;
 
+int tex_size = 16;
+
 Group *make_geometry( )
 {
   Group* group=new Group();
 
-  Object* obj0=new TextureSphere(Point(0,0,0), 1 );
-  group->add( obj0 );
+  group->add( new TextureSphere(Point(-1,-1,-1), 1, tex_size ) );
+  
+  group->add( new TextureSphere(Point(-1,1,-1), 1, tex_size ) );
 
-  /*
-  Object* obj0=new TextureSphere(Point(-1,-1,0), 1 );
-  group->add( obj0 );
+  group->add( new TextureSphere(Point(1,1,-1), 1, tex_size ) );
 
-  Object* obj1=new TextureSphere(Point(-1,1,0), 1 );
-  group->add( obj1 );
+  group->add( new TextureSphere(Point(1,-1,-1), 1, tex_size ) );
+  
+  group->add( new TextureSphere(Point(-1,-1,1), 1, tex_size ) );
 
-  Object* obj2=new TextureSphere(Point(1,1,0), 1 );
-  group->add( obj2 );
+  group->add( new TextureSphere(Point(-1,1,1), 1, tex_size ) );
 
-  Object* obj3=new TextureSphere(Point(1,-1,0), 1 );
-  group->add( obj3 );
-  */
+  group->add( new TextureSphere(Point(1,1,1), 1, tex_size ) );
+
+  group->add( new TextureSphere(Point(1,-1,1), 1, tex_size ) );
   
   return group;
 }
@@ -53,13 +67,22 @@ int main(int argc, char** argv)
     else if(strcmp(argv[i],"-depth")==0) {
       depth=atoi(argv[++i]);
     }
+    else if(strcmp(argv[i],"-tex_size")==0) {
+      tex_size=atoi(argv[++i]);
+    }
     else {
       cerr<<"unrecognized option \""<<argv[i]<<"\""<<endl;
+
+      cerr << "valid options are: \n";
+      cerr << "-intensity <float>\n";
+      cerr << "-num_samples <int>\n";
+      cerr << "-depth <int>\n";
+      cerr << "-tex_size <int>\n";
       exit(1);
     }
   }
   
-  PathTraceLight ptlight(Point(10,10,10), 1.0, intensity*Color(1,1,1));
+  PathTraceLight ptlight(Point(-5, 10, 7.5), 1.0, intensity*Color(1,1,1));
   Group *group = make_geometry();
   PathTraceContext ptcontext(Color(0.1,0.7,0.2), ptlight, group,
 			     num_samples, depth);
