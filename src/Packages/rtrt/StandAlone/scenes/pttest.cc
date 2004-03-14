a2f42816e8b2bc66774ffe9cc9ef04cec32e95e7
James Bigler <bigler@sci.utah.edu>
2004-03-14 07:53:54 +0000

    Better error checking on reading textures.

--- src/Packages/rtrt/StandAlone/scenes/pttest.cc
+++ src/Packages/rtrt/StandAlone/scenes/pttest.cc
@@ -22,34 +22,38 @@ using namespace rtrt;
 
 float radius = 1;
 
-// Returns whether the object was added or not.  This is based on if
-// the texture was found.
-bool add_sphere(char *tex_name, const Point& center, Group *group) {
+// Returns 1 if there was an error.  This is based on if the texture
+// was found.
+int add_sphere(char *tex_name, const Point& center, Group *group) {
   SharedTexture* matl = new SharedTexture(tex_name);
   if (!matl->valid())
   {
     cerr << "AddSphere::texture is bad :" << tex_name << endl;
-    return false;
+    return 1;
   }
   group->add( new UVSphere2(matl, center, radius) );
-  return true;
+  return 0;
 }
 
 Group *make_geometry(char* tex_names[NUM_TEXTURES])
 {
   Group* group=new Group();
 
-  add_sphere(tex_names[0], Point(-1,-1,-1), group);
-  add_sphere(tex_names[1], Point(-1, 1,-1), group);
-  add_sphere(tex_names[2], Point( 1, 1,-1), group);
-  add_sphere(tex_names[3], Point( 1,-1,-1), group);
+  int E = 0;
+  if (!E) E |= add_sphere(tex_names[0], Point(-1,-1,-1), group);
+  if (!E) E |= add_sphere(tex_names[1], Point(-1, 1,-1), group);
+  if (!E) E |= add_sphere(tex_names[2], Point( 1, 1,-1), group);
+  if (!E) E |= add_sphere(tex_names[3], Point( 1,-1,-1), group);
 
-  add_sphere(tex_names[4], Point(-1,-1, 1), group);
-  add_sphere(tex_names[5], Point(-1, 1, 1), group);
-  add_sphere(tex_names[6], Point( 1, 1, 1), group);
-  add_sphere(tex_names[7], Point( 1,-1, 1), group);
+  if (!E) E |= add_sphere(tex_names[4], Point(-1,-1, 1), group);
+  if (!E) E |= add_sphere(tex_names[5], Point(-1, 1, 1), group);
+  if (!E) E |= add_sphere(tex_names[6], Point( 1, 1, 1), group);
+  if (!E) E |= add_sphere(tex_names[7], Point( 1,-1, 1), group);
   
-  return group;
+  if (!E)
+    return group;
+  else
+    return 0;
 }
 
 extern "C" 
@@ -80,6 +84,9 @@ Scene* make_scene(int argc, char** argv, int /*nworkers*/)
   }
   
   Group *group=make_geometry(tex_names);
+  if (!group)
+    // Then something went wrong and you should kill the scene
+    return 0;
 
   Camera cam(Point(0,0,10), Point(0,0,0), Vector(0,1,0), 45.0);
 
@@ -93,11 +100,20 @@ Scene* make_scene(int argc, char** argv, int /*nworkers*/)
     ambient_scale, Arc_Ambient);
 
   EnvironmentMapBackground *emap=new EnvironmentMapBackground(bg, Vector(0,1,0));
+  if (emap->valid() != true) {
+    // try a local copy
+    delete emap;
+    emap = new EnvironmentMapBackground("./envmap.ppm", Vector(0,1,0));
+    if (emap->valid() != true) {
+      return 0;
+    }
+  }
   scene->set_background_ptr(emap);
     
   Light* mainLight = new Light(Point(-5,10,7.5), Color(1,1,1), 1.0);
   mainLight->name_ = "main light";
   scene->add_light( mainLight );
+  scene->turnOffAllLights( 0.0 ); 
 
   scene->select_shadow_mode(No_Shadows);
   
