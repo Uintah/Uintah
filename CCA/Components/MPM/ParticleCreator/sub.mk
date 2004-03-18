aec1d1e3858976e391f6858b62237cb37474ced4
Jim Guilkey <guilkey@sci.utah.edu>
2004-03-18 18:41:45 +0000

    Figured out a better way to handle the creation of the fiber direction, so these files aren't needed.

--- CCA/Components/MPM/ParticleCreator/sub.mk
+++ CCA/Components/MPM/ParticleCreator/sub.mk
@@ -6,7 +6,6 @@ SRCS     += \
 	$(SRCDIR)/ParticleCreator.cc	\
 	$(SRCDIR)/DefaultParticleCreator.cc	\
 	$(SRCDIR)/ImplicitParticleCreator.cc	\
-	$(SRCDIR)/ExpTIParticleCreator.cc	\
 	$(SRCDIR)/FractureParticleCreator.cc	\
 	$(SRCDIR)/MembraneParticleCreator.cc	\
 	$(SRCDIR)/ShellParticleCreator.cc
