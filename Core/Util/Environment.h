b605d5cb60f310f8fd0a33c835063af48c1e19a2
J. Davison de St. Germain <dav@sci.utah.edu>
2004-03-25 00:16:04 +0000

    Added sci_getenv.  Moved in some comments.

--- Core/Util/Environment.h
+++ Core/Util/Environment.h
@@ -33,11 +33,32 @@
 
 namespace SCIRun {
   using std::string;
-  void store_existing_environment(char **environ);
+
   bool find_and_parse_scirunrc();
   bool parse_scirunrc(const string filename);
+
+  // sci_putenv will check the existing_env set string (filled in
+  // store_existing_env) If the environment key existed when the
+  // program was run, then it will not be overwritten.  This follows
+  // the Unix convention of using environment variables to supercede
+  // default program settings and .rc files.  Note we actually use tcl
+  // to store the environment so that it remains consistent on both
+  // the tcl and the C sides.  Therefore it is HIGHLY RECOMMENDED that
+  // you use the sci versions of putenv and getenv to keep things
+  // consistent.
   void sci_putenv(const string &key, const string &val, bool force=false);
+
+  // sci_getenv_p will lookup the value of the environment variable
+  // 'key' and returns false if the variable is equal to 'false',
+  // 'no', 'off', or '0', it returns true otherwise.  (Case
+  // insensitive)
   bool sci_getenv_p(const string &key);
+
+  // Use the following function to get environment variables.  If you
+  // don't, there is a chance that the Tcl and C side environments
+  // will not be aligned and you will get incorrect values.  Also, you
+  // must free the char* returned, or you will have a memory leak.
+  char *sci_getenv( const string & key );
 }
 
 #endif // #ifndef Core_Util_Environment_h
