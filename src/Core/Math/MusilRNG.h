2b8f6ba1b84d65ca4041cc73dda4bfe6a42e3988
J. Davison de St. Germain <dav@sci.utah.edu>
2004-03-25 00:13:13 +0000

    Fix indentation.  Add parameter name to constructor

--- src/Core/Math/MusilRNG.h
+++ src/Core/Math/MusilRNG.h
@@ -35,26 +35,25 @@
 #include <Core/share/share.h>
 
 class SCICORESHARE MusilRNG {
-	int d[16], n[16];
-	int stab[2][32];
-	int point;
-	int d1,d2,d3,d4,d5,d6,d7,d8,d9,d10,d11,d12;
-	int a1,b1;
-	int a2,b2;
-	int a3,b3;
-	int a4,b4;
-	int a5,b5;
-	int a6,b6;
-	int a7,b7;
-	int a8,b8;
-	int a9,b9;
-	int a10,b10;
-	int a11,b11;
-	int a12,b12;
+  int d[16], n[16];
+  int stab[2][32];
+  int point;
+  int d1,d2,d3,d4,d5,d6,d7,d8,d9,d10,d11,d12;
+  int a1,b1;
+  int a2,b2;
+  int a3,b3;
+  int a4,b4;
+  int a5,b5;
+  int a6,b6;
+  int a7,b7;
+  int a8,b8;
+  int a9,b9;
+  int a10,b10;
+  int a11,b11;
+  int a12,b12;
 public:
-	MusilRNG(int=0);
-	double operator()();
+  MusilRNG( int seed = 0 );
+  double operator()();
 };
 
 #endif
-
