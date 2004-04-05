67d01f2f61691664a1b70142646beb179d0246f5
James Bigler <bigler@sci.utah.edu>
2004-04-05 19:41:34 +0000

    Added multithreading for codeword search.  Read and write data as a nrrd.  Cleaned up unused verbose, stats, pedantic, and double distortion test bits of code.

--- Packages/rtrt/StandAlone/utils/vq.cc
+++ Packages/rtrt/StandAlone/utils/vq.cc
@@ -8,20 +8,10 @@
 #endif
 #define CHECK_VERBOSITY 1
 
-#ifdef PARTIAL_DISTORTION
-#  undef PARTIAL_DISTORTION
-#endif
-#define PARTIAL_DISTORTION 1
+#include <Core/Thread/Thread.h>
+#include <Core/Thread/WorkQueue.h>
 
-// #ifdef DOUBLE_TEST
-// #  undef DOUBLE_TEST
-// #endif
-// #define DOUBLE_TEST 1
-
-#ifdef COLLECT_STATS
-#  undef COLLECT_STATS
-#endif
-#define COLLECT_STATS 1
+#include <teem/nrrd.h>
 
 #include <fstream>
 #include <iostream>
@@ -36,47 +26,88 @@
 #include <sys/stat.h>
 
 using namespace std;
+using namespace SCIRun;
 
-#ifdef PARTIAL_DISTORTION
-#ifdef DOUBLE_TEST
-char *search_type="double test (with partial distortion)";
-#else
 char *search_type="standard test (with partial distortion)";
-#endif
-#else
-#ifdef DOUBLE_TEST
-char *search_type="double test";
-#else
-char *search_type="standard test";
-#endif
-#endif
+
+WorkQueue work("Work Queue");
+
+int num_dims=-1;
+int num_vecs=10;
+int num_cwords=2;
+// forward declare data structures for vectors
+int* ir_idx = 0;
+float* vec = 0;
+int* cluster_idx = 0;
+
+// Code word stuff
+float* prev_cw = 0;
+float* current_cw = 0;
+
+void assign_cw(int vector_index) {
+  float min=FLT_MAX;
+  int my_cluster=-1;
+  for (int j=0;j<num_cwords;j++) {
+    float distance=0;
+    // compute the real distortion
+    distance=0;
+    for (int k=0;k<num_dims;k++) {
+      float tmp=vec[vector_index*num_dims+k]-current_cw[j*num_dims+k];
+      distance+=tmp*tmp;
+      if (distance>min) {
+	break;
+      }
+    }
+    if (distance>min)
+      continue;
+	
+    // set the cluster for the vector
+    min=distance;
+    my_cluster=j;
+  }
+	
+  cluster_idx[vector_index]=my_cluster;
+}
+
+
+class ClusterParallel {
+public:
+  void cluster_it(int /*proc*/) {
+    int start, end;
+    while(work.nextAssignment(start, end))
+      for(int vector_index = start; vector_index < end; vector_index++)
+	assign_cw(vector_index);
+  }
+};
+
+ClusterParallel cp;
 
 int main(int argc, char *argv[]) {
+  char *me = argv[0];
+  char *err = 0;
   char *infilename=0;
-  char *cw_outfilename=0;
-  char *idx_outfilename=0;
-  int num_dims=2;
-  int num_vecs=10;
-  int num_cwords=2;
+  char *cw_outfilename="cw.nhdr";
+  char *idx_outfilename="cbidx.nhdr";
   float thresh=1e-5;
   int max_iteration = 100;
   int seed=12;
-#ifdef COLLECT_STATS
-  bool stats=false;
-#endif
-  int verbosity=0;
+  int np = 1;
 
   for (int i=1;i<argc;i++) {
     if (strcmp(argv[i], "-i")==0) {
       infilename=argv[++i];
+    } else if (strcmp(argv[i], "-np")==0) {
+      np=atoi(argv[++i]);
     } else if (strcmp(argv[i], "-cw_out")==0) {
       cw_outfilename=argv[++i];
     } else if (strcmp(argv[i], "-idx_out")==0) {
       idx_outfilename=argv[++i];
+#if 0
     } else if (strcmp(argv[i], "-ndims")==0) {
       num_dims=atoi(argv[++i]);
     } else if (strcmp(argv[i], "-nvecs")==0) {
       num_vecs=atoi(argv[++i]);
+#endif
     } else if (strcmp(argv[i], "-ncwords")==0) {
       num_cwords=atoi(argv[++i]);
     } else if (strcmp(argv[i], "-thresh")==0) {
@@ -85,12 +116,6 @@ int main(int argc, char *argv[]) {
       seed=atoi(argv[++i]);
     } else if (strcmp(argv[i], "-niters")==0) {
       max_iteration=atoi(argv[++i]);
-#ifdef COLLECT_STATS
-    } else if (strcmp(argv[i], "-stats")==0) {
-      stats=true;
-#endif
-    } else if (strcmp(argv[i], "-v")==0) {
-      verbosity=atoi(argv[++i]);
     } else if (strcmp(argv[i], "--search")==0) {
       cout<<"using "<<search_type<<" for codebook search"<<endl;
     } else {
@@ -99,54 +124,67 @@ int main(int argc, char *argv[]) {
       cerr<<"usage:  vq [options]"<<endl;
       cerr<<"  options:"<<endl;
       cerr<<"    -i <filename>         input filename (null)"<<endl;
+      cerr<<"    -np <int>             number of threads to use (1)"<<endl;
       cerr<<"    -cw_out <filename>    write codewords to file (null)"<<endl;
       cerr<<"    -idx_out <filename>   write vector indices to file (null)"<<endl;
+#if 0
       cerr<<"    -ndims <int>          dimensionality of the vectors (2)"<<endl;
       cerr<<"    -nvecs <int>          number of vectors to quantize (10)"<<endl;
+#endif
       cerr<<"    -ncwords <int>        number of codewords (2)"<<endl;
       cerr<<"    -thresh <float>       termination threshold (1e-5)"<<endl;
       cerr<<"    -niters <int>         maximum number of iterations (100)"<<endl;
       cerr<<"    -seed <int>           random number seed (12)"<<endl;
-#ifdef COLLECT_STATS
-      cerr<<"    -stats                report statistics (false)"<<endl;
-#endif      
-      cerr<<"    -v <int>              verbosity level (0)"<<endl;
       cerr<<"    --search              print codebook search method"<<endl;
       cerr<<"    --help                print this message and exit"<<endl;
       exit(1);
     }
   }
 
-  // forward declare data structures for vectors
-  float* vec;
-  int* cluster_idx;
-  int* ir_idx;
-
-  // allocate memory for codewords
-  float* prev_cw=new float[num_cwords*num_dims];
-  float* current_cw=new float[num_cwords*num_dims];
+  if (!infilename) {
+    cerr << "Need to specify an input filename\n";
+    exit(1);
+  }
 
   // seed the RNGs
   srand48(seed);
   srand(seed);
 
-  int in_fd=-1;
-  if (infilename) {
-    // read vectors from a file
-    in_fd=open(infilename, O_RDONLY);
-    if (in_fd==-1) {
-      cerr<<"failed to open file \""<<infilename<<"\" for reading"<<endl;
-      exit(1);
-    }
-    
-    struct stat statbuf;
-    if (fstat(in_fd, &statbuf)==-1) {
-      cerr<<"cannot stat file \""<<infilename<<"\""<<endl;
-      exit(1);
-    }
-    
-    num_vecs=(int)(statbuf.st_size/(num_dims*sizeof(float)));
+  // Load in the vectors
+  Nrrd *nvec = nrrdNew();
+  if (nrrdLoad(nvec, infilename, 0)) {
+    err = biffGet(NRRD);
+    fprintf(stderr, "%s: Error loading nrrd:\n%s", me, err);
+    exit(2);
   }
+  // Convert to float if need be
+  if (nvec->type != nrrdTypeFloat) {
+    Nrrd *new_n = nrrdNew();
+    if (nrrdConvert(new_n, nvec, nrrdTypeFloat)) {
+      err = biffGet(NRRD);
+      cerr << me << ": unable to convert nrrd: " << err << endl;
+      biffDone(NRRD);
+      exit(2);
+    }
+    // since the data was copied blow away the memory for the old nrrd
+    nrrdNuke(nvec);
+    nvec = new_n;
+  }
+  // Compute the dimensionality of the vectors.  Assume the last
+  // dimension is the number of vectors.
+  num_vecs = nvec->axis[nvec->dim-1].size;
+  num_dims = 1;
+  for(int i = 0; i < nvec->dim-1; i++)
+    num_dims *= nvec->axis[i].size;
+  cout << "num_vecs = "<<num_vecs<<", num_dims = "<<num_dims<<"\n";
+
+  size_t num_elements = nrrdElementNumber(nvec);
+  size_t num_elements_maybe = num_vecs * num_dims;
+  if (num_elements_maybe != num_elements) {
+    cerr << "num_vecs*num_dims ("<<num_elements_maybe<<") != num_elements ("<<num_elements<<")\n";
+    exit(2);
+  }
+  
 
   if (num_cwords>num_vecs) {
     cerr<<"number of codewords ("<<num_cwords<<") is greater than the "
@@ -154,53 +192,15 @@ int main(int argc, char *argv[]) {
     exit(1);
   }
   
+  // allocate memory for codewords
+  prev_cw=new float[num_cwords*num_dims];
+  current_cw=new float[num_cwords*num_dims];
+
   // allocate the memory for vector structures
-  vec=new float[num_vecs*num_dims];
+  vec=(float*)nvec->data;
   cluster_idx=new int[num_vecs];
   ir_idx=new int[num_vecs];
 
-  if (in_fd>-1) {
-    // slurp up the vector data
-    float* data=&(vec[0]);
-    size_t data_size=num_vecs*num_dims*sizeof(float);
-
-    if (verbosity>4) {
-      cout<<"slurping vector data ("<<num_vecs<<" vectors = " <<data_size
-	  <<" bytes) from "<<infilename<<endl;
-    }
-    size_t num_read=0;
-    num_read=read(in_fd, data, data_size);
-    if(num_read==-1 || num_read!=data_size) {
-      cerr<<"did not read "<<data_size<<" bytes from "
-	  <<infilename<<endl;
-      exit(1);
-    }
-
-    // close the file
-    close(in_fd);
-  }
-  else {
-    // create random vectors
-    for (int i=0;i<num_vecs;i++) {
-      for (int j=0;j<num_dims;j++) {
-	vec[i*num_dims+j]=drand48();
-      }
-    }
-  }
-
-  // output the vectors
-  if (verbosity>9) {
-    cout<<"Input vectors"<<endl;
-    cout<<"-----------------------"<<endl;
-    for (int i=0;i<num_vecs;i++) {
-      cout<<"vec["<<i<<"] = (";
-      for (int j=0;j<num_dims-1;j++)
-	cout<<vec[i*num_dims+j]<<", ";
-      cout<<vec[i*num_dims+num_dims-1]<<")"<<endl;
-    }
-    cout<<endl;
-  }
-  
   // randomly choose the initial codewords
   for (int i=0;i<num_vecs;i++)
     ir_idx[i] = i;
@@ -217,153 +217,24 @@ int main(int argc, char *argv[]) {
     for (int j=0;j<num_dims;j++)
       current_cw[i*num_dims+j] = vec[ir_idx[i]*num_dims+j];
 
-  // output the initial codewords
-  if (verbosity>9) {
-    cout<<"Initial codewords"<<endl;
-    cout<<"-----------------------"<<endl;    
-    for (int i=0;i<num_cwords;i++) {
-      cout<<"codeword["<<i<<"] = (";
-      for (int j=0;j<num_dims-1;j++)
-	cout<<current_cw[i*num_dims+j]<<", ";
-      cout<<current_cw[i*num_dims+num_dims-1]<<")"<<endl;
-    }
-    cout<<endl;
-  }
-
-#ifdef DOUBLE_TEST  
-  // precompute the maximum component of each vector and
-  // twice the sum of each vector's components
-  float* vec_max=new float[num_vecs];
-  float* vec_2sum=new float[num_vecs];
-  for (int i=0;i<num_vecs;i++)
-    vec_max[i]=0;
-  
-  for (int i=0;i<num_vecs;i++) {
-    float tmp=0;
-    for (int j=0;j<num_dims;j++) {
-      if (vec[i*num_dims+j]>vec_max[i])
-	vec_max[i]=vec[i*num_dims+j];
-      
-      tmp+=vec[i*num_dims+j];
-    }
-    
-    vec_2sum[i]=2*tmp;
-  }
-#endif  
-  
   // cluster the vectors around the codewords
   bool done=false;
   bool first=true;
   unsigned int iteration=0;
   float* tmp_mean=new float[num_dims];
-#ifdef DOUBLE_TEST  
-  float* codeword_mag=new float[num_cwords];
-  float* codeword_max=new float[num_cwords];
-  float* codeword_2sum=new float[num_cwords];
-#endif
-#ifdef COLLECT_STATS
-  unsigned long real_distortion_cnt=0;
-  unsigned long partial_distortion_cnt=0;
-#ifdef DOUBLE_TEST  
-  unsigned long early_exit_d1=0;
-  unsigned long early_exit_d2=0;
-#endif
-#endif
+
   do {
-#ifdef CHECK_VERBOSITY
     // output a status message
-    if (verbosity>4)
-      cout<<"Beginning iteration "<<iteration<<endl;
-#endif
+    cout<<"Beginning iteration "<<iteration<<endl;
     
-#ifdef DOUBLE_TEST    
-    // compute the magnitude and maximum component of each codeword, and
-    // twice the sum of each codeword's components
-    for (int i=0;i<num_cwords;i++)
-      codeword_max[i]=0;
+    if (np > 1) {
+      work.refill(num_vecs, np);
 
-    for (int i=0;i<num_cwords;i++) {
-      float tmp=0;
-      for (int j=0;j<num_dims;j++) {
-        if (current_cw[i*num_dims+j]>codeword_max[i])
-          codeword_max[i]=current_cw[i*num_dims+j];
-	
-	tmp+=current_cw[i*num_dims+j];
-      }
-
-      codeword_mag[i]=sqrt(tmp);
-      codeword_2sum[i]=2*tmp;
-    }
-#endif    
-    
-    for (int i=0;i<num_vecs;i++) {
-      float min=FLT_MAX;
-      int my_cluster=-1;
-      for (int j=0;j<num_cwords;j++) {
-	float distance=0;
-#ifdef DOUBLE_TEST	
-	// compute the first distortion
-	distance=codeword_mag[j]-vec_max[i]*codeword_2sum[j];
-	if (distance>min) {
-#ifdef COLLECT_STATS	  
-	  early_exit_d1++;
-#endif
-	  continue;
-	}
-
-	// compute the second distortion
-	distance=codeword_mag[j]-codeword_max[j]*vec_2sum[i];
-	if (distance>min) {
-#ifdef COLLECT_STATS	  
-	  early_exit_d2++;
-#endif
-	  continue;
-	}
-#endif	
-
-	// compute the real distortion
-#ifdef COLLECT_STATS
-	real_distortion_cnt++;
-#endif
-	distance=0;
-	for (int k=0;k<num_dims;k++) {
-	  float tmp=vec[i*num_dims+k]-current_cw[j*num_dims+k];
-	  distance+=tmp*tmp;
-#ifdef PARTIAL_DISTORTION
-	  if (distance>min) {
-#ifdef COLLECT_STATS
-	    partial_distortion_cnt+=num_dims-k;
-#endif
-	    break;
-	  }
-#endif
-        }
-	if (distance>min)
-	  continue;
-	
-	// set the cluster for the vector
-	min=distance;
-	my_cluster=j;
-	
-#ifdef PEDANTIC	
-        if (verbosity > 4 && min==0 && iteration==0)
-          cout<<"found initial codeword for cluster["<<j<<"]"<<endl;
-#endif	
-      }
-
-#ifdef PEDANTIC      
-      if (my_cluster>=0) {
-        cluster_idx[i]=my_cluster;
-        if (verbosity > 4)
-          cout << "placing vector["<<i<<"] in cluster["<<cluster_idx[i]
-	       <<"]"<<endl;
-      } else {
-	cerr << "couldn't determine a cluster for vector["<<i<<"]"<<endl;
-	exit(2);
-      }
-#else
-      cluster_idx[i]=my_cluster;
-#endif
+      Parallel<ClusterParallel> phelper(&cp, &ClusterParallel::cluster_it);
+      Thread::parallel(phelper, np, true);
+    } else {
+      for (int i=0;i<num_vecs;i++)
+	assign_cw(i);
     }
     
     // compute the mean vector of each cluster
@@ -382,22 +253,14 @@ int main(int argc, char *argv[]) {
 	}
       }
 
-#ifdef PEDANTIC      
-      // sanity check
-      if (iteration == 0 && !count) {
-	cerr<<"cluster["<<i<<"] is empty in iteration 0"<<endl;
-	exit(2);
-      }
-#endif      
-      
       // update codewords
+      done=true;
       for (int k=0;k<num_dims;k++) {
 	prev_cw[i*num_dims+k]=current_cw[i*num_dims+k];
 	current_cw[i*num_dims+k]=tmp_mean[k]/(float)count;
 	
 	// compute the difference between iterations
 	if (!first) {
-	  done=true;
 	  done&=((current_cw[i*num_dims+k]-prev_cw[i*num_dims+k])<thresh);
 	} else {
 	  first=false;
@@ -406,109 +269,33 @@ int main(int argc, char *argv[]) {
       }
     }
 
-#ifdef CHECK_VERBOSITY    
-    // output the codewords for the next iteration
-    if (verbosity>4) {
-      cout<<"Codewords (iteration="<<iteration<<")"<<endl;
-      cout<<"-----------------------"<<endl;
-      for (int i=0;i<num_cwords;i++) {
-	cout<<"codeword["<<i<<"] = (";
-	for (int j=0;j<num_dims-1;j++)
-	  cout<<current_cw[i*num_dims+j]<<", ";
-	cout<<current_cw[i*num_dims+num_dims-1]<<")"<<endl;
-      }
-      cout<<endl;
-    }
-#endif
-    
     iteration++;
   } while (!done && iteration < max_iteration);
 
-#ifdef COLLECT_STATS
-  // output early exit statistics
-  if (stats) {
-    cout<<"----------------------------------------------------"<<endl;
-#ifdef DOUBLE_TEST
-    cout<<"           First distortion early exits = "<<early_exit_d1<<endl;
-    cout<<"          Second distortion early exits = "<<early_exit_d2<<endl;
-    cout<<"                      Total early exits = "
-	<<(early_exit_d1+early_exit_d2)<<endl;
-    cout<<endl;
-#endif
-    cout<<"           Real distortion computations = "
-	<<real_distortion_cnt<<endl;
-#ifdef PARTIAL_DISTORTION
-    cout<<" Iterations saved by partial distortion = "
-	<<partial_distortion_cnt<<endl;
-#endif
-    cout<<"----------------------------------------------------"<<endl;
-    cout<<endl;
-  }
-#endif
-  
-  // output the final codewords
-  if (verbosity>9) {
-    cout<<"Codewords (after "<<iteration<<" iterations)";
-    cout<<endl;
-    cout<<"-----------------------"<<endl;
-    for (int i=0;i<num_cwords;i++) {
-      cout<<"codeword["<<i<<"] = (";
-      for (int j=0;j<num_dims-1;j++)
-	cout<<current_cw[i*num_dims+j]<<", ";
-      cout<<current_cw[i*num_dims+num_dims-1]<<")"<<endl;
-    }
+  // Write out codebook
+  Nrrd *nout = nrrdNew();
+  if (nrrdWrap(nout, current_cw, nrrdTypeFloat, 2, num_dims, num_cwords) ||
+      nrrdSave(cw_outfilename, nout, 0)) {
+    err = biffGet(NRRD);
+    fprintf(stderr, "%s: Error wrapping or saving codebook:\n%s", me, err);
+    exit(2);
   }
 
-  // write out codebook
-  if (cw_outfilename) {
-    FILE *cw_outfile=fopen(cw_outfilename, "wb");
-    if (cw_outfile) {
-      unsigned long num_bytes=fwrite(current_cw, sizeof(float),
-				     num_cwords*num_dims, cw_outfile);
-      if (num_bytes!=num_cwords*num_dims) {
-	cerr<<"couldn't write all "<<num_cwords<<" codewords"<<endl;
-      } else {
-	cout<<"wrote "<<num_cwords<<" codewords to \""<<cw_outfilename
-	    <<"\""<<endl;
-      }
-      
-      // close the file
-      fclose(cw_outfile);
-    }
-  }
-
-  // write out cluster indices
-  if (idx_outfilename) {
-    FILE *idx_outfile=fopen(idx_outfilename, "wb");
-    if (idx_outfile) {
-      unsigned long num_bytes=fwrite(cluster_idx, sizeof(int),
-				     num_vecs, idx_outfile);
-      if (num_bytes!=num_vecs) {
-	cerr<<"couldn't write all "<<num_vecs<<" indices"<<endl;
-      } else {
-	cout<<"wrote "<<num_vecs<<" indices to \""<<idx_outfilename
-	    <<"\""<<endl;
-      }
-      
-      // close the file
-      fclose(idx_outfile);
-    }
+  // Write out the index file
+  if (nrrdWrap(nout, cluster_idx, nrrdTypeInt, 1, num_vecs) ||
+      nrrdSave(idx_outfilename, nout, 0)) {
+    err = biffGet(NRRD);
+    fprintf(stderr, "%s: Error wrapping or saving index file:\n%s", me, err);
+    exit(2);
   }
   
   // delete allocated memory
-  delete vec;
+  nrrdNuke(nvec);
   delete cluster_idx;
   delete ir_idx;
   delete prev_cw;
   delete current_cw;
   delete tmp_mean;
-#ifdef DOUBLE_TEST  
-  delete vec_max;
-  delete vec_2sum;
-  delete codeword_mag;
-  delete codeword_max;
-  delete codeword_2sum;
-#endif
   
   return 0;
 }
