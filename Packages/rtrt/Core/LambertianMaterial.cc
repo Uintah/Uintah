734a1757837b080f84c02c2972dfebf40faf1101
James Bigler <bigler@sci.utah.edu>
2004-04-07 06:27:26 +0000

    Make use of lambertianshade function in Material class now.

--- Packages/rtrt/Core/LambertianMaterial.cc
+++ Packages/rtrt/Core/LambertianMaterial.cc
@@ -33,51 +33,7 @@ void LambertianMaterial::shade(Color& result, const Ray& ray,
 		  double , const Color& ,
 		  Context* cx)
 {
-
-    double nearest=hit.min_t;
-    Object* obj=hit.hit_obj;
-    Point hitpos(ray.origin()+ray.direction()*nearest);
-    Vector normal(obj->normal(hitpos, hit));
-    double incident_angle=-Dot(normal, ray.direction());
-    if(incident_angle<0){
-	incident_angle=-incident_angle;
-	normal=-normal;
-    }
-    double ray_objnormal_dot(Dot(ray.direction(),normal));
-    
-    result = R * ambient(cx->scene, normal);
-    int ngloblights=cx->scene->nlights();
-    int nloclights=my_lights.size();
-    int nlights=ngloblights+nloclights;
-    cx->stats->ds[depth].nshadow+=nlights;
-    for(int i=0;i<nlights;i++){
-        Light* light;
-        if (i<ngloblights)
-	  light=cx->scene->light(i);
-        else 
-	  light=my_lights[i-ngloblights];
-
-	if( !light->isOn() )
-	  continue;
-
-        Vector light_dir=light->get_pos()-hitpos;
-	if (ray_objnormal_dot*Dot(normal,light_dir)>0) {
-	  cx->stats->ds[depth].inshadow++;
-	  continue;
-	}
-	double dist=light_dir.normalize();
-	Color shadowfactor(1,1,1);
-	if(cx->scene->lit(hitpos, light, light_dir, dist, shadowfactor, depth, cx) ){
-	    double cos_theta=Dot(light_dir, normal);
-	    if(cos_theta < 0){
-		cos_theta=-cos_theta;
-		light_dir=-light_dir;
-	    }
-	    result+=light->get_color(light_dir)*R*(cos_theta*shadowfactor);
-	} else {
-	    cx->stats->ds[depth].inshadow++;
-	}
-    }
+  lambertianshade(result, R, ray, hit, depth, cx);
 }
 
 
