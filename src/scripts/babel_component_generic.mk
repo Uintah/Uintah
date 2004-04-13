147a597b5d46e996aca942abc8e9cd3295ad3dad
Joshua Cates <cates@sci.utah.edu>
2004-04-13 17:40:04 +0000

    ENH: Changes to enable compatibility with babel 0.9.0.  Backwards compatibility with previous versions of babel should be preserved.

--- src/scripts/babel_component_generic.mk
+++ src/scripts/babel_component_generic.mk
@@ -5,16 +5,23 @@ SRCS := $(XSRCS)
 
 COMPONENT:=$(notdir $(SRCDIR))
 
-OUTDIR := ${SRCTOP_ABS}/${SRCDIR}
+#OUTDIR := ${SRCTOP_ABS}/${SRCDIR}
+OUTDIR := ${OBJTOP_ABS}/${SRCDIR}
 
 $(COMPONENT)_LANGUAGE := $(BABEL_LANGUAGE)
 
-${OUTDIR}/${COMPONENT}.make: ${OUTDIR}/${COMPONENT}.sidl Core/Babel/timestamp
-	$(BABEL) -s$($(basename $(notdir $@))_LANGUAGE) -o$(dir $@) -R${BABEL_REPOSITORY} $<	
+# This rule uses the babel compiler to generate the language-specific glue and implementation
+# files.  Since implementation code (*Impl*) already exists in the source tree, we first copy this existing
+# code to the babel target output directory.  Babel will not overwrite critical sections of the 
+# existing implementation code.
+${OUTDIR}/${COMPONENT}.make: ${SRCDIR}/${COMPONENT}.sidl Core/Babel/timestamp
+	if ! test -d $(dir $@); then mkdir -p $(dir $@); fi
+	cp -u $(dir $<)*Impl.* $(dir $@)
+	$(BABEL) --server=$($(basename $(notdir $@))_LANGUAGE) --output-directory=$(dir $@) --repository-path=${BABEL_REPOSITORY} --hide-glue --suppress-timestamp --language-subdir $<
 	mv  $(dir $@)babel.make $@
 
-${OUTDIR}/${COMPONENT}_stub.make: ${OUTDIR}/${COMPONENT}.sidl Core/Babel/timestamp
-	$(BABEL) -cC++ -o$(dir $@) -R${BABEL_REPOSITORY} $<	
+${OUTDIR}/${COMPONENT}_stub.make: ${SRCDIR}/${COMPONENT}.sidl Core/Babel/timestamp
+	$(BABEL) --client=C++ --output-directory=$(dir $@) --repository-path=${BABEL_REPOSITORY} $< 
 	mv  $(dir $@)babel.make $@
 
 #${OUTDIR}/cca.make: ${CCASIDL} 
