76c2265b4b0d975993ac43dee1ea9f7c9853d3d4
Steven G. Parker <sparker@sci.utah.edu>
2004-04-15 16:19:48 +0000

    Moved Labels to Packages/Uintah/Core/Labels Removed unused NotFinished.h Mac fixes

--- Packages/Uintah/CCA/Components/MPMICE/MPMICERF.cc
+++ Packages/Uintah/CCA/Components/MPMICE/MPMICERF.cc
@@ -2,6 +2,7 @@
 #include <Packages/Uintah/CCA/Components/MPMICE/MPMICE.h>
 #include <Packages/Uintah/CCA/Components/ICE/ICEMaterial.h>
 #include <Packages/Uintah/CCA/Components/ICE/BoundaryCond.h>
+#include <Packages/Uintah/CCA/Components/ICE/EOS/EquationOfState.h>
 #include <Packages/Uintah/CCA/Components/MPM/ConstitutiveModel/MPMMaterial.h>
 #include <Packages/Uintah/CCA/Components/MPM/ConstitutiveModel/ConstitutiveModel.h>
 #include <Packages/Uintah/Core/Grid/Task.h>
--- Packages/Uintah/CCA/Components/MPMICE/sub.mk
+++ Packages/Uintah/CCA/Components/MPMICE/sub.mk
@@ -7,12 +7,13 @@ SRCDIR   := Packages/Uintah/CCA/Components/MPMICE
 SRCS     += \
 	$(SRCDIR)/MPMICE.cc \
 	$(SRCDIR)/MPMICERF.cc \
-	$(SRCDIR)/MPMICELabel.cc \
        $(SRCDIR)/MPMICEDebug.cc \
 
 PSELIBS := \
 	Packages/Uintah/CCA/Ports          \
+	Packages/Uintah/Core/Disclosure    \
 	Packages/Uintah/Core/Grid          \
+	Packages/Uintah/Core/Labels        \
 	Packages/Uintah/Core/Parallel      \
 	Packages/Uintah/Core/ProblemSpec   \
 	Packages/Uintah/Core/Exceptions    \
@@ -24,7 +25,7 @@ PSELIBS := \
 	Core/Geometry   \
 	Core/Util
 
-LIBS := $(XML_LIBRARY) $(M_LIBRARY)
+LIBS := $(XML_LIBRARY) $(MPI_LIBRARY) $(M_LIBRARY)
 
 include $(SCIRUN_SCRIPTS)/smallso_epilogue.mk
 
