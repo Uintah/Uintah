1a7268603729149ebb7cb240c3b45a81cbbe94fd
Andrew Brydon <abrydon@sci.utah.edu>
2004-04-22 19:02:04 +0000

    Allow format of point file to be specified as text, split or binary. The split is the default, since that is the current format.
    Generalize the <var> fields to allow them to be read in any
    order. Should make extra fields easier to implement.
    
    Modify the format of the example in the comments to make them
    consistent with other object.

--- Core/Grid/GeomPiece/FileGeometryPiece.cc
+++ Core/Grid/GeomPiece/FileGeometryPiece.cc
@@ -2,9 +2,11 @@
 #include <Packages/Uintah/Core/ProblemSpec/ProblemSpec.h>
 #include <Packages/Uintah/Core/Exceptions/ProblemSetupException.h>
 #include <Core/Malloc/Allocator.h>
+#include <Core/Util/Endian.h>
 #include <sgi_stl_warnings_off.h>
 #include <fstream>
 #include <iostream>
+#include <sstream>
 #include <sgi_stl_warnings_on.h>
 
 using namespace Uintah;
@@ -12,44 +14,66 @@ using namespace SCIRun;
 using namespace std;
 using std::ifstream;
 
+static string numbered_str(const string & s, int is)
+{
+  ostringstream b;
+  b << s << is;
+  return b.str();
+}
+
 FileGeometryPiece::FileGeometryPiece(ProblemSpecP& ps)
 {
   setName("file");
   ps->require("name",d_file_name);
-  ps->get("var1",d_var_name1);
-  ps->get("var2",d_var_name2);
-  ps->get("var3",d_var_name3);
-  d_var1_bool = false;
-  d_var2_bool = false;
-  d_var3_bool = false;
-  if(d_var_name1 == "p.volume"){
-    d_var1_bool=true;
+  
+  int iparam=0;
+  string next_var_name;
+  while(ps->get(numbered_str("var", iparam++), next_var_name))
+  {
+    if      (next_var_name=="p.volume")         d_vars.push_back(IVvolume);
+    else if (next_var_name=="p.externalforces") d_vars.push_back(IVextforces);
+    else if (next_var_name=="p.fiberdir")       d_vars.push_back(IVfiberdirn);
+    else 
+      throw ProblemSetupException("Unexpected field variable of '"+next_var_name+"'");
   }
-  if(d_var_name2 == "p.externalforce"){
-    d_var2_bool=true;
+  
+  d_file_format = FFSplit; // default expects input to have been been processed with pfs
+  string fformat_txt;
+  if(ps->get("format",fformat_txt)) {
+    if     (fformat_txt=="split") d_file_format = FFSplit;
+    else if(fformat_txt=="text")  d_file_format = FFText;
+    else if(fformat_txt=="lsb")   d_file_format = FFLSBBin;
+    else if(fformat_txt=="msb")   d_file_format = FFMSBBin;
+    else if(fformat_txt=="gzip")  d_file_format = FFGzip;
+    else
+      throw ProblemSetupException("Unexpected file geometry format");
   }
-  if(d_var_name3 == "p.fiberdir"){
-    d_var3_bool=true;
+  
+  if(d_file_format==FFSplit) {
+    // read points now to find bounding box
+    
+    // We must first read in the min and max from file.0 so
+    // that we can determine the BoundingBox for the geometry
+    string file_name = numbered_str(d_file_name+".", 0);
+    ifstream source(file_name.c_str());
+    if (!source ){
+      throw ProblemSetupException("ERROR: opening MPM geometry file '"+file_name+"'\nFailed to find point file");
+    }
+    
+    Point min, max;
+    source >> min(0) >> min(1) >> min(2) >> max(0) >> max(1) >> max(2);
+    source.close();
+    
+    Vector fudge(1.e-5,1.e-5,1.e-5);
+    min = min - fudge;
+    max = max + fudge;
+    d_box = Box(min,max);
+    cout << "bbox of " << min << " " << max << endl;
+    
+  } else {
+    // if not using split format, have to read points now to find bounding box
+    readPoints(-1);
   }
-
-  // We must first read in the min and max from file.0 so
-  // that we can determine the BoundingBox for the geometry
-  char fnum[5];
-  sprintf(fnum,".%d",0);
-  string file_name = d_file_name+fnum;
-  ifstream source(file_name.c_str());
-  if (!source ){
-    throw ProblemSetupException("ERROR: opening MPM geometry file: \n The file must be in the same directory as sus");
-  }
-  double minx,miny,minz,maxx,maxy,maxz;
-  source >> minx >> miny >> minz >> maxx >> maxy >> maxz;
-  source.close();
-  Point min(minx,miny,minz),max(maxx,maxy,maxz);
-  Vector fudge(1.e-5,1.e-5,1.e-5);
-  min = min - fudge;
-  max = max + fudge;
-  d_box = Box(min,max);
-  cout << min << " " << max << endl;
 }
 
 FileGeometryPiece::FileGeometryPiece(const string& file_name)
@@ -68,7 +92,6 @@ FileGeometryPiece* FileGeometryPiece::clone()
 bool FileGeometryPiece::inside(const Point& p) const
 {
   //Check p with the lower coordinates
-
   if (p == Max(p,d_box.lower()) && p == Min(p,d_box.upper()) )
     return true;
   else
@@ -80,64 +103,127 @@ Box FileGeometryPiece::getBoundingBox() const
   return d_box;
 }
 
+bool
+FileGeometryPiece::read_line(istream & is, Point & xmin, Point & xmax)
+{
+  double x1,x2,x3;
+  if(d_file_format==FFSplit || d_file_format==FFText) {
+    double v1,v2,v3;
+    
+    // line always starts with coordinates
+    is >> x1 >> x2 >> x3;
+    if(is.eof()) return false; // out of points
+    d_points.push_back(Point(x1,x2,x3));
+    
+    for(list<InputVar>::const_iterator vit(d_vars.begin());vit!=d_vars.end();vit++) {
+      if       (*vit==IVvolume) {
+        if(is >> v1) d_volume.push_back(v1);
+      } else if(*vit==IVextforces) {
+        if(is >> v1 >> v2 >> v3) d_forces.push_back(Vector(v1,v2,v3));
+      } else if(*vit==IVfiberdirn) {
+        if(is >> v1 >> v2 >> v3) d_fiberdirs.push_back(Vector(v1,v2,v3));
+      }
+      if(!is)
+        throw ProblemSetupException("Failed while reading point text point file");
+    }
+    
+  } else if(d_file_format==FFLSBBin || d_file_format==FFMSBBin) {
+    // read unformatted binary numbers
+    
+    double v[3];
+    
+    // never changes, should save this !
+    const bool iamlittle = isLittleEndian();
+    const bool needflip = (iamlittle && (d_file_format==FFMSBBin)) || (!iamlittle && (d_file_format==FFLSBBin));
+    
+    is.read((char*)&x1, sizeof(double)); if(!is) return false;
+    is.read((char*)&x2, sizeof(double));
+    is.read((char*)&x3, sizeof(double));
+    
+    d_points.push_back(Point(x1,x2,x3));
+    
+    for(list<InputVar>::const_iterator vit(d_vars.begin());vit!=d_vars.end();vit++) {
+      if (*vit==IVvolume) {
+        if(is.read((char*)&v[0], sizeof(double))) {
+          if(needflip) swapbytes(v[0]);
+          d_volume.push_back(v[0]);
+      } else if(*vit==IVextforces) {
+        if(is.read((char*)&v[0], sizeof(double)*3)) {
+          if(needflip) {
+            swapbytes(v[0]);
+            swapbytes(v[1]);
+            swapbytes(v[2]);
+          }
+          d_forces.push_back(Vector(v[0],v[1],v[2]));
+        }
+      } else if(*vit==IVfiberdirn) {
+        if(is.read((char*)&v[0], sizeof(double)*3)) {
+          if(needflip) {
+            swapbytes(v[0]);
+            swapbytes(v[1]);
+            swapbytes(v[2]);
+          }
+          d_fiberdirs.push_back(Vector(v[0],v[1],v[2]));
+        }
+      }
+      if(!is)
+        throw ProblemSetupException("Failed while reading point text point file");
+      }
+    }
+    
+  } else if(d_file_format==FFGzip) {
+    throw ProblemSetupException("Sorry - gzip not implemented !");
+  }
+  
+  xmin = Min(xmin, Point(x1,x2,x3));
+  xmax = Max(xmax, Point(x1,x2,x3));
+  return true;
+}
+
 void FileGeometryPiece::readPoints(int pid)
 {
-  char fnum[5];
-  sprintf(fnum,".%d",pid);
-  string file_name = d_file_name+fnum;
-  ifstream source(file_name.c_str());
+  if(d_points.size()>0) return; // have already read points
+  
+  ifstream source;
+  
+  Point minpt( 1e30, 1e30, 1e30);
+  Point maxpt(-1e30,-1e30,-1e30);
+  
+  string file_name;
+  if(d_file_format==FFSplit) {
+    // pre-processed split point files
+    char fnum[5];
+    sprintf(fnum,".%d",pid);
+    file_name = d_file_name+fnum;
+    
+    source.open(file_name.c_str());
+    
+    // re-reading these
+    source >> minpt(0) >> minpt(1) >> minpt(2) >> maxpt(0) >> maxpt(1) >> maxpt(2);
+    
+  } else {
+    file_name = d_file_name;
+    source.open(file_name.c_str());
+  }    
+  
   if (!source ){
-    throw ProblemSetupException("ERROR: opening MPM geometry file:  The file must be in the same directory as sus");
+    throw ProblemSetupException("ERROR: opening MPM point file '"+file_name+"'\n:  The file must be in the same directory as sus");
   }
-
-  double x,y,z,vol,fx,fy,fz,fibx,fiby,fibz;
-  double minx,miny,minz,maxx,maxy,maxz;
-  source >> minx >> miny >> minz >> maxx >> maxy >> maxz;
-  Point min(minx,miny,minz),max(maxx,maxy,maxz);
-  Vector fudge(1.e-5,1.e-5,1.e-5);
-  min = min - fudge;
-  max = max + fudge;
-  d_box = Box(min,max);
-
-  if(d_var1_bool==true && d_var2_bool == false && d_var3_bool == false){
-    while(source >> x >> y >> z >> vol) {
-      d_points.push_back(Point(x,y,z));
-      d_volume.push_back(vol);
-    }
+  
+  int readpts = 0;
+  while(source) {
+    read_line(source, minpt, maxpt);
+    if(source)
+      readpts++;
   }
-  if(d_var1_bool==false && d_var2_bool == true && d_var3_bool == false){
-    while(source >> x >> y >> z >> fx >> fy >> fz) {
-      d_points.push_back(Point(x,y,z));
-      d_forces.push_back(Vector(-fx,-fy,-fz));
-    }
+  
+  if(d_file_format!=FFSplit) {
+    Vector fudge(1.e-5,1.e-5,1.e-5);
+    d_box = Box(minpt-fudge,maxpt+fudge);
   }
-  if(d_var1_bool==false && d_var2_bool == true && d_var3_bool == true){
-    while(source >> x >> y >> z >> fx >> fy >> fz >> fibx >> fiby >> fibz) {
-      d_points.push_back(Point(x,y,z));
-      d_forces.push_back(Vector(-fx,-fy,-fz));
-      d_fiberdirs.push_back(Vector(fibx,fiby,fibz));
-    }
-  }
-  if(d_var1_bool==false && d_var2_bool == false && d_var3_bool == true){
-    while(source >> x >> y >> z >> fibx >> fiby >> fibz) {
-      d_points.push_back(Point(x,y,z));
-      d_fiberdirs.push_back(Vector(fibx,fiby,fibz));
-    }
-  }
-  if(d_var1_bool==true && d_var2_bool == true && d_var3_bool == true){
-    while(source >> x >> y >> z >> vol >> fx >> fy >> fz >> fibx >> fiby >> fibz) {
-      d_volume.push_back(vol);
-      d_points.push_back(Point(x,y,z));
-      d_forces.push_back(Vector(-fx,-fy,-fz));
-      d_fiberdirs.push_back(Vector(fibx,fiby,fibz));
-    }
-  }
-  if(d_var1_bool==false && d_var2_bool == false && d_var3_bool == false){
-    while (source >> x >> y >> z) {
-      d_points.push_back(Point(x,y,z));
-    }
-  }
-  source.close();
+  
+  cerr << "   Read " << readpts << " from file " << endl;
+  cerr << "   Box = " << d_box << endl;
 }
 
 int FileGeometryPiece::createPoints()
