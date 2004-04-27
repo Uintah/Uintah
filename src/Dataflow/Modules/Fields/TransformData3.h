1a621631c8acd9bcaa33c9499f3a5b952a2f347d
Michael Callahan <callahan@sci.utah.edu>
2004-04-27 22:00:59 +0000

    Fix typo for third input field type

--- src/Dataflow/Modules/Fields/TransformData3.h
+++ src/Dataflow/Modules/Fields/TransformData3.h
@@ -73,7 +73,7 @@ execute(FieldHandle f0_h, FieldHandle f1_h, FieldHandle f2_h)
 {
   IFIELD0 *ifield0 = dynamic_cast<IFIELD0 *>(f0_h.get_rep());
   IFIELD1 *ifield1 = dynamic_cast<IFIELD1 *>(f1_h.get_rep());
-  IFIELD1 *ifield2 = dynamic_cast<IFIELD2 *>(f2_h.get_rep());
+  IFIELD2 *ifield2 = dynamic_cast<IFIELD2 *>(f2_h.get_rep());
   typename IFIELD0::mesh_handle_type imesh = ifield0->get_typed_mesh();
   OFIELD *ofield = scinew OFIELD(imesh, ifield0->data_at());
 
