4ceebf0948339ed70bbcd19ff9af0bd01307f0de
Jim Guilkey <guilkey@sci.utah.edu>
2004-04-30 23:38:46 +0000

    Added a simpler version of PressureBC which allows a load curve to be used with particles where a surface normal is already known.  Still a bit of a hack, but better than before.

--- src/Packages/Uintah/CCA/Components/MPM/PhysicalBC/MPMPhysicalBCFactory.cc
+++ src/Packages/Uintah/CCA/Components/MPM/PhysicalBC/MPMPhysicalBCFactory.cc
@@ -1,6 +1,7 @@
 #include <Packages/Uintah/CCA/Components/MPM/PhysicalBC/MPMPhysicalBCFactory.h>
 
 #include <Packages/Uintah/CCA/Components/MPM/PhysicalBC/ForceBC.h>
+#include <Packages/Uintah/CCA/Components/MPM/PhysicalBC/NormalForceBC.h>
 #include <Packages/Uintah/CCA/Components/MPM/PhysicalBC/PressureBC.h>
 #include <Packages/Uintah/CCA/Components/MPM/PhysicalBC/CrackBC.h>
 #include <Packages/Uintah/Core/ProblemSpec/ProblemSpec.h>
@@ -25,6 +26,12 @@ void MPMPhysicalBCFactory::create(const ProblemSpecP& ps)
        child = child->findNextBlock("force") )
    {
       mpmPhysicalBCs.push_back(scinew ForceBC(child));
+   }
+
+   for(ProblemSpecP child = current_ps->findBlock("normal_force"); child != 0;
+       child = child->findNextBlock("normal_force") )
+   {
+      mpmPhysicalBCs.push_back(scinew NormalForceBC(child));
    }
 
    for(ProblemSpecP child = current_ps->findBlock("pressure"); child != 0;
