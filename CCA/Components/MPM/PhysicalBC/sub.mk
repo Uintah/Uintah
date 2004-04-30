9b5dda0fcd5b7dac7092d84e7868583b32736349
Jim Guilkey <guilkey@sci.utah.edu>
2004-04-30 23:38:46 +0000

    Added a simpler version of PressureBC which allows a load curve to be used with particles where a surface normal is already known.  Still a bit of a hack, but better than before.

--- CCA/Components/MPM/PhysicalBC/sub.mk
+++ CCA/Components/MPM/PhysicalBC/sub.mk
@@ -5,6 +5,7 @@ SRCDIR   := Packages/Uintah/CCA/Components/MPM/PhysicalBC
 SRCS     += \
 	$(SRCDIR)/MPMPhysicalBCFactory.cc \
 	$(SRCDIR)/ForceBC.cc              \
+	$(SRCDIR)/NormalForceBC.cc              \
 	$(SRCDIR)/PressureBC.cc              \
 	$(SRCDIR)/CrackBC.cc
 
