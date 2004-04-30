1944b0f0871319a3cfee2eaac8e6d40b78943473
Michael Callahan <callahan@sci.utah.edu>
2004-04-30 19:55:10 +0000

    Remove unneeded Node::array_type constructors, cleanup get_nodes usage

--- Packages/BioPSE/Dataflow/Modules/Forward/DipoleMatrixSourceRHSQuadratic.cc
+++ Packages/BioPSE/Dataflow/Modules/Forward/DipoleMatrixSourceRHSQuadratic.cc
@@ -159,7 +159,7 @@ DipoleMatrixSourceRHSQuadratic::execute()
       qtvm_->get_gradient_basis(loc,p,g[0],g[1],g[2],g[3],g[4],
 				g[5],g[6],g[7],g[8],g[9]);
       vector<int> s(10);
-      TetVolMesh::Node::array_type cell_nodes(10);
+      TetVolMesh::Node::array_type cell_nodes;
       qtvm_->get_nodes(cell_nodes, loc);
 
       for (int k = 0; k < 10; k++)
