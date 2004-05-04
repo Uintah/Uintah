1fbd8fdfaa0331a2f55380768f222b0a2031dcee
John Schmidt <jas@sci.utah.edu>
2004-05-04 20:19:22 +0000

    Accepts the material id you would like converted.

--- scripts/graphics.sh
+++ scripts/graphics.sh
@@ -1,21 +1,26 @@
 #! /bin/sh
 
-if test $# -ne 3; then
-    echo "Usage: $0 'uda_to_convert' -ttts path_to_TextToTriSurf or"
-    echo "Usage: $0 -ttts path_to_TextToTriSurf 'uda_to_convert' "
+if test $# -ne 4; then
+    echo "Usage: $0 mat_id 'uda_to_convert' -ttts path_to_TextToTriSurf or"
+    echo "Usage: $0 mat_id -ttts path_to_TextToTriSurf 'uda_to_convert' "
     exit
 fi
 
-
-if test $1 = "-ttts"; then
-    ttts=$2
-    uda_dir=$3
-fi
+if test $1 -ge 0; then
+    mat_id=$1
+fi 
 
 if test $2 = "-ttts"; then
-    uda_dir=$1
     ttts=$3
+    uda_dir=$4
 fi
+
+if test $3 = "-ttts"; then
+    uda_dir=$2
+    ttts=$4
+fi
+
+if test $3
 
 #  Find the location of the TextToTriSurfField
 
@@ -27,8 +32,8 @@ do
   cd $i
   if test  -d crackData; then
       cd crackData
-      `$ttts  cx.mat000 ce.mat000 mat000.fld -noPtsCount -noElementsCount`
-      wc -l ce.mat000
+      `$ttts  cx.mat00$mat_id ce.mat00$mat_id mat00$mat_id.fld -noPtsCount -noElementsCount`
+      wc -l ce.mat00$mat_id
       cd ../..
   fi
 done
