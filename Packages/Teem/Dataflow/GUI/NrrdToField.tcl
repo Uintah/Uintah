3cc816cf0e42f0aee943b7ccb26d8f439eb907bc
Darby Brown <darbyb@sci.utah.edu>
2004-05-13 20:04:28 +0000

    Better formatting.  Added input Names to UI for informational use only

--- Packages/Teem/Dataflow/GUI/NrrdToField.tcl
+++ Packages/Teem/Dataflow/GUI/NrrdToField.tcl
@@ -31,11 +31,13 @@ itcl_class Teem_DataIO_NrrdToField {
 	global $this-build-eigens
 	global $this-quad-or-tet
 	global $this-struct-unstruct
+	global $this-datasets
 
 	set $this-permute 0
 	set $this-build-eigens 0
 	set $this-quad-or-tet "Auto"
 	set $this-struct-unstruct "Auto"
+	set $this-datasets ""
     }
 
     method ui {} {
@@ -53,13 +55,13 @@ itcl_class Teem_DataIO_NrrdToField {
 
 	checkbutton $w.f.options.permute -text "Permute Data" \
 	    -variable $this-permute
-	pack $w.f.options.permute -side top  -expand yes -fill x
+	pack $w.f.options.permute -side top -anchor nw 
 	
-	checkbutton $w.f.options.eigens -text "Build Eigen decomposition for Tensor field" -variable $this-build-eigens
-	pack $w.f.options.eigens -side top -expand yes -fill x
+	checkbutton $w.f.options.eigens -text "Build Eigen Decomposition for Tensor Field" -variable $this-build-eigens
+	pack $w.f.options.eigens -side top -anchor nw
 
 	iwidgets::labeledframe $w.f.options.quadtet \
-	    -labelpos nw -labeltext "Unstructured Cell Type when Points per Connection equals 4:"
+	    -labelpos nw -labeltext "Unstructured Cell Type when\nPoints per Connection = 4:"
 	pack $w.f.options.quadtet -side top -expand yes -fill x
 
 	set quadtet [$w.f.options.quadtet childsite]
@@ -87,10 +89,52 @@ itcl_class Teem_DataIO_NrrdToField {
 	    -variable $this-struct-unstruct -value "StructCurve"
 	pack $pccurve.auto $pccurve.pc $pccurve.curve  -side left -anchor nw -padx 3
 
+	# Input Dataset
+	label $w.f.options.datasetslab -text "Datasets:"
+	pack $w.f.options.datasetslab -side top -anchor nw -pady 3
+
+	frame $w.f.options.datasets
+	pack $w.f.options.datasets -side top -anchor nw -pady 5
+
+	global $this-datasets
+	set_names [set $this-datasets]
+
 	makeSciButtonPanel $w $w $this
 	moveToCursor $w
 
 	pack $w.f -expand 1 -fill x
+    }
+
+    method set_names {datasets} {
+
+	global $this-datasets
+	set $this-datasets $datasets
+
+        set w .ui[modname]
+
+	if [ expr [winfo exists $w.f.options] ] {
+
+	    for {set i 0} {$i < 4} {incr i 1} {
+		if [ expr [winfo exists $w.f.options.datasets.$i] ] {
+		    pack forget $w.f.options.datasets.$i
+		}
+	    }
+	    set i 0
+
+	    foreach dataset $datasets {
+		if [ expr [winfo exists $w.f.options.datasets.$i] ] {
+		    $w.f.options.datasets.$i configure -text $dataset
+		} else {
+		    set len [expr [string length $dataset] + 5 ]
+		    label $w.f.options.datasets.$i -text $dataset \
+			-anchor w -just left -fore darkred 
+		}
+
+		pack $w.f.options.datasets.$i -side top -anchor nw -fill x
+
+		incr i 1
+	    }
+	}
     }
 }
 
