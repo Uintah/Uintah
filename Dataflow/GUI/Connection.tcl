2437629be6261b5855f04f075bfeb5a8c9bf7ad7
McKay Davis <mdavis@sci.utah.edu>
2004-05-13 22:26:06 +0000

    Change straight connection width to 5 from 7

--- Dataflow/GUI/Connection.tcl
+++ Dataflow/GUI/Connection.tcl
@@ -75,18 +75,28 @@ proc drawConnections { connlist } {
 		 ([lsearch $TracedConnections $conn] != -1) } {
 	    set color red
 	}
-	set flags "-width [expr $disabled?3:7] -fill \"$color\" -tags $id"
+
+	set linetype bline
+	set width 7
+	if { [envBool SCIRUN_STRAIGHT_CONNECTIONS] } {
+	    set linetype line
+	    set width 5
+	} 
+	if { $disabled } {
+	    set width 3
+	}
+
+	set flags "-width $width -fill \"$color\" -tags $id"
 	set miniflags "-width 1 -fill \"$color\" -tags $id"
 	if { ![canvasExists $canvas $id] } {
-	    set style [expr [envBool SCIRUN_STRAIGHT_CONNECTIONS]?"line":"bline"]
-	    eval $canvas create $style $path $flags
+	    eval $canvas create $linetype $path $flags
 	    eval $minicanvas create line [scalePath $path] $miniflags
 
 	    $canvas bind $id <1> "$canvas raise $id
                                   traceConnection \{$conn\}
                                   moduleStartDrag [oMod conn] %X %Y 0"
 	    $canvas bind $id <B1-Motion> "moduleDrag [oMod conn] %X %Y"
-	    $canvas bind $id <ButtonRelease-1> "moduleEndDrag [oMod conn] %X %Y;
+	    $canvas bind $id <ButtonRelease-1> "moduleEndDrag [oMod conn] %X %Y
                                                 deleteTraces"
 	    $canvas bind $id <Control-Button-1> "$canvas raise $id
 						 traceConnection {$conn} 1"
@@ -489,7 +499,7 @@ proc routeConnection { conn } {
     set ix [expr int([lindex $inpos 0])]
     set iy [expr int([lindex $inpos 1])]
     if {[envBool SCIRUN_STRAIGHT_CONNECTIONS] } {
-	return [list $ox $oy $ox [expr $oy+3] $ix [expr $iy-4] $ix $iy]
+	return [list $ox $oy $ox [expr $oy+2] $ix [expr $iy-3] $ix $iy]
     } elseif { $ox == $ix && $oy <= $iy } {
 	return [list $ox $oy $ix $iy]
     } elseif {[expr $oy+19] < $iy} {
