72d24a7bae8ff6d2b2173561da1ac15b952b035c
Darby Brown <darbyb@sci.utah.edu>
2004-05-20 16:30:12 +0000

    Changed license.  In the case where there was no license, added it.  I have compiled SCIRun, BioPSE, Teem and MatlabInterface.  I wasn't able to compile and check the doc stuff though. I will be doing Insight, DataIO, and Fusion later today.

--- src/Packages/BioPSE/Dataflow/Modules/Modeling/SmoothSepSurf.cc
+++ src/Packages/BioPSE/Dataflow/Modules/Modeling/SmoothSepSurf.cc
@@ -1,3 +1,31 @@
+/*
+   For more information, please see: http://software.sci.utah.edu
+
+   The MIT License
+
+   Copyright (c) 2004 Scientific Computing and Imaging Institute,
+   University of Utah.
+
+   License for the specific language governing rights and limitations under
+   Permission is hereby granted, free of charge, to any person obtaining a
+   copy of this software and associated documentation files (the "Software"),
+   to deal in the Software without restriction, including without limitation
+   the rights to use, copy, modify, merge, publish, distribute, sublicense,
+   and/or sell copies of the Software, and to permit persons to whom the
+   Software is furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included
+   in all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+   THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+   DEALINGS IN THE SOFTWARE.
+*/
+
 //static char *id="@(#) $Id$";
 
 /*
@@ -414,6 +442,9 @@ void SmoothSepSurf::tcl_command(TCLArgs& args, void* userdata)
 
 //
 // $Log$
+// Revision 1.2  2004/05/20 16:27:19  darbyb
+// Changed license.  In the case where there was no license, added it.  I have compiled SCIRun, BioPSE, Teem and MatlabInterface.  I wasn't able to compile and check the doc stuff though. I will be doing Insight, DataIO, and Fusion later today.
+//
 // Revision 1.1  2003/03/06 07:42:52  dmw
 // - Adding segmentation processing modules into BioPSE
 //
