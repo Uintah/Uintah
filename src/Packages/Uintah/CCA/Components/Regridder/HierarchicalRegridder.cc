f617194a78cdbfc8e0c033c4f144aaf3a62e51a2
Bryan Jeffrey Worthen <worthen@sci.utah.edu>
2004-06-22 02:00:47 +0000

    Initial Regridder code - 2 algorithms to choose from - Hierarchical and Berger-Rigoutsos

---
+++ src/Packages/Uintah/CCA/Components/Regridder/HierarchicalRegridder.cc
@@ -0,0 +1,19 @@
+#include <Packages/Uintah/CCA/Components/Regridder/HierarchicalRegridder.h>
+#include <Packages/Uintah/Core/Grid/Grid.h>
+
+using namespace Uintah;
+
+HierarchicalRegridder::HierarchicalRegridder(ProcessorGroup* pg) : RegridderCommon(pg)
+{
+
+}
+
+HierarchicalRegridder::~HierarchicalRegridder()
+{
+
+}
+
+Grid* HierarchicalRegridder::regrid(Grid* oldgrid, SchedulerP sched)
+{
+  return oldgrid;
+}
