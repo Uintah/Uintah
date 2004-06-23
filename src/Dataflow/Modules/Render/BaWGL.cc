3d711f13424dfbd09c71a67ab7fa58b52ee60ff3
Milan Ikits <ikits@sci.utah.edu>
2004-06-23 04:36:33 +0000

    replaced <GL/gl.h> with <sci_gl.h>

--- src/Dataflow/Modules/Render/BaWGL.cc
+++ src/Dataflow/Modules/Render/BaWGL.cc
@@ -48,14 +48,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-#if defined( HAVE_GLEW )
-#include <GL/glew.h>
-#include <GL/glxew.h>
-#else
-#include <GL/gl.h>
+#include <sci_gl.h>
 #include <sci_glu.h>
-#include <GL/glx.h>
-#endif
+#include <sci_glx.h>
 
 #include <Dataflow/Modules/Render/BaWGL.h>
 
