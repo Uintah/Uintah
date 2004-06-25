1ad10a1beadbcc77def96f352ab042185ad55580
Biswajit Banerjee <banerjee@sci.utah.edu>
2004-06-25 21:55:51 +0000

    Slight changes.

--- src/Packages/Uintah/CCA/Components/MPM/ConstitutiveModel/DruckerBeckerCheck.cc
+++ src/Packages/Uintah/CCA/Components/MPM/ConstitutiveModel/DruckerBeckerCheck.cc
@@ -41,33 +41,6 @@ DruckerBeckerCheck::checkStability(const Matrix3& stress,
   Vector sig(0.0,0.0,0.0);
   Matrix3 evec;
   sigma.eigen(sig, evec);
-  //cout << "stress = \n";
-  //cout << stress << endl;
-  //cout << "Eigenvalues : " << sig << endl;
-  //cout << "Eigenvectors : " << evec << endl;
-
-  /*  OLD CODE USING MATRIX3 Routines
-  // If all three principal stresses are equal, numEV = 1,
-  // If two principal stresses are equal, numEV = 2, and the largest
-  // is in sig[0].
-  // If no principal stresses are equal, numEV = 3, and the largest
-  // is in sig[0] and the smallest is in sig[2].
-  double sig[3];
-  int numEV = stress.getEigenValues(sig[0], sig[1], sig[2]);
-
-  cout << "stress = \n";
-  cout << stress;
-  cout << "numEV = " << numEV << " s1 = " << sig[0] << " s2 = " << sig[1]
-       << " s3 = " << sig[2] << endl;
-  // Get the eigenvectors of the stress tensor
-  vector<Vector> eigVec;
-  for (int ii = 0; ii < numEV; ii++)  
-     eigVec = stress.getEigenVectors(sig[ii], sig[0]);
-
-  // Calculate the coefficients of the quadric
-  if (numEV == 1) sig[2] = sig[0];
-  else if (numEV == 2) sig[2] = sig[1];
-  */
 
   double A = M(2,0,2,0)*(-sig[2] + 2.0*M(2,2,2,2));
   double C = M(2,0,2,0)*(-sig[0] + 2.0*M(0,0,0,0));
