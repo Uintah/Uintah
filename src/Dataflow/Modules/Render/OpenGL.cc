b7a23ff620e85519a1f8a3bda2d8048c6e0df832
Milan Ikits <ikits@sci.utah.edu>
2004-06-27 19:27:55 +0000

    init GLEW only once

--- src/Dataflow/Modules/Render/OpenGL.cc
+++ src/Dataflow/Modules/Render/OpenGL.cc
@@ -695,14 +695,19 @@ OpenGL::redraw_frame()
     glXMakeCurrent(dpy, win, cx);
     glXWaitX();
 #if defined(HAVE_GLEW)
+    static bool glew_init = false;
+    if (!glew_init) {
+      fprintf(stdout, "Initing GLEW\n");
       glewExperimental = GL_TRUE;
       GLenum err = glewInit();
       if (GLEW_OK != err )
       {
-	  /* problem: glewInit failed, something is seriously wrong */
+        /* problem: glewInit failed, something is seriously wrong */
 	fprintf(stderr, "Error: %s\n", glewGetErrorString(err)); 
       }
       fprintf(stdout, "Status: Using GLEW %s\n", glewGetString(GLEW_VERSION));
+      glew_init = true;
+    }
 #endif
     current_drawer=this;
     GLint data[1];
