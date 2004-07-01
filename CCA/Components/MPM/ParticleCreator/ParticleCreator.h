15e31f0cb93e0fa796a1fa3f1c3a53285a79c694
Todd Harman <harman@sci.utah.edu>
2004-07-01 18:54:08 +0000

    Wrapped all pcolor code with if(d_with_color).
    Add
      <MPM>
      <withColor>  true  </withColor>
      </MPM>
     to turn it on.

--- CCA/Components/MPM/ParticleCreator/ParticleCreator.h
+++ CCA/Components/MPM/ParticleCreator/ParticleCreator.h
@@ -111,6 +111,7 @@ namespace Uintah {
     int d_8or27;
     bool d_useLoadCurves;
     bool d_doErosion;
+    bool d_with_color;
 
     vector<const VarLabel* > particle_state, particle_state_preReloc;
     typedef map<pair<const Patch*,GeometryObject*>,vector<Point> > geompoints;
