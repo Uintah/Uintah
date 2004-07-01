2dd9158c78466350bedcdaf7c9f419822c20912e
McKay Davis <mdavis@sci.utah.edu>
2004-07-01 19:21:18 +0000

    Undo last checkin, fixed by moving TCL code to Core/GuiInterface/TCLInterface

--- v1_22_0/src/main/sub.mk
+++ v1_22_0/src/main/sub.mk
@@ -43,7 +43,7 @@ else
   endif
 endif
 
-LIBS := $(TCL_LIBRARY)
+LIBS := 
 ifeq ($(NEED_SONAME),yes)
   LIBS := $(LIBS) $(XML_LIBRARY) $(PLPLOT_LIBRARY) $(TK_LIBRARY) $(DL_LIBRARY) $(Z_LIBRARY) $(SCISOCK_LIBRARY)
 endif
