14ba522fc15f1dba3c102dd4373ac98e88afeeed
David Weinstein <dmw@sci.utah.edu>
2004-07-13 22:18:56 +0000

    - Add sci_defs/insight_defs.h so we can correctly check whether we HAVE_INSIGHT

--- Packages/Teem/Dataflow/Modules/DataIO/DicomNrrdReader.cc
+++ Packages/Teem/Dataflow/Modules/DataIO/DicomNrrdReader.cc
@@ -57,6 +57,8 @@
 #include <Packages/Teem/Dataflow/Ports/NrrdPort.h>
 #include <Core/GuiInterface/GuiVar.h>
 
+#include <sci_defs/insight_defs.h>
+
 #ifdef HAVE_INSIGHT
 #include <Core/Algorithms/DataIO/DicomSeriesReader.h>
 #include <Core/Algorithms/DataIO/DicomImage.h>
