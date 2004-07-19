f6d062f858325eac9d1bfae437d5e767f04a70e2
Allen Sanderson <allen@sci.utah.edu>
2004-07-19 18:38:12 +0000

    add defs include

--- Dataflow/Modules/Visualization/GenAxes.cc
+++ Dataflow/Modules/Visualization/GenAxes.cc
@@ -39,6 +39,8 @@
  *  Copyright (C) 2003 SCI Group
  */
 
+#include <sci_defs/environment_defs.h>
+
 #include <Dataflow/Network/Module.h>
 #include <Dataflow/Ports/FieldPort.h>
 #include <Dataflow/Ports/GeometryPort.h>
