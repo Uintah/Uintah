29c74938482db3ac157ebd44af7bda2b7f19b325
McKay Davis <mdavis@sci.utah.edu>
2004-08-05 00:45:59 +0000

    Remove redundant 'Execute' button

--- Dataflow/GUI/LinearAlgebra.tcl
+++ Dataflow/GUI/LinearAlgebra.tcl
@@ -62,11 +62,7 @@ itcl_class SCIRun_Math_LinearAlgebra {
 	bind $w.row1 <Leave> "$this update_text"
 	$w.row1 insert end [set $this-function]
 
-	frame $w.row2
-	button $w.row2.execute -text "Execute" -command "$this-c needexecute"
-	pack $w.row2.execute -side left -e y -f both -padx 5 -pady 5
 	pack $w.row1 -side top -e y -f both -padx 5 -pady 5
-	pack $w.row2 -side top -e n -f x -padx 5 -pady 5
 
 	makeSciButtonPanel $w $w $this
 	moveToCursor $w
