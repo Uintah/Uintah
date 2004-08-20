9cd46d84982b7bb6659c5a5db62855bc85febcec
Bryan Jeffrey Worthen <worthen@sci.utah.edu>
2004-08-20 04:08:18 +0000

    Revert to v. 1.5 (we're memory savvy here)

--- src/Packages/Uintah/Core/Grid/PerPatch.h
+++ src/Packages/Uintah/Core/Grid/PerPatch.h
@@ -5,9 +5,6 @@
 #include <Packages/Uintah/Core/Exceptions/TypeMismatchException.h>
 #include <Core/Malloc/Allocator.h>
 
-#include <iostream>
-using namespace std;
-
 namespace Uintah {
   class TypeDescription;
 /**************************************
@@ -41,32 +38,26 @@ WARNING
 
    class Variable;
 
+   // 'T' should be a Handle to be something that's RefCounted.
+   // Otherwise, do your own memory management...
    template<class T> class PerPatch : public PerPatchBase {
    public:
-      inline PerPatch() { data = new T; deleteData = true; }
-      inline PerPatch(const T& value) {
-        data = new T;
-        setData(value);
-        deleteData = true;
-      }
+      inline PerPatch() {}
+      inline PerPatch(T value) : value(value) {}
       virtual void copyPointer(const PerPatchBase&);
-      inline PerPatch(const PerPatch<T>& copy) {
-        // copy the pointer not the data
-        data = copy.data;
-      }
-
+      inline PerPatch(const PerPatch<T>& copy) : value(copy.value) {}
       virtual ~PerPatch();
       
       static const TypeDescription* getTypeDescription();
       
       inline operator T () const {
-	 return *data;      
+	 return value;
       }
       inline T& get() {
-	 return *data;
+	 return value;
       }
       inline const T& get() const {
-	 return *data;
+	 return value;
       }
       void setData(const T&);
       virtual PerPatchBase* clone() const;
@@ -75,14 +66,10 @@ WARNING
 			       void*& ptr) const {
 	elems="1";
 	totsize=sizeof(T);
-	ptr=(void*)data;
+	ptr=(void*)&value;
       }
    private:
-      T* data;
-
-      //! Try to remember if this PerPatch needs to delete the memory, since we copy the pointer
-      //! Don't use RefCounted, as simple and/or pre-existing types probably don't use refcounted.
-      mutable bool deleteData; 
+      T value;
       // this function only exists to satisfy the TypeDescription, it will return null.
       static Variable* maker();
    };
@@ -103,7 +90,7 @@ WARNING
           // this is a hack to get a non-null perpatch
           // var for some functions the perpatches are used in (i.e., task->computes).
           // Since they're not fully-qualified variables, maker 
-          // would fail anyway.
+          // would fail anyway.  And since most instances use Handle, it would be difficult.
           td = scinew TypeDescription(TypeDescription::PerPatch,
                                       "PerPatch", &maker,
                                       fun_getTypeDescription((int*)0));
@@ -114,27 +101,21 @@ WARNING
    template<class T>
       PerPatch<T>::~PerPatch()
       {
-        if (data && deleteData) delete data;
       }
    
    template<class T>
       PerPatchBase*
       PerPatch<T>::clone() const
       {
-        // copy the pointer, not the data
-        PerPatch<T> *var  = scinew PerPatch<T>;
-        var->data = data;
-        deleteData = false;
-        return var;
+	 return scinew PerPatch<T>(*this);
       }
    
    template<class T>
       PerPatch<T>&
       PerPatch<T>::operator=(const PerPatch<T>& copy)
       {
-        // copy the pointer, not the data.
-        data = copy.data;
-        return *this;
+	 value = copy.value;
+	 return *this;
       }
 
    template<class T>
@@ -144,8 +125,7 @@ WARNING
          const PerPatch<T>* c = dynamic_cast<const PerPatch<T>* >(&copy);
          if(!c)
 	   SCI_THROW(TypeMismatchException("Type mismatch in PerPatch variable"));
-         data = c->data;
-         deleteData = false;
+         *this = *c;
       }
 
 
@@ -153,7 +133,7 @@ WARNING
       void
       PerPatch<T>::setData(const T& val)
       {
-	*data = val;
+	value = val;
       }
 } // End namespace Uintah
 
