c709b5ef81e5db477bfe907d89f48c1fc9e29553
No Author <null@sci.utah.edu>
2004-08-30 23:50:20 +0000

    This commit was manufactured by cvs2svn to create tag 'PRE_ON_DEMAND_PATCH_READING'.

--- src/Packages/Uintah/CCA/Components/Models/HEChem/sub.mk
+++
@@ -1,9 +0,0 @@
-# Makefile fragment for this subdirectory
-
-SRCDIR   := Packages/Uintah/CCA/Components/Models/HEChem
-
-SRCS	+= \
-       $(SRCDIR)/Simple_Burn.cc \
-       $(SRCDIR)/IandG.cc       \
-       $(SRCDIR)/LightTime.cc       \
-       $(SRCDIR)/JWLpp.cc
