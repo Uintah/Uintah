6121b09361e4e9a4506e1cb29236446b50776935
Allen Sanderson <allen@sci.utah.edu>
2004-09-01 16:58:32 +0000

    made into a class

--- src/Packages/DataIO/Dataflow/Modules/Readers/HDF5Dump.h
+++ src/Packages/DataIO/Dataflow/Modules/Readers/HDF5Dump.h
@@ -48,18 +48,34 @@ namespace DataIO {
 
 using namespace std;
 
-void HDF5Dump_tab( ostream *iostr );
+herr_t hdf5_attr_iter (hid_t obj_id, const char *name, void *op_data);
+herr_t hdf5_group_iter(hid_t obj_id, const char *name, void *op_data);
 
-herr_t HDF5Dump_file(const std::string fname, ostream *iostr);
-herr_t HDF5Dump_attr(hid_t group_id, const char *name, void *op_data);
-herr_t HDF5Dump_all(hid_t obj_id, const char *name, void *op_data);
-herr_t HDF5Dump_group(hid_t group_id, const char *name, ostream *iostr );
-herr_t HDF5Dump_dataset(hid_t dataset_id, const char *name, ostream *iostr);
-herr_t HDF5Dump_datatype(hid_t obj_id, hid_t type, ostream *iostr);
-herr_t HDF5Dump_dataspace(hid_t file_space_id, ostream *iostr);
-herr_t HDF5Dump_data(hid_t obj_id, hid_t type, ostream *iostr);
-std::string HDF5Dump_error();
+class HDF5Dump {
+public:
+  HDF5Dump( ostream *iostr );
 
+  void tab( );
+
+
+  herr_t file(const std::string fname);
+  herr_t group    (hid_t group_id,   const char *name);
+  herr_t all      (hid_t obj_id,     const char *name);
+  herr_t attr     (hid_t group_id,   const char *name);
+  herr_t dataset  (hid_t dataset_id, const char *name);
+  herr_t datatype (hid_t obj_id,     hid_t type);
+  herr_t data     (hid_t obj_id,     hid_t type);
+  herr_t dataspace(hid_t file_space_id);
+
+  std::string error() { return error_msg_; };
+
+private:
+  unsigned int indent_;
+  unsigned int max_indent_;
+
+  ostream *iostr_;
+  std::string error_msg_;
+};
 }
 
 #endif  // HAVE_HDF5
