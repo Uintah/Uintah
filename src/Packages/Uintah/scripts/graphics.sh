c286c73673724cf01810fbf480bbb295a1320218
John Schmidt <jas@sci.utah.edu>
2004-09-09 17:33:43 +0000

    Read the location of the binary TextToTriSurfaceField from the TTTS enviroment variable.

--- src/Packages/Uintah/scripts/graphics.sh
+++ src/Packages/Uintah/scripts/graphics.sh
@@ -1,24 +1,20 @@
 #! /bin/sh
 
-if test $# -ne 4; then
-    echo "Usage: $0 mat_id 'uda_to_convert' -ttts path_to_TextToTriSurf or"
-    echo "Usage: $0 mat_id -ttts path_to_TextToTriSurf 'uda_to_convert' "
+if test ${#TTTS} = 0; then
+    echo "Must set the enviroment variable TTTS to the path to the TextToTriSurf exectuble"
+    exit
+fi
+
+if test $# -ne 2; then
+    echo "Usage: $0 mat_id 'uda_to_convert' "
     exit
 fi
 
 if test $1 -ge 0; then
     mat_id=$1
+    uda_dir=$2
 fi 
 
-if test $2 = "-ttts"; then
-    ttts=$3
-    uda_dir=$4
-fi
-
-if test $3 = "-ttts"; then
-    uda_dir=$2
-    ttts=$4
-fi
 
 #  Find the location of the TextToTriSurfField
 
@@ -30,7 +26,7 @@ do
   cd $i
   if test  -d crackData; then
       cd crackData
-      `$ttts  cx.mat00$mat_id ce.mat00$mat_id mat00$mat_id.fld -noPtsCount -noElementsCount`
+      `$TTTS  cx.mat00$mat_id ce.mat00$mat_id mat00$mat_id.fld -noPtsCount -noElementsCount`
       wc -l ce.mat00$mat_id
       cd ../..
   fi
