a2788a277d57d2bd5a3bd5e75b5ec645f3c12fe8
Steven G. Parker <sparker@sci.utah.edu>
2004-09-17 23:48:40 +0000

    Updated tests for new interface Added performance tests for FastMatrix

--- testprograms/TestFastMatrix/sub.mk
+++ testprograms/TestFastMatrix/sub.mk
@@ -12,3 +12,12 @@ LIBS := $(M_LIBRARY)
 
 include $(SCIRUN_SCRIPTS)/program.mk
 
+SRCS := $(SRCDIR)/perffastmatrix.cc
+
+PSELIBS := \
+	Packages/Uintah/Core/Math Core/Thread
+PROGRAM := $(SRCDIR)/perffastmatrix
+
+LIBS := $(M_LIBRARY)
+
+include $(SCIRUN_SCRIPTS)/program.mk
