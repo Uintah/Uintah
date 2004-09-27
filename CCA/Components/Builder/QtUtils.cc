32da05bfdf063d246080b7f5a236a3576de22dab
Ayla Khan <ayla@sci.utah.edu>
2004-09-27 21:58:40 +0000

    Added macro definition and fixed command line arguments to get rid of Qt error "Qt: Cannot set locale modifiers: (null)" when SCIRun2's Qt GUI starts.

--- CCA/Components/Builder/QtUtils.cc
+++ CCA/Components/Builder/QtUtils.cc
@@ -43,44 +43,48 @@
 #include <Core/Thread/Semaphore.h>
 #include <Core/Thread/Thread.h>
 #include <iostream>
-using namespace SCIRun;
-using namespace std;
 
 #include <qapplication.h>
+
+
+using namespace SCIRun;
 
 static QApplication* theApp;
 static Semaphore* startup;
 
 class QtThread : public Runnable {
 public:
-  QtThread() {}
-  ~QtThread() {}
-  void run();
+    QtThread() {}
+    ~QtThread() {}
+    void run();
 };
+
 
 QApplication* QtUtils::getApplication()
 {
-  if(!theApp){
-    startup=new Semaphore("Qt Thread startup wait", 0);
-    Thread* t = new Thread(new QtThread(), "SCIRun Builder", 0, Thread::NotActivated);
-    t->setStackSize(8*256*1024);
-    t->activate(false);
-    t->detach();
-    startup->down();
-  }
-  return theApp;
+    if ( !theApp ) {
+	startup = new Semaphore("Qt Thread startup wait", 0);
+	Thread* t = new Thread(new QtThread(), "SCIRun Builder",
+				0, Thread::NotActivated);
+	t->setStackSize(8*256*1024);
+	t->activate(false);
+	t->detach();
+	startup->down();
+    }
+    return theApp;
 }
 
 void QtThread::run()
 {
-  cerr<<"******************QtThread::run()**********************\n"; 
-  int argc=1;
-  char* argv[4];
-  argv[0]="SCIRun2";
-  argv[1]="-im";
-  argv[2]="";
-  argv[3]=0;
-  theApp = new QApplication(argc, argv);
-  startup->up();
-  theApp->exec();
+    std::cerr << "******************QtThread::run()**********************" << std::endl;
+    int argc = 3;
+    char* argv[3];
+    argv[0] = "SCIRun2";
+    argv[1] = "-im";
+    argv[2] = "-iconic";
+
+    theApp = new QApplication(argc, argv);
+    startup->up();
+
+    theApp->exec();
 }
