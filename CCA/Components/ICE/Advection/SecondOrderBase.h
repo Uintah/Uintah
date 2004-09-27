03339212516377d7de133a82387933c8ca1ce444
Todd Harman <harman@sci.utah.edu>
2004-09-27 23:04:36 +0000

    gradQ now operates over the whole domain.
    added:
     limitedGradient()   --compute the limited gradient
     flux_to_primitive() --computes q_CC = A_CC/mass
     Q_vertex            --computes q at the vertices w/ modification for compatible fluxes
    
    Deleted:
      q_vertexMaxMin()

--- CCA/Components/ICE/Advection/SecondOrderBase.h
+++ CCA/Components/ICE/Advection/SecondOrderBase.h
@@ -4,6 +4,8 @@
 #include <Packages/Uintah/CCA/Components/ICE/Advection/Advector.h>
 #include <Packages/Uintah/CCA/Ports/DataWarehouse.h>
 #include <Packages/Uintah/Core/Disclosure/TypeDescription.h>
+#include <Core/Exceptions/InternalError.h>
+#include <Packages/Uintah/Core/Grid/CellIterator.h>
 #include <Packages/Uintah/Core/Grid/Patch.h>
 #include <Packages/Uintah/Core/Grid/CCVariable.h>
 #include <Packages/Uintah/Core/Grid/Ghost.h>
@@ -18,79 +20,95 @@ public:
   virtual ~SecondOrderBase();
   
   template<class T> 
-    inline void gradientLimiter(const CCVariable<T>& q_CC,
-                                const IntVector& c,
-                                Vector dx_2,
-				    T& grad_lim, 
-				    T& q_grad_x,
-				    T& q_grad_y,
-				    T& q_grad_z);
-                      
+  void limitedGradient(const CCVariable<T>& q_CC,
+                       const Patch* patch,
+                       const CCVariable<vertex<T> >& q_vertex, 
+			  CCVariable<T>& q_grad_x,
+			  CCVariable<T>& q_grad_y,
+			  CCVariable<T>& q_grad_z);
+                       
+  template<class T> 
+  void Q_vertex( const bool useCompatibleFluxes,
+                 const CCVariable<T>& q_CC,
+                 CCVariable<vertex<T> >& q_vertex,
+                 const Patch* patch,
+	          const CCVariable<T>& q_grad_x,
+	          const CCVariable<T>& q_grad_y,
+	          const CCVariable<T>& q_grad_z);
+  
+                                 
   template<class T> 
     inline void q_CCMaxMin(const CCVariable<T>& q_CC,
                            const IntVector& c,
 			      T& q_CC_max, 
 			      T& q_CC_min);
-                                           
-  template<class T> 
-    inline void q_vertexMaxMin(T& q_CC,
-                               Vector dx_2,
-				   T& q_grad_x,
-				   T& q_grad_y,
-				   T& q_grad_z,
-				   T& q_vrtx_max, 
-				   T& q_vrtx_min);
-                                    
-  template<class T> 
-    inline void gradQ( const CCVariable<T>& q_CC,
-                       const IntVector& c,
-                       Vector inv_2del,
-			  T& q_grad_x,
-			  T& q_grad_y,
-			  T& q_grad_z);
-                                  
-  CCVariable<fflux>  r_out_x, r_out_y, r_out_z; 
+                           
+  template<class T>                                    
+  void gradQ( const CCVariable<T>& q_CC,
+              const Patch* patch,
+	       CCVariable<T>& q_grad_x,
+	       CCVariable<T>& q_grad_y,
+	       CCVariable<T>& q_grad_z);
+ 
+  void mass_massVertex_ratio( const CCVariable<double>& mass_CC,
+                            const Patch* patch,
+			       const CCVariable<double>& q_grad_x,
+			       const CCVariable<double>& q_grad_y,
+			       const CCVariable<double>& q_grad_z);
+
+  template <class T>
+  bool 
+  flux_to_primitive( const bool useCompatibleFluxes,
+                     const bool is_Q_mass_specific,
+                     const Patch* patch,
+                     const CCVariable<T>& A_CC,
+                     const CCVariable<double>& mass,
+                     CCVariable<T>& q_CC);
+                       
+  CCVariable<fflux>  r_out_x, r_out_y, r_out_z;
+  CCVariable<vertex<double> > d_mass_massVertex;
 };
 
-/* ---------------------------------------------------------------------
- Function~ gradientLimiter
+/*_____________________________________________________________________
+ Function~ gradQ
+ Purpose   Find the x, y, z gradients of q_CC, centered differencing 
 _____________________________________________________________________*/
 template <class T>
 inline void
-SecondOrderBase::gradientLimiter(const CCVariable<T>& q_CC,
-                                const IntVector& c,
-                                Vector dx_2,
-				    T& grad_lim, 
-				    T& q_grad_x,
-				    T& q_grad_y,
-				    T& q_grad_z)
+SecondOrderBase::gradQ( const CCVariable<T>& q_CC,
+                        const Patch* patch,
+			   CCVariable<T>& q_grad_x,
+			   CCVariable<T>& q_grad_y,
+			   CCVariable<T>& q_grad_z)
 {
-  T  frac,temp, zero(0.);
-  T  grad_lim_max, grad_lim_min;
-  T unit = T(1.0);
-  T SN = T(d_SMALL_NUM);
-  
-  T q_vrtx_max, q_vrtx_min, q_CC_max, q_CC_min;
+  Vector dx = patch->dCell();
+  Vector inv_2del;
+  inv_2del.x(1.0/(2.0 * dx.x()) );
+  inv_2del.y(1.0/(2.0 * dx.y()) );
+  inv_2del.z(1.0/(2.0 * dx.z()) );
 
   //__________________________________
-  // find q_cc max and min and
-  // vertex max and min
-  T Q_CC = q_CC[c];
-  q_CCMaxMin( q_CC, c, q_CC_max, q_CC_min);
-  q_vertexMaxMin(Q_CC, dx_2, q_grad_x, q_grad_y, q_grad_z,
-		   q_vrtx_max, q_vrtx_min);
+  //  At patch boundaries you need to extend
+  // the computational footprint by one cell in ghostCells
+  CellIterator iter = patch->getCellIterator();
+  CellIterator iterPlusGhost = patch->addGhostCell_Iter(iter,1);
+    
+  for(CellIterator iter = iterPlusGhost; !iter.done(); iter++) {  
+    const IntVector& c = *iter;
+    
+    IntVector r = c + IntVector( 1, 0, 0);
+    IntVector l = c + IntVector(-1, 0, 0);
+    IntVector t = c + IntVector( 0, 1, 0);
+    IntVector b = c + IntVector( 0,-1, 0);
+    IntVector f = c + IntVector( 0, 0, 1);    
+    IntVector bk= c + IntVector( 0, 0,-1);
 
-  frac = (q_CC_max - Q_CC + SN)/(q_vrtx_max - Q_CC + SN);
-  grad_lim_max = Max(zero, frac);
-
-  frac = (q_CC_min - Q_CC + SN)/(q_vrtx_min - Q_CC + SN);
-  grad_lim_min = Max(zero, frac);
-
-  temp = Min(unit, grad_lim_max);
-  temp = Min(temp, grad_lim_min);
-  grad_lim = temp; 
+    q_grad_x[c] = (q_CC[r] - q_CC[l]) * inv_2del.x();
+    q_grad_y[c] = (q_CC[t] - q_CC[b]) * inv_2del.y();
+    q_grad_z[c] = (q_CC[f] - q_CC[bk])* inv_2del.z();
+  }
 }
-/* ---------------------------------------------------------------------
+/*_____________________________________________________________________
 Function~  q_CCMaxMin
 Purpose~   This calculates the max and min values of q_CC from the surrounding
            cells
@@ -129,80 +147,167 @@ SecondOrderBase::q_CCMaxMin(const CCVariable<T>& q_CC,
   q_CC_min = min_tmp;
 }                                                   
 
-/* ---------------------------------------------------------------------
- Function~ q_vertexMaxMin
- Purpose   Find the max and min vertex value of q at the vertex.  
+/*_____________________________________________________________________
+ Function~ q_vertex
+ Purpose   compute q_vertex with gradient limited gradients 
+           Equation 3.3.5 of reference.  Note that there 2 different
+           equations depending on if q_CC is mass specific and we're using
+           compatible flux formulation
 _____________________________________________________________________*/
-template <class T>
-inline void
-SecondOrderBase::q_vertexMaxMin( T& q_CC,
-                                 Vector dx_2,
-				     T& q_grad_x,
-				     T& q_grad_y,
-				     T& q_grad_z,
-				     T& q_vrtx_max, 
-				     T& q_vrtx_min)
+template<class T> 
+void
+SecondOrderBase::Q_vertex( const bool usingCompatibleFluxes,
+                           const CCVariable<T>& q_CC,
+                           CCVariable<vertex<T> >& q_vertex,
+                           const Patch* patch,
+			      const CCVariable<T>& q_grad_x,
+			      const CCVariable<T>& q_grad_y,
+			      const CCVariable<T>& q_grad_z)
 {
-  T q_vrtx1, q_vrtx2,q_vrtx3,q_vrtx4,q_vrtx5,q_vrtx6,q_vrtx7,q_vrtx8;
-  T q_vrtx_tmp_max, q_vrtx_tmp_min;
+  CellIterator iter = patch->getCellIterator();
+  CellIterator iterPlusGhost = patch->addGhostCell_Iter(iter,1);
+  Vector dx = patch->dCell();
+  double dx_2 = dx.x()/2.0;  // distance from cell center to vertex
+  double dy_2 = dx.y()/2.0;
+  double dz_2 = dx.z()/2.0;
+  
+  if(!usingCompatibleFluxes) {        // non-compatible advection
+    for(CellIterator iter = iterPlusGhost; !iter.done(); iter++) {  
+      const IntVector& c = *iter;
 
-  T xterm = q_grad_x * dx_2.x();
-  T yterm = q_grad_y * dx_2.y();
-  T zterm = q_grad_z * dx_2.z();
-  //__________________________________
-  // THINK ABOUT PULLING OUT QCC  --TODD
-  q_vrtx1 = xterm  + yterm + zterm + q_CC;
-  q_vrtx2 = xterm  + yterm - zterm + q_CC;
-  q_vrtx3 = xterm  - yterm + zterm + q_CC;     
-  q_vrtx4 = xterm  - yterm - zterm + q_CC;
-  q_vrtx5 = -xterm + yterm + zterm + q_CC;
-  q_vrtx6 = -xterm + yterm - zterm + q_CC;     
-  q_vrtx7 = -xterm - yterm + zterm + q_CC;
-  q_vrtx8 = -xterm - yterm - zterm + q_CC;
-
-  q_vrtx_tmp_max = Max(q_vrtx1,q_vrtx2);
-  q_vrtx_tmp_min = Min(q_vrtx1,q_vrtx2);
-  q_vrtx_tmp_max = Max(q_vrtx3,q_vrtx_tmp_max);
-  q_vrtx_tmp_min = Min(q_vrtx3,q_vrtx_tmp_min);
-  q_vrtx_tmp_max = Max(q_vrtx4,q_vrtx_tmp_max);
-  q_vrtx_tmp_min = Min(q_vrtx4,q_vrtx_tmp_min);
-  q_vrtx_tmp_max = Max(q_vrtx5,q_vrtx_tmp_max);
-  q_vrtx_tmp_min = Min(q_vrtx5,q_vrtx_tmp_min);
-  q_vrtx_tmp_max = Max(q_vrtx6,q_vrtx_tmp_max);
-  q_vrtx_tmp_min = Min(q_vrtx6,q_vrtx_tmp_min);
-  q_vrtx_tmp_max = Max(q_vrtx7,q_vrtx_tmp_max);
-  q_vrtx_tmp_min = Min(q_vrtx7,q_vrtx_tmp_min);
-  q_vrtx_tmp_max = Max(q_vrtx8,q_vrtx_tmp_max);
-  q_vrtx_tmp_min = Min(q_vrtx8,q_vrtx_tmp_min);
-
-  q_vrtx_max = q_vrtx_tmp_max;
-  q_vrtx_min = q_vrtx_tmp_min;  
+      T xterm = q_grad_x[c] * dx_2;
+      T yterm = q_grad_y[c] * dy_2;
+      T zterm = q_grad_z[c] * dz_2;
+      T q = q_CC[c];
+      
+      vertex<T>& q_vrtx = q_vertex[c];
+      q_vrtx.d_vrtx[0] = q + (-xterm - yterm - zterm);
+      q_vrtx.d_vrtx[1] = q + (-xterm - yterm + zterm);
+      q_vrtx.d_vrtx[2] = q + (-xterm + yterm - zterm);     
+      q_vrtx.d_vrtx[3] = q + (-xterm + yterm + zterm);
+      q_vrtx.d_vrtx[4] = q + (xterm - yterm - zterm);
+      q_vrtx.d_vrtx[5] = q + (xterm - yterm + zterm);    
+      q_vrtx.d_vrtx[6] = q + (xterm + yterm - zterm);
+      q_vrtx.d_vrtx[7] = q + (xterm + yterm + zterm);
+    }
+  }
+  if(usingCompatibleFluxes) {         // compatible advection
+    for(CellIterator iter = iterPlusGhost; !iter.done(); iter++) {  
+      const IntVector& c = *iter;
+      T xterm = q_grad_x[c] * dx_2;
+      T yterm = q_grad_y[c] * dy_2;
+      T zterm = q_grad_z[c] * dz_2;
+      
+      T q = q_CC[c];
+      vertex<T>& q_vrtx = q_vertex[c];
+      const vertex<double>& mass_ratio = d_mass_massVertex[c];
+      
+      q_vrtx.d_vrtx[0] = q + (-xterm - yterm - zterm) * mass_ratio.d_vrtx[0];
+      q_vrtx.d_vrtx[1] = q + (-xterm - yterm + zterm) * mass_ratio.d_vrtx[1];
+      q_vrtx.d_vrtx[2] = q + (-xterm + yterm - zterm) * mass_ratio.d_vrtx[2];     
+      q_vrtx.d_vrtx[3] = q + (-xterm + yterm + zterm) * mass_ratio.d_vrtx[3];
+      q_vrtx.d_vrtx[4] = q + (xterm - yterm - zterm)  * mass_ratio.d_vrtx[4];
+      q_vrtx.d_vrtx[5] = q + (xterm - yterm + zterm)  * mass_ratio.d_vrtx[5];    
+      q_vrtx.d_vrtx[6] = q + (xterm + yterm - zterm)  * mass_ratio.d_vrtx[6];
+      q_vrtx.d_vrtx[7] = q + (xterm + yterm + zterm)  * mass_ratio.d_vrtx[7];
+    }
+  }
 }
-/* ---------------------------------------------------------------------
- Function~ gradQ
- Purpose   Find the x, y, z gradients of q_CC. centered
-           differencing 
+/*_____________________________________________________________________
+ Function~ convertFlux_to_primitive.
+ Purpose: - IF using compatible fluxes convert flux into primitive variable  
+          - For compatible fluxes, q_CC MUST be a mass-specific quantity
+            Test for it.
 _____________________________________________________________________*/
 template <class T>
-inline void
-SecondOrderBase::gradQ( const CCVariable<T>& q_CC,
-                        const IntVector& c,
-                        Vector inv_2del,
-			   T& q_grad_x,
-			   T& q_grad_y,
-			   T& q_grad_z)
-{    
-  IntVector r = c + IntVector( 1, 0, 0);
-  IntVector l = c + IntVector(-1, 0, 0);
-  IntVector t = c + IntVector( 0, 1, 0);
-  IntVector b = c + IntVector( 0,-1, 0);
-  IntVector f = c + IntVector( 0, 0, 1);    
-  IntVector bk= c + IntVector( 0, 0,-1);
+bool 
+SecondOrderBase::flux_to_primitive( const bool useCompatibleFluxes,
+                                    const bool is_Q_mass_specific,
+                                    const Patch* patch,
+                                    const CCVariable<T>& A_CC,
+                                    const CCVariable<double>& mass,
+                                    CCVariable<T>& q_CC)
+{
+                // compatible fluxes.
+  if(useCompatibleFluxes && is_Q_mass_specific) {
+    CellIterator iter = patch->getExtraCellIterator();
+    CellIterator iterPlusGhost = patch->addGhostCell_Iter(iter,2);
+  
+    for(CellIterator iter = iterPlusGhost; !iter.done(); iter++) {  
+      const IntVector& c = *iter;
+      q_CC[c] = A_CC[c]/mass[c];
+    }
+    return true;
+  } else {      // non-compatible fluxes
+    q_CC.copyData(A_CC);
+    return false;
+  }        
+  
+  // bulletproofing
+  if(useCompatibleFluxes && !is_Q_mass_specific){
+    throw InternalError("ICE:SecondOrderAdvection:\n"
+    " For compatible fluxes, Q_CC must be a mass-specific quantity \n");
+  }
+  return false;
+}
+/*_____________________________________________________________________
+ Function~ limitedGradient
+_____________________________________________________________________*/
+template <class T>
+void SecondOrderBase::limitedGradient(const CCVariable<T>& q_CC,
+                                      const Patch* patch,
+                                      const CCVariable<vertex<T> >& q_vertex,
+				          CCVariable<T>& q_grad_x,
+				          CCVariable<T>& q_grad_y,
+				          CCVariable<T>& q_grad_z)
+{
+  T  frac,temp, zero(0.);
+  T  gradLim_max, gradLim_min;
+  T unit = T(1.0);
+  T SN = T(d_SMALL_NUM);
+    
+  //__________________________________
+  //  At patch boundaries you need to extend
+  // the computational footprint by one cell in ghostCells
+  CellIterator iter = patch->getCellIterator();
+  CellIterator iterPlusGhost = patch->addGhostCell_Iter(iter,1);
+  
+  for(CellIterator iter = iterPlusGhost; !iter.done(); iter++) {  
+    const IntVector& c = *iter;
 
-  q_grad_x = (q_CC[r] - q_CC[l]) * inv_2del.x();
-  q_grad_y = (q_CC[t] - q_CC[b]) * inv_2del.y();
-  q_grad_z = (q_CC[f] - q_CC[bk])* inv_2del.z();
-} 
+    T q_vrtx_max, q_vrtx_min, q_CC_max, q_CC_min;
+    T Q_CC = q_CC[c];
+    
+    //__________________________________
+    // q_vertex & q_CC (max/min)
+    q_CCMaxMin( q_CC, c, q_CC_max, q_CC_min);
+    
+    q_vrtx_max = q_vertex[c].d_vrtx[0];
+    q_vrtx_min = q_vertex[c].d_vrtx[0];
+    
+    for (int i=1;i<8;i++){
+      q_vrtx_max = Max(q_vrtx_max,q_vertex[c].d_vrtx[i]);
+      q_vrtx_min = Min(q_vrtx_min,q_vertex[c].d_vrtx[i]);
+    }
+    
+    //__________________________________
+    // gradient limiter
+    frac = (q_CC_max - Q_CC + SN)/(q_vrtx_max - Q_CC + SN);
+    gradLim_max = Max(zero, frac);
+
+    frac = (q_CC_min - Q_CC + SN)/(q_vrtx_min - Q_CC + SN);
+    gradLim_min = Max(zero, frac);
+
+    temp = Min(unit, gradLim_max);
+    temp = Min(temp, gradLim_min);
+    T gradLim = temp;
+           
+    q_grad_x[c] = q_grad_x[c] * gradLim;
+    q_grad_y[c] = q_grad_y[c] * gradLim;
+    q_grad_z[c] = q_grad_z[c] * gradLim; 
+  }
+}
+
 } // end namespace Uintah
 
 #endif
