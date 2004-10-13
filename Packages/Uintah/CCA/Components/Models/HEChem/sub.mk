53f5bbb2046012a1fd9f00b88e9b13b4355aa878
Changwei Xiong <xiongcw@sci.utah.edu>
2004-10-13 00:02:51 +0000

    Added Steady_Burn.cc and Steady_Burn.h to sub.mk to make the files.

--- Packages/Uintah/CCA/Components/Models/HEChem/sub.mk
+++ Packages/Uintah/CCA/Components/Models/HEChem/sub.mk
@@ -4,6 +4,7 @@ SRCDIR   := Packages/Uintah/CCA/Components/Models/HEChem
 
 SRCS	+= \
        $(SRCDIR)/Simple_Burn.cc \
+       $(SRCDIR)/Steady_Burn.cc \
        $(SRCDIR)/IandG.cc       \
-       $(SRCDIR)/LightTime.cc       \
+       $(SRCDIR)/LightTime.cc   \
        $(SRCDIR)/JWLpp.cc
