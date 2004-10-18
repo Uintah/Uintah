7ddb45d17dd9fa8de6308c534b2d27c80d91de86
Bryan Jeffrey Worthen <worthen@sci.utah.edu>
2004-10-18 16:14:42 +0000

    Only do the check-for-restart reduction on a toplevel scheduler

--- Packages/Uintah/CCA/Components/Schedulers/MPIScheduler.cc
+++ Packages/Uintah/CCA/Components/Schedulers/MPIScheduler.cc
@@ -826,7 +826,7 @@ MPIScheduler::execute()
   ASSERT(sends_.numRequests() == 0);
   if(d_logTimes)
     emitTime("final wait");
-  if(restartable){
+  if(restartable && !parentScheduler){  //only do this on a toplevel scheduler
     // Copy the restart flag to all processors
     int myrestart = dws[dws.size()-1]->timestepRestarted();
     int netrestart;
