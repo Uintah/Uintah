c156880f9031ac85bd1c9a2ac054d3a28770a031
Bryan Jeffrey Worthen <worthen@sci.utah.edu>
2004-11-16 23:31:24 +0000

    Get the mpi regridder to complete and successfully create the new grid

--- CCA/Components/Regridder/PerPatchVars.h
+++ CCA/Components/Regridder/PerPatchVars.h
@@ -21,6 +21,7 @@ WARNING
 #include <Packages/Uintah/Core/ProblemSpec/Handle.h>
 #include <Packages/Uintah/Core/Grid/CCVariable.h>
 #include <Core/Geometry/IntVector.h>
+#include <Core/Exceptions/InternalError.h>
 
 namespace Uintah {
 
@@ -32,22 +33,52 @@ struct PatchFlag : public RefCounted {
 
 // used to indicate which sub patches within a patch will be used to create the next level
 struct SubPatchFlag : public RefCounted {
-  inline SubPatchFlag(const IntVector& low, const IntVector& high) { rewindow(low, high); }
-  inline SubPatchFlag() {};
-  inline void rewindow(const IntVector& low, const IntVector& high) {     
-    subpatches.rewindow(low, high);
-    subpatches.initialize(0);
+  inline SubPatchFlag(const IntVector& low, const IntVector& high) { 
+    initialize(low, high);
   }
-  inline void set(IntVector& index) { subpatches[index] = 1;}
-  inline void clear(IntVector& index) { subpatches[index] = 0;}
-  inline IntVector getLowIndex() { return subpatches.getLowIndex(); }
-  inline IntVector getHighIndex() { return subpatches.getHighIndex(); }
-  inline const int operator[](const IntVector& idx) const {
-    int x = subpatches[idx];
-    return x;
+  inline SubPatchFlag() {subpatches = 0; delSubpatches = false;}
+  inline ~SubPatchFlag() { if (delSubpatches) delete subpatches; }
+  
+  // initialize with our own memory
+  inline void initialize(const IntVector& low, const IntVector& high) {     
+    low_ = low; 
+    high_ = high;
+    range_ = high - low; 
+    size_ = range_.x() * range_.y() * range_.z();
+    subpatches = scinew int[size_];
+    delSubpatches = true;
+    for (int i = 0; i < size_; i++)
+      subpatches[i] = 0;
   }
 
-  CCVariable<int> subpatches;
+  // use external memory
+  inline void initialize(const IntVector& low, const IntVector& high, int* data) {
+    if (delSubpatches)
+      throw InternalError("Memory already allocated for this subpatch flag");
+    low_ = low; 
+    high_ = high;
+    range_ = high - low; 
+    size_ = range_.x() * range_.y() * range_.z();
+    subpatches = data;
+
+  }
+  inline int getIndex(const IntVector& index) const { 
+    IntVector tmp(index-low_);
+    return tmp.x() * range_.y() * range_.z() + tmp.y() * range_.z() + tmp.z(); 
+  }
+  inline void set(IntVector& index) { subpatches[getIndex(index)] = 1; }
+  inline void clear(IntVector& index) { subpatches[getIndex(index)] = 0;}
+  inline const int operator[](const IntVector& idx) const {
+    int x = subpatches[getIndex(idx)];
+    return x;
+  }
+  // array
+  int *subpatches;
+  IntVector low_, high_, range_;
+  int size_;
+
+  // whether we allocated the memory ourselves or not.
+  bool delSubpatches;
 };
 
 typedef Handle<PatchFlag> PatchFlagP;
