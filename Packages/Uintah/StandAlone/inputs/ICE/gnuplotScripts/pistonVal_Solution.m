c3d87d1ca9ad0dab4dc69c738ddddb7182148563
Todd Harman <harman@sci.utah.edu>
2004-11-19 20:41:54 +0000

    eliminated the need for printData to be turned on. now using lineextract to grab the data.

--- Packages/Uintah/StandAlone/inputs/ICE/gnuplotScripts/pistonVal_Solution.m
+++ Packages/Uintah/StandAlone/inputs/ICE/gnuplotScripts/pistonVal_Solution.m
@@ -9,6 +9,7 @@ clear all;
 close all;
 %_____________________________________________________________________
 % Problem specific variables
+uda           = sprintf('pistonValidationTest_hack.uda.000');    % uda to compare against
 piston_vel    = 0.001;
 piston_height = 0.001;
 delta_y        = 0.0025./100.0;           % domain length/# cells
@@ -18,58 +19,60 @@ t_final        = 0.8;                     % when the piston stops
 gamma          = 1.4;   
 p_initial      = 1.01325;                 % initial pressure in chamber
 delT_dump      = 1e-2;                    % how often printdata is dumped
-uda            = sprintf('pistonValidation.uda');    % uda to compare against
 
-%_____________________
+startEnd = '-istart 0 0 0 -iend 0 100 0'  % lineExtract start and stop
+
+%_____________________________________________________________________
 % gamma law for the gas.
 % P_chamber = p_initial(Vol_initial/Vol_t) ^ gamma
 time_sec=[0:delT_dump:t_final];
 
-tmp = (h_initial1./(h_initial1-(piston_vel*time_sec)))
-p_chamber_1 = p_initial*tmp.^gamma
+tmp = (h_initial1./(h_initial1-(piston_vel*time_sec)));
+p_chamber_1 = p_initial*tmp.^gamma;
 
-tmp = (h_initial2./(h_initial1-(piston_vel*time_sec)))
-p_chamber_2 = p_initial*tmp.^gamma
+tmp = (h_initial2./(h_initial1-(piston_vel*time_sec)));
+p_chamber_2 = p_initial*tmp.^gamma;
   
-
 % plot the chamber pressure
-figure
-plot(time_sec, p_chamber_1, time_sec, p_chamber_2)
-xlabel('time')
-ylabel('Pressure')
-grid on;
+% figure
+% plot(time_sec, p_chamber_1, time_sec, p_chamber_2)
+% xlabel('time')
+% ylabel('Pressure')
+% grid on;
 
 %_____________________
-%Write out data files
+%Write out the exact solution
 file_id=fopen('pistonVal.dat','w');
 fprintf(file_id,'%6.4f %15.3f  %15.3f\n',[time_sec;p_chamber_1;p_chamber_2]);
 fclose(file_id);
 
 
-%______________________________________________________________________
-% compare against simulation
-unix('setenv MATLAB_SHELL "/bin/csh -b"')
-command1 = sprintf('cd %s; ../inputs/ICE/gnuplotScripts/findTimesteps>&../ts',uda);
-unix(command1);                           % 
-timesteps = importdata('ts');   % find the printdata dump indicies
+%________________________________
+%  extract the physical time of each dump
+c0 = sprintf('puda -timesteps %s | grep : | cut -f 2 -d":" >& tmp',uda);
+[status0, result0]=unix(c0);
+physicalTime  = importdata('tmp');
+nDumps = length(physicalTime) - 1;
 
 %_________________________________
 % Loop over all the timesteps
-%  - put printData array into pressArray
-for( t = 1:length(timesteps) ) 
-  here = timesteps(t);
-  p  = sprintf('%s/BOT_equilibration/%d/L-0/patch_0/Press_CC_equil', uda,here); 
-  tt = sprintf('%s/BOT_equilibration/%d/simTime', uda,here); 
-  pressArray     = sortrows(importdata(p),1);    % put printData Array into pressArray
-  p_probe(t)     = pressArray(1:1,2);            %extract pressure at probe index 1
-  time_probe(t)  = importdata(tt)               %time when measurement was made
+fprintf ('Extracting data from timestep');
+for( ts = 1:nDumps )  
+  fprintf('%i ',ts);
+  c1 = sprintf('lineextract -v press_CC -timestep %i %s -o press -m 0 -uda %s',ts,startEnd,uda);
+  [status1, result1]=unix(c1);
+  pressArray  = importdata('press');
+  
+  p_probe(ts)     = pressArray(1:1,4);     %extract pressure at probe index 1
+  time_probe(ts)  = physicalTime(ts);      %time when measurement was made
 end
   
-
 %________________________________
-% plot up the comparison
+% plot up the the exact solution and 
+% simulation results
 figure
 plot(time_sec, p_chamber_1, time_sec, p_chamber_2, time_probe, p_probe, '.')
 legend('Exact Solutions', '', 'Simulation results');
 grid on;
   
+unix('/bin/rm press pistonVal.dat');
