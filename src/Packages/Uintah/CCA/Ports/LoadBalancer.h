c9bb54ee7f54d8ecf212e2201510523926f653bd
J. Davison de St. Germain <dav@sci.utah.edu>
2004-11-30 02:25:07 +0000

    Fix warnings (SGI/Linux).  Update some things to coding standard.  (Class variables using '_' at end of name.)

--- src/Packages/Uintah/CCA/Ports/LoadBalancer.h
+++ src/Packages/Uintah/CCA/Ports/LoadBalancer.h
@@ -98,13 +98,13 @@ WARNING
     virtual const PatchSet* createPerProcessorPatchSet(const LevelP& level) = 0;
 
     //! For dynamic load balancers, Check if we need to rebalance the load, and do so if necessary.
-    virtual bool possiblyDynamicallyReallocate(const GridP&, bool force) { return false;}
+    virtual bool possiblyDynamicallyReallocate(const GridP&, bool /*force*/) { return false;}
 
     //! Returns the value of n (every n procs it performs output tasks).
     virtual int getNthProc() { return 1; }
 
     //! Tells the load balancer on which procs data was output.
-    virtual void restartInitialize(ProblemSpecP&, XMLURL tsurl) {}
+    virtual void restartInitialize(ProblemSpecP&, XMLURL /*tsurl*/) {}
   private:
     LoadBalancer(const LoadBalancer&);
     LoadBalancer& operator=(const LoadBalancer&);
