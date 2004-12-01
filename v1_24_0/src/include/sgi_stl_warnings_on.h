dca87dfa8c9c95e9941c6ad8c1ed867139805178
J. Davison de St. Germain <dav@sci.utah.edu>
2004-12-01 19:17:29 +0000

    Fix compiler warnings by turning off note about 'includes in a extern C' statement.  Added comments about what these files are for.  (Sync with trunk.)

--- v1_24_0/src/include/sgi_stl_warnings_on.h
+++ v1_24_0/src/include/sgi_stl_warnings_on.h
@@ -26,10 +26,15 @@
    DEALINGS IN THE SOFTWARE.
 */
 
+//
+// This file is used to turn back on SGI CC compiler warnings that
+// have been turned off by using sgi_stl_warnings_off.h.
+//
 
 #if defined(__sgi) && !defined(__GNUC__) && (_MIPS_SIM != _MIPS_SIM_ABI32)
 #pragma reset woff 1110
 #pragma reset woff 1424
 #pragma reset woff 1506
 #pragma reset woff 3201
+#pragma reset woff 3625
 #endif
