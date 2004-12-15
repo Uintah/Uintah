06143fea4ee0ebfdaff6748dc101a8d5adf2c067
Todd Harman <harman@sci.utah.edu>
2004-12-15 00:44:11 +0000

    updated prototypes

--- Packages/Uintah/CCA/Components/Solvers/HypreSolver.h
+++ Packages/Uintah/CCA/Components/Solvers/HypreSolver.h
@@ -12,15 +12,19 @@ namespace Uintah {
     virtual ~HypreSolver2();
 
     virtual SolverParameters* readParameters(ProblemSpecP& params,
-					     const std::string& name);
+                                             const std::string& name);
 
     virtual void scheduleSolve(const LevelP& level, SchedulerP& sched,
-			       const MaterialSet* matls,
-			       const VarLabel* A, const VarLabel* x,
-			       bool modifies_x,
-			       const VarLabel* b, const VarLabel* guess,
-			       Task::WhichDW guess_dw,
-			       const SolverParameters* params);
+                               const MaterialSet* matls,
+                               const VarLabel* A,    
+                               Task::WhichDW which_A_dw,  
+                               const VarLabel* x,
+                               bool modifies_x,
+                               const VarLabel* b,    
+                               Task::WhichDW which_b_dw,  
+                               const VarLabel* guess,
+                               Task::WhichDW guess_dw,
+                               const SolverParameters* params);
   private:
   };
 }
