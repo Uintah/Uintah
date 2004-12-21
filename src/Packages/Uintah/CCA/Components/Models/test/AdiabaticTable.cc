fd1880e81a86e9c3a8b07cfa00d8d70a8e5ea325
Todd Harman <harman@sci.utah.edu>
2004-12-21 20:40:21 +0000

    removed the warning that forced the user to   setenv SCI_DEBUG oldStyleAdvect:+ That option has been removed from ICE.cc

--- src/Packages/Uintah/CCA/Components/Models/test/AdiabaticTable.cc
+++ src/Packages/Uintah/CCA/Components/Models/test/AdiabaticTable.cc
@@ -39,9 +39,7 @@ using namespace std;
 //  ADIABATIC_TABLE_DBG:  dumps out during problemSetup
 static DebugStream cout_doing("MODELS_DOING_COUT", false);
 static DebugStream cout_dbg("ADIABATIC_TABLE_DBG_COUT", false);
-/*`==========TESTING==========*/
-static DebugStream oldStyleAdvect("oldStyleAdvect",false); 
-/*==========TESTING==========`*/
+
 //______________________________________________________________________              
 AdiabaticTable::AdiabaticTable(const ProcessorGroup* myworld, 
                      ProblemSpecP& params)
@@ -97,19 +95,6 @@ AdiabaticTable::Region::Region(GeometryPiece* piece, ProblemSpecP& ps)
 void AdiabaticTable::problemSetup(GridP&, SimulationStateP& in_state,
                         ModelSetup* setup)
 {
-/*`==========TESTING==========*/
-if (!oldStyleAdvect.active()){
-  ostringstream desc;
-  desc<< "\n----------------------------\n"
-      <<" ICE need the following environmental variable \n"
-       << " \t setenv SCI_DEBUG oldStyleAdvect:+ \n"
-       << "for this model to work.  This is gross--Todd"
-       << "\n----------------------------\n";
-  throw ProblemSetupException(desc.str());  
-} 
-/*==========TESTING==========`*/
-
-
   cout_doing << "Doing problemSetup \t\t\t\tADIABATIC_TABLE" << endl;
   sharedState = in_state;
   d_matl = sharedState->parseAndLookupMaterial(params, "material");
