10d1586306b3109c41dadfeb99e23e8807495b5c
J. Davison de St. Germain <dav@sci.utah.edu>
2004-12-28 22:21:51 +0000

    Changed names to use capital first letter.  Untabified files.  Added moveToMouse

---
+++ v1_24_0/src/Dataflow/GUI/BundleSetBundle.tcl
@@ -0,0 +1,139 @@
+##
+#
+#  For more information, please see: http://software.sci.utah.edu
+# 
+#  The MIT License
+# 
+#  Copyright (c) 2004 Scientific Computing and Imaging Institute,
+#  University of Utah.
+# 
+#  License for the specific language governing rights and limitations under
+#  Permission is hereby granted, free of charge, to any person obtaining a
+#  copy of this software and associated documentation files (the "Software"),
+#  to deal in the Software without restriction, including without limitation
+#  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+#  and/or sell copies of the Software, and to permit persons to whom the
+#  Software is furnished to do so, subject to the following conditions:
+# 
+#  The above copyright notice and this permission notice shall be included
+#  in all copies or substantial portions of the Software.
+# 
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+#  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+#  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+#  DEALINGS IN THE SOFTWARE.
+#
+
+
+itcl_class SCIRun_Bundle_BundleSetBundle {
+    inherit Module
+    constructor {config} {
+        set name BundleSetBundle
+        set_defaults
+    }
+
+    method set_defaults {} {
+
+        global $this-bundle1-name
+        global $this-bundle2-name
+        global $this-bundle3-name
+        global $this-bundle1-usename
+        global $this-bundle2-usename
+        global $this-bundle3-usename
+        global $this-bundlename
+        
+        set $this-bundle1-name "bundle1"
+        set $this-bundle2-name "bundle2"
+        set $this-bundle3-name "bundle3"
+        set $this-bundle1-usename 0
+        set $this-bundle2-usename 0
+        set $this-bundle3-usename 0
+        set $this-bundlename ""
+
+    }
+
+    method ui {} {
+        
+        set w .ui[modname]
+        if {[winfo exists $w]} {
+            return
+        }
+
+        # input bundle names
+
+        global $this-bundle1-name
+        global $this-bundle2-name
+        global $this-bundle3-name
+        global $this-bundle1-usename
+        global $this-bundle2-usename
+        global $this-bundle3-usename
+        global $this-bundlename
+
+        toplevel $w 
+
+        wm minsize $w 100 150
+
+        
+        iwidgets::labeledframe $w.frame -labeltext "BUNDLE INPUTS"
+        set childframe [$w.frame childsite]
+        pack $w.frame -fill x
+        frame $w.frame2
+        pack $w.frame2 -fill x
+        label $w.frame2.label -text "Name bundle object :"
+        entry $w.frame2.entry -textvariable $this-bundlename
+        pack $w.frame2.label -side left 
+        pack $w.frame2.entry -side left -fill x
+        
+        iwidgets::tabnotebook $childframe.pw -height 100 -tabpos n
+        $childframe.pw add -label "bundle1"
+        $childframe.pw add -label "bundle2" 
+        $childframe.pw add -label "bundle3" 
+        $childframe.pw select 0
+
+        pack $childframe.pw -fill x -expand yes
+
+        set bundle1 [$childframe.pw childsite 0]
+        set bundle2 [$childframe.pw childsite 1]
+        set bundle3 [$childframe.pw childsite 2]
+
+        frame $bundle1.name
+        frame $bundle1.options
+        pack $bundle1.name $bundle1.options -side top -fill x -expand yes -padx 5p
+
+        label $bundle1.name.label -text "Name"
+        entry $bundle1.name.entry -textvariable $this-bundle1-name
+        pack $bundle1.name.label -side left 
+        pack $bundle1.name.entry -side left -fill x -expand yes
+        checkbutton $bundle1.options.usename -text "Use object name" -variable $this-bundle1-usename
+        pack $bundle1.options.usename -side top -fill x
+        
+        frame $bundle2.name
+        frame $bundle2.options
+        pack $bundle2.name $bundle2.options -side top -fill x -expand yes -padx 5p
+
+        label $bundle2.name.label -text "Name"
+        entry $bundle2.name.entry -textvariable $this-bundle2-name
+        pack $bundle2.name.label -side left 
+        pack $bundle2.name.entry -side left -fill x -expand yes
+        checkbutton $bundle2.options.usename -text "Use object name" -variable $this-bundle2-usename
+        pack $bundle2.options.usename -side top -fill x
+
+        
+        frame $bundle3.name
+        frame $bundle3.options
+        pack $bundle3.name $bundle3.options -side top -fill x -expand yes -padx 5p
+
+        label $bundle3.name.label -text "Name"
+        entry $bundle3.name.entry -textvariable $this-bundle3-name
+        pack $bundle3.name.label -side left 
+        pack $bundle3.name.entry -side left -fill x -expand yes
+        checkbutton $bundle3.options.usename -text "Use object name" -variable $this-bundle3-usename
+        pack $bundle3.options.usename -side top -fill x
+
+        makeSciButtonPanel $w $w $this
+        moveToCursor $w
+    }
+}
