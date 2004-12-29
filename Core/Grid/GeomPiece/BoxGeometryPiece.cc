13ca98e59281cc7c4ada18515129417971d82a2b
John Schmidt <jas@sci.utah.edu>
2004-12-29 23:30:43 +0000

    Remove uneeded include file.

--- Core/Grid/GeomPiece/BoxGeometryPiece.cc
+++ Core/Grid/GeomPiece/BoxGeometryPiece.cc
@@ -1,5 +1,4 @@
 #include <Packages/Uintah/Core/Grid/GeomPiece/BoxGeometryPiece.h>
-#include <Packages/Uintah/Core/Grid/GeomPiece/GeometryPieceFactory.h>
 #include <Packages/Uintah/Core/ProblemSpec/ProblemSpec.h>
 #include <Packages/Uintah/Core/Exceptions/ProblemSetupException.h>
 #include <Core/Malloc/Allocator.h>
