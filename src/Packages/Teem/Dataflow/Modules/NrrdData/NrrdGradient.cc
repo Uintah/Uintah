1c0299b8a7faf503e7ff52f7a55dac5ae3a017ed
David Weinstein <dmw@sci.utah.edu>
2004-12-30 17:27:36 +0000

    - Complete implementation, but not yet tested

--- src/Packages/Teem/Dataflow/Modules/NrrdData/NrrdGradient.cc
+++ src/Packages/Teem/Dataflow/Modules/NrrdData/NrrdGradient.cc
@@ -64,7 +64,7 @@ NrrdGradient::~NrrdGradient() {
 void 
 NrrdGradient::execute()
 {
-  NrrdDataHandle nrrd_handle;
+  NrrdDataHandle nin_handle;
   update_state(NeedData);
   inrrd_ = (NrrdIPort *)get_iport("InputNrrd");
   onrrd_ = (NrrdOPort *)get_oport("OutputNrrd");
@@ -77,38 +77,106 @@ NrrdGradient::execute()
     error("Unable to initialize oport 'OutputNrrd'.");
     return;
   }
-  if (!inrrd_->get(nrrd_handle))
+  if (!inrrd_->get(nin_handle))
     return;
 
-  if (!nrrd_handle.get_rep()) {
+  if (!nin_handle.get_rep()) {
     error("Empty input Nrrd.");
     return;
   }
 
-  Nrrd *nin = nrrd_handle->nrrd;
+  Nrrd *nin = nin_handle->nrrd;
   Nrrd *nout = nrrdNew();
+  int size[NRRD_DIM_MAX];
+  int dim;
+
+  // Create a local array of axis sizes, so we can allocate the output Nrrd
+  for (dim=0; dim<nin->dim; dim++)
+    size[dim+1]=nin->axis[dim].size;
+  size[0]=nin->dim;
+
+  // Allocate the nrrd's data, set the size of each axis
+  nrrdAlloc(nout, nrrdTypeDouble, nin->dim+1, size);
+
+  // Set axis info for (new) axis 0
+  if (nin->dim == 2) nout->axis[0].kind=nrrdKind2Vector;
+  else if (nin->dim == 3) nout->axis[0].kind=nrrdKind3Vector;
+  else if (nin->dim == 4) nout->axis[0].kind=nrrdKind4Vector;
+  else nout->axis[0].kind=nrrdKindList;
+  nout->axis[0].center=nin->axis[0].center;
+  nout->axis[0].spacing=AIR_NAN;
+  nout->axis[0].min=AIR_NAN;
+  nout->axis[0].max=AIR_NAN;
+
+  double spacing_inv[NRRD_DIM_MAX];
+  double min, max, spacing;
+  int center;
+
+  // Copy the other axes' info
+  int nsamples=1;
+  for (dim=0; dim<nin->dim; dim++) {
+    nout->axis[dim+1].kind=nin->axis[dim].kind;
+    nout->axis[dim+1].center=center=nin->axis[dim].center;
+    nout->axis[dim+1].spacing=spacing=nin->axis[dim].spacing;
+    nout->axis[dim+1].min=min=nin->axis[dim].min;
+    nout->axis[dim+1].max=max=nin->axis[dim].max;
+    nsamples *= nin->axis[dim].size;
+    // Note spacing so we can correctly scale derivatives
+    if (min != AIR_NAN && max != AIR_NAN) {
+      if (center == nrrdCenterNode) 
+	spacing_inv[dim]=(size[dim+1]-1.)/(max-min);
+      else
+	spacing_inv[dim]=(size[dim+1]*1.)/(max-min);
+    } else {
+      if (spacing != AIR_NAN)
+	spacing_inv[dim]=spacing;
+      else
+	spacing_inv[dim]=AIR_NAN;
+    }
+  }
   
-  if (nrrdFlip(nout, nin, 0)) {
-   char *err = biffGetDone(NRRD);
-    error(string("Error Flipping nrrd: ") + err);
-    free(err);
+  // Build the resampling kernel
+  NrrdResampleInfo *infoD = nrrdResampleInfoNew();
+  infoD->boundary = nrrdBoundaryBleed;
+  infoD->type = nrrdTypeDouble;
+  infoD->renormalize = AIR_TRUE;
+  for (dim=0; dim<nin->dim; dim++) {
+    infoD->samples[dim] = nin->axis[dim].size;
+    infoD->min[dim] = nin->axis[dim].min;
+    infoD->max[dim] = nin->axis[dim].max;
   }
 
-  NrrdData *nrrd = scinew NrrdData;
-  nrrd->nrrd = nout;
+  // This Nrrd will be reused through each loop iteration -- it holds
+  //   the first spatial derivative of 'nin' with respect to the current
+  //   dimension, 'dim'
+  Nrrd *deriv=nrrdNew();
+  for (dim=0; dim<nin->dim; dim++) {
+    for (int d=0; d<nin->dim; d++) 
+      if (d==dim)
+	infoD->kernel[d]=nrrdKernelCentDiff;
+      else
+	infoD->kernel[d]=NULL;
+    nrrdSpatialResample(deriv, nin, infoD);
 
-  NrrdDataHandle out(nrrd);
-  // Copy the properties.
-  *((PropertyManager *) out.get_rep()) =
-    *((PropertyManager *) nrrd_handle.get_rep());
-
-  // Copy the axis kinds
-  for (int i=0; i<nin->dim; i++) {
-    nout->axis[i].kind = nin->axis[i].kind;
+    // copy data out of NrrdCurr into nout
+    double *nout_data_ptr = (double *)(nout->data);
+    double *deriv_data_ptr = (double *)(deriv->data);
+    nout_data_ptr += dim;
+    for (int s=0; s<nsamples; s++, nout_data_ptr+=nin->dim, deriv_data_ptr++)
+      *nout_data_ptr = *deriv_data_ptr * spacing_inv[dim];
   }
+  nrrdNuke(deriv);
+  nrrdResampleInfoNix(infoD);
 
-  onrrd_->send(out);
+  // Create SCIRun data structure wrapped around nout
+  NrrdData *nd = scinew NrrdData;
+  nd->nrrd = nout;
+  NrrdDataHandle nout_handle(nd);
 
+  // Copy the properties
+  *((PropertyManager *) nout_handle.get_rep()) =
+    *((PropertyManager *) nin_handle.get_rep());
+  onrrd_->send(nout_handle);
 }
 
 } // End namespace SCITeem
