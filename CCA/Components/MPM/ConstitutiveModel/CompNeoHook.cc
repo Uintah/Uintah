019bd908121d9095370dcac3534af1b878778d76
Biswajit Banerjee <banerjee@sci.utah.edu>
2005-01-04 23:43:15 +0000

    Made previously private variables protected for used in derived classes CNHDamage and CNHPDamage.

--- CCA/Components/MPM/ConstitutiveModel/CompNeoHook.cc
+++ CCA/Components/MPM/ConstitutiveModel/CompNeoHook.cc
@@ -435,7 +435,8 @@ namespace Uintah {
     static TypeDescription* td = 0;
     if(!td){
       td = scinew TypeDescription(TypeDescription::Other,
-                                  "CompNeoHook::StateData", true, &makeMPI_CMData);
+                                  "CompNeoHook::StateData", 
+                                  true, &makeMPI_CMData);
     }
     return td;
   }
