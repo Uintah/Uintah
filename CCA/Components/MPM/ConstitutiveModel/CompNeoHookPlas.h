65f199665b1c71cb287c8a64d2f3f2150f60d5c7
Biswajit Banerjee <banerjee@sci.utah.edu>
2005-01-04 23:49:07 +0000

    Rearranged a bit.

--- CCA/Components/MPM/ConstitutiveModel/CompNeoHookPlas.h
+++ CCA/Components/MPM/ConstitutiveModel/CompNeoHookPlas.h
@@ -2,7 +2,7 @@
 #define __NEOHOOKPLAS_CONSTITUTIVE_MODEL_H__
 
 
-#include "ConstitutiveModel.h"	
+#include "ConstitutiveModel.h"  
 #include <math.h>
 #include <Packages/Uintah/Core/Math/Matrix3.h>
 #include <Packages/Uintah/Core/ProblemSpec/ProblemSpecP.h>
@@ -51,15 +51,15 @@ WARNING
       double FlowStress;
       double K;
       double Alpha;
-    };	 
+    };   
     struct StateData {
       double Alpha;
-    };	 
+    };   
   private:
     friend const TypeDescription* fun_getTypeDescription(StateData*);
 
     CMData d_initialData;
-	 
+         
     // Prevent copying of this class
     // copy constructor
     //CompNeoHookPlas(const CompNeoHookPlas &cm);
@@ -69,31 +69,31 @@ WARNING
     // constructors
     CompNeoHookPlas(ProblemSpecP& ps, MPMLabel* lb,MPMFlags* flag);
     CompNeoHookPlas(const CompNeoHookPlas* cm);
-	 
+         
     // destructor 
     virtual ~CompNeoHookPlas();
-	 
+         
     // compute stable timestep for this patch
     virtual void computeStableTimestep(const Patch* patch,
-				       const MPMMaterial* matl,
-				       DataWarehouse* new_dw);
+                                       const MPMMaterial* matl,
+                                       DataWarehouse* new_dw);
 
     // compute stress at each particle in the patch
     virtual void computeStressTensor(const PatchSubset* patches,
-				     const MPMMaterial* matl,
-				     DataWarehouse* old_dw,
-				     DataWarehouse* new_dw);
+                                     const MPMMaterial* matl,
+                                     DataWarehouse* old_dw,
+                                     DataWarehouse* new_dw);
 
     virtual void computeStressTensor(const PatchSubset* ,
-				     const MPMMaterial* ,
-				     DataWarehouse* ,
-				     DataWarehouse* ,
+                                     const MPMMaterial* ,
+                                     DataWarehouse* ,
+                                     DataWarehouse* ,
 #ifdef HAVE_PETSC
                                      MPMPetscSolver* ,
 #else
                                      SimpleSolver* ,
 #endif
-				     const bool )
+                                     const bool )
     {
     }
 
@@ -105,44 +105,44 @@ WARNING
 
     // initialize  each particle's constitutive model data
     virtual void initializeCMData(const Patch* patch,
-				  const MPMMaterial* matl,
-				  DataWarehouse* new_dw);
+                                  const MPMMaterial* matl,
+                                  DataWarehouse* new_dw);
 
     virtual void allocateCMDataAddRequires(Task* task, const MPMMaterial* matl,
-					   const PatchSet* patch, 
-					   MPMLabel* lb) const;
+                                           const PatchSet* patch, 
+                                           MPMLabel* lb) const;
 
     virtual void allocateCMDataAdd(DataWarehouse* new_dw,
-				   ParticleSubset* subset,
-				   map<const VarLabel*, ParticleVariableBase*>* newState,
-				   ParticleSubset* delset,
-				   DataWarehouse* old_dw);
+                                   ParticleSubset* subset,
+                                   map<const VarLabel*, ParticleVariableBase*>* newState,
+                                   ParticleSubset* delset,
+                                   DataWarehouse* old_dw);
 
 
     virtual void addInitialComputesAndRequires(Task* task,
-					       const MPMMaterial* matl,
-					       const PatchSet* patches) const;
+                                               const MPMMaterial* matl,
+                                               const PatchSet* patches) const;
 
     virtual void addComputesAndRequires(Task* task,
-					const MPMMaterial* matl,
-					const PatchSet* patches) const;
+                                        const MPMMaterial* matl,
+                                        const PatchSet* patches) const;
 
     virtual void addComputesAndRequires(Task* task,
-					const MPMMaterial* matl,
-					const PatchSet* patches,
-					const bool recursion) const;
+                                        const MPMMaterial* matl,
+                                        const PatchSet* patches,
+                                        const bool recursion) const;
 
     virtual void addParticleState(std::vector<const VarLabel*>& from,
-				  std::vector<const VarLabel*>& to);
+                                  std::vector<const VarLabel*>& to);
 
     virtual double computeRhoMicroCM(double pressure,
-				     const double p_ref,
-				     const MPMMaterial* matl);
+                                     const double p_ref,
+                                     const MPMMaterial* matl);
 
     virtual void computePressEOSCM(double rho_m, double& press_eos,
-				   double p_ref,
-				   double& dp_drho, double& ss_new,
-				   const MPMMaterial* matl);
+                                   double p_ref,
+                                   double& dp_drho, double& ss_new,
+                                   const MPMMaterial* matl);
 
     virtual double getCompressibility();
 
