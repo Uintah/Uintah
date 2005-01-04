2a36e5220e113c22a10ca390182ea5f1fdd9ff73
Biswajit Banerjee <banerjee@sci.utah.edu>
2005-01-04 23:56:53 +0000

    1) Does not need any elastic properties any more (provided by default). 2) Can be used with implicit mpm.

--- CCA/Components/MPM/ConstitutiveModel/RigidMaterial.cc
+++ CCA/Components/MPM/ConstitutiveModel/RigidMaterial.cc
@@ -32,8 +32,10 @@ RigidMaterial::RigidMaterial(ProblemSpecP& ps, MPMLabel* Mlb, MPMFlags* Mflag)
   lb = Mlb;
   flag = Mflag;
   NGN = 1;
-  ps->require("shear_modulus",d_initialData.G);
-  ps->require("bulk_modulus",d_initialData.K);
+  d_initialData.G = 1.0e20;
+  ps->get("shear_modulus",d_initialData.G);
+  d_initialData.K = 1.0e20;
+  ps->get("bulk_modulus",d_initialData.K);
 }
 
 RigidMaterial::RigidMaterial(const RigidMaterial* cm)
@@ -56,42 +58,13 @@ RigidMaterial::initializeCMData(const Patch* patch,
 {
   // Initialize the variables shared by all constitutive models
   // This method is defined in the ConstitutiveModel base class.
-  initSharedDataForExplicit(patch, matl, new_dw);
-  computeStableTimestep(patch, matl, new_dw);
-}
-
-void 
-RigidMaterial::computeStableTimestep(const Patch* patch,
-                                     const MPMMaterial* matl,
-                                     DataWarehouse* new_dw)
-{
-  // This is only called for the initial timestep - all other timesteps
-  // are computed as a side-effect of computeStressTensor
-  Vector dx = patch->dCell();
-  int dwi = matl->getDWIndex();
-  ParticleSubset* pset = new_dw->getParticleSubset(dwi, patch);
-  if (pset->numParticles() == 0) return;
-
-  constParticleVariable<double> pmass, pvolume;
-  constParticleVariable<Vector> pvelocity;
-
-  new_dw->get(pmass,     lb->pMassLabel,     pset);
-  new_dw->get(pvolume,   lb->pVolumeLabel,   pset);
-  new_dw->get(pvelocity, lb->pVelocityLabel, pset);
-
-  // Compute wave speed at the first particle
-  particleIndex idx = *(pset->begin());
-  double G = d_initialData.G;
-  double K = d_initialData.K;
-  double c_dil = sqrt((K + 4.*G/3.)*pvolume[idx]/pmass[idx]);
-  Vector WaveSpeed(1.e-12,1.e-12,1.e-12);
-  WaveSpeed=Vector(Max(c_dil+fabs(pvelocity[idx].x()),WaveSpeed.x()),
-                   Max(c_dil+fabs(pvelocity[idx].y()),WaveSpeed.y()),
-                   Max(c_dil+fabs(pvelocity[idx].z()),WaveSpeed.z()));
-  WaveSpeed = dx/WaveSpeed;
-  double delT_new = WaveSpeed.minComponent();
-  new_dw->put(delt_vartype(patch->getLevel()->adjustDelt(delT_new)), 
+  if (flag->d_integrator == MPMFlags::Implicit) 
+    initSharedDataForImplicit(patch, matl, new_dw);
+  else {
+    initSharedDataForExplicit(patch, matl, new_dw);
+    new_dw->put(delt_vartype(patch->getLevel()->adjustDelt(1.0)), 
               lb->delTLabel);
+  }
 }
 
 void 
@@ -103,7 +76,11 @@ RigidMaterial::addComputesAndRequires(Task* task,
   // constitutive models.  The method is defined in the ConstitutiveModel
   // base class.
   const MaterialSubset* matlset = matl->thisMaterial();
-  addSharedCRForExplicit(task, matlset, patches);
+  if (flag->d_integrator == MPMFlags::Implicit) {
+    addSharedCRForImplicit(task, matlset, patches);
+  } else {
+    addSharedCRForExplicit(task, matlset, patches);
+  }
 }
 
 void 
@@ -112,7 +89,29 @@ RigidMaterial::computeStressTensor(const PatchSubset* patches,
                                    DataWarehouse* old_dw,
                                    DataWarehouse* new_dw)
 {
+  if (flag->d_integrator == MPMFlags::Implicit) {
+    computeStressTensorImplicit(patches, matl, old_dw, new_dw);
+    return;
+  }
   carryForward(patches, matl, old_dw, new_dw);
+}
+
+void 
+RigidMaterial::computeStressTensorImplicit(const PatchSubset* patches,
+                                           const MPMMaterial* matl,
+                                           DataWarehouse* old_dw,
+                                           DataWarehouse* new_dw)
+{
+  int dwi = matl->getDWIndex();
+  for(int p=0;p<patches->size();p++){
+    const Patch* patch = patches->get(p);
+    ParticleSubset* pset = old_dw->getParticleSubset(dwi, patch);
+
+    // Carry forward the data common to all constitutive models 
+    // when using RigidMPM.
+    // This method is defined in the ConstitutiveModel base class.
+    carryForwardSharedData(pset, old_dw, new_dw, matl);
+  }
 }
 
 void 
@@ -130,11 +129,41 @@ RigidMaterial::carryForward(const PatchSubset* patches,
     // when using RigidMPM.
     // This method is defined in the ConstitutiveModel base class.
     carryForwardSharedData(pset, old_dw, new_dw, matl);
-
-    // Carry forward the data local to this constitutive model 
-    new_dw->put(delt_vartype(patch->getLevel()->adjustDelt(1.e10)), 
+    new_dw->put(delt_vartype(patch->getLevel()->adjustDelt(1.0)), 
                 lb->delTLabel);
     new_dw->put(sum_vartype(0.),     lb->StrainEnergyLabel);
+  }
+}
+
+void
+RigidMaterial::addComputesAndRequires(Task* task,
+                                      const MPMMaterial* matl,
+                                      const PatchSet* patches,
+                                      const bool recurse) const
+{
+  const MaterialSubset* matlset = matl->thisMaterial();
+  addSharedCRForImplicit(task, matlset, patches, recurse);
+}
+
+void 
+RigidMaterial::computeStressTensor(const PatchSubset* patches,
+                                   const MPMMaterial* matl,
+                                   DataWarehouse* old_dw,
+                                   DataWarehouse* new_dw,
+#ifdef HAVE_PETSC
+                                   MPMPetscSolver* ,
+#else
+                                   SimpleSolver* ,
+#endif
+                                   const bool )
+
+{
+  int dwi = matl->getDWIndex();
+  DataWarehouse* parent_dw = new_dw->getOtherDataWarehouse(Task::ParentOldDW);
+  for (int pp = 0; pp < patches->size(); pp++) {
+    const Patch* patch = patches->get(pp);
+    ParticleSubset* pset = parent_dw->getParticleSubset(dwi, patch);
+    carryForwardSharedData(pset, parent_dw, new_dw, matl);
   }
 }
 
@@ -172,8 +201,4 @@ double RigidMaterial::getCompressibility()
 {
   return (1.0/d_initialData.K);
 }
-
-#if defined(__sgi) && !defined(__GNUC__) && (_MIPS_SIM != _MIPS_SIM_ABI32)
-#pragma set woff 1209
-#endif
 
