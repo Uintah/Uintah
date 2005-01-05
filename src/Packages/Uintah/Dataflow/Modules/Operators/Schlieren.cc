064acd4bbe39cebe14b7d98d4acc5a90b26c0bd3
Todd Harman <harman@sci.utah.edu>
2005-01-05 01:20:10 +0000

    -Put back some necessary code. -Turned on gz calculation.

--- src/Packages/Uintah/Dataflow/Modules/Operators/Schlieren.cc
+++ src/Packages/Uintah/Dataflow/Modules/Operators/Schlieren.cc
@@ -1,8 +1,6 @@
 
 #include <Packages/Uintah/Dataflow/Modules/Operators/Schlieren.h>
-
 #include <Packages/Uintah/Core/Disclosure/TypeUtils.h>
-
 #include <Core/Datatypes/LatVolMesh.h>
 #include <Core/Datatypes/LatVolField.h>
 #include <Core/Geometry/BBox.h>
@@ -40,33 +38,36 @@ Schlieren::execute(void)
 
   FieldHandle fh = 0;
   
-  
-  if( LatVolField<double> *scalarField1 =
+  if( LatVolField<double> *scalarField =
       dynamic_cast<LatVolField<double>*>(hTF.get_rep())) {
     
     LatVolField<double>  *output = 0;  
     output = scinew LatVolField<double>(hTF->basis_order());
     
     // compute the image
-    computeSchlierenImage(scalarField1,output); 
+    computeSchlierenImage(scalarField,output); 
     
     fh = output;
   }
   if( fh.get_rep() != 0 )
     sfout_->send(fh);
 }
-
+//______________________________________________________________________
+//  This computes a schlieren image of the density field.
+//  SI = sqrt(gx^2 + gy^2 + gz^2)
 template<class ScalarField>       
 void
-computeSchlierenImage(ScalarField* density, ScalarField* output)
+Schlieren::computeSchlierenImage(ScalarField* density, ScalarField* output)
 {
+
+  initField(density, output);
   // Grab the values from the GUI at the beginning of the loop (so
   // that the user doesn't change them in the middle.
   double dx = dx_.get();
   double dy = dy_.get();
   double dz = dz_.get();
 
-  // only CC data
+  // only works with cell Centered data
   ASSERT( density->basis_order() == 0 );
   typename ScalarField::mesh_handle_type sfmh =density->get_typed_mesh();
   typename ScalarField::mesh_type *mesh =density->get_typed_mesh().get_rep();
@@ -83,41 +84,33 @@ computeSchlierenImage(ScalarField* density, ScalarField* output)
   LatVolMesh::Cell::index_type min_index, max_index;
   mesh->get_cells(min_index, max_index, BBox(min, max));
   
-  cout << " mesh min: " << min << " max " << max << endl;
-  cout << " min " << min_index << " max " << max_index << endl;
-  cout << " v_begin " << v_it << " v_end " << v_end << endl;
+//  debugging
+//  cout << " dx " << dx << " dy " << dy << " dz " << dz << endl; 
+//  cout << " mesh min: " << min << " max " << max << endl;
+//  cout << " min " << min_index << " max " << max_index << endl;
+//  cout << " v_begin " << v_it << " v_end " << v_end << endl;
 
   for( ; v_it != v_end; ++v_it){ 
     double schlieren = 0;    
-    LatVolMesh::Cell::index_type c(mesh, v_it.i_+1,  v_it.j_+1, v_it.k_);
-   
-   
-    if ( (v_it.i_ > min_index.i_    && v_it.j_ > min_index.j_    && v_it.k_ > min_index.k_ ) && 
-         (v_it.i_ < max_index.i_ -1 && v_it.j_ < max_index.j_-1  && v_it.k_ < max_index.k_ ) ){
+    LatVolMesh::Cell::index_type c(mesh, v_it.i_,  v_it.j_, v_it.k_);
+
+    // don't get near the edge of the computational domain
+    if ( (c.i_ > min_index.i_  && c.j_ > min_index.j_    && c.k_ > min_index.k_ ) &&   
+         (c.i_ < max_index.i_  && c.j_ < max_index.j_    && c.k_ < max_index.k_ ) ){   
 
       LatVolMesh::Cell::index_type R(mesh, c.i_+1, c.j_,   c.k_);
       LatVolMesh::Cell::index_type L(mesh, c.i_-1, c.j_,   c.k_);
       LatVolMesh::Cell::index_type T(mesh, c.i_,   c.j_+1, c.k_); 
       LatVolMesh::Cell::index_type B(mesh, c.i_,   c.j_-1, c.k_);
-     // LatVolMesh::Cell::index_type F(mesh, c.i_,   c.j_,   c.k_+1);
-     // LatVolMesh::Cell::index_type BK(mesh,c.i_,   c.j_,   c.k_-1);
-      
-#if 0  
-      cout<< "C " << c << "R " << R << " L " << L << " T " << T << " B " << B  << " F " << F << " BK " << BK << endl;
-      cout<< "R " << density->fdata()[R] << " L " << density->fdata()[L]  <<endl;
-      cout<< "T " << density->fdata()[T] << " B " << density->fdata()[B]  << endl;
-      cout<< "F " << density->fdata()[F] << " BK "<< density->fdata()[BK] << endl;
-#endif    
+      LatVolMesh::Cell::index_type F(mesh, c.i_,   c.j_,   c.k_+1);
+      LatVolMesh::Cell::index_type BK(mesh,c.i_,   c.j_,   c.k_-1);
+     
       double gx = (density->fdata()[R] - density->fdata()[L])/2.0  * dx;
       double gy = (density->fdata()[T] - density->fdata()[B])/2.0  * dy;
-      //double gz = (density->fdata()[F] - density->fdata()[BK])/2.0 * dz;
-      double gz = 0;
-
+      double gz = (density->fdata()[F] - density->fdata()[BK])/2.0 * dz;
+ 
       schlieren = sqrt(gx*gx + gy*gy + gz*gz);
-      
-//      Vector gradient(gx,gy,gz);
-//    cout << " gradient " << gradient << endl;
    }
-   density->fdata()[*v_it] =schlieren;
+   output->fdata()[c] =schlieren;
   }
 }
