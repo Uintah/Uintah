5e853a11d7cb8e05dd48c905b32869d35c9fc25b
Marty Cole <mjc@sci.utah.edu>
2005-01-06 01:22:21 +0000

    changes for latest MPM needs

--- src/StandAlone/utils/GenerateMPMData.cc
+++ src/StandAlone/utils/GenerateMPMData.cc
@@ -36,6 +36,7 @@
 #include <Core/Persistent/Pstreams.h>
 #include <Core/Datatypes/TetVolMesh.h>
 #include <Core/Datatypes/LatVolMesh.h>
+#include <Core/Datatypes/HexVolMesh.h>
 #include <Core/Geometry/BBox.h>
 #include <iostream>
 #include <fstream>
@@ -57,6 +58,115 @@ vector<unsigned> num_files;
 vector<string> prename;
 
 const unsigned int max_lines = 25000000;
+
+template<class Fld>
+void
+write_MPM_fibdir(FieldHandle& field_h, const string &outdir)
+{
+  // this outputs only one material
+  Fld *ifield = (Fld *) field_h.get_rep();
+  typename Fld::mesh_handle_type mesh = ifield->get_typed_mesh();
+
+  files = new vector<ofstream*>;
+  fcount.push_back(0);
+  num_files.push_back(1);
+  stringstream fname(stringstream::in | stringstream::out);
+  string nm;
+  fname << outdir << "/" << "heart-diastole";
+  fname >> nm;
+  prename.push_back(nm);
+
+  fname.clear();
+  fname << nm << "-"; 
+  fname.fill('0');
+  fname.width(4);
+  fname << right << 1;
+  string nm1;
+  fname >> nm1;
+  files->push_back(new ofstream(nm1.c_str(), ios_base::out));
+
+#if defined(__sgi)
+#  define round(var) ((var)>=0?(int)((var)+0.5):(int)((var)-0.5))
+#endif
+
+  mesh->synchronize(Mesh::NODES_E | Mesh::EDGES_E | 
+		    Mesh::FACES_E | Mesh::CELLS_E);
+  typename Fld::mesh_type::Cell::iterator iter, end;
+  mesh->begin( iter );
+  mesh->end( end );
+  
+  while (iter != end) {
+    Point o, s, t, u;
+    vector<double> coord(3, 0.0);
+    mesh->interpolate(o, coord, *iter);
+    coord[0] = 1.0;
+    mesh->interpolate(s, coord, *iter);
+    coord[0] = 0.0;
+    coord[1] = 1.0;
+    mesh->interpolate(t, coord, *iter);
+    coord[1] = 0.0;
+    coord[2] = 1.0;
+    mesh->interpolate(u, coord, *iter);
+
+
+    // assume length in mm's
+    double dx = (s - o).length();
+    double dy = (t - o).length();
+    double dz = (u - o).length();
+
+    double vol = dx * dy * dz; 
+    
+    const double part_per_mm = 4.;
+    const int div_per_x = (int)round(part_per_mm * dx);
+    const int div_per_y = (int)round(part_per_mm * dy);
+    const int div_per_z = (int)round(part_per_mm * dz);
+
+    const double part_vol = vol / (div_per_x * div_per_y * div_per_z);
+    
+    dx = 1. / (double)div_per_x;
+    dy = 1. / (double)div_per_y;
+    dz = 1. / (double)div_per_z;
+
+    for (double  k = dz * 0.5; k < 1.0; k += dz) {
+      for (double j = dy * 0.5; j < 1.0; j += dy) {
+	for (double i = dx * 0.5; i < 1.0; i += dx) {
+	  
+	  coord[0] = i;
+	  coord[1] = j;
+	  coord[2] = k;
+	  //cerr << "at : [" << i << "," << j << "," << k << "]" << endl;
+	  Point c;
+	  mesh->interpolate(c, coord, *iter);
+
+	  Vector v;
+	  ifield->interpolate(v, coord, *iter);
+
+	  if (fcount[0] > max_lines) {
+	    string nm;
+	    stringstream fname(stringstream::in | stringstream::out);
+	    fname << prename[0] << "-";
+	    fname.fill('0');
+	    fname.width(4);
+	    fname << right << ++num_files[0];
+	    fname >> nm;
+	    delete (*files)[0];
+	    (*files)[0] = new ofstream(nm.c_str(), ios_base::out);
+	    fcount[0] = 0;
+	  }
+	  ofstream* str = (*files)[0];
+	  (*str) << setprecision (9) 
+		 << c.x() << " " << c.y() << " " << c.z() << " " 
+		 << part_vol << " "
+		 << v.x() << " " << v.y() << " " << v.z() << endl;
+	  fcount[0]++;
+	}
+      }
+    }
+    cout << "." << *iter << ".";
+    ++iter;
+  }
+  cout << endl;
+}
 
 template<class Fld>
 void
@@ -138,7 +248,7 @@ write_MPM(FieldHandle& field_h, const string &outdir)
       ofstream* str = (*files)[val];
       (*str) << setprecision (9) <<c.x() << " " << c.y() << " " << c.z() 
 	     << " " << vol << endl;
-      fcount[val]++;
+       fcount[val]++;
     }
     
     if (count % ((int)(sizex*sizey*sizez*0.01)) == 0) { cout << "."; }
@@ -184,12 +294,19 @@ main(int argc, char **argv)
   }
 
   Pio(*in_stream, field_handle);
-  delete in_stream;
-  typedef TetVolMesh<TetLinearLgn<Point> > TVMesh;
-  typedef TetLinearLgn<int>  DatBasis;
-  typedef GenericField<TVMesh, DatBasis,    vector<int> > TVField;
-  write_MPM<TVField>(field_handle, outdir);
+//   delete in_stream;
+//   typedef TetVolMesh<TetLinearLgn<Point> > TVMesh;
+//   typedef TetLinearLgn<int>  DatBasis;
+//   typedef GenericField<TVMesh, DatBasis,    vector<int> > TVField;
+//   write_MPM<TVField>(field_handle, outdir);
 
+  typedef HexVolMesh<HexTrilinearLgn<Point> >    HVLinMesh;
+  typedef HexTrilinearLgn<double>                ScaDatBasis;
+  typedef HexTrilinearLgn<Vector>                VecDatBasis;
+  typedef GenericField<HVLinMesh, ScaDatBasis, vector<double> > HVLinSField;
+  typedef GenericField<HVLinMesh, VecDatBasis, vector<Vector> > HVLinVField;
+
+  write_MPM_fibdir<HVLinVField>(field_handle, outdir);
 
   return 0;
 }    
