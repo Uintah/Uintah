5c4f6d6586913c813ca3b1686c80fd0c8d83bcca
McKay Davis <mdavis@sci.utah.edu>
2005-01-20 19:58:43 +0000

    Remove unused reset button. Change calls from resize to redraw.

--- v1_24_0/src/Dataflow/GUI/EditColorMap2D.tcl
+++ v1_24_0/src/Dataflow/GUI/EditColorMap2D.tcl
@@ -503,15 +503,15 @@ itcl_class SCIRun_Visualization_EditColorMap2D {
 	    create_swatches
 	}
 
-        makeSciButtonPanel $w $w $this "\"Reset\" \"$this-c reset_gui\" \"\""
+        makeSciButtonPanel $w $w $this
         moveToCursor $w
     }
             
     method bind_events {w} {
         bind $w <Destroy>		"$this-c destroygl"
         # every time the OpenGL widget is displayed, redraw it
-        bind $w <Expose>		"$this-c resize"
-        bind $w <Configure>		"$this-c resize"
+        bind $w <Expose>		"$this-c redraw"
+        bind $w <Configure>		"$this-c redraw"
         bind $w <ButtonPress-1>		"$this-c mouse push %x %y %b"
         bind $w <ButtonPress-2>		"$this-c mouse push %x %y %b"
         bind $w <ButtonPress-3>		"$this-c mouse push %x %y %b"
@@ -531,10 +531,6 @@ itcl_class SCIRun_Visualization_EditColorMap2D {
         bind $w <Shift-ButtonPress-3>   "$this-c mouse scale_start %x %y %b"
         bind $w <Shift-Button3-Motion>  "$this-c mouse scale_motion %x %y %b"
         bind $w <Shift-ButtonRelease-3> "$this-c mouse scale_end %x %y %b"
-    }
-            
-    method create_gl { frame } {
-
     }
 }
 
