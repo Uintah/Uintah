76f3aa77336fa0e95c66d4993caae2b10a62469b
Michael Callahan <callahan@sci.utah.edu>
2005-01-21 19:09:38 +0000

    Remove bogus interpolant field instantiation

--- src/Core/Datatypes/cd_templates_fields_4.cc
+++ src/Core/Datatypes/cd_templates_fields_4.cc
@@ -55,7 +55,6 @@ template class GenericField<TriSurfMesh, vector<char> >;
 template class GenericField<TriSurfMesh, vector<unsigned int> >;
 template class GenericField<TriSurfMesh, vector<unsigned short> >;
 template class GenericField<TriSurfMesh, vector<unsigned char> >;
-template class GenericField<TriSurfMesh, vector<vector<pair<NodeIndex<int>,double> > > >;
 
 template class GenericField<CurveMesh, vector<Tensor> >;
 template class GenericField<CurveMesh, vector<Vector> >;
@@ -78,7 +77,6 @@ template class TriSurfField<char>;
 template class TriSurfField<unsigned int>;
 template class TriSurfField<unsigned short>;
 template class TriSurfField<unsigned char>;
-template class TriSurfField<vector<pair<NodeIndex<int>,double> > >;
 
 const TypeDescription* get_type_description(TriSurfField<Tensor> *);
 const TypeDescription* get_type_description(TriSurfField<Vector> *);
@@ -90,7 +88,6 @@ const TypeDescription* get_type_description(TriSurfField<char> *);
 const TypeDescription* get_type_description(TriSurfField<unsigned int> *);
 const TypeDescription* get_type_description(TriSurfField<unsigned short> *);
 const TypeDescription* get_type_description(TriSurfField<unsigned char> *);
-const TypeDescription* get_type_description(TriSurfField<vector<pair<NodeIndex<int>,double> > > *);
 
 template class CurveField<Tensor>;
 template class CurveField<Vector>;
