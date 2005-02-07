c74f941e1be426966fbae0cf9fadd43da6cc0397
McKay Davis <mdavis@sci.utah.edu>
2005-02-07 23:39:25 +0000

    Change names of a few parameter variables to be more descriptive

--- src/Dataflow/GUI/EditColorMap2D.tcl
+++ src/Dataflow/GUI/EditColorMap2D.tcl
@@ -60,7 +60,7 @@ itcl_class SCIRun_Visualization_EditColorMap2D {
         trace vdelete $this-marker w "$this unpickle"
     }
     
-    method raise_color {col color colMsg} {
+    method raise_color {button color module_command} {
         global $color
         set windowname .ui[modname]_color
         if {[winfo exists $windowname]} {
@@ -68,18 +68,18 @@ itcl_class SCIRun_Visualization_EditColorMap2D {
 	}
 	# makeColorPicker now creates the $window.color toplevel.
 	makeColorPicker $windowname $color \
-	    "$this set_color $col $color $colMsg" \
+	    "$this set_color $button $color $module_command" \
 	    "destroy $windowname"
     }
     
-    method set_color { col color { colMsg "" } } {
+    method set_color { button color { module_command "" } } {
 	upvar \#0 $color-r r $color-g g $color-b b
 	# format the r,g,b colors into a hexadecimal string representation
 	set colstr [format \#%04x%04x%04x [expr int($r * 65535)] \
 			[expr int($g * 65535)] [expr int($b * 65535)]]
-	$col config -background $colstr -activebackground $colstr
-	if { [string length $colMsg] } {
-	    $this-c $colMsg
+	$button config -background $colstr -activebackground $colstr
+	if { [string length $module_command] } {
+	    $this-c $module_command
 	}
     }
     
