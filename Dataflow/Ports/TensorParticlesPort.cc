5ee8e2a301e92f0c394d59686e17123ac1d337e3
Bryan Jeffrey Worthen <worthen@sci.utah.edu>
2005-02-22 17:50:17 +0000

    remove unnecessary share

--- Dataflow/Ports/TensorParticlesPort.cc
+++ Dataflow/Ports/TensorParticlesPort.cc
@@ -1,17 +1,16 @@
 
 #include <Packages/Uintah/Dataflow/Ports/TensorParticlesPort.h>
-#include <Packages/Uintah/share/share.h>
 #include <Core/Malloc/Allocator.h>
 
 using namespace SCIRun;
 using namespace Uintah;
 
 extern "C" {
-UINTAHSHARE IPort* make_TensorParticlesIPort(Module* module,
+IPort* make_TensorParticlesIPort(Module* module,
 					     const string& name) {
   return scinew SimpleIPort<TensorParticlesHandle>(module,name);
 }
-UINTAHSHARE OPort* make_TensorParticlesOPort(Module* module,
+OPort* make_TensorParticlesOPort(Module* module,
 					     const string& name) {
   return scinew SimpleOPort<TensorParticlesHandle>(module,name);
 }
