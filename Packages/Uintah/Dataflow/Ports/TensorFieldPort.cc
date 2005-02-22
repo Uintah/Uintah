13a31d8e7bf42c40f4569475a75a38da2ba646a6
Bryan Jeffrey Worthen <worthen@sci.utah.edu>
2005-02-22 17:50:17 +0000

    remove unnecessary share

--- Packages/Uintah/Dataflow/Ports/TensorFieldPort.cc
+++ Packages/Uintah/Dataflow/Ports/TensorFieldPort.cc
@@ -1,17 +1,16 @@
 
 #include <Packages/Uintah/Core/Datatypes/TensorFieldPort.h>
-#include <Packages/Uintah/share/share.h>
 #include <Core/Malloc/Allocator.h>
 
 using namespace SCIRun;
 using namespace Uintah;
 
 extern "C" {
-UINTAHSHARE IPort* make_TensorFieldIPort(Module* module,
+IPort* make_TensorFieldIPort(Module* module,
 					 const string& name) {
   return scinew SimpleIPort<TensorFieldHandle>(module,name);
 }
-UINTAHSHARE OPort* make_TensorFieldOPort(Module* module,
+OPort* make_TensorFieldOPort(Module* module,
 					 const string& name) {
   return scinew SimpleOPort<TensorFieldHandle>(module,name);
 }
