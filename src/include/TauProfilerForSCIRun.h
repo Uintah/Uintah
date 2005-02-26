db86ebe87a5e492fce8525f5fa38df6a11219591
Alan Morris <amorris@sci.utah.edu>
2005-02-26 00:18:31 +0000

    Added instructions for using TAU and PDT with SCIRun.

--- src/include/TauProfilerForSCIRun.h
+++ src/include/TauProfilerForSCIRun.h
@@ -26,11 +26,50 @@
    DEALINGS IN THE SOFTWARE.
 */
 
-//
 // This file defines all the TAU profiling macros to be "nothing".
 // If TAU is installed and SCIRun is configured to USE_TAU_PROFILING
 // then the real TAU Profile.h file will be used.
-// 
+
+
+
+/*
+   Instructions for using TAU with PDT (note that the version numbers used in
+   the example below will change in the future):
+
+
+   # get and build pdt and tau:
+   
+   wget http://www.cs.uoregon.edu/research/paracomp/proj/pdtoolkit/Download/pdtoolkit-3.3.1.tar.gz
+   tar -xzf pdtoolkit-3.3.1.tar.gz
+   cd pdtoolkit-3.3.1
+   ./configure
+   make ; make install
+   cd ..
+   
+   
+   wget http://www.cs.uoregon.edu/research/paracomp/proj/tau/tauprofile/dist/tau_latest.tar.gz
+   tar -xzf tau_latest.tar.gz
+   cd tau-2.14.2.1
+   ./configure -pdt=/path/to/pdtoolkit-3.3.1 -pthread
+   make install -j
+   cd ..
+   
+   # configure SCIRun with
+   --tau-makefile=/path/to/tau-2.14.2.1/include/Makefile
+   
+   # clean and build SCIRun:
+   gmake cleanreally
+   gmake
+   
+   
+   # At this point, you must clear the network before exiting to get profile data. 
+   # You should find files named profile.X.X.X in your directory after clearing
+   # the network.  Run /path/to/tau-2.14.2.1/ARCH/bin/paraprof (where ARCH is your 
+   # architecture, e.g. i386_linux) to view the profile data. 
+   # email amorris@cs.uoregon.edu or sameer@cs.uoregon.edu if you have questions
+*/
+
+
 
 #ifdef USE_TAU_PROFILING
 
