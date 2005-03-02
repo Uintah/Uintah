d8addc647c6e08de5e62e0bfa41300abe794279a
Bryan Jeffrey Worthen <worthen@sci.utah.edu>
2005-03-02 04:04:20 +0000

    New Scheduler3 components

---
+++ CCA/Components/Scheduler3/PatchBasedDataWarehouse3P.h
@@ -0,0 +1,11 @@
+
+#ifndef UINTAH_HOMEBREW_PatchBasedDataWarehouse3P_H
+#define UINTAH_HOMEBREW_PatchBasedDataWarehouse3P_H
+
+namespace Uintah {
+  class PatchBasedDataWarehouse3;
+  template<class T> class Handle;
+  typedef Handle<PatchBasedDataWarehouse3> PatchBasedDataWarehouse3P;
+}
+
+#endif
