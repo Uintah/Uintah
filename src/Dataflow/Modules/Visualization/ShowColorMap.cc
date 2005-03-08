be66743e5851e57903608747ce0e172903b536d4
James Bigler <bigler@sci.utah.edu>
2005-03-08 19:51:02 +0000

    If the colormap min and max are too close the colormap looks like a single color.  The code now checks for this and rescales a copy of the colormap to make it display properly.

--- src/Dataflow/Modules/Visualization/ShowColorMap.cc
+++ src/Dataflow/Modules/Visualization/ShowColorMap.cc
@@ -195,11 +195,28 @@ ShowColorMap::execute()
   }
 
   const Point  ref0(ref1 - out);
+  // Create a new colormap that we can send to ColorMapTex.  We need
+  // to do this, because if the colormap min/max are too close you get
+  // problems.  This is because the min and max are used to lookup
+  // into the texture as floats.  Precion problems occur when the min
+  // == max in floats, but not as doubles.
+  ColorMapHandle cmap_rescaled;
+  // Only rescale it when the min != max or min and max are too close.
+  float too_close = fabsf((float)(cmap->getMin()) - (float)(cmap->getMax()));
+  // Replace zero compared with too_close with an epsilon if desired.
+  if (too_close <= 0) {
+    // Make a copy of the colormap we can rescale
+    cmap_rescaled = cmap->clone();
+    cmap_rescaled->Scale(0, 1);
+  } else {
+    cmap_rescaled = cmap;
+  }
+    
   ColorMapTex *sq = scinew ColorMapTex(ref0,
 				       ref0 + along,
 				       ref0 + along + out,
 				       ref0 + out,
-				       cmap);
+				       cmap_rescaled);
   all->add( sq );
 
   double scale = gui_scale_.get();
