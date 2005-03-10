6c1990a1200a0daedd0b338d25a26e71fb45b2e6
Marty Cole <mjc@sci.utah.edu>
2005-03-10 15:12:58 +0000

    add Teem libs to the link

--- src/Packages/VS/Dataflow/Modules/Render/sub.mk
+++ src/Packages/VS/Dataflow/Modules/Render/sub.mk
@@ -25,7 +25,7 @@ PSELIBS := Core/Datatypes Dataflow/Network Dataflow/Ports \
         Core/Exceptions Core/Thread Core/GuiInterface \
         Core/Geom Core/Datatypes Core/Geometry  Core/GeomInterface \
         Core/TkExtensions
-LIBS := $(TK_LIBRARY) $(GL_LIBRARY) $(M_LIBRARY) 
+LIBS := $(TK_LIBRARY) $(GL_LIBRARY) $(M_LIBRARY) $(TEEM_LIBRARY)
 
 include $(SCIRUN_SCRIPTS)/smallso_epilogue.mk
 
