8d18c3eb875f4453bd61da49c34735da70568a31
Oren Livne <livne@sci.utah.edu>
2005-03-24 23:30:24 +0000

    Oren updates to the MATLAB ICE script: using correct mass instead of rho variable names in advectRho and advectQ; updated small-number treatment; firstorder/secondorder advection switch; separated plots and uintah results loading into separate scripts; added various debugging prinouts; nicified code. Compares vs. Uintah in second order mode, relative errors are about 1e-9.

--- src/Packages/Uintah/CCA/Components/ICE/Matlab_1dShockTube/outFluxVol.m
+++ src/Packages/Uintah/CCA/Components/ICE/Matlab_1dShockTube/outFluxVol.m
@@ -1,10 +1,12 @@
 function [ofs, rx] = outFluxVol(xvel_FC, delT, delX, nCells)
 %_____________________________________________________________
-%  Function:  outFluxVol -- computes the slab volume and the 
+%  Function:  outFluxVol -- computes the slab volume and the
 %  corresponding centroid (rx) on the outflux face
 %  This assumes that dy and dz = 1.0;
-
-fprintf('outFluxVol()\n');
+global P
+if (P.debugSteps)
+    fprintf('outFluxVol()\n');
+end
 ofs     = zeros(1,nCells);
 rx      = zeros(1,nCells);
 
@@ -14,12 +16,12 @@ for j = 1:nCells
         delX_slab = xvel_FC(j) * delT;
         rx(j) = delX/2.0 - delX_slab/2.0;
     end
-    
+
     if xvel_FC(j) < 0
         delX_slab = xvel_FC(j) * delT;
         rx(j) = delX_slab/2.0 - delX/2.0;
     end
-    
+
     % Outflux volumes
     delY_Z = 1.0;
     ofs(j) = delX_slab * delY_Z;
