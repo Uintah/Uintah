c0324e9b046587103d2a2ee850ebb6bf751fd680
Oren Livne <livne@sci.utah.edu>
2005-03-24 23:30:24 +0000

    Oren updates to the MATLAB ICE script: using correct mass instead of rho variable names in advectRho and advectQ; updated small-number treatment; firstorder/secondorder advection switch; separated plots and uintah results loading into separate scripts; added various debugging prinouts; nicified code. Compares vs. Uintah in second order mode, relative errors are about 1e-9.

--- Packages/Uintah/CCA/Components/ICE/Matlab_1dShockTube/advectQ.m
+++ Packages/Uintah/CCA/Components/ICE/Matlab_1dShockTube/advectQ.m
@@ -2,71 +2,123 @@
 % Function:  AdvectQ
 % Computes the advection of q using the compatible flux limiter.
 function [q_advected,gradLim,grad_x] = ...
-    advectQ(q,rho,rho_slab,rho_vrtx_1, rho_vrtx_2, ofs, rx, xvel_FC, delX, nCells)
+    advectQ(q,mass,mass_slab,mass_vrtx_1, mass_vrtx_2, ofs, rx, xvel_FC, delX, nCells)
 globalParams;
 
-% If compatible fluxes: q will be T=Energy/rho for this routine, as we want to limit
-% the gradient of T
-%qOverRho = q ./ (rho);  % oren:  this is only for compatible fluxes
+if (P.advectionOrder == 1)
+    qOverRho = q;
+else
+    % For compatible fluxes only:
+    % q is written as q=T/mass for this routine - we will
+    % limit the gradient of T, not q.
+    qOverRho = q ./ mass;
+end
 
-qOverRho = q;
-
-[gradLim, grad_x]           = gradientLimiter(qOverRho, rho, rho_vrtx_1, rho_vrtx_2, delX, nCells);
-[q_slab,gradLim, grad_x]    = qAverageFlux(qOverRho, rho, rho_slab, rx, grad_x, gradLim, delX, nCells);
+[gradLim, grad_x]           = gradientLimiter(qOverRho, mass, mass_vrtx_1, mass_vrtx_2, delX, nCells);
+[q_slab,gradLim, grad_x]    = qAverageFlux(qOverRho, mass, mass_slab, rx, grad_x, gradLim, delX, nCells);
 q_advected                  = advectSlabs(xvel_FC, q_slab, ofs, nCells, delX);
 
 %_____________________________________________________________
 % Function: gradientLimiter
 % Compute compatible flux Gradient limiter
-function [gradLim, grad_x] = gradientLimiter(q, rho, rho_vrtx_1, rho_vrtx_2, delX, nCells)
+function [gradLim, grad_x] = gradientLimiter(q, mass, mass_vrtx_1, mass_vrtx_2, delX, nCells)
 globalParams;
 
-fprintf('gradientLimiter\n');
+if (P.debugSteps)
+    fprintf('gradientLimiter()\n');
+end
 gradLim     = zeros(nCells,1);
 grad_x      = zeros(nCells,1);
 q_vrtx_1    = zeros(nCells+1,1);
 q_vrtx_2    = zeros(nCells+1,1);
 
-for j = 2:nCells-1    
+for j = 2:nCells-1
     %----------- Trial gradient, central differencing
     grad_x(j) = (q(j+1) - q(j-1))/(2.0*delX);
-    
+
     %----------- Compute q at vertices before limiting and min/max of these vertex values
-    %     % Compatible flux limiter - nonlinear temp=q/rho distribution
-    %     d1          = rho(j)/(rho_vrtx_1(j) + 1e-100);
-    %     d2          = rho(j)/(rho_vrtx_2(j) + 1e-100);
+    % Compatible flux limiter - nonlinear temp=q/mass distribution
+    d1          = mass(j)/(mass_vrtx_1(j) + d_SMALL_NUM);
+    d2          = mass(j)/(mass_vrtx_2(j) + d_SMALL_NUM);
     % van Leer limiter, linear q distribution
-    d1          = 1.0;
-    d2          = 1.0;
+    %     d1          = 1.0;
+    %     d2          = 1.0;
 
     q_vrtx_1    = q(j) + (grad_x(j) * delX/2.0) * d1;
     q_vrtx_2    = q(j) - (grad_x(j) * delX/2.0) * d2;
     q_vrtx_max  = max(q_vrtx_1, q_vrtx_2);
     q_vrtx_min  = min(q_vrtx_1, q_vrtx_2);
-    
+
     %---------- Compute q_CC min/max
     q_max       = max(q(j+1), q(j-1));
     q_min       = min(q(j+1), q(j-1));
-    
+
     %---------- Gradient limiter, pp.13-14
-    frac1       = (q_max - q(j))/(max( (q_vrtx_max - q(j)), 1e-100) );
+    frac1       = (q_max - q(j) + d_SMALL_NUM)/...
+        (q_vrtx_max - q(j) + d_SMALL_NUM);
     alphaMax    = max(0,frac1);                                 % Eq.(3.2.10c)
-    frac2       = (q(j) - q_min)/(max( (q(j) - q_vrtx_min), 1e-100) );
-    alphaMin    = max(0,frac2);                                 % Eq.(3.2.10d)    
+    frac2       = (q(j) - q_min + d_SMALL_NUM)/...
+        (q(j) - q_vrtx_min + d_SMALL_NUM);
+    alphaMin    = max(0,frac2);                                 % Eq.(3.2.10d)
     gradLim(j)  = min([1, alphaMax, alphaMin]);                 % Eq.(3.2.10b)
-end    
+
+    %gradLim(j) = 1.0;           % Testing - phi=1
+end
 
 
 %_____________________________________________________________
 % Function:  qAverageFlux
 % Computes the value of q at each slab volume.
 % Equation 3.3.3 or the reference
-function [q_slab, gradLim, grad_x] = qAverageFlux(q, rho, rho_slab, rx, grad_x, gradLim, delX, nCells)
+function [q_slab, gradLim, grad_x] = qAverageFlux(q, mass, mass_slab, rx, grad_x, gradLim, delX, nCells)
+globalParams;
 
-fprintf('Inside qAverageFlux\n');
+if (P.debugSteps)
+    fprintf('qAverageFlux()\n');
+end
 q_slab      = zeros(nCells,1);
-for j = 1:nCells                                                % For cells 1 and nCells, disregard contributions in this matlab code (note: grads=0 there)
-    q_slab(j) = q(j);                                           % Limiter=0, first order upwind scheme
-    %    q_slab(j) = q(j) * rho_slab(j) + rho(j) * grad_x(j) * gradLim(j) * rx(j);   % compatible flux fomulation; here q = energy (NOT temperature)
-%    q_slab(j) = q(j) + grad_x(j) * gradLim(j) * rx(j);                          % van Leer limiter
+if (P.advectionOrder == 1)
+    for j = 1:nCells                                                % For cells 1 and nCells, disregard contributions in this matlab code (note: grads=0 there)
+        q_slab(j) = q(j);                                           % Limiter=0, first order upwind scheme
+    end
+else
+    for j = 1:nCells                                                % For cells 1 and nCells, disregard contributions in this matlab code (note: grads=0 there)
+        q_slab(j) = q(j) * mass_slab(j) + mass(j) * grad_x(j) * gradLim(j) * rx(j);   % compatible flux fomulation; here q = energy (NOT temperature)
+        %    q_slab(j) = q(j) + grad_x(j) * gradLim(j) * rx(j);                          % van Leer limiter
+    end
+end
+
+%===================
+% PRINTOUTS
+if (P.debugAdvectQ)
+    fprintf('End of AdvectQ():\n');
+    range = P.printRange;   
+    
+    fprintf('rx = ');
+    fprintf('%.10f ' ,rx(range));
+    fprintf('\n');
+
+    fprintf('gradLim = ');
+    fprintf('%.10f ',gradLim(range));
+    fprintf('\n');
+
+    fprintf('grad_x =');
+    fprintf('%10f ',grad_x(range));
+    fprintf('\n');
+
+    fprintf('q = ');
+    fprintf('%.10f ',q(range));
+    fprintf('\n');
+
+    fprintf('mass_slab = ');
+    fprintf('%.10f ',mass_slab(range));
+    fprintf('\n');
+
+    fprintf('mass = ');
+    fprintf('%.10f ',mass(range));
+    fprintf('\n');
+
+    fprintf('q_slab = ');
+    fprintf('%.10f ',q_slab(range));
+    fprintf('\n');
 end
--- Packages/Uintah/CCA/Components/ICE/Matlab_1dShockTube/advectRho.m
+++ Packages/Uintah/CCA/Components/ICE/Matlab_1dShockTube/advectRho.m
@@ -1,21 +1,25 @@
 %_____________________________________________________________
 % Function: advectRho
-% computes the advection of rho using vanLeer Limiter
-function [q_advected,gradLim,grad_x, rho_slab, rho_vrtx_1, rho_vrtx_2] = ...
-    advectRho( rho,ofs, rx,xvel_FC, delX, nCells)
+% computes the advection of mass using vanLeer Limiter
+function [q_advected,gradLim,grad_x, mass_slab, mass_vrtx_1, mass_vrtx_2] = ...
+    advectRho( mass,ofs, rx,xvel_FC, delX, nCells)
+globalParams;
 
-[gradLim, grad_x, rho_vrtx_1, rho_vrtx_2]   = gradientLimiter_Rho(rho, delX, nCells);
-[rho_slab, gradLim, grad_x]                 = qAverageFluxRho(rho, rx, grad_x, gradLim, nCells);
-q_advected                                  = advectSlabs(xvel_FC, rho_slab, ofs, nCells,delX);
+[gradLim, grad_x, mass_vrtx_1, mass_vrtx_2]   = gradientLimiterRho(mass, delX, nCells);
+[mass_slab, gradLim, grad_x]                 = qAverageFluxRho(mass, rx, grad_x, gradLim, nCells);
+q_advected                                  = advectSlabs(xvel_FC, mass_slab, ofs, nCells,delX);
 
 %_____________________________________________________________
-% Function: gradientLimiter_Rho
-% Computes standard the gradient, the vanLeer limiter 
-% and vertex values of rho (q_vertex1, q_vrtx2).  The vertex values
+% Function: gradientLimiterRho
+% Computes standard the gradient, the vanLeer limiter
+% and vertex values of mass (q_vertex1, q_vrtx2).  The vertex values
 % are needed to compute the compatible flux limiter.
-function [gradLim, grad_x, q_vrtx_1, q_vrtx_2] = gradientLimiter_Rho(q,delX, nCells)
+function [gradLim, grad_x, q_vrtx_1, q_vrtx_2] = gradientLimiterRho(q,delX, nCells)
+globalParams;
 
-fprintf('gradientLimiter_Rho\n');
+if (P.debugSteps)
+    fprintf('gradientLimiterRho()\n');
+end
 gradLim     = zeros(1,nCells);
 grad_x      = zeros(1,nCells);
 q_vrtx_1    = zeros(1,nCells+1);
@@ -24,36 +28,71 @@ q_vrtx_2    = zeros(1,nCells+1);
 for j = 2:nCells-1
     %----------- Trial gradient, central differencing
     grad_x(j) = (q(j+1) - q(j-1))./(2.0*delX);
-    
+
     %----------- Compute q at vertices before limiting and min/max of these vertex values
     q_vrtx_1_tmp = q(j) + grad_x(j) * delX/2.0;                 % Eq.(3.2.8) for right vertex value
-    q_vrtx_2_tmp = q(j) - grad_x(j) * delX/2.0;                 % Eq.(3.2.8) for left vertex value    
+    q_vrtx_2_tmp = q(j) - grad_x(j) * delX/2.0;                 % Eq.(3.2.8) for left vertex value
     q_vrtx_max  = max(q_vrtx_1_tmp, q_vrtx_2_tmp);
     q_vrtx_min  = min(q_vrtx_1_tmp, q_vrtx_2_tmp);
-    
+
     %---------- Compute q_CC min/max
     q_max = max(q(j+1), q(j-1));
     q_min = min(q(j+1), q(j-1));
-    
+
     %---------- Gradient limiter, pp.13-14
-    frac1       = (q_max - q(j))/(max( (q_vrtx_max - q(j)), 1e-100) );
+    frac1       = (q_max - q(j) + d_SMALL_NUM)/...
+        (q_vrtx_max - q(j) + d_SMALL_NUM);
     alphaMax    = max(0,frac1);                                 % Eq.(3.2.10c)
-    frac2       = (q(j) - q_min)/(max( (q(j) - q_vrtx_min), 1e-100) );
-    alphaMin    = max(0,frac2);                                 % Eq.(3.2.10d)    
+    frac2       = (q(j) - q_min + d_SMALL_NUM)/...
+        (q(j) - q_vrtx_min + d_SMALL_NUM);
+    alphaMin    = max(0,frac2);                                 % Eq.(3.2.10d)
     gradLim(j)  = min([1, alphaMax, alphaMin]);                 % Eq.(3.2.10b)
 
     %---------- Save vertex values after gradient limiting, for advectQ
     q_vrtx_1(j) = q(j) + (grad_x(j) * gradLim(j) * delX/2.0);   % Eq.(3.2.8) for right vertex value
     q_vrtx_2(j) = q(j) - (grad_x(j) * gradLim(j) * delX/2.0);   % Eq.(3.2.8) for left vertex value
+
+    %gradLim(j) = 1.0;           % Testing - phi=1
 end
 
 %_____________________________________________________________
 % Function: qAverageFluxRho
-% computes Q in the slab according to the reference. 
+% computes Q in the slab according to the reference.
 function [q_slab, gradLim, grad_x] = qAverageFluxRho(q, rx, grad_x, gradLim, nCells)
-fprintf('Inside qAverageFluxRho\n');
+globalParams;
+if (P.debugSteps)
+    fprintf('qAverageFluxRho\n');
+end
 q_slab      = zeros(1,nCells);
-for j = 1:nCells                                                % For cells 1 and nCells, disregard contributions in this matlab code (note: grads=0 there)
-    q_slab(j) = q(j);                                           % Limiter=0, first order upwind scheme
-%    q_slab(j) = q(j) + grad_x(j) * gradLim(j) * rx(j);          % Eq.(3.2.9)
+if (P.advectionOrder == 1)
+    for j = 1:nCells                                                % For cells 1 and nCells, disregard contributions in this matlab code (note: grads=0 there)
+        q_slab(j) = q(j);                                           % Limiter=0, first order upwind scheme
+    end
+else
+    for j = 1:nCells                                                % For cells 1 and nCells, disregard contributions in this matlab code (note: grads=0 there)
+        q_slab(j) = q(j) + grad_x(j) * gradLim(j) * rx(j);          % Eq.(3.2.9)
+    end
+end
+
+%===================
+% PRINTOUTS
+if (P.debugAdvectRho)
+    fprintf('End of advectRho():\n');
+    range = P.printRange;
+    
+    fprintf('rx = ');
+    fprintf('%.10f ' ,rx(range));
+    fprintf('\n');
+
+    fprintf('gradLim = ');
+    fprintf('%.10f ',gradLim(range));
+    fprintf('\n');
+
+    fprintf('grad_x =');
+    fprintf('%10f ',grad_x(range));
+    fprintf('\n');
+
+    fprintf('q = ');
+    fprintf('%.10f ',q(range));
+    fprintf('\n');
 end
