2b9461ecfd05de1da93326bf219c048529f2bbd2
Todd Harman <harman@sci.utah.edu>
2005-03-31 00:45:08 +0000

    updated funcion call to scalarDiffusion() operator

--- CCA/Components/Models/test/SimpleRxn.cc
+++ CCA/Components/Models/test/SimpleRxn.cc
@@ -277,9 +277,11 @@ void SimpleRxn::initialize(const ProcessorGroup*,
     
     for( int i =1 ; i < d_scalar->initialize_diffusion_knob; i++ ){
       bool use_vol_frac = false; // don't include vol_frac in diffusion calc.
-      constCCVariable<double> placeHolder;
-      scalarDiffusionOperator(new_dw, patch, use_vol_frac,
-                              placeHolder, placeHolder,  f,
+      SFCXVariable<double> placeHolderX;
+      SFCYVariable<double> placeHolderY;
+      SFCZVariable<double> placeHolderZ;
+      scalarDiffusionOperator(new_dw, patch, use_vol_frac, f,
+                              placeHolderX, placeHolderY, placeHolderZ,
                               f, FakeDiffusivity, fakedelT);
     }
         
@@ -528,10 +530,12 @@ void SimpleRxn::computeModelSources(const ProcessorGroup*,
     if(diff_coeff_test != 0.0){ 
       
       bool use_vol_frac = false; // don't include vol_frac in diffusion calc.
-      constCCVariable<double> placeHolder;
+      SFCXVariable<double> placeHolderX;
+      SFCYVariable<double> placeHolderY;
+      SFCZVariable<double> placeHolderZ;
 
-      scalarDiffusionOperator(new_dw, patch, use_vol_frac,
-                              placeHolder, placeHolder,  f_old,
+      scalarDiffusionOperator(new_dw, patch, use_vol_frac, f_old,
+                              placeHolderX, placeHolderY, placeHolderZ,
                               f_src, diff_coeff, delT);
     }
 
