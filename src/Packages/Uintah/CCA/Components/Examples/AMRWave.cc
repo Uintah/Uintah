532982ccc45be39dd480d2c251410c5222c113e7
Bryan Jeffrey Worthen <worthen@sci.utah.edu>
2005-04-02 01:07:52 +0000

    Full AMR Wave implementation complete with interpolation.  It currently finishes running on 2 levels, and soon I will make sure the numbers are correct and get it to complete on 3+ levels

--- src/Packages/Uintah/CCA/Components/Examples/AMRWave.cc
+++ src/Packages/Uintah/CCA/Components/Examples/AMRWave.cc
@@ -122,10 +122,7 @@ void AMRWave::errorEstimate(const ProcessorGroup*,
         IntVector high = patch->getHighIndex();
 
         // Compute curl
-        double curlPhi = sumdx2 * phi[c]
-          + (phi[c+IntVector(1,0,0)] + phi[c-IntVector(1,0,0)]) * inv_dx2.x()
-          + (phi[c+IntVector(0,1,0)] + phi[c-IntVector(0,1,0)]) * inv_dx2.y()
-          + (phi[c+IntVector(0,0,1)] + phi[c-IntVector(0,0,1)]) * inv_dx2.z();
+        double curlPhi = curl(phi, c, sumdx2, inv_dx2);
 
         if(curlPhi > thresh){
           numFlag++;
@@ -139,6 +136,62 @@ void AMRWave::errorEstimate(const ProcessorGroup*,
   }
 }
 
+void AMRWave::refineCell(CCVariable<double>& finevar, constCCVariable<double>& coarsevar, IntVector fineIndex,
+                    const Level* fineLevel, const Level* coarseLevel)
+{
+  Point cell_pos = fineLevel->getCellPosition(fineIndex);
+  double tmp=0;
+  double total_weight = 0;
+
+  // starting coarse point - will interp 8 cells starting here.  On the +x, +y, +z faces, subtract 1
+  IntVector coarseStart(fineLevel->mapCellToCoarser(fineIndex));
+  IntVector coarseHigh(coarsevar.getHighIndex());
+  if (coarseStart.x() == coarseHigh.x()-1)
+    coarseStart[0]--;
+  if (coarseStart.y() == coarseHigh.y()-1)
+    coarseStart[1]--;
+  if (coarseStart.z() == coarseHigh.z()-1)
+    coarseStart[2]--;
+  
+  // find eight cells, add up values multiplied by the weight of their distance
+  for(CellIterator inside(IntVector(0,0,0), IntVector(2,2,2)); !inside.done(); inside++){
+    IntVector coarse_idx = coarseStart+*inside;
+    Point coarse_pos = coarseLevel->getCellPosition(coarse_idx);
+    double distance = (coarse_pos - cell_pos).length();
+    
+    tmp +=coarsevar[coarse_idx]*distance;
+    total_weight += distance;
+  }
+
+  // average value
+  finevar[fineIndex]=tmp/total_weight;
+
+}
+
+void AMRWave::coarsenCell(CCVariable<double>& coarsevar, constCCVariable<double>& finevar, IntVector coarseIndex,
+                    const Level* fineLevel, const Level* coarseLevel)
+{
+  Point cell_pos = coarseLevel->getCellPosition(coarseIndex);
+  double tmp=0; 
+  double total_weight = 0;
+
+  // starting coarse point - will interp 8 cells starting here.  On the +x, +y, +z faces, subtract 1
+  IntVector fineStart(coarseLevel->mapCellToFiner(coarseIndex));
+  
+  // find eight cells, add up values multiplied by the weight of their distance
+  for(CellIterator inside(IntVector(0,0,0), IntVector(2,2,2)); !inside.done(); inside++){
+    IntVector fine_idx = fineStart+*inside;
+    Point fine_pos = fineLevel->getCellPosition(fine_idx);
+    double distance = (fine_pos - cell_pos).length();
+    
+    tmp +=finevar[fine_idx]*distance;
+    total_weight += distance;
+  }
+
+  // average value
+  coarsevar[coarseIndex]=tmp/total_weight;
+
+}
 
 void AMRWave::refine(const ProcessorGroup*,
                      const PatchSubset* patches,
@@ -150,56 +203,44 @@ void AMRWave::refine(const ProcessorGroup*,
 
   for (int p = 0; p < patches->size(); p++) {  
     const Patch* finePatch = patches->get(p);
-    cerr << "  Refine for patch " << finePatch->getID() << endl;
-
-    // Find the overlapping regions...
-    Level::selectType coarsePatches;
-    finePatch->getCoarseLevelPatches(coarsePatches);
 
     for(int m = 0;m<matls->size();m++){
       int matl = matls->get(m);
 
       CCVariable<double> pi;
       CCVariable<double> phi;
+      constCCVariable<double> coarse_pi;
+      constCCVariable<double> coarse_phi;
       
       new_dw->allocateAndPut(phi, phi_label, matl, finePatch);
       new_dw->allocateAndPut(pi, pi_label, matl, finePatch);
       
-      for ( int i = 0; i < coarsePatches.size(); i++ ) {
-        const Patch* coarsePatch = coarsePatches[i];
-        constCCVariable<double> coarse_pi;
-        constCCVariable<double> coarse_phi;
-        new_dw->get(coarse_pi, pi_label, matl, coarsePatch, Ghost::AroundCells, 1);
-        new_dw->get(coarse_phi, phi_label, matl, coarsePatch, Ghost::AroundCells, 1);
+      IntVector coarsePhiLow  = fineLevel->mapCellToCoarser(phi.getLowIndex());
+      IntVector coarsePhiHigh = fineLevel->mapCellToCoarser(phi.getHighIndex()+fineLevel->getRefinementRatio() - IntVector(1,1,1));
+      IntVector coarsePiLow  = fineLevel->mapCellToCoarser(pi.getLowIndex());
+      IntVector coarsePiHigh = fineLevel->mapCellToCoarser(pi.getHighIndex()+fineLevel->getRefinementRatio() - IntVector(1,1,1));
+
+      cout << "  Doing refine on patch " << finePatch->getID() << " matl " << matl << endl;
+      cout << "  Getting phi from " << coarsePhiLow << " " << coarsePhiHigh << " and pi from " << coarsePiLow << " " << coarsePiHigh << endl;
+
+      new_dw->getRegion(coarse_phi, phi_label, matl, coarseLevel, coarsePhiLow, coarsePhiHigh);
+      new_dw->getRegion(coarse_pi, pi_label, matl, coarseLevel, coarsePiLow, coarsePiHigh);
+
+      // simple linear interpolation (maybe)
+      for(CellIterator iter(phi.getLowIndex(), phi.getHighIndex()); !iter.done(); iter++){
+        // refine phi
+        refineCell(phi, coarse_phi, *iter, fineLevel, coarseLevel);
+
+        // phi extends (potentially) one more cell than pi.
+        IntVector pilow, pihigh;
+        pilow = pi.getLow();
+        pihigh = pi.getHigh();
         
-        IntVector l = Max(coarseLevel->mapCellToFiner(coarsePatch->getCellLowIndex()), 
-                          finePatch->getCellLowIndex());
-        IntVector h = Min(coarseLevel->mapCellToFiner(coarsePatch->getCellHighIndex()),
-                          finePatch->getCellHighIndex());
-        
-        // simple linear interpolation (maybe)
-        for(CellIterator iter(l, h); !iter.done(); iter++){
-          Point cell_pos = fineLevel->getCellPosition(*iter);
-          double tmp_phi=0;
-          double tmp_pi=0;
-          double total_weight = 0;
-          IntVector coarseStart(fineLevel->mapCellToCoarser(*iter));
-          
-          for(CellIterator inside(IntVector(-1,-1,-1), coarseLevel->getRefinementRatio() - IntVector(1,1,1));
-              !inside.done(); inside++){
-            IntVector coarse_idx = coarseStart+*inside;
-            Point coarse_pos = coarseLevel->getCellPosition(coarse_idx);
-            double distance = (coarse_pos - cell_pos).length();
-            
-            tmp_phi +=coarse_phi[coarse_idx]*distance;
-            tmp_pi  +=coarse_pi[coarse_idx]*distance;
-            total_weight += distance;
-          }
-          phi[*iter]=tmp_phi/total_weight;
-          pi[*iter]=tmp_pi/total_weight;
+        if ((*iter).x() < pilow.x() || (*iter).y() < pilow.y() || (*iter).z() < pilow.z() || 
+          (*iter).x() > pihigh.x() || (*iter).y() < pihigh.y() || (*iter).z() < pihigh.z()) {
+          refineCell(pi, coarse_pi, *iter, fineLevel, coarseLevel);
         }
-      } // for(int i=0;i<coarsePatches.size();i++)
-        
+      }
     } // matls
   } // finePatches
 }
@@ -209,5 +250,99 @@ void AMRWave::coarsen(const ProcessorGroup*,
                       const MaterialSubset* matls,
                       DataWarehouse*, DataWarehouse* new_dw)
 {
-  //cerr << "AMRWave::coarsen not finished\n";
+  const Level* coarseLevel = getLevel(patches);
+  const Level* fineLevel = coarseLevel->getFinerLevel().get_rep();
+
+  for (int p = 0; p < patches->size(); p++) {  
+    const Patch* coarsePatch = patches->get(p);
+
+    for(int m = 0;m<matls->size();m++){
+      int matl = matls->get(m);
+
+      CCVariable<double> pi;
+      CCVariable<double> phi;
+      constCCVariable<double> fine_pi;
+      constCCVariable<double> fine_phi;
+      
+      new_dw->getModifiable(phi, phi_label, matl, coarsePatch);
+      new_dw->getModifiable(pi, pi_label, matl, coarsePatch);
+      
+      IntVector fineLow  = coarseLevel->mapCellToFiner(phi.getLowIndex());
+      IntVector fineHigh = coarseLevel->mapCellToFiner(phi.getHighIndex());
+
+      cout << "  Doing coarsen on patch " << coarsePatch->getID() << " matl " << matl << endl;
+      cout << "  Getting phi from " << fineLow << " " << fineHigh << endl;
+
+      new_dw->getRegion(fine_phi, phi_label, matl, fineLevel, fineLow, fineHigh);
+      new_dw->getRegion(fine_pi, pi_label, matl, fineLevel, fineLow, fineHigh);
+
+      // simple linear interpolation (maybe)
+      for(CellIterator iter(phi.getLowIndex(), phi.getHighIndex()); !iter.done(); iter++){
+        // refine phi
+        coarsenCell(phi, fine_phi, *iter, fineLevel, coarseLevel);
+        coarsenCell(pi, fine_phi, *iter, fineLevel, coarseLevel);
+      }
+    } // matls
+  } // finePatches
+}
+
+void AMRWave::addRefineDependencies(Task* task, const VarLabel* var,
+                                    int step, int nsteps)
+{
+  ASSERTRANGE(step, 0, nsteps+1);
+  Ghost::GhostType gc = Ghost::AroundCells;
+
+  if(step != nsteps)
+    task->requires(Task::CoarseOldDW, var,
+		   0, Task::CoarseLevel, 0, Task::NormalDomain, gc, 1);
+  if(step != 0)
+    task->requires(Task::CoarseNewDW, var,
+		   0, Task::CoarseLevel, 0, Task::NormalDomain, gc, 1);
+}
+
+void AMRWave::refineFaces(const Patch* finePatch, 
+                 const Level* fineLevel,
+		 const Level* coarseLevel, 
+		 CCVariable<double>& finevar, 
+                 const VarLabel* label,
+		 int step, int nsteps, int matl, 
+                 DataWarehouse* coarse_old_dw,
+		 DataWarehouse* coarse_new_dw)
+{
+  for (int f = 0; f < 6; f++) {
+    Patch::FaceType face = (Patch::FaceType) f;
+    if (finePatch->getBCType(face) == Patch::Coarse) {
+      IntVector low;
+      IntVector high;
+      finePatch->getFace(face, IntVector(0,0,0), IntVector(1,1,1), low, high);
+
+      constCCVariable<double> coarse_old_var;
+      constCCVariable<double> coarse_new_var;
+
+      if (step != nsteps)
+        coarse_old_dw->getRegion(coarse_old_var, label, matl, coarseLevel, low, high);
+
+      if (step != 0)
+        coarse_new_dw->getRegion(coarse_new_var, label, matl, coarseLevel, low, high);
+
+      // loop across the face, and depending on where we are in time, use the values 
+      // from the old dw, new dw, or both (based on a weight)
+      for (CellIterator iter(low, high); !iter.done(); iter++) {
+        double x0 = 0, x1 = 0;
+        double w1 = ((double) step) / (double) nsteps;
+        double w0 = 1-w1;
+
+        if (step != nsteps) {
+          refineCell(finevar, coarse_old_var, *iter, fineLevel, coarseLevel);
+          x0 = finevar[*iter];
+        }
+        if (step != 0) {
+          refineCell(finevar, coarse_new_var, *iter, fineLevel, coarseLevel);
+          x1 = finevar[*iter];
+        }
+        finevar[*iter] = x0*w0 + x1*w1;
+      }
+
+    } // if (finePatch->getBCType(face) == Patch::Coarse) {
+  } // for (Patch::FaceType face = 0; face < Patch::numFaces; face++) {
 }
