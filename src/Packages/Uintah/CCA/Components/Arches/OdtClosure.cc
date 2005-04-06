9602763340530188e74ee04d51e2c2d07053fda0
John Schmidt <jas@sci.utah.edu>
2005-04-06 03:15:18 +0000

    Move OdtData to its own file so it will compile with gcc-3.4.

--- src/Packages/Uintah/CCA/Components/Arches/OdtClosure.cc
+++ src/Packages/Uintah/CCA/Components/Arches/OdtClosure.cc
@@ -1,7 +1,7 @@
 //----- OdtClosure.cc --------------------------------------------------
-
-#include <Packages/Uintah/CCA/Components/Arches/debug.h>
+#include <Packages/Uintah/CCA/Components/Arches/OdtData.h>
 #include <Packages/Uintah/CCA/Components/Arches/OdtClosure.h>
+#include <Packages/Uintah/CCA/Components/Arches/debug.h>
 #include <Packages/Uintah/CCA/Components/Arches/PhysicalConstants.h>
 #include <Packages/Uintah/CCA/Components/Arches/BoundaryCondition.h>
 #include <Packages/Uintah/CCA/Components/Arches/CellInformation.h>
@@ -57,141 +57,7 @@ OdtClosure::~OdtClosure()
 {
 }
 
-//__________________________________
-  static MPI_Datatype makeMPI_odtData()
-  {
-//    ASSERTEQ(sizeof(odtData), sizeof(double)*10);
-//    MPI_Datatype mpitype;
-//    MPI_Type_vector(1, 10, 10, MPI_DOUBLE, &mpitype);
-//    MPI_Type_commit(&mpitype);
-//    return mpitype;
-      MPI_Datatype odt_type;
-      MPI_Type_contiguous(210, MPI_DOUBLE, &odt_type);
-      MPI_Type_commit(&odt_type);
-      return odt_type;
-  }
 
-  const Uintah::TypeDescription* Uintah::fun_getTypeDescription(Uintah::odtData*)
-  {
-    static Uintah::TypeDescription* td = 0;
-    if(!td){
-      td = scinew Uintah::TypeDescription(Uintah::TypeDescription::Other,
-					  "odtData", true, 
-					  &makeMPI_odtData);
-    }
-    return td;
-  }
-
-namespace SCIRun {
-
-  void swapbytes( Uintah::odtData& d) {
-    double *p = d.x_x;
-    for (int i = 0; i < 10; i++) {
-      SWAP_8(*p); 
-      p++;
-    }
-    p = d.y_y;
-    for (int i = 0; i < 10; i++) {
-      SWAP_8(*p); 
-      p++;
-    }
-    p = d.z_z;
-    for (int i = 0; i < 10; i++) {
-      SWAP_8(*p); 
-      p++;
-    }
-    p = d.x_u;
-    for (int i = 0; i < 10; i++) {
-      SWAP_8(*p); 
-      p++;
-    }
-    p = d.x_v;
-    for (int i = 0; i < 10; i++) {
-      SWAP_8(*p); 
-      p++;
-    }
-    p = d.x_w;
-    for (int i = 0; i < 10; i++) {
-      SWAP_8(*p); 
-      p++;
-    }
-    p = d.x_rho;
-    for (int i = 0; i < 10; i++) {
-      SWAP_8(*p); 
-      p++;
-    }
-    p = d.x_T;
-    for (int i = 0; i < 10; i++) {
-      SWAP_8(*p); 
-      p++;
-    }
-    p = d.x_Phi;
-    for (int i = 0; i < 10; i++) {
-      SWAP_8(*p); 
-      p++;
-    }
-    p = d.y_u;
-    for (int i = 0; i < 10; i++) {
-      SWAP_8(*p); 
-      p++;
-    }
-    p = d.y_v;
-    for (int i = 0; i < 10; i++) {
-      SWAP_8(*p); 
-      p++;
-    }
-    p = d.y_w;
-    for (int i = 0; i < 10; i++) {
-      SWAP_8(*p); 
-      p++;
-    }
-    p = d.y_rho;
-    for (int i = 0; i < 10; i++) {
-      SWAP_8(*p); 
-      p++;
-    }
-    p = d.y_T;
-    for (int i = 0; i < 10; i++) {
-      SWAP_8(*p); 
-      p++;
-    }
-    p = d.y_Phi;
-    for (int i = 0; i < 10; i++) {
-      SWAP_8(*p); 
-      p++;
-    }
-    p = d.z_u;
-    for (int i = 0; i < 10; i++) {
-      SWAP_8(*p); 
-      p++;
-    }
-    p = d.z_v;
-    for (int i = 0; i < 10; i++) {
-      SWAP_8(*p); 
-      p++;
-    }
-    p = d.z_w;
-    for (int i = 0; i < 10; i++) {
-      SWAP_8(*p); 
-      p++;
-    }
-    p = d.z_rho;
-    for (int i = 0; i < 10; i++) {
-      SWAP_8(*p); 
-      p++;
-    }
-    p = d.z_T;
-    for (int i = 0; i < 10; i++) {
-      SWAP_8(*p); 
-      p++;
-    }
-    p = d.z_Phi;
-    for (int i = 0; i < 10; i++) {
-      SWAP_8(*p); 
-      p++;
-    }
-  }
-}
 
 //****************************************************************************
 // Problem Setup 
@@ -1212,7 +1078,7 @@ OdtClosure::reComputeTurbSubmodel(const ProcessorGroup* pc,
 		  odt_variableNew[currCell].x_x[iOdt]=location.x()+(iOdt-d_odtPoints/2+0.5)*
 			  sewcur/d_odtPoints;
 	     if (colZ==(indexHigh.z()+1)/2) {
-		if (abs(colX*cellinfo->sew[colX]-3.14)<2.0*cellinfo->sew[colX]) {
+		if (fabs(colX*cellinfo->sew[colX]-3.14)<2.0*cellinfo->sew[colX]) {
 		   cout << "in Odt " << colX <<  "  " << colY << "  " << colZ << " " 
 			<< location.x() << " " << uVel[currCell] << "\n ";
 		   for (iOdt=0; iOdt<d_odtPoints; iOdt++)
