8b2845fddce5282e027f1ab98a93e64519bfd0bf
Anup Bhawalkar <anup@sci.utah.edu>
2005-04-12 21:30:35 +0000

    Added ZerilliArmstrong plasticity model.

--- src/Packages/Uintah/CCA/Components/MPM/ConstitutiveModel/PlasticityModels/PlasticityModelFactory.cc
+++ src/Packages/Uintah/CCA/Components/MPM/ConstitutiveModel/PlasticityModels/PlasticityModelFactory.cc
@@ -1,6 +1,7 @@
-#include "PlasticityModelFactory.h"
+#include "PlasticityModelFactory.h"                                             
 #include "IsoHardeningPlastic.h"
 #include "JohnsonCookPlastic.h"
+#include "ZerilliArmstrongPlastic.h"
 #include "MTSPlastic.h"
 #include "SCGPlastic.h"
 #include "PTWPlastic.h"
@@ -26,11 +27,12 @@ PlasticityModel* PlasticityModelFactory::create(ProblemSpecP& ps)
    string mat_type;
    if(!child->getAttribute("type", mat_type))
       throw ProblemSetupException("No type for plasticity_model");
-   
    if (mat_type == "isotropic_hardening")
       return(scinew IsoHardeningPlastic(child));
    else if (mat_type == "johnson_cook")
       return(scinew JohnsonCookPlastic(child));
+   else if (mat_type == "zerilli_armstrong")
+      return(scinew ZerilliArmstrongPlastic(child));
    else if (mat_type == "mts_model")
       return(scinew MTSPlastic(child));
    else if (mat_type == "steinberg_cochran_guinan")
@@ -45,19 +47,28 @@ PlasticityModel*
 PlasticityModelFactory::createCopy(const PlasticityModel* pm)
 {
    if (dynamic_cast<const IsoHardeningPlastic*>(pm))
-      return(scinew IsoHardeningPlastic(dynamic_cast<const IsoHardeningPlastic*>(pm)));
+      return(scinew IsoHardeningPlastic(dynamic_cast<const 
+					IsoHardeningPlastic*>(pm)));
 
    else if (dynamic_cast<const JohnsonCookPlastic*>(pm))
-      return(scinew JohnsonCookPlastic(dynamic_cast<const JohnsonCookPlastic*>(pm)));
+      return(scinew JohnsonCookPlastic(dynamic_cast<const 
+				       JohnsonCookPlastic*>(pm)));
+
+   else if (dynamic_cast<const ZerilliArmstrongPlastic*>(pm))
+      return(scinew ZerilliArmstrongPlastic(dynamic_cast<const 
+					    ZerilliArmstrongPlastic*>(pm)));
 
    else if (dynamic_cast<const MTSPlastic*>(pm))
       return(scinew MTSPlastic(dynamic_cast<const MTSPlastic*>(pm)));
-
+   
    else if (dynamic_cast<const SCGPlastic*>(pm))
       return(scinew SCGPlastic(dynamic_cast<const SCGPlastic*>(pm)));
+
    else if (dynamic_cast<const PTWPlastic*>(pm))
       return(scinew PTWPlastic(dynamic_cast<const PTWPlastic*>(pm)));
-
+   
    else 
-      throw ProblemSetupException("Cannot create copy of unknown plasticity model");
+      throw ProblemSetupException("Cannot create copy of unknown plasticity 
+                                   model");
 }
+
