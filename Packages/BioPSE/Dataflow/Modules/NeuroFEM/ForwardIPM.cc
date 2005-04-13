ce06add2304239211c2cb8dc1486294d6103f01e
Michael Callahan <callahan@sci.utah.edu>
2005-04-13 21:51:47 +0000

    Various fixes, include simple par file

--- Packages/BioPSE/Dataflow/Modules/NeuroFEM/ForwardIPM.cc
+++ Packages/BioPSE/Dataflow/Modules/NeuroFEM/ForwardIPM.cc
@@ -403,7 +403,151 @@ ForwardIPM::write_par_file(string filename)
     return false;
   }
 
-  fprintf(f, "Random parameters\n");
+  fprintf(f, "#Parameter file: FEM for source simulation\n");
+  fprintf(f, "\n");
+  fprintf(f, "[ReferenceData]\n");
+  fprintf(f, "#Number of channels\n");
+  fprintf(f, "numchan= 71\n");
+  fprintf(f, "numsamples=  1\n");
+  fprintf(f, "startsample= 0\n");
+  fprintf(f, "stopsample=  0\n");
+  fprintf(f, "\n");
+  fprintf(f, "[RegularizationTikhonov]\n");
+  fprintf(f, "# estimated signal to noise ratio\n");
+  fprintf(f, "estimatedSNR= 10000.0\n");
+  fprintf(f, "\n");
+  fprintf(f, "[STR]\n");
+  fprintf(f, "# spatial regularization parameter\n");
+  fprintf(f, "lambda= 0.1\n");
+  fprintf(f, "# temporal regularization parameter\n");
+  fprintf(f, "mue= 0.1\n");
+  fprintf(f, "# degree of temporal smoothness\n");
+  fprintf(f, "degree= 2\n");
+  fprintf(f, "# fast modus 1 leads to approximated solution, much faster.\n");
+  fprintf(f, "fast= 1\n");
+  fprintf(f, "\n");
+  fprintf(f, "[NeuroFEMSimulator]\n");
+  fprintf(f, "# GENERAL PARAMETERS\n");
+  fprintf(f, "# residuum of the forward solution \n");
+  fprintf(f, "toleranceforwardsolution= .10000E-08\n");
+  fprintf(f, "# degree of Gaussian integration (2 is recommended) \n");
+  fprintf(f, "degreeofintegration= 2\n");
+  fprintf(f, "# different analytical solutions for the EEG problem \n");
+  fprintf(f, "analyticalsolution= 1\n");
+  fprintf(f, "\n");
+  fprintf(f, "# should METIS repartition for parallel run?\n");
+  fprintf(f, "metisrepartitioning= 1\n");
+  fprintf(f, "\n");
+  fprintf(f, "# SOLVER (1:Jakobi-CG, 2:IC(0)-CG, 3:AMG-CG, 4:PILUTS(ILDLT)-CG) \n");
+  fprintf(f, "solvermethod= 3\n");
+  fprintf(f, "# use or use not lead field basis approach\n");
+  fprintf(f, "associativity= 1\n");
+  fprintf(f, "# NeuroFEM Solver\n");
+  fprintf(f, "# parameter file for Pebbles solver \n");
+  fprintf(f, "pebbles= pebbles.inp\n");
+  fprintf(f, "# ONLY for MultiRHS-Pebbles: number of right-hand-sides which are solved simultaneously\n");
+  fprintf(f, "pebblesnumrhs= 1\n");
+  fprintf(f, "\n");
+  fprintf(f, "# SOURCE SIMULATION\n");
+  fprintf(f, "# threshold (percentage of the greatest dipole strength) of all dipoles to appear in the result files \n");
+  fprintf(f, "dipolethreshold= .10000E+01\n");
+  fprintf(f, "# blurring single loads to adjacent nodes by means of the Gaussian (normal) distribution \n");
+  fprintf(f, "sourcesimulationepsilondirac= .10000E-08\n");
+  fprintf(f, "\n");
+  fprintf(f, "# DIPOLE MODELING\n");
+  fprintf(f, "# dipole modeling; weighting of the source distribution with the power of the declared value \n");
+  fprintf(f, "dipolemodelingsmoothness= 2\n");
+  fprintf(f, "# power of the dipole moments to be considered \n");
+  fprintf(f, "dipolemodelingorder= 2\n");
+  fprintf(f, "# necessary internal scaling factor; should be larger than twice the element edge length \n");
+  fprintf(f, "dipolemodelingscale= 20.000\n");
+  fprintf(f, "# Lagrangian multiplier for the (inverse) dipole modeling \n");
+  fprintf(f, "dipolemodelinglambda= .10000E-05\n");
+  fprintf(f, "# source-sink separation of the analytical dipole model \n");
+  fprintf(f, "dipolemodelingdistance= 1.0000\n");
+  fprintf(f, "# use rango dipole model \n");
+  fprintf(f, "dipolemodelingrango= 0\n");
+  fprintf(f, "\n");
+  fprintf(f, "# Monopole/Dipole\n");
+  fprintf(f, "# calculation of the potential distribution for spherical, homogeneous  structures by means of a analytical description \n");
+  fprintf(f, "analyticaldipole= 0\n");
+  fprintf(f, "# forward solution computation with monopoles or dipoles\n");
+  fprintf(f, "dipolesources= 1\n");
+  fprintf(f, "# spread a nodal load to adjacent nodes\n");
+  fprintf(f, "sourcesimulation= 1\n");
+  fprintf(f, "#to compare analytical and numerical solutions, an integral average value of the potential is subtracted\n");
+  fprintf(f, "#from the analytical results because they are not related to a mean potential (in contrast to the numerical solution)\n");
+  fprintf(f, "averagecorrection= 0\n");
+  fprintf(f, "\n");
+  fprintf(f, "[NeuroFEMGridGenerator] \n");
+  fprintf(f, "# Number of materials \n");
+  fprintf(f, "nummaterials= 7\n");
+  fprintf(f, "# Conductivities of fem head model\n");
+  fprintf(f, "\n");
+  fprintf(f, "#define EXTRA           3       // tissue codes (see D1.2b)\n");
+  fprintf(f, "#define SKULL           1\n");
+  fprintf(f, "#define CSF             8\n");
+  fprintf(f, "#define GREY_MATTER     7\n");
+  fprintf(f, "#define WHITE_MATTER    6\n");
+  fprintf(f, "\n");
+  fprintf(f, "#skin_cond       0.33;\n");
+  fprintf(f, "#skull_cond      0.0042;\n");
+  fprintf(f, "#csf_cond        1.79;\n");
+  fprintf(f, "#grey_cond       0.337;\n");
+  fprintf(f, "#white_cond_iso  0.14;\n");
+  fprintf(f, "\n");
+  fprintf(f, "# The first value corresponds to the electrode which will be added to the mesh \n");
+  fprintf(f, "conductivities= \n");
+  fprintf(f, "1.0 0.33 0.0042 0.33 0.33 0.33 0.033\n");
+  fprintf(f, "# Labels in the head model corresponding to the different tissues\n");
+  fprintf(f, "# The first value corresponds to the electrode which will be added to the mesh \n");
+  fprintf(f, "labels=\n");
+  fprintf(f, "1000 3 1 8 7 6 9\n");
+  fprintf(f, "\n");
+  fprintf(f, "# Tissue labels in the head model for which the tensor valued coductivity \n");
+  fprintf(f, "# should be used if available\n");
+  fprintf(f, "tensorlabels=\n");
+  fprintf(f, "#0 0 1 0 0 1 0\n");
+  fprintf(f, "0 0 1 0 0 1 0\n");
+  fprintf(f, "\n");
+  fprintf(f, "# -1- for sorted conductivities\n");
+  fprintf(f, "sortedconductivities= 0\n");
+  fprintf(f, "\n");
+  fprintf(f, "[InitialGuess]\n");
+  fprintf(f, "numinitialguesspos= 1\n");
+  fprintf(f, "posx=\n");
+  fprintf(f, "#0.044175999 \n");
+  fprintf(f, "0.097\n");
+  fprintf(f, "posy=\n");
+  fprintf(f, "#0.077307999\n");
+  fprintf(f, "0.154\n");
+  fprintf(f, "posz=\n");
+  fprintf(f, "#0.135539993\n");
+  fprintf(f, "0.128\n");
+  fprintf(f, "numinitialguessdir= 1\n");
+  fprintf(f, "dirx=\n");
+  fprintf(f, "#0.0\n");
+  fprintf(f, "1.0  \n");
+  fprintf(f, "diry=\n");
+  fprintf(f, "#1.0\n");
+  fprintf(f, "0.0\n");
+  fprintf(f, "dirz=\n");
+  fprintf(f, "#1.0\n");
+  fprintf(f, "0.0 \n");
+  fprintf(f, "\n");
+  fprintf(f, "[FileFormats]   \n");
+  fprintf(f, "#ReferenceData file: 1= Vista, 2= ASA, 3= ASCII\n");
+  fprintf(f, "ReferenceDataIn= 2\n");
+  fprintf(f, "#LeadfieldMatrix input file: 1= Vista, 2= ASA \n");
+  fprintf(f, "LeadfieldIn= 1\n");
+  fprintf(f, "#SensorConfiguration file: 1= Vista, 2= ASA \n");
+  fprintf(f, "SensorconfigurationIn= 2 \n");
+  fprintf(f, "#Source Space Grid: 1= Vista, 2= ASA, 3= CAUCHY\n");
+  fprintf(f, "SourceSpaceIn= 2\n");
+  fprintf(f, "#FEM HeadGrid: 1= Vista, 2= ASA, 3= CAUCHY\n");
+  fprintf(f, "HeadGridIn= 3\n");
+  fprintf(f, "#Output File; Format: 1= Vista, 2 = ASA, 3 = Vista + ASA, 4 = Vista + Ascii, 5 = Cauchy, 6 = SCIRun \n");
+  fprintf(f, "ResultOut= 4\n");
   
   fclose(f);
   return true;
@@ -460,7 +604,7 @@ ForwardIPM::execute()
     return;
   }
 
-  MatrixIPort *dipole_port2 = (MatrixIPort *)get_iport("Dipole Vector");
+  MatrixIPort *dipole_port2 = (MatrixIPort *)get_iport("Dipole Moments");
   MatrixHandle dipole2;
   
   if (!(dipole_port2->get(dipole2) && dipole2.get_rep()))
@@ -527,7 +671,8 @@ ForwardIPM::execute()
 
     // Construct our command line.
     const string impfile = "imp";
-    const string command = "(cd tmpfile;" + impfile + " -i sourcesimulation" +
+    const string command = "(cd " + tmpdir + ";" +
+      impfile + " -i sourcesimulation" +
       " -h " + condmeshfile + " -p " + parafile + " -s " + electrodefile +
       " -d " + dipolefile + " -o " + resultfile + " -fwd FEM -sens EEG)";
 
