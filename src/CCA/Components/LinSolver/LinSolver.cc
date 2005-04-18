a5882400905073d6d1f938f6579b495d86fad771
Keming Zhang <kzhang@sci.utah.edu>
2005-04-18 20:05:39 +0000

    updated LinSolver component

--- src/CCA/Components/LinSolver/LinSolver.cc
+++ src/CCA/Components/LinSolver/LinSolver.cc
@@ -51,46 +51,29 @@ using namespace SCIRun;
 
 extern "C" sci::cca::Component::pointer make_SCIRun_LinSolver()
 {
-  return sci::cca::Component::pointer(new LinSolver());
+  return sci::cca::Component::pointer(new LinSolver);
 }
 
-LinSolver::LinSolver()
-{
-  fieldPort.setParent(this);
-  goPort.setParent(this);
-}
-
-LinSolver::~LinSolver()
-{
-
+LinSolver::LinSolver(){
 }
 
 void LinSolver::setServices(const sci::cca::Services::pointer& svc)
 {
-
   services=svc;
-  //add provides ports here ...  
-
-  sci::cca::TypeMap::pointer props = svc->createTypeMap();
-  myGoPort::pointer gop(&goPort);
-  myField2DPort::pointer fdp(&fieldPort);
-  svc->addProvidesPort(gop,"go","sci.cca.ports.GoPort", props);
-  svc->addProvidesPort(fdp,"field","sci.cca.ports.Field2DPort", props);
-  svc->registerUsesPort("matrix", "sci.cca.ports.PDEMatrixPort",props);
-  // Remember that if the PortInfo is created but not used in a call to the svc object
-  // then it must be freed.
-  // Actually - the ref counting will take care of that automatically - Steve
+  myLinSolverPort::pointer lsp(new myLinSolverPort);
+  svc->addProvidesPort(lsp,"linsolver","sci.cca.ports.LinSolverPort", sci::cca::TypeMap::pointer(NULL));
 }
 
-bool LinSolver::jacobi(const SSIDL::array2<double> &A, 
-		       const SSIDL::array1<double> &b)
+int 
+myLinSolverPort::jacobi(const SSIDL::array2<double> &A, 
+			const SSIDL::array1<double> &b,::SSIDL::array1< double>& x)
 {
   //we might set the accurracy by UI
   double eps=1e-6;
   int maxiter=1000;
   
-  SSIDL::array1<double> x;
   int N=b.size();
+  x.clear();
   while(x.size()<b.size()) x.push_back(1.0);
 
   int iter;
@@ -120,51 +103,6 @@ bool LinSolver::jacobi(const SSIDL::array2<double> &A,
     x=tempx;
   }
 
-  solution=x;
-
-  return iter!=maxiter;
+  if(iter!=maxiter) return 0;
+  else return 1;
 }  
-
-SSIDL::array1<double> myField2DPort::getField() 
-{
-  return com->solution;
-}
-
-int myGoPort::go() 
-{
-  
-  sci::cca::Port::pointer pp=com->getServices()->getPort("matrix");	
-  if(pp.isNull()){
-    QMessageBox::warning(0, "LinSolver", "Port matrix is not available!");
-    return 1;
-  }  
-  sci::cca::ports::PDEMatrixPort::pointer matrixPort=
-    pidl_cast<sci::cca::ports::PDEMatrixPort::pointer>(pp);
-  
-  SSIDL::array2<double> A;
-  matrixPort->getMatrix(A);
-  SSIDL::array1<double> b=matrixPort->getVector();
-
-  cerr<<"#### A.sizes="<<A.size1()<<":"<<A.size2()<<endl;
-  cerr<<"#### b.sizes="<<b.size()<<endl;
- 
-  com->getServices()->releasePort("matrix");	
-  
-  if(A.size1()==0 || A.size2()==0){
-    QMessageBox::warning(0, "LinSolver", "Bad input matrix and vector!");
-    return 0;
-  }
-
-  //cerr<<"A.size="<<A->numOfRows()<<" x "<<A->numOfCols()<<endl;
-  //cerr<<"b.size="<<b.size()<<endl;
-   
-  if(!com->jacobi(A,b)){
-    QMessageBox::warning(0, "LinSolver", "Jacobi method failed!");
-  }
-  
-  return 0;
-}
-  
-
-
-
