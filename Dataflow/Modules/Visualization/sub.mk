151103fdcbbd8f358ba5d44a1b5db1a4a1e5984e
J. Davison de St. Germain <dav@sci.utah.edu>
2005-04-19 16:21:00 +0000

    Looks like Modules/Visualization now depends on Modules/Fields...

--- Dataflow/Modules/Visualization/sub.mk
+++ Dataflow/Modules/Visualization/sub.mk
@@ -61,7 +61,7 @@ SRCS     += \
 	$(SRCDIR)/VolumeVisualizer.cc\
 #[INSERT NEW CODE FILE HERE]
 
-PSELIBS := Dataflow/Network Dataflow/Widgets Dataflow/Ports \
+PSELIBS := Dataflow/Network Dataflow/Widgets Dataflow/Ports Dataflow/Modules/Fields \
 	Dataflow/Modules/Render Core/Datatypes Core/Containers \
 	Core/Exceptions Core/Thread Core/GuiInterface Core/Geom \
 	Core/Persistent Core/Geometry Core/2d Core/Util \
