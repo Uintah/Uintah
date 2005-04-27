33c5229369330009b76fce7f0969b681439c80af
Todd Harman <harman@sci.utah.edu>
2005-04-27 21:23:52 +0000

    added a new custom initialization (McDermott's method of manufactured solutions)

--- CCA/Components/ICE/customInitialize.h
+++ CCA/Components/ICE/customInitialize.h
@@ -1,16 +1,28 @@
 #ifndef _CUSTOMINITIALIZE_H
 #define _CUSTOMINITIALIZE_H
 
+#include <Packages/Uintah/CCA/Components/ICE/ICEMaterial.h>
 #include <Packages/Uintah/Core/Grid/Variables/CCVariable.h>
 #include <Packages/Uintah/Core/Grid/Patch.h>
 
 namespace Uintah {
 
+  struct vortices{    // multiple vortices
+    vector<Point> origin;
+    vector<double> strength;
+    vector<double> radius;
+    ~vortices() {};
+  };
+  
+  struct mms{         // method of manufactured solutions
+    double A;
+    ~mms() {};
+  };
+
   struct customInitialize_basket{
-      vector<Point> origin;
-      vector<double> strength;
-      vector<double> radius;
-      string which;
+    vortices* vortex_inputs;
+    mms* mms_inputs;
+    string which;
   };
   void customInitialization_problemSetup( const ProblemSpecP& cfd_ice_ps,
                                         customInitialize_basket* cib);
@@ -20,6 +32,7 @@ namespace Uintah {
                             CCVariable<double>& temp,
                             CCVariable<Vector>& vel_CC,
                             CCVariable<double>& press_CC,
+                            ICEMaterial* ice_matl,
                             const customInitialize_basket* cib);
 
 }// End namespace Uintah
