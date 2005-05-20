4a0576690d72e5cd7899aad71976ee923dd1e7ff
Steven G. Parker <sparker@sci.utah.edu>
2005-05-20 00:51:55 +0000

    Need to link against MPM for gcc 4.0 build.  This dependency should be removed.

--- src/Packages/Uintah/CCA/Components/ICE/sub.mk
+++ src/Packages/Uintah/CCA/Components/ICE/sub.mk
@@ -23,6 +23,7 @@ SUBDIRS := $(SRCDIR)/EOS $(SRCDIR)/Advection $(SRCDIR)/CustomBCs
 include $(SCIRUN_SCRIPTS)/recurse.mk          
 
 PSELIBS := \
+       Packages/Uintah/CCA/Components/MPM              \
        Packages/Uintah/CCA/Ports                       \
        Packages/Uintah/Core/Grid                       \
        Packages/Uintah/Core/Util                       \
