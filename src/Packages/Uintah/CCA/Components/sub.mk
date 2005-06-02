e95ac07a79a4c65dc895136df6f018e77067711b
Bryan Jeffrey Worthen <worthen@sci.utah.edu>
2005-06-02 18:11:25 +0000

    Undo last Fake Arches commit

--- src/Packages/Uintah/CCA/Components/sub.mk
+++ src/Packages/Uintah/CCA/Components/sub.mk
@@ -14,6 +14,12 @@ SUBDIRS := \
 	$(SRCDIR)/MPM \
 	$(SRCDIR)/ICE \
 	$(SRCDIR)/MPMICE \
+	$(SRCDIR)/MPMArches \
+	$(SRCDIR)/Arches \
+	$(SRCDIR)/Arches/fortran \
+	$(SRCDIR)/Arches/Mixing \
+	$(SRCDIR)/Arches/Radiation \
+	$(SRCDIR)/Arches/Radiation/fortran \
 	$(SRCDIR)/ProblemSpecification \
 	$(SRCDIR)/PatchCombiner \
 	$(SRCDIR)/Solvers
