e8655bb35198ac5607c2c74936a7d26e0f31eea7
John Schmidt <jas@sci.utah.edu>
2005-06-04 00:44:37 +0000

    Add SolverFactory.

---
+++ Packages/Uintah/CCA/Components/Solvers/SolverFactory.h
@@ -0,0 +1,24 @@
+#ifndef Packages_Uintah_CCA_Components_Solvers_SolverFactory_h
+#define Packages_Uintah_CCA_Components_Solvers_SolverFactory_h
+
+#include <Packages/Uintah/Core/ProblemSpec/ProblemSpecP.h>
+#include <Packages/Uintah/CCA/Ports/SolverInterface.h>
+
+namespace Uintah {
+
+  class ProcessorGroup;
+
+  class SolverFactory
+  {
+  public:
+    // this function has a switch for all known solvers
+    
+    static SolverInterface* create(ProblemSpecP& ps,
+                                   const ProcessorGroup* world);
+
+
+  };
+} // End namespace Uintah
+
+
+#endif
