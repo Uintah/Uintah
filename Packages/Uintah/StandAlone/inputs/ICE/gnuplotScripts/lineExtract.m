281456ba09e4cd56e491584b4270cb5f33e32cd3
Todd Harman <harman@sci.utah.edu>
2005-06-05 21:30:59 +0000

    Added ability to plot data on multiple levels.

--- Packages/Uintah/StandAlone/inputs/ICE/gnuplotScripts/lineExtract.m
+++ Packages/Uintah/StandAlone/inputs/ICE/gnuplotScripts/lineExtract.m
@@ -1,8 +1,7 @@
 %_________________________________
 % 09/01/04   --Todd
 % This matLab script generates 1D plots
-% from two different uda directories
-% using line extract
+% of piston validation data and makes a movie
 %
 %  NOTE:  You must have 
 %  setenv MATLAB_SHELL "/bin/tcsh -f"
@@ -15,132 +14,170 @@ clear function;
 
 %________________________________
 % USER INPUTS
-uda1 = sprintf('shockTube.uda.001');      %----change this
-uda2 = sprintf('shockTube.uda.001');      %----change this
-desc     = sprintf('');
-uda1_txt = sprintf('');
-uda2_txt = sprintf('');
-timestep_begin = 1;
-timestep_end   = 20;
+uda = 'AMR_HotBlob.uda.000'
+
+desc = '1D hotBlob';
+legendText = {'L-0','L-1'}
+pDir = 1;                    
+symbol = {'+','*r'}
+mat      = 0;
+
+
+numPlotCols = 1;
+numPlotRows = 4;
+
+plotRho   = true;
+plotTemp  = true;           
+plotPress    = true;
+plotVol_frac = false;
+plotSp_vol   = true
+plotVel      = false; 
 
 % lineExtract start and stop
-startEnd = '-istart 0 0 0 -iend 100 0 0'
+startEnd ={'-istart 0 0 0 -iend 100 0 0';'-istart 0 0 0 -iend 100 0 0' };
+
 %________________________________
+%  extract the physical time for each dump
+c0 = sprintf('puda -timesteps %s | grep : | cut -f 2 -d":" >& tmp',uda)
+[status0, result0]=unix(c0);
+physicalTime  = importdata('tmp');
+nDumps = length(physicalTime) - 1;
 
-
-
-[ans, test]=unix('printenv MATLAB_SHELL');   
-set(0,'DefaultFigurePosition',[0,0,1024,768]);
+  
+%set(0,'DefaultFigurePosition',[0,0,1024,768]);
 %_________________________________
 % Loop over all the timesteps
-for(ts = timestep_begin:timestep_end )
+for(ts = 1:nDumps )
   ts = input('input timestep') 
- 
-  %use line extract to pull out the data
-  c1 = sprintf('lineextract -v rho_CC   -timestep %i %s -o rho      -uda %s',ts,startEnd,uda1)
-  c2 = sprintf('lineextract -v vel_CC   -timestep %i %s -o vel_tmp  -uda %s',ts,startEnd,uda1)
-  c3 = sprintf('lineextract -v temp_CC  -timestep %i %s -o temp      -uda %s',ts,startEnd,uda1)
-  c4 = sprintf('lineextract -v press_CC -timestep %i %s -o press     -uda %s',ts,startEnd,uda1)
-  c5 = sprintf('lineextract -v delP_Dilatate -timestep %i %s -o delP -uda %s',ts,startEnd,uda1)
-  c6 = sprintf('lineextract -v scalar-f -timestep %i %s -o scalarf   -uda %s',ts,startEnd,uda1)
+  time = sprintf('%d sec',physicalTime(ts+1));
   
-  [status1, result1]=unix(c1)
-  [status2, result2]=unix(c2)
-  [status3, result3]=unix(c3)
-  [status4, result4]=unix(c4)
-  [status5, result5]=unix(c5)
-  [status6, result6]=unix(c6)
-    
-  % rip out [ ] from velocity data
-  c7 = sprintf('sed ''s/\\[//g'' vel_tmp | sed ''s/\\]//g'' >vel')
-  [status7, result7]=unix(c7)
-  
-  
-  % import the data into arrays
-  press1  = importdata('press');
-  temp1   = importdata('temp');
-  rho1    = importdata('rho');
-  delp1   = importdata('delP');
-  vel1    = importdata('vel');
-  f1      = importdata('scalarf');
-  press2  = importdata('press');
-  temp2   = importdata('temp');
-  rho2    = importdata('rho');
-  delp2   = importdata('delP');
-  vel2    = importdata('vel');
-  f2      = importdata('scalarf');
-  
-  %____________________________
-  % find principal direction in the data set
-  if max(press1(:,1)) ~= min(press1(:,1))
-    pDir = 1;
-  end
-  if max(press1(:,2)) ~= min(press1(:,2)) 
-    pDir = 2;
-  end
-  if max(press1(:,3)) ~= min(press1(:,3))
-    pDir = 3;
-  end
-  %__________________________________________________
-  %  Now plot it up
-  %__________________________________________________
-  %  temperature
-  subplot(2,3,1), plot(temp1(:,pDir),temp1(:,4),'-r',temp2(:,pDir),temp2(:,4),'-g')
-  legend(uda1_txt, uda2_txt);
-  %xlim ([0 5])
-  %axis([0 5 295 320])
-  xlabel('x')
-  ylabel('temp')
-  grid on;
+  %find max number of levels
+  c0 = sprintf('puda -gridstats %s -timesteplow %i -timestephigh %i |grep "Number of levels" | grep -o \\[0-9\\] >& tmp',uda, ts,ts);
+  [s, maxLevel]=unix(c0);
+  maxLevel  = importdata('tmp');
+  levelExists{1} = 0;
+   
+  % find what levels exists
+  for(L = 2:maxLevel)
+    c0 = sprintf('puda -gridstats %s -timesteplow %i -timestephigh %i >& tmp ; grep "Level: index %i" tmp',uda, ts,ts, L-1);
+    [levelExists{L}, result0]=unix(c0);
+  end 
   
   %______________________________
-  % pressure
-  subplot(2,3,2),plot(press1(:,pDir),press1(:,4),'-r',press2(:,pDir),press2(:,4),'-g')
-  %xlim ([0 5])
-  %axis([0 5 101000 109000])
-  xlabel('x')
-  ylabel('pressure')
-  title(desc);
-  grid on;
-  
-  %_____________________________
-  %  Density
-  subplot(2,3,3), plot(rho1(:,pDir),rho1(:,4), '-r', rho2(:,pDir),rho2(:,4), '-g')
-  %xlim ([0 5])
-  %axis([0 5 1.75 1.9])
-  xlabel('x')
-  ylabel('rho')
-  grid on;
-  
-  %____________________________
-  %   delP
-  subplot(2,3,4), plot(delp1(:,pDir),delp1(:,4), '-r', delp2(:,pDir), delp2(:,4), '-g')
-  %xlim ([0 5])
-  %axis([0 5 -10 10])
-  ylabel('DelP');
-  grid on;
-  
-  %____________________________
-  %  velocity
-  subplot(2,3,5), plot(vel1(:,pDir),vel1(:,4), '-r', vel1(:,pDir), vel1(:,5), '-g',vel1(:,pDir),vel2(:,6))
-    legend('x','y','z');
-  %xlim ([0 5])
-  %axis([0 5 -10 10])
-  ylabel('vel_CC');
-  grid on;
-  
-  %____________________________
-  %  scalar-f
-  subplot(2,3,6), plot(f1(:,pDir),f1(:,4), '-r', f2(:,pDir), f2(:,4), '-g')
-  %xlim ([0 5])
-  %axis([0 5 -10 10])
-  ylabel('scalar-f');
-  grid on;
-  %M(t) = getframe(gcf);
-  
-  clear press1,press2, temp1, temp2, rho1, rho2,;
-  clear delp1, delp2, vel2, vel2;
- 
+  for(L = 1:maxLevel) 
+    level = L-1;
+    if (levelExists{L} == 0) 
+     
+      S_E = startEnd{L};
+      unix('/bin/rm -f press temp rho vel sp_vol vol_frac');
+      plotNum = 1;
+      level
+      
+      %______________________________
+      %  temperature   
+      if plotTemp
+        c1 = sprintf('lineextract -v temp_CC -l %i -cellCoords -timestep %i %s -o temp -m %i  -uda %s',level,ts,S_E,mat,uda);
+        [s1, r1]   = unix(c1);
+        temp1{1,L} = importdata('temp');
+        
+        x = temp1{1,L}(:,pDir)
+        subplot(numPlotRows,numPlotCols,plotNum), plot(x,temp1{1,L}(:,4),symbol{L})
+        %xlim ([0 50])
+        %axis([0 5 295 320])
+        xlabel('x')
+        ylabel('temp')
+        title(time);
+        grid on;
+        hold
+        plotNum = plotNum+ 1;
+      end
+      %______________________________
+      % pressure
+      if plotPress
+        c2 = sprintf('lineextract -v press_CC -l %i -cellCoords -timestep %i %s -o press -m 0 -uda %s',level,ts,S_E,uda);
+        [s2, r2]    = unix(c2);
+        press1{1,L} = importdata('press');
+        
+       
+        subplot(numPlotRows,numPlotCols,plotNum),plot(x,press1{1,L}(:,4),symbol{L})
+        %xlim ([0 50])
+        %axis([0 5 101000 109000])
+        xlabel('x')
+        ylabel('pressure')
+        title(desc);
+        grid on;
+        hold
+        plotNum = plotNum+ 1;
+      end
+      %_____________________________
+      %  Density
+      if plotRho
+        c3 = sprintf('lineextract -v rho_CC -l %i -cellCoords -timestep %i %s -o rho  -m %i  -uda %s',level,ts,S_E,mat,uda);
+        [s3, r3]  = unix(c3);
+        rho1{1,L} = importdata('rho');
+        
+        
+        subplot(numPlotRows,numPlotCols,plotNum), plot(x,rho1{1,L}(:,4),symbol{L})
+        %xlim ([0 50])
+        %axis([0 5 1.75 1.9])
+        xlabel('x')
+        ylabel('rho')
+        grid on;
+        hold
+        plotNum = plotNum+ 1;
+      end
+      %____________________________
+      %   sp_vol
+      if plotSp_vol
+        c4 = sprintf('lineextract -v sp_vol_CC -l %i -cellCoords -timestep %i %s -o sp_vol -m %i  -uda %s',level,ts,S_E,mat,uda);
+        [s4, r4] = unix(c4);
+        f1{1,L}  = importdata('sp_vol');
+        
+        
+        subplot(numPlotRows,numPlotCols,plotNum), plot(x,f1{1,L}(:,4),symbol{L})
+        %xlim ([0 50])
+        %axis([0 50 0 0.01])
+        ylabel('sp vol');
+        grid on;
+        hold
+        plotNum = plotNum+ 1;
+      end
+      %____________________________
+      %  velocity
+      if plotVel
+        c5 = sprintf('lineextract -v vel_CC      -l %i -cellCoords -timestep %i %s -o vel_tmp  -m %i  -uda %s',level,ts,S_E,mat,uda);
+        [s5, r5]=unix(c5);
+        % rip out [ ] from velocity data
+        c6 = sprintf('sed ''s/\\[//g'' vel_tmp | sed ''s/\\]//g'' >vel');
+        [s6, r6]  = unix(c5);
+        vel1{1,L} = importdata('vel');
+        
+        subplot(numPlotRows,numPlotCols,plotNum), plot(x, vel1{1,L}(:,5),symbol{L})
+        legend('y');
+        %xlim ([0 50])
+        %axis([0 5 -10 10])
+        ylabel('vel CC');
+        grid on;
+        plotNum = plotNum+ 1;
+      end
+      %____________________________
+      %   vol_frac
+      if plotVol_frac
+        c6 = sprintf('lineextract -v vol_frac_CC -l %i -cellCoords -timestep %i %s -o vol_frac -m %i  -uda %s',level,ts,S_E,mat,uda);
+        [s6, r6]=unix(c6);
+        vf{1,L} = importdata('vol_frac');
+        
+        
+        subplot(numPlotRows,numPlotCols,plotNum), plot(x,vf{1,L}(:,4),symbol{L})
+        %xlim ([0 50])
+        %axis([0 5 -10 10])
+        ylabel('vol frac');
+        grid on;
+        hold
+        plotNum = plotNum+ 1;
+      end
+    end  % if level exists
+ end
 end
 %__________________________________
 % show the move and make an avi file
