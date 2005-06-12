0eca8d6cec972c23e8fe4482f7ef9ced6f116eec
Todd Harman <harman@sci.utah.edu>
2005-06-12 19:10:43 +0000

    Added the dummy label "AMR_SyncTaskgraph"   With recursive tasks + AMR you need to compute something   inside of refineCoarseFineInterfaces() to sync the task graph   on level > 0

--- ICE.cc
+++ ICE.cc
@@ -942,7 +942,13 @@ void ICE::scheduleComputeThermoTransportProperties(SchedulerP& sched,
   Task* t;
   cout_doing << "ICE::schedulecomputeThermoTransportProperties" << endl;
   t = scinew Task("ICE::computeThermoTransportProperties", 
-            this, &ICE::computeThermoTransportProperties);   
+            this, &ICE::computeThermoTransportProperties); 
+            
+  if(d_doAMR && level->getIndex() !=0){
+  // dummy variable needed to keep the taskgraph in sync
+    t->requires(Task::NewDW,lb->AMR_SyncTaskgraphLabel,Ghost::None,0);
+  }           
+  
   t->computes(lb->viscosityLabel);
   t->computes(lb->thermalCondLabel);
   t->computes(lb->gammaLabel);
