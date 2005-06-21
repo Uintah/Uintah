c61d93e25959487ebaeb50a92250547788c5cce0
Ayla Khan <ayla@sci.utah.edu>
2005-06-21 17:42:41 +0000

    Add ZListWriter to component subdirectories.

--- src/CCA/Components/sub.mk
+++ src/CCA/Components/sub.mk
@@ -32,10 +32,22 @@
 SRCDIR   := CCA/Components
 
 ifeq ($(HAVE_QT),yes)
-SUBDIRS := $(SRCDIR)/Builder $(SRCDIR)/TxtBuilder $(SRCDIR)/Hello  $(SRCDIR)/ListPlotter \
-	$(SRCDIR)/ZList $(SRCDIR)/Viewer $(SRCDIR)/LinSolver \
-	$(SRCDIR)/FileReader $(SRCDIR)/FEM $(SRCDIR)/Tri $(SRCDIR)/TableTennis \
-	$(SRCDIR)/TTClient $(SRCDIR)/World $(SRCDIR)/PDEdriver 
+  SUBDIRS := \
+             $(SRCDIR)/Builder \
+             $(SRCDIR)/TxtBuilder \
+             $(SRCDIR)/Hello  \
+             $(SRCDIR)/ListPlotter \
+             $(SRCDIR)/ZList \
+             $(SRCDIR)/ZListWriter \
+             $(SRCDIR)/Viewer \
+             $(SRCDIR)/LinSolver \
+             $(SRCDIR)/FileReader \
+             $(SRCDIR)/FEM \
+             $(SRCDIR)/Tri \
+             $(SRCDIR)/TableTennis \
+             $(SRCDIR)/TTClient \
+             $(SRCDIR)/World \
+             $(SRCDIR)/PDEdriver 
 else
  SUBDIRS := \
             $(SRCDIR)/TxtBuilder \
@@ -44,11 +56,11 @@ else
 endif
 
 ifeq ($(HAVE_TAO),yes)
-SUBDIRS += $(SRCDIR)/TAO
+  SUBDIRS += $(SRCDIR)/TAO
 endif
 
 ifeq ($(HAVE_MPI),yes)
-SUBDIRS += $(SRCDIR)/PWorld $(SRCDIR)/PHello 
+  SUBDIRS += $(SRCDIR)/PWorld $(SRCDIR)/PHello 
 # $(SRCDIR)/PLinSolver
 endif
 
