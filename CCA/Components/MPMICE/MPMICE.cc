32f8b4b379f333d9449e647306b358c3a61d6c43
John Schmidt <jas@sci.utah.edu>
2005-06-22 17:39:46 +0000

    Change particleRelocation arguments to used sharedState instead of Mlb.

--- CCA/Components/MPMICE/MPMICE.cc
+++ CCA/Components/MPMICE/MPMICE.cc
@@ -421,8 +421,8 @@ MPMICE::scheduleTimeAdvance(const LevelP& level, SchedulerP& sched,
 
   sched->scheduleParticleRelocation(level,
                                 Mlb->pXLabel_preReloc, 
-                                Mlb->d_particleState_preReloc,
-                                Mlb->pXLabel, Mlb->d_particleState,
+                                d_sharedState->d_particleState_preReloc,
+                                Mlb->pXLabel, d_sharedState->d_particleState,
                                 Mlb->pParticleIDLabel, mpm_matls);
 
   //__________________________________
