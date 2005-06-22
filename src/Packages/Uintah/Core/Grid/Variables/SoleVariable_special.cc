435f31a6cc7d43c4d5956a986b273a339eb75e68
John Schmidt <jas@sci.utah.edu>
2005-06-22 17:29:05 +0000

    Add specializations for different data types for SoleVariable. Use swapbytes defined in Endian.cc for ReductionVariable.

--- src/Packages/Uintah/Core/Grid/Variables/SoleVariable_special.cc
+++ src/Packages/Uintah/Core/Grid/Variables/SoleVariable_special.cc
@@ -37,4 +37,61 @@ SoleVariable<double>::putMPIData(vector<char>& data, int& index)
 }
 
 
+template<>
+void
+SoleVariable<int>::getMPIInfo(int& count, MPI_Datatype& datatype)
+{
+   datatype = MPI_INT;
+   count = 1;
+}
+
+template<>
+void
+SoleVariable<int>::getMPIData(vector<char>& data, int& index)
+{
+  ASSERTRANGE(index, 0, static_cast<int>(data.size()+1-sizeof(int)));
+  int* ptr = reinterpret_cast<int*>(&data[index]);
+  *ptr = value;
+  index += sizeof(int);
+}
+
+template<>
+void
+SoleVariable<int>::putMPIData(vector<char>& data, int& index)
+{
+  ASSERTRANGE(index, 0, static_cast<int>(data.size()+1-sizeof(int)));
+  int* ptr = reinterpret_cast<int*>(&data[index]);
+  value = *ptr;
+  index += sizeof(int);
+}
+
+template<>
+void
+SoleVariable<bool>::getMPIInfo(int& count, MPI_Datatype& datatype)
+{
+   datatype = MPI_CHAR;
+   count = 1;
+}
+
+template<>
+void
+SoleVariable<bool>::getMPIData(vector<char>& data, int& index)
+{
+  ASSERTRANGE(index, 0, static_cast<int>(data.size()+1-sizeof(char)));
+  char* ptr = reinterpret_cast<char*>(&data[index]);
+  *ptr = value;
+  index += sizeof(char);
+}
+
+template<>
+void
+SoleVariable<bool>::putMPIData(vector<char>& data, int& index)
+{
+  ASSERTRANGE(index, 0, static_cast<int>(data.size()+1-sizeof(char)));
+  char* ptr = reinterpret_cast<char*>(&data[index]);
+  value = *ptr;
+  index += sizeof(char);
+}
+
+
 } // end namespace Uintah
