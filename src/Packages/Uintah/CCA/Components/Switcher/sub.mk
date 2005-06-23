6d2e6548c7dc388312e7b8e50b9a933bcd0d4758
Steven G. Parker <sparker@sci.utah.edu>
2005-06-23 22:28:05 +0000

    More libraries needed for the mac

--- src/Packages/Uintah/CCA/Components/Switcher/sub.mk
+++ src/Packages/Uintah/CCA/Components/Switcher/sub.mk
@@ -8,10 +8,14 @@ SRCS    := $(SRCDIR)/Switcher.cc
 
 PSELIBS := \
         Packages/Uintah/CCA/Ports        \
+	Packages/Uintah/Core/Disclosure  \
         Packages/Uintah/Core/Exceptions  \
+	Packages/Uintah/Core/Grid        \
         Packages/Uintah/Core/Parallel    \
         Packages/Uintah/Core/ProblemSpec \
-        Packages/Uintah/Core/Util
+        Packages/Uintah/Core/Util        \
+	Core/Exceptions \
+	Core/Util
 
 LIBS    := $(XML_LIBRARY) $(MPI_LIBRARY)
 
