96156e4a3ab158314d5bae5fe8aa27fb45fc45ea
Oren Livne <livne@sci.utah.edu>
2005-06-30 18:04:59 +0000

    2-level problem shows signs of working, but need to convert ghost variables to point values and put 0 on deleted boxes of coarse patches so that we can have a real comparison.

--- Packages/Uintah/CCA/Components/ICE/PressureSolve/LaplacePanayot/printSparse.m
+++ Packages/Uintah/CCA/Components/ICE/PressureSolve/LaplacePanayot/printSparse.m
@@ -1,24 +1,24 @@
-function list = printSparse(A,fileName)
-if (nargin < 2)
+function list = printSparse(A,b,fileName)
+if (nargin < 3)
     f = 1;
 else
     f = fopen(fileName,'w');
 end
 
-[i,j] = find(A);
-data = full(A(find(A)));
+[i,j,data] = find(A);
 list = [i j data];
 list = sortrows(list);
 
 for k = 1:size(list,1)
     if (k > 1)
         if (list(k,1) ~= list(k-1,1))
+            fprintf(f,'b(%d) = %+f\n',list(k-1,1),b(list(k-1,1)));
             fprintf(f,'\n');
         end
     end
     fprintf(f,'(%5d , %5d)   %+f\n',list(k,:));
 end
 
-if (nargin >= 2)
+if (nargin >= 3)
     fclose(f);
 end
