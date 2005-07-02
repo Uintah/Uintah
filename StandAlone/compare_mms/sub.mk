143f4f7c4eafaf5f614fb32de001dbfc6cff3d0a
Bryan Jeffrey Worthen <worthen@sci.utah.edu>
2005-07-02 14:47:51 +0000

    don't link against CCA/Components

--- StandAlone/compare_mms/sub.mk
+++ StandAlone/compare_mms/sub.mk
@@ -26,7 +26,6 @@ else
         Packages/Uintah/Core/Disclosure  \
         Packages/Uintah/Core/Exceptions  \
         Packages/Uintah/CCA/Ports        \
-	Packages/Uintah/CCA/Components        \
 	Packages/Uintah/CCA/Components/Models \
         Packages/Uintah/CCA/Components/MPM    \
         Packages/Uintah/CCA/Components/MPMICE \
