bd0f57362755245306e2d1e8aed7d2f97f9a3763
Yarden Livnat <yarden@sci.utah.edu>
2005-07-02 22:10:45 +0000

    initial commit

---
+++ Packages/Plume/sub.mk
@@ -0,0 +1,27 @@
+#Makefile fragment for the Packages/Plume directory
+
+include $(SCIRUN_SCRIPTS)/largeso_prologue.mk
+
+SRCDIR := Packages/Plume
+
+SUBDIRS := \
+	$(SRCDIR)/Core         \
+	$(SRCDIR)/Dataflow     \
+
+include $(SCIRUN_SCRIPTS)/recurse.mk
+
+PSELIBS := 
+
+LIBS := $(BOOST_LIB) $(LOKI_LIBRARY)  $(M_LIBRARY) $(THREAD_LIBRARY) 
+#LIBS := $(OOGL_LIBRARY) $(GL_LIBRARY) $(FASTM_LIBRARY) $(M_LIBRARY) $(THREAD_LIBRARY) $(PERFEX_LIBRARY) 
+
+include $(SCIRUN_SCRIPTS)/largeso_epilogue.mk
+
+SUBDIRS := \
+	$(SRCDIR)/StandAlone
+include $(SCIRUN_SCRIPTS)/recurse.mk
+
+#CFLAGS := $(CFLAGS) -OPT:IEEE_arithmetic=3 
+#CFLAGS := $(CFLAGS) -OPT:Olimit=16383
+#CFLAGS := $(CFLAGS) -OPT:Olimit_opt=on
+
