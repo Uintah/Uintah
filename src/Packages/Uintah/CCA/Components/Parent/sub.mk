5405eb238b20e01dedae55ffe63d2fcb856efe5f
J. Davison de St. Germain <dav@sci.utah.edu>
2005-07-05 22:38:08 +0000

    Rearrange entires for easier fake arches manipulation

--- src/Packages/Uintah/CCA/Components/Parent/sub.mk
+++ src/Packages/Uintah/CCA/Components/Parent/sub.mk
@@ -7,7 +7,6 @@ SRCDIR  := Packages/Uintah/CCA/Components/Parent
 SRCS    := $(SRCDIR)/Switcher.cc \
 	   $(SRCDIR)/ComponentFactory.cc 
 
-
 PSELIBS := \
 	Core/Exceptions \
 	Core/Util \
@@ -18,11 +17,11 @@ PSELIBS := \
         Packages/Uintah/Core/Parallel    \
         Packages/Uintah/Core/ProblemSpec \
         Packages/Uintah/Core/Util        \
-        Packages/Uintah/CCA/Components/Arches \
-        Packages/Uintah/CCA/Components/Examples \
-        Packages/Uintah/CCA/Components/ICE      \
-        Packages/Uintah/CCA/Components/MPM      \
-        Packages/Uintah/CCA/Components/MPMArches   \
+        Packages/Uintah/CCA/Components/Arches    \
+        Packages/Uintah/CCA/Components/MPMArches \
+        Packages/Uintah/CCA/Components/Examples  \
+        Packages/Uintah/CCA/Components/ICE       \
+        Packages/Uintah/CCA/Components/MPM       \
         Packages/Uintah/CCA/Components/MPMICE  
 
 LIBS    := $(XML_LIBRARY) $(MPI_LIBRARY)
