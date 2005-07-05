af1efcfe3852fc6e264863afeff5e913616dfe92
J. Davison de St. Germain <dav@sci.utah.edu>
2005-07-05 23:59:50 +0000

    Just added some documentation.

--- src/Packages/Uintah/CCA/Components/Dummy/sub.mk
+++ src/Packages/Uintah/CCA/Components/Dummy/sub.mk
@@ -1,6 +1,15 @@
 # Makefile fragment for this subdirectory
 #
 
+#
+# The Dummy components are used when the user does not wish to compile
+# the real component.  They allow for the linker to satisfy the need
+# for all of the components.  However, if a dummy component is used
+# (accidentally) in a 'simulation', then an error message is printed
+# out so that the user can know that he or she is using a dummy
+# component and not the real one.
+#
+
 include $(SCIRUN_SCRIPTS)/smallso_prologue.mk
 
 SRCDIR := Packages/Uintah/CCA/Components/Dummy
