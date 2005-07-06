0965024e47abc7dde28391a07d3f535875b662b4
Oren Livne <livne@sci.utah.edu>
2005-07-06 01:09:44 +0000

    Cleaned MATLAB directories, part 3

--- Packages/Uintah/CCA/Components/ICE/PressureSolve/LaplacePanayot/refineIndex.m
+++
@@ -1,9 +0,0 @@
-function indexF = refineIndex(grid,k,indexC)
-
-if (k == grid.numLevels)
-    error('Cannot refine index from finest level');
-end
-
-r = grid.level{k+1}.refRatio;
-indexF = repmat(r,size(indexC)./size(r)).*(indexC - 1) + 1;
-
