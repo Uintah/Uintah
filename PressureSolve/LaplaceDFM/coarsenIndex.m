2ef930dc9e62484b47456f6863c8eca668f3cc7c
Oren Livne <livne@sci.utah.edu>
2005-07-06 01:09:44 +0000

    Cleaned MATLAB directories, part 3

--- PressureSolve/LaplaceDFM/coarsenIndex.m
+++
@@ -1,8 +0,0 @@
-function indexC = coarsenIndex(grid,k,indexF)
-
-if (k == 1)
-    error('Cannot coarsen index from coarsest level');
-end
-
-r = grid.level{k}.refRatio;
-indexC = ceil(indexF./repmat(r,size(indexF)./size(r)));
