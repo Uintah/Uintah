8607eaf84beccd27a798bf30a2b824bc5518d985
Oren Livne <livne@sci.utah.edu>
2005-07-12 18:53:49 +0000

    Added and updated comments on half of the files

--- src/Packages/Uintah/CCA/Components/ICE/Matlab/PressureSolve/LaplacePanayot/exactSolution.m
+++ src/Packages/Uintah/CCA/Components/ICE/Matlab/PressureSolve/LaplacePanayot/exactSolution.m
@@ -2,12 +2,31 @@ function u = exactSolution(x)
 %EXACTSOLUTION  Exact solution for test problems.
 %   U = EXACTSOLUTION(X) returns the exact solution evaluated at the
 %   locations X. X is a Dx1 cell array of coordinates X{1},...,X{D} in D
-%   dimensions. The global struct entry PARAM.problemType controls which
-%   solution is output.
+%   dimensions. The global struct entry param.problemType controls which
+%   solution is output. Options are:
+%       'quadratic'
+%           U = quadratic function with Dirichlet B.C. on the 2D unit
+%           square. Diffusion a=1 (Laplace operator). U is smooth.
+%       'sinsin'
+%           U = sin(pi*x1)*sin(pi*x2) with Dirichlet B.C. on the 2D unit
+%           square. Diffusion a=1 (Laplace operator). U is smooth.
+%       'GaussianSource'
+%           U = is the solution to Laplace's equation with Gaussian right
+%           hand side, centered at (0.5,0.5) with standard deviation of
+%           (0.05,0.05) on the 2D unit square. Diffusion a=1 (Laplace
+%           operator). U is smooth but localized around the source, so at
+%           coarse level it is beneficial to locally refine around the
+%           center of the domain.
+%       'Lshaped'
+%           U = r^(2/3)*sin(2*theta/3) is the solution the Laplace's
+%           equation with Dirichlet B.C. on the L-shaped domain [0,1]^2 \
+%           [0.5,1]^2.Diffusion a=1 (Laplace operator). This is a
+%           re-entrant corner problem where U is singular.
 %
-%   See also: TESTDISC, RHS, RHSBC, EXACTSOLUTIONAMR.
+%   See also: TESTDISC, RHS, RHSBC, EXACTSOLUTIONAMR, DIFFUSION.
 
-% Exact solution
+% Revision history:
+% 12-JUL-2005    Oren Livne    Added comments.
 
 globalParams;
 
@@ -16,10 +35,10 @@ switch (param.problemType)
     case 'quadratic',
         u       = x{1}.*(1-x{1}).*x{2}.*(1-x{2});
 
-    case 'ProblemA',
+    case 'sinsin',
         u       = sin(pi*x{1}).*sin(pi*x{2});
 
-    case 'ProblemB',
+    case 'GaussianSource',
         K       = 1;
         x0      = [0.5 0.5];
         sigma   = [0.05 0.05];
@@ -37,10 +56,3 @@ switch (param.problemType)
     otherwise,
         error('Unknown problem type');
 end
-
-%center = find((abs(x-0.5) <= 0.25) & (abs(y-0.5) <= 0.25));
-%u(center) = u(center) + (sin(2*pi*(x(center)-0.25)).*sin(2*pi*(y(center)-0.25))).^2;
-%u(center) = u(center) + sin(2*pi*(x(center)-0.25)).^4;
-%u(center) = u(center) + 100*(x(center)-0.25).^4.*(x(center)-0.75).^4;
-%u = x;
-%u = log(sqrt((x{1}-0.5).^2 + (x{2}-0.5).^2));
--- src/Packages/Uintah/CCA/Components/ICE/Matlab/PressureSolve/LaplacePanayot/rhs.m
+++ src/Packages/Uintah/CCA/Components/ICE/Matlab/PressureSolve/LaplacePanayot/rhs.m
@@ -1,11 +1,15 @@
 function f = rhs(x)
-%RHS  Right hand side function.
+%RHS  Right hand side function for the diffusion equation.
 %   F = RHS(X) returns the diffusion equation's right hand side,
 %   evaluated at the locations X. X is a Dx1 cell array of coordinates
 %   X{1},...,X{D} in D dimensions. The global struct entry
-%   PARAM.problemType controls which RHS is output.
+%   PARAM.problemType controls which RHS is output. See "help exactsolution"
+%   for a list of possible problems.
 %
-%   See also: TESTDISC, EXACTSOLUTION, RHSBC.
+%   See also: TESTDISC, EXACTSOLUTION, RHSBC, DIFFUSION.
+
+% Revision history:
+% 12-JUL-2005    Oren Livne    Added comments.
 
 globalParams;
 
@@ -14,10 +18,10 @@ switch (param.problemType)
     case 'quadratic',
         f       = 2*(x{1}.*(1-x{1}) + x{2}.*(1-x{2}));
 
-    case 'ProblemA',
+    case 'sinsin',
         f       = 2*pi*pi*sin(pi*x{1}).*sin(pi*x{2});
 
-    case 'ProblemB',
+    case 'GaussianSource',
         K       = 1;
         x0      = [0.5 0.5];
         sigma   = [0.05 0.05];
@@ -31,20 +35,3 @@ switch (param.problemType)
     otherwise,
         error('Unknown problem type');
 end
-
-%f = zeros(size(x{1}));
-
-%center = find((abs(x{1}-0.5) <= 0.25) & (abs(x{2}-0.5) <= 0.25));
-% f(center) = f(center) + 8*pi*pi*(...
-%     sin(4*pi*(x{1}(center)-0.25)).*sin(2*pi*(x{2}(center)-0.25)).^2 + ...
-%     sin(4*pi*(x{2}(center)-0.25)).*sin(2*pi*(x{1}(center)-0.25)).^2 ...
-%     );
-% f(center) = f(center) - ...
-%     16*pi*pi*(3*sin(2*pi*(x{1}(center)-0.25)).^2 - 4*sin(2*pi*(x{1}(center)-0.25)).^4);
-%     f(center) = f(center) - ...
-%         100*((12*(x{1}(center)-0.25).^2.*(x{1}(center)-0.75).^4 + 12*(x{1}(center)-0.25).^4.*(x{1}(center)-0.75).^2 + ...
-%         2*4*4*(x{1}(center)-0.25).^3.*(x{1}(center)-0.75).^3));
-
-% Delta function (how to average to cell centered grid?)
-% f = zeros(size(x{1}));
-% find((abs(x{1}-0.5) <= eps) & (abs(x{2}-0.5) <= eps));
