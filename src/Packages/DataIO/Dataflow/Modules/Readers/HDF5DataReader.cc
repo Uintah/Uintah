94bdbee30f2e56a699fe8b54f482b555e32e8e91
Allen Sanderson <allen@sci.utah.edu>
2005-07-13 15:54:52 +0000

    removed extra space

--- src/Packages/DataIO/Dataflow/Modules/Readers/HDF5DataReader.cc
+++ src/Packages/DataIO/Dataflow/Modules/Readers/HDF5DataReader.cc
@@ -1287,7 +1287,7 @@ NrrdDataHandle HDF5DataReader::readDataset( string filename,
       }
 
 #if H5_VERS_RELEASE == 2
-      hssize_t *start  = new hssize_t[ndims];
+      hssize_t *start = new hssize_t[ndims];
 #else
       hsize_t *start  = new hsize_t[ndims];
 #endif
