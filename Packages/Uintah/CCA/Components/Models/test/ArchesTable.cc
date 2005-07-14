35800e68ebbbb84ff84bc2fa36a8abfc6cdcdb22
Steven G. Parker <sparker@sci.utah.edu>
2005-07-14 22:20:52 +0000

    Don't free 0th axis (doesn't exist)

--- Packages/Uintah/CCA/Components/Models/test/ArchesTable.cc
+++ Packages/Uintah/CCA/Components/Models/test/ArchesTable.cc
@@ -515,7 +515,7 @@ void ArchesTable::setup()
   // Free up the input deps
   for(int i=0;i<(int)in_inds.size();i++)
     delete inds[i];
-  for(int i=0;i<(int)in_axes.size();i++)
+  for(int i=1;i<(int)in_axes.size();i++)
     if(--in_axes[i]->useCount == 0)
       delete in_axes[i];
   for(int i=0;i<(int)in_deps.size();i++){
