8bb50b46924d80b2322f3776d94afd042caff040
Jim Guilkey <guilkey@sci.utah.edu>
2005-07-14 17:48:47 +0000

    Adding the ability to turn off the thermal expansion calculation by using a flag, rather than being forced to edit the material properties files. "DoThermalExpansion" is true by default.

--- src/Packages/Uintah/CCA/Components/MPM/MPMFlags.h
+++ src/Packages/Uintah/CCA/Components/MPM/MPMFlags.h
@@ -44,6 +44,7 @@ namespace Uintah {
                                          // new particles after failure
     bool        d_addNewMaterial; // Flag to decide whether to create
     bool        d_doErosion; // Flag to decide whether to erode or not
+    bool        d_doThermalExpansion; // Decide whether to do thermExp or not
     bool        d_with_color;         // to turn on the color variable
     bool        d_fracture;         // to turn on fracture
 
