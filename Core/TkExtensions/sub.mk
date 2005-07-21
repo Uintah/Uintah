5a2d47f7f1fc60654bc1c7105bd02847b75bc2b2
Bryan Jeffrey Worthen <worthen@sci.utah.edu>
2005-07-21 20:06:12 +0000

    Fix for windows again

--- Core/TkExtensions/sub.mk
+++ Core/TkExtensions/sub.mk
@@ -37,7 +37,9 @@ SRCS     += $(SRCDIR)/tk3d2.c $(SRCDIR)/tkAppInit.c $(SRCDIR)/tkBevel.c \
 	$(SRCDIR)/tkCanvBLine.c $(SRCDIR)/tkCursor.c $(SRCDIR)/tkOpenGL.c \
 	$(SRCDIR)/tk3daux.c $(SRCDIR)/tkRange.c $(SRCDIR)/tkUnixRange.c \
 
+ifneq ($(IS_WIN),yes) 
 SRCS += $(SRCDIR)/tclUnixNotify-$(TK_VERSION).c
+endif
 
 #	$(SRCDIR)/tclTimer.c \
 
