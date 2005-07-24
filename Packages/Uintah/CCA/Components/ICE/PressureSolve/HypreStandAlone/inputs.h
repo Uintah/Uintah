57b63b41435dc876c9ad0cf0c14077f818058af6
Oren Livne <livne@sci.utah.edu>
2005-07-24 22:31:55 +0000

    Added inputs file with diffusion, RHSs etc.

---
+++ Packages/Uintah/CCA/Components/ICE/PressureSolve/HypreStandAlone/inputs.h
@@ -0,0 +1,13 @@
+#ifndef __INPUTS_H__
+#define __INPUTS_H__
+
+#include <vector>
+#include <stdlib.h>
+
+#include "mydriver.h"
+
+using std::vector;
+
+double diffusion(const Location& x);
+
+#endif // __INPUTS_H__
