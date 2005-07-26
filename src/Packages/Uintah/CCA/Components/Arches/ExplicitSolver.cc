348e8e87d3f129ceb5562946549ba98dba36c1eb
David Groulx <dgroulx@sci.utah.edu>
2005-07-26 20:27:49 +0000

    Made the InvalidValue exception inherit from the global SCIRun Exception class instead of ProblemSetupException.  Also added a few include lines for files that were effected by this change.

--- src/Packages/Uintah/CCA/Components/Arches/ExplicitSolver.cc
+++ src/Packages/Uintah/CCA/Components/Arches/ExplicitSolver.cc
@@ -22,6 +22,7 @@
 #include <Packages/Uintah/CCA/Ports/DataWarehouse.h>
 #include <Packages/Uintah/CCA/Ports/Scheduler.h>
 #include <Packages/Uintah/Core/Exceptions/InvalidValue.h>
+#include <Packages/Uintah/Core/Exceptions/ProblemSetupException.h>
 #include <Packages/Uintah/Core/Grid/Variables/CCVariable.h>
 #include <Packages/Uintah/Core/Grid/Level.h>
 #include <Packages/Uintah/Core/Grid/Variables/PerPatch.h>
--- src/Packages/Uintah/Core/Exceptions/InvalidValue.cc
+++ src/Packages/Uintah/Core/Exceptions/InvalidValue.cc
@@ -5,22 +5,27 @@
 using namespace Uintah;
 
 InvalidValue::InvalidValue(const std::string& msg, const char* file, int line)
-    : ProblemSetupException(msg, __FILE__, __LINE__)
+  : d_msg(msg)
 {
 #ifdef EXCEPTIONS_CRASH
-  std::cout << "An Invalid Value exception was thrown.\n";
+  std::cout << "An InvalidValue exception was thrown.\n";
   std::cout << file << ":" << line << "\n";
   std::cout << msg << "\n";
 #endif
 }
 
 InvalidValue::InvalidValue(const InvalidValue& copy)
-    : ProblemSetupException(copy)
+  : d_msg(copy.d_msg)
 {
 }
 
 InvalidValue::~InvalidValue()
 {
+}
+
+const char* InvalidValue::message() const
+{
+    return d_msg.c_str();
 }
 
 const char* InvalidValue::type() const
