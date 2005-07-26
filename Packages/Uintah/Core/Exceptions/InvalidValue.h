8f9a1093bbdfb77d46b13bf7b2e487b00e06cd8a
David Groulx <dgroulx@sci.utah.edu>
2005-07-26 20:27:49 +0000

    Made the InvalidValue exception inherit from the global SCIRun Exception class instead of ProblemSetupException.  Also added a few include lines for files that were effected by this change.

--- Packages/Uintah/Core/Exceptions/InvalidValue.h
+++ Packages/Uintah/Core/Exceptions/InvalidValue.h
@@ -14,18 +14,25 @@
 #ifndef UINTAH_EXCEPTIONS_INVALIDVALUE_H
 #define UINTAH_EXCEPTIONS_INVALIDVALUE_H
 
-#include <Packages/Uintah/Core/Exceptions/ProblemSetupException.h>
+#include <Core/Exceptions/Exception.h>
+#include <sgi_stl_warnings_off.h>
+#include <string>
+#include <sgi_stl_warnings_on.h>
 
 namespace Uintah {
 
-   class InvalidValue : public ProblemSetupException {
+   using namespace SCIRun;
+
+   class InvalidValue : public Exception {
    public:
       InvalidValue(const std::string&, const char* file, int line);
       InvalidValue(const InvalidValue&);
       virtual ~InvalidValue();
+      virtual const char* message() const;
       virtual const char* type() const;
    protected:
    private:
+      std::string d_msg;
       InvalidValue& operator=(const InvalidValue&);
    };
 
