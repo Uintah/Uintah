750ae11e4c45a64c88c1483e3e89f69b6122d90a
Scott Bardenhagen <bard@sci.utah.edu>
2005-07-27 22:19:58 +0000

    Here is a complete revision of the standalone utility dumpfields.  It does a variety of things (run dumpfields -help), probably best to contact Andrew or myself here at LANL.

---
+++ src/Packages/Uintah/StandAlone/tools/dumpfields/InfoDumper.h
@@ -0,0 +1,65 @@
+#ifndef DUMPFIELDS_INFO_DUMPER_H
+#define DUMPFIELDS_INFO_DUMPER_H
+
+#include "FieldDumper.h"
+#include "Args.h"
+#include "FieldSelection.h"
+
+namespace Uintah {
+  
+  class InfoOpts {
+  public:
+    InfoOpts(Args & args);
+    
+    bool  showeachmat;
+    
+    static std::string options() { 
+      return \
+        std::string("      -allmats               show material-by-material information");
+    }
+  };
+  
+  class InfoDumper : public FieldDumper 
+  {
+  public:
+    InfoDumper(DataArchive* da, string basedir, 
+               const InfoOpts & opts, const FieldSelection & fselect);
+  
+    string directoryExt() const { return "info"; }
+    void addField(string /*fieldname*/, const Uintah::TypeDescription * /*theType*/) {}
+  
+    class Step : public FieldDumper::Step {
+    public:
+      Step(DataArchive * da, string tsdir, int index, double time, 
+           const InfoOpts & opts, const FieldSelection & fselect);
+      
+      void storeGrid () {}
+      void storeField(string fieldname, const Uintah::TypeDescription * type);
+      
+      string infostr() const { return info_; }
+      
+    private:
+      string                 info_;
+      DataArchive*           da_;
+      InfoOpts               opts_;
+      const FieldSelection & fselect_;
+    };
+    
+    //
+    Step * addStep(int index, double time, int iset);
+    void   finishStep(FieldDumper::Step * s);
+  
+  private:
+    ofstream idxos_;
+    int      nbins_;
+    double   range[2];
+    FILE*    filelist_;
+    
+    InfoOpts               opts_;
+    const FieldSelection & fselect_;
+  };
+
+}
+
+#endif
+
