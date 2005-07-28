5083f2beaeadffe5bd9bbb69ec2ae1e40d0cd86f
Ayla Khan <ayla@sci.utah.edu>
2005-07-28 23:46:25 +0000

    Regenerate Babel implementation files for NewPort test component.

--- trunk/src/CCA/Components/BabelTest/NewPort/NewPort_StringPort_Impl.cc
+++ trunk/src/CCA/Components/BabelTest/NewPort/NewPort_StringPort_Impl.cc
@@ -2,45 +2,48 @@
 // File:          NewPort_StringPort_Impl.cc
 // Symbol:        NewPort.StringPort-v1.0
 // Symbol Type:   class
-// Babel Version: 0.7.4
-// SIDL Created:  20040301 18:38:02 MST
-// Generated:     20040301 18:38:04 MST
+// Babel Version: 0.10.2
 // Description:   Server-side implementation for NewPort.StringPort
 // 
 // WARNING: Automatically generated; only changes within splicers preserved
 // 
-// babel-version = 0.7.4
-// source-line   = 15
-// source-url    = file:/home/sci/damevski/SCIRun/src/CCA/Components/BabelTest/NewPort/NewPort.sidl
+// babel-version = 0.10.2
 // 
 #include "NewPort_StringPort_Impl.hh"
 
 // DO-NOT-DELETE splicer.begin(NewPort.StringPort._includes)
-// Put additional includes or other arbitrary code here...
+// Insert-Code-Here {NewPort.StringPort._includes} (additional includes or code)
 // DO-NOT-DELETE splicer.end(NewPort.StringPort._includes)
 
-// user defined constructor
+// user-defined constructor.
 void NewPort::StringPort_impl::_ctor() {
   // DO-NOT-DELETE splicer.begin(NewPort.StringPort._ctor)
-  // add construction details here
+  // Insert-Code-Here {NewPort.StringPort._ctor} (constructor)
   // DO-NOT-DELETE splicer.end(NewPort.StringPort._ctor)
 }
 
-// user defined destructor
+// user-defined destructor.
 void NewPort::StringPort_impl::_dtor() {
   // DO-NOT-DELETE splicer.begin(NewPort.StringPort._dtor)
-  // add destruction details here
+  // Insert-Code-Here {NewPort.StringPort._dtor} (destructor)
   // DO-NOT-DELETE splicer.end(NewPort.StringPort._dtor)
 }
 
-// user defined static methods: (none)
+// static class initializer.
+void NewPort::StringPort_impl::_load() {
+  // DO-NOT-DELETE splicer.begin(NewPort.StringPort._load)
+  // Insert-Code-Here {NewPort.StringPort._load} (class initialization)
+  // DO-NOT-DELETE splicer.end(NewPort.StringPort._load)
+}
 
-// user defined non-static methods:
+// user-defined static methods: (none)
+
+// user-defined non-static methods:
 /**
  * Method:  getString[]
  */
 ::std::string
-NewPort::StringPort_impl::getString () 
+NewPort::StringPort_impl::getString ()
 throw () 
 
 {
@@ -51,16 +54,6 @@ throw ()
 
 
 // DO-NOT-DELETE splicer.begin(NewPort.StringPort._misc)
-// Put miscellaneous code here
+// Insert-Code-Here {NewPort.StringPort._misc} (miscellaneous code)
 // DO-NOT-DELETE splicer.end(NewPort.StringPort._misc)
 
-/**
- * ================= BEGIN UNREFERENCED METHOD(S) ================
- * The following code segment(s) belong to unreferenced method(s).
- * This can result from a method rename/removal in the SIDL file.
- * Move or remove the code in order to compile cleanly.
- */
-  // DO-NOT-DELETE splicer.begin(NewPort.StringPort.setService)
-  // insert implementation here
-  // DO-NOT-DELETE splicer.end(NewPort.StringPort.setService)
-// ================== END UNREFERENCED METHOD(S) =================
