8bc457772aa7f39d34d22633885889d6626770bb
Oren Livne <livne@sci.utah.edu>
2005-08-02 15:20:39 +0000

    Added some comments and started working on HypreDriver

--- CCA/Components/Solvers/HypreDriver.h
+++ CCA/Components/Solvers/HypreDriver.h
@@ -1,13 +1,48 @@
 /*--------------------------------------------------------------------------
- * File: HypreDriver.h
- *
- * class HypreDriver is a wrapper for calling Hypre solvers and
- * preconditioners. It contains all Hypre interface data types:
- * Struct, SStruct, ParCSR. Only one of them is activated per solver.
- *
- * Note: This interface is written for Hypre version 1.9.0b (released 2005)
- * or later.
- *--------------------------------------------------------------------------*/
+CLASS
+   HypreDriver
+   
+   Wrapper of a Hypre solver for a particular variable type.
+
+GENERAL INFORMATION
+
+   File: HypreDriver.h
+
+   Oren E. Livne
+   Department of Computer Science
+   University of Utah
+
+   Center for the Simulation of Accidental Fires and Explosions (C-SAFE)
+  
+   Copyright (C) 2005 SCI Group
+
+KEYWORDS
+   HypreDriver, HypreSolverParams, RefCounted, solve, Hypre_Struct,
+   Hypre_SStruct, HypreSolverAMR.
+
+DESCRIPTION
+   Class HypreDriver is a wrapper for calling Hypre solvers and
+   preconditioners. It contains all Hypre interface data types:
+   Struct (structured grid), SStruct (composite AMR grid), ParCSR (parallel
+   compressed sparse row representation of the matrix).
+   Only one of them is activated per solver.
+   The solver is called through the solve() function. This is also the
+   task-scheduled function in HypreSolverAMR::scheduleSolve() that is
+   called by Components/ICE/impICE.cc.
+  
+WARNING
+   * If we intend to use other Hypre system interfaces (e.g., IJ interface),
+   their data types (Matrix, Vector, etc.) should be added to the data
+   members of this class. Specific solvers deriving from HypreSolverGeneric
+   should have a specific Hypre interface they work with that exists in
+   HypreDriver.
+   * Each new variable type (CC, NC, etc.) and Hypre interface combination
+   requires its own makeLinearSystem() function. Currently only CC is
+   implemented for the pressure solver in implicit AMR ICE.
+   * This interface is written for Hypre 1.9.0b (released 2005). However,
+   it may still work with the Struct solvers in earlier Hypre versions (e.g., 
+   1.7.7).
+   --------------------------------------------------------------------------*/
 #ifndef Packages_Uintah_CCA_Components_Solvers_HypreDriver_h
 #define Packages_Uintah_CCA_Components_Solvers_HypreDriver_h
 
@@ -17,6 +52,7 @@
 // hypre includes
 #include <utilities.h>
 #include <HYPRE_struct_ls.h>
+#include <HYPRE_sstruct_ls.h>
 #include <krylov.h>
 
 namespace Uintah {
@@ -26,15 +62,8 @@ namespace Uintah {
 
   template<class Types>
     class HypreDriver : public RefCounted {
-    /*_____________________________________________________________________
-      class HypreDriver: solver wrapper for a specific variable type.
-      In this class we implement the actual AMR solver call. The class is
-      templated against the variable type: cell centered, node centered, etc.
-      NOTE: currently only CC is implemented for the pressure solver in 
-      implicit AMR ICE.
-      _____________________________________________________________________*/
-  public:
 
+  public:
     /*---------- Types ----------*/
 
     enum HypreInterface {       // Hypre system interface for the solver
@@ -88,23 +117,25 @@ namespace Uintah {
 
     HypreInterface        _hypreInterface; // Currently active type
 
-    /* Struct objects */
+    /* Hypre Struct interface objects */
     HYPRE_StructMatrix    _StructA;
     HYPRE_StructVector    _StructB;
     HYPRE_StructVector    _StructX;
 
-    /* SStruct objects */
+    /* Hypre SStruct interface objects */
     HYPRE_SStructMatrix   _SStructA;
     HYPRE_SStructVector   _SStructB;
     HYPRE_SStructVector   _SStructX;
     HYPRE_SStructGraph    _SStructGraph;
     
-    /* ParCSR objects */
+    /* Hypre ParCSR interface objects */
     HYPRE_ParCSRMatrix    _ParA;
     HYPRE_ParVector       _ParB;
     HYPRE_ParVector       _ParX;
 
-    void makeLinearSystem(void); // Depends on Types
+    /* Generating A,b,x; depends on Types */
+    void makeLinearSystemStruct(void);
+    void makeLinearSystemSStruct(void);
 
     /* Make these functions virtual or incorporate them into a generic
        solver setup function? */
