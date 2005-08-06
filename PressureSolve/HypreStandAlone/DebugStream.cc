b1b97c9beadf0989856392004853a26ae5791b1c
Oren Livne <livne@sci.utah.edu>
2005-08-06 04:21:30 +0000

    serialized beginning, added overflow()

--- PressureSolve/HypreStandAlone/DebugStream.cc
+++ PressureSolve/HypreStandAlone/DebugStream.cc
@@ -11,37 +11,83 @@ DebugBuf::~DebugBuf(void)
 
 int DebugBuf::overflow(int ch)
 {
-  if (owner->active()) {
-    return(*(owner->outstream) << (char)ch ? 0 : EOF);
-  }
-  return 0;
-}
-
-streamsize DebugBuf::xsputn (const char* s,
-                             streamsize num) {
-  if (owner->getLevel() < owner->getVerboseLevel()) {
-    if (s[num-1] == '\n')
-    {
+  //  cerr << " (xsputn begin) ";
+  //  cerr << "*owner = " << *owner << "\n";
+  if ((owner->getLevel() < owner->getVerboseLevel())
+      || (!owner->active())) {
+    if (ch == '\n') {
+      //      cerr << "HERE1\n";
       _lineBegin = true;
       // Set back level to verboseLevel so that stream will
       // print everything until the next explicit setLevel() call.
+      //      cerr << "HERE2\n";
       owner->setLevel(owner->getVerboseLevel());
+      owner->flush();
+      //      cerr << "HERE3\n";
     }
-    return num;
+    //    cerr << "(xsputn end1) ";
+    return 0;
   }
+  //  cerr << "HERE4\n";
   if (_lineBegin) {
+    //    cerr << "HERE5\n";
+    //    cerr << "lineHeader = " <<lineHeader()<<"\n";
     *(owner->outstream) << lineHeader();
+    //    cerr << "HERE6\n";
     _lineBegin = false;
   }
-  *(owner->outstream) << s;
-  if (s[num-1] == '\n')
-  {
+  if (ch == '\n') {
     _lineBegin = true;
     // Set back level to verboseLevel so that stream will
     // print everything until the next explicit setLevel() call.
     owner->setLevel(owner->getVerboseLevel());
+    owner->flush();
+    //    cerr << "HERE9\n";
   }
-  
+  //  cerr << "HERE7\n";
+  return(*(owner->outstream) << (char)ch ? 0 : EOF);
+  //  return 0;
+}
+
+streamsize DebugBuf::xsputn (const char* s,
+                             streamsize num) {
+  //  cerr << " (xsputn begin) ";
+  //  cerr << "*owner = " << *owner << "\n";
+  if ((owner->getLevel() < owner->getVerboseLevel())
+      || (!owner->active())) {
+    if ((num >= 1) && (s[num-1] == '\n')) {
+      //      cerr << "HERE1\n";
+      _lineBegin = true;
+      // Set back level to verboseLevel so that stream will
+      // print everything until the next explicit setLevel() call.
+      //      cerr << "HERE2\n";
+      owner->setLevel(owner->getVerboseLevel());
+      owner->flush();
+      //      cerr << "HERE3\n";
+    }
+    //    cerr << "(xsputn end1) ";
+    return num;
+  }
+  //  cerr << "HERE4\n";
+  if (_lineBegin) {
+    //    cerr << "HERE5\n";
+    //    cerr << "lineHeader = " <<lineHeader()<<"\n";
+    *(owner->outstream) << lineHeader();
+    //    cerr << "HERE6\n";
+    _lineBegin = false;
+  }
+  //  cerr << "HERE7\n";
+  *(owner->outstream) << s;
+  //  cerr << "HERE8\n";
+  if ((num >= 1) && (s[num-1] == '\n')) {
+    _lineBegin = true;
+    // Set back level to verboseLevel so that stream will
+    // print everything until the next explicit setLevel() call.
+    owner->setLevel(owner->getVerboseLevel());
+    owner->flush();
+    //    cerr << "HERE9\n";
+  }
+  //  cerr << "(xsputn end2) ";  
   return num;
 }
 
