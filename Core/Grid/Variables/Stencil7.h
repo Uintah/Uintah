087d63b41045177db251906e23c25827d2ea33d7
Steven G. Parker <sparker@sci.utah.edu>
2005-08-11 16:46:42 +0000

    Added operator[] to access elements by an index

--- Core/Grid/Variables/Stencil7.h
+++ Core/Grid/Variables/Stencil7.h
@@ -2,13 +2,25 @@
 #ifndef Packages_Uintah_Core_Grid_Stencil7_h
 #define Packages_Uintah_Core_Grid_Stencil7_h
 
+#include <Core/Util/FancyAssert.h>
+
 namespace Uintah {
   class TypeDescription;
   struct Stencil7 {
+    // The order of this is designed to match the order of faces in Patch
+    // Do not change it!
+    //     -x +x -y +y -z +z
+    double  w, e, s, n, b, t;
     // diagonal term
     double p;
-    //     +x -x +y -y +z -z
-    double  e, w, n, s, t, b;
+    double& operator=(int index) {
+      ASSERTRANGE(index, 0, 7);
+      return (&w)[index];
+    }
+    const double& operator=(int index) const {
+      ASSERTRANGE(index, 0, 7);
+      return (&w)[index];
+    }
   };
 }
 
