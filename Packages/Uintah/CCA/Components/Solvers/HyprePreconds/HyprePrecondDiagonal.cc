01754f8304701dec17ed27117d6107d996ecb126
Oren Livne <livne@sci.utah.edu>
2005-08-22 18:07:31 +0000

    Moved solvers, preconds to subdirs

---
+++ Packages/Uintah/CCA/Components/Solvers/HyprePreconds/HyprePrecondDiagonal.cc
@@ -0,0 +1,69 @@
+//--------------------------------------------------------------------------
+// File: HyprePrecondDiagonal.cc
+// 
+// Hypre Diagonal (geometric multigrid #2) preconditioner.
+//--------------------------------------------------------------------------
+
+#include <Packages/Uintah/CCA/Components/Solvers/HyprePrecondDiagonal.h>
+#include <Packages/Uintah/CCA/Components/Solvers/HypreSolverBase.h>
+#include <Packages/Uintah/CCA/Components/Solvers/HypreDriver.h>
+#include <Packages/Uintah/Core/Exceptions/ProblemSetupException.h>
+#include <Packages/Uintah/CCA/Components/Solvers/HypreSolverParams.h>
+#include <Packages/Uintah/Core/Parallel/ProcessorGroup.h>
+#include <Core/Util/DebugStream.h>
+
+using namespace Uintah;
+//__________________________________
+//  To turn on normal output
+//  setenv SCI_DEBUG "HYPRE_DOING_COUT:+"
+
+static DebugStream cout_doing("HYPRE_DOING_COUT", false);
+
+Priorities
+HyprePrecondDiagonal::initPriority(void)
+  //___________________________________________________________________
+  // Function HyprePrecondDiagonal::initPriority~
+  // Set the Hypre interfaces that Diagonal can work with. Currently, only
+  // the Struct interface is supported here. The vector of interfaces
+  // is sorted by descending priority.
+  //___________________________________________________________________
+{
+  Priorities priority;
+  priority.push_back(HypreStruct);
+  return priority;
+}
+
+void
+HyprePrecondDiagonal::setup(void)
+  //___________________________________________________________________
+  // Function HyprePrecondDiagonal::setup~
+  // Set up the preconditioner object. After this function call, a
+  // Hypre solver can use the preconditioner.
+  //___________________________________________________________________
+{
+  const HypreDriver* driver = _solver->getDriver();
+  //  const HypreSolverParams* params = driver->getParams();
+  const HypreInterface& interface = driver->getInterface();
+  if (interface == HypreStruct) {
+#ifdef HYPRE_USE_PTHREADS
+    for (i = 0; i < hypre_NumThreads; i++)
+      precond[i] = NULL;
+#else
+    _precond = NULL;
+#endif
+    _precond = (HYPRE_PtrToSolverFcn)HYPRE_StructDiagScale;
+    _pcsetup = (HYPRE_PtrToSolverFcn)HYPRE_StructDiagScaleSetup;
+  }
+}
+
+HyprePrecondDiagonal::~HyprePrecondDiagonal(void)
+  //___________________________________________________________________
+  // HyprePrecondDiagonal destructor~
+  // Destroy the Hypre objects associated with the preconditioner.
+  //___________________________________________________________________
+{
+  const HypreDriver* driver = _solver->getDriver();
+  const HypreInterface& interface = driver->getInterface();
+  if (interface == HypreStruct) {
+  }
+}
---
+++ Packages/Uintah/CCA/Components/Solvers/HyprePreconds/HyprePrecondPFMG.h
@@ -0,0 +1,61 @@
+#ifndef Packages_Uintah_CCA_Components_Solvers_HyprePrecondPFMG_h
+#define Packages_Uintah_CCA_Components_Solvers_HyprePrecondPFMG_h
+
+/*--------------------------------------------------------------------------
+CLASS
+   HyprePrecondPFMG
+   
+   A Hypre PFMG (geometric multigrid #2) preconditioner.
+
+GENERAL INFORMATION
+
+   File: HyprePrecondPFMG.h
+
+   Oren E. Livne
+   Department of Computer Science
+   University of Utah
+
+   Center for the Simulation of Accidental Fires and Explosions (C-SAFE)
+  
+   Copyright (C) 2005 SCI Group
+
+KEYWORDS
+   HypreSolverBase, Precond, HypreSolverBase, HypreSolverParams.
+
+DESCRIPTION
+   Class HyprePrecondPFMG sets up and destroys the Hypre PFMG
+   preconditioner to be used with Hypre solvers. PFMG is the Parallel
+   Full Multigrid solver that uses geometric interpolation and
+   restriction transfers and well suited for Poisson or a diffusion
+   operator with a smooth diffusion coefficient.
+   PFMG preconditioner is often used with CG or GMRES.
+  
+WARNING
+   Works with Hypre Struct interface only.
+   --------------------------------------------------------------------------*/
+
+#include <Packages/Uintah/CCA/Components/Solvers/HyprePrecondBase.h>
+
+namespace Uintah {
+  
+  //---------- Types ----------
+  
+  class HyprePrecondPFMG : public HyprePrecondBase {
+
+    //========================== PUBLIC SECTION ==========================
+  public:
+  
+    HyprePrecondPFMG(void) : HyprePrecondBase(initPriority()) {}
+    virtual ~HyprePrecondPFMG(void);
+
+    virtual void setup(void);
+    
+    //========================== PROTECTED SECTION ==========================
+  protected:
+    static Priorities initPriority(void);
+
+  }; // end class HyprePrecondPFMG
+
+} // end namespace Uintah
+
+#endif // Packages_Uintah_CCA_Components_Solvers_HyprePrecondPFMG_h
