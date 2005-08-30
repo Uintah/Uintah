e894015c99229efa8632422eff8e6797df6a79d0
John Schmidt <jas@sci.utah.edu>
2005-08-30 22:27:29 +0000

    Add SwitchingCriteria directory.  Add two switching criteria: None (no switch between components), and TimestepNumber which switches based on the timestep number of the running simulation.

---
+++ src/Packages/Uintah/CCA/Components/SwitchingCriteria/SwitchingCriteriaFactory.cc
@@ -0,0 +1,44 @@
+#include <Packages/Uintah/CCA/Components/SwitchingCriteria/SwitchingCriteriaFactory.h>
+#include <Packages/Uintah/CCA/Components/SwitchingCriteria/None.h>
+#include <Packages/Uintah/CCA/Components/SwitchingCriteria/TimestepNumber.h>
+#include <Packages/Uintah/Core/Parallel/ProcessorGroup.h>
+#include <Packages/Uintah/Core/Exceptions/ProblemSetupException.h>
+#include <Packages/Uintah/Core/ProblemSpec/ProblemSpec.h>
+
+
+#include <iostream>
+#include <string>
+using std::cerr;
+using std::endl;
+using std::string;
+
+using namespace Uintah;
+
+SwitchingCriteria* SwitchingCriteriaFactory::create(ProblemSpecP& ps,
+                                                    const ProcessorGroup* world)
+{
+  string criteria("none");
+  ProblemSpecP switch_ps = ps->findBlock("SwitchCriteria");
+  if (switch_ps) {
+    map<string,string> attributes;
+    switch_ps->getAttributes(attributes);
+    criteria = attributes["type"];
+  } else {
+    cerr << "No switch criteria found" << endl;
+  }
+
+  SwitchingCriteria* switch_criteria = 0;
+  if (criteria == "none" || criteria == "None" || criteria == "NONE") {
+    switch_criteria = new None(switch_ps);
+  } else if (criteria == "timestep" || criteria == "Timestep" || 
+             criteria == "TIMESTEP")  {
+    switch_criteria = new TimestepNumber(switch_ps);
+  } else {
+    throw ProblemSetupException("Unknown switching criteria."
+                                "Valid criteria: None",
+                                __FILE__, __LINE__);
+  }
+
+  return switch_criteria;
+
+}
