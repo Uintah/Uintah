c36e92f6eb0b21ff2405138741f6746895cfb208
Ayla Khan <ayla@sci.utah.edu>
2005-08-31 21:12:06 +0000

    Fix some compile warnings.

--- testprograms/Component/subsetter/pingpong.cc
+++ testprograms/Component/subsetter/pingpong.cc
@@ -59,12 +59,12 @@ using namespace SCIRun;
 
 void usage(char* progname)
 {
-    cerr << "usage: " << progname << " [options]\n";
-    cerr << "valid options are:\n";
-    cerr << "  -server  - server process\n";
-    cerr << "  -client URL  - client process\n";
-    cerr << "\n";
-    exit(1);
+  cerr << "usage: " << progname << " [options]\n";
+  cerr << "valid options are:\n";
+  cerr << "  -server  - server process\n";
+  cerr << "  -client URL  - client process\n";
+  cerr << "\n";
+  exit(1);
 }
 
 void init(SSIDL::array1<int>& a, int s, int f)
@@ -76,137 +76,138 @@ void init(SSIDL::array1<int>& a, int s, int f)
 
 int main(int argc, char* argv[])
 {
-    using std::string;
+  using std::string;
 
-    using PingPong_ns::PingPong_impl;
-    using PingPong_ns::PingPong;
+  using PingPong_ns::PingPong_impl;
+  using PingPong_ns::PingPong;
 
-    SSIDL::array1<int> arr;
+  SSIDL::array1<int> arr;
 
-    int myrank = 0;
-    int mysize = 0;
-    int arrsize, sta, fin;
+  int myrank = 0;
+  int mysize = 0;
+  int arrsize, sta, fin;
 
-    try {
-        PIDL::initialize();
+  try {
+    PIDL::initialize();
         
-        MPI_Init(&argc,&argv);
+    MPI_Init(&argc,&argv);
 
-	bool client=false;
-	bool server=false;
-	vector <URL> server_urls;
-	int reps=1;
+    bool client=false;
+    bool server=false;
+    vector <URL> server_urls;
+    int reps=1;
 
-        MPI_Comm_size(MPI_COMM_WORLD,&mysize);
-        MPI_Comm_rank(MPI_COMM_WORLD,&myrank);
+    MPI_Comm_size(MPI_COMM_WORLD,&mysize);
+    MPI_Comm_rank(MPI_COMM_WORLD,&myrank);
 
-	for(int i=1;i<argc;i++){
-	    string arg(argv[i]);
-	    if(arg == "-server"){
-		if(client)
-		    usage(argv[0]);
-		server=true;
-	    } else if(arg == "-client"){
-		if(server)
-		    usage(argv[0]);
-                i++;
-                for(;i<argc;i++){		
-                  string url_arg(argv[i]);
-		  server_urls.push_back(url_arg);
-                }
-		client=true;
-                break;
-	    } else if(arg == "-reps"){
-		if(++i>=argc)
-		    usage(argv[0]);
-		reps=atoi(argv[i]);
-	    } else {
-		usage(argv[0]);
-	    }
-	}
-	if(!client && !server)
-	    usage(argv[0]);
-
-	if(server) {
-	  PingPong_impl* pp=new PingPong_impl;
-
-          //Set up server's requirement of the distribution array 
-	  Index** dr = new Index* [1]; 
-	  dr[0] = new Index(myrank,99,mysize);
-	  MxNArrayRep* arrr = new MxNArrayRep(1,dr);
-	  pp->setCalleeDistribution("D",arrr);
-          std::cerr << "setCalleeDistribution completed\n";
-
-	  cerr << "Waiting for pingpong connections...\n";
-          /*Reduce all URLs and have root print them out*/
-          typedef char urlString[100] ;
-          urlString s;
-	  strcpy(s, pp->getURL().getString().c_str());
-	  urlString *buf;
-          if(myrank==0){
-	    buf=new urlString[mysize];
-          }
-	  MPI_Gather(s, 100, MPI_CHAR, buf, 100, MPI_CHAR, 0, MPI_COMM_WORLD);
-          if(myrank==0)
-	    for(int i=0; i<mysize; i++) 
-	      cerr << buf[i] << '\n';
-
-	} else {
-
-          //Creating a multiplexing proxy from all the URLs
-	  Object::pointer obj=PIDL::objectFrom(server_urls,mysize,myrank);
-	  PingPong::pointer pp=pidl_cast<PingPong::pointer>(obj);
-	  if(pp.isNull()){
-	    cerr << "pp_isnull\n";
-	    abort();
-	  }
-
-	  //Set up the array and the timer  
-	  double stime=Time::currentSeconds();
-          cerr << mysize << ", " << myrank << "\n";
-          arrsize = 100 / mysize;
-          sta = myrank * arrsize;
-          fin = (myrank * arrsize) + arrsize - 1;
-          cerr << arrsize << "::" << sta << " -- " << fin << "\n";
-          init(arr,sta,fin+1);
-
-	  //Inform everyone else of my distribution
-          //(this sends a message to all the callee objects)
-          Index** dr = new Index* [1];
-          dr[0] = new Index(sta,fin,1);
-          MxNArrayRep* arrr = new MxNArrayRep(1,dr);
-	  pp->setCallerDistribution("D",arrr); 
-	  std::cerr << "setCallerDistribution completed\n";
-
-	  pp->pingpong(arr);
-	  for(int j=1;j<server_urls.size();j++) {
-	    /*Create all possible subsets*/
-	    ::std::cout << "Subsetting from " << myrank+1 << " to " << j << "\n";
-	    pp->createSubset(myrank+1,j);
-	    int m=pp->pingpong(arr);
-	    ::std::cout << "pingpong finished from " << myrank+1 << " to " << j << "\n";
-	  }
-	  double dt=Time::currentSeconds()-stime;
-	  cerr << reps << " reps in " << dt << " seconds\n";
-	  double us=dt/reps*1000*1000;
-	  cerr << us << " us/rep\n";
-	}
-    } catch(const MalformedURL& e) {
-	cerr << "pingpong.cc: Caught MalformedURL exception:\n";
-	cerr << e.message() << '\n';
-    } catch(const Exception& e) {
-	cerr << "pingpong.cc: Caught exception:\n";
-	cerr << e.message() << '\n';
-	abort();
-    } catch(...) {
-	cerr << "Caught unexpected exception!\n";
-        getchar();
-	abort();
+    for(int i=1;i<argc;i++){
+      string arg(argv[i]);
+      if(arg == "-server"){
+        if(client)
+          usage(argv[0]);
+        server=true;
+      } else if(arg == "-client"){
+        if(server)
+          usage(argv[0]);
+        i++;
+        for(;i<argc;i++){       
+          string url_arg(argv[i]);
+          server_urls.push_back(url_arg);
+        }
+        client=true;
+        break;
+      } else if(arg == "-reps"){
+        if(++i>=argc)
+          usage(argv[0]);
+        reps=atoi(argv[i]);
+      } else {
+        usage(argv[0]);
+      }
     }
-    PIDL::serveObjects();
-    PIDL::finalize();
-    MPI_Finalize();
+    if(!client && !server)
+      usage(argv[0]);
 
-    return 0;
+    if(server) {
+      PingPong_impl* pp=new PingPong_impl;
+
+      //Set up server's requirement of the distribution array 
+      Index** dr = new Index* [1]; 
+      dr[0] = new Index(myrank,99,mysize);
+      MxNArrayRep* arrr = new MxNArrayRep(1,dr);
+      pp->setCalleeDistribution("D",arrr);
+      std::cerr << "setCalleeDistribution completed\n";
+
+      cerr << "Waiting for pingpong connections...\n";
+      /*Reduce all URLs and have root print them out*/
+      typedef char urlString[100] ;
+      urlString s;
+      strcpy(s, pp->getURL().getString().c_str());
+      urlString *buf;
+      if(myrank==0){
+        buf=new urlString[mysize];
+      }
+      MPI_Gather(s, 100, MPI_CHAR, buf, 100, MPI_CHAR, 0, MPI_COMM_WORLD);
+      if(myrank==0)
+        for(int i=0; i<mysize; i++) 
+          cerr << buf[i] << '\n';
+
+    } else {
+
+      //Creating a multiplexing proxy from all the URLs
+      Object::pointer obj=PIDL::objectFrom(server_urls,mysize,myrank);
+      PingPong::pointer pp=pidl_cast<PingPong::pointer>(obj);
+      if(pp.isNull()){
+        cerr << "pp_isnull\n";
+        abort();
+      }
+
+      //Set up the array and the timer  
+      double stime=Time::currentSeconds();
+      cerr << mysize << ", " << myrank << "\n";
+      arrsize = 100 / mysize;
+      sta = myrank * arrsize;
+      fin = (myrank * arrsize) + arrsize - 1;
+      cerr << arrsize << "::" << sta << " -- " << fin << "\n";
+      init(arr,sta,fin+1);
+
+      //Inform everyone else of my distribution
+      //(this sends a message to all the callee objects)
+      Index** dr = new Index* [1];
+      dr[0] = new Index(sta,fin,1);
+      MxNArrayRep* arrr = new MxNArrayRep(1,dr);
+      pp->setCallerDistribution("D",arrr); 
+      std::cerr << "setCallerDistribution completed\n";
+
+      pp->pingpong(arr);
+      for (unsigned int j = 1; j < server_urls.size(); j++) {
+        // Create all possible subsets
+        ::std::cout << "Subsetting from " << myrank+1 << " to " << j << "\n";
+        pp->createSubset(myrank+1,j);
+        // unused variable
+        //int m=pp->pingpong(arr);
+        ::std::cout << "pingpong finished from " << myrank+1 << " to " << j << "\n";
+      }
+      double dt=Time::currentSeconds()-stime;
+      cerr << reps << " reps in " << dt << " seconds\n";
+      double us=dt/reps*1000*1000;
+      cerr << us << " us/rep\n";
+    }
+  } catch(const MalformedURL& e) {
+    cerr << "pingpong.cc: Caught MalformedURL exception:\n";
+    cerr << e.message() << '\n';
+  } catch(const Exception& e) {
+    cerr << "pingpong.cc: Caught exception:\n";
+    cerr << e.message() << '\n';
+    abort();
+  } catch(...) {
+    cerr << "Caught unexpected exception!\n";
+    getchar();
+    abort();
+  }
+  PIDL::serveObjects();
+  PIDL::finalize();
+  MPI_Finalize();
+
+  return 0;
 }
 
