12d76bfb7248b32408d98cdd01a7e2a5b8cd55f4
Jim Guilkey <guilkey@sci.utah.edu>
2005-08-31 21:55:18 +0000

    Adding a switching criteria based on the temperature of the PBX.  Currently still the same as the Timestep version.  Trying to be more like Oren.  :)

---
+++ Packages/Uintah/CCA/Components/SwitchingCriteria/PBXTemperature.cc
@@ -0,0 +1,53 @@
+#include <Packages/Uintah/CCA/Components/SwitchingCriteria/PBXTemperature.h>
+#include <Packages/Uintah/Core/ProblemSpec/ProblemSpec.h>
+#include <Packages/Uintah/Core/Grid/Task.h>
+#include <Packages/Uintah/Core/Grid/Variables/VarTypes.h>
+#include <Packages/Uintah/CCA/Ports/Scheduler.h>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+using namespace Uintah;
+
+PBXTemperature::PBXTemperature(ProblemSpecP& ps)
+{
+  ps->require("timestep",d_timestep);
+  cout << "timestep = " << d_timestep << endl;
+}
+
+PBXTemperature::~PBXTemperature()
+{
+}
+
+void PBXTemperature::problemSetup(const ProblemSpecP& ps, 
+                                  SimulationStateP& state)
+{
+  d_sharedState = state;
+}
+
+void PBXTemperature::scheduleSwitchTest(const LevelP& level, SchedulerP& sched)
+{
+  Task* t = scinew Task("switchTest", this, &PBXTemperature::switchTest);
+
+  t->computes(d_sharedState->get_switch_label(), level.get_rep());
+  sched->addTask(t, level->eachPatch(),d_sharedState->allMaterials());
+}
+
+void PBXTemperature::switchTest(const ProcessorGroup* group,
+                                const PatchSubset* patches,
+                                const MaterialSubset* matls,
+                                DataWarehouse* old_dw,
+                                DataWarehouse* new_dw)
+{
+  double sw = 0;
+
+  unsigned int time_step = d_sharedState->getCurrentTopLevelTimeStep();
+  if (time_step == d_timestep)
+    sw = 1;
+  else
+    sw = 0;
+
+  max_vartype switch_condition(sw);
+  new_dw->put(switch_condition,d_sharedState->get_switch_label(),getLevel(patches));
+}
