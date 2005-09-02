ff65200cc0f59501171e88f9e0ce26c48f6d5876
J. Davison de St. Germain <dav@sci.utah.edu>
2005-09-02 00:09:49 +0000

    Add all libraries that are needed for linking.

--- src/Packages/Uintah/CCA/Components/SwitchingCriteria/sub.mk
+++ src/Packages/Uintah/CCA/Components/SwitchingCriteria/sub.mk
@@ -6,16 +6,21 @@ SRCDIR   := Packages/Uintah/CCA/Components/SwitchingCriteria
 
 SRCS     += \
 	$(SRCDIR)/SwitchingCriteriaFactory.cc \
-	$(SRCDIR)/None.cc   \
-	$(SRCDIR)/TimestepNumber.cc \
+	$(SRCDIR)/None.cc                     \
+	$(SRCDIR)/TimestepNumber.cc           \
 	$(SRCDIR)/PBXTemperature.cc
 
 PSELIBS := \
 	Core/Exceptions                  \
+	Core/Util                        \
+	Packages/Uintah/CCA/Components/MPM \
 	Packages/Uintah/CCA/Ports        \
+	Packages/Uintah/Core/Disclosure  \
 	Packages/Uintah/Core/Exceptions  \
+	Packages/Uintah/Core/Grid        \
+	Packages/Uintah/Core/Labels      \
 	Packages/Uintah/Core/Parallel    \
-	Packages/Uintah/Core/Util    \
+	Packages/Uintah/Core/Util        \
 	Packages/Uintah/Core/ProblemSpec 
 
 LIBS := $(XML_LIBRARY) 
