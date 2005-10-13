884734b7ddb56f6298dadd8a3b9f0a644c0b5d8e
J. Davison de St. Germain <dav@sci.utah.edu>
2005-10-13 20:28:10 +0000

    Indented

--- src/include/sgi_stl_warnings_on.h
+++ src/include/sgi_stl_warnings_on.h
@@ -32,10 +32,10 @@
 //
 
 #if defined(__sgi) && !defined(__GNUC__) && (_MIPS_SIM != _MIPS_SIM_ABI32)
-#pragma reset woff 1110
-#pragma reset woff 1209
-#pragma reset woff 1424
-#pragma reset woff 1506
-#pragma reset woff 3201
-#pragma reset woff 3625
+#  pragma reset woff 1110
+#  pragma reset woff 1209
+#  pragma reset woff 1424
+#  pragma reset woff 1506
+#  pragma reset woff 3201
+#  pragma reset woff 3625
 #endif
