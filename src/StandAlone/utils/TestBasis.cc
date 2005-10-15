673073c9773bf1887bd0c8eea1184ab5a47c25a9
Frank Sachse <sachse@sci.utah.edu>
2005-10-15 21:00:19 +0000

    Extend for testing of CurveMesh

--- src/StandAlone/utils/TestBasis.cc
+++ src/StandAlone/utils/TestBasis.cc
@@ -68,43 +68,37 @@ void TestCrvLinearLgn()
 
   typedef CurveMesh<CrvLinearLgn<Point> > MESH;
   MESH *mesh = new MESH();
-  cerr<<"!\n";
 
   CrvLinearLgn<Point> u;
   MESH::Node::array_type n;
   n.resize(u.number_of_vertices());
-  cerr<<"!\n";
 
   for(int i=0; i<u.number_of_vertices(); i++) {
     Point p(u.unit_vertices[i][0]+1, u.unit_vertices[i][0]+2, u.unit_vertices[i][0]+3);
     mesh->add_point(p);
     n[i]=i;
-    cerr<<i<<"\n";
   }
   MESH::Elem::index_type ei=mesh->add_elem(n); 
 
   cerr<<"Element index: " << ei << "\n"; 
 
   vector<double> coords;
-  coords.push_back(0.5);
+  coords.push_back(0.2);
   Point p;
-
-  //  MESH::CellData e(*mesh,0);
   
   mesh->interpolate(p, coords, 0);
 
   //  if (sqrt(pow(p.x()-coords[0],2.0)+pow(p.y()-coords[1],2.0)+pow(p.z()-coords[1],2.0))>1e-7)    
-  cerr << "coords " << coords[0] << "=>" << p << endl;
+   cerr << "Transform L->G " << coords[0] << " => " << p << endl;
 
-  vector<double> lc;
+  vector<double> lc(u.domain_dimension());
   bool rc=mesh->get_coords(lc, p, 0);
-  cerr << rc<<endl;
+  cerr << "Transform G->L " << p << " => ";
   if (rc) 
-    cerr << "coords " << lc[0] << endl;
+    cerr << lc[0] << endl;
   else
-    cerr << "coords " << p.x() <<", " << p.y() <<", " << p.z() << " not found" << endl;
-    
-
+    cerr << " not found" << endl;
+  
   typedef TetLinearLgn<double>  DatBasis;
   typedef GenericField<MESH, DatBasis, vector<double> > TVField;
   TVField *field = scinew TVField(mesh);
@@ -128,11 +122,9 @@ void TestTetLinearLgn()
   TetLinearLgn<Point> u;
   MESH::Node::array_type n;
   n.resize(u.number_of_vertices());
-  cerr<<"!\n";
-
-  for(int i=0; i<u.number_of_vertices(); i++) {
-    cerr<<i<<"\n";
-    Point p(u.unit_vertices[i][0]+1, u.unit_vertices[i][1]+1, u.unit_vertices[i][2]+1);
+ 
+ for(int i=0; i<u.number_of_vertices(); i++) {
+    Point p(u.unit_vertices[i][0]+1, u.unit_vertices[i][1]+2, u.unit_vertices[i][2]+3);
     mesh->add_point(p);
     n[i]=i;
   }
@@ -146,22 +138,20 @@ void TestTetLinearLgn()
   coords.push_back(.2);
   Point p;
 
-  //  MESH::ElemData e(*mesh,0);
-  
   mesh->interpolate(p, coords, 0);
 
   //  if (sqrt(pow(p.x()-coords[0],2.0)+pow(p.y()-coords[1],2.0)+pow(p.z()-coords[1],2.0))>1e-7)    
-  cerr << "coords " << coords[0] <<", " << coords[1] <<", " << coords[2] << "=>" << p << endl;
+  cerr << "Transform L->G " << coords[0] <<", " << coords[1] <<", " << coords[2] << " => " << p << endl;
 
-  vector<double> lc;
+  vector<double> lc(u.domain_dimension());
+  
   bool rc=mesh->get_coords(lc, p, 0);
-  cerr << rc<<endl;
+  cerr << "Transform G->L " << p << " => ";
   if (rc) 
-    cerr << "coords " << lc[0] <<", " << lc[1] <<", " << lc[2] << endl;
+    cerr << lc[0] <<", " << lc[1] <<", " << lc[2] << endl;
   else
-    cerr << "coords " << p.x() <<", " << p.y() <<", " << p.z() << " not found" << endl;
+    cerr << " not found" << endl;
     
-
   typedef TetLinearLgn<double>  DatBasis;
   typedef GenericField<MESH, DatBasis, vector<double> > TVField;
   TVField *field = scinew TVField(mesh);
