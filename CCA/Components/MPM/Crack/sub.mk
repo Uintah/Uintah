7b3258e8e324d6a23a7b60b9c63fe1c9ba5f2829
John Schmidt <jas@sci.utah.edu>
2005-10-22 03:54:27 +0000

    Remove the empty Fracture directory. Add different crack structures: quad, curved quad, arc, elliptic, partial elliptic, triangular, and null.  Implement a general interface to reading in cracks which will replace the functions in ReadAndDiscretizeCracks.cc.

--- CCA/Components/MPM/Crack/sub.mk
+++ CCA/Components/MPM/Crack/sub.mk
@@ -4,6 +4,15 @@ SRCDIR   := Packages/Uintah/CCA/Components/MPM/Crack
 
 SRCS     += \
 	$(SRCDIR)/ReadAndDiscretizeCracks.cc \
+	$(SRCDIR)/CrackGeometryFactory.cc \
+	$(SRCDIR)/CrackGeometry.cc \
+	$(SRCDIR)/NullCrack.cc \
+	$(SRCDIR)/QuadCrack.cc \
+	$(SRCDIR)/CurvedQuadCrack.cc \
+	$(SRCDIR)/TriangularCrack.cc \
+	$(SRCDIR)/ArcCrack.cc \
+	$(SRCDIR)/EllipticCrack.cc \
+	$(SRCDIR)/PartialEllipticCrack.cc \
 	$(SRCDIR)/ParticleNodePairVelocityField.cc \
 	$(SRCDIR)/CrackSurfaceContact.cc \
 	$(SRCDIR)/FractureParametersCalculation.cc \
