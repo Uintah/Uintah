47ec97c93d0b0e6efcbe1a9e5385910628ad682c
Jeroen Stinstra <jeroen@sci.utah.edu>
2005-11-07 20:28:34 +0000

    bug fix

--- src/Dataflow/GUI/CreateString.tcl
+++ src/Dataflow/GUI/CreateString.tcl
@@ -8,15 +8,12 @@ itcl_class SCIRun_String_CreateString {
 
     method set_defaults {} {
         global $this-inputstring
-        global $this-get-inputstring
         set $this-inputstring ""
-        set $this-get-inputstring "$this update_text"
     }
 
     method ui {} {
 
         global $this-inputstring
-        global $this-get-inputstring
         set w .ui[modname]
         if {[winfo exists $w]} {
             return
@@ -31,8 +28,6 @@ itcl_class SCIRun_String_CreateString {
             -labeltext "String Contents" 
         $w.f.str insert end [set $this-inputstring]
         pack $w.f.str -fill both -expand yes
-
-        set $this-inputstring "$this update_text"
 
         makeSciButtonPanel $w $w $this
         moveToCursor $w
