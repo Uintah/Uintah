e2366740e7a0c900cc2f7ba96ea9edbf807884fd
Frank Sachse <sachse@sci.utah.edu>
2005-12-12 23:25:09 +0000

    Upgrade BuildFEMatrix part 1

--- src/Packages/BioPSE/Core/Algorithms/NumApproximation/sub.mk
+++ src/Packages/BioPSE/Core/Algorithms/NumApproximation/sub.mk
@@ -33,10 +33,6 @@ include $(SCIRUN_SCRIPTS)/smallso_prologue.mk
 SRCDIR   := Packages/BioPSE/Core/Algorithms/NumApproximation
 
 SRCS += \
-	$(SRCDIR)/BuildFEMatrix.cc \
-	$(SRCDIR)/BuildHexFEMatrix.cc \
-	$(SRCDIR)/BuildTriFEMatrix.cc \
-	$(SRCDIR)/ReferenceElement.cc \
 
 PSELIBS := Core/Datatypes Core/Persistent Core/Exceptions Core/Containers \
 	Core/Thread Core/Geometry Core/Geom Core/GuiInterface \
