cffde96218d0afda36861fe4dfb34426822a6d25
Marty Cole <mjc@sci.utah.edu>
2005-12-13 15:44:57 +0000

    fix dynamic compilation

--- src/Packages/BioPSE/Dataflow/Modules/Forward/SetupFEMatrix.h
+++ src/Packages/BioPSE/Dataflow/Modules/Forward/SetupFEMatrix.h
@@ -62,213 +62,223 @@ namespace BioPSE {
 #define DEBUG 0
 
 #ifdef DEBUG
-  using std::cerr;
+using std::cerr;
 #endif
 
-  class SetupFEMatrixParam 
+class SetupFEMatrixParam 
+{
+public:
+  SetupFEMatrixParam() :
+    UseCond_(-1),
+    UseBasis_(-1),
+    gen_(-1),
+    nprocessors_(1) 
   {
-  public:
-    SetupFEMatrixParam() :
-      UseCond_(-1),
-      UseBasis_(-1),
-      gen_(-1),
-      nprocessors_(1) 
-      {
-      };
-
-      FieldHandle fieldH_;
-      int UseCond_;
-      int UseBasis_;
-      int gen_;
-      int nprocessors_;
   };
+
+  FieldHandle fieldH_;
+  int UseCond_;
+  int UseBasis_;
+  int gen_;
+  int nprocessors_;
+};
 
         
-  class SetupFEMatrixAlgo : public DynamicAlgoBase
-  {
-  public:
-    virtual MatrixHandle execute(const SetupFEMatrixParam& param) = 0;
+class SetupFEMatrixAlgo : public DynamicAlgoBase
+{
+public:
+  virtual MatrixHandle execute(const SetupFEMatrixParam& param) = 0;
 
-    //! support the dynamically compiled algorithm concept
-    static CompileInfoHandle get_compile_info(const TypeDescription *ftd,
-					      const TypeDescription *mtd,
-					      const TypeDescription *btd,
-					      const TypeDescription *dtd);
-  };
+  //! support the dynamically compiled algorithm concept
+  static CompileInfoHandle get_compile_info(const TypeDescription *ftd,
+					    const TypeDescription *mtd,
+					    const TypeDescription *btd,
+					    const TypeDescription *dtd);
+};
 
-  template<class FIELD>
-    void create_conductivities(FIELD, ConstantBasis<Tensor>, const SetupFEMatrixParam&, vector<pair<string, Tensor> >&)
-  {
-  };
+template<class FIELD>
+void create_conductivities(FIELD, ConstantBasis<Tensor>, 
+			   const SetupFEMatrixParam&, 
+			   vector<pair<string, Tensor> >&)
+{
+};
 
-  template<class FIELD> 
-    void create_conductivities(FIELD *pField, ConstantBasis<int>, const SetupFEMatrixParam& SFP, vector<pair<string, Tensor> > &tens)
+template<class FIELD> 
+void create_conductivities(FIELD *pField, ConstantBasis<int>, 
+			   const SetupFEMatrixParam& SFP, 
+			   vector<pair<string, Tensor> > &tens)
+{
+  if (!SFP.UseCond_ || !SFP.fieldH_->get_property("conductivity_table", tens)) 
   {
-    if (!SFP.UseCond_ || !SFP.fieldH_->get_property("conductivity_table", tens)) {
-      int maxf=*max_element(pField->fdata().begin(), pField->fdata().end()); 
+    unsigned int maxf = *max_element(pField->fdata().begin(), 
+				     pField->fdata().end()); 
       
-      vector<double> t(6);
-      t[0] = t[3] = t[5] = 1;
-      t[1] = t[2] = t[4] = 0;
-      Tensor ten(t);
-      for (unsigned int i = 0; i <= maxf; i++) 
-	tens.push_back(pair<string, Tensor>(to_string((int)i), ten));
-    }
-  }; 
+    vector<double> t(6);
+    t[0] = t[3] = t[5] = 1;
+    t[1] = t[2] = t[4] = 0;
+    Tensor ten(t);
+    for (unsigned int i = 0; i <= maxf; i++) 
+      tens.push_back(pair<string, Tensor>(to_string((int)i), ten));
+  }
+}; 
   
 
 
-  template<class FIELD>
-    class SetupFEMatrixAlgoT : public SetupFEMatrixAlgo
-  {
-    //! For the per-conductivity bases
-    int gen_;
-    int UseCond_;
-    int UseBasis_;
-    int nprocessors_;
+template<class FIELD>
+class SetupFEMatrixAlgoT : public SetupFEMatrixAlgo
+{
+  //! For the per-conductivity bases
+  int gen_;
+  int UseCond_;
+  int UseBasis_;
+  int nprocessors_;
 
-    Array1<Array1<double> > dataBasis_;
-    MatrixHandle AmatH_;  // shape information
+  Array1<Array1<double> > dataBasis_;
+  MatrixHandle AmatH_;  // shape information
 
-    MatrixHandle hGblMtrx_;
+  MatrixHandle hGblMtrx_;
   
 
-  public:
-    SetupFEMatrixAlgoT() : gen_(-1), UseBasis_(-1), UseCond_(-1), nprocessors_(1)
-    {
-    };
+public:
+  SetupFEMatrixAlgoT() : 
+    gen_(-1), 
+    UseCond_(-1), 
+    UseBasis_(-1), 
+    nprocessors_(1)
+  {
+  };
       
-      //! virtual interface. 
-      virtual MatrixHandle execute(const SetupFEMatrixParam& SFP)
-      {
+  //! virtual interface. 
+  virtual MatrixHandle execute(const SetupFEMatrixParam& SFP)
+  {
 #ifdef DEBUG
-	cerr << "SetupFEMatrixAlgoT::execute" << endl;
+    cerr << "SetupFEMatrixAlgoT::execute" << endl;
 #endif
-	FIELD *pField= dynamic_cast<FIELD *>(SFP.fieldH_.get_rep());
-	assert(pField);
+    FIELD *pField= dynamic_cast<FIELD *>(SFP.fieldH_.get_rep());
+    ASSERT(pField);
 	
-	const string hftn = SFP.fieldH_->get_type_description()->get_name();
+    const string hftn = SFP.fieldH_->get_type_description()->get_name();
 #ifdef DEBUG
-	cerr << "SetupFEMatrixAlgoT::execute " << hftn << endl;
+    cerr << "SetupFEMatrixAlgoT::execute " << hftn << endl;
 #endif
-	TypeDescription::td_vec *htdv = 
-	  SFP.fieldH_->get_type_description(Field::FDATA_TD_E)->get_sub_type();
-	const string hfvaltype = (*htdv)[0]->get_name();
+    TypeDescription::td_vec *htdv = 
+      SFP.fieldH_->get_type_description(Field::FDATA_TD_E)->get_sub_type();
+    const string hfvaltype = (*htdv)[0]->get_name();
 
-	bool index_based;
-	if (hfvaltype == "int") 
-	  index_based = true;
-	else if (hfvaltype == "Tensor") 
-	  index_based = false;
-	else 
-	  assert(0);
+    bool index_based;
+    if (hfvaltype == "int") 
+      index_based = true;
+    else if (hfvaltype == "Tensor") 
+      index_based = false;
+    else 
+      ASSERT(0);
 
-	if (SFP.gen_ == gen_ 
-	    && hGblMtrx_.get_rep() 
-	    && SFP.UseCond_==UseCond_
-	    && SFP.UseBasis_==UseBasis_)
-	  return hGblMtrx_;
+    if (SFP.gen_ == gen_ 
+	&& hGblMtrx_.get_rep() 
+	&& SFP.UseCond_==UseCond_
+	&& SFP.UseBasis_==UseBasis_)
+      return hGblMtrx_;
 
-	//! Either use supplied tensors, or make an array of identity tensors
-	vector<pair<string, Tensor> > tens;
+    //! Either use supplied tensors, or make an array of identity tensors
+    vector<pair<string, Tensor> > tens;
  
-	typename FIELD::basis_type dummy;
-	create_conductivities(pField, dummy, SFP, tens);
+    typename FIELD::basis_type dummy;
+    create_conductivities(pField, dummy, SFP, tens);
 
-	//! Cache data values for comparison next time
-	gen_ = SFP.gen_;
-	UseBasis_ = SFP.UseBasis_;
+    //! Cache data values for comparison next time
+    gen_ = SFP.gen_;
+    UseBasis_ = SFP.UseBasis_;
 
-	//! Compute the scale of this geometry based on its "units" property
-	double unitsScale = 1.;
-	string units;
-	if (SFP.UseCond_==1 /*&& fieldH->mesh()->get_property("units", units)*/) 
-	  if(SFP.fieldH_->get_property("units", units)) {
+    //! Compute the scale of this geometry based on its "units" property
+    double unitsScale = 1.;
+    string units;
+    if (SFP.UseCond_==1 /*&& fieldH->mesh()->get_property("units", units)*/) 
+      if(SFP.fieldH_->get_property("units", units)) {
 #ifdef DEBUG
 	cerr << "SetupFEMatrixAlgoT::execute units" << endl;
 #endif
-	    //	    msgStream_  << "units = "<< units <<"\n";
-	    if (units == "mm") unitsScale = 1./1000;
-	    else if (units == "cm") unitsScale = 1./100;
-	    else if (units == "dm") unitsScale = 1./10;
-	    else if (units == "m") unitsScale = 1./1;
-	    else {
-	      //	      warning("Did not recognize units of mesh '" + units + "'.");
-	    }
-	    //	    msgStream_ << "unitsScale = "<< unitsScale <<"\n";
-	  }
+	//	    msgStream_  << "units = "<< units <<"\n";
+	if (units == "mm") unitsScale = 1./1000;
+	else if (units == "cm") unitsScale = 1./100;
+	else if (units == "dm") unitsScale = 1./10;
+	else if (units == "m") unitsScale = 1./1;
+	else {
+	  //	      warning("Did not recognize units of mesh '" + units + "'.");
+	}
+	//	    msgStream_ << "unitsScale = "<< unitsScale <<"\n";
+      }
 
-	nprocessors_ = SFP.nprocessors_;
-	if (nprocessors_ > Thread::numProcessors() * 4) 
-	  nprocessors_ = Thread::numProcessors() * 4;
+    nprocessors_ = SFP.nprocessors_;
+    if (nprocessors_ > Thread::numProcessors() * 4) 
+      nprocessors_ = Thread::numProcessors() * 4;
  
-	if (index_based && UseBasis_) { 
-	  //! If the user wants to use basis matrices, 
-	  //!    first check to see if we need to recompute them
-	  if (SFP.fieldH_->mesh()->generation != gen_ || 
-	      tens.size() != (unsigned int)(dataBasis_.size())) {
-	    gen_ = SFP.fieldH_->mesh()->generation;
-	    //! Need to build basis matrices
-	    build_basis_matrices(SFP.fieldH_, tens.size(), unitsScale);
-	  }
-	  //! Have basis matrices, compute combined matrix
-	  hGblMtrx_ = build_composite_matrix(tens);
-	} else           
-	  BuildFEMatrix<FIELD>::build_FEMatrix(SFP.fieldH_, tens, hGblMtrx_, 
-					       unitsScale, nprocessors_);
+    if (index_based && UseBasis_) { 
+      //! If the user wants to use basis matrices, 
+      //!    first check to see if we need to recompute them
+      if (SFP.fieldH_->mesh()->generation != gen_ || 
+	  tens.size() != (unsigned int)(dataBasis_.size())) {
+	gen_ = SFP.fieldH_->mesh()->generation;
+	//! Need to build basis matrices
+	build_basis_matrices(SFP.fieldH_, tens.size(), unitsScale);
+      }
+      //! Have basis matrices, compute combined matrix
+      hGblMtrx_ = build_composite_matrix(tens);
+    } else           
+      BuildFEMatrix<FIELD>::build_FEMatrix(SFP.fieldH_, tens, hGblMtrx_, 
+					   unitsScale, nprocessors_);
   
-	return hGblMtrx_;
-      }
+    return hGblMtrx_;
+  }
 
-      void build_basis_matrices(FieldHandle fieldH, unsigned int nconds, double unitsScale)
-      {
+  void build_basis_matrices(FieldHandle fieldH, unsigned int nconds, double unitsScale)
+  {
 #ifdef DEBUG
-	cerr << "SetupFEMatrixAlgoT::build_basis_matrices" << endl;
+    cerr << "SetupFEMatrixAlgoT::build_basis_matrices" << endl;
 #endif
-	Tensor zero(0);
-	Tensor identity(1);
+    Tensor zero(0);
+    Tensor identity(1);
     
-	MatrixHandle aH;
-	vector<pair<string, Tensor> > tens(nconds, pair<string, Tensor>("", zero));
-	BuildFEMatrix<FIELD>::build_FEMatrix(fieldH, tens, aH, 
-						  unitsScale, nprocessors_);
-	AmatH_ = aH;
-	AmatH_.detach(); //! Store our matrix shape
+    MatrixHandle aH;
+    vector<pair<string, Tensor> > tens(nconds, pair<string, Tensor>("", zero));
+    BuildFEMatrix<FIELD>::build_FEMatrix(fieldH, tens, aH, 
+					 unitsScale, nprocessors_);
+    AmatH_ = aH;
+    AmatH_.detach(); //! Store our matrix shape
     
-	dataBasis_.resize(nconds);
-	for (unsigned int i=0; i<nconds; i++) {
-	  tens[i].first=to_string(i);
-	  tens[i].second=identity;
-	  BuildFEMatrix<FIELD>::build_FEMatrix(fieldH, tens, aH, 
-						    unitsScale, nprocessors_);
-	  SparseRowMatrix *m = dynamic_cast<SparseRowMatrix*>(aH.get_rep());
-	  dataBasis_[i].resize(m->nnz);
-	  for (int j=0; j<m->nnz; j++)
-	    dataBasis_[i][j] = m->a[j];
-	  tens[i].second=zero;
-	}
-      }
+    dataBasis_.resize(nconds);
+    for (unsigned int i=0; i<nconds; i++) {
+      tens[i].first=to_string(i);
+      tens[i].second=identity;
+      BuildFEMatrix<FIELD>::build_FEMatrix(fieldH, tens, aH, 
+					   unitsScale, nprocessors_);
+      SparseRowMatrix *m = dynamic_cast<SparseRowMatrix*>(aH.get_rep());
+      dataBasis_[i].resize(m->nnz);
+      for (int j=0; j<m->nnz; j++)
+	dataBasis_[i][j] = m->a[j];
+      tens[i].second=zero;
+    }
+  }
 
-      MatrixHandle build_composite_matrix(const vector<pair<string,Tensor> >&tens)
-      {
+  MatrixHandle build_composite_matrix(const vector<pair<string,Tensor> >&tens)
+  {
 #ifdef DEBUG
-	cerr << "SetupFEMatrixAlgoT::build_composite_matrix" << endl;
+    cerr << "SetupFEMatrixAlgoT::build_composite_matrix" << endl;
 #endif
-	MatrixHandle fem_mat = AmatH_;
-	fem_mat.detach();
-	SparseRowMatrix *m = dynamic_cast<SparseRowMatrix*>(fem_mat.get_rep());
-	double *sum = m->a;
-	for (unsigned int i=0; i<tens.size(); i++)
-	{
-	  double weight = tens[i].second.mat_[0][0];
-	  for (int j=0; j<dataBasis_[i].size(); j++)
-	  {
-	    sum[j] += weight*dataBasis_[i][j];
-	  }
-	}
-	return fem_mat;
+    MatrixHandle fem_mat = AmatH_;
+    fem_mat.detach();
+    SparseRowMatrix *m = dynamic_cast<SparseRowMatrix*>(fem_mat.get_rep());
+    double *sum = m->a;
+    for (unsigned int i=0; i<tens.size(); i++)
+    {
+      double weight = tens[i].second.mat_[0][0];
+      for (int j=0; j<dataBasis_[i].size(); j++)
+      {
+	sum[j] += weight*dataBasis_[i][j];
       }
-  };
+    }
+    return fem_mat;
+  }
+};
 
   
  
