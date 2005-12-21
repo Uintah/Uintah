7c407b1d5bbb37609c37b67184475d55c79605c5
Ayla Khan <ayla@sci.utah.edu>
2005-12-21 20:59:48 +0000

    Use optional default XML search path in component models.  All component model headers moved to sci_metacomponents.h

--- to_libxml2/src/SCIRun/SCIRunFramework.h
+++ to_libxml2/src/SCIRun/SCIRunFramework.h
@@ -44,7 +44,7 @@
 #include <include/sci_defs/dataflow_defs.h>
 
 #ifdef BUILD_DATAFLOW
-  #include <SCIRun/Dataflow/SCIRunComponentModel.h>
+#  include <SCIRun/Dataflow/SCIRunComponentModel.h>
 #endif
 
 #include <Core/Thread/Mutex.h>
@@ -72,7 +72,7 @@ class ComponentEventService;
 
 /**
  * \class SCIRunFramework
- * 
+ *
  * \brief An implementation of the CCA AbstractFramework for SCIRun2.
  *
  * The SCIRunFramework is a wrapper around a set of services for creating and
@@ -95,8 +95,8 @@ class ComponentEventService;
  * BuilderService) uses to instantiate components registered in that component
  * model.
  *
- * 
- * \sa BuilderService AbstractFramework 
+ *
+ * \sa BuilderService AbstractFramework
  * \todo createTypeMap is not fully implemented
  * \todo releaseServices is not fully implemented
  * \todo shutdownFramework is not fully implemented
@@ -112,93 +112,93 @@ public:
     virtual ~SCIRunFramework();
 
     /** Standard CCA::AbstractFramework method that creates and returns an empty
-        CCA Map object appropriate for use in an ensuing call to getServices. */
+	CCA Map object appropriate for use in an ensuing call to getServices. */
     virtual sci::cca::TypeMap::pointer
     createTypeMap();
 
     /** Returns a handle to the services provided by this framework (getting and
-        releasing ports, registering ports, querying port properties, etc.).  The
-        framework services are encapsulated in a standard CCA interface called a
-        Services object.  The Services object is the real interface to the
-        underlying framework and is used to retrieve ports, including the 
-        BuilderService Port, which handles connections among components.
+	releasing ports, registering ports, querying port properties, etc.).  The
+	framework services are encapsulated in a standard CCA interface called a
+	Services object.  The Services object is the real interface to the
+	underlying framework and is used to retrieve ports, including the
+	BuilderService Port, which handles connections among components.
 
-        The getServices method creates a new Services object that appears as a
-        component instance in the Framework, effectively registering an image of
-        the calling program as a component instance within the framework.
-        Multiple Services objects may be created with different names from the
-        same or different calling programs.
+	The getServices method creates a new Services object that appears as a
+	component instance in the Framework, effectively registering an image of
+	the calling program as a component instance within the framework.
+	Multiple Services objects may be created with different names from the
+	same or different calling programs.
 
-        The \em selfInstanceName parameter is the name given to the component
-        instance. The \em selfClassName parameter is the component type of the
-        calling program, as it will appear in the framework.  The
-        \em selfProperties are the properties of the component instance (and may
-        be null).  If the selfInstanceName is already in use by another component
-        in the framework, this method will throw a CCAException.     */
+	The \em selfInstanceName parameter is the name given to the component
+	instance. The \em selfClassName parameter is the component type of the
+	calling program, as it will appear in the framework.  The
+	\em selfProperties are the properties of the component instance (and may
+	be null).  If the selfInstanceName is already in use by another component
+	in the framework, this method will throw a CCAException.     */
     virtual sci::cca::Services::pointer
     getServices(const ::std::string& selfInstanceName,
-                const ::std::string& selfClassName,
-                const sci::cca::TypeMap::pointer& selfProperties);
+		const ::std::string& selfClassName,
+		const sci::cca::TypeMap::pointer& selfProperties);
 
     /** Informs the framework that the Services object referenced by \em svc
-        is no longer needed by the calling program.  This method invalidates the
-        associated component image in the underlying framework and also
-        invalidates any ComponentIDs or ConnectionIDs contained in the referenced
-        Services  object. */
+	is no longer needed by the calling program.  This method invalidates the
+	associated component image in the underlying framework and also
+	invalidates any ComponentIDs or ConnectionIDs contained in the referenced
+	Services  object. */
     virtual void
     releaseServices(const sci::cca::Services::pointer& svc);
 
     /** Tells the framework it is no longer needed and to clean up
-        after itself. */  
+	after itself. */
     virtual void shutdownFramework();
 
     /** Creates a new SCIRunFramework instance.  The new framework instance is
-        not a copy of the existing framework and does not contain any of the
-        user-instantiated components from the original.  This method exists as a
-        kind of object factory for creating a new SCIRunFramework using an
-        abstract API. */ 
+	not a copy of the existing framework and does not contain any of the
+	user-instantiated components from the original.  This method exists as a
+	kind of object factory for creating a new SCIRunFramework using an
+	abstract API. */
     virtual sci::cca::AbstractFramework::pointer
     createEmptyFramework();
 
     /** Eliminates the component instance ``cid'' from the scope of the
-        framework.  The ``timeout'' parameter specifies the maximum allowable
-        wait time for this operation.  A timeout of 0 leaves the wait time up to
-        the framework.  If the destroy operation is not completed in the maximum
-        allowed number of seconds, or the referenced component does not exist,
-        then a CCAException is thrown.
+	framework.  The ``timeout'' parameter specifies the maximum allowable
+	wait time for this operation.  A timeout of 0 leaves the wait time up to
+	the framework.  If the destroy operation is not completed in the maximum
+	allowed number of seconds, or the referenced component does not exist,
+	then a CCAException is thrown.
 
-        Like createComponentInstance, this method is only intended to be called
-        by the BuilderService class.  It searches the list of registered
-        components (compIDs) for the matching component ID, unregisters it, finds
-        the correct ComponentModel for the type, then calls
-        ComponentModel::destroyInstance to properly destroy the component. */
+	Like createComponentInstance, this method is only intended to be called
+	by the BuilderService class.  It searches the list of registered
+	components (compIDs) for the matching component ID, unregisters it, finds
+	the correct ComponentModel for the type, then calls
+	ComponentModel::destroyInstance to properly destroy the component. */
     void
     destroyComponentInstance(const sci::cca::ComponentID::pointer &cid,
-                             float timeout);
+			     float timeout);
 
     /** ? */
     sci::cca::Port::pointer
     getFrameworkService(const std::string& type,
-                        const std::string& componentName);
+			const std::string& componentName);
 
     /** ? */
     bool
     releaseFrameworkService(const std::string& type,
-                            const std::string& componentName);
+			    const std::string& componentName);
 
     /** Adds a description of a component instance (class ComponentInstance) to
-        the list of active components.  The component instance description
-        includes the component type name, the instance name, and the pointer to
-        the allocated component.  When a \em name conflicts with an existing
-        registered component instance name, this method will automatically append
-        an integer to create a new, unique instance name.*/ 
+	the list of active components.  The component instance description
+	includes the component type name, the instance name, and the pointer to
+	the allocated component.  When a \em name conflicts with an existing
+	registered component instance name, this method will automatically append
+	an integer to create a new, unique instance name.*/
     sci::cca::ComponentID::pointer
     registerComponent(ComponentInstance *ci,
-                      const std::string& name);
+		      const std::string& name);
 
     /** Removes a component instance description from the list of active
-        framework components.  Returns the pointer to the component description
-        that was successfully unregistered. */
+	framework components.  Returns the pointer to the component description
+	that was successfully unregistered. */
     ComponentInstance*
     unregisterComponent(const std::string& instanceName);
 
@@ -206,14 +206,14 @@ public:
     //void shutdownComponent(const std::string& name);
 
     /** Compiles a list of all ComponentDescriptions in all ComponentModels
-        contained in this framework.*/
+	contained in this framework.*/
     void
     listAllComponentTypes(std::vector<ComponentDescription*>&, bool);
 
     /** ?  */
     virtual int
     registerLoader(const std::string &slaveName,
-                   const SSIDL::array1<std::string> &slaveURLs);
+		   const SSIDL::array1<std::string> &slaveURLs);
 
     /** ? */
     virtual int
@@ -223,8 +223,8 @@ public:
     int destroyLoader(const std::string &loaderName);
 
     /** Returns a component instance description (ComponentInstance) from the
-        list of active framework components.  Returns a null pointer if the
-        instance \em name is not found. */
+	list of active framework components.  Returns a null pointer if the
+	instance \em name is not found. */
     ComponentInstance*
     lookupComponent(const std::string &name);
 
@@ -242,9 +242,9 @@ public:
 
 #ifdef BUILD_DATAFLOW
     /** A pointer to the framework's SCIRun dataflow component model.  This
-        pointer is also in the \em models list.  The \em dflow ivar is public so
-        that it can be accessed by the SCIRun scheduler service.  */
-    SCIRunComponentModel* dflow; 
+	pointer is also in the \em models list.  The \em dflow ivar is public so
+	that it can be accessed by the SCIRun scheduler service.  */
+    SCIRunComponentModel* dflow;
 #endif
 
 protected:
@@ -255,19 +255,19 @@ protected:
     friend class ServiceRegistry;
 
     /** Creates an instance of the component defined by the string ``type'',
-        which must uniquely define the type of the component.  The component
-        instance is given the name ``name''.   If the instance name is not
-        specified (i.e. the method is passed an empty string), then the component
-        will be assigned a unique name automatically.
+	which must uniquely define the type of the component.  The component
+	instance is given the name ``name''.   If the instance name is not
+	specified (i.e. the method is passed an empty string), then the component
+	will be assigned a unique name automatically.
 
-        This method is ``semi-private'' and intended to be called only by the
-        BuilderService class.  It works by searching the list of ComponentModels
-        (the ivar \em models) for a matching registered type, and then calling
-        the createInstance method on the appropriate ComponentModel object. */
+	This method is ``semi-private'' and intended to be called only by the
+	BuilderService class.  It works by searching the list of ComponentModels
+	(the ivar \em models) for a matching registered type, and then calling
+	the createInstance method on the appropriate ComponentModel object. */
     sci::cca::ComponentID::pointer
     createComponentInstance(const std::string& name,
-                            const std::string& className,
-                            const sci::cca::TypeMap::pointer &tm);
+			    const std::string& className,
+			    const sci::cca::TypeMap::pointer &tm);
 
     /** ? */
     void
@@ -283,28 +283,28 @@ private:
 
     /** ? */
     std::vector<sci::cca::ComponentID::pointer> compIDs;
-    SCIRun::Mutex lock_compIDs;  
+    SCIRun::Mutex lock_compIDs;
 
     /** The set of registered components available in the framework, indexed by
-        their instance names. */
+	their instance names. */
     ComponentInstanceMap activeInstances;
     SCIRun::Mutex lock_activeInstances;
 
     /** A pointer to the special SCIRun \em internal component model, whose
-        framework-related components are not intended to be exposed to the user.
-        These components include the BuilderService port. */
+	framework-related components are not intended to be exposed to the user.
+	These components include the BuilderService port. */
     InternalComponentModel* internalServices;
 
     /** A pointer to the CCA component model. This pointer is also stored in the
-        \em models list. */
+	\em models list. */
     CCAComponentModel* cca;
 
     /** A pointer to the babel componet model.  This pointer is also stored in
-        the \em models list. (Can this ivar be removed?)*/
+	the \em models list. (Can this ivar be removed?)*/
     BabelComponentModel* babel;
 
     /** A pointer to the Vtk component model.  This pointer is also stored in the
-        \em models list. (Can this ivar be removed?) */
+	\em models list. (Can this ivar be removed?) */
     VtkComponentModel* vtk;
 
     CorbaComponentModel* corba;
