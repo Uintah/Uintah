defe4ce71fe1288127ad539af478b843a35e7d72
Justin Luitjens <luitjens@sci.utah.edu>
2006-01-20 17:46:41 +0000

    inital space-filling curve commit

---
+++ Packages/Uintah/testprograms/SFCTest/sub.mk
@@ -0,0 +1,15 @@
+
+# Makefile fragment for this subdirectory
+
+SRCDIR := Packages/Uintah/testprograms/SFCTest
+
+LIBS := $(MPI_LIBRARY) 
+
+PROGRAM := $(SRCDIR)/sfctest
+SRCS := $(SRCDIR)/sfctest.cc
+PSELIBS := Packages/Uintah/CCA/Components/SFC \
+	   Packages/Uintah/CCA/Ports
+
+include $(SCIRUN_SCRIPTS)/program.mk
+
+
