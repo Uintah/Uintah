eaff91eb2526f45cc91b0dc49642f19a4f40fff1
Bryan Jeffrey Worthen <worthen@sci.utah.edu>
2006-01-23 15:11:59 +0000

    Fix sgi compile

--- Packages/Uintah/testprograms/SFCTest/sub.mk
+++ Packages/Uintah/testprograms/SFCTest/sub.mk
@@ -7,7 +7,11 @@ LIBS := $(MPI_LIBRARY)
 
 PROGRAM := $(SRCDIR)/sfctest
 SRCS := $(SRCDIR)/sfctest.cc
-PSELIBS := Packages/Uintah/CCA/Ports
+PSELIBS := Packages/Uintah/CCA/Ports \
+           Packages/Uintah/Core/Parallel \
+           Core/Thread
+
+LIBS := $(M_LIBRARY) $(MPI_LIBRARY)
 
 include $(SCIRUN_SCRIPTS)/program.mk
 
