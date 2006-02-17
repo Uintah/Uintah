a0b62748764cf610b63bb9dddf1bc36bfafe152a
J. Davison de St. Germain <dav@sci.utah.edu>
2006-02-17 00:26:40 +0000

    M    Core/GUI/Util.tcl
    Added the label_pair utility function.  Creates a label/text_var pair.
     
    M    Packages/Uintah/Dataflow/GUI/ScalarMinMax.tcl
    
    Added in the field_name to the GUI.  Added separators between the min
    and max sections.  Now use the label_pair utility from Core/Util.
    
    M    Packages/Uintah/Dataflow/XML/CompareMMS.xml
    
    Take in a Scalar Field (instead of an UDA) (at least for now).
    
    M    Packages/Uintah/Dataflow/GUI/CompareMMS.tcl
    
    Basic GUI updates, still needs lots of work.

--- Packages/Uintah/Dataflow/GUI/ScalarMinMax.tcl
+++ Packages/Uintah/Dataflow/GUI/ScalarMinMax.tcl
@@ -8,10 +8,8 @@ itcl_class Uintah_Operators_ScalarMinMax {
     }
 
     method set_defaults {} {
-	# the width of the first column of the data display
-	global $this-firstwidth
-	set $this-firstwidth 12
 
+	global $this-field_name
 	global $this-min_data
 	global $this-max_data
 	global $this-min_index
@@ -19,12 +17,13 @@ itcl_class Uintah_Operators_ScalarMinMax {
 	global $this-min_values
 	global $this-max_values
 
+	set $this-field_name "---"
 	set $this-min_data "---"
 	set $this-max_data "---"
 	set $this-min_index "---"
 	set $this-max_index "---"
 	set $this-min_values "---"
-	set $this-max_values "___"
+	set $this-max_values "---"
     }
 
     method ui {} {
@@ -40,29 +39,25 @@ itcl_class Uintah_Operators_ScalarMinMax {
 	pack $w.lf
 	set lf [$w.lf childsite]
 
-	labelpair $lf.l1 "Min Value" $this-min_data
-	labelpair $lf.l2 "Min index" $this-min_index
-	labelpair $lf.l3 "\# Min Values" $this-min_values
-	label $lf.l4 -text ""
-	labelpair $lf.l5 "Max Value" $this-max_data
-	labelpair $lf.l6 "Max index" $this-max_index
-	labelpair $lf.l7 "\# Max Values" $this-max_values
-	pack $lf.l1 $lf.l2 $lf.l3 $lf.l4 $lf.l5 \
-	     $lf.l7 -side top -expand y -fill x
+	label_pair $lf.l0 "Field Name" $this-field_name
+	frame $lf.separator0 -height 2 -relief sunken -borderwidth 2
+        
+	label_pair $lf.l1 "Min Value" $this-min_data
+	label_pair $lf.l2 "Min index" $this-min_index
+	label_pair $lf.l3 "\# Min Values" $this-min_values
+
+	frame $lf.separator1 -height 2 -relief sunken -borderwidth 2
+
+	label_pair $lf.l5 "Max Value" $this-max_data
+	label_pair $lf.l6 "Max index" $this-max_index
+	label_pair $lf.l7 "\# Max Values" $this-max_values
+
+	pack $lf.l0 $lf.separator0 $lf.l1 $lf.l2 $lf.l3 $lf.separator1 $lf.l5 $lf.l6 $lf.l7 \
+	       -side top -expand y -fill x -padx 5
 
 	makeSciButtonPanel $w $w $this
         moveToCursor $w
     }
 
-    method labelpair { win text1 text2 } {
-	frame $win 
-	pack $win -side top -padx 5
-	label $win.l1 -text $text1 -width [set $this-firstwidth] \
-		      -anchor w -just left
-	label $win.colon  -text ":" -width 2 -anchor w -just left 
-	label $win.l2 -textvar $text2 -width 40 -anchor w -just left \
-		-fore darkred -borderwidth 0
-	pack $win.l1 $win.colon $win.l2 -side left
-    } 
 }
 
