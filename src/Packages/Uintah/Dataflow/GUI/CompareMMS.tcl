26f8a69bf36708fce0ce53270d38c57a2d58fd79
J. Davison de St. Germain <dav@sci.utah.edu>
2006-02-17 00:26:40 +0000

    M    Core/GUI/Util.tcl
    Added the label_pair utility function.  Creates a label/text_var pair.
     
    M    Packages/Uintah/Dataflow/GUI/ScalarMinMax.tcl
    
    Added in the field_name to the GUI.  Added separators between the min
    and max sections.  Now use the label_pair utility from Core/Util.
    
    M    Packages/Uintah/Dataflow/XML/CompareMMS.xml
    
    Take in a Scalar Field (instead of an UDA) (at least for now).
    
    M    Packages/Uintah/Dataflow/GUI/CompareMMS.tcl
    
    Basic GUI updates, still needs lots of work.

--- src/Packages/Uintah/Dataflow/GUI/CompareMMS.tcl
+++ src/Packages/Uintah/Dataflow/GUI/CompareMMS.tcl
@@ -8,6 +8,8 @@ itcl_class Uintah_Operators_CompareMMS {
     }
 
     method set_defaults {} {
+        global $this-field_name
+        set    $this-field_name "---"
     }
 
     method ui {} {
@@ -17,6 +19,13 @@ itcl_class Uintah_Operators_CompareMMS {
             return
         }
         toplevel $w
+
+        frame $w.f1
+        pack  $w.f1
+
+        label_pair $w.f1.field_name "Field Name" $this-field_name
+
+        pack $w.f1.field_name -padx 10 -pady 10
 
 	makeSciButtonPanel $w $w $this
         moveToCursor $w
