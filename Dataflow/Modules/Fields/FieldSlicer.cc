4372fa7ddf6720db4a291df72b5e4795054f35dc
Allen Sanderson <allen@sci.utah.edu>
2006-03-24 19:45:47 +0000

    added lots of comments so that the modules can be used an example

--- Dataflow/Modules/Fields/FieldSlicer.cc
+++ Dataflow/Modules/Fields/FieldSlicer.cc
@@ -26,19 +26,15 @@
    DEALINGS IN THE SOFTWARE.
 */
 
+//    File   : FieldSlicer.h
+//    Author : Michael Callahan &&
+//             Allen Sanderson
+//             SCI Institute
+//             University of Utah
+//    Date   : March 2006
+//
+//    Copyright (C) 2006 SCI Group
 
-/*
- *  FieldSlicer.cc:
- *
- *  Written by:
- *   Michael Callahan &
- *   Allen Sanderson
- *   School of Computing
- *   University of Utah
- *   December 2002
- *
- *  Copyright (C) 2002 SCI Group
- */
 
 #include <Dataflow/Modules/Fields/FieldSlicer.h>
 
@@ -76,12 +72,13 @@ private:
   GuiInt               gui_index_j_;
   GuiInt               gui_index_k_;
 
-  //! gui_update_type_ must be declared after all gui vars because
-  //! some are traced in the tcl code. If updateType_ is set to Auto
-  //! having it last will prevent the net from executing when it is
-  //! instantiated.
+  //! gui_update_type_ must be declared after all gui vars because !
+  //! some are traced in the tcl code. If gui_update_type_ is set to
+  //! Auto having it last will prevent the net from executing when it
+  //! is instantiated.
 
   GuiString            gui_update_type_;
+  GuiInt               gui_continuous_;
 };
 
 
@@ -105,7 +102,8 @@ FieldSlicer::FieldSlicer(GuiContext *context)
     gui_index_j_(context->subVar("index-j"), 1),
     gui_index_k_(context->subVar("index-k"), 1),
 
-    gui_update_type_(context->subVar("update_type"), "Manual")
+    gui_update_type_(context->subVar("update_type"), "Manual"),
+    gui_continuous_(context->subVar("continuous"), 0)
 {
 }
 
@@ -120,43 +118,39 @@ FieldSlicer::execute()
 {
   FieldHandle field_in_handle;
 
+  //! Get the input field handle from the port.
   if( !get_input_handle( "Input Field",  field_in_handle,  true  ) ) return;
+
+  //! Get the optional matrix handle from the port. Note if a matrix is
+  //! present it is sent down stream. Otherwise it will be created.
   if( !get_input_handle( "Input Matrix", matrix_out_handle_, false ) ) return;
 
+  // Because the field slicer is index based it can only work on
+  // structured data. For unstructured data SamplePlane should be used.
   if( !(field_in_handle->mesh()->topology_geometry() & Mesh::STRUCTURED) ) {
 
     error( field_in_handle->get_type_description(Field::FIELD_NAME_ONLY_E)->get_name() );
     error( "Only availible for topologically structured data." );
+    error( "For topologically unstructured data use SamplePlane." );
     return;
   }
 
+  //! For now ?? Slice only node based data. How does one slice cell data??
   if( field_in_handle->basis_order() != 1 ) {
     error( field_in_handle->get_type_description(Field::FIELD_NAME_ONLY_E)->get_name() );
     error( "Currently only available for node data." );
     return;
   }
 
-  //! The matrix is optional.
-  if( matrix_out_handle_.get_rep() &&
-      (matrix_out_handle_->nrows() != 3 || matrix_out_handle_->ncols() != 3) ) {
-
-    error( "Input matrix is not a 3x3 matrix" );
-    return;
-  }
-
-  // Get the type and dimensions of the mesh.
+  // Get the dimensions of the mesh.
   vector<unsigned int> dims;
 
   field_in_handle->mesh()->get_dim( dims );
 
   bool update_dims = false;
 
-  if( gui_dims_.get() != (int) dims.size() ) {
-    gui_dims_.set( dims.size() );
-    update_dims = true;
-  }
-
   if( dims.size() >= 1 ) {
+    //! Check to see if the gui dimensions are different than the field.
     if( gui_dim_i_.get() != (int) dims[0] ) {
       gui_dim_i_.set( dims[1] );
       update_dims = true;
@@ -164,6 +158,7 @@ FieldSlicer::execute()
   }
 
   if( dims.size() >= 2 ) {
+    //! Check to see if the gui dimensions are different than the field.
     if( gui_dim_j_.get() != (int) dims[1] ) {
       gui_dim_j_.set( dims[1] );
       update_dims = true;
@@ -171,6 +166,7 @@ FieldSlicer::execute()
   }
 
   if( dims.size() >= 3 ) {
+    //! Check to see if the gui dimensions are different than the field.
     if( gui_dim_k_.get() != (int) dims[2] ) {
       gui_dim_k_.set( dims[2] );
       update_dims = true;
@@ -178,6 +174,13 @@ FieldSlicer::execute()
   }
 
   //! Check to see if the gui dimensions are different than the field.
+  //! This is last because the GUI var has a callback on it.
+  if( gui_dims_.get() != (int) dims.size() ) {
+    gui_dims_.set( dims.size() );
+    update_dims = true;
+  }
+
+  //! If the gui dimensions are different than the field then update the gui.
   if( update_dims ) {
 
     //! Dims has callback on it, so it must be set it after i, j, and k.
@@ -190,8 +193,22 @@ FieldSlicer::execute()
 
   //! An input matrix is present so use the values in it to override
   //! the variables set in the gui.
+  //! Column 0 selected axis to slice.
+  //! Column 1 index of the axis to slice.
+  //! Column 2 dimensions of the data.
   if( matrix_out_handle_.get_rep() ) {
 
+    //! The matrix is optional. If present make sure it is a 3x3 matrix.
+    //! The row indices is the axis index. The column is the data.
+    if( (matrix_out_handle_->nrows() != 3 ||
+	 matrix_out_handle_->ncols() != 3) ) {
+      
+      error( "Input matrix is not a 3x3 matrix" );
+      return;
+    }
+
+    //! Sanity check. Make sure the gui dimensions match the matrix
+    //! dimensions.
     if( gui_dim_i_.get() != matrix_out_handle_->get(0, 2) ||
 	gui_dim_j_.get() != matrix_out_handle_->get(1, 2) ||
 	gui_dim_k_.get() != matrix_out_handle_->get(2, 2) ) {
@@ -210,7 +227,8 @@ FieldSlicer::execute()
       return;
     }
 
-    //! Check to see what axis has been selected.
+    //! Check to see what axis has been selected. Only one should be
+    //! selected.
     for (int i=0; i < matrix_out_handle_->nrows(); i++) {
       if( matrix_out_handle_->get(i, 0) == 1 && gui_axis_.get() != i ) {
 	gui_axis_.set( i );	
@@ -218,7 +236,8 @@ FieldSlicer::execute()
       }
     }
 
-    //! Check to see what index has been selected.
+    //! Check to see what index has been selected and if it matches
+    //! the gui index.
     if( gui_index_i_.get() != matrix_out_handle_->get(0, 1) ||
 	gui_index_j_.get() != matrix_out_handle_->get(1, 1) ||
 	gui_index_k_.get() != matrix_out_handle_->get(2, 1) ) {
@@ -238,7 +257,8 @@ FieldSlicer::execute()
     }
   }
 
-  //! If no data or ainput change recreate the mesh.
+  //! If no data or an input change recreate the field. I.e Only
+  //! execute when neeed.
   if( inputs_changed_  ||
       
       !field_out_handle_.get_rep() ||
@@ -249,8 +269,11 @@ FieldSlicer::execute()
       (gui_axis_.get() == 1 && gui_index_j_.changed( true )) ||
       (gui_axis_.get() == 2 && gui_index_k_.changed( true )) ) {
 
+    // Update the state. Other state changes are handled in either
+    // getting handles or in the calling method Module::do_execute.
     update_state(Executing);
 
+    //! Create the new field via a dynamically compiled algorithm.
     const TypeDescription *ftd = field_in_handle->get_type_description();
     const TypeDescription *ttd =
       field_in_handle->get_type_description(Field::FDATA_TD_E);
@@ -259,6 +282,9 @@ FieldSlicer::execute()
     Handle<FieldSlicerAlgo> algo;
     if (!module_dynamic_compile(ci, algo)) return;
 
+    field_out_handle_ = algo->execute(field_in_handle, gui_axis_.get());
+
+    //! Get the index for the axis selected.
     unsigned int index;
     if (gui_axis_.get() == 0) {
       index = gui_index_i_.get();
@@ -268,9 +294,8 @@ FieldSlicer::execute()
       index = gui_index_k_.get();
     }
 
-    field_out_handle_ = algo->execute(field_in_handle, gui_axis_.get());
-
-    //! Now the new field is defined so do the work on it.
+    //! Now the new field is defined so slice the field via a
+    //! dynamically compiled algorithm.
     const TypeDescription *iftd = field_in_handle->get_type_description();
     const TypeDescription *oftd = field_out_handle_->get_type_description();
     const bool geom_irreg =
@@ -283,27 +308,28 @@ FieldSlicer::execute()
   
     workalgo->execute( field_in_handle, field_out_handle_,
 		       index, gui_axis_.get() );
+
+    //! Create the output matrix with the axis selected, index, and
+    //! dimensions.
+    if( matrix_out_handle_ == 0 ) {
+      DenseMatrix *selected = scinew DenseMatrix(3,3);
+
+      for (int i=0; i < 3; i++)
+	selected->put(i, 0, (double) (gui_axis_.get() == i) );
+
+      selected->put(0, 1, gui_index_i_.get() );
+      selected->put(1, 1, gui_index_j_.get() );
+      selected->put(2, 1, gui_index_k_.get() );
+
+      selected->put(0, 2, gui_dim_i_.get() );
+      selected->put(1, 2, gui_dim_j_.get() );
+      selected->put(2, 2, gui_dim_k_.get() );
+
+      matrix_out_handle_ = MatrixHandle(selected);
+    }
   }
 
-  //! Create the output matrix with the axis and index
-  if( matrix_out_handle_ == 0 ) {
-    DenseMatrix *selected = scinew DenseMatrix(3,3);
-
-    for (int i=0; i < 3; i++)
-      selected->put(i, 0, (double) (gui_axis_.get() == i) );
-
-    selected->put(0, 1, gui_index_i_.get() );
-    selected->put(1, 1, gui_index_j_.get() );
-    selected->put(2, 1, gui_index_k_.get() );
-
-    selected->put(0, 2, gui_dim_i_.get() );
-    selected->put(1, 2, gui_dim_j_.get() );
-    selected->put(2, 2, gui_dim_k_.get() );
-
-    matrix_out_handle_ = MatrixHandle(selected);
-  }
-
-  // Send the data downstream
+  // Send the data downstream.
   send_output_handle( "Output Field",  field_out_handle_, true );
   send_output_handle( "Output Matrix", matrix_out_handle_, true );
 }
