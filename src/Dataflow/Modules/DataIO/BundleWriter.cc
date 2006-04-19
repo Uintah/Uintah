b2ae6d01dbf2d8fccd336326880321a8bcfdb8ad
Jeroen Stinstra <jeroen@sci.utah.edu>
2006-04-19 19:34:55 +0000

    Bug fix

--- src/Dataflow/Modules/DataIO/BundleWriter.cc
+++ src/Dataflow/Modules/DataIO/BundleWriter.cc
@@ -61,8 +61,8 @@ protected:
 DECLARE_MAKER(BundleWriter)
 BundleWriter::BundleWriter(GuiContext* ctx)
   : GenericWriter<BundleHandle>("BundleWriter", ctx, "DataIO", "SCIRun"),
-    guiTypes_(get_ctx()->subVar("types"), false),
-    guiFileType_(get_ctx()->subVar("filetype", "Binary"))
+    guiTypes_(get_ctx()->subVar("types", false)),
+    guiFileType_(get_ctx()->subVar("filetype"),"Binary")
 {
   string exporttypes = "{";
   exporttypes += "{{SCIRun Bundle File} {.bdl} } ";
