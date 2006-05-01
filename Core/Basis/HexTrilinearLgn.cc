188a6f77d3189661b35fe3f5e51429c16de763f5
Marty Cole <mjc@sci.utah.edu>
2006-05-01 16:46:35 +0000

    make hex vol order match hex basis order

--- Core/Basis/HexTrilinearLgn.cc
+++ Core/Basis/HexTrilinearLgn.cc
@@ -39,11 +39,12 @@ double HexTrilinearLgnUnitElement::unit_vertices[8][3] =
     {1.0L, 1.0L, 1.0L}, {0.0L, 1.0L, 1.0L} };
 int HexTrilinearLgnUnitElement::unit_edges[12][2] = 
   {{0,1}, {1,2}, {2,3}, {3,0},
-   {0,4}, {1,5}, {2,6}, {3,7},
-   {4,5}, {5,6}, {6,7}, {7,4}};
+   {4,5}, {5,6}, {6,7}, {7,4},
+   {0,4}, {5,1}, {2,6}, {7,3}};
+
 int HexTrilinearLgnUnitElement::unit_faces[6][4] = 
-  {{0,3,2,1}, {0,1,5,4}, {1,2,6,5},
-   {2,3,7,6}, {3,0,4,7}, {4,5,6,7}};
+  {{0,3,2,1}, {4,5,6,7}, {0,1,5,4},
+   {2,3,7,6}, {3,0,4,7}, {1,2,6,5}};
 
 double HexTrilinearLgnUnitElement::unit_face_normals[6][3] = 
   { {0.0L, 0.0L, -1.0L}, {0.0L, -1.0L, 0.0L},  {1.0L, 0.0L, 0.0L},  
