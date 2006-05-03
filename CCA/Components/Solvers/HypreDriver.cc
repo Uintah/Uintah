2be5ba73c023e5035a9478c4c64ceba43b50d047
Todd Harman <harman@sci.utah.edu>
2006-05-03 19:38:12 +0000

    Simplify: if it's not currently or ever going to be used, it's not needed.   - nuked getSolution_(SFC(x,y,z), NC)   - nuked makeLinearSystem_(SFC(x,y,z), NC)

--- CCA/Components/Solvers/HypreDriver.cc
+++ CCA/Components/Solvers/HypreDriver.cc
@@ -42,34 +42,6 @@ static DebugStream cout_dbg("HYPRE_DBG", false);
   class HypreDriver implementation common to all variable types
   and all Hypre interfaces
   _____________________________________________________________________*/
-
-void
-HypreDriver::makeLinearSystem_SFCX(const int matl)
-{
-  throw InternalError("makeLinearSystem is not implemented for SFCX variables",
-                      __FILE__, __LINE__);
-}
-
-void
-HypreDriver::makeLinearSystem_SFCY(const int matl)
-{
-  throw InternalError("makeLinearSystem is not implemented for SFCY variables",
-                      __FILE__, __LINE__);
-}
-
-void
-HypreDriver::makeLinearSystem_SFCZ(const int matl)
-{
-  throw InternalError("makeLinearSystem is not implemented for SFCZ variables",
-                      __FILE__, __LINE__);
-}
-
-void
-HypreDriver::makeLinearSystem_NC(const int matl)
-{
-  throw InternalError("makeLinearSystem is not implemented for NC variables",
-                      __FILE__, __LINE__);
-}
 void
 HypreDriver::makeLinearSystem_CC(const int matl)
 {
@@ -78,38 +50,13 @@ HypreDriver::makeLinearSystem_CC(const int matl)
 }
 
 void
-HypreDriver::getSolution_SFCX(const int matl)
-{
-  throw InternalError("getSolution is not implemented for SFCX variables",
-                      __FILE__, __LINE__);
-}
-
-void
-HypreDriver::getSolution_SFCY(const int matl)
-{
-  throw InternalError("getSolution is not implemented for SFCY variables",
-                      __FILE__, __LINE__);
-}
-
-void
-HypreDriver::getSolution_SFCZ(const int matl)
-{
-  throw InternalError("getSolution is not implemented for SFCZ variables",
-                      __FILE__, __LINE__);
-}
-
-void
-HypreDriver::getSolution_NC(const int matl)
-{
-  throw InternalError("getSolution is not implemented for NC variables",
-                      __FILE__, __LINE__);
-}
-void
 HypreDriver::getSolution_CC(const int matl)
 {
   throw InternalError("getSolution is not implemented for CC variables",
                       __FILE__, __LINE__);
 }
+
+//__________________________________
 
 bool
 HypreDriver::isConvertable(const HypreInterface& to)
@@ -126,49 +73,9 @@ HypreDriver::isConvertable(const HypreInterface& to)
   return false;
 } // end isConvertable()
 
-//_____________________________________________________________________
-// Function TypeTemplate2Enum~
-// Convert a template variable type to an enum variarable type.
-// Add instantiations for every new variable type that is later added.
-//_____________________________________________________________________
+//__________________________________
 
 namespace Uintah {
-
-  template<>
-  TypeDescription::Type
-  TypeTemplate2Enum(const SFCXTypes& /*t*/)
-  {
-    return TypeDescription::SFCXVariable;
-  }
-
-  template<>
-  TypeDescription::Type
-  TypeTemplate2Enum(const SFCYTypes& /*t*/)
-  {
-    return TypeDescription::SFCYVariable;
-  }
-
-  template<>
-  TypeDescription::Type
-  TypeTemplate2Enum(const SFCZTypes& /*t*/)
-  {
-    return TypeDescription::SFCZVariable;
-  }
-
-  template<>
-  TypeDescription::Type
-  TypeTemplate2Enum(const NCTypes& /*t*/)
-  {
-    return TypeDescription::NCVariable;
-  }
-
-  template<>
-  TypeDescription::Type
-  TypeTemplate2Enum(const CCTypes& /*t*/)
-  {
-    return TypeDescription::CCVariable;
-  }
-
   HypreDriver*
   newHypreDriver(const HypreInterface& interface,
                  const Level* level,
@@ -202,46 +109,7 @@ namespace Uintah {
     } // end switch (interface)
   }
 
-  //_____________________________________________________________________
-  // HypreInterface operations
-  //_____________________________________________________________________
-
-  HypreInterface& operator++(HypreInterface &s)
-  {
-    return s = HypreInterface(2*s);
-  }
-  
-  std::ostream&
-  operator << (std::ostream& os, const HypreInterface& i)
-    // Write Hypre interface i to the stream os.
-  {
-    if      (i == HypreStruct     ) os << "HypreStruct (1)";
-    else if (i == HypreSStruct    ) os << "HypreSStruct (2)";
-    else if (i == HypreParCSR     ) os << "HypreParCSR (4)";
-    else if (i == HypreInterfaceNA) os << "HypreInterfaceNA (8)";
-    else os << "N/A";
-    return os;
-  }
-
-  //_____________________________________________________________________
-  // BoxSide operations
-  //_____________________________________________________________________
-
-  BoxSide& operator++(BoxSide &s)
-  {
-    return s = BoxSide(s+2);
-  }
-  
-  std::ostream&
-  operator << (std::ostream& os, const BoxSide& s)
-    // Write side s to the stream os.
-  {
-    if      (s == LeftSide ) os << "Left ";
-    else if (s == RightSide) os << "Right";
-    else os << "N/A";
-    return os;
-  }
-
+ //__________________________________
   BoxSide
   patchFaceSide(const Patch::FaceType& patchFace)
     // Correspondence between Uintah patch face and their BoxSide
