05656024ebf7cea52c789217d85280c9096a61b8
Ashwin Deepak Swamin <ashwinds@sci.utah.edu>
2006-05-22 18:00:50 +0000

    Added a table to hold list of ports, in ComponentWizardDialog

--- src/CCA/Components/GUIBuilder/ComponentWizardDialog.cc
+++ src/CCA/Components/GUIBuilder/ComponentWizardDialog.cc
@@ -46,6 +46,7 @@ BEGIN_EVENT_TABLE(ComponentWizardDialog, wxDialog)
   EVT_BUTTON( wxID_OK, ComponentWizardDialog::OnOk )
   EVT_BUTTON( ID_AddProvidesPort, ComponentWizardDialog::OnAddProvidesPort )
   EVT_BUTTON( ID_AddUsesPort, ComponentWizardDialog::OnAddUsesPort )
+  EVT_BUTTON( ID_RemovePort, ComponentWizardDialog::OnRemovePort )
 END_EVENT_TABLE()
 
 ComponentWizardDialog::ComponentWizardDialog(wxWindow *parent, wxWindowID id, const wxString &title,
@@ -53,9 +54,11 @@ ComponentWizardDialog::ComponentWizardDialog(wxWindow *parent, wxWindowID id, co
 					     long style, const wxString& name)
     : wxDialog( parent, id, title, pos, size, style)
 {
-  wxBoxSizer *topSizer = new wxBoxSizer( wxVERTICAL );
+  count_table=0;
+  // wxBoxSizer *topSizer = new wxBoxSizer( wxVERTICAL );
+  wxFlexGridSizer *topSizer = new wxFlexGridSizer(4,1,2,2);
 
-  topSizer->AddSpacer(10);
+  topSizer->AddSpacer(20);
   wxBoxSizer *componentSizer = new wxBoxSizer( wxHORIZONTAL );
   const int centerFlags = wxALIGN_CENTER|wxLEFT|wxRIGHT|wxALIGN_CENTER_VERTICAL;
   const int leftFlags = wxALIGN_LEFT|wxLEFT|wxRIGHT|wxALIGN_CENTER_VERTICAL;
@@ -64,26 +67,47 @@ ComponentWizardDialog::ComponentWizardDialog(wxWindow *parent, wxWindowID id, co
   componentSizer->Add(new wxStaticText(this, wxID_ANY, wxT("Component Name")), 0, centerFlags, 2);
   componentName = new wxTextCtrl( this, wxID_ANY, wxT(""), wxDefaultPosition, wxSize(150, wxDefaultSize.GetHeight()));
   componentName->SetToolTip(wxT("The Name of this component\nUsually begins with a capital letter\nExample: Hello,Linsolver..etc"));
-  componentSizer->Add(componentName, 1, rightFlags, 2);
-  topSizer->Add( componentSizer, 1, wxALIGN_CENTER, 0 );
-  topSizer->AddSpacer(30);
+  componentSizer->Add(componentName, 0, rightFlags, 2);
+  topSizer->Add( componentSizer, 0, wxALIGN_CENTER, 0 );
+  topSizer->AddSpacer(15);
 
   wxBoxSizer *portsSizer = new wxBoxSizer( wxHORIZONTAL );
-  portsSizer->Add(new wxButton(this, ID_AddProvidesPort, wxT("Add Provides Port")), 1, leftFlags, 4);
-  portsSizer->Add(new wxButton(this, ID_AddUsesPort, wxT("Add Uses Port")), 1, rightFlags, 4);
-  topSizer->Add( portsSizer, 1, wxALIGN_CENTER, 0 );
-  topSizer->AddSpacer(10);
+  portsSizer->Add(new wxButton(this, ID_AddProvidesPort, wxT("Add &Provides Port")), 1, leftFlags, 4);
+  portsSizer->Add(new wxButton(this, ID_AddUsesPort, wxT("Add &Uses Port")), 1, rightFlags, 4);
+  portsSizer->Add(new wxButton(this, ID_RemovePort, wxT("&Remove Port")), 1, rightFlags, 4);
+  topSizer->Add( portsSizer, 0, wxALIGN_CENTER, 0 );
+  topSizer->AddSpacer(20);
 
+
+  wxBoxSizer *gridSizer = new wxBoxSizer( wxVERTICAL);
+  listofPorts = new wxGrid(this, wxID_ANY,wxDefaultPosition ,wxSize(420,150));
+  listofPorts->CreateGrid(4,4);
+  listofPorts->SetColLabelValue(0,"Port Class");
+  listofPorts->SetColLabelValue(1,"Data Type");
+  listofPorts->SetColLabelValue(2,"Port Name");
+  listofPorts->SetColLabelValue(3,"Port Type");
+  gridSizer->Add(new wxStaticText(this, wxID_ANY, wxT("List of Ports")), 0, leftFlags,2);
+  gridSizer->AddSpacer(10);
+  gridSizer->Add(listofPorts,0,centerFlags,0);
+  topSizer->Add( gridSizer, 0, wxALIGN_CENTER, 0 );
+  topSizer->AddSpacer(20);
+
+  
   wxBoxSizer *okCancelSizer = new wxBoxSizer( wxHORIZONTAL );
   okCancelSizer->Add( new wxButton( this, wxID_OK, wxT("&Generate") ), 1, leftFlags, 4 );
   okCancelSizer->Add( new wxButton( this, wxID_CANCEL, wxT("&Cancel") ), 1, rightFlags, 4 );
-  topSizer->Add( okCancelSizer, 1, wxALIGN_CENTER, 0 );
-  topSizer->AddSpacer(10);
+  topSizer->Add( okCancelSizer, 3, wxALIGN_CENTER, 0 );
+  topSizer->AddSpacer(5);
 
+
+  topSizer->AddGrowableRow(0);
+  topSizer->AddGrowableRow(1);
+  topSizer->AddGrowableRow(2);
+  topSizer->AddGrowableRow(3);
+  topSizer->AddGrowableCol(0);
   SetAutoLayout( TRUE );     // tell dialog to use sizer
   SetSizer( topSizer );      // actually set the sizer
-
-  topSizer->Fit( this );            // set size to minimum size as calculated by the sizer
+  topSizer->Fit( this );           // set size to minimum size as calculated by the sizer
   topSizer->SetSizeHints( this );   // set size hints to honour mininum size
 }
 
@@ -108,11 +132,28 @@ void ComponentWizardDialog::OnOk(wxCommandEvent& event)
 
        wxMessageBox(msg, wxT("Create Component"),
 	       wxOK | wxICON_INFORMATION, this);
-    } else {
-	ComponentSkeletonWriter newComponent(componentName->GetValue(),pp,up);
-	newComponent.GenerateCode();
-	event.Skip();
-    }
+    } 
+     else 
+       {
+	      pp.clear();
+	      up.clear();
+	  
+	      for(int i=0;i<count_table;i++)
+	      {
+		if(listofPorts->GetCellValue(i,3).compare("ProvidesPort")==0)
+		  {
+		     pp.push_back(new PortDescriptor(listofPorts->GetCellValue(i,0),listofPorts->GetCellValue(i,1),listofPorts->GetCellValue(i,2)));
+		  }
+	        if(listofPorts->GetCellValue(i,3).compare("UsesPort")==0)
+		  {
+		     up.push_back(new PortDescriptor(listofPorts->GetCellValue(i,0),listofPorts->GetCellValue(i,1),listofPorts->GetCellValue(i,2)));
+		  }
+	      }
+
+	     ComponentSkeletonWriter newComponent(componentName->GetValue(),pp,up);
+	     newComponent.GenerateCode();
+	     event.Skip();
+       }
 
 }
 
@@ -161,12 +202,13 @@ void ComponentWizardDialog::OnAddProvidesPort(wxCommandEvent& event)
 		   wxOK | wxICON_INFORMATION, this);
 
     } else {
-      PortDescriptor p(addpport.GetPortNameText(), addpport.GetDataTypeText(),addpport.GetDescriptionText());
-
-#if DEBUG
-      std::cout << p.GetClassName() << "\t" << p.GetType() << "\t" << p.GetUniqueName() << std::endl;
-#endif
-      pp.push_back(new PortDescriptor(addpport.GetPortNameText(), addpport.GetDataTypeText(),addpport.GetDescriptionText()));
+      count_table++;
+      int row=count_table-1;
+      listofPorts->InsertRows(row,1);
+      listofPorts->SetCellValue(row,0,addpport.GetPortNameText());
+      listofPorts->SetCellValue(row,1,addpport.GetDataTypeText());
+      listofPorts->SetCellValue(row,2,addpport.GetDescriptionText());
+      listofPorts->SetCellValue(row,3,"ProvidesPort");
     }
   }
 }
@@ -202,16 +244,28 @@ void ComponentWizardDialog::OnAddUsesPort(wxCommandEvent& event)
       wxMessageBox(msg, wxT("Add Uses Port"),
 		   wxOK | wxICON_INFORMATION, this);
     } else {
-      PortDescriptor p(addpport.GetPortNameText(), addpport.GetDataTypeText(),addpport.GetDescriptionText());
-#if DEBUG
-      std::cout << p.GetClassName() << "\t" << p.GetType() << "\t" << p.GetUniqueName() << std::endl;
-#endif
-      up.push_back(new PortDescriptor(addpport.GetPortNameText(), addpport.GetDataTypeText(), addpport.GetDescriptionText()));
-
+     
+      count_table++;
+      int row=count_table-1;
+      listofPorts->InsertRows(row,1);
+      listofPorts->SetCellValue(row,0,addpport.GetPortNameText());
+      listofPorts->SetCellValue(row,1,addpport.GetDataTypeText());
+      listofPorts->SetCellValue(row,2,addpport.GetDescriptionText());
+      listofPorts->SetCellValue(row,3,"UsesPort");
     }
   }
 }
+void ComponentWizardDialog::OnRemovePort(wxCommandEvent& event)
+{
+  count_table--;
+  wxArrayInt sel_rows = listofPorts->GetSelectedRows();
+  for(int row_num=0;row_num<(int)sel_rows.Count();row_num++)
+    {
+      listofPorts->DeleteRows(sel_rows.Item(row_num),1);
+    }
 
+
+}
 
 //////////////////////////////////////////////////////////////////////////
 // AddPortDialog helper class
