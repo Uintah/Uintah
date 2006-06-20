853e71666ba48f58047f8e306234f6495f356947
Zhaosheng Gao <zgao@sci.utah.edu>
2006-06-20 19:22:18 +0000

    add more mms schemes

---
+++ Packages/Uintah/StandAlone/compare_mms/LinearMMS.h
@@ -0,0 +1,26 @@
+#ifndef UINTAH_COMPARE_MMS_LinearMMS_H
+#define UINTAH_COMPARE_MMS_LinearMMS_H
+
+
+#include <Packages/Uintah/StandAlone/compare_mms/MMS.h>
+
+class LinearMMS : public MMS {
+
+public:
+  LinearMMS::LinearMMS(double cu, double cv, double cw, double cp, double p_ref);
+  LinearMMS::~LinearMMS();
+  double pressure( double x, double y, double z, double time );
+  double uVelocity( double x, double y, double z, double time );
+  double vVelocity( double x, double y, double z, double time );
+  double wVelocity( double x, double y, double z, double time );
+  
+  
+private:
+  double c_u;
+  double c_v;
+  double c_w;
+  double c_p;
+  double p_ref_;
+};
+
+#endif
