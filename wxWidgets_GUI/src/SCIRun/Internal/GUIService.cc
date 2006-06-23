dce9649f10900175211e02017c92c2413b0bdfd6
Ayla Khan <ayla@sci.utah.edu>
2006-06-23 19:27:26 +0000

    Adding new application loading files, gui service.

---
+++ wxWidgets_GUI/src/SCIRun/Internal/GUIService.cc
@@ -0,0 +1,69 @@
+// GUIService.cc
+
+#include <SCIRun/Internal/GUIService.h>
+#include <SCIRun/SCIRunFramework.h>
+#include <SCIRun/CCA/CCAException.h>
+
+namespace SCIRun {
+
+
+GUIService::GUIService(SCIRunFramework* framework)
+  : InternalFrameworkServiceInstance(framework, "internal:GUIService")
+{
+}
+
+sci::cca::ComponentID::pointer
+GUIService::createInstance(const std::string& instanceName, const std::string& className, const sci::cca::TypeMap::pointer& properties)
+{
+  if (instanceName.size()) {
+    if (framework->lookupComponent(instanceName) != 0) {
+      throw sci::cca::CCAException::pointer(new CCAException("Component instance name " + instanceName + " is not unique"));
+    }
+    return framework->createComponentInstance(instanceName, className, properties);
+  }
+  return framework->createComponentInstance(framework->getUniqueName(className), className, properties);
+}
+
+
+InternalFrameworkServiceInstance* GUIService::create(SCIRunFramework* framework)
+{
+  GUIService* n = new GUIService(framework);
+  return n;
+}
+
+sci::cca::Port::pointer
+GUIService::getService(const std::string&)
+{
+    return sci::cca::Port::pointer(this);
+}
+
+void GUIService::addBuilder(const std::string& builderName, const sci::cca::GUIBuilder::pointer& builder)
+{
+  GUIBuilderMap::iterator iter = builders.find(builderName);
+  if (iter != builders.end()) {
+    // TODO: need exception!
+    return;
+  }
+  builders[builderName] = builder;
+}
+
+void GUIService::removeBuilder(const std::string& builderName)
+{
+  GUIBuilderMap::iterator iter = builders.find(builderName);
+  if (iter != builders.end()) {
+    builders.erase(iter);
+    iter->second->deleteReference();
+  }
+}
+
+// void GUIService::builderDisplayMessage(const std::string& messageLine)
+// {
+// }
+
+// void GUIService::buildComponentMenus()
+// {
+//   for (GUIBuilderMap::iterator iter = builders.begin(); iter != builders.end(); iter++) {
+//   }
+// }
+
+}
