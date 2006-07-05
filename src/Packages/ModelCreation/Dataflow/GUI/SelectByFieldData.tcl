c97a4de410518eba8146a7141fe8d76084ed65f2
Jeroen Stinstra <jeroen@sci.utah.edu>
2006-07-05 20:24:43 +0000

    merging in my latest changes

--- src/Packages/ModelCreation/Dataflow/GUI/SelectByFieldData.tcl
+++ src/Packages/ModelCreation/Dataflow/GUI/SelectByFieldData.tcl
@@ -64,6 +64,8 @@ itcl_class ModelCreation_FieldsData_SelectByFieldData {
         option add *textBackground white	
         iwidgets::scrolledtext $function.function -height 60 -hscrollmode dynamic
         $function.function insert end [set $this-function]
+        bind $function.function <Leave> "$this update_text"
+                
         pack $w.ff -side top -anchor w -fill both 
         pack $function.function -side top -fill both 
 
---
+++ src/Packages/ModelCreation/Dataflow/Modules/FieldsCreate/IndexedDomainBoundary.cc
@@ -0,0 +1,69 @@
+/*
+   For more information, please see: http://software.sci.utah.edu
+
+   The MIT License
+
+   Copyright (c) 2004 Scientific Computing and Imaging Institute,
+   University of Utah.
+
+   License for the specific language governing rights and limitations under
+   Permission is hereby granted, free of charge, to any person obtaining a
+   copy of this software and associated documentation files (the "Software"),
+   to deal in the Software without restriction, including without limitation
+   the rights to use, copy, modify, merge, publish, distribute, sublicense,
+   and/or sell copies of the Software, and to permit persons to whom the
+   Software is furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included
+   in all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+   THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+   DEALINGS IN THE SOFTWARE.
+*/
+
+#include <Dataflow/Network/Module.h>
+#include <Core/Malloc/Allocator.h>
+
+#include <Core/Algorithms/Fields/FieldsAlgo.h>
+#include <Core/Datatypes/Field.h>
+#include <Dataflow/Network/Ports/FieldPort.h>
+
+namespace ModelCreation {
+
+using namespace SCIRun;
+
+class IndexedDomainBoundary : public Module {
+public:
+  IndexedDomainBoundary(GuiContext*);
+  virtual void execute();
+  
+};
+
+
+DECLARE_MAKER(IndexedDomainBoundary)
+IndexedDomainBoundary::IndexedDomainBoundary(GuiContext* ctx)
+  : Module("IndexedDomainBoundary", ctx, Source, "FieldsCreate", "ModelCreation")
+{
+}
+
+void IndexedDomainBoundary::execute()
+{
+  FieldHandle ifield, ofield;
+  MatrixHandle ElemLink;
+  SCIRunAlgo::FieldsAlgo algo(dynamic_cast<ProgressReporter *>(this));
+ 
+  if(!(get_input_handle("Field",ifield,true))) return;
+  if (ifield->is_property("ElemLink")) ifield->get_property("ElemLink",ElemLink);
+  
+  if(!(algo.IndexedDomainBoundary(ifield,ofield,ElemLink,0.0,0.0,false,true,false,false))) return;
+  
+  send_output_handle("Field",ofield,true);
+}
+
+} // End namespace ModelCreation
+
