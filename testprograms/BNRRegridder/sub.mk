840ce92fe1eb2393ec228edca97953a22071dd39
J. Davison de St. Germain <dav@sci.utah.edu>
2006-07-06 20:13:37 +0000

    M    CCA/Components/Regridder/BNRTask.h
    Indented, untabified, use c++ comments, pulled into 80 columns (this
    only effected the spacing on comments), other general readability
    updates.
    
    Killed the namespace pollution of "using namespace std/Uintah" in the
    .h file.
    
    Still needs to have class member variables updated to use naming
    convention (ie: _ on names).
    
    
    M    testprograms/sub.mk
    M    testprograms/BNRRegridder/sub.mk
    
    Add in full list of libraries to all compilation on SGI/Mac.
    
    M    testprograms/BNRRegridder/bnrtest.cc
    
    Use "using namespace Uintah" in the .cc file.
    
    Also:
    
    > const int X=700, Y=700;
    
    Please don't use global variables that have a single letter for their
    name... this makes it impossible to search quickly for them and in
    general is just a bad idea.  Please fix this when you get a chance.

--- testprograms/BNRRegridder/sub.mk
+++ testprograms/BNRRegridder/sub.mk
@@ -7,13 +7,21 @@ LIBS := $(MPI_LIBRARY)
 
 PROGRAM := $(SRCDIR)/bnrtest
 SRCS := $(SRCDIR)/bnrtest.cc
-PSELIBS := Packages/Uintah/CCA/Ports \
-           Packages/Uintah/Core/Parallel \
-					 Packages/Uintah/CCA/Components/Regridder \
-           Core/Thread
+
+PSELIBS := \
+        Core/Exceptions                          \
+        Core/Geometry                            \
+        Core/Thread                              \
+        Core/Util                                \
+        Packages/Uintah/Core/Disclosure          \
+        Packages/Uintah/Core/Exceptions          \
+        Packages/Uintah/Core/Grid                \
+        Packages/Uintah/Core/Parallel            \
+        Packages/Uintah/Core/Util                \
+        Packages/Uintah/CCA/Ports                \
+        Packages/Uintah/CCA/Components/Regridder
 
 LIBS := $(M_LIBRARY) $(MPI_LIBRARY)
 
 include $(SCIRUN_SCRIPTS)/program.mk
-
 
