e4c922541a87965907ca8af99d60c16f4762df04
Michael Callahan <callahan@sci.utah.edu>
2006-07-10 16:50:36 +0000

    Use GLEW to determine graphics capabilities.  This makes GLEW a requirement.

--- src/Core/Geom/ShaderProgramARB.cc
+++ src/Core/Geom/ShaderProgramARB.cc
@@ -44,145 +44,6 @@ using std::cerr;
 using std::endl;
 using std::string;
 
-#if defined(GL_ARB_fragment_program) || defined(GL_ATI_fragment_shader)
-#  ifndef GL_ARB_vertex_program
-#     define GL_VERTEX_PROGRAM_ARB 0x8620
-#     define GL_PROGRAM_ERROR_POSITION_ARB 0x864B
-#     define GL_PROGRAM_FORMAT_ASCII_ARB 0x8875
-#  endif /* GL_ARB_vertex_program */
-#  ifndef GL_ARB_fragment_program
-#    define GL_FRAGMENT_PROGRAM_ARB 0x8804
-#  endif /* GL_ARB_fragment_program */
-
-#  if !defined(GLX_ARB_get_proc_address) || !defined(GLX_GLXEXT_PROTOTYPES)
-      extern "C" void ( * glXGetProcAddressARB (const GLubyte *procName)) (void);
-#  endif /* !defined(GLX_ARB_get_proc_address) || !defined(GLX_GLXEXT_PROTOTYPES) */
-
-#  ifdef __APPLE__
-#    include <mach-o/dyld.h>
-#    include <stdlib.h>
-#    include <string.h>
-
-     static void *NSGLGetProcAddress (const GLubyte *name)
-     {
-       NSSymbol symbol;
-       char *symbolName;
-       /* prepend a '_' for the Unix C symbol mangling convention */
-       symbolName = (char*)malloc(strlen((const char *)name) + 2);
-       strcpy(symbolName+1, (const char *)name);
-       symbolName[0] = '_';
-       symbol = NULL;
-       if (NSIsSymbolNameDefined(symbolName))
-         symbol = NSLookupAndBindSymbol(symbolName);
-       free(symbolName);
-       return symbol ? NSAddressOfSymbol(symbol) : NULL;
-     }
-#    define getProcAddress(x) (NSGLGetProcAddress((const GLubyte*)x))
-#  else
-#    ifdef _WIN32
-#      define getProcAddress(x) (wglGetProcAddress((LPCSTR) x))
-#    else
-#      define getProcAddress(x) ((*glXGetProcAddressARB)((const GLubyte*)x))
-#    endif // _WIN32
-#  endif /* APPLE */
-
-
-typedef void (GLAPIENTRY * SCIPFNGLGENPROGRAMSARBPROC) (GLsizei n, GLuint* programs);
-typedef void (GLAPIENTRY * SCIPFNGLDELETEPROGRAMSARBPROC) (GLsizei n, const GLuint* programs);
-typedef void (GLAPIENTRY * SCIPFNGLBINDPROGRAMARBPROC) (GLenum target, GLuint program);
-typedef void (GLAPIENTRY * SCIPFNGLPROGRAMSTRINGARBPROC) (GLenum target, GLenum format, GLsizei len, const void* string);
-typedef GLboolean (GLAPIENTRY * SCIPFNGLISPROGRAMARBPROC) (GLuint program);
-typedef void (GLAPIENTRY * SCIPFNGLPROGRAMLOCALPARAMETER4FARBPROC) (GLenum target, GLuint index, GLfloat x, GLfloat y, GLfloat z, GLfloat w);
-
-static SCIPFNGLGENPROGRAMSARBPROC glGenProgramsARB_SCI = 0;
-static SCIPFNGLDELETEPROGRAMSARBPROC glDeleteProgramsARB_SCI = 0;
-static SCIPFNGLBINDPROGRAMARBPROC glBindProgramARB_SCI = 0;
-static SCIPFNGLPROGRAMSTRINGARBPROC glProgramStringARB_SCI = 0;
-static SCIPFNGLISPROGRAMARBPROC glIsProgramARB_SCI = 0;
-static SCIPFNGLPROGRAMLOCALPARAMETER4FARBPROC glProgramLocalParameter4fARB_SCI = 0;
-#endif /* GL_ARB_fragment_program */
-
-
-#ifdef _WIN32
-/* ----------------------- WGL_ARB_extensions_string ----------------------- */
-
-#ifndef WGL_ARB_extensions_string
-#define WGL_ARB_extensions_string 1
-
-typedef const char* (WINAPI * PFNWGLGETEXTENSIONSSTRINGARBPROC) (HDC hdc);
-
-
-#endif /* WGL_ARB_extensions_string */
-
-static PFNWGLGETEXTENSIONSSTRINGARBPROC wglGetExtensionsStringARB = 0;
-
-bool WGLisExtensionSupported(const char *extension)
-{
-  const size_t extlen = strlen(extension);
-  const char *supported = NULL;
-  
-  // Try To Use wglGetExtensionStringARB On Current DC, If Possible
-  if (!wglGetExtensionsStringARB)
-    wglGetExtensionsStringARB = 
-      (PFNWGLGETEXTENSIONSSTRINGARBPROC)wglGetProcAddress("wglGetExtensionsStringARB");
-  
-  if (wglGetExtensionsStringARB)
-    supported = wglGetExtensionsStringARB(wglGetCurrentDC());
-  
-  if (supported) {
-
-    for (const char* p = supported; ; p++)
-      {
-	// Advance p Up To The Next Possible Match
-	p = strstr(p, extension);
-	
-	if (p == NULL) {
-	  break;
-	}
-	
-	// Make Sure That Match Is At The Start Of The String Or That
-	// The Previous Char Is A Space, Or Else We Could Accidentally
-	// Match "wglFunkywglExtension" With "wglExtension"
-	
-	// Also, Make Sure That The Following Character Is Space Or NULL
-	// Or Else "wglExtensionTwo" Might Match "wglExtension"
-	if ((p==supported || p[-1]==' ') && (p[extlen]=='\0' || p[extlen]==' '))
-	  {
-	    return true;  // Match
-	  }
-      }
-  }
-
-  // If That Failed, Try Standard Opengl Extensions String
-    supported = (char*)glGetString(GL_EXTENSIONS);
-  
-  // If That Failed Too, Must Be No Extensions Supported
-  if (supported == NULL)
-    return false;
-  
-  // Begin Examination At Start Of String, Increment By 1 On False Match
-  for (const char* p = supported; ; p++)
-    {
-      // Advance p Up To The Next Possible Match
-      p = strstr(p, extension);
-      
-      if (p == NULL) {
-	return false;						// No Match
-      }
-      
-      // Make Sure That Match Is At The Start Of The String Or That
-      // The Previous Char Is A Space, Or Else We Could Accidentally
-      // Match "wglFunkywglExtension" With "wglExtension"
-      
-      // Also, Make Sure That The Following Character Is Space Or NULL
-      // Or Else "wglExtensionTwo" Might Match "wglExtension"
-      if ((p==supported || p[-1]==' ') && (p[extlen]=='\0' || p[extlen]==' ')) {
-	return true;						// Match
-      }
-    }
-}
-#endif // _WIN32
-
 
 namespace SCIRun {
 
@@ -203,18 +64,14 @@ ShaderProgramARB::~ShaderProgramARB ()
 bool
 ShaderProgramARB::valid()
 {
-#if defined(GL_ARB_fragment_program) || defined(GL_ATI_fragment_shader)
-  return shaders_supported() ? glIsProgramARB_SCI(id_) : false;
-#else
-  return false;
-#endif
+  return shaders_supported() ? glIsProgramARB(id_) : false;
 }
 
 
 void
 ShaderProgramARB::init_shaders_supported()
 {
-  if(!init_)
+  if (!init_)
   {
     ShaderProgramARB_init_Mutex.lock();
     if (!init_)
@@ -227,11 +84,12 @@ ShaderProgramARB::init_shaders_supported()
       else
       {
 	// Create a test context.
-
 	TkOpenGLContext *context =
 	  new TkOpenGLContext(".testforshadersupport", 0, 0,0);
 
 	context->make_current();
+
+        glewInit();
 
 	GLenum err = glGetError();
 	if (err != GL_NO_ERROR)
@@ -287,48 +145,13 @@ ShaderProgramARB::init_shaders_supported()
           }
         }
         max_texture_size_4_ = i;
-#endif
+#endif // !sgi
 
 	// Check for non-power-of-two texture support.
-#ifndef _WIN32
-  non_2_textures_ = 
-    gluCheckExtension((const GLubyte*)"GL_ARB_texture_non_power_of_two", 
-			    glGetString(GL_EXTENSIONS));
-#else
-  non_2_textures_ =
-    WGLisExtensionSupported("GL_ARB_texture_non_power_of_two");
-#endif
+        non_2_textures_ = GLEW_ARB_texture_non_power_of_two;
 
-#if defined(GL_ARB_fragment_program) || defined(GL_ATI_fragment_shader)
+	supported_ = GLEW_ARB_vertex_program && GLEW_ARB_fragment_program;
 
-	supported_ =
-
-#ifndef _WIN32
-	  gluCheckExtension((const GLubyte*)"GL_ARB_vertex_program", 
-			    glGetString(GL_EXTENSIONS)) &&
-	  gluCheckExtension((const GLubyte*)"GL_ARB_fragment_program", 
-			    glGetString(GL_EXTENSIONS)) &&
-#else
-	  WGLisExtensionSupported("GL_ARB_vertex_program") &&
-	  WGLisExtensionSupported("GL_ARB_fragment_program") &&
-
-#endif
-	  (glGenProgramsARB_SCI = (SCIPFNGLGENPROGRAMSARBPROC)
-	   getProcAddress("glGenProgramsARB")) &&
-	  (glDeleteProgramsARB_SCI = (SCIPFNGLDELETEPROGRAMSARBPROC)
-	   getProcAddress("glDeleteProgramsARB")) &&
-	  (glBindProgramARB_SCI = (SCIPFNGLBINDPROGRAMARBPROC) 
-	   getProcAddress("glBindProgramARB")) &&
-	  (glProgramStringARB_SCI = (SCIPFNGLPROGRAMSTRINGARBPROC)
-	   getProcAddress("glProgramStringARB")) &&
-	  (glIsProgramARB_SCI = (SCIPFNGLISPROGRAMARBPROC)
-	   getProcAddress("glIsProgramARB")) &&
-	  (glProgramLocalParameter4fARB_SCI = 
-	   (SCIPFNGLPROGRAMLOCALPARAMETER4FARBPROC)
-	   getProcAddress("glProgramLocalParameter4fARB"));
-#else
-	supported_ = false;
-#endif
 	delete context;
       }
       init_ = true;
@@ -367,12 +190,12 @@ ShaderProgramARB::texture_non_power_of_two()
 bool
 ShaderProgramARB::create()
 {
-#if defined(GL_ARB_fragment_program) || defined(GL_ATI_fragment_shader)
-  if(shaders_supported()) {
+  if (shaders_supported())
+  {
     glEnable(type_);
-    glGenProgramsARB_SCI(1, &id_);
-    glBindProgramARB_SCI(type_, id_);
-    glProgramStringARB_SCI(type_, GL_PROGRAM_FORMAT_ASCII_ARB,
+    glGenProgramsARB(1, &id_);
+    glBindProgramARB(type_, id_);
+    glProgramStringARB(type_, GL_PROGRAM_FORMAT_ASCII_ARB,
                            program_.length(), program_.c_str());
     if (glGetError() != GL_NO_ERROR)
     {
@@ -391,7 +214,7 @@ ShaderProgramARB::create()
       string underline = line;
       for (uint i=0; i<end-start+1; i++) underline[i] = '-';
       underline[position-start] = '#';
-      glBindProgramARB_SCI(type_, 0);
+      glBindProgramARB(type_, 0);
       switch(type_) {
       case GL_VERTEX_PROGRAM_ARB:
         cerr << "Vertex ";
@@ -410,7 +233,6 @@ ShaderProgramARB::create()
     }
     return false;
   }
-#endif
   
   return true;
 }
@@ -419,66 +241,60 @@ ShaderProgramARB::create()
 void
 ShaderProgramARB::destroy ()
 {
-#if defined(GL_ARB_fragment_program) || defined(GL_ATI_fragment_shader)
-  if(shaders_supported()) {
-    glDeleteProgramsARB_SCI(1, &id_);
+  if (shaders_supported())
+  {
+    glDeleteProgramsARB(1, &id_);
     id_ = 0;
   }
-#endif
 }
 
 void
 ShaderProgramARB::bind ()
 {
-#if defined(GL_ARB_fragment_program) || defined(GL_ATI_fragment_shader)
-  if(shaders_supported()) {
+  if (shaders_supported())
+  {
     glEnable(type_);
-    glBindProgramARB_SCI(type_, id_);
+    glBindProgramARB(type_, id_);
   }
-#endif
 }
 
 void
 ShaderProgramARB::release ()
 {
-#if defined(GL_ARB_fragment_program) || defined(GL_ATI_fragment_shader)
-  if(shaders_supported()) {
-    glBindProgramARB_SCI(type_, 0);
+  if (shaders_supported())
+  {
+    glBindProgramARB(type_, 0);
     glDisable(type_);
   }
-#endif
 }
 
 void
 ShaderProgramARB::setLocalParam(int i, float x, float y, float z, float w)
 {
-#if defined(GL_ARB_fragment_program) || defined(GL_ATI_fragment_shader)
-  if(shaders_supported()) {
-    glProgramLocalParameter4fARB_SCI(type_, i, x, y, z, w);
+  if (shaders_supported())
+  {
+    glProgramLocalParameter4fARB(type_, i, x, y, z, w);
   }
-#endif
 }
 
 VertexProgramARB::VertexProgramARB(const string& program)
   : ShaderProgramARB(program)
 {
-#if defined(GL_ARB_fragment_program) || defined(GL_ATI_fragment_shader)
   type_ = GL_VERTEX_PROGRAM_ARB;
-#endif
 }
 
 VertexProgramARB::~VertexProgramARB()
-{}
+{
+}
 
 FragmentProgramARB::FragmentProgramARB(const string& program)
   : ShaderProgramARB(program)
 {
-#if defined(GL_ARB_fragment_program) || defined(GL_ATI_fragment_shader)
   type_ = GL_FRAGMENT_PROGRAM_ARB;
-#endif
 }
 
 FragmentProgramARB::~FragmentProgramARB()
-{}
+{
+}
 
 } // end namespace SCIRun
