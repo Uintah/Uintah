1aa72bdbba455dc0c8da210bb806e895e0aaeb10
Jeroen Stinstra <jeroen@sci.utah.edu>
2006-07-17 16:47:38 +0000

    Fix NaN support

--- event_management/src/Dataflow/Modules/Math/CreateMatrix.cc
+++ event_management/src/Dataflow/Modules/Math/CreateMatrix.cc
@@ -1,11 +1,30 @@
 /*
- *  CreateMatrix.cc:
- *
- *  Written by:
- *   jeroen
- *   TODAY'S DATE HERE
- *
- */
+   For more information, please see: http://software.sci.utah.edu
+
+   The MIT License
+
+   Copyright (c) 2004 Scientific Computing and Imaging Institute,
+   University of Utah.
+
+   License for the specific language governing rights and limitations under
+   Permission is hereby granted, free of charge, to any person obtaining a
+   copy of this software and associated documentation files (the "Software"),
+   to deal in the Software without restriction, including without limitation
+   the rights to use, copy, modify, merge, publish, distribute, sublicense,
+   and/or sell copies of the Software, and to permit persons to whom the
+   Software is furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included
+   in all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+   THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+   DEALINGS IN THE SOFTWARE.
+*/
 
 #include <Dataflow/Network/Module.h>
 #include <Core/Malloc/Allocator.h>
@@ -13,6 +32,12 @@
 #include <Dataflow/Network/Ports/MatrixPort.h>
 #include <Core/Datatypes/DenseColMajMatrix.h>
 #include <Core/Datatypes/DenseMatrix.h>
+
+#include <sci_defs/teem_defs.h>
+
+#ifdef HAVE_TEEM
+#include <teem/air.h>
+#endif
 
 namespace SCIRun {
 
@@ -80,26 +105,27 @@ CreateMatrix::execute()
     {
       char ibuf[5];
       iss.clear();
-      iss.get(ibuf,3);
+      iss.get(ibuf,4);
+      
       // Make sure the comparison is case insensitive.
       airToLower(ibuf);
       if (strncmp(ibuf,"nan",3)==0)
       {
-        data = (double) AIR_NAN;
+        ptr[p] = static_cast<double>(AIR_NAN);
       }
       else if (strncmp(ibuf,"inf",3)==0)
       {
-        data = (double) AIR_POS_INF;
+        ptr[p] = static_cast<double>(AIR_POS_INF);
       }
       else
       {
         iss.clear();
-        iss.get(&(ibuf[3]),1);
+        iss.get(&(ibuf[3]),2);
         // Set the last character to null for airToLower.
         airToLower(ibuf);
         if (strncmp(ibuf,"-inf",4)==0)
         {
-          data = (double) AIR_NEG_INF;
+          ptr[p] = static_cast<double>(AIR_NEG_INF);
         }  	  	
         else
         {
