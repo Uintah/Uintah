b4264ae7a722a399018a3e589371d6b2d38f01e2
Jeroen Stinstra <jeroen@sci.utah.edu>
2006-07-24 00:40:52 +0000

    move code from trunk to branch

--- event_management/src/Dataflow/Modules/Fields/IsoRefine.cc
+++ event_management/src/Dataflow/Modules/Fields/IsoRefine.cc
@@ -114,13 +114,9 @@ void
 IsoRefine::execute()
 {
   // Get input field.
-  FieldIPort *ifp = (FieldIPort *)get_iport("Input");
   FieldHandle ifieldhandle;
-  if (!(ifp->get(ifieldhandle) && ifieldhandle.get_rep()))
-  {
-    return;
-  }
-
+  if (!get_input_handle("Input", ifieldhandle)) return;
+  
   MatrixIPort *imp = (MatrixIPort *)get_iport("Optional Isovalue");
   MatrixHandle isomat;
   if (imp->get(isomat) && isomat.get_rep() &&
