7d983d48e2fd3fc4c21e03882b8e9d6cf601b084
Changwei Xiong <xiongcw@sci.utah.edu>
2006-08-01 22:00:35 +0000

    Unsteady_Burn model

--- src/Packages/Uintah/CCA/Components/Models/HEChem/sub.mk
+++ src/Packages/Uintah/CCA/Components/Models/HEChem/sub.mk
@@ -3,8 +3,9 @@
 SRCDIR   := Packages/Uintah/CCA/Components/Models/HEChem
 
 SRCS	+= \
-       $(SRCDIR)/Simple_Burn.cc \
-       $(SRCDIR)/Steady_Burn.cc \
-       $(SRCDIR)/IandG.cc       \
-       $(SRCDIR)/LightTime.cc   \
+       $(SRCDIR)/Simple_Burn.cc   \
+       $(SRCDIR)/Steady_Burn.cc   \
+       $(SRCDIR)/Unsteady_Burn.cc \
+       $(SRCDIR)/IandG.cc         \
+       $(SRCDIR)/LightTime.cc     \
        $(SRCDIR)/JWLpp.cc
