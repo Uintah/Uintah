9ffb533f3a5575632682636dcabdf3dc2f5443c8
Darby Brown <darbyb@sci.utah.edu>
2006-08-10 18:59:32 +0000

    Use port 8081.

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,4 +1,4 @@
 SET (CTEST_PROJECT_NAME "SCIRUN")
 SET (CTEST_DROP_METHOD "xmlrpc")
-SET (CTEST_DROP_SITE "http://dashboards.sci.utah.edu:80")
+SET (CTEST_DROP_SITE "http://dashboards.sci.utah.edu:8081")
 SET (CTEST_DROP_LOCATION "SCIRun")
