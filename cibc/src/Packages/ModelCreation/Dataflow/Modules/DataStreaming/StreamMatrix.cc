6c572448a1375b922c63c36b62f23c505f975486
Jeroen Stinstra <jeroen@sci.utah.edu>
2006-08-29 04:24:03 +0000

    fix warnings

--- cibc/src/Packages/ModelCreation/Dataflow/Modules/DataStreaming/StreamMatrix.cc
+++ cibc/src/Packages/ModelCreation/Dataflow/Modules/DataStreaming/StreamMatrix.cc
@@ -120,7 +120,7 @@ void StreamMatrix::execute()
   
   if (!(datafile_.open(filename)))
   {
-    error("Could not open hea der file: " + filename);
+    error("Could not open header file: " + filename);
     return;
   }
   
@@ -278,7 +278,8 @@ void StreamMatrix::execute()
       ScaledIndices.detach();
       double* data = ScaledIndices->get_data_pointer();
       double spacing = datafile_.get_rowspacing();
-      for (unsigned int p=0; p<ScaledIndices->get_data_size(); p++)
+
+      for (size_t p=0;p<ScaledIndices->get_data_size();p++)
       {
         data[p] = spacing * data[p];
       }
@@ -298,7 +299,8 @@ void StreamMatrix::execute()
       ScaledIndices.detach();
       double* data = ScaledIndices->get_data_pointer();
       double spacing = datafile_.get_colspacing();
-      for (unsigned int p=0; p<ScaledIndices->get_data_size(); p++)
+
+      for (size_t p=0;p<ScaledIndices->get_data_size();p++)
       {
         data[p] = spacing * data[p];
       }
