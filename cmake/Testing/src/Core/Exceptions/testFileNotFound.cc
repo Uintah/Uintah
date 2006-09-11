449c7dac355d10ca0997964a8da21ee3ea30f796
McKay Davis <mdavis@sci.utah.edu>
2006-09-11 23:13:51 +0000

    Remove cmake branch. All cmake development will now happen in cibc branch.

--- cmake/Testing/src/Core/Exceptions/testFileNotFound.cc
+++
@@ -1,8 +0,0 @@
-#include <Core/Exceptions/FileNotFound.h>
-
-int main()
-{
-  SCIRun::FileNotFound *ex = new SCIRun::FileNotFound("Error","filename.txt",0);
-  delete ex;
-  return 0;
-}
--- cmake/src/StandAlone/Apps/Painter/CTestConfig.cmake
+++
@@ -1,4 +0,0 @@
-SET (CTEST_PROJECT_NAME "Painter")
-SET (CTEST_DROP_METHOD "xmlrpc")
-SET (CTEST_DROP_SITE "http://dashboards.sci.utah.edu:8081")
-SET (CTEST_DROP_LOCATION "Painter")
