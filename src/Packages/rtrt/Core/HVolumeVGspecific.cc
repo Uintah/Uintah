3b9fa8d4915369c6c975af3eb7a549bb5bc3cfa9
James Bigler <bigler@sci.utah.edu>
2006-09-27 06:35:42 +0000

    Core/HVolumeVG.h Core/HVolumeVGspecific.cc
      GCC 4.1 seems to be a bit more picky about template specialization.
      This checking fixes this.  You declare the specialized function in
      the header and then make sure the function implementations are
      inside the appropriate namespace.

--- src/Packages/rtrt/Core/HVolumeVGspecific.cc
+++ src/Packages/rtrt/Core/HVolumeVGspecific.cc
@@ -4,30 +4,34 @@
 
 using rtrt::VG;
 
-template<>
-void VG<unsigned short, unsigned short>::setmin()
-{
-  v=0;
-  g=0;
-}
+namespace rtrt {
+  
+  template<>
+  void VG<unsigned short, unsigned short>::setmin()
+  {
+    v=0;
+    g=0;
+  }
 
-template<>
-void VG<unsigned short, unsigned short>::setmax()
-{
-  v=USHRT_MAX;
-  g=USHRT_MAX;
-}
+  template<>
+  void VG<unsigned short, unsigned short>::setmax()
+  {
+    v=USHRT_MAX;
+    g=USHRT_MAX;
+  }
 
-template<>
-void VG<unsigned char, unsigned char>::setmin()
-{
-  v=0;
-  g=0;
-}
+  template<>
+  void VG<unsigned char, unsigned char>::setmin()
+  {
+    v=0;
+    g=0;
+  }
 
-template<>
-void VG<unsigned char, unsigned char>::setmax()
-{
-  v=255;
-  g=255;
-}
+  template<>
+  void VG<unsigned char, unsigned char>::setmax()
+  {
+    v=255;
+    g=255;
+  }
+  
+} // end namespace rtrt
