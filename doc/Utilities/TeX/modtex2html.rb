cc9a212c54cef304d24c16ace7caa40ed6344123
McKay Davis <mdavis@sci.utah.edu>
2006-10-16 18:59:21 +0000

    Prune extraneous line from license info.
    Command used was:
    
    find . -type d \( -name .svn \) -prune -o -exec sed -i "s/License for the specific language governing rights and limitations under//g" {} \;

--- doc/Utilities/TeX/modtex2html.rb
+++ doc/Utilities/TeX/modtex2html.rb
@@ -6,7 +6,7 @@
 #  Copyright (c) 2004 Scientific Computing and Imaging Institute,
 #  University of Utah.
 # 
-#  License for the specific language governing rights and limitations under
+#  
 #  Permission is hereby granted, free of charge, to any person obtaining a
 #  copy of this software and associated documentation files (the "Software"),
 #  to deal in the Software without restriction, including without limitation
@@ -172,7 +172,7 @@ with the License.
 
 Software distributed under the License is distributed on an "AS IS"
 basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
-License for the specific language governing rights and limitations under
+
 the License.
 
 The Original Source Code is SCIRun, released March 12, 2001.
