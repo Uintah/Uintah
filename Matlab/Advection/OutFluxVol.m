fd02c57425852c0993c4daa775423a80b29869a8
Todd Harman <harman@sci.utah.edu>
2006-10-20 16:43:42 +0000

    plot(sumMass) advectSlabs()   - divide by dx(j)
    many small changes

--- Matlab/Advection/OutFluxVol.m
+++ Matlab/Advection/OutFluxVol.m
@@ -6,19 +6,19 @@ function[ofs, rx] = OutFluxVol(xvel_FC, delT, dx, nCells)
   clear j;
   fprintf( 'inside OutFluxVol \n');
   for( j =1:nCells)
+    
+    % flow is to the right
     if xvel_FC(j) >= 0
       dx_slab = xvel_FC(j) * delT;
       rx(j) = dx(j)/2.0 - dx_slab/2.0;
     end
-    
+    % flow is to the left
     if xvel_FC(j) < 0
       dx_slab = xvel_FC(j) * delT;
       rx(j) = dx_slab/2.0 - dx(j)/2.0;
     end
     
     %outflux volumes
-    dz = 1.0;
-    dy = 1.0;
-    ofs(j) = dx_slab * dz * dy;
+    ofs(j) = dx_slab;
   end
 end
