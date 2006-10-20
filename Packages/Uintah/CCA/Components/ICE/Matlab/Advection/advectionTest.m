d8035e001bbf2c9a01031c080eabe686702a8053
Todd Harman <harman@sci.utah.edu>
2006-10-20 16:43:42 +0000

    plot(sumMass) advectSlabs()   - divide by dx(j)
    many small changes

--- Packages/Uintah/CCA/Components/ICE/Matlab/Advection/advectionTest.m
+++ Packages/Uintah/CCA/Components/ICE/Matlab/Advection/advectionTest.m
@@ -16,14 +16,20 @@ set(0,'DefaultFigurePosition',[0,0,1024,768]);
 %______________________________________________________________________
 %     Problem Setup
 nCells   = 100;             % number of cells
-CFL      = 0.999;         
-velocity = 1.0;             % uniform velocity
+CFL      = 0.75;         
+velocity = -3.125;             % uniform velocity
+density  = 1.0;
 delT     = 1000;
-dx_initial  = 1.0;
-dx          = [1:nCells];   % cell length
-gradLim     =[1:nCells];    %gradient Limiter 
-grad_x      =[1:nCells];    %gradient
+nTimesteps  = 50;
+dx_initial  = 0.01;
 
+refineRatio = 2;
+
+gradLim     =[0:nCells+1];    %gradient Limiter 
+grad_x      =[0:nCells+1];    %gradient
+
+x           =[0:nCells+1];  % CC position
+dx          =[0:nCells+1];  % cell length
 q           =[0:nCells+1];  
 q_advected  =[0:nCells+1];  % advected value
 xvel_FC     =[0:nCells+1];  % face-centered vel (u)
@@ -37,28 +43,49 @@ mass_vrtx_2 =[0:nCells+1];  % --------//-------
 
 %__________________________________
 %     Initialization    
+x(1) = 0;
+dx = dx_initial;
+for(j = 2:nCells+1 )
+  dx(j) = dx_initial;
+  
+  % fine level
+  if(x(j) > 3 & x(j) < 5 )
+    dx(j) = dx_initial;
+  end
+  x(j) = x(j-1) + dx(j);
+end
+
+% Top hat distribution for mass 
+% linear distribution for temperature
 for(j = 1:nCells+1 )
-  dx(j)    = dx_initial;
   xvel_FC(j) = velocity;
-  mass(j)  = 0.5;
-  temp(j)  = 0.0;
+  mass(j)  = density;
+  temp(j)  = j/nCells;
+  
   if (j >10) & (j < 30)
-    mass(j)  = 0.001;
-    temp(j)  = 1.0;
+    mass(j)  = (density*pi);
   end
 end
-for(j = 1:nCells+1 )
+
+sumMassInitial = sum(mass(1:nCells));
+fprintf('Initial conditions: sum Mass %e', sumMassInitial);
+
+
+% aggressive timestepping
+for(j = 1:nCells )
     delT = min(delT,(CFL* dx(j)/velocity));
 end
+
+delT
 %______________________________________________________________________
 %     Time integration loop
-for( t = 1:50)
+for( t = 1:nTimesteps)
   fprintf('\n___________________________________________%i\n', t);
-  
+ 
   %__________________________________
   % Compute Lagrangian Values
   for(j = 1:nCells+1 )
-    mass_L(j)     = mass(j);
+    mass_L(j)    = mass(j);
     temp_L(j)    = temp(j);
     int_eng_L(j) = mass_L(j) * temp_L(j);
   end
@@ -79,15 +106,20 @@ for( t = 1:50)
   end
   
   % plot results
-  subplot(4,1,1), plot(mass, '-+r')
-  xlim([0 100]);
+  subplot(4,1,1), plot(x(1:nCells),mass(1:nCells), '-+r')
+  xlim([0 x(nCells)]);
   legend('mass');
+  tit = sprintf('CFL %i',CFL);
+  title(tit);
   grid on;
   
-  subplot(4,1,2), plot(gradLim, '-+r')
-  xlim([0 100]);
+  subplot(4,1,2), plot(x(1:nCells-1),gradLim(1:nCells-1), '-+r')
+  xlim([0 x(nCells)]);
   legend('gradient Limiter mass')
   grid on;
+  
+  sumMass(t) = sum(mass(1:nCells))/sumMassInitial;
+  sumMass(t)
   %__________________________________
   %  I N T E R N A L   E N E R G Y
   % uses compatible flux limiter
@@ -99,18 +131,23 @@ for( t = 1:50)
   end
   
   % plot results
-  subplot(4,1,3), plot(temp,'-+b')
-  xlim([0 100]);
+  subplot(4,1,3), plot(x(1:nCells),temp(1:nCells),'-+b')
+  xlim([0 x(nCells)]);
   legend('Temperature');
   
-  subplot(4,1,4), plot(gradLim,'-+b')
-  xlim([0 100]);
+  subplot(4,1,4), plot(x(1:nCells-1),gradLim(1:nCells-1),'-+b')
+  xlim([0 x(nCells)]);
   legend('gradient Limiter Temperature');
-  grid on;
-  
+   grid on;
+   
   M(t) = getframe(gcf);
 end
 
+%______________
+% plot the conserved sumMass
+figure(2)
+plot(sumMass);
+ylim( [(1.0 -1e-15) (1.0 + 1e-15)] )
 
-hFig = figure;
-movie(hFig,M,2,10)
+%hFig = figure;
+%movie(hFig,M,2,10)
