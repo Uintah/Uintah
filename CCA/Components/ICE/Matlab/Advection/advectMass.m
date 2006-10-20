b78532dfb727b620062dfd04969883ca17921875
Todd Harman <harman@sci.utah.edu>
2006-10-20 16:43:42 +0000

    plot(sumMass) advectSlabs()   - divide by dx(j)
    many small changes

--- CCA/Components/ICE/Matlab/Advection/advectMass.m
+++ CCA/Components/ICE/Matlab/Advection/advectMass.m
@@ -5,7 +5,7 @@ function[q_advected,gradLim,grad_x, mass_slab, mass_vrtx_1, mass_vrtx_2] = advec
   
   [gradLim, grad_x, mass_vrtx_1, mass_vrtx_2] = gradientLimiter_Mass(mass, dx, nCells);
   [mass_slab, gradLim, grad_x]                = qAverageFluxMass(mass, rx, grad_x, gradLim, nCells);
-  q_advected                                  = advectSlabs(xvel_FC, mass_slab, ofs, nCells);
+  q_advected                                  = advectSlabs(xvel_FC, mass_slab, ofs, nCells,dx);
 
 end
 
@@ -16,14 +16,16 @@ end
 % are needed to compute the compatible flux limiter.
 function[gradLim, grad_x, q_vrtx_1, q_vrtx_2] = gradientLimiter_Mass(q,dx, nCells)
   fprintf('gradientLimiter_Mass\n');
-  for( j =2:nCells-1)
+  for( j =2:nCells)
   
-    alpha = dx(j+1)./dx(j-1);
+    %central difference non-uniform spacing
+    alpha       = dx(j+1)/dx(j-1);
     numerator   = q(j+1) + (alpha^2 - 1.0)* q(j) - alpha^2 * q(j-1);
     denominator = alpha * (alpha + 1.0) * dx(j-1);
-    grad_x(j) = numerator./denominator;
+    grad_x(j)   = numerator/denominator;
   
-    %grad_x(j) = (q(j+1) - q(j-1))./(2.0*dx(j));
+    %central difference uniform spacing 
+    %grad_x(j) = (q(j+1) - q(j-1))/(2.0*dx(j));
 
     %-----------q vertex min/max
     q_vrtx_1_tmp = q(j) + grad_x(j) * dx(j)/2.0;
@@ -37,23 +39,27 @@ function[gradLim, grad_x, q_vrtx_1, q_vrtx_2] = gradientLimiter_Mass(q,dx, nCell
     q_min = min(q(j+1), q(j-1));
 
     %----------gradient limiter
-    frac1 = (q_max - q(j))/(max( (q_vrtx_max - q(j)), 1e-100) );
+    smallNum = 1e-100;
+    frac1    = (q_max - q(j) + smallNum)/(q_vrtx_max - q(j) + smallNum );
     alphaMax = max(0,frac1);
-    frac2 = (q(j) - q_min)/(max( (q(j) - q_vrtx_min), 1e-100) );
+    frac2    = (q(j) - q_min + smallNum)/(q(j) - q_vrtx_min + smallNum );
     alphaMin = max(0,frac2);
 
     tmp        = min(1,alphaMax);
     gradLim(j) = min(tmp, alphaMin);
-        
+       
+    %-----------mass vertex (limited)
     q_vrtx_1(j) = q(j) + (grad_x(j) * gradLim(j) * dx(j)/2.0);
     q_vrtx_2(j) = q(j) - (grad_x(j) * gradLim(j) * dx(j)/2.0);
       
-%     if (q_vrtx_max > q_max) | (q_vrtx_min < q_min)
+%     if (q_vrtx_max > q_max) | (q_vrtx_min < q_min) 
 %       fprintf(' j %i grad_x %e gradLim %e alphaMax %e alphaMin %e\n',j, grad_x(j), gradLim(j), alphaMax, alphaMin);
 %       fprintf(' q %e q_max %e q_min %e, q_vrtx_max %e q_vrtx_min %e\n', q(j), q_max, q_min, q_vrtx_max, q_vrtx_min);
+%       fprintf(' q(j-1) %e q(j) %e q(j+1) %e \n', q(j-1), q(j), q(j+1));
 %       fprintf(' frac1 %e frac2 %e \n',frac1, frac2);
 %       fprintf(' q_vrtx_1 %e q_vrtx_2 %e \n\n',q_vrtx_1(j), q_vrtx_2(j));
 %     end
+    
   end
 end
   
@@ -64,7 +70,8 @@ end
 function[q_slab, gradLim, grad_x] = qAverageFluxMass(q, rx, grad_x, gradLim, nCells)
   clear j;
   fprintf( 'inside qAverageFluxRho \n');
-  for( j =1:nCells-1)
+  for( j =1:nCells)
     q_slab(j) = q(j) + grad_x(j) * gradLim(j) * rx(j);
+%   fprintf(' j %i  q_slab: %e q(j): %e grad_x(j): %e  gradLim(j): %e  rx(j): %e\n',j, q_slab(j),q(j), grad_x(j), gradLim(j), rx(j));
   end
 end
