a62db38677c0f7b9b2d8462437e1fae6264f5295
Todd Harman <harman@sci.utah.edu>
2006-10-23 16:56:18 +0000

    Final cleanup before putting these script back on the shelf.
    Works on non-uniform grid or the simulation of a CFI.

--- src/Packages/Uintah/CCA/Components/ICE/Matlab/Advection/advectionTest.m
+++ src/Packages/Uintah/CCA/Components/ICE/Matlab/Advection/advectionTest.m
@@ -15,59 +15,66 @@ close all;
 set(0,'DefaultFigurePosition',[0,0,1024,768]);
 %______________________________________________________________________
 %     Problem Setup
-nCells   = 100;             % number of cells
+nCells   = 100;               % number of cells
 CFL      = 0.75;         
-velocity = -3.125;             % uniform velocity
+velocity = 3.125;             % uniform velocity
 density  = 1.0;
 delT     = 1000;
 nTimesteps  = 50;
-dx_initial  = 0.01;
+dx_initial  = 0.01;          % initial value for dx
 
 refineRatio = 2;
+fl_begin    = 0.3;           % fine level begin
+fl_end      = 0.5;           % fine level end
 
-gradLim     =[0:nCells+1];    %gradient Limiter 
-grad_x      =[0:nCells+1];    %gradient
+gradLim     =[0:nCells+1];   %gradient Limiter 
+grad_x      =[0:nCells+1];   %gradient
 
-x           =[0:nCells+1];  % CC position
-dx          =[0:nCells+1];  % cell length
+x           =[0:nCells+1];   % CC position
+dx          =[0:nCells+1];   % cell length
 q           =[0:nCells+1];  
-q_advected  =[0:nCells+1];  % advected value
-xvel_FC     =[0:nCells+1];  % face-centered vel (u)
-mass        =[0:nCells+1];  % mass
-mass_L      =[0:nCells+1];  % mass (Lagrangian)
-int_eng_L   =[0:nCells+1];  % Internal Energy (Lagrangian)
-temp        =[0:nCells+1];  % Temperature
-mass_slab   =[0:nCells+1];  % mass in slab
-mass_vrtx_1 =[0:nCells+1];  % mass at vertex
-mass_vrtx_2 =[0:nCells+1];  % --------//-------
+q_advected  =[0:nCells+1];   % advected value
+xvel_FC     =[0:nCells+1];   % face-centered vel (u)
+mass        =[0:nCells+1];   % mass
+mass_L      =[0:nCells+1];   % mass (Lagrangian)
+int_eng_L   =[0:nCells+1];   % Internal Energy (Lagrangian)
+temp        =[0:nCells+1];   % Temperature
+mass_slab   =[0:nCells+1];   % mass in slab
+mass_vrtx_1 =[0:nCells+1];   % mass at vertex
+mass_vrtx_2 =[0:nCells+1];   % --------//-------
 
 %__________________________________
 %     Initialization    
-x(1) = 0;
-dx = dx_initial;
+x(1)  = 0;
+dx(1) = dx_initial;
+
 for(j = 2:nCells+1 )
   dx(j) = dx_initial;
   
   % fine level
-  if(x(j) > 3 & x(j) < 5 )
-    dx(j) = dx_initial;
+  if(x(j-1) > fl_begin) && (x(j-1) < fl_end) 
+    dx(j) = dx_initial/refineRatio;
   end
   x(j) = x(j-1) + dx(j);
 end
 
 % Top hat distribution for mass 
-% linear distribution for temperature
+% and temperature
 for(j = 1:nCells+1 )
   xvel_FC(j) = velocity;
-  mass(j)  = density;
-  temp(j)  = j/nCells;
+  mass(j)  = density * dx(j);
+  temp(j)  = x(j);
+  temp(j)   = 0.0;
   
-  if (j >10) & (j < 30)
+  if (j >20) & (j < 50)
     mass(j)  = (density*pi);
+    temp(j)   = 1.0;
   end
 end
 
-sumMassInitial = sum(mass(1:nCells));
+junk = [0:nCells+1];
+sum_int_engInitial = sum(junk(1:nCells));
+sumMassInitial     = sum(mass(1:nCells));
 fprintf('Initial conditions: sum Mass %e', sumMassInitial);
 
 
@@ -89,7 +96,9 @@ for( t = 1:nTimesteps)
     temp_L(j)    = temp(j);
     int_eng_L(j) = mass_L(j) * temp_L(j);
   end
-  
+  sumMass(t) = sum(mass_L(1:nCells))/sumMassInitial;
+  sumMass(t)
+  sumIntEng(t) = sum(int_eng_L(1:nCells)); 
   %__________________________________
   % Advect and advance in time 
   % compute the outflux volumes
@@ -113,13 +122,13 @@ for( t = 1:nTimesteps)
   title(tit);
   grid on;
   
+  
   subplot(4,1,2), plot(x(1:nCells-1),gradLim(1:nCells-1), '-+r')
   xlim([0 x(nCells)]);
   legend('gradient Limiter mass')
   grid on;
   
-  sumMass(t) = sum(mass(1:nCells))/sumMassInitial;
-  sumMass(t)
+
   %__________________________________
   %  I N T E R N A L   E N E R G Y
   % uses compatible flux limiter
@@ -134,20 +143,24 @@ for( t = 1:nTimesteps)
   subplot(4,1,3), plot(x(1:nCells),temp(1:nCells),'-+b')
   xlim([0 x(nCells)]);
   legend('Temperature');
+  grid on;
   
   subplot(4,1,4), plot(x(1:nCells-1),gradLim(1:nCells-1),'-+b')
   xlim([0 x(nCells)]);
   legend('gradient Limiter Temperature');
-   grid on;
-   
+  grid on;
+
   M(t) = getframe(gcf);
 end
 
 %______________
 % plot the conserved sumMass
-figure(2)
-plot(sumMass);
-ylim( [(1.0 -1e-15) (1.0 + 1e-15)] )
+ figure(2)
+ subplot(2,1,1),plot(sumMass);
+ legend('sumMass');
+ subplot(2,1,2),plot(sumIntEng);
+ legend('sumIntEng');
+%ylim( [(1.0 -1e-15) (1.0 + 1e-15)] )
 
 %hFig = figure;
 %movie(hFig,M,2,10)
