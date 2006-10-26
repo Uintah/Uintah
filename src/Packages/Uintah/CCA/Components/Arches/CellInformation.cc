a841594683294a1b66f0419cd2969f2b07c36419
Stanislav Borodai <borodai@sci.utah.edu>
2006-10-26 18:19:13 +0000

    Cell information now works in parallel for nouniform grid. Pressure projection seems to work for nonuniform grid too.

--- src/Packages/Uintah/CCA/Components/Arches/CellInformation.cc
+++ src/Packages/Uintah/CCA/Components/Arches/CellInformation.cc
@@ -19,8 +19,9 @@ CellInformation::CellInformation(const Patch* patch)
   IntVector domLo = patch->getGhostCellLowIndex(Arches::ONEGHOSTCELL);
   IntVector domHi = patch->getGhostCellHighIndex(Arches::ONEGHOSTCELL);
 
-  IntVector idxLo = patch->getCellFORTLowIndex();
-  IntVector idxHi = patch->getCellFORTHighIndex()+IntVector(1,1,1);
+  IntVector locationLo = patch->getGhostCellLowIndex(Arches::THREEGHOSTCELLS);
+  IntVector locationHi = patch->getGhostCellHighIndex(Arches::THREEGHOSTCELLS);
+
   IntVector idxLoU = patch->getSFCXFORTLowIndex();
   IntVector idxHiU = patch->getSFCXFORTHighIndex();
   IntVector idxLoV = patch->getSFCYFORTLowIndex();
@@ -28,21 +29,19 @@ CellInformation::CellInformation(const Patch* patch)
   IntVector idxLoW = patch->getCellFORTLowIndex();
   IntVector idxHiW = patch->getCellFORTHighIndex();
 
-  // cell information
-  xx.resize(domLo.x(), domHi.x());
-  yy.resize(domLo.y(), domHi.y());
-  zz.resize(domLo.z(), domHi.z());
+  // grid cell information
+  xx.resize(locationLo.x(), locationHi.x());
+  yy.resize(locationLo.y(), locationHi.y());
+  zz.resize(locationLo.z(), locationHi.z());
 
-  // cell grid information, for nonuniform grid it will be more
-  // complicated
   const Level* level = patch->getLevel();
-  for (int ii = domLo.x(); ii < domHi.x(); ii++)
-    xx[ii] = level->getCellPosition(IntVector(ii, domLo.y(), domLo.z())).x();
-  for (int ii = domLo.y(); ii < domHi.y(); ii++)
-    yy[ii] = level->getCellPosition(IntVector(domLo.x(), ii, domLo.z())).y();
-  for (int ii = domLo.z(); ii < domHi.z(); ii++)
-    zz[ii] = level->getCellPosition(IntVector(domLo.x(), domLo.y(), ii)).z();
-  
+  for (int ii = locationLo.x(); ii < locationHi.x(); ii++)
+    xx[ii] = level->getCellPosition(IntVector(ii, locationLo.y(), locationLo.z())).x();
+  for (int ii = locationLo.y(); ii < locationHi.y(); ii++)
+    yy[ii] = level->getCellPosition(IntVector(locationLo.x(), ii, locationLo.z())).y();
+  for (int ii = locationLo.z(); ii < locationHi.z(); ii++)
+    zz[ii] = level->getCellPosition(IntVector(locationLo.x(), locationLo.y(), ii)).z();
+
   //  allocate memory for x-dim arrays
   dxep.resize(domLo.x(), domHi.x());
   dxpw.resize(domLo.x(), domHi.x());
@@ -110,11 +109,18 @@ CellInformation::CellInformation(const Patch* patch)
   fac4w.resize(domLo.z(), domHi.z());
   kbsdw.resize(domLo.z(), domHi.z());
   // for fortran
-  idxHi = idxHi - IntVector(1,1,1);
   domHi = domHi - IntVector(1,1,1);
 
+  bool xminus = patch->getBCType(Patch::xminus) != Patch::Neighbor;
+  bool xplus =  patch->getBCType(Patch::xplus) != Patch::Neighbor;
+  bool yminus = patch->getBCType(Patch::yminus) != Patch::Neighbor;
+  bool yplus =  patch->getBCType(Patch::yplus) != Patch::Neighbor;
+  bool zminus = patch->getBCType(Patch::zminus) != Patch::Neighbor;
+  bool zplus =  patch->getBCType(Patch::zplus) != Patch::Neighbor;
+
+
   // for computing geometry parameters
-  fort_cellg(domLo, domHi, idxLo, idxHi, idxLoU, idxHiU, idxLoV, idxHiV,
+  fort_cellg(domLo, domHi, idxLoU, idxHiU, idxLoV, idxHiV,
 	     idxLoW, idxHiW,
 	     sew, sns, stb, sewu, snsv, stbw, dxep, dynp, dztp,
 	     dxepu, dynpv, dztpw, dxpw, dyps, dzpb, dxpwu, dypsv, dzpbw,
@@ -123,7 +129,7 @@ CellInformation::CellInformation(const Patch* patch)
 	     xx, xu, yy, yv, zz, zw, efac, wfac, enfac, sfac, tfac, bfac,
 	     fac1u, fac2u, fac3u, fac4u, fac1v, fac2v, fac3v, fac4v,
 	     fac1w, fac2w, fac3w, fac4w, iesdu, iwsdu, jnsdv, jssdv, 
-	     ktsdw, kbsdw);
+	     ktsdw, kbsdw,xminus,xplus,yminus,yplus,zminus,zplus);
 
 }
 
