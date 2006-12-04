812faf8f10b3955211fac5107dce4cd9b045a850
Kurt Zimmerman <kuzimmer@sci.utah.edu>
2006-12-04 23:13:36 +0000

    update tcl interface

--- Packages/Kurt/Dataflow/GUI/ParticleFlow.tcl
+++ Packages/Kurt/Dataflow/GUI/ParticleFlow.tcl
@@ -15,7 +15,7 @@ itcl_class Kurt_Visualization_ParticleFlow {
 
         frame $w.f
 	frame $w.f1
-        frame $w.f2 -borderwidth 2 -relief groove 
+        frame $w.f2 -borderwidth 2 -relief flat
         frame $w.f3 -borderwidth 2  
         frame $w.f4 -borderwidth 2 
         frame $w.f5 -borderwidth 2 -relief groove
@@ -30,29 +30,34 @@ itcl_class Kurt_Visualization_ParticleFlow {
         checkbutton $w.f1.freeze -text "Freeze Particles" \
             -variable $this-freeze-particles -command $n
         label $w.f2.l -text "Particle life time decrement"
-        scale $w.f2.scale -to 1.0 -from 0.0002 -orient horizontal \
-            -variable $this-time -resolution 0.0002 
+#         scale $w.f2.scale -to -1.0 -from -0.0001 -orient horizontal \
+#             -variable $this-time -resolution 0.0001 
+        expscale $w.f2.scale -variable $this-time -orient horizontal \
+             -label "Particle life time decrement"
         pack  $w.f1.animate $w.f1.freeze -anchor nw
-        pack $w.f2.l $w.f2.scale  -side top -expand yes -fill x
+        pack $w.f2.scale -side top -expand yes -fill x
 
         frame $w.f3.f1
         frame $w.f3.f2
         pack  $w.f3.f1 $w.f3.f2 -side left -expand yes -fill x -anchor w \
             -padx 2 -pady 2
 
-        label $w.f3.f1.l -text "iterations: "
-        entry $w.f3.f2.e -textvariable "$this-nsteps"
-        pack $w.f3.f1.l $w.f3.f2.e
+#         label $w.f3.f1.l -text "iterations: "
+#         entry $w.f3.f2.e -textvariable "$this-nsteps"
+#        pack $w.f3.f1.l $w.f3.f2.e
 
-        frame $w.f4.f1
-        frame $w.f4.f2
-        pack  $w.f4.f1 $w.f4.f2 -side left -expand yes -fill x -anchor w \
-            -padx 2 -pady 2
+#         frame $w.f4.f1
+#         frame $w.f4.f2
+#         pack  $w.f4.f1 $w.f4.f2 -side left -expand yes -fill x -anchor w \
+#             -padx 2 -pady 2
 
-        label $w.f4.f1.l -text "step size: "
-        scale $w.f4.f2.s -variable "$this-step-size" -from 0.00001 -to 1.0 \
-            -resolution 0.00002 -orient horizontal
-        pack $w.f4.f1.l $w.f4.f2.s -expand yes -fill x
+#        label $w.f4.f1.l -text "step size: "
+#         scale $w.f4.f2.s -variable "$this-step-size" -from 0.00001 -to 1.0 \
+#             -resolution 0.00002 -orient horizontal
+        expscale $w.f4.s -variable $this-step-size -orient horizontal \
+            -label "step size:  "
+#        pack $w.f4.f1.l $w.f4.f2.s -expand yes -fill x
+        pack $w.f4.s -side top -expand yes -fill x
 
         frame $w.f5.f1
         frame $w.f5.f2
@@ -60,7 +65,7 @@ itcl_class Kurt_Visualization_ParticleFlow {
             -padx 2 -pady 2
 
         label $w.f5.f1.l -text "2\u207f x 2\u207f  particles.  n = "
-        scale $w.f5.f2.s -variable "$this-nparticles" -from 1 -to 20 \
+        scale $w.f5.f2.s -variable "$this-nparticles" -from 1 -to 10 \
             -orient horizontal
         pack $w.f5.f1.l $w.f5.f2.s -expand yes -fill x -anchor s
 
@@ -71,8 +76,9 @@ itcl_class Kurt_Visualization_ParticleFlow {
 
         pack $w.recompute $w.reset -side top -expand yes -fill x
 
-        bind $w.f3.f2.e <Return> $n 
-        bind $w.f4.f2.s <ButtonRelease> $n 
+#         bind $w.f3.f2.e <Return> $n 
+#         bind $w.f4.f2.s <ButtonRelease> $n 
+         bind $w.f4.s <ButtonRelease> $n 
         bind $w.f5.f2.s <ButtonRelease> $n 
 
         makeSciButtonPanel $w $w $this
