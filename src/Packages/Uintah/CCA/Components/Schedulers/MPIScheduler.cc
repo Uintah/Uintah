5332ac5352bf168d5081a92f44c2b8bcfbecb466
Justin Luitjens <luitjens@sci.utah.edu>
2006-12-20 19:16:04 +0000

    Fixed a timing bug

--- src/Packages/Uintah/CCA/Components/Schedulers/MPIScheduler.cc
+++ src/Packages/Uintah/CCA/Components/Schedulers/MPIScheduler.cc
@@ -783,7 +783,7 @@ MPIScheduler::execute(int tgnum /*=0*/, int iteration /*=0*/)
   }
 
   if (d_sharedState != 0) { // subschedulers don't have a sharedState
-    d_sharedState->taskExecTime += mpi_info_.totaltask;
+    d_sharedState->taskExecTime += mpi_info_.totaltask - d_sharedState->outputTime; // don't count output time...
     d_sharedState->taskCommTime += mpi_info_.totalrecv + mpi_info_.totalsend + mpi_info_.totalreduce;
   }
 
