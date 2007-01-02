a9aff0e93d0e5fa902cb96d60bbdd80f94b32442
Todd Harman <harman@sci.utah.edu>
2007-01-02 20:51:27 +0000

    missing ;

--- Packages/Uintah/StandAlone/sub.mk
+++ Packages/Uintah/StandAlone/sub.mk
@@ -17,9 +17,9 @@ include $(SCIRUN_SCRIPTS)/recurse.mk
 #
 COMPONENTS      = Packages/Uintah/CCA/Components
 CA              = Packages/Uintah/CCA/Components/Arches
-#DUMMY_LIB      = Packages/Uintah/CCA/Components/Dummy
-ARCHES_SUB_LIBS = $(CA)/Mixing $(CA)/fortran $(CA)/Radiation $(CA)/Radiation/fortran
-ARCHES_LIBS     = $(COMPONENTS)/Arches $(COMPONENTS)/MPMArches
+DUMMY_LIB       = Packages/Uintah/CCA/Components/Dummy
+#ARCHES_SUB_LIBS= $(CA)/Mixing $(CA)/fortran $(CA)/Radiation $(CA)/Radiation/fortran
+#ARCHES_LIBS    = $(COMPONENTS)/Arches $(COMPONENTS)/MPMArches
 MPM_LIB         = Packages/Uintah/CCA/Components/MPM
 ICE_LIB         = Packages/Uintah/CCA/Components/ICE
 MPMICE_LIB      = Packages/Uintah/CCA/Components/MPMICE
@@ -477,7 +477,7 @@ link_regression_tester:
 	   fi )
 
 noFortran:
-	@( $(SRCTOP_ABS)/Packages/Uintah/scripts/noRadiation $(SRCTOP_ABS) \
+	@( $(SRCTOP_ABS)/Packages/Uintah/scripts/noRadiation $(SRCTOP_ABS) ; \
           $(SRCTOP_ABS)/Packages/Uintah/scripts/useFakeArches.sh $(OBJTOP_ABS) on)
        
 fake_arches:
