8e081791d386a4b253d8f92b078913d30a299231
Todd Harman <harman@sci.utah.edu>
2007-02-05 23:27:23 +0000

    - Attempt at adding radio buttons - By default the extra cells are not included when differencing   the mms solution and the computed.  User can control which extra cells are visualized.

--- src/Packages/Uintah/Dataflow/GUI/CompareMMS.tcl
+++ src/Packages/Uintah/Dataflow/GUI/CompareMMS.tcl
@@ -11,10 +11,16 @@ itcl_class Uintah_Operators_CompareMMS {
         global $this-field_name
         global $this-field_time
         global $this-output_choice
+        global $this-extraCells_x
+        global $this-extraCells_y
+        global $this-extraCells_z
         
         set $this-field_name "---"
         set $this-field_time "---"
         set $this-output_choice 2
+        set $this-extraCells_x 0
+        set $this-extraCells_y 0
+        set $this-extraCells_z 0
     }
 
     method ui {} {
@@ -24,22 +30,33 @@ itcl_class Uintah_Operators_CompareMMS {
             return
         }
         toplevel $w
-	wm minsize $w 400 150
-
+	wm minsize $w 200 100
+        
+        #  Variable name and time
         frame $w.f1
         pack  $w.f1
-
         label_pair $w.f1.field_name "Input Field Name" $this-field_name 20
         label_pair $w.f1.field_time "Input Field Time" $this-field_time 20
 
-        frame $w.output_choice -borderwidth 2 -relief groove
-
-        set the_var "$this-output_choice"
+        # include extra cells?
+        frame $w.f2 -borderwidth 2 -relief groove
+        pack  $w.f2
         set the_com "$this-c needexecute"
-        radiobutton $w.output_choice.original  -text "Original"    -value 0  -variable $the_var -command $the_com 
-        radiobutton $w.output_choice.exact     -text "Exact"       -value 1  -variable $the_var -command $the_com
-        radiobutton $w.output_choice.diff      -text "Difference"  -value 2  -variable $the_var -command $the_com
+        
+        #label -text "Include extra cells in difference calculation"
+        radiobutton $w.f2.extraCells_x  -text "(x)" -value 1  -variable $this-extraCells_x -command $the_com
+        radiobutton $w.f2.extraCells_y  -text "(y)" -value 1  -variable $this-extraCells_y -command $the_com
+        radiobutton $w.f2.extraCells_z  -text "(z)" -value 1  -variable $this-extraCells_z -command $the_com
+        
+        # Select the type of output you'd like
+        frame $w.output_choice -borderwidth 2 -relief groove
+        set the_var "$this-output_choice"
+        radiobutton $w.output_choice.original  -text "1) Original Data"                    -value 0  -variable $the_var -command $the_com 
+        radiobutton $w.output_choice.exact     -text "2) Method of Manufactured Solution"  -value 1  -variable $the_var -command $the_com
+        radiobutton $w.output_choice.diff      -text "Difference (1) - (2)"                -value 2  -variable $the_var -command $the_com
+        
         pack $w.output_choice.original $w.output_choice.exact $w.output_choice.diff -anchor w
+        pack $w.f2.extraCells_x $w.f2.extraCells_y $w.f2.extraCells_z  -anchor w
 
         pack $w.f1.field_name $w.f1.field_time -padx 10
         pack $w.output_choice -padx 10
