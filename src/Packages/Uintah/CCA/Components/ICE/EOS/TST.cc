bbb0da5e246e3821bb52bba03bb1471d4a633c73
Changwei Xiong <xiongcw@sci.utah.edu>
2007-02-26 22:12:59 +0000

    solver for cubic equation to replace the old iterative solver

--- src/Packages/Uintah/CCA/Components/ICE/EOS/TST.cc
+++ src/Packages/Uintah/CCA/Components/ICE/EOS/TST.cc
@@ -15,6 +15,16 @@ TST::TST(ProblemSpecP& ps){
   ps->require("u", u);
   ps->require("w", w);
   ps->require("Gamma", Gamma);
+
+  /* initialize all the constants */
+  bb   = b*b;
+  bu   = b*u;
+  bw   = b*w;
+  bbuw = bu*bw;
+  b1uw = b*(1-u-w);
+  bbuwuw = bb*(u+w-u*w);
+  bubw = bu+bw;
+  ab = a*b;
 }
 
 
@@ -37,109 +47,25 @@ void TST::outputProblemSpec(ProblemSpecP& ps){
 //__________________________________
 double TST::computeRhoMicro(double press, double gamma,
                             double cv, double Temp,double rho_guess){
-  Pressure = press;
-  Temperature = Temp;
-  SpecificHeat = cv;
+  /*
+   * This is solved by use of cubic equation solver
+   * c's are the coefficients of the cubic equation
+   */
+  double k  = (gamma-1)*cv*Temp;
+  double c1 = -press;
+  double c2 = press*b1uw + k;
+  double c3 = k*bubw - a + press*bbuwuw;
+  double c4 = (press*b+k)*bbuw + ab;
 
-  /* Use a hybrid Newton-Bisection Method to compute the rho_micro.
-     The solver guarantees to converge to a solution.
-  */
-  const double epsilon  = 1e-15;
-  const double frac = 0.85;
-  double rho_min = 0.0;     // Such that f(min) = -P < 0
-  double rho_max = frac/b;  // Such that f(max) > 0
-  if(func(rho_max) <= 0 ){
-    cout<<"Pressure="<<press<<", TST EOS cannot handle such high pressure. Please choose other EOS ... "<<endl;
-    exit(1);
-  }
-  
-  IL = rho_min;
-  IR = rho_max;
+  double p = fabs(c3/c1 - pow(c2/c1, 2.)/3.);
+  double q = pow((c2/c1),3.)/13.5 - c2*c3/(c1*c1)/3. + c4/c1;
 
-  double f = 0;
-  double df_drho = 0;
-  double delta_old, delta_new;
+  double phi = acos(-q / pow(p,1.5) * 2.5980762113533159402911695122588); 
+  //2.59807621135331594 = sqrt(27)/2.0; 
 
-  double rhoM = rho_guess <= rho_max ? rho_guess : rho_max/2.0;
-  double rhoM_start = rhoM;
- 
-  int iter = 0;  
-  while(1){
-    f = func(rhoM);
-    setInterval(f, rhoM);
-    
-    if(fabs((IL-IR)/rhoM)<epsilon){
-      return (IL+IR)/2.0;
-    }
-    
-    delta_new = 1e100;
-    while(1){
-      df_drho = deri(rhoM);
-      delta_old = delta_new;
-      delta_new = -f/df_drho; 
-      rhoM += delta_new;
+  double sp_v = 2.*sqrt(p/3.)*cos(phi/3.) - c2/c1/3.;
 
-      if(fabs(delta_new/rhoM)<epsilon){
-	return rhoM;
-      }      
-
-      if(iter>=100){
-	cout<<setprecision(15);
-	cout<<"Not converging after 100 iterations in TST.cc."<<endl;
-	cout<<"P="<<press<<" T="<<Temp<<" f="<<func(rhoM)<<" delta="<<delta_new
-	    <<" rhoM="<<rhoM<<" rho_start="<<rhoM_start<<" "<<rho_max<<endl;
-	exit(1);
-      }
-      
-      if(rhoM<IL || rhoM>IR || fabs(delta_new)>fabs(delta_old*0.7)){
-	break;
-      }
-      
-      f = func(rhoM);
-      setInterval(f, rhoM);      
-      iter++;
-    }
-    
-    rhoM = (IL+IR)/2.0;
-    iter++;
-  }
-  return rhoM;
-}
-
-
-double TST::func(double rhoM){
-  double P1 = (Gamma-1)*SpecificHeat*Temperature*rhoM/(1-b*rhoM);
-  double P2 =  -a*rhoM*rhoM/((1+u*b*rhoM)*(1+w*b*rhoM));
-  return P1 + P2 - Pressure;
-}
-
-
-double TST::deri(double rhoM){
-  double denom = 1-b*rhoM;
-  double P1 = (Gamma-1)*SpecificHeat*Temperature/(denom*denom);
-
-  double d1 = 1+u*b*rhoM;
-  double d2 = 1+w*b*rhoM;
-  denom = d1*d2;
-  double P2 = -2*a*rhoM/denom;
-
-  double com = a*b*rhoM*rhoM/denom;
-  double P3 =  com*(u/d1 + w/d2);
-
-  return P1 + P2 + P3;
-}
-
-
-void TST::setInterval(double f, double rhoM){
-  if(f < 0)   
-    IL = rhoM;
-  else if(f > 0)  
-    IR = rhoM;
-  else if(f ==0){    
-    IL = rhoM;
-    IR = rhoM; 
-  } 
-  return;
+  return  1./sp_v;
 }
 
 
@@ -147,16 +73,16 @@ void TST::setInterval(double f, double rhoM){
 // Return (1/v)*(dv/dT)  (constant pressure thermal expansivity)
 double TST::getAlpha(double Temp, double sp_v, double P, double cv){
   // Cheating here a bit, computing v*(dT/dv) and returning the inverse of that
-  double n1 = 2*P*sp_v + P*w*b + P*u*b;
-  double n2 = sp_v-b;
-  double n3 = P*sp_v*sp_v + P*sp_v*w*b + P*u*b*sp_v + P*u*b*b*w + a;
+  double n1 = 2*P*sp_v + P*bw + P*bu;
+  double n2 = sp_v - b;
+  double n3 = P*sp_v*sp_v + P*sp_v*(bu+bw) + P*bbuw + a;
 
-  double d1 = (sp_v+u*b) * (sp_v+w*b) * (Gamma-1) * cv;
-  double d2 = sp_v + u*b;
-  double d3 = sp_v + w*b;
+  double d1 = (sp_v+bu) * (sp_v+bw) * (Gamma-1) * cv;
+  double d2 = sp_v + bu;
+  double d3 = sp_v + bw;
 
-  double dT_dv = n1*n2/d1 - n3*n2/d1*(1/d2+1/d3) + n3/d1;
-  return 1/(sp_v*dT_dv);
+  double dT_dv = n1*n2/d1 - n3*n2/d1*(1./d2+1./d3) + n3/d1;
+  return 1./(sp_v*dT_dv);
 }
 
 
@@ -176,9 +102,9 @@ void TST::computeTempCC(const Patch* patch,
       IntVector c = *iter;
       sp_v = 1.0/rhoM[c];
       P = press[c];
-      n1 = sp_v-b;
-      n2 = P*sp_v*sp_v + P*sp_v*w*b + P*u*b*sp_v + P*u*b*b*w + a;
-      d1 = (sp_v+u*b) * (sp_v+w*b) * (Gamma-1) * cv[c];
+      n1 = sp_v - b;
+      n2 = P*sp_v*sp_v + P*sp_v*(bu+bw) + P*bbuw + a;
+      d1 = (sp_v+bu) * (sp_v+bw) * (Gamma-1) * cv[c];
       Temp[c]= n1*n2/d1;
     }
   } 
@@ -189,9 +115,9 @@ void TST::computeTempCC(const Patch* patch,
       IntVector c = *iter;
       sp_v = 1.0/rhoM[c];
       P = press[c];
-      n1 = sp_v-b;
-      n2 = P*sp_v*sp_v + P*sp_v*w*b + P*u*b*sp_v + P*u*b*b*w + a;
-      d1 = (sp_v+u*b) * (sp_v+w*b) * (Gamma-1) * cv[c];
+      n1 = sp_v - b;
+      n2 = P*sp_v*sp_v + P*sp_v*(bu+bw) + P*bbuw + a;
+      d1 = (sp_v+bu) * (sp_v+bw) * (Gamma-1) * cv[c];
       Temp[c]= n1*n2/d1;
     }
   }
@@ -207,12 +133,12 @@ void TST::computePressEOS(double rhoM, double gamma,
   // Pointwise computation of thermodynamic quantities
   double sp_v = 1/rhoM;
   double p1 = (Gamma-1)*cv*Temp/(sp_v-b);
-  double p2 = a/((sp_v+u*b)*(sp_v+w*b));
+  double p2 = a/((sp_v+bu)*(sp_v+bw));
   double d1 = sp_v - b;
-  double d2 = sp_v + u*b;
-  double d3 = sp_v + w*b;
+  double d2 = sp_v + bu;
+  double d3 = sp_v + bw;
 
-  press = p1-p2;
+  press   = p1-p2;
   dp_drho = sp_v*sp_v * (p1/d1 - p2*(1/d2+1/d3));
   dp_de   = (Gamma-1)/(sp_v-b);
   return;
