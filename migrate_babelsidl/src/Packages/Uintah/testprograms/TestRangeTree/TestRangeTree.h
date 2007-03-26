679ab58d776219e480e64429ffd3cea399cf70dc
Ayla Khan <ayla@sci.utah.edu>
2007-03-26 23:00:51 +0000

    Start babel migration from new branch.

--- migrate_babelsidl/src/Packages/Uintah/testprograms/TestRangeTree/TestRangeTree.h
+++
@@ -1,5 +0,0 @@
-#include "../TestSuite/SuiteTree.h"
-#include <Packages/Uintah/testprograms/TestRangeTree/share.h>
-
-SCISHARE SuiteTree* RangeTreeTestTree(bool verbose = false, int n = 100000,
-			     int seed = 0);
