1497a33984851572297a3fb9dea71a3671bad3f8
Justin Luitjens <luitjens@sci.utah.edu>
2007-04-03 16:54:52 +0000

    Also flush cout

--- src/Packages/Uintah/CCA/Components/SimulationController/SimulationController.cc
+++ src/Packages/Uintah/CCA/Components/SimulationController/SimulationController.cc
@@ -623,6 +623,7 @@ SimulationController::printSimulationStats ( int timestep, double delt, double t
 #endif
     dbg << message.str() << endl;
     dbg.flush();
+    cout.flush();
 
     if (stats.active() && d_myworld->size() > 1) {
       for (unsigned i = 1; i < statLabels.size(); i++) { // index 0 is memuse
