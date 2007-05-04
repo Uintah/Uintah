3dcdbb62b6c6df8f2fd282f98bd2f11a7279d767
Todd Harman <harman@sci.utah.edu>
2007-05-04 17:17:13 +0000

    small correction

--- Packages/Uintah/StandAlone/inputs/IS/ArchesHeatUpExplosion/plotHeatFlux.m
+++ Packages/Uintah/StandAlone/inputs/IS/ArchesHeatUpExplosion/plotHeatFlux.m
@@ -25,10 +25,10 @@ clear all;
 % USER INPUTS
 %'
 dir = 'case3'
-%dat = {'polynomial_47.dat', 'polynomial_48.dat','polynomial_49.dat','polynomial_50.dat','polynomial_51.dat','polynomial_52.dat'}
+%dat = {'polynomial_46.dat','polynomial_47.dat', 'polynomial_48.dat','polynomial_49.dat','polynomial_50.dat','polynomial_51.dat','polynomial_52.dat','polynomial_53.dat'}
 dat = {'polynomial_96.dat','polynomial_97.dat','polynomial_98.dat','polynomial_99.dat','polynomial_100.dat','polynomial_101.dat','polynomial_102.dat','polynomial_103.dat'}
 
-%legendText = {'47','48', '49', '50','51','52'}
+%legendText = {'46','47','48', '49', '50','51','52','53'}
 legendText = {'96','97', '98', '99','100','101','102','103'}
 
 symbol = {'-b','-r','-g','-c','-m','-k','.b','.r'};
@@ -42,6 +42,8 @@ for( i = 1:maxFiles )
   f = sprintf('%s/%s',dir,dat{i});
   
   coeff  = importdata(f)
+  
+  %coeff = [ 0, 45000, 0, 0, 0, 0]
   
   theta= 0:.01:2*pi;
   HT_Flux = coeff(2) + coeff(3)*sin(theta) + coeff(4)*cos(theta) + coeff(5)*sin(2*theta) + coeff(6)*cos(2*theta);
