293c76152f921dd814ee431ac5ace58a8a7e9757
Amjidanutpan Ramanuj <ramanuja@sci.utah.edu>
2007-06-22 16:45:34 +0000

    A command-line utility for error analysis of advection of passive scalar using ice.

---
+++ src/Packages/Uintah/StandAlone/tools/compare_mms/compare_scalar.h
@@ -0,0 +1,9 @@
+#ifndef _COMPARE_SCALAR_H_
+#define _COMPARE_SCALAR_H_
+
+// Checks if the double value can qualify as an integer
+// within the tolerence limits of tol.
+
+int is_int(double val, double tol);
+
+#endif
