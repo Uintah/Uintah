2c0d38b02415ac2e4a32b919d4795436e15f5bca
Todd Harman <harman@sci.utah.edu>
2007-08-01 23:42:37 +0000

    polynomial_ -> curveFit_coefs

--- src/Packages/Uintah/StandAlone/inputs/IS/ArchesHeatUpExplosion/plotHeatFlux.m
+++ src/Packages/Uintah/StandAlone/inputs/IS/ArchesHeatUpExplosion/plotHeatFlux.m
@@ -2,16 +2,16 @@
 %_________________________________
 % 05/01/07   --Todd
 % This matLab script plots the head flux curves
-% The format of the "polynomial_#.dat" files 
+% The format of the "curveFit_coefs_#.dat" files 
 % 
-% --start file polynomial_#.dat---
+% --start file curveFit_coefs_#.dat---
 % relative axial location (from low end of the cylinder)
 % A
 % B
 % C
 % D
 % E
-%  ---end file polynomial_#.dat---
+%  ---end file curveFit_coefs_#.dat---
 % 
 % The coefficients are for a function of the form:
 % 
@@ -24,9 +24,9 @@ clear all;
 %________________________________
 % USER INPUTS
 %'
-dir = 'case1'
-dat = {'polynomial_47.dat', 'polynomial_48.dat','polynomial_49.dat','polynomial_50.dat','polynomial_51.dat','polynomial_52.dat'}
-%dat = {'polynomial_97.dat','polynomial_98.dat','polynomial_99.dat','polynomial_100.dat','polynomial_101.dat','polynomial_102.dat'}
+dir = 'case11'
+dat = {'curveFit_coefs_47.dat','curveFit_coefs_48.dat','curveFit_coefs_49.dat','curveFit_coefs_50.dat','curveFit_coefs_51.dat','curveFit_coefs_52.dat'}
+%dat = {'curveFit_coefs_97.dat','curveFit_coefs_98.dat','curveFit_coefs_99.dat','curveFit_coefs_100.dat','curveFit_coefs_101.dat','curveFit_coefs_102.dat'}
 
 legendText = {'47','48', '49', '50','51','52'}
 %legendText = {'97', '98', '99','100','101','102'}
@@ -34,7 +34,7 @@ legendText = {'47','48', '49', '50','51','52'}
 symbol = {'-b','-r','-g','-c','-m','-k','.b','.r'};
 
 maxFiles = length(dat)
-
+set(0,'DefaultFigurePosition',[0,0,700,510]);
 for( i = 1:maxFiles )
   disp('working on');
   i
