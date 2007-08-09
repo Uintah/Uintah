493d4ed35b5a1506147b23af12ad27578c042c23
Amjidanutpan Ramanuj <ramanuja@sci.utah.edu>
2007-08-09 04:55:20 +0000

    Undoing the change made in the last commit.
    The number cells in the output is redundant.

--- Packages/Uintah/StandAlone/tools/compare_mms/compare_scalar.cc
+++ Packages/Uintah/StandAlone/tools/compare_mms/compare_scalar.cc
@@ -102,11 +102,7 @@ string udaFileName;
 void
 usage()
 {
-  fprintf(stderr, "\nUsage: compare_scalar <Uda Directory Name>\n\n" );
-  cerr << "Valid options are:\n";
-  cerr << "-h[elp]              : This usage information.\n";
-  cerr << "-uda <archive file>\n";
-  cerr << "-o <output_file_name>\n";
+  printf( "\nUsage: compare_scalar <Uda Directory Name>\n\n" );
   exit(1);
 }
 
@@ -248,7 +244,6 @@ main( int argc, char *argv[] )
 
     cout<<"resolution"<<resolution<<endl;
     cout<<"velocity"<<velocity<<endl;
-    cout<<"Offset:"<<offset<<endl;
 
     if (!(is_int(offset.x()) && is_int(offset.y()) && is_int(offset.z()) ) )
     {
@@ -375,13 +370,11 @@ main( int argc, char *argv[] )
 		    exit(1);
 		  }
 		  
-		  total_error=total_error + (diff*diff);
+		  total_error+=diff*diff;
 		  if( diff > maxDiff ) maxDiff = diff;
 		  if( diff < minDiff ) minDiff = diff;
 		  
 		}
-//		if (new_y == 50)
-//		cerr<<cell<<": " <<scalarVar[cell]<< a <<" :"<< analytic_value[a]<<endl;
                 
                 i=i+1;
               }
@@ -392,8 +385,8 @@ main( int argc, char *argv[] )
 	  cout << "i= " << i << endl << "L2norm of error: " << endl << sqrt(total_error/i) << "\n";
 
 	  if (!initialize_analytical_values)
-	    fprintf(outFile, "%d %le\n",i,sqrt(total_error/i)) ;
-
+	    fprintf(outFile, "%le\n",sqrt(total_error/i)) ;
+	  
         } // end variable iteration
       } // end levels iteration
       if (initialize_analytical_values)
