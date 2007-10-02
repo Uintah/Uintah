96edcef6e6656ff35ec0ae31cdfa5a3dda0953f4
A.N.M. Imroz Choudhu <roni@sci.utah.edu>
2007-10-02 19:29:22 +0000

    All programs depend on 'prereqs' target... this will allow 'make scirun' to work correctly as the first make command (before you had to 'make; make scirun' or some such.

--- src/scripts/program.mk
+++ src/scripts/program.mk
@@ -64,7 +64,7 @@ $(PROGRAM)_LIBS := $(LIBS)
 # to lib/libCore_Thread.so.  The second transforms it from
 # lib/libCore_Thread.so to -lCore_Thread.  This is so that
 # we can use the -l syntax to link, but still express the dependicies.
-$(PROGRAM): $(OBJS) $(patsubst %,$(LIBDIR)/lib%.$(SO_OR_A_FILE),$(PSELIBS))
+$(PROGRAM): prereqs $(OBJS) $(patsubst %,$(LIBDIR)/lib%.$(SO_OR_A_FILE),$(PSELIBS))
 ifeq ($(IS_WIN),yes)
 	$(CXX) $(filter %.$(OBJEXT),$^) -o $@ $(LDFLAGS) $(PROGRAM_LDFLAGS) $(SCI_THIRDPARTY_LIBRARY) $(LDRUN_PREFIX)$(LIBDIR_ABS) $(patsubst $(LIBDIR)/%.$(SO_OR_A_FILE),%.lib,$(filter %.$(SO_OR_A_FILE),$^)) $(REPOSITORIES_$@) $($@_LIBS) $(TAU_LIBRARY)
 else
