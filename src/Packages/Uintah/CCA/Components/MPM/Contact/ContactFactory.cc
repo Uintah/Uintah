8ebf42e7724f994b5f4a015671d671dd8b886a4a
Todd Harman <harman@sci.utah.edu>
2007-10-17 20:25:15 +0000

    added bulletproofing.
    Warn the user that they're missing something in the input file.

--- src/Packages/Uintah/CCA/Components/MPM/Contact/ContactFactory.cc
+++ src/Packages/Uintah/CCA/Components/MPM/Contact/ContactFactory.cc
@@ -23,6 +23,12 @@ Contact* ContactFactory::create(const ProcessorGroup* myworld,
 
    ProblemSpecP mpm_ps = ps->findBlock("MaterialProperties")->findBlock("MPM");
    
+   
+   if(!mpm_ps){
+    string warn = "ERROR: Missing either <MaterialProperties> or <MPM> block from input file";
+    throw ProblemSetupException(warn, __FILE__, __LINE__);
+   }
+   
    CompositeContact * contact_list = scinew CompositeContact(myworld, lb, flag);
    
    for (ProblemSpecP child = mpm_ps->findBlock("contact"); child != 0;
