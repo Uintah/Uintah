ffe8f4d12fb3f9813f5f518726a19728f758e3fc
Bryan Jeffrey Worthen <worthen@sci.utah.edu>
2007-10-24 19:44:22 +0000

    Make the toplevel ProblemSpec responsible for deleting its xml tree when deleted.  Comment out calls to releaseDocument

--- Packages/Uintah/CCA/Components/SimulationController/AMRSimulationController.cc
+++ Packages/Uintah/CCA/Components/SimulationController/AMRSimulationController.cc
@@ -338,7 +338,7 @@ void AMRSimulationController::run()
    calcWallTime();
    printSimulationStats(d_sharedState->getCurrentTopLevelTimeStep(),delt,t);
 
-   d_ups->releaseDocument();
+   //d_ups->releaseDocument();
 }
 
 //______________________________________________________________________
