4ed93ff82754abc2392b03efa879eef842419e8e
Kurt Zimmerman <kuzimmer@sci.utah.edu>
2007-11-09 23:02:27 +0000

    Add the ability to view patches and grid cells along an axis aligned slice

--- src/Packages/Uintah/Dataflow/Modules/Visualization/PatchVisualizer.cc
+++ src/Packages/Uintah/Dataflow/Modules/Visualization/PatchVisualizer.cc
@@ -24,6 +24,7 @@
 #include <Dataflow/Network/Ports/GeometryPort.h>
 #include <Dataflow/Network/Ports/ColorMapPort.h>
 #include <Dataflow/Network/Ports/MatrixPort.h>
+#include <Core/Containers/StringUtil.h>
 #include <Core/Geom/GeomGroup.h>
 #include <Core/Geom/GeomLine.h>
 #include <Core/Geom/Material.h>
@@ -58,6 +59,18 @@ using namespace std;
 #define Z_DIM 3
 #define RANDOM 4
 #define PROC_NUM 5
+
+struct PatchData {
+  Box box;
+  int proc_id;
+  IntVector min_idx;
+  IntVector max_idx;
+};
+
+typedef void(Point::* Point_vfd)(const double ); // vfd = void function(double)
+typedef double(Point::* Point_dfv)() const; // dfv = double function(void)
+typedef int(IntVector::* IntVector_ifv)() const; // ifv = int function(void)
+
   
 class PatchVisualizer : public Module {
 public:
@@ -69,6 +82,19 @@ public:
 private:
   void addBoxGeometry(GeomLines* edges, const Box& box,
 		      const Vector & change, const Transform& shift);
+  void addSliceGeometry(GeomLines* slice_lines, 
+                        BBox& bbox,
+                        const PatchData& pd,
+                        const Point& min, const Point& max,
+                        const Vector&  change,
+                        const Transform& shift);
+  void addSlices( GeomLines* slice_lines, BBox& bbox, bool show_cells,
+                  Point &p0, Point &p1, Point& p2, Point& P3,
+                  Point_vfd set_u, Point_vfd set_v,
+                  const IntVector& min_idx, const IntVector& max_idx,
+                  IntVector_ifv u_idx, IntVector_ifv v_idx,
+                  const Point &p_min, const Point& p_max,
+                  Point_dfv get_u, Point_dfv get_v);
   bool getGrid();
   void setupColors();
   int getScheme(string scheme);
@@ -94,6 +120,14 @@ private:
   GuiString level5_color_scheme;
   GuiInt nl;
   GuiInt patch_seperate;
+
+  GuiInt show_slices;
+  GuiInt show_x, show_x_cells;
+  GuiInt show_y, show_y_cells;
+  GuiInt show_z, show_z_cells;
+  GuiDouble x_loc;
+  GuiDouble y_loc;
+  GuiDouble z_loc;
   
   vector< double > times;
   DataArchiveHandle archive;
@@ -124,6 +158,16 @@ DECLARE_MAKER(PatchVisualizer)
   level5_color_scheme(get_ctx()->subVar("level5_color_scheme")),
   nl(get_ctx()->subVar("nl")),
   patch_seperate(get_ctx()->subVar("patch_seperate")),
+  show_slices(get_ctx()->subVar("show_slices"), 0),
+  show_x(get_ctx()->subVar("show_x"),1),
+  show_x_cells(get_ctx()->subVar("show_x_cells"), 0),
+  show_y(get_ctx()->subVar("show_y"),0),
+  show_y_cells(get_ctx()->subVar("show_y_cells"), 0),
+  show_z(get_ctx()->subVar("show_z"),0),
+  show_z_cells(get_ctx()->subVar("show_z_cells"), 0),
+  x_loc(get_ctx()->subVar("x_loc"),0.5),
+  y_loc(get_ctx()->subVar("y_loc"),0.5),
+  z_loc(get_ctx()->subVar("z_loc"),0.5),
   old_generation(-1), timestep(0), numLevels(0),
   grid(NULL)
 {
@@ -170,6 +214,138 @@ bool PatchVisualizer::getGrid()
     return true;
   }
   return false;
+}
+
+
+void
+PatchVisualizer::addSliceGeometry(GeomLines* slice_lines, 
+                                  BBox& bbox,
+                                  const PatchData& pd,
+                                  const Point& min, const Point& max,
+                                  const Vector&  change,
+                                  const Transform& shift)
+{
+  if( !(show_slices.get() && (show_x.get() || show_y.get() || show_z.get()))) {
+    return;
+  }
+
+  Point low(pd.box.lower()), hi(pd.box.upper());
+  if( show_x.get() ){
+    double xl = min.x() + (max.x() - min.x()) * x_loc.get();
+    if( low.x() <= xl && hi.x() >= xl ){
+      // create the slice frame
+      Point p0(xl, low.y(), low.z());  Point p1(xl, hi.y(), low.z());
+      Point p2(xl, hi.y(), hi.z());    Point p3(xl, low.y(), hi.z());
+      addSlices(slice_lines, bbox, show_x_cells.get() == 1,
+                p0, p1, p2, p3, &Point::y, &Point::z,
+                pd.min_idx, pd.max_idx, &IntVector::y, &IntVector::z,
+                low, hi, &Point::y, &Point::z);
+    }
+  }
+  if( show_y.get() ){
+    double yl = min.y() + (max.y() - min.y()) * y_loc.get();
+    if( low.y() <= yl && hi.y() >= yl ){
+      // create the slice frame
+      Point p0(low.x(), yl, low.z());    Point p1(hi.x(), yl, low.z());
+      Point p2(hi.x(), yl, hi.z());      Point p3(low.x(), yl, hi.z());
+      addSlices(slice_lines, bbox, show_y_cells.get() == 1,
+                p0, p1, p2, p3, &Point::x, &Point::z,
+                pd.min_idx, pd.max_idx, &IntVector::x, &IntVector::z,
+                low, hi, &Point::x, &Point::z);
+    }
+  }
+  if( show_z.get() ){
+    double zl = min.z() + (max.z() - min.z()) * z_loc.get();
+    if( low.z() <= zl && hi.z() >= zl ){
+      // create the slice frame
+      Point p0(low.x(), low.y(), zl);    Point p1(hi.x(), low.y(), zl);
+      Point p2(hi.x(), hi.y(), zl);      Point p3(low.x(), hi.y(), zl);
+      addSlices(slice_lines, bbox, show_z_cells.get() == 1,
+                p0, p1, p2, p3, &Point::x, &Point::y,
+                pd.min_idx, pd.max_idx, &IntVector::x, &IntVector::y,
+                low, hi, &Point::x, &Point::y);
+    }
+  }
+}
+                             
+
+void
+PatchVisualizer::addSlices( GeomLines* slice_lines, 
+                            BBox &bbox, bool show_cells,
+                            Point &p0, Point &p1, Point& p2, Point & p3,
+                            Point_vfd set_u, Point_vfd set_v,
+                            const IntVector& min_idx, const IntVector& max_idx,
+                            IntVector_ifv u_idx, IntVector_ifv v_idx,
+                            const Point &p_min, const Point& p_max,
+                            Point_dfv get_u, Point_dfv get_v)
+{
+
+  if( show_cells ){
+    int u_cells = (max_idx.*u_idx)() - (min_idx.*u_idx)();
+    int v_cells = (max_idx.*v_idx)() - (min_idx.*v_idx)();
+    double u_range = (p_max.*get_u)() - (p_min.*get_u)();
+    double v_range = (p_max.*get_v)() - (p_min.*get_v)();
+    double du = u_range/u_cells;
+    double dv = v_range/v_cells;
+    int i,j;
+    if(bbox.valid()){
+      for(j = 0; j < v_cells; j++){
+        for(i = 0; i < u_cells; i++) {
+          if( j == v_cells -1 ){
+            (p0.*set_u)((p_min.*get_u)() + (i * du));
+            (p0.*set_v)((p_min.*get_v)() + ((j+1) * dv));
+            (p1.*set_u)((p_min.*get_u)() + ((i+1) * du));
+            (p1.*set_v)((p_min.*get_v)() + ((j+1) * dv));
+            if( !( bbox.inside(p0)|| bbox.inside(p1) )) {
+              slice_lines->add(p0, p1);
+            }
+          }
+          (p0.*set_u)((p_min.*get_u)() + (i * du));
+          (p0.*set_v)((p_min.*get_v)() + (j * dv));
+          (p1.*set_u)((p_min.*get_u)() + (i * du));
+          (p1.*set_v)((p_min.*get_v)() + ((j+1) * dv));
+          if( !( bbox.inside(p0)|| bbox.inside(p1) )) {
+            slice_lines->add(p0,p1);
+          }
+          (p1.*set_u)((p_min.*get_u)() + ((i+1) * du));
+          (p1.*set_v)((p_min.*get_v)() + (j * dv));
+          if( !( bbox.inside(p0)|| bbox.inside(p1) )) {
+            slice_lines->add(p0, p1);
+          }
+        }
+        (p0.*set_u)((p_max.*get_u)());
+        (p0.*set_v)((p_min.*get_v)() + ((j+1) * dv));
+        if( !( bbox.inside(p0) || bbox.inside(p1) )) {
+          slice_lines->add(p1, p0);
+        } 
+      }
+      
+    } else {
+      slice_lines->add(p0,p1);
+      slice_lines->add(p1,p2);
+      slice_lines->add(p2,p3);
+      slice_lines->add(p3,p0);
+      (p0.*set_v)((p_min.*get_v)());
+      (p1.*set_v)((p_max.*get_v)());
+      for(i = 1; i < u_cells; i++){
+        (p0.*set_u)( (p_min.*get_u)() + (i * du));
+        (p1.*set_u)( (p_min.*get_u)() + (i * du));
+        slice_lines->add(p0, p1);
+      }
+      (p0.*set_u)( (p_min.*get_u)());
+      (p1.*set_u)( (p_max.*get_u)());
+      for(j = 1; j < v_cells; j++){
+        (p0.*set_v)( (p_min.*get_v)() + (j * dv));
+        (p1.*set_v)( (p_min.*get_v)() + (j * dv));
+        slice_lines->add(p0, p1);
+      }
+    }
+  } else {
+    slice_lines->add(p0,p1);
+    slice_lines->add(p1,p2);
+    slice_lines->add(p2,p3);
+    slice_lines->add(p3,p0);
+  }
 }
 
 // adds the lines to edges that make up the box defined by box 
@@ -330,7 +506,7 @@ void PatchVisualizer::execute()
   //////////////////////////////////////////////////////////////////
   
   // it's faster when we don't use push_back and know the exact size
-  vector< vector<pair<Box, int> > > patches(numLevels);
+  vector< vector<PatchData > > patches(numLevels);
 
   // initialize the min and max for the entire region.
   // Note: there are already function that compute this, but they currently
@@ -339,20 +515,27 @@ void PatchVisualizer::execute()
   //       This will make the code faster.
   Point min(DBL_MAX,DBL_MAX,DBL_MAX), max(DBL_MIN,DBL_MIN,DBL_MIN);
 
-  for(int l = 0;l<numLevels;l++){
+  for(int l = 0; l < numLevels;l++){
     LevelP level = grid->getLevel(l);
 
-    vector<pair<Box, int> > patch_list(level->numPatches());
+//     vector<pair<Box, int> > patch_list(level->numPatches());
+    vector<PatchData> patch_list(level->numPatches());
     Level::const_patchIterator iter;
     //---------------------------------------
     // for each patch in the level
     int i = 0;
     for(iter=level->patchesBegin();iter != level->patchesEnd(); iter++){
       const Patch* patch=*iter;
-      Box box = patch->getBox();
-      patch_list[i].first = box;
-      patch_list[i].second = 
-        archive->queryPatchwiseProcessor( patch, timestep); 
+      Box box = patch->getInteriorBox();
+//       patch_list[i].first = box;
+//       patch_list[i].second = 
+//         archive->queryPatchwiseProcessor( patch, timestep); 
+
+      patch_list[i].box = box;
+      patch_list[i].proc_id =
+        archive->queryPatchwiseProcessor( patch, timestep);
+      patch_list[i].min_idx = patch->getInteriorCellLowIndex();
+      patch_list[i].max_idx = patch->getInteriorCellHighIndex();
 
       // determine boundaries
       if (box.upper().x() > max.x())
@@ -372,6 +555,9 @@ void PatchVisualizer::execute()
     }
     patches[l] = patch_list;
   }
+  min = pt.project(min);
+  max = pt.project(max);
+
   // the change in size of the patches.
   // This is based on the actual size of the data, so it should be a pretty
   // good guess.
@@ -389,11 +575,14 @@ void PatchVisualizer::execute()
   // Create the geometry for export
   //////////////////////////////////////////////////////////////////
   
+  BBox bbox, level_bbox;
   // loops over all the levels
-  for(unsigned int l = 0;l<patches.size();l++){
+  for(unsigned int l = patches.size() ;l > 0; --l){
+
+    level_bbox.reset();
     // there can be up to 6 levels only, after that the value of the last
     // level is used.
-    unsigned int level_index = l;
+    unsigned int level_index = l - 1;
     if (level_index >= 6)
       level_index = 5;
 
@@ -401,6 +590,7 @@ void PatchVisualizer::execute()
     // seperately in order to be able to select and unselect them from
     // the renderer.
     GeomGroup *level_geom = scinew GeomGroup();
+    GeomGroup *level_slice_geom = scinew GeomGroup();
 
     int scheme;
     // if we don't have a colormap, then use solid color coloring
@@ -415,14 +605,24 @@ void PatchVisualizer::execute()
       
       // edges is all the edges made up all the patches in the level
       GeomLines* edges = scinew GeomLines();
-      
+      GeomLines* slice_lines = scinew GeomLines();
       //---------------------------------------
       // for each patch in the level
-      for(unsigned int i = 0; i < patches[l].size(); i++){
-	addBoxGeometry(edges, patches[l][i].first, change_v, pt);
-      }
+      for(unsigned int i = 0; i < patches[level_index].size(); i++){
+ 	addBoxGeometry(edges, patches[level_index][i].box, change_v, pt);
+        addSliceGeometry(slice_lines, bbox,
+                         patches[level_index][i], 
+                         min, max,
+                         change_v, pt); 
+        level_bbox.extend( patches[level_index][i].box.lower());
+        level_bbox.extend( patches[level_index][i].box.upper());
       
+      }
       level_geom->add(scinew GeomMaterial(edges, level_color[level_index]));
+      if(slice_lines->size() > 0){
+        level_slice_geom->add(scinew GeomMaterial(slice_lines,
+                                            level_color[level_index]));
+      }
     }
     break;
     case X_DIM:
@@ -430,11 +630,23 @@ void PatchVisualizer::execute()
 
       //---------------------------------------
       // for each patch in the level
-      for(unsigned int i = 0; i < patches[l].size(); i++){
+      for(unsigned int i = 0; i < patches[level_index].size(); i++){
 	GeomLines* edges = scinew GeomLines();
-	addBoxGeometry(edges, patches[l][i].first, change_v, pt);
+        GeomLines* slice_lines = scinew GeomLines();
+	addBoxGeometry(edges, patches[level_index][i].box, change_v, pt);
+        addSliceGeometry(slice_lines, bbox,
+                         patches[level_index][i], 
+                         min, max,
+                         change_v, pt); 
+        level_bbox.extend( patches[level_index][i].box.lower());
+        level_bbox.extend( patches[level_index][i].box.upper());
+      
 	level_geom->add(scinew GeomMaterial(edges,
-			       cmap->lookup(patches[l][i].first.lower().x())));
+                       cmap->lookup(patches[level_index][i].box.lower().x())));
+        if(slice_lines->size() > 0){
+          level_slice_geom->add(scinew GeomMaterial(slice_lines,
+                      cmap->lookup(patches[level_index][i].box.lower().x())));
+        }
       }
       
       break;
@@ -443,11 +655,20 @@ void PatchVisualizer::execute()
 
       //---------------------------------------
       // for each patch in the level
-      for(unsigned int i = 0; i < patches[l].size(); i++){
+      for(unsigned int i = 0; i < patches[level_index].size(); i++){
 	GeomLines* edges = scinew GeomLines();
-	addBoxGeometry(edges, patches[l][i].first, change_v, pt);
+        GeomLines* slice_lines = scinew GeomLines();
+	addBoxGeometry(edges, patches[level_index][i].box, change_v, pt);
+        addSliceGeometry(slice_lines, bbox,
+                         patches[level_index][i], 
+                         min, max,
+                         change_v, pt); 
 	level_geom->add(scinew GeomMaterial(edges,
-			       cmap->lookup(patches[l][i].first.lower().y())));
+                       cmap->lookup(patches[level_index][i].box.lower().y())));
+        if(slice_lines->size() > 0){
+          level_slice_geom->add(scinew GeomMaterial(slice_lines,
+                       cmap->lookup(patches[level_index][i].box.lower().y())));
+        }
       }
       
       break;
@@ -456,11 +677,20 @@ void PatchVisualizer::execute()
 
       //---------------------------------------
       // for each patch in the level
-      for(unsigned int i = 0; i < patches[l].size(); i++){
+      for(unsigned int i = 0; i < patches[level_index].size(); i++){
 	GeomLines* edges = scinew GeomLines();
-	addBoxGeometry(edges, patches[l][i].first, change_v, pt);
+        GeomLines* slice_lines = scinew GeomLines();
+	addBoxGeometry(edges, patches[level_index][i].box, change_v, pt);
+        addSliceGeometry(slice_lines, bbox,
+                         patches[level_index][i], 
+                         min, max,
+                         change_v, pt); 
 	level_geom->add(scinew GeomMaterial(edges,
-			       cmap->lookup(patches[l][i].first.lower().z())));
+		       cmap->lookup(patches[level_index][i].box.lower().z())));
+        if(slice_lines->size() > 0){
+          level_slice_geom->add(scinew GeomMaterial(slice_lines,
+                       cmap->lookup(patches[level_index][i].box.lower().z())));
+        }
       }
       
       break;
@@ -470,10 +700,20 @@ void PatchVisualizer::execute()
 
       //---------------------------------------
       // for each patch in the level
-      for(unsigned int i = 0; i < patches[l].size(); i++){
+      for(unsigned int i = 0; i < patches[level_index].size(); i++){
 	GeomLines* edges = scinew GeomLines();
-	addBoxGeometry(edges, patches[l][i].first, change_v, pt);
+        GeomLines* slice_lines = scinew GeomLines();
+// 	addBoxGeometry(edges, patches[level_index][i].first, change_v, pt);
+	addBoxGeometry(edges, patches[level_index][i].box, change_v, pt);
+        addSliceGeometry(slice_lines, bbox,
+                         patches[level_index][i], 
+                         min, max,
+                         change_v, pt); 
 	level_geom->add(scinew GeomMaterial(edges, cmap->lookup(drand48())));
+        if(slice_lines->size() > 0){
+          level_slice_geom->add(scinew GeomMaterial(slice_lines,
+                       cmap->lookup(drand48())));
+        }
       }
       
       break;
@@ -488,13 +728,25 @@ void PatchVisualizer::execute()
       }
       //---------------------------------------
       // for each patch in the level
-      for(unsigned int i = 0; i < patches[l].size(); i++){
+      for(unsigned int i = 0; i < patches[level_index].size(); i++){
 	GeomLines* edges = scinew GeomLines();
-	addBoxGeometry(edges, patches[l][i].first, change_v, pt);
-        int patch_proc = patches[l][i].second;
+        GeomLines* slice_lines = scinew GeomLines();
+// 	addBoxGeometry(edges, patches[level_index][i].first, change_v, pt);
+//         int patch_proc = patches[level_index][i].second;
+	addBoxGeometry(edges, patches[level_index][i].box, change_v, pt);
+        addSliceGeometry(slice_lines, bbox,
+                         patches[level_index][i], 
+                         min, max,
+                         change_v, pt); 
+        int patch_proc = patches[level_index][i].proc_id;
 	level_geom-> add(scinew GeomMaterial(edges, 
                                              cmap->lookup((patch_proc == -1) ? 
                                                           0.0:patch_proc )));
+        if(slice_lines->size() > 0){
+          level_slice_geom->add(scinew GeomMaterial(slice_lines,
+                                             cmap->lookup((patch_proc == -1) ? 
+                                                          0.0:patch_proc )));
+        }
       }
       break;
     } // end of switch
@@ -503,6 +755,15 @@ void PatchVisualizer::execute()
     ostringstream name_edges;
     name_edges << "Patches - level " << l;
     ogeom->addObj(level_geom, name_edges.str().c_str());
+    ogeom->addObj(level_slice_geom, "Slice - level" + to_string(l));
+    // extend the bounding box for computation with a slightly shrunken
+    // level_bbox, shrink the level_bbox around its center.
+    Vector half_minus = (level_bbox.max() - level_bbox.center()) * 0.99999;
+    Point lb_min( level_bbox.center() - half_minus );
+    Point lb_max( level_bbox.center() + half_minus );
+    bbox.extend( lb_min );
+    bbox.extend( lb_max );
+
   }
 }
 
