340cef76f81f240aaaa8b3b81fca6ae00d16bdf7
Bryan Jeffrey Worthen <worthen@sci.utah.edu>
2007-11-19 18:44:16 +0000

    Put Matrix3 at top of includes for g++ template satisfaction

--- trunk/src/Dataflow/Modules/Selectors/FieldExtractor.h
+++ trunk/src/Dataflow/Modules/Selectors/FieldExtractor.h
@@ -30,10 +30,10 @@ LOG
 #define FIELDEXTRACTOR_H 1
 
 
+#include <Core/Math/Matrix3.h>
 #include <SCIRun/Core/Basis/Constant.h>
 #include <SCIRun/Core/Basis/HexTrilinearLgn.h>
 #include <SCIRun/Core/Datatypes/LatVolMesh.h>
-#include <Core/Math/Matrix3.h>
 #include <SCIRun/Core/Datatypes/MultiLevelField.h>
 #include <SCIRun/Core/Geometry/IntVector.h>
 #include <SCIRun/Core/Geometry/Point.h>
