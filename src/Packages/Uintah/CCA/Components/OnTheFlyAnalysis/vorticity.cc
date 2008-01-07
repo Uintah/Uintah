18da87f71090dd4586bb823f5bc65368306029ab
Todd Harman <harman@sci.utah.edu>
2008-01-07 21:40:48 +0000

    Added analysis module to compute the vorticity field
    To activate add
    
        <DataAnalysis>
           <Module name="vorticity">
            <material>Atmosphere</material>
          </Module>
        </DataAnalysis>
    
    and 
    
     <save label="vorticity"/>
    
    to the ups file.
    
    Status: runs, rough draft, needs testing of all vorticity components,

---
+++ src/Packages/Uintah/CCA/Components/OnTheFlyAnalysis/vorticity.cc
@@ -0,0 +1,181 @@
+#include <Packages/Uintah/CCA/Components/OnTheFlyAnalysis/vorticity.h>
+#include <Packages/Uintah/CCA/Components/ICE/ICEMaterial.h>
+#include <Packages/Uintah/CCA/Ports/Scheduler.h>
+#include <Packages/Uintah/Core/Exceptions/ProblemSetupException.h>
+#include <Packages/Uintah/Core/Grid/Grid.h>
+#include <Packages/Uintah/Core/Grid/SimulationState.h>
+#include <Packages/Uintah/Core/Grid/Variables/CellIterator.h>
+#include <Packages/Uintah/Core/Labels/ICELabel.h>
+#include <Packages/Uintah/Core/Parallel/ProcessorGroup.h>
+
+#include <Core/Exceptions/InternalError.h>
+#include <Core/Util/DebugStream.h>
+#include <sys/stat.h>
+#ifndef _WIN32
+#include <dirent.h>
+#endif
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+
+
+using namespace Uintah;
+using namespace std;
+//__________________________________
+//  To turn on the output
+//  setenv SCI_DEBUG "VORTICITY_DBG_COUT:+" 
+static DebugStream cout_doing("VORTICITY_DOING_COUT", false);
+static DebugStream cout_dbg("VORTICITY_DBG_COUT", false);
+//______________________________________________________________________              
+vorticity::vorticity(ProblemSpecP& module_spec,
+                     SimulationStateP& sharedState,
+                     Output* dataArchiver)
+  : AnalysisModule(module_spec, sharedState, dataArchiver)
+{
+  d_sharedState = sharedState;
+  d_prob_spec = module_spec;
+  d_dataArchiver = dataArchiver;
+  d_matl_set = 0;
+  v_lb = scinew vorticityLabel();
+  I_lb  = scinew ICELabel();
+}
+
+//__________________________________
+vorticity::~vorticity()
+{
+  cout_doing << " Doing: destorying vorticity " << endl;
+  if(d_matl_set && d_matl_set->removeReference()) {
+    delete d_matl_set;
+  }
+  VarLabel::destroy(v_lb->vorticityLabel);
+  delete v_lb;
+  delete I_lb;
+}
+
+//______________________________________________________________________
+//     P R O B L E M   S E T U P
+void vorticity::problemSetup(const ProblemSpecP& prob_spec,
+                             GridP& grid,
+                             SimulationStateP& sharedState)
+{
+  cout_doing << "Doing problemSetup \t\t\t\tvorticity" << endl;
+  
+  if(!d_dataArchiver){
+    throw InternalError("vorticity:couldn't get output port", __FILE__, __LINE__);
+  }
+  
+  v_lb->vorticityLabel = VarLabel::create("vorticity", CCVariable<Vector>::getTypeDescription());
+  
+  d_matl = d_sharedState->parseAndLookupMaterial(d_prob_spec, "material");
+  
+  vector<int> m(1);
+  m[0] = d_matl->getDWIndex();
+  d_matl_set = scinew MaterialSet();
+  d_matl_set->addAll(m);
+  d_matl_set->addReference();
+}
+
+//______________________________________________________________________
+void vorticity::scheduleInitialize(SchedulerP& sched,
+                                   const LevelP& level)
+{
+  return;  // do nothing
+  cout_doing << "vorticity::scheduleInitialize " << endl;
+  Task* t = scinew Task("vorticity::initialize", 
+                  this, &vorticity::initialize);
+  
+  sched->addTask(t, level->eachPatch(), d_matl_set);
+}
+//______________________________________________________________________
+void vorticity::initialize(const ProcessorGroup*, 
+                           const PatchSubset* patches,
+                           const MaterialSubset*,
+                           DataWarehouse*,
+                           DataWarehouse* new_dw)
+{  
+}
+
+void vorticity::restartInitialize()
+{
+}
+
+//______________________________________________________________________
+void vorticity::scheduleDoAnalysis(SchedulerP& sched,
+                                   const LevelP& level)
+{
+  cout_doing << "vorticity::scheduleDoAnalysis " << endl;
+  Task* t = scinew Task("vorticity::doAnalysis", 
+                   this,&vorticity::doAnalysis);
+  
+  Ghost::GhostType gac = Ghost::AroundCells;
+  
+  t->requires(Task::NewDW, I_lb->vel_CCLabel, gac,1);
+  t->computes(v_lb->vorticityLabel);
+  
+  sched->addTask(t, level->eachPatch(), d_matl_set);
+}
+
+//______________________________________________________________________
+// Compute the vorticity field.
+void vorticity::doAnalysis(const ProcessorGroup* pg,
+                           const PatchSubset* patches,
+                           const MaterialSubset*,
+                           DataWarehouse* old_dw,
+                           DataWarehouse* new_dw)
+{       
+  const Level* level = getLevel(patches);
+  
+  for(int p=0;p<patches->size();p++){
+    const Patch* patch = patches->get(p);
+    
+    cout_doing << pg->myrank() << " " 
+                << "Doing doAnalysis (vorticity)\t\t\t\tL-"
+                << level->getIndex()
+                << " patch " << patch->getGridIndex()<< endl;
+    Ghost::GhostType gac = Ghost::AroundCells;
+    
+    CCVariable<Vector> vorticity;
+    constCCVariable<Vector> vel_CC;     //  To Do: use a different index beside 0
+
+    new_dw->get(vel_CC, I_lb->vel_CCLabel, 0,patch,gac, 1);
+    
+    new_dw->allocateAndPut(vorticity,v_lb->vorticityLabel, 0 ,patch);
+    vorticity.initialize(Vector(0.0));
+    
+    //__________________________________
+    // cell spacing
+    Vector dx = patch->dCell();
+    double delX = dx.x();
+    double delY = dx.y();
+    double delZ = dx.z();    
+    
+    for (CellIterator iter=patch->getCellIterator();!iter.done();iter++){
+      IntVector c = *iter;
+      
+      IntVector r   = c + IntVector(1,0,0);   // right
+      IntVector l   = c - IntVector(1,0,0);   // left
+
+      IntVector t   = c + IntVector(0,1,0);   // top
+      IntVector b   = c - IntVector(0,1,0);   // bottom
+      
+      IntVector frt = c + IntVector(0,0,1);   // front
+      IntVector bck = c - IntVector(0,0,1);   // back
+                        
+       // First-order central difference     
+      double du_dy = (vel_CC[ t ].x() - vel_CC[ b ].x())/delY;
+      double du_dz = (vel_CC[frt].x() - vel_CC[bck].x())/delZ;
+
+      double dv_dx = (vel_CC[ r ].y() - vel_CC[ l ].y())/delX;
+      double dv_dz = (vel_CC[frt].y() - vel_CC[bck].y())/delZ;    
+
+      double dw_dx = (vel_CC[ r ].z() - vel_CC[ l ].z())/delX;
+      double dw_dy = (vel_CC[ t ].z() - vel_CC[ b ].z())/delY;
+      
+      double omega_x = dw_dy - dv_dz;
+      double omega_y = du_dz - dw_dx;
+      double omega_z = dv_dx - du_dy;
+
+      vorticity[c] = Vector(omega_x,omega_y,omega_z);
+    }         
+  }  // patches
+}
---
+++ src/Packages/Uintah/CCA/Components/OnTheFlyAnalysis/vorticity.h
@@ -0,0 +1,100 @@
+
+#ifndef Packages_Uintah_CCA_Components_ontheflyAnalysis_vorticity_h
+#define Packages_Uintah_CCA_Components_ontheflyAnalysis_vorticity_h
+#include <Packages/Uintah/CCA/Components/OnTheFlyAnalysis/AnalysisModule.h>
+#include <Packages/Uintah/CCA/Ports/Output.h>
+#include <Packages/Uintah/Core/Grid/Variables/VarTypes.h>
+#include <Packages/Uintah/Core/Grid/Variables/CCVariable.h>
+#include <Packages/Uintah/Core/Grid/GridP.h>
+#include <Packages/Uintah/Core/Grid/LevelP.h>
+
+#include <map>
+#include <vector>
+
+namespace Uintah {
+  
+
+/**************************************
+
+CLASS
+   vorticity
+   
+GENERAL INFORMATION
+
+   vorticity.h
+
+   Todd Harman
+   Department of Mechanical Engineering
+   University of Utah
+
+   Center for the Simulation of Accidental Fires and Explosions (C-SAFE)
+  
+   Copyright (C) 2000 SCI Group
+
+KEYWORDS
+   vorticity
+
+DESCRIPTION
+   Long description...
+  
+WARNING
+  
+****************************************/
+  class vorticity : public AnalysisModule {
+  public:
+    vorticity(ProblemSpecP& prob_spec,
+              SimulationStateP& sharedState,
+		Output* dataArchiver);
+              
+    vorticity();
+                    
+    virtual ~vorticity();
+   
+    virtual void problemSetup(const ProblemSpecP& prob_spec,
+                              GridP& grid,
+                              SimulationStateP& sharedState);
+                              
+    virtual void scheduleInitialize(SchedulerP& sched,
+                                    const LevelP& level);
+                                    
+    virtual void restartInitialize();
+                                    
+    virtual void scheduleDoAnalysis(SchedulerP& sched,
+                                    const LevelP& level);
+   
+                                      
+  private:
+
+    void initialize(const ProcessorGroup*, 
+                    const PatchSubset* patches,
+                    const MaterialSubset*,
+                    DataWarehouse*,
+                    DataWarehouse* new_dw);
+                    
+    void doAnalysis(const ProcessorGroup* pg,
+                    const PatchSubset* patches,
+                    const MaterialSubset*,
+                    DataWarehouse*,
+                    DataWarehouse* new_dw);
+                    
+    
+    // general labels
+    class vorticityLabel {
+    public:
+      VarLabel* vorticityLabel;
+    };
+    
+    vorticityLabel* v_lb;
+    ICELabel* I_lb;
+       
+    //__________________________________
+    // global constants
+    SimulationStateP d_sharedState;
+    Output* d_dataArchiver;
+    ProblemSpecP d_prob_spec;
+    const Material* d_matl;
+    MaterialSet* d_matl_set;
+  };
+}
+
+#endif
