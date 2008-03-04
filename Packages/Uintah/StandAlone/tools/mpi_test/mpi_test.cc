c304fc63ec4a2bc6d0d270efc26b47820ebf15af
J. Davison de St. Germain <dav@sci.utah.edu>
2008-03-04 01:20:27 +0000

    Added timings to tests.  Place main() at top of file.  Added some white space.  Update async tests to use MPI_Testsome and run point to point async first to try to isolate bad nodes.  These tests may take much too long as they stand (testing all to all) as shown in the timings.txt file.  I've added some (commented out) 'simulated' failures to the code, but until we run into real failures, it is hard to determine if these checks will find them and/or report somewhat specifically the proc with the problem.  I think this is progress, but Justin and I probably should talk about how to make the test program better.

--- Packages/Uintah/StandAlone/tools/mpi_test/mpi_test.cc
+++ Packages/Uintah/StandAlone/tools/mpi_test/mpi_test.cc
@@ -35,8 +35,10 @@
 #include <string>
 #include <sstream>
 
-#include <Core/Util/FileUtils.h>
+#include <Core/Thread/Time.h>    // for currentSeconds()
+#include <Core/Util/FileUtils.h> // for testFilesystem()
 
+using namespace SCIRun;
 using namespace std;
 
 const int HOST_NAME_SIZE = 100;
@@ -54,40 +56,7 @@ int gather_test();
 int point2pointasync_test();
 int point2pointsync_test();
 int fileSystem_test();
-
-int
-testme(int (*testfunc)(void),char* name)
-{
-  if( rank == 0 ) {
-    cout << "Testing '" << name << "': ";
-    cout.flush();
-  }
-
-  int pass     = testfunc();
-  int all_pass = false;
-  
-  MPI_Allreduce(&pass,&all_pass,1,MPI_INT,MPI_LOR,MPI_COMM_WORLD);
- 
-  if( rank == 0) {
-    if( all_pass ) {
-      cout << "Passed\n" ;
-    }
-    else {
-      cout << "Failed\n" ;
-    }
-  }
-  
-  if( !all_pass ) {
-    // Sync processors so output is in sync
-    MPI_Barrier(MPI_COMM_WORLD);
-    cout << error_stream.str();
-    cout.flush();
-    error_stream.str("");
-    // Sync processors so output is in sync
-    MPI_Barrier(MPI_COMM_WORLD);
-  }
-  return all_pass;
-}
+int testme( int (*testfunc)(void), char* name );
 
 int
 main( int argc, char** argv )
@@ -113,6 +82,52 @@ main( int argc, char** argv )
   
   MPI_Finalize();
   return 0;
+}
+
+int
+testme(int (*testfunc)(void),char* name)
+{
+  if( rank == 0 ) {
+    cout << "Testing '" << name << "': ";
+    cout.flush();
+  }
+
+  double startTime = -1, endTime = -1;
+  if( rank == 0) {
+    startTime = Time::currentSeconds();
+  }
+
+  int pass = testfunc();
+
+  if( rank == 0) {
+    endTime   = Time::currentSeconds();  
+  }
+
+  int all_pass = false;
+  
+  MPI_Allreduce( &pass, &all_pass, 1, MPI_INT, MPI_LAND, MPI_COMM_WORLD);
+ 
+  if( rank == 0) {
+
+    if( all_pass ) {
+      cout << "Passed" ;
+    }
+    else {
+      cout << "Failed" ;
+    }
+    cout << " (Test took " << endTime - startTime << " seconds.)\n";
+  }
+  
+  if( !all_pass ) {
+    // Sync processors so output is in sync
+    MPI_Barrier(MPI_COMM_WORLD);
+    cout << error_stream.str();
+    cout.flush();
+    error_stream.str("");
+    // Sync processors so output is in sync
+    MPI_Barrier(MPI_COMM_WORLD);
+  }
+  return all_pass;
 }
 
 // Each Processor all reduces their rank
@@ -219,10 +234,10 @@ fileSystem_test()
   string host = string( hostname ).substr( 0, 3 );
 
   if( host == "inf" ) {
-    bool raid1 = SCIRun::testFilesystem( "/usr/csafe/raid1", error_stream, rank );
-    bool raid2 = SCIRun::testFilesystem( "/usr/csafe/raid2", error_stream, rank );
-    bool raid3 = SCIRun::testFilesystem( "/usr/csafe/raid3", error_stream, rank );
-    bool raid4 = SCIRun::testFilesystem( "/usr/csafe/raid4", error_stream, rank );
+    bool raid1 = testFilesystem( "/usr/csafe/raid1", error_stream, rank );
+    bool raid2 = testFilesystem( "/usr/csafe/raid2", error_stream, rank );
+    bool raid3 = testFilesystem( "/usr/csafe/raid3", error_stream, rank );
+    bool raid4 = testFilesystem( "/usr/csafe/raid4", error_stream, rank );
 
     pass = raid1 && raid2 && raid3 && raid4;
   }
@@ -236,30 +251,115 @@ point2pointasync_test()
 {
   int                 pass = true;
   vector<int>         messages(procs);
-  vector<MPI_Request> srequest(procs),rrequest(procs);
+  MPI_Request * srequest, * rrequest;
   
-  for(int p=0;p<procs;p++)
-  {
+  srequest = new MPI_Request[ procs ];
+  rrequest = new MPI_Request[ procs ];
+
+  int data = rank;
+
+  for( int p = 0; p < procs; p++ ) {
+
+    //if( rank > 4 ) { // Slow things down just a little for testing purposes...
+    //  usleep( 100000 * rank ); 
+    //}
+
     //start send
-    MPI_Isend(&rank,1,MPI_INT,p,p,MPI_COMM_WORLD,&srequest[p]);
+    MPI_Isend( &data, 1, MPI_INT, p, p, MPI_COMM_WORLD, &srequest[p] );
     
     //start recv
-    MPI_Irecv(&messages[p],1,MPI_INT,p,rank,MPI_COMM_WORLD,&rrequest[p]);
-  }
-  
-  // Waitsome loop
-  for(int p=0;p<procs;p++)
-  {
-    MPI_Status status;
-    MPI_Wait(&srequest[p],&status);
+    MPI_Irecv( &messages[p], 1, MPI_INT, p, rank, MPI_COMM_WORLD, &rrequest[p] );
 
-    MPI_Wait(&rrequest[p],&status);
-    if(messages[p]!=p)
-    {
-      pass=false;
-      error_stream << "     rank " << rank << ": point to point async from  " << p << " is invalid\n";
-     }
+    //if( rank == 5 ) { // Simulate proc 5 sending bad info....
+    //  data = 99;
+    //}
+
+    //if( rank == 5 ) { // Simulate proc 5 broken....
+    //  while(1) {}
+    //}
   }
+
+  int          numCompleted = -1;
+  int        * completedBuffer = new int[ procs ]; // Passed to MPI
+  bool       * completed       = new bool[ procs ]; // Used to keep track over all
+  MPI_Status * status = new MPI_Status[ procs ];
+
+  for( int pos = 0; pos < procs; pos++ ) {
+    completed[ pos ] = false;
+    // The following don't need to be initialized as they are overwritten... 
+    // however, when debugging, it is sometimes useful to set values to
+    // more easily see what has changed:
+    //completedBuffer[ pos ] = -9;
+    //status[ pos ].MPI_SOURCE = -2;
+    //status[ pos ].MPI_TAG    = -3;
+    //status[ pos ].MPI_ERROR  = -4;
+  }
+
+  //MPI_Waitsome( procs, srequest, &numCompleted, completedBuffer, status );
+
+  //printf("%d: completed %d\n", rank, numCompleted );
+
+  bool done = false;
+  int  totalCompleted = 0;
+
+  double startTime = -1, curTime = -1;
+
+  startTime = Time::currentSeconds();
+
+  while( !done ) {
+
+    // While it is unclear in the docs, apparently the MPI_Testsome
+    // not only tests for the data having arrived, but handles the
+    // recv too (ie, places the data in the specified buffer).
+    MPI_Testsome( procs, rrequest, &numCompleted, completedBuffer, status );
+
+    curTime = Time::currentSeconds();
+
+    double secsToWait = 50.0;
+    if( curTime > startTime + secsToWait ) { // Give it 'secsToWait' seconds to finish
+      if( rank == 0 ) {
+        cout << "ERROR: Some processors have not responded after " << secsToWait << " seconds.\n";
+      }
+
+      // Find out (and display) which processors did not successfully respond...
+      for( int pos = 0; pos < procs; pos++ ) {
+        if( completed[ pos ] == false ) {
+          printf( "Proc %d failed to hear from processor %d.\n", rank, pos );
+          pass = false;
+        }
+      }
+      done = true;
+    }
+
+    // Record the fact that a receive has completed.
+    if( numCompleted > 0 ) {
+      for( int pos = 0; pos < numCompleted; pos++ ) {
+        completed[ completedBuffer[ pos ] ] = true;
+      }
+    }
+
+    totalCompleted += numCompleted;
+    if( totalCompleted == procs ) {
+
+      // All messages in... verify data is valid:
+
+      for( int pos = 0; pos < procs; pos++ ) {
+
+        if( messages[pos] != pos ) {
+          pass = false;
+          error_stream << "     rank " << rank << ": data in point to point async message from " << pos 
+                       << " is invalid\n";
+        }
+      }
+      done = true;
+    }
+  }
+
+  delete [] status;
+  delete [] completed;
+  delete [] srequest;
+  delete [] rrequest;
+
   return pass;
 }
 
@@ -269,29 +369,24 @@ point2pointasync_test()
 int
 point2pointsync_test()
 {
-  int pass=true;
+  int pass = true;
   int message;
-  for(int pp=0;pp<procs;pp++)
-  {
-    if(pp==rank) //sending
-    {
-      for(int p=0;p<procs;p++)
-      {
+
+  for(int pp=0;pp<procs;pp++) {
+    if( pp == rank ) { // sending
+      for(int p=0;p<procs;p++) {
         MPI_Send(&rank,1,MPI_INT,p,p,MPI_COMM_WORLD);
       }
     }
-    else //recieving
-    {
-        MPI_Status status;
-        message=-1;
-        MPI_Recv(&message,1,MPI_INT,pp,rank,MPI_COMM_WORLD,&status);
-        if(message!=pp)
-        {
-          pass=false;
-          error_stream << "     rank " << rank << ": point to point sync from  " << pp << " is invalid\n";
-        }
+    else { // recieving
+      MPI_Status status;
+      message=-1;
+      MPI_Recv(&message,1,MPI_INT,pp,rank,MPI_COMM_WORLD,&status);
+      if( message != pp ) {
+        pass = false;
+        error_stream << "     rank " << rank << ": point to point sync from  " << pp << " is invalid\n";
+      }
     }
   }
-
   return pass;
 }
