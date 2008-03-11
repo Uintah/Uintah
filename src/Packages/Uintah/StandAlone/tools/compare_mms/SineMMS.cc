14817691065e6d2ccf3de3f9ccf19617b98211a7
Todd Harman <harman@sci.utah.edu>
2008-03-11 19:05:35 +0000

    Removed MMS1.(cc/h)  Doesn't appear to be used.
    variable name change
    A_ -> d_A
    viscosity_ -> d_viscosity
    p_ref_ -> d_p_ref
    
    Now passing in A, viscosity and p_ref into ExpMMS()

--- src/Packages/Uintah/StandAlone/tools/compare_mms/SineMMS.cc
+++ src/Packages/Uintah/StandAlone/tools/compare_mms/SineMMS.cc
@@ -9,13 +9,10 @@ using std::cout;
 // Depending on speed issues and how large these functions become, it
 // is possible that they should be moved to the .h files.
 
-// !!!!WHAT IS A?  It should be renamed with a better name!!!!
-
-
 SineMMS::SineMMS(double A, double viscosity, double p_ref) {
-	A_=A;
-	viscosity_=viscosity;
-	p_ref_=p_ref;
+	d_A=A;
+	d_viscosity=viscosity;
+	d_p_ref=p_ref;
 }
 
 SineMMS::~SineMMS() {
@@ -24,25 +21,25 @@ SineMMS::~SineMMS() {
 double
 SineMMS::pressure( double x, double y, double z, double time )
 {
-  return p_ref_ - ( 0.25 * A_ * A_ * 
-                   ( cos( 2.0*(x-time) ) + cos( 2.0*(y-time))) * exp( -4.0 * viscosity_ * time ) );
+  return d_p_ref - ( 0.25 * d_A * d_A * 
+                   ( cos( 2.0*(x-time) ) + cos( 2.0*(y-time))) * exp( -4.0 * d_viscosity * time ) );
 }
 
 double
 SineMMS::uVelocity( double x, double y, double z, double time )
 {
-  return 1- A_*cos(x-time)*sin(y-time)*exp(-2.0*viscosity_*time);
+  return 1- d_A*cos(x-time)*sin(y-time)*exp(-2.0*d_viscosity*time);
 }
   
 double
 SineMMS::vVelocity( double x, double y, double z, double time )
 {
-  return 1+ A_*sin(x-time)*cos(y-time)*exp(-2.0*viscosity_*time);
+  return 1+ d_A*sin(x-time)*cos(y-time)*exp(-2.0*d_viscosity*time);
 }
 
 double
 SineMMS::wVelocity( double x, double y, double z, double time )
 {
-  return 1+ A_*sin(x-time)*cos(y-time)*exp(-2.0*viscosity_*time);
+  return 1+ d_A*sin(x-time)*cos(y-time)*exp(-2.0*d_viscosity*time);
 }
 
