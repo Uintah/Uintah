f7fb5e4072a70d76744c0686a9f0cd0fad18a3cb
Todd Harman <harman@sci.utah.edu>
2008-03-11 22:27:31 +0000

    More thrashing:   - ExpMMS.cc  ( this was a copy paste of SineMMS.cc) All functions are now     returning -99999
      -SineMMS.cc
        wvelocity is not defined.  Now returning -99999
        
      compare_mms.cc
        All over the place.  Treat the velocity as a vector, not separate doubles.
        added -verbose option

--- src/Packages/Uintah/StandAlone/tools/compare_mms/ExpMMS.cc
+++ src/Packages/Uintah/StandAlone/tools/compare_mms/ExpMMS.cc
@@ -6,8 +6,6 @@
 // Depending on speed issues and how large these functions become, it
 // is possible that they should be moved to the .h files.
 
-// !!!!WHAT IS A?  It should be renamed with a better name!!!!
-
 ExpMMS::ExpMMS(double A, double viscosity, double p_ref) {
     	d_A=A;
 	d_viscosity=viscosity;
@@ -19,25 +17,24 @@ ExpMMS::~ExpMMS() {
 double
 ExpMMS::pressure( double x, double y, double z, double time )
 {
-  return d_p_ref - ( 0.25 * d_A * d_A * 
-                   ( cos( 2.0*(x-time) ) + cos( 2.0*(y-time))) * exp( -4.0 * d_viscosity * time ) );
+  return -999999;
 }
 
 double
 ExpMMS::uVelocity( double x, double y, double z, double time )
 {
-  return 1- d_A*cos(x-time)*sin(y-time)*exp(-2.0*d_viscosity*time);
+  return -999999;
 }
   
 double
 ExpMMS::vVelocity( double x, double y, double z, double time )
 {
-  return 1- d_A*cos(x-time)*sin(y-time)*exp(-2.0*d_viscosity*time);
+  return -99999;
 }
 
 double
 ExpMMS::wVelocity( double x, double y, double z, double time )
 {
-  return 1- d_A*cos(x-time)*sin(y-time)*exp(-2.0*d_viscosity*time);
+  return -99999;
 }
 
