78e02dea99f5db5c1d82a3de489462c3d45763d3
John Schmidt <jas@sci.utah.edu>
2008-03-17 20:22:43 +0000

    Fix compiler warnings usually having to do with const char declarations.

--- Filter.cc
+++ Filter.cc
@@ -67,11 +67,11 @@ Filter::problemSetup(const ProblemSpecP& params)
   int argc = 4;
   char** argv;
   argv = scinew char*[argc];
-  argv[0] = "Filter::problemSetup";
+  argv[0] = const_cast<char*>("Filter::problemSetup");
   //argv[1] = "-on_error_attach_debugger";
-  argv[1] = "-no_signal_handler";
-  argv[2] = "-log_exclude_actions";
-  argv[3] = "-log_exclude_objects";
+  argv[1] = const_cast<char*>("-no_signal_handler");
+  argv[2] = const_cast<char*>("-log_exclude_actions");
+  argv[3] = const_cast<char*>("-log_exclude_objects");
   int ierr = PetscInitialize(&argc, &argv, PETSC_NULL, PETSC_NULL);
   if(ierr)
     throw PetscError(ierr, "PetscInitialize", __FILE__, __LINE__);
