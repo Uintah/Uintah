6ba3682948764b6d79b3acf79cc9f54db57c26e8
Todd Harman <harman@sci.utah.edu>
2008-04-02 23:24:41 +0000

    saved as unix file instead of dos file
    now supporting CCVariable<int, double, Vector>, SFC(X,Y,Z)<double>
    
    The file header is now sorted by variable type and subtype.
    
    Data is written out by variable type and subtype.

--- Packages/Uintah/CCA/Components/OnTheFlyAnalysis/lineExtract.h
+++ Packages/Uintah/CCA/Components/OnTheFlyAnalysis/lineExtract.h
@@ -5,6 +5,9 @@
 #include <Packages/Uintah/CCA/Ports/Output.h>
 #include <Packages/Uintah/Core/Grid/Variables/VarTypes.h>
 #include <Packages/Uintah/Core/Grid/Variables/CCVariable.h>
+#include <Packages/Uintah/Core/Grid/Variables/SFCXVariable.h>
+#include <Packages/Uintah/Core/Grid/Variables/SFCYVariable.h>
+#include <Packages/Uintah/Core/Grid/Variables/SFCZVariable.h>
 #include <Packages/Uintah/Core/Grid/GridP.h>
 #include <Packages/Uintah/Core/Grid/LevelP.h>
 
