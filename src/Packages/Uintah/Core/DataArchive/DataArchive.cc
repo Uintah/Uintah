8d7e62ba95b17e3a0baa2d38fd46857cbe165932
Justin Luitjens <luitjens@sci.utah.edu>
2008-05-02 00:43:52 +0000

    Missed one conversion of totalCells to getNumExtraCells

--- src/Packages/Uintah/Core/DataArchive/DataArchive.cc
+++ src/Packages/Uintah/Core/DataArchive/DataArchive.cc
@@ -373,9 +373,8 @@ DataArchive::queryGrid( int index, const ProblemSpec* ups)
             throw InternalError( "DataArchive::queryGrid:Error parsing patch total cells",
                                  __FILE__, __LINE__ );
           }
-          /*Patch* patch = */ 
-          level->addPatch(lowIndex, highIndex,inLowIndex, inHighIndex,id);
-          // ASSERTEQ(patch->totalCells(), totalCells);
+          Patch* patch = level->addPatch(lowIndex, highIndex,inLowIndex, inHighIndex,id);
+           ASSERTEQ(patch->getNumExtraCells(), totalCells);
           PatchData pi;
           r->get("proc", pi.proc); // defaults to -1 if not available
           timedata.d_patchInfo[levelIndex].push_back(pi);
