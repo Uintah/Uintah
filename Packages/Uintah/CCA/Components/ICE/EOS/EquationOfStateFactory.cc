6a9e6c0707f38e1fa37998cd8f0a72c75f76670b
Todd Harman <harman@sci.utah.edu>
2008-05-06 20:46:50 +0000

    added Thomsen & Hartka equation of state for water.  Originally written by Jennifer VanRij.

--- Packages/Uintah/CCA/Components/ICE/EOS/EquationOfStateFactory.cc
+++ Packages/Uintah/CCA/Components/ICE/EOS/EquationOfStateFactory.cc
@@ -7,6 +7,7 @@
 #include <Packages/Uintah/CCA/Components/ICE/EOS/Murnahan.h>
 #include <Packages/Uintah/CCA/Components/ICE/EOS/Gruneisen.h>
 #include <Packages/Uintah/CCA/Components/ICE/EOS/Tillotson.h>
+#include <Packages/Uintah/CCA/Components/ICE/EOS/Thomsen_Hartka_water.h>
 #include <Packages/Uintah/Core/ProblemSpec/ProblemSpec.h>
 #include <Packages/Uintah/Core/Exceptions/ProblemSetupException.h>
 #include <Core/Malloc/Allocator.h>
@@ -22,30 +23,46 @@ using namespace Uintah;
 
 EquationOfState* EquationOfStateFactory::create(ProblemSpecP& ps)
 {
-    ProblemSpecP child = ps->findBlock("EOS");
-    if(!child)
-      throw ProblemSetupException("Cannot find EOS tag", __FILE__, __LINE__);
-    std::string mat_type;
-    if(!child->getAttribute("type",mat_type))
-      throw ProblemSetupException("No type for EOS", __FILE__, __LINE__); 
-    
-    if (mat_type == "ideal_gas") 
-      return(scinew IdealGas(child));
-    else if (mat_type == "hard_sphere_gas") 
-      return(scinew HardSphereGas(child));
-    else if (mat_type == "TST") 
-      return(scinew TST(child));
-    else if (mat_type == "JWL") 
-      return(scinew JWL(child));
-    else if (mat_type == "JWLC") 
-      return(scinew JWLC(child));
-    else if (mat_type == "Murnahan") 
-      return(scinew Murnahan(child));
-    else if (mat_type == "Gruneisen") 
-      return(scinew Gruneisen(child));
-    else if (mat_type == "Tillotson") 
-      return(scinew Tillotson(child));    
-    else
-      throw ProblemSetupException("Unknown EOS Type R ("+mat_type+")", __FILE__, __LINE__);
+  ProblemSpecP EOS_ps = ps->findBlock("EOS");
+  if(!EOS_ps){
+    throw ProblemSetupException("ERROR ICE: Cannot find EOS tag", __FILE__, __LINE__);
+  }
+  
+  std::string EOS;
+  if(!EOS_ps->getAttribute("type",EOS)){
+    throw ProblemSetupException("ERROR ICE: Cannot find EOS 'type' tag", __FILE__, __LINE__); 
+  }
+  if (EOS == "ideal_gas") 
+    return(scinew IdealGas(EOS_ps));
+  else if (EOS == "hard_sphere_gas") 
+    return(scinew HardSphereGas(EOS_ps));
+  else if (EOS == "TST") 
+    return(scinew TST(EOS_ps));
+  else if (EOS == "JWL") 
+    return(scinew JWL(EOS_ps));
+  else if (EOS == "JWLC") 
+    return(scinew JWLC(EOS_ps));
+  else if (EOS == "Murnahan") 
+    return(scinew Murnahan(EOS_ps));
+  else if (EOS == "Gruneisen") 
+    return(scinew Gruneisen(EOS_ps));
+  else if (EOS == "Tillotson") 
+    return(scinew Tillotson(EOS_ps));    
+  else if (EOS == "Thomsen_Hartka_water") 
+    return(scinew Thomsen_Hartka_water(EOS_ps));    
+  else{
+    ostringstream warn;
+    warn << "ERROR ICE: Unknown Equation of State ("<< EOS << " )\n"
+         << "Valid equations of State:\n" 
+         << "ideal_gas\n"
+         << "TST\n"
+         << "JWL\n"
+         << "JWLC\n"
+         << "Murnahan\n"
+         << "Gruneisen\n"
+         << "Tillotson\n"
+         << "Thomsen_Hartka_water" << endl;
+    throw ProblemSetupException(warn.str(), __FILE__, __LINE__);
+  }
 
 }
