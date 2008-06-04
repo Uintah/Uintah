e3ce711b24a221228ccff0ca185a5b1bc106dbb0
Todd Harman <harman@sci.utah.edu>
2008-06-04 21:00:32 +0000

    cleanup and update

--- src/Packages/Uintah/CCA/Components/Examples/Burger.cc
+++ src/Packages/Uintah/CCA/Components/Examples/Burger.cc
@@ -14,7 +14,8 @@
 #include <Core/Malloc/Allocator.h>
 
 using namespace Uintah;
-
+//______________________________________________________________________
+//  Preliminary
 Burger::Burger(const ProcessorGroup* myworld)
   : UintahParallelComponent(myworld)
 {
@@ -25,10 +26,12 @@ Burger::~Burger()
 {
   delete lb_;
 }
-
+//______________________________________________________________________
+//
 void Burger::problemSetup(const ProblemSpecP& params, 
                           const ProblemSpecP& restart_prob_spec, 
-                          GridP& /*grid*/,  SimulationStateP& sharedState)
+                          GridP& /*grid*/,  
+                          SimulationStateP& sharedState)
 {
   sharedState_ = sharedState;
   ProblemSpecP burger = params->findBlock("Burger");
@@ -37,156 +40,156 @@ void Burger::problemSetup(const ProblemSpecP& params,
   sharedState->registerSimpleMaterial(mymat_);
 }
  
+//______________________________________________________________________
+// 
 void Burger::scheduleInitialize(const LevelP& level,
-			       SchedulerP& sched)
+                                   SchedulerP& sched)
 {
-  Task* task = scinew Task("initialize",
-			   this, &Burger::initialize);
+  Task* task = scinew Task("Burger::initialize",
+                     this, &Burger::initialize);
   task->computes(lb_->u);
   sched->addTask(task, level->eachPatch(), sharedState_->allMaterials());
 }
- 
+//______________________________________________________________________
+// 
 void Burger::scheduleComputeStableTimestep(const LevelP& level,
-					  SchedulerP& sched)
+                                          SchedulerP& sched)
 {
-  Task* task = scinew Task("computeStableTimestep",
-			   this, &Burger::computeStableTimestep);
+  Task* task = scinew Task("Burger::computeStableTimestep",
+                     this, &Burger::computeStableTimestep);
+                     
   task->computes(sharedState_->get_delt_label());
   sched->addTask(task, level->eachPatch(), sharedState_->allMaterials());
 }
-
-void
-Burger::scheduleTimeAdvance( const LevelP& level, SchedulerP& sched)
+//______________________________________________________________________
+//
+void  Burger::scheduleTimeAdvance( const LevelP& level, 
+                                   SchedulerP& sched)
 {
-  Task* task = scinew Task("timeAdvance",
-			   this, &Burger::timeAdvance);
+  Task* task = scinew Task("Burger::timeAdvance",
+                     this, &Burger::timeAdvance);
+                     
   task->requires(Task::OldDW, lb_->u, Ghost::AroundNodes, 1);
   task->requires(Task::OldDW, sharedState_->get_delt_label());
+  
   task->computes(lb_->u);
   sched->addTask(task, level->eachPatch(), sharedState_->allMaterials());
 }
 
+//______________________________________________________________________
+//
 void Burger::computeStableTimestep(const ProcessorGroup*,
-				  const PatchSubset*,
-				  const MaterialSubset*,
-				  DataWarehouse*, DataWarehouse* new_dw)
+                                   const PatchSubset*,
+                                   const MaterialSubset*,
+                                   DataWarehouse*, 
+                                   DataWarehouse* new_dw)
 {
   new_dw->put(delt_vartype(delt_), sharedState_->get_delt_label());
 }
 
+//______________________________________________________________________
+//
 void Burger::initialize(const ProcessorGroup*,
-			const PatchSubset* patches,
-			const MaterialSubset* matls,
-			DataWarehouse*, DataWarehouse* new_dw)
+                        const PatchSubset* patches,
+                        const MaterialSubset* matls,
+                        DataWarehouse*, 
+                        DataWarehouse* new_dw)
 {
+  int matl = 0;
   for(int p=0;p<patches->size();p++){
     const Patch* patch = patches->get(p);
-    for(int m = 0;m<matls->size();m++){
-      int matl = matls->get(m);
-      NCVariable<double> u;
-      new_dw->allocateAndPut(u, lb_->u, matl, patch);
-      //Initialize according to the function
-      // u = sin( pi*x ) + sin( pi*2*y )
-      IntVector l = patch->getNodeLowIndex__New();
-      IntVector h = patch->getNodeHighIndex__New();
-      for( NodeIterator iter(l,h); !iter.done(); iter++ ){
-        Point p = patch->nodePosition(*iter);
-        u[*iter] = sin( p.x() * 3.14159265358 ) + sin( p.y() * 2*3.14159265358 );
-      }
-      // allocateAndPut instead:
-      /* new_dw->put(u, lb_->u, matl, patch); */;
+
+    NCVariable<double> u;
+    new_dw->allocateAndPut(u, lb_->u, matl, patch);
+    
+    //Initialize
+    // u = sin( pi*x ) + sin( pi*2*y ) + sin(pi*3z )
+    IntVector l = patch->getNodeLowIndex__New();
+    IntVector h = patch->getNodeHighIndex__New();
+    
+    for( NodeIterator iter=patch->getNodeIterator(); !iter.done(); iter++ ){
+      IntVector n = *iter;
+      Point p = patch->nodePosition(n);
+      u[n] = sin( p.x() * 3.14159265358 ) + sin( p.y() * 2*3.14159265358)  +  sin( p.z() * 3*3.14159265358);
     }
   }
 }
 
+//______________________________________________________________________
+//
 void Burger::timeAdvance(const ProcessorGroup*,
-			const PatchSubset* patches,
-			const MaterialSubset* matls,
-			DataWarehouse* old_dw, DataWarehouse* new_dw)
+                         const PatchSubset* patches,
+                         const MaterialSubset* matls,
+                         DataWarehouse* old_dw, 
+                         DataWarehouse* new_dw)
 {
+  int matl = 0;
   //Loop for all patches on this processor
   for(int p=0;p<patches->size();p++){
     const Patch* patch = patches->get(p);
-    for(int m = 0;m<matls->size();m++){
-      int matl = matls->get(m);
+    
+    //  Get data from the data warehouse including 1 layer of
+    // "ghost" nodes from surrounding patches
+    constNCVariable<double> u;
+    old_dw->get(u, lb_->u, matl, patch, Ghost::AroundNodes, 1);
 
-     delt_vartype dt;
-     old_dw->get(dt, sharedState_->get_delt_label());
+    // dt, dx
+    Vector dx = patch->getLevel()->dCell();
+    delt_vartype dt;
+    old_dw->get(dt, sharedState_->get_delt_label());
+    
+    // allocate memory
+    NCVariable<double> new_u;
+    new_dw->allocateAndPut(new_u, lb_->u, matl, patch);
+    
+    // define iterator range
+    IntVector l = patch->getNodeLowIndex__New();
+    IntVector h = patch->getNodeHighIndex__New();
 
-     constNCVariable<double> u;
+    //offset to prevent accessing memory out-of-bounds
+    l += IntVector(patch->getBCType(Patch::xminus) == Patch::Neighbor?0:1,
+                   patch->getBCType(Patch::yminus) == Patch::Neighbor?0:1,
+                   patch->getBCType(Patch::zminus) == Patch::Neighbor?0:1 );
+                   
+    h -= IntVector(patch->getBCType(Patch::xplus) == Patch::Neighbor?0:1,
+                   patch->getBCType(Patch::yplus) == Patch::Neighbor?0:1,
+                   patch->getBCType(Patch::zplus) == Patch::Neighbor?0:1 );
+                   
+    //Iterate through all the nodes
+    for(NodeIterator iter(l, h);!iter.done(); iter++){    
+      IntVector n = *iter;
+      double dudx = (u[n+IntVector(1,0,0)] - u[n-IntVector(1,0,0)]) /(2.0 * dx.x());
+      double dudy = (u[n+IntVector(0,1,0)] - u[n-IntVector(0,1,0)]) /(2.0 * dx.y());
+      double dudz = (u[n+IntVector(0,0,1)] - u[n-IntVector(0,0,1)]) /(2.0 * dx.z());
+      double du = - u[n] * dt * (dudx + dudy + dudz);
+      new_u[n]= u[n] + du;
+    }
 
-      // Pull the data off the shelf for the old data warehouse, including 1 layer of
-      // "ghost" nodes from the surrounding patches
-      old_dw->get(u, lb_->u, matl, patch, Ghost::AroundNodes, 1);
+    //__________________________________
+    // Boundary conditions: Neumann
+    // Iterate over the faces encompassing the domain
+    vector<Patch::FaceType>::const_iterator iter;
+    vector<Patch::FaceType> bf;
+    patch->getBoundaryFaces(bf);
+    for (iter  = bf.begin(); iter != bf.end(); ++iter){
+      Patch::FaceType face = *iter;
 
-      // Also get dt, dx, and dy
-      old_dw->get(dt, sharedState_->get_delt_label());
+      IntVector axes = patch->faceAxes(face);
+      int P_dir = axes[0]; // find the principal dir of that face
 
-      NCVariable<double> newu;
-      new_dw->allocateAndPut(newu, lb_->u, matl, patch);
-      IntVector l = patch->getNodeLowIndex__New();
-      IntVector h = patch->getNodeHighIndex__New(); 
-
-      //On true edges, stay one node inside the patch
-      l += IntVector(patch->getBCType(Patch::xminus) == Patch::Neighbor?0:1,
-		     patch->getBCType(Patch::yminus) == Patch::Neighbor?0:1,
-                     0 );
-      h -= IntVector(patch->getBCType(Patch::xplus) == Patch::Neighbor?0:1,
-		     patch->getBCType(Patch::yplus) == Patch::Neighbor?0:1,
-                     0 );
-
-      //Do some invariant calculations outside of loop:
-      Vector v = patch->getLevel()->dCell();
-      double dt_div_twodx, dt_div_twody;
-      dt_div_twodx = dt/(2*v.x());
-      dt_div_twody = dt/(2*v.y());
-
-      //Iterate through all the nodes and assign the new values
-      for(NodeIterator iter(l, h);!iter.done(); iter++){
-	double dudx_mult_dt = (u[*iter+IntVector(1,0,0)] - u[*iter-IntVector(1,0,0)]) * dt_div_twodx;
-	double dudy_mult_dt = (u[*iter+IntVector(0,1,0)] - u[*iter-IntVector(0,1,0)]) * dt_div_twody;
-	double du = - u[*iter] * (dudx_mult_dt + dudy_mult_dt);
-	newu[*iter]= u[*iter] + du;
+      IntVector offset(0,0,0);
+      if (face == Patch::xminus || face == Patch::yminus || face == Patch::zminus){
+        offset[P_dir] += 1; 
+      }
+      if (face == Patch::xplus || face == Patch::yplus || face == Patch::zplus){
+        offset[P_dir] -= 1;
       }
 
-      //Make sure the trailing edges are updated to the same value as the adjacent interior
-      // point to prevent the creation of discontinuities
-      if (patch->getBCType(Patch::xplus) != Patch::Neighbor) {
-        l = patch->getNodeLowIndex__New();
-        h = patch->getNodeHighIndex__New();
-        l.x( h.x()-1 );
-        for (NodeIterator iter(l, h);!iter.done(); iter++){
-          newu[*iter] = newu[*iter-IntVector(1,0,0)];
-        }
+      Patch::FaceIteratorType FN = Patch::FaceNodes;
+      for (CellIterator iter = patch->getFaceIterator__New(face,FN);!iter.done(); iter++){
+        IntVector n = *iter;
+        new_u[n] = new_u[n + offset];
       }
-      if (patch->getBCType(Patch::yplus) != Patch::Neighbor) {
-        l = patch->getNodeLowIndex__New();
-        h = patch->getNodeHighIndex__New();
-        l.y( h.y()-1 );
-        for (NodeIterator iter(l, h);!iter.done(); iter++){
-          newu[*iter] = newu[*iter-IntVector(0,1,0)];
-        }
-      }
-      if (patch->getBCType(Patch::xminus) != Patch::Neighbor) {
-        l = patch->getNodeLowIndex__New();
-        h = patch->getNodeHighIndex__New();
-        h.x( l.x()+1 );
-        for (NodeIterator iter(l, h);!iter.done(); iter++){
-          newu[*iter] = u[*iter];
-        }
-      }
-      if (patch->getBCType(Patch::yminus) != Patch::Neighbor) {
-        l = patch->getNodeLowIndex__New();
-        h = patch->getNodeHighIndex__New();
-        h.y( l.y()+1 );
-        for (NodeIterator iter(l, h);!iter.done(); iter++){
-          newu[*iter] = u[*iter];
-        }
-      }
-
-      // Store all the new values calculated into the new data warehouse
-      // allocateAndPut instead:
-      /* new_dw->put(newu, lb_->u, matl, patch); */;
     }
   }
 }
