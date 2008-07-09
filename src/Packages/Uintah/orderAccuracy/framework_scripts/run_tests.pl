274ef24cb82f0775264839da665ce0cba50cd2e6
Todd Harman <harman@sci.utah.edu>
2008-07-09 20:53:10 +0000

    -documentation -take into account xml tag name change (Interactive->sus_cmd & compCommand->compare_cmd) -removed code related to pbs

--- src/Packages/Uintah/orderAccuracy/framework_scripts/run_tests.pl
+++ src/Packages/Uintah/orderAccuracy/framework_scripts/run_tests.pl
@@ -1,5 +1,36 @@
 #!/usr/bin/perl -w
 
+
+#______________________________________________________________________
+#  run_tests.pl
+#  Perl script used to modify an input file and run the tests listed in 
+#  the tst file.
+#
+# Algorithm
+#   - read in each test in the tst file
+#   - make a symbolic link to the comparison utility used to compute the L2nomr
+#   - read in the replacement patterns for each test and all tests
+#   - perform global replacements on the ups file
+#   - perform global findReplace on the ups file
+#
+#   Loop over tests
+#     - create a new ups file
+#     - change the output uda name
+#     - replace lines in ups file
+#     - <pbs section not used>
+#     - create a xml file that is read by analyze_results.pl
+#     - run the test
+#
+#     if(comparison Command )
+#       -run analyze_results.pl
+#     endif
+#   end Loop
+#
+#  Perl Dependencies:  
+#    libxml-simple-perl
+#    libxml-dumper-perl
+#______________________________________________________________________
+
 use XML::Simple;
 use Data::Dumper;
 use Cwd;
@@ -22,12 +53,11 @@ $ups_basename    =~ s/.ups//;                               # Removing the exten
 foreach $e (@{$data->{Test}}){
 
   $test_title[$i]     =$e->{Title}->[0];                    # test title
-  $test_pbsF[$i]      =$e->{pbsFile}->[0];                  # pbs filename for the queue
-  $int_command[$i]    =$e->{Interactive}->[0];              # interactive command
+  $sus_cmd[$i]        =$e->{sus_cmd}->[0];                 # sus command
 
   $study[$i]          =$e->{Study}->[0];                    #Study Name
   $x[$i]              =$e->{x}->[0];
-  $compUtil_cmd[$i]   =$e->{compCommand}->[0];              #comparison utility command
+  $compUtil_cmd[$i]   =$e->{compare_cmd}->[0];              #comparison utility command
   
   $i++;     
 }
@@ -116,17 +146,21 @@ open(statsFile,">out.stat");
 #__________________________________
 # Creating new ups files for each test
 for ($i=0;$i<$num_of_tests;$i++){
-  open(inpFile, $upsFile) or die("ERROR(run_tests.pl): $upsFile, File Not Found");
+  #open(inpFile, $upsFile) or die("ERROR(run_tests.pl): $upsFile, File Not Found");
 
+  if (! -e $upsFile ){
+    print "\n\nERROR(run_tests.pl): $upsFile, File Not Found";
+    print " Now exiting\n";
+    exit
+  }
+  
   my $test_ups;
   my $test_output;
 
   $test_ups     = $ups_basename."_$test_title[$i]".".ups";
   $udaFilename  = $ups_basename."_$test_title[$i]".".uda";
-  $test_pbs     = $test_title[$i].".pbs";
   $compFilename = $test_title[$i]."_comp.xml";
   $test_output  = "out.".$test_title[$i];
-  $int          = $int_command[$i];
 
   # change the uda filename in each ups file
   print "---------------------\n";
@@ -145,47 +179,6 @@ for ($i=0;$i<$num_of_tests;$i++){
     print "\t\t$rp\n"
   }
   print "---------------------\n";
-
-  #__________________________________
-  # Modifying the pbs file 
-  # If the <interactive> tag and the <pbsFile> are both given then <interactive> will get preference
-
-  if ( $int eq ""){
-    open(inpFile, $test_pbsF[$i]) or die("ERROR(run_tests.pl): $test_pbsF[$i], File Not Found\n");
-    open(outFile, ">$test_pbs")   or die("ERROR(run_tests.pl): $test_pbs, File cannot be created\n");
-     
-    while($line=<inpFile>){
-      # Set LAMJOB to point to the new ups file
-
-      if ($line =~ m/set\s*LAMJOB/){
-        @tmp_arr = split(" ",$line);
-        
-        foreach $tmp_var (@tmp_arr){
-          if ($tmp_var =~ /\.ups/){
-            $tmp_var =~ s/\S*/$test_ups/;  # This replaces the ups file argument  in the pbs file
-          }       
-        }
-        $line = join(' ',@tmp_arr)."'"."\n";
-      }
-      
-      # Set OUT file to correspond to the new ups name
-
-      if($line =~ m/set\s*OUT/){
-        $line = "set OUT = \"out.$ups_basename"."_$test_title[$i]".".000\"\n";
-      }
-
-      # Right before we exit we need to schedule the batch_compare script
-#     $string =~ s/^\s+//;
-#     $string =~ s/\s+$//;
-      if ($compUtil_cmd[$i]){
-        if($line =~ m/^\s*exit/){
-          $line = "analyze_results.pl $compFilename \nexit \n";
-        }
-      }
-      print outFile $line;   
-    } # while
-  }
-  close(outFile);
 
   #__________________________________
   # Create a comparison config file _if_ the comparison command is specified
@@ -212,30 +205,24 @@ for ($i=0;$i<$num_of_tests;$i++){
   print statsFile "output:     "."$test_output"."\n";
   print statsFile "compareCmd: "."$compUtil_cmd[$i]"."\n";
   
-  if ($int eq ""){
-    print statsFile "Queue file (pbs) : "."$test_pbs"."\n";
-    $tmp=`qsub $test_pbs`;
-  }else{
-    print statsFile "Command Used (interactive) : "."$int $test_ups"."\n";
-    print "Launching $int $test_ups\n";
-    $now = time();
+  print statsFile "Command Used : "."$sus_cmd[$i] $test_ups"."\n";
+  print "Launching $sus_cmd[$i] $test_ups\n";
+  $now = time();
 
-    @args = ("$int","$test_ups",">& $test_output");
-    system("@args")==0 or die("ERROR(run_tests.pl): @args failed: $?");
-    
-    #__________________________________
-    # execute comparison
-    if($compUtil_cmd[$i]){
+  @args = ("$sus_cmd[$i]","$test_ups",">& $test_output");
+  system("@args")==0 or die("ERROR(run_tests.pl): @args failed: $?");
 
-      print "\n\nLaunching analyze_results.pl $compFilename\n\n";
-      @args = ("analyze_results.pl","$compFilename");
-      system("@args")==0 or die("ERROR(run_tests.pl): \t\tFailed running: (@args)\n");
-      
-      system("rm $compFilename");
-    }
-    $fin = time()-$now;
-    print  statsFile "Running Time : ".$fin."\n";
+  #__________________________________
+  # execute comparison
+  if($compUtil_cmd[$i]){
+    print "\n\nLaunching analyze_results.pl $compFilename\n\n";
+    @args = ("analyze_results.pl","$compFilename");
+    system("@args")==0 or die("ERROR(run_tests.pl): \t\tFailed running: (@args)\n");
+
+    system("rm $compFilename");
   }
+  $fin = time()-$now;
+  print  statsFile "Running Time : ".$fin."\n";
   print statsFile "---------------------------------------------\n";
 }  # all tests loop
 
