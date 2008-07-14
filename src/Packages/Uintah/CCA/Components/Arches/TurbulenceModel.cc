480ea66e0c84dc5de13f2287c9a0fa3f79939d4f
Todd Harman <harman@sci.utah.edu>
2008-07-14 22:14:44 +0000

    This commit should not change the answers: rework requires/allocatePut
    Ghost::AroundCells -> gac
    Ghost::None        -> gn
    Ghost::AroundFaces -> gaf
    
    added {}
    2 space indentation
    formatting

--- src/Packages/Uintah/CCA/Components/Arches/TurbulenceModel.cc
+++ src/Packages/Uintah/CCA/Components/Arches/TurbulenceModel.cc
@@ -48,9 +48,8 @@ TurbulenceModel::sched_initFilterMatrix(const LevelP& level,
   d_filter->sched_buildFilterMatrix(level, sched);
   Task* tsk = scinew Task("TurbulenceModel::initFilterMatrix",this,
                           &TurbulenceModel::initFilterMatrix);
-                          
-  tsk->requires(Task::NewDW, d_lab->d_cellTypeLabel, 
-                Ghost::AroundCells, Arches::ONEGHOSTCELL);
+                                              
+  tsk->requires(Task::NewDW, d_lab->d_cellTypeLabel, Ghost::AroundCells, 1);
   sched->addTask(tsk, patches, matls);
 }
 //______________________________________________________________________
@@ -61,20 +60,21 @@ TurbulenceModel::initFilterMatrix(const ProcessorGroup* pg,
                                   const MaterialSubset*,
                                   DataWarehouse*,
                                   DataWarehouse* new_dw)
-{
+{ 
   for (int p = 0; p < patches->size(); p++) {
     const Patch* patch = patches->get(p);
     int archIndex = 0; // only one arches material
     int matlIndex = d_lab->d_sharedState->getArchesMaterial(archIndex)->getDWIndex(); 
     constCCVariable<int> cellType;
-    new_dw->get(cellType, d_lab->d_cellTypeLabel,
-                matlIndex, patch, Ghost::AroundCells, Arches::ONEGHOSTCELL);
+    
+    new_dw->get(cellType, d_lab->d_cellTypeLabel,matlIndex, patch, Ghost::AroundCells, 1);
 
     PerPatch<CellInformationP> cellInfoP;
-    if (new_dw->exists(d_lab->d_cellInfoLabel, matlIndex, patch)) 
+    if (new_dw->exists(d_lab->d_cellInfoLabel, matlIndex, patch)){ 
       new_dw->get(cellInfoP, d_lab->d_cellInfoLabel, matlIndex, patch);
-    else 
+    }else{ 
       throw VariableNotFoundInGrid("cellInformation"," ", __FILE__, __LINE__);
+    }
     CellInformation* cellinfo = cellInfoP.get().get_rep();
 
     d_filter->setFilterMatrix(pg, patch, cellinfo, cellType);
