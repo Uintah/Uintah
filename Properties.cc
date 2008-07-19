12efc739310133131a1d97900556337c9c4d37bf
Jeremy Nicholas Thornock <jthornoc@sci.utah.edu>
2008-07-19 00:30:37 +0000

    Fixed a missing curly-brace on an ambiguous "if" statement that was causing a warning.

--- Properties.cc
+++ Properties.cc
@@ -1794,9 +1794,9 @@ Properties::averageRKProps(const ProcessorGroup*,
           IntVector currCell(colX, colY, colZ);
           
 //The following if statement is to eliminate Kumar's zero block density problem
-          if (new_density[currCell] > 0.0) {
           double predicted_density;
-          if (old_density[currCell] > 0.0)
+          if (new_density[currCell] > 0.0) {
+            if (old_density[currCell] > 0.0)
 //            predicted_density = rho1_density[currCell];
             if (d_inverse_density_average)
               predicted_density = 1.0/((factor_old/old_density[currCell] +
@@ -1804,40 +1804,41 @@ Properties::averageRKProps(const ProcessorGroup*,
             else
               predicted_density = (factor_old*old_density[currCell] +
                                  factor_new*new_density[currCell])/factor_divide;
-          else
+          }
+          else 
             predicted_density = new_density[currCell];
 
           bool average_failed = false;
-            if (d_inverse_density_average)
-              new_scalar[currCell] = (factor_old*old_scalar[currCell] +
-                  factor_new*new_scalar[currCell])/factor_divide;
-            else
-              new_scalar[currCell] = (factor_old*old_density[currCell]*
-                  old_scalar[currCell] + factor_new*new_density[currCell]*
-                  new_scalar[currCell])/(factor_divide*predicted_density);
+          if (d_inverse_density_average)
+            new_scalar[currCell] = (factor_old*old_scalar[currCell] +
+                factor_new*new_scalar[currCell])/factor_divide;
+          else
+            new_scalar[currCell] = (factor_old*old_density[currCell]*
+                old_scalar[currCell] + factor_new*new_density[currCell]*
+                new_scalar[currCell])/(factor_divide*predicted_density);
 // Following lines to fix density delay problem for helium.
 // One would also need to edit fortran/explicit.F to use it.
 //            (new_scalar)[currCell] = (new_scalar)[currCell]*predicted_density;
 //            (new_scalar)[currCell] = (new_scalar)[currCell]*0.133/(
 //              0.133*1.184344+(new_scalar)[currCell]*(0.133-1.184344));
-            if (new_scalar[currCell] > 1.0) {
-              if (new_scalar[currCell] < 1.0 + epsilon)
-                new_scalar[currCell] = 1.0;
-              else {
-                cout << "average failed with scalar > 1 at " << currCell << " , average value was " << new_scalar[currCell] << endl;
-                new_scalar[currCell] = fe_scalar[currCell];
-                average_failed = true;
-              }
+          if (new_scalar[currCell] > 1.0) {
+            if (new_scalar[currCell] < 1.0 + epsilon)
+              new_scalar[currCell] = 1.0;
+            else {
+              cout << "average failed with scalar > 1 at " << currCell << " , average value was " << new_scalar[currCell] << endl;
+              new_scalar[currCell] = fe_scalar[currCell];
+              average_failed = true;
+             }
+          }
+          else if (new_scalar[currCell] < 0.0) {
+            if (new_scalar[currCell] > - epsilon)
+              new_scalar[currCell] = 0.0;
+            else {
+              cout << "average failed with scalar < 0 at " << currCell << " , average value was " << new_scalar[currCell] << endl;
+              new_scalar[currCell] = fe_scalar[currCell];
+              average_failed = true;
             }
-            else if (new_scalar[currCell] < 0.0) {
-              if (new_scalar[currCell] > - epsilon)
-                new_scalar[currCell] = 0.0;
-              else {
-                cout << "average failed with scalar < 0 at " << currCell << " , average value was " << new_scalar[currCell] << endl;
-                new_scalar[currCell] = fe_scalar[currCell];
-                average_failed = true;
-              }
-            }
+          }
           //__________________________________
           if (d_calcReactingScalar) {
             if (!average_failed) {
@@ -1847,17 +1848,17 @@ Properties::averageRKProps(const ProcessorGroup*,
                 new_reactScalar[currCell])/
                 (factor_divide*predicted_density);
                 
-            if (new_reactScalar[currCell] > 1.0){
-              new_reactScalar[currCell] = 1.0;
-            }else if (new_reactScalar[currCell] < 0.0)
-              new_reactScalar[currCell] = 0.0;
+              if (new_reactScalar[currCell] > 1.0){
+                new_reactScalar[currCell] = 1.0;
+              }else if (new_reactScalar[currCell] < 0.0)
+                new_reactScalar[currCell] = 0.0;
             }else{
               new_reactScalar[currCell] = fe_reactScalar[currCell];
             }
           }
           
           //__________________________________
-          if (d_calcEnthalpy)
+          if (d_calcEnthalpy) {
             if (!average_failed){
               new_enthalpy[currCell] = (factor_old*old_density[currCell]*
                 old_enthalpy[currCell] + factor_new*new_density[currCell]*
@@ -1865,9 +1866,9 @@ Properties::averageRKProps(const ProcessorGroup*,
             }else{
               new_enthalpy[currCell] = fe_enthalpy[currCell];
             }
-            density_guess[currCell] = predicted_density;
-
           }
+          density_guess[currCell] = predicted_density;
+
         }
       }
     }
