d0c32abead936697362573f820813c87dff78a44
Justin Luitjens <luitjens@sci.utah.edu>
2008-10-01 04:55:40 +0000

    Undid an optimization to the load balancer I added earlier.  This appears to be causing some problems.

--- Packages/Uintah/CCA/Components/LoadBalancers/DynamicLoadBalancer.cc
+++ Packages/Uintah/CCA/Components/LoadBalancers/DynamicLoadBalancer.cc
@@ -821,7 +821,7 @@ bool DynamicLoadBalancer::assignPatchesFactor(const GridP& grid, bool force)
     if(minProcLoc!=-1 && num_procs>1)
     {
       //broadcast load balance
-      MPI_Bcast(&d_tempAssignment[level_offset],num_patches,MPI_INT,minProcLoc,d_myworld->getComm());
+      MPI_Bcast(&d_tempAssignment[0],d_tempAssignment.size(),MPI_INT,minProcLoc,d_myworld->getComm());
     }
 
     if(!d_levelIndependent)
