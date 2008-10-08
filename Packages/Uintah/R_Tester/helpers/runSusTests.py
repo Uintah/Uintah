120bfbebf11e4c33bf6553f787b00f1f9eb95c42
Todd Harman <harman@sci.utah.edu>
2008-10-08 17:10:50 +0000

    Remove -nsigs it does not work with openmpi

--- Packages/Uintah/R_Tester/helpers/runSusTests.py
+++ Packages/Uintah/R_Tester/helpers/runSusTests.py
@@ -417,7 +417,7 @@ def runSusTest(test, susdir, inputxml, compare_root, ALGO, dbg_opt, max_parallel
   # set the command name for mpirun - differs on different platforms
   MPIHEAD="mpirun -np"
   if environ['OS'] == "Linux":
-    MPIHEAD="mpirun -nsigs -x MALLOC_STATS,SCI_SIGNALMODE -np" 
+    MPIHEAD="mpirun -x MALLOC_STATS,SCI_SIGNALMODE -np" 
 
   # set where to view the log files
   logpath = environ['WEBLOG']
