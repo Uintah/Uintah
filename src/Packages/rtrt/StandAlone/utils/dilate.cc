6ca45ec6b147ac0f949daab13c5a24307cb342a9
John Schmidt <jas@sci.utah.edu>
2009-01-26 19:49:43 +0000

    Add the MIT License to every *.cc, *.h,*.F, and sub.mk file.

--- src/Packages/rtrt/StandAlone/utils/dilate.cc
+++ src/Packages/rtrt/StandAlone/utils/dilate.cc
@@ -1,5 +1,35 @@
 /*
 
+The MIT License
+
+Copyright (c) 1997-2009 Center for the Simulation of Accidental Fires and 
+Explosions (CSAFE), and  Scientific Computing and Imaging Institute (SCI), 
+University of Utah.
+
+License for the specific language governing rights and limitations under
+Permission is hereby granted, free of charge, to any person obtaining a 
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation 
+the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+and/or sell copies of the Software, and to permit persons to whom the 
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included 
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+DEALINGS IN THE SOFTWARE.
+
+*/
+
+
+/*
+
 cd /home/bigler/SCIRun/src/Packages/rtrt/Core/PathTracer/
 g++ -I/home/bigler/SCIRun/Thirdparty/1.20/Linux/gcc-3.2.2-32bit/include -g dilate.cc -o ~/SCIRun/rtrt/Packages/rtrt/StandAlone/dilate -Wl,-rpath -Wl,/home/bigler/SCIRun/Thirdparty/1.20/Linux/gcc-3.2.2-32bit/lib -L/home/bigler/SCIRun/Thirdparty/1.20/Linux/gcc-3.2.2-32bit/lib -lteem -lm
 
