a24d84bb8454b611f32e026033e1e82c83a1e4a0
John Schmidt <jas@sci.utah.edu>
2009-02-12 01:44:06 +0000

    Fix compiler errors.

--- src/Packages/Uintah/CCA/Components/Arches/MCRT/ArchesRMCRT/BottomRealSurface.h
+++ src/Packages/Uintah/CCA/Components/Arches/MCRT/ArchesRMCRT/BottomRealSurface.h
@@ -47,7 +47,7 @@ public:
 
 // n top -- n = 0 i + 0 j + 1 k  
   inline
-  void BottomRealSurface::get_n(){
+  void get_n(){
     n[0] = 0;
     n[1] = 0;
     n[2] = 1;
@@ -56,7 +56,7 @@ public:
 
 // t1 top -- t1 = 1 i + 0 j + 0 k
  inline
- void BottomRealSurface::get_t1(){
+ void get_t1(){
    t1[0] = 1;
    t1[1] = 0;
    t1[2] = 0;
@@ -66,7 +66,7 @@ public:
   
   // t2 top -- t2 = 0 i + 1 j + 0 k
   inline
-  void BottomRealSurface::get_t2(){
+  void get_t2(){
     t2[0] = 0;
     t2[1] = 1;
     t2[2] = 0;
@@ -75,14 +75,14 @@ public:
   
   // get private normal vector n, for class ray
   inline
-  void BottomRealSurface::set_n(double *nn){
+  void set_n(double *nn){
     for ( int i = 0; i < 3; i ++ )
       nn[i] = n[i];
   }
   
 
   inline
-  void BottomRealSurface::get_limits(const double *X,
+  void get_limits(const double *X,
 				     const double *Y,
 				     const double *Z){
     
