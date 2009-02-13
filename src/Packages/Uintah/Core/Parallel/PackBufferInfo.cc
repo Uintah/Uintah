f0b16d2e02fe17d0f0f1b1af11e79fa1037e38eb
Justin Luitjens <luitjens@sci.utah.edu>
2009-02-13 16:47:17 +0000

    Removed mpi compression code.  This never provided any benifit and hasn't been used.

--- src/Packages/Uintah/Core/Parallel/PackBufferInfo.cc
+++ src/Packages/Uintah/Core/Parallel/PackBufferInfo.cc
@@ -41,12 +41,6 @@ using namespace Uintah;
 #include <zlib.h>
 #include <string.h>
 
-//this should probably be made dynamic
-const unsigned long COMPRESS_BUF_SIZE=10000000;
-char compress_buf[COMPRESS_BUF_SIZE];
-
-int PackBufferInfo::compression_level=5;
-unsigned long PackBufferInfo::compression_threshold=50;
 PackBufferInfo::PackBufferInfo()
   : BufferInfo()
 {
@@ -114,48 +108,6 @@ PackBufferInfo::pack(MPI_Comm comm, int& out_count)
   
   out_count = position;
 
-  //if larger than compression threshold
-  if(compression_level>0 && out_count>(int)compression_threshold)
-  {
-    ASSERT(out_count*1.001+12<COMPRESS_BUF_SIZE);
-    unsigned long size=COMPRESS_BUF_SIZE;
-    //compress the buffer
-
-    int retval;
-    if( (retval=compress2((Bytef*)compress_buf,&size,(const Bytef*)buf,out_count,compression_level)) != Z_OK)
-    {
-      switch(retval)
-      {
-        case Z_MEM_ERROR:
-          throw SCIRun::InternalError("Compression returned Z_MEM_ERROR", __FILE__, __LINE__);
-          break;
-        case Z_BUF_ERROR:
-          throw SCIRun::InternalError("Compression returned Z_BUF_ERROR", __FILE__, __LINE__);
-          break;
-        case Z_DATA_ERROR:
-          throw SCIRun::InternalError("Compression returned Z_DATA_ERROR", __FILE__, __LINE__);
-          break;
-        case Z_STREAM_ERROR:
-          throw SCIRun::InternalError("Compression returned Z_STREAM_ERROR", __FILE__, __LINE__);
-          break;
-        default:
-          throw SCIRun::InternalError("Compression of MPI message failed", __FILE__, __LINE__);
-          break;
-      }
-    }
-    else
-    {
-      //only use compressed buffer if the size has improved
-      if(size<(unsigned long)out_count)
-      {
-        //copy to buffer
-        memcpy(buf,compress_buf,size);
-        //set compressed buffer size
-        out_count=size;
-      }
-    }
-  }
-  
   // When it is all packed, only the buffer necessarily needs to be kept
   // around until after it is sent.
   delete sendlist;
@@ -170,48 +122,6 @@ PackBufferInfo::unpack(MPI_Comm comm,MPI_Status &status)
   ASSERT(have_datatype);
   
   unsigned long bufsize = packedBuffer->getBufSize();
-
-  
-  if(compression_level>0 && bufsize>compression_threshold)
-  {
-    int compressed_size;
-    MPI_Get_count(&status,MPI_BYTE,&compressed_size);
-  
-    //if the recieved size is less than the bufsize then the message was likely compressed
-      //there are cases where it still might be uncompressed.
-      //for example:  if MPI_Pack_size returned a value that is larger than the actual space needed
-    if((unsigned long)compressed_size<bufsize)
-    {
-      //copy to the buffer
-      memcpy(compress_buf,buf,compressed_size);
-
-      int retval;
-      //uncompress buffer
-      if( (retval=uncompress((Bytef*)buf,&bufsize,(const Bytef*)compress_buf,compressed_size))  != Z_OK)
-      {
-        switch(retval)
-        {
-          case Z_MEM_ERROR:
-            throw SCIRun::InternalError("Uncompression returned Z_MEM_ERROR", __FILE__, __LINE__);
-            break;
-          case Z_BUF_ERROR:
-            throw SCIRun::InternalError("Uncompression returned Z_BUF_ERROR", __FILE__, __LINE__);
-            break;
-          case Z_STREAM_ERROR:
-            throw SCIRun::InternalError("Uncompression returned Z_STREAM_ERROR", __FILE__, __LINE__);
-            break;
-          case Z_DATA_ERROR: 
-            //this likely means the data was not compressed so just copy the message over
-            memcpy(buf,compress_buf,compressed_size);
-            break;
-          default:
-            throw SCIRun::InternalError("Uncompression of MPI message failed", __FILE__, __LINE__);
-            break;
-        }
-      }
-      ASSERT(bufsize==(unsigned long)packedBuffer->getBufSize());
-    }
-  }
 
   int position = 0;
   for (int i = 0; i < (int)startbufs.size(); i++) {
