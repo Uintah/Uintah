006aef6f31ec5029333f419da6e8550ec76beb6b
Justin Luitjens <luitjens@sci.utah.edu>
2009-03-21 18:07:13 +0000

    Modified the WAIT_FOR_DEBUGGER macro so that it is now a function call.  This is nicer in gdb.

--- Core/Util/Assert.h
+++ Core/Util/Assert.h
@@ -108,6 +108,7 @@
 
 #define ASSERT(condition) ASSERTL2(condition)
 
-#define WAIT_FOR_DEBUGGER { bool wait=true; char hostname[100]; gethostname(hostname,100); printf("%s:%d waiting for debugger\n",hostname,getpid()); while(wait); }
+//#define WAIT_FOR_DEBUGGER { bool wait=true; char hostname[100]; gethostname(hostname,100); printf("%s:%d waiting for debugger\n",hostname,getpid()); while(wait); }
 
+void WAIT_FOR_DEBUGGER();
 #endif
