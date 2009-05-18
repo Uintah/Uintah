c5243973c6bd77c1f9caedbf04c569776d602e43
John Schmidt <jas@sci.utah.edu>
2009-05-18 20:40:23 +0000

    Shell script to build the documentation and post it to the uintah website.

---
+++ scripts/build_documentation.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+
+cd /tmp
+
+svn checkout https://gforge.sci.utah.edu/svn/uintah/trunk/doc uintah/doc
+
+cd uintah/doc
+
+./runLatex
+
+cp *.pdf /var/www/uintah/htdocs
+
+chown www-data.root /var/www/uintah/htdocs/*.pdf
+chmod go+r /var/www/uintah/htdocs/*.pdf
+
+exit
