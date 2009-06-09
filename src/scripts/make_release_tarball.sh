24c7732c3213709a63b949c286b5370bb3d28a7a
John Schmidt <jas@sci.utah.edu>
2009-06-09 03:07:40 +0000

    merge revisions 44365-44368 and 44380 from the release.

--- src/scripts/make_release_tarball.sh
+++ src/scripts/make_release_tarball.sh
@@ -1,6 +1,28 @@
 #! /bin/sh
 
-cd ..
+#VERSION
+
+VERSION="1.1.0-alpha"
+
+while getopts v: opt
+
+do 
+  	case "$opt" in
+	   v) VERSION="$OPTARG";;
+        esac
+done
+shift `expr $OPTIND - 1`
+
+
+# Start off in the src/scripts directory.
+
+cd ../..
+
+mkdir uintah-$VERSION
+
+cp -a doc src uintah-$VERSION
+
+cd uintah-$VERSION/src
 
 for i in `find . -name "*.release"`; do 
     j=`echo $i | sed 's/.release//'`;  
@@ -12,9 +34,11 @@ cd ../doc
 
 ./runLatex
 
-cd ..
+cd ../..
 
-tar -X src/exclude.txt --exclude-vcs -cvf uintah.tar doc src
+tar -X src/exclude.txt --exclude-vcs -cvf uintah-$VERSION.tar uintah-$VERSION
+
+gzip uintah-$VERSION.tar
 
 
 
