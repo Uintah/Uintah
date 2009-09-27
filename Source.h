a60dedbbe6a697bdc49f04a046426037cd082310
Jeremy Nicholas Thornock <jthornoc@sci.utah.edu>
2009-09-27 03:23:10 +0000

    Reverting some recently committed code.

--- Source.h
+++ Source.h
@@ -207,13 +207,6 @@ public:
 
 
   ////////////////////////////////////////////////////////////////////////
-  // Add particles source term
-   void computeParticleSource(const ProcessorGroup* pc,
-                               const Patch* patch,
-                               CellInformation* cellinfo,
-                               ArchesVariables* vars,
-                               ArchesConstVariables* constvars);
-  ////////////////////////////////////////////////////////////////////////
   // Add multimaterial source term
   void computemmMomentumSource(const ProcessorGroup* pc,
                                const Patch* patch,
