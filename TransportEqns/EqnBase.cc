611ed99bde7373c6a3e11154b58160f66af0a345
Charles Reid <charlesreid@sci.utah.edu>
2009-10-12 18:39:29 +0000

    Fixing some compiler warnings

--- TransportEqns/EqnBase.cc
+++ TransportEqns/EqnBase.cc
@@ -12,8 +12,8 @@ using namespace Uintah;
 EqnBase::EqnBase(ArchesLabel* fieldLabels, ExplicitTimeInt* timeIntegrator, string eqnName):
 d_fieldLabels(fieldLabels), d_timeIntegrator(timeIntegrator), d_eqnName(eqnName),
 d_doClipping(false), d_doLowClip(false), d_doHighClip(false), d_lowClip(-999999), d_highClip(-999999), d_smallClip(-999999),
-d_constant_init(0.0), d_step_dir("x"), d_step_start(0.0), d_step_end(0.0), d_step_cellstart(0.0), d_step_cellend(0.0), d_step_value(0.0),
-b_stepUsesCellLocation(false), b_stepUsesPhysicalLocation(false)
+b_stepUsesCellLocation(false), b_stepUsesPhysicalLocation(false),
+d_constant_init(0.0), d_step_dir("x"), d_step_start(0.0), d_step_end(0.0), d_step_cellstart(0.0), d_step_cellend(0.0), d_step_value(0.0)
 {
   d_boundaryCond = scinew BoundaryCondition_new( d_fieldLabels ); 
   d_disc = scinew Discretization_new(); 
