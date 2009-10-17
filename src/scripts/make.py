135da9b1ae766c12166af8423eb4a1952bc7ee58
John Schmidt <jas@sci.utah.edu>
2009-10-17 02:05:10 +0000

    Helper script for buildbot compilation.

---
+++ src/scripts/make.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+
+import os,sys
+
+def build(num):
+
+	make_command='make -j' + str(num)
+	make=os.system(make_command)
+
+	if make > 0:
+		os.system('make cleanreally')
+		make=os.system(make_command)
+
+
+	return make
+
+build(sys.argv[1])
+
+exit
