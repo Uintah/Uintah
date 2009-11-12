371578f53d993acb050878f4b6078fe1ef4a09ed
Justin Luitjens <luitjens@sci.utah.edu>
2009-11-12 22:43:29 +0000

    Added a new iterator class called GridSurface iterator.
    This iterator will iterate over the cells on the surface of a grid which is specified 
    by the low and high points of an interior and exterior grid.  Each cell on the surface 
    will be touched exactly once.

---
+++ src/Core/Grid/Variables/GridSurfaceIterator.h
@@ -0,0 +1,288 @@
+/*
+
+The MIT License
+
+Copyright (c) 1997-2009 Center for the Simulation of Accidental Fires and 
+Explosions (CSAFE), and  Scientific Computing and Imaging Institute (SCI), 
+University of Utah.
+
+License for the specific language governing rights and limitations under
+Permission is hereby granted, free of charge, to any person obtaining a 
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation 
+the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+and/or sell copies of the Software, and to permit persons to whom the 
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included 
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+DEALINGS IN THE SOFTWARE.
+
+*/
+
+
+
+#ifndef UINTAH_HOMEBREW_GridSurfaceIterator_H
+#define UINTAH_HOMEBREW_GridSurfaceIterator_H
+
+#include <Core/Geometry/IntVector.h>
+#include <Core/Util/Assert.h>
+#include <iterator>
+#include <Core/Malloc/Allocator.h>
+#include <Core/Grid/uintahshare.h>
+#include <Core/Grid/Variables/GridIterator.h>
+
+namespace Uintah {
+
+using SCIRun::IntVector;
+ using std::ostream;
+
+/**************************************
+
+CLASS
+   GridSurfaceIterator
+   
+   This iterator will iterate over cells that are on the 
+   surface of a grid which is specified by low and
+   high points of and interior and exterior grid.  The iterator
+   will touch all cells that are in the exterior grid but not 
+   in the interior grid exactly once.
+
+
+GENERAL INFORMATION
+
+   GridSurfaceIterator.h
+
+   Justin Luitjens
+   Department of Computer Science
+   University of Utah
+
+   Center for the Simulation of Accidental Fires and Explosions (C-SAFE)
+  
+   Copyright (C) 2000 SCI Group
+
+KEYWORDS
+   GridSurfaceIterator
+
+DESCRIPTION
+   Long description...
+  
+WARNING
+  
+****************************************/
+
+ class UINTAHSHARE GridSurfaceIterator : 
+ public BaseIterator {
+   public:
+     inline ~GridSurfaceIterator() {}
+
+     //////////
+     // Insert Documentation Here:
+     inline void operator++(int) {
+       this->operator++();
+     }
+
+     inline GridSurfaceIterator& operator++() {
+
+       //increment current Face Iterator
+       d_iter++;
+
+       //increment face iterator if necessary
+       while(d_iter.done() && d_curFace<NUMFACES)
+       {
+         d_curFace=Face(static_cast<int>(d_curFace)+1);
+         d_iter=getFaceIterator(d_curFace);
+       }
+
+       //set done flag
+       d_done=d_iter.done();
+
+       return *this;
+     }
+
+     //////////
+     // Insert Documentation Here:
+     inline GridSurfaceIterator operator+=(int step) {
+       GridSurfaceIterator old(*this);
+       for (int i = 0; i < step; i++) 
+         ++(*this);
+       return old;
+     }
+
+     //////////
+     // Insert Documentation Here:
+     inline bool done() const {
+       return d_done;
+     }
+
+     IntVector operator*() const {
+       ASSERT(!d_done);
+       return *d_iter;
+     }
+     inline GridSurfaceIterator(const IntVector& int_low, const IntVector& int_high, const IntVector& ext_low, const IntVector& ext_high)
+       : d_int_low(int_low), d_int_high(int_high), d_ext_low(ext_low), d_ext_high(ext_high) {
+         reset();
+       }
+     inline IntVector begin() const {
+       return d_ext_low;
+     }
+     inline IntVector end() const {
+       return d_ext_high;
+     }
+     inline GridSurfaceIterator(const GridSurfaceIterator& copy)
+       : d_int_low(copy.d_int_low), d_int_high(copy.d_int_high), d_ext_low(copy.d_ext_low), d_ext_high(copy.d_ext_high), d_iter(copy.d_iter), d_done(copy.d_done) {
+       }
+
+     inline GridSurfaceIterator& operator=( const GridSurfaceIterator& copy ) {
+       d_int_low=copy.d_int_low;
+       d_int_high=copy.d_int_high;
+       d_ext_low=copy.d_ext_low;
+       d_ext_high=copy.d_ext_high;
+       d_iter=copy.d_iter;
+       d_done = copy.d_done;
+       return *this;
+     }
+     bool operator==(const GridSurfaceIterator& o) const
+     {
+       return d_int_low==o.d_int_low &&
+         d_int_high==o.d_int_high &&
+         d_ext_low==o.d_ext_low &&
+         d_ext_high==o.d_ext_high &&
+         *d_iter==*o.d_iter;
+     }
+
+     bool operator!=(const GridSurfaceIterator& o) const
+     {
+       return d_int_low!=o.d_int_low ||
+         d_int_high!=o.d_int_high ||
+         d_ext_low!=o.d_ext_low ||
+         d_ext_high!=o.d_ext_high ||
+         *d_iter!=*o.d_iter;
+     }
+     friend std::ostream& operator<<(std::ostream& out, const Uintah::GridSurfaceIterator& b);
+
+     friend class GridIterator;
+
+     inline void reset()
+     {
+       d_curFace=XMINUS;
+       d_iter=getFaceIterator(d_curFace);
+       //increment face iterator if necessary
+       while(d_iter.done() && d_curFace<NUMFACES)
+       {
+         d_curFace=Face(static_cast<int>(d_curFace)+1);
+         d_iter=getFaceIterator(d_curFace);
+       }
+
+       d_done=d_iter.done();
+     }
+
+     ostream& limits(ostream& out) const
+     {
+       out << begin() << " " << end() - IntVector(1,1,1);
+       return out;
+     }
+
+   private:
+     GridSurfaceIterator();
+
+     GridSurfaceIterator* clone() const
+     {
+       return scinew GridSurfaceIterator(*this);
+     }
+
+     ostream& put(std::ostream& out) const
+     {
+       out << *this;
+       return out;
+     }
+
+     enum Face {XMINUS=0,YMINUS=1,ZMINUS=2,XPLUS=3,YPLUS=4,ZPLUS=5,NUMFACES=6};
+
+     GridIterator getFaceIterator(Face curFace)
+     {
+       IntVector low,high;
+       switch(curFace)
+       {
+          case XMINUS:
+            low=d_ext_low;
+            high=d_ext_high;
+            //restrict to x face
+            high.modifiable_x()=d_int_low.x();
+            break;
+          case YMINUS:
+            low=d_ext_low;
+            high=d_ext_high;
+            //restrict to the y face
+            high.modifiable_y()=d_int_low.y();
+            //remove x face cells
+            low.modifiable_x()=d_int_low.x();
+            high.modifiable_x()=d_int_high.x();
+            break;
+          case ZMINUS:
+            low=d_ext_low;
+            high=d_ext_high;
+            //restrict to the z face
+            high.modifiable_z()=d_int_low.z();
+            //remove x face cells
+            low.modifiable_x()=d_int_low.x();
+            high.modifiable_x()=d_int_high.x();
+            //remove y face cells
+            low.modifiable_y()=d_int_low.y();
+            high.modifiable_y()=d_int_high.y();
+            break;
+          case XPLUS:
+            low=d_ext_low;
+            high=d_ext_high;
+            //restrict to x face
+            low.modifiable_x()=d_int_high.x();
+            break;
+          case YPLUS:
+            low=d_ext_low;
+            high=d_ext_high;
+            //restrict to y face
+            low.modifiable_y()=d_int_high.y();
+            //remove x face cells
+            low.modifiable_x()=d_int_low.x();
+            high.modifiable_x()=d_int_high.x();
+            break;
+          case ZPLUS:
+            low=d_ext_low;
+            high=d_ext_high;
+            //restrict to z face
+            low.modifiable_z()=d_int_high.z();
+            //remove x face cells
+            low.modifiable_x()=d_int_low.x();
+            high.modifiable_x()=d_int_high.x();
+            //remove y face cells
+            low.modifiable_y()=d_int_low.y();
+            high.modifiable_y()=d_int_high.y();
+            break;
+          default:
+            //return an empty iterator
+            low=IntVector(0,0,0),high=IntVector(0,0,0);
+    
+       }
+       return GridIterator(low,high);
+     }
+
+     //////////
+     // Insert Documentation Here:
+     IntVector d_int_low, d_int_high;
+     IntVector d_ext_low, d_ext_high;
+     Face d_curFace;            //The current face we are iterating over
+     GridIterator d_iter;      //The current face iterator
+     bool d_done;
+
+}; // end class GridSurfaceIterator
+
+} // End namespace Uintah
+  
+#endif
