0727c9937500cd0026259bbe5a79791e64de4d34
J. Davison de St. Germain <dav@sci.utah.edu>
2009-11-24 23:22:16 +0000

    Initial commit for global min/max display.  Seems to work, but hasn't been extensively tested yet.  Also not tested on multi-level UDAs.

--- StandAlone/tools/puda/varsummary.h
+++ StandAlone/tools/puda/varsummary.h
@@ -37,4 +37,8 @@ namespace Uintah {
 
   void varsummary( DataArchive * da, CommandLineFlags & clf, int matl );
 
+  // Must be called after varsummary.  Will print out a list of the global min/max for each variable
+  // that was parsed in varsummary().
+  void displayGlobalMinMax();
+
 }
