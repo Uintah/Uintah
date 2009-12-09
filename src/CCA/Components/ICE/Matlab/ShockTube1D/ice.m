df044ddfeb1121abf64253d1215111bd3e69c9f3
Todd Harman <harman@sci.utah.edu>
2009-12-09 20:51:55 +0000

    Added initialCondition function.
    Order of analysis for Matlab:ICE is now working for Toro's test1 problem.

--- src/CCA/Components/ICE/Matlab/ShockTube1D/ice.m
+++ src/CCA/Components/ICE/Matlab/ShockTube1D/ice.m
@@ -39,16 +39,14 @@ P.nCells            = nCells;                        % Number of cells in each d
 P.extraCells        = 1;                            % Number of ghost cells in each direction
 
 % Time-stepping
-P.maxTime           = 5e-4;                         % Maximum simulation time [sec]
 P.initTime          = 0.0;                          % Initial simulation time [sec]
 P.writeData         = 1;                            % output the final timestep to a .dat file
 P.delt_init         = 1e-7;                         % First timestep [sec]
 P.maxTimeSteps      = 10000;                          % Maximum number of timesteps [dimensionless]
 P.CFL               = 0.25;                         % Courant number (~velocity*delT/delX) [dimensionless]
 P.advectionOrder    = 1;                            % 1=1st-order advection operator; 2=possibly-limited-2nd-order
-% Material properties (ideal gas)
-P.cv                = 717.5;                        % Specific_heat
-P.gamma             = 1.4;                          % gamma coefficient in the Equation of State (EOS)
+
+[P, Region, numRegions] = initialConditions()
 
 %================ ICE Interal Parameters, Debugging Flags ================
 % Debug flags
@@ -70,32 +68,6 @@ G.first_FC    = 2;                                  % Index of first interior xm
 G.last_CC     = G.nCells+1;                         % Index of last interior cell
 G.last_FC     = G.ghost_Right;                      % index of last xminus face
 
-
-%================ Partition of the domain into regions ================
-numRegions          = 2;                            % Partition of the domain into numRegions regions; physical properties are uniform within each region
-Region              = cell(numRegions,1);           % This array holds the geometrical partition physical info
-count               = 0;
-
-%********** Parameters of the high density left region **********
-count               = count+1;
-R.label             = 'leftpartition';              % Title of this region
-R.min               = 0;                            % Location of lower-left corner of this region [length]
-R.max               = 0.5;                          % Location of upper-right corner of this region [length]
-R.velocity          = 0.0;                          % Initial velocity
-R.temperature       = 300.0;                        % Initial temperature [Kelvin]
-R.density           = 1.1768292682926831000;        % Initial density
-R.pressure          = 101325.0;                     % Initial pressure (1 atmosphere)
-Region{count}       = R;                            % Add region to list
-
-%********** Parameters of the low density right region **********
-count               = count+1;
-R.min               = 0.5;                          % Location of lower-left corner of this region [length]
-R.max               = 1;                            % Location of upper-right corner of this region [length]
-R.velocity          = 0.0;                          % Initial velocity
-R.temperature       = 300.0;                        % Initial temperature [Kelvin]
-R.density           = 0.11768292682926831000;        % Initial density
-R.pressure          = 10132.5;                       % Initial pressure (0.1 atmosphere)
-Region{count}       = R;                            % Add region to list
 
 %______________________________________________________________________
 %     Allocate arrays
