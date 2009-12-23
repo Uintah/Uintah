f7669e0f563f4f5a029f564ec285f2740b51904e
Jeremy Nicholas Thornock <jthornoc@sci.utah.edu>
2009-12-23 01:08:01 +0000

    Hopefully cleaning up a few memory leaks.

--- CCA/Components/Arches/SourceTerms/SourceTermFactory.cc
+++ CCA/Components/Arches/SourceTerms/SourceTermFactory.cc
@@ -16,12 +16,12 @@ SourceTermFactory::~SourceTermFactory()
 {
   // delete the builders
   for( BuildMap::iterator i=builders_.begin(); i!=builders_.end(); ++i ){
-    //delete *i;
+      delete i->second;
     }
 
   // delete all constructed solvers
   for( SourceMap::iterator i=sources_.begin(); i!=sources_.end(); ++i ){
-    //delete *i;
+      delete i->second;
   }
 }
 
