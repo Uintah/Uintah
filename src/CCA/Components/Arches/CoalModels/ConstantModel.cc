8e9c6d914962e35cba7f8e28ce8272d3658c2be6
Julien Pedel <jpedel@sci.utah.edu>
2010-01-27 22:58:49 +0000

    reverting a unwanted change

--- src/CCA/Components/Arches/CoalModels/ConstantModel.cc
+++ src/CCA/Components/Arches/CoalModels/ConstantModel.cc
@@ -235,11 +235,12 @@ ConstantModel::computeModel( const ProcessorGroup* pc,
 
     for (CellIterator iter=patch->getCellIterator(); !iter.done(); iter++){
       IntVector c = *iter;
-      if(d_quadNode ==1){
-        model[c] = -d_constant;
-      } else { 
-        model[c] = d_constant;
-      }
+      //if(d_quadNode ==1){
+      //  model[c] = -d_constant;
+      //} else { 
+      //  model[c] = d_constant;
+      //}
+      model[c] = d_constant;
       gas_source[c] = 0.0;
     }
   }
