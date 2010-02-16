1ed81d93e6439cf5e570aa0121de3536c3d3310d
John M. Schreiner <jmschrei@sci.utah.edu>
2010-02-16 22:58:10 +0000

    updating molecule instancing renderer patch for visit - works with current trunk.  Fixes some robustness issues with muliple plots and reinitializing the class.

--- src/VisIt/Molecule/Instancing.Fragment.glsl
+++ src/VisIt/Molecule/Instancing.Fragment.glsl
@@ -31,7 +31,5 @@ void main()
 	color += gl_LightSource[0].specular.xyz * specular * attenuation *  gl_FrontMaterial.specular.xyz;
 	color = clamp( color, 0.0, 1.0 );
 
-	//color = vec3(data_attribute);
 	gl_FragColor = vec4(color,1);
-	// gl_FragColor = vec4(1, 0, 0, 1);
 }
--- src/VisIt/Molecule/Instancing.Vertex.glsl
+++ src/VisIt/Molecule/Instancing.Vertex.glsl
@@ -14,12 +14,10 @@ void main(void)
 {
 	vec4 instance_position_radius = texelFetchBuffer (per_instance_data_position_radius, gl_InstanceID);
 	vec3 instance_attribute =  texelFetchBuffer (per_instance_data_attribute, gl_InstanceID).xyz;
-        
+       
 	vec3 center = instance_position_radius.xyz;
 	float r = instance_position_radius.w;
-	// float a = instance_attribute;
 
-	// data_attribute = a;
 	data_attribute = instance_attribute;
 
 	float theta = gl_Vertex.x;
@@ -32,16 +30,13 @@ void main(void)
 	float cos_phi = cos(phi);
 	float sin_phi = sin(phi);
 
-	float x = sin_phi * cos_theta ;
-	float y = sin_phi * sin_theta  ;
+	float x = sin_phi * cos_theta;
+	float y = sin_phi * sin_theta;
 	float z = cos_phi;
 
 	vec3 object_position = vec3(x,y,z);
 	vec3 object_normal = vec3(x,y,z);
 
-	//object_position = vec3(gl_Vertex.x, gl_Vertex.y, 0);
-	//object_normal  = vec3(0,0,1);
-	//data_attribute = gl_VertexID /  12.0f;
 
 	vec4 position = vec4(center + r * object_position, 1);
 	
