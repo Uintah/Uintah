1e2052661670a4246104233f2dad27455b06b6ed
Todd Harman <harman@sci.utah.edu>
2010-03-26 16:08:15 +0000

    formatting

--- src/Core/Grid/ParticleInterpolator.h
+++ src/Core/Grid/ParticleInterpolator.h
@@ -56,24 +56,32 @@ namespace Uintah {
     
     virtual ParticleInterpolator* clone(const Patch*) = 0;
     
-    virtual void findCellAndWeights(const Point& p,vector<IntVector>& ni, 
-                                    vector<double>& S,const Vector& size, const Matrix3& defgrad) = 0;
+    virtual void findCellAndWeights(const Point& p,
+                                    vector<IntVector>& ni, 
+                                    vector<double>& S,
+                                    const Vector& size, 
+                                    const Matrix3& defgrad) = 0;
+                                    
     virtual void findCellAndShapeDerivatives(const Point& pos,
                                              vector<IntVector>& ni,
                                              vector<Vector>& d_S,
-                                             const Vector& size, const Matrix3& defgrad) = 0;
+                                             const Vector& size, 
+                                             const Matrix3& defgrad=Matrix3(0)) = 0;
+                                             
     virtual void findCellAndWeightsAndShapeDerivatives(const Point& pos,
                                                        vector<IntVector>& ni,
                                                        vector<double>& S,
                                                        vector<Vector>& d_S,
-                                                       const Vector& size, const Matrix3& defgrad) = 0;
+                                                       const Vector& size, 
+                                                       const Matrix3& defgrad) = 0;
 
     virtual void findCellAndWeights(const Point& p,vector<IntVector>& ni,
                                     vector<double>& S,
                                     constNCVariable<Stencil7>& zoi,
                                     constNCVariable<Stencil7>& zoi_fine,
                                     const bool& getFiner,
-                                    int& num_cur,int& num_fine,int& num_coarse,                                     const Vector& size, bool coarse_part,
+                                    int& num_cur,int& num_fine,int& num_coarse,                                     
+                                    const Vector& size, bool coarse_part,
                                     const Patch* patch) {};
 
     virtual int size() = 0;
