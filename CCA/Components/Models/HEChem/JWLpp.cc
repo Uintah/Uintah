5d8c84c5e2e5893aab799dfa872933dc7085aaa5
Todd Harman <harman@sci.utah.edu>
2010-04-14 21:16:13 +0000

    simplified code for computing materialSet

--- CCA/Components/Models/HEChem/JWLpp.cc
+++ CCA/Components/Models/HEChem/JWLpp.cc
@@ -126,24 +126,15 @@ void JWLpp::problemSetup(GridP&, SimulationStateP& sharedState, ModelSetup*)
 
     //__________________________________
     //  define the materialSet
-    vector<int> m_tmp(2);
-    m_tmp[0] = matl0->getDWIndex();
-    m_tmp[1] = matl1->getDWIndex();
-    mymatls = scinew MaterialSet();            
- 
-    if( m_tmp[0] != 0 && m_tmp[1] != 0){
-      vector<int> m(3);
-      m[0] = 0;    // needed for the pressure and NC_CCWeight 
-      m[1] = m_tmp[0];
-      m[2] = m_tmp[1];
-      mymatls->addAll(m);
-    }else{
-      vector<int> m(2);
-      m[0] = m_tmp[0];
-      m[1] = m_tmp[1];
-      mymatls->addAll(m);
-    }
-    mymatls->addReference();
+    mymatls = scinew MaterialSet();
+
+    vector<int> m;
+    m.push_back(0);                       // needed for the pressure and NC_CCWeight
+    m.push_back(matl0->getDWIndex());
+    m.push_back(matl1->getDWIndex());
+
+    mymatls->addAll_unique(m);            // elimiate duplicate entries
+    mymatls->addReference(); 
   }
 }
 //______________________________________________________________________
@@ -179,26 +170,18 @@ void JWLpp::activateModel(GridP&, SimulationStateP& sharedState, ModelSetup*)
 
   matl0 = sharedState->parseAndLookupMaterial(d_params, "fromMaterial");
   matl1 = sharedState->parseAndLookupMaterial(d_params, "toMaterial");
+ 
   //__________________________________
   //  define the materialSet
-  vector<int> m_tmp(2);
-  m_tmp[0] = matl0->getDWIndex();
-  m_tmp[1] = matl1->getDWIndex();
   mymatls = scinew MaterialSet();
-                                                                                
-  if( m_tmp[0] != 0 && m_tmp[1] != 0){
-    vector<int> m(3);
-    m[0] = 0;    // needed for the pressure and NC_CCWeight
-    m[1] = m_tmp[0];
-    m[2] = m_tmp[1];
-    mymatls->addAll(m);
-  }else{
-    vector<int> m(2);
-    m[0] = m_tmp[0];
-    m[1] = m_tmp[1];
-    mymatls->addAll(m);
-  }
-  mymatls->addReference();
+
+  vector<int> m;
+  m.push_back(0);                       // needed for the pressure and NC_CCWeight
+  m.push_back(matl0->getDWIndex());
+  m.push_back(matl1->getDWIndex());
+
+  mymatls->addAll_unique(m);            // elimiate duplicate entries
+  mymatls->addReference(); 
 }
 
 //______________________________________________________________________
--- CCA/Components/Models/HEChem/LightTime.cc
+++ CCA/Components/Models/HEChem/LightTime.cc
@@ -114,27 +114,17 @@ void LightTime::problemSetup(GridP&, SimulationStateP& sharedState,
   // if point  ignition is desired, direction_if_plane = (0.,0.,0)
   // if planar ignition is desired, direction_if_plane is normal in
   // the direction of burning
-
   //__________________________________
   //  define the materialSet
-  vector<int> m_tmp(2);
-  m_tmp[0] = matl0->getDWIndex();
-  m_tmp[1] = matl1->getDWIndex();
-  mymatls = scinew MaterialSet();            
- 
-  if( m_tmp[0] != 0 && m_tmp[1] != 0){
-    vector<int> m(3);
-    m[0] = 0;    // needed for the pressure and NC_CCWeight 
-    m[1] = m_tmp[0];
-    m[2] = m_tmp[1];
-    mymatls->addAll(m);
-  }else{
-    vector<int> m(2);
-    m[0] = m_tmp[0];
-    m[1] = m_tmp[1];
-    mymatls->addAll(m);
-  }
-  mymatls->addReference();
+  mymatls = scinew MaterialSet();
+
+  vector<int> m;
+  m.push_back(0);                                 // needed for the pressure and NC_CCWeight
+  m.push_back(matl0->getDWIndex());
+  m.push_back(matl1->getDWIndex());
+
+  mymatls->addAll_unique(m);                    // elimiate duplicate entries
+  mymatls->addReference(); 
 }
 //______________________________________________________________________
 //     
