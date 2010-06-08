3519e2ab15ec028c7432f08539bc6e15bf868abc
Jim Guilkey <guilkey@sci.utah.edu>
2010-06-08 23:23:54 +0000

    Ooops, flags should have been flag

--- CCA/Components/MPM/Contact/FrictionContact.cc
+++ CCA/Components/MPM/Contact/FrictionContact.cc
@@ -74,7 +74,7 @@ FrictionContact::FrictionContact(const ProcessorGroup* myworld,
   if(flag->d_8or27==8){
     NGP=1;
     NGN=1;
-  } else if(flag->d_8or27==27 || flags->d_8or27==64){
+  } else if(flag->d_8or27==27 || flag->d_8or27==64){
     NGP=2;
     NGN=2;
   }
