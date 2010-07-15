946e1a83fe10710e2c3901bad252b59f6bb7fed3
John Schmidt <jas@sci.utah.edu>
2010-07-15 00:35:43 +0000

    Do not delete the CheckPoints directory on the initial rsync.

--- src/scripts/rsync_testdata.sh
+++ src/scripts/rsync_testdata.sh
@@ -3,7 +3,7 @@
 cd /usr/local
 
 #rsync -aP --delete --no-g --no-p --no-t blaze.sci.utah.edu:/usr/local/home/csafe-tester/Linux/TestData .
-rsync -aPz --delete blaze.sci.utah.edu:/usr/local/home/csafe-tester/Linux/TestData .
+rsync -aPz --delete --exclude CheckPoints blaze.sci.utah.edu:/usr/local/home/csafe-tester/Linux/TestData .
 
 rsync -aPz --delete blaze.sci.utah.edu:/usr/local/home/csafe-tester/CheckPoints TestData
 
