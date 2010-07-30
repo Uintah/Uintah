a7565d721f3ba1a1959030933ad6fdb56f0a1cb2
J. Davison de St. Germain <dav@sci.utah.edu>
2010-07-30 20:16:30 +0000

    another test to create a new branch from a modified source tree

---
+++ davs_test_branch/CCA/Components/Wasatch/transport/sub.mk
@@ -0,0 +1,8 @@
+# Makefile fragment for this subdirectory
+
+SRCDIR   := CCA/Components/Wasatch/transport
+
+SRCS     +=                             		\
+	$(SRCDIR)/TemperatureTransportEquation.cc	\
+	$(SRCDIR)/ScalarTransportEquation.cc		\
+	$(SRCDIR)/ParseEquation.cc
