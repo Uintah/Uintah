7e6973f547d5b2e9616c9bf549fa38ab5e622d03
James Sutherland <jsutherland@sci.utah.edu>
2010-10-07 16:14:26 +0000

    Fix a mistake in an earlier commit that resulted from a bad merge

--- src/CCA/Components/Wasatch/Expressions/ConvectiveFlux.h
+++ src/CCA/Components/Wasatch/Expressions/ConvectiveFlux.h
@@ -17,34 +17,31 @@
 #include <spatialops/SpatialFieldStore.h>
 
 /**
- *  \class  ConvectiveFlux
- *  \author James C. Sutherland
- *  \author Tony Saad
- *  \date   July, 2010
+ *  \ingroup WasatchExpressions
+ *  \class   ConvectiveFlux
+ *  \author  Tony Saad
+ *  \date    July, 2010
  *
  *  \brief Creates an expression for the convective flux of a scalar
- *         given a velocity field \f$\mathbf{u}\f$. We write the
- *         convective flux in conservation form as \f$ J_i = \rho
- *         \varphi u_i = \phi u_i \f$ where \f$i=1,2,3\f$ is the
- *         coordinate direction. This requires knowledge of the
- *         velocity field.
+ *  given a velocity field \f$\mathbf{u}\f$. We write the convective
+ *  flux in conservation form as \f$ J_i = \rho \varphi u_i = \phi u_i
+ *  \f$ where \f$i=1,2,3\f$ is the coordinate direction. This requires
+ *  knowledge of the velocity field.
  *
  *  Here, we are constructing the convective flux J_i, therefore, it
  *  is convenient to set \f$ \rho \varphi \equiv \phi\f$
  *
  *  \par Template Parameters
- *    <ul>
- *     <li> \b PhiInterpT The type of operator used in forming
- *           \f$\frac{\partial \phi}{\partial x}\f$.  Moves the
- *           volume-centered field \f$\phi\f$ to a face centered
- *           field.
- *      <li> \b VelInterpT The type of operator used in interpolating the
- *           velocity from volume to face fields
- *    </ul>
+ *  <ul>
+ *  <li> \b PhiInterpT The type of operator used in forming 
+ *       \f$\frac{\partial \phi}{\partial x}\f$
+ *  <li> \b VelInterpT The type of operator used in interpolating the
+ *       velocity from volume to face fields
+ *  </ul>
  */
 template< typename PhiInterpT, typename VelInterpT > // scalar interpolant and velocity interpolant
 class ConvectiveFlux
-: public Expr::Expression<typename PhiInterpT::DestFieldType>
+  : public Expr::Expression<typename PhiInterpT::DestFieldType>
 {  
   // PhiInterpT: an interpolant from staggered or non-staggered volume field to staggered or non-staggered face field
   typedef typename PhiInterpT::SrcFieldType  PhiVolT; // source field is a scalar volume
@@ -54,8 +51,8 @@ class ConvectiveFlux
   typedef typename VelInterpT::SrcFieldType  VelVolT; // source field is always a staggered volume field.
   typedef typename VelInterpT::DestFieldType VelFaceT;
   // the destination field of VelInterpT should be a PhiFaceT
-
-private:
+  
+protected:
 
   const Expr::Tag phiTag_, velTag_;
   const PhiVolT* phi_;
@@ -68,7 +65,7 @@ public:
                   const Expr::Tag velTag,
                   const Expr::ExpressionID& id,
                   const Expr::ExpressionRegistry& reg  );
-  ~ConvectiveFlux();
+  virtual ~ConvectiveFlux();
   
   void advertise_dependents( Expr::ExprDeps& exprDeps );
   void bind_fields( const Expr::FieldManagerList& fml );
@@ -78,9 +75,9 @@ public:
   class Builder : public Expr::ExpressionBuilder
   {
     
-  private:
+  protected:
     const Expr::Tag phiT_, velT_;
-
+    
   public:
     /**
      *  \brief Construct a convective flux given an expression for
@@ -98,7 +95,76 @@ public:
     Expr::ExpressionBase* build( const Expr::ExpressionID& id,
                                  const Expr::ExpressionRegistry& reg ) const;		
   };
-	
+};
+
+/**
+ *  \ingroup WasatchExpressions
+ *  \class   ConvectiveFluxUpwind
+ *  \author  James C. Sutherland
+ *  \author  Tony Saad
+ *  \date    July, 2010
+ *
+ *  \brief Creates an expression for the convective flux of a scalar
+ *  given a velocity field \f$\mathbf{u}\f$. We write the convective
+ *  flux in conservation form as \f$ J_i = \rho \varphi u_i = \phi u_i
+ *  \f$ where \f$i=1,2,3\f$ is the coordinate direction. This requires
+ *  knowledge of the velocity field.
+ *
+ *  Here, we are constructing the convective flux J_i, therefore, it
+ *  is convenient to set \f$ \rho \varphi \equiv \phi\f$
+ *
+ *  \par Template Parameters
+ *  <ul>
+ *  <li> \b PhiInterpT The type of operator used in forming 
+ *       \f$\frac{\partial \phi}{\partial x}\f$
+ *  <li> \b VelInterpT The type of operator used in interpolating the
+ *        velocity from volume to face fields
+ *  </ul>
+ */
+template< typename PhiInterpT, typename VelInterpT > // scalar interpolant and velocity interpolant
+class ConvectiveFluxUpwind
+  : public ConvectiveFlux<PhiInterpT, VelInterpT>
+{  
+  // PhiInterpT: an interpolant from staggered or non-staggered volume field to staggered or non-staggered face field
+  typedef typename PhiInterpT::SrcFieldType  PhiVolT; // source field is a scalar volume
+  typedef typename PhiInterpT::DestFieldType PhiFaceT; // destination field is scalar face
+  
+  // VelInterpT: an interpolant from Staggered volume field to scalar face field
+  typedef typename VelInterpT::SrcFieldType  VelVolT; // source field is always a staggered volume field.
+  typedef typename VelInterpT::DestFieldType VelFaceT;
+  
+public:
+  class Builder : public Expr::ExpressionBuilder
+  {
+    
+  protected:
+    const Expr::Tag phiT_, velT_;
+    
+  public:
+    /**
+     *  \brief Construct a convective flux given an expression for
+     *         \f$\phi\f$.
+     *
+     *  \param phiTag  the Expr::Tag for the scalar field.
+     *         This is located at cell centroids.
+     *
+     *  \param velTag the Expr::Tag for the velocity field.
+     *         The velocity field is a face field.
+     */
+    Builder( const Expr::Tag phiTag,
+             const Expr::Tag velTag ):phiT_(phiTag), velT_(velTag) {}
+		
+    Expr::ExpressionBase* build( const Expr::ExpressionID& id,
+                                 const Expr::ExpressionRegistry& reg ) const;		
+  };
+  
+  ConvectiveFluxUpwind( const Expr::Tag phiTag,
+                        const Expr::Tag velTag,
+                        const Expr::ExpressionID& id,
+                        const Expr::ExpressionRegistry& reg  );
+  ~ConvectiveFluxUpwind();
+  
+  void evaluate();
 };
 
 // ###################################################################
@@ -139,9 +205,9 @@ template< typename PhiInterpT, typename VelInterpT >
 void ConvectiveFlux<PhiInterpT, VelInterpT>::
 bind_fields( const Expr::FieldManagerList& fml )
 {
-  const Expr::FieldManager<PhiVolT>& phiVolFM = fml.template field_manager<PhiVolT>(); // get references to field manager
-  phi_ = &phiVolFM.field_ref( phiTag_ ); // get reference to phiTag_ and have phi_ point to it
-
+  const Expr::FieldManager<PhiVolT>& phiVolFM = fml.template field_manager<PhiVolT>();
+  phi_ = &phiVolFM.field_ref( phiTag_ );
+  
   const Expr::FieldManager<VelVolT>& velVolFM = fml.template field_manager<VelVolT>();
   vel_ = &velVolFM.field_ref( velTag_ );
 }
@@ -169,12 +235,9 @@ void ConvectiveFlux<PhiInterpT, VelInterpT>::evaluate()
   // move the velocity from staggered volume to phi faces
   velInterpOp_->apply_to_field( *vel_, *velInterp );
 
-  // set the advective velocity in the interpolant operator.
-  // NOTE: this is not a thread-safe operation!
-  phiInterpOp_->set_advective_velocity( *velInterp );
-  
+  // intepolate phi to the control volume faces
   phiInterpOp_->apply_to_field( *phi_, result );
-  // calculate the convective flux: multiply phi by the velocity
+
   result *= *velInterp;
 }
 
@@ -183,7 +246,7 @@ void ConvectiveFlux<PhiInterpT, VelInterpT>::evaluate()
 template< typename PhiInterpT, typename VelInterpT > 
 Expr::ExpressionBase* ConvectiveFlux<PhiInterpT, VelInterpT>::
 Builder::build( const Expr::ExpressionID& id,
-               const Expr::ExpressionRegistry& reg ) const
+                const Expr::ExpressionRegistry& reg ) const
 {
   return new ConvectiveFlux<PhiInterpT,VelInterpT>( phiT_, velT_, id, reg );
 }
@@ -193,31 +256,47 @@ Builder::build( const Expr::ExpressionID& id,
 template< typename PhiInterpT, typename VelInterpT >
 ConvectiveFluxUpwind<PhiInterpT, VelInterpT>::
 ConvectiveFluxUpwind( const Expr::Tag phiTag,
-                     const Expr::Tag velTag,
-                     const Expr::ExpressionID& id,
-                     const Expr::ExpressionRegistry& reg  )
-: ConvectiveFlux<PhiInterpT, VelInterpT>(phiTag, velTag, id, reg)
-//Expr::Expression<PhiFaceT>(id,reg)//, phiTag_( phiTag ), velTag_( velTag )
-{
-}
+                      const Expr::Tag velTag,
+                      const Expr::ExpressionID& id,
+                      const Expr::ExpressionRegistry& reg  )
+  : ConvectiveFlux<PhiInterpT, VelInterpT>(phiTag, velTag, id, reg)
+{}
 
 //--------------------------------------------------------------------
 
 template< typename PhiInterpT, typename VelInterpT > 
 ConvectiveFluxUpwind<PhiInterpT, VelInterpT>::
 ~ConvectiveFluxUpwind()
-{
-}
+{}
+
 //--------------------------------------------------------------------
 
 template< typename PhiInterpT, typename VelInterpT > 
 Expr::ExpressionBase* ConvectiveFluxUpwind<PhiInterpT, VelInterpT>::
 Builder::build( const Expr::ExpressionID& id,
-               const Expr::ExpressionRegistry& reg ) const
+                const Expr::ExpressionRegistry& reg ) const
 {
-	return new ConvectiveFluxUpwind<PhiInterpT,VelInterpT>( this->phiT_, this->velT_, id, reg );
+  return new ConvectiveFluxUpwind<PhiInterpT,VelInterpT>( this->phiT_, this->velT_, id, reg );
 }
 
 //--------------------------------------------------------------------
+
+template< typename PhiInterpT, typename VelInterpT > 
+void ConvectiveFluxUpwind<PhiInterpT, VelInterpT>::evaluate()
+{
+  PhiFaceT& result = this->value();
+
+  // note that PhiFaceT and VelFaceT should on the same mesh location
+  SpatialOps::SpatFldPtr<VelFaceT> velInterp = SpatialOps::SpatialFieldStore<VelFaceT>::self().get( result );
+
+  // move the velocity from staggered volume to phi faces
+  this->velInterpOp_->apply_to_field( *this->vel_, *velInterp );
+
+  this->phiInterpOp_->set_advective_velocity( *velInterp );
+  this->phiInterpOp_->apply_to_field( *this->phi_, result );
+
+  result *= *velInterp;
+}
+
 
 #endif // /ConvectiveFlux_Expr_h
