f0601856a22bdbbbc99701bbe580ecdd3af31d39
Todd Harman <harman@sci.utah.edu>
2010-11-30 21:58:15 +0000

    Global:   removed the testing version of getFineLevelRangeNodes(), using the original   version that has been cleaned up.
    getCoarseLevelRange()
      -now passing in the IntVector boundaryLayer and number of ghost cells.
       Previously, boundaryLayer was hardcoded to (0,0,0)
    
    AMRMPM
      interpolateToParticlesAnUpdate_CFI()
        - now interpolating gvelocityStar_fine & gAcceleration_fine to the CFI
          particles on the coarse level.
          
      interpolateParticlestoGrid()
        - removed debugging code.
        
      interpolateParticlestoGrid_CFI()  
        - using cell based getCoarseLevelRange instead of nodal based getCoarseLevelRangeNodes()
        
      interpolateToParticlesAndUpdate()
        - removed hardwired vel & acc

--- CCA/Components/ICE/AMRICE.h
+++ CCA/Components/ICE/AMRICE.h
@@ -595,7 +595,10 @@ void AMRICE::CoarseToFineOperator(CCVariable<T>& q_CC,
                        
   // region of fine space that will correspond to the coarse we need to get
   IntVector cl, ch, fl, fh;
-  getCoarseLevelRange(finePatch, coarseLevel, cl, ch, fl, fh, 1);
+  IntVector bl(0,0,0);  // boundary layer or padding
+  int nghostCells = 1;
+  
+  getCoarseLevelRange(finePatch, coarseLevel, cl, ch, fl, fh, bl, nghostCells);
 
   cout_dbg <<" coarseToFineOperator: " << varLabel->getName()
            <<" finePatch  "<< finePatch->getID() << " fl " << fl << " fh " << fh
