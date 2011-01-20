3d1aa40f5ad8f3050413f0f85fd5b3c2593eeb85
James Sutherland <jsutherland@sci.utah.edu>
2011-01-20 20:27:59 +0000

    Stress tensor expression modifications.

--- CCA/Components/Wasatch/Expressions/Stress.h
+++ CCA/Components/Wasatch/Expressions/Stress.h
@@ -2,6 +2,7 @@
 #define Stress_Expr_h
 
 #include <expression/Expr_Expression.h>
+#include <CCA/Components/Wasatch/Operators/OperatorTypes.h>
 
 /**
  *  \class Stress
@@ -12,8 +13,6 @@
  *
  *  The stress tensor is given as
  *  \[ \tau_{ij} = -\mu \left( \frac{\partial u_i}{\partial x_j} + \frac{\partial u_j}{\partial x_i} \right) - \frac{2}{3} \mu \delta_{ij} \frac{\partial u_k}{\partial x_k} \]
- *  This expression calculates only the off-diagonal components of the stress tensor,
- *  \[ \tau_{ij} = -\mu \left( \frac{\partial u_i}{\partial x_j} + \frac{\partial u_j}{\partial x_i} \right) \]
  *
  *  \par Template Parameters
  *  <ul>
@@ -30,19 +29,19 @@ template< typename StressT,
 class Stress
  : public Expr::Expression<StressT>
 {
-  const Expr::Tag visct_, vel1t_, vel2t_;
+  const Expr::Tag visct_, vel1t_, vel2t_, dilt_;
 
   typedef typename OperatorTypeBuilder< Interpolant, ViscT, StressT >::type  ViscInterpT;
   typedef typename OperatorTypeBuilder< Gradient,    Vel1T, StressT >::type  Vel1GradT;
   typedef typename OperatorTypeBuilder< Gradient,    Vel2T, StressT >::type  Vel2GradT;
 
-  ViscInterpT* const viscInterpOp_; ///< Interpolate viscosity to the face where we are building the stress
-  Vel1GradT*   const vel1GradOp_;   ///< Calculate the velocity gradient dui/dxj at the stress face
-  Vel2GradT*   const vel2GradOp_;   ///< Calculate the velocity gradient duj/dxi at the stress face
+  const ViscInterpT* viscInterpOp_; ///< Interpolate viscosity to the face where we are building the stress
+  const Vel1GradT*   vel1GradOp_;   ///< Calculate the velocity gradient dui/dxj at the stress face
+  const Vel2GradT*   vel2GradOp_;   ///< Calculate the velocity gradient duj/dxi at the stress face
 
-  ViscT* const visc_;
-  Vel1T* const vel1_;
-  Vel2T* const vel2_;
+  const ViscT* visc_;
+  const Vel1T* vel1_;
+  const Vel2T* vel2_;
 
   Stress( const Expr::Tag viscTag,
           const Expr::Tag vel1Tag,
@@ -54,9 +53,18 @@ public:
   class Builder : public Expr::ExpressionBuilder
   {
   public:
+    /**
+     *  \param viscTag the viscosity
+     *  \param vel1Tag the first velocity component
+     *  \param vel2Tag the second velocity component
+     *
+     *  \param delTag the dilatation tag. If supplied, the
+     *         dilatational stress term will be added.
+     */
     Builder( const Expr::Tag viscTag,
              const Expr::Tag vel1Tag,
-             const Expr::Tag vel2Tag );
+             const Expr::Tag vel2Tag,
+             const Expr::Tag dilTag );
 
     Expr::ExpressionBase*
     build( const Expr::ExpressionID& id,
@@ -77,116 +85,69 @@ public:
 
 
 
-// ###################################################################
-//
-//                          Implementation
-//
-// ###################################################################
-
-
-
-template< typename StressT, typename Vel1T, typename Vel2T, typename ViscT >
-Stress<StressT,Vel1T,Vel2T,ViscT>::
-Stress( const Expr::Tag viscTag,
-        const Expr::Tag vel1Tag,
-        const Expr::Tag vel2Tag,
-        const Expr::ExpressionID& id,
-        const Expr::ExpressionRegistry& reg  )
-  : Expr::Expression<StressT>(id,reg),
-    visct_( viscTag ),
-    vel1t_( vel1Tag ),
-    vel2t_( vel2Tag )
-{}
-
-//--------------------------------------------------------------------
-
-template< typename StressT, typename Vel1T, typename Vel2T, typename ViscT >
-Stress<StressT,Vel1T,Vel2T,ViscT>::
-~Stress()
-{}
-
-//--------------------------------------------------------------------
-
-template< typename StressT, typename Vel1T, typename Vel2T, typename ViscT >
-void
-Stress<StressT,Vel1T,Vel2T,ViscT>::
-advertise_dependents( Expr::ExprDeps& exprDeps )
+/**
+ *  specialized version of the stress tensor for normal stresses.
+ *
+ */
+template< typename StressT,
+          typename VelT,
+          typename ViscT >
+class Stress< StressT, VelT, VelT, ViscT >
+ : public Expr::Expression<StressT>
 {
-  exprDeps.requires_expression( visct_ );
-  exprDeps.requires_expression( vel1t_ );
-  exprDeps.requires_expression( vel2t_ );
-}
+  const Expr::Tag visct_, velt_, dilt_;
 
-//--------------------------------------------------------------------
+  typedef typename OperatorTypeBuilder< Interpolant, ViscT, StressT >::type  ViscInterpT;
+  typedef typename OperatorTypeBuilder< Gradient,    VelT,  StressT >::type  VelGradT;
 
-template< typename StressT, typename Vel1T, typename Vel2T, typename ViscT >
-void
-Stress<StressT,Vel1T,Vel2T,ViscT>::
-bind_fields( const Expr::FieldManagerList& fml )
-{
-  const Expr::FieldManager<ViscT>& viscfm = fml.template field_manager<ViscT>();
-  const Expr::FieldManager<Vel1T>& vel1fm = fml.template field_manager<Vel1T>();
-  const Expr::FieldManager<Vel2T>& vel2fm = fml.template field_manager<Vel2T>();
+  const ViscInterpT* viscInterpOp_; ///< Interpolate viscosity to the face where we are building the stress
+  const VelGradT*    velGradOp_;    ///< Calculate the velocity gradient dui/dxj at the stress face
 
-  visc_ = &viscfm.field_ref( visct_ );
-  vel1_ = &vel1fm.field_ref( vel1t_ );
-  vel2_ = &vel1fm.field_ref( vel2t_ );
-}
+  const ViscT* visc_;
+  const VelT*  vel_;
+  const ViscT* dil_;
 
-//--------------------------------------------------------------------
+  Stress( const Expr::Tag viscTag,
+          const Expr::Tag velTag,
+          const Expr::Tag dilTag,
+          const Expr::ExpressionID& id,
+          const Expr::ExpressionRegistry& reg );
 
-template< typename StressT, typename Vel1T, typename Vel2T, typename ViscT >
-void
-Stress<StressT,Vel1T,Vel2T,ViscT>::
-bind_operators( const SpatialOps::OperatorDatabase& opDB )
-{
-  viscInterpOp_ = opDB.retrieve_operator<ViscInterpT>();
-  vel1GradOp_   = opDB.retrieve_operator<Vel1GradT  >();
-  vel2GradOp_   = opDB.retrieve_operator<Vel2GradT  >();
-}
-
-//--------------------------------------------------------------------
-
-template< typename StressT, typename Vel1T, typename Vel2T, typename ViscT >
-void
-Stress<StressT,Vel1T,Vel2T,ViscT>::
-evaluate()
-{
-  StressT& stress = this->value();
-  SpatialOps::SpatFldPtr<StressT> tmp = SpatialOps::SpatialFieldStore<StressT>::self().get( stress );
-
-  vel1GradOp_->apply_to_field( *vel1_, stress );
-  vel2GradOp_->apply_to_field( *vel2_, *tmp   );
-
-  stress += *tmp;
-
-  viscInterpOp_->apply_to_field( *visc_, *tmp );
-  stress *= *tmp;
-  stress *= -1.0;
-}
-
-//--------------------------------------------------------------------
-
-template< typename StressT, typename Vel1T, typename Vel2T, typename ViscT >
-Stress<StressT,Vel1T,Vel2T,ViscT>::
-Builder::Builder( const Expr::Tag viscTag,
+public:
+  class Builder : public Expr::ExpressionBuilder
+  {
+  public:
+    /**
+     *  \param viscTag the viscosity
+     *  \param vel1Tag the first velocity component
+     *  \param vel2Tag the second velocity component
+     *
+     *  \param delTag the dilatation tag. If supplied, the
+     *         dilatational stress term will be added.
+     *
+     *  note that in this case, the second velocity component will be
+     *  ignored.  It is kept for consistency with the off-diagonal
+     *  stress builder.
+     */
+    Builder( const Expr::Tag viscTag,
              const Expr::Tag vel1Tag,
-             const Expr::Tag vel2Tag )
-  : visct_( viscTag ),
-    vel1t_( vel1Tag ),
-    vel2t_( vel2Tag )
-{}
+             const Expr::Tag vel2Tag,
+             const Expr::Tag dilTag );
 
-//--------------------------------------------------------------------
+    Expr::ExpressionBase*
+    build( const Expr::ExpressionID& id,
+           const Expr::ExpressionRegistry& reg ) const;
 
-template< typename StressT, typename Vel1T, typename Vel2T, typename ViscT >
-Expr::ExpressionBase*
-Stress<StressT,Vel1T,Vel2T,ViscT>::
-Builder::build( const Expr::ExpressionID& id,
-                const Expr::ExpressionRegistry& reg ) const
-{
-  return new Stress<StressT,Vel1T,Vel2T,ViscT>( visct_, vel1t_, vel2t, id, reg );
-}
+  private:
+    const Expr::Tag visct_, velt_, dilt_;
+  };
 
+  ~Stress();
+
+  void advertise_dependents( Expr::ExprDeps& exprDeps );
+  void bind_fields( const Expr::FieldManagerList& fml );
+  void bind_operators( const SpatialOps::OperatorDatabase& opDB );
+  void evaluate();
+};
 
 #endif // Stress_Expr_h
