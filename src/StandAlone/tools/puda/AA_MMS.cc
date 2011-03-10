7756cc90908b2d43c0bf6cc1a6886c1ced455b6b
Todd Harman <harman@sci.utah.edu>
2011-03-10 21:26:47 +0000

    - added horizontal and vertical spaces. - changes so it will work on multiple levels.

--- src/StandAlone/tools/puda/AA_MMS.cc
+++ src/StandAlone/tools/puda/AA_MMS.cc
@@ -24,13 +24,17 @@ Uintah::AA_MMS( DataArchive * da, CommandLineFlags & clf )
   vector<const Uintah::TypeDescription*> types;
   da->queryVariables(vars, types);
   ASSERTEQ(vars.size(), types.size());
+  
   cout << "There are " << vars.size() << " variables:\n";
+  
   for(int i=0;i<(int)vars.size();i++)
     cout << vars[i] << ": " << types[i]->getName() << endl;
       
   vector<int> index;
   vector<double> times;
+  
   da->queryTimesteps(index, times);
+  
   ASSERTEQ(index.size(), times.size());
   cout << "There are " << index.size() << " timesteps:\n";
   for( int i = 0; i < (int)index.size(); i++ ) {
@@ -43,44 +47,66 @@ Uintah::AA_MMS( DataArchive * da, CommandLineFlags & clf )
     double time = times[t];
     GridP grid = da->queryGrid(t);
 
+    //__________________________________
+    //  hard coded constants!!!!
     double error, max_error=0.;
-    double mu = 3846.;
-    double bulk = 8333.;
-    double E = 9.*bulk*mu/(3.*bulk+mu);
-    double rho0=1.0;
-    double c = sqrt(E/rho0);
-    double A=.1;
+    double mu    = 3846.;
+    double bulk  = 8333.;
+    double E     = 9.*bulk*mu/(3.*bulk+mu);
+    double rho0  = 1.0;
+    double c     = sqrt(E/rho0);
+    double A     = 1e-2;
+    Point worstPos = Point(-9,-9,-9);
+    
+    //__________________________________
+    //  Level loop
     for(int l=0;l<grid->numLevels();l++){
       LevelP level = grid->getLevel(l);
+    
+      // Patch loop
       for(Level::const_patchIterator iter = level->patchesBegin();
           iter != level->patchesEnd(); iter++){
-        particleIndex worst_idx = 0;
+
         const Patch* patch = *iter;
+        
         int matl = clf.matl_jim;
-        //__________________________________
-        //   P A R T I C L E   V A R I A B L E
-        ParticleVariable<Point> value_pos;
+        ParticleVariable<Point>  value_pos;
         ParticleVariable<Vector> value_disp;
+
         da->query(value_pos,  "p.x",           matl, patch, t);
         da->query(value_disp, "p.displacement",matl, patch, t);
-        ParticleSubset* pset = value_pos.getParticleSubset();
-        if(pset->numParticles() > 0){
+          
+        ParticleSubset* pset = value_pos.getParticleSubset(); 
+        
+        //__________________________________
+        //  Compute the error.       
+        if(pset->numParticles() > 0){  // are there particles on this patch
+        
+
           ParticleSubset::iterator iter = pset->begin();
           for(;iter != pset->end(); iter++){
+
             Point refx = value_pos[*iter]-value_disp[*iter];
-            Vector u_exact=A*Vector(sin(M_PI*refx.x())*sin(c*M_PI*time),
-                             sin(M_PI*refx.y())*sin(2.*M_PI/3.+c*M_PI*time),
-                             sin(M_PI*refx.z())*sin(4.*M_PI/3.+c*M_PI*time));
-            error = (u_exact-value_disp[*iter]).length();
+          
+            Vector u_exact = A*Vector(sin(M_PI * refx.x()) * sin(c * M_PI*time),
+                                      sin(M_PI * refx.y()) * sin( (2./3.) * M_PI + c * M_PI * time),
+                                      sin(M_PI * refx.z()) * sin( (4./3.) * M_PI + c * M_PI * time));
+            
+            error = (u_exact - value_disp[*iter]).length();
+            
             if (error>max_error){
-                 max_error=error;
-                 worst_idx=*iter;
+              max_error = error;
+              worstPos  = value_pos[*iter];
             }
-          } // for
+          }  // particle Loop
+            
         }  //if
-    cout << "time = " << time << ", L_inf Error = " << max_error << ", Worst particle = " << value_pos[worst_idx] << endl;
       }  // for patches
+      
+      cout << "     Level: " << level->getIndex() << " L_inf Error: " << max_error << endl;
+      
     }   // for levels
+    cout << "time: " << time << " , L_inf Error: " << max_error << " , Worst particle: " << worstPos << endl;
   }
 } // end AA_MMS()
 
