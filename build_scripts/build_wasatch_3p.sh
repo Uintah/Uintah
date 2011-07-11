bc39aaf2d4535d38e31ad3e876a71e060dd27005
J. Davison de St. Germain <dav@sci.utah.edu>
2011-07-11 23:27:19 +0000

    M    configure M    configure.ac
    - If Uintah is built in debug mode, then build the Wasatch 3P in debug
      mode too (assuming it is requested).
    
    - Updated configure to require autoconf version 2.68 as the last
      commit was made using it and this will help reduce the 2+ MB diffs
      that happen when people switch between versions.  If you don't have
      2.68 on your machine, it takes less then 2 minutes to download it,
      and build it.
    
    M    build_scripts/build_wasatch_3p.sh
    
    - If Uintah is being built as a debug build, then build these libs in
      debug mode too.
    
    - Use the named variable BASE_BUILD_DIR instead of '$1' throughout the
      script.  Added a tiny bit of indentation.

--- build_scripts/build_wasatch_3p.sh
+++ build_scripts/build_wasatch_3p.sh
@@ -6,10 +6,15 @@
 #
 # $1 - location to build the 3P
 # $2 - boost location
+# $3 - whether Uintah is being built in debug mode or not...
 #
 
 BASE_BUILD_DIR=$1
 BOOST_DIR=$2
+
+if test $3 != "no"; then
+  DEBUG="-DCMAKE_BUILD_TYPE=Debug"
+fi
 
 ############################################################################
 
@@ -47,7 +52,7 @@ echo ""
 ############################################################################
 # Go to build/install directory
 
-run "cd $1/Wasatch3P"
+run "cd $BASE_BUILD_DIR/Wasatch3P"
 run "mkdir -p src"
 run "mkdir -p install"
 
@@ -57,18 +62,19 @@ run "mkdir -p install"
 run "cd src"
 run "rm -rf SpatialOps"
 run "git clone git://software.crsim.utah.edu/SpatialOps"
-run "cd $1/Wasatch3P/src/SpatialOps"
+run "cd $BASE_BUILD_DIR/Wasatch3P/src/SpatialOps"
 
-INSTALL_HERE=$1/Wasatch3P/install/SpatialOps
+INSTALL_HERE=$BASE_BUILD_DIR/Wasatch3P/install/SpatialOps
 
 run \
 "cmake \
- -DENABLE_TESTS=OFF \
- -DENABLE_STENCIL=ON \
- -DBOOST_ROOT=$BOOST_DIR \
- -DCMAKE_INSTALL_PREFIX=${INSTALL_HERE} \
- -DCMAKE_CXX_FLAGS="-fPIC" \
- ."
+  $DEBUG \
+  -DENABLE_TESTS=OFF \
+  -DENABLE_STENCIL=ON \
+  -DBOOST_ROOT=$BOOST_DIR \
+  -DCMAKE_INSTALL_PREFIX=${INSTALL_HERE} \
+  -DCMAKE_CXX_FLAGS="-fPIC" \
+  ."
 
 run "make -j4"
 run "make install"
@@ -79,24 +85,25 @@ run "make install"
 run "cd .."
 run "rm -rf ExprLib"
 run "git clone git://software.crsim.utah.edu/ExprLib"
-run "cd $1/Wasatch3P/src/ExprLib"
+run "cd $BASE_BUILD_DIR/Wasatch3P/src/ExprLib"
 
-INSTALL_HERE=$1/Wasatch3P/install/ExprLib
-SPATIAL_OPS_INSTALL_DIR=$1/Wasatch3P/install/SpatialOps
+INSTALL_HERE=$BASE_BUILD_DIR/Wasatch3P/install/ExprLib
+SPATIAL_OPS_INSTALL_DIR=$BASE_BUILD_DIR/Wasatch3P/install/SpatialOps
 
 run \
 "cmake \
- -DENABLE_TESTS=OFF \
- -DBOOST_ROOT=$BOOST_DIR \
- \
- -DSpatialOps_DIR=${SPATIAL_OPS_INSTALL_DIR}/share \
- \
- -DENABLE_UINTAH=ON \
- -DENABLE_THREADS=OFF \
- \
- -DCMAKE_INSTALL_PREFIX=${INSTALL_HERE} \
- -DCMAKE_CXX_FLAGS="-fPIC" \
- ."
+  $DEBUG \
+  -DENABLE_TESTS=OFF \
+  -DBOOST_ROOT=$BOOST_DIR \
+  \
+  -DSpatialOps_DIR=${SPATIAL_OPS_INSTALL_DIR}/share \
+  \
+  -DENABLE_UINTAH=ON \
+  -DENABLE_THREADS=OFF \
+  \
+  -DCMAKE_INSTALL_PREFIX=${INSTALL_HERE} \
+  -DCMAKE_CXX_FLAGS="-fPIC" \
+  ."
 
 run "make -j4"
 run "make install"
@@ -107,12 +114,13 @@ run "make install"
 run "cd .."
 run "rm -rf TabProps"
 run "git clone git://software.crsim.utah.edu/TabProps"
-run "cd $1/Wasatch3P/src/TabProps"
+run "cd $BASE_BUILD_DIR/Wasatch3P/src/TabProps"
 
-INSTALL_HERE=$1/Wasatch3P/install/TabProps
+INSTALL_HERE=$BASE_BUILD_DIR/Wasatch3P/install/TabProps
 
 run \
 "cmake \
+  $DEBUG \
   -DTabProps_PREPROCESSOR=OFF \
   -DBoostOutput=ON \
   -DBOOST_ROOT=$BOOST_DIR \
