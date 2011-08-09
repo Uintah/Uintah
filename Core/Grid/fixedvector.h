5b23152fd55ee7cf61ed218b8fc0ea1833b01a66
John Schmidt <jas@sci.utah.edu>
2011-08-09 03:19:53 +0000

    Fixes for static compilation for gcc 4.6.

--- Core/Grid/fixedvector.h
+++ Core/Grid/fixedvector.h
@@ -73,7 +73,7 @@ namespace Uintah {
         typedef value_type& reference;
         typedef const value_type& const_reference;
         typedef int size_type;
-        typedef ptrdiff_t difference_type;
+        typedef std::ptrdiff_t difference_type;
 
         fixedvector() {
           curalloc=Len;
