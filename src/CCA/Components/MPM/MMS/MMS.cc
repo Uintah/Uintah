20e7563545dd196c000ba03373d8f5a0a4e5a426
John Schmidt <jas@sci.utah.edu>
2011-09-27 00:40:34 +0000

    Update copyright date.

--- src/CCA/Components/MPM/MMS/MMS.cc
+++ src/CCA/Components/MPM/MMS/MMS.cc
@@ -2,7 +2,7 @@
 
 The MIT License
 
-Copyright (c) 1997-2010 Center for the Simulation of Accidental Fires and 
+Copyright (c) 1997-2011 Center for the Simulation of Accidental Fires and 
 Explosions (CSAFE), and  Scientific Computing and Imaging Institute (SCI), 
 University of Utah.
 
@@ -54,7 +54,7 @@ computeExternalForceForMMS : Computes the analytically determined body force for
 			 Department of Mechanical Engineering
 			 University of Utah.
 		Date   : 110824
-Copyright (C) 2010 SCI Group
+Copyright (C) 2011 SCI Group
 	
 */
 
