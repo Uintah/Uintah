fcff1f6cf9d87b2640a790c62002256d26936cb3
Tony Saad <tsaad@sci.utah.edu>
2011-10-03 19:44:51 +0000

    Remove the wasatch failing test from the regression tester until we fix the new persistency issue with ExprLib/Wasatch.

--- src/R_Tester/Wasatch.py
+++ src/R_Tester/Wasatch.py
@@ -41,8 +41,7 @@ NIGHTLYTESTS = [
   ("momentum-test-mms-xz", 		"momentum-test-mms-xz.ups",		4,	"Linux",	["exactComparison","no_restart","no_memoryTest","no_dbg"] ),                \
   ("momentum-test-mms-yz", 		"momentum-test-mms-yz.ups",		4,	"Linux",	["exactComparison","no_restart","no_memoryTest","no_dbg"] ),                \
   ("scalability-test", 		"scalability-test.ups",		1,	"Linux",	["exactComparison","no_restart","no_memoryTest","no_dbg"] ),                        \
-  ("momentum-test-mms-3D", 		"momentum-test-mms-3D.ups",		8,	"Linux",	["exactComparison","no_restart","no_memoryTest","no_dbg"] ),                 \
-  ("rk3-verification-ode", 		"rk3-verification-ode.ups",		1,	"Linux",	["exactComparison","no_restart","no_memoryTest","no_dbg"] )      
+  ("momentum-test-mms-3D", 		"momentum-test-mms-3D.ups",		8,	"Linux",	["exactComparison","no_restart","no_memoryTest","no_dbg"] )
 ]
 
 
@@ -60,8 +59,7 @@ LOCALTESTS = [
   ("momentum-test-mms-xz", 		"momentum-test-mms-xz.ups",		4,	"All",	["exactComparison","no_restart","no_memoryTest","no_dbg"] ),            \
   ("momentum-test-mms-yz", 		"momentum-test-mms-yz.ups",		4,	"All",	["exactComparison","no_restart","no_memoryTest","no_dbg"] ),            \
   ("scalability-test", 		"scalability-test.ups",		1,	"All",	["exactComparison","no_restart","no_memoryTest","no_dbg"] ),                    \
-  ("momentum-test-mms-3D", 		"momentum-test-mms-3D.ups",		8,	"All",	["exactComparison","no_restart","no_memoryTest","no_dbg"] ),             \
-  ("rk3-verification-ode", 		"rk3-verification-ode.ups",		1,	"All",	["exactComparison","no_restart","no_memoryTest","no_dbg"] )    
+  ("momentum-test-mms-3D", 		"momentum-test-mms-3D.ups",		8,	"All",	["exactComparison","no_restart","no_memoryTest","no_dbg"] )
 ]
 
 #__________________________________
