079cd7336bc0bd665d0520ae963077890c5fcaea
Jeremy Nicholas Thornock <jthornoc@sci.utah.edu>
2011-10-20 16:48:15 +0000

    Making all interior geometry of the same cell type.

--- CCA/Components/Arches/ExplicitSolver.cc
+++ CCA/Components/Arches/ExplicitSolver.cc
@@ -527,6 +527,8 @@ int ExplicitSolver::nonlinearSolve(const LevelP& level,
 
       }
 
+      d_boundaryCondition->sched_setIntrusionTemperature( sched, patches, matls ); 
+
       // Property models after table lookup
       for ( PropertyModelFactory::PropMap::iterator iprop = all_prop_models.begin(); 
             iprop != all_prop_models.end(); iprop++){
