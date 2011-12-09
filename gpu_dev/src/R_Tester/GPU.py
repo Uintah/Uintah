bd1df7fcecd66cdd73a52c15f4de5528bbb2cf66
Alan Humphrey <ahumphrey@sci.utah.edu>
2011-12-09 00:51:00 +0000

    Add GPU args to runSusTests. Minimize GPU tests for now.

--- gpu_dev/src/R_Tester/GPU.py
+++ gpu_dev/src/R_Tester/GPU.py
@@ -25,13 +25,11 @@ from helpers.runSusTests import runSusTests
 #  3) Performance_tests are not run on a debug build.
 #______________________________________________________________________
 NIGHTLYTESTS = [   ("poissonGPU1",       "poissonGPU1.ups",       1, "Linux"),
-                   ("gpuSchedulerTest",  "gpuSchedulerTest.ups",  1, "Linux"),
-                   ("advectSlabsGPU",    "advectSlabsGPU.ups",    1, "Linux") ]
+                   ("gpuSchedulerTest",  "gpuSchedulerTest.ups",  1, "Linux") ]
 
 # Tests that are run during local regression testing
 LOCALTESTS   = [   ("poissonGPU1",       "poissonGPU1.ups",       1, "Linux"),
-                   ("gpuSchedulerTest",  "gpuSchedulerTest.ups",  1, "Linux"),
-                   ("advectSlabsGPU",    "advectSlabsGPU.ups",    1, "Linux") ]
+                   ("gpuSchedulerTest",  "gpuSchedulerTest.ups",  1, "Linux") ]
 
 
 #__________________________________
