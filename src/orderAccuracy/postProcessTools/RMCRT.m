e4c7a415b36a1b4f62a660b3bb5d3dd42b8a9bbc
Isaac Hunsaker <ihunsaker@sci.utah.edu>
2011-12-21 19:21:08 +0000

    Added benchmark 4. Made paths to exact solutions relative.

--- src/orderAccuracy/postProcessTools/RMCRT.m
+++ src/orderAccuracy/postProcessTools/RMCRT.m
@@ -44,6 +44,7 @@ function [DivQ] = benchMark2(x_CC)
 
   [s,r1] = unix('grep -m1 -w "Total Number of Cells" tmp |cut -d":" -f2 | tr -d "[]int"');
   resolution = str2num(r1);
+  zDir = 3;
   n = resolution(zDir); %number of cells in the z direction (between the plates)
 
   %This script computes the exact solution to any resolution at the cell centers 
@@ -96,7 +97,7 @@ endfunction
 function [ExactSol] = benchMark3(x_CC)
   printf("BenchMark 3");
   
-  load /scratch/ibrix/icse_perf/isaac/uintah_Oct/src/orderAccuracy/postProcessTools/bench3Exact.txt;
+  load bench3Exact.txt;
 
   [s,r1] = unix('grep -m1 -w "Total Number of Cells" tmp |cut -d":" -f2 | tr -d "[]int"');
   resolution = str2num(r1);
@@ -131,8 +132,48 @@ function [ExactSol] = benchMark3(x_CC)
 
 endfunction
 
+%______________________________________________________________________
+%   B E N C H M A R K   4
+function [ExactSol] = benchMark4(x_CC)
+  printf("BenchMark 4");
+  
+  load b4divQCenterExact.txt;
+
+  [s,r1] = unix('grep -m1 -w "Total Number of Cells" tmp |cut -d":" -f2 | tr -d "[]int"');
+  resolution = str2num(r1);
+
+  zDir = 3;
+  n = resolution(zDir); %number of cells in the z direction (between the plates)
+
+  ExactSol = b4divQCenterExact;
+  
+  x243 = 1/(2*243):1/243 :1-1/(2*243);
+
+  Exact81 = ExactSol(2:3:242);
+  Exact27 = Exact81(2:3:80);
+  Exact9  = Exact27(2:3:26);
+  Exact3  = Exact9(2:3:8);
+
+  if (n == 81)
+    ExactSol = Exact81;
+  end
+  if (n == 27)
+    ExactSol = Exact27; 
+  end
+  if (n == 9)   
+    ExactSol = Exact9;
+  end
+  if (n == 3)   
+    ExactSol = Exact3;
+  end
+  if (n !=3 && n != 9 && n!= 27 && n != 81 && n!=243)
+    ExactSol = interp1(x243, ExactSol, x_CC, 'pchip');
+  end
+
+endfunction
 
 %______________________________________________________________________
+
 
 function Usage
   printf('RMCRT.m <options>\n') 
