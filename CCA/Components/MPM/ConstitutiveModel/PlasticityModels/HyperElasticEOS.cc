7f1f7a3b35ea022a3ff5bf7e8646c5f7ad260601
Biswajit Banerjee <banerjee@sci.utah.edu>
2012-01-24 22:28:58 +0000

    Added extra methods for computing pressure and density for proper coupling with MPMICE (old code used a generic eos for those calculations).

--- CCA/Components/MPM/ConstitutiveModel/PlasticityModels/HyperElasticEOS.cc
+++ CCA/Components/MPM/ConstitutiveModel/PlasticityModels/HyperElasticEOS.cc
@@ -30,20 +30,25 @@ DEALINGS IN THE SOFTWARE.
 
 
 #include "HyperElasticEOS.h"
+#include <Core/Exceptions/ParameterNotFound.h>
+#include <Core/Exceptions/InvalidValue.h>
 #include <cmath>
 
 using namespace Uintah;
 
 HyperElasticEOS::HyperElasticEOS()
 {
+  d_bulk = -1.0;
 } 
 
 HyperElasticEOS::HyperElasticEOS(ProblemSpecP&)
 {
+  d_bulk = -1.0;
 } 
          
-HyperElasticEOS::HyperElasticEOS(const HyperElasticEOS*)
+HyperElasticEOS::HyperElasticEOS(const HyperElasticEOS* cm)
 {
+  d_bulk = cm->d_bulk;
 } 
          
 HyperElasticEOS::~HyperElasticEOS()
@@ -86,4 +91,90 @@ HyperElasticEOS::eval_dp_dJ(const MPMMaterial* matl,
 
   double dpdJ = 0.5*kappa*(1.0 + 1.0/(J*J));
   return dpdJ;
+}
+
+// Compute pressure (option 1)
+double 
+HyperElasticEOS::computePressure(const double& rho_orig,
+                                 const double& rho_cur)
+{
+  if (d_bulk < 0.0) {
+    throw ParameterNotFound("Please initialize bulk modulus in EOS before computing pressure",
+                            __FILE__, __LINE__);
+  }
+
+  double J = rho_orig/rho_cur;
+  double p = 0.5*d_bulk*(J - 1.0/J);
+  return p;
+}
+
+// Compute pressure (option 2)
+void 
+HyperElasticEOS::computePressure(const double& rho_orig,
+                                 const double& rho_cur,
+                                 double& pressure,
+                                 double& dp_drho,
+                                 double& csquared)
+{
+  if (d_bulk < 0.0) {
+    throw ParameterNotFound("Please initialize bulk modulus in EOS before computing pressure",
+                            __FILE__, __LINE__);
+  }
+
+  double J = rho_orig/rho_cur;
+  pressure = 0.5*d_bulk*(J - 1.0/J);
+  double dp_dJ = 0.5*d_bulk*(1.0 + 1.0/J*J);
+  dp_drho = -0.5*d_bulk*(1.0 + J*J)/rho_orig;
+  csquared = dp_dJ/rho_cur;
+}
+
+// Compute bulk modulus
+double 
+HyperElasticEOS::computeBulkModulus(const double& rho_orig,
+                                    const double& rho_cur)
+{
+  if (d_bulk < 0.0) {
+    throw ParameterNotFound("Please initialize bulk modulus in EOS before computing modulus",
+                            __FILE__, __LINE__);
+  }
+
+  double J = rho_orig/rho_cur;
+  double bulk = 0.5*d_bulk*(1.0 + 1.0/J*J);
+  return bulk;
+}
+
+// Compute strain energy
+double 
+HyperElasticEOS::computeStrainEnergy(const double& rho_orig,
+                                     const double& rho_cur)
+{
+  if (d_bulk < 0.0) {
+    throw ParameterNotFound("Please initialize bulk modulus in EOS before computing energy",
+                            __FILE__, __LINE__);
+  }
+
+  double J = rho_orig/rho_cur;
+  double U = 0.5*d_bulk*(0.5*(J*J - 1.0) - log(J));
+  return U;
+}
+
+// Compute density given pressure (tension +ve)
+double 
+HyperElasticEOS::computeDensity(const double& rho_orig,
+                                const double& pressure)
+{
+  if (d_bulk < 0.0) {
+    throw ParameterNotFound("Please initialize bulk modulus in EOS before computing density",
+                            __FILE__, __LINE__);
+  }
+  double numer1 = d_bulk*d_bulk + pressure*pressure;
+  double sqrtNumer = sqrt(numer1);
+  double rho = rho_orig/d_bulk*(-pressure + sqrtNumer);
+  if (rho < 0) {
+    ostringstream desc;
+    desc << "Value of pressure (" << pressure << ") is beyond the range of validity of model" << endl
+         << "  density = " << rho << endl;
+    throw InvalidValue(desc.str(), __FILE__, __LINE__);
+  }
+  return rho;
 }
