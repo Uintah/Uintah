1a41959b2cab4353ac49afed21f60507df6182a7
Todd Harman <harman@sci.utah.edu>
2012-02-15 00:38:30 +0000

    moved fineToCoarseOperator() out of AMRICE up to Core/Grid/AMR_CoarsenRefine

--- src/Core/Grid/AMR_CoarsenRefine.cc
+++ src/Core/Grid/AMR_CoarsenRefine.cc
@@ -34,10 +34,11 @@ DEALINGS IN THE SOFTWARE.
 #include <Core/Geometry/Vector.h>
 #include <Core/Grid/AMR.h>
 #include <Core/Grid/Level.h>
+#include <Core/Util/DebugStream.h>
 
 namespace Uintah {
 
-
+static SCIRun::DebugStream cout_dbg("AMR_CoarsenRefine", false);
 //______________________________________________________________________
 //
 template<typename T>
@@ -110,8 +111,72 @@ void coarsenDriver_massWeighted( const IntVector & cl,
 }
 
 
+//_____________________________________________________________________
+//   Averages the interior fine patch data onto the coarse patch
+// 
+template<class T>
+void fineToCoarseOperator(CCVariable<T>& q_CC,
+                          const bool computesAve,
+                          const VarLabel* varLabel,
+                          const int indx,
+                          DataWarehouse* new_dw,
+                          const Patch* coarsePatch,
+                          const Level* coarseLevel,
+                          const Level* fineLevel)
+{
+  Level::selectType finePatches;
+  coarsePatch->getFineLevelPatches(finePatches);
+                          
+  for(int i=0;i<finePatches.size();i++){
+    const Patch* finePatch = finePatches[i];
+
+    IntVector cl, ch, fl, fh;
+    getFineLevelRange(coarsePatch, finePatch, cl, ch, fl, fh);
+
+    if (fh.x() <= fl.x() || fh.y() <= fl.y() || fh.z() <= fl.z()) {
+      continue;
+    }
+    
+    constCCVariable<T> fine_q_CC;
+    new_dw->getRegion(fine_q_CC,  varLabel, indx, fineLevel, fl, fh, false);
+
+    cout_dbg << " fineToCoarseOperator: finePatch "<< fl << " " << fh 
+             << " coarsePatch "<< cl << " " << ch << endl;
+             
+    IntVector r_Ratio = fineLevel->getRefinementRatio();
+    
+    double inv_RR = 1.0;    
+    
+    if(computesAve){
+      inv_RR = 1.0/( (double)(r_Ratio.x() * r_Ratio.y() * r_Ratio.z()) );
+    }
+
+    T zero(0.0);
+    // iterate over coarse level cells
+    for(CellIterator iter(cl, ch); !iter.done(); iter++){
+      IntVector c = *iter;
+      T q_CC_tmp(zero);
+      IntVector fineStart = coarseLevel->mapCellToFiner(c);
+    
+      // for each coarse level cell iterate over the fine level cells   
+      for(CellIterator inside(IntVector(0,0,0),r_Ratio );
+                                          !inside.done(); inside++){
+        IntVector fc = fineStart + *inside;        
+        
+        if( fc.x() >= fl.x() && fc.y() >= fl.y() && fc.z() >= fl.z() &&
+            fc.x() <= fh.x() && fc.y() <= fh.y() && fc.z() <= fh.z() ) {
+          q_CC_tmp += fine_q_CC[fc];
+        }
+      }
+                         
+      q_CC[c] =q_CC_tmp * inv_RR;
+    }
+  }
+  cout_dbg.setActive(false);// turn off the switch for cout_dbg
+}
 
 
+//______________________________________________________________________
 // Explicit template instantiations:
 template void coarsenDriver_std<double>( const IntVector& cl, const IntVector& ch, const IntVector& fl, const IntVector& fh,
                                          const IntVector& refinementRatio, const double ratio,
@@ -125,5 +190,10 @@ template void coarsenDriver_massWeighted<double>( const IntVector & cl, const In
                                                   const Level* coarseLevel, constCCVariable<double>& cMass, constCCVariable<double>& fine_q_CC, CCVariable<double>& coarse_q_CC );
 template void coarsenDriver_massWeighted<Vector>( const IntVector & cl, const IntVector & ch, const IntVector & fl, const IntVector & fh, const IntVector & refinementRatio,
                                                   const Level* coarseLevel, constCCVariable<double>& cMass, constCCVariable<Vector>& fine_q_CC, CCVariable<Vector>& coarse_q_CC );
+                                                  
+template void fineToCoarseOperator<double>(CCVariable<double>& q_CC, const bool computesAve, const VarLabel* varLabel, const int indx, DataWarehouse* new_dw,
+                                   const Patch* coarsePatch, const Level* coarseLevel, const Level* fineLevel);
+template void fineToCoarseOperator<Vector>(CCVariable<Vector>& q_CC, const bool computesAve, const VarLabel* varLabel, const int indx, DataWarehouse* new_dw,
+                                   const Patch* coarsePatch, const Level* coarseLevel, const Level* fineLevel);
 
 }  // end namespace Uintah
