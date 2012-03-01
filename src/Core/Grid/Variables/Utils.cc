6563bef067fd846d371a08e5ede34baca348a679
Jim Guilkey <guilkey@sci.utah.edu>
2012-03-01 18:49:52 +0000

    Add the function areAllNodeValuesNumbers.  This takes in an NCVariable of Vectors and returns the first node that it finds with an inf or nan.

--- src/Core/Grid/Variables/Utils.cc
+++ src/Core/Grid/Variables/Utils.cc
@@ -27,7 +27,9 @@ DEALINGS IN THE SOFTWARE.
 */
 #include <Core/Grid/Variables/Utils.h>
 
+#include <Core/Grid/Variables/NodeIterator.h>
 #include <Core/Grid/Variables/CellIterator.h>
+#include <Core/Grid/Variables/NCVariable.h>
 #include <Core/Grid/Variables/CCVariable.h>
 #include <Core/Grid/Variables/SFCXVariable.h>
 #include <Core/Grid/Variables/SFCYVariable.h>
@@ -114,6 +116,40 @@ areAllValuesNumbers( T & src, IntVector & neg_cell )
   return true;      
 }
 
+//template< class T >
+bool
+areAllNodeValuesNumbers(NCVariable<Vector> & src, IntVector & neg_node )
+{
+  double    numCells = 0;
+  double    sum_src = 0;
+  int       sumNan = 0;
+  int       sumInf = 0;
+  IntVector l = src.getLowIndex();
+  IntVector h = src.getHighIndex();
+  NodeIterator iterLim = NodeIterator(l,h);
+
+  for(NodeIterator iter=iterLim; !iter.done();iter++) {
+    IntVector c = *iter;
+    sumInf += isinf(src[c].length());       // check for infs
+    sumNan += isnan(src[c].length());       // check for nans
+    sum_src += src[c].length()/(fabs(src[c].length()) + d_SMALLNUM);
+    numCells++;
+  }
+
+  // now find the first cell where the value is inf or nan
+  if ( (fabs(sum_src - numCells) > 1.0e-2) || sumNan !=0) {
+    for(NodeIterator iter=iterLim; !iter.done();iter++) {
+      IntVector c = *iter;
+      if (isinf(src[c].length()) != 0 || isnan(src[c].length()) !=0) {
+        neg_node = c;
+        return false;
+      }
+    }
+  }
+  neg_node = IntVector(0,0,0);
+  return true;
+}
+
 // Explicit template instantiations:
 template bool areAllValuesPositive( CCVariable<double> &, IntVector & );
 template bool areAllValuesPositive( SFCXVariable<double> &, IntVector & );
@@ -126,4 +162,5 @@ template bool areAllValuesNumbers( SFCXVariable<double> &, IntVector & );
 template bool areAllValuesNumbers( SFCYVariable<double> &, IntVector & );
 template bool areAllValuesNumbers( SFCZVariable<double> &, IntVector & );
 
+//template bool areAllNodeValuesNumbers( NCVariable<double> &, IntVector & );
 } // end namespace Uintah
