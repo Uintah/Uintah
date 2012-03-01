2ee10ce910f70d21de65790f544645bf5ef67ba5
Jeremy Nicholas Thornock <jthornoc@sci.utah.edu>
2012-03-01 06:23:55 +0000

    Missing header files.

--- CCA/Components/Arches/SmagorinskyModel.h
+++ CCA/Components/Arches/SmagorinskyModel.h
@@ -69,6 +69,9 @@ WARNING
 
 #include <CCA/Components/Arches/Arches.h>
 #include <CCA/Components/Arches/TurbulenceModel.h>
+#include <Core/Grid/Variables/SFCXVariable.h>
+#include <Core/Grid/Variables/SFCYVariable.h>
+#include <Core/Grid/Variables/SFCZVariable.h>
 
 namespace Uintah {
 class PhysicalConstants;
