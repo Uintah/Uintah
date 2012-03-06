edbc45b0e6c8aa67227282f23e9c9c4c61e85a4c
Alan Humphrey <ahumphrey@sci.utah.edu>
2012-03-06 16:47:19 +0000

    - Cleanup includes for RayGPU.cu - Add MIT license to CCA/Components/Models/Radiation/RMCRT sources and headers.

--- src/CCA/Components/Models/Radiation/RMCRT/Ray.h
+++ src/CCA/Components/Models/Radiation/RMCRT/Ray.h
@@ -1,3 +1,33 @@
+/*
+
+ The MIT License
+
+ Copyright (c) 1997-2012 Center for the Simulation of Accidental Fires and
+ Explosions (CSAFE), and  Scientific Computing and Imaging Institute (SCI),
+ University of Utah.
+
+ License for the specific language governing rights and limitations under
+ Permission is hereby granted, free of charge, to any person obtaining a
+ copy of this software and associated documentation files (the "Software"),
+ to deal in the Software without restriction, including without limitation
+ the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ and/or sell copies of the Software, and to permit persons to whom the
+ Software is furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included
+ in all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ DEALINGS IN THE SOFTWARE.
+
+ */
+
+
 #ifndef Uintah_Component_Arches_Ray_h
 #define Uintah_Component_Arches_Ray_h
 
@@ -146,6 +176,7 @@ namespace Uintah{
                      bool modifies_divQ,
                      Task::WhichDW which_abskg_dw,
                      Task::WhichDW which_sigmaT4_dw );
+
 #ifdef HAVE_CUDA
       //----------------------------------------
       void rayTraceGPU( const ProcessorGroup* pc,
@@ -157,7 +188,14 @@ namespace Uintah{
                         bool modifies_divQ,
                         Task::WhichDW which_abskg_dw,
                         Task::WhichDW which_sigmaT4_dw );
+
+      //______________________________________________________________________
+      inline bool containsCellGPU(const dim3 &low,
+                                  const dim3 &high,
+                                  const dim3 &cell,
+                                  const int &face);
 #endif
+
       //__________________________________
       void rayTrace_dataOnion( const ProcessorGroup* pc, 
                                const PatchSubset* patches, 
--- src/CCA/Components/Models/Radiation/RMCRT/RayGPU.cu
+++ src/CCA/Components/Models/Radiation/RMCRT/RayGPU.cu
@@ -1,17 +1,37 @@
-//----- Ray.cc ----------------------------------------------
+/*
+
+ The MIT License
+
+ Copyright (c) 1997-2012 Center for the Simulation of Accidental Fires and
+ Explosions (CSAFE), and  Scientific Computing and Imaging Institute (SCI),
+ University of Utah.
+
+ License for the specific language governing rights and limitations under
+ Permission is hereby granted, free of charge, to any person obtaining a
+ copy of this software and associated documentation files (the "Software"),
+ to deal in the Software without restriction, including without limitation
+ the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ and/or sell copies of the Software, and to permit persons to whom the
+ Software is furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included
+ in all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ DEALINGS IN THE SOFTWARE.
+
+ */
+
+
+//----- RayGPU.cu ----------------------------------------------
 #include <CCA/Components/Models/Radiation/RMCRT/MersenneTwister.h>
 #include <CCA/Components/Models/Radiation/RMCRT/Ray.h>
-#include <CCA/Components/Regridder/PerPatchVars.h>
-#include <Core/Containers/StaticArray.h>
-#include <Core/Exceptions/InternalError.h>
-#include <Core/Exceptions/ProblemSetupException.h>
-#include <Core/Geometry/BBox.h>
-#include <Core/Grid/AMR.h>
-#include <Core/Grid/AMR_CoarsenRefine.h>
-#include <Core/Grid/BoundaryConditions/BCUtils.h>
 #include <Core/Grid/DbgOutput.h>
-#include <Core/Grid/Variables/PerPatch.h>
-#include <time.h>
 
 #include <sci_defs/cuda_defs.h>
 
@@ -35,16 +55,28 @@ __global__ void rayTraceKernel ()
 // Method: The GPU ray tracer
 //---------------------------------------------------------------------------
 void Ray::rayTraceGPU( const ProcessorGroup* pc,
-                  const PatchSubset* patches,
-                  const MaterialSubset* matls,
-                  DataWarehouse* old_dw,
-                  DataWarehouse* new_dw,
-                  int device,
-                  bool modifies_divQ,
-                  Task::WhichDW which_abskg_dw,
-                  Task::WhichDW which_sigmaT4_dw )
+                       const PatchSubset* patches,
+                       const MaterialSubset* matls,
+                       DataWarehouse* old_dw,
+                       DataWarehouse* new_dw,
+                       int device,
+                       bool modifies_divQ,
+                       Task::WhichDW which_abskg_dw,
+                       Task::WhichDW which_sigmaT4_dw )
 {
   // stub
+}
+
+//---------------------------------------------------------------------------
+// Method:
+//---------------------------------------------------------------------------
+inline bool Ray::containsCellGPU(const dim3 &low,
+                                 const dim3 &high,
+                                 const dim3 &cell,
+                                 const int &face)
+{
+  // stub
+  return false;
 }
 
 
