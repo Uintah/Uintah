81c78485d56513ace5736688900073171b19d32a
Todd Harman <harman@sci.utah.edu>
2012-03-12 23:27:09 +0000

    Adding in a template instantiation for 'double' data.  ('Float' was already there.)

--- src/Core/Algorithms/Fields/FieldsAlgo.cc
+++ src/Core/Algorithms/Fields/FieldsAlgo.cc
@@ -440,5 +440,7 @@ template void cast_to_mesh_here< GenericField<TriSurfMesh<TriLinearLgn<Point> >,
 template void cast_to_mesh_here< TriSurfMesh<TriLinearLgn<Point> > >( void *, TriSurfMesh<TriLinearLgn<Point> > *& );
 
 template void cast_to_mesh_here<GenericField<LatVolMesh<HexTrilinearLgn<Point> >, ConstantBasis<float>, FData3d<float, LatVolMesh<HexTrilinearLgn<Point> > > > >(void*, GenericField<LatVolMesh<HexTrilinearLgn<Point> >, ConstantBasis<float>, FData3d<float, LatVolMesh<HexTrilinearLgn<Point> > > >*&);
+template void cast_to_mesh_here<GenericField<LatVolMesh<HexTrilinearLgn<Point> >, ConstantBasis<double>, FData3d<double, LatVolMesh<HexTrilinearLgn<Point> > > > >(void*, GenericField<LatVolMesh<HexTrilinearLgn<Point> >, ConstantBasis<double>, FData3d<double, LatVolMesh<HexTrilinearLgn<Point> > > >*&);
+
 template void cast_to_mesh_here<GenericField<ImageMesh<QuadBilinearLgn<Point> >, ConstantBasis<float>, FData2d<float, ImageMesh<QuadBilinearLgn<Point> > > > >(void*, GenericField<ImageMesh<QuadBilinearLgn<Point> >, ConstantBasis<float>, FData2d<float, ImageMesh<QuadBilinearLgn<Point> > > >*&);
 template void cast_to_mesh_here<GenericField<CurveMesh<CrvLinearLgn<Point> >, CrvLinearLgn<double>, std::vector<double, std::allocator<double> > > >(void*, GenericField<CurveMesh<CrvLinearLgn<Point> >, CrvLinearLgn<double>, std::vector<double, std::allocator<double> > >*&);
