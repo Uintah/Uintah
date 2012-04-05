b7f9d80cc9319fa313eff10e95780c4d863651c7
James Sutherland <jsutherland@sci.utah.edu>
2012-04-05 03:54:36 +0000

    Remove an unnecessary implementation file Remove a namespace opening in a header file (bad!)

--- src/CCA/Components/Wasatch/WasatchMaterial.h
+++ src/CCA/Components/Wasatch/WasatchMaterial.h
@@ -3,24 +3,23 @@
 
 #include <Core/Grid/Material.h>
 #include <Core/Grid/uintahshare.h>
+
 namespace Uintah {
 
-using namespace SCIRun;
-/**
- *  \class  WasatchMaterial
- *  \author Tony Saad
- *  \date   December 2011
- *
- *  \brief Create a simple wasatch material.
- */
-  
-      class UINTAHSHARE WasatchMaterial : public Material {
-      public:
-         WasatchMaterial();         
-         ~WasatchMaterial();
- 
-      private:
-      };
+  /**
+   *  \class  WasatchMaterial
+   *  \author Tony Saad
+   *  \date   December 2011
+   *
+   *  \brief Create a simple wasatch material.
+   */
+
+  class UINTAHSHARE WasatchMaterial : public Material {
+  public:
+    WasatchMaterial(){}
+    ~WasatchMaterial(){}
+  private:
+  };
 
 } // End namespace Uintah
 
