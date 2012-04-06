cc5c8ff436898078aa35d2e216fcec66be769598
Todd Harman <harman@sci.utah.edu>
2012-04-06 00:12:22 +0000

    Throw an exception if the user selects Gurson yield condition with radial return. The user is told to add:
      <plastic_convergence_algo> biswajit </plastic_convergence_algo>
    
    to the input file

--- src/CCA/Components/MPM/ConstitutiveModel/PlasticityModels/YieldConditionFactory.cc
+++ src/CCA/Components/MPM/ConstitutiveModel/PlasticityModels/YieldConditionFactory.cc
@@ -42,22 +42,34 @@ using namespace Uintah;
 /// Create an instance of a Yield Condition.
 /*! Available yield conditions are : von Mises, Gurson-Tvergaard-Needleman,
     Rosselier */
-YieldCondition* YieldConditionFactory::create(ProblemSpecP& ps)
+YieldCondition* YieldConditionFactory::create(ProblemSpecP& ps, const bool usingRR)
 {
-   ProblemSpecP child = ps->findBlock("yield_condition");
-   if(!child)
-      throw ProblemSetupException("MPM::ConstitutiveModel:Cannot find yield condition.", __FILE__, __LINE__);
-   string mat_type;
-   if(!child->getAttribute("type", mat_type))
-      throw ProblemSetupException("MPM::ConstitutiveModel:No type for yield condition.", __FILE__, __LINE__);
-   
-   if (mat_type == "vonMises")
-      return(scinew VonMisesYield(child));
-   else if (mat_type == "gurson")
-      return(scinew GursonYield(child));
-   else 
-      throw ProblemSetupException("MPM::ConstitutiveModel:Unknown Yield Condition ("+mat_type+")",
-                                  __FILE__, __LINE__);
+  ProblemSpecP child = ps->findBlock("yield_condition");
+  if(!child)
+    throw ProblemSetupException("MPM::ConstitutiveModel:Cannot find yield condition.", __FILE__, __LINE__);
+
+  string mat_type;
+  if(!child->getAttribute("type", mat_type))
+    throw ProblemSetupException("MPM::ConstitutiveModel:No type for yield condition.", __FILE__, __LINE__);
+
+  if (mat_type == "vonMises")
+    return(scinew VonMisesYield(child));
+  else if (mat_type == "gurson"){
+    if( usingRR ){
+      ostringstream warn;
+      warn << "MPM::ConstitutiveModel:Yield Condition ("+mat_type+")"
+           << " only works with the 'biswajit' plastic convergence algorithm\n"
+           << " Add: \n"
+           << "    <plastic_convergence_algo> biswajit </plastic_convergence_algo> \n"
+           << " to your input file ";
+           
+      throw ProblemSetupException(warn.str(), __FILE__, __LINE__);
+    }
+    return(scinew GursonYield(child));
+  }
+  else 
+    throw ProblemSetupException("MPM::ConstitutiveModel:Unknown Yield Condition ("+mat_type+")",
+                                 __FILE__, __LINE__);
 }
 
 YieldCondition* 
