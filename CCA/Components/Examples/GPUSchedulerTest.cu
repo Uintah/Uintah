94bd487629dcfaf862aa7b739dc882c0fcb19a35
Alan Humphrey <ahumphrey@sci.utah.edu>
2012-05-31 21:58:52 +0000

    Removed dependence on CUDA SDK (cutil headers and libs).
    CUDA support is now enabled with a single option:
    --with-cuda=DIR         (CUDA installation is in DIR)
    
    Also generated new configure script.

--- CCA/Components/Examples/GPUSchedulerTest.cu
+++ CCA/Components/Examples/GPUSchedulerTest.cu
@@ -359,7 +359,7 @@ void GPUSchedulerTest::timeAdvanceGPU(const ProcessorGroup* pg,
 
   // set the CUDA context
   cudaError_t retVal;
-  CUDA_SAFE_CALL( retVal = cudaSetDevice(device) );
+  CUDA_RT_SAFE_CALL( retVal = cudaSetDevice(device) );
 
   // get a handle on the GPU scheduler to query for device and host pointers, etc
   GPUThreadedMPIScheduler* sched = dynamic_cast<GPUThreadedMPIScheduler*>(getPort("scheduler"));
