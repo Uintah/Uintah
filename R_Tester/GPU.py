855ccf2165805c1e49464ab2c1a974e9f291a381
Alan Humphrey <ahumphrey@sci.utah.edu>
2012-06-12 00:25:30 +0000

    Simplify nightly vs local GPU RT.

--- R_Tester/GPU.py
+++ R_Tester/GPU.py
@@ -31,10 +31,7 @@ NIGHTLYTESTS = [   ("poissonGPU1",       "poissonGPU1.ups",         1, "Linux", 
                    ("RMCRT_test_1L",     "RMCRT_test_1L.ups",     1.1, "Linux", ["gpu", "no_restart", "no_uda_comparison", "sus_options=-nthreads 4 -gpu"]) ]
 
 # Tests that are run during local regression testing
-LOCALTESTS   = [   ("poissonGPU1",       "poissonGPU1.ups",         1, "Linux", ["gpu"]),
-                   ("gpuSchedulerTest",  "gpuSchedulerTest.ups",  1.1, "Linux", ["gpu", "no_restart", "no_uda_comparison", "sus_options=-nthreads 4 -gpu"]),
-                   ("RMCRT_test_1L",     "RMCRT_test_1L.ups",     1.1, "Linux", ["gpu", "no_restart", "no_uda_comparison", "sus_options=-nthreads 4 -gpu"]) ]
-
+LOCALTESTS = NIGHTLYTESTS
 
 
 #__________________________________
