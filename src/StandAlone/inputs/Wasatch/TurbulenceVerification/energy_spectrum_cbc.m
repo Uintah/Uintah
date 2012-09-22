1e3efa5e2d1a80b526b6423bc4694044069266a9
Tony Saad <tsaad@sci.utah.edu>
2012-09-22 04:28:35 +0000

    add cbc turbulence verification data and input files to Wasatch. more scripts to follow.

---
+++ src/StandAlone/inputs/Wasatch/TurbulenceVerification/energy_spectrum_cbc.m
@@ -0,0 +1,19 @@
+% function: energy_spectrum_cbc
+% author:   Tony Saad
+% date:     September, 2012
+%
+% energy_spectrum_cbc: Plots the energy spectrum data reported by
+% Comte-Bellot & Corrsin, a.k.a cbc data. You can find the original paper
+% here ( http://dx.doi.org/10.1017/S0022112071001599) :
+% Comte-Bellot, G. & Corrsin, S. 1971 Simple Eulerian time correlation of 
+%     full- and narrow-band velocity signals in grid-generated, ?isotropic? 
+%     turbulence. J. Fluid Mech. 48, 273?337.
+%
+% The data in cbc_spectrum.txt is from table 3 on page 298 of that paper.
+%
+function energy_spectrum_cbc()
+exp = load('cbc_spectrum.txt');
+loglog(exp(:,1)*100,exp(:,2)*1e-6); hold on;
+loglog(exp(:,1)*100,exp(:,3)*1e-6);
+loglog(exp(:,1)*100,exp(:,4)*1e-6);
+axis([10 1e3 1e-6 1e-3])
