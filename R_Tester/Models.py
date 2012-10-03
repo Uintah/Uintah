adf46d16ee13629bfc340f512193c1074f6379b1
Todd Harman <harman@sci.utah.edu>
2012-10-03 16:57:00 +0000

    using 4 processors on HePlume test nuked darwin test.

--- R_Tester/Models.py
+++ R_Tester/Models.py
@@ -27,14 +27,13 @@ from helpers.runSusTests import runSusTests
 #  3) Performance_tests are not run on a debug build.
 #______________________________________________________________________
 
-NIGHTLYTESTS = [   ("HePlume",       "HePlume.ups",     1.1, "Linux",  ["exactComparison"]), \
-                   ("HePlume",       "HePlume.ups",     1.1, "Darwin", ["doesTestRun"]), \
+NIGHTLYTESTS = [   ("HePlume",       "HePlume.ups",       4, "Linux",  ["exactComparison"]), \
                    ("JP8_Radiation", "JP8_Radiation.ups", 4, "Linux",  ["exactComparison"])
     	        ]
                
                
 # Tests that are run during local regression testing               
-LOCALTESTS   = [   ("HePlume",       "HePlume.ups",     1.1, "Linux",  ["exactComparison"]), \
+LOCALTESTS   = [   ("HePlume",       "HePlume.ups",       4, "Linux",  ["exactComparison"]), \
                    ("JP8_Radiation", "JP8_Radiation.ups", 4, "Linux",  ["exactComparison"])
     	        ]
 
