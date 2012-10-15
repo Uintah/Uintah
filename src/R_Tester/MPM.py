6dcc8fc4b84377359d633afd1d0141a6c2247835
Todd Harman <harman@sci.utah.edu>
2012-10-15 14:26:22 +0000

    missing comma

--- src/R_Tester/MPM.py
+++ src/R_Tester/MPM.py
@@ -48,7 +48,7 @@ NIGHTLYTESTS = [
                   ("test_gurson_beckerdrucker_mts",       "test_gurson_beckerdrucker_mts.ups",       1,  "Linux", ["exactComparison"] ),  \
                   ("test_hypoviscoelastic_radial_return", "test_hypoviscoelastic_radial_return.ups", 1,  "Linux", ["exactComparison"] ),  \
                  #("advect_3L_3D",                        "advect_3L_3D.ups",                        1,  "Linux", ["no_restart"]      ),  \
-                  ("advect_2L_3D_slabs",                  "advect_2L_3D_slabs.ups",                  3,  "Linux", ["exactComparison" "no_restart"] ),  \
+                  ("advect_2L_3D_slabs",                  "advect_2L_3D_slabs.ups",                  3,  "Linux", ["exactComparison", "no_restart"] ),  \
                   ("Charpy",                              "Charpy.ups",                              8,  "Linux", ["exactComparison"] ),  \
                   ("disks_complex",                       "disks_complex.ups",                       4,  "Darwin", ["doesTestRun"]    ),     \
                   ("heatcond2mat",                        "heatcond2mat.ups",                        1,  "Darwin", ["doesTestRun"]    ),     \
