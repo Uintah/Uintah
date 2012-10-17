cb11630042c003e36225846789faabf16bc8bc31
Todd Harman <harman@sci.utah.edu>
2012-10-17 23:25:02 +0000

    Another AMRMPM sanity check.  Passing particles through the edges of the coarse fine interface along all edges.  The particles have with equal and opposite velocities so the total momentum is 0,0,0.

--- R_Tester/MPM.py
+++ R_Tester/MPM.py
@@ -49,6 +49,7 @@ NIGHTLYTESTS = [
                   ("test_hypoviscoelastic_radial_return", "test_hypoviscoelastic_radial_return.ups", 1,  "Linux", ["exactComparison"] ),  \
                   ("advect_3L_3D",                        "advect_3L_3D.ups",                        4,  "Linux", ["exactComparison", "no_restart"] ),  \
                   ("advect_2L_3D_slabs",                  "advect_2L_3D_slabs.ups",                  3,  "Linux", ["exactComparison", "no_restart"] ),  \
+                  ("advect_2L_3D_edges",                  "advect_2L_3D_edges.ups",                  1,  "Linux", ["exactComparison", "no_restart"] ),  \
                   ("Charpy",                              "Charpy.ups",                              8,  "Linux", ["exactComparison"] ),  \
                   ("disks_complex",                       "disks_complex.ups",                       4,  "Darwin", ["doesTestRun"]    ),     \
                   ("heatcond2mat",                        "heatcond2mat.ups",                        1,  "Darwin", ["doesTestRun"]    ),     \
