b5c724dbd0e0780c209a3e169192357393dc4b14
Jeremy Nicholas Thornock <jthornoc@sci.utah.edu>
2012-11-05 04:04:09 +0000

    Adding begining fragments of the wall model interface.

---
+++ src/CCA/Components/Arches/WallHTModels/WallModelDriver.h
@@ -0,0 +1,27 @@
+#ifndef Uintah_Components_Arches_WallModelDriver_h
+#define Uintah_Components_Arches_WallModelDriver_h
+
+
+//============================================
+
+/** 
+ * @class  Wall Model driver 
+ * @author Jeremy Thornock
+ * @date   Oct, 2012
+ *
+ * @brief  Driver class for various wall models. 
+ *
+ */
+
+namespace Uintah{ 
+
+  class WallModelDriver { 
+
+    public: 
+
+      WallModelDriver(); 
+      ~WallModelDriver(); 
+  }; 
+} // namespace Uintah
+
+#endif
