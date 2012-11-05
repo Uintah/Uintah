aa50c2c65582898f34fe6293d9c32beda86710d1
Jeremy Nicholas Thornock <jthornoc@sci.utah.edu>
2012-11-05 04:04:18 +0000

    More updates to the wall model.  We might want to change this to a patch-by-patch calculation rather than all-to-all.

--- CCA/Components/Arches/WallHTModels/WallModelDriver.h
+++ CCA/Components/Arches/WallHTModels/WallModelDriver.h
@@ -1,6 +1,13 @@
 #ifndef Uintah_Components_Arches_WallModelDriver_h
 #define Uintah_Components_Arches_WallModelDriver_h
 
+#include <Core/Grid/LevelP.h>
+#include <Core/Util/DebugStream.h>
+#include <Core/Grid/SimulationStateP.h>
+#include <Core/ProblemSpec/ProblemSpecP.h>
+#include <CCA/Ports/SimulationInterface.h>
+#include <Core/Parallel/UintahParallelComponent.h>
+
 
 //============================================
 
@@ -15,12 +22,94 @@
 
 namespace Uintah{ 
 
+  // setenv SCI_DEBUG WALL_MODEL_DRIVER_DEBUG:+   (tcsh)
+  // export SCI_DEBUG="WALL_MODEL_DRIVER_DEBUG:+" (bash)
+  static DebugStream cout_wmd_dbg("WALL_MODEL_DRIVER",false);
+
+  class VarLabel; 
+
   class WallModelDriver { 
 
     public: 
 
-      WallModelDriver(); 
+      WallModelDriver( SimulationStateP& shared_state ); 
       ~WallModelDriver(); 
+
+      /** @brief Input file interface **/
+      void problemSetup( const ProblemSpecP& db ); 
+
+      /** @brief Compute the heat tranfer to the walls/tubes **/
+      void sched_doWallHT( const LevelP& level, SchedulerP& sched, const int time_substep );
+
+    private: 
+
+      std::string _T_label_name; 
+
+      //varlabel references
+      const VarLabel* _T_label;
+      const VarLabel* _cellType_label; 
+      const VarLabel* _HF_E_label; 
+      const VarLabel* _HF_W_label; 
+      const VarLabel* _HF_N_label; 
+      const VarLabel* _HF_S_label; 
+      const VarLabel* _HF_T_label; 
+      const VarLabel* _HF_B_label; 
+
+      SimulationStateP& _shared_state; 
+
+      int _matl_index; 
+
+      void doWallHT( const ProcessorGroup* my_world,
+                     const PatchSubset* patches, 
+                     const MaterialSubset* matls, 
+                     DataWarehouse* old_dw, 
+                     DataWarehouse* new_dw, 
+                     const int time_substep );
+
+      bool check_varlabels(){ 
+        bool result = true; 
+        std::string offender = "none";
+
+        if ( _T_label == 0 ){ 
+          result = false; 
+          offender = "temperature";
+        } 
+        if ( _cellType_label == 0 ){ 
+          result = false; 
+          offender = "cell_type"; 
+        } 
+        if ( _HF_E_label == 0 ){ 
+          result = false; 
+          offender = "heat_flux_e"; 
+        } 
+        if ( _HF_W_label == 0 ){ 
+          result = false; 
+          offender = "heat_flux_w"; 
+        } 
+        if ( _HF_N_label == 0 ){ 
+          result = false; 
+          offender = "heat_flux_n"; 
+        } 
+        if ( _HF_S_label == 0 ){ 
+          result = false; 
+          offender = "heat_flux_s"; 
+        } 
+        if ( _HF_T_label == 0 ){ 
+          result = false; 
+          offender = "heat_flux_t"; 
+        } 
+        if ( _HF_B_label == 0 ){ 
+          result = false; 
+          offender = "heat_flux_b"; 
+        } 
+
+        cout_wmd_dbg << " WallModelDriver:: The missing varlabel = " << offender << std::endl;
+
+        return result; 
+        
+      } 
+
+
   }; 
 } // namespace Uintah
 
