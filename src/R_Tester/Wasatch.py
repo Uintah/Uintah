64dbb7bbe3ec0d0abd4c1bf229840f2e101af7e8
Tony Saad <tsaad@sci.utah.edu>
2012-12-10 18:47:11 +0000

    Add nightly performance tests for Wasatch.

--- src/R_Tester/Wasatch.py
+++ src/R_Tester/Wasatch.py
@@ -44,6 +44,11 @@ liddrivencavity3DRe1000rk3_ups = modUPS( the_dir, \
 UNUSED_TESTS = []
 
 NIGHTLYTESTS = [
+  ("turb-lid-driven-cavity-3D-WALE_perf",   "turb-lid-driven-cavity-3D-WALE.ups",   8,  "Linux",  ["no_uda_comparison","no_restart","do_performance_test"] ),               \
+  ("turb-lid-driven-cavity-3D-SMAGORINSKY_perf",   "turb-lid-driven-cavity-3D-SMAGORINSKY.ups",   8,  "Linux",  ["no_uda_comparison","no_restart","do_performance_test"] ), \
+  ("turb-lid-driven-cavity-3D-VREMAN_perf",   "turb-lid-driven-cavity-3D-VREMAN.ups",   8,  "Linux",  ["no_uda_comparison","no_restart","do_performance_test"] ),           \
+  ("lid-driven-cavity-3D-Re1000_perf",   "lid-driven-cavity-3D-Re1000.ups",   8,  "Linux",  ["no_uda_comparison","no_restart","do_performance_test"] ),                 \
+  ("scalability-test_perf",              "scalability-test.ups",              1,  "Linux",  ["no_uda_comparison","no_restart","do_performance_test"] ),                 \
   ("turb-lid-driven-cavity-3D-VREMAN",   "turb-lid-driven-cavity-3D-VREMAN.ups",   8,  "Linux",  ["exactComparison","no_restart"] ),                 \
   ("projection_rk3_verification_dt0.01s",      "order-verification/projection_rk3_verification_dt0.01s.ups",   16,  "All",   ["exactComparison","no_restart"] ), \
   ("momentum-test-TGVortex-pressure-src",      "momentum-test-TGVortex-pressure-src.ups",   4,  "Linux",   ["exactComparison","no_restart"] ),                 \
@@ -95,7 +100,7 @@ NIGHTLYTESTS = [
 
 # Tests that are run during local regression testing
 LOCALTESTS = [
-  ("turb-lid-driven-cavity-3D-VREMAN",           "turb-lid-driven-cavity-3D-VREMAN.ups",   8,  "All",  ["exactComparison","no_restart"] ),                       \
+  ("turb-lid-driven-cavity-3D-VREMAN",         "turb-lid-driven-cavity-3D-VREMAN.ups",   8,  "All",  ["exactComparison","no_restart"] ),                         \
   ("projection_rk3_verification_dt0.01s",      "order-verification/projection_rk3_verification_dt0.01s.ups",   16,  "All",   ["exactComparison","no_restart"] ), \
   ("momentum-test-TGVortex-pressure-src",      "momentum-test-TGVortex-pressure-src.ups",   4,  "All",   ["exactComparison","no_restart"] ),                 \
   ("rk3-verification-ode",                     "rk3-verification-ode.ups",   1,  "All",   ["exactComparison","no_restart"] ),                                \
