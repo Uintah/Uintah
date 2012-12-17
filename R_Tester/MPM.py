8cf245e823348b2778a1198dc8f0428d7ac843e2
Dave Austi <austin@sci.utah.edu>
2012-12-17 21:43:34 +0000

    Minor touchup and getting R_Tester ready for new tests. -Dave A.

--- R_Tester/MPM.py
+++ R_Tester/MPM.py
@@ -29,43 +29,71 @@ from helpers.runSusTests import runSusTests
 #______________________________________________________________________
 
 NIGHTLYTESTS = [  
-                  ("disks_complex",                       "disks_complex.ups",                       4,  "Linux", ["exactComparison"] ), \
-                  ("heatcond2mat",                        "heatcond2mat.ups",                        1,  "Linux", ["exactComparison"] ),  \
-                  ("inclined_plane_sphere",               "inclined_plane_sphere.ups",               1,  "Linux", ["exactComparison"] ),  \
-                  ("foam_crush",                          "foam_crush.ups",                          4,  "Linux", ["exactComparison"] ),  \
-                  ("periodic_disks",                      "periodic_disks.ups",                      1,  "Linux", ["exactComparison"] ),  \
-                  ("periodic_spheres3D",                  "periodic_spheres3D.ups",                  8,  "Linux", ["exactComparison"] ),  \
-                  ("const_test_hypo",                     "const_test_hypo.ups",                     1,  "Linux", ["exactComparison"] ),  \
-                  ("const_test_cmr",                      "const_test_cmr.ups",                      1,  "Linux", ["exactComparison"] ),  \
-                  ("const_test_nhp",                      "const_test_nhp.ups",                      1,  "Linux", ["exactComparison"] ),  \
-                  ("const_test_vs",                       "const_test_vs.ups",                       1,  "Linux", ["exactComparison"] ),  \
-                  ("adiCuJC4000s696K",                    "adiCuJC4000s696K.ups",                    1,  "Linux", ["exactComparison"] ),  \
-                  ("adiCuMTS4000s696K",                   "adiCuMTS4000s696K.ups",                   1,  "Linux", ["exactComparison"] ),  \
-                  ("adiCuPTW4000s696K",                   "adiCuPTW4000s696K.ups",                   1,  "Linux", ["exactComparison"] ),  \
-                  ("adiCuSCG4000s696K",                   "adiCuSCG4000s696K.ups",                   1,  "Linux", ["exactComparison"] ),  \
-                  ("adiCuZA4000s696K",                    "adiCuZA4000s696K.ups",                    1,  "Linux", ["exactComparison"] ),  \
-                  ("test_corrug_plate",                   "test_corrug_plate.ups",                   1,  "Linux", ["exactComparison"] ),  \
-                  ("test_cyl_pene_no_ero",                "test_cyl_pene_no_ero.ups",                1,  "Linux", ["exactComparison"] ),  \
-                  ("test_gurson_beckerdrucker_mts",       "test_gurson_beckerdrucker_mts.ups",       1,  "Linux", ["exactComparison"] ),  \
-                  ("test_hypoviscoelastic_radial_return", "test_hypoviscoelastic_radial_return.ups", 1,  "Linux", ["exactComparison"] ),  \
-                  ("advect_3L_3D",                        "advect_3L_3D.ups",                        4,  "Linux", ["exactComparison", "no_restart"] ),  \
-                  ("advect_2L_3D_slabs",                  "advect_2L_3D_slabs.ups",                  3,  "Linux", ["exactComparison", "no_restart"] ),  \
-                  ("advect_2L_3D_edges",                  "advect_2L_3D_edges.ups",                  1,  "Linux", ["exactComparison", "no_restart"] ),  \
-                  ("riemannMPM_ML",                       "riemannMPM_ML.ups",                       1,  "Linux", ["exactComparison"] ),  \
-                  ("Charpy",                              "Charpy.ups",                              8,  "Linux", ["exactComparison"] ),  \
-                  ("disks_complex",                       "disks_complex.ups",                       4,  "Darwin", ["doesTestRun"]    ),     \
-                  ("heatcond2mat",                        "heatcond2mat.ups",                        1,  "Darwin", ["doesTestRun"]    ),     \
-                  ("inclined_plane_sphere",               "inclined_plane_sphere.ups",               1,  "Darwin", ["doesTestRun"]    ),     \
-                  ("const_test_cmr",                      "const_test_cmr.ups",                      1,  "Darwin", ["doesTestRun"]    ),     \
-                  ("const_test_nhp",                      "const_test_nhp.ups",                      1,  "Darwin", ["doesTestRun"]    ),     \
-                  ("adiCuJC4000s696K",                    "adiCuJC4000s696K.ups",                    1,  "Darwin", ["doesTestRun"]    ),     \
-                  ("adiCuMTS4000s696K",                   "adiCuMTS4000s696K.ups",                   1,  "Darwin", ["doesTestRun"]    ),     \
-                  ("adiCuPTW4000s696K",                   "adiCuPTW4000s696K.ups",                   1,  "Darwin", ["doesTestRun"]    ),     \
-                  ("adiCuSCG4000s696K",                   "adiCuSCG4000s696K.ups",                   1,  "Darwin", ["doesTestRun"]    ),     \
-                  ("adiCuZA4000s696K",                    "adiCuZA4000s696K.ups",                    1,  "Darwin", ["doesTestRun"]    ),     \
-                  ("test_corrug_plate",                   "test_corrug_plate.ups",                   1,  "Darwin", ["doesTestRun"]    ),     \
-                  ("test_cyl_pene_no_ero",                "test_cyl_pene_no_ero.ups",                1,  "Darwin", ["doesTestRun"]    ),     \
-                  ("test_gurson_beckerdrucker_mts",       "test_gurson_beckerdrucker_mts.ups",       1,  "Darwin", ["doesTestRun"]    ) 
+                  #----------  Linux Tests ---------  #
+                  ("disks_complex",                       "disks_complex.ups",                       4,  "Linux", ["exactComparison"] ), 
+                  ("heatcond2mat",                        "heatcond2mat.ups",                        1,  "Linux", ["exactComparison"] ),  
+                  ("inclined_plane_sphere",               "inclined_plane_sphere.ups",               1,  "Linux", ["exactComparison"] ),  
+                  ("foam_crush",                          "foam_crush.ups",                          4,  "Linux", ["exactComparison"] ),  
+                  ("periodic_disks",                      "periodic_disks.ups",                      1,  "Linux", ["exactComparison"] ),  
+                  ("periodic_spheres3D",                  "periodic_spheres3D.ups",                  8,  "Linux", ["exactComparison"] ),  
+                  ("const_test_hypo",                     "const_test_hypo.ups",                     1,  "Linux", ["exactComparison"] ),  
+                  ("const_test_cmr",                      "const_test_cmr.ups",                      1,  "Linux", ["exactComparison"] ),  
+                  ("const_test_nhp",                      "const_test_nhp.ups",                      1,  "Linux", ["exactComparison"] ),  
+                  ("const_test_vs",                       "const_test_vs.ups",                       1,  "Linux", ["exactComparison"] ),  
+                  ("adiCuJC4000s696K",                    "adiCuJC4000s696K.ups",                    1,  "Linux", ["exactComparison"] ),  
+                  ("adiCuMTS4000s696K",                   "adiCuMTS4000s696K.ups",                   1,  "Linux", ["exactComparison"] ),  
+                  ("adiCuPTW4000s696K",                   "adiCuPTW4000s696K.ups",                   1,  "Linux", ["exactComparison"] ),  
+                  ("adiCuSCG4000s696K",                   "adiCuSCG4000s696K.ups",                   1,  "Linux", ["exactComparison"] ),  
+                  ("adiCuZA4000s696K",                    "adiCuZA4000s696K.ups",                    1,  "Linux", ["exactComparison"] ),  
+                  ("test_corrug_plate",                   "test_corrug_plate.ups",                   1,  "Linux", ["exactComparison"] ),  
+                  ("test_cyl_pene_no_ero",                "test_cyl_pene_no_ero.ups",                1,  "Linux", ["exactComparison"] ),  
+                  ("test_gurson_beckerdrucker_mts",       "test_gurson_beckerdrucker_mts.ups",       1,  "Linux", ["exactComparison"] ),  
+                  ("test_hypoviscoelastic_radial_return", "test_hypoviscoelastic_radial_return.ups", 1,  "Linux", ["exactComparison"] ),  
+                  ("advect_3L_3D",                        "advect_3L_3D.ups",                        4,  "Linux", ["exactComparison", "no_restart"] ),  
+                  ("advect_2L_3D_slabs",                  "advect_2L_3D_slabs.ups",                  3,  "Linux", ["exactComparison", "no_restart"] ),  
+                  ("advect_2L_3D_edges",                  "advect_2L_3D_edges.ups",                  1,  "Linux", ["exactComparison", "no_restart"] ),  
+                  ("riemannMPM_ML",                       "riemannMPM_ML.ups",                       1,  "Linux", ["exactComparison"] ),  
+                  ("Charpy",                              "Charpy.ups",                              8,  "Linux", ["exactComparison"] ),  
+                  
+                  #("AreniscaTest_01_UniaxialStrainRotate",                  "./Arenisca/AreniscaTest_01_UniaxialStrainRotate.ups",                  1,  "Linux", ["exactComparison"] ), 
+                  #("AreniscaTest_02_VertexTreatment",                       "./Arenisca/AreniscaTest_02_VertexTreatment.ups",                       1,  "Linux", ["exactComparison"] ), 
+                  #("AreniscaTest_03a_UniaxialStrain_NoHardening",           "./Arenisca/AreniscaTest_03a_UniaxialStrain_NoHardening.ups",           1,  "Linux", ["exactComparison"] ),
+                  #("AreniscaTest_03b_UniaxialStrain_wIsotropicHardening",   "./Arenisca/AreniscaTest_03b_UniaxialStrain_wIsotropicHardening.ups",   1,  "Linux", ["exactComparison"] ),                  
+                  #("AreniscaTest_03c_UniaxialStrain_wKinematicHardening",   "./Arenisca/AreniscaTest_03c_UniaxialStrain_wKinematicHardening.ups",   1,  "Linux", ["exactComparison"] ),
+                  #("AreniscaTest_04_CurvedYieldSurface",                    "./Arenisca/AreniscaTest_04_CurvedYieldSurface.ups",                    1,  "Linux", ["exactComparison"] ),
+                  #("AreniscaTest_05_HydrostaticCompressionFixedCap",        "./Arenisca/AreniscaTest_05_HydrostaticCompressionFixedCap.ups",        1,  "Linux", ["exactComparison"] ),
+                  #("AreniscaTest_06_UniaxialStrainCapEvolution",            "./Arenisca/AreniscaTest_06_UniaxialStrainCapEvolution.ups",            1,  "Linux", ["exactComparison"] ),
+                  #("AreniscaTest_07_HydrostaticCompressionCapEvolution",    "./Arenisca/AreniscaTest_07_HydrostaticCompressionCapEvolution.ups",    1,  "Linux", ["exactComparison"] ),
+                  #("AreniscaTest_08_LoadingUnloading",                      "./Arenisca/AreniscaTest_08_LoadingUnloading.ups",                      1,  "Linux", ["exactComparison"] ),
+                  #("AreniscaTest_09_FluidFilledPoreSpace",                  "./Arenisca/AreniscaTest_09_FluidFilledPoreSpace.ups",                  1,  "Linux", ["exactComparison"] ),                              
+                  
+                  #----------  Darwin Tests ---------  #
+                  ("disks_complex",                       "disks_complex.ups",                       4,  "Darwin", ["doesTestRun"]    ),     
+                  ("heatcond2mat",                        "heatcond2mat.ups",                        1,  "Darwin", ["doesTestRun"]    ),     
+                  ("inclined_plane_sphere",               "inclined_plane_sphere.ups",               1,  "Darwin", ["doesTestRun"]    ),     
+                  ("const_test_cmr",                      "const_test_cmr.ups",                      1,  "Darwin", ["doesTestRun"]    ),     
+                  ("const_test_nhp",                      "const_test_nhp.ups",                      1,  "Darwin", ["doesTestRun"]    ),     
+                  ("adiCuJC4000s696K",                    "adiCuJC4000s696K.ups",                    1,  "Darwin", ["doesTestRun"]    ),     
+                  ("adiCuMTS4000s696K",                   "adiCuMTS4000s696K.ups",                   1,  "Darwin", ["doesTestRun"]    ),     
+                  ("adiCuPTW4000s696K",                   "adiCuPTW4000s696K.ups",                   1,  "Darwin", ["doesTestRun"]    ),     
+                  ("adiCuSCG4000s696K",                   "adiCuSCG4000s696K.ups",                   1,  "Darwin", ["doesTestRun"]    ),     
+                  ("adiCuZA4000s696K",                    "adiCuZA4000s696K.ups",                    1,  "Darwin", ["doesTestRun"]    ),     
+                  ("test_corrug_plate",                   "test_corrug_plate.ups",                   1,  "Darwin", ["doesTestRun"]    ),     
+                  ("test_cyl_pene_no_ero",                "test_cyl_pene_no_ero.ups",                1,  "Darwin", ["doesTestRun"]    ),     
+                  ("test_gurson_beckerdrucker_mts",       "test_gurson_beckerdrucker_mts.ups",       1,  "Darwin", ["doesTestRun"]    ),
+                  
+                  #("AreniscaTest_01_UniaxialStrainRotate",                  "./Arenisca/AreniscaTest_01_UniaxialStrainRotate.ups",                  1,  "Darwin", ["exactComparison"] ), 
+                  #("AreniscaTest_02_VertexTreatment",                       "./Arenisca/AreniscaTest_02_VertexTreatment.ups",                       1,  "Darwin", ["exactComparison"] ), 
+                  #("AreniscaTest_03a_UniaxialStrain_NoHardening",           "./Arenisca/AreniscaTest_03a_UniaxialStrain_NoHardening.ups",           1,  "Darwin", ["exactComparison"] ),
+                  #("AreniscaTest_03b_UniaxialStrain_wIsotropicHardening",   "./Arenisca/AreniscaTest_03b_UniaxialStrain_wIsotropicHardening.ups",   1,  "Darwin", ["exactComparison"] ),                  
+                  #("AreniscaTest_03c_UniaxialStrain_wKinematicHardening",   "./Arenisca/AreniscaTest_03c_UniaxialStrain_wKinematicHardening.ups",   1,  "Darwin", ["exactComparison"] ),
+                  #("AreniscaTest_04_CurvedYieldSurface",                    "./Arenisca/AreniscaTest_04_CurvedYieldSurface.ups",                    1,  "Darwin", ["exactComparison"] ),
+                  #("AreniscaTest_05_HydrostaticCompressionFixedCap",        "./Arenisca/AreniscaTest_05_HydrostaticCompressionFixedCap.ups",        1,  "Darwin", ["exactComparison"] ),
+                  #("AreniscaTest_06_UniaxialStrainCapEvolution",            "./Arenisca/AreniscaTest_06_UniaxialStrainCapEvolution.ups",            1,  "Darwin", ["exactComparison"] ),
+                  #("AreniscaTest_07_HydrostaticCompressionCapEvolution",    "./Arenisca/AreniscaTest_07_HydrostaticCompressionCapEvolution.ups",    1,  "Darwin", ["exactComparison"] ),
+                  #("AreniscaTest_08_LoadingUnloading",                      "./Arenisca/AreniscaTest_08_LoadingUnloading.ups",                      1,  "Darwin", ["exactComparison"] ),
+                  #("AreniscaTest_09_FluidFilledPoreSpace",                  "./Arenisca/AreniscaTest_09_FluidFilledPoreSpace.ups",                  1,  "Darwin", ["exactComparison"] ),                  
+
             ]
               
 # Tests that are run during local regression testing              
