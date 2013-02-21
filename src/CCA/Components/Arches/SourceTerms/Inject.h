095f139e5837154a6bb0e32da74ba9414adf3563
Jeremy Nicholas Thornock <jthornoc@sci.utah.edu>
2013-02-21 19:25:03 +0000

    Fixing a couple minor issues with the Inject source term.

--- src/CCA/Components/Arches/SourceTerms/Inject.h
+++ src/CCA/Components/Arches/SourceTerms/Inject.h
@@ -166,7 +166,7 @@ private:
       db->getWithDefault("constant",_constant, 0.0); 
       _injet_type = CONSTANT; 
 
-    } else if ( inject_type == "file" ){ 
+    } else if ( inject_type == "fromfile" ){ 
 
       std::string file_name;
       db->require("inputfile", file_name); 
@@ -187,7 +187,7 @@ private:
         std::string varname  = getString( file );
         input_file  = getString( file ); 
 
-        if ( input_file == _src_name ){ 
+        if ( varname == _src_name ){ 
           found_file = true; 
           break; 
         } 
