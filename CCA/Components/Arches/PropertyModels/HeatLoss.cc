1059ee429b47f98f80e32aa58d77db1f9f8acf4e
Jeremy Nicholas Thornock <jthornoc@sci.utah.edu>
2013-03-11 22:20:45 +0000

    This allows you to compute heat loss, but use a constant value when doing table look ups.  This is useful for debugging and verification work.

--- CCA/Components/Arches/PropertyModels/HeatLoss.cc
+++ CCA/Components/Arches/PropertyModels/HeatLoss.cc
@@ -26,6 +26,11 @@ HeatLoss::HeatLoss( std::string prop_name, SimulationStateP& shared_state ) : Pr
 HeatLoss::~HeatLoss( )
 {
   delete _boundary_condition; 
+
+  if ( _constant_heat_loss ){ 
+    VarLabel::destroy( _actual_hl_label ); 
+  } 
+
 }
 
 
@@ -41,25 +46,26 @@ HeatLoss::problemSetup( const ProblemSpecP& inputdb )
 
     _constant_heat_loss = true; 
 
+    std::string name = _prop_name + "_actual";
+    _actual_hl_label = VarLabel::create( name, CCVariable<double>::getTypeDescription() ); 
+
   } 
-  else {
 
-    db->require( "enthalpy_label", _enthalpy_label_name ); 
-    db->getWithDefault( "adiabatic_enthalpy_label" , _adiab_h_label_name , "adiabaticenthalpy" );
-    db->getWithDefault( "sensible_enthalpy_label"  , _sen_h_label_name   , "sensibleenthalpy" );
+  db->require( "enthalpy_label", _enthalpy_label_name ); 
+  db->getWithDefault( "adiabatic_enthalpy_label" , _adiab_h_label_name , "adiabaticenthalpy" );
+  db->getWithDefault( "sensible_enthalpy_label"  , _sen_h_label_name   , "sensibleenthalpy" );
 
-    _low_hl  = -1; 
-    _high_hl =  1;
-    if ( db->findBlock( "hl_bounds" ) ) { 
-      db->findBlock( "hl_bounds" )->getAttribute("low"  , _low_hl );
-      db->findBlock( "hl_bounds" )->getAttribute("high" , _high_hl);
-    }
-
-    _noisy_heat_loss = false; 
-    if ( db->findBlock( "noisy_hl_warning" ) ){ 
-      _noisy_heat_loss = true;
-    } 
+  _low_hl  = -1; 
+  _high_hl =  1;
+  if ( db->findBlock( "hl_bounds" ) ) { 
+    db->findBlock( "hl_bounds" )->getAttribute("low"  , _low_hl );
+    db->findBlock( "hl_bounds" )->getAttribute("high" , _high_hl);
   }
+
+  _noisy_heat_loss = false; 
+  if ( db->findBlock( "noisy_hl_warning" ) ){ 
+    _noisy_heat_loss = true;
+  } 
 
   commonProblemSetup( inputdb ); 
 }
@@ -73,53 +79,49 @@ void HeatLoss::sched_computeProp( const LevelP& level, SchedulerP& sched, int ti
   std::string taskname = "HeatLoss::computeProp"; 
   Task* tsk = scinew Task( taskname, this, &HeatLoss::computeProp, time_substep ); 
 
-  if ( !_constant_heat_loss ){ 
-	  _enthalpy_label = 0; 
-	  _adiab_h_label  = 0; 
-	  _sen_h_label    = 0; 
+	_enthalpy_label = 0; 
+	_adiab_h_label  = 0; 
+	_sen_h_label    = 0; 
 
-	  _enthalpy_label = VarLabel::find( _enthalpy_label_name ); 
-	  _adiab_h_label  = VarLabel::find( _adiab_h_label_name  ); 
-	  _sen_h_label    = VarLabel::find( _sen_h_label_name    ); 
+	_enthalpy_label = VarLabel::find( _enthalpy_label_name ); 
+	_adiab_h_label  = VarLabel::find( _adiab_h_label_name  ); 
+	_sen_h_label    = VarLabel::find( _sen_h_label_name    ); 
 
-	  if ( _enthalpy_label == 0 ){ 
-    	throw InvalidValue( "Error: Could not find enthalpy label with name: "+_enthalpy_label_name, __FILE__, __LINE__); 
-	  } 
-	  if ( _sen_h_label == 0 ){ 
-    	throw InvalidValue( "Error: Could not find sensible enthalpy label with name: "+_sen_h_label_name, __FILE__, __LINE__); 
-	  } 
-	  if ( _adiab_h_label == 0 ){ 
-    	throw InvalidValue( "Error: Could not find adiabatic enthalpy label with name: "+_adiab_h_label_name, __FILE__, __LINE__); 
-	  } 
+	if ( _enthalpy_label == 0 ){ 
+  	throw InvalidValue( "Error: Could not find enthalpy label with name: "+_enthalpy_label_name, __FILE__, __LINE__); 
+	} 
+	if ( _sen_h_label == 0 ){ 
+  	throw InvalidValue( "Error: Could not find sensible enthalpy label with name: "+_sen_h_label_name, __FILE__, __LINE__); 
+	} 
+	if ( _adiab_h_label == 0 ){ 
+  	throw InvalidValue( "Error: Could not find adiabatic enthalpy label with name: "+_adiab_h_label_name, __FILE__, __LINE__); 
+	} 
 
-	  if ( time_substep == 0 && !_has_been_computed ){ 
+	if ( time_substep == 0 && !_has_been_computed ){ 
 
-	  	tsk->computes( _prop_label ); 
+		tsk->computes( _prop_label ); 
 
-	  	tsk->requires( Task::OldDW , _enthalpy_label , Ghost::None , 0 );
-	  	tsk->requires( Task::OldDW , _sen_h_label    , Ghost::None , 0 );
-	  	tsk->requires( Task::OldDW , _adiab_h_label  , Ghost::None , 0 );
+    if ( _constant_heat_loss ){ 
+      tsk->computes( _actual_hl_label ); 
+    } 
 
-	  } else { 
+		tsk->requires( Task::OldDW , _enthalpy_label , Ghost::None , 0 );
+		tsk->requires( Task::OldDW , _sen_h_label    , Ghost::None , 0 );
+		tsk->requires( Task::OldDW , _adiab_h_label  , Ghost::None , 0 );
 
-	  	tsk->modifies( _prop_label ); 
+	} else { 
 
-	  	tsk->requires( Task::NewDW , _enthalpy_label , Ghost::None , 0 );
-	  	tsk->requires( Task::NewDW , _sen_h_label    , Ghost::None , 0 );
-	  	tsk->requires( Task::NewDW , _adiab_h_label  , Ghost::None , 0 );
+		tsk->modifies( _prop_label ); 
 
-	  } 
-  } else { 
-	  if ( time_substep == 0 && !_has_been_computed ){ 
+    if ( _constant_heat_loss ){ 
+      tsk->modifies( _actual_hl_label ); 
+    } 
 
-	  	tsk->computes( _prop_label ); 
+		tsk->requires( Task::NewDW , _enthalpy_label , Ghost::None , 0 );
+		tsk->requires( Task::NewDW , _sen_h_label    , Ghost::None , 0 );
+		tsk->requires( Task::NewDW , _adiab_h_label  , Ghost::None , 0 );
 
-	  } else { 
-
-	  	tsk->modifies( _prop_label ); 
-
-	  } 
-  }
+	} 
 
   sched->addTask( tsk, level->eachPatch(), _shared_state->allArchesMaterials() ); 
 	_has_been_computed = true; 
@@ -130,11 +132,11 @@ void HeatLoss::sched_computeProp( const LevelP& level, SchedulerP& sched, int ti
 //Method: Actually Compute Property
 //---------------------------------------------------------------------------
 void HeatLoss::computeProp(const ProcessorGroup* pc, 
-                                    const PatchSubset* patches, 
-                                    const MaterialSubset* matls, 
-                                    DataWarehouse* old_dw, 
-                                    DataWarehouse* new_dw, 
-                                    int time_substep )
+                           const PatchSubset* patches, 
+                           const MaterialSubset* matls, 
+                           DataWarehouse* old_dw, 
+                           DataWarehouse* new_dw, 
+                           int time_substep )
 {
   //patch loop
   for (int p=0; p < patches->size(); p++){
@@ -148,86 +150,86 @@ void HeatLoss::computeProp(const ProcessorGroup* pc,
 
 		bool oob_up = false; 
 		bool oob_dn = false; 
+
+    CCVariable<double> prop; 
+
+		constCCVariable<double> h;       //enthalpy
+		constCCVariable<double> h_sen;   //sensible enthalpy
+		constCCVariable<double> h_ad;    //adiabatic enthalpy
+
+		if ( time_substep == 0 ){ 
+
+      new_dw->allocateAndPut( prop, _prop_label, matlIndex, patch ); 
+      prop.initialize(0.0); 
+
+			old_dw->get( h     , _enthalpy_label , matlIndex , patch , Ghost::None , 0 );
+			old_dw->get( h_sen , _sen_h_label    , matlIndex , patch , Ghost::None , 0 );
+			old_dw->get( h_ad  , _adiab_h_label  , matlIndex , patch , Ghost::None , 0 );
+
+		} else { 
+
+      new_dw->getModifiable( prop, _prop_label, matlIndex, patch ); 
+
+			new_dw->get( h     , _enthalpy_label , matlIndex , patch , Ghost::None , 0 );
+			new_dw->get( h_sen , _sen_h_label    , matlIndex , patch , Ghost::None , 0 );
+			new_dw->get( h_ad  , _adiab_h_label  , matlIndex , patch , Ghost::None , 0 );
+
+		} 
+
+    for (iter.begin(); !iter.done(); iter++){
+
+			IntVector c = *iter; 
+
+			double small = 1e-16;
+      double numerator = h_ad[c] - h[c]; 
+
+      double hl = ( numerator ) / ( h_sen[c] + small ); 
+
+			if ( hl < _low_hl ){ 
+				hl     = -1;
+				oob_dn = true;
+			} 
+			if ( hl > _high_hl ){ 
+				hl     = 1;
+				oob_up = true;
+			} 
+
+			prop[c] = hl;
+
+    }
+
+    //Apply boundary conditions
+    _boundary_condition->setScalarValueBC( 0, patch, prop, _prop_name ); 
+
+    if ( _noisy_heat_loss ) { 
+     
+      if ( oob_up || oob_dn ) {  
+				std::cout << "Patch with bounds: " << patch->getCellLowIndex() << " to " << patch->getCellHighIndex()  << std::endl;
+        if ( oob_dn ) 
+          std::cout << "   --> lower heat loss exceeded. " << std::endl;
+        if ( oob_up ) 
+          std::cout << "   --> upper heat loss exceeded. " << std::endl;
+      } 
+    } 
+
     if ( _constant_heat_loss ){ 
 
-      CCVariable<double> prop; 
+      CCVariable<double> actual_heat_loss; 
 
 		  if ( time_substep == 0 ){ 
 
-        new_dw->allocateAndPut( prop, _prop_label, matlIndex, patch ); 
+        new_dw->allocateAndPut( actual_heat_loss, _actual_hl_label, matlIndex, patch ); 
 
 		  } else { 
 
-        new_dw->getModifiable( prop, _prop_label, matlIndex, patch ); 
+        new_dw->getModifiable( actual_heat_loss, _actual_hl_label, matlIndex, patch ); 
 
 		  } 
 
+      actual_heat_loss.copyData( prop ); 
       prop.initialize( _const_init ); 
 
-    } else { 
-
-      CCVariable<double> prop; 
-
-		  constCCVariable<double> h;       //enthalpy
-		  constCCVariable<double> h_sen;   //sensible enthalpy
-		  constCCVariable<double> h_ad;    //adiabatic enthalpy
-
-		  if ( time_substep == 0 ){ 
-
-        new_dw->allocateAndPut( prop, _prop_label, matlIndex, patch ); 
-        prop.initialize(0.0); 
-
-		  	old_dw->get( h     , _enthalpy_label , matlIndex , patch , Ghost::None , 0 );
-		  	old_dw->get( h_sen , _sen_h_label    , matlIndex , patch , Ghost::None , 0 );
-		  	old_dw->get( h_ad  , _adiab_h_label  , matlIndex , patch , Ghost::None , 0 );
-
-		  } else { 
-
-        new_dw->getModifiable( prop, _prop_label, matlIndex, patch ); 
-
-		  	new_dw->get( h     , _enthalpy_label , matlIndex , patch , Ghost::None , 0 );
-		  	new_dw->get( h_sen , _sen_h_label    , matlIndex , patch , Ghost::None , 0 );
-		  	new_dw->get( h_ad  , _adiab_h_label  , matlIndex , patch , Ghost::None , 0 );
-
-		  } 
-
-      for (iter.begin(); !iter.done(); iter++){
-
-		  	IntVector c = *iter; 
-
-		  	double small = 1e-16;
-        double numerator = h_ad[c] - h[c]; 
-
-        double hl = ( numerator ) / ( h_sen[c] + small ); 
-
-		  	if ( hl < _low_hl ){ 
-		  		hl     = -1;
-		  		oob_dn = true;
-		  	} 
-		  	if ( hl > _high_hl ){ 
-		  		hl     = 1;
-		  		oob_up = true;
-		  	} 
-
-		  	prop[c] = hl;
-
-      }
-
-      //Apply boundary conditions
-      _boundary_condition->setScalarValueBC( 0, patch, prop, _prop_name ); 
-
-
-      if ( _noisy_heat_loss ) { 
-       
-        if ( oob_up || oob_dn ) {  
-		  		std::cout << "Patch with bounds: " << patch->getCellLowIndex() << " to " << patch->getCellHighIndex()  << std::endl;
-          if ( oob_dn ) 
-            std::cout << "   --> lower heat loss exceeded. " << std::endl;
-          if ( oob_up ) 
-            std::cout << "   --> upper heat loss exceeded. " << std::endl;
-        } 
-      } 
-    }
+    } 
   }
 }
 
