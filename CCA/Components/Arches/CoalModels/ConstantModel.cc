8b933afa893c9bce03c6b6efde48dd6f86281d54
Jeremy Nicholas Thornock <jthornoc@sci.utah.edu>
2013-03-21 19:29:46 +0000

    Do not use d_labelSchedInit to check if the label should be computed or modified.

--- CCA/Components/Arches/CoalModels/ConstantModel.cc
+++ CCA/Components/Arches/CoalModels/ConstantModel.cc
@@ -188,7 +188,7 @@ ConstantModel::sched_computeModel( const LevelP& level, SchedulerP& sched, int t
 
   d_timeSubStep = timeSubStep; 
 
-  if (d_timeSubStep == 0 && !d_labelSchedInit) {
+  if (d_timeSubStep == 0) {
     // Every model term needs to set this flag after the varLabel is computed. 
     // transportEqn.cleanUp should reinitialize this flag at the end of the time step. 
     d_labelSchedInit = true;
