b5f62a35087a616e5db338743ce9a2f259bd551b
Jeremy Nicholas Thornock <jthornoc@sci.utah.edu>
2013-03-29 19:20:52 +0000

    Only add adiabatic and sensible enthalpy if the model is actually a heat loss model.

--- CCA/Components/Arches/ChemMix/ClassicTableInterface.cc
+++ CCA/Components/Arches/ChemMix/ClassicTableInterface.cc
@@ -177,12 +177,11 @@ ClassicTableInterface::problemSetup( const ProblemSpecP& propertiesParameters )
 
       std::string adiab_name, sens_name; 
 
-	    model_db->getWithDefault( "adiabatic_enthalpy_label" , adiab_name  , "adiabaticenthalpy" );
-	    model_db->getWithDefault( "sensible_enthalpy_label"  , sens_name   , "sensibleenthalpy" );
-
       if ( type == "heat_loss" ){ 
         insertIntoMap( adiab_name ); 
         insertIntoMap( sens_name ); 
+	      model_db->getWithDefault( "adiabatic_enthalpy_label" , adiab_name  , "adiabaticenthalpy" );
+	      model_db->getWithDefault( "sensible_enthalpy_label"  , sens_name   , "sensibleenthalpy" );
       } 
 
     }
