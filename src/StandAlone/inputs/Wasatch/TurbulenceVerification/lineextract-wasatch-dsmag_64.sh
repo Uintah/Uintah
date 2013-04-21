11363f992c91c1e4da980df79ab49e1c5b7a8664
Tony Saad <tsaad@sci.utah.edu>
2013-04-21 23:47:30 +0000

    add verification tests for Wasatch's dynamic model.

---
+++ src/StandAlone/inputs/Wasatch/TurbulenceVerification/lineextract-wasatch-dsmag_64.sh
@@ -0,0 +1,11 @@
+./lineextract -pr 32 -v x-mom -timestep 0 -istart 0 0 0 -iend 63 63 63 -o uvel_64_wasatch_dsmag_t0.0s.txt -uda decay-isotropic-turbulence-dsmag-64.uda
+./lineextract -pr 32 -v y-mom -timestep 0 -istart 0 0 0 -iend 63 63 63 -o vvel_64_wasatch_dsmag_t0.0s.txt -uda decay-isotropic-turbulence-dsmag-64.uda
+./lineextract -pr 32 -v z-mom -timestep 0 -istart 0 0 0 -iend 63 63 63 -o wvel_64_wasatch_dsmag_t0.0s.txt -uda decay-isotropic-turbulence-dsmag-64.uda
+#
+./lineextract -pr 32 -v x-mom -timestep 28 -istart 0 0 0 -iend 63 63 63 -o uvel_64_wasatch_dsmag_t0.28s.txt -uda decay-isotropic-turbulence-dsmag-64.uda
+./lineextract -pr 32 -v y-mom -timestep 28 -istart 0 0 0 -iend 63 63 63 -o vvel_64_wasatch_dsmag_t0.28s.txt -uda decay-isotropic-turbulence-dsmag-64.uda
+./lineextract -pr 32 -v z-mom -timestep 28 -istart 0 0 0 -iend 63 63 63 -o wvel_64_wasatch_dsmag_t0.28s.txt -uda decay-isotropic-turbulence-dsmag-64.uda
+#
+./lineextract -pr 32 -v x-mom -timestep 66 -istart 0 0 0 -iend 63 63 63 -o uvel_64_wasatch_dsmag_t0.66s.txt -uda decay-isotropic-turbulence-dsmag-64.uda
+./lineextract -pr 32 -v y-mom -timestep 66 -istart 0 0 0 -iend 63 63 63 -o vvel_64_wasatch_dsmag_t0.66s.txt -uda decay-isotropic-turbulence-dsmag-64.uda
+./lineextract -pr 32 -v z-mom -timestep 66 -istart 0 0 0 -iend 63 63 63 -o wvel_64_wasatch_dsmag_t0.66s.txt -uda decay-isotropic-turbulence-dsmag-64.uda
