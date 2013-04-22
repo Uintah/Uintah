753e475ea7828850d35d583b941b444ef5e7a498
Tony Saad <tsaad@sci.utah.edu>
2013-04-22 22:10:44 +0000

    add usage example to remove_xml_block.sh

--- src/StandAlone/inputs/Wasatch/remove_xml_block.sh
+++ src/StandAlone/inputs/Wasatch/remove_xml_block.sh
@@ -1,2 +1,4 @@
+# usage: ./remove_xml_block blockToRemove
+# example: ./remove_xml_block Kolmogorov  - will remove all LINES that contains the word Kolmogorov
 echo Removing all lines that contain $1
 sed -i '/'$1'/d' *.ups
