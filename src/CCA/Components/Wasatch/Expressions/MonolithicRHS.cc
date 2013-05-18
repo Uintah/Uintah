2afc701621e5416c02e7bfd8f07122f68b8f9ee3
James Sutherland <jsutherland@sci.utah.edu>
2013-05-18 16:33:59 +0000

    Refactor MonolithicRHS to use new inlined/chained operators.

--- src/CCA/Components/Wasatch/Expressions/MonolithicRHS.cc
+++ src/CCA/Components/Wasatch/Expressions/MonolithicRHS.cc
@@ -47,11 +47,7 @@ MonolithicRHS( const Expr::Tag& dCoefTag,
     phiTag_      ( phiTag       ),
     srcTag_      ( srcTag       )
 {
-  // right now we must have all directions active with convection and diffusion.
-//  assert( xconvFluxTag != Expr::Tag() );
-//  assert( yconvFluxTag != Expr::Tag() );
-//  assert( zconvFluxTag != Expr::Tag() );
-  assert( dCoefTag     != Expr::Tag() );
+  assert( dCoefTag != Expr::Tag() );
 }
 
 //--------------------------------------------------------------------
@@ -128,96 +124,33 @@ void
 MonolithicRHS<FieldT>::
 evaluate()
 {
+  using namespace SpatialOps;
   FieldT& result = this->value();
 
-  using namespace SpatialOps;
-  using SpatialOps::structured::IntVec;
+  const Expr::Tag nullTag = Expr::Tag();
 
-  const IntVec neutral( 0, 0, 0 );
-  const IntVec neg_X  (-1, 0, 0 );
-  const IntVec pos_X  ( 1, 0, 0 );
-  const IntVec neg_Y  ( 0,-1, 0 );
-  const IntVec pos_Y  ( 0, 1, 0 );
-  const IntVec neg_Z  ( 0, 0,-1 );
-  const IntVec pos_Z  ( 0, 0, 1 );
+  if( xconvFluxTag_ == nullTag ){
+    result <<= (*divX_)( -(*interpX_)(*dCoef_) * (*gradX_)(*phi_) );
+  }
+  else{
+    result <<= (*divX_)( -(*interpX_)(*dCoef_) * (*gradX_)(*phi_) + *convFluxX_ );
+  }
 
-  FieldT r = build_field(result, neutral);
+  if( yconvFluxTag_ == nullTag ){
+    result <<= result +  (*divY_)( -(*interpY_)(*dCoef_) * (*gradY_)(*phi_) );
+  }
+  else{
+    result <<= result +  (*divY_)( -(*interpY_)(*dCoef_) * (*gradY_)(*phi_) + *convFluxY_ );
+  }
 
-  const FieldT phi_xminus = build_field( (*phi_), neg_X   );
-  const FieldT phi_x0_1   = build_field( (*phi_), neutral );
-  const FieldT phi_x0_2   = build_field( (*phi_), neutral );
-  const FieldT phi_xplus  = build_field( (*phi_), pos_X   );
-  const FieldT phi_yminus = build_field( (*phi_), neg_Y   );
-  const FieldT phi_y0_1   = build_field( (*phi_), neutral );
-  const FieldT phi_y0_2   = build_field( (*phi_), neutral );
-  const FieldT phi_yplus  = build_field( (*phi_), pos_Y   );
-  const FieldT phi_zminus = build_field( (*phi_), neg_Z   );
-  const FieldT phi_z0_1   = build_field( (*phi_), neutral );
-  const FieldT phi_z0_2   = build_field( (*phi_), neutral );
-  const FieldT phi_zplus  = build_field( (*phi_), pos_Z   );
+  if( zconvFluxTag_ == nullTag ){
+    result <<= result + (*divZ_)( -(*interpZ_)(*dCoef_) * (*gradZ_)(*phi_) );
+  }
+  else{
+    result <<= result + (*divZ_)( -(*interpZ_)(*dCoef_) * (*gradZ_)(*phi_) + *convFluxZ_ );
+  }
 
-//  const FieldT cflux_xm   = build_field( (*convFluxX_), neutral );
-//  const FieldT cflux_xp   = build_field( (*convFluxX_), pos_X   );
-//  const FieldT cflux_ym   = build_field( (*convFluxY_), neutral );
-//  const FieldT cflux_yp   = build_field( (*convFluxY_), pos_Y   );
-//  const FieldT cflux_zm   = build_field( (*convFluxZ_), neutral );
-//  const FieldT cflux_zp   = build_field( (*convFluxZ_), pos_Z   );
-  const double cflux_xm=0, cflux_xp=0, cflux_ym=0, cflux_yp=0, cflux_zm=0, cflux_zp=0;
-
-  const FieldT dCoef_xminus = build_field( (*dCoef_), neg_X   );
-  const FieldT dCoef_x0_1   = build_field( (*dCoef_), neutral );
-  const FieldT dCoef_x0_2   = build_field( (*dCoef_), neutral );
-  const FieldT dCoef_xplus  = build_field( (*dCoef_), pos_X   );
-  const FieldT dCoef_yminus = build_field( (*dCoef_), neg_Y   );
-  const FieldT dCoef_y0_1   = build_field( (*dCoef_), neutral );
-  const FieldT dCoef_y0_2   = build_field( (*dCoef_), neutral );
-  const FieldT dCoef_yplus  = build_field( (*dCoef_), pos_Y   );
-  const FieldT dCoef_zminus = build_field( (*dCoef_), neg_Z   );
-  const FieldT dCoef_z0_1   = build_field( (*dCoef_), neutral );
-  const FieldT dCoef_z0_2   = build_field( (*dCoef_), neutral );
-  const FieldT dCoef_zplus  = build_field( (*dCoef_), pos_Z   );
-
-  // get the stencil coefficients from the operators
-  const double gXl = gradX_  ->get_minus_coef();
-  const double gXh = gradX_  ->get_plus_coef();
-  const double iXl = interpX_->get_minus_coef();
-  const double iXh = interpX_->get_plus_coef();
-  const double dXl = divX_   ->get_minus_coef();
-  const double dXh = divX_   ->get_plus_coef();
-
-  const double gYl = gradY_  ->get_minus_coef();
-  const double gYh = gradY_  ->get_plus_coef();
-  const double iYl = interpY_->get_minus_coef();
-  const double iYh = interpY_->get_plus_coef();
-  const double dYl = divY_   ->get_minus_coef();
-  const double dYh = divY_   ->get_plus_coef();
-
-  const double gZl = gradZ_  ->get_minus_coef();
-  const double gZh = gradZ_  ->get_plus_coef();
-  const double iZl = interpZ_->get_minus_coef();
-  const double iZh = interpZ_->get_plus_coef();
-  const double dZl = divZ_   ->get_minus_coef();
-  const double dZh = divZ_   ->get_plus_coef();
-
-  // build the full RHS including diffusive & reactive terms
-  r <<= (
-      ( /* x-direction convective and diffusive flux contributions - building diffusive flux inline */
-          dXl * ( -cflux_xm + (gXl * phi_xminus + gXh * phi_x0_1 ) * (iXl * dCoef_xminus + iXh * dCoef_x0_1 ) ) +
-          dXh * ( -cflux_xp + (gXl * phi_x0_2   + gXh * phi_xplus) * (iXl * dCoef_x0_2   + iXh * dCoef_xplus) )
-      )
-      +
-      ( /* y-direction convective and diffusive flux contributions - building diffusive flux inline */
-          dYl * ( -cflux_ym + (gYl * phi_yminus + gYh * phi_y0_1 ) * (iYl * dCoef_yminus + iYh * dCoef_y0_1 ) ) +
-          dYh * ( -cflux_yp + (gYl * phi_y0_2   + gYh * phi_yplus) * (iYl * dCoef_y0_2   + iYh * dCoef_yplus) )
-      )
-      +
-      ( /* z-direction convective and diffusive flux contributions - building diffusive flux inline */
-          dZl * ( -cflux_zm + (gZl * phi_zminus + gZh * phi_z0_1 ) * (iZl * dCoef_zminus + iZh * dCoef_z0_1 ) ) +
-          dZh * ( -cflux_zp + (gZl * phi_z0_2   + gZh * phi_zplus) * (iZl * dCoef_z0_2   + iZh * dCoef_zplus) )
-      )
-  );
-
-  if( srcTag_ != Expr::Tag() ) result <<= result + *src_;
+  if( srcTag_ != nullTag ) result <<= result + *src_;
 }
 
 //--------------------------------------------------------------------
@@ -226,18 +159,18 @@ template< typename FieldT >
 MonolithicRHS<FieldT>::
 Builder::Builder( const Expr::Tag& resultTag,
                   const Expr::Tag& dCoefTag,
-//                  const Expr::Tag& xconvFluxTag,
-//                  const Expr::Tag& yconvFluxTag,
-//                  const Expr::Tag& zconvFluxTag,
+                  const Expr::Tag& xconvFluxTag,
+                  const Expr::Tag& yconvFluxTag,
+                  const Expr::Tag& zconvFluxTag,
                   const Expr::Tag& phiTag,
                   const Expr::Tag& srcTag )
   : ExpressionBuilder( resultTag ),
-    dCoefTag_   ( dCoefTag    ),
-    xconvFluxTag_( Expr::Tag() ), //xconvFluxTag ),
-    yconvFluxTag_( Expr::Tag() ), //yconvFluxTag ),
-    zconvFluxTag_( Expr::Tag() ), //zconvFluxTag ),
-    phiTag_     ( phiTag      ),
-    srcTag_     ( srcTag      )
+    dCoefTag_    ( dCoefTag     ),
+    xconvFluxTag_( xconvFluxTag ),
+    yconvFluxTag_( yconvFluxTag ),
+    zconvFluxTag_( zconvFluxTag ),
+    phiTag_      ( phiTag       ),
+    srcTag_      ( srcTag       )
 {}
 
 //--------------------------------------------------------------------
