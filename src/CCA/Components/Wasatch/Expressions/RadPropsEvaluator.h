d3aa061718e608c39cf0f2131cada9e2b440c4f7
James Sutherland <jsutherland@sci.utah.edu>
2013-06-11 21:40:58 +0000

    Particle radiative property evaluation in Wasatch.

--- src/CCA/Components/Wasatch/Expressions/RadPropsEvaluator.h
+++ src/CCA/Components/Wasatch/Expressions/RadPropsEvaluator.h
@@ -4,6 +4,7 @@
 #include <expression/Expression.h>
 
 #include <radprops/AbsCoeffGas.h>
+#include <complex>
 
 typedef std::map<RadiativeSpecies,Expr::Tag> RadSpecMap;
 
@@ -63,6 +64,63 @@ public:
   void evaluate();
 };
 
+
+enum ParticleRadProp{
+  PLANCK_SCATTERING_COEFF,
+  PLANCK_ABSORPTION_COEFF,
+  ROSSELAND_SCATTERING_COEFF,
+  ROSSELAND_ABSORPTION_COEFF,
+};
+
+class ParticleRadCoeffs; // forward declaration
+
+/**
+ *  \class ParticleRadProps
+ */
+template< typename FieldT >
+class ParticleRadProps
+ : public Expr::Expression<FieldT>
+{
+  const ParticleRadCoeffs* const props_;
+  const ParticleRadProp prop_;
+  const Expr::Tag tempTag_, pRadiusTag_;
+  const FieldT* temp_;
+  const FieldT* pRadius_;
+
+  ParticleRadProps( const ParticleRadProp prop,
+                    const Expr::Tag& tempTag,
+                    const Expr::Tag& pRadiusTag,
+                    const std::complex<double> refIndex );
+public:
+  class Builder : public Expr::ExpressionBuilder
+  {
+  public:
+    /**
+     *  @brief Build a ParticleRadProps expression
+     *  @param resultTag the tag for the value that this expression computes
+     *  @param tempTag the tag for the temperature
+     *  @param pRadiusTag the tag for the particle radius
+     *  @param refIndex the refractive index for the particles
+     */
+    Builder( const ParticleRadProp prop,
+             const Expr::Tag& resultTag,
+             const Expr::Tag& tempTag,
+             const Expr::Tag& pRadiusTag,
+             const std::complex<double> refIndex );
+
+    Expr::ExpressionBase* build() const;
+
+  private:
+    const ParticleRadProp prop_;
+    const Expr::Tag tempTag_, pRadiusTag_;
+    const std::complex<double> refIndex_;
+  };
+
+  ~ParticleRadProps();
+  void advertise_dependents( Expr::ExprDeps& exprDeps );
+  void bind_fields( const Expr::FieldManagerList& fml );
+  void evaluate();
+};
 
 
 #endif // RadPropsEvaluator_Expr_h
