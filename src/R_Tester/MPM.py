99db378f92165b216ccd0ff2696dd1c9533f78bd
Jim Guilkey <guilkey@sci.utah.edu>
2013-08-14 21:03:23 +0000

    Fix the updated filename in the RT script.

--- src/R_Tester/MPM.py
+++ src/R_Tester/MPM.py
@@ -47,7 +47,7 @@ NIGHTLYTESTS = [
                   ("adiCuZA4000s696K",                    "adiCuZA4000s696K.ups",                    1,  "Linux", ["exactComparison"] ),  
                   ("test_cyl_pene_no_ero",                "test_cyl_pene_no_ero.ups",                1,  "Linux", ["exactComparison"] ),  
                   ("test_gurson_beckerdrucker_mts",       "test_gurson_beckerdrucker_mts.ups",       1,  "Linux", ["exactComparison"] ),  
-                  ("test_hypoviscoelastic_radial_return", "test_hypoviscoelastic_radial_return.ups", 1,  "Linux", ["exactComparison"] ),  
+                  ("test_hypoviscoelastic_radial_return", "test_hypoviscoelastic_rad_ret.ups", 1,  "Linux", ["exactComparison"] ),  
                   ("advect_3L_3D",                        "advect_3L_3D.ups",                        4,  "Linux", ["exactComparison", "no_restart"] ),  
                   ("advect_2L_3D_slabs",                  "advect_2L_3D_slabs.ups",                  3,  "Linux", [ "no_restart","no_dbg"] ),  
                   ("advect_2L_3D_edges",                  "advect_2L_3D_edges.ups",                  1,  "Linux", ["exactComparison", "no_restart"] ),  
