e9982e44f68c8a469e653a7b9acf2d113c06de57
James Sutherland <jsutherland@sci.utah.edu>
2013-11-02 16:49:21 +0000

    It looks like ComputeSet implements an is_sorted member function.  Switch to use that where applicable.

--- CCA/Components/Schedulers/DetailedTasks.cc
+++ CCA/Components/Schedulers/DetailedTasks.cc
@@ -214,13 +214,12 @@ DetailedTask::DetailedTask(Task* task,
 {
   if (patches) {
     // patches and matls must be sorted
-    ASSERT(std::is_sorted(patches->getVector().begin(), patches->getVector().end(),
-            Patch::Compare()));
+    ASSERT(patches->is_sorted());
     patches->addReference();
   }
   if (matls) {
     // patches and matls must be sorted
-    ASSERT(std::is_sorted(matls->getVector().begin(), matls->getVector().end()));
+    ASSERT(matls->is_sorted());
     matls->addReference();
   }
 #ifdef HAVE_CUDA
