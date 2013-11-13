5c55a39c184e670b540d09918c2b312405e59f4a
Jeremy Nicholas Thornock <jthornoc@sci.utah.edu>
2013-11-13 19:15:37 +0000

    Removing an _new.

--- CCA/Components/Arches/Arches.cc
+++ CCA/Components/Arches/Arches.cc
@@ -1016,7 +1016,7 @@ Arches::scheduleInitialize(const LevelP& level,
   bool initialize_it = true;
   bool modify_ref_den = true;
   d_props->doTableMatching();
-  d_props->sched_reComputeProps_new( level, sched, init_timelabel, initialize_it, modify_ref_den );
+  d_props->sched_computeProps( level, sched, init_timelabel, initialize_it, modify_ref_den );
   
 
   //Setup BC areas
