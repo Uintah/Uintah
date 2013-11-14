4d9debfa618505b4a9310d1939cabe0c0e0669b8
Tony Saad <tsaad@sci.utah.edu>
2013-11-14 17:22:45 +0000

    Enforce density specification for ALL wasatch simulations

--- CCA/Components/Wasatch/Properties.cc
+++ CCA/Components/Wasatch/Properties.cc
@@ -481,6 +481,13 @@ namespace Wasatch{
     // extract the density tag in the cases that it is needed
 
     Uintah::ProblemSpecP densityParams  = params->findBlock("Density");
+    
+    if (!densityParams) {
+      std::ostringstream msg;
+      msg << std::endl << "Error: You must specify a <Density> block. Please revise your input file." << std::endl;
+      throw Uintah::ProblemSetupException( msg.str(), __FILE__, __LINE__ );
+    }
+    
     Uintah::ProblemSpecP tabPropsParams = params->findBlock("TabProps");
     Uintah::ProblemSpecP radPropsParams = params->findBlock("RadProps");
     Uintah::ProblemSpecP twoStreamParams= params->findBlock("TwoStreamMixing");
@@ -492,9 +499,8 @@ namespace Wasatch{
     if( twoStreamParams ){
       parse_twostream_mixing( twoStreamParams, gc );
     }
-    
-    // if no density block was specified, then assume we have constant density...
-    const bool isConstDensity = (densityParams) ? densityParams->findBlock("Constant") : true;
+
+    const bool isConstDensity = densityParams->findBlock("Constant");
     const bool doDenstPlus = !isConstDensity
                            && params->findBlock("MomentumEquations")
                            && params->findBlock("TransportEquation");
