5c144cf75bba3dd4d66e78e79923e2668d6a578d
Alan Humphrey <ahumphrey@sci.utah.edu>
2014-01-03 03:45:44 +0000

    Report particle creation stats via std::cout

--- CCA/Components/MPM/AMRMPM.cc
+++ CCA/Components/MPM/AMRMPM.cc
@@ -1237,7 +1237,7 @@ void AMRMPM::printParticleCount(const ProcessorGroup* pg,
   new_dw->get(pcount, lb->partCountLabel);
   
   if(pg->myrank() == 0){
-    cerr << "Created " << (long) pcount << " total particles\n";
+    std::cout << "Created " << (long) pcount << " total particles" << std::endl;
   }
 }
 //______________________________________________________________________
