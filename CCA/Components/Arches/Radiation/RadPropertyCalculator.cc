0baae8930593320815b27daf326cc6b46dc452a8
Derek Harris <derekhar@sci.utah.edu>
2014-06-25 17:29:25 +0000

    Changed how Arches.cc interfaces with radproperty_Calculator. Changed default name of abskp to abskp_HS. Removed redundant and unneeded line in DORadiation.cc.

--- CCA/Components/Arches/Radiation/RadPropertyCalculator.cc
+++ CCA/Components/Arches/Radiation/RadPropertyCalculator.cc
@@ -206,9 +206,9 @@ bool
 RadPropertyCalculator::HottelSarofim::extraProblemSetup( const ProblemSpecP& db ) {
   ProblemSpecP db_h = db;
   _local_abskp = false; 
-if(db_h->findBlock("DORadiationModel") && db_h->findBlock("DORadiationModel")->findBlock("property_calculator")->findBlock("abskp")){
-//if(db_h->findBlock("RMCRT") && db_h->findBlock("RMCRT")->findBlock("property_calculator")->findBlock("abskp")) // needed in future for rmcrt
-    db_h->findBlock("DORadiationModel")->findBlock("property_calculator")->findBlock("abskp")->getAttribute("label",_abskp_name);
+  if( db_h->getRootNode()->findBlock("CFD")->findBlock("ARCHES")->findBlock("TransportEqns")->findBlock("Sources")->findBlock("src")->findBlock("DORadiationModel")->findBlock("property_calculator")->findBlock("abskp")){
+    //if(db_h->findBlock("RMCRT") && db_h->findBlock("RMCRT")->findBlock("property_calculator")->findBlock("abskp")) // needed in future for rmcrt
+    db_h->getRootNode()->findBlock("CFD")->findBlock("ARCHES")->findBlock("TransportEqns")->findBlock("Sources")->findBlock("src")->findBlock("DORadiationModel")->findBlock("property_calculator")->findBlock("abskp")->getAttribute("label",_abskp_name);  // This long syntax is needed becuase the incorrect variable (db_h) is being passed to this function
     _abskp_label = VarLabel::find(_abskp_name);  
 
     _use_abskp = true; 
@@ -216,7 +216,7 @@ if(db_h->findBlock("DORadiationModel") && db_h->findBlock("DORadiationModel")->f
   } else { 
     _local_abskp = true; 
     _use_abskp = false; 
-    _abskp_name = "abskp";  //derek
+    _abskp_name = "abskp_HS";  
     _abskp_label = VarLabel::create(_abskp_name, CCVariable<double>::getTypeDescription()); 
   }
 
