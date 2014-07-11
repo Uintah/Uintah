3978b98602fe19130e01860c377caa061105fb6c
James Sutherland <jsutherland@sci.utah.edu>
2014-07-11 00:26:55 +0000

    Workaround for a strange compiler bug that showed up on a few gcc compilers. A big thanks to Dav for coming up with a workaround!

--- src/CCA/Components/Wasatch/TaskInterface.cc
+++ src/CCA/Components/Wasatch/TaskInterface.cc
@@ -585,7 +585,8 @@ namespace Wasatch{
                     << endl;
           if( dbg_tasks_on ) fml_->dump_fields(std::cout);
 
-          fml_->allocate_fields( AllocInfo( oldDW, newDW, material, patch, pg, isGPUTask ) );
+          AllocInfo ainfo( oldDW, newDW, material, patch, pg, isGPUTask )
+          fml_->allocate_fields( ainfo );
 
           if( hasPressureExpression_ ){
             Pressure& pexpr = dynamic_cast<Pressure&>( factory.retrieve_expression( pressure_tag(), patchID, true ) );
