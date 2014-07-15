0211a31e0928b8fc6dd5668845986ac729b01082
Tony Saad <tsaad@sci.utah.edu>
2014-07-15 17:07:04 +0000

    add first round of comprehensive particle test coverage to the RT

--- R_Tester/Wasatch.py
+++ R_Tester/Wasatch.py
@@ -128,7 +128,9 @@ DEBUGTESTS = [
 NIGHTLYTESTS = [
 #   ("scalar-transport-equation_perf",    scalarequationperf_ups,   1.1,  "Linux",  ["no_uda_comparison","no_restart","do_performance_test"] ),
 #	("particle-test-driven-cavity-upper-half-IC", "particle-test-driven-cavity-upper-half-IC.ups",   4,  "Linux",   ["exactComparison","no_memoryTest"] ),
-	("particle-test-free-fall-uniformIC", "particle-test-free-fall-uniformIC.ups",   1,  "Linux",   ["exactComparison","no_restart","no_memoryTest"] ),
+	("particle-test-free-fall-xdir", "particle-test-free-fall-xdir.ups",   1,  "Linux",   ["exactComparison","no_restart","no_memoryTest"] ),
+	("particle-test-free-fall-ydir", "particle-test-free-fall-ydir.ups",   1,  "Linux",   ["exactComparison","no_restart","no_memoryTest"] ),
+	("particle-test-free-fall-zdir", "particle-test-free-fall-zdir.ups",   1,  "Linux",   ["exactComparison","no_restart","no_memoryTest"] ),		
   ("turbulent-flow-over-cavity",                         "turbulent-flow-over-cavity.ups",    8,  "Linux",  ["abs_tolerance=1e-8","no_restart","no_memoryTest","no_dbg"]                                          ),
   ("coal-boiler-mini",                         "coal-boiler-mini.ups",    16,  "Linux",  ["exactComparison","no_restart","no_memoryTest"]                                          ),
   ("bc-linear-inlet-channel-flow-test",     "bc-linear-inlet-channel-flow-test.ups",             6,  "Linux",   ["exactComparison","no_restart","no_memoryTest"] ),
@@ -342,8 +344,9 @@ RADIATIONTESTS=[
 ]
 
 PARTICLETESTS=[
-	("particle-test-driven-cavity-upper-half-IC", "particle-test-driven-cavity-upper-half-IC.ups",   4,  "All",   ["exactComparison","no_memoryTest"] ),
-  ("particle-test-free-fall-uniformIC", "particle-test-free-fall-uniformIC.ups",   1,  "All",   ["exactComparison","no_restart","no_memoryTest"] )
+	("particle-test-free-fall-xdir", "particle-test-free-fall-xdir.ups",   1,  "All",   ["exactComparison","no_restart","no_memoryTest"] ),
+	("particle-test-free-fall-ydir", "particle-test-free-fall-ydir.ups",   1,  "All",   ["exactComparison","no_restart","no_memoryTest"] ),
+	("particle-test-free-fall-zdir", "particle-test-free-fall-zdir.ups",   1,  "All",   ["exactComparison","no_restart","no_memoryTest"] )
 ]
 
 #  ("radprops",                      "RadPropsInterface.ups",             2,  "Linux",  ["exactComparison","no_restart","no_memoryTest"] )
