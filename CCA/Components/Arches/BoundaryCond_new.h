45cd88ea0e07cb492412eb061ae981e1c21fa9cd
Jeremy Nicholas Thornock <jthornoc@sci.utah.edu>
2014-09-12 23:57:42 +0000

    Name the boundary if no name is present.

--- CCA/Components/Arches/BoundaryCond_new.h
+++ CCA/Components/Arches/BoundaryCond_new.h
@@ -639,6 +639,20 @@ private:
                      DataWarehouse* new_dw,
                      const std::string eqnName );
 
+  /** @brief Generate a random name with a fixed length **/  
+  void get_random_name(char* s, const int len ){
+        static const char alphanum[] =
+        "0123456789"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz";
+
+    for (int i = 0; i < len; ++i) {
+        s[i] = alphanum[rand() % (sizeof(alphanum) - 1)];
+    }
+
+    s[len] = 0;
+  }
+
 }; // class BoundaryCondition_new
 
 } // namespace Uintah
