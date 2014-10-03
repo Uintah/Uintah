d370cac897b4a6a19ac7b2f27c379d8d5cd49e1d
Sergey Yakovlev <yakovs@sci.utah.edu>
2014-10-03 20:27:02 +0000

    Added MPM AMR source code. Initial commit.
    Former-commit-id: 0e79158ac222026829f63239b054fcd9b7833271

---
+++ amr/mpm/mainwindow.h
@@ -0,0 +1,37 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QGraphicsScene>
+#include <QTimer>
+#include <QListWidget>
+#include "Solver.h"
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+    
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+    
+private slots:
+
+    void on_next_clicked();
+
+    void on_run_toggled(bool checked);
+
+    void step();
+
+private:
+    Solver mpm;
+    Ui::MainWindow *ui;
+    QGraphicsScene scene;
+    QTimer *timer;
+};
+
+#endif // MAINWINDOW_H
