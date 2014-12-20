1e1037eda3803ff98edc60938962facf8d3fa2c9
Jeremy Nicholas Thornock <jthornoc@sci.utah.edu>
2014-12-20 07:42:00 +0000

    Merge conflicts.

--- src/CCA/Components/Arches/ExplicitSolver.cc
+++ src/CCA/Components/Arches/ExplicitSolver.cc
@@ -502,6 +502,36 @@ int ExplicitSolver::nonlinearSolve(const LevelP& level,
         dqmom_eqn->sched_getUnscaledValues( level, sched );
       }
 
+      // Evaluate DQMOM equations
+      for ( DQMOMEqnFactory::EqnMap::iterator iEqn = weights_eqns.begin();
+            iEqn != weights_eqns.end(); iEqn++){
+
+        DQMOMEqn* dqmom_eqn = dynamic_cast<DQMOMEqn*>(iEqn->second);
+
+        dqmom_eqn->sched_updateTransportEqn( level, sched, curr_level );
+      }
+
+      for ( DQMOMEqnFactory::EqnMap::iterator iEqn = abscissas_eqns.begin();
+            iEqn != abscissas_eqns.end(); iEqn++){
+
+        DQMOMEqn* dqmom_eqn = dynamic_cast<DQMOMEqn*>(iEqn->second);
+
+        dqmom_eqn->sched_updateTransportEqn( level, sched, curr_level );
+      }
+
+
+      // ---- schedule the solution of the transport equations ----
+
+
+      for( DQMOMEqnFactory::EqnMap::iterator iEqn = dqmom_eqns.begin();
+           iEqn!=dqmom_eqns.end(); ++iEqn ) {
+
+        DQMOMEqn* dqmom_eqn = dynamic_cast<DQMOMEqn*>(iEqn->second);
+
+        //also get the abscissa values
+        dqmom_eqn->sched_getUnscaledValues( level, sched );
+      }
+
       // calculate the moments
       bool saveMoments = d_dqmomSolver->getSaveMoments();
       if( saveMoments ) {
