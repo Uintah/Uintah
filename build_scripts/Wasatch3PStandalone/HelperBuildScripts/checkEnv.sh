4509c4f7a2420601f87f1f492b91af8374f4fae3
J. Davison de St. Germain <dav@sci.utah.edu>
2015-01-22 00:50:59 +0000

    More cleaning up and testing of the W3P standalone build scripts.

--- build_scripts/Wasatch3PStandalone/HelperBuildScripts/checkEnv.sh
+++ build_scripts/Wasatch3PStandalone/HelperBuildScripts/checkEnv.sh
@@ -4,7 +4,7 @@ host=`hostname`
 
 echo
 if test "$MACHINE" = ""; then
-   echo "Please set the env var MACHINE to:
+   echo "Please set the env var MACHINE to:"
    echo "  At Utah: Ember or Ash"
    echo "  At LLNL: Vulcan or Cab"
    echo "  At LANL: Mustang or Mapache"
@@ -25,13 +25,13 @@ if test "$COMPILER" = ""; then
 fi
 
 if test "$COMPILER" = "icc"; then
-   echo "Building with ICC"
+   echo "  Building with ICC"
    CC=icc
    CXX=icpc
    COMP=icc-15.0.0
 else
    if test "$COMPILER" = "gcc"; then
-      echo "Building with GCC"
+      echo f  "Building with GCC"
       CC=gcc
       CXX=g++
       COMP=gcc-4.4.7
@@ -42,79 +42,93 @@ else
    fi
 fi
 
+TPI=Thirdparty-Install
+
 echo
 if test "$MACHINE" = "Ember"; then
-  echo "Building for Ember"
+  echo "  Building for Ember"
   if [[ $host != ember* ]]; then
      echo "Error: hostname did not return ember... Goodbye."
      exit
   fi
   NAME="ember.arches"
   NAME2="Ember"
-  INSTALL_BASE=/uufs/$NAME/sys/pkg/uintah/Thirdparty-Install/$NAME2/Wasatch3P
+  TPI=thirdparty-install
+  INSTALL_BASE=/uufs/$NAME/sys/pkg/uintah/$TPI/$NAME2/Wasatch3P
   BOOST_LOC=/uufs/$NAME/sys/pkg/boost/1.54.0_mvapich2-1.9
 else
 if test "$MACHINE" = "Ash"; then
 
+  echo "Building for Ash"
   if test "$PHOENIX" = ""; then
-    echo "Please set the env var PHOENIX to yes or no!"
+    echo
+    echo "ERROR: Please set the env var PHOENIX to yes or no!"
     echo
     exit
   fi
 
   if test ! "$PHOENIX" = "yes" -a ! "$PHOENIX" = "no" ; then
-    echo "Please set the env var PHOENIX to 'yes' or 'no' - it is currently '$PHOENIX'!"
+    echo 
+    echo "ERROR: Please set the env var PHOENIX to 'yes' or 'no' - it is currently '$PHOENIX'!"
     echo
     exit
   fi
 
   if test "$PHOENIX" = "yes"; then
-    echo "  Building for Phoenix"
+    echo "    Building for Phoenix"
     echo
     PHOENIXEXT="-phoenix"
   else
     PHOENIXEXT=""
   fi
 
-
-  echo "Building for Ash"
   if [[ $host != ash* ]]; then
-     echo "Error: hostname did not return ash... Goodbye."
+     echo
+     echo "ERROR: hostname did not return ash... Goodbye."
      exit
   fi
   NAME="ash.peaks"
   NAME2="Ash"
-  INSTALL_BASE=/uufs/$NAME/sys/pkg/uintah/Thirdparty-Install$PHOENIXEXT/$NAME2/Wasatch3P
-  BOOST_LOC=/uufs/$NAME/sys/pkg/boost/1.54.0_mvapich2-1.9
-fi
+  TPI=thirdparty-install
+  INSTALL_BASE=/uufs/$NAME/sys/pkg/uintah/$TPI$PHOENIXEXT/$NAME2/Wasatch3P
+  BOOSTLOC=/uufs/$NAME/sys/pkg/boost/1.54.0_mvapich2-1.9
 else
   echo ""
   echo "$MACHINE not supported yet... add it."
   echo ""
   exit
 fi
+fi
 
-$INSTALL_BASE=$INSTALL_BASE-$DATE
-
-############################################################################
+###########################################################################
 #
 # Some bullet proofing...
+#
 
-if test "$BOOST_LOC" = "" -o ! -d "$BOOST_LOC"; then
-   echo "Env var BOOST_LOC ($BOOST_LOC) not set or is not a valid directory...":
+if test "$BOOSTLOC" = ""; then
+   echo "ERROR: Env var BOOST_LOC \($BOOSTLOC\) not set or is not a valid directory..."
    echo "Exiting..."
    echo
    exit
 fi
 
 if test ! -d "$INSTALL_BASE"; then
-   echo "INSTALL_BASE $(INSTALL_BASE) is not a valid directory...":
+   echo "ERROR: INSTALL_BASE ($INSTALL_BASE) is not a valid directory..."
    echo "Exiting..."
    echo
    exit
 fi
 
 ############################################################################
+
+# Add date to complete installation dir variable:
+
+echo "  CC:   $CC"
+echo "  CC:   $CXX"
+echo "  COMP: $COMP"
+
+
+INSTALL_BASE=$INSTALL_BASE-$DATE
 
 echo 
 
--- build_scripts/Wasatch3PStandalone/HelperBuildScripts/setupEnv.sh
+++ build_scripts/Wasatch3PStandalone/HelperBuildScripts/setupEnv.sh
@@ -1,2 +1,2 @@
 
-INSTALL_HERE=/uufs/$NAME/sys/pkg/uintah/thirdparty-install$PHOENIXEXT/$NAME2/Wasatch3P/$THELIB/$COMP
+INSTALL_HERE=$INSTALL_BASE/$THELIB/$COMP
