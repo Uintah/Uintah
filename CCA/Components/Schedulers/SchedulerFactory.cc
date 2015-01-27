1145da7c22eabacfafd19b7847f6137e01078900
Alan Humphrey <ahumphrey@sci.utah.edu>
2015-01-27 21:41:43 +0000

    Fix behavior of sus runs started without mpirun.
    * sus -nthreads 2 -gpu input.ups (Unified scheduler with GPU support)
    * sus -nthreads 2 input.ups (ThreadedMPI scheduler)
    * sus  input.ups (SingleProcessor scheduler)

--- CCA/Components/Schedulers/SchedulerFactory.cc
+++ CCA/Components/Schedulers/SchedulerFactory.cc
@@ -49,9 +49,9 @@ static DebugStream dynamicMPI(     "DynamicMPIScheduler",      false);
 static DebugStream unified(        "UnifiedScheduler",         false);
 
 SchedulerCommon*
-SchedulerFactory::create(const ProblemSpecP&   ps,
-                         const ProcessorGroup* world,
-                         const Output*         output)
+SchedulerFactory::create( const ProblemSpecP   &   ps,
+                          const ProcessorGroup * world,
+                          const Output         *         output )
 {
   SchedulerCommon* sch = 0;
   std::string scheduler = "";
@@ -61,9 +61,13 @@ SchedulerFactory::create(const ProblemSpecP&   ps,
     sc_ps->getAttribute("type", scheduler);
   }
 
-  // Default settings
+  // Default settings - nothing specified in the input file
   if (scheduler == "") {
+
+    // Using MPI
     if (Uintah::Parallel::usingMPI()) {
+
+      // Using MPI without threads
       if (!(Uintah::Parallel::getNumThreads() > 0)) {
         if (singleProcessor.active()) {
           throw ProblemSetupException("Cannot use Single Processor Scheduler with MPI.", __FILE__, __LINE__);
@@ -75,6 +79,8 @@ SchedulerFactory::create(const ProblemSpecP&   ps,
           scheduler = "MPI";
         }
       }
+
+      // Using MPI and threads (Unified and ThreadedMPI schedulers)
       else if (Uintah::Parallel::usingDevice() || unified.active()) {
         scheduler = "Unified";
       }
@@ -82,11 +88,30 @@ SchedulerFactory::create(const ProblemSpecP&   ps,
         scheduler = "ThreadedMPI";
       }
     }
-    else {
-      if (dynamicMPI.active() || unified.active()) {
-        std::string message = "Cannot use MPI schedulers without -mpi option. SCI_DEBUG flags: DynamicMPI or Unified may also be active.";
-        throw ProblemSetupException(message, __FILE__, __LINE__);
+
+    // No MPI
+    else if (Uintah::Parallel::getNumThreads() > 0) {
+
+      // Threads + GPU
+      if (Uintah::Parallel::usingDevice()) {
+        scheduler = "Unified";
       }
+
+      // Threads
+      else {
+        scheduler = "ThreadedMPI";
+      }
+    }
+
+    // Throw an exception of all the above fails
+    else if (dynamicMPI.active() || unified.active()) {
+      std::string message =
+          "Cannot use MPI schedulers without -mpi option. SCI_DEBUG flags: DynamicMPI or Unified may also be active.";
+      throw ProblemSetupException(message, __FILE__, __LINE__);
+    }
+
+    // Otherwise use the SingleProcessorScheduler
+    else {
       scheduler = "SingleProcessor";
     }
   }
@@ -109,19 +134,18 @@ SchedulerFactory::create(const ProblemSpecP&   ps,
   }
   else {
     sch = 0;
-    std::string error = "Unknown scheduler: '" + scheduler + "' Please check UPS Spec for valid scheduler options (.../src/StandAlone/inputs/UPS_SPEC/ups_spec.xml)'";
+    std::string error = "Unknown scheduler: '" + scheduler
+                        + "' Please check UPS Spec for valid scheduler options (.../src/StandAlone/inputs/UPS_SPEC/ups_spec.xml)'";
     throw ProblemSetupException(error, __FILE__, __LINE__);
   }
 
-  // "-nthreads" at command line, something other than "ThreadedMPI" specified in UPS file
+  // "-nthreads" at command line, something other than "ThreadedMPI" specified in UPS file (w/ -do_not_validate)
   if ((Uintah::Parallel::getNumThreads() > 0) && ((scheduler != "Unified") && (scheduler != "ThreadedMPI"))) {
     throw ProblemSetupException("ThreadedMPI or Unified Scheduler needed for '-nthreads <n>' option", __FILE__, __LINE__);
   }
 
   // Output which scheduler will be used
-  if (world->myrank() == 0) {
-    std::cout << "Scheduler: \t\t" << scheduler << std::endl;
-  }
+  proc0cout << "Scheduler: \t\t" << scheduler << std::endl;
 
   return sch;
 
