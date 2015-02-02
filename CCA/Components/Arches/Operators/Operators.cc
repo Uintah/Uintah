c5ea798b4f58a0a67d35818370c4d8fd42d12648
Jeremy Nicholas Thornock <jthornoc@sci.utah.edu>
2015-02-02 06:19:33 +0000

    Moving the patch operator creation to the scheduling phase. Needed to reproduce some wasatch functionality because the needed methods were private to wasatch. Messy, but seems to work.

--- CCA/Components/Arches/Operators/Operators.cc
+++ CCA/Components/Arches/Operators/Operators.cc
@@ -10,6 +10,7 @@
 #include <CCA/Components/Wasatch/Operators/UpwindInterpolant.h>
 #include <CCA/Components/Wasatch/Operators/FluxLimiterInterpolant.h>
 #include <CCA/Components/Wasatch/ConvectiveInterpolationMethods.h>
+#include <CCA/Components/Wasatch/Wasatch.h>
 
 using namespace Uintah; 
 
@@ -55,15 +56,6 @@ Operators::Operators()
 Operators::~Operators()
 {} 
 
-void 
-Operators::sched_create_patch_operators( const LevelP& level, SchedulerP& sched, const MaterialSet* matls ){ 
-
-  Task* tsk = scinew Task( "Operators::create_patch_operators", this, &Operators::create_patch_operators);
-  
-  sched->addTask( tsk, level->eachPatch(), matls );
-
-}
-
 template<typename FieldT>
 const SCIRun::Point arches_get_low_position(const Uintah::Patch& patch);
 
@@ -101,57 +93,87 @@ const SCIRun::Point arches_get_low_position<SpatialOps::ZVolField>(const Uintah:
 }
 
 void 
-Operators::create_patch_operators( const ProcessorGroup* pg,
-                                   const PatchSubset* patches,
-                                   const MaterialSubset* matls,
-                                   DataWarehouse* old_dw,
-                                   DataWarehouse* new_dw)
-{
-  for (int p = 0; p < patches->size(); p++) {
+Operators::create_patch_operators( const LevelP& level, SchedulerP& sched, const MaterialSet* matls ){ 
 
-    using namespace SpatialOps; 
+  const Uintah::PatchSet* patches = get_patchset( USE_FOR_OPERATORS, level, sched );
+  for( int ipss=0; ipss<patches->size(); ++ipss ){
+    const Uintah::PatchSubset* pss = patches->getSubset(ipss);
+    for( int ip=0; ip<pss->size(); ++ip ){
+      SpatialOps::OperatorDatabase* const opdb = scinew SpatialOps::OperatorDatabase();
+      const Uintah::Patch* const patch = pss->get(ip);
 
-    const Patch* patch = patches->get(p);
+      IntVector low = patch->getExtraCellLowIndex(); 
+      IntVector high = patch->getExtraCellHighIndex(); 
 
-    IntVector low = patch->getExtraCellLowIndex(); 
-    IntVector high = patch->getExtraCellHighIndex(); 
+      const SCIRun::IntVector udim = patch->getCellHighIndex() - patch->getCellLowIndex();
 
-    const SCIRun::IntVector udim = patch->getCellHighIndex() - patch->getCellLowIndex();
+      std::vector<int> dim(3,1);
+      for( size_t i=0; i<3; ++i ){ dim[i] = udim[i];}
 
-    std::vector<int> dim(3,1);
-    for( size_t i=0; i<3; ++i ){ dim[i] = udim[i];}
+      std::vector<bool> bcPlus(3,false);
+      bcPlus[0] = patch->getBCType(Uintah::Patch::xplus) != Uintah::Patch::Neighbor;
+      bcPlus[1] = patch->getBCType(Uintah::Patch::yplus) != Uintah::Patch::Neighbor;
+      bcPlus[2] = patch->getBCType(Uintah::Patch::zplus) != Uintah::Patch::Neighbor;
+      
+      // check if there are any physical boundaries present on the minus side of the patch
+      std::vector<bool> bcMinus(3,false);
+      bcMinus[0] = patch->getBCType(Uintah::Patch::xminus) != Uintah::Patch::Neighbor;
+      bcMinus[1] = patch->getBCType(Uintah::Patch::yminus) != Uintah::Patch::Neighbor;
+      bcMinus[2] = patch->getBCType(Uintah::Patch::zminus) != Uintah::Patch::Neighbor;
 
-    std::vector<bool> bcPlus(3,false);
-    bcPlus[0] = patch->getBCType(Uintah::Patch::xplus) != Uintah::Patch::Neighbor;
-    bcPlus[1] = patch->getBCType(Uintah::Patch::yplus) != Uintah::Patch::Neighbor;
-    bcPlus[2] = patch->getBCType(Uintah::Patch::zplus) != Uintah::Patch::Neighbor;
-    
-    // check if there are any physical boundaries present on the minus side of the patch
-    std::vector<bool> bcMinus(3,false);
-    bcMinus[0] = patch->getBCType(Uintah::Patch::xminus) != Uintah::Patch::Neighbor;
-    bcMinus[1] = patch->getBCType(Uintah::Patch::yminus) != Uintah::Patch::Neighbor;
-    bcMinus[2] = patch->getBCType(Uintah::Patch::zminus) != Uintah::Patch::Neighbor;
+      IntVector size = high - low; 
 
-    IntVector size = high - low; 
+      Vector Dx = patch->dCell(); 
+      Vector L(size[0]*Dx.x(),size[1]*Dx.y(),size[2]*Dx.z());
 
-    Vector Dx = patch->dCell(); 
-    Vector L(size[0]*Dx.x(),size[1]*Dx.y(),size[2]*Dx.z());
+      Operators::PatchInfo pi;
 
-    Operators::PatchInfo pi;
+      int pid = patch->getID(); 
 
-    int pid = patch->getID(); 
+      BUILD_UPWIND(SpatialOps::SVolField); 
+      BUILD_UPWIND_LIMITER(SpatialOps::SVolField);
+      BUILD_PARTICLE_OPS(SpatialOps::XVolField); 
+      BUILD_PARTICLE_OPS(SpatialOps::YVolField); 
+      BUILD_PARTICLE_OPS(SpatialOps::ZVolField); 
 
-    BUILD_UPWIND(SpatialOps::SVolField); 
-    BUILD_UPWIND_LIMITER(SpatialOps::SVolField);
-    BUILD_PARTICLE_OPS(SpatialOps::XVolField); 
-    BUILD_PARTICLE_OPS(SpatialOps::YVolField); 
-    BUILD_PARTICLE_OPS(SpatialOps::ZVolField); 
+      SpatialOps::build_stencils( size[0], size[1], size[2],
+                                  L[0],    L[1],    L[2],
+                                  pi._sodb );
 
-    SpatialOps::build_stencils( size[0], size[1], size[2],
-                                L[0],    L[1],    L[2],
-                                pi._sodb );
+      this->patch_info_map.insert(std::make_pair(pid, pi)); 
 
-    this->patch_info_map.insert(std::make_pair(pid, pi)); 
-
+    }
   }
 }
+
+
+const Uintah::PatchSet*
+Operators::get_patchset( const PatchsetSelector pss,
+                       const Uintah::LevelP& level,
+                       Uintah::SchedulerP& sched )
+{
+  switch ( pss ) {
+
+    case USE_FOR_TASKS:
+      // return sched->getLoadBalancer()->getPerProcessorPatchSet(level);
+      return level->eachPatch();
+      break;
+
+    case USE_FOR_OPERATORS: {
+      const int levelID = level->getID();
+      const Uintah::PatchSet* const allPatches = sched->getLoadBalancer()->getPerProcessorPatchSet(level);
+      const Uintah::PatchSubset* const localPatches = allPatches->getSubset( _myworld->myrank() );
+
+      //std::map< int, const Uintah::PatchSet* >::iterator ip = patchesForOperators_.find( levelID );
+
+      //if( ip != patchesForOperators_.end() ) return ip->second;
+
+      Uintah::PatchSet* patches = new Uintah::PatchSet;
+      patches->addEach( localPatches->getVector() );
+      //patchesForOperators_[levelID] = patches;
+      return patches;
+    }
+  }
+  return NULL;
+}
+
