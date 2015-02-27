f5e7723ba18b9139110a7bed15b3a3f3e55878fb
Alan Humphrey <ahumphrey@sci.utah.edu>
2015-02-27 22:28:05 +0000

    * Fix multithreaded proc0cout problems. We now get expected (full) output from Thread 0.
    * Fix MPI_Testsome issue in ThreadedMPI scheduler
            -> "Fatal error in MPI_Testsome: Invalid MPI_Request ... Supplied request in array element 0 was invalid."
            
    * Add in some multi-threaded, multi-process nightly tests. These should catch things like PackBuffer leaks, etc.

--- src/CCA/Components/Schedulers/ThreadedMPIScheduler.h
+++ src/CCA/Components/Schedulers/ThreadedMPIScheduler.h
@@ -67,7 +67,7 @@ class TaskWorker;
 
  WARNING
    Tasks must be thread-safe when using this Scheduler.
-   Requires MPI THREAD MULTIPLE support.
+   Requires MPI_THREAD_MULTIPLE support.
 
  ****************************************/
 
@@ -87,8 +87,6 @@ class ThreadedMPIScheduler : public MPIScheduler {
 
     virtual bool useInternalDeps() { return !d_sharedState->isCopyDataTimestep(); }
 
-            void assignTask( DetailedTask* task, int iteration );
-
     ConditionVariable d_nextsignal;
     Mutex             d_nextmutex;             // conditional wait mutex
     TaskWorker*       t_worker[MAX_THREADS];   // workers
@@ -100,11 +98,12 @@ class ThreadedMPIScheduler : public MPIScheduler {
     ThreadedMPIScheduler( const ThreadedMPIScheduler& );
     ThreadedMPIScheduler& operator=( const ThreadedMPIScheduler& );
 
-    Output*    oport_t;
-    QueueAlg   taskQueueAlg_;
-    int        numThreads_;
+    void assignTask( DetailedTask* task, int iteration );
 
     int getAviableThreadNum();
+
+    QueueAlg   taskQueueAlg_;
+    int        numThreads_;
 };
 
 
@@ -113,10 +112,6 @@ class TaskWorker : public Runnable {
   public:
 
     TaskWorker( ThreadedMPIScheduler* scheduler, int thread_id ) ;
-
-    void assignTask( DetailedTask* task, int iteration );
-
-    DetailedTask* getTask();
 
     void run();
 
