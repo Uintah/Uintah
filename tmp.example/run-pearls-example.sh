62ae1d22df7e4863ba4bb81d5f7478c7ae16bc0b
John Holmen <jholmen@sci.utah.edu>
2015-03-14 16:23:38 +0000

    * Cleanup

--- tmp.example/run-pearls-example.sh
+++ tmp.example/run-pearls-example.sh
@@ -1,19 +1,18 @@
 echo
-echo "-------------------------------------------------------------------------------"
-echo " Starting a simulation with the fastest run configuration for 25 sampling rays"
-echo "-------------------------------------------------------------------------------"
+echo "---------------------------------------------"
+echo " Starting a simulation with 25 rays per cell"
+echo "---------------------------------------------"
 echo
 
 mpirun -genv SCI_DEBUG="ComponentTimings:+,ExecTimes:+,ThreadedMPI_CompactAffinity:-" -genv I_MPI_MIC=enable -np 1 ./sus.mic-example -mpi -nthreads 242 25r480p.ups | tee 25-ray-results.txt
-
 
 mv exectimes.1.0 exectimes.25-ray-results
 date
 
 echo
-echo "-------------------------------------------------------------------------------"
-echo " Starting a simulation with the fastest run configuration for 50 sampling rays"
-echo "-------------------------------------------------------------------------------"
+echo "---------------------------------------------"
+echo " Starting a simulation with 50 rays per cell"
+echo "---------------------------------------------"
 echo
 
 mpirun -genv SCI_DEBUG="ComponentTimings:+,ExecTimes:+,ThreadedMPI_CompactAffinity:-" -genv I_MPI_MIC=enable -np 1 ./sus.mic-example -mpi -nthreads 242 50r480p.ups | tee 50-ray-results.txt
@@ -22,9 +21,9 @@ mv exectimes.1.0 exectimes.50-ray-results
 date
 
 echo
-echo "-------------------------------------------------------------------------------"
-echo " Starting a simulation with the fastest run configuration for 100 sampling rays"
-echo "-------------------------------------------------------------------------------"
+echo "---------------------------------------------"
+echo " Starting a simulation with 100 rays per cell"
+echo "---------------------------------------------"
 echo
 
 mpirun -genv SCI_DEBUG="ComponentTimings:+,ExecTimes:+,ThreadedMPI_CompactAffinity:-" -genv I_MPI_MIC=enable -np 1 ./sus.mic-example -mpi -nthreads 244 100r486p.ups | tee 100-ray-results.txt
