2138767ccbd3d56452a31d0686ea811260e38a52
Jeremy Nicholas Thornock <jthornoc@sci.utah.edu>
2015-03-24 21:32:10 +0000

    Changed max Temp in WallHT to be (q/sigma)^0.25.

--- src/CCA/Components/Arches/WallHTModels/WallModelDriver.cc
+++ src/CCA/Components/Arches/WallHTModels/WallModelDriver.cc
@@ -561,7 +561,7 @@ WallModelDriver::RegionHT::computeHT( const Patch* patch, const HTVariables& var
                 total_area_face+=area_face[container_flux_ind[pp]];// this is the total cell surface area exposed to radiation.
               }
               rad_q/=total_area_face; // representative radiative flux to the cell. 
-
+              double T_max;
               double d_tol = 1e-15;
               double delta = 1;
               int NIter = 15;
@@ -569,6 +569,7 @@ WallModelDriver::RegionHT::computeHT( const Patch* patch, const HTVariables& var
               double f1 = 0.0;
               double TW = vars.T_old[c];
               double TW_guess, TW_tmp, TW_old, TW_new;
+              T_max=pow( rad_q/_sigma_constant, 0.25); // if k=0.0
               TW_guess = vars.T_old[c];
               TW_old = TW_guess-delta;
               net_q = rad_q - _sigma_constant * pow( TW_old, 4 );
@@ -586,7 +587,7 @@ WallModelDriver::RegionHT::computeHT( const Patch* patch, const HTVariables& var
                 TW_tmp = TW_old;
                 TW_old = TW_new;
                 TW_new = TW_tmp - (TW_new - TW_tmp)/(f1-f0) * f0;
-                TW_new = max( wi.T_inner , min(wi.max_TW, TW_new));            
+                TW_new = max( wi.T_inner , min( T_max, TW_new ) );            
                 if (std::abs(TW_new-TW_old) < d_tol){
                   TW=TW_new;
                   net_q = rad_q - _sigma_constant * pow( TW_new, 4 );
@@ -664,121 +665,3 @@ WallModelDriver::RegionHT::copySolution( const Patch* patch, CCVariable<double>&
   } 
 }
 
-// removing for the time being
-////_________________________________________
-//void 
-//WallModelDriver::sched_doWallHT_alltoall( const LevelP& level, SchedulerP& sched, const int time_subset )
-//{
-//
-//  Task* task = scinew Task( "WallModelDriver::doWallHT_alltoall", this, 
-//                           &WallModelDriver::doWallHT_alltoall, time_subset ); 
-//
-//  _T_label        = VarLabel::find( _T_label_name );
-//  _cellType_label = VarLabel::find( "cellType" );
-//  _HF_E_label     = VarLabel::find( "radiationFluxE" );
-//  _HF_W_label     = VarLabel::find( "radiationFluxW" );
-//  _HF_N_label     = VarLabel::find( "radiationFluxN" );
-//  _HF_S_label     = VarLabel::find( "radiationFluxS" );
-//  _HF_T_label     = VarLabel::find( "radiationFluxT" );
-//  _HF_B_label     = VarLabel::find( "radiationFluxB" );
-//
-//  if ( !check_varlabels() ){ 
-//    throw InvalidValue("Error: One of the varlabels for the wall model was not found.", __FILE__, __LINE__);
-//  } 
-//
-//  task->modifies(_T_label);
-//
-//  if ( time_subset == 0 ){ 
-//
-//    task->requires(Task::OldDW , _cellType_label , Ghost::AroundNodes , SHRT_MAX);
-//    task->requires(Task::OldDW , _T_label        , Ghost::AroundNodes , SHRT_MAX);
-//    task->requires(Task::OldDW , _HF_E_label     , Ghost::AroundNodes , SHRT_MAX);
-//    task->requires(Task::OldDW , _HF_W_label     , Ghost::AroundNodes , SHRT_MAX);
-//    task->requires(Task::OldDW , _HF_N_label     , Ghost::AroundNodes , SHRT_MAX);
-//    task->requires(Task::OldDW , _HF_S_label     , Ghost::AroundNodes , SHRT_MAX);
-//    task->requires(Task::OldDW , _HF_T_label     , Ghost::AroundNodes , SHRT_MAX);
-//    task->requires(Task::OldDW , _HF_B_label     , Ghost::AroundNodes , SHRT_MAX);
-//
-//  } else { 
-//
-//    task->requires(Task::NewDW , _cellType_label , Ghost::AroundNodes , SHRT_MAX);
-//    task->requires(Task::NewDW , _T_label        , Ghost::AroundNodes , SHRT_MAX);
-//    task->requires(Task::NewDW , _HF_E_label     , Ghost::AroundNodes , SHRT_MAX);
-//    task->requires(Task::NewDW , _HF_W_label     , Ghost::AroundNodes , SHRT_MAX);
-//    task->requires(Task::NewDW , _HF_N_label     , Ghost::AroundNodes , SHRT_MAX);
-//    task->requires(Task::NewDW , _HF_S_label     , Ghost::AroundNodes , SHRT_MAX);
-//    task->requires(Task::NewDW , _HF_T_label     , Ghost::AroundNodes , SHRT_MAX);
-//    task->requires(Task::NewDW , _HF_B_label     , Ghost::AroundNodes , SHRT_MAX);
-//
-//  } 
-//
-//  vector<const Patch*>my_patches;
-//
-//  my_patches.push_back(level->getPatchFromPoint(Point(0.0,0.0,0.0), false));
-//
-//  PatchSet *my_each_patch = scinew PatchSet();
-//  my_each_patch->addReference();
-//  my_each_patch->addEach(my_patches);
-//  
-//  sched->addTask(task, my_each_patch, _shared_state->allArchesMaterials());
-//  
-//}
-//
-////_________________________________________
-//void 
-//WallModelDriver::doWallHT_alltoall( const ProcessorGroup* my_world,
-//                                    const PatchSubset* patches, 
-//                                    const MaterialSubset* matls, 
-//                                    DataWarehouse* old_dw, 
-//                                    DataWarehouse* new_dw, 
-//                                    const int time_subset )
-//{
-//  const Level* level = getLevel(patches);
-//
-//  // Determine the size of the domain.
-//  IntVector domainLo, domainHi;
-//  IntVector domainLo_EC, domainHi_EC;
-//  
-//  level->findInteriorCellIndexRange(domainLo, domainHi);     // excluding extraCells
-//  level->findCellIndexRange(domainLo_EC, domainHi_EC);       // including extraCells
-//  
-//  CCVariable<double> T; 
-//  constCCVariable<int>    celltype;
-//  constCCVariable<double> const_T;
-//  constCCVariable<double> hf_e;
-//  constCCVariable<double> hf_w;
-//  constCCVariable<double> hf_n;
-//  constCCVariable<double> hf_s;
-//  constCCVariable<double> hf_t;
-//  constCCVariable<double> hf_b;
-//
-//  DataWarehouse* which_dw; 
-//  if ( time_subset == 0 ) { 
-//    which_dw = old_dw; 
-//  } else { 
-//    which_dw = new_dw; 
-//  }
-//
-//  which_dw->getRegion(   celltype , _cellType_label , _matl_index , level , domainLo_EC , domainHi_EC);
-//  which_dw->getRegion(   const_T  , _T_label        , _matl_index , level , domainLo_EC , domainHi_EC);
-//  which_dw->getRegion(   hf_e     , _HF_E_label     , _matl_index , level , domainLo_EC , domainHi_EC);
-//  which_dw->getRegion(   hf_w     , _HF_W_label     , _matl_index , level , domainLo_EC , domainHi_EC);
-//  which_dw->getRegion(   hf_n     , _HF_N_label     , _matl_index , level , domainLo_EC , domainHi_EC);
-//  which_dw->getRegion(   hf_s     , _HF_S_label     , _matl_index , level , domainLo_EC , domainHi_EC);
-//  which_dw->getRegion(   hf_t     , _HF_T_label     , _matl_index , level , domainLo_EC , domainHi_EC);
-//  which_dw->getRegion(   hf_b     , _HF_B_label     , _matl_index , level , domainLo_EC , domainHi_EC);
-//
-//  //patch loop
-//  for (int p=0; p < patches->size(); p++){
-//
-//    const Patch* patch = patches->get(p);
-//
-//    new_dw->getModifiable( T, _T_label, _matl_index, patch ); 
-//
-//    // actually perform the ht calculation 
-//    // pass fluxes, T, const_T
-//    // return new T
-//
-//
-//  }
-//}
