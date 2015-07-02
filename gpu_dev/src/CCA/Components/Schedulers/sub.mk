790d8ea7e687cadac250101b7491920ade152dbd
Tony Saad <tsaad@sci.utah.edu>
2015-07-02 22:20:22 +0000

    fix gpudev branch build.
    Former-commit-id: ab8cfc5e3091077c3d430ae94fb8d05ef7cc2528

--- gpu_dev/src/CCA/Components/Schedulers/sub.mk
+++ gpu_dev/src/CCA/Components/Schedulers/sub.mk
@@ -35,7 +35,6 @@ SRCS += \
         $(SRCDIR)/DynamicMPIScheduler.cc      \
         $(SRCDIR)/GPUGridVariableInfo.cc      \
         $(SRCDIR)/GPUGridVariableGhosts.cc    \
-        $(SRCDIR)/GPUUtilities.cc             \
         $(SRCDIR)/IncorrectAllocation.cc      \
         $(SRCDIR)/MemoryLog.cc                \
         $(SRCDIR)/MessageLog.cc               \
