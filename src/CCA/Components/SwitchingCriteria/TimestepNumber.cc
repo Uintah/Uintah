d777e8883f40a44cf2745faf84707d8df16b36d9
Alan Humphrey <ahumphrey@sci.utah.edu>
2015-08-18 17:09:28 +0000

    * Add initialization for d_switchCriteria * Cosmetic cleanup, formatting * Cleanup dbg output, better error reporting from timestep# switch criteria
    Former-commit-id: 635ec1a44a588ac91f72e4d74fd985aee1d89c77

--- src/CCA/Components/SwitchingCriteria/TimestepNumber.cc
+++ src/CCA/Components/SwitchingCriteria/TimestepNumber.cc
@@ -23,66 +23,73 @@
  */
 
 #include <CCA/Components/SwitchingCriteria/TimestepNumber.h>
-#include <Core/ProblemSpec/ProblemSpec.h>
+#include <CCA/Ports/Scheduler.h>
+
 #include <Core/Grid/DbgOutput.h>
 #include <Core/Grid/Task.h>
 #include <Core/Grid/Variables/VarTypes.h>
-#include <CCA/Ports/Scheduler.h>
 #include <Core/Parallel/Parallel.h>
+#include <Core/ProblemSpec/ProblemSpec.h>
+
 #include <string>
 #include <iostream>
 
-using namespace std;
 
 using namespace Uintah;
+
 static DebugStream dbg("SWITCHER", false);
 
 TimestepNumber::TimestepNumber(ProblemSpecP& ps)
 {
   ps->require("timestep",d_timestep);
-  proc0cout << "Switching criteria: \tTimestep Number: switch components on timestep " << d_timestep << endl;
+  proc0cout << "Switching criteria: \tTimestep Number: switch components on timestep " << d_timestep << std::endl;
 }
 
 TimestepNumber::~TimestepNumber()
 {
 }
 
-void TimestepNumber::problemSetup(const ProblemSpecP& ps, 
-                                  const ProblemSpecP& restart_prob_spec, 
-                                  SimulationStateP& state)
+void TimestepNumber::problemSetup( const ProblemSpecP     & ps,
+                                   const ProblemSpecP     & restart_prob_spec,
+                                         SimulationStateP & state )
 {
   d_sharedState = state;
 }
 
-void TimestepNumber::scheduleSwitchTest(const LevelP& level, SchedulerP& sched)
+void TimestepNumber::scheduleSwitchTest( const LevelP     & level,
+                                               SchedulerP & sched )
 {
-  printSchedule(level,dbg,"Switchinng Criteria:TimestepNumber::scheduleSwitchTest");
-  
+  printSchedule(level, dbg, "Switchinng Criteria:TimestepNumber::scheduleSwitchTest");
+
   Task* t = scinew Task("switchTest", this, &TimestepNumber::switchTest);
 
   t->computes(d_sharedState->get_switch_label());
-  sched->addTask(t, level->eachPatch(),d_sharedState->allMaterials());
+
+  sched->addTask(t, level->eachPatch(), d_sharedState->allMaterials());
 }
 
-void TimestepNumber::switchTest(const ProcessorGroup* group,
-                                const PatchSubset* patches,
-                                const MaterialSubset* matls,
-                                DataWarehouse* old_dw,
-                                DataWarehouse* new_dw)
+void TimestepNumber::switchTest( const ProcessorGroup * group,
+                                 const PatchSubset    * patches,
+                                 const MaterialSubset * matls,
+                                       DataWarehouse  * old_dw,
+                                       DataWarehouse  * new_dw)
 {
-  dbg << "Doing Switch Criteria:TimestepNumber";
+  if (dbg.active()) {
+    dbg << "Doing Switch Criteria:TimestepNumber";
+  }
+
   double sw = 0;
 
   unsigned int time_step = d_sharedState->getCurrentTopLevelTimeStep();
-  if (time_step == d_timestep)
-    sw = 1;
-  else
-    sw = 0;
-  
-  dbg  << " is it time to switch components: " << sw << endl;
 
+  if (dbg.active()) {
+    std::string answer = (time_step == d_timestep) ? "yes" : "no";
+    dbg << " is it time to switch components: " << answer << std::endl;
+  }
+
+  sw = (time_step == d_timestep) ? 1 : 0;
   max_vartype switch_condition(sw);
-  
+
   const Level* allLevels = 0;
-  new_dw->put(switch_condition,d_sharedState->get_switch_label(),allLevels);
+  new_dw->put(switch_condition, d_sharedState->get_switch_label(), allLevels);
 }
