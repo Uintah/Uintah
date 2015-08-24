d072f3a1cb43a191ac07199fe3c4ba0c5c69a9c7
Todd Harman <harman@sci.utah.edu>
2015-08-24 23:23:22 +0000

    Formatting:   - alignment   - added vertical and white space.

--- CCA/Components/LoadBalancers/CostModeler.h
+++ CCA/Components/LoadBalancers/CostModeler.h
@@ -73,29 +73,57 @@ namespace Uintah {
 
   class CostModeler : public CostForecasterBase {
     public:
-      void getWeights(const Grid* grid, std::vector<std::vector<int> > num_particles, std::vector<std::vector<double> >&costs)
+      void getWeights(const Grid* grid, 
+                      std::vector<std::vector<int> > num_particles, 
+                      std::vector<std::vector<double> >&costs)
       {
         costs.resize(grid->numLevels());
         //for each level
-        for (int l=0; l<grid->numLevels();l++)
-        {
+        
+        for (int l=0; l<grid->numLevels();l++){
           LevelP level=grid->getLevel(l);
           costs[l].resize(level->numPatches());
-          for(int p=0; p<level->numPatches();p++)
-          {
+          
+          for(int p=0; p<level->numPatches();p++){
             const Patch *patch=level->getPatch(p);
-            costs[l][p]=d_patchCost+patch->getNumCells()*d_cellCost+(patch->getNumExtraCells()-patch->getNumCells())*d_extraCellCost+num_particles[l][p]*d_particleCost;
+            
+            costs[l][p] = d_patchCost 
+                        + patch->getNumCells() * d_cellCost 
+                        + (patch->getNumExtraCells()-patch->getNumCells()) * d_extraCellCost
+                        + num_particles[l][p] * d_particleCost;
           }
         }
       }
-      void setCosts(double patchCost, double cellCost, double extraCellCost, double particleCost)
+      //______________________________________________________________________
+      //
+      void setCosts(double patchCost, 
+                    double cellCost, 
+                    double extraCellCost, 
+                    double particleCost)
       {
-        d_patchCost=patchCost;d_cellCost=cellCost;d_extraCellCost=extraCellCost;d_particleCost=particleCost;
+        d_patchCost = patchCost;
+        d_cellCost  = cellCost;
+        d_extraCellCost = extraCellCost;
+        d_particleCost  = particleCost;
       }
-
-      CostModeler(double patchCost, double cellCost, double extraCellCost, double particleCost) { setCosts(patchCost,cellCost,extraCellCost,particleCost); };
+      
+      //______________________________________________________________________
+      //
+      CostModeler(double patchCost, 
+                  double cellCost, 
+                  double extraCellCost, 
+                  double particleCost) 
+      { 
+        setCosts(patchCost,cellCost,extraCellCost,particleCost); 
+      };
+      
+    //______________________________________________________________________
+    //  
     protected:
-      double d_patchCost, d_cellCost, d_extraCellCost, d_particleCost;
+      double d_patchCost;
+      double d_cellCost;
+      double d_extraCellCost;
+      double d_particleCost;
 
   };
 } // End namespace Uintah
