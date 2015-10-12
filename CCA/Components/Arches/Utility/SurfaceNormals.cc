67f486eac3bdad425a7ece6f42f88b98a811b4dd
Jeremy Nicholas Thornock <jthornoc@sci.utah.edu>
2015-10-12 16:30:29 +0000

    Undo the restartInitialize commit. This is breaking RT. Add a surface tracer in the normal calc. Save out some new variables in the heptane_pipe.

--- CCA/Components/Arches/Utility/SurfaceNormals.cc
+++ CCA/Components/Arches/Utility/SurfaceNormals.cc
@@ -5,22 +5,34 @@
 
 using namespace Uintah;
 
+using namespace SpatialOps;
+using SpatialOps::operator *;
 //field types
-typedef SpatialOps::SSurfXField SurfX;
-typedef SpatialOps::SSurfYField SurfY;
-typedef SpatialOps::SSurfZField SurfZ;
-typedef SpatialOps::SVolField   SVolF;
-typedef SpatialOps::XVolField   XVolF;
-typedef SpatialOps::YVolField   YVolF;
-typedef SpatialOps::ZVolField   ZVolF;
-typedef SpatialOps::SpatFldPtr<SVolF> SVolFP;
-typedef SpatialOps::SpatFldPtr<XVolF> XVolFP;
-typedef SpatialOps::SpatFldPtr<YVolF> YVolFP;
-typedef SpatialOps::SpatFldPtr<ZVolF> ZVolFP;
+typedef SSurfXField SurfX;
+typedef SSurfYField SurfY;
+typedef SSurfZField SurfZ;
+typedef SVolField   SVolF;
+typedef XVolField   XVolF;
+typedef YVolField   YVolF;
+typedef ZVolField   ZVolF;
+typedef SpatFldPtr<SVolF> SVolFP;
+typedef SpatFldPtr<XVolF> XVolFP;
+typedef SpatFldPtr<YVolF> YVolFP;
+typedef SpatFldPtr<ZVolF> ZVolFP;
 //operators
-typedef SpatialOps::OperatorTypeBuilder< SpatialOps::Gradient, SVolF, SpatialOps::XVolField >::type GradX;
-typedef SpatialOps::OperatorTypeBuilder< SpatialOps::Gradient, SVolF, SpatialOps::YVolField >::type GradY;
-typedef SpatialOps::OperatorTypeBuilder< SpatialOps::Gradient, SVolF, SpatialOps::ZVolField >::type GradZ;
+typedef OperatorTypeBuilder< SpatialOps::Gradient, SVolF, SpatialOps::XVolField >::type GradX;
+typedef OperatorTypeBuilder< SpatialOps::Gradient, SVolF, SpatialOps::YVolField >::type GradY;
+typedef OperatorTypeBuilder< SpatialOps::Gradient, SVolF, SpatialOps::ZVolField >::type GradZ;
+
+typedef BasicOpTypes<SVolF>::DivX DivX;
+typedef BasicOpTypes<SVolF>::DivY DivY;
+typedef BasicOpTypes<SVolF>::DivZ DivZ;
+
+typedef OperatorTypeBuilder< SpatialOps::Interpolant, XVolF, SVolF >::type InterpTX;
+typedef OperatorTypeBuilder< SpatialOps::Interpolant, YVolF, SVolF >::type InterpTY;
+typedef OperatorTypeBuilder< SpatialOps::Interpolant, ZVolF, SVolF >::type InterpTZ;
+
+
 //helper
 typedef std::vector<ArchesFieldContainer::VariableInformation> VIVec;
 
@@ -57,6 +69,10 @@ SurfaceNormals::create_local_labels(){
   register_new_variable<SFCYVariable<double> >( "surf_in_normY" );
   register_new_variable<SFCZVariable<double> >( "surf_in_normZ" );
 
+  //to trace out the solid/gas interface
+  // This marks the first solid cell
+  register_new_variable<CCVariable<double> >("gas_solid_interface");
+
 }
 
 //--------------------------------------------------------------------------------------------------
@@ -70,6 +86,8 @@ SurfaceNormals::register_initialize( VIVec& variable_registry ){
   register_variable( "surf_in_normX" , ArchesFieldContainer::COMPUTES , variable_registry );
   register_variable( "surf_in_normY" , ArchesFieldContainer::COMPUTES , variable_registry );
   register_variable( "surf_in_normZ" , ArchesFieldContainer::COMPUTES , variable_registry );
+
+  register_variable( "gas_solid_interface" , ArchesFieldContainer::COMPUTES , variable_registry );
 
   register_variable( "volFraction", ArchesFieldContainer::REQUIRES, 1, ArchesFieldContainer::NEWDW,
                       variable_registry );
@@ -93,9 +111,21 @@ SurfaceNormals::initialize( const Patch* patch, ArchesTaskInfoManager* tsk_info,
   YVolFP n_out_y = tsk_info->get_so_field<YVolF>("surf_out_normY");
   ZVolFP n_out_z = tsk_info->get_so_field<ZVolF>("surf_out_normZ");
 
+  SpatialOps::SpatFldPtr<SVolF> ccnormal = SpatialFieldStore::get<SVolF>(*vol_fraction);
+
+  SVolFP gas_solid_interface = tsk_info->get_so_field<SVolF>("gas_solid_interface");
+
   const GradX* const gradx = opr.retrieve_operator<GradX>();
   const GradY* const grady = opr.retrieve_operator<GradY>();
   const GradZ* const gradz = opr.retrieve_operator<GradZ>();
+
+  const DivX* const divx = opr.retrieve_operator<DivX>();
+  const DivY* const divy = opr.retrieve_operator<DivY>();
+  const DivZ* const divz = opr.retrieve_operator<DivZ>();
+
+  const InterpTX* const interpx = opr.retrieve_operator<InterpTX>();
+  const InterpTY* const interpy = opr.retrieve_operator<InterpTY>();
+  const InterpTZ* const interpz = opr.retrieve_operator<InterpTZ>();
 
   double noise  = 1e-10;
 
@@ -110,24 +140,33 @@ SurfaceNormals::initialize( const Patch* patch, ArchesTaskInfoManager* tsk_info,
   *n_in_z <<= ( *gradz )( 1. - *vol_fraction ) /
     SpatialOps::abs( (*gradz)( 1. - *vol_fraction )+noise);
 
+  *ccnormal <<= ((*interpx)(abs(*n_in_x)))
+                  + ((*interpy)(abs(*n_in_y)))
+                  + ((*interpz)(abs(*n_in_z)));
+
+  *gas_solid_interface <<= 0.0;
+
+  *gas_solid_interface <<= cond( *ccnormal != 0 && *vol_fraction < .5, 1. )
+                                ( 0. );
+
 }
 
 //--------------------------------------------------------------------------------------------------
 void
 SurfaceNormals::register_timestep_init( VIVec& variable_registry ){
 
-  register_variable( "surf_out_normX" , ArchesFieldContainer::REQUIRES , 1,
+  register_variable( "surf_out_normX" , ArchesFieldContainer::REQUIRES , 0,
                       ArchesFieldContainer::OLDDW, variable_registry );
-  register_variable( "surf_out_normY" , ArchesFieldContainer::REQUIRES , 1,
+  register_variable( "surf_out_normY" , ArchesFieldContainer::REQUIRES , 0,
                       ArchesFieldContainer::OLDDW, variable_registry );
-  register_variable( "surf_out_normZ" , ArchesFieldContainer::REQUIRES , 1,
+  register_variable( "surf_out_normZ" , ArchesFieldContainer::REQUIRES , 0,
                       ArchesFieldContainer::OLDDW, variable_registry );
 
-  register_variable( "surf_in_normX" , ArchesFieldContainer::REQUIRES, 1,
+  register_variable( "surf_in_normX" , ArchesFieldContainer::REQUIRES, 0,
                      ArchesFieldContainer::OLDDW , variable_registry );
-  register_variable( "surf_in_normY" , ArchesFieldContainer::REQUIRES, 1,
+  register_variable( "surf_in_normY" , ArchesFieldContainer::REQUIRES, 0,
                      ArchesFieldContainer::OLDDW , variable_registry );
-  register_variable( "surf_in_normZ" , ArchesFieldContainer::REQUIRES, 1,
+  register_variable( "surf_in_normZ" , ArchesFieldContainer::REQUIRES, 0,
                      ArchesFieldContainer::OLDDW , variable_registry );
 
   register_variable( "surf_out_normX" , ArchesFieldContainer::COMPUTES , variable_registry );
@@ -137,6 +176,10 @@ SurfaceNormals::register_timestep_init( VIVec& variable_registry ){
   register_variable( "surf_in_normX" , ArchesFieldContainer::COMPUTES , variable_registry );
   register_variable( "surf_in_normY" , ArchesFieldContainer::COMPUTES , variable_registry );
   register_variable( "surf_in_normZ" , ArchesFieldContainer::COMPUTES , variable_registry );
+
+  register_variable( "gas_solid_interface" , ArchesFieldContainer::COMPUTES, variable_registry );
+  register_variable( "gas_solid_interface" , ArchesFieldContainer::REQUIRES, 0,
+                     ArchesFieldContainer::OLDDW, variable_registry );
 }
 
 void
@@ -162,6 +205,9 @@ SurfaceNormals::timestep_init( const Patch* patch, ArchesTaskInfoManager* tsk_in
   YVolFP old_n_out_y = tsk_info->get_const_so_field<YVolF>("surf_out_normY");
   ZVolFP old_n_out_z = tsk_info->get_const_so_field<ZVolF>("surf_out_normZ");
 
+  SVolFP old_surf = tsk_info->get_const_so_field<SVolF>("gas_solid_interface");
+  SVolFP new_surf = tsk_info->get_so_field<SVolF>("gas_solid_interface");
+
   *n_in_x <<= *old_n_in_x;
   *n_in_y <<= *old_n_in_y;
   *n_in_z <<= *old_n_in_z;
@@ -169,5 +215,7 @@ SurfaceNormals::timestep_init( const Patch* patch, ArchesTaskInfoManager* tsk_in
   *n_out_x <<= *old_n_out_x;
   *n_out_y <<= *old_n_out_y;
   *n_out_z <<= *old_n_out_z;
+
+  *new_surf <<= *old_surf;
 
 }
