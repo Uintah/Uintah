897e1956c13ec5e540592d51417075bbda966d15
Jeremy Nicholas Thornock <jthornoc@sci.utah.edu>
2015-10-16 19:03:06 +0000

    Add the ability to create new tasks on-the-fly.

--- src/CCA/Components/Arches/Utility/UtilityFactory.cc
+++ src/CCA/Components/Arches/Utility/UtilityFactory.cc
@@ -58,8 +58,50 @@ UtilityFactory::register_all_tasks( ProblemSpecP& db )
 
     }
   }
+}
 
+void
+UtilityFactory::add_task( ProblemSpecP& db ){
 
+  typedef SpatialOps::SVolField SVol;
+  typedef SpatialOps::XVolField XVol;
+  typedef SpatialOps::YVolField YVol;
+  typedef SpatialOps::ZVolField ZVol;
+
+  ProblemSpecP db_all_util = db->findBlock("Utilities");
+
+  //<Utilities>
+  if ( db_all_util ){
+    for ( ProblemSpecP db_util = db_all_util->findBlock("utility"); db_util != 0;
+          db_util = db_util->findNextBlock("utility")){
+
+      std::string name;
+      std::string type;
+      db_util->getAttribute("label", name);
+      db_util->getAttribute("type", type);
+
+      if ( type == "variable_math" ){
+
+        //Assume all SVOl for now:
+        //otherwise would need to determine or parse for the variable types
+        TaskInterface::TaskBuilder* tsk_builder = scinew TaskAlgebra<SVol,SVol,SVol>::Builder( name, 0 );
+        register_task(name, tsk_builder);
+
+        //also must build it here:
+        TaskInterface* tsk = retrieve_task(name);
+        tsk->problemSetup(db_util);
+        tsk->create_local_labels();
+
+      } else {
+
+        throw InvalidValue("Error: Utility type not recognized.",__FILE__,__LINE__);
+
+      }
+
+      assign_task_to_type_storage(name, type);
+
+    }
+  }
 }
 
 void
@@ -73,7 +115,7 @@ UtilityFactory::build_all_tasks( ProblemSpecP& db )
 
   tsk = retrieve_task("surface_normals");
   tsk->problemSetup(db);
-  tsk->create_local_labels(); 
+  tsk->create_local_labels();
 
   //<Utilities>
   ProblemSpecP db_all_util = db->findBlock("Utilities");
