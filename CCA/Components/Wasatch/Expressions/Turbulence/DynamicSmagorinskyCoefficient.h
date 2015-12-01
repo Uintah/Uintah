281101e26f1098a3330d4a167375919317fbf7ef
Tony Saad <tsaad@sci.utah.edu>
2015-12-01 23:00:32 +0000

    Generalize the turbulence closure models to handle a collocated grid arrangement. Also cleanup some warnings.

--- CCA/Components/Wasatch/Expressions/Turbulence/DynamicSmagorinskyCoefficient.h
+++ CCA/Components/Wasatch/Expressions/Turbulence/DynamicSmagorinskyCoefficient.h
@@ -18,19 +18,22 @@
  calculated, the TurbulentViscosity expression will take their product.
  */
 
+template<typename ResultT, typename Vel1T, typename Vel2T, typename Vel3T>
 class DynamicSmagorinskyCoefficient
-: public StrainTensorBase
+: public StrainTensorBase<ResultT, Vel1T, Vel2T, Vel3T>
 {
+
+private:
   const bool isConstDensity_;
   const bool doExtraFiltering_; // experimental
   
   DECLARE_FIELD(SVolField, rho_)
   
   // filtering operators
-  typedef  SpatialOps::OperatorTypeBuilder< SpatialOps::Filter, SVolField, SVolField >::type BoxFilterT;
-  typedef  SpatialOps::OperatorTypeBuilder< SpatialOps::Filter, XVolField, XVolField >::type XBoxFilterT;
-  typedef  SpatialOps::OperatorTypeBuilder< SpatialOps::Filter, YVolField, YVolField >::type YBoxFilterT;
-  typedef  SpatialOps::OperatorTypeBuilder< SpatialOps::Filter, ZVolField, ZVolField >::type ZBoxFilterT;
+  typedef  typename SpatialOps::OperatorTypeBuilder< SpatialOps::Filter, SVolField, SVolField >::type BoxFilterT;
+  typedef  typename SpatialOps::OperatorTypeBuilder< SpatialOps::Filter, Vel1T, Vel1T >::type XBoxFilterT;
+  typedef  typename SpatialOps::OperatorTypeBuilder< SpatialOps::Filter, Vel2T, Vel2T >::type YBoxFilterT;
+  typedef  typename SpatialOps::OperatorTypeBuilder< SpatialOps::Filter, Vel3T, Vel3T >::type ZBoxFilterT;
 
   const BoxFilterT*  boxFilterOp_;
   const XBoxFilterT* xBoxFilterOp_;
@@ -38,9 +41,9 @@ class DynamicSmagorinskyCoefficient
   const ZBoxFilterT* zBoxFilterOp_;
 
   // velocity interpolants
-  typedef SpatialOps::OperatorTypeBuilder< SpatialOps::Interpolant, XVolField, SVolField >::type Vel1InterpT;
-  typedef SpatialOps::OperatorTypeBuilder< SpatialOps::Interpolant, YVolField, SVolField >::type Vel2InterpT;
-  typedef SpatialOps::OperatorTypeBuilder< SpatialOps::Interpolant, ZVolField, SVolField >::type Vel3InterpT;
+  typedef typename SpatialOps::OperatorTypeBuilder< SpatialOps::Interpolant, Vel1T, ResultT >::type Vel1InterpT;
+  typedef typename SpatialOps::OperatorTypeBuilder< SpatialOps::Interpolant, Vel2T, ResultT >::type Vel2InterpT;
+  typedef typename SpatialOps::OperatorTypeBuilder< SpatialOps::Interpolant, Vel3T, ResultT >::type Vel3InterpT;
 
   const Vel1InterpT* vel1InterpOp_;
   const Vel2InterpT* vel2InterpOp_;
@@ -48,10 +51,10 @@ class DynamicSmagorinskyCoefficient
   
   
   // extrapolant operators
-  typedef Wasatch::OpTypes<SVolField>::BoundaryExtrapolant ExOpT;
-  typedef Wasatch::OpTypes<XVolField>::BoundaryExtrapolant XExOpT;
-  typedef Wasatch::OpTypes<YVolField>::BoundaryExtrapolant YExOpT;
-  typedef Wasatch::OpTypes<ZVolField>::BoundaryExtrapolant ZExOpT;
+  typedef typename Wasatch::OpTypes<SVolField>::BoundaryExtrapolant ExOpT;
+  typedef typename Wasatch::OpTypes<Vel1T>::BoundaryExtrapolant XExOpT;
+  typedef typename Wasatch::OpTypes<Vel2T>::BoundaryExtrapolant YExOpT;
+  typedef typename Wasatch::OpTypes<Vel3T>::BoundaryExtrapolant ZExOpT;
   
   ExOpT*   exOp_;
   XExOpT*  xexOp_;
@@ -61,7 +64,12 @@ class DynamicSmagorinskyCoefficient
   DynamicSmagorinskyCoefficient( const Expr::TagList& velTags,
                                  const Expr::Tag& rhoTag,
                                  const bool isConstDensity);
-  
+protected:
+  //A_SURF_B_Field = A vol, B surface
+  typedef typename SpatialOps::SpatFldPtr<ResultT> SVolPtr;
+  typedef typename std::vector< SVolPtr  > SVolVecT;
+  typedef typename std::vector< SVolVecT > SVolTensorT;
+
 
 public:
   class Builder : public Expr::ExpressionBuilder
