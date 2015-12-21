b45bfeadfb945436fd89371322e773dc01b4d369
James Sutherland <jsutherland@sci.utah.edu>
2015-12-21 15:15:08 +0000

    For now, set ExprLib to a specific version while we nail down why a recent change is causing diffs.

--- src/build_scripts/build_wasatch_3p.sh
+++ src/build_scripts/build_wasatch_3p.sh
@@ -42,7 +42,7 @@ fi
 # GIT Hash Tags for the various libraries
 
 # SPATIAL_OPS_TAG=
-# EXPR_LIB_TAG=
+EXPR_LIB_TAG=7c583362f4b6d69f700eaf7e6151807c50087220
 # TAB_PROPS_TAG=
 # RAD_PROPS_TAG=
 
@@ -140,7 +140,7 @@ run "cd ../../.."  # back to Wasatch3P
 
 run "cd src"
 run "rm -rf ExprLib"
-run "env GIT_SSL_NO_VERIFY=true git clone --depth 1 https://software.crsim.utah.edu:8443/James_Research_Group/ExprLib.git ExprLib"
+run "env GIT_SSL_NO_VERIFY=true git clone --depth 20 https://software.crsim.utah.edu:8443/James_Research_Group/ExprLib.git ExprLib"
 if test ! -z $EXPR_LIB_TAG ; then
     run "cd ExprLib"
     run "git reset --hard $EXPR_LIB_TAG"
