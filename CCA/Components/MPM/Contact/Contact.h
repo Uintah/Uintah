2f30590f7847ac2b6a148a5b44b99ee436b3f388
Jim Guilkey <guilkey@sci.utah.edu>
2016-01-01 20:38:50 +0000

    Bringing some of the ARL changes into the trunk.

--- CCA/Components/MPM/Contact/Contact.h
+++ CCA/Components/MPM/Contact/Contact.h
@@ -108,8 +108,8 @@ WARNING
       };
       
       inline bool compare(double num1, double num2) {
+            //double EPSILON=1.e-20;
             double EPSILON=1.e-14;
-            
             return (fabs(num1-num2) <= EPSILON);
       }
 
