42c5b6d8bcea99f151c0c637d0166dcb12273c9c
Dan Sunderland <dsunder@sci.utah.edu>
2016-02-03 00:27:37 +0000

    Implement parallel_for and parallel_reduce over 3D ranges

--- src/Core/Grid/Variables/BlockRange.h
+++ src/Core/Grid/Variables/BlockRange.h
@@ -114,6 +114,7 @@ private:
     return x / m_dim[d];
   }
 
+public:
   size_type m_offset[rank];
   size_type m_dim[rank];
 };
@@ -191,10 +192,88 @@ private:
     idx = x + m_offset[d];
   }
 
+public:
   size_type m_offset[rank];
   size_type m_dim[rank];
 };
 
+
+template <typename SizeType, typename Functor>
+void parallel_for( const RowMajorRange<SizeType,3> & r, const Functor & f )
+{
+  const SizeType ib = r.m_offset[0];
+  const SizeType jb = r.m_offset[1];
+  const SizeType kb = r.m_offset[2];
+
+  const SizeType ie = ib + r.m_dim[0];
+  const SizeType je = jb + r.m_dim[1];
+  const SizeType ke = kb + r.m_dim[2];
+
+#pragma omp parallel for collapse(3)
+  for (SizeType i=ib; i<ie; ++i) {
+  for (SizeType j=jb; j<je; ++j) {
+  for (SizeType k=kb; k<ke; ++k) {
+    f(i,j,k);
+  }}}
+};
+
+template <typename SizeType, typename Functor>
+void parallel_for( const ColumnMajorRange<SizeType,3> & r, const Functor & f )
+{
+  const SizeType ib = r.m_offset[0];
+  const SizeType jb = r.m_offset[1];
+  const SizeType kb = r.m_offset[2];
+
+  const SizeType ie = ib + r.m_dim[0];
+  const SizeType je = jb + r.m_dim[1];
+  const SizeType ke = kb + r.m_dim[2];
+
+#pragma omp parallel for collapse(3)
+  for (SizeType k=kb; k<ke; ++k) {
+  for (SizeType j=jb; j<je; ++j) {
+  for (SizeType i=ib; i<ie; ++i) {
+    f(i,j,k);
+  }}}
+};
+
+
+template <typename SizeType, typename Functor, typename ReductionType>
+void parallel_reduce( const RowMajorRange<SizeType,3> & r, const Functor & f, ReductionType & red  )
+{
+  const SizeType ib = r.m_offset[0];
+  const SizeType jb = r.m_offset[1];
+  const SizeType kb = r.m_offset[2];
+
+  const SizeType ie = ib + r.m_dim[0];
+  const SizeType je = jb + r.m_dim[1];
+  const SizeType ke = kb + r.m_dim[2];
+
+#pragma omp parallel for collapse(3) reduction(+:red)
+  for (SizeType i=ib; i<ie; ++i) {
+  for (SizeType j=jb; j<je; ++j) {
+  for (SizeType k=kb; k<ke; ++k) {
+    f(i,j,k,red);
+  }}}
+};
+
+template <typename SizeType, typename Functor, typename ReductionType>
+void parallel_reduce( const ColumnMajorRange<SizeType,3> & r, const Functor & f, ReductionType & red  )
+{
+  const SizeType ib = r.m_offset[0];
+  const SizeType jb = r.m_offset[1];
+  const SizeType kb = r.m_offset[2];
+
+  const SizeType ie = ib + r.m_dim[0];
+  const SizeType je = jb + r.m_dim[1];
+  const SizeType ke = kb + r.m_dim[2];
+
+#pragma omp parallel for collapse(3) reduction(+:red)
+  for (SizeType k=kb; k<ke; ++k) {
+  for (SizeType j=jb; j<je; ++j) {
+  for (SizeType i=ib; i<ie; ++i) {
+    f(i,j,k,red);
+  }}}
+};
 
 } // namespace Uintah
 
