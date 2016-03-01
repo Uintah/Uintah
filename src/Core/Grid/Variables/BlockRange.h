b336f9f390752e28c2c51714901c18e7c93d011c
Dan Sunderland <dsunder@sci.utah.edu>
2016-03-01 19:48:13 +0000

    Add operator()(int i, int j, int k) to constVariable and update Poisson1 to always use a functor

--- src/Core/Grid/Variables/BlockRange.h
+++ src/Core/Grid/Variables/BlockRange.h
@@ -25,30 +25,16 @@
 #ifndef UINTAH_HOMEBREW_BLOCK_RANGE_H
 #define UINTAH_HOMEBREW_BLOCK_RANGE_H
 
-#include <type_traits>
-
 namespace Uintah {
 
-/// RowMajor mapping of [0,n) -> [ (i0,j0,k0,..), (i1,j1,k1,...) )
-template <  typename SizeType = int
-          , int Rank = 3
-         >
-class RowMajorRange
+class BlockRange
 {
-  static_assert( std::is_integral<SizeType>::value || std::is_enum<SizeType>::value
-                ,"Error: SizeType must be an integral type"
-               );
-
-  static_assert( Rank > 0
-                ,"Error: Rank must be greater than 0"
-               );
 public:
 
-  using size_type = SizeType;
-  static constexpr int rank = Rank;
+  enum { rank = 3 };
 
   template <typename ArrayType>
-  RowMajorRange( ArrayType const & c0, ArrayType const & c1 )
+  BlockRange( ArrayType const & c0, ArrayType const & c1 )
   {
     for (int i=0; i<rank; ++i) {
       m_offset[i] = c0[i] < c1[i] ? c0[i] : c1[i];
@@ -56,224 +42,51 @@ public:
     }
   }
 
-  size_type size() const
+  int begin( int r ) const { return m_offset[r]; }
+  int   end( int r ) const { return m_offset[r] + m_dim[r]; }
+
+  size_t size() const
   {
-    size_type result{1};
+    size_t result = 1u;
     for (int i=0; i<rank; ++i) {
       result *= m_dim[i];
     }
     return result;
   }
 
-  // populate the multi-index associated with the given linear index
-  // e.g.
-  //   x is the linear index
-  //   range(x, i, j, k);
-  template <typename IType, typename... Indices>
-  inline __attribute__((always_inline))
-  void operator()(size_type x, IType & idx, Indices &... indices) const
-  {
-    static_assert( (sizeof...(Indices) == (rank-1))
-                  ,"Error: The number of indices does not equal the rank" );
-
-    static_assert( std::is_integral<IType>::value
-                  ,"Error: Index is not a intergral type" );
-    static_assert( !std::is_signed<size_type>::value || std::is_signed<IType>::value
-                  ,"Error: Signed size_type requires using signed indices" );
-
-    x = apply(1, x, indices...);
-    idx = x + m_offset[0];
-  }
-
 private:
-
-  template <typename IType, typename... Indices>
-  inline __attribute__((always_inline))
-  int apply(int d, size_type x, IType & idx, Indices &... indices) const
-  {
-    static_assert( std::is_integral<IType>::value
-                  ,"Error: Index is not a intergral type" );
-    static_assert( !std::is_signed<size_type>::value || std::is_signed<IType>::value
-                  ,"Error: Signed size_type requires using signed indices" );
-
-    x = apply( d+1, x, indices...);
-    idx = (x % m_dim[d]) + m_offset[d];
-    return x / m_dim[d];
-  }
-
-  template <typename IType>
-  inline __attribute__((always_inline))
-  int apply(int d, size_type x, IType & idx) const
-  {
-    static_assert( std::is_integral<IType>::value
-                  ,"Error: Index is not a intergral type" );
-    static_assert( !std::is_signed<size_type>::value || std::is_signed<IType>::value
-                  ,"Error: Signed size_type requires using signed indices" );
-
-    idx = (x % m_dim[d]) + m_offset[d];
-    return x / m_dim[d];
-  }
-
-public:
-  size_type m_offset[rank];
-  size_type m_dim[rank];
+  int m_offset[rank];
+  int m_dim[rank];
 };
 
 
-/// ColumnMajor mapping of [0,n) -> [ (i0,j0,k0,..), (i1,j1,k1,...) )
-template <  typename SizeType = int
-          , int Rank = 3
-         >
-class ColumnMajorRange
+template <typename Functor>
+void parallel_for( BlockRange const & r, const Functor & f )
 {
-  static_assert( std::is_integral<SizeType>::value || std::is_enum<SizeType>::value
-                ,"Error: SizeType must be an integral type"
-               );
-public:
-
-  using size_type = SizeType;
-  static constexpr int rank = Rank;
-
-  template <typename ArrayType>
-  ColumnMajorRange( ArrayType const & c0, ArrayType const & c1 )
-  {
-    for (int i=0; i<rank; ++i) {
-      m_offset[i] = c0[i] < c1[i] ? c0[i] : c1[i];
-      m_dim[i] =   (c0[i] < c1[i] ? c1[i] : c0[i]) - m_offset[i];
-    }
-  }
-
-  size_type size() const
-  {
-    size_type result{1};
-    for (int i=0; i<rank; ++i) {
-      result *= m_dim[i];
-    }
-    return result;
-  }
-
-  // populate the multi-index associated with the given linear index
-  // e.g.
-  //   x is the linear index
-  //   range(x, i, j, k);
-  template <typename... Indices>
-  inline __attribute__((always_inline))
-  void operator()(size_type x, Indices &... indices) const
-  {
-    static_assert( sizeof...(Indices) == rank
-                  ,"Error: The number of indices does not equal the rank" );
-    apply(0, x, indices...);
-  }
-
-private:
-
-  template <typename IType, typename... Indices>
-  inline __attribute__((always_inline))
-  void apply(int d, size_type x, IType & idx, Indices &... indices) const
-  {
-    static_assert( std::is_integral<IType>::value
-                  ,"Error: Index is not a intergral type" );
-    static_assert( !std::is_signed<size_type>::value || std::is_signed<IType>::value
-                  ,"Error: Signed size_type requires using signed indices" );
-
-    idx = (x % m_dim[d]) + m_offset[d];
-    apply( d+1, x/m_dim[d], indices...);
-  }
-
-  template <typename IType>
-  inline __attribute__((always_inline))
-  void apply(int d, size_type x, IType & idx) const
-  {
-    static_assert( std::is_integral<IType>::value
-                  ,"Error: Index is not a intergral type" );
-    static_assert( !std::is_signed<size_type>::value || std::is_signed<IType>::value
-                  ,"Error: Signed size_type requires using signed indices" );
-
-    idx = x + m_offset[d];
-  }
-
-public:
-  size_type m_offset[rank];
-  size_type m_dim[rank];
-};
-
-
-template <typename SizeType, typename Functor>
-void parallel_for( const RowMajorRange<SizeType,3> & r, const Functor & f )
-{
-  const SizeType ib = r.m_offset[0];
-  const SizeType jb = r.m_offset[1];
-  const SizeType kb = r.m_offset[2];
-
-  const SizeType ie = ib + r.m_dim[0];
-  const SizeType je = jb + r.m_dim[1];
-  const SizeType ke = kb + r.m_dim[2];
+  const int ib = r.begin(0); const int ie = r.end(0);
+  const int jb = r.begin(1); const int je = r.end(1);
+  const int kb = r.begin(2); const int ke = r.end(2);
 
 #pragma omp parallel for collapse(3)
-  for (SizeType i=ib; i<ie; ++i) {
-  for (SizeType j=jb; j<je; ++j) {
-  for (SizeType k=kb; k<ke; ++k) {
+  for (int k=kb; k<ke; ++k) {
+  for (int j=jb; j<je; ++j) {
+  for (int i=ib; i<ie; ++i) {
     f(i,j,k);
   }}}
 };
 
-template <typename SizeType, typename Functor>
-void parallel_for( const ColumnMajorRange<SizeType,3> & r, const Functor & f )
+template <typename Functor, typename ReductionType>
+void parallel_reduce( BlockRange const & r, const Functor & f, ReductionType & red  )
 {
-  const SizeType ib = r.m_offset[0];
-  const SizeType jb = r.m_offset[1];
-  const SizeType kb = r.m_offset[2];
-
-  const SizeType ie = ib + r.m_dim[0];
-  const SizeType je = jb + r.m_dim[1];
-  const SizeType ke = kb + r.m_dim[2];
-
-#pragma omp parallel for collapse(3)
-  for (SizeType k=kb; k<ke; ++k) {
-  for (SizeType j=jb; j<je; ++j) {
-  for (SizeType i=ib; i<ie; ++i) {
-    f(i,j,k);
-  }}}
-};
-
-
-template <typename SizeType, typename Functor, typename ReductionType>
-void parallel_reduce( const RowMajorRange<SizeType,3> & r, const Functor & f, ReductionType & red  )
-{
-  const SizeType ib = r.m_offset[0];
-  const SizeType jb = r.m_offset[1];
-  const SizeType kb = r.m_offset[2];
-
-  const SizeType ie = ib + r.m_dim[0];
-  const SizeType je = jb + r.m_dim[1];
-  const SizeType ke = kb + r.m_dim[2];
-
-  ReductionType tmp = red;
-#pragma omp parallel for collapse(3) reduction(+:tmp)
-  for (SizeType i=ib; i<ie; ++i) {
-  for (SizeType j=jb; j<je; ++j) {
-  for (SizeType k=kb; k<ke; ++k) {
-    f(i,j,k,tmp);
-  }}}
-  red = tmp;
-};
-
-template <typename SizeType, typename Functor, typename ReductionType>
-void parallel_reduce( const ColumnMajorRange<SizeType,3> & r, const Functor & f, ReductionType & red  )
-{
-  const SizeType ib = r.m_offset[0];
-  const SizeType jb = r.m_offset[1];
-  const SizeType kb = r.m_offset[2];
-
-  const SizeType ie = ib + r.m_dim[0];
-  const SizeType je = jb + r.m_dim[1];
-  const SizeType ke = kb + r.m_dim[2];
+  const int ib = r.begin(0); const int ie = r.end(0);
+  const int jb = r.begin(1); const int je = r.end(1);
+  const int kb = r.begin(2); const int ke = r.end(2);
 
 ReductionType tmp = red;
 #pragma omp parallel for collapse(3) reduction(+:tmp)
-  for (SizeType k=kb; k<ke; ++k) {
-  for (SizeType j=jb; j<je; ++j) {
-  for (SizeType i=ib; i<ie; ++i) {
+  for (int k=kb; k<ke; ++k) {
+  for (int j=jb; j<je; ++j) {
+  for (int i=ib; i<ie; ++i) {
     f(i,j,k,tmp);
   }}}
   red = tmp;
