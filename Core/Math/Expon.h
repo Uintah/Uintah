c3fadd2d44b798d6eb11651872111516f0cb33d9
Todd Harman <harman@sci.utah.edu>
2016-03-30 22:35:40 +0000

    Expon.cc/h   - added several approximate and fast exponential functions.  Consider using these if    there is an exp() in the inner most loop and you don't need 16 digits of precision.    They are best suited for arguments -1 < x < 1.
    RMCRT:
      - Added machinery to utilize the fast exponent methods.  Currently, disabled.  We need to 
      characterize the range of the optical thickness before we can take advantage of them.  Preliminary,
      tests show a speedup of 1.4 - 1.7 for the Burns & Christon problem while maintaininig ray 
      convergence.

--- Core/Math/Expon.h
+++ Core/Math/Expon.h
@@ -36,8 +36,11 @@
 
 #ifndef SCI_Math_Expon_h
 #define SCI_Math_Expon_h 1
-
+#include <Core/Util/Assert.h>
+#include <Core/Util/FancyAssert.h>
+#include <map>
 #include <cmath>
+#include <iostream>
 
 namespace Uintah {
 
@@ -139,6 +142,141 @@ namespace Uintah {
   {
     return expf(x);
   }
+  
+  //______________________________________________________________________
+  //
+  // A collection of fast approximate exp() functions
+  // Use these calls if the exp() is in the inner most loop
+  
+  class fastApproxExponent {
+    public: 
+      fastApproxExponent(); 
+      ~fastApproxExponent();
+             
+      //__________________________________
+      // These methods are fast and reasonably accurate for arguments < 1
+      // If the argument is near 0 to 1 and you don't need the precision consider one
+      // of these.  Obviously the more terms you include the more accurate it is.
+      //
+      // http://www.musicdsp.org/showone.php?id=222
+      // These are computed using a Taylor series with limited number of terms.  For example exp4():
+      //  exp(x) = 1 + x + x^2/2 + x^3/6 + x^4/24 + ...
+      //         = (24 + 24x + x^2*12 + x^3*4 + x^4) / 24
+      //    (using Horner-scheme:)
+      //         = (24 + x * (24 + x * (12 + x * (4 + x)))) * 0.041666666f
+      
+      inline double exp1(double x) { 
+        return (6+x*(6+x*(3+x)))*0.16666666f; 
+      }
+
+      inline double exp2(double x) {
+        return (24+x*(24+x*(12+x*(4+x))))*0.041666666f;
+      }
+
+      inline double exp3(double x) {
+        return (120+x*(120+x*(60+x*(20+x*(5+x)))))*0.0083333333f;
+      }
+
+      inline double exp4(double x) {
+        return (720+x*(720+x*(360+x*(120+x*(30+x*(6+x))))))*0.0013888888f;
+      }
+
+      inline double exp5(double x) {
+        return (5040+x*(5040+x*(2520+x*(840+x*(210+x*(42+x*(7+x)))))))*0.00019841269f;
+      }
+
+      inline double exp6(double x) {
+        return (40320+x*(40320+x*(20160+x*(6720+x*(1680+x*(336+x*(56+x*(8+x))))))))*2.4801587301e-5;
+      }
+
+      inline double exp7(double x) {
+        return (362880+x*(362880+x*(181440+x*(60480+x*(15120+x*(3024+x*(504+x*(72+x*(9+x)))))))))*2.75573192e-6;
+      }       
+
+      void populateExp_int(const int low, 
+                           const int high);
+      //______________________________________________________________________
+      //  Minimize the error by splitting the arguement (x)
+      //  into the integer and mantisa. 
+      // Then we can use: 
+      //  exp(z) = exp(x+y)  = exp(x) * exp(y)
+      //                         A        B
+      //             where -1 < y < 1
+      //  exp(x)   has been pre-populated map and is d_exp_integer[x]
+      //  exp5(y):  Taylor Series expansion with 5 terms
+      //  
+      //  Note you must pre-populate d_exp_integer before calling this method
+      //  This is 1.2 times faster than std:exp()
+      //
+      //   This is experimental
+      //______________________________________________________________________
+      inline double fast_exp(const double z)
+      {
+
+        ASSERTMSG( (d_exp_integer.size() > 0), "The d_exp_integer map must be populated before using faster_exp()");
+        double x;
+        double y = std::modf(z, &x);
+
+        ASSERTRANGE( x, d_int_min, d_int_max);   // is the value of x contained in the map?
+
+        double A = d_exp_integer[(int)x];
+        double B = exp5(y); 
+       // std::cout << "  fast_exp: z: " << z << " x: " << x << " exp(x): " << A << " y: " << y << " exp(y) " << B<<  " d_int_min: " << d_int_min << " d_int_max: " << d_int_max <<  std::endl;
+        return A * B;
+      }
+
+      //______________________________________________________________________
+      //  Fast approximation to exponential function.
+      //                                                               
+      // Reference:                                                    
+      //    N.N. Schraudolph,                                          
+      //    A Fast, Compact Approximation of the Exponential Function,
+      //    Neural Computation, 11(4), 1999.                           
+      //    http://www.inf.ethz.ch/~schraudo/pubs/
+      //
+      //    G.C. Cawley,
+      //    On a Fast Compact Approximation of the Exponential Function
+      //    Neural Computation 12, 2009-2012 (2000)
+      //
+      //    http://www.cecs.uci.edu/~papers/neuromorphic2008/lectures07/ieee754twiddles.pdf
+      //
+      //    Avoid this method if the argument is near 0 to 1 ish.  The taylor
+      //    series methods are more accurate.
+      //______________________________________________________________________
+      #define EXP_A 1048576/M_LN2
+      #define EXP_C 60801
+      inline double Schraudolph_exp( const double x)
+      {
+        double y = 0.5 * x;     // splitting x in two improve error characteristics.  See pdf
+        
+        ASSERTRANGE( y, -700, 700);
+        union{
+          double d;
+      #ifdef LITTLE_ENDIAN
+          struct{
+            int j;
+            int i;
+          } n;
+      #else
+          struct{
+            int i;
+            int j;
+          } n;
+      #endif     
+        } eco;
+
+        eco.n.i = (int)(EXP_A*(y)) + (1072693248 - EXP_C);
+        eco.n.j = 0;
+        double    exp_x_2 = eco.d;      // exp(x/2)
+        return exp_x_2 * exp_x_2;       // exp(x/2 + x/2) = exp(x/2)*exp(x/2
+      }
+
+    //__________________________________
+    private:
+      std::map<int,double> d_exp_integer;       // contains exp(Integer)
+      int d_int_min;                            // min & max range of argument.
+      int d_int_max;                            // used for bulleproofing
+   };
 }
 
 #endif
