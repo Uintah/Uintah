c4191900a79f8aa96e089b18dedd1ca1feec8ff9
Jeremy Nicholas Thornock <jthornoc@sci.utah.edu>
2016-08-09 20:02:06 +0000

    Remove debugging print statements in SurfaceNormals.
    Kokkos for DensityPredictor.
    
    Added a helper KokkosTools header where we can place some commonly used
    lines of code.

---
+++ src/CCA/Components/Arches/KokkosTools.h
@@ -0,0 +1,14 @@
+#ifndef Uintah_Component_Arches_KokkosTools_h
+#define Uintah_Component_Arches_KokkosTools_h
+
+namespace Uintah{ namespace ArchesCore {
+
+
+#define KOKKOS_INITIALIZE_TO_CONSTANT_EXTRA_CELL(phi, const) \
+  Uintah::BlockRange range(patch->getExtraCellLowIndex(), patch->getExtraCellHighIndex() ); \
+  Uintah::parallel_for( range, [&](int i, int j, int k){ \
+    phi(i,j,k) = const; \
+  }); \
+
+}} //end namespace Uintah::ArchesCore
+#endif
