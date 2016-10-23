1a9f442e009f9df5ff24615fe263460259c81921
Justin Hooper <jbhooper@sci.utah.edu>
2016-10-23 22:50:26 +0000

    Replaced the geometry based EllipsoidGeometryPiece instantiation and boundary check with a linear algebra based one. Ellipsoids with arbitrary axes vector directions now work properly.

--- src/Core/GeometryPiece/EllipsoidGeometryPiece.cc
+++ src/Core/GeometryPiece/EllipsoidGeometryPiece.cc
@@ -28,12 +28,12 @@
 #include <Core/ProblemSpec/ProblemSpec.h>
 #include <Core/Exceptions/ProblemSetupException.h>
 #include <Core/Malloc/Allocator.h>
-#include <Core/Math/Matrix3.h>
 
 using namespace Uintah;
 using namespace std;
 
 const string EllipsoidGeometryPiece::TYPE_NAME = "ellipsoid";
+const double EllipsoidGeometryPiece::geomTol = 1.0e-12;
 
 EllipsoidGeometryPiece::EllipsoidGeometryPiece(ProblemSpecP& ps)
 {
@@ -49,9 +49,9 @@ EllipsoidGeometryPiece::EllipsoidGeometryPiece(ProblemSpecP& ps)
   ps->getWithDefault("v3",d_v3,     zero);
 
   // Get orthagonal axes
-  ps->getWithDefault("rx",d_radiusX, 0.0);
-  ps->getWithDefault("ry",d_radiusY, 0.0);
-  ps->getWithDefault("rz",d_radiusZ, 0.0);
+  ps->getWithDefault("rx",d_r1, 0.0);
+  ps->getWithDefault("ry",d_r2, 0.0);
+  ps->getWithDefault("rz",d_r3, 0.0);
   
   // Run helper function to determine if inputs are correct
   initializeEllipsoidData();
@@ -61,14 +61,14 @@ EllipsoidGeometryPiece::EllipsoidGeometryPiece(const Point& origin,
                                                double radx, double rady, double radz )
 {
   d_origin = origin;
-  d_radiusX = radx;
-  d_radiusY = rady;
-  d_radiusZ = radz;
+  d_r1 = radx;
+  d_r2 = rady;
+  d_r3 = radz;
   
   // Make sure there is no uninitialized variables going into initialization routine
-  d_v1 = *(new Vector(0.0, 0.0, 0.0));
-  d_v2 = *(new Vector(0.0, 0.0, 0.0));
-  d_v3 = *(new Vector(0.0, 0.0, 0.0));
+  d_v1 = Vector(1.0, 0.0, 0.0);
+  d_v2 = Vector(0.0, 1.0, 0.0);
+  d_v3 = Vector(0.0, 0.0, 1.0);
   
   // Run helper function to determine if inputs are correct
   initializeEllipsoidData();
@@ -83,9 +83,9 @@ EllipsoidGeometryPiece::EllipsoidGeometryPiece(const Point& origin,
   d_v3 = three;
   
   // Make sure there is no uninitialized variables going into initialization routine
-  d_radiusX = 0.0;
-  d_radiusY = 0.0;
-  d_radiusZ = 0.0;
+  d_r1 = 0.0;
+  d_r2 = 0.0;
+  d_r3 = 0.0;
   
   // Run helper function to determine if inputs are correct
   initializeEllipsoidData();
@@ -99,9 +99,9 @@ void EllipsoidGeometryPiece::outputHelper( ProblemSpecP & ps ) const
 {
   ps->appendElement("origin",d_origin);
   if(xyzAligned) { // only need to output radii
-    ps->appendElement("rx",    d_radiusX);
-    ps->appendElement("ry",    d_radiusX);
-    ps->appendElement("rz",    d_radiusX);
+    ps->appendElement("rx",    d_r1);
+    ps->appendElement("ry",    d_r1);
+    ps->appendElement("rz",    d_r1);
   } else {
     ps->appendElement("v1",    d_v1);
     ps->appendElement("v2",    d_v2);
@@ -116,75 +116,52 @@ GeometryPieceP EllipsoidGeometryPiece::clone() const
 
 bool EllipsoidGeometryPiece::inside(const Point& p) const
 {
-  // Variable initialization
-  Point *pTransformed = new Point(p.x()-d_origin.x(),p.y()-d_origin.y(),p.z()-d_origin.z());
-
-  // create rotate
-  if(!xyzAligned) {
-    // Rotate point
-    // Note, angles are negated so that it's opposite of what ellipse is rotated
-    pTransformed = new Point(cos(-thetaz)*pTransformed->x() - sin(-thetaz)*pTransformed->y(), 
-                             cos(-thetaz)*pTransformed->y() + sin(-thetaz)*pTransformed->x(), 
-                             pTransformed->z());
-    pTransformed = new Point(cos(-thetay)*pTransformed->x() + sin(-thetay)*pTransformed->z(), 
-                             pTransformed->y(), 
-                             cos(-thetay)*pTransformed->z() - sin(-thetay)*pTransformed->x());
-    pTransformed = new Point(pTransformed->x(),
-                             cos(-thetax)*pTransformed->y() + sin(-thetax)*pTransformed->z(),
-                             cos(-thetax)*pTransformed->z() - sin(-thetax)*pTransformed->y());
+  // This can be sped up, but this is simple.
+  Vector pTransformed = p-d_origin;
+  if (Dot(pTransformed,d_m3E*(pTransformed)) - 1.0 < geomTol)
+  {
+    return true;
   }
+  return false;
   
-  // Check if in unit distance from sphere center after scaling
-  if (sqrt(pTransformed->x()*pTransformed->x()/(d_radiusX*d_radiusX) +
-           pTransformed->y()*pTransformed->y()/(d_radiusY*d_radiusY) +
-           pTransformed->z()*pTransformed->z()/(d_radiusZ*d_radiusZ) ) <= 1.0) {
-    return true;                           
-  } else {
-    return false;
-  }
 }
 
 Box EllipsoidGeometryPiece::getBoundingBox() const
 {
-  double highX = 0.0;
-  double highY = 0.0;
-  double highZ = 0.0;
+  Vector high(0.0);
 
-  // Use vectors to find highest xyz
-  // X
-  highX = fabs(d_v1.x());
-  if(fabs(d_v2.x()) > highX)
-    highX = fabs(d_v2.x());
-  if(fabs(d_v3.x()) > highX)
-    highX = fabs(d_v3.x());
-  // Y
-  highY = fabs(d_v1.y());
-  if(fabs(d_v2.y()) > highY)
-    highY = fabs(d_v2.y());
-  if(fabs(d_v3.y()) > highY)
-    highY = fabs(d_v3.y());
-  // X
-  highZ = fabs(d_v1.z());
-  if(fabs(d_v2.z()) > highZ)
-    highZ = fabs(d_v2.z());
-  if(fabs(d_v3.z()) > highZ)
-    highZ = fabs(d_v3.z());
-  
-    Point low( d_origin.x()-fabs(highX),d_origin.y()-fabs(highY),
-           d_origin.z()-fabs(highZ) );
+  for (int direction = 0; direction < 3; ++direction)
+  {
+    high[direction] = fabs(d_v1[direction]);
+    double check = fabs(d_v2[direction]);
+    if (check > high[direction]) high[direction] = check;
+    check = fabs(d_v3[direction]);
+    if (check > high[direction]) high[direction] = check;
+  }
 
-    Point high( d_origin.x()+fabs(highX),d_origin.y()+fabs(highY),
-           d_origin.z()+fabs(highZ) );
+  Point minCorner(d_origin-high);
+  Point maxCorner(d_origin+high);
 
-    return Box(low,high);
+  return Box(minCorner,maxCorner);
 }
 
 void EllipsoidGeometryPiece::initializeEllipsoidData()
 {
+
+  // Linear algebraic representation using formula:
+  // E := { x | (x-c)^T US^2U^T (x-c) <= 1.0 }
+
+  // U is vector whose columns are unit vector representations of the
+  //   directions of the ellipsoid axes.
+  // S is the diagonal matrix with the ii component equivalent to the
+  //   inverse squared magnitude of the ellipsoid axes
+  //   (i.e. S_11 := 1.0/||d_v1||^2 )
   // determine whether input is from vector or double
-  if(d_v1.length() > 0.0 &&
-     d_v2.length() > 0.0 &&
-     d_v3.length() > 0.0){
+
+  if (    d_v1.length() > geomTol
+      &&  d_v2.length() > geomTol
+      &&  d_v3.length() > geomTol)
+  {
     // Check for orthagonality
     if((fabs(Dot(d_v1,d_v2)) >= 1e-12) ||
        (fabs(Dot(d_v2,d_v3)) >= 1e-12) ||
@@ -192,101 +169,30 @@ void EllipsoidGeometryPiece::initializeEllipsoidData()
     {
       throw ProblemSetupException("Input File Error: (Ellipsoid initialization) input vectors (v1,v2,v3) are not orthagonal to within 1e-12 or each other", __FILE__, __LINE__, false);
     }
-     
-    // compute radius of each vector when aligned to grid
-    d_radiusX = d_v1.length();
-    d_radiusY = d_v2.length();
-    d_radiusZ = d_v3.length();
 
+    // Express in compact matrix representation
+    Vector u1 = d_v1;
+    d_r1 = u1.normalize();
+    Vector u2 = d_v2;
+    d_r2 = u2.normalize();
+    Vector u3 = d_v3;
+    d_r3 = u3.normalize();
     // Initialize variables for rotation
-    thetaz = 0.0, thetay = 0.0, thetax = 0.0;
-    
-    Vector unitX = *(new Vector(1.0,0.0,0.0));
-    Vector unitY = *(new Vector(0.0,1.0,0.0));
-    Vector unitZ = *(new Vector(0.0,0.0,1.0));
-    
-    // Find vector with largest deviation in each direction
-    Vector one = d_v1;
-    double highX = fabs(d_v1.x());
-    if(fabs(d_v2.x()) > highX){
-      highX = fabs(d_v2.x());
-      one = d_v2;
-    }
-    if(fabs(d_v3.x()) > highX){
-      one = d_v3;
-    }
-    Vector two = d_v1;
-    double highY = fabs(d_v1.y());
-    if(fabs(d_v2.y()) > highY){
-      highY = fabs(d_v2.y());
-      two = d_v2;
-    }
-    if(fabs(d_v3.y()) > highY){
-      two = d_v3;
-    }    
-    Vector three = d_v1;
-    double highZ = fabs(d_v1.z());
-    if(fabs(d_v2.z()) > highZ){
-      highZ = fabs(d_v2.z());
-      three = d_v2;
-    }
-    if(fabs(d_v3.z()) > highZ){
-      three = d_v3;
-    }
-    
-    Vector temporary = *(new Vector(two));
-    // Compute degree to which it is rotated
-    // Find rotation about Z
-    Vector projection = temporary - unitZ*(Dot(unitZ,temporary));
-//    cout << "projection Z = " << projection << endl;
-//    if(fabs(projection[0]) > 1.e-12){
-//      thetaz = atan2(projection[1],projection[0]);
-//    } else {
-//      thetaz = 0.0;
-//    }
-      thetaz = atan2(d_v1.y(),d_v1.x());
-    
-    // Find rotation about Y
-    // rotate second vector about z and then find rotation about y
-    temporary = *(new Vector(cos(-thetaz)*one.x() - sin(-thetaz)*one.y(), 
-                           cos(-thetaz)*one.y() + sin(-thetaz)*one.x(), 
-                           one.z()));
-    
-    projection = temporary - unitY*(Dot(unitY,temporary));
-    if(fabs(projection[0]) > 1.e-12){
-      thetay = atan2(projection[2],projection[0]);
-    } else {
-      thetay = 0.0;
-    }
 
-    // Find rotation about X
-    temporary = *(new Vector(cos(-thetay)*three.z() - sin(-thetay)*three.y(), 
-                             three.y(),
-                             cos(-thetay)*three.y() + sin(-thetay)*three.z()));
-    
-    projection = temporary - unitX*(Dot(unitX,temporary));
-    if(fabs(projection[1]) > 1.e-12){
-      thetax = atan2(projection[2],projection[1]);
-    } else {
-      thetax = 0.0;
-    }
-//    cout << "thetax = " << thetax << endl;
-//    cout << "thetay = " << thetay << endl;
-//    cout << "thetaz = " << thetaz << endl;
-//    cout << "radX = " << d_radiusX << endl;
-//    cout << "radY = " << d_radiusY << endl;
-//    cout << "radZ = " << d_radiusZ << endl;
+    Matrix3 U(u1[0], u2[0], u3[0],
+              u1[1], u2[1], u3[1],
+              u1[2], u2[2], u3[2]);
+    Matrix3 S(1.0/(d_r1*d_r1), 0.0            , 0.0,
+              0.0            , 1.0/(d_r2*d_r2), 0.0,
+              0.0            , 0.0            , 1.0/(d_r3*d_r3) );
 
-    // set flag so that each time a point is checked using inside() the 
-    //   point is rotated the correct amount
-    xyzAligned = false;
-  } else if(d_radiusX > 0.0 &&
-            d_radiusY > 0.0 &&
-            d_radiusZ > 0.0){
+    d_m3E = U*S*U.Transpose();
+
+  } else if(d_r1 > 0.0 && d_r2 > 0.0 && d_r3 > 0.0) {
     // create vector representation along the cartesian axes
-    d_v1 = *(new Vector(d_radiusX, 0.0, 0.0));
-    d_v2 = *(new Vector(0.0, d_radiusY, 0.0));
-    d_v3 = *(new Vector(0.0, 0.0, d_radiusZ));
+    d_v1 = *(new Vector(d_r1, 0.0, 0.0));
+    d_v2 = *(new Vector(0.0, d_r2, 0.0));
+    d_v3 = *(new Vector(0.0, 0.0, d_r3));
     
     // set flag such that rotation doesnt need to occur in inside()
     xyzAligned = true;
