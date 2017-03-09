06a8a88d931cabbe2eaaab457fc12d9ada34fbd9
Todd Harman <harman@sci.utah.edu>
2017-03-09 16:57:40 +0000

    added comment

--- src/CCA/Components/Schedulers/TaskGraph.cc
+++ src/CCA/Components/Schedulers/TaskGraph.cc
@@ -1559,6 +1559,7 @@ CompTable::findcomp(       Task::Dependency   * req
 
     // TODO - fix why this assert is tripped when the gold standard,
     // MPM/ARL/NanoPillar2D_FBC_sym.ups is run using a non-optimized build.
+    // Clue: This assertion is tripped if there are two modifies() in a single task.
     ASSERT(!result || p->m_comp->m_task->getSortedOrder() != result->m_comp->m_task->getSortedOrder());
 
     if (p->m_comp->m_task->getSortedOrder() < req->m_task->getSortedOrder()) {
