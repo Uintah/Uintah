ba83bc32aed663d057c38d65b8105feb34ee6437
Todd Harman <harman@sci.utah.edu>
2017-09-08 16:09:42 +0000

    Formatting in the output file mem_leak_check.log
    Fixed syntax error
    
    cd to $HERE after merging the malloc_stats files in the gold standards dirs.

--- src/R_Tester/helpers/highwater_percent.pl
+++ src/R_Tester/helpers/highwater_percent.pl
@@ -6,43 +6,50 @@ $comparefilename = $ARGV[1];
 $test_highwater = 0;
 $compare_highwater = 0;
 
+#__________________________________
+print STDERR "    Reading the files (" . $testfilename ." " . $comparefilename . " ).\n";
 open(TESTFILE, $testfilename);
-open(COMPAREFILE, $comparefilename);
 
 @test_text = <TESTFILE>;
 $test_text = join("", @test_text);
-while ($test_text =~ /highwater alloc:\s*(\w+)\s/) {
+while ( $test_text =~ /highwater alloc:\s*(\w+)\s/ ) {
     $test_highwater += $1;
     $test_text = $';
 }
+close(TESTFILE);
+print STDERR "    New total memory highwater " . $test_highwater . "\n";
+
+
+open(COMPAREFILE, $comparefilename);
 
 @compare_text = <COMPAREFILE>;
 $compare_text = join("", @compare_text);
-while ($compare_text =~ /highwater alloc:\s*(\w+)\s/) {
+while ( $compare_text =~ /highwater alloc:\s*(\w+)\s/ ) {
     $compare_highwater += $1;
     $compare_text = $';
 }
+close(COMPAREFILE);
+print STDERR "    GS total memory highwater " . $compare_highwater . "\n";
+
+#__________________________________
 
 # bulletproofing
-if ($compare_highwater == 0 || $test_highwater == 0) {
-  print STDERR "    Memory high water test failed.  Error reading the files (" . $testfilename ." " . $comparefilename . " )."
+if ( $compare_highwater == 0 || $test_highwater == 0 ) {
+  print STDERR "    Memory high water test failed.  Error reading the files (" . $testfilename ." " . $comparefilename . " ).\n";
   exit 2 
 }
 
-print STDERR "New total memory highwater " . $test_highwater . "\n";
-print STDERR "Old total memory highwater " . $compare_highwater . "\n";
 
-if ($compare_highwater == 0 && $test_highwater > 0) {
+if ( $compare_highwater == 0 && $test_highwater > 0 ) {
     $percent = 99999;
     $abs = 0;
 }
-elsif ($test_highwater == 0 && $compare_highwater > 0) {
+elsif ( $test_highwater == 0 && $compare_highwater > 0 ) {
     $percent = -99999;
     $abs = 0;
 }
-elsif ($test_highwater > 0 && $compare_highwater > 0) {
-    $percent = int(($test_highwater - $compare_highwater) / 
-		   $compare_highwater * 100 + 0.5);
+elsif ( $test_highwater > 0 && $compare_highwater > 0 ) {
+    $percent = int(($test_highwater - $compare_highwater) / $compare_highwater * 100 + 0.5);
     $abs = $test_highwater - $compare_highwater;
 }
 else {
@@ -51,13 +58,13 @@ else {
 }
  
 if ($percent > 0) {
-    print STDERR "Memory usage increased (worsened) by %" . $percent . " " . $abs . " bytes\n";
+    print STDERR "    Memory usage increased (worsened) by %" . $percent . " " . $abs . " bytes\n";
 }
 elsif ($percent < 0) {
-    print STDERR "Memory usage decreased (improved) by %" . -$percent . " " . $abs . " bytes\n";
+    print STDERR "    Memory usage decreased (improved) by %" . -$percent . " " . $abs . " bytes\n";
 }
 else {
-    print STDERR "Memory usage stayed the same.\n"
+    print STDERR "    Memory usage stayed the same.\n"
 }
 print "$percent $abs $test_highwater";
 exit 0
