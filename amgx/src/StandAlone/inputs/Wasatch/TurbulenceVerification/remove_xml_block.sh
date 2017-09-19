34cb419c8981426cdd5ea1338d90653fc46f302f
John Schmidt <jas@sci.utah.edu>
2017-09-19 18:40:47 +0000

    Remove amgx branch.
    Former-commit-id: 9a13006d6a06a3aac7219e6a6dba0a53bfdcb357

--- amgx/src/StandAlone/inputs/Wasatch/TurbulenceVerification/remove_xml_block.sh
+++
@@ -1,4 +0,0 @@
-# usage: ./remove_xml_block blockToRemove
-# example: ./remove_xml_block Kolmogorov  - will remove all LINES that contains the word Kolmogorov
-echo Removing all lines that contain $1
-sed -i '/'$1'/d' *.ups
