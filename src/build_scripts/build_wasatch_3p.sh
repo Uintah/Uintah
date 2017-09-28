d61631545346ccd9b0b1a83b1de423c4e665d6d0
James Sutherland <jsutherland@sci.utah.edu>
2017-09-28 22:44:31 +0000

    Update to new SpatialOps

--- src/build_scripts/build_wasatch_3p.sh
+++ src/build_scripts/build_wasatch_3p.sh
@@ -88,7 +88,7 @@ fi
 ###########################################################################
 # GIT Hash Tags for the various libraries
 
-SPATIAL_OPS_TAG=f54e87c63ad50b9fb05634455cacb7d0ab44e7eb
+SPATIAL_OPS_TAG=68036ba62662eed2e5e17bdbc006a5fbf05bdaa2
 TAB_PROPS_TAG=0a671fcdaf53cb517666ba760513a7ce141ecb6e
 RAD_PROPS_TAG=a2626cdccfbb97ccb7d68d43bfe085194cccf4d5
 EXPR_LIB_TAG=5e613b3ea02014ca5a568734bde2ca82f8a5f7db
