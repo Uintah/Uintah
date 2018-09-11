de5acb2d6ca95fd4bf16f9ecef7025721cd043fa
Josh McConnell <mcconnell@sci.utah.edu>
2018-09-11 17:30:41 +0000

    Tag
    Former-commit-id: 759cf9fef0cb4ac88825da2fab80b9085d93a24b

--- src/CCA/Components/Wasatch/Transport/LowMachMomentumTransportEquation.cc
+++ src/CCA/Components/Wasatch/Transport/LowMachMomentumTransportEquation.cc
@@ -116,7 +116,6 @@ namespace WasatchCore{
     //__________________
     // Pressure source term        
     if( !factory.have_entry( tagNames.pressuresrc ) ){
-      const Expr::Tag densNP1Tag  = Expr::Tag(densTag.name(), Expr::STATE_NP1);
       
       // register the expression for pressure source term
       Expr::TagList psrcTagList;
@@ -140,7 +139,7 @@ namespace WasatchCore{
                                                                                   tagNames.divu,
                                                                                   isConstDensity,
                                                                                   densTag,
-                                                                                  densNP1Tag ) );
+                                                                                  densNP1Tag_ ) );
       
       factory.cleave_from_parents( psrcID  );
       factory.cleave_from_children( psrcID  );
