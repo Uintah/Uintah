ea33d9e6091a7beafb5d51c8469c51d04d384de4
John Holmen <jholmen@sci.utah.edu>
2019-01-23 20:53:41 +0000

    * Merge of trunk with /branches/kokkos_dev/src (svn merge -r59174:59176 https://gforge.sci.utah.edu/svn/uintah/trunk/src).
    Former-commit-id: d052826ec34708318212e5bbfd1992744b37ab63

--- src/Core/Grid/Variables/VarLabelMatlMemspace.h
+++ src/Core/Grid/Variables/VarLabelMatlMemspace.h
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright (c) 1997-2018 The University of Utah
+ * Copyright (c) 1997-2019 The University of Utah
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
@@ -21,7 +21,6 @@
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  * IN THE SOFTWARE.
  */
-
 
 #ifndef UINTAH_HOMEBREW_VarLabelMatlMemspace_H
 #define UINTAH_HOMEBREW_VarLabelMatlMemspace_H
