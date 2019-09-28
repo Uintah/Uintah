0f1dab8eeb16dfe593bc2a4c6cebb176196d050e
Jeremy Nicholas Thornock <jthornoc@sci.utah.edu>
2019-09-28 03:48:31 +0000

    Removing an unused solver.
    Former-commit-id: 0cd051c5f1ec42005f48f253283228b942d8b34b

--- src/CCA/Components/Arches/KokkosSolver.h
+++ src/CCA/Components/Arches/KokkosSolver.h
@@ -95,9 +95,6 @@ namespace Uintah{
   /** @brief Solve the system with an SSP-RK method, Gottlieb et al, 2001, SIAM Review **/
   void SSPRKSolve( const LevelP& level, SchedulerP& sched );
 
-  /** @brief Solve the system with an SSP-RK method, Gottlieb et al, 2001, SIAM Review : using production code algorithm**/
-  void SSPRKv2Solve( const LevelP& level, SchedulerP& sched );
-
   /** @brief A Sandbox solver **/
   void SandBox( const LevelP& level, SchedulerP& sched );
 
@@ -127,14 +124,12 @@ namespace Uintah{
   private:
 
     /** @brief Determines/schedules the work performed for the timestep **/
-    enum NONLINEARSOLVER {SSPRK, SANDBOX, HELIUM_PLUME};
+    enum NONLINEARSOLVER {SSPRK, SANDBOX};
 
     /** @brief Map a string value to the enum for the solver type **/
     void setSolver( std::string solver_string ){
       if ( solver_string == "ssprk" ){
         m_nonlinear_solver =  SSPRK;
-      } else if ( solver_string == "helium_plume" ){
-        m_nonlinear_solver = HELIUM_PLUME;
       } else if ( solver_string == "sandbox" ){
         m_nonlinear_solver = SANDBOX;
       } else {
