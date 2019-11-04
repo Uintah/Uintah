85696ece7bb327527e2e8a3eff08d46995d7773b
Todd Harman <harman@sci.utah.edu>
2019-11-04 17:33:25 +0000

    Added comments. Finished populating the ghost type names.
    Former-commit-id: 2d37caa4e3fcf14bca20c5a6679249ec7b83793f

--- src/Core/Grid/Ghost.cc
+++ src/Core/Grid/Ghost.cc
@@ -27,24 +27,44 @@
 using namespace Uintah;
 
 
-std::string Ghost::names[numGhostTypes] =
-{ "None", "nodes", "cells", "x faces", "y faces", "z faces", "all faces" };
-  
+std::string Ghost::names[numGhostTypes] ={
+  "None",
+  "nodes",
+  "cells",
+  "x faces",
+  "y faces",
+  "z faces",
+  "all faces",
+  "xpypzp",
+  "xpypzm",
+  "xpymzp",
+  "xpymzm",
+  "xmypzp",
+  "xmypzm",
+  "xmymzp",
+  "xmymzm",
+};
+
 
 IntVector Ghost::directions[numGhostTypes] =
-{ IntVector(0,0,0), IntVector(1,1,1), IntVector(1,1,1),
-  IntVector(1,0,0), IntVector(0,1,0), IntVector(0,0,1),
-  IntVector(1,1,1),
+{
+  IntVector(0,0,0),     // None
+  IntVector(1,1,1),     // AroundNodes
+  IntVector(1,1,1),     // AroundCells
+  IntVector(1,0,0),     // AroundFacesX
+  IntVector(0,1,0),     // AroundFacesY
+  IntVector(0,0,1),     // AroundFacesZ
+  IntVector(1,1,1),     // AroundFaces
   // One sided ghosts
-  IntVector(1,1,1),
-  IntVector(1,1,0),
-  IntVector(1,0,1),
-  IntVector(1,0,0),
-  IntVector(0,1,1),
-  IntVector(0,1,0),
-  IntVector(0,0,1),
-  IntVector(0,0,0)  
-  };
+  IntVector(1,1,1),     // xpypzp
+  IntVector(1,1,0),     // xpypzm
+  IntVector(1,0,1),     // xpymzp
+  IntVector(1,0,0),     // xpymzm
+  IntVector(0,1,1),     // xmypzp
+  IntVector(0,1,0),     // xmypzm
+  IntVector(0,0,1),     // xmymzp
+  IntVector(0,0,0)      // xmymzm
+};
 
 
 
