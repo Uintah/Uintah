ba0cbdc9ee909203ac755acb09230ce09d20206a
Allen Sanderson <allen@sci.utah.edu>
2019-11-13 01:56:21 +0000

    clean up for the task infomapper
    Former-commit-id: 48e74119d89291f3299ee9fea2c4dca0c54d7a70

--- src/VisIt/libsim/visit_libsim_database.h
+++ src/VisIt/libsim/visit_libsim_database.h
@@ -201,9 +201,9 @@ void addVectorReductionStats( visit_handle md, VectorInfoMapper< ENUM, T > stats
 }
 
 template< class KEY, class ENUM, class T >
-void addMapStats( const visit_handle md, MapInfoMapper< KEY, ENUM, T > stats,
-                  std::string statName, std::string statIndex,
-                  std::string meshName )
+void addMapAllStats( const visit_handle md, MapInfoMapper< KEY, ENUM, T > stats,
+		     std::string statName, std::string statIndex,
+		     std::string meshName )
 {
   if( stats.size() == 0 )
     return;
@@ -235,6 +235,52 @@ void addMapStats( const visit_handle md, MapInfoMapper< KEY, ENUM, T > stats,
       VisIt_VariableMetaData_setTreatAsASCII(vmd, false);
       
       VisIt_SimulationMetaData_addVariable(md, vmd);
+    }
+  }
+}
+
+template< class KEY, class ENUM, class T >
+void addMapIndividualStats( const visit_handle md, MapInfoMapper< KEY, ENUM, T > stats,
+			    std::string statName, std::string statIndex,
+			    std::string meshName )
+{
+  if( stats.size() == 0 )
+    return;
+  
+  for( unsigned int k=0; k<stats.size(); ++k )
+  {
+    KEY key = stats.getKey(k);
+
+    std::ostringstream keyName;
+    keyName << key;
+    
+    for( unsigned int i=0; i<stats[key].size(); ++i )
+    {
+      visit_handle vmd = VISIT_INVALID_HANDLE;
+      
+      if(VisIt_VariableMetaData_alloc(&vmd) == VISIT_OKAY)
+      {
+	std::string tmp_name = statName +
+	  keyName.str() + "/" + stats[key].getName( i );
+	
+	if( statIndex.size() )
+	  tmp_name += statIndex;
+
+	std::string units = stats[key].getUnits( i );
+	
+	VisIt_VariableMetaData_setName(vmd, tmp_name.c_str());
+	VisIt_VariableMetaData_setMeshName(vmd, meshName.c_str());
+	VisIt_VariableMetaData_setCentering(vmd, VISIT_VARCENTERING_ZONE);
+	VisIt_VariableMetaData_setType(vmd, VISIT_VARTYPE_SCALAR);
+	VisIt_VariableMetaData_setNumComponents(vmd, 1);
+	VisIt_VariableMetaData_setUnits(vmd, units.c_str());
+	
+	// ARS - FIXME
+	//      VisIt_VariableMetaData_setHasDataExtents(vmd, false);
+	VisIt_VariableMetaData_setTreatAsASCII(vmd, false);
+	
+	VisIt_SimulationMetaData_addVariable(md, vmd);
+      }
     }
   }
 }
