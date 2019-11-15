4ca6c0dadcb33693d0e1d642cb9940f2b4836c53
Jeremy Nicholas Thornock <jthornoc@sci.utah.edu>
2019-11-15 23:00:58 +0000

    Adding the option of saving out specific radiation intensities. Updated an RT input file which will cause this test to fail due to the addition of new saved variables.
    Former-commit-id: c70b52555f3855d96d51be26b3f870d5a5533c3c

--- src/CCA/Components/Arches/UPSHelper.h
+++ src/CCA/Components/Arches/UPSHelper.h
@@ -19,7 +19,7 @@ namespace Uintah{ namespace ArchesCore {
                     DENSITY_ROLE,
             TOTAL_VISCOSITY_ROLE };
 
-  /** @brief Map a string to an enum **/ 
+  /** @brief Map a string to an enum **/
   CFD_ROLE role_string_to_enum( const std::string role );
 
   /** @brief Map a enum to a string **/
@@ -27,6 +27,12 @@ namespace Uintah{ namespace ArchesCore {
 
   /** @brief Parse the VarID section in the UPS file for a specific CFD role **/
   std::string parse_ups_for_role( CFD_ROLE role_enum, ProblemSpecP db, std::string mydefault="NotSet"  );
+
+  /** @brief Parse the dataArchiver save labels for this variable **/
+  // istart and iend are the lengths for string compare if partial match is true
+  std::vector<bool> save_in_archiver( const std::vector<std::string> variables,
+                                      ProblemSpecP& db, bool partial_match=false,
+                                      const int starting_pos_in=0, const int starting_pos_lab=0 );
 
   /** @brief Find a node with a specific attribute **/
   /** Use the *=> combination of characters to delineate between nodes. **/
