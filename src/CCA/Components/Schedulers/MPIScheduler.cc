2743dd12df0de37723009089a1825e9a69ae4c87
John Holmen <jholmen@sci.utah.edu>
2020-01-17 04:20:23 +0000

    * Merge of trunk with /branches/kokkos_dev/src (svn merge -r59951:59956 https://gforge.sci.utah.edu/svn/uintah/trunk/src).
    Former-commit-id: 4b2a209b7591edd5395e0d7b18ebbfdf067540bc

--- src/CCA/Components/Schedulers/MPIScheduler.cc
+++ src/CCA/Components/Schedulers/MPIScheduler.cc
@@ -875,6 +875,7 @@ MPIScheduler::execute( int tgnum     /* = 0 */
     }
     else {
       initiateTask( dtask, abort, abort_point, iteration );
+      dtask->markInitiated();
       processMPIRecvs( WAIT_ALL );
       ASSERT( m_recvs.size() == 0u );
       runTask( dtask, iteration );
