95d6325496dcb1d26add74b5abac7d6212c26073
James Guilkey <guilkey@maple.mech.utah.edu>
2020-04-18 10:41:37 -0600

    Add bulletproofing to catch triangles that have zero area, e.g., all the points are on a line.  Throw an exception if this is detected.

--- src/Core/GeometryPiece/UniformGrid.cc
+++ src/Core/GeometryPiece/UniformGrid.cc
@@ -23,6 +23,7 @@
  */
 
 #include <Core/GeometryPiece/UniformGrid.h>
+#include <Core/Exceptions/ProblemSetupException.h>
 #include <Core/Malloc/Allocator.h>
 #include <iostream>
 #include <map>
@@ -67,6 +68,17 @@ list<Tri> Tri::makeTriList(vector<IntVector>& tris ,vector<Point>& pts)
   list<Tri> tri_list;
   vector<IntVector>::const_iterator tri_itr;
   for (tri_itr = tris.begin(); tri_itr != tris.end(); ++tri_itr) {
+    Vector A = pts[(*tri_itr).y()] - pts[(*tri_itr).x()];
+    Vector B = pts[(*tri_itr).z()] - pts[(*tri_itr).x()];
+    double areaX2 = Cross(A,B).length();
+    if(areaX2==0.0){
+      std::ostringstream warn;
+      warn << "\n ERROR: triangle with points: \n"
+           << pts[(*tri_itr).x()] << " " << pts[(*tri_itr).y()]
+                                  << " " << pts[(*tri_itr).z()]
+         << ").\n  has zero area.\n";
+      throw ProblemSetupException(warn.str(),__FILE__, __LINE__);
+    }
     Tri triangle = Tri(pts[(*tri_itr).x()],pts[(*tri_itr).y()],
                        pts[(*tri_itr).z()]);
     tri_list.push_back(triangle);
