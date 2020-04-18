dd1fd11c066fdb4c07461b1c1b09214aa7525cdc
James Guilkey <guilkey@maple.mech.utah.edu>
2020-04-18 16:16:04 -0600

    Add bulletproofing to catch triangles that are too large relative to the grid cell size.

--- src/CCA/Components/MPM/Triangle/Triangle.cc
+++ src/CCA/Components/MPM/Triangle/Triangle.cc
@@ -59,6 +59,8 @@ Triangle::createTriangles(TriangleMaterial* matl,
                           const string fileroot)
 {
   int dwi = matl->getDWIndex();
+  Vector dx = patch->dCell();
+  double gridLength = dx.minComponent();
   Matrix3 Identity; Identity.Identity();
   ParticleSubset* subset = allocateVariables(numTriangles,dwi,patch,new_dw);
 
@@ -80,119 +82,130 @@ Triangle::createTriangles(TriangleMaterial* matl,
                                                          __FILE__, __LINE__);
   }
 
-    // Read in pts files
-    vector<double> px, py, pz;
-    double p1,p2,p3 = 0.0;
-    particleIndex start = 0;
-    int numpts = 0;
-    while (pts >> p1 >> p2 >> p3) {
-     px.push_back(p1);
-     py.push_back(p2);
-     pz.push_back(p3);
-     numpts++;
-    } // while lines in the pts file
+  // Read in pts files
+  vector<double> px, py, pz;
+  double p1,p2,p3 = 0.0;
+  particleIndex start = 0;
+  int numpts = 0;
+  while (pts >> p1 >> p2 >> p3) {
+   px.push_back(p1);
+   py.push_back(p2);
+   pz.push_back(p3);
+   numpts++;
+  } // while lines in the pts file
 
-    // Create a set to hold the triangle indices for each point
-    // Also, create a place to hold the "vertex area" for each point
-    vector<set<int> > triangles(numpts);
-    vector<double> ptArea(numpts);
+  // Create a set to hold the triangle indices for each point
+  // Also, create a place to hold the "vertex area" for each point
+  vector<set<int> > triangles(numpts);
+  vector<double> ptArea(numpts);
 
-    // Read in tri file
-    // Put the triangle index into a set 
-    // Keep track of which triangles each point is part of by inserting
-    // the triangle index into each set
-    // Compute the area of each triangle
-    vector<int> i0, i1, i2;
-    vector<long64> TID;
-    int ip0,ip1,ip2;
-    unsigned int numtri = 0;
-    vector<double> triAreaNow(numtri);
-    while (tri >> ip0 >> ip1 >> ip2) {
-     long64 tid = numtri;
-     i0.push_back(ip0);
-     i1.push_back(ip1);
-     i2.push_back(ip2);
-     TID.push_back(tid);
-     triangles[ip0].insert(numtri);
-     triangles[ip1].insert(numtri);
-     triangles[ip2].insert(numtri);
-     Point P0(px[ip0], py[ip0], pz[ip0]);
-     Point P1(px[ip1], py[ip1], pz[ip1]);
-     Point P2(px[ip2], py[ip2], pz[ip2]);
-     Vector A = P1-P0;
-     Vector B = P2-P0;
-     triAreaNow.push_back(0.5*Cross(A,B).length());
-     numtri++;
-    } // while lines in the tri file
+  // Read in tri file
+  // Put the triangle index into a set 
+  // Keep track of which triangles each point is part of by inserting
+  // the triangle index into each set
+  // Compute the area of each triangle
+  vector<int> i0, i1, i2;
+  vector<long64> TID;
+  int ip0,ip1,ip2;
+  unsigned int numtri = 0;
+  vector<double> triAreaNow(numtri);
+  while (tri >> ip0 >> ip1 >> ip2) {
+   long64 tid = numtri;
+   i0.push_back(ip0);
+   i1.push_back(ip1);
+   i2.push_back(ip2);
+   TID.push_back(tid);
+   triangles[ip0].insert(numtri);
+   triangles[ip1].insert(numtri);
+   triangles[ip2].insert(numtri);
+   Point P0(px[ip0], py[ip0], pz[ip0]);
+   Point P1(px[ip1], py[ip1], pz[ip1]);
+   Point P2(px[ip2], py[ip2], pz[ip2]);
+   Vector A = P1-P0;
+   Vector B = P2-P0;
+   Vector C = P2-P1;
+   if(A.length() > 3.*gridLength || 
+      B.length() > 3.*gridLength ||
+      C.length() > 3.*gridLength){
+    ostringstream warn;
+    warn <<"Triangle " << numtri << " in " << trifilename 
+         << " is too large relative to the grid cell size\n" 
+         << "Its points are = \n"
+         << P0 << "\n" << P1 << "\n" << P2 << "\n";
+    throw ProblemSetupException(warn.str(), __FILE__, __LINE__);
+   }
+   triAreaNow.push_back(0.5*Cross(A,B).length());
+   numtri++;
+  } // while lines in the tri file
 
-    // Compute the area at each vertex by putting 1/3 of the area of each
-    // triangle that touches a point into ptArea
-    double totalArea = 0;
-    for(int i = 0; i<numpts; i++){
-      for (set<int>::iterator it1 = triangles[i].begin(); 
-                              it1!= triangles[i].end();  it1++){
-       ptArea[i]+=triAreaNow[*it1]/3.; 
-      }
-      totalArea+=ptArea[i];
-      if(triangles[i].size() > 30){
-         cout << "This node has " << triangles[i].size() << " triangles" << endl;
-      }
-    } // while lines in the pts file
-
-    vector<int>    useInPen(numpts,1);
-
-    // make triangles from subsequent points if their midpoint is on patch
-    // Choose which points to use in penalty contact so that each point is
-    // used just once.
-    for(unsigned int i = 0; i<numtri; i++){
-      Point P0(px[i0[i]], py[i0[i]], pz[i0[i]]);
-      Point P1(px[i1[i]], py[i1[i]], pz[i1[i]]);
-      Point P2(px[i2[i]], py[i2[i]], pz[i2[i]]);
-      Point test((px[i0[i]]+px[i1[i]]+px[i2[i]])/3.,
-                 (py[i0[i]]+py[i1[i]]+py[i2[i]])/3.,
-                 (pz[i0[i]]+pz[i1[i]]+pz[i2[i]])/3.);
-      if(patch->containsPoint(test)){
-        particleIndex pidx   = start;
-        triangle_pos[pidx]   = test;
-        triangleID[pidx]     = TID[i];
-        triangleArea[pidx]   = triAreaNow[i];
-        triangleMidToNode0[pidx] = P0 - test;
-        triangleMidToNode1[pidx] = P1 - test;
-        triangleMidToNode2[pidx] = P2 - test;
-
-        triangleAreaAtNodes[pidx]+=Vector(ptArea[i0[i]], 
-                                          ptArea[i1[i]], 
-                                          ptArea[i2[i]]);
-
-        triangleUseInPenalty[pidx] = IntVector(useInPen[i0[i]],
-                                               useInPen[i1[i]],
-                                               useInPen[i2[i]]);
-        useInPen[i0[i]]=0;
-        useInPen[i1[i]]=0;
-        useInPen[i2[i]]=0;
-
-        triangleSize[pidx]    = Identity;
-        triangleDefGrad[pidx] = Identity;
-        start++;
-#if 0
-        Vector r0 = P1 - P0;
-        Vector r1 = P2 - P0;
-        Vector r2 = -.1*Cross(r1,r0);
-        Matrix3 size =Matrix3(r0.x()/dx.x(), r1.x()/dx.x(), r2.x()/dx.x(),
-                              r0.y()/dx.y(), r1.y()/dx.y(), r2.y()/dx.y(),
-                              r0.z()/dx.z(), r1.z()/dx.z(), r2.z()/dx.z());
-
-        double Jsize = size.Determinant();
-        if(Jsize <= 0.0){
-         cout << "negative J" << endl;
-        }
-        triangleSize[pidx]    = size;
-#endif
-      }
+  // Compute the area at each vertex by putting 1/3 of the area of each
+  // triangle that touches a point into ptArea
+  double totalArea = 0;
+  for(int i = 0; i<numpts; i++){
+    for (set<int>::iterator it1 = triangles[i].begin(); 
+                            it1!= triangles[i].end();  it1++){
+     ptArea[i]+=triAreaNow[*it1]/3.; 
     }
+    totalArea+=ptArea[i];
+    if(triangles[i].size() > 30){
+       cout << "This node has " << triangles[i].size() << " triangles" << endl;
+    }
+  } // while lines in the pts file
 
-    tri.close();
-    pts.close();
+  vector<int>    useInPen(numpts,1);
+
+  // make triangles from subsequent points if their midpoint is on patch
+  // Choose which points to use in penalty contact so that each point is
+  // used just once.
+  for(unsigned int i = 0; i<numtri; i++){
+    Point P0(px[i0[i]], py[i0[i]], pz[i0[i]]);
+    Point P1(px[i1[i]], py[i1[i]], pz[i1[i]]);
+    Point P2(px[i2[i]], py[i2[i]], pz[i2[i]]);
+    Point test((px[i0[i]]+px[i1[i]]+px[i2[i]])/3.,
+               (py[i0[i]]+py[i1[i]]+py[i2[i]])/3.,
+               (pz[i0[i]]+pz[i1[i]]+pz[i2[i]])/3.);
+    if(patch->containsPoint(test)){
+      particleIndex pidx   = start;
+      triangle_pos[pidx]   = test;
+      triangleID[pidx]     = TID[i];
+      triangleArea[pidx]   = triAreaNow[i];
+      triangleMidToNode0[pidx] = P0 - test;
+      triangleMidToNode1[pidx] = P1 - test;
+      triangleMidToNode2[pidx] = P2 - test;
+
+      triangleAreaAtNodes[pidx]+=Vector(ptArea[i0[i]], 
+                                        ptArea[i1[i]], 
+                                        ptArea[i2[i]]);
+
+      triangleUseInPenalty[pidx] = IntVector(useInPen[i0[i]],
+                                             useInPen[i1[i]],
+                                             useInPen[i2[i]]);
+      useInPen[i0[i]]=0;
+      useInPen[i1[i]]=0;
+      useInPen[i2[i]]=0;
+
+      triangleSize[pidx]    = Identity;
+      triangleDefGrad[pidx] = Identity;
+      start++;
+#if 0
+      Vector r0 = P1 - P0;
+      Vector r1 = P2 - P0;
+      Vector r2 = -.1*Cross(r1,r0);
+      Matrix3 size =Matrix3(r0.x()/dx.x(), r1.x()/dx.x(), r2.x()/dx.x(),
+                            r0.y()/dx.y(), r1.y()/dx.y(), r2.y()/dx.y(),
+                            r0.z()/dx.z(), r1.z()/dx.z(), r2.z()/dx.z());
+
+      double Jsize = size.Determinant();
+      if(Jsize <= 0.0){
+       cout << "negative J" << endl;
+      }
+      triangleSize[pidx]    = size;
+#endif
+    }
+  }
+
+  tri.close();
+  pts.close();
 
   return subset;
 }
