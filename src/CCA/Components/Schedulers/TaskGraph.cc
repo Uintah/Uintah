34219fa2d26fba7db5769e6fef9c8c31afea7952
damodars <damodars@sci.utah.edu>
2020-06-03 14:43:58 -0600

    Reverted TaskGraph.cc from last commit to avoid errors in arches examples.
    Call overrideGhostCells on for cuda and not for cpu version.

--- src/CCA/Components/Schedulers/TaskGraph.cc
+++ src/CCA/Components/Schedulers/TaskGraph.cc
@@ -233,8 +233,9 @@ TaskGraph::createDetailedTasks(       bool    useInternalDeps
 
   nullSort(sorted_tasks);
 
+#ifdef HAVE_CUDA
   overrideGhostCells(sorted_tasks); //DS: 01042020: fix for OnDemandDW race condition - only if using device. Do not disturb legacy CPU execution
-
+#endif
 //  topologicalSort(sorted_tasks);
 
   ASSERT(grid != nullptr);
