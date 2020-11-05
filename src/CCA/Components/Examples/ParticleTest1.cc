2de1aeceed68a90db281c1885a6081ef4b936ed2
Todd Harman <t.harman@utah.edu>
2020-11-05 11:46:42 -0700

    Further work on Jim's bug.  No answers.

--- src/CCA/Components/Examples/ParticleTest1.cc
+++ src/CCA/Components/Examples/ParticleTest1.cc
@@ -25,17 +25,19 @@
 
 #include <CCA/Components/Examples/ParticleTest1.h>
 #include <CCA/Components/Examples/ExamplesLabel.h>
-#include <Core/ProblemSpec/ProblemSpec.h>
+
+#include <CCA/Ports/Scheduler.h>
+#include <Core/Exceptions/InternalError.h>
+#include <Core/Grid/Level.h>
+#include <Core/Grid/MaterialManager.h>
+#include <Core/Grid/SimpleMaterial.h>
+#include <Core/Grid/Task.h>
 #include <Core/Grid/Variables/NCVariable.h>
 #include <Core/Grid/Variables/NodeIterator.h>
-#include <Core/Grid/MaterialManager.h>
-#include <Core/Grid/Task.h>
-#include <Core/Grid/Level.h>
-#include <Core/Grid/SimpleMaterial.h>
 #include <Core/Grid/Variables/VarTypes.h>
-#include <Core/Parallel/ProcessorGroup.h>
-#include <CCA/Ports/Scheduler.h>
 #include <Core/Malloc/Allocator.h>
+#include <Core/Parallel/ProcessorGroup.h>
+#include <Core/ProblemSpec/ProblemSpec.h>
 #include <Core/Util/DOUT.hpp>
 
 #include <iostream>
@@ -105,20 +107,20 @@ void ParticleTest1::scheduleComputeStableTimeStep(const LevelP& level,
 //______________________________________________________________________
 //
 void
-ParticleTest1::scheduleTimeAdvance( const LevelP& level, 
+ParticleTest1::scheduleTimeAdvance( const LevelP& level,
                                     SchedulerP  & sched)
 {
   schedTask1( level, sched);
-  
+
   schedTask2( level, sched);
-  
+
   //__________________________________
   //
   lb_->d_particleState.clear();
   lb_->d_particleState_preReloc.clear();
 
   const MaterialSet* matls = m_materialManager->allMaterials();
-  
+
   for (int m = 0; m < matls->size(); m++) {
     vector<const VarLabel*> vars;
     vector<const VarLabel*> vars_preReloc;
@@ -161,7 +163,7 @@ void ParticleTest1::initialize(const ProcessorGroup*,
     const Patch* patch = patches->get(p);
     const Point low = patch->cellPosition(patch->getCellLowIndex());
     const Point high = patch->cellPosition(patch->getCellHighIndex());
-    
+
     for(int m = 0;m<matls->size();m++){
       srand(1);
       const int numParticles = 10;
@@ -180,10 +182,10 @@ void ParticleTest1::initialize(const ProcessorGroup*,
         //const Point pos( (((float) rand()) / RAND_MAX * ( high.x() - low.x()-1) + low.x()),
         //                 (((float) rand()) / RAND_MAX * ( high.y() - low.y()-1) + low.y()),
        //                  (((float) rand()) / RAND_MAX * ( high.z() - low.z()-1) + low.z()) );
-                         
+
         const Point pos( ( ((double) i/numParticles)   * ( high.x() - low.x()-1) + low.x() ),
                          ( ((float) rand()) / RAND_MAX * ( high.y() - low.y()-1) + low.y()),
-                         ( ((float) rand()) / RAND_MAX * ( high.z() - low.z()-1) + low.z()) );                  
+                         ( ((float) rand()) / RAND_MAX * ( high.z() - low.z()-1) + low.z()) );
         px[i] = pos;
         pids[i] = patch->getID()*numParticles+i;
         pmass[i] = ((float) rand()) / RAND_MAX * 10;
@@ -195,27 +197,25 @@ void ParticleTest1::initialize(const ProcessorGroup*,
 //______________________________________________________________________
 //
 void
-ParticleTest1::schedTask1( const LevelP & level, 
+ParticleTest1::schedTask1( const LevelP & level,
                            SchedulerP   & sched)
 {
   Task* task = scinew Task("task1", this, &ParticleTest1::task1);
 
-  // set this in problemSetup.  0 is no ghost cells, 1 is all with 1 ghost
-  // atound-node, and 2 mixes them
   if (m_NumGC == 0) {
     task->requires( Task::OldDW, lb_->pParticleIDLabel, m_gn, 0 );
     task->requires( Task::OldDW, lb_->pXLabel,          m_gn, 0 );
     task->requires( Task::OldDW, lb_->pMassLabel,       m_gn, 0 );
   }
   else if (m_NumGC > 0) {
-    task->requires( Task::OldDW, lb_->pXLabel,          m_gac, m_NumGC );
-    task->requires( Task::OldDW, lb_->pMassLabel,       m_gac, m_NumGC );
-    task->requires( Task::OldDW, lb_->pParticleIDLabel, m_gac, m_NumGC );
+    task->requires( Task::OldDW, lb_->pParticleIDLabel, m_gn, 0 );
+    task->requires( Task::OldDW, lb_->pXLabel,          m_gac, m_NumGC+1 );
+    task->requires( Task::OldDW, lb_->pMassLabel,       m_gac, m_NumGC+1 );
   }
 
   task->computes( lb_->pXLabel_1 );
   task->computes( lb_->pMassLabel_1 );
-  task->computes( lb_->pParticleIDLabel_preReloc );
+
   sched->addTask(task, level->eachPatch(), m_materialManager->allMaterials());
 }
 
@@ -229,9 +229,9 @@ void ParticleTest1::task1(const ProcessorGroup * pg,
 {
   for( int p=0; p<patches->size(); ++p ){
     const Patch* patch = patches->get(p);
+
     for( int m = 0; m<matls->size(); ++m ){
       int matl = matls->get(m);
-
 
       // Get the arrays of particle values to be changed
       constParticleVariable<Point> px;
@@ -239,9 +239,7 @@ void ParticleTest1::task1(const ProcessorGroup * pg,
 
       constParticleVariable<double> pmass;
       ParticleVariable<double> pmass_1;
-
       constParticleVariable<long64> pids;
-      ParticleVariable<long64> pids_1;
 
       //__________________________________
       // particle sets
@@ -252,7 +250,7 @@ void ParticleTest1::task1(const ProcessorGroup * pg,
         pset_gc = pset_gn;
       }
       else if (m_NumGC >0 ) {
-        pset_gc = old_dw->getParticleSubset( matl, patch, m_gac, m_NumGC, lb_->pXLabel );
+        pset_gc = old_dw->getParticleSubset( matl, patch, m_gac, m_NumGC+1, lb_->pXLabel );
       }
 
       int rank = pg->myRank();
@@ -261,31 +259,36 @@ void ParticleTest1::task1(const ProcessorGroup * pg,
       ParticleSubset* delset = scinew ParticleSubset(0,matl,patch);
 
 
-      old_dw->get( pmass, lb_->pMassLabel,       pset_gc);        
-      old_dw->get( px,    lb_->pXLabel,          pset_gc);        
-      old_dw->get( pids,  lb_->pParticleIDLabel, pset_gc);        
+      old_dw->get( pmass, lb_->pMassLabel,       pset_gc);
+      old_dw->get( px,    lb_->pXLabel,          pset_gc);
+      old_dw->get( pids,  lb_->pParticleIDLabel, pset_gn);
 
-      new_dw->allocateAndPut( pmass_1, lb_->pMassLabel_1,       pset_gn);
-      new_dw->allocateAndPut( px_1,    lb_->pXLabel_1,          pset_gn);
-      new_dw->allocateAndPut( pids_1,  lb_->pParticleIDLabel_preReloc, pset_gn);
+      new_dw->allocateAndPut( pmass_1, lb_->pMassLabel_1, pset_gn);
+      new_dw->allocateAndPut( px_1,    lb_->pXLabel_1,    pset_gn);
 
       //__________________________________
-      // every timestep, move down the +x axis, and decay the mass a little bit
-      for(auto iter = pset_gn->begin();iter != pset_gn->end(); iter++){
+      //
+      auto i = pset_gn->begin();
+
+      for(auto iter = pset_gc->begin(); iter != pset_gc->end(); iter++){
         particleIndex idx = *iter;
+
         // Point pos( px[i].x() + .25, px[i].y(), px[i].z() );
         // pmass_1[i] = pmass[i] *.9;
-        
-        // do nothing
-        px_1[idx]    = px[idx];
-        pids_1[idx]  = pids[idx];
-        pmass_1[idx] = pmass[idx];
-       
-        if (m_doOutput) {
-         DOUT( true, "["<<rank<<"] Task 1 Patch " << patch->getID()
-                      << ": ID "   << pids_1[idx]
-                      << ", pos "  << px_1[idx]
-                      << ", mass " << pmass_1[idx] );
+
+        if ( patch->containsPoint(px[idx]) ){
+          // do nothing
+          px_1[*i]    = px[idx];
+          pmass_1[*i] = pmass[idx];
+
+          if (m_doOutput) {
+           DOUT( true, "["<<rank<<"] Task 1 Patch " << patch->getID()
+                        << ": i " << *i
+                        << ": ID "   << pids[*i]
+                        << ", pos "  << px_1[*i].x()
+                        << ", mass " << pmass_1[*i] );
+            ++i;
+          }
         }
       }
       new_dw->deleteParticles(delset);
@@ -296,7 +299,7 @@ void ParticleTest1::task1(const ProcessorGroup * pg,
 //______________________________________________________________________
 //
 void
-ParticleTest1::schedTask2( const LevelP & level, 
+ParticleTest1::schedTask2( const LevelP & level,
                            SchedulerP   & sched)
 {
   Task* task = scinew Task("task2", this, &ParticleTest1::task2);
@@ -304,19 +307,22 @@ ParticleTest1::schedTask2( const LevelP & level,
   // set this in problemSetup.  0 is no ghost cells, 1 is all with 1 ghost
   // atound-node, and 2 mixes them
   if (m_NumGC == 0) {
-    task->requires( Task::NewDW, lb_->pXLabel_1,                 m_gn, 0);      
-    task->requires( Task::NewDW, lb_->pMassLabel_1,              m_gn, 0);
-    task->requires( Task::NewDW, lb_->pParticleIDLabel_preReloc, m_gn, 0);    
+    task->requires( Task::NewDW, lb_->pXLabel_1,        m_gn, 0);
+    task->requires( Task::NewDW, lb_->pMassLabel_1,     m_gn, 0);
+    task->requires( Task::NewDW, lb_->pParticleIDLabel, m_gn, 0);
   }
 
   else if (m_NumGC >0) {
-    task->requires( Task::NewDW, lb_->pXLabel_1,                  m_gac, m_NumGC);     
-    task->requires( Task::NewDW, lb_->pMassLabel_1,               m_gac, m_NumGC);
-    task->requires( Task::NewDW, lb_->pParticleIDLabel_preReloc,  m_gac, m_NumGC);     
+    task->requires( Task::OldDW, lb_->pParticleIDLabel, m_gn, 0);
+    task->requires( Task::OldDW, lb_->pXLabel,          m_gac, m_NumGC);
+    task->requires( Task::NewDW, lb_->pXLabel_1,        m_gac, m_NumGC);
+    task->requires( Task::NewDW, lb_->pMassLabel_1,     m_gac, m_NumGC);
   }
+
   task->computes(lb_->pXLabel_preReloc);
-  task->computes(lb_->pMassLabel_preReloc);  
-  
+  task->computes(lb_->pMassLabel_preReloc);
+  task->computes( lb_->pParticleIDLabel_preReloc );
+
   sched->addTask(task, level->eachPatch(), m_materialManager->allMaterials());
 }
 
@@ -330,18 +336,19 @@ void ParticleTest1::task2(const ProcessorGroup * pg,
 {
   for( int p=0; p<patches->size(); ++p ){
     const Patch* patch = patches->get(p);
+
     for( int m = 0; m<matls->size(); ++m ){
       int matl = matls->get(m);
 
-      // Get the arrays of particle values to be changed
+      constParticleVariable<Point> px_old;
       constParticleVariable<Point> px_1;
       ParticleVariable<Point>      px_new;
 
       constParticleVariable<double> pmass_1;
       ParticleVariable<double>      pmass_new;
       constParticleVariable<long64> pids;
+      ParticleVariable<long64>      pids_new;
 
-      
       //__________________________________
       // particle sets
       ParticleSubset* pset_gc = nullptr;
@@ -358,28 +365,45 @@ void ParticleTest1::task2(const ProcessorGroup * pg,
       DOUT( true, "["<<rank<<"] Task2 pset_GC: " << *pset_gc);
       DOUT( true, "["<<rank<<"] Task2 pset_GN: " << *pset_gn);
 
-      new_dw->allocateAndPut(pmass_new, lb_->pMassLabel_preReloc, pset_gn);
-      new_dw->allocateAndPut(px_new,    lb_->pXLabel_preReloc,    pset_gn);
-      
-      new_dw->get( pmass_1, lb_->pMassLabel_1,       pset_gc);             
-      new_dw->get( px_1,    lb_->pXLabel_1,          pset_gc);        
-      new_dw->get( pids,    lb_->pParticleIDLabel_preReloc, pset_gc);     
+      new_dw->get( pmass_1, lb_->pMassLabel_1,       pset_gc);
+      new_dw->get( px_1,    lb_->pXLabel_1,          pset_gc);
+      old_dw->get( pids,    lb_->pParticleIDLabel,   pset_gn);
 
-      //__________________________________
-      // every timestep, move down the +x axis, and decay the mass a little bit
-      for(auto iter = pset_gn->begin();iter != pset_gn->end(); iter++){
+      new_dw->allocateAndPut( pmass_new, lb_->pMassLabel_preReloc, pset_gn);
+      new_dw->allocateAndPut( px_new,    lb_->pXLabel_preReloc,    pset_gn);
+      new_dw->allocateAndPut( pids_new,  lb_->pParticleIDLabel_preReloc, pset_gn);
+
+      pids_new.copyData(pids);
       
+      //__________________________________
+      // Move particles along +x axis, and decrement the mass
+
+      auto i = pset_gn->begin();
+
+      for(auto iter = pset_gc->begin(); iter != pset_gc->end(); iter++){
         particleIndex idx = *iter;
-        Point pos( px_1[idx].x() + .25, px_1[idx].y(), px_1[idx].z());
-        px_new[idx]    = pos;
-        pmass_new[idx] = pmass_1[idx] *.9;
+
+        //Point pos_new( px_1[idx].x() + 0.5, px_1[idx].y(), px_1[idx].z());
         
-        if (m_doOutput) {
-         DOUT( true, "["<<rank<<"] Task2 Patch " << patch->getID()
-                      << ": ID " << pids[idx]
-                      << ": px " << px_new[idx]
-                      << ", mass " << pmass_new[idx] );
+        Point pos_new(px_1[idx]);
+
+        if ( patch->containsPoint(px_1[idx]) ){
+          px_new[*i]    = pos_new;
+          pmass_new[*i] = pmass_1[idx] *.9;
+
+          if (m_doOutput) {
+           DOUT( true, "["<<rank<<"] Task2 Patch " << patch->getID()
+                        << ": i " << *i
+                        << ": ID " << pids[*i]
+                        << ": px " << px_new[*i].x()
+                        << ", mass " << pmass_new[*i] );
+          }
+         ++i;
         }
+      }
+
+      if( i != pset_gn->end() ){
+        throw InternalError("task2:: there's a problem with the pset_gn iterator", __FILE__, __LINE__);
       }
     }
   }
