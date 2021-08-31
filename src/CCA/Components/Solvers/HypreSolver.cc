d5a107b3e232826026f366cdcc51c751fb4b1a49
Damodar Sahasrabudhe <damodars@sci.utah.edu>
2021-08-31 15:55:23 -0600

    Another fix for MPI conflicts between Hypre and Uintah
    On few occasions while running on Frontera, one rank started Hypre task
    and was watining on MPI_reduce within Hypre, while other ranks started
    Uintah's reduction tasks waiting for MPI_reduce in it. So added
    task->usesMPI(true) in hypre task with a hope that the taskgraph and the
    scheduler will create a different phase for Hypre task and avoid these
    conflicts. Fixed scheduler logic to backtrack for hypre task to include
    phases.

--- src/CCA/Components/Solvers/HypreSolver.cc
+++ src/CCA/Components/Solvers/HypreSolver.cc
@@ -1564,6 +1564,8 @@ namespace Uintah {
     //  Computes and requires
     auto TaskDependencies = [&](Task* task) {
 
+      task->usesMPI(true);
+
       // Matrix A
       task->requires(which_A_dw, A_label, Ghost::None, 0);
 
