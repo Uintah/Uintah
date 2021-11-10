b6a652aad5afef6c09aa5524fa74c3b5c6d64aa4
Marta Garcia Martinez <mgarcia@anl.gov>
2021-11-10 15:11:18 -0600

    Adding Kokkos generic tags as comments

--- src/CCA/Components/Arches/Transport/VelRhoHatBC.cc
+++ src/CCA/Components/Arches/Transport/VelRhoHatBC.cc
@@ -31,9 +31,11 @@ TaskAssignedExecutionSpace VelRhoHatBC::loadTaskInitializeFunctionPointers()
 TaskAssignedExecutionSpace VelRhoHatBC::loadTaskEvalFunctionPointers()
 {
   return create_portable_arches_tasks<TaskInterface::TIMESTEP_EVAL>( this
-                                     , &VelRhoHatBC::eval<UINTAH_CPU_TAG>     // Task supports non-Kokkos builds
-                                     , &VelRhoHatBC::eval<KOKKOS_OPENMP_TAG>  // Task supports Kokkos::OpenMP builds
-                                     , &VelRhoHatBC::eval<KOKKOS_CUDA_TAG>    // Task supports Kokkos::Cuda builds
+                                     , &VelRhoHatBC::eval<UINTAH_CPU_TAG>               // Task supports non-Kokkos builds
+                                     , &VelRhoHatBC::eval<KOKKOS_OPENMP_TAG>            // Task supports Kokkos::OpenMP builds
+                                     //, &VelRhoHatBC::eval<KOKKOS_DEFAULT_HOST_TAG>    // Task supports Kokkos::DefaultHostExecutionSpace builds
+                                     //, &VelRhoHatBC::eval<KOKKOS_DEFAULT_DEVICE_TAG>  // Task supports Kokkos::DefaultExecutionSpace builds
+                                     , &VelRhoHatBC::eval<KOKKOS_CUDA_TAG>              // Task supports Kokkos::Cuda builds
                                      );
 }
 
