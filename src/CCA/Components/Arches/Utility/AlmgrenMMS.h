cd255e7df17b8b4d145207f4e2f2536889a51e51
Marta Garcia Martinez <mgarcia@anl.gov>
2021-11-10 16:17:10 -0600

    Adding Kokkos generic tags as comments

--- src/CCA/Components/Arches/Utility/AlmgrenMMS.h
+++ src/CCA/Components/Arches/Utility/AlmgrenMMS.h
@@ -111,9 +111,11 @@ private:
   TaskAssignedExecutionSpace AlmgrenMMS<T>::loadTaskInitializeFunctionPointers()
   {
     return create_portable_arches_tasks<TaskInterface::INITIALIZE>( this
-                                       , &AlmgrenMMS<T>::initialize<UINTAH_CPU_TAG>     // Task supports non-Kokkos builds
-                                       //, &AlmgrenMMS<T>::initialize<KOKKOS_OPENMP_TAG>  // Task supports Kokkos::OpenMP builds
-                                       //, &AlmgrenMMS<T>::initialize<KOKKOS_CUDA_TAG>    // Task supports Kokkos::Cuda builds
+                                       , &AlmgrenMMS<T>::initialize<UINTAH_CPU_TAG>               // Task supports non-Kokkos builds
+                                       //, &AlmgrenMMS<T>::initialize<KOKKOS_OPENMP_TAG>          // Task supports Kokkos::OpenMP builds
+                                       //, &AlmgrenMMS<T>::initialize<KOKKOS_DEFAULT_HOST_TAG>    // Task supports Kokkos::DefaultHostExecutionSpace builds
+                                       //, &AlmgrenMMS<T>::initialize<KOKKOS_DEFAULT_DEVICE_TAG>  // Task supports Kokkos::DefaultExecutionSpace builds
+                                       //, &AlmgrenMMS<T>::initialize<KOKKOS_CUDA_TAG>            // Task supports Kokkos::Cuda builds
                                        );
   }
 
