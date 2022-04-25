67b63c43324ab183ab1ac8a8d8ed813f4080a19a
James Guilkey <guilkey@debian-gnu-linux-vm.localdomain>
2022-04-25 12:52:04 -0600

    Uncommenting one line from previous commit.

--- src/CCA/Components/MPM/Materials/Contact/SpecifiedBodyContact.cc
+++ src/CCA/Components/MPM/Materials/Contact/SpecifiedBodyContact.cc
@@ -391,7 +391,7 @@ void SpecifiedBodyContact::exMomIntegrated(const ProcessorGroup*,
 
         if (!compare(gmass[d_material][c], 0.)
         && (totalNodalVol/cell_vol) > d_vol_const){
-          //Vector old_vel = gvelocity_star[n][c];
+          Vector old_vel = gvelocity_star[n][c];
           gvelocity_star[n][c] =  new_vel;
           //reaction_force += gmass[n][c]*(new_vel-old_vel)/delT;
           reaction_force[n]  -= ginternalForce[n][c];
