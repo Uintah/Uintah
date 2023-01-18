4e55bb6d8f9587b091a44c9f981b28aa68c163ad
ARSanderson <allen@sci.utah.edu>
2023-01-18 13:35:28 -0700

    changed to HAVE_KOKKOS and added HAVE_GPU for Kokkos only builds

--- src/CCA/Components/Schedulers/sub.mk
+++ src/CCA/Components/Schedulers/sub.mk
@@ -2,17 +2,17 @@
 #  The MIT License
 #
 #  Copyright (c) 1997-2020 The University of Utah
-# 
+#
 #  Permission is hereby granted, free of charge, to any person obtaining a copy
 #  of this software and associated documentation files (the "Software"), to
 #  deal in the Software without restriction, including without limitation the
 #  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
 #  sell copies of the Software, and to permit persons to whom the Software is
 #  furnished to do so, subject to the following conditions:
-# 
+#
 #  The above copyright notice and this permission notice shall be included in
 #  all copies or substantial portions of the Software.
-# 
+#
 #  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 #  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 #  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -20,9 +20,9 @@
 #  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 #  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 #  IN THE SOFTWARE.
-# 
-# 
-# Makefile fragment for this subdirectory 
+#
+#
+# Makefile fragment for this subdirectory
 
 include $(SCIRUN_SCRIPTS)/smallso_prologue.mk
 
@@ -48,12 +48,12 @@ SRCS += \
         $(SRCDIR)/TaskGraph.cc                \
         $(SRCDIR)/UnifiedScheduler.cc
 
-ifeq ($(HAVE_CUDA),yes)
+ifeq ($(HAVE_GPU),yes)
   SRCS += $(SRCDIR)/GPUDataWarehouse.cu       \
           $(SRCDIR)/GPUGridVariableInfo.cc    \
           $(SRCDIR)/GPUGridVariableGhosts.cc  \
           $(SRCDIR)/GPUMemoryPool.cc
-          
+
   DLINK_FILES += CCA/Components/Schedulers/GPUDataWarehouse.o
 endif
 
@@ -74,4 +74,3 @@ PSELIBS := \
 LIBS := $(XML2_LIBRARY) $(MPI_LIBRARY) $(CUDA_LIBRARY)
 
 include $(SCIRUN_SCRIPTS)/smallso_epilogue.mk
-
