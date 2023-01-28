089cdb5915786fd215f929f4ca9c9d4c39ee2d26
ARSanderson <allen@sci.utah.edu>
2023-01-27 17:11:36 -0700

    removed unneeded cuda

--- src/CCA/Components/Models/Radiation/RMCRT/RayKokkos.h
+++ src/CCA/Components/Models/Radiation/RMCRT/RayKokkos.h
@@ -37,13 +37,7 @@
 #include <Core/Grid/Variables/CCVariable.h>
 
 #include <sci_defs/uintah_defs.h>
-#include <sci_defs/cuda_defs.h>
 #include <sci_defs/kokkos_defs.h>
-
-#if defined(HAVE_KOKKOS_GPU)
-  #include <curand.h>
-  #include <curand_kernel.h>
-#endif
 
 #include <iostream>
 #include <cmath>
