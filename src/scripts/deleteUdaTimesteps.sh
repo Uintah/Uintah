ace8ee5deed8affd0e1a0889b22bcfa331f3adc0
Todd Harman <t.harman@utah.edu>
2023-02-28 11:21:17 -0700

    The user can select to delete the timestep directories immediately insteady of moving them to /tmp/ and deleting them in 3 days.
    Changed options:
    ______________________________________________________________________
    
    DESCRIPTION
     This script deletes timesteps directories from an uda.
     The timestep directories are moved to a temporary directory that will be deleted
     3 days after the script is executed.  When deleting a large number of timesteps
     use the -n, --now option to avoid filling /tmp directory
    
    Usage:
      1) Edit the uda/index.xml file and remove all timestep directories to delete
    
      2) Run:
           deleteUdaTimesteps.sh [options] ... [index.xml]
    
    Options:
        -h, --help     display usage
        -n, --now      delete all directories now
    
    Dependencies:  at, xmlstarlet
    ______________________________________________________________________

--- src/scripts/deleteUdaTimesteps.sh
+++ src/scripts/deleteUdaTimesteps.sh
@@ -24,118 +24,188 @@
 #/
 source "$(dirname $0)/bashFunctions"    # pull in common functions
 
-#______________________________________________________________________
-# 
-#______________________________________________________________________
-
-if [[ $# -eq 0 ]] ; then
-  echo
+usage()
+{
+  echo "______________________________________________________________________"
+  echo 
   echo "DESCRIPTION"
   echo " This script deletes timesteps directories from an uda."
   echo " The timestep directories are moved to a temporary directory that will be deleted"  
-  echo " 3 days after the script is executed."
+  echo " 3 days after the script is executed.  When deleting a large number of timesteps "
+  echo " use the -n, --now option to avoid filling /tmp directory"
   echo
   echo "Usage:"
-  echo "  1) Edit the index.xml file and remove all timestep directories to delete"
+  echo "  1) Edit the uda/index.xml file and remove all timestep directories to delete"
   echo 
   echo "  2) Run:"
-  echo "       deleteUdaTimesteps.sh < index.xml>"
-  echo
+  echo "       deleteUdaTimesteps.sh [options] ... [index.xml]"
+  echo ""
+  echo "Options:"
+  echo "    -h, --help     display usage"
+  echo "    -n, --now      delete all directories now"
+  echo ""
   echo "Dependencies:  at, xmlstarlet"
+  echo "______________________________________________________________________"
   exit
-fi
-
-index_xml="$1"
+}
 
 
-#__________________________________
+#______________________________________________________________________
 # 
-TMP="/tmp"                                       # temporary directory
-declare -x rmDirDelay="+3days"                   # Time delay before tmpDir will be deleted
+#______________________________________________________________________
+main()
+{
+  #__________________________________
+  #  defaults
+  TMP="/tmp"                                       # temporary directory
+  declare -x rmDirDelay="+3days"                   # Time delay before tmpDir will be deleted
+  
+  #__________________________________
+  # parse arguments
+  options=$( getopt --name "deleteUdaTimesteps.sh" --options="h,n"  --longoptions=help,now -- "$@" )
 
-#__________________________________
-# bulletproofing
-if [ ! -e "${index_xml}" ]; then
-  end_die "The file (${index_xml}) does not exist."
-fi
+  if [[ $? -ne 0 || $# -eq 0 ]] ; then
+    echo "Incorrect option provided"
+    usage
+    exit 1
+  fi
+  
+  # set is to preserve white spaces and punctuation
+  eval set -- "$options"
 
-commandExists xmlstarlet
-commandExists atq
-
-#__________________________________
-#  - find the uda name
-#  - create the tmp directory
-
-fullpath=$(realpath "$index_xml" | xargs dirname)
-
-uda=$( echo "$fullpath" | rev | cut --delimit "/" --field 1 | rev )
-
-tmpDir="$(mktemp -d $TMP/$uda-XXX)"
-
-if [[ "$?" != "0" ]]; then
-  end_die "\n Error: could not create the temporary directory($tmpDir)"
-fi
-
-
-#__________________________________
-#  Parse the index.xml file for the timestep directories
-
-keepDirs=$(xmlstarlet sel --text --template --value-of "/Uintah_DataArchive/timesteps/timestep/@href" "$index_xml" ) 
-
-echo $keepDirs | xargs dirname > "$tmpDir/keepDirs"
-
-#__________________________________
-# find all timestep directories in the uda
-
-cd "$fullpath"
-
-find ./t* -maxdepth 0 -type d  -exec basename {} \; > "$tmpDir/allDirs"
-
-if [[ "$?" != "0" ]]; then
-  end_die "\n Error: did not find any timestep directories in $uda"
-fi
-
-#__________________________________
-# find the directories to delete.  This is the difference of allDirs vs keepDirs
-
-declare -a deleteDirs=( $( diff  --changed-group-format='%>' --unchanged-group-format='' "$tmpDir/keepDirs"  "$tmpDir/allDirs" | tr  "\n" " ") )
-
-
-if [[ ${#deleteDirs[@]} -eq 0 ]]; then
-  end_die "Error:  No directories will be deleted.  Make sure you've edited the index.xml file and remove timestep directories."
-fi
-
-
-#__________________________________
-#   Move the directories to the tmpdir
-echo "Proceed with moving the timestep directories to $tmpDir and then delete that directory $rmDirDelay?"
-
-for f in ${deleteDirs[@]}; do
-  echo "mv -r $f $tmpDir"
-done
-
-
-while true; do
-    read -p "Y/N  " ans
-    case $ans in
-      
-      [Yy]* ) 
-      
-        for f in ${deleteDirs[@]}; do
-          echo "cp -r $f $tmpDir"
-          cp -r "$f" "$tmpDir"
-        done
-      
-        cleanUp "${tmpDir}" ${rmDirDelay}
-        break;;
-        
-      [Nn]* ) 
-        exit;;
-      * ) echo "Please answer Y or N.";;
+  while true ; do
+    case "$1" in
+      -n|--now)
+        rmDirDelay="now"
+        ;;
+      -h|--help)
+        usage
+        ;;
+      --)
+        shift
+        break
+        ;;
     esac
-done
+    shift
+  done
+  
+  index_xml="$1"
 
-echo "If you've made a mistake you can simply move the directories from $tmpDir/ back to $uda/"
+  echo "__________________________________"
+  echo "  Time delay before timesteps are removed:  $rmDirDelay"
+  echo "  index.xml:                                $index_xml"
+  echo "__________________________________"
+  echo ""
 
-exit
+  #__________________________________
+  # bulletproofing
+  if [ ! -e "${index_xml}" ]; then
+    end_die "Couldn't find the index.xml file (${index_xml})."
+  fi
+
+  commandExists xmlstarlet
+  commandExists atq
+
+  #__________________________________
+  #  - find the uda name
+  #  - create the tmp directory
+
+  fullpath=$(realpath "$index_xml" | xargs dirname)
+
+  uda=$( echo "$fullpath" | rev | cut --delimit "/" --field 1 | rev )
+
+  tmpDir="$(mktemp -d $TMP/$uda-XXX)"
+
+  if [[ "$?" != "0" ]]; then
+    end_die "\n Error: could not create the temporary directory($tmpDir)"
+  fi
+
+
+  #__________________________________
+  #  Parse the index.xml file for the timestep directories
+
+  keepDirs=$(xmlstarlet sel --text --template --value-of "/Uintah_DataArchive/timesteps/timestep/@href" "$index_xml" ) 
+
+  echo $keepDirs | xargs dirname > "$tmpDir/keepDirs"
+
+  #__________________________________
+  # find all timestep directories in the uda
+
+  cd "$fullpath"
+
+  find ./t* -maxdepth 0 -type d  -exec basename {} \; > "$tmpDir/allDirs"
+
+  sort --version-sort  "$tmpDir/allDirs" --output "$tmpDir/allDirs"
+
+  if [[ "$?" != "0" ]]; then
+    end_die "\n Error: did not find any timestep directories in $uda"
+  fi
+
+  #__________________________________
+  # find the directories to delete.  This is the difference of allDirs vs keepDirs
+
+  declare -a deleteDirs=( $( diff  --changed-group-format='%>' --unchanged-group-format='' "$tmpDir/keepDirs"  "$tmpDir/allDirs" | tr  "\n" " ") )
+
+
+  if [[ ${#deleteDirs[@]} -eq 0 ]]; then
+    end_die "Error:  No directories will be deleted.  Make sure you've edited the index.xml file and remove timestep directories."
+  fi
+
+
+  #__________________________________
+  #   Move the directories to the tmpdir or delete them
+  if [[ "$rmDirDelay" == "now" ]]; then
+    echo "Proceed with deleting the timestep directories immediately?" 
+  else
+    echo "Proceed with moving the timestep directories to $tmpDir and then delete that directory $rmDirDelay?"
+  fi
+
+  for f in "${deleteDirs[@]}"; do
+    
+    if [[ "$rmDirDelay" == "now" ]]; then
+      echo "   /bin/rm -rf $f"
+    else
+      echo "   mv -r $f $tmpDir"
+    fi
+  done
+
+
+  while true; do
+      read -p "Y/N  " ans
+      case $ans in
+
+        [Yy]* ) 
+          
+          if [[ "$rmDirDelay" != "now" ]]; then
+            for f in "${deleteDirs[@]}"; do
+              echo "   mv -r $f $tmpDir"
+              mv -r "$f" "$tmpDir"
+            done
+          fi
+          
+          if [[ "$rmDirDelay" == "now" ]]; then
+            for f in "${deleteDirs[@]}"; do
+              echo "   /bin/rm -rf $f"
+              /bin/rm -rf "$f"
+            done
+          fi          
+          
+          cleanUp "${tmpDir}" ${rmDirDelay}
+          break;;
+
+        [Nn]* ) 
+          exit;;
+        * ) echo "Please answer Y or N.";;
+      esac
+  done
+
+  if [[ "$rmDirDelay" != "now" ]]; then
+    echo "If you've made a mistake you can simply move the directories from $tmpDir/ back to $uda/"
+  fi
+  exit
+}
+#______________________________________________________________________
+#______________________________________________________________________
+
+main "$@"
 
