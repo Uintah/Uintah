1232f3bdb1c2723edb4bab91d4fd56fdb476745b
Todd Harman <t.harman@utah.edu>
2023-07-27 11:16:57 -0600

    Added instructions for installing dependencies
    Write out the patch configurations in a bash dictionary so it's easy to incorporate
    into the scaling study scripts.

--- src/scripts/findOptimalPatchLayout.m
+++ src/scripts/findOptimalPatchLayout.m
@@ -22,7 +22,7 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 # IN THE SOFTWARE.
 #______________________________________________________________________
- 
+
 
 pkg load symbolic
 clear all
@@ -36,6 +36,16 @@ clear all
 #  of nodes that will be used.
 #
 #  - For production runs nTotalCells and a range of nodes to test
+#
+#    The dependencies are Octave, Python, and SymPy. Consult the SymPy website for details on how to install SymPy.
+#
+#    Start Octave.
+#
+#    At Octave prompt type pkg install -forge symbolic.
+#
+#    At Octave prompt, type pkg load symbolic.
+#
+#    At Octave prompt, type syms x, then f = (sin(x/2))^3, diff(f, x), etc.
 #______________________________________________________________________
 
 #______________________________________________________________________
@@ -118,7 +128,7 @@ endfunction
 function [out] = intToStr(size)
   suffixes = ['', 'K', 'M', 'G', 'T'];
   base     = log( size ) / log( 1000 );
-   
+
   unit = suffixes( floor(base) );
   me   = power(1000, base - floor(base) );
   out  = sprintf( "%3.2f %s", me, unit );
@@ -128,16 +138,16 @@ endfunction
 #   MAIN
 #______________________________________________________________________
 
-coresPerNode    = 56;                 # number of cores per node
+coresPerNode    = 20;                 # number of cores per node
 targetPatchSize = [20,20,20];         # target number of cells per patch
 analysisType    = "scaling"           # scaling or rangeOfNodes
 
 #__________________________________
 #   Inputs for find
 if( strcmp( analysisType, "rangeOfNodes") )
-  minNodes    = 180;                          # minimum number of nodes
-  maxNodes    = 200;                          # maximum number of nodes
-  nTotalCells = [[1024,648,512]];                                                          
+  minNodes    = 1;                          # minimum number of nodes
+  maxNodes    = 16;                          # maximum number of nodes
+  nTotalCells = [[1024,648,512]];
 end
 
 
@@ -176,6 +186,8 @@ s_nTotalCells   = intToStr(prod(nTotalCells) )
 #______________________________________________________________________
 # examine user defined nodes for the optimal patch layout over an array of
 # nodes
+string="declare -A patches=(";
+
 
 if( strcmp( analysisType, "scaling" ) )
   printf( "__________________________________  \n")
@@ -194,7 +206,11 @@ if( strcmp( analysisType, "scaling" ) )
     patchSize  = round( nTotalCells./patches );
 
     printf( "#  nodes %5i cores  %5i  Patches: [%i,%i,%i]  nPatches %5i cellsPerPatch [ %i,%i,%i ] diff: %f\n", n, cores, patches, nPatches, patchSize, diff   )
+
+    patch=sprintf(  " [\"%i\"]=\"[%i,%i,%i]\"",n,patches);
+    string=strcat(string, patch  );
   end
+
 end
 
 #______________________________________________________________________
@@ -223,10 +239,14 @@ if( strcmp( analysisType, "rangeOfNodes") )
 
     printf( "#  nodes %5i cores  %5i  Patches: [%i,%i,%i]  nPatches %5i cellsPerPatch [ %i,%i,%i ] diff: %f\n", nodes(i), cores(i), patches(i,:), nPatches(i), patchSize(i,:), diff(i)   )
 
+    patch=sprintf(  " [\"%i\"]=\"[%i,%i,%i]\"",n,patches);
+    string=strcat(string, patch  );
+
   end
   [minVal, idx] = min( diff )
   printf( "__________________________________ optimal number of nodes\n")
   printf( "#  nodes %5i cores  %5i  Patches: [%i,%i,%i]  nPatches %5i cellsPerPatch [ %i,%i,%i ] diff: %f\n", nodes(idx), cores(idx), patches(idx,:), nPatches(idx), patchSize(idx,:), diff(idx)   )
 end
 
+printf("%s)",string)
 printf( " here" )
