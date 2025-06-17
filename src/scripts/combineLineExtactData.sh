89331e6495eefd3fa0d1b43e7b27e167e6ea6da4
Todd Harman <t.harman@utah.edu>
2025-06-17 11:17:18 -0600

    lineExtract: Added ability to output
    CCVariable<Matrix3>
    NCVariable<Matrix3>
    
    Updated script so the filename is the timestep.

--- src/scripts/combineLineExtactData.sh
+++ src/scripts/combineLineExtactData.sh
@@ -32,64 +32,68 @@ source "${helpersPath}"/bashFunctions
 #
 #   This script is used in conjunction with on-the-fly data analysis module
 #       <DataAnalysis>
-#           <Module name="lineExtract">
-#             <material>Atmosphere</material>
-#             <samplingFrequency> 1e10 </samplingFrequency>
-#             <timeStart>          0   </timeStart>
-#             <timeStop>          100  </timeStop>
+#               <Module name="lineExtract">
 #
-#             <Variables>
-#               <analyze label="press_CC" matl="0"/>
-#               <analyze label="rho_CC"/>
-#               <analyze label="temp_CC"/>
-#               <analyze label="delP_Dilatate"/>
-#             </Variables>
+#                 <material>           atmosphere  </material>
+#                 <samplingFrequency>  1000        </samplingFrequency>
+#                 <timeStart>          0.000       </timeStart>
+#                 <timeStop>           10000       </timeStop>
 #
-#             <lines>
-#               <line name="X_line">
-#                      <startingPt>  [0.0, 0.5, 0.5]   </startingPt>
-#                      <endingPt>    [1.0, 0.5, 0.5]   </endingPt>
+#                 <Variables>
+#                   <analyze label="g.mass"     matl="0"/>
+#                   <analyze label="g.velocity" matl="0"/>
+#                   <analyze label="g.stressFS" matl="0"/>
+#                 </Variables>
+#
+#                 <lines>
+#                   <line name="X_line">
+#                      <startingPt>  [0.0, 1.25, 1.25]   </startingPt>
+#                      <endingPt>    [2.5, 1.25, 1.25]   </endingPt>
 #                      <stepSize> 0.1 </stepSize>
-#                </line>
-#            </Module>
-#       </DataAnalysis>
+#                    </line>
+#                    <snip>
+#                   </lines>
+#                 </Module>
+#         </DataAnalysis>
 #
 #   The script glues together all data files in a given line for each timestep.
 #   It assumes that following directory structure exists:
 #
-#    X_line/                        < Extracted line
-#    |-- L-0                        <level
-#    |   |-- i0_j20_k20             < cell indice
-#    |   |-- i12_j20_k20
-#    |   |-- i16_j20_k20
-#    |   |-- i20_j20_k20
-#    |   |-- i24_j20_k20
-#    |   |-- i28_j20_k20
-#    |   |-- i32_j20_k20
-#    |   |-- i36_j20_k20
-#    |   |-- i4_j20_k20
-#    |   `-- i8_j20_k20
-#    `-- L-1                        < optional level
-#        |-- i20_j40_k40
-#        |-- i28_j40_k40
-#        |-- i30_j40_k40
-#        |-- i38_j40_k40
-#        |-- i40_j40_k40
-#        |-- i48_j40_k40
-#        |-- i50_j40_k40
-#        `-- i58_j40_k40
-#
+#    X_line/
+#    `-- L-0
+#        |-- i0_j25_k25
+#        |-- i10_j25_k25
+#        |-- i12_j25_k25
+#        |-- i14_j25_k25
+#        |-- i16_j25_k25
+#        |-- i18_j25_k25
+#        |-- i20_j25_k25
+#        |-- i22_j25_k25
+#        |-- i24_j25_k25
+#        |-- i25_j25_k25
+#        |-- i27_j25_k25
+
 #
 #  Each file has the format of
 #
-#   X_CC           Y_CC             Z_CC             Time [s]        press_CC_0      rho_CC_0        temp_CC_0       delP_Dilatate_0
-#   1.250000E-02     5.125000E-01     5.125000E-01     2.701815E-05    1.013250E+05    1.789991E+00    3.000000E+02    -0.000000E+00
-#   1.250000E-02     5.125000E-01     5.125000E-01     5.403629E-05    1.013250E+05    1.789991E+00    3.000000E+02    -0.000000E+00
-#   1.250000E-02     5.125000E-01     5.125000E-01     8.105444E-05    1.013250E+05    1.789991E+00    3.000000E+02    -0.000000E+00
+#
+# X_NC           Y_NC             Z_NC             Timestep  Time [s]        g.mass_0        g.velocity_0.x   g.velocity_0.y   g.velocity_0.z  g.stressFS_0(0,0)    g.stressFS_0(0,1)   <snip>
+#  0.000000E+00     1.250000E+00     1.250000E+00     1       2.101867E-05    1.000000E-200   0.000000E+00     0.000000E+00     0.000000E+00    0.000000E+00         0.000000E+00
+#  0.000000E+00     1.250000E+00     1.250000E+00     49      1.029915E-03    1.000000E-200   0.000000E+00     0.000000E+00     0.000000E+00    0.000000E+00         0.000000E+00
+#  0.000000E+00     1.250000E+00     1.250000E+00     97      2.038811E-03    1.000000E-200   0.000000E+00     0.000000E+00     0.000000E+00    0.000000E+00         0.000000E+00
 #   <snip>
 #
-#   This script generates a new directory inside each level
+#   This script generates a new directory (timesteps) inside each level
 #   #______________________________________________________________________
+# X_line/
+# `-- L-0
+#     `-- timesteps
+#         |-- t00001
+#         |-- t00049
+#         `-- t00097
+#
+#
+
 #      X_line/
 #      |-- L-0
 #      |   `-- timesteps
@@ -105,21 +109,14 @@ source "${helpersPath}"/bashFunctions
 #              |-- t1.621089E-04
 #              |-- t1.891270E-04
 #              <snip>
-# 
-#  Where X_line/L-0/timesteps/t1.080726E-4
-# X_CC           Y_CC             Z_CC             Time [s]        press_CC_0      rho_CC_0        temp_CC_0       delP_Dilatate_0 
-#  1.250000E-02     5.125000E-01     5.125000E-01     1.080726E-04    1.013250E+05    1.789991E+00    3.000000E+02    -0.000000E+00   
-#  1.125000E-01     5.125000E-01     5.125000E-01     1.080726E-04    1.013250E+05    1.789991E+00    3.000000E+02    -0.000000E+00   
-#  2.125000E-01     5.125000E-01     5.125000E-01     1.080726E-04    1.013250E+05    1.789991E+00    3.000000E+02    5.310202E-12    
-#  3.125000E-01     5.125000E-01     5.125000E-01     1.080726E-04    1.013250E+05    1.789991E+00    3.000000E+02    -0.000000E+00   
-#  4.125000E-01     5.125000E-01     5.125000E-01     1.080726E-04    1.013250E+05    7.711993E-01    3.000000E+02    -3.625098E-12   
-#  5.125000E-01     5.125000E-01     5.125000E-01     1.080726E-04    1.013250E+05    1.789991E-12    3.000000E+02    -0.000000E+00   
-#  6.125000E-01     5.125000E-01     5.125000E-01     1.080726E-04    1.013250E+05    1.018791E+00    3.000000E+02    -7.250196E-12   
-#  7.125000E-01     5.125000E-01     5.125000E-01     1.080726E-04    1.013250E+05    1.789991E+00    3.000000E+02    1.770067E-12    
-#  8.125000E-01     5.125000E-01     5.125000E-01     1.080726E-04    1.013250E+05    1.789991E+00    3.000000E+02    -0.000000E+00   
-#  9.125000E-01     5.125000E-01     5.125000E-01     1.080726E-04    1.013250E+05    1.789991E+00    3.000000E+02    -0.000000E+00 
-# 
-# 
+#
+#  Where X_line/L-0/timesteps/t00097
+#  X_NC           Y_NC             Z_NC             Timestep  Time [s]        g.mass_0        g.velocity_0.x   g.velocity_0.y   g.velocity_0.z  g.stressFS_0(0,0)    g.stressFS_0(0,1)  <snip>
+# 0.000000E+00     1.250000E+00     1.250000E+00     97        2.038811E-03    1.000000E-200   0.000000E+00     0.000000E+00     0.000000E+00    0.000000E+00         0.000000E+00
+# 1.000000E-01     1.250000E+00     1.250000E+00     97        2.038811E-03    1.000000E-200   0.000000E+00     0.000000E+00     0.000000E+00    0.000000E+00         0.000000E+00
+# 2.000000E-01     1.250000E+00     1.250000E+00     97        2.038811E-03    1.000000E-200   0.000000E+00     0.000000E+00     0.000000E+00    0.000000E+00         0.000000E+00
+# 3.000000E-01     1.250000E+00     1.250000E+00     97        2.038811E-03    1.000000E-200   0.000000E+00     0.000000E+00     0.000000E+00    0.000000E+00         0.000000E+00
+
 #______________________________________________________________________
 
 usage()
@@ -129,9 +126,9 @@ usage()
   echo "DESCRIPTION"
   echo " This scripts creates a series of files, one for each timestep output"
   echo " and each file contains the columns"
-  echo "#   X_CC           Y_CC             Z_CC             Time [s]         <Var1>          <Var2>        ...."
-  echo "   1.250000E-02     5.125000E-01     5.125000E-01     2.701815E-05    1.013250E+05    1.789991E+00  ...."
-  echo "   1.250000E-02     5.125000E-01     5.125000E-01     5.403629E-05    1.013250E+05    1.789991E+00  ...."
+  echo "#   X_CC           Y_CC             Z_CC           Timestep    Time [s]         <Var1>          <Var2>        ...."
+  echo "   1.250000E-02     5.125000E-01     5.125000E-01  1           2.701815E-05    1.013250E+05    1.789991E+00  ...."
+  echo "   1.250000E-02     5.125000E-01     5.125000E-01  97          5.403629E-05    1.013250E+05    1.789991E+00  ...."
   echo ""
   echo " These files can then be used to plot profiles of the quantities of interest."
   echo ""
@@ -150,10 +147,12 @@ usage()
 #______________________________________________________________________
 main()
 {
+  #  Defaults
+  declare -i width=5     #"used for filename format t<width> timestep"
+
   #__________________________________
   # parse arguments
   options=$( getopt --name "combineLineExtractData.sh" --options="h,d:"  --longoptions=help,dir: -- "$@" )
-
 
 
   if [[ $? -ne 0 || $# -eq 0 ]] ; then
@@ -182,6 +181,8 @@ main()
     shift
   done
 
+  #__________________________________
+  #  create the sorting order which depends on the line direction
   case "$dir" in
     X)
       sortOrder=(-k1,1g -k2,2g -k3,3g)
@@ -221,26 +222,35 @@ main()
 
     #    extract the header
     header="timesteps/header"
-    head -1 "${cellFiles[1]}" > "$header"
+    grep ^# "${cellFiles[1]}" > "$header"
 
-    #   find all the timesteps in the cell file
-    mapfile -t timesteps < <(sed  -e "1d" "${cellFiles[1]}" | \
+
+    #   create an array of all the timesteps in the cell file
+    mapfile -t timesteps < <(sed  '/^#/d' "${cellFiles[1]}" | \
                              tr --squeeze-repeats ' '| \
                              cut -d ' ' -f4 )
 
+    #   create an array of all the physical times in the cell file
+    mapfile -t phyTimes < <(sed  '/^#/d' "${cellFiles[1]}" | \
+                             tr --squeeze-repeats ' '| \
+                             cut -d ' ' -f5 )
+
     #__________________________________
-    #     Loop over the timesteps and 
+    #     Loop over the timesteps and
     #     create a file with a line of data
-    for timestep in "${timesteps[@]}"; do
+    for ((t=0; t < "${#timesteps[@]}"; t++)); do
 
-      ts="timesteps/t$timestep"
-      cp "$header" "$ts"
+      printf -v padded_ts "timesteps/t%0${width}d" "${timesteps[t]}"
 
-      grep -h "$timestep"  "${cellFiles[@]}" >> "$ts"
-      echo "    Working on timestep $ts"
 
-      # sort the data by $sortOrder
-      sort "${sortOrder[@]}" "$ts" -o "$ts"
+      cp "$header" "$padded_ts"
+
+      #       search all the files for the physical time, don't use the timestep:
+      grep -h "${phyTimes[t]}"  "${cellFiles[@]}" >> "$padded_ts"
+      echo "    Working on timestep $padded_ts"
+
+      #       sort the data by $sortOrder
+      sort "${sortOrder[@]}" "$padded_ts" -o "$padded_ts"
 
     done
     #     cleanup
