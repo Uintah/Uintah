32ad1d8b00c51c60c8ed303a9f9f548eb8cb13f4
Todd Harman <t.harman@utah.edu>
2026-01-13 16:01:18 -0700

    updated copyright date.

--- src/CCA/Components/Wasatch/Expressions/MMS/sub.mk
+++ src/CCA/Components/Wasatch/Expressions/MMS/sub.mk
@@ -1,7 +1,7 @@
 #
 #  The MIT License
 #
-#  Copyright (c) 2010-2024 The University of Utah
+#  Copyright (c) 2010-2026 The University of Utah
 #
 #  Permission is hereby granted, free of charge, to any person obtaining a copy
 #  of this software and associated documentation files (the "Software"), to
